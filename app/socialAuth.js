f4cd9a14357b6f3c1c43cdc5a0ba7d9135e01e07
Chanaka Sandaruwan <chanakasan@gmail.com>
2017-03-03 05:55:57 +0530

    create socialAuth.js

---
+++ app/socialAuth.js
@@ -0,0 +1,31 @@
+import hello from 'hellojs';
+
+hello.init({
+  facebook: 1759232984393029,
+});
+
+
+const socialAuth = (network) => ({
+  login: () =>
+    hello(network).login({ force: true }).then(() => {
+      console.log('[DEBUG] logged in'); // eslint-disable-line
+    }, (e) => {
+      console.log('[DEBUG] login error: ' + e.error.message); // eslint-disable-line
+    }),
+
+  logout: () =>
+    hello(network).logout().then(() => {
+      console.log('[DEBUG] logged out'); // eslint-disable-line
+    }, (e) => {
+      console.log('[DEBUG] logout error: ' + e.error.message); // eslint-disable-line
+    }),
+
+  isLoggedIn: () => {
+    const session = hello(network).getAuthResponse();
+    const currentTime = (new Date()).getTime() / 1000;
+    return session && session.access_token && session.expires > currentTime;
+  },
+
+});
+
+export default socialAuth;
