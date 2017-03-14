69ab994fbe044a70cee2f1d69fb4dc033e87e194
kogre <koen.gremmelprez@gmail.com>
2017-03-14 11:42:40 +0100

    Added fr and nl locales

--- app/i18n.js
+++ app/i18n.js
@@ -6,15 +6,23 @@
  */
 import { addLocaleData } from 'react-intl';
 import enLocaleData from 'react-intl/locale-data/en';
+import frLocaleData from 'react-intl/locale-data/fr';
+import nlLocaleData from 'react-intl/locale-data/nl';
 
 import { DEFAULT_LOCALE } from './containers/App/constants'; // eslint-disable-line
 import enTranslationMessages from './translations/en.json';
+import frTranslationMessages from './translations/fr.json';
+import nlTranslationMessages from './translations/nl.json';
 
 export const appLocales = [
   'en',
+  'fr',
+  'nl',
 ];
 
 addLocaleData(enLocaleData);
+addLocaleData(frLocaleData);
+addLocaleData(nlLocaleData);
 
 export const formatTranslationMessages = (locale, messages) => {
   const defaultFormattedMessages = locale !== DEFAULT_LOCALE
@@ -31,4 +39,6 @@ export const formatTranslationMessages = (locale, messages) => {
 
 export const translationMessages = {
   en: formatTranslationMessages('en', enTranslationMessages),
+  fr: formatTranslationMessages('fr', frTranslationMessages),
+  nl: formatTranslationMessages('nl', nlTranslationMessages),
 };
