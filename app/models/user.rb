3005a1eb021fe3d29ee2580a082d0cbbc7f02ac9
Chanaka Sandaruwan <chanakasan@gmail.com>
2017-03-14 11:10:59 +0530

    add avatar uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
+  mount_uploader :avatar, AvatarUploader
 
   validates :email, :slug, uniqueness: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :name, :slug, :email, presence: true
 
   before_validation :generate_slug
+
+  def avatar_blank?
+    avatar.file.nil?
+  end
 
   private
 
