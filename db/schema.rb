f483bb6fc10604406d515633185ce37c60a7fabc
Chanaka Sandaruwan <chanakasan@gmail.com>
2017-03-14 11:08:35 +0530

    add avatar field to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302155043) do
+ActiveRecord::Schema.define(version: 20170314053812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170302155043) do
     t.jsonb    "roles",           default: []
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["slug"], name: "index_users_on_slug", using: :btree
   end
