766cacaf3bd127204196deaf063ae7a59bd72fb0
Chanaka Sandaruwan <chanakasan@gmail.com>
2017-03-14 16:31:11 +0530

    store jwt in localStorage after social login

--- app/api.js
+++ app/api.js
@@ -1,6 +1,19 @@
 import request from 'utils/request';
 import { API_PATH } from 'containers/App/constants';
 
+export function socialLogin(network, accessToken) {
+  const data = {
+    auth: {
+      network,
+      access_token: accessToken,
+    },
+  };
+
+  return request(`${API_PATH}/social_login`, data, {
+    method: 'POST',
+  });
+}
+
 export function createUser(values) {
   return request(`${API_PATH}/users`, { user: values }, {
     method: 'POST',
@@ -14,4 +27,5 @@ export function fetchIdeas() {
 export default {
   createUser,
   fetchIdeas,
+  socialLogin,
 };
