9a4335d78a6f4980e62e0f1cab09ef560bb23816
Chanaka Sandaruwan <chanakasan@gmail.com>
2017-03-15 14:24:59 +0530

    add AWS dev credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,14 @@
 CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
+  config.fog_public     = true              # optional, defaults to true
+
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],     # required
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required
-    region:                ENV['AWS_REGION']             # optional, defaults to 'us-east-1'
+    aws_access_key_id:     '***REMOVED***',                        # required
+    aws_secret_access_key: '***REMOVED***,                        # required
+    region:                'eu-central-1',                  # optional, defaults to 'us-east-1'
   }
 
-  config.fog_directory  = ENV['AWS_BUCKET'] # required
-  config.fog_public     = true              # optional, defaults to true
+  config.fog_directory    = 'cl2-tenants-development'
 end
 
