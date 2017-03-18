3939bfae217f8a210b3b0d4fafc3947ea65d3451
Manuel Gajo <gajo.manuel@gmail.com>
2017-03-18 17:11:54 +0100

    CL2-27. restore logic to load and store draft

--- app/components/SubmitIdeaEditor/index.js
+++ app/components/SubmitIdeaEditor/index.js
@@ -1,42 +1,60 @@
-import React, { Component } from 'react';
-import { convertToRaw } from 'draft-js';
+import React, { PropTypes } from 'react';
+import { EditorState, convertToRaw, convertFromRaw } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
+// eslint-disable-next-line no-unused-vars
 import styles from 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
-export default class SubmitIdeaEditor extends Component {
-
+export default class SubmitIdeaEditor extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      editorContents: [],
+      editorState: EditorState.createEmpty(),
     };
   }
 
-  onEditorStateChange = (index, editorContent) => {
-    let editorContents = this.state.editorContents;
-    editorContents[index] = editorContent;
-    editorContents = [...editorContents];
-    this.setState({
-      editorContents,
-    });
-  };
+  componentDidMount() {
+    this.props.loadDraft();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.onEditorStateChange = (editorState) => {
+      this.setState({
+        editorState,
+      });
+
+      // store temp draft
+      nextProps.onEditorChange(convertToRaw(editorState.getCurrentContent()));
+    };
+
+    // load eventually existing draft
+    if (nextProps.content && this.state.editorState && !(this.state.editorState.getCurrentContent().equals(nextProps.content))) {
+      this.setState({
+        editorState: EditorState.createWithContent(convertFromRaw(nextProps.content)),
+      });
+    }
+  }
+
 
   render() {
-    const { editorContents } = this.state;
+    const { editorState } = this.state;
     return (
       <div className="demo-root">
         <div className="demo-editorSection">
           <Editor
             hashtag={{}}
-            editorState={editorContents[0]}
+            editorState={editorState}
             toolbarClassName="demo-toolbar"
             wrapperClassName="demo-wrapper"
             editorClassName="demo-editor"
-            onEditorStateChange={this.onEditorStateChange.bind(this, 0)}
+            onEditorStateChange={this.onEditorStateChange}
           />
         </div>
       </div>
     );
   }
 }
+
+SubmitIdeaEditor.propTypes = {
+  loadDraft: PropTypes.func.isRequired,
+};
