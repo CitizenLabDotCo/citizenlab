6776483bebcf34469f2634ad1c400c7d3a9c96e0
Manuel Gajo <gajo.manuel@gmail.com>
2017-03-21 15:13:42 +0100

    edito state test

--- app/components/SubmitIdeaEditor/tests/index.test.js
+++ app/components/SubmitIdeaEditor/tests/index.test.js
@@ -1,15 +1,5 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-
-// import SubmitIdeaForm from '../index';
-
 describe('<SubmitIdeaEditor />', () => {
   it('editor should render', () => {
-    // TODO
-    expect(true).toEqual(true);
-  });
-
-  it('when initial value was set, shouldn\'t reinitialize', () => {
     // TODO
     expect(true).toEqual(true);
   });
