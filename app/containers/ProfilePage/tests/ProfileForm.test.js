a9383b795891f40ae684a18332640ba4eccaca59
Manuel Gajo <gajo.manuel@gmail.com>
2017-03-21 14:41:29 +0100

    tests profile form - index

--- app/containers/ProfilePage/tests/ProfileForm.test.js
+++ app/containers/ProfilePage/tests/ProfileForm.test.js
@@ -1,23 +1,37 @@
-// import React from 'react';
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import { createComponentWithIntl, mountWithIntl } from '../../../utils/intlTest';
+import messages from '../messages';
+import ProfileForm from '../ProfileForm';
 import { mapDispatchToProps } from '../index';
 import { storeProfile } from '../actions';
-// import { mount } from 'enzyme';
-
-// import { mountWithIntl } from './intlHelper';
-// import ProfileForm from '../ProfileForm';
 
 describe('<ProfileForm />', () => {
+  it('calls onSubmit handler', () => {
+    const handleSubmitMock = jest.fn();
+    const wrapper = mountWithIntl(<ProfileForm onFormSubmit={handleSubmitMock} />);
+    wrapper.find('[type="submit"]').get(0).click();
+    expect(handleSubmitMock.mock.calls.length).toEqual(1);
+  });
+
   describe('onFormSubmit', () => {
-    it(' STORE_PROFILE action should be dispatched', () => {
-      // TODO
-      expect(true).toEqual(true);
+    it('STORE_PROFILE action should be dispatched', () => {
+      const dispatch = jest.fn();
+      const result = mapDispatchToProps(dispatch);
+      result.onProfileFormSubmit();
+      expect(dispatch).toHaveBeenCalledWith(storeProfile());
     });
   });
 
   describe('<LabelInputPair />', () => {
-    it ('should render correctly', () => {
-      // TODO [test with first name]
-      expect(true).toEqual(true);
+    it('input\'s label should render correctly', () => {
+      const id = 'firstName';
+      const tree = createComponentWithIntl(
+        <label htmlFor={id}>
+          <FormattedMessage {...messages[id]} />
+        </label>
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
     });
   });
 });
