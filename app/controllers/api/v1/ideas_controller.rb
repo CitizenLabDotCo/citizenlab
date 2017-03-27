eeb632396818b90473a92974a5c76b0f9963b2ef
Chanaka Sandaruwan <chanakasan@gmail.com>
2017-03-27 15:59:36 +0530

    accept base64 images

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::IdeasController < ApplicationController
-  # TODO: fix
-  # before_action :skip_authorization
-  # before_action :skip_policy_scope
+  UnsupportedImageError = Class.new(StandardError)
+
+  rescue_from UnsupportedImageError, with: :send_unsupported_image_error
+
   before_action :set_idea, only: [:show, :update, :destroy]
 
   def index
@@ -22,6 +23,8 @@ class Api::V1::IdeasController < ApplicationController
     else
       render json: { errors: @idea.errors }, status: :unprocessable_entity
     end
+  ensure
+    clean_tempfile
   end
 
   # patch
@@ -51,7 +54,7 @@ class Api::V1::IdeasController < ApplicationController
   end
 
   def idea_params
-    params.require(:idea).permit(
+    p = params.require(:idea).permit(
 			:publication_status,
 			:lab_id,
 			:author_id,
@@ -60,7 +63,57 @@ class Api::V1::IdeasController < ApplicationController
       images: [],
       files: []
     )
+
+    images = p.delete(:images)
+    parsed_images = []
+
+    images.to_a.each do |image|
+      parsed_images << parse_image(image)
+    end
+
+    p[:images] = parsed_images
+    p
   end
 
+  def parse_image(base64_image)
+    # data uri given?
+    if "data:" == base64_image[0..4]
+      base64_string = base64_image.sub(/^[^,]*/, "")
+      base64_string = base64_string.sub(/^,/, "")
+    else
+      base64_string = base64_image
+    end
 
+    @tempfile = Tempfile.new("idea_image")
+    @tempfile.binmode
+    @tempfile.write Base64.decode64(base64_string)
+    @tempfile.rewind
+
+    # Note: we are using external shell program to detect the mime type
+    content_type = `file --mime -b #{@tempfile.path}`.split(";")[0]
+    extension = content_type.match(/jpg|jpeg|gif|png/).to_s
+
+    raise UnsupportedImageError if extension.blank?
+
+    # generate a unique filename
+    unique_filename = SecureRandom.uuid
+    unique_filename += ".#{extension}"
+
+    ActionDispatch::Http::UploadedFile.new({
+      tempfile: @tempfile,
+      filename: unique_filename,
+      content_type: content_type,
+    })
+  end
+
+  def clean_tempfile
+    return unless @tempfile
+
+    @tempfile.close
+    @tempfile.unlink
+  end
+
+  def send_unsupported_image_error
+    render json: { message: "Image type must be one of jpg|jpeg|gif|png" }, status: 422
+  end
 end
