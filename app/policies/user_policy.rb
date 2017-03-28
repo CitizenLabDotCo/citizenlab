8c52aee288509d223e85227793a781b28c21f161
kogre <koen.gremmelprez@gmail.com>
2017-03-28 17:09:59 +0200

    Serialize email field for authenticated user only

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -27,4 +27,8 @@ class UserPolicy < ApplicationPolicy
   def update?
     true
   end
+
+  def view_private_attributes?
+    user && record.id == user.id
+  end
 end
--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,7 +1,15 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
+  include Knock::Authenticable
+
   attributes :id, :name, :slug, :avatar
+  attribute :email, if: :view_private_attributes?
+
+  def view_private_attributes?
+    Pundit.policy!(current_user, object).view_private_attributes?
+  end
 
   def avatar
     object.avatar.url
   end
+
 end
