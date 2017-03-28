e99da2015b1a3a04980308bc20a4521d12275927
Manuel Gajo <gajo.manuel@gmail.com>
2017-03-28 11:23:13 +0200

    CL2-76 attachment count test

--- app/containers/IdeasNewPage/tests/AttachmentList.test.js
+++ app/containers/IdeasNewPage/tests/AttachmentList.test.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import AttachmentList from '../AttachmentList';
+import { mount } from 'enzyme';
+import AttachmentList, { Attachments } from '../AttachmentList';
 import { mountWithIntl } from '../../../utils/intlTest';
 
 // import Attachment from '../index';
 
 describe('<AttachmentList />', () => {
-  const attachments = ["1", "2"];
+  const attachments = ['1', '2'];
   const jestFn = jest.fn();
-  const wrapper = mountWithIntl(
-    <AttachmentList
-      loadAttachments={jestFn}
-      storeAttachment={jestFn}
-      attachments={attachments}
-      storeAttachmentError={false}
-      loadAttachmentsError={false}
-    />
-  );
-
   it('should receive attachments prop', () => {
+    const wrapper = mountWithIntl(
+      <AttachmentList
+        loadAttachments={jestFn}
+        storeAttachment={jestFn}
+        attachments={attachments}
+        storeAttachmentError={false}
+        loadAttachmentsError={false}
+      />
+    );
+
     expect(wrapper.prop('attachments')).toEqual(attachments);
+  });
+
+  it('should render the right number of attachments', () => {
+    const wrapper = mount(
+      <Attachments attachments={attachments} />
+    );
+    expect(wrapper.find('Attachment')).toHaveLength(2);
   });
 });
