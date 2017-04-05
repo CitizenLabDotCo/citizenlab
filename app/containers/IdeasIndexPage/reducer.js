c18b418bab7df3edced72650ea0fe00a5a2fdd6a
Manuel Gajo <gajo.manuel@gmail.com>
2017-04-05 16:07:18 +0200

    page number parsing tests

--- app/containers/IdeasIndexPage/reducer.js
+++ app/containers/IdeasIndexPage/reducer.js
@@ -15,6 +15,16 @@ const initialState = fromJS({
   loading: false,
 });
 
+export function getPageNumberFromUrl(url) {
+  if (!url) return null;
+
+  const nextPageLink = decodeURI(url);
+
+  const nextPageStringIndex = nextPageLink.indexOf('page[number]=') + 13;
+  const result = nextPageLink.substr(nextPageLink.indexOf('page[number]=') + 13, nextPageLink.indexOf('&page') - nextPageStringIndex);
+  return (result < 0 ? null : parseInt(result, 10));
+}
+
 function ideasIndexPageReducer(state = initialState, action) {
   switch (action.type) {
     case LOAD_IDEAS_REQUEST:
@@ -22,14 +32,10 @@ function ideasIndexPageReducer(state = initialState, action) {
         .set('loading', true);
     case IDEAS_LOADED: {
       const ids = action.payload.data.map((idea) => idea.id);
-      const nextPageLink = decodeURI(action.payload.links.next);
-      const nextPageStringIndex = nextPageLink.indexOf('page[number]=') + 13;
+
       return state
         .set('ideas', fromJS(state.get('ideas')).concat(ids))
-        .set('nextPageNumber', nextPageLink
-          ? parseInt(nextPageLink.substr(nextPageLink.indexOf('page[number]=') + 13, nextPageLink.indexOf('&page') - nextPageStringIndex), 10)
-          : null
-        )
+        .set('nextPageNumber', getPageNumberFromUrl(action.payload.links.next))
         .set('loading', false);
     }
     default:
