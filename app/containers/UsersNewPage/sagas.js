0c82cac3c44ed71f552d578375e96b26a71d8dea
Manuel Gajo <gajo.manuel@gmail.com>
2017-04-12 13:07:59 +0200

    fix no redirect after registration

--- app/containers/UsersNewPage/sagas.js
+++ app/containers/UsersNewPage/sagas.js
@@ -6,13 +6,22 @@ import {
   CREATE_USER_REJECTED,
 } from './constants';
 import { authenticateRequest } from '../SignInPage/actions';
+import { AUTHENTICATE_REQUEST } from '../SignInPage/constants';
+import { fetchJwt } from '../SignInPage/sagas';
 
 
 export function* createUser(action) {
   try {
     const json = yield call(Api.createUser, action.payload); // eslint-disable-line
     yield put({ type: CREATE_USER_FULFILLED, payload: json });
-    yield put(authenticateRequest(json));
+
+    // automatically sign in by passing in username and password
+    const credentials = {
+      email: action.payload.email,
+      password: action.payload.password,
+    };
+
+    yield put(authenticateRequest(credentials));
   } catch (e) {
     yield put({ type: CREATE_USER_REJECTED, payload: e, error: true });
   }
@@ -22,6 +31,11 @@ function* watchCreateUser() {
   yield takeLatest(CREATE_USER_PENDING, createUser);
 }
 
+export function* watchUserSignIn() {
+  yield takeLatest(AUTHENTICATE_REQUEST, fetchJwt);
+}
+
 export default [
   watchCreateUser,
+  watchUserSignIn,
 ];
