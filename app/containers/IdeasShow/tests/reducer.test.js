36d0d4c9881a8590f83144342cccf419c9c5ef30
Manuel Gajo <gajo.manuel@gmail.com>
2017-04-18 17:59:55 +0200

    move generateResourcesCommentValue to separate file

--- app/containers/IdeasShow/tests/reducer.test.js
+++ app/containers/IdeasShow/tests/reducer.test.js
@@ -2,35 +2,7 @@ import { fromJS } from 'immutable';
 import ideasShowReducer from '../reducer';
 import { expectPropertyNotNull } from '../../../utils/testUtils';
 import { commentsLoaded, ideaLoadError, loadComments, resetIdeaAndComments } from '../actions';
-
-const generateResourcesCommentValue = (id) => ({
-  id,
-  attributes: {
-    body_multiloc: {
-      en: '<p>Voluptatem pariatur corporis ea assumenda qui. Ut accusamus alias delectus nobis autem soluta quo. Nihil tempore aut officiis esse. Cumque eum minima minus.</p><p>Error ut similique est enim non. Velit veniam dolore. Velit minus excepturi ut. Laborum doloribus ab provident iusto voluptatem sint et.</p><p>Odio aut minus aut qui dolorem repellendus. Omnis repellendus quisquam et id. Mollitia dolores et non et possimus.</p>',
-      nl: '<p>Voluptas eos sed dicta in in iusto. Quam voluptatem labore illo modi sit vitae. Quia aperiam fugit autem rem. Aliquam non provident dolor necessitatibus hic dolores reiciendis.</p><p>Ratione omnis qui quia provident molestias dolores. Necessitatibus sed quia facilis rerum nam. Aut ratione et id ad rerum enim autem. Doloribus harum soluta voluptatem dicta aliquid amet.</p><p>Dignissimos dicta adipisci exercitationem. Sint fuga minima. Consequatur vero ipsum ex fugiat quas temporibus voluptate.</p>',
-    },
-    created_at: '2017-04-13T08:14:34.183Z',
-    updated_at: '2017-04-14T15:34:41.536Z',
-  },
-  relationships: {
-    idea: {
-      data: {
-        id: 'c0b352b7-ccc0-4439-a277-78115799adab',
-        type: 'ideas',
-      },
-    },
-    author: {
-      data: {
-        id: '5f5f228f-fc43-4c4d-b5bb-cdab05237185',
-        type: 'users',
-      },
-    },
-    parent: {
-      data: null,
-    },
-  },
-});
+import { generateResourcesCommentValue } from './__shared.test';
 
 describe('ideasShowReducer', () => {
   const initialState = {
