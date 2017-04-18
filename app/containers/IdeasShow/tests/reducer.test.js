022ac1aaebe3b183671138f007225ad8ce1e83fd
Manuel Gajo <gajo.manuel@gmail.com>
2017-04-18 16:42:18 +0200

    fix failing test

--- app/containers/IdeasShow/tests/reducer.test.js
+++ app/containers/IdeasShow/tests/reducer.test.js
@@ -3,7 +3,14 @@ import { fromJS } from 'immutable';
 import ideasShowReducer from '../reducer';
 
 describe('ideasShowReducer', () => {
+  const expectedInitialState = {
+    idea: null,
+    upVotes: [],
+    downVotes: [],
+    ideaVotesLoadError: null,
+  };
+
   it('returns the initial state', () => {
-    expect(ideasShowReducer(undefined, {})).toEqual(fromJS({ idea: null }));
+    expect(ideasShowReducer(undefined, {})).toEqual(fromJS(expectedInitialState));
   });
 });
