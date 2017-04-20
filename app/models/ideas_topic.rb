0cc1ad027e8bb4c4c21a89bfb4f616685eac8623
kogre <koen.gremmelprez@gmail.com>
2017-04-20 11:40:38 +0200

    Added unique constraint between idea <> {topics, areas}

--- app/models/ideas_topic.rb
+++ app/models/ideas_topic.rb
@@ -3,4 +3,8 @@ class IdeasTopic < ApplicationRecord
   belongs_to :topic
 
   validates :idea, :topic, presence: true
+  # We would do this:
+  # validates :topic_id, uniqueness: {scope: :idea_id}
+  # but the uniqueness validation fails on records without primary key, so there's
+  # a database-level unique index instead
 end
