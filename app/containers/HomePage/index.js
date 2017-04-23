c438f1b5bc6196f4f4dd97d4f884a49a391cd55c
kogre <koen.gremmelprez@gmail.com>
2017-04-23 18:53:55 +0200

    Added WithFeature container. CL2-103 #done

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -18,6 +18,7 @@ import { makeSelectCurrentTenant } from 'utils/tenant/selectors';
 import TopicSelect from 'components/TopicSelect';
 import { Reveal } from 'components/Foundation';
 import T from 'containers/T';
+import WithFeature, { Without } from 'containers/WithFeature';
 import messages from './messages';
 
 const topics = [
@@ -72,6 +73,13 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
             My name is Reveal. I am a Foundation modal.
           </div>
         </Reveal>
+
+        <WithFeature feature="core">
+          <div>Commenting is allowed!</div>
+          <Without>
+            <div>Can't comment here :(!</div>
+          </Without>
+        </WithFeature>
       </div>
     );
   }
