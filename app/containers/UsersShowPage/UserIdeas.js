d29827aba51f9220266f497c0718ed76d831a4a3
Manuel Gajo <gajo.manuel@gmail.com>
2017-04-28 11:14:53 +0200

    convert UsersShowPage + test for Avatar display

--- app/containers/UsersShowPage/UserIdeas.js
+++ app/containers/UsersShowPage/UserIdeas.js
@@ -3,27 +3,37 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { FormattedMessage } from 'react-intl';
 import IdeaCard from 'components/IdeaCard';
-import { Row, Column } from 'components/Foundation';
+import { Label, Grid } from 'semantic-ui-react';
 
 import messages from './messages';
+
+// eslint-disable-next-line react/prop-types
+const UserIdeasWrapper = ({ userIdeas, goToIdea }) => (<Grid.Row>{userIdeas.map((userIdea, index) => (
+  <Grid.Column key={index}>
+    <IdeaCard
+      idea={userIdea}
+      onClick={() => goToIdea(userIdea.id)}
+    />
+  </Grid.Column>
+))}</Grid.Row>);
 
 function UserIdeas(props) {
   const { loadingUserIdeas, loadUserIdeasError, userIdeas, goToIdea } = props;
 
   return (
     <div className={props.className}>
-      {loadingUserIdeas && <FormattedMessage {...messages.loadingIdeas} />}
-      {loadUserIdeasError && <div>{loadUserIdeasError}</div>}
-      <Row data-equalizer>
-        {userIdeas && userIdeas.map((idea) => (
-          <Column key={idea.id} small={12} medium={4} large={3}>
-            <IdeaCard
-              idea={idea}
-              onClick={() => goToIdea(idea.id)}
-            />
-          </Column>
-        ))}
-      </Row>
+      {loadingUserIdeas && <Label>
+        <FormattedMessage {...messages.loadingIdeas} />
+      </Label>}
+      {loadUserIdeasError && <Label>
+        {loadUserIdeasError}
+        </Label>}
+      <Grid columns={2} centered>
+        <UserIdeasWrapper
+          userIdeas={userIdeas}
+          goToIdea={goToIdea}
+        />
+      </Grid>
     </div>
   );
 }
@@ -32,6 +42,11 @@ UserIdeas.propTypes = {
   className: PropTypes.string,
   loadingUserIdeas: PropTypes.bool.isRequired,
   loadUserIdeasError: PropTypes.string,
+  userIdeas: PropTypes.any,
+  goToIdea: PropTypes.func.isRequired,
+};
+
+UserIdeasWrapper.PropTypes = {
   userIdeas: PropTypes.any.isRequired,
   goToIdea: PropTypes.func.isRequired,
 };
