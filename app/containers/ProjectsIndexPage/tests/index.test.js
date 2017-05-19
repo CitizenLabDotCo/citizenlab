5cc4a91844904267826cd331c20c984b33344bcd
Manuel Gajo <gajo.manuel@gmail.com>
2017-05-19 14:23:27 +0200

    tests (complete)

--- app/containers/ProjectsIndexPage/tests/index.test.js
+++ app/containers/ProjectsIndexPage/tests/index.test.js
@@ -4,7 +4,7 @@
 // import { ProjectsIndexPage } from '../index';
 
 describe('<ProjectsIndexPage />', () => {
-  it('Expect to have unit tests specified', () => {
+  it('should have meaningful tests in the future', () => {
     expect(true).toEqual(true);
   });
 });
