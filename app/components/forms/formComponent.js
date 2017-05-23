222e88c05eb8e4dcaeeefe321a7ff5c0f198c970
DESKTOP-F5EVBDP\Pensarfeo <dryto25@hotmail.com>
2017-05-23 17:09:59 +0200

    solved conflicts with masterr

---
+++ app/components/forms/formComponent.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+// Comoponents
+
+class FormComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = { errors: {} };
+  }
+
+  componentDidUpdate(prevProvs, prevState) {
+    const { run } = this.context.sagas;
+    if (!prevState.loading && this.state.loading) {
+      run(this.saga, this.values, this.handleSuccess, this.handleErrors);
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ loading: true, errors: {}, error: false });
+  }
+
+  handleErrors = (e = {}) => {
+    const toNewState = { loading: false, error: true };
+    const errorsObje = {};
+    // translate Errors to arrays snake case strings
+    Object.keys(e).forEach((type) => {
+      errorsObje[type] = e[type].map(
+        (ele) => `${type}_error_${ele.error}`
+      );
+    });
+    toNewState.errors = errorsObje;
+
+    return this.setState(toNewState);
+  }
+}
+
+FormComponent.contextTypes = {
+  sagas: PropTypes.func.isRequired,
+};
+
+export default FormComponent;
