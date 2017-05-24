0dbab1e08feaf05f7de37190ef49964ff7a9ce1c
DESKTOP-F5EVBDP\Pensarfeo <dryto25@hotmail.com>
2017-05-24 11:44:03 +0200

    fix linting errors for store/reducer.js

--- app/utils/store/reducer.js
+++ app/utils/store/reducer.js
@@ -1,7 +1,6 @@
 import { Map, fromJS } from 'immutable';
 import { SET_GO_BACK_TO_CODE, DELETE_GO_BACK_TO_CODE } from './constants';
 
-/*eslint-disable*/
 function utilsReducer(state = Map(), action) {
   switch (action.type) {
     case SET_GO_BACK_TO_CODE:
@@ -16,40 +15,37 @@ function utilsReducer(state = Map(), action) {
 // listens for all actions and according to the affix (REQUEST, ERROR || SUCCESS, will save the state of the request.)
 // Already connected to the main state
 const toDefaultType = (type) => {
-  const actionArray = type.split('_')
-  actionArray.pop()
-  actionArray.push('REQUEST')
-  return actionArray.join('_')
-}
+  const actionArray = type.split('_');
+  actionArray.pop();
+  actionArray.push('REQUEST');
+  return actionArray.join('_');
+};
 
-function tempStateReducer(state=Map(), action) {
-  const actionKind = action.type.split('_').slice(-1)[0]
-  const cleanSlateArray = action.type.split('#')
+function tempStateReducer(state = Map(), action) {
+  const actionKind = action.type.split('_').slice(-1)[0];
+  const cleanSlateArray = action.type.split('#');
 
   if (cleanSlateArray[1]) {
-    return state.remove(action.uid)
+    return state.remove(action.uid);
   }
 
-  const actionUId = action.uid || toDefaultType(action.type)
+  const actionUId = action.uid || toDefaultType(action.type);
 
   if (actionKind === 'REQUEST') {
-    return state.set(actionUId, fromJS({loading: true, errors: {}}))
+    return state.set(actionUId, fromJS({ loading: true, errors: {} }));
   }
 
   if (actionKind === 'ERROR') {
-    const newState = {loading: false, error: true, errors: action.errors}
-    return state.set(actionUId, fromJS(newState))
+    const newState = { loading: false, error: true, errors: action.errors };
+    return state.set(actionUId, fromJS(newState));
   }
 
   if (actionKind === 'SUCCESS') {
-    return state.delete(actionUId)
+    return state.delete(actionUId);
   }
 
-  return state
+  return state;
 }
-
-
-
 
 /*
   Usefull to add local tempState behavior to your local reducer.
@@ -61,7 +57,7 @@ const withTemState = (reducer, filter = []) => (state, action) => {
   if (filter.some((ele) => toDefaultType(ele).toDefaultType(action.type))) {
     newState = tempStateReducer(state, action);
   }
-  return reducer(newState, action)
-}
+  return reducer(newState, action);
+};
 
 export { utilsReducer, tempStateReducer, withTemState };
