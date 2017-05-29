0e13fa8d7bc2f0b401f0e61569d839771ef55958
kogre <koen.gremmelprez@gmail.com>
2017-05-29 11:33:01 +0200

    Fixed tests

--- app/mailers/concerns/liquid_template_helpers.rb
+++ app/mailers/concerns/liquid_template_helpers.rb
@@ -15,7 +15,7 @@ module LiquidTemplateHelpers
 
   def default_liquid_params
     {
-      "organization_name" => MultilocService.new.t(Tenant.settings('core', 'organization_name'))
+      "organization_name" => MultilocService.new.t(Tenant.settings('core', 'organization_name')),
       "organization_type" => Tenant.settings('core', 'organization_type')
     }
   end
--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Activity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "Default factory" do
+    it "is valid" do
+      expect(build(:activity)).to be_valid
+    end
+  end
+
 end
