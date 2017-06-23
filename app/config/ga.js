8ab9906fc9ad1468893effda68190540af815899
DESKTOP-F5EVBDP\Pensarfeo <dryto25@hotmail.com>
2017-06-23 14:21:04 +0200

    enabled mix pannel, user is added on each traking send

--- app/config/ga.js
+++ app/config/ga.js
@@ -1,6 +1,9 @@
 import { createSelector } from 'reselect';
-// import * as mixpanel from 'mixpanel-browser';
-import trakingSaga from './gaSagas';
+import { init as mixpanelInit } from 'mixpanel-browser';
+import trakingSaga from 'resources/analitics/sagas';
+import { CL_GA_TRACKING_ID, CL_GA_TRACKER_NAME } from 'containers/App/constants';
+
+const getStore = {};
 
 /* eslint-disable */
 (function(i, s, o, g, r, a, m){
@@ -23,8 +26,9 @@ import trakingSaga from './gaSagas';
   m = s.getElementsByTagName(o)[0];
   a.async = 1;
   a.src = g;
+  a.onload = () => getStore.store.dispatch({type: "@@CL2INTERNAL/GOOGLE_ANALYTICS_LOADED"})
   m.parentNode.insertBefore(a, m)
-})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 // user the following script for debuggin
 // https://www.google-analytics.com/analytics_debug.js
 // or 
@@ -42,73 +46,69 @@ const getcurrentUser = createSelector(
   (id, users) => users && users.get(id),
 );
 
-export const logPageView = (path, name) => {
-  // console.log(path)
+export const logPageView = (path, name, userId) => {
   window.ga(`${name}.send`, 'pageview', {
     hitType: 'pageview',
     page: path,
-    // hitCallback: () => console.log('hit')
+    userId
   });
 };
 
+// this will trigger twice need to add some form of debounce or create a custom memoizer
+
+let prevPath;
 const gaHitCurrentPath = (trackerName) => createSelector(
   (state) => state.getIn(['route', 'locationBeforeTransitions', 'pathname']),
-  (path) => {
-    logPageView(path, trackerName)
+  (state) => state.getIn(['auth', 'id']),
+  (path, userId) => {
+    if (prevPath !== path) logPageView(path, trackerName, userId)
+    prevPath = path;
   },
 );
 
-const initialized = false;
-const clTrakerName = 'Citizenlab2';
-
-let tenantTrakerName;
-
-let unsubscribeAnalitics;
+let firstInitialize = true;
+let unsubscribeGaTrackingInit;
 
 const initAnalitics = (store, runSaga) => createSelector(
   getTenant,
-  getcurrentUser,
   () => window.ga,
   (state) => state.getIn(['route', 'locationBeforeTransitions', 'pathname']),
-  (tenant, user, ga, initialPath) => {
+  (tenant, ga, initialPath) => {
     if (!tenant || !ga ) return;
+    let trakerNames = [CL_GA_TRACKER_NAME]
+
     // TODO: Get tennat's traking id!!!
     const cookieDomain = 'auto'; // TODO: replace with 'auto'
 
     // Citizenlab tracker
-    const userId = user && user.get('id');
-    const clGaTrackingId = 'UA-65562281-44';
-    ga('create', clGaTrackingId, { userId, cookieDomain, name: clTrakerName });
+    ga('create', CL_GA_TRACKING_ID, cookieDomain, CL_GA_TRACKER_NAME);
     ga('Citizenlab2.require', 'displayfeatures', {}); // Remarketing, user info tracking and advertisting
-    //logPageView(initialPath, clTrakerName) // initial page view
-    const clTraker = gaHitCurrentPath(clTrakerName)
+    const clTraker = gaHitCurrentPath(CL_GA_TRACKER_NAME);
     store.subscribe(() => clTraker(store.getState())) // subscribe
+
     // Tenant tracker
     // TODO(?): do proper usage based on https://support.google.com/analytics/answer/3450482
     const tennatId = tenant.get('id');
-    const TenantGaTrackingId = tenant.tracking_id; // need propler traking id location
-    if (TenantGaTrackingId) { // not needed if we get traking id
-      ga('create', TenantGaTrackingId, cookieDomain, tennatId)
-      logPageView(initialPath, TenantGaTrackingId) // initial page view
-      const tenantTraker = gaHitCurrentPath(TenantGaTrackingId)
+    const TenantGaTrackingId = tenant.getIn(['attributes', 'settings', 'ga_tracking', 'traking_id']); // need propler traking id location
+    if (TenantGaTrackingId) {
+      trakerNames = trakerNames.concat(TenantGaTrackingId);
+      ga('create', TenantGaTrackingId, cookieDomain, TenantGaTrackingId)
+      const tenantTraker = gaHitCurrentPath(TenantGaTrackingId);
       store.subscribe(() => tenantTraker(store.getState())) // subscribe
     };
 
+    // Mixpanel
+    mixpanelInit('2899a45f48980c604de03e171f698e5c');
 
-    /*
-     * Mixpanel
-     */
-    // mixpanel.init('2899a45f48980c604de03e171f698e5c');
-
-    // initi traking through actions
-    trakingSaga(runSaga, { cl: clTrakerName, tenant: tennatId });
-
-    unsubscribeAnalitics();
+    // initi traking through actions only once;
+    trakingSaga(runSaga, trakerNames);
+    unsubscribeGaTrackingInit();
   }
 );
 
 
 export const createGa = (store, runSaga) => {
+  getStore.store = store;
   const initializer = initAnalitics(store, runSaga);
-  unsubscribeAnalitics = store.subscribe(() => initializer(store.getState()));
+  unsubscribeGaTrackingInit = store.subscribe(() => initializer(store.getState()));
 };
