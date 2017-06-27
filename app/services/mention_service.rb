f3fed14118f495ecca7081752eb976d409bb97a4
kogre <koen.gremmelprez@gmail.com>
2017-06-27 16:39:10 +0200

    Finished mentioning. Added notification and activity logging. CL2-337 #done

--- app/services/mention_service.rb
+++ app/services/mention_service.rb
@@ -9,10 +9,26 @@ class MentionService
   end
 
   def extract_mentions text
-    full_mentions = text.scan(/(@\w+-\w+)/).flatten
+    full_mentions = text.scan(/(@\w+-[\w-]+)/).flatten
     full_mentions.map do |fm|
       mention_to_slug(fm)
     end
+  end
+
+  def extract_expanded_mention_users text
+    doc = Nokogiri::HTML.fragment(text)
+    expanded_mentions = doc.css("span.cl-mention-user")
+    user_ids = expanded_mentions.map do |mention|
+      mention.attr('data-user-id')
+    end
+    User.where(id: user_ids.uniq)
+  end
+
+  def remove_expanded_mentions text
+    doc = Nokogiri::HTML.fragment(text)
+    expanded_mentions = doc.css("span.cl-mention-user")
+    expanded_mentions.each{|el| el.replace(el.inner_html)}
+    doc.to_s
   end
 
   def add_span_around text, mention, user_id
@@ -20,13 +36,33 @@ class MentionService
   end
 
   def process_mentions text
-    mention_candidates = extract_mentions(text)
-    users = mention_candidates.inject(User) do |scope, slug|
-      scope.or(User.where(slug: slug))
-    end
-    users.all.inject(text) do |memo, user|
+    users_mentioned_before = extract_expanded_mention_users(text)
+
+    cleaned_text = remove_expanded_mentions(text)
+
+    mention_candidates = extract_mentions(cleaned_text)
+
+    return [text, []] if mention_candidates.empty?
+
+    users_mentioned_now = User.where(slug: mention_candidates)
+    new_users_mentioned = users_mentioned_now.map(&:id) - users_mentioned_before.map(&:id)
+
+    new_text = users_mentioned_now.all.inject(cleaned_text) do |memo, user|
       add_span_around(memo, user_to_mention(user), user.id)
     end
+
+    [new_text, new_users_mentioned]
+  end
+
+  def extract_mentioned_users text
+    mentions = extract_mentions(text)
+    User.where(slug: mentions)
+  end
+
+  def new_mentioned_users old_text, new_text
+    old_users = extract_expanded_mention_users(old_text).all
+    new_users = extract_expanded_mention_users(new_text).all
+    new_users - old_users
   end
 
   def users_from_idea slug, idea, limit
