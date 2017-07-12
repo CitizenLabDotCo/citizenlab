65a54e1ec710b2a1dcd65edeacd7f980bd72b4a7
Mathieu Dubois <mathieu@citizenlab.co>
2017-07-12 10:59:20 +0200

    fix(lint): apply linting rules to the Proof of Concept component

--- app/components/Typescript-poc/index.tsx
+++ app/components/Typescript-poc/index.tsx
@@ -4,24 +4,24 @@ import * as React from 'react';
 // Define props for the component
 type Props = {
   name: string,
-}
+};
 
 // Define state for the component
 type State = {
   counter: number,
-}
+};
 
 export default class TSComponent extends React.Component<Props, State> {
   constructor () {
-    super()
+    super();
 
     this.state = {
       counter: 0,
-    }
+    };
   }
 
   incrementCounter = (): void => {
-    this.setState({counter: this.state.counter + 1})
+    this.setState({ counter: this.state.counter + 1 });
   }
 
   render () {
@@ -34,6 +34,6 @@ export default class TSComponent extends React.Component<Props, State> {
         <p>Counter: {counter}</p>
         <button onClick={this.incrementCounter}>Increment</button>
       </div>
-    )
+    );
   }
 }
