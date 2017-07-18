d1bab0c7bcf50f4528f5ea0fe248395251d86d69
Mathieu Dubois <mathieu@citizenlab.co>
2017-07-18 12:00:17 +0200

    WIP - basic structure for the projects admin

---
+++ app/containers/Admin/projects/all/index.tsx
@@ -0,0 +1,64 @@
+// Libraries
+import * as React from 'react';
+import * as Rx from 'rxjs/Rx';
+
+// Services
+import { observeProjects, IProject } from 'services/projects';
+
+// Localisation
+import { FormattedMessage } from 'react-intl';
+import t from 'utils/containers/t';
+const T = t;
+import messages from '../messages';
+
+// Component typing
+type Props = {
+  projects: IProject[]
+};
+
+type State = {
+  projects: IProject[] | null;
+};
+
+class AdminProjectsList extends React.Component<Props, State> {
+  subscription: Rx.Subscription;
+
+  constructor () {
+    super();
+
+    this.state = {
+      projects: null,
+    };
+  }
+
+  componentDidMount() {
+    this.subscription = observeProjects().observable.subscribe((projects) => {
+      console.log(projects);
+      this.setState({ projects: projects.data });
+    });
+  }
+
+  componentWillUnmount() {
+    this.subscription.unsubscribe();
+  }
+
+  render () {
+    const { projects } = this.state;
+
+    return (
+      <ul>
+        {projects && projects.map((project) => (
+          <li key={project.id}>
+            <T value={project.attributes.title_multiloc} />
+
+            <a href={`/admin/projects/${project.attributes.slug}/edit`}>
+              <FormattedMessage {...messages.editProject} />
+            </a>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default AdminProjectsList;
