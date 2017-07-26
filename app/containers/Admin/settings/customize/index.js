fd407a8a9f3fd2d2dabc61c247e4726d206f67a7
kogre <koen.gremmelprez@gmail.com>
2017-07-26 18:41:07 +0200

    Added titles and subtitles

--- app/containers/Admin/settings/customize/index.js
+++ app/containers/Admin/settings/customize/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { connect } from 'react-redux';
-import { Form, Checkbox } from 'semantic-ui-react';
+import { Checkbox } from 'semantic-ui-react';
 import { createStructuredSelector } from 'reselect';
 import { injectTFunc } from 'containers/T/utils';
 import { FormattedMessage } from 'react-intl';
 import Label from 'components/UI/Label';
+import Button from 'components/UI/Button';
 import ColorPickerInput from 'components/UI/ColorPickerInput';
 import { makeSelectLocale } from 'containers/LanguageProvider/selectors';
 import { makeSelectCurrentTenant } from '../selectors';
@@ -39,66 +40,69 @@ class SettingsCustomizeTab extends React.Component { // eslint-disable-line reac
     return (
       <div>
 
-        <Form>
 
-          <h1><FormattedMessage {...messages.titleBranding} /></h1>
+        <h1><FormattedMessage {...messages.titleBranding} /></h1>
+        <p><FormattedMessage {...messages.subTitleBranding} /></p>
 
-          <div>
-            <Label><FormattedMessage {...messages.mainColor} /></Label>
-            <ColorPickerInput
-              value={settings.getIn(['core', 'color_main'])}
-              onChange={(value) => this.changeAttribute(['settings', 'core', 'color_main'], value)}
-            />
-          </div>
+        <div>
+          <Label><FormattedMessage {...messages.mainColor} /></Label>
+          <ColorPickerInput
+            value={settings.getIn(['core', 'color_main'])}
+            onChange={(value) => this.changeAttribute(['settings', 'core', 'color_main'], value)}
+          />
+        </div>
 
-          <div>
-            <Label><FormattedMessage {...messages.menuBgColor} /></Label>
-            <ColorPickerInput
-              value={settings.getIn(['core', 'color_menu_bg'])}
-              onChange={(value) => this.changeAttribute(['settings', 'core', 'color_menu_bg'], value)}
-            />
-          </div>
+        <div>
+          <Label><FormattedMessage {...messages.menuBgColor} /></Label>
+          <ColorPickerInput
+            value={settings.getIn(['core', 'color_menu_bg'])}
+            onChange={(value) => this.changeAttribute(['settings', 'core', 'color_menu_bg'], value)}
+          />
+        </div>
 
-          <h1><FormattedMessage {...messages.titleSignupFields} /></h1>
+        <h1><FormattedMessage {...messages.titleSignupFields} /></h1>
+        <p><FormattedMessage {...messages.subTitleSignupFields} /></p>
 
-          <div>
-            <Label><FormattedMessage {...messages.gender} /></Label>
-            <Checkbox
-              slider
-              checked={settings.getIn(['demographic_fields', 'gender'])}
-              onChange={(event, value) => this.changeAttribute(['settings', 'demographic_fields', 'gender'], value.checked)}
-            />
-          </div>
 
-          <div>
-            <Label><FormattedMessage {...messages.domicile} /></Label>
-            <Checkbox
-              slider
-              checked={settings.getIn(['demographic_fields', 'domicile'])}
-              onChange={(event, value) => this.changeAttribute(['settings', 'demographic_fields', 'domicile'], value.checked)}
-            />
-          </div>
+        <div>
+          <Label><FormattedMessage {...messages.gender} /></Label>
+          <Checkbox
+            slider
+            checked={settings.getIn(['demographic_fields', 'gender'])}
+            onChange={(event, value) => this.changeAttribute(['settings', 'demographic_fields', 'gender'], value.checked)}
+          />
+        </div>
 
-          <div>
-            <Label><FormattedMessage {...messages.birthyear} /></Label>
-            <Checkbox
-              slider
-              checked={settings.getIn(['demographic_fields', 'birthyear'])}
-              onChange={(event, value) => this.changeAttribute(['settings', 'demographic_fields', 'birthyear'], value.checked)}
-            />
-          </div>
+        <div>
+          <Label><FormattedMessage {...messages.domicile} /></Label>
+          <Checkbox
+            slider
+            checked={settings.getIn(['demographic_fields', 'domicile'])}
+            onChange={(event, value) => this.changeAttribute(['settings', 'demographic_fields', 'domicile'], value.checked)}
+          />
+        </div>
 
-          <div>
-            <Label><FormattedMessage {...messages.education} /></Label>
-            <Checkbox
-              slider
-              checked={settings.getIn(['demographic_fields', 'education'])}
-              onChange={(event, value) => this.changeAttribute(['settings', 'demographic_fields', 'education'], value.checked)}
-            />
-          </div>
+        <div>
+          <Label><FormattedMessage {...messages.birthyear} /></Label>
+          <Checkbox
+            slider
+            checked={settings.getIn(['demographic_fields', 'birthyear'])}
+            onChange={(event, value) => this.changeAttribute(['settings', 'demographic_fields', 'birthyear'], value.checked)}
+          />
+        </div>
 
-          <Form.Button onClick={this.save}>Submit</Form.Button>
-        </Form>
+        <div>
+          <Label><FormattedMessage {...messages.education} /></Label>
+          <Checkbox
+            slider
+            checked={settings.getIn(['demographic_fields', 'education'])}
+            onChange={(event, value) => this.changeAttribute(['settings', 'demographic_fields', 'education'], value.checked)}
+          />
+        </div>
+
+        <Button onClick={this.save}>
+          <FormattedMessage {...messages.save} />
+        </Button>
 
       </div>
     );
