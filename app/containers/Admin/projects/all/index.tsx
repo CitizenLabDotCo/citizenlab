b318881e4f834660def4759831e31db0a96a0afa
Mathieu Dubois <mathieu@citizenlab.co>
2017-07-28 13:35:02 +0200

    WIP - basic styling for the list of projects

--- app/containers/Admin/projects/all/index.tsx
+++ app/containers/Admin/projects/all/index.tsx
@@ -21,18 +21,48 @@ const ProjectsList = styled.ul`
 `;
 
 const ProjectCard = styled.li`
+  background: white;
+  border-radius: 5px;
   display: flex;
+  flex: 1 0 300px;
   flex-direction: column;
   justify-content: center;
   margin: 1rem;
+  overflow: hidden;
+  max-width: calc(100% - 2rem);
+
+  @media (min-width: 850px) {
+    & {
+      max-width: calc(50% - 2rem);
+    }
+  }
+
+  @media (min-width: 1150px) {
+    & {
+      max-width: calc(33% - 2rem);
+    }
+  }
+
+  img {
+    max-width: 100%;
+  }
 
   h1 {
+    font-size: 1.5rem;
+    font-weight: normal;
+    margin: 1rem;
     text-align: center;
   }
 
   a {
+    background-color: #d70066;
+    border-radius: 5px;
+    color: white;
     display: block;
+    line-height: 3rem;
+    margin: 0 1rem 1rem;
     text-align: center;
+    text-decoration: none;
   }
 `;
 
@@ -73,6 +103,10 @@ class AdminProjectsList extends React.Component<Props, State> {
       <ProjectsList>
         {projects && projects.map((project) => (
           <ProjectCard key={project.id}>
+            {project.attributes.header_bg.small &&
+              <img src={project.attributes.header_bg.small} alt="" role="presentation" />
+            }
+
             <h1><T value={project.attributes.title_multiloc} /></h1>
 
             <a href={`/admin/projects/${project.attributes.slug}/edit`}>
