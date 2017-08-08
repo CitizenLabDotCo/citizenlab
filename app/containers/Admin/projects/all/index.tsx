5d8188ea75c56e160773f1c8872d398f5d9e6d75
Mathieu Dubois <mathieu@citizenlab.co>
2017-08-08 10:37:41 +0200

    WIP - Styling the list view

--- app/containers/Admin/projects/all/index.tsx
+++ app/containers/Admin/projects/all/index.tsx
@@ -15,6 +15,8 @@ import messages from '../messages';
 
 // Components
 import { Link } from 'react-router';
+import Icon from 'components/UI/Icon';
+const headerBG = require('assets/img/gray-header.png');
 
 // Styles
 const ProjectsList = styled.ul`
@@ -25,11 +27,11 @@ const ProjectsList = styled.ul`
 
 const ProjectCard = styled.li`
   background: white;
-  border-radius: 5px;
+  border-radius: 5px;
   display: flex;
   flex: 1 0 300px;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-end;
   margin: 1rem;
   overflow: hidden;
   max-width: calc(100% - 2rem);
@@ -51,9 +53,10 @@ const ProjectCard = styled.li`
   }
 
   h1 {
+    flex: 1;
     font-size: 1.5rem;
     font-weight: normal;
-    margin: 1rem;
+    margin: 1.5rem;
     text-align: center;
   }
 
@@ -66,6 +69,20 @@ const ProjectCard = styled.li`
     margin: 0 1rem 1rem;
     text-align: center;
     text-decoration: none;
+  }
+
+  &.new-project a {
+    align-items: center;
+    background: none;
+    border: 1px dashed #d4d4d4;
+    color: #d70066;
+    display: flex;
+    flex-direction: column;
+    font-size: 1.5rem;
+    height: 100%;
+    justify-content: center;
+    margin: 0;
+    padding: 1rem;
   }
 `;
 
@@ -104,10 +121,17 @@ class AdminProjectsList extends React.Component<Props, State> {
 
     return (
       <ProjectsList>
+        <ProjectCard className="new-project">
+          <Link to="/admin/projects/new">
+            <Icon name="plus" />
+            <FormattedMessage {...messages.addNewProject} />
+          </Link>
+        </ProjectCard>
         {projects && projects.map((project) => (
           <ProjectCard key={project.id}>
             {project.attributes.header_bg.small &&
               <img src={project.attributes.header_bg.small} alt="" role="presentation" />
+              || <img className="no-img" src={headerBG} alt="" role="presentation" />
             }
 
             <h1><T value={project.attributes.title_multiloc} /></h1>
