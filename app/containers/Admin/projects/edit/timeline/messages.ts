2a14b2531e302589f33b9807d7c9d303101ae66a
Mathieu Dubois <mathieu@citizenlab.co>
2017-08-09 15:50:46 +0200

    WIP - Messages & chronology

--- app/containers/Admin/projects/edit/timeline/messages.ts
+++ app/containers/Admin/projects/edit/timeline/messages.ts
@@ -9,4 +9,24 @@ export default defineMessages({
     id: 'app.containers.AdminPage.ProjectTimeline.saveLabel',
     defaultMessage: 'Save',
   },
+  orderColumnTitle: {
+    id: 'app.containers.AdminPage.ProjectTimeline.orderColumnTitle',
+    defaultMessage: 'Order',
+  },
+  nameColumnTitle: {
+    id: 'app.containers.AdminPage.ProjectTimeline.nameColumnTitle',
+    defaultMessage: 'Name & Dates',
+  },
+  addPhaseButton: {
+    id: 'app.containers.AdminPage.ProjectTimeline.addPhaseButton',
+    defaultMessage: 'Add a phase',
+  },
+  editPhaseButton: {
+    id: 'app.containers.AdminPage.ProjectTimeline.editPhaseButton',
+    defaultMessage: 'Edit',
+  },
+  deletePhaseButton: {
+    id: 'app.containers.AdminPage.ProjectTimeline.deletePhaseButton',
+    defaultMessage: 'Delete',
+  },
 });
