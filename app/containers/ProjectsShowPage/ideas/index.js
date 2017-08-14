a18a9e08d34c3fb5cfea92c5a5e66db573163b71
kogre <koen.gremmelprez@gmail.com>
2017-08-14 16:30:11 +0200

    Fixed linter errors

--- app/containers/ProjectsShowPage/ideas/index.js
+++ app/containers/ProjectsShowPage/ideas/index.js
@@ -9,12 +9,7 @@ import PropTypes from 'prop-types';
 import HelmetIntl from 'components/HelmetIntl';
 
 // components
-import OverlayChildren from 'utils/containers/overlayChildren';
-import PageView from 'containers/IdeasIndexPage/pageView';
-
-// store
-import { preprocess } from 'utils';
-import { push } from 'react-router-redux';
+import IdeaCards from 'components/IdeaCards';
 
 // messages
 import messages from 'containers/IdeasIndexPage/messages';
@@ -22,40 +17,22 @@ import messages from 'containers/IdeasIndexPage/messages';
 // need to implement Helmet
 class ProjectIdeasIndex extends React.Component {
 
-  isMainView = ({ params }) => !params.ideaId
-
-  backToMainView = () => {
-    const goTo = this.props.push;
-    const { params } = this.props;
-    return () => goTo(`/projects/${params.slug}/ideas`);
-  };
-
   render() {
-    const { children, params } = this.props;
+    const { params } = this.props;
     return (
       <div>
         <HelmetIntl
           title={messages.helmetTitle}
           description={messages.helmetDescription}
         />
-        <OverlayChildren
-          component={PageView}
-          isMainView={this.isMainView}
-          handleClose={this.backToMainView}
-          filter={{ project: params.slug }}
-          {...this.props}
-        >
-          {children}
-        </OverlayChildren>
+        <IdeaCards filter={{ project: params.slug }} />
       </div>
     );
   }
 }
 
 ProjectIdeasIndex.propTypes = {
-  children: PropTypes.element,
-  push: PropTypes.func.isRequired,
   params: PropTypes.object.isRequired,
 };
 
-export default preprocess(null, { push })(ProjectIdeasIndex);
+export default ProjectIdeasIndex;
