f5c475c8b74c1587536cb3023b0e313a3a1f4d0b
kogre <koen.gremmelprez@gmail.com>
2017-08-16 19:42:56 +0200

    Cleaned up lots of deprecated projects code. Styled projects header and info page. CL2-433

--- app/containers/ProjectsShowPage/ideas/index.js
+++ app/containers/ProjectsShowPage/ideas/index.js
@@ -1,38 +1,18 @@
-/*
- *
- * IdeasIndexPage
- *
- */
-
 import React from 'react';
 import PropTypes from 'prop-types';
-import HelmetIntl from 'components/HelmetIntl';
-
-// components
 import IdeaCards from 'components/IdeaCards';
 
-// messages
-import messages from 'containers/IdeasIndexPage/messages';
+// import ActionButton from 'components/buttons/action.js';
+// import IdeasBorad from 'containers/IdeasIndexPage/pageView';
 
-// need to implement Helmet
-class ProjectIdeasIndex extends React.Component {
 
-  render() {
-    const { params } = this.props;
-    return (
-      <div>
-        <HelmetIntl
-          title={messages.helmetTitle}
-          description={messages.helmetDescription}
-        />
-        <IdeaCards filter={{ project: params.slug }} />
-      </div>
-    );
-  }
-}
+const AllIdeas = ({ params }) => (
+  <IdeaCards filter={{ project: params.projectId }} />
+);
 
-ProjectIdeasIndex.propTypes = {
+AllIdeas.propTypes = {
   params: PropTypes.object.isRequired,
 };
 
-export default ProjectIdeasIndex;
+export default AllIdeas;
+
--- app/containers/ProjectsShowPage/info.js
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-export default () => <h1> THIS WILL BE THE project INFO PAGE </h1>;
