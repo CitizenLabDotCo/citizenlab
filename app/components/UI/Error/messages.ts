6965480163ba0c399a12183d3d4d980ebf0c5d45
Mathieu Dubois <mathieu@citizenlab.co>
2017-08-24 14:52:55 +0200

    WIP - Integration into the general project form

--- app/components/UI/Error/messages.ts
+++ app/components/UI/Error/messages.ts
@@ -1,4 +1,12 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
+  blank: {
+    id: 'app.errors.generics.blank',
+    defaultMessage: 'This field cannot be empty.'
+  },
+  title_multiloc_blank: {
+    id: 'app.errors.generics.title_multiloc_blank',
+    defaultMessage: 'The Title cannot be empty.'
+  },
 });
