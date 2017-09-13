369e0a0bf1e89463f6fcbdc155e56a40766a1e2c
DavidCnoops <david@citizenlab.co>
2017-09-13 10:16:21 +0200

    Update tenant.ts

--- app/services/tenant.ts
+++ app/services/tenant.ts
@@ -1,10 +1,37 @@
-import * as Rx from 'rxjs/Rx';
-import * as _ from 'lodash';
 import { API_PATH } from 'containers/App/constants';
-import streams, { IStreamParams } from 'utils/streams';
-import request from 'utils/request';
+import streams from 'utils/streams';
 
-const apiEndpoint = `${API_PATH}/tenants`;
+export interface ITenantSettings {
+  core: {
+    allowed: boolean;
+    enabled: boolean;
+    locales: string[];
+    timezone: string;
+    organization_name: {
+      [key: string]: string;
+    };
+    organization_type: string;
+    header_slogan: {
+      [key: string]: string;
+    } | null;
+    meta_title: {
+      [key: string]: string;
+    } | null;
+    meta_description: {
+      [key: string]: string;
+    } | null;
+    color_main: string | null;
+    color_menu_bg: string | null;
+  };
+  demographic_fields: {
+    allowed: boolean;
+    enabled: boolean;
+    gender: boolean;
+    birthyear: boolean;
+    domicile: boolean;
+    education: boolean;
+  };
+}
 
 export interface ITenantData {
   id: string;
@@ -12,37 +39,7 @@ export interface ITenantData {
   attributes: {
     name: string;
     host: string;
-    settings: {
-      core: {
-        allowed: boolean;
-        enabled: boolean;
-        locales: string[];
-        timezone: string;
-        organization_name: {
-          [key: string]: string;
-        };
-        organization_type: string;
-        header_slogan: {
-          [key: string]: string;
-        } | null;
-        meta_title: {
-          [key: string]: string;
-        } | null;
-        meta_description: {
-          [key: string]: string;
-        } | null;
-        color_main: string | null;
-        color_menu_bg: string | null;
-      };
-      demographic_fields: {
-        allowed: boolean;
-        enabled: boolean;
-        gender: boolean;
-        birthyear: boolean;
-        domicile: boolean;
-        education: boolean;
-      }
-    };
+    settings: ITenantSettings;
     logo: {
       small: string | null;
       medium: string | null;
@@ -60,6 +57,16 @@ export interface ITenant {
   data: ITenantData;
 }
 
-export function currentTenantStream(streamParams: IStreamParams<ITenant> | null = null) {
-  return streams.get<ITenant>({ apiEndpoint: `${apiEndpoint}/current`, ...streamParams });
+export interface IUpdatedTenantProperties {
+  settings: Partial<ITenantSettings>;
+  logo: string;
+  header_bg: string;
+}
+
+export function currentTenantStream() {
+  return streams.get<ITenant>({ apiEndpoint: `${API_PATH}/tenants/current` });
+}
+
+export function updateTenant(tenantId: string, object: IUpdatedTenantProperties) {
+  return streams.update<ITenant>(`${API_PATH}/tenants/${tenantId}`, tenantId, { tenant: object });
 }
