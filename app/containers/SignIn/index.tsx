b53474e7ef0633f9e3cf46bf7313d16eba7e21f0
Mathieu Dubois <mathieu@citizenlab.co>
2017-09-19 17:06:47 +0200

    fix(signin): prevents display of browser validation system

--- app/containers/SignIn/index.tsx
+++ app/containers/SignIn/index.tsx
@@ -170,7 +170,7 @@ export default class SignIn extends React.PureComponent<Props, State> {
 
     return (
       <Container>
-        <Form onSubmit={this.handleOnSubmit}>
+        <Form onSubmit={this.handleOnSubmit} noValidate={true}>
           <FormElement>
             {/* <Label value={formatMessage(messages.emailLabel)} htmlFor="email" /> */}
             <Input
