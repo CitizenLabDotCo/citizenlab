615d17403af6a5259a333c31127dae0b476c2615
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-09-21 15:14:43 +0200

    works both on host machine as from another docker container: always use localhost schema when in development env

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -89,7 +89,11 @@ Rails.application.config.middleware.use Apartment::Elevators::Generic, (Proc.new
   if request.path =~ /^\/admin_api\/.*/
     nil
   else
-    request.host.gsub(/\./, "_")
+    if Rails.env.development?
+      "localhost"
+    else
+      request.host.gsub(/\./, "_")
+    end
   end
 end)
 # Rails.application.config.middleware.use RescuedApartmentElevator, Proc.new { |request| puts request.path; request.host.gsub(/\./, "_") }
