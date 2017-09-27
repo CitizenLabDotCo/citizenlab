6ffedb6e7cbe3808edfdeabbb5c47fc0dc077f3a
Mathieu Dubois <mathieu@mathieudubois.be>
2017-09-27 15:34:49 +0200

    fix(e2e): fix signin page

--- e2e/pages/signin.js
+++ e2e/pages/signin.js
@@ -1,20 +1,21 @@
+const signinCommands = {
+  signin(email, password) {
+    return this
+    .waitForElementVisible('@form')
+    .setValue('@email', email)
+    .setValue('@password', password)
+    .click('@submit');
+  },
+};
+
+
 module.exports = {
   url: 'localhost:3000/sign-in',
   elements: {
-    form: '#signin',
-    email: '#email',
-    password: '#password',
-    submit: '#signin button',
+    form: { selector: '#signin' },
+    email: { selector: '#email' },
+    password: { selector: '#password' },
+    submit: { selector: '#signin button' },
   },
-  commands: [
-    {
-      signin(email, password) {
-        return this
-        .waitForElementVisible('@form')
-        .setValue('@email', email)
-        .setValue('@password', password)
-        .click('@submit');
-      },
-    },
-  ],
+  commands: [signinCommands],
 };
