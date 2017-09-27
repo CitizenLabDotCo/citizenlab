5115a1a12eee70bbe784aefeaf6a6b87bdb01c7c
Mathieu Dubois <mathieu@mathieudubois.be>
2017-09-27 15:35:10 +0200

    feat(e2e): voting scenario

--- app/components/IdeaCard/index.tsx
+++ app/components/IdeaCard/index.tsx
@@ -219,7 +219,7 @@ export default class IdeaCard extends React.PureComponent<Props, State> {
   }
 
   render() {
-    const className = `${this.props['className']} idea-card`;
+    const className = `${this.props['className']} idea-card ${!_.get(this.state, 'idea.data.relationships.user_vote.data', undefined) ? 'not-voted' : 'voted' }`;
     const { idea, ideaImage, ideaAuthor, isAuthenticated, locale, showUnauthenticated, loading } = this.state;
 
     if (!loading && idea && ideaAuthor && locale) {
