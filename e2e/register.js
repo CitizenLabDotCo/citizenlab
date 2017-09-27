c49c57839e1dc77ed2a17c657d092c0dab825344
Mathieu Dubois <mathieu@mathieudubois.be>
2017-09-27 17:07:22 +0200

    feat(e2e): use time to prevent test users colliding

--- e2e/register.js
+++ e2e/register.js
@@ -1,11 +1,13 @@
 module.exports = {
   register: (browser) => {
+    const time = new Date().getTime();
+
     browser
     .url('localhost:3000/sign-up')
     .waitForElementVisible('#signup-step1')
-    .setValue('#firstName', 'Test')
-    .setValue('#lastName', 'Account')
-    .setValue('#email', 'test@citizenlab.co')
+    .setValue('#firstName', `Test ${time}`)
+    .setValue('#lastName', `Account ${time}`)
+    .setValue('#email', `test+${time}@citizenlab.co`)
     .setValue('#password', '123456')
     .click('#signup-step1 button')
     .waitForElementVisible('#signup-step2')
