22b095de6c3a202c530177a5e62b5a017ad953f3
kogre <koen.gremmelprez@gmail.com>
2017-09-28 14:19:43 +0200

    Inject segment API key through ENV variable. Changed default for development

--- app/containers/App/constants.js
+++ app/containers/App/constants.js
@@ -17,4 +17,4 @@ export const LOADED_CURRENT_TENANT = 'app/App/LOADED_CURRENT_TENANT';
 export const LOAD_CURRENT_USER = 'app/App/LOAD_CURRENT_USER';
 export const CL_GA_TRACKING_ID = 'UA-65562281-44';
 export const CL_GA_TRACKER_NAME = 'CitizenLab2';
-export const CL_SEGMENT_API_KEY = 'VsvvuYyv10ZcFx6hoO0zLfpAwqBx89nY';
+export const CL_SEGMENT_API_KEY = process.env.SEGMENT_API_KEY || 'sIoYsVoTTCBmrcs7yAz1zRFRGhAofBlg';
