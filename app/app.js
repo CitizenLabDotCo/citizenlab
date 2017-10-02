73406dce3aa015fc54982a680ecc5844a75dc53d
Mathieu Dubois <mathieu@citizenlab.co>
2017-10-02 14:44:39 +0200

    feat(ci): flag staging builds as staging

--- app/app.js
+++ app/app.js
@@ -168,6 +168,6 @@ if (!window.Intl) {
 // Install ServiceWorker and AppCache in the end since
 // it's not most important operation and if main code fails,
 // we do not want it installed
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'development') {
   require('offline-plugin/runtime').install(); // eslint-disable-line global-require
 }
