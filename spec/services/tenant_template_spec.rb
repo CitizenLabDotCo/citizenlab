07fe7d40a9a949e5bae8cb7bc825026cbd6e25a6
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-10-05 15:44:21 +0200

    made test for applying tenant template from spreadsheet

--- spec/services/tenant_template_spec.rb
+++ spec/services/tenant_template_spec.rb
@@ -22,7 +22,7 @@ describe TenantTemplateService do
       end
     end
 
-    it "Successfully applies the base template" do # deze wel uitwerken omdat deze omzetting test
+    it "Successfully generates and applies tenant templates (those acquired from spreadsheets)" do
       tenant = service.apply_template('spec/services/tenant_template.yml', is_path=true)
       expect(IdeaStatus.count).to be 5
       expect(Topic.count).to be 12
@@ -30,8 +30,17 @@ describe TenantTemplateService do
       lea = User.find_by(email: 'princesslea@gmail.com')
       expect(lea).to be_present
       expect(lea.last_name).to eq('Skywalker')
-
+      expect(lea.avatar).to be_present
+      expect(Comment.count).to be 3
+      cs = Comment.all.select { |c| c.body_multiloc['en'].include? 'never seen you riding a bicycle' }
+      expect(cs.size).to be 1
+      c2 = cs.first
+      c1 = c2.parent
+      expect(c2.idea.id).to eq(c1.idea.id)
       expect(Project.count).to be 1
+      expect(Project.all.first.project_images.size).to be 1
+      expect(Event.count).to be 1
+      expect(Phase.count).to be 3
     end
   end
 
