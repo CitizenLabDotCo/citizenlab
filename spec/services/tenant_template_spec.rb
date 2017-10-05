34f4101a022a40e358771851d9f817c355417f27
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-10-05 14:10:58 +0200

    fixed some flaws in generating the yml file and extended apply_template to also work for these yml files

--- spec/services/tenant_template_spec.rb
+++ spec/services/tenant_template_spec.rb
@@ -16,11 +16,20 @@ describe TenantTemplateService do
 
   describe "apply_template" do
 
-    it "Successfully applies the base template" do 
-      tenant = service.apply_template('base')
+    it "Successfully applies the base template" do  
+      tenant = service.apply_template('base') # doen voor elke yml in deze dir
+      ### Overbodig
       expect(IdeaStatus.count).to be 5
       expect(Topic.count).to be 12
       expect(Page.count).to be 3
+      ###
+    end
+
+    it "Successfully applies the base template" do # deze wel uitwerken omdat deze omzetting test
+      tenant = service.apply_template('spec/services/tenant_template.yml', is_path=true)
+      expect(IdeaStatus.count).to be 5
+      expect(Topic.count).to be 12
+      expect(Project.count).to be 1
     end
   end
 
