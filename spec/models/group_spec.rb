5a9e833f420121919aa25199ec0e6f7f502dca3b
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-10-10 15:32:07 +0200

    initial test creating groups with factory seem to work

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "users (members)" do
+    it "can be assigned to groups" do
+      g = create(:group)
+    end
+
+    it "can be added to and removed from groups" do
+      g = create(:group)
+    end
+  end
 end
