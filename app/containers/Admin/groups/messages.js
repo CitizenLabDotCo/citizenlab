6142128ca63b99b00a1ddc5d6deed952a10b6223
Mathieu Dubois <mathieu@citizenlab.co>
2017-10-12 16:51:59 +0200

    feat(groups): adds route for the admin of groups

---
+++ app/containers/Admin/groups/messages.js
@@ -0,0 +1,20 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  helmetTitle: {
+    id: 'app.containers.AdminPage.Groups.helmetTitle',
+    defaultMessage: 'Admin - Groups dashboard',
+  },
+  helmetDescription: {
+    id: 'app.containers.AdminPage.Groups.helmetDescription',
+    defaultMessage: 'List of groups on the platform',
+  },
+  listTitle: {
+    id: 'app.containers.AdminPage.Groups.listTitle',
+    defaultMessage: 'Groups of users',
+  },
+  editTitle: {
+    id: 'app.containers.AdminPage.Groups.editTitle',
+    defaultMessage: 'Editing a group',
+  },
+});
