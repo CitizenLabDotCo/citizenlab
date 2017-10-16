d842ae2d4e4049bedf4d0402a0b086c738ccd64e
Mathieu Dubois <mathieu@citizenlab.co>
2017-10-16 11:47:55 +0200

    WIP - List styling

--- app/containers/Admin/groups/all/messages.js
+++ app/containers/Admin/groups/all/messages.js
@@ -1,9 +1,17 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
+  listTitle: {
+    id: 'app.containers.AdminPage.Groups.listTitle',
+    defaultMessage: 'Groups of users',
+  },
   members: {
     id: 'app.containers.AdminPage.groups.list.members',
     defaultMessage: '{count, plural, =0 {No members} one {one member} other {{count} members}}',
+  },
+  addGroupButton: {
+    id: 'app.containers.AdminPage.groups.list.addGroupButton',
+    defaultMessage: 'Add a group',
   },
   emptyListMessage: {
     id: 'app.containers.AdminPage.groups.list.emptyListMessage',
@@ -11,7 +19,7 @@ export default defineMessages({
   },
   loadingMessage: {
     id: 'app.containers.AdminPage.groups.list.loadingMessage',
-    defaultMessage: 'Loading…',
+    defaultMessage: 'Loading the list of groups…',
   },
   deleteButtonLabel: {
     id: 'app.containers.AdminPage.groups.list.deleteButtonLabel',
