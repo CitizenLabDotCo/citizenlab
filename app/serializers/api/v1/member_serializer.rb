cd3c156cc4f0214ec239cbceb803de69e98ce722
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-10-16 16:55:41 +0200

    a bit of cleaning: no email viewability permission verification, and member_of predicate in user model

--- app/serializers/api/v1/member_serializer.rb
+++ app/serializers/api/v1/member_serializer.rb
@@ -1,19 +1,13 @@
 class Api::V1::MemberSerializer < ActiveModel::Serializer
-  # include Knock::Authenticable
-
   attributes :id, :first_name, :last_name, :slug, :avatar, :is_member
-  attribute :email # , if: :view_private_attributes?
+  attribute :email
 
   def avatar
     object.avatar && object.avatar.versions.map{|k, v| [k.to_s, v.url]}.to_h
   end
 
   def is_member
-    !object.memberships.select{ |m| m.group_id == instance_options[:group_id] }.empty?
+  	object.member_of? instance_options[:group_id]
   end
-
-  # def view_private_attributes?
-   #  Pundit.policy!(current_user, object).view_private_attributes?
-  # end
 
 end
