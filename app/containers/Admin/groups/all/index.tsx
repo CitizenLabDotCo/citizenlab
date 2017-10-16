c422cd37217f182655cc3cda62ab919117e48787
Mathieu Dubois <mathieu@citizenlab.co>
2017-10-16 10:30:29 +0200

    WIP - Groups list

--- app/containers/Admin/groups/all/index.tsx
+++ app/containers/Admin/groups/all/index.tsx
@@ -7,16 +7,41 @@ import { FormattedMessage } from 'react-intl';
 import T from 'components/T';
 import messages from '../messages';
 
+// Components
+import PageWrapper from 'components/admin/PageWrapper';
+import GroupList from './GroupList';
+
 // Style
 import styled from 'styled-components';
 
+const PageTitle = styled.h1`
+  font-size: 2.5rem;
+  margin-bottom: 3rem;
+`;
+
+// Typing
+interface Props {
+}
+
+interface State {
+}
+
 class GroupsList extends React.Component {
+  constructor () {
+    super();
+  }
 
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.listTitle} />
-      </h1>
+      <div>
+        <PageTitle>
+          <FormattedMessage {...messages.listTitle} />
+        </PageTitle>
+        <PageWrapper>
+          Add Groups
+          <GroupList />
+        </PageWrapper>
+      </div>
     );
   }
 }
