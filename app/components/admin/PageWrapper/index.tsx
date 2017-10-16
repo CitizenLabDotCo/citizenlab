d1189a95a6d9e9c77cb34a7de8224a22c092d3be
Mathieu Dubois <mathieu@citizenlab.co>
2017-10-16 10:30:16 +0200

    feat(admin): PageWrapper component

---
+++ app/components/admin/PageWrapper/index.tsx
@@ -0,0 +1,21 @@
+// Libraries
+import * as React from 'react';
+
+// Style
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background: #FFFFFF;
+  border-radius: 5px;
+  border: 1px solid #EAEAEA;
+  box-sizing: border-box;
+  padding: 3.5rem 4rem;
+`;
+
+export default class PageWrapper extends React.Component{
+  render() {
+    return (
+      <Wrapper>{this.props.children}</Wrapper>
+    );
+  }
+}
