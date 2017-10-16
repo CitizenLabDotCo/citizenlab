34a6214c1830a0e9710f7fdd11dc34a98f119b4e
Mathieu Dubois <mathieu@citizenlab.co>
2017-10-16 12:02:55 +0200

    feat(groups): add membership stream

--- app/services/groups.ts
+++ app/services/groups.ts
@@ -16,6 +16,28 @@ export interface IGroups {
   data: IGroupData[];
 }
 
+
+export interface Membership {
+  id: string;
+  type: 'memberships';
+  relationships: {
+    user: {
+      data: {
+        id: string;
+        type: 'users';
+      };
+    };
+  };
+}
+
+export interface GroupsResponse {
+  data: Membership[];
+}
+
 export function listGroups(streamParams: IStreamParams<IGroups> | null = null) {
   return streams.get<IGroups>({ apiEndpoint: `${API_PATH}/groups`, ...streamParams });
+}
+
+export function listMembership(groupId: string, streamParams: IStreamParams<GroupsResponse> | null = null) {
+  return streams.get<GroupsResponse>({ apiEndpoint: `${API_PATH}/groups/${groupId}/memberships`, ...streamParams });
 }
