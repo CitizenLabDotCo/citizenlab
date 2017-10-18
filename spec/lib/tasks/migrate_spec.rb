09eb294c75374ccae83075b781eb7dd8c0608368
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-10-18 14:28:54 +0200

    Think I managed to create a test for the rake task

---
+++ spec/lib/tasks/migrate_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+require 'rake'
+# require 'rake/rdoctask'
+require 'rake/testtask'
+# require 'tasks/rails'
+
+
+def capture_stdout
+  s = StringIO.new
+  oldstdout = $stdout
+  $stdout = s
+  yield
+  s.string
+ensure
+  $stdout = oldstdout
+end
+
+
+RSpec.describe "Rake Migrate" do
+  context "from_cl1 task" do
+    it "can be invoked" do
+      Rails.application.load_tasks
+      Rake.application.rake_require '../../lib/tasks/migrate'
+      results = capture_stdout {Rake.application['migrate:from_cl1'].invoke}
+    end
+  end
+end
+#   before :each do
+#     host! "example.org"
+#   end
+#
+#   def endpoint_url
+#     "/api/v1/ideas"
+#   end
+#
+#   def endpoint_url_with_id(id)
+#     "/api/v1/ideas/#{id}"
+#   end
+#
+#   def json_response
+#     json_parse(response.body) if response.body
+#   end
+#
+#   context "list" do
+#     it 'returns the list of all records' do
+#       create_idea
+#       get endpoint_url
+#
+#       assert_status(200)
+#       assert_count(json_response, 1)
+#       expect(json_response[:data].first[:attributes][:"title_multiloc"]).to eq({ en: 'My idea' })
+#     end
+#
+#     it "returns an empty array if no records are found" do
+#       get endpoint_url
+#
+#       expect(response.status).to eq(200)
+#       expect(json_response[:data]).to eq([])
+#     end
+#   end
