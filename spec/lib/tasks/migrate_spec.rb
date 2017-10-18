54850e0b6d553d156e8ca6045dcecf056758c4b9
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-10-18 17:35:31 +0200

    more user migration

--- spec/lib/tasks/migrate_spec.rb
+++ spec/lib/tasks/migrate_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 require 'rake'
-# require 'rake/rdoctask'
 require 'rake/testtask'
-# require 'tasks/rails'
 
 
 RSpec.describe "Rake Migrate" do
@@ -12,40 +10,6 @@ RSpec.describe "Rake Migrate" do
       Rails.application.load_tasks
       Rake.application.rake_require '../../lib/tasks/migrate'
       results = Rake.application['migrate:from_cl1'].invoke
-      byebug
     end
   end
 end
-#   before :each do
-#     host! "example.org"
-#   end
-#
-#   def endpoint_url
-#     "/api/v1/ideas"
-#   end
-#
-#   def endpoint_url_with_id(id)
-#     "/api/v1/ideas/#{id}"
-#   end
-#
-#   def json_response
-#     json_parse(response.body) if response.body
-#   end
-#
-#   context "list" do
-#     it 'returns the list of all records' do
-#       create_idea
-#       get endpoint_url
-#
-#       assert_status(200)
-#       assert_count(json_response, 1)
-#       expect(json_response[:data].first[:attributes][:"title_multiloc"]).to eq({ en: 'My idea' })
-#     end
-#
-#     it "returns an empty array if no records are found" do
-#       get endpoint_url
-#
-#       expect(response.status).to eq(200)
-#       expect(json_response[:data]).to eq([])
-#     end
-#   end
