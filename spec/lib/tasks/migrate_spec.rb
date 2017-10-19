b07a6405f3642a8752362186700d2d078ff7c556
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-10-19 13:06:44 +0200

    user migration largely ok, have to branch now

--- spec/lib/tasks/migrate_spec.rb
+++ spec/lib/tasks/migrate_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 require 'rake'
-require 'rake/testtask'
+# require 'rake/testtask'
 
 
 RSpec.describe "Rake Migrate" do
   context "from_cl1 task" do
     it "can be invoked" do
       Rails.application.load_tasks
-      Rake.application.rake_require '../../lib/tasks/migrate'
+      # Rake.application.rake_require '../../lib/tasks/migrate'
       results = Rake.application['migrate:from_cl1'].invoke
     end
   end
