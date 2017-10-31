db839143a3ea8a18e22d4b84eaa80fcfe9210572
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-10-31 19:23:38 +0100

    almost there, but page_links attribute of page is a list of ids instead of a list of pages

--- spec/models/page_link_spec.rb
+++ spec/models/page_link_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe PageLink, type: :model do
   describe "Default factory" do
     it "is valid" do
-      expect(build(:page_link, linking_page: build(:page), linked_page: build(:page)).to be_valid
+      expect(build(:page_link, linking_page: build(:page), linked_page: build(:page))).to be_valid
     end
   end
 end
