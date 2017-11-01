48f08c03c60b92df63f97f956f84b4644a5a8b64
kogre <koen.gremmelprez@gmail.com>
2017-11-01 11:43:37 +0100

    Added correct default values for projects title and description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026133657) do
+ActiveRecord::Schema.define(version: 20171101102506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -222,8 +222,8 @@ ActiveRecord::Schema.define(version: 20171026133657) do
   end
 
   create_table "projects", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.jsonb "title_multiloc"
-    t.jsonb "description_multiloc"
+    t.jsonb "title_multiloc", default: {}
+    t.jsonb "description_multiloc", default: {}
     t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -275,14 +275,6 @@ ActiveRecord::Schema.define(version: 20171026133657) do
     t.string "last_name"
     t.string "locale"
     t.jsonb "bio_multiloc", default: {}
-    t.string "encrypted_password", default: "", null: false
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email"
     t.index ["slug"], name: "index_users_on_slug", unique: true
   end
