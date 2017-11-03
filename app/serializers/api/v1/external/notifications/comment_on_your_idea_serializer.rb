f055dddc4bd6b805a8c1a716ed30af2a39f08249
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-11-03 16:43:37 +0100

    done for comment on your comment and comment on your idea

--- app/serializers/api/v1/external/notifications/comment_on_your_idea_serializer.rb
+++ app/serializers/api/v1/external/notifications/comment_on_your_idea_serializer.rb
@@ -1,8 +1,11 @@
 class Api::V1::External::Notifications::CommentOnYourIdeaSerializer < Api::V1::External::Notifications::NotificationSerializer
-  class CustomCommentSerializer < ActiveModel::Serializer
-    attributes :id, :body_multiloc, :upvotes_count, :downvotes_count
-  end
-
+  belongs_to :user, serializer: CustomUserSerializer
   belongs_to :comment, serializer: CustomCommentSerializer
+  belongs_to :comment_author, serializer: CustomUserSerializer
+  belongs_to :idea, serializer: CustomIdeaSerializer
+  belongs_to :idea_author, serializer: CustomUserSerializer
+  has_many :idea_images, serializer: CustomImageSerializer
+  belongs_to :project, serializer: CustomProjectSerializer
+  has_many :project_images, serializer: CustomImageSerializer
 
 end
