41632a6fa78c8c6cd164b9614086a90f12c39a89
Mathieu Dubois <mathieu@citizenlab.co>
2017-11-03 16:52:32 +0100

    feat(tests): implementing the projects admin page helper

--- e2e/admin/projects.js
+++ e2e/admin/projects.js
@@ -3,49 +3,55 @@ const time = new Date().getTime();
 module.exports = {
   projectsList: (browser) => {
     const signinPage = browser.page.signin();
+    const adminProjectsPage = browser.page.adminProjects();
 
     signinPage
     .navigate()
     .signin('koen@citizenlab.co', 'testtest');
 
+    adminProjectsPage
+    .navigate()
+    .waitForElementVisible('@projectsList')
+    .waitForElementVisible('@newProject')
+    .waitForElementVisible('@projectCard');
+
     browser
-    .url('localhost:3000/admin/projects')
-    .waitForElementVisible('.e2e-projects-list')
-    .waitForElementVisible('.e2e-new-project')
-    .waitForElementVisible('.e2e-project-card')
     .end();
   },
 
   createNewProject: (browser) => {
     const signinPage = browser.page.signin();
+    const adminProjectsPage = browser.page.adminProjects();
 
     signinPage
     .navigate()
     .signin('koen@citizenlab.co', 'testtest');
 
-    browser
-    .url('localhost:3000/admin/projects')
-    .waitForElementVisible('.e2e-projects-list')
-    .waitForElementVisible('.e2e-new-project')
-    .click('.e2e-new-project')
+    adminProjectsPage
+    .navigate()
+    .waitForElementVisible('@newProject')
+    .click('@newProject')
+    .waitForElementVisible('@generalForm')
     .assert.urlEquals('http://localhost:3000/admin/projects/new')
     .setValue('#project-title', `Test Project ${time}`)
-    .click('.e2e-submit-wrapper-button')
-    .waitForElementVisible('.e2e-project-general-form')
+    .click('@submitButton')
+    .waitForElementVisible('@generalForm')
 
     // Test for the description insertion
-    .waitForElementVisible('.e2e-resource-tabs')
-    .click('.e2e-resource-tabs li:nth-child(2)')
+    .waitForElementVisible('@descriptionTab')
+    .click('@descriptionTab')
     .waitForElementVisible('.e2e-project-description-form')
     .setValue('.public-DraftEditor-content', 'Test Description please ignore')
-    .click('.e2e-submit-wrapper-button')
-    .waitForElementVisible('.e2e-submit-wrapper-button + .success')
+    .click('@submitButton')
+    .waitForElementVisible('@submitSuccess')
 
     // Test for project presence in the list
     .click('.e2e-projects-list-link')
-    .waitForElementVisible('.e2e-projects-list')
+    .waitForElementVisible('@projectsList')
     .assert.urlEquals('http://localhost:3000/admin/projects')
-    .assert.containsText('.e2e-projects-list', `Test Project ${time}`)
+    .assert.containsText('@projectsList', `Test Project ${time}`);
+
+    browser
     .end();
   },
 };
