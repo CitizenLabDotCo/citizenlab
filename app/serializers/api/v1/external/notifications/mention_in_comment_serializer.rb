03c965ebf8185cfe71dbcceff94d5350d5bec228
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-11-03 16:54:57 +0100

    done

--- app/serializers/api/v1/external/notifications/mention_in_comment_serializer.rb
+++ app/serializers/api/v1/external/notifications/mention_in_comment_serializer.rb
@@ -1,8 +1,23 @@
 class Api::V1::External::Notifications::MentionInCommentSerializer < Api::V1::External::Notifications::NotificationSerializer
-  class CustomCommentSerializer < ActiveModel::Serializer
-    attributes :id, :body_multiloc, :upvotes_count, :downvotes_count
+  belongs_to :user, serializer: CustomUserSerializer
+  belongs_to :comment, serializer: CustomCommentSerializer
+  belongs_to :comment_author, serializer: CustomUserSerializer
+  belongs_to :idea, serializer: CustomIdeaSerializer
+  belongs_to :idea_author, serializer: CustomUserSerializer
+  has_many :idea_images, serializer: CustomImageSerializer
+  belongs_to :project, serializer: CustomProjectSerializer
+  has_many :project_images, serializer: CustomImageSerializer
+
+  belongs_to :parent_comment, serializer: CustomCommentSerializer
+  belongs_to :parent_comment_author, serializer: CustomUserSerializer
+
+
+  def parent_comment
+  	object.comment&.parent
   end
 
-  belongs_to :comment, serializer: CustomCommentSerializer
+  def parent_comment_author
+  	object.comment&.parent&.author
+  end
 
 end
