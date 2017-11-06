bd4f8999aacc03c1188acf600300d0e119924beb
kogre <koen.gremmelprez@gmail.com>
2017-11-06 14:37:27 +0100

    Reworked notifications in TS and using data streams

---
+++ app/containers/Navbar/components/NotificationMenu/components/CommentOnYourCommentNotification/index.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import * as Rx from 'rxjs/Rx';
+
+import { INotificationData } from 'services/notifications';
+import { ideaByIdStream } from 'services/ideas';
+
+import messages from '../../messages';
+
+import { FormattedMessage } from 'react-intl';
+import NotificationWrapper from '../NotificationWrapper';
+import { Link } from 'react-router';
+
+
+type Props = {
+  notification: INotificationData;
+};
+
+type State = {
+  ideaSlug?: string,
+};
+
+export default class CommentOnYourCommentNotification extends React.PureComponent<Props, State> {
+  subscriptions: Rx.Subscription[];
+
+  constructor() {
+    super();
+    this.state = {
+      ideaSlug: undefined,
+    };
+  }
+
+  componentWillMount() {
+    if (this.props.notification.relationships.idea.data) {
+      const idea$ = ideaByIdStream(this.props.notification.relationships.idea.data.id).observable;
+      this.subscriptions = [
+        idea$.subscribe((response) => {
+          this.setState({
+            ideaSlug: response.data.attributes.slug,
+          });
+        })
+      ];
+    }
+  }
+
+  componentWillUnmount() {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
+
+  render() {
+    const { notification } = this.props;
+    const { ideaSlug } = this.state;
+    return (
+      <NotificationWrapper
+        linkTo={`/ideas/${ideaSlug}`}
+        timing={notification.attributes.created_at}
+        icon="notification_comment"
+        isRead={!!notification.attributes.read_at}
+      >
+        <FormattedMessage
+          {...messages.userReactedToYourComment}
+          values={{
+            name:
+            <Link
+              to={`/profile/${notification.attributes.user_slug}`}
+              onClick={(event) => event.stopPropagation()}
+            >
+              {notification.attributes.user_first_name}
+            </Link>,
+          }}
+        />
+      </NotificationWrapper>
+    );
+  }
+}
