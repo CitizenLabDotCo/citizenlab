bb1500ff769231e6ecf13db97dfab0da57dd8a20
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2017-11-13 11:35:37 +0100

    Made api token tenant scoping work and controlled token lifetime

--- engines/public_api/app/models/public_api/api_token.rb
+++ engines/public_api/app/models/public_api/api_token.rb
@@ -8,12 +8,30 @@ module PublicApi
     before_validation :generate_secret, on: :create
 
     def authenticate(secret)
-      self.secret == secret
+      rights_for_tenant = self.tenant_id.nil? || self.tenant_id == Tenant.current.id
+      self.secret == secret && rights_for_tenant
     end
 
     def self.from_token_request request
       id = request.params["auth"] && request.params["auth"]["client_id"]
       self.find(id)
+    end
+
+    def self.from_token_payload payload
+      byebug
+      if payload["tenant_id"]
+        self.find_by(id: payload["sub"], tenant_id: payload["tenant_id"])
+      else
+        self.find(payload["sub"])
+      end
+    end
+
+    def to_token_payload
+      {
+        sub: self.id,
+        tenant_id: self.tenant_id,
+        exp: 1.day.from_now.to_i
+      }
     end
 
     private
