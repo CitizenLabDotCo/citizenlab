efee2b5f839a7ee02a4913dba91e66ebb656adc8
Mathieu Dubois <mathieu@citizenlab.co>
2017-11-13 11:50:34 +0100

    feat(pages): allow admin edition/creation of target pages

---
+++ app/containers/Admin/settings/pages/messages.js
@@ -0,0 +1,28 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  titleLabel: {
+    id: 'app.containers.AdminPage.PagesEdition.titleLabel',
+    defaultMessage: 'Page Title',
+  },
+  buttonSave: {
+    id: 'app.containers.AdminPage.PagesEdition.buttonSave',
+    defaultMessage: 'Save',
+  },
+  buttonError: {
+    id: 'app.containers.AdminPage.PagesEdition.buttonError',
+    defaultMessage: 'Error',
+  },
+  buttonSuccess: {
+    id: 'app.containers.AdminPage.PagesEdition.buttonSuccess',
+    defaultMessage: 'Saved',
+  },
+  messageSuccess: {
+    id: 'app.containers.AdminPage.PagesEdition.messageSuccess',
+    defaultMessage: 'Your page has been saved successfully',
+  },
+  messageError: {
+    id: 'app.containers.AdminPage.PagesEdition.messageError',
+    defaultMessage: 'There was an error saving your page',
+  },
+});
