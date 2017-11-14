04cba26cf8b597760c8ec2b3f282b03fde68c514
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-11-14 11:23:03 +0100

    added slug to page link serialization

--- app/serializers/api/v1/page_link_serializer.rb
+++ app/serializers/api/v1/page_link_serializer.rb
@@ -1,7 +1,11 @@
 class Api::V1::PageLinkSerializer < ActiveModel::Serializer
-  attributes :id, :linked_page_title_multiloc, :ordering
+  attributes :id, :linked_page_title_multiloc, :linked_page_slug, :ordering
 
   def linked_page_title_multiloc
   	object.linked_page.title_multiloc
+  end
+
+  def linked_page_slug
+  	object.linked_page.slug
   end
 end
