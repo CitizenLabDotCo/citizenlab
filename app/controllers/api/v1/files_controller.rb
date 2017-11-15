5dbf2f48f104f2ec4a2005138b8d7c258ff47025
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-11-15 14:54:14 +0100

    handling carrier wave errors in project files

--- app/controllers/api/v1/files_controller.rb
+++ app/controllers/api/v1/files_controller.rb
@@ -19,7 +19,7 @@ class Api::V1::FilesController < ApplicationController
     if @file.save
       render json: @file, status: :created, serializer: Api::V1::FileSerializer
     else
-      render json: {errors: @file.errors.details}, status: :unprocessable_entity
+      render json: {errors: transform_errors_details(@file.errors.details)}, status: :unprocessable_entity
     end
   end
 
@@ -27,7 +27,7 @@ class Api::V1::FilesController < ApplicationController
     if @file.update(file_params)
       render json: @file, status: :ok, serializer: Api::V1::FileSerializer
     else
-      render json: {errors: @file.errors.details}, status: :unprocessable_entity
+      render json: {errors: transform_errors_details(@file.errors.details)}, status: :unprocessable_entity
     end
   end
 
@@ -41,6 +41,11 @@ class Api::V1::FilesController < ApplicationController
   end
 
   private
+
+  def transform_errors_details error_details
+    # carrierwave does not return the error code symbols by default
+    {file: error_details[:file]&.uniq{|e| e[:error]}}
+  end
 
   def secure_controller?
     false
