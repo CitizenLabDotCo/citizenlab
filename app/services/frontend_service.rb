f12bb2e8673f808eb8b60a15a6dbb23479bc656a
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-11-17 15:52:23 +0100

    created frontend_service and added background image in notification project serialization

---
+++ app/services/frontend_service.rb
@@ -0,0 +1,37 @@
+class FrontendService
+  # The main purpose of this service is to decouple all assumptions the backend
+  # makes about the frontend into a common location.
+
+  def home_url tenant=Tenant.current
+    tenant.base_frontend_uri
+  end
+
+  def model_to_url model_instance, tenant=Tenant.current
+    subroute = nil
+    slug = nil
+    if model_instance.kind_of? Project
+      subroute = 'projects'
+      slug = model_instance.slug
+    elsif model_instance.kind_of? Idea
+      subroute = 'ideas'
+      slug = model_instance.slug
+    elsif model_instance.kind_of? Comment ### comments do not have a URL yet, we return the idea URL for now
+      subroute = 'ideas'
+      slug = model_instance.idea.slug
+    elsif model_instance.kind_of? Page
+      subroute = 'pages'
+      slug = model_instance.slug
+    end
+
+    subroute && slug && "#{tenant.base_frontend_uri}/#{subroute}/#{slug}"
+  end
+
+  def signon_success_url tenant=Tenant.current
+    "#{tenant.base_frontend_uri}/complete-signup"
+  end
+
+  def signon_failure_url tenant=Tenant.current
+    "#{tenant.base_frontend_uri}/authentication-error"
+  end
+
+end
