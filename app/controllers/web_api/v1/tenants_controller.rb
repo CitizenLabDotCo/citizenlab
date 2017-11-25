47b7dd97f27de1dbbf5ff2ac41d6d0cd17818eb7
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2017-11-25 21:17:11 +0100

    Revert "Test with session cookie on staging"
    This reverts commit f65a56d2a8f1a4793621c267a11240a4289823ea.

--- app/controllers/web_api/v1/tenants_controller.rb
+++ app/controllers/web_api/v1/tenants_controller.rb
@@ -3,7 +3,6 @@ class WebApi::V1::TenantsController < ApplicationController
   before_action :set_tenant, only: [:current, :update]
 
   def current
-    session[:test_value] = 'somevalue'
     render json: @tenant
   end
 
