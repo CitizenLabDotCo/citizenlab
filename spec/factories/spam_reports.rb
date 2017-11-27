27e111ca7b714f22fd266a65a4d2c0ecea0b944f
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-11-27 15:01:08 +0100

    spam report policy and sidefx service added and almost full acceptance test, seems to work so far!

--- spec/factories/spam_reports.rb
+++ spec/factories/spam_reports.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :spam_report do
     association :spam_reportable, factory: :idea
     reported_at "2017-11-17 15:54:22"
-    reason_code nil
+    reason_code "wrong_content"
     other_reason "I've seen this text like over a thousand times."
     user
   end
