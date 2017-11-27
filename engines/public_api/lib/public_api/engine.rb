54281572749a002d7d47dcfb8566774d3a583da3
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2017-11-27 11:50:53 +0100

    Copied migrations from public_api engine to main app

--- engines/public_api/lib/public_api/engine.rb
+++ engines/public_api/lib/public_api/engine.rb
@@ -1,15 +1,5 @@
 module PublicApi
   class Engine < ::Rails::Engine
     isolate_namespace PublicApi
-
-    # from https://content.pivotal.io/blog/leave-your-migrations-in-your-rails-engines
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        config.paths["db/migrate"].expanded.each do |expanded_path|
-          app.config.paths["db/migrate"] << expanded_path
-        end
-      end
-    end
-
   end
 end
