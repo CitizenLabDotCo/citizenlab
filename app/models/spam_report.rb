2da0342ffa106059a798fcfd21ea4ea48dc67986
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-11-27 15:21:19 +0100

    added other reason code, tests will work now too

--- app/models/spam_report.rb
+++ app/models/spam_report.rb
@@ -1,11 +1,11 @@
 class SpamReport < ApplicationRecord
-  REASON_CODES = %w(wrong_content inapropriate)
+  REASON_CODES = %w(wrong_content inapropriate other)
 
   belongs_to :spam_reportable, polymorphic: true
   belongs_to :user
 
   validates :spam_reportable, presence: true
-  validates :reason_code, inclusion: { in: REASON_CODES }, presence: false
+  validates :reason_code, inclusion: { in: REASON_CODES }, presence: true
 
   after_validation :set_reported_at
 
