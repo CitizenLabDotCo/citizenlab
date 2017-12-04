199fb1e2dbb75a9eac553b69370727e890362a0b
Mathieu Dubois <mathieu@citizenlab.co>
2017-12-04 18:16:00 +0100

    WIP - Tooltip component

---
+++ app/components/UI/Tooltip/index.tsx
@@ -0,0 +1,52 @@
+// Libraries
+import * as React from 'react';
+
+// Components
+import clickOutside from 'utils/containers/clickOutside';
+import Transition from 'react-transition-group/Transition';
+
+// Styling
+import styled from 'styled-components';
+import { color, fontSize } from 'utils/styleUtils';
+
+const Container = styled(clickOutside)`
+  background: white;
+  border-radius: 5px;
+  border: 1px solid ${color('separation')};
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
+`;
+
+// Typing
+interface Props {
+  visible: boolean;
+  hideTooltip: {(): void};
+}
+
+interface State {}
+
+export default class Tooltip extends React.Component<Props, State> {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  handleClickOutside = () => {
+    if (this.props.visible) {
+      this.props.hideTooltip();
+    }
+  }
+
+  render () {
+    return (
+      <Transition in={this.props.visible} timeout={300}>
+        {(status) => (
+          <Container onClickOutside={this.handleClickOutside} className={`e2e-tooltip ${status}`}>
+            {this.props.children}
+          </Container>
+        )}
+      </Transition>
+    );
+  }
+}
