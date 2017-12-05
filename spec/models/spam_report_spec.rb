641c81b0f3cacece8e6ca74c3764bd96435a7873
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-12-05 14:18:25 +0100

    spam report model test

--- spec/models/spam_report_spec.rb
+++ spec/models/spam_report_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe SpamReport, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Default factory" do
+    it "is valid" do
+      expect(build(:spam_report)).to be_valid
+    end
+  end
 end
