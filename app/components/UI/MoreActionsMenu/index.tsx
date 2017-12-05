f6d03d283793def8a9f75fca1e7f821dc8eb069f
Mathieu Dubois <mathieu@mathieudubois.be>
2017-12-05 13:03:43 +0100

    WIP - More Actions menu component

---
+++ app/components/UI/MoreActionsMenu/index.tsx
@@ -0,0 +1,54 @@
+// Libraries
+import * as React from 'react';
+
+// Components
+import Tooltip from 'components/UI/Tooltip';
+import Button from 'components/UI/Button';
+
+// Typing
+interface Props {
+  actions: {
+    label: string;
+    handler: {(): void};
+    icon?: string;
+  }[];
+}
+interface State {
+  visible: boolean;
+}
+
+export default class MoreActions extends React.Component<Props, State> {
+  constructor (props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
+  }
+
+  hideTooltip = () => {
+    this.toggleTooltip(false);
+  }
+
+  toggleTooltip = (value: boolean | null = null) => {
+    this.setState({ visible : value || !this.state.visible });
+  }
+
+  render () {
+    return (
+      <div>
+        <Button>+</Button>
+        <Tooltip visible={this.state.visible} hideTooltip={this.hideTooltip}>
+          {this.props.actions.map((action) => (
+            <Button
+              key={action.label}
+              onClick={action.handler}
+              icon={action.icon}
+              text={action.label}
+            />
+          ))}
+        </Tooltip>
+      </div>
+    );
+  }
+
+}
