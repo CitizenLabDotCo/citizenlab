0afe8ed4b4e4ec64687cfba21df9fe201dfbe8cc
Mathieu Dubois <mathieu@citizenlab.co>
2017-12-08 18:02:03 +0100

    WIP - spam report window

--- app/containers/SpamReport/index.tsx
+++ app/containers/SpamReport/index.tsx
@@ -6,16 +6,54 @@ import { sendSpamReport, Report } from 'services/spamReports';
 
 // Components
 import { Section, SectionField, SectionTitle } from 'components/admin/Section';
+import Radio from 'components/UI/Radio';
 
 // i18n
-import { FormattedMessage } from 'utils/cl-intl';
+import { injectIntl, FormattedMessage } from 'utils/cl-intl';
+import { InjectedIntlProps } from 'react-intl';
 import messages from './messages';
+
+// Style
+import styled from 'styled-components';
+
+const RadioButtonsWrapper = styled.div`
+  margin-top: 15px;
+  margin-bottom: 30px;
+`;
+
+const StyledRadio = styled(Radio)`
+  margin-bottom: 10px;
+  cursor: pointer;
+
+  .text {
+    color: #333;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 22px;
+  }
+`;
 
 // Typings
 interface Props {}
-interface State {}
+interface State {
+  reasonCode: Report['reason_code'] | null;
+  reasonText: Report['other_reason'] | null;
+}
 
-export default class SpamReportForm extends React.Component<Props, State> {
+class SpamReportForm extends React.Component<Props & InjectedIntlProps, State> {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      reasonCode: null,
+      reasonText: null,
+    };
+  }
+
+  handleSelectionChange = (reasonCode) => {
+    this.setState({ reasonCode });
+  }
 
   handleSubmit = (event) => {
     event.preventDefault();
@@ -23,12 +61,30 @@ export default class SpamReportForm extends React.Component<Props, State> {
   }
 
   render () {
+    const { formatMessage } = this.props.intl;
+
     return (
       <form onSubmit={this.handleSubmit}>
         <Section>
           <SectionTitle><FormattedMessage {...messages.title} /></SectionTitle>
+
+          <RadioButtonsWrapper>
+            {['wrong_content', 'inappropriate', 'other'].map((reasonCode) => (
+              <StyledRadio
+                onChange={this.handleSelectionChange}
+                currentValue={this.state.reasonCode}
+                name="reasonCode"
+                label={formatMessage(messages[reasonCode])}
+                value={reasonCode}
+                id={`reason-${reasonCode}`}
+                key={reasonCode}
+              />
+            ))}
+          </RadioButtonsWrapper>
         </Section>
       </form>
     );
   }
 }
+
+export default injectIntl<Props>(SpamReportForm);
