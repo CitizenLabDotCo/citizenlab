50cc04aecd576bec008c99ca3411924a3784810f
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2017-12-12 17:57:27 +0100

    Working functional permissions system

---
+++ app/services/permissions/rules/commentPermissions.ts
@@ -0,0 +1,20 @@
+import { definePermissionRule } from '../.';
+import { isAdmin } from '../roles';
+import { ICommentData } from 'services/comments';
+import { IUser } from 'services/users';
+
+const isAuthor = (idea: ICommentData, user?: IUser) => {
+  return user && idea.relationships.author.data && idea.relationships.author.data.id === user.data.id;
+};
+
+definePermissionRule('comments', 'create', (idea: ICommentData, user: IUser) => {
+  return !!user;
+});
+
+definePermissionRule('comments', 'edit', (idea: ICommentData, user: IUser) => {
+  return !!(isAuthor(idea, user) || isAdmin(user));
+});
+
+definePermissionRule('comments', 'markAsSpam', (idea: ICommentData, user: IUser) => {
+  return true;
+});
--- app/services/permissions/rules/ideaPermissions.ts
+++ app/services/permissions/rules/ideaPermissions.ts
@@ -3,14 +3,18 @@ import { isAdmin } from '../roles';
 import { IIdeaData } from 'services/ideas';
 import { IUser } from 'services/users';
 
-const isAuthor = (idea: IIdeaData, user: IUser) => {
-  return idea.relationships.author.data && idea.relationships.author.data.id === user.data.id;
+const isAuthor = (idea: IIdeaData, user?: IUser) => {
+  return user && idea.relationships.author.data && idea.relationships.author.data.id === user.data.id;
 };
+
+definePermissionRule('ideas', 'create', (idea: IIdeaData, user: IUser) => {
+  return !!user;
+});
 
 definePermissionRule('ideas', 'edit', (idea: IIdeaData, user: IUser) => {
   return !!(isAuthor(idea, user) || isAdmin(user));
 });
 
-definePermissionRule('ideas', 'markAsSpam', (idea: IIdeaData, user) => {
+definePermissionRule('ideas', 'markAsSpam', (idea: IIdeaData, user: IUser) => {
   return true;
 });
