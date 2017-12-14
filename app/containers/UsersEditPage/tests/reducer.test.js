f6429bebe3c5a53f94b8c4a32b9d101c072e405f
Mathieu Dubois <mathieu@citizenlab.co>
2017-12-14 13:30:07 +0100

    Removed deprecated tests

--- app/containers/UsersEditPage/tests/reducer.test.js
+++
@@ -1,8 +0,0 @@
-// import { fromJS } from 'immutable';
-// import { expectNestedPropertyNotNull, expectPropertyNotNull } from 'utils/testing/methods';
-
-describe('usersEditPageReducer', () => {
-  it('should have meaningful tests in the future', () => {
-    expect(true).toEqual(true);
-  });
-});
