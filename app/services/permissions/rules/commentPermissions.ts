3a5d7ab25d015ef9a418a6385349b876ad352b21
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2017-12-14 17:58:08 +0100

    Fix c/p mistakes

--- app/services/permissions/rules/commentPermissions.ts
+++ app/services/permissions/rules/commentPermissions.ts
@@ -3,18 +3,18 @@ import { isAdmin } from '../roles';
 import { ICommentData } from 'services/comments';
 import { IUser } from 'services/users';
 
-const isAuthor = (idea: ICommentData, user?: IUser) => {
-  return user && idea.relationships.author.data && idea.relationships.author.data.id === user.data.id;
+const isAuthor = (comment: ICommentData, user?: IUser) => {
+  return user && comment.relationships.author.data && comment.relationships.author.data.id === user.data.id;
 };
 
-definePermissionRule('comments', 'create', (idea: ICommentData, user: IUser) => {
+definePermissionRule('comments', 'create', (comment: ICommentData, user: IUser) => {
   return !!user;
 });
 
-definePermissionRule('comments', 'edit', (idea: ICommentData, user: IUser) => {
-  return !!(isAuthor(idea, user) || isAdmin(user));
+definePermissionRule('comments', 'edit', (comment: ICommentData, user: IUser) => {
+  return !!(isAuthor(comment, user) || isAdmin(user));
 });
 
-definePermissionRule('comments', 'markAsSpam', (idea: ICommentData, user: IUser) => {
+definePermissionRule('comments', 'markAsSpam', (comment: ICommentData, user: IUser) => {
   return true;
 });
