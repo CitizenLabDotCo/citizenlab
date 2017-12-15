33001960007ca96ea175e7b8f58085c1dbf8b791
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2017-12-15 13:18:08 +0100

    Implemented activity endpoint for ideas and upgraded rubyzip

--- spec/services/side_fx_idea_spec.rb
+++ spec/services/side_fx_idea_spec.rb
@@ -30,24 +30,31 @@ describe SideFxIdeaService do
 
     it "logs a 'changed_title' action job when the title has changed" do
       idea = create(:idea)
+      old_idea_title = idea.title_multiloc
       idea.update(title_multiloc: {'en': 'changed'})
       expect {service.after_update(idea, user)}.
-        to have_enqueued_job(LogActivityJob).with(idea, 'changed_title', user, idea.updated_at.to_i)
+        to have_enqueued_job(LogActivityJob).with{|*arguments|
+          expect(arguments).to match [idea, 'changed_title', user, idea.updated_at.to_i, payload: {change: [old_idea_title, idea.title_multiloc]}]
+        }
     end
 
     it "logs a 'changed_body' action job when the body has changed" do
       idea = create(:idea)
+      old_idea_body = idea.body_multiloc
       idea.update(body_multiloc: {'en': 'changed'})
       expect {service.after_update(idea, user)}.
-        to have_enqueued_job(LogActivityJob).with(idea, 'changed_body', user, idea.updated_at.to_i)
+        to have_enqueued_job(LogActivityJob).with{|*arguments|
+          expect(arguments).to match [idea, 'changed_body', user, idea.updated_at.to_i, payload: {change: [old_idea_body, idea.body_multiloc]}]
+        }
     end
 
     it "logs a 'changed_status' action job when the idea_status has changed" do
       idea = create(:idea)
+      old_idea_status = idea.idea_status
       new_idea_status = create(:idea_status)
       idea.update(idea_status: new_idea_status)
       expect {service.after_update(idea, user)}.
-        to have_enqueued_job(LogActivityJob).with(idea, 'changed_status', user, idea.updated_at.to_i, payload: {code: new_idea_status.code})
+        to have_enqueued_job(LogActivityJob).with(idea, 'changed_status', user, idea.updated_at.to_i, payload: {change: [old_idea_status.id, new_idea_status.id]})
     end
   end
 
