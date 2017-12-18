08ebdbee826aa5c5d4578b3cbadb15fda4787b2e
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-12-18 17:51:31 +0100

    consultation method attribute added, still need TODO validation to check idea linking to phase that phase should be in ideation and test that

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214174008) do
+ActiveRecord::Schema.define(version: 20171218160850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -236,6 +236,7 @@ ActiveRecord::Schema.define(version: 20171214174008) do
     t.date "end_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "consultation_method", null: false
     t.index ["project_id"], name: "index_phases_on_project_id"
   end
 
--- spec/factories/phases.rb
+++ spec/factories/phases.rb
@@ -9,6 +9,7 @@ FactoryGirl.define do
       "en" => "<p>In this phase we gather ideas. Don't be shy, there are no stupid ideas!</p>",
       "nl" => "<p>In deze fase verzamelen we ideeën. Wees niet verlegen, er zijn geen domme ideeën!</p>"
     }}
+    consultation_method 'ideation'
     start_at "2017-05-01"
     end_at "2017-06-30"
   end
