aa63a71cc77f97e5a6118a5fd2cde20abcf1c261
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2017-12-19 12:20:24 +0100

    added error return when creating or updating idea to be linked to phase of a different project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171209082850) do
+ActiveRecord::Schema.define(version: 20171214174008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -345,6 +345,16 @@ ActiveRecord::Schema.define(version: 20171209082850) do
     t.boolean "cl1_migrated", default: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["slug"], name: "index_users_on_slug", unique: true
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.integer "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object"
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   create_table "votes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
