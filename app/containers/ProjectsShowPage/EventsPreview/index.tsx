850bb9209dfd3a86d2ed129179f394660c5cc84a
Mathieu Dubois <mathieu@citizenlab.co>
2017-12-20 15:07:27 +0100

    WIP - Adding title & link to all events page

--- app/containers/ProjectsShowPage/EventsPreview/index.tsx
+++ app/containers/ProjectsShowPage/EventsPreview/index.tsx
@@ -8,14 +8,28 @@ import { times } from 'lodash';
 import { eventsStream, IEventData } from 'services/events';
 import { Observable } from 'rxjs/Observable';
 
+// i18n
+import { FormattedMessage } from 'utils/cl-intl';
+import messages from '../messages';
+
 // Components
 import { Transition } from 'react-transition-group';
 import PreviewWrapper from './PreviewWrapper';
 import EventBlock from './EventBlock';
+import Button from 'components/UI/Button';
+
+// Styling
+import styled from 'styled-components';
+
+const StyledButton = styled(Button)`
+  flex: 0 !important;
+  justify-self: flex-end;
+`;
 
 // Typing
 interface Props {
   projectId: string | null;
+  eventsPageUrl: string;
 }
 
 interface State {
@@ -76,6 +90,12 @@ class EventsPreview extends React.Component<Props, State> {
       <Transition in={this.state.events.length > 0} timeout={200}>
         {(status) => (
           <PreviewWrapper.Background className={`e2e-events-preview ${status}`}>
+            <PreviewWrapper.Container>
+              <h2><FormattedMessage {...messages.upcomingEvents} /></h2>
+              <StyledButton circularCorners={false} style="primary-outlined" linkTo={this.props.eventsPageUrl}>
+                <FormattedMessage {...messages.allEvents} />
+              </StyledButton>
+            </PreviewWrapper.Container>
             <PreviewWrapper.Container>
               {this.state.events.slice(0, 3).map((event) => (
                 <EventBlock event={event} key={event.id} />
