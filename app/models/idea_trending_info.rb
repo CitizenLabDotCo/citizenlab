cc9ef53e637e3fdb9fa3eed2c311d7786e646b40
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-01-04 14:18:30 +0100

    readded associations and validations in rails model and made it read-only

--- app/models/idea_trending_info.rb
+++ app/models/idea_trending_info.rb
@@ -4,4 +4,16 @@ class IdeaTrendingInfo
   TREND_NUM_COMMENTS = 5
   TREND_SINCE_ACTIVITY = 30 * 24 * 60 * 60 # 30 days
 
+
+  belongs_to :idea
+
+  validates :last_activity_at, presence: true
+  validates :mean_activity_at, presence: true
+  validates :idea, presence: true
+
+
+  def readonly?
+    true
+  end
+
 end
