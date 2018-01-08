a10970d9a1e0dddf0ad7b22c89ebc483def723bd
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-01-08 16:42:02 +0100

    adding entpoint for idea markers, looking into querrying sql to get locations within rectangular area, but I have to switch

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -73,6 +73,14 @@ class Idea < ApplicationRecord
       .where(ideas_phases: {phase_id: phase_id})
   end)
 
+  scope :with_bounding_box, (Proc.new do |coordinates|
+    x1,y1,x2,y2 = coordinates
+    where("x < ?", x1)
+      .where("x > ?", x2)
+      .where("y > ?", y1)
+      .where("y < ?", y2)
+  end)
+
   scope :order_new, -> (direction=:desc) {order(published_at: direction)}
   scope :order_popular, -> (direction=:desc) {order("(upvotes_count - downvotes_count) #{direction}")}
   # based on https://medium.com/hacking-and-gonzo/how-hacker-news-ranking-algorithm-works-1d9b0cf2c08d
