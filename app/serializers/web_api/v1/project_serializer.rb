aa61c9057be7290ed183d1c4fa4296e85e021614
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-01-08 15:01:55 +0100

    added migration for added column of projects table, updated model with validations and default value, updated serializer and controller, updated acceptance test with api doc on presentation_mode parameter

--- app/serializers/web_api/v1/project_serializer.rb
+++ app/serializers/web_api/v1/project_serializer.rb
@@ -1,5 +1,5 @@
 class WebApi::V1::ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title_multiloc, :description_multiloc, :description_preview_multiloc, :slug, :header_bg, :visible_to, :ideas_count, :created_at, :updated_at
+  attributes :id, :title_multiloc, :description_multiloc, :description_preview_multiloc, :slug, :header_bg, :visible_to, :ideas_count, :created_at, :updated_at, :presentation_mode
 
   has_many :project_images, serializer: WebApi::V1::ImageSerializer
   has_many :areas
