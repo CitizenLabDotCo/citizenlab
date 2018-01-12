ba0dd23d51810d0cf564fb811fbdaaedb3b1b1e8
Mathieu Dubois <mathieu@citizenlab.co>
2018-01-12 14:13:17 +0100

    feat(projects): styling of the idea block

---
+++ app/components/IdeasMap/messages.js
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  seeIdea: {
+    id: 'app.components.IdeasMap.seeIdea',
+    defaultMessage: 'See the full idea',
+  },
+});
