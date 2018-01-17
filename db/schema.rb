2e4d5d4df2d2c05fbf7faa76c8584baa51793c5c
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-01-17 16:37:58 +0100

    open idea project in base template instead of seed, renamed internal_ref to internal_role (tenant template service would otherwise misinterpret the name)

--- db/schema.rb
+++ db/schema.rb
@@ -292,7 +292,7 @@ ActiveRecord::Schema.define(version: 20180117103530) do
     t.string "voting_method", default: "unlimited"
     t.integer "voting_limited_max", default: 10
     t.string "process_type", default: "timeline", null: false
-    t.string "internal_ref"
+    t.string "internal_role"
     t.index ["created_at"], name: "index_projects_on_created_at"
     t.index ["slug"], name: "index_projects_on_slug", unique: true
   end
