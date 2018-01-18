e1bbebcafd5cccba1b588debccff78cb0e4569de
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-01-18 15:02:11 +0100

    works! shema is as it should be

--- db/migrate/20180118125241_change_notifications.rb
+++ db/migrate/20180118125241_change_notifications.rb
@@ -1,13 +1,19 @@
 class ChangeNotifications < ActiveRecord::Migration[5.1]
   def change
-  	change_column :notifications, :idea_id, :uuid, foreign_key: { to_table: :ideas }, using: 'idea_id::uuid'
-  	change_column :notifications, :comment_id, :uuid, foreign_key: { to_table: :comments }, using: 'comment_id::uuid'
-  	change_column :notifications, :project_id, :uuid, foreign_key: { to_table: :projects }, using: 'project_id::uuid'
+  	change_column :notifications, :idea_id, :uuid, null: true, using: 'idea_id::uuid'
+  	change_column :notifications, :comment_id, :uuid, null: true, using: 'comment_id::uuid'
+  	change_column :notifications, :project_id, :uuid, null: true, using: 'project_id::uuid'
 
   	remove_foreign_key :notifications, :users # once for initiating_user_id
   	remove_foreign_key :notifications, :users # once for recipient_id
+  	remove_foreign_key :notifications, :spam_reports
 
-  	add_foreign_key :notifications, :users, column: :initiating_user_id
-    add_foreign_key :notifications, :users, column: :recipient_id
+  	add_foreign_key :notifications, :users, column: :initiating_user_id, on_delete: :nullify
+    add_foreign_key :notifications, :users, column: :recipient_id, on_delete: :cascade
+    add_foreign_key :notifications, :spam_reports, column: :spam_report_id, on_delete: :nullify
+
+    add_foreign_key :notifications, :ideas, column: :idea_id, on_delete: :nullify
+    add_foreign_key :notifications, :comments, column: :comment_id , on_delete: :nullify
+    add_foreign_key :notifications, :projects, column: :project_id, on_delete: :nullify
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -399,9 +399,12 @@ ActiveRecord::Schema.define(version: 20180118125241) do
   add_foreign_key "identities", "users"
   add_foreign_key "memberships", "groups"
   add_foreign_key "memberships", "users"
+  add_foreign_key "notifications", "comments", on_delete: :nullify
+  add_foreign_key "notifications", "ideas", on_delete: :nullify
+  add_foreign_key "notifications", "projects", on_delete: :nullify
   add_foreign_key "notifications", "spam_reports"
-  add_foreign_key "notifications", "users", column: "initiating_user_id"
-  add_foreign_key "notifications", "users", column: "recipient_id"
+  add_foreign_key "notifications", "users", column: "initiating_user_id", on_delete: :nullify
+  add_foreign_key "notifications", "users", column: "recipient_id", on_delete: :cascade
   add_foreign_key "page_links", "pages", column: "linked_page_id"
   add_foreign_key "page_links", "pages", column: "linking_page_id"
   add_foreign_key "pages", "projects"
