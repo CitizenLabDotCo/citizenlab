6be9f8eac4f025e0cecddb08738c4bd2d205b1ae
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-01-19 15:27:14 +0100

    recipient issue also solved, can not have multiple has_many"s with same name (thanks again Koen!)

--- db/migrate/20180118125241_change_notifications_foreign_keys.rb
+++ db/migrate/20180118125241_change_notifications_foreign_keys.rb
@@ -4,12 +4,6 @@ class ChangeNotificationsForeignKeys < ActiveRecord::Migration[5.1]
   	change_column :notifications, :comment_id, :uuid, null: true, using: 'comment_id::uuid'
   	change_column :notifications, :project_id, :uuid, null: true, using: 'project_id::uuid'
 
-  	remove_foreign_key :notifications, :users # once for initiating_user_id
-  	remove_foreign_key :notifications, :users # once for recipient_id
-
-  	add_foreign_key :notifications, :users, column: :initiating_user_id
-    # add_foreign_key :notifications, :users, column: :recipient_id, on_delete: :cascade # can't remove this one
-
     add_foreign_key :notifications, :ideas, column: :idea_id
     add_foreign_key :notifications, :comments, column: :comment_id
     add_foreign_key :notifications, :projects, column: :project_id
