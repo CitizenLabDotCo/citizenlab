ea8eeaaae8204752b950d65a26db44c3154e8b57
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-01-26 04:49:01 +0100

    Revert "Attempt to fix mapboxgl/uglify issue"
    This reverts commit 23263fb4a7f95370fa34d85e21530c8d15543bc2.

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -13,7 +13,6 @@ module.exports = (options) => ({
     publicPath: '/',
   }, options.output), // Merge with env dependent settings
   module: {
-    noParse: /(mapbox-gl)\.js$/,
     loaders: [{
       test: /\.js$/, // Transform all .js files required somewhere with Babel
       loader: 'babel-loader',
