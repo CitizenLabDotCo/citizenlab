6e6289b84da79055dd7356f5b9e6aae406f40c4f
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-02-01 18:00:03 +0100

    updated factorygirl to factorybot!

--- spec/factories/email_snippets.rb
+++ spec/factories/email_snippets.rb
@@ -1,4 +1,4 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :email_snippet do
     email "MyString"
     snippet "MyString"
--- spec/factories/topics.rb
+++ spec/factories/topics.rb
@@ -1,4 +1,4 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :topic do
     title_multiloc {{
       "en" => "Health",
