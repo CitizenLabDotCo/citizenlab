4bbb3b2858b6ad78decbe6769601a0cd42d7923d
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-02-15 14:58:15 +0100

    fixed small bugs

--- db/migrate/20180215130118_add_ordering_to_projects.rb
+++ db/migrate/20180215130118_add_ordering_to_projects.rb
@@ -1,9 +1,9 @@
 class AddOrderingToProjects < ActiveRecord::Migration[5.1]
   def change
-  	add_column :projects, :ordering, :integer, null: false
+  	add_column :projects, :ordering, :integer
 
     order = 0
-  	Project.order(created_at: :desc).each do |p|
+  	Project.order(created_at: :asc).each do |p|
   		p.ordering = order
   		p.save!
   		order += 1
