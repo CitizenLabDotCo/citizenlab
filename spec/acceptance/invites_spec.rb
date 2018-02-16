7c0bb85e6466a6cdaab581f7bcb8eddd44638d15
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-02-16 17:54:53 +0100

    not authorized renders not authorized

--- spec/acceptance/invites_spec.rb
+++ spec/acceptance/invites_spec.rb
@@ -6,8 +6,27 @@ resource "Invites" do
     header "Content-Type", "application/json"
   end
 
-
   post "web_api/v1/invites" do
+    with_options scope: :invite do
+      parameter :email, "The email of the invitee.", required: false
+      parameter :first_name, "The first name of the invitee.", required: false
+      parameter :last_name, "The last name of the invitee.", required: false
+      parameter :password, "The password of the invitee.", required: false
+      parameter :avatar, "The avatar of the invitee.", required: false
+      parameter :locale, "The locale of the invitee.", required: false
+      parameter :gender, "The gender of the invitee.", required: false
+      parameter :roles, "Roles array, only allowed when admin", required: false
+      parameter :gender, "Either 'male', 'female' or 'unspecified'", required: false
+      parameter :birthyear, "The year, as an integer, the user was born", required: false
+      parameter :domicile, "Either an exisiting Area id or 'outside', to specify the user does not live in the city", required: false
+      parameter :education, "An integer from 0 to 8 (inclusive), corresponding to the ISCED 2011 standard", required: false
+      parameter :bio_multiloc, "A little text, allowing the user to describe herself. Multiloc and non-html", required: false
+      parameter :group_ids, "A list of group identifiers to which the invited user will be made member", required: false
+    end
+    ValidationErrorHelper.new.error_fields(self, Invite)
+    ValidationErrorHelper.new.error_fields(self, User)
+    ValidationErrorHelper.new.error_fields(self, Membership)
+
     context "when admin" do
       before do
         @admin = create(:admin)
@@ -16,28 +35,6 @@ resource "Invites" do
 
         @groups = create_list(:group, 5)
       end
-      
-
-
-      with_options scope: :invite do
-        parameter :email, "The email of the invitee.", required: false
-        parameter :first_name, "The first name of the invitee.", required: false
-        parameter :last_name, "The last name of the invitee.", required: false
-        parameter :password, "The password of the invitee.", required: false
-        parameter :avatar, "The avatar of the invitee.", required: false
-        parameter :locale, "The locale of the invitee.", required: false
-        parameter :gender, "The gender of the invitee.", required: false
-        parameter :roles, "Roles array, only allowed when admin", required: false
-        parameter :gender, "Either 'male', 'female' or 'unspecified'", required: false
-        parameter :birthyear, "The year, as an integer, the user was born", required: false
-        parameter :domicile, "Either an exisiting Area id or 'outside', to specify the user does not live in the city", required: false
-        parameter :education, "An integer from 0 to 8 (inclusive), corresponding to the ISCED 2011 standard", required: false
-        parameter :bio_multiloc, "A little text, allowing the user to describe herself. Multiloc and non-html", required: false
-        parameter :group_ids, "A list of group identifiers to which the invited user will be made member", required: false
-      end
-      ValidationErrorHelper.new.error_fields(self, Invite)
-      ValidationErrorHelper.new.error_fields(self, User)
-      ValidationErrorHelper.new.error_fields(self, Membership)
       
       let(:email) { 'nonexistinguser@cocacola.gov' }
       let(:group_ids) { @groups.shuffle.take(3).map(&:id) }
@@ -52,12 +49,32 @@ resource "Invites" do
         expect(Membership.count).to eq 3
       end
 
-      #example "Inviting the same email to the same group twice fails", document: false do
-      #  create(:invite, email: email, group: @group)
+      example_request "[error] Authenticating an invited user", document: false do
+        invitee = User.find_by(email: email)
+        expect(invitee.authenticate '').to eq false
+      end
 
-      #  do_request
-      #  expect(response_status).to eq 422
-      # end
+      example "[error] Inviting the same email twice", document: false do
+        invite = create(:invite, invitee: create(:user, email: email))
+
+        do_request
+        expect(response_status).to eq 422
+      end
+    end
+
+    context "when not admin" do
+      before do
+        @inviter = create(:user)
+        token = Knock::AuthToken.new(payload: { sub: @inviter.id }).token
+        header 'Authorization', "Bearer #{token}"
+      end
+
+      let(:first_name) { 'Bart' }
+      let(:last_name) { 'Boulettos' }
+
+      #example_request "[error] invite", document: false do
+      #  expect(response_status).to eq 401
+      #end
     end
   end
 
