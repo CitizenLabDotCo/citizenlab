9c1c6894dba3e2649d067bb2dd5015c0d5e9ae8e
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-02-16 18:17:33 +0100

    testing accept invite, add accept invite url to serializer

--- app/serializers/web_api/v1/invite_serializer.rb
+++ app/serializers/web_api/v1/invite_serializer.rb
@@ -1,8 +1,12 @@
 class WebApi::V1::InviteSerializer < ActiveModel::Serializer
 
-  attributes :id, :token, :accepted_at, :created_at
+  attributes :id, :token, :accepted_at, :created_at, :activate_invite_url
 
   belongs_to :invitee
   belongs_to :inviter
+
+  def activate_invite_url
+    FrontendService.new.invite_url object.token
+  end
 
 end
