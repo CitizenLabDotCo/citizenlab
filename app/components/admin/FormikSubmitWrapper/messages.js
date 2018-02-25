af90a200d7a85aa04c17e538a5f65a4b367a1e35
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-02-25 15:41:41 +0100

    Functional crud for custom fields

---
+++ app/components/admin/FormikSubmitWrapper/messages.js
@@ -0,0 +1,30 @@
+/*
+ * UsersEditPage Messages
+ *
+ * This contains all the text for the UsersEditPage component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+
+  buttonSave: {
+    id: 'app.components.admin.FormikSubmitWrapper.buttonSave',
+    defaultMessage: 'Save',
+  },
+  buttonError: {
+    id: 'app.components.admin.FormikSubmitWrapper.buttonError',
+    defaultMessage: 'Error',
+  },
+  buttonSuccess: {
+    id: 'app.components.admin.FormikSubmitWrapper.buttonSuccess',
+    defaultMessage: 'Success',
+  },
+  messageSuccess: {
+    id: 'app.components.admin.FormikSubmitWrapper.messageSuccess',
+    defaultMessage: 'Saved successfully',
+  },
+  messageError: {
+    id: 'app.components.admin.FormikSubmitWrapper.messageError',
+    defaultMessage: 'There was an error during saving',
+  },
+});
