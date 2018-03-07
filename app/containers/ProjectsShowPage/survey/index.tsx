1b8d46849011753681cf3f73b48f362bcde929b6
Mathieu Dubois <mathieu@mathieudubois.be>
2018-03-07 14:13:58 +0100

    WIP - fix the survey display

--- app/containers/ProjectsShowPage/survey/index.tsx
+++ app/containers/ProjectsShowPage/survey/index.tsx
@@ -6,10 +6,12 @@ import GetProject from 'utils/resourceLoaders/components/GetProject';
 import Header from '../Header';
 import ContentContainer from 'components/ContentContainer';
 import Survey from '../process/survey';
+import Spinner from 'components/UI/Spinner';
+import { IProjectData } from 'services/projects';
 
-const SurveyContent = styled(ContentContainer)`
-  margin: 70px 0;
+const SurveyContainer = styled.div`
   min-height: 500px;
+  padding: 70px 0 90px 0;
 `;
 
 type Props = {};
@@ -24,18 +26,21 @@ export default class ProjectSurvey extends React.Component<Props & WithRouterPro
 
   render() {
     return (
-      <GetProject slug={this.props.params.slug}>
-        {({ project }) => (
-          <>
-            {project && <>
-              <Header slug={this.props.params.slug} />
-              <SurveyContent>
-                <Survey surveyService={project.attributes.survey_service} surveyEmbedUrl={project.attributes.survey_embed_url} />
-              </SurveyContent>
-            </>}
-          </>
-        )}
-      </GetProject>
+      <React.Fragment>
+        <Header slug={this.props.params.slug} />
+        <ContentContainer>
+          <SurveyContainer>
+            <GetProject slug={this.props.params.slug}>
+              {({ project }: {project: IProjectData}) => (<>
+                {project
+                  ? <Survey surveyService={project.attributes.survey_service} surveyEmbedUrl={project.attributes.survey_embed_url} />
+                  : <Spinner />
+                }
+              </>)}
+            </GetProject>
+          </SurveyContainer>
+        </ContentContainer>
+      </React.Fragment>
     );
   }
 }
