fb05f4d4df4ab31492ed39e655ac05499ce02163
David <david@citizenlab.co>
2018-03-08 13:45:01 +0100

    package-lock.json

--- app/components/UI/Button/index.tsx
+++ app/components/UI/Button/index.tsx
@@ -202,7 +202,7 @@ const Container: any = styled.div`
     &.secondary-outlined {
       &:not(.disabled) {
         background: #fff;
-        border: solid 1px #e0e0e0;
+        border: solid 2px #e0e0e0;
         ${(props: any) => setFillColor(props.theme.colorMain)}
 
         &:not(.processing):hover {
@@ -323,6 +323,11 @@ class Button extends React.PureComponent<Props, State> {
     if (style === 'primary-outlined' || style === 'secondary-outlined') {
       const theme = this.props.theme as object;
       return theme['colorMain'];
+    }
+
+    if (style === 'secondary') {
+      const theme = this.props.theme as object;
+      return theme['colors']['label'];
     }
 
     return '#fff';
