0d7588c17085aafd45a1abe6e6f7364bdb476456
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-03-14 11:36:00 +0100

    Render disabled custom fields as hidden and remove dangling custom_field_values for field deletion

--- app/services/custom_field_service.rb
+++ app/services/custom_field_service.rb
@@ -15,7 +15,7 @@ class CustomFieldService
     {
       type: "object",
       additionalProperties: false,
-      properties: fields.select(&:enabled).inject({}) do |memo, field|
+      properties: fields.inject({}) do |memo, field|
         memo[field.key] = 
           if field.code && self.respond_to?("#{field.key}_to_json_schema_field", true)
             send("#{field.key}_to_json_schema_field", field, locale)
@@ -38,7 +38,7 @@ class CustomFieldService
   end
 
   def fields_to_ui_schema fields, locale="en"
-    fields.select(&:enabled).inject({}) do |memo, field|
+    fields.inject({}) do |memo, field|
       memo[field.key] = 
         if field.code && self.respond_to?("#{field.key}_to_ui_schema_field", true)
           send("#{field.key}_to_ui_schema_field", field, locale)
@@ -47,8 +47,14 @@ class CustomFieldService
         end
       memo
     end.tap do |output|
-      output['ui:order'] = fields.select(&:enabled).sort_by{|f| f.ordering || Float::INFINITY }.map(&:key)
+      output['ui:order'] = fields.sort_by{|f| f.ordering || Float::INFINITY }.map(&:key)
     end
+  end
+
+  def delete_custom_field_values field
+    User
+      .where("custom_field_values \? '#{field.key}'")
+      .update_all("custom_field_values = custom_field_values - '#{field.key}'")
   end
 
   private
@@ -65,10 +71,14 @@ class CustomFieldService
     end
   end
 
+  def base_ui_schema_field field, locale
+    field.enabled ? {} : {"ui:widget": 'hidden'}
+  end
+
 # *** text ***
 
   def text_to_ui_schema_field field, locale
-    {}
+    base_ui_schema_field(field, locale)
   end
 
   def text_to_json_schema_field field, locale
@@ -82,7 +92,12 @@ class CustomFieldService
   # *** multiline_text ***
 
   def multiline_text_to_ui_schema_field field, locale
-    {"ui:widget": "textarea"}
+    base = base_ui_schema_field(field, locale)
+    if base[:"ui:widget"]
+      base 
+    else
+      {"ui:widget": "textarea"}
+    end
   end
 
   def multiline_text_to_json_schema_field field, locale
@@ -96,7 +111,7 @@ class CustomFieldService
   # *** select ***
 
   def select_to_ui_schema_field field, locale
-    {}
+    base_ui_schema_field(field, locale)
   end
 
   def select_to_json_schema_field field, locale
@@ -112,7 +127,7 @@ class CustomFieldService
   # *** multiselect ***
 
   def multiselect_to_ui_schema_field field, locale
-    {}
+    base_ui_schema_field(field, locale)
   end
 
   def multiselect_to_json_schema_field field, locale
@@ -126,14 +141,14 @@ class CustomFieldService
         enum: field.custom_field_options.map(&:key),
         enumNames: field.custom_field_options.map{|o| handle_title(o, locale)}
       },
-      minItems: field.required ? 1 : 0
+      minItems: (field.enabled && field.required) ? 1 : 0
     }
   end 
   
   # *** checkbox ***
 
   def checkbox_to_ui_schema_field field, locale
-    {}
+    base_ui_schema_field(field, locale)
   end
 
   def checkbox_to_json_schema_field field, locale
@@ -147,7 +162,7 @@ class CustomFieldService
   # *** date ***
 
   def date_to_ui_schema_field field, locale
-    {}
+    base_ui_schema_field(field, locale)
   end
 
   def date_to_json_schema_field field, locale
@@ -168,7 +183,7 @@ class CustomFieldService
   end
 
   def birthyear_to_ui_schema_field field, locale
-    {}
+    base_ui_schema_field(field, locale)
   end
 
   # *** Built-in domicile field ***
@@ -186,6 +201,6 @@ class CustomFieldService
   end
 
   def domicile_to_ui_schema_field field, locale
-    {}
+    base_ui_schema_field(field, locale)
   end
 end
