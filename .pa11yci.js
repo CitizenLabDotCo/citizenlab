e397485b28a51c360413fe0593adc19e7b307bad
Mathieu Dubois <mathieu@citizenlab.co>
2018-03-14 14:40:40 +0100

    WIP - run pa11y-ci to identify a11y issues

---
+++ .pa11yci.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "defaults": {
+    "timeout": 10000
+  },
+  "urls": [
+    "https://demo.stg.citizenlab.co/",
+    "https://demo.stg.citizenlab.co/projects",
+    "https://demo.stg.citizenlab.co/projects/renewing-westbrook-parc/process",
+    "https://demo.stg.citizenlab.co/projects/renewing-westbrook-parc/info",
+    "https://demo.stg.citizenlab.co/projects/renewing-westbrook-parc/events",
+    "https://demo.stg.citizenlab.co/ideas",
+    "https://demo.stg.citizenlab.co/ideas/more-box-parking-for-bikes-in-the-centrum",
+    "https://demo.stg.citizenlab.co/sign-in",
+    "https://demo.stg.citizenlab.co/sign-up",
+    "https://demo.stg.citizenlab.co/ideas/new"
+  ]
+}
