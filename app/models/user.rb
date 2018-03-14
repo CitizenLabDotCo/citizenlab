ff0f41da084724a9001ee6aad7b800f68b4a390a
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-03-14 14:55:18 +0100

    Fixed creation of user through social networks with existing custom field values

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ApplicationRecord
   validates :custom_field_values, json: {
     schema: lambda { CustomFieldService.new.fields_to_json_schema(CustomField.fields_for(User)) },
     message: ->(errors) { errors }
-  }, if: :custom_field_values_changed?
+  }, if: [:custom_field_values_changed?, :active?]
 
   validates :password, length: { in: 5..72 }, allow_nil: true
   validate do |record|
