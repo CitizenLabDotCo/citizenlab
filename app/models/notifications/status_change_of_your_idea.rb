96e12c83421dbf6979cd3759929066d9f854b4de
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-03-20 12:06:36 +0100

    notifications for status change of your idea

---
+++ app/models/notifications/status_change_of_your_idea.rb
@@ -0,0 +1,34 @@
+module Notifications
+  class StatusChangeOfYourIdea < Notification
+    
+    belongs_to :idea
+    belongs_to :project, optional: true
+
+    validates :idea_id, presence: true
+
+
+    ACTIVITY_TRIGGERS = {'Idea' => {'changed_status' => true}}
+    EVENT_NAME = 'Status change of your idea'
+    
+
+    def self.make_notifications_on activity
+      idea = activity.item
+
+      idea_id = idea&.id
+      recipient_id = idea&.author_id
+      project_id = idea&.project_id
+
+      if idea_id && recipient_id
+        [self.create(
+           recipient_id: recipient_id,
+           idea_id: idea_id,
+           project_id: project_id
+         )]
+      else
+        []
+      end
+    end
+
+  end
+end
+
