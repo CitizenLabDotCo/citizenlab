93bb15a0495142375da288a6bdabed542df1f65d
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-03-21 17:35:33 +0100

    service to export and import projects done, around_action to wrap admin controllers in tenant switch block, and new version of migration script

--- app/controllers/admin_api/admin_api_controller.rb
+++ app/controllers/admin_api/admin_api_controller.rb
@@ -1,10 +1,21 @@
 class AdminApi::AdminApiController < ActionController::API
   before_action :authenticate_request
+  around_action :switch_tenant
 
   def authenticate_request
     unless request.headers["Authorization"] == ENV.fetch("ADMIN_API_TOKEN")
       render json: { error: 'Not Authorized' }, status: 401
       false
     end
+  end
+
+  def switch_tenant
+  	if params[:tenant_id]
+  	  Apartment::Tenant.switch(params[:tenant_id]) do
+  		  yield
+  	  end
+  	else
+  		yield
+  	end
   end
 end
