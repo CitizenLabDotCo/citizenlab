38fc68a7b999eb76e343f5ca43c8a90bcaa92762
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-03-22 16:04:04 +0100

    implemented project controller of admin api, with working acceptance tests

--- app/controllers/admin_api/admin_api_controller.rb
+++ app/controllers/admin_api/admin_api_controller.rb
@@ -2,6 +2,9 @@ class AdminApi::AdminApiController < ActionController::API
   before_action :authenticate_request
   around_action :switch_tenant
 
+  rescue_from ActiveRecord::RecordNotFound, with: :send_not_found
+  
+
   def authenticate_request
     unless request.headers["Authorization"] == ENV.fetch("ADMIN_API_TOKEN")
       render json: { error: 'Not Authorized' }, status: 401
@@ -11,11 +14,19 @@ class AdminApi::AdminApiController < ActionController::API
 
   def switch_tenant
   	if params[:tenant_id]
-  	  Apartment::Tenant.switch(params[:tenant_id]) do
+  	  Apartment::Tenant.switch(Tenant.find(params[:tenant_id]).schema_name) do
   		  yield
   	  end
   	else
   		yield
   	end
+  end
+
+  def send_not_found(error=nil)
+    if error.nil?
+      head 404, "content_type" => 'text/plain'
+    else
+      render json: error, status: 404
+    end
   end
 end
