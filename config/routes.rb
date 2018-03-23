0051d298863d3f86a3def29d37c8c3ee598362d2
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-03-23 11:12:38 +0100

    engine almost done, gonna switch

--- config/routes.rb
+++ config/routes.rb
@@ -103,20 +103,6 @@ Rails.application.routes.draw do
 
   end
 
-
-
-  namespace :admin_api, :defaults => {:format => :json} do
-    resources :tenants do
-      get :settings_schema, on: :collection
-      get :templates, on: :collection
-    end
-
-    resources :projects, only: [:index] do
-      get :template_export, on: :collection
-      post :template_import, on: :collection
-    end
-  end
-
   get '/auth/:provider/callback', to: 'omniauth_callback#create'
   get '/auth/failure', to: 'omniauth_callback#failure'
 
--- engines/admin_api/config/routes.rb
+++ engines/admin_api/config/routes.rb
@@ -1,2 +1,15 @@
 AdminApi::Engine.routes.draw do
+
+	namespace :admin_api, :defaults => {:format => :json} do
+    resources :tenants do
+      get :settings_schema, on: :collection
+      get :templates, on: :collection
+    end
+
+    resources :projects, only: [:index] do
+      get :template_export, on: :collection
+      post :template_import, on: :collection
+    end
+  end
+  
 end
