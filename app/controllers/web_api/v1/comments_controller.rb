bb81a1bc5112b773af5aaf3b2c3393f1a65e6a5c
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-03-27 12:07:32 +0200

    fixed tiny syntax err

--- app/controllers/web_api/v1/comments_controller.rb
+++ app/controllers/web_api/v1/comments_controller.rb
@@ -68,8 +68,8 @@ class WebApi::V1::CommentsController < ApplicationController
 
   def mark_as_deleted
     mad_params = mark_as_deleted_params
-    if (MARK_AS_DELETED_REASON_CODES.include? mad_params[:reason_code])
-      && (mad_params[:reason_code] != 'other' || mad_params[:other_reason].present?)
+    if ((MARK_AS_DELETED_REASON_CODES.include? mad_params[:reason_code]) &&
+      (mad_params[:reason_code] != 'other' || mad_params[:other_reason].present?))
       @comment.publication_status = 'deleted'
       if @comment.save
         SideFxCommentService.new.after_mark_as_deleted(@comment, current_user, mad_params[:reason_code], mad_params[:other_reason])
