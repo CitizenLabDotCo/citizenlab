d4f1dd51d39ae1e86f308b67e8f5f87b522c290a
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-04-04 15:36:49 +0200

    fixed invitee and inviter props in external serializer

--- app/serializers/web_api/v1/external/invite_serializer.rb
+++ app/serializers/web_api/v1/external/invite_serializer.rb
@@ -1,2 +1,12 @@
-class WebApi::V1::External::InviteSerializer < WebApi::V1::InviteSerializer
+class WebApi::V1::External::InviteSerializer < ActiveModel::Serializer
+
+  attributes :id, :invite_text, :accepted_at, :activate_invite_url
+
+  belongs_to :invitee, serializer: WebApi::V1::External::UserSerializer
+  belongs_to :inviter, serializer: WebApi::V1::External::UserSerializer
+  
+
+  def activate_invite_url
+    FrontendService.new.invite_url object.token
+  end
 end
