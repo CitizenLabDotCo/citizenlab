747b1406cb26703de2005c9c9f564f73cfdf9661
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-04-04 18:02:53 +0200

    added serializers for comment deleted by admin

---
+++ app/serializers/web_api/v1/notifications/comment_deleted_by_admin_serializer.rb
@@ -0,0 +1,27 @@
+class WebApi::V1::Notifications::CommentDeletedByAdminSerializer < WebApi::V1::Notifications::NotificationSerializer
+
+  belongs_to :initiating_user, serializer: WebApi::V1::UserSerializer
+  belongs_to :idea, serializer: WebApi::V1::IdeaSerializer
+  belongs_to :comment, serializer: WebApi::V1::CommentSerializer  
+  belongs_to :project, serializer: WebApi::V1::ProjectSerializer
+
+  attributes :initiating_user_first_name, :initiating_user_last_name, :initiating_user_slug, :idea_title
+
+
+  def initiating_user_first_name
+    object.initiating_user&.first_name
+  end
+
+  def initiating_user_last_name
+    object.initiating_user&.last_name
+  end
+
+  def initiating_user_slug
+    object.initiating_user&.slug
+  end
+
+  def idea_title
+    object.idea&.title_multiloc
+  end
+
+end
