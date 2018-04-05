06cc9972edd655f7eeb13ea578b378f5025a943b
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-04-05 13:45:54 +0200

    added test to see if all notifications have working factories

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe Notification, type: :model do
       expect(build(:comment_on_your_comment)).to be_valid
     end
   end
+
+  describe "all notification subclass factories" do
+    before do
+      # Make sure that we can find all notification
+      # subclasses, but without enabling eager 
+      # loading for the other tests.
+      Cl2Back::Application.eager_load!
+    end
+    it "are valid" do
+      Notification.descendants.each do |notification_subclass|
+        expect(build(notification_subclass.model_name.element.to_sym)).to be_valid
+      end
+    end
+  end
 end
