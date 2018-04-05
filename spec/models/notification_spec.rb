110582d7c9725775ad128b00b3c76d5fadf98dda
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-04-05 14:38:26 +0200

    adding make_notifications_on tests

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -7,12 +7,6 @@ RSpec.describe Notification, type: :model do
     end
   end
 
-  describe "comment_on_your_comment factory" do
-    it "is valid" do
-      expect(build(:comment_on_your_comment)).to be_valid
-    end
-  end
-
   describe "all notification subclass factories" do
     before do
       # Make sure that we can find all notification
@@ -26,6 +20,20 @@ RSpec.describe Notification, type: :model do
           expect(build(notification_subclass.model_name.element.to_sym)).to be_valid
         end
       end
+    end
+  end
+
+  describe "make_notifications_on" do
+    it "makes a comment on your comment and comment on your idea notification on created comment activity" do
+      idea = create(:idea)
+      parent_comment = create(:comment, idea: idea)
+      child_comment = create(:comment, parent: parent_comment)
+      activity = create(:activity, item: child_comment, action: 'created')
+
+      notifications = Notifications::CommentOnYourComment.make_notifications_on activity
+      expect(notifications).to be_present
+      notifications = Notifications::CommentOnYourIdea.make_notifications_on activity
+      expect(notifications).to be_present
     end
   end
 end
