4984f6676872bb6752d9580c080d8123dde03eb3
Mathieu Dubois <mathieu@citizenlab.co>
2018-04-10 12:19:22 +0200

    WIP - throttle HTML conversion for performance

--- app/components/UI/FormikEditorMultiloc/index.tsx
+++ app/components/UI/FormikEditorMultiloc/index.tsx
@@ -1,15 +1,23 @@
 // Libraries
 import React from 'react';
 import { FieldProps } from 'formik';
+import { throttle } from 'lodash';
 
-import { getEditorStateFromHtmlString, getHtmlStringFromEditorState } from 'utils/editorTools';
+import { getHtmlStringFromEditorState } from 'utils/editorTools';
 
 import EditorMultiloc, { Props as VanillaProps } from 'components/UI/EditorMultiloc';
-import { MultilocEditorState } from 'typings';
+import { MultilocEditorState, Locale } from 'typings';
 
 interface State {
   editorStateMultiloc: MultilocEditorState;
 }
+
+// Pure function to update Formik state that will be throttled for performance
+function formikUpdate(multilocEditorState, locale, existingValues, fieldName, setFieldValue) {
+  const newValues = {...existingValues, [locale]: getHtmlStringFromEditorState(multilocEditorState[locale])}
+  setFieldValue(fieldName, newValues);
+}
+const throttledFormikUpdate = throttle(formikUpdate, 500);
 
 class FormikEditorMultiloc extends React.Component<FieldProps & VanillaProps, State> {
   constructor(props) {
@@ -20,11 +28,13 @@ class FormikEditorMultiloc extends React.Component<FieldProps & VanillaProps, St
     };
   }
 
-  handleEditorOnChange = (value, locale) => {
-    const editorStateMultiloc = { ...this.state.editorStateMultiloc, [locale]: value };
-
+  handleEditorOnChange = (values: MultilocEditorState, locale: Locale) => {
+    // Update state
+    const editorStateMultiloc = values;
     this.setState({ editorStateMultiloc });
-    this.props.form.setFieldValue(this.props.field.name, getHtmlStringFromEditorState(value));
+
+    // Throttled update to Formik
+    if (values) throttledFormikUpdate(values, locale, this.props.field.value, this.props.field.name, this.props.form.setFieldValue);
   }
 
   render() {
