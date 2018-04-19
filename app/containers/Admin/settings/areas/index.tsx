13caec11cd5e285878bfc0dbcafaa76d6809ce1b
Unknown <sara@citizenlab.co>
2018-04-19 20:39:18 +0200

    display the list of areas WIP

--- app/containers/Admin/settings/areas/index.tsx
+++ app/containers/Admin/settings/areas/index.tsx
@@ -6,6 +6,10 @@ import { Section, SectionTitle } from 'components/admin/Section';
 import { List, Row } from 'components/admin/ResourceList';
 import { FormattedMessage } from 'utils/cl-intl';
 import Button from 'components/UI/Button';
+import GetAreas, { GetAreasChildProps } from 'resources/GetAreas';
+import T from 'components/T';
+
+import { isEmpty } from 'lodash';
 
 const TextCell = styled.div`
   color: #333;
@@ -14,17 +18,22 @@ const TextCell = styled.div`
   line-height: 20px;
 `;
 
-export default class AdminSettingsAreas extends React.PureComponent {
+
+class AreasList extends React.PureComponent{
   render() {
+    const { areas } = this.props;
+    const isAreas = areas !== null && areas.length > 0;
     return (
       <Section key={'signup_fields'}>
         <SectionTitle>
           <FormattedMessage {...messages.titleAreas} />
         </SectionTitle>
+        { isAreas &&
         <List>
-          <Row>
+          { areas.map(area =>
+          <Row key={area.id}>
             <TextCell className="expand">
-              <FormattedMessage {...messages.titleAreas} />
+              <T value={area.attributes.title_multiloc}/>
             </TextCell>
             <Button style="secondary" circularCorners={false} icon="edit">
               <FormattedMessage {...messages.editButtonLabel} />
@@ -32,9 +41,15 @@ export default class AdminSettingsAreas extends React.PureComponent {
             <Button style="text" circularCorners={false} icon="delete">
               <FormattedMessage {...messages.deleteButtonLabel} />
             </Button>
-          </Row>
-        </List>
+          </Row>)}
+        </List>}
       </Section>
     );
   }
 }
+
+export default () => (
+  <GetAreas>
+    {areas => (<AreasList areas={areas} />)}
+  </GetAreas>
+);
