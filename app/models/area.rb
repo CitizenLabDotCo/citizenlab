0fc2c942623da9f3b2180b4716ecb6f49138706f
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-04-19 18:04:43 +0200

    minor

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,9 +1,9 @@
 class Area < ApplicationRecord
-  validates :title_multiloc, presence: true, multiloc: {presence: true}
-  validates :description_multiloc, multiloc: {presence: false}
-
   has_many :projects, through: :areas_projects
   has_many :areas_projects, dependent: :destroy
   has_many :ideas, through: :areas_ideas
   has_many :areas_ideas, dependent: :destroy
+
+  validates :title_multiloc, presence: true, multiloc: {presence: true}
+  validates :description_multiloc, multiloc: {presence: false}
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,10 +1,10 @@
 class Topic < ApplicationRecord
-  validates :title_multiloc, presence: true, multiloc: {presence: true}
-  validates :description_multiloc, multiloc: {presence: false}
-  #TODO Settle on iconset and validate icon to be part of it
-
   has_many :projects, through: :projects_topics
   has_many :projects_topics, dependent: :destroy
   has_many :ideas, through: :ideas_topics
   has_many :ideas_topics, dependent: :destroy
+
+  validates :title_multiloc, presence: true, multiloc: {presence: true}
+  validates :description_multiloc, multiloc: {presence: false}
+  #TODO Settle on iconset and validate icon to be part of it
 end
