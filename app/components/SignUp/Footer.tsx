b426ecd9aabb8dc7c1db30786221b7681f5ce4e6
David <david@citizenlab.co>
2018-04-23 15:18:11 +0200

    tac checkboxes WIP

--- app/components/SignUp/Footer.tsx
+++ app/components/SignUp/Footer.tsx
@@ -3,7 +3,7 @@ import Rx from 'rxjs/Rx';
 import { get } from 'lodash';
 
 // components
-import Button from 'components/UI/Button';
+// import Button from 'components/UI/Button';
 import FeatureFlag from 'components/FeatureFlag';
 
 // services
@@ -22,6 +22,10 @@ import { AUTH_PATH } from 'containers/App/constants';
 
 // style
 import styled from 'styled-components';
+
+// logos
+const googleLogo = require('./google.svg') as string;
+const facebookLogo = require('./facebook.svg') as string;
 
 const Container = styled.div`
   width: 100%;
@@ -42,32 +46,64 @@ const FooterContent = styled.div`
   flex-direction: column;
 `;
 
-const SocialSignInButton = styled(Button)`
-  .Button {
-    background: #fff !important;
-    border: solid 1px #eaeaea !important;
+const SocialSignInButtons = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SocialSignInButton = styled.div`
+  width: 100%;
+  height: 60px;
+  padding: 15px;
+  margin-bottom: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #fff;
+  border-radius: 5px;
+  border: solid 1px #e4e4e4;
+  cursor: pointer;
+
+  &.google:hover,
+  &.google.active {
+    border-color: #2a81f4;
   }
 
-  &:hover {
-    .Button {
-      border-color: #ccc !important;
-    }
+  &.facebook:hover,
+  &.facebook.active {
+    border-color: #345697;
   }
 `;
 
-const GoogleLogin = SocialSignInButton.extend`
-  margin-right: 15px;
+// const SocialSignInButtonLogo = styled.div``;
 
-  .Button {
-    color: #518EF8 !important;
-  }
-`;
+// const SocialSignInButton = styled(Button)`
+//   .Button {
+//     background: #fff !important;
+//     border: solid 1px #eaeaea !important;
+//   }
 
-const FacebookLogin = SocialSignInButton.extend`
-  .Button {
-    color: #4B6696 !important;
-  }
-`;
+//   &:hover {
+//     .Button {
+//       border-color: #ccc !important;
+//     }
+//   }
+// `;
+
+// const GoogleLogin = SocialSignInButton.extend`
+//   margin-right: 15px;
+
+//   .Button {
+//     color: #518EF8 !important;
+//   }
+// `;
+
+// const FacebookLogin = SocialSignInButton.extend`
+//   .Button {
+//     color: #4B6696 !important;
+//   }
+// `;
 
 const SocialSignInText = styled.div`
   color: ${(props) => props.theme.colors.label};
@@ -76,11 +112,6 @@ const SocialSignInText = styled.div`
   line-height: 20px;
   margin-left: 4px;
   margin-bottom: 20px;
-`;
-
-const SocialSignInButtons = styled.div`
-  width: 100%;
-  display: flex;
 `;
 
 interface InputProps {
@@ -143,31 +174,20 @@ class Footer extends React.PureComponent<Props & InjectedIntlProps, State> {
       return (
         <Container>
           <Separator />
-
           <FooterContent>
             <SocialSignInText>
               {formatMessage(messages.orSignUpWith)}
             </SocialSignInText>
             <SocialSignInButtons>
               <FeatureFlag name="google_login">
-                <GoogleLogin
-                  text="Google"
-                  style="primary"
-                  size="1"
-                  icon="google-colored"
-                  onClick={this.handleOnSSOClick('google')}
-                  circularCorners={true}
-                />
+                <SocialSignInButton className={`google ${}`} onClick={this.handleOnSSOClick('google')}>
+                  <img src={googleLogo} height="31px" role="presentation" alt="" />
+                </SocialSignInButton>
               </FeatureFlag>
               <FeatureFlag name="facebook_login">
-                <FacebookLogin
-                  text="Facebook"
-                  style="primary"
-                  size="1"
-                  icon="facebook-blue"
-                  onClick={this.handleOnSSOClick('facebook')}
-                  circularCorners={true}
-                />
+                <SocialSignInButton className={`facebook ${}`} onClick={this.handleOnSSOClick('google')}>
+                  <img src={facebookLogo} height="23px" role="presentation" alt="" />
+                </SocialSignInButton>
               </FeatureFlag>
             </SocialSignInButtons>
           </FooterContent>
