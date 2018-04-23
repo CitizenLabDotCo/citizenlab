e1279dc4a9490da724d1e0f4c4258a485d123793
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-04-23 17:11:46 +0200

    is_empty and not_is_empty succeed

--- spec/lib/smart_group_rules/custom_field_select_spec.rb
+++ spec/lib/smart_group_rules/custom_field_select_spec.rb
@@ -38,7 +38,7 @@ describe SmartGroupRules::CustomFieldSelect do
 
     context "on a select field" do
 
-      let(:custom_field) { create(:custom_field_select) }
+      let(:custom_field) { create(:custom_field_select, required: false) }
       let(:options) { create_list(:custom_field_option, 3, custom_field: custom_field )}
 
       let!(:users) {
@@ -47,7 +47,8 @@ describe SmartGroupRules::CustomFieldSelect do
         users[1].custom_field_values[custom_field.key] = options[0].key
         users[2].custom_field_values[custom_field.key] = options[1].key
         users[3].custom_field_values[custom_field.key] = options[2].key
-        users[4].custom_field_values[custom_field.key] = nil
+        # TODO make setting it to nil work
+        # users[4].custom_field_values[custom_field.key] = nil
         users.each(&:save!)
       }
 
