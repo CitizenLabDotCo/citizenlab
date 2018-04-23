40bf6e53e456afd9ca4aaa84a6434bf5e1fcee8e
Brent Guffens <brentguf@users.noreply.github.com>
2018-04-23 14:11:10 +0200

    Area edit form logic

--- app/containers/Admin/settings/areas/Edit/index.tsx
+++ app/containers/Admin/settings/areas/Edit/index.tsx
@@ -1,24 +1,71 @@
 import React from 'react';
-import { browserHistory } from 'react-router';
-
-import GoBackButton from 'components/UI/GoBackButton';
-import { Section, SectionTitle } from 'components/admin/Section';
+import { withRouter, WithRouterProps, browserHistory } from 'react-router';
+import { Formik } from 'formik';
 import { FormattedMessage } from 'utils/cl-intl';
 import messages from '../messages';
 
-export default class Edit extends React.PureComponent {
+import GetArea, { GetAreaChildProps } from 'resources/GetArea';
+
+import GoBackButton from 'components/UI/GoBackButton';
+import { Section, SectionTitle } from 'components/admin/Section';
+import AreaForm, { FormValues } from '../AreaForm';
+import PageWrapper from 'components/admin/PageWrapper';
+
+interface InputProps {}
+
+interface DataProps {
+  area: GetAreaChildProps
+}
+
+interface Props extends InputProps, DataProps {}
+
+class Edit extends React.PureComponent<Props> {
+  
+  handleSubmit = () => {
+    return;
+  }
+  
   goBack = () => {
     browserHistory.push('/admin/settings/areas');
   }
 
+  renderFn = (props) => {
+    return <AreaForm {...props} />;
+  }
+
+  initialValues = () => {
+    const { area } = this.props;
+    return area && ({
+      title_multiloc: area.attributes.title_multiloc,
+      description_multiloc: area.attributes.description_multiloc
+    });
+  }
+
   render() {
+    const { area } = this.props;
     return (
-      <Section>
-        <GoBackButton onClick={this.goBack} />
-        <SectionTitle>
-          <FormattedMessage {...messages.editFormTitle} />
-        </SectionTitle>
-      </Section>
+      <>
+        <Section>
+          <GoBackButton onClick={this.goBack} />
+          <SectionTitle>
+            <FormattedMessage {...messages.editFormTitle} />
+          </SectionTitle>
+        </Section>
+        <PageWrapper>
+        { area && <Formik 
+          initialValues={this.initialValues()}
+          render={this.renderFn}
+          onSubmit={this.handleSubmit}
+          validate={AreaForm.validate}
+        />}
+      </PageWrapper>
+     </>
     )
   }
 }
+
+export default withRouter((inputProps: InputProps & WithRouterProps ) => (
+  <GetArea id={inputProps.params.areaId} >
+    {area => (<Edit area={area} />)}
+  </GetArea>
+));
