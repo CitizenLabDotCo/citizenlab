ece40c38606b7de62ea4f4cf3b83977ca3cff8be
Unknown <sara@citizenlab.co>
2018-04-23 15:02:19 +0200

    switched description from textArea to Editor

--- app/containers/Admin/settings/areas/AreaForm/index.tsx
+++ app/containers/Admin/settings/areas/AreaForm/index.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { isEmpty, values as getValues, every } from 'lodash';
-import { Form, Field, InjectedFormikProps, FormikErrors } from 'formik';
+
 import { FormattedMessage } from 'utils/cl-intl';
-import { Multiloc } from 'typings';
 import messages from '../messages';
 
+import { Form, Field, InjectedFormikProps, FormikErrors } from 'formik';
 import FormikInputMultiloc from 'components/UI/FormikInputMultiloc';
-import FormikTextAreaMultiloc from 'components/UI/FormikTextAreaMultiloc';
+import FormikEditorMultiloc from 'components/UI/FormikEditorMultiloc';
 import FormikSubmitWrapper from 'components/admin/FormikSubmitWrapper';
 
 import { Section, SectionField } from 'components/admin/Section';
 import Error from 'components/UI/Error';
 
+//Typings
+import { Multiloc } from 'typings';
+export interface Props {}
 export interface FormValues {
   title_multiloc: Multiloc;
   description_multiloc: Multiloc;
 }
-
-export interface Props {}
 
 export default class AreaForm extends React.Component<InjectedFormikProps<Props, FormValues>> {
 
@@ -27,13 +28,12 @@ export default class AreaForm extends React.Component<InjectedFormikProps<Props,
     if (every(getValues(values.title_multiloc), isEmpty)) {
       errors.title_multiloc = (errors.title_multiloc || []).concat({ error: 'blank' });
     }
-
     return errors;
   }
 
   render() {
     const { isSubmitting, errors, isValid, touched } = this.props;
-    
+
     return (
       <Form>
         <Section>
@@ -42,7 +42,7 @@ export default class AreaForm extends React.Component<InjectedFormikProps<Props,
               name="title_multiloc"
               component={FormikInputMultiloc}
               label={<FormattedMessage {...messages.fieldTitle} />}
-            /> 
+            />
             {touched.title_multiloc && <Error
               fieldName="title_multiloc"
               apiErrors={errors.title_multiloc}
@@ -50,9 +50,26 @@ export default class AreaForm extends React.Component<InjectedFormikProps<Props,
           </SectionField>
           <SectionField>
             <Field
+              component={FormikEditorMultiloc}
+              id="project-description"
               name="description_multiloc"
-              component={FormikTextAreaMultiloc}
               label={<FormattedMessage {...messages.fieldDescription} />}
+              toolbarConfig={{
+                options: ['inline', 'list', 'link', 'blockType'],
+                inline: {
+                  options: ['bold', 'italic'],
+                },
+                list: {
+                  options: ['unordered', 'ordered'],
+                },
+                blockType: {
+                  inDropdown: false,
+                  options: ['Normal', 'H1'],
+                  className: undefined,
+                  component: undefined,
+                  dropdownClassName: undefined,
+                }
+              }}
             />
             {touched.description_multiloc && <Error
               fieldName="description_multiloc"
