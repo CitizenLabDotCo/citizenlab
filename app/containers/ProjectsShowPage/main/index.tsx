670bf393e7a7899c4bc18f36419581a9985cf0e9
David <david@citizenlab.co>
2018-04-23 10:59:34 +0200

    resource components WIP

--- app/containers/ProjectsShowPage/main/index.tsx
+++ app/containers/ProjectsShowPage/main/index.tsx
@@ -9,34 +9,18 @@ import ProjectInfoPage from '../info';
 import {  getProjectUrl } from 'services/projects';
 
 // resources
-import GetProject, { GetProjectChildProps } from 'resources/GetProject';
+import GetProject from 'resources/GetProject';
 
-interface InputProps {}
+export default withRouter((props: WithRouterProps) => (
+  <GetProject slug={props.params.slug}>
+    {project => {
+      if (project) {
+        const redirectUrl = getProjectUrl(project);
+        browserHistory.replace(redirectUrl);
+        return project.attributes.process_type === 'timeline' ? <ProjectTimelinePage /> : <ProjectInfoPage />;
+      }
 
-interface DataProps {
-  project: GetProjectChildProps;
-}
-
-interface Props extends InputProps, DataProps {}
-
-interface State {}
-
-class ProjectMainPage extends React.PureComponent<Props & WithRouterProps, State> {
-  render() {
-    const { project } = this.props;
-
-    if (project) {
-      const redirectUrl = getProjectUrl(project);
-      browserHistory.replace(redirectUrl);
-      return project.attributes.process_type === 'timeline' ? <ProjectTimelinePage /> : <ProjectInfoPage />;
-    }
-
-    return null;
-  }
-}
-
-export default withRouter((inputProps: InputProps & WithRouterProps) => (
-  <GetProject slug={inputProps.params.slug}>
-    {project => <ProjectMainPage {...inputProps} project={project} />}
+      return null;
+    }}
   </GetProject>
 ));
