100d611cd614d9c58d359514b504c877a6e0e9d3
Sara <sara@citizenlab.co>
2018-04-26 12:08:07 +0200

    fixed status of submit button in project description form

--- e2e/tests/admin/projects.js
+++ e2e/tests/admin/projects.js
@@ -3,7 +3,7 @@ const hash = crypto.randomBytes(5).toString('hex');
 const afterEach = require('../../updateBSStatus');
 
 module.exports = {
-  '@tags': ['city', 'projects', 'projects-basics', 'bleh'],
+  '@tags': ['city', 'projects', 'projects-basics', 'sara'],
   afterEach,
   projectsList: (browser) => {
     const signinPage = browser.page.signin();
