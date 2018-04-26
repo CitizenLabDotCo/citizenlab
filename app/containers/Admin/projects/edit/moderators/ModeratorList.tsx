6dd0228722a9a409d4a4059591bed47f6938b052
Sara <sara@citizenlab.co>
2018-04-26 16:08:46 +0200

    display moderator list WIP

---
+++ app/containers/Admin/projects/edit/moderators/ModeratorList.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+import { FormattedMessage } from 'utils/cl-intl';
+import messages from '../../messages';
+
+import Button from 'components/UI/Button';
+import { List, Row } from 'components/admin/ResourceList';
+import Avatar from 'components/Avatar';
+
+import { IUserData } from 'services/users';
+
+const StyledAvatar = styled(Avatar)`
+  width: 2rem;
+  height: 2rem;
+`;
+
+interface Props {
+  users: IUserData[] | null;
+}
+
+export default class ModeratorList extends React.PureComponent<Props>{
+  render() {
+    const moderators = this.props.users;
+    return (
+      <List>
+        { moderators && moderators.map(moderator =>
+          <Row key={moderator.id}>
+            <StyledAvatar userId={moderator.id} size="small" />
+            <div className="expand">{`${moderator.attributes.first_name} ${moderator.attributes.last_name}`}</div>
+            <div className="expand">{moderator.attributes.email}</div>
+            <Button
+              style="text"
+              circularCorners={false}
+              icon="delete"
+            >
+              <FormattedMessage {...messages.deleteModeratorLabel} />
+            </Button>
+          </Row>
+        )}
+      </List>
+    );
+  }
+}
--- app/containers/Admin/projects/edit/moderators/index.tsx
+++ app/containers/Admin/projects/edit/moderators/index.tsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import { withRouter, WithRouterProps } from 'react-router';
 
-export default () => {
-  return <div>Hi</div>;
-};
+import { Section, SectionTitle } from 'components/admin/Section';
+
+import { FormattedMessage } from 'utils/cl-intl';
+import messages from '../../messages';
+
+import ModeratorSelect from './ModeratorSelect';
+import ModeratorList from './ModeratorList';
+
+import GetUsers, { GetUsersChildProps } from 'resources/GetUsers';
+
+interface InputProps {
+  projectId: string;
+}
+
+interface Props extends InputProps, GetUsersChildProps {}
+
+class Moderators extends React.PureComponent<Props>{
+  render() {
+    console.log(this.props.projectId);
+    return (
+      <Section>
+        <SectionTitle>
+          <FormattedMessage {...messages.moderatorsTabTitle} />
+        </SectionTitle>
+        <ModeratorSelect />
+        <ModeratorList users={this.props.usersList} />
+      </Section>
+    );
+  }
+}
+export default withRouter((inputProps: InputProps & WithRouterProps) => (
+  <GetUsers>
+    {users => <Moderators {... users} projectId={inputProps.params.slug} />}
+  </GetUsers>
+));
