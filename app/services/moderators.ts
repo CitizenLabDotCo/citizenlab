6cf0d4450aa0899ca2cc670bfb10703f635d9efa
Unknown <sara@citizenlab.co>
2018-04-27 14:58:11 +0200

    CRUD for moderators

--- app/services/moderators.ts
+++ app/services/moderators.ts
@@ -1,11 +1,33 @@
 import { API_PATH } from 'containers/App/constants';
-import streams from 'utils/streams';
+import streams, { IStreamParams } from 'utils/streams';
 import { IUsers } from 'services/users';
+import { API } from 'typings';
+
+export interface FoundUser {
+  id: string;
+  type: 'users';
+  attributes: {
+    first_name: string;
+    last_name: string;
+    slug: string;
+    avatar: API.ImageSizes;
+    is_moderator: boolean;
+    email: string;
+  };
+}
 
 export function moderatorsStream(projectId: string) {
   return streams.get<IUsers>({ apiEndpoint: `${API_PATH}/projects/${projectId}/moderators` });
 }
 
 export async function deleteModerator(projectId: string, moderatorId: string) {
-  return streams.delete(`${API_PATH}/projects/${projectId}/moderators`, moderatorId);
+  return streams.delete(`${API_PATH}/projects/${projectId}/moderators/${moderatorId}`, moderatorId);
+}
+
+export function findMembership(projectId: string, streamParams: IStreamParams | null = null) {
+  return streams.get<{data: FoundUser[]}>({ apiEndpoint: `${API_PATH}/projects/${projectId}/moderators/users_search`, ...streamParams, cacheStream: false });
+}
+
+export function addMembership(projectId: string, user_id: string) {
+  return streams.add(`${API_PATH}/projects/${projectId}/moderators`, { moderator: { user_id } });
 }
