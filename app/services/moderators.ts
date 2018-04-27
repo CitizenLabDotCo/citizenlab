80eecd71f045ad892b7d5f6b89a33b79dcbf7066
Unknown <sara@citizenlab.co>
2018-04-27 11:43:32 +0200

    Delete button functional + first step towards error handling in GetModerator component

--- app/services/moderators.ts
+++ app/services/moderators.ts
@@ -2,6 +2,10 @@ import { API_PATH } from 'containers/App/constants';
 import streams from 'utils/streams';
 import { IUsers } from 'services/users';
 
-export function moderatorsStream(projectId) {
+export function moderatorsStream(projectId: string) {
   return streams.get<IUsers>({ apiEndpoint: `${API_PATH}/projects/${projectId}/moderators` });
+}
+
+export async function deleteModerator(projectId: string, moderatorId: string) {
+  return streams.delete(`${API_PATH}/projects/${projectId}/moderators`, moderatorId);
 }
