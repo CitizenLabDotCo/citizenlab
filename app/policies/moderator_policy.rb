407c0c3d98b84c6cdeb4cf6d60aaa06ae8f2d35c
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-04-27 11:50:14 +0200

    moderator user_search

--- app/policies/moderator_policy.rb
+++ app/policies/moderator_policy.rb
@@ -16,10 +16,16 @@ class ModeratorPolicy < ApplicationPolicy
     admin_or_moderator?
   end
 
+  def users_search?
+    admin_or_moderator?
+  end
+
 
   private
 
   def admin_or_moderator?
+    # In the case of moderator, the user must be moderator of that project
+    # (not just of any project).
     user&.active? && (user.admin? || user.project_moderator?(record.project_id))
   end
 
