0a361e82cb4c0f9b0a55a68b91da532fe93ac86e
Unknown <sara@citizenlab.co>
2018-05-02 11:52:20 +0200

    Little styling improvement

--- app/containers/Admin/projects/edit/permissions/Moderators.tsx
+++ app/containers/Admin/projects/edit/permissions/Moderators.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Label from 'components/UI/Label';
-import { SectionField } from 'components/admin/Section';
 
 import { FormattedMessage } from 'utils/cl-intl';
 import messages from '../../messages';
@@ -20,17 +20,22 @@ interface Props extends InputProps {
   moderators: GetModeratorsChildProps;
 }
 
+const Container = styled.div`
+  width: 100%;
+  margin-bottom: 20px;
+`;
+
 export default class Moderators extends React.PureComponent<Props>{
   render() {
     const { moderators, projectId } = this.props;
     return (
-      <SectionField>
+      <Container>
         <Label>
           <FormattedMessage {...messages.moderatorsSectionTitle} />
         </Label>
         <UserSearch resourceId={projectId} messages={messages} searchFunction={findMembership} addFunction={addMembership} />
         <ModeratorList moderators={moderators} projectId={projectId}/>
-      </SectionField>
+      </Container>
     );
   }
 }
