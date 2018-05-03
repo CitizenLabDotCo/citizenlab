e26e01da1867d86a602a230b0d3badc69a1bc247
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-05-03 21:41:15 +0200

    Made update member count on group edit synchronous

--- app/jobs/update_member_count_job.rb
+++ app/jobs/update_member_count_job.rb
@@ -1,9 +1,13 @@
 class UpdateMemberCountJob < ApplicationJob
   queue_as :default
 
-  def perform
-    Group.where(membership_type: 'rules').each do |group|
+  def perform group=nil
+    if group
       group.update_memberships_count!
+    else
+      Group.where(membership_type: 'rules').each do |group|
+        group.update_memberships_count!
+      end
     end
   end
 
