5b33c60b5675ba6b201d88f46be3fbc12a6b962d
Unknown <sara@citizenlab.co>
2018-05-04 14:00:16 +0200

    improvements

--- e2e/tests/admin/invitations.js
+++ e2e/tests/admin/invitations.js
@@ -24,26 +24,40 @@ module.exports = {
     .assert.containsText('table', email);
   },
   useInvite: (browser) => {
+    const signinPage = browser.page.signin();
+
+    signinPage
+    .navigate()
+    .signin('koen@citizenlab.co', 'testtest');
+
     browser
     .url(`http://${process.env.ROOT_URL}/admin/users/invitations`)
     .waitForElementVisible('table')
-    .getAttribute('table tbody tr', 'class', (attr) => {
-      browser.url(`http://${process.env.ROOT_URL}/invite?token=${attr.value}`)
-      .waitForElementVisible('#e2e-signup-step1')
-      .assert.value('#email', email)
-      .setValue('#firstName', `Test ${hash}`)
-      .setValue('#lastName', `Account ${hash}`)
-      .setValue('#password', '123456789')
-      .click('.e2e-terms-and-conditions > :first-child')
-      .click('#e2e-signup-step1-button')
-      .waitForElementVisible('#e2e-signup-step2-button')
-      .pause(500)
-      .click('#e2e-signup-step2-button')
-      .waitForElementVisible('#e2e-landing-page')
-      .click('#e2e-user-menu-container')
-      .waitForElementVisible('#e2e-user-menu-dropdown')
-      .waitForElementVisible('#e2e-sign-out-link')
-      .end();
+    .getAttribute('table tbody tr', 'class', (token) => {
+      browser
+      .getText('table tbody tr td', (result) => {
+        browser
+        .click('#e2e-user-menu-container')
+        .waitForElementVisible('#e2e-user-menu-dropdown')
+        .waitForElementVisible('#e2e-sign-out-link')
+        .click('#e2e-sign-out-link')
+        .url(`http://${process.env.ROOT_URL}/invite?token=${token.value}`)
+        .waitForElementVisible('#e2e-signup-step1')
+        .assert.value('#email', result.value)
+        .setValue('#firstName', `Test ${hash}`)
+        .setValue('#lastName', `Account ${hash}`)
+        .setValue('#password', '123456789')
+        .click('.e2e-terms-and-conditions > :first-child')
+        .click('#e2e-signup-step1-button')
+        .waitForElementVisible('#e2e-signup-step2-button')
+        .pause(500)
+        .click('#e2e-signup-step2-button')
+        .waitForElementVisible('#e2e-landing-page')
+        .click('#e2e-user-menu-container')
+        .waitForElementVisible('#e2e-user-menu-dropdown')
+        .waitForElementVisible('#e2e-sign-out-link')
+        .end();
+      });
     });
   },
 };
