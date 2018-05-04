2d624ae5a6883ddb793aedb2ffb3a4bf8687e45d
Sébastien Hoorens <shoorens@ai.vub.ac.be>
2018-05-04 17:18:04 +0200

    custom field date (finally) done with specs on invalid and valid date format rules, custom field number still to do as wel as better registration completed at

--- lib/smart_group_rules/custom_field_number.rb
+++ lib/smart_group_rules/custom_field_number.rb
@@ -1,17 +1,16 @@
 module SmartGroupRules
-  class CustomFieldDate
+  class CustomFieldName
 
     PREDICATE_VALUES = %w(is_before is_exactly is_after is_empty not_is_empty)
     VALUELESS_PREDICATES = %w(is_empty not_is_empty)
 
-    RULE_TYPE = "custom_field_date"
+    RULE_TYPE = "custom_field_number"
 
     include CustomFieldRule
 
     validates :custom_field_id, inclusion: { in: proc { CustomField.where(input_type: 'date').map(&:id) } }
     validates :value, absence: true, unless: :needs_value?
     validates :value, presence: true, if: :needs_value?
-
 
     def self.to_json_schema
       [   
@@ -32,9 +31,7 @@ module SmartGroupRules
               "enum": PREDICATE_VALUES - VALUELESS_PREDICATES,
             },
             "value" => {
-              "type": "string",
-              "format": "date",
-              "pattern": "[0-9]{4}-[0-9]{2}-[0-9]{2}$"
+              "type": "string"
             }
           },
         },
@@ -62,11 +59,7 @@ module SmartGroupRules
     def initialize custom_field_id, predicate, value=nil
       self.custom_field_id = custom_field_id
       self.predicate = predicate
-      if value.class == Time
-        self.value = value.to_date.to_s
-      else
-        self.value = value.to_s
-      end
+      self.value = value.to_s
     end
 
     def filter users_scope
