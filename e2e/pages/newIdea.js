838d76fc1eb51b418f38954ec9d63b95fcb32e13
Unknown <sara@citizenlab.co>
2018-05-04 16:48:48 +0200

    fixed tests on little res for comments

--- e2e/pages/newIdea.js
+++ e2e/pages/newIdea.js
@@ -5,15 +5,16 @@ const newIdeaCommands = {
     // Select project
     .waitForElementVisible('.e2e-project-card.e2e-open-project')
     .click('.e2e-project-card.e2e-open-project');
-    this.api.execute('window.scrollTo(0,document.body.scrollHeight);');
-    this.click('.e2e-submit-project-select-form')
-    .waitForElementVisible('@form')
+    this.api.execute('window.scrollTo(0,document.body.scrollHeight);')
+    .click('.e2e-submit-project-select-form')
+    .click('.e2e-submit-project-select-form-mobile');
+    this.waitForElementPresent('@form')
 
     // Fill in the form
     .setValue('@title', title)
     .setValue('@content', content);
-
-    return this.click('.e2e-submit-form').waitForElementNotPresent('@form');
+    this.api.execute('window.scrollTo(0,document.body.scrollHeight);');
+    return this.click('@submit').click('.e2e-submit-idea-form-mobile');
   },
 };
 
