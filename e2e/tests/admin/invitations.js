05a2c097527712159990e826aff30e8205acd54c
Unknown <sara@citizenlab.co>
2018-05-04 13:43:26 +0200

    added invites test

---
+++ e2e/tests/admin/invitations.js
@@ -0,0 +1,49 @@
+const crypto = require('crypto');
+const hash = crypto.randomBytes(5).toString('hex');
+const email = `test-${hash}@citizenlab.co`;
+const afterEach = require('../../updateBSStatus');
+
+module.exports = {
+  '@tags': ['city', 'invitations'],
+  afterEach,
+  createInvite: (browser) => {
+    const signinPage = browser.page.signin();
+
+    signinPage
+    .navigate()
+    .signin('koen@citizenlab.co', 'testtest');
+
+    browser
+    .url(`http://${process.env.ROOT_URL}/admin/users/invitations`)
+    .waitForElementVisible('.e2e-manual-invite')
+    .click('.e2e-manual-invite')
+    .waitForElementVisible('#emails')
+    .setValue('#emails', email)
+    .click('#invitation button')
+    .pause(300)
+    .assert.containsText('table', email);
+  },
+  useInvite: (browser) => {
+    browser
+    .url(`http://${process.env.ROOT_URL}/admin/users/invitations`)
+    .waitForElementVisible('table')
+    .getAttribute('table tbody tr', 'class', (attr) => {
+      browser.url(`http://${process.env.ROOT_URL}/invite?token=${attr.value}`)
+      .waitForElementVisible('#e2e-signup-step1')
+      .assert.value('#email', email)
+      .setValue('#firstName', `Test ${hash}`)
+      .setValue('#lastName', `Account ${hash}`)
+      .setValue('#password', '123456789')
+      .click('.e2e-terms-and-conditions > :first-child')
+      .click('#e2e-signup-step1-button')
+      .waitForElementVisible('#e2e-signup-step2-button')
+      .pause(500)
+      .click('#e2e-signup-step2-button')
+      .waitForElementVisible('#e2e-landing-page')
+      .click('#e2e-user-menu-container')
+      .waitForElementVisible('#e2e-user-menu-dropdown')
+      .waitForElementVisible('#e2e-sign-out-link')
+      .end();
+    });
+  },
+};
