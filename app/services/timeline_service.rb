21ed222fb6242e7bb9b7c3e957f554f7b314ddda
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-05-04 17:30:38 +0200

    CL2-1660 Include last day of phase as active

--- app/services/timeline_service.rb
+++ app/services/timeline_service.rb
@@ -1,21 +1,24 @@
 class TimelineService
 
   def future_phases project, time=Time.now
+    date = time.to_date
     project.phases.select do |phase|
-      phase.start_at > time
+      phase.start_at > date
     end
   end
 
   def past_phases project, time=Time.now
+    date = time.to_date
     project.phases.select do |phase|
-      phase.end_at < time
+      phase.end_at < date
     end
   end
 
   def current_phase project, time=Time.now
+    date = time.to_date
     if project.timeline?
       project.phases.find do |phase|
-        phase.start_at < time && phase.end_at > time
+        phase.start_at < date && phase.end_at >= date.to_date
       end
     end
   end
