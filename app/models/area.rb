e60236fc3681bd30dd5af1a9415b9d545694c713
Sébastien Hoorens <sebastien@citizenlab.co>
2018-05-07 17:11:04 +0200

    area, event, group, ideastatus and topic titles stripped

--- app/models/area.rb
+++ app/models/area.rb
@@ -6,4 +6,16 @@ class Area < ApplicationRecord
 
   validates :title_multiloc, presence: true, multiloc: {presence: true}
   validates :description_multiloc, multiloc: {presence: false}
+
+  before_validation :strip_title
+
+
+  private
+
+  def strip_title
+    self.title_multiloc.each do |key, value|
+      self.title_multiloc[key] = value.strip
+    end
+  end
+  
 end
