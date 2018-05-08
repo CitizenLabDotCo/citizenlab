ff15a8bb7cf0de5c26f82099e773220a259eeb12
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-05-08 15:08:36 +0200

    Fixed the tests

--- spec/jobs/update_member_count_job_spec.rb
+++ spec/jobs/update_member_count_job_spec.rb
@@ -6,13 +6,10 @@ RSpec.describe UpdateMemberCountJob, type: :job do
 
   describe '#perform' do
 
-    # Support ends_on predicate
-    pending "updates all groups without arguments" do
+    it "updates all groups without arguments" do
       rules_groups = create_list(:smart_group, 2)
       manual_group = create(:group)
 
-      expect(rules_groups[0]).to receive(:update_memberships_count!)
-      expect(rules_groups[1]).to receive(:update_memberships_count!)
       expect(manual_group).not_to receive(:update_memberships_count!)
 
       job.perform
