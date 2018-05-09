354f4155c1917e68eb248748dac930c12ebb7254
Sara <sara@citizenlab.co>
2018-05-09 12:15:05 +0200

    scrolling manually

--- e2e/tests/single-idea.js
+++ e2e/tests/single-idea.js
@@ -1,7 +1,7 @@
 const afterEach = require('../updateBSStatus');
 
 module.exports = {
-  '@tags': ['citizen', 'ideas'],
+  '@tags': ['citizen', 'ideas', 'single-idea'],
   afterEach,
   modalIdea: (browser) => {
     let ideaUrl;
@@ -10,7 +10,8 @@ module.exports = {
     .url(`http://${process.env.ROOT_URL}`)
     .waitForElementVisible('#e2e-ideas-container')
     .waitForElementVisible('#e2e-ideas-list>:first-child')
-    .click('#e2e-ideas-list :first-child')
+    .execute('var element = document.getElementsByClassName("e2e-idea-card");element[0].scrollIntoView(true);')
+    .click('.e2e-idea-card')
     .waitForElementVisible('#e2e-fullscreenmodal-content')
     .waitForElementVisible('#e2e-idea-show')
     .url((location) => {
