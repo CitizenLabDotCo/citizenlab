6e3d241af00373a2b65211136a2bd2f7aa081afc
Sébastien Hoorens <sebastien@citizenlab.co>
2018-05-09 10:43:24 +0200

    added project side fx service, rm moderators upon project deletion and minor changes project controller

---
+++ app/services/side_fx_project_service.rb
@@ -0,0 +1,29 @@
+class SideFxProjectService
+
+  include SideFxHelper
+
+  def after_create project, user
+    LogActivityJob.perform_later(project, 'created', user, project.created_at.to_i)
+  end
+
+  def after_update project, user
+    LogActivityJob.perform_later(project, 'changed', user, project.updated_at.to_i)
+  end
+
+
+  def after_destroy frozen_project, user
+    remove_moderators frozen_projec.id
+    frozen_project = clean_time_attributes(frozen_project.attributes)
+    LogActivityJob.perform_later(encode_frozen_resource(frozen_project), 'deleted', user, Time.now.to_i, payload: {project: frozen_project})
+  end
+
+
+  private
+
+  def remove_moderators project_id
+    User.project_moderators(project_id).all.each do |moderator|
+      moderator.delete_role 'project_moderator', project_id: project_id
+    end
+  end
+
+end
