8c55033d293baee27bafaad5e73733ac25ced9bb
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-05-09 18:37:20 +0200

    Added value selector component for areas. CL2-1716

--- app/components/admin/UserFilterConditions/ValueSelector/AreaValueSelector.tsx
+++ app/components/admin/UserFilterConditions/ValueSelector/AreaValueSelector.tsx
@@ -1,30 +1,59 @@
 import * as React from 'react';
 
-import Input from 'components/UI/Input';
+import { TRule } from '../rules';
+import { IOption } from 'typings';
+
+import Select from 'components/UI/Select';
+
+import { injectTFunc } from 'components/T/utils';
+import GetAreas, { GetAreasChildProps } from 'resources/GetAreas';
 
 type Props = {
+  rule: TRule;
   value: string;
   onChange: (string) => void;
+  areas: GetAreasChildProps;
+  tFunc: any;
 };
 
 type State = {};
 
 class AreaValueSelector extends React.PureComponent<Props, State> {
 
-  handleOnChange = (value) => {
-    this.props.onChange(value);
+  generateOptions = (): IOption[] => {
+    if (this.props.areas) {
+      return this.props.areas.map((area) => (
+        {
+          value: area.id,
+          label: this.props.tFunc(area.attributes.title_multiloc),
+        }
+      ));
+    } else {
+      return [];
+    }
+  }
+
+  handleOnChange = (option: IOption) => {
+    this.props.onChange(option.value);
   }
 
   render() {
     const { value } = this.props;
     return (
-      <Input
-        type="text"
+      <Select
         value={value}
+        options={this.generateOptions()}
         onChange={this.handleOnChange}
+        clearable={false}
       />
     );
   }
 }
 
-export default AreaValueSelector;
+const AreaValueSelectorWithHOC = injectTFunc(AreaValueSelector);
+
+export default (inputProps) => (
+  <GetAreas>
+    {(areas) => <AreaValueSelectorWithHOC {...inputProps} areas={areas} />}
+  </GetAreas>
+);
