5d70f1ea0669e81a3bac72cd4b90c2c654a23ab4
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-05-09 18:22:26 +0200

    Added value selector component for custom field options. CL2-1714

--- app/components/admin/UserFilterConditions/ValueSelector/CustomFieldOptionValueSelector.tsx
+++ app/components/admin/UserFilterConditions/ValueSelector/CustomFieldOptionValueSelector.tsx
@@ -1,30 +1,59 @@
 import * as React from 'react';
 
-import Input from 'components/UI/Input';
+import GetCustomFieldOptions, { GetCustomFieldOptionsChildProps } from 'resources/GetCustomFieldOptions';
+import { TRule } from '../rules';
+import { IOption } from 'typings';
+
+import Select from 'components/UI/Select';
+
+import { injectTFunc } from 'components/T/utils';
 
 type Props = {
+  rule: TRule;
   value: string;
   onChange: (string) => void;
+  options: GetCustomFieldOptionsChildProps;
+  tFunc: any;
 };
 
 type State = {};
 
 class CustomFieldOptionValueSelector extends React.PureComponent<Props, State> {
 
-  handleOnChange = (value) => {
-    this.props.onChange(value);
+  generateOptions = (): IOption[] => {
+    if (this.props.options) {
+      return this.props.options.map((option) => (
+        {
+          value: option.id,
+          label: this.props.tFunc(option.attributes.title_multiloc),
+        }
+      ));
+    } else {
+      return [];
+    }
+  }
+
+  handleOnChange = (option: IOption) => {
+    this.props.onChange(option.value);
   }
 
   render() {
     const { value } = this.props;
     return (
-      <Input
-        type="text"
+      <Select
         value={value}
+        options={this.generateOptions()}
         onChange={this.handleOnChange}
+        clearable={false}
       />
     );
   }
 }
 
-export default CustomFieldOptionValueSelector;
+const CustomFieldOptionValueSelectorWithHOC = injectTFunc(CustomFieldOptionValueSelector);
+
+export default (inputProps) => (
+  <GetCustomFieldOptions customFieldId={inputProps.rule.customFieldId}>
+    {(options) => <CustomFieldOptionValueSelectorWithHOC {...inputProps} options={options} />}
+  </GetCustomFieldOptions>
+);
