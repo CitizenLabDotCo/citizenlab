f0e28e0c8788bca0bf645bcfe5b416aece1afa98
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-05-09 14:26:48 +0200

    Added custom field support to field selector. CL2-1710

--- app/components/admin/UserFilterConditions/FieldSelector.tsx
+++ app/components/admin/UserFilterConditions/FieldSelector.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 // import styled from 'styled-components';
-
 import Select from 'components/UI/Select';
 import { IOption } from 'typings';
 import { TRule, staticRuleTypes } from './rules';
@@ -8,6 +7,9 @@ import { TRule, staticRuleTypes } from './rules';
 import messages from './messages';
 import { injectIntl } from 'utils/cl-intl';
 import { InjectedIntlProps } from 'react-intl';
+import GetCustomFields, { GetCustomFieldsChildProps } from 'resources/GetCustomFields';
+import { ICustomFieldData } from 'services/userCustomFields';
+import { injectTFunc } from 'components/T/utils';
 
 export interface FieldDescriptor {
   ruleType?: TRule['ruleType'];
@@ -17,6 +19,8 @@ export interface FieldDescriptor {
 type Props = {
   field: FieldDescriptor;
   onChange: (FieldDescriptor: FieldDescriptor) => void;
+  customFields: GetCustomFieldsChildProps;
+  tFunc: any;
 };
 
 type State = {};
@@ -24,24 +28,49 @@ type State = {};
 class FieldSelector extends React.PureComponent<Props & InjectedIntlProps, State> {
 
   generateOptions = (): IOption[] => {
-    return staticRuleTypes.map((ruleType) => (
+    const staticOptions = staticRuleTypes.map((ruleType) => (
       {
         value: this.descriptorToOptionValue({ ruleType }),
         label: this.props.intl.formatMessage(messages[`field_${ruleType}`]),
       }
     ));
+    const customFieldOptions = (this.props.customFields || []).map((customField) => (
+      {
+        value: this.descriptorToOptionValue(this.customFieldToDescriptor(customField)),
+        label: this.props.tFunc(customField.attributes.title_multiloc),
+      }
+    ));
+    return staticOptions.concat(customFieldOptions);
   }
 
   handleOnChange = (option: IOption) => {
     this.props.onChange(this.optionValueToDescriptor(option.value));
   }
 
+  customFieldToDescriptor = (customField: ICustomFieldData) => {
+    let ruleType;
+    switch (customField.attributes.input_type) {
+      case 'multiline_text':
+        ruleType = 'custom_field_text';
+        break;
+      case 'multiselect':
+        ruleType = 'custom_field_select';
+        break;
+      default:
+        ruleType = `custom_field_${customField.attributes.input_type}`;
+    }
+    return {
+      ruleType,
+      customFieldId: customField.id,
+    };
+  }
+
   descriptorToOptionValue = (fieldDescriptor: FieldDescriptor) => {
-    return fieldDescriptor.ruleType;
+    return JSON.stringify(fieldDescriptor);
   }
 
   optionValueToDescriptor = (value) => {
-    return { ruleType: value };
+    return JSON.parse(value);
   }
 
   render() {
@@ -56,4 +85,10 @@ class FieldSelector extends React.PureComponent<Props & InjectedIntlProps, State
   }
 }
 
-export default injectIntl(FieldSelector);
+const FieldSelectorWithHocs = injectTFunc(injectIntl(FieldSelector));
+
+export default (inputProps) => (
+  <GetCustomFields>
+    {(customFields) => <FieldSelectorWithHocs {...inputProps} customFields={customFields} />}
+  </GetCustomFields>
+);
