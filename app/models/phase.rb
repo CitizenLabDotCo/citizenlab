8d2c1e18a696fb1fad68215f2f4dfffab2e8671c
Sébastien Hoorens <sebastien@citizenlab.co>
2018-05-09 15:33:58 +0200

    validation to check if no other phases of project overlap + spec

--- app/models/phase.rb
+++ app/models/phase.rb
@@ -13,9 +13,19 @@ class Phase < ApplicationRecord
   validates :start_at, :end_at, presence: true
   validate :validate_start_at_before_end_at
   validate :validate_belongs_to_timeline_project
+  validate :validate_no_other_overlapping_phases
 
   before_validation :sanitize_description_multiloc
   before_validation :strip_title
+
+
+  def overlaps? other_phase
+    !((self.end_at.to_date < other_phase.start_at.to_date) || (other_phase.end_at.to_date < self.start_at.to_date)) 
+  end
+
+  def other_project_phases
+    Phase.where(project_id: self.project_id)].all.select{|p| p.id != self.id}
+  end
 
 
   private
@@ -38,9 +48,18 @@ class Phase < ApplicationRecord
     end
   end
 
+  def validate_no_other_overlapping_phases
+    self.other_project_phases.each do |other_phase|
+      if self.overlaps? other_phase
+        errors.add(:project, :has_other_overlapping_phases, message: 'has other phases which overlap in start and end date')
+      end
+    end
+  end
+
   def strip_title
     self.title_multiloc.each do |key, value|
       self.title_multiloc[key] = value.strip
     end
   end
+
 end
