36e47952503a3802b82ea481cc068e1eceb003b7
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-05-11 14:48:46 +0200

    Added limited graphql endpoint to admin api WIP

---
+++ engines/admin_api/app/graphql/admin_api/types/idea_type.rb
@@ -0,0 +1,27 @@
+module AdminApi
+  class Types::IdeaType < GraphQL::Schema::Object
+    description "Single unit of citizen input"
+
+    class PublicationStatus < GraphQL::Schema::Enum
+      Idea::PUBLICATION_STATUSES.each do |ps|
+        value ps
+      end
+    end
+
+
+    field :id, ID, null: false
+    field :title_multiloc, Types::MultilocType, null: false
+    field :slug, String, null: false
+    field :publication_status, PublicationStatus, null: false
+    field :updated_at, String, null: false
+    field :created_at, String, null: false
+    field :published_at, String, null: false
+    field :href, String, null: true
+
+    @@frontend_service = FrontendService.new
+    def href
+      @@frontend_service.model_to_url(object)
+    end
+
+  end
+end
