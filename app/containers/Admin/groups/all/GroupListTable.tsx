1a15cf01e6d97f233e91a58b7765ae13a00560f9
Sara <sara@citizenlab.co>
2018-05-14 16:16:58 +0200

    cleaned up groups page

--- app/containers/Admin/groups/all/GroupListTable.tsx
+++ app/containers/Admin/groups/all/GroupListTable.tsx
@@ -19,7 +19,6 @@ import Icon from 'components/UI/Icon';
 import GroupAvatar from './GroupAvatar';
 import Spinner from 'components/UI/Spinner';
 import { List, Row } from 'components/admin/ResourceList';
-import MultipleSelectDropdown from 'components/admin/MultipleSelectDropdown';
 
 // Style
 import styled from 'styled-components';
@@ -124,11 +123,7 @@ class GroupsListTable extends React.PureComponent<Props & InjectedIntlProps, Sta
       }
     };
   }
-  getChoices = (groups) => {
-    return groups.map((group) => {
-      return { id: group.id, text: group.attributes.title_multiloc, selected: false };
-    });
-  }
+
   addToGroups = (ids) => {
     console.log(ids);
   }
@@ -154,14 +149,6 @@ class GroupsListTable extends React.PureComponent<Props & InjectedIntlProps, Sta
     }
 
     return (
-      <>
-      <MultipleSelectDropdown
-        messages={messages}
-        choices={this.getChoices(groups)}
-        onSubmit={this.addToGroups}
-      >
-        <div>choose</div>
-      </MultipleSelectDropdown>
       <List>
         {groups.map((group) => (
           <Row key={group.id}>
@@ -181,7 +168,6 @@ class GroupsListTable extends React.PureComponent<Props & InjectedIntlProps, Sta
           </Row>
         ))}
       </List>
-      </>
     );
   }
 }
