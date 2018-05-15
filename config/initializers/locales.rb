6e9977f652fd3b0af00b240d5b92e7abd0e74b8b
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-05-15 17:29:09 +0200

    Updated available_locales and removed obsolete translation files

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -1,1 +1,2 @@
-I18n.available_locales = [:en, :nl, :fr, :de, :da, :no]
+# I18n.available_locales = [:en, :nl, :fr, :de, :da, :no]
+I18n.available_locales = [:en, :"en-GB", :"en-CA", :"nl-BE", :"nl-NL", :"fr-BE", :"fr-FR", :"de-DE", :"da-DK", :"nb-NO"]
