7a18499d4001664d08fa62a195a75d6239bc2917
Unknown <sara@citizenlab.co>
2018-05-15 14:14:00 +0200

    Empty groups page WIP

---
+++ app/containers/Admin/groups/NoUsers.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router';
+
+import messages from './messages';
+import { FormattedMessage } from 'utils/cl-intl';
+import Icon from 'components/UI/Icon';
+
+const NoUsersPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 21px;
+  color: #044D6C;
+  font-weight: bold;
+  line-height: 25px;
+  padding-top: 50px;
+  svg {
+    margin-bottom: 20px;
+  }
+`;
+
+const SFormattedMessage = styled.div`
+  color: #84939E;
+  font-weight: 400;
+  font-size: 16px;
+  a {
+    color: #84939E;
+    font-weight: bold;
+    text-decoration: underline;
+  }
+`;
+
+interface Props {
+  type: 'normal' | 'smart';
+}
+
+export default class NoUsers extends React.PureComponent<Props> {
+  render() {
+    if (this.props.type === 'normal') {
+      return (
+        <NoUsersPage>
+          <Icon name="blankPage" />
+          <FormattedMessage {...messages.emptyGroup} />
+          <SFormattedMessage>
+            <FormattedMessage
+              {...messages.goToAllUsers}
+              values={{
+                allUsersLink: (
+                  <Link to="/users/all">
+                    <FormattedMessage {...messages.allUsers} />
+                  </Link>),
+              }}
+            />
+        </SFormattedMessage>
+        </NoUsersPage>
+      );
+    } else {
+      return (
+        <NoUsersPage>
+          <FormattedMessage {...messages.emptySmartGroup} />
+          <SFormattedMessage>
+            <FormattedMessage {...messages.noUserMatching} />
+          </SFormattedMessage>
+        </NoUsersPage>
+      );
+    }
+  }
+}
