2cfe90c3be430ba05c215fe47e27cc947ed42ea1
Sara <sara@citizenlab.co>
2018-05-18 15:49:16 +0200

    Normal group edition

--- app/containers/Admin/users/UsersGroup.tsx
+++ app/containers/Admin/users/UsersGroup.tsx
@@ -1,6 +1,7 @@
 // Libraries
 import React from 'react';
 import { withRouter, WithRouterProps } from 'react-router';
+import { Formik } from 'formik';
 
 // utils
 import { isNilOrError } from 'utils/helperUtils';
@@ -8,7 +9,11 @@ import { isNilOrError } from 'utils/helperUtils';
 // Components
 import NoUsers from './NoUsers';
 import GroupHeader from './GroupHeader';
+import Modal from 'components/UI/Modal';
+import NormalGroupForm, { NormalFormValues } from './NormalGroupForm';
 
+// i18n
+import FormattedMessage from 'utils/cl-intl/FormattedMessage';
 import messages from './messages';
 import { injectIntl } from 'utils/cl-intl';
 import { InjectedIntlProps } from 'react-intl';
@@ -17,24 +22,53 @@ import { InjectedIntlProps } from 'react-intl';
 import GetGroup, { GetGroupChildProps } from 'resources/GetGroup';
 
 // Services
-import { deleteGroup } from 'services/groups';
+import { IGroupData, deleteGroup, updateGroup } from 'services/groups';
+
 
 // Typings
-interface InputProps {}
+import { API } from 'typings';
+interface InputProps { }
 interface DataProps {
   group: GetGroupChildProps;
 }
-interface Props extends InputProps, DataProps {}
-export interface State {}
+interface Props extends InputProps, DataProps { }
+export interface State {
+  groupEditionModal: false | IGroupData['attributes']['membership_type'];
+}
 
 export class UsersGroup extends React.PureComponent<Props & InjectedIntlProps, State> {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      groupEditionModal: false,
+    };
+  }
+  closeGroupEditionModal = () => {
+    this.setState({ groupEditionModal: false });
   }
 
-  editGroup = () => {
+  renderNormalForm = (props) => {
+    return <NormalGroupForm {...props} />;
+  }
 
+  openGroupEditionModal = () => {
+    const { group } = this.props;
+    if (!isNilOrError(group)) {
+      this.setState({ groupEditionModal: group.attributes.membership_type });
+    }
+  }
+  handleSubmitNormalForm = (id) => (values: NormalFormValues, { setErrors, setSubmitting }) => {
+    updateGroup(id, {
+      ...values
+    })
+      .then(() => {
+        this.closeGroupEditionModal();
+      })
+      .catch((errorResponse) => {
+        const apiErrors = (errorResponse as API.ErrorResponse).json.errors;
+        setErrors(apiErrors);
+        setSubmitting(false);
+      });
   }
   deleteGroup = (groupId: string) => () => {
     const deleteMessage = this.props.intl.formatMessage(messages.groupDeletionConfirmation);
@@ -50,17 +84,41 @@ export class UsersGroup extends React.PureComponent<Props & InjectedIntlProps, S
 
   render() {
     const { group } = this.props;
+    const { groupEditionModal } = this.state;
+
+    let ModalHeader;
+    switch (groupEditionModal) {
+      case 'manual':
+        ModalHeader = <FormattedMessage tagName="h3" {...messages.modalHeaderManual} />;
+        break;
+      case 'rules':
+        ModalHeader = <FormattedMessage tagName="h3" {...messages.modalHeaderRules} />;
+        break;
+    }
+
+
     if (!isNilOrError(group)) {
       return (
         <>
           <GroupHeader
             title={group.attributes.title_multiloc}
             smartGroup={group.attributes.membership_type === 'rules'}
-            onEdit={this.editGroup}
+            onEdit={this.openGroupEditionModal}
             onDelete={this.deleteGroup(group.id)}
             onSearch={this.searchGroup}
           />
           <NoUsers />
+          <Modal header={ModalHeader} fixedHeight={false} opened={groupEditionModal !== false} close={this.closeGroupEditionModal}>
+            <>
+              {groupEditionModal === 'manual' &&
+                <Formik
+                  initialValues={group.attributes}
+                  validate={NormalGroupForm.validate}
+                  render={this.renderNormalForm}
+                  onSubmit={this.handleSubmitNormalForm(group.id)}
+                />}
+            </>
+          </Modal>
         </>
       );
     }
