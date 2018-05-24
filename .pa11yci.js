323b9c1cb8c7e86fb9cc22ae42cbf554a8dae060
Mathieu Dubois <mathieu@mathieudubois.be>
2018-05-24 08:11:42 +0200

    Attempt to run without sandbox in CI

--- .pa11yci.js
+++ .pa11yci.js
@@ -1,17 +1,25 @@
-module.exports = {
-  "defaults": {
-    "timeout": 10000
+const browser = await puppeteer.launch({args: ['--no-sandbox']});
+
+const config = {
+  defaults: {
+    timeout: 10000
   },
-  "urls": [
-    "https://demo.stg.citizenlab.co/",
-    "https://demo.stg.citizenlab.co/projects",
-    "https://demo.stg.citizenlab.co/projects/renewing-westbrook-parc/process",
-    "https://demo.stg.citizenlab.co/projects/renewing-westbrook-parc/info",
-    "https://demo.stg.citizenlab.co/projects/renewing-westbrook-parc/events",
-    "https://demo.stg.citizenlab.co/ideas",
-    "https://demo.stg.citizenlab.co/ideas/more-box-parking-for-bikes-in-the-centrum",
-    "https://demo.stg.citizenlab.co/sign-in",
-    "https://demo.stg.citizenlab.co/sign-up",
-    "https://demo.stg.citizenlab.co/ideas/new"
+  urls: [
+    'https://demo.stg.citizenlab.co/',
+    'https://demo.stg.citizenlab.co/projects',
+    'https://demo.stg.citizenlab.co/projects/renewing-westbrook-parc/process',
+    'https://demo.stg.citizenlab.co/projects/renewing-westbrook-parc/info',
+    'https://demo.stg.citizenlab.co/projects/renewing-westbrook-parc/events',
+    'https://demo.stg.citizenlab.co/ideas',
+    'https://demo.stg.citizenlab.co/ideas/more-box-parking-for-bikes-in-the-centrum',
+    'https://demo.stg.citizenlab.co/sign-in',
+    'https://demo.stg.citizenlab.co/sign-up',
+    'https://demo.stg.citizenlab.co/ideas/new'
   ]
 }
+
+if (process.env.CI) {
+  config.browser = browser;
+}
+
+module.exports = config;
