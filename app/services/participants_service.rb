9dad1f71496254a545e2dcb71cadb2edef55e3e8
Sébastien Hoorens <sebastien@citizenlab.co>
2018-05-24 10:41:38 +0200

    participants of a project (specs succeed but in multiple querries)

--- app/services/participants_service.rb
+++ app/services/participants_service.rb
@@ -4,9 +4,17 @@ class ParticipantsService
     project = options[:project]
     since = options[:since]
     if project
-      User.all # TODO
+      ideas = Idea.where(project: project)
+      comments = Comment.all.where(idea_id: ideas.map(&:id))
+      votes = Vote.where(votable_id: (ideas.map(&:id) + comments.map(&:id)))
+      if since
+        ideas = ideas.where('created_at::date >= (?)::date', since)
+        comments = comments.where('created_at::date >= (?)::date', since)
+        votes = votes.where('created_at::date >= (?)::date', since)
+      end
+      User.where(id: (ideas.map(&:author_id) + comments.map(&:author_id) + votes.map(&:user_id)).uniq)
     else
-      activities = Activity.all.left_outer_joins(:user)
+      activities = Activity.all
       if since
         activities = activities.where('acted_at::date >= (?)::date', since)
       end
