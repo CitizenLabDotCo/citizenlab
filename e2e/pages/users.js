5d0569157e3151cbd67c87669a16de4072c80137
Sara <sara@citizenlab.co>
2018-05-24 15:51:10 +0200

    Add smart group creation testing

--- e2e/pages/users.js
+++ e2e/pages/users.js
@@ -10,6 +10,27 @@ const commands = {
     .click('@submit')
     .waitForElementNotPresent('@submit');
   },
+  newRulesGroup(title) {
+    this
+    .waitForElementVisible('@createGroupButton')
+    .click('@createGroupButton')
+    .waitForElementVisible('@createRulesGroupButton')
+    .click('@createRulesGroupButton')
+    .waitForElementVisible('@groupNameField')
+    .fillMultiloc('@groupNameField', title)
+    .click('@addConditionButton')
+    // From here the test is pretty hacky. It selects Role > is admin
+    .click('.e2e-rules-field-section .Select')
+    .click("#react-select-2--option-3");
+    this.api.keys(this.api.Keys.TAB)
+    .keys(this.api.Keys.ENTER)
+    .keys(this.api.Keys.ENTER);
+    // note : in a page, you need the api keyword to use keys and pause and execute
+    // but when chaining after a api keyword, the @ don't work anymore
+    this
+    .click('@submit')
+    .waitForElementNotPresent('@submit');
+  },
 };
 
 module.exports = {
@@ -19,8 +40,9 @@ module.exports = {
     createNormalGroupButton: { selector: '.e2e-create-normal-group-button' },
     createRulesGroupButton: { selector: '.e2e-create-rules-group-button' },
     groupsList: { selector: '.e2e-groups-list' },
-    groupNameField: { selector: '#group-title' },
-    submit: { selector: 'form .e2e-submit-wrapper-button button' },
+    groupNameField: { selector: '#group-title-field' },
+    addConditionButton: { selector: '.e2e-add-condition-button' },
+    submit: { selector: '.e2e-submit-wrapper-button button' },
   },
   commands: [commands],
 };
