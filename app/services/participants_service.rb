4a1394ce8bf6e1bcbafe2d727b6c2781f8e5ec49
Sébastien Hoorens <sebastien@citizenlab.co>
2018-05-24 14:21:31 +0200

    some minor optimi

--- app/services/participants_service.rb
+++ app/services/participants_service.rb
@@ -5,20 +5,20 @@ class ParticipantsService
     since = options[:since]
     if project
       ideas = Idea.where(project: project)
-      comments = Comment.all.where(idea_id: ideas.map(&:id))
-      votes = Vote.where(votable_id: (ideas.map(&:id) + comments.map(&:id)))
+      comments = Comment.all.where(idea_id: ideas.ids)
+      votes = Vote.where(votable_id: (ideas.ids + comments.ids))
       if since
         ideas = ideas.where('created_at::date >= (?)::date', since)
         comments = comments.where('created_at::date >= (?)::date', since)
         votes = votes.where('created_at::date >= (?)::date', since)
       end
-      User.where(id: (ideas.map(&:author_id) + comments.map(&:author_id) + votes.map(&:user_id)).uniq)
+      User.where(id: (ideas.distinct.pluck(:author_id) + comments.distinct.pluck(:author_id) + votes.distinct.pluck(:user_id)).uniq)
     else
       activities = Activity.all
       if since
         activities = activities.where('acted_at::date >= (?)::date', since)
       end
-      User.where(id: activities.map(&:user_id))
+      User.where(id: activities.distinct.pluck(:user_id))
     end
   end
 
