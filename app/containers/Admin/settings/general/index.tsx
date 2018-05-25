5d75699a7e35b5dd8cab2ed5bcc22c724df45754
Sara <sara@citizenlab.co>
2018-05-25 16:05:10 +0200

    Erased the Meta tag fields cf CL2-1367

--- app/containers/Admin/settings/general/index.tsx
+++ app/containers/Admin/settings/general/index.tsx
@@ -146,8 +146,6 @@ export default class SettingsGeneralTab extends React.PureComponent<Props, State
       const tenantLocales: string[] | null = get(tenantAttrs, 'settings.core.locales', null);
       const organizationType: string | null = get(tenantAttrs, 'settings.core.organization_type', null);
       const organizationNameMultiloc: Multiloc | null = get(tenantAttrs, 'settings.core.organization_name', null);
-      const metaTitleMultiloc: Multiloc | null = get(tenantAttrs, 'settings.core.meta_title', null);
-      const metaDescriptionMultiloc: Multiloc | null = get(tenantAttrs, 'settings.core.meta_description', null);
       const localeOptions = this.localeOptions();
       const selectedLocaleOptions = this.localesToOptions(tenantLocales);
 
@@ -177,26 +175,6 @@ export default class SettingsGeneralTab extends React.PureComponent<Props, State
                 value={selectedLocaleOptions}
                 onChange={this.handleLocalesOnChange}
                 options={localeOptions}
-              />
-            </SectionField>
-
-            <SectionField>
-              <InputMultiloc
-                type="text"
-                id="meta_title"
-                label={<FormattedMessage {...messages.metaTitle} />}
-                valueMultiloc={metaTitleMultiloc}
-                onChange={this.handleCoreMultilocSettingOnChange('meta_title')}
-              />
-            </SectionField>
-
-            <SectionField>
-              <TextAreaMultiloc
-                label={<FormattedMessage {...messages.metaDescription} />}
-                name="meta_description"
-                rows={5}
-                valueMultiloc={metaDescriptionMultiloc}
-                onChange={this.handleCoreMultilocSettingOnChange('meta_description')}
               />
             </SectionField>
 
