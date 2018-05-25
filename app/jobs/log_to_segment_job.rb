18de55ead7bbca646f3badcb31147f6d8a879d65
Sébastien Hoorens <sebastien@citizenlab.co>
2018-05-25 12:14:07 +0200

    added service for generating segment events and made lifecycle_stage public

--- app/jobs/log_to_segment_job.rb
+++ app/jobs/log_to_segment_job.rb
@@ -2,55 +2,34 @@ class LogToSegmentJob < ApplicationJob
   queue_as :default
 
   def perform(activity)
-
-    trackingMessage = {
-      event: "#{activity.item_type} #{activity.action}",
+    event = LogToSegmentService.new.tracking_message(
+      "#{activity.item_type} #{activity.action}", 
+      user_id: activity.user_id,
       timestamp: activity.acted_at,
-      properties: {
-        source: 'cl2-back',
-        item_type: activity.item_type,
-        item_id: activity.item_id,
-        action: activity.action,
-        payload: activity.payload
-      }
-    }
+      payload: activity.payload
+      )
 
-    # Segment requires us to send either a user_id or an anonymous_id
-    if activity.user_id
-      trackingMessage[:user_id] = activity.user_id
-    else
-      trackingMessage[:anonymous_id] = SecureRandom.base64
-    end
+    event[:properties][:item_type] = activity.item_type
+    event[:properties][:item_id]   = activity.item_id
+    event[:properties][:action]    = activity.action
 
     if activity.item
-      begin
-        serializer = "WebApi::V1::External::#{activity.item_type}Serializer".constantize
-        serialization = ActiveModelSerializers::SerializableResource.new(activity.item, {
-          serializer: serializer,
-          adapter: :json
-        })
-        item_content = serialization.serializable_hash
-        if activity.item.kind_of? Notification
-          trackingMessage[:event] = "Notification for #{activity.item.class::EVENT_NAME} created"
-          item_content = item_content.flatten.second
-        end
-        trackingMessage[:properties][:item_content] = item_content
-      rescue NameError => e
-        # There's no serializer, so we don't add anything
+      serializer = "WebApi::V1::External::#{activity.item_type}Serializer".constantize
+      serialization = ActiveModelSerializers::SerializableResource.new(activity.item, {
+        serializer: serializer,
+        adapter: :json
+      })
+      item_content = serialization.serializable_hash
+      if activity.item.kind_of? Notification
+        event[:event] = "Notification for #{activity.item.class::EVENT_NAME} created"
+        item_content = item_content.flatten.second
       end
+      event[:properties][:item_content] = item_content
     end
 
-    begin
-      tenant = Tenant.current
-      trackingMessage[:properties][:tenantId] = tenant.id
-      trackingMessage[:properties][:tenantName] = tenant.name
-      trackingMessage[:properties][:tenantHost] = tenant.host
-      trackingMessage[:properties][:tenantOrganizationType] = Tenant.settings('core', 'organization_type')
-    rescue  ActiveRecord::RecordNotFound => e
-      # Tenant can't be found, so we don't add anything
-    end
-
-    Analytics.track(trackingMessage)
+    Analytics.track(event)
+  rescue NameError => e
+    # There's no serializer, so we don't add anything
   end
 
 end
