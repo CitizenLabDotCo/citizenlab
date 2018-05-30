0ca5ab7116518f4a48546bd54cab489007960314
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-05-30 17:02:09 +0200

    Fixed and improved side fx tests for ideas and tenants

--- spec/services/side_fx_tenant_spec.rb
+++ spec/services/side_fx_tenant_spec.rb
@@ -15,12 +15,28 @@ describe SideFxTenantService do
   end
 
   describe "after_update" do
-    it "logs a changed action job when the tenant has changed" do
+    it "logs a 'changed' action job when the tenant has changed" do
       settings = tenant.settings
       settings['core']['organization_name'] = "New name"
       tenant.update(settings: settings)
       expect {service.after_update(tenant, current_user)}.
         to have_enqueued_job(LogActivityJob).with(tenant, 'changed', current_user, tenant.updated_at.to_i)
+    end
+
+    it "logs a 'changed_host' action job when the tenant host has changed" do
+      old_host = tenant.host
+      tenant.update(host: 'some-domain.net')
+      expect {service.after_update(tenant, current_user)}.
+        to have_enqueued_job(LogActivityJob).with(tenant, 'changed_host', current_user, tenant.updated_at.to_i, payload: {changes: [old_host, "some-domain.net"]})
+    end
+
+    it "logs a 'changed_lifecycle_stage' action job when the tenant has changed" do
+      settings = tenant.settings
+      old_lifecycle_stage = settings['core']['lifecycle_stage']
+      settings['core']['lifecycle_stage'] = "churned"
+      tenant.update(settings: settings)
+      expect {service.after_update(tenant, current_user)}.
+        to have_enqueued_job(LogActivityJob).with(tenant, 'changed_lifecycle_stage', current_user, tenant.updated_at.to_i, payload: {changes: [old_lifecycle_stage, "churned"]})
     end
 
   end
