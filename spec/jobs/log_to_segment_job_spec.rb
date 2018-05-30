811418fe4ad7851d9668b3a7af1e115ce90e2ff9
Sébastien Hoorens <sebastien@citizenlab.co>
2018-05-30 14:55:25 +0200

    also test case for event about notification

--- spec/jobs/log_to_segment_job_spec.rb
+++ spec/jobs/log_to_segment_job_spec.rb
@@ -24,5 +24,23 @@ RSpec.describe LogToSegmentJob, type: :job do
       job.perform activity
     end
 
+    it "generates an event with the desired content for activities about notifications" do
+      user = create(:user)
+      notification = create(:comment_on_your_comment, recipient: user)
+      activity = create(:activity, item: notification, item_type: notification.type, action: 'created', user: user)
+      
+      expect(Analytics).to receive(:track) do |event|
+        expect(event[:event]).to eq("Notification for Comment on your comment created")
+        expect(event[:userId]).to eq(user.id)
+        expect(event.dig(:properties, :source)).to eq("cl2-back")
+        expect(event.dig(:properties, :tenantId)).to eq(Tenant.current.id)
+        expect(event.dig(:properties, :action)).to eq("created")
+        expect(event.dig(:properties, :item_id)).to eq(notification.id)
+        expect(event.dig(:properties, :item_type)).to eq('Notifications::CommentOnYourComment')
+        expect(event.dig(:properties, :item_content, :recipient, :id)).to eq(user.id)
+      end
+      job.perform activity
+    end
+
   end
 end
