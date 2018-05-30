3ffa6e06c9556012d5b6e73e14f814aadb3b3cf2
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-05-30 16:23:20 +0200

    Added events and endpoints needed to submit content/platform to SEO service. CL2-1622 CL2-1738

--- app/services/side_fx_tenant_service.rb
+++ app/services/side_fx_tenant_service.rb
@@ -18,6 +18,16 @@ class SideFxTenantService
     if tenant.host_previously_changed?
       LogActivityJob.perform_later(tenant, 'changed_host', current_user, tenant.updated_at.to_i, payload: {changes: tenant.host_previous_change})
     end
+
+    if tenant.settings_previously_changed?
+      old_settings = tenant.settings_previous_change[0]
+      new_settings = tenant.settings
+
+      lifecycle_change_diff = [old_settings, new_settings].map{|s| s&.dig('core', 'lifecycle_stage')}
+      if lifecycle_change_diff.uniq.size > 1
+        LogActivityJob.perform_later(tenant, 'changed_lifecycle_stage', current_user, tenant.updated_at.to_i, payload: {changes: lifecycle_change_diff})
+      end
+    end
   end
 
   def after_destroy frozen_tenant, current_user
