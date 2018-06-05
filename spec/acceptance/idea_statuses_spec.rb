7c83297aa2956d592e92615041e11aed4796a067
Sébastien Hoorens <sebastien@citizenlab.co>
2018-06-05 18:05:08 +0200

    api docs cleanup idea and idea status, plus removed random test failure

--- spec/acceptance/idea_statuses_spec.rb
+++ spec/acceptance/idea_statuses_spec.rb
@@ -3,13 +3,15 @@ require 'rspec_api_documentation/dsl'
 
 resource "IdeaStatuses" do
 
+  explanation "Idea statuses reflect the cities attitude towards an idea. Each tenant has its own custom set of idea statuses."
+
   before do
     header "Content-Type", "application/json"
     @statuses = create_list(:idea_status, 3)
   end
 
   get "web_api/v1/idea_statuses" do
-    example_request "List the available idea statuses for the tenant" do
+    example_request "List all idea statuses" do
       expect(status).to eq(200)
       json_response = json_parse(response_body)
       expect(json_response[:data].size).to eq 3
@@ -20,7 +22,7 @@ resource "IdeaStatuses" do
   get "web_api/v1/idea_statuses/:id" do
     let(:id) { @statuses.first.id }
 
-    example_request "Get metadata of one idea status" do
+    example_request "Get one idea status" do
       expect(status).to eq 200
       json_response = json_parse(response_body)
       expect(json_response.dig(:data, :id)).to eq @statuses.first.id
