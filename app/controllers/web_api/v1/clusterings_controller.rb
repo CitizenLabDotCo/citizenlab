5226b3049d1ab72bc39afc45b03b2ec9b3931bb6
Sébastien Hoorens <sebastien@citizenlab.co>
2018-06-13 16:21:55 +0200

    filters for ideas for clustering creation + specs

--- app/controllers/web_api/v1/clusterings_controller.rb
+++ app/controllers/web_api/v1/clusterings_controller.rb
@@ -18,11 +18,29 @@ class WebApi::V1::ClusteringsController < ApplicationController
     options = {}
     options[:drop_empty] = params[:clustering][:drop_empty] != 'false'
 
-    ideas = policy_scope(Idea)
+    @ideas = policy_scope(Idea)
+    @ideas = @ideas.with_some_topics(params[:topics]) if params[:topics].present?
+    @ideas = @ideas.with_some_areas(params[:areas]) if params[:areas].present?
+    @ideas = @ideas.in_phase(params[:phase]) if params[:phase].present?
+    @ideas = @ideas.where(project_id: params[:project]) if params[:project].present?
+    @ideas = @ideas.where(author_id: params[:author]) if params[:author].present?
+    @ideas = @ideas.where(idea_status_id: params[:idea_status]) if params[:idea_status].present?
+    @ideas = @ideas.search_by_all(params[:search]) if params[:search].present?
+    if params[:publication_status].present?
+      @ideas = @ideas.where(publication_status: params[:publication_status])
+    else
+      @ideas = @ideas.where(publication_status: 'published')
+    end
+    if (params[:filter_trending] == 'true') && !params[:search].present?
+      @ideas = trending_idea_service.filter_trending @ideas
+    end
+    @ideas = @ideas.where('upvotes_count >= ?', params[:minimal_upvotes]) if params[:minimal_upvotes].present?
+    @ideas = @ideas.where('downvotes_count >= ?', params[:minimal_downvotes]) if params[:minimal_downvotes].present?
+    @ideas = @ideas.where('(upvotes_count + downvotes_count) >= ?', params[:minimal_total_votes]) if params[:minimal_total_votes].present?
 
     @clustering.structure = ClusteringService.new.build_structure(
       params[:clustering][:levels],
-      ideas,
+      @ideas,
       options
     )
 
