0cddae73d52bb7ce0d82f2520ecbae3bae341dc7
Sara <sara@citizenlab.co>
2018-06-13 13:08:18 +0200

    added e2e test to move a user to a group

--- e2e/tests/admin/groups.js
+++ e2e/tests/admin/groups.js
@@ -25,6 +25,34 @@ module.exports = {
 
     browser.end();
   },
+  addUsersToNormalGroup: (browser) => {
+    const signinPage = browser.page.signin();
+    const usersPage = browser.page.users();
+
+    signinPage
+      .navigate()
+      .signin('koen@citizenlab.co', 'testtest');
+
+    usersPage
+      .navigate()
+      .getText('@firstGroupUserCount', (count) => {
+        browser.expect(count.value).to.equal("0");
+        browser.getText('.e2e-user-table tbody tr:first-child td:nth-child(3)', (nom) => {
+          browser
+            .click('.e2e-user-table tbody tr:first-child td:first-child')
+            .click('.e2e-move-users')
+            .click('.e2e-dropdown-item:first-child')
+            .click('.e2e-dropdown-submit')
+            .pause(500);
+          usersPage
+            .getText('@firstGroupUserCount', (newCount) => {
+              browser.expect(newCount.value).to.equal("1");
+              usersPage.click('@firstGroupUserCount');
+              browser.expect.element('.e2e-user-table').text.to.contain(nom.value);
+            });
+        });
+      });
+  },
   newRulesGroup: (browser) => {
     const signinPage = browser.page.signin();
     const usersPage = browser.page.users();
@@ -45,7 +73,7 @@ module.exports = {
 
     // This assertion will break if the newly created group is not the last in the list anymore
     // We are testing that the number of users is a positive integer
-    usersPage.expect.element('@lastGroupUserCount').text.to.match(/^[1-9]\d*$/);
+    usersPage.expect.element('@firstGroupUserCount').text.to.match(/^[1-9]\d*$/);
 
     browser.end();
   },
