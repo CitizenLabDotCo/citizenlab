cf6c4141cd964348443410c6a2a95473f88769e3
Alex Dubois <alex.dubois@citizenlab.co>
2018-06-13 15:07:46 +0200

    Update routes for the projects

--- app/containers/Admin/projects/routes.ts
+++ app/containers/Admin/projects/routes.ts
@@ -19,57 +19,57 @@ export default () => ({
       },
       childRoutes: [
         {
-          path: 'description',
+          path: '/:locale/admin/projects/:projectId/description',
           name: 'admin projects description',
           getComponent: loadAndRender(import('containers/Admin/projects/edit/description')),
         },
         {
-          path: 'ideas',
+          path: '/:locale/admin/projects/:projectId/ideas',
           name: 'admin projects ideas manager',
           getComponent: loadAndRender(import('components/admin/IdeaManager')),
         },
         {
-          path: 'timeline',
+          path: '/:locale/admin/projects/:projectId/timeline',
           name: 'admin projects timeline',
           getComponent: loadAndRender(import('containers/Admin/projects/edit/timeline')),
         },
         {
-          path: 'timeline/new',
+          path: '/:locale/admin/projects/:projectId/timeline/new',
           name: 'admin projects timeline create',
           getComponent: loadAndRender(import('containers/Admin/projects/edit/timeline/edit')),
         },
         {
-          path: 'timeline/:id',
+          path: '/:locale/admin/projects/:projectId/timeline/:id',
           name: 'admin projects timeline edit',
           getComponent: loadAndRender(import('containers/Admin/projects/edit/timeline/edit')),
         },
         {
-          path: 'events',
+          path: '/:locale/admin/projects/:projectId/events',
           name: 'admin projects events',
           getComponent: loadAndRender(import('containers/Admin/projects/edit/events')),
         },
         {
-          path: 'events/new',
+          path: '/:locale/admin/projects/:projectId/events/new',
           name: 'admin projects events create',
           getComponent: loadAndRender(import('containers/Admin/projects/edit/events/edit')),
         },
         {
-          path: 'events/:id',
+          path: '/:locale/admin/projects/:projectId/events/:id',
           name: 'admin projects events edit',
           getComponent: loadAndRender(import('containers/Admin/projects/edit/events/edit')),
         },
         {
-          path: '/admin/projects/new',
+          path: '/:locale/admin/projects/new',
           name: 'admin projects create new',
           getComponent: loadAndRender(import('containers/Admin/projects/edit/general')),
         },
         {
-          path: 'events',
+          path: '/:locale/admin/projects/:projectId/events',
           name: 'admin projects edit events',
           getComponent: loadAndRender(import('containers/Admin/projects/edit/events')),
         },
         {
-          path: 'permissions',
+          path: '/:locale/admin/projects/:projectId/permissions',
           name: 'admin projects edit permissions',
           getComponent: loadAndRender(import('containers/Admin/projects/edit/permissions')),
         },
