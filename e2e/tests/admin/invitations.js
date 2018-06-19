e0072f4f964be83f8b971125549c1c22bce8aef2
Unknown <sara@citizenlab.co>
2018-06-19 15:14:24 +0200

    Few fixes to hopefully have passing tests.
    only groups was actually wrong, the other failures are all timout due to browserstack I think

--- e2e/tests/admin/invitations.js
+++ e2e/tests/admin/invitations.js
@@ -10,50 +10,50 @@ module.exports = {
     const signinPage = browser.page.signin();
 
     signinPage
-    .navigate()
-    .signin('koen@citizenlab.co', 'testtest');
+      .navigate()
+      .signin('koen@citizenlab.co', 'testtest');
 
     browser
-    .url(`http://${process.env.ROOT_URL}/admin/invitations`)
-    .waitForElementVisible('.e2e-manual-invite')
-    .click('.e2e-manual-invite')
-    .waitForElementVisible('#e2e-emails')
-    .setValue('#e2e-emails', email)
-    .click('.e2e-submit-wrapper-button button')
-    .waitForElementPresent('.Button.success')
-    .assert.containsText('table', email);
+      .url(`http://${process.env.ROOT_URL}/admin/invitations`)
+      .waitForElementVisible('.e2e-manual-invite')
+      .click('.e2e-manual-invite')
+      .waitForElementVisible('#e2e-emails')
+      .setValue('#e2e-emails', email)
+      .click('.e2e-submit-wrapper-button button')
+      .waitForElementPresent('.Button.success')
+      .assert.containsText('table', email);
   },
   useInvite: (browser) => {
     const signinPage = browser.page.signin();
     const signupPage = browser.page.signup();
 
     signinPage
-    .navigate()
-    .signin('koen@citizenlab.co', 'testtest');
+      .navigate()
+      .signin('koen@citizenlab.co', 'testtest');
 
     browser
-    .url(`http://${process.env.ROOT_URL}/admin/invitations`)
-    .waitForElementVisible('table')
-    .getAttribute('table tbody tr', 'class', (token) => {
-      browser
-      .getText('table tbody tr td', (resEmail) => {
+      .url(`http://${process.env.ROOT_URL}/admin/invitations`)
+      .waitForElementVisible('table')
+      .getAttribute('table tbody tr', 'class', (token) => {
         browser
-        .click('#e2e-user-menu-container')
-        .waitForElementVisible('#e2e-user-menu-dropdown')
-        .waitForElementVisible('#e2e-sign-out-link')
-        .click('#e2e-sign-out-link')
-        .url(`http://${process.env.ROOT_URL}/invite?token=${token.value}`)
-        .waitForElementVisible('#e2e-signup-step1')
-        .assert.value('#email', resEmail.value);
-        signupPage
-        .signup(`Test ${hash}`, `Account ${hash}`, resEmail.value, '123456789');
-        browser
-        .waitForElementVisible('#e2e-landing-page')
-        .click('#e2e-user-menu-container')
-        .waitForElementVisible('#e2e-user-menu-dropdown')
-        .waitForElementVisible('#e2e-sign-out-link')
-        .end();
+          .getText('table tbody tr td', (resEmail) => {
+            browser
+              .click('#e2e-user-menu-container')
+              .waitForElementVisible('#e2e-user-menu-dropdown')
+              .waitForElementVisible('#e2e-sign-out-link')
+              .click('#e2e-sign-out-link')
+              .url(`http://${process.env.ROOT_URL}/invite?token=${token.value}`)
+              .waitForElementVisible('#e2e-signup-step1')
+              .assert.value('#email', resEmail.value);
+            signupPage
+              .signup(`Test ${hash}`, `Account ${hash}`, resEmail.value, '123456789');
+            browser
+              .waitForElementVisible('#e2e-landing-page')
+              .click('#e2e-user-menu-container')
+              .waitForElementVisible('#e2e-user-menu-dropdown')
+              .waitForElementVisible('#e2e-sign-out-link')
+              .end();
+          });
       });
-    });
   },
 };
