9acf62f9b9b3cf2963ab42add2a66a60294f751f
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-07-18 15:41:51 +0200

    Added draft badge and time encoding to campaign index. CL2-2130

--- app/containers/Admin/campaigns/All/SentCampaignRow.tsx
+++ app/containers/Admin/campaigns/All/SentCampaignRow.tsx
@@ -5,6 +5,7 @@ import T from 'components/T';
 import Button from 'components/UI/Button';
 import { FormattedMessage } from 'utils/cl-intl';
 import messages from '../messages';
+import { FormattedDate } from 'react-intl';
 
 interface Props {
   campaign: ICampaignData;
@@ -18,6 +19,7 @@ const SentCampaignRow = ({ campaign }: Props) => (
     <TextCell className="expand">
       <T value={campaign.attributes.subject_multiloc} />
     </TextCell>
+    <FormattedDate value={campaign.attributes.sent_at} />
     <Button linkTo={`/campaigns/${campaign.id}`} style="text" circularCorners={false} icon="search">
       <FormattedMessage {...messages.showButtonLabel} />
     </Button>
