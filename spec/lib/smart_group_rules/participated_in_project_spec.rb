e1ce946b72b1aaee77f40a1e8c241eebc3a373b8
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-07-19 12:13:28 +0200

    Added spec for participated_in_project rule

---
+++ spec/lib/smart_group_rules/participated_in_project_spec.rb
@@ -0,0 +1,54 @@
+require "rails_helper"
+
+describe SmartGroupRules::ParticipatedInProject do
+
+  let(:valid_json_rule) {{
+    'ruleType' => 'participated_in_project',
+    'predicate' => 'is',
+    'value' => create(:project).id
+  }}
+  let(:valid_rule) { SmartGroupRules::ParticipatedInProject.from_json(valid_json_rule) }
+
+  describe "from_json" do
+
+    it "successfully parses a valid json" do
+      expect(valid_rule.predicate).to eq valid_json_rule['predicate']
+      expect(valid_rule.value).to eq valid_json_rule['value']
+    end
+
+  end
+
+  describe "validations" do
+    it "successfully validate the valid rule" do
+      expect(valid_rule).to be_valid
+    end
+  end
+
+  describe "filter" do
+
+    before do
+      @project1 = create(:project)
+      @project2 = create(:project)
+      @user1 = create(:user)
+      @user2 = create(:user)
+      @user3 = create(:user)
+      @idea1 = create(:idea, project: @projects[0], author: @user1)
+      @vote = create(:vote, voteable: @idea, user: @user2)
+      @comment = create(:comment, idea: @idea, author: @user3)
+      @idea2 = create(:idea, project: @projects[1], author: @user3)
+
+    end
+
+    it "correctly filters on 'is' predicate" do
+      rule = SmartGroupRules::ParticipatedInProject.new('is', @project1.id)
+      expect(rule.filter(User)).to match_array [@user1, @user2, @user3]
+    end
+
+    it "correctly filters on 'not_is' predicate" do
+      rule = SmartGroupRules::ParticipatedInProject.new('not_is', @project2.id)
+      expect(rule.filter(User)).to match_array [@user1, @user2]
+    end
+
+  end
+
+end
