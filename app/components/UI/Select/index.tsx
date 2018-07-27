7121abff9dc0c0eb9ebc086f70359ab297ab295a
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-07-27 16:37:44 +0200

    Implemented basic style and functioning react-select v2. CL2-2099 a11y checks todo

--- app/components/UI/Select/index.tsx
+++ app/components/UI/Select/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { isBoolean } from 'lodash';
 import ReactSelect from 'react-select';
 import { IOption } from 'typings';
-import GetTenant from 'resources/GetTenant';
-import { isNilOrError } from 'utils/helperUtils';
 import selectStyles from 'components/UI/Select/styles';
 
 export type Props = {
@@ -14,7 +12,6 @@ export type Props = {
   autoBlur?: boolean | undefined;
   clearable?: boolean | undefined;
   searchable?: boolean | undefined;
-  multi?: boolean | undefined;
   onChange: (arg: IOption) => void;
   onBlur?: () => void;
   disabled?: boolean;
@@ -22,66 +19,59 @@ export type Props = {
 
 type State = {};
 
-export class Select extends React.PureComponent<Props & {tenantColor: string | null}, State> {
+export default class Select extends React.PureComponent<Props, State> {
   private emptyArray: never[];
-  private customStyle: any = {};
 
   constructor(props: Props & {tenantColor: string | null}) {
     super(props as any);
     this.emptyArray = [];
   }
 
-  componentDidUpdate(prevProps) {
-    if (this.props.tenantColor && !prevProps.tenantColor) {
-      const { tenantColor } = this.props;
-      this.customStyle = selectStyles(tenantColor);
-    }
-  }
-
   handleOnChange = (newValue: IOption) => {
     this.props.onChange(newValue || null);
+  }
+
+  //  Needed to keep our API compatible with react-select v1
+  //  For a native react-select solution, follow this issue:
+  //  https://github.com/JedWatson/react-select/issues/2669
+  findFullOptionValue = () => {
+    const { options, value } = this.props;
+    if (typeof value === 'string') {
+      return options && options.find((option) => option.value === value);
+    } else {
+      return value;
+    }
   }
 
   render() {
     const className = this.props['className'];
     const { id } = this.props;
-    let { value, placeholder, options, autoBlur, clearable, searchable, multi } = this.props;
+    let { value, placeholder, options, autoBlur, clearable, searchable } = this.props;
     const { disabled } = this.props;
 
-    value = (value || undefined);
+    value = this.findFullOptionValue();
     placeholder = (placeholder || '');
     options = (options || this.emptyArray);
     autoBlur = (isBoolean(autoBlur) ? autoBlur : true);
     clearable = (isBoolean(clearable) ? clearable : true);
     searchable = (isBoolean(searchable) ? searchable : false);
-    multi = (isBoolean(multi) ? multi : false);
 
     return (
       <ReactSelect
         id={id}
         className={className}
-        openOnFocus={false}
-        clearable={clearable}
-        searchable={searchable}
-        scrollMenuIntoView={false}
-        autoBlur={autoBlur}
+        isClearable={clearable}
+        isSearchable={searchable}
+        menuShouldScrollIntoView={false}
+        blurInputOnSelect={autoBlur}
         value={value}
         placeholder={placeholder}
         options={options}
         onChange={this.handleOnChange}
         onBlur={this.props.onBlur}
-        isMulti={multi}
-        disabled={disabled}
-        styles={this.customStyle}
+        isDisabled={disabled}
+        styles={selectStyles}
       />
     );
   }
 }
-
-export default (props: Props) => (
-  <GetTenant>
-    {(tenant) => (
-      <Select tenantColor={isNilOrError(tenant) ? null : tenant.attributes.settings.core.color_main} {...props} />
-    )}
-  </GetTenant>
-);
