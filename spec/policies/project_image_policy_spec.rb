d0bc6cc2556966def46908b80dfffa41fb9cab67
Sébastien Hoorens <sebastien@citizenlab.co>
2018-08-03 16:37:08 +0200

    project img pol spec

---
+++ spec/policies/project_image_policy_spec.rb
@@ -0,0 +1,109 @@
+require 'rails_helper'
+
+describe ProjectImagePolicy do
+  subject { ProjectImagePolicy.new(user, image) }
+  let(:scope) { ProjectImagePolicy::Scope.new(user, idea.idea_images) }
+
+  context "on an image in a public project" do 
+    let(:project) { create(:continuous_project) }
+    let!(:image) { create(:project_image, project: project)}
+
+	  context "for a visitor" do 
+	  	let(:user) { nil }
+
+	    it { should     permit(:show)    }
+	    it { should_not permit(:create)  }
+	    it { should_not permit(:update)  }
+	    it { should_not permit(:destroy) }
+
+	    it "should index the image" do
+	      expect(scope.resolve.size).to eq 1
+	    end
+	  end
+
+	  context "for a user who is not the idea author" do
+      let(:user) { create(:user) }
+
+      it { should     permit(:show)    }
+      it { should_not permit(:create)  }
+      it { should_not permit(:update)  }
+      it { should_not permit(:destroy) }
+
+      it "should index the image" do
+        expect(scope.resolve.size).to eq 1
+      end
+    end
+
+    context "for a user who is the idea author" do
+      let(:user) { idea.author }
+
+      it { should     permit(:show)    }
+      it { should     permit(:create)  }
+      it { should     permit(:update)  }
+      it { should     permit(:destroy) }
+
+      it "should index the image" do
+        expect(scope.resolve.size).to eq 1
+      end
+    end
+
+    context "for an admin" do
+      let(:user) { create(:admin) }
+
+      it { should permit(:show)    }
+      it { should permit(:create)  }
+      it { should permit(:update)  }
+      it { should permit(:destroy) }
+
+      it "should index the image" do
+        expect(scope.resolve.size).to eq 1
+      end
+    end
+
+    context "for a moderator" do
+      let(:user) { create(:moderator, project: project) }
+
+      it { should permit(:show)    }
+      it { should permit(:create)  }
+      it { should permit(:update)  }
+      it { should permit(:destroy) }
+
+      it "should index the image"  do
+        expect(scope.resolve.size).to eq 1
+      end
+    end
+	end
+
+	 context "on an image in a private admins project" do 
+	 	let(:project) { create(:private_admins_project) }
+    let!(:image) { create(:project_image, project: project)}
+
+    context "for a user" do
+      let(:user) { create(:user) }
+
+      it { should_not permit(:show)    }
+      it { should_not permit(:create)  }
+      it { should_not permit(:update)  }
+      it { should_not permit(:destroy) }
+
+      it "should not index the image" do
+        expect(scope.resolve.size).to eq 0
+      end
+    end
+
+    context "for an admin" do
+      let(:user) { create(:admin) }
+
+      it { should permit(:show)    }
+      it { should permit(:create)  }
+      it { should permit(:update)  }
+      it { should permit(:destroy) }
+
+      it "should index the image" do
+        expect(scope.resolve.size).to eq 1
+      end
+    end
+  end
+
+end
+    
