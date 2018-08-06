9ba5c9aa73e2fbf0767bc2b9af29a7cb1453adcb
Sébastien Hoorens <sebastien@citizenlab.co>
2018-08-06 17:59:40 +0200

    initial version

--- engines/public_api/config/routes.rb
+++ engines/public_api/config/routes.rb
@@ -4,6 +4,8 @@ PublicApi::Engine.routes.draw do
     post 'authenticate' => "api_token#create"
     resources :ideas, only: [:index, :show]
     resources :projects, only: [:index, :show]
+
+    match 'manifest.json', to: 'manifest#show', via: :get
   end
   
 end
