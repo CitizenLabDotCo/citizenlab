89d89794c9b65e5537f582dd80614da623ed98b7
Brent Guffens <brentguf@users.noreply.github.com>
2018-08-10 11:03:21 +0200

    Only set NODE_ENV for CI

--- .pa11yci.js
+++ .pa11yci.js
@@ -4,21 +4,7 @@ const config = {
     timeout: 10000
   },
   standard: 'WCAG2A',
-  urls: process.env.NODE_ENV === 'development' ? [
-    'http://localhost:3000/en/',
-    'http://localhost:3000/en/projects',
-    'http://localhost:3000/en/projects/ratione-rerum-minus-quisquam-aperiam/process',
-    'http://localhost:3000/en/projects/ratione-rerum-minus-quisquam-aperiam/info',
-    'http://localhost:3000/en/projects/ratione-rerum-minus-quisquam-aperiam/events',
-    'http://localhost:3000/en/ideas',
-    'http://localhost:3000/en/ideas/ut-maiores-dolorem-optio-aut-quas',
-    'http://localhost:3000/en/sign-in',
-    'http://localhost:3000/en/sign-up',
-    'http://localhost:3000/en/profile/koen-gremmelprez',
-    'http://localhost:3000/en/ideas/new',
-    'http://localhost:3000/en/projects/open-idea-project/ideas/new',
-    'http://localhost:3000/en/pages/information',
-  ] : [
+  urls: process.env.NODE_ENV === 'staging' ? [
     'https://demo.stg.citizenlab.co/en/',
     'https://demo.stg.citizenlab.co/en/projects',
     'https://demo.stg.citizenlab.co/en/projects/renewing-westbrook-parc/process',
@@ -32,6 +18,20 @@ const config = {
     'https://demo.stg.citizenlab.co/en/ideas/new',
     'https://demo.stg.citizenlab.co/en/projects/open-idea-project/ideas/new',
     'https://demo.stg.citizenlab.co/en/pages/information'
+  ] : [
+    'http://localhost:3000/en/',
+    'http://localhost:3000/en/projects',
+    'http://localhost:3000/en/projects/ratione-rerum-minus-quisquam-aperiam/process',
+    'http://localhost:3000/en/projects/ratione-rerum-minus-quisquam-aperiam/info',
+    'http://localhost:3000/en/projects/ratione-rerum-minus-quisquam-aperiam/events',
+    'http://localhost:3000/en/ideas',
+    'http://localhost:3000/en/ideas/ut-maiores-dolorem-optio-aut-quas',
+    'http://localhost:3000/en/sign-in',
+    'http://localhost:3000/en/sign-up',
+    'http://localhost:3000/en/profile/koen-gremmelprez',
+    'http://localhost:3000/en/ideas/new',
+    'http://localhost:3000/en/projects/open-idea-project/ideas/new',
+    'http://localhost:3000/en/pages/information',
   ]
 }
 
