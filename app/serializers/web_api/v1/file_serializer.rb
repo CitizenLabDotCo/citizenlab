6e239296e4cd4c72ed6c6c1a6131fab567fbf64e
Sébastien Hoorens <sebastien@citizenlab.co>
2018-08-13 11:44:41 +0200

    added name to files tables

--- app/serializers/web_api/v1/file_serializer.rb
+++ app/serializers/web_api/v1/file_serializer.rb
@@ -1,11 +1,7 @@
 class WebApi::V1::FileSerializer < ActiveModel::Serializer
-  attributes :id, :file, :ordering, :file_name, :file_size, :created_at, :updated_at
+  attributes :id, :file, :ordering, :name, :size, :created_at, :updated_at
 
-  def file_name
-    object.file.file.filename
-  end
-
-  def file_size
+  def size
     object.file.file.size
   end
 end
