92d62e4b1472a40bec86effbdeb6e32b9646a716
Sébastien Hoorens <sebastien@citizenlab.co>
2018-08-13 16:13:58 +0200

    non-shallow files in shallow phases and events

--- config/routes.rb
+++ config/routes.rb
@@ -63,13 +63,19 @@ Rails.application.routes.draw do
         get 'by_slug/:slug', on: :collection, to: 'pages#by_slug'
       end
 
+      # Events and phases are split in two because we cannot have a non-shallow
+      # resource (i.e. files) nested in a shallow resource. File resources have
+      # to be shallow so we can determine their container class. See e.g.
+      # https://github.com/rails/rails/pull/24405
+      resources :events, only: [:show, :edit, :update, :destroy] do
+        resources :files, defaults: {container_class: Event, file_class: EventFile}, shallow: false
+      end
+      resources :phases, only: [:show, :edit, :update, :destroy] do
+        resources :files, defaults: {container_class: Phase, file_class: PhaseFile}, shallow: false
+      end
       resources :projects do
-        resources :phases do
-          resources :files, defaults: {container_class: Phase, file_class: PhaseFile}
-        end
-        resources :events do
-          resources :files, defaults: {container_class: Event, file_class: EventFile}
-        end
+        resources :events, only: [:index, :new, :create]
+        resources :phases, only: [:index, :new, :create]
         resources :images, defaults: {container_class: Project, image_class: ProjectImage}
         resources :files, defaults: {container_class: Project, file_class: ProjectFile}
         resources :groups_projects, shallow: true, except: [:update]
