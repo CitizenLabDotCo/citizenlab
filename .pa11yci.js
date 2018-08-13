2a925f32607e448bc91b342c5f9dcee77a27ae02
Brent Guffens <brentguf@users.noreply.github.com>
2018-08-13 16:40:29 +0200

    Add rule to ignore 'form without submit button' pa11y error

--- .pa11yci.js
+++ .pa11yci.js
@@ -5,12 +5,37 @@ const config = {
   },
   standard: 'WCAG2A',
   urls: process.env.NODE_ENV === 'staging' ? [
-    'https://demo.stg.citizenlab.co/en/',
+    // Every page that has the IdeaCards component
+    // will complain a missing submit button for the search field
+    // So we ignore this specific rule for such pages
+    {
+      url: 'https://demo.stg.citizenlab.co/en/',
+      ignore: [
+        'WCAG2AA.Principle3.Guideline3_2.3_2_2.H32.2'
+      ]
+    }, {
+      url: 'https://demo.stg.citizenlab.co/en/ideas',
+      ignore: [
+        'WCAG2AA.Principle3.Guideline3_2.3_2_2.H32.2'
+      ]
+    }, {
+      url: 'https://demo.stg.citizenlab.co/en/projects/renewing-westbrook-parc/process',
+      ignore: [
+        'WCAG2AA.Principle3.Guideline3_2.3_2_2.H32.2'
+      ]
+    }, {
+      url: 'https://demo.stg.citizenlab.co/en/projects/renewing-westbrook-parc/info',
+      ignore: [
+        'WCAG2AA.Principle3.Guideline3_2.3_2_2.H32.2'
+      ]
+    }, {
+      url: 'http://localhost:3000/en/profile/koen-gremmelprez',
+      ignore: [
+        'WCAG2AA.Principle3.Guideline3_2.3_2_2.H32.2'
+      ]
+    },
     'https://demo.stg.citizenlab.co/en/projects',
-    'https://demo.stg.citizenlab.co/en/projects/renewing-westbrook-parc/process',
-    'https://demo.stg.citizenlab.co/en/projects/renewing-westbrook-parc/info',
-    'https://demo.stg.citizenlab.co/en/projects/renewing-westbrook-parc/events',
-    'https://demo.stg.citizenlab.co/en/ideas',
+    'https://demo.stg.citizenlab.co/en/projects/renewing-westbrook-parc/events',,
     'https://demo.stg.citizenlab.co/en/ideas/more-box-parking-for-bikes-in-the-centrum',
     'https://demo.stg.citizenlab.co/en/sign-in',
     'https://demo.stg.citizenlab.co/en/sign-up',
@@ -27,8 +52,7 @@ const config = {
     'http://localhost:3000/en/ideas',
     'http://localhost:3000/en/ideas/ut-maiores-dolorem-optio-aut-quas',
     'http://localhost:3000/en/sign-in',
-    'http://localhost:3000/en/sign-up',
-    'http://localhost:3000/en/profile/koen-gremmelprez',
+    'http://localhost:3000/en/sign-up',,
     'http://localhost:3000/en/ideas/new',
     'http://localhost:3000/en/projects/open-idea-project/ideas/new',
     'http://localhost:3000/en/pages/information',
