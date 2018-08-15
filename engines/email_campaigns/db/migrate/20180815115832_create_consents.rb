14d9d98baee6e94bf7f1bd13072a79e2ca6e70f0
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-08-15 13:46:48 +0200

    Added migrations for campaigns, consents and deliveries

---
+++ engines/email_campaigns/db/migrate/20180815115832_create_consents.rb
@@ -0,0 +1,12 @@
+class CreateConsents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :email_campaigns_consents, id: :uuid do |t|
+      t.string :campaign_type, null: false
+      t.references :user, type: :uuid, index: true, null: false
+      t.boolean :consented, null: false
+
+      t.index [:campaign_type, :user_id], unique: true
+    end
+
+  end
+end
