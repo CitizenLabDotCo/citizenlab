378ab7c4dd94514a8140da875cc1b047cd0beae2
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-08-19 00:39:41 +0200

    Added API for consents

--- engines/email_campaigns/app/models/email_campaigns/consent.rb
+++ engines/email_campaigns/app/models/email_campaigns/consent.rb
@@ -7,5 +7,17 @@ module EmailCampaigns
     validates :consented, inclusion: {in: [true, false]}
     # validates :campaign_type, inclusion: {in: Proc.new{DeliveryService.new.campaign_types}}
 
+    def self.create_for_user! user_id
+      defined_types = where(user_id: user_id).pluck(:campaign_type)&.uniq
+      available_types = DeliveryService.new.campaign_types
+      (available_types - defined_types).each do |campaign_type|
+        create!(
+          user_id: user_id,
+          campaign_type: campaign_type,
+          consented: true
+        )
+      end
+
+    end
   end
 end
