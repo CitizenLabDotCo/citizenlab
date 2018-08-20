8f1557aeb352719a43b51eb198c96cf810bf9499
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-08-20 17:32:12 +0200

    CL2-2300 Added campaign_type_description_multiloc to consent attributes

--- engines/email_campaigns/app/serializers/email_campaigns/web_api/v1/consent_serializer.rb
+++ engines/email_campaigns/app/serializers/email_campaigns/web_api/v1/consent_serializer.rb
@@ -1,5 +1,12 @@
 module EmailCampaigns
   class WebApi::V1::ConsentSerializer < ActiveModel::Serializer
-    attributes :id, :campaign_type, :consented, :created_at, :updated_at
+    attributes :id, :campaign_type, :campaign_type_description_multiloc, :consented, :created_at, :updated_at
+
+    def campaign_type_description_multiloc
+      @multiloc_service ||= MultilocService.new
+      @multiloc_service.i18n_to_multiloc(
+        "email_campaigns.campaign_type_description.#{object.campaign_type}"
+      )
+    end
   end
 end
