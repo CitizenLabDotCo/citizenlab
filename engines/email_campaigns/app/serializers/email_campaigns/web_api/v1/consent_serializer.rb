7826e8b9d960ff70cec452d4265030d17dcca502
Sébastien Hoorens <sebastien@citizenlab.co>
2018-08-21 11:39:40 +0200

    campaign name as locale keys and provided by consent serializer

--- engines/email_campaigns/app/serializers/email_campaigns/web_api/v1/consent_serializer.rb
+++ engines/email_campaigns/app/serializers/email_campaigns/web_api/v1/consent_serializer.rb
@@ -1,11 +1,16 @@
 module EmailCampaigns
   class WebApi::V1::ConsentSerializer < ActiveModel::Serializer
-    attributes :id, :campaign_type, :campaign_type_description_multiloc, :consented, :created_at, :updated_at
+    attributes :id, :campaign_name, :campaign_type_description_multiloc, :consented, :created_at, :updated_at
+
+    def campaign_name
+    	object.campaign_type.safe_constantize&.campaign_name
+    end
 
     def campaign_type_description_multiloc
+      # TODO campaign_name might be nil if deleted campaign
       @multiloc_service ||= MultilocService.new
       @multiloc_service.i18n_to_multiloc(
-        "email_campaigns.campaign_type_description.#{object.campaign_type}"
+        "email_campaigns.campaign_type_description.#{campaign_name}"
       )
     end
   end
