1f676fd1213d1d3becd4dfaedf32974fd26c183a
Brent Guffens <brentguf@users.noreply.github.com>
2018-08-21 17:09:14 +0200

    WIP: Add submit button/success state to form

--- app/containers/UsersEditPage/CampaignsConsentForm.tsx
+++ app/containers/UsersEditPage/CampaignsConsentForm.tsx
@@ -6,9 +6,10 @@ import ProfileSection from './ProfileSection';
 import GetCampaignConsents, { GetCampaignConsentsChildProps } from 'resources/GetCampaignConsents';
 import { isNilOrError } from 'utils/helperUtils';
 import Checkbox from 'components/UI/Checkbox';
-import { IConsentData, updateConsent } from 'services/campaignConsents';
+import { IConsentData, updateConsent, IConsent } from 'services/campaignConsents';
 import styled from 'styled-components';
 import T from 'components/T';
+import { find } from 'lodash';
 
 const CheckboxContainer = styled.div`
   padding-bottom: 0.5rem;
@@ -22,27 +23,72 @@ type DataProps = {
 
 type Props = InputProps & DataProps;
 
-class CampaignsConsentForm extends PureComponent<Props> {
+interface State {
+  consentChanges: {};
+}
+
+class CampaignsConsentForm extends PureComponent<Props, State> {
+
+  constructor(props: Props) {
+    super(props as any);
+    this.state = {
+      consentChanges: {},
+    };
+  }
 
   handleOnChange = (consent: IConsentData) => () => {
-    updateConsent(consent.id, { consented: !consent.attributes.consented });
+      const becomesConsented = this.isConsented(consent.id);
+    this.setState(prevState => ({
+      consentChanges: {
+        ...prevState.consentChanges,
+        [consent.id]: !becomesConsented,
+      }
+    }));
+  }
+
+  isConsented = (consentId) => {
+    const { consents } = this.props;
+    if (!isNilOrError(consents)) {
+      const consent = find(consents, { id: consentId }) as IConsentData;
+      if (typeof(this.state.consentChanges[consentId]) === 'undefined') {
+        return (consent && consent.attributes.consented);
+      } else {
+        return this.state.consentChanges[consentId];
+      }
+    }
+    return false;
+  }
+
+  handleOnSubmit() {
+    const { consentChanges } = this.state;
+    let consentUpdates: Promise<IConsent>[] = [];
+
+    if (consentChanges) {
+      consentUpdates = Object.keys(consentChanges).map(consentId => {
+        return updateConsent(consentId, { consented: this.isConsented(consentId) });
+      });
+    }
+
+    Promise.all(consentUpdates).then(() => this.setState({ consentChanges: {} }));
   }
 
   render() {
     const { consents } = this.props;
     return (
       <ProfileSection>
-        <SectionTitle><FormattedMessage {...messages.notificationsTitle} /></SectionTitle>
-        <SectionSubtitle><FormattedMessage {...messages.notificationsSubTitle} /></SectionSubtitle>
-        {!isNilOrError(consents) && consents.map((consent) => (
-          <CheckboxContainer key={consent.id}>
-            <Checkbox
-              value={consent.attributes.consented}
-              onChange={this.handleOnChange(consent)}
-              label={<T value={consent.attributes.campaign_type_description_multiloc} />}
-            />
-          </CheckboxContainer>
-        ))}
+        <form action="">
+          <SectionTitle><FormattedMessage {...messages.notificationsTitle} /></SectionTitle>
+          <SectionSubtitle><FormattedMessage {...messages.notificationsSubTitle} /></SectionSubtitle>
+          {!isNilOrError(consents) && consents.map((consent) => (
+            <CheckboxContainer key={consent.id}>
+              <Checkbox
+                value={this.isConsented(consent.id)}
+                onChange={this.handleOnChange(consent)}
+                label={<T value={consent.attributes.campaign_type_description_multiloc} />}
+              />
+            </CheckboxContainer>
+          ))}
+        </form>
       </ProfileSection>
     );
   }
