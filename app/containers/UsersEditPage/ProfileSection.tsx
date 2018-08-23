b746c3d1041ca0f72a69f7dee5c3b3552729df4b
Brent Guffens <brentguf@users.noreply.github.com>
2018-08-23 15:09:14 +0200

    Replace semantic UI with custom styles

--- app/containers/UsersEditPage/ProfileSection.tsx
+++ app/containers/UsersEditPage/ProfileSection.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
-import ContentContainer from 'components/ContentContainer';
-import { color } from 'utils/styleUtils';
-import { Grid, Segment } from 'semantic-ui-react';
 
-const StyledContentContainer = styled(ContentContainer)`
-  background: ${color('background')};
-  padding-top: 25px;
-  padding-bottom: 40px;
+const StyledContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 700px;
+  background: #fff;
+  padding: 40px;
+  border: 1px solid #ddd;
+  box-shadow: 0 0 1px #ddd;
+  border-radius: 5px;
+  margin: 0 auto 30px;
 `;
 
 export default ({ children }) => (
   <StyledContentContainer>
-    <Grid centered>
-      <Grid.Row>
-        <Grid.Column computer={12} mobile={16}>
-          <Segment padded="very">
-            {children}
-          </Segment>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+    {children}
   </StyledContentContainer>
 );
