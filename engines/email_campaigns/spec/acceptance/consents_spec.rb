4c736670014169d966e466ab2c1591b5b79bcb29
Sébastien Hoorens <sebastien@citizenlab.co>
2018-08-24 10:56:27 +0200

    added campaings for mortals to consents spec

--- engines/email_campaigns/spec/acceptance/consents_spec.rb
+++ engines/email_campaigns/spec/acceptance/consents_spec.rb
@@ -18,6 +18,8 @@ resource "Campaign consents" do
     before do
       @campaigns = [
         create(:comment_on_your_comment_campaign),
+        create(:comment_on_your_idea_campaign),
+        create(:user_digest_campaign),
         create(:manual_campaign),
       ]
       @consents = @campaigns.map.with_index do |campaign, i|
