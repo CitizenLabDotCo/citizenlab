974530addeab16bd7a64698fa1334eac0d16da6f
Sébastien Hoorens <sebastien@citizenlab.co>
2018-08-27 16:14:43 +0200

    admin rights received speced and fixed

--- engines/email_campaigns/spec/factories/campaigns.rb
+++ engines/email_campaigns/spec/factories/campaigns.rb
@@ -20,6 +20,38 @@ FactoryBot.define do
     enabled true
   end
 
+  factory :admin_rights_received_campaign, class: EmailCampaigns::Campaigns::AdminRightsReceived do
+    enabled true
+  end
+
+  factory :comment_deleted_by_admin_campaign, class: EmailCampaigns::Campaigns::CommentDeletedByAdmin do
+    enabled true
+  end
+
+  factory :comment_marked_as_spam_campaign, class: EmailCampaigns::Campaigns::CommentMarkedAsSpam do
+    enabled true
+  end
+
+  factory :idea_marked_as_spam_campaign, class: EmailCampaigns::Campaigns::IdeaMarkedAsSpam do
+    enabled true
+  end
+
+  factory :invite_accepted_campaign, class: EmailCampaigns::Campaigns::InviteAccepted do
+    enabled true
+  end
+
+  factory :mention_in_comment_campaign, class: EmailCampaigns::Campaigns::MentionInComment do
+    enabled true
+  end
+
+  factory :project_moderation_rights_received_campaign, class: EmailCampaigns::Campaigns::ProjectModerationRightsReceived do
+    enabled true
+  end
+
+  factory :status_change_of_your_idea_campaign, class: EmailCampaigns::Campaigns::StatusChangeOfYourIdea do
+    enabled true
+  end
+
   factory :admin_digest_campaign, class: EmailCampaigns::Campaigns::AdminDigest do
     enabled true
     schedule { weekly_schedule }
