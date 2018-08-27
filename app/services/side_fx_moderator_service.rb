586cc57075d139ff1fa74c84ed00cc3f43c4ebad
Sébastien Hoorens <sebastien@citizenlab.co>
2018-08-27 17:09:27 +0200

    moderator given speced and fixed and more fixes

--- app/services/side_fx_moderator_service.rb
+++ app/services/side_fx_moderator_service.rb
@@ -8,7 +8,6 @@ class SideFxModeratorService
       current_user, Time.now.to_i,
       payload: {project_id: project.id}
       )
-    log_project_moderation_rights_received moderator, project, current_user
   end
 
   def after_destroy moderator, project, current_user
@@ -17,27 +16,5 @@ class SideFxModeratorService
       current_user, Time.now.to_i
       )
   end
-
-
-  private 
-
-  def log_project_moderation_rights_received moderator, project, current_user
-    project_serializer = "WebApi::V1::External::ProjectSerializer".constantize 
-    serialized_project = ActiveModelSerializers::SerializableResource.new(project, {
-      serializer: project_serializer,
-      adapter: :json
-     }).serializable_hash
-    current_user_serializer = "WebApi::V1::External::UserSerializer".constantize
-    serialized_current_user = ActiveModelSerializers::SerializableResource.new(current_user, {
-      serializer: current_user_serializer,
-      adapter: :json
-     }).serializable_hash
-    LogActivityJob.set(wait: 5.seconds).perform_later(
-      moderator, 'project_moderation_rights_received', 
-      moderator, Time.now.to_i, 
-      payload: {project: serialized_project, initiator: serialized_current_user}
-      ) 
-  end
-
 
 end
