cc39b9763717a5fb155afb6551c85c4456e47485
Sébastien Hoorens <sebastien@citizenlab.co>
2018-08-27 16:28:49 +0200

    added serializers of notification campaigns

---
+++ engines/email_campaigns/app/serializers/email_campaigns/campaigns/comment_deleted_by_admin_serializer.rb
@@ -0,0 +1,15 @@
+module EmailCampaigns::Campaigns
+  class CommentDeletedByAdminSerializer < NotificationSerializer
+    attributes :reason_code, :other_reason
+
+    belongs_to :initiating_user, serializer: CustomUserSerializer
+    belongs_to :comment, serializer: CustomCommentSerializer
+    belongs_to :comment_author, serializer: CustomUserSerializer
+    belongs_to :idea, serializer: CustomIdeaSerializer
+    belongs_to :idea_author, serializer: CustomUserSerializer
+    has_many :idea_images, serializer: CustomImageSerializer
+    has_many :idea_topics, serializer: ::WebApi::V1::TopicSerializer
+    belongs_to :project, serializer: CustomProjectSerializer
+    has_many :project_images, serializer: CustomImageSerializer
+  end
+end
---
+++ engines/email_campaigns/app/serializers/email_campaigns/campaigns/comment_marked_as_spam_serializer.rb
@@ -0,0 +1,11 @@
+module EmailCampaigns::Campaigns
+	class CommentMarkedAsSpamSerializer < NotificationSerializer
+	  belongs_to :initiating_user, serializer: CustomUserSerializer
+	  belongs_to :spam_report, serializer: ::WebApi::V1::External::SpamReportSerializer
+	  belongs_to :comment, serializer: CustomCommentSerializer
+	  belongs_to :idea, serializer: CustomIdeaSerializer
+	  has_many :idea_images, serializer: CustomImageSerializer
+	  belongs_to :project, serializer: CustomProjectSerializer
+	  has_many :project_images, serializer: CustomImageSerializer
+	end
+end
