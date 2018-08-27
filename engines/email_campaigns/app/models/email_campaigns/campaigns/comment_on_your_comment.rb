af47039889d5403fa94ee3dac6b4a9bf9e617201
Sébastien Hoorens <sebastien@citizenlab.co>
2018-08-27 14:06:31 +0200

    notification campaign: shared code for all notification campaigns

--- engines/email_campaigns/app/models/email_campaigns/campaigns/comment_on_your_comment.rb
+++ engines/email_campaigns/app/models/email_campaigns/campaigns/comment_on_your_comment.rb
@@ -1,26 +1,4 @@
 module EmailCampaigns
-  class Campaigns::CommentOnYourComment < Campaign
-    include Disableable
-    include Consentable
-    include ActivityTriggerable
-    include RecipientConfigurable
-
-    recipient_filter :filter_notification_recipient
-
-    def activity_triggers
-      {'Notifications::CommentOnYourComment' => {'created' => true}}
-    end
-
-    def filter_notification_recipient users_scope, activity:, time: nil
-      users_scope.where(id: activity.item.recipient.id)
-    end
-
-    def generate_commands recipient:, activity:
-      notification = activity.item
-      [{
-        event_payload: serialize_campaign(notification).values.first,
-      }]
-    end
-
+  class Campaigns::CommentOnYourComment < Campaigns::NotificationCampaign
   end
 end
--- engines/email_campaigns/app/models/email_campaigns/campaigns/comment_on_your_idea.rb
+++ engines/email_campaigns/app/models/email_campaigns/campaigns/comment_on_your_idea.rb
@@ -1,26 +1,4 @@
 module EmailCampaigns
-  class Campaigns::CommentOnYourIdea < Campaign
-    include Disableable
-    include Consentable
-    include ActivityTriggerable
-    include RecipientConfigurable
-
-    recipient_filter :filter_notification_recipient
-
-    def activity_triggers
-      {'Notifications::CommentOnYourIdea' => {'created' => true}}
-    end
-
-    def filter_notification_recipient users_scope, activity:, time: nil
-      users_scope.where(id: activity.item.recipient.id)
-    end
-
-    def generate_commands recipient:, activity:
-      notification = activity.item
-      [{
-        event_payload: serialize_campaign(notification).values.first,
-      }]
-    end
-
+  class Campaigns::CommentOnYourIdea < Campaigns::NotificationCampaign
   end
 end
