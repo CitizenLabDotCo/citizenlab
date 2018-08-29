5cbd78cbc22dbf109682e20c714cd76cea2352d7
Sébastien Hoorens <sebastien@citizenlab.co>
2018-08-29 14:18:53 +0200

    welcome spec

---
+++ engines/email_campaigns/spec/models/email_campaigns/campaigns/welcome_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe EmailCampaigns::Campaigns::Welcome, type: :model do
+  describe "Welcome Campaign default factory" do
+    it "is valid" do
+      expect(build(:welcome_campaign)).to be_valid
+    end
+  end
+
+  describe '#generate_command' do
+  	let(:campaign) { create(:welcome_campaign) }
+    let(:user) { create(:user) }
+    let(:activity) { create(:activity, item: user, action: 'completed_registration', user: user) }
+
+  	it "generates a command with the desired payload and tracked content" do
+  		commands = campaign.generate_commands(recipient: user, activity: activity)
+
+      expect(commands.size).to eq(1)
+  	end
+  end
+end
