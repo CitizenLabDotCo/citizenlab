81468d00099d58d611cf7b468b6ea03b888cb511
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-09-02 23:18:29 +0200

    Improved IdeasWidget form to use collapses and expanded options. WIP CL2-2344

--- app/containers/Admin/settings/widgets/IdeasWidget/Form.tsx
+++ app/containers/Admin/settings/widgets/IdeasWidget/Form.tsx
@@ -3,14 +3,13 @@ import { Form, Field, InjectedFormikProps } from 'formik';
 
 // Components
 import FormikMultipleSelect from 'components/UI/FormikMultipleSelect';
-import { Section, SectionField, SectionTitle } from 'components/admin/Section';
+import { Section, SectionField } from 'components/admin/Section';
 import Error from 'components/UI/Error';
 import FormikInput from 'components/UI/FormikInput';
 import Label from 'components/UI/Label';
 
 // I18n
-import { FormattedMessage, injectIntl } from 'utils/cl-intl';
-import { InjectedIntlProps } from 'react-intl';
+import { FormattedMessage } from 'utils/cl-intl';
 import localize, { injectedLocalized } from 'utils/localize';
 import messages from '../messages';
 
@@ -20,18 +19,34 @@ import GetProjects from 'resources/GetProjects';
 
 // Utils
 import { isNilOrError } from 'utils/helperUtils';
-import FormikSubmitWrapper from 'components/admin/FormikSubmitWrapper';
+import Collapse from 'components/admin/Collapse';
+import FormikToggle from 'components/UI/FormikToggle';
+import Button from 'components/UI/Button';
 
 export interface Props { }
 
 export interface FormValues {
+  width: number;
+  height: number;
+  showHeader: boolean;
+  headerText: string;
   topics: string[];
   projects: string[];
   limit: number;
 }
 
-class WidgetForm extends PureComponent<InjectedFormikProps<Props & injectedLocalized & InjectedIntlProps, FormValues>> {
+interface State {
+  openedCollapse: 'ideas' | 'style' | 'headerAndFooter' | null;
+}
 
+class WidgetForm extends PureComponent<InjectedFormikProps<Props & injectedLocalized, FormValues>, State> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      openedCollapse: null,
+    };
+  }
   resourcesToOptionList = (resources) => {
     return resources && resources.map((resource) => ({
       label: this.props.localize(resource.attributes.title_multiloc),
@@ -39,71 +54,170 @@ class WidgetForm extends PureComponent<InjectedFormikProps<Props & injectedLocal
     }));
   }
 
+  handleCollapseToggle = (collapse) => () => {
+    this.setState({
+      openedCollapse: this.state.openedCollapse === collapse ? null : collapse,
+    });
+  }
+
+  handleSubmit = () => {
+    this.props.handleSubmit();
+  }
+
   render() {
-    const { errors, touched, isValid, isSubmitting } = this.props;
+    const { errors, touched, values } = this.props;
+    const { openedCollapse } = this.state;
 
     return (
       <Form>
 
-        <Section>
-          <SectionTitle>
-            <FormattedMessage {...messages.titleIdeasFilters} />
-          </SectionTitle>
+        <Collapse
+          opened={openedCollapse === 'style'}
+          onToggle={this.handleCollapseToggle('style')}
+          label={<FormattedMessage {...messages.titleStyle} />}
+        >
+          <Section>
 
-          <SectionField>
-            <Label>
-              <FormattedMessage {...messages.fieldProjects} />
-            </Label>
-            <GetProjects publicationStatuses={['published', 'archived']}>
-              {(projects) => (projects && isNilOrError(projects)) ? null : (
+            <SectionField>
+              <Label>
+                <FormattedMessage {...messages.fieldWidth} />
+              </Label>
                 <Field
-                  name="projects"
-                  component={FormikMultipleSelect}
-                  options={this.resourcesToOptionList(projects.projectsList)}
+                  name="width"
+                  component={FormikInput}
+                  type="number"
                 />
-              )}
-            </GetProjects>
-          </SectionField>
+              {touched.width && <Error
+                fieldName="width"
+                apiErrors={errors.width as any}
+              />}
+            </SectionField>
 
-          <SectionField>
-            <Label>
-              <FormattedMessage {...messages.fieldTopics} />
-            </Label>
-            <GetTopics>
-              {(topics) => (topics && isNilOrError(topics)) ? null : (
+            <SectionField>
+              <Label>
+                <FormattedMessage {...messages.fieldHeight} />
+              </Label>
+              <Field
+                name="height"
+                component={FormikInput}
+                type="number"
+              />
+              {touched.height && <Error
+                fieldName="height"
+                apiErrors={errors.height as any}
+              />}
+            </SectionField>
+
+          </Section>
+
+        </Collapse>
+
+        <Collapse
+          opened={openedCollapse === 'headerAndFooter'}
+          onToggle={this.handleCollapseToggle('headerAndFooter')}
+          label={<FormattedMessage {...messages.titleHeaderAndFooter} />}
+        >
+          <Section>
+
+            <SectionField>
+              <Field
+                name="showHeader"
+                component={FormikToggle}
+                label={<FormattedMessage {...messages.fieldShowHeader} />}
+              />
+              {touched.showHeader && <Error
+                fieldName="showHeader"
+                apiErrors={errors.showHeader as any}
+              />}
+            </SectionField>
+
+            {values.showHeader &&
+              <SectionField>
+                <Label>
+                  <FormattedMessage {...messages.fieldHeaderText} />
+                </Label>
                 <Field
-                  name="topics"
-                  component={FormikMultipleSelect}
-                  options={this.resourcesToOptionList(topics)}
+                  name="headerText"
+                  component={FormikInput}
+                  type="text"
                 />
-              )}
-            </GetTopics>
-          </SectionField>
+                {touched.headerText && <Error
+                  fieldName="headerText"
+                  apiErrors={errors.headerText as any}
+                />}
+              </SectionField>
+            }
 
-          <SectionField>
-            <Label>
-              <FormattedMessage {...messages.fieldLimit} />
-            </Label>
-            <Field
-              name="limit"
-              component={FormikInput}
-              type="number"
-            />
-            {touched.limit && <Error
-              fieldName="limit"
-              apiErrors={errors.limit as any}
-            />}
-          </SectionField>
+          </Section>
 
-        </Section>
+        </Collapse>
 
-        <FormikSubmitWrapper
-          {...{ isValid, isSubmitting, status, touched }}
-        />
+        <Collapse
+          opened={openedCollapse === 'ideas'}
+          onToggle={this.handleCollapseToggle('ideas')}
+          label={<FormattedMessage {...messages.titleIdeaContent} />}
+        >
+          <Section>
+
+            <SectionField>
+              <Label>
+                <FormattedMessage {...messages.fieldProjects} />
+              </Label>
+              <GetProjects publicationStatuses={['published', 'archived']}>
+                {(projects) => (projects && isNilOrError(projects)) ? null : (
+                  <Field
+                    name="projects"
+                    component={FormikMultipleSelect}
+                    options={this.resourcesToOptionList(projects.projectsList)}
+                  />
+                )}
+              </GetProjects>
+            </SectionField>
+
+            <SectionField>
+              <Label>
+                <FormattedMessage {...messages.fieldTopics} />
+              </Label>
+              <GetTopics>
+                {(topics) => (topics && isNilOrError(topics)) ? null : (
+                  <Field
+                    name="topics"
+                    component={FormikMultipleSelect}
+                    options={this.resourcesToOptionList(topics)}
+                  />
+                )}
+              </GetTopics>
+            </SectionField>
+
+            <SectionField>
+              <Label>
+                <FormattedMessage {...messages.fieldLimit} />
+              </Label>
+              <Field
+                name="limit"
+                component={FormikInput}
+                type="number"
+              />
+              {touched.limit && <Error
+                fieldName="limit"
+                apiErrors={errors.limit as any}
+              />}
+            </SectionField>
+
+          </Section>
+
+        </Collapse>
+
+        <Button
+          fullWidth={false}
+          onClick={this.handleSubmit}
+        >
+          <FormattedMessage {...messages.updatePreview} />
+        </Button>
 
       </Form>
     );
   }
 }
 
-export default injectIntl(localize(WidgetForm));
+export default localize(WidgetForm);
--- app/containers/Admin/settings/widgets/WidgetPreview.tsx
+++ app/containers/Admin/settings/widgets/WidgetPreview.tsx
@@ -1,4 +1,10 @@
 import * as React from 'react';
+import styled from 'styled-components';
+
+const StyledIframe = styled.iframe`
+  border-radius: 5px;
+  border: solid 1px #ddd;
+`;
 
 type Props = {
   path: string;
@@ -13,7 +19,9 @@ class WidgetPreview extends React.Component<Props, State> {
   render() {
     const { path, width, height } = this.props;
     return (
-      <iframe
+      <StyledIframe
+        frameBorder={0}
+        scrolling="no"
         src={`/widgets${path}`}
         width={width}
         height={height}
