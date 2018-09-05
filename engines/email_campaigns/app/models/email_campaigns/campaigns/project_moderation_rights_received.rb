a8bcdac1003519ea9c999edbf80268b3520f6f39
Sébastien Hoorens <sebastien@citizenlab.co>
2018-09-05 18:00:50 +0200

    admin and moderat not consentable, only invite to admin and spam not to mortals

--- engines/email_campaigns/app/models/email_campaigns/campaigns/project_moderation_rights_received.rb
+++ engines/email_campaigns/app/models/email_campaigns/campaigns/project_moderation_rights_received.rb
@@ -1,7 +1,6 @@
 module EmailCampaigns
   class Campaigns::ProjectModerationRightsReceived < Campaigns::NotificationCampaign
   	include Disableable
-    include Consentable
     include ActivityTriggerable
     include RecipientConfigurable
 
