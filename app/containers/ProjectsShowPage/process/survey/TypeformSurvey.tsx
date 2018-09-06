52ce3428d2547b46f01df2e7bba2f46a6cce683b
David <david@citizenlab.co>
2018-09-06 13:53:40 +0200

    survey fix WIP

--- app/containers/ProjectsShowPage/process/survey/TypeformSurvey.tsx
+++ app/containers/ProjectsShowPage/process/survey/TypeformSurvey.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { makeWidget } from '@typeform/embed';
+import { makeWidget, makePopup } from '@typeform/embed';
 import styled from 'styled-components';
+import Button from 'components/UI/Button';
 
 const Container = styled.div`
   width: 100%;
   position: relative;
+`;
+
+const ButtonWrapper = styled.div`
+  margin: 2rem 0;
 `;
 
 type Props = {
@@ -27,10 +32,14 @@ class TypeformSurvey extends React.PureComponent<Props, State> {
     const surveyUrl = (email ? `${typeformUrl}?email=${email}` : typeformUrl);
 
     if (this.typeformElement) {
-      makeWidget(this.typeformElement, surveyUrl, {
-        hideFooter: true,
-        hideScrollbars: false,
-        hideHeaders: true,
+      // makeWidget(this.typeformElement, surveyUrl, {
+      //   hideFooter: true,
+      //   hideHeaders: true
+      // });
+
+      makePopup(surveyUrl, {
+        mode: 'drawer_left',
+        autoOpen: true
       });
     }
   }
@@ -39,15 +48,29 @@ class TypeformSurvey extends React.PureComponent<Props, State> {
     this.typeformElement = element;
   }
 
+  openSurvey = () => {
+    const { email, typeformUrl } = this.props;
+    const surveyUrl = (email ? `${typeformUrl}?email=${email}` : typeformUrl);
+
+    makePopup(surveyUrl, {
+      mode: 'drawer_left',
+      autoOpen: true,
+    });
+  }
+
   render() {
     const style = {
+      width: '100%',
       height: '500px',
       border: '1px solid #e4e4e4'
     };
 
     return (
       <Container className={this.props['className']}>
-        <div ref={this.setRef} style={style} />
+        <ButtonWrapper>
+          <Button onClick={this.openSurvey}>Save</Button>
+        </ButtonWrapper>
+        {/* <div ref={this.setRef} style={style} /> */}
       </Container>
     );
   }
