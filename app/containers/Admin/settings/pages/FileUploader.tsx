ef2646980db4b471ef5d82690a0c0fd78f26685b
Brent Guffens <brentguf@users.noreply.github.com>
2018-09-07 16:25:51 +0200

    WIP: add file uploader to settings/pages

--- app/containers/Admin/settings/pages/FileUploader.tsx
+++ app/containers/Admin/settings/pages/FileUploader.tsx
@@ -13,13 +13,19 @@ import FileDisplay from 'components/UI/FileDisplay';
 // typings
 import { UploadFile } from 'typings';
 
+// services
+import { addPageFile, deletePageFile } from 'services/pageFiles';
+
 // resources
 import GetResourceFileObjects, { GetResourceFileObjectsChildProps } from 'resources/GetResourceFileObjects';
 
 // utils
 import { isNilOrError } from 'utils/helperUtils';
 
-interface InputProps {}
+interface InputProps {
+  onFileAdd: (fileToAdd: UploadFile) => void;
+  localPageFiles: UploadFile[] | null | Error | undefined;
+}
 
 interface DataProps {
   remotePageFiles: GetResourceFileObjectsChildProps;
@@ -29,6 +35,8 @@ interface Props extends InputProps, DataProps {}
 
 interface State {
   localPageFiles: UploadFile[] | null | Error | undefined;
+  saving: boolean;
+  submitState: 'disabled' | 'enabled' | 'error' | 'success';
 }
 
 class FileUploader extends React.PureComponent<Props & WithRouterProps, State>{
@@ -36,20 +44,13 @@ class FileUploader extends React.PureComponent<Props & WithRouterProps, State>{
     super(props as any);
     this.state = {
       localPageFiles: [],
+      saving: false,
+      submitState: 'disabled',
     };
   }
 
   handlePageFileOnAdd = (fileToAdd: UploadFile) => {
-    this.setState((prevState: State) => {
-      const oldLocalPageFiles = !isNilOrError(prevState.localPageFiles) ? prevState.localPageFiles : [];
-
-      return {
-        localPageFiles: [
-          ...oldLocalPageFiles,
-          fileToAdd
-        ]
-      };
-    });
+    this.props.onFileAdd(fileToAdd);
   }
 
   handlePageFileOnRemove = (fileToRemove: UploadFile) => () => {
@@ -64,6 +65,74 @@ class FileUploader extends React.PureComponent<Props & WithRouterProps, State>{
         localPageFiles
       };
     });
+  }
+
+  getFilesToAddPromises = () => {
+    const { localPageFiles } = this.state;
+    const { remotePageFiles } = this.props;
+    const { id } = this.props.params;
+    let filesToAdd = localPageFiles;
+    let filesToAddPromises: Promise<any>[] = [];
+
+    if (!isNilOrError(localPageFiles) && Array.isArray(remotePageFiles)) {
+      // localPageFiles = local state of files
+      // This means those previously uploaded + files that have been added/removed
+      // remotePageFiles = last saved state of files (remote)
+
+      filesToAdd = localPageFiles.filter((localPageFile) => {
+        return !remotePageFiles.some(remotePageFile => remotePageFile.filename === localPageFile.filename);
+      });
+    }
+
+    if (id && !isNilOrError(filesToAdd) && filesToAdd.length > 0) {
+      filesToAddPromises = filesToAdd.map((fileToAdd: any) => addPageFile(id as string, fileToAdd.base64, fileToAdd.name));
+    }
+
+    return filesToAddPromises;
+  }
+
+  getFilesToRemovePromises = () => {
+    const { localPageFiles } = this.state;
+    const { remotePageFiles } = this.props;
+    const { id } = this.props.params;
+    let filesToRemove = remotePageFiles;
+    let filesToRemovePromises: Promise<any>[] = [];
+
+    if (!isNilOrError(localPageFiles) && Array.isArray(remotePageFiles)) {
+      // localPageFiles = local state of files
+      // This means those previously uploaded + files that have been added/removed
+      // remotePageFiles = last saved state of files (remote)
+
+      filesToRemove = remotePageFiles.filter((remotePageFile) => {
+        return !localPageFiles.some(localPageFile => localPageFile.filename === remotePageFile.filename);
+      });
+    }
+
+    if (id && !isNilOrError(filesToRemove) && filesToRemove.length > 0) {
+      filesToRemovePromises = filesToRemove.map((fileToRemove: any) => deletePageFile(id as string, fileToRemove.id));
+    }
+
+    return filesToRemovePromises;
+  }
+
+  handleOnSubmit = async () => {
+    const filesToAddPromises: Promise<any>[] = this.getFilesToAddPromises();
+    const filesToRemovePromises: Promise<any>[] = this.getFilesToRemovePromises();
+
+    const allPromises = [
+      ...filesToAddPromises,
+      ...filesToRemovePromises
+    ];
+
+    if (allPromises.length > 0) {
+      this.setState({ saving: true });
+      try {
+        await Promise.all(allPromises);
+        this.setState({ saving: false });
+      } catch (errors) {
+        this.setState({ saving: false });
+      }
+    }
   }
 
   render() {
