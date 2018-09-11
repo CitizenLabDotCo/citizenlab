034e07314ea1c6023ecd33e6d151bc028ea780bc
Sébastien Hoorens <sebastien@citizenlab.co>
2018-09-11 10:31:54 +0200

    requested change

--- engines/email_campaigns/app/models/email_campaigns/campaigns/comment_deleted_by_admin.rb
+++ engines/email_campaigns/app/models/email_campaigns/campaigns/comment_deleted_by_admin.rb
@@ -1,7 +1,7 @@
 module EmailCampaigns
   class Campaigns::CommentDeletedByAdmin < Campaigns::NotificationCampaign
   	include Disableable
-    # include Consentable
+    include Consentable
     include ActivityTriggerable
     include RecipientConfigurable
 
