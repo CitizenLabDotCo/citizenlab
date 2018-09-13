c6e899809608f8484eb42326fe132732a816e523
Sébastien Hoorens <sebastien@citizenlab.co>
2018-09-13 11:32:06 +0200

    basket helper methods and some minor model fixes

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,7 +1,18 @@
 class Basket < ApplicationRecord
   belongs_to :user
   belongs_to :participation_context, polymorphic: true
-  
+
   has_many :baskets_ideas, dependent: :destroy
   has_many :ideas, through: :baskets_ideas
+
+  validates :user, :participation_context, presence: true
+
+
+  def total_budget
+  	self.ideas.pluck(:participatory_budget).compact.inject(0){|sum,x| sum + x }
+  end
+
+  def budget_exceeds_limit?
+  	self.total_budget > self.participation_context.max_budget
+  end
 end
--- app/models/ideas_phase.rb
+++ app/models/ideas_phase.rb
@@ -6,24 +6,26 @@ class IdeasPhase < ApplicationRecord
   validate :idea_and_phase_same_project
   validate :phase_is_ideation
 
-  private 
-    def idea_and_phase_same_project
-      unless idea.project_id == phase.project_id
-        self.errors.add(
-          :base, 
-          :idea_and_phase_not_same_project, 
-          message: 'The idea and the phase do not belong to the same project'
-        )
-      end
-    end
 
-    def phase_is_ideation
-        unless phase.ideation?
-        self.errors.add(
-          :phase_id, 
-          :phase_not_ideation, 
-          message: 'You can\'t add an idea to a non-ideation phase'
-        )
-      end
+  private 
+
+  def idea_and_phase_same_project
+    unless idea.project_id == phase.project_id
+      self.errors.add(
+        :base, 
+        :idea_and_phase_not_same_project, 
+        message: 'The idea and the phase do not belong to the same project'
+      )
     end
+  end
+
+  def phase_is_ideation
+    unless phase.ideation?
+      self.errors.add(
+        :phase_id, 
+        :phase_not_ideation, 
+        message: 'You can\'t add an idea to a non-ideation phase'
+      )
+    end
+  end
 end
