79e19b0de9b9d6df2c327a070b37f1c7fbd7698d
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-09-19 12:28:13 +0200

    CL2-2427 Made campaign serializer more flexible

--- engines/email_campaigns/app/serializers/email_campaigns/web_api/v1/campaign_serializer.rb
+++ engines/email_campaigns/app/serializers/email_campaigns/web_api/v1/campaign_serializer.rb
@@ -1,9 +1,17 @@
 module EmailCampaigns
   class WebApi::V1::CampaignSerializer < ActiveModel::Serializer
-    attributes :id, :sender, :reply_to, :campaign_name, :subject_multiloc, :body_multiloc, :created_at, :updated_at
+    attributes :id, :campaign_name, :created_at, :updated_at
 
-    belongs_to :author
-    has_many :groups
+    attribute :enabled, if: -> { object.respond_to? :enabled }
+    attribute :schedule, if: -> { object.respond_to? :schedule }
+    attribute :sender, if: -> { object.respond_to? :sender }
+    attribute :reply_to, if: -> { object.respond_to? :reply_to }
+    attribute :subject_multiloc, if: -> { object.respond_to? :subject_multiloc }
+    attribute :body_multiloc, if: -> { object.respond_to? :body_multiloc }
+
+
+    belongs_to :author, if: -> { object.respond_to? :author }
+    has_many :groups, if: -> { object.respond_to? :groups }
     
     def campaign_name
       object.class.campaign_name
