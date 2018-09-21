4ea9e41d1d580606085bc7d55a943fd0a1d7ad97
Sébastien Hoorens <sebastien@citizenlab.co>
2018-09-21 14:49:00 +0200

    added some validations and fixes

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -6,6 +6,9 @@ class Permission < ApplicationRecord
 	has_many :groups_permissions, dependent: :destroy
   has_many :groups, through: :groups_permissions
 
-  validates :action, :permitted_by, :participation_context, presence: true
+  validates :participation_context, presence: true
+  validates :action, presence: true, inclusion: {in: ACTIONS}
+  validates :permitted_by, presence: true, inclusion: {in: PERMITTED_BIES}
+  validates :action, uniqueness: {scope: [:participation_context_id, :participation_context_type]}
 
 end
--- db/migrate/20180920155127_create_groups_permissions.rb
+++ db/migrate/20180920155127_create_groups_permissions.rb
@@ -1,8 +1,8 @@
 class CreateGroupsPermissions < ActiveRecord::Migration[5.1]
   def change
     create_table :groups_permissions, id: :uuid do |t|
-      t.reference :permission, null: false, index: true
-      t.reference :group, null: false
+      t.references :permission, null: false, index: true
+      t.references :group, null: false
 
       t.timestamps
     end
