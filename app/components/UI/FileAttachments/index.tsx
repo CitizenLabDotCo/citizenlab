406783d4e1fc6e8a162685c2ef3633b8e794bf49
Brent Guffens <brentguf@users.noreply.github.com>
2018-09-26 12:17:01 +0200

    Remove header

--- app/components/UI/FileAttachments/index.tsx
+++ app/components/UI/FileAttachments/index.tsx
@@ -6,32 +6,26 @@ import { IPhaseFileData } from 'services/phaseFiles';
 import { IEventFileData } from 'services/eventFiles';
 import { IPageFileData } from 'services/pageFiles';
 import { IIdeaFileData } from 'services/ideaFiles';
-import { injectIntl, FormattedMessage } from 'utils/cl-intl';
-import { InjectedIntlProps } from 'react-intl';
-import messages from './messages';
 
-const Header = styled.h3``;
+const Container = styled.div`
+  margin-top: 25px;
+`;
 
 interface Props {
   files: IProjectFileData[] | IPhaseFileData[] | IEventFileData[] | IPageFileData[] | IIdeaFileData[];
 }
 
-const FileAttachments = ({ files }: Props & InjectedIntlProps) => {
+const FileAttachments = ({ files }: Props) => {
   return (
-    <>
-      <Header>
-        <FormattedMessage {...messages.attachmentsHeader} />
-      </Header>
+    <Container>
       {Array.isArray(files) && files.map(file => (
         <FileDisplay
           key={file.id}
           file={file}
         />
       ))}
-    </>
+    </Container>
   );
 };
 
-const FileAttachmentsWithIntl = injectIntl<Props & InjectedIntlProps>(FileAttachments);
-
-export default FileAttachmentsWithIntl;
+export default FileAttachments;
