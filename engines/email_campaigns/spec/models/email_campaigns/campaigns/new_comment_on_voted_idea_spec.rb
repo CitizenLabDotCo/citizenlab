11b634555d36e083ffc6ba7c4f8b083dab12765e
sebastienhoorens <31925816+sebastienhoorens@users.noreply.github.com>
2018-09-26 17:43:52 +0200

    Update new_comment_on_voted_idea_spec.rb

--- engines/email_campaigns/spec/models/email_campaigns/campaigns/new_comment_on_voted_idea_spec.rb
+++ engines/email_campaigns/spec/models/email_campaigns/campaigns/new_comment_on_voted_idea_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe EmailCampaigns::Campaigns::NewCommentOnVotedIdea, type: :model do
     let(:idea) { create(:idea) }
     let!(:vote) { create(:vote, votable: idea) }
     let(:comment) { create(:comment, idea: idea) }
-    let(:activity) { create(:activity, item: comment, action: 'created', user: comment.user) }
+    let(:activity) { create(:activity, item: comment, action: 'created', user: comment.author) }
 
   	it "generates a command with the desired payload and tracked content" do
   		command = campaign.generate_commands(recipient: vote.user, activity: activity).first
