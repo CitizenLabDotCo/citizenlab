f63ad9661dcc5cd7f8c4f69c2f3b1f249f9ce937
Sara <sara@citizenlab.co>
2018-09-27 16:01:51 +0200

    Banner styled and functional

--- app/components/ConsentManager/Banner.tsx
+++ app/components/ConsentManager/Banner.tsx
@@ -1,22 +1,44 @@
 import React, { PureComponent } from 'react';
+
+import FormattedMessage from 'utils/cl-intl/FormattedMessage';
+import messages from './messages';
+
+import Link from 'utils/cl-router/Link';
+import Button from 'components/UI/Button';
+import ContentContainer from 'components/ContentContainer';
+
 import styled from 'styled-components';
 import { colors } from 'utils/styleUtils';
 
 const Root = styled.div`
-  position: relative;
-  padding: 8px;
-  padding-right: 40px;
-  background: ${colors.clBlueLight};
-  color: ${colors.clBlueLightest};
-  text-align: center;
-  font-size: 12px;
+  position: fixed;
+  bottom: 0;
+  color: white;
+  background: ${(props) => props.theme.colorMain};
+  font-size: 16px;
   line-height: 1.3;
   z-index: 10;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 30px;
 `;
 
+const Spacer = styled.div`
+  flex: 1;
+`;
+const StyledContentContainer = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: ${(props) => props.theme.maxPageWidth}px;
+  Button.button.primary {
+    border-color: white;
+    margin-right: 10px;
+  }
+`;
 const Content = styled.div`
-  a,
-  button {
+  a {
     display: inline;
     padding: 0;
     border: none;
@@ -26,13 +48,13 @@ const Content = styled.div`
     text-decoration: underline;
     cursor: pointer;
   }
-`;
-
-const P = styled.p`
-  margin: 0;
-  &:not(:last-child) {
-    margin-bottom: 6px;
+  p:first-child {
+    font-weight: 700;
+    margin-bottom: 5px;
   }
+`;
+const ManageButton = styled(Button)`
+  border-color: white;
 `;
 
 const CloseButton = styled.button`
@@ -45,7 +67,6 @@ const CloseButton = styled.button`
   background: none;
   color: inherit;
   font: inherit;
-  font-size: 14px;
   line-height: 1;
   cursor: pointer;
 `;
@@ -66,14 +87,25 @@ export default class Banner extends PureComponent<Props> {
 
     return (
       <Root>
+        <StyledContentContainer>
         <Content>
-          <P>Hi</P>
-          <P>
-            <button type="button" onClick={onChangePreferences}>
-              Click to change
-            </button>
-          </P>
+          <FormattedMessage
+            tagName="p"
+            {...messages.mainText}
+            values={{
+              // tslint:disable-next-line
+              privacyPolicyLink: (
+                <Link to="/pages/privacy-policy" target="_blank">
+                  <FormattedMessage {...messages.privacyPolicy} />
+                </Link>)
+            }}
+          />
+          <FormattedMessage {...messages.subText} tagName="p" />
         </Content>
+        <Spacer/>
+        <Button style="primary" onClick={onChangePreferences}><FormattedMessage {...messages.manage} /></Button>
+        <Button style="primary-inverse" onClick={onAccept}><FormattedMessage {...messages.accept} /></Button>
+      </StyledContentContainer>
 
         <CloseButton
           type="button"
