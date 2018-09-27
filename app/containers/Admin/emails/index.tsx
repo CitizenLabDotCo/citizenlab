519054ca17afc5ddf980d450add6b7abb3413fd4
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-09-27 13:36:47 +0200

    Dealt with combination or routing and permissions for admin emails

--- app/containers/Admin/emails/index.tsx
+++ app/containers/Admin/emails/index.tsx
@@ -1,57 +1,77 @@
 import * as React from 'react';
+import { isEmpty } from 'lodash-es';
 import { adopt } from 'react-adopt';
-import { injectIntl } from 'utils/cl-intl';
+import { withRouter, WithRouterProps } from 'react-router';
+import clHistory from 'utils/cl-router/history';
+import { injectIntl, FormattedMessage } from 'utils/cl-intl';
 import HelmetIntl from 'components/HelmetIntl';
 import TabbedResource from 'components/admin/TabbedResource';
 import { InjectedIntlProps } from 'react-intl';
 import messages from './messages';
 import GetPermission from 'resources/GetPermission';
+import GetFeatureFlag from 'resources/GetFeatureFlag';
 
 type Props = {
   canManageAutomatedCampaigns: boolean | null;
   canManageManualCampaigns: boolean | null;
+  manualEmailingEnabled: boolean | null;
+  automatedEmailingEnabled: boolean | null;
 };
 
 type State = {};
 
-class EmailsDashboard extends React.PureComponent<Props & InjectedIntlProps, State> {
+class EmailsDashboard extends React.PureComponent<Props & InjectedIntlProps & WithRouterProps, State> {
 
   tabs = () => {
-    const { intl: { formatMessage }, canManageAutomatedCampaigns, canManageManualCampaigns } = this.props;
+    const { intl: { formatMessage }, location: { pathname } } = this.props;
     const tabs: any = [];
-    if (canManageManualCampaigns) {
+    if (this.props.canManageManualCampaigns && this.props.manualEmailingEnabled) {
       tabs.push({ label: formatMessage(messages.tabManual), url: '/admin/emails/manual' });
     }
-    if (canManageAutomatedCampaigns) {
+    if (this.props.canManageAutomatedCampaigns && this.props.automatedEmailingEnabled) {
       tabs.push({ label: formatMessage(messages.tabAutomated), url: '/admin/emails/automated' });
     }
+
+    if (pathname.match(/\/admin\/emails$/) && !isEmpty(tabs)) {
+      clHistory.push(tabs[0].url);
+    }
+
     return tabs;
   }
 
   render() {
     const { children, intl: { formatMessage } } = this.props;
+    const tabs = this.tabs();
 
     return (
-      <TabbedResource
-        resource={{ title: formatMessage(messages.titleEmails) }}
-        messages={messages}
-        tabs={this.tabs()}
-      >
-        <HelmetIntl
-          title={messages.helmetTitle}
-          description={messages.helmetDescription}
-        />
-        {children}
-      </TabbedResource>
+      <>
+        <TabbedResource
+          resource={{ title: formatMessage(messages.titleEmails) }}
+          messages={messages}
+          tabs={this.tabs()}
+        >
+          <HelmetIntl
+            title={messages.helmetTitle}
+            description={messages.helmetDescription}
+          />
+          {isEmpty(tabs) ?
+            <FormattedMessage {...messages.noAccess} />
+          :
+            children
+          }
+        </TabbedResource>
+      </>
     );
   }
 }
 
-const EmailsDashboardWithHOCs = injectIntl(EmailsDashboard);
+const EmailsDashboardWithHOCs = withRouter(injectIntl(EmailsDashboard));
 
 const Data = adopt<Props, {}>({
   canManageAutomatedCampaigns: <GetPermission item="automatedCampaigns" action="manage" />,
   canManageManualCampaigns: <GetPermission item="manualCampaigns" action="manage" />,
+  manualEmailingEnabled: <GetFeatureFlag name="manual_emailing" />,
+  automatedEmailingEnabled: <GetFeatureFlag name="automated_emailing_control" />,
 });
 
 export default (inputProps) => (
