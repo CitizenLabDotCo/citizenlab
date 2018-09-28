6080f00e7aebb938097d3e07e51a4a6cf112a8ed
Sébastien Hoorens <sebastien@citizenlab.co>
2018-09-28 12:14:30 +0200

    removing _enabled attrs of part contxt

--- app/serializers/web_api/v1/project_serializer.rb
+++ app/serializers/web_api/v1/project_serializer.rb
@@ -2,9 +2,6 @@ class WebApi::V1::ProjectSerializer < ActiveModel::Serializer
   attributes :id, :title_multiloc, :description_multiloc, :description_preview_multiloc, :slug, :header_bg, :visible_to, :process_type, :ideas_count, :internal_role, :publication_status, :created_at, :updated_at, :ordering
   # ParticipationContext attributes
   attribute :participation_method, if: :is_participation_context?
-  attribute :posting_enabled, if: :is_participation_context?
-  attribute :commenting_enabled, if: :is_participation_context?
-  attribute :voting_enabled, if: :is_participation_context?
   attribute :voting_method, if: :is_participation_context?
   attribute :voting_limited_max, if: :is_participation_context?
   attribute :presentation_mode, if: :is_participation_context?
