11375f0b1367bad929efdfd788a58817b29a96ce
Sébastien Hoorens <sebastien@citizenlab.co>
2018-09-28 12:14:30 +0200

    removing _enabled attrs of part contxt

--- spec/policies/vote_policy_spec.rb
+++ spec/policies/vote_policy_spec.rb
@@ -65,7 +65,7 @@ describe VotePolicy do
 
   context "for a mortal user who owns the vote on an idea in a private groups project where she's no member of a manual group with access" do
     let!(:user) { create(:user) }
-    let!(:project) { create(:private_groups_project) }
+    let!(:project) { create(:private_groups_project, with_permissions: true) }
     let!(:idea) { create(:idea, project: project) }
     let!(:vote) { create(:vote, votable: idea, user: user) }
 
@@ -79,9 +79,13 @@ describe VotePolicy do
     end
   end
 
-  context "for a mortal user who owns the vote on an idea in a project where voting is disabled" do
+  context "for a mortal user who owns the vote on an idea in a project where voting is not permitted" do
     let!(:user) { create(:user) }
-    let!(:project) { create(:continuous_project, voting_enabled: false) }
+    let!(:project) { 
+      p = create(:continuous_project, with_permissions: true) 
+      p.permissions.find_by(action: 'voting').update!(permitted_by: 'admins_moderators')
+      p
+    }
     let!(:idea) { create(:idea, project: project) }
     let!(:vote) { create(:vote, votable: idea, user: user) }
 
