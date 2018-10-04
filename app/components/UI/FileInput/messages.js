3e346a7dc13dd35099268ebc803cabc74950e504
Brent Guffens <brentguf@users.noreply.github.com>
2018-10-04 16:11:53 +0200

    Clean up FileUploader component

--- app/components/UI/FileInput/messages.js
+++
@@ -1,8 +0,0 @@
-import { defineMessages } from 'react-intl';
-
-export default defineMessages({
-  FileInputDescription: {
-    id: 'app.components.FileInput.FileInputDescription.',
-    defaultMessage: 'Select a file to upload...',
-  },
-});
