377051c02a837f0d6a1cfa95852d746d1032932a
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-10-05 14:44:44 +0200

    CL2-2536 Allowed enabling/disabling of campaigns that have deliveries

--- engines/email_campaigns/app/models/email_campaigns/campaigns/status_change_of_your_idea.rb
+++ engines/email_campaigns/app/models/email_campaigns/campaigns/status_change_of_your_idea.rb
@@ -1,6 +1,5 @@
 module EmailCampaigns
   class Campaigns::StatusChangeOfYourIdea < Campaigns::NotificationCampaign
-  	include Disableable
     include Consentable
     include ActivityTriggerable
     include RecipientConfigurable
