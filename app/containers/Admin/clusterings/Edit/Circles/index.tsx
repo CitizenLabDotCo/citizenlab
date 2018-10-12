74b1a73d94fa0a3f00c0c5947852876edadc287c
Brent Guffens <brentguf@users.noreply.github.com>
2018-10-12 13:24:40 +0200

    Ctrl key support for Mac

--- app/containers/Admin/clusterings/Edit/Circles/index.tsx
+++ app/containers/Admin/clusterings/Edit/Circles/index.tsx
@@ -20,7 +20,7 @@ interface InputProps {
   activeComparison: number;
   onClickNode: (Node) => void;
   onShiftClickNode: (Node) => void;
-  onCtrlCickNode: (Node) => void;
+  onCtrlClickNode: (Node) => void;
 }
 
 interface DataProps {
@@ -33,6 +33,7 @@ type State = {
   svgSize: number | null;
   nodes: D3Node[];
   hoveredNode: D3Node | null;
+  ctrlKeyPressed: boolean;
 };
 
 const Container = styled.div`
@@ -50,17 +51,22 @@ class Circles extends PureComponent<Props, State> {
       svgSize: null,
       nodes: [],
       hoveredNode: null,
+      ctrlKeyPressed: false
     };
     this.containerRef = null;
   }
 
   componentDidMount() {
     window.addEventListener('resize', this.calculateNodePositions);
+    window.addEventListener('keydown', this.handleOnKeyDown);
+    window.addEventListener('keyup', this.handleOnKeyUp);
     this.calculateNodePositions();
   }
 
   componentWillUnmount() {
     window.removeEventListener('resize', this.calculateNodePositions);
+    window.addEventListener('keydown', this.handleOnKeyDown);
+    window.addEventListener('keyup', this.handleOnKeyUp);
   }
 
   calculateNodePositions = () => {
@@ -92,10 +98,22 @@ class Circles extends PureComponent<Props, State> {
   handleOnClickNode = (node: D3Node, event: MouseEvent) => {
     if (event.shiftKey) {
       this.props.onShiftClickNode(node.data);
-    } else if (event.ctrlKey) {
-      this.props.onCtrlCickNode(node.data);
+    } else if (this.state.ctrlKeyPressed) {
+      this.props.onCtrlClickNode(node.data);
     } else {
       this.props.onClickNode(node.data);
+    }
+  }
+
+  handleOnKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Control') {
+      this.setState({ ctrlKeyPressed: true });
+    }
+  }
+
+  handleOnKeyUp = (event: KeyboardEvent) => {
+    if (event.key === 'Control') {
+      this.setState({ ctrlKeyPressed: false });
     }
   }
 
