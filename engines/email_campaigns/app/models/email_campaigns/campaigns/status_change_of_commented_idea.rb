bb6f67079ddf021a79142874848951dab838ecbe
Sébastien Hoorens <sebastien@citizenlab.co>
2018-10-22 11:39:07 +0200

    removed enabled db default of campaigns and set new defaults on ruby level

--- engines/email_campaigns/app/models/email_campaigns/campaigns/status_change_of_commented_idea.rb
+++ engines/email_campaigns/app/models/email_campaigns/campaigns/status_change_of_commented_idea.rb
@@ -2,8 +2,10 @@ module EmailCampaigns
   class Campaigns::StatusChangeOfCommentedIdea < Campaign
     include ActivityTriggerable
     include RecipientConfigurable
+    include Disableable
 
     recipient_filter :filter_recipient
+
 
     def activity_triggers
       {'Idea' => {'changed_status' => true}}
@@ -40,6 +42,13 @@ module EmailCampaigns
           }
         }
       }]
+    end
+
+
+    protected
+
+    def set_enabled
+      self.enabled = false if self.enabled.nil?
     end
   end
 end
