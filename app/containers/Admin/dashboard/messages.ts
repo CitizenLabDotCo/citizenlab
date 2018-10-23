8820aa6bc9aae8e8316f147b5b50164cd6f5834e
Sara <sara@citizenlab.co>
2018-10-23 16:38:49 +0200

    Fixes active state of the Sidebar

---
+++ app/containers/Admin/dashboard/messages.ts
@@ -0,0 +1,124 @@
+/*
+ * AdminPage.DashboardPage Messages
+ *
+ * This contains all the text for the AdminPage.DashboardPage component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  // NEW MESSAGES
+  tabSummary: {
+    id: 'app.containers.AdminPage.DashboardPage.tabSummary',
+    defaultMessage: 'Summary',
+  },
+  tabUsers: {
+    id: 'app.containers.AdminPage.DashboardPage.tabUsers',
+    defaultMessage: 'Users',
+  },
+  tabAcquisition: {
+    id: 'app.containers.AdminPage.DashboardPage.tabAcquisition',
+    defaultMessage: 'Acquisition',
+  },
+  viewPublicResource: {
+    id: 'app.containers.AdminPage.DashboardPage.viewPublicResource',
+    defaultMessage: 'Dashboard',
+  },
+  // OLD MESSAGES CONFIRMED STILL IN USE
+  helmetTitle: {
+    id: 'app.containers.AdminPage.DashboardPage.helmetTitle',
+    defaultMessage: 'Admin dashboard page',
+  },
+  helmetDescription: {
+    id: 'app.containers.AdminPage.DashboardPage.helmetDescription',
+    defaultMessage: 'Dashboard for activities on the platform',
+  },
+  // OLD MESSAGES
+  loading: {
+    id: 'app.containers.AdminPage.DashboardPage.loading',
+    defaultMessage: 'Loading...',
+  },
+  loadError: {
+    id: 'app.containers.AdminPage.DashboardPage.loadError',
+    defaultMessage: 'Couldn\' load data',
+  },
+  usersOverTime: {
+    id: 'app.containers.AdminPage.DashboardPage.usersOverTime',
+    defaultMessage: 'Users over time',
+  },
+  usersByGenderTitle: {
+    id: 'app.containers.AdminPage.DashboardPage.usersByGenderTitle',
+    defaultMessage: 'Users by gender',
+  },
+  usersByAgeTitle: {
+    id: 'app.containers.AdminPage.DashboardPage.usersByAgeTitle',
+    defaultMessage: 'Users by age',
+  },
+  ideasByTimeTitle: {
+    id: 'app.containers.AdminPage.DashboardPage.ideasByTimeTitle',
+    defaultMessage: 'Ideas over time',
+  },
+  usersByTimeTitle: {
+    id: 'app.containers.AdminPage.DashboardPage.usersByTimeTitle',
+    defaultMessage: 'Registrations over time',
+  },
+  ideasByTopicTitle: {
+    id: 'app.containers.AdminPage.DashboardPage.ideasByTopicTitle',
+    defaultMessage: 'Ideas by topic',
+  },
+  ideasByArea: {
+    id: 'app.containers.AdminPage.DashboardPage.ideasByArea',
+    defaultMessage: 'Ideas by area',
+  },
+  interval: {
+    id: 'app.containers.AdminPage.DashboardPage.interval',
+    defaultMessage: 'Interval',
+  },
+  day: {
+    id: 'app.containers.AdminPage.DashboardPage.day',
+    defaultMessage: 'Day',
+  },
+  week: {
+    id: 'app.containers.AdminPage.DashboardPage.week',
+    defaultMessage: 'Week',
+  },
+  month: {
+    id: 'app.containers.AdminPage.DashboardPage.month',
+    defaultMessage: 'Month',
+  },
+  year: {
+    id: 'app.containers.AdminPage.DashboardPage.year',
+    defaultMessage: 'Year',
+  },
+  male: {
+    id: 'app.containers.AdminPage.DashboardPage.male',
+    defaultMessage: 'male',
+  },
+  female: {
+    id: 'app.containers.AdminPage.DashboardPage.female',
+    defaultMessage: 'female',
+  },
+  unspecified: {
+    id: 'app.containers.AdminPage.DashboardPage.unspecified',
+    defaultMessage: 'unspecified',
+  },
+  _blank: {
+    id: 'app.containers.AdminPage.DashboardPage._blank',
+    defaultMessage: 'unknown',
+  },
+  numberOfRegistrations: {
+    id: 'app.containers.AdminPage.DashboardPage.numberOfRegistrations',
+    defaultMessage: '#registrations',
+  },
+  numberOfIdeas: {
+    id: 'app.containers.AdminPage.DashboardPage.numberOfIdeas',
+    defaultMessage: '#ideas',
+  },
+  tryOutInsights: {
+    id: 'app.containers.AdminPage.DashboardPage.tryOutInsights',
+    defaultMessage: 'Try out {insightsLink}, our new beta feature',
+  },
+  insightsLinkText: {
+    id: 'app.containers.AdminPage.DashboardPage.insightsLinkText',
+    defaultMessage: 'insights',
+  },
+});
