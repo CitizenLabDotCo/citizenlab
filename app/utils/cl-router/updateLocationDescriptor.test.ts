1f587a6db72c33a89ce6335895e93a936acdf519
Sara <sara@citizenlab.co>
2018-10-26 17:20:44 +0200

    tests one util function updateLocationDescriptor

--- app/utils/cl-router/updateLocationDescriptor.test.ts
+++ app/utils/cl-router/updateLocationDescriptor.test.ts
@@ -2,13 +2,42 @@ import updateLocationDescriptor from 'utils/cl-router/updateLocationDescriptor';
 
 import * as serviceLocale from 'services/locale';
 
-jest.spyOn(serviceLocale, 'getUrlLocale').mockReturnValue('fr-be');
+const serviceSpy = jest.spyOn(serviceLocale, 'getUrlLocale');
 
-test('updates "/fr-BE" to "/nl-BE"', () => {
-  expect(updateLocationDescriptor('/nl-BE', 'fr-BE')).toEqual({
-    pathname: '/nl-BE',
+test('updates / to /nl-BE/', () => {
+  expect(updateLocationDescriptor('/', 'nl-BE')).toEqual({
+    pathname: '/nl-BE/',
     state: {
-      locale: 'fr-BE'
+      locale: 'nl-BE'
+    }
+  });
+  expect(serviceSpy).toBeCalled;
+});
+
+test('updates /ideas to /nl-BE/ideas', () => {
+  expect(updateLocationDescriptor('/ideas', 'nl-BE')).toEqual({
+    pathname: '/nl-BE/ideas',
+    state: {
+      locale: 'nl-BE'
+    }
+  });
+  expect(serviceSpy.mock.results[1]).toBeNull;
+});
+
+test('updates /fr-BE/ to /nl-BE/', () => {
+  expect(updateLocationDescriptor('/fr-BE/', 'nl-BE')).toEqual({
+    pathname: '/nl-BE/',
+    state: {
+      locale: 'nl-BE'
+    }
+  });
+});
+
+test('updates /fr-BE/ideas to /nl-BE/ideas', () => {
+  expect(updateLocationDescriptor('/fr-BE/ideas', 'nl-BE')).toEqual({
+    pathname: '/nl-BE/ideas',
+    state: {
+      locale: 'nl-BE'
     }
   });
 });
