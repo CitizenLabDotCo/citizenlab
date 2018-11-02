91260b37400f0e1075f4113520a8d9c530bcc249
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-11-02 11:01:59 +0100

    CL2-2580 Fixed poc integration test for <GetArea>

--- app/resources/GetArea.integration.test.tsx
+++ app/resources/GetArea.integration.test.tsx
@@ -5,7 +5,14 @@ import request, { __setResponseFor } from 'utils/request';
 jest.mock('utils/request');
 jest.mock('services/auth');
 
-// Integration tests
+/** This is an example of a potential integration test for data fetching
+ * It tests the code from the resource component until the http request done by utils/request.ts
+ * and everything in between (mostly streams.ts)
+ *
+ * To be evaluated whether this is the way to go.
+ * Advantage is that coverage is high with quite low efforts.
+ * Downside is that is might be too fragile.
+ */
 
 describe('GetArea with streams', () => {
   let child: jest.Mock;
@@ -14,7 +21,7 @@ describe('GetArea with streams', () => {
     child = jest.fn();
   });
 
-  it('passes the idea data to the child function received from the request', () => {
+  it('passes the idea data to the child function received from the request', (done) => {
     const httpResponse = {
       data: {
         id: 'f93ad1a1-3a0c-4c3d-b225-e1b63ca782cb',
@@ -36,7 +43,13 @@ describe('GetArea with streams', () => {
     shallow(<GetArea id="f93ad1a1-3a0c-4c3d-b225-e1b63ca782cb">{child}</GetArea>);
     expect(request.mock.calls[0][0]).toEqual('/web_api/v1/areas/f93ad1a1-3a0c-4c3d-b225-e1b63ca782cb');
     expect(child.mock.calls[0][0]).toBeUndefined;
-    expect(child.mock.calls[1][0]).toEqual(httpResponse.data);
+    // This is a little bit hacky, but without mocking/touching streams.ts internals
+    // it seems to be very hard to get hold of a promise that resolves when the
+    // data should be passed in. Maybe a sign that it's not the way to go.
+    setTimeout(() => {
+      expect(child.mock.calls[1][0]).toEqual(httpResponse.data);
+      done();
+    });
   });
 
 });
