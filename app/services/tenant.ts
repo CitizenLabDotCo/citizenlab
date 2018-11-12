d84370dd1898118c3c2e8507cbcaa250536631ee
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-11-12 13:27:35 +0100

    CL2-2680 Feature flagged PB in idea manager

--- app/services/tenant.ts
+++ app/services/tenant.ts
@@ -68,6 +68,7 @@ export interface ITenantSettings {
   excel_export?: TenantFeature;
   private_projects?: TenantFeature;
   maps?: TenantMapSettings;
+  participatory_budgeting: TenantFeature;
 }
 interface TenantMapSettings extends TenantFeature {
   map_center: {
