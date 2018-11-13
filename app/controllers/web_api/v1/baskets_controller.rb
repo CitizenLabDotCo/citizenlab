abc3494f8595aa6f60a9a53bb1f4369b8cfdbee7
Sébastien Hoorens <sebastien@citizenlab.co>
2018-11-13 16:26:49 +0100

    speced and fixed ugh

--- app/controllers/web_api/v1/baskets_controller.rb
+++ app/controllers/web_api/v1/baskets_controller.rb
@@ -18,7 +18,15 @@ class WebApi::V1::BasketsController < ApplicationController
   end
 
   def update
-    @basket.assign_attributes basket_params
+    new_idea_ids = basket_params[:idea_ids]
+    if new_idea_ids
+      old_idea_ids = @basket.idea_ids
+      ideas_to_add = new_idea_ids - old_idea_ids
+      ideas_to_rmv = old_idea_ids - new_idea_ids
+      @basket.baskets_ideas.where(idea_id: ideas_to_rmv).each(&:destroy!)
+      ideas_to_add.each{ |idea_id| @basket.baskets_ideas.create!(idea_id: idea_id) }
+    end
+    @basket.assign_attributes basket_params.except(:idea_ids)
     if @basket.save
       SideFxBasketService.new.after_update @basket, current_user
       render json: @basket, status: :ok
