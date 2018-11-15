f0c65ee723d53bb3bee7b2ae0a62db5a77c3ba65
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-11-15 11:12:09 +0100

    CL2-2627 WIP Added moderator policies for comments stats

--- app/policies/stat_comment_policy.rb
+++ app/policies/stat_comment_policy.rb
@@ -1,47 +1,65 @@
 class StatCommentPolicy < ApplicationPolicy
 
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user  = user
+      @scope = scope
+    end
+
+    def resolve
+      if user&.active? && user.admin?
+        scope.all
+      else user&.active? && user.project_moderator?
+        projects = ProjectPolicy::Scope.new(user, Project.all).moderatable
+        scope.joins(:idea).where(ideas: {project_id: projects})
+      end
+    end
+  end
+
   def ideas_count?
-    user&.active? && user.admin?
+    user&.active? && (user.admin? || user.project_moderator?)
   end
 
   def ideas_by_topic?
-    user&.active? && user.admin?
+    user&.active? && (user.admin? || user.project_moderator?)
   end
 
   def ideas_by_area?
-    user&.active? && user.admin?
+    user&.active? && (user.admin? || user.project_moderator?)
   end
 
   def ideas_by_project?
-    user&.active? && user.admin?
+    user&.active? && (user.admin? || user.project_moderator?)
   end
 
   def ideas_by_time?
-    user&.active? && user.admin?
+    user&.active? && (user.admin? || user.project_moderator?)
   end
 
   def ideas_by_time_cumulative?
-    user&.active? && user.admin?
+    user&.active? && (user.admin? || user.project_moderator?)
   end
 
   def comments_count?
-    user&.active? && user.admin?
+    user&.active? && (user.admin? || user.project_moderator?)
   end
 
   def comments_by_time?
-    user&.active? && user.admin?
+    user&.active? && (user.admin? || user.project_moderator?)
   end
 
   def comments_by_time_cumulative?
-    user&.active? && user.admin?
+    user&.active? && (user.admin? || user.project_moderator?)
   end
 
   def comments_by_topic?
-    user&.active? && user.admin?
+    user&.active? && (user.admin? || user.project_moderator?)
   end
 
   def comments_by_project?
-    user&.active? && user.admin?
+    user&.active? && (user.admin? || user.project_moderator?)
   end
 
 end
