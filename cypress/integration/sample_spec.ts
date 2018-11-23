0617264bfb8e1299fe24d710c467afc960456823
David <david@citizenlab.co>
2018-11-23 17:36:03 +0100

    e2e tests WIP

--- cypress/integration/sample_spec.ts
+++ cypress/integration/sample_spec.ts
@@ -1,23 +1,24 @@
 /// <reference types="cypress"/>
 
-const loginAsAdmin = () => {
+Cypress.Commands.add('loginAsAdmin', () => {
   cy.visit('/sign-in');
   cy.get('#email').type('koen@citizenlab.co');
   cy.get('#password').type('testtest');
   cy.get('.e2e-submit-signin').click();
-};
+});
 
-const logOut = () => {
+Cypress.Commands.add('logOut', () => {
   cy.get('#e2e-user-menu-container').click().get('#e2e-sign-out-link').click();
-};
+});
 
-const createNewProject = (projectName: string) => {
+Cypress.Commands.add('createNewContinuousProject', (projectName: string) => {
   cy.visit('/admin/projects');
   cy.get('.e2e-admin-add-project').click();
   cy.get('#project-title-en').type(projectName);
   cy.get('#project-title-nl-BE').type(projectName);
+  cy.get('.e2e-project-type-continuous').click();
   cy.get('.e2e-submit-wrapper-button').click();
-};
+});
 
 // describe('Home page', () => {
 //   it('successfully loads', () => {
@@ -67,41 +68,27 @@ const createNewProject = (projectName: string) => {
 //   });
 // });
 
-describe('Admin > projects', () => {
-  it('saves and idea, signs you in, publishes the idea and redirects you to the idea page', () => {
+describe('Lazy idea posting flow', () => {
+  it('fills in the idea form, signs you in and redirects you to the idea page', () => {
     const projectName = Math.random().toString(36).substr(2, 5);
-    loginAsAdmin();
-    createNewProject(projectName);
-    logOut();
+    const ideaTitle = Math.random().toString(36).substr(2, 5);
+    const ideaDescription = Math.random().toString(36).substr(2, 5);
+
+    cy.loginAsAdmin();
+    cy.createNewContinuousProject(projectName);
+    cy.logOut();
     cy.get('.addIdea').click();
     cy.location('pathname').should('eq', '/en/ideas/new');
-    cy.visit('/admin/projects');
-    cy.get('.e2e-admin-add-project').click();
-    cy.get('#project-title-en').type('e2e test project');
-    cy.get('#project-title-nl-BE').type('e2e test project');
-    cy.get('.e2e-submit-wrapper-button').click();
-  });
-});
-
-describe('Lazy idea posting flow', () => {
-  // beforeEach(() => {
-  //   cy.visit('/sign-in');
-  //   cy.get('#email').type('koen@citizenlab.co');
-  //   cy.get('#password').type('testtest');
-  //   cy.get('.e2e-submit-signin').click();
-  // });
-
-  it('saves and idea, signs you in, publishes the idea and redirects you to the idea page', () => {
-    cy.visit('/sign-up');
-    cy.get('#firstName').type(firstName);
-    cy.get('#lastName').type(lastName);
-    cy.get('#email').type(email);
-    cy.get('#password').type(password);
-    cy.get('.e2e-terms-and-conditions .e2e-checkbox').click();
-    cy.get('#e2e-signup-step1-button').click();
-    cy.get('.e2e-signup-step2-button').click();
-    cy.location('pathname').should('eq', '/en/');
-    cy.getCookie('cl2_jwt').should('exist');
-    cy.get('#e2e-user-menu-container').should('contain', firstName);
+    cy.contains(projectName).click();
+    cy.get('.e2e-submit-project-select-form').click();
+    cy.location('pathname').should('eq', `/en/projects/${projectName}/ideas/new`);
+    cy.get('#title').type(ideaTitle);
+    cy.get('.ql-editor').type(ideaDescription);
+    cy.contains('Submit idea').click();
+    cy.get('#email').type('koen@citizenlab.co');
+    cy.get('#password').type('testtest');
+    cy.get('.e2e-submit-signin').click();
+    cy.location('pathname').should('eq', `/en/ideas/${ideaTitle}`);
+    cy.get('.e2e-ideatitle').should('eq', ideaTitle);
   });
 });
