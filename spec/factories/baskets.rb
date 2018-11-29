d591cb3ebe6505b77f7b0ac167e1f2c2ae72330e
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-11-29 11:36:54 +0100

    CL2-2787 Updated db related gems and fixed factory bot deprecation warnings

--- spec/factories/baskets.rb
+++ spec/factories/baskets.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :basket do
-    submitted_at "2018-09-13 08:51:08"
-    user { create(:user) }
+    submitted_at { "2018-09-13 08:51:08" }
+    user
     participation_context { create(:continuous_budgeting_project) }
     ideas { create_list(:idea, 3) }
   end
