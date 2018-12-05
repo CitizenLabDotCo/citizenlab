d05094ed0fc551e53087af5dbad8dd1e4467174b
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2018-12-05 14:50:42 +0100

    Added matras database indexes and constraints

--- db/migrate/20181205134744_create_machine_translations.machine_translations.rb
+++ db/migrate/20181205134744_create_machine_translations.machine_translations.rb
@@ -1,3 +1,4 @@
+# This migration comes from machine_translations (originally 20181126142621)
 class CreateMachineTranslations < ActiveRecord::Migration[5.1]
   def change
     create_table :machine_translations_machine_translations, id: :uuid do |t|
