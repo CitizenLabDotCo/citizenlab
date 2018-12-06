a4829f8bb59312d8048a6781f3e3ff2fd42876d8
Sébastien Hoorens <sebastien@citizenlab.co>
2018-12-06 15:10:24 +0100

    minor cleanup of matras controller

--- engines/machine_translations/app/controllers/machine_translations/web_api/v1/machine_translations_controller.rb
+++ engines/machine_translations/app/controllers/machine_translations/web_api/v1/machine_translations_controller.rb
@@ -4,25 +4,13 @@ module MachineTranslations
       class MachineTranslationsController < ApplicationController
 
         def show
-          translatable_type = params[:translatable]
-          translatable_id = params[:"#{translatable_type.underscore}_id"]
-          raise RuntimeError, "must not be blank" if translatable_type.blank? or translatable_id.blank?
-          translation_params = params.require(:machine_translation).permit(
-            :attribute_name,
-            :locale_to,
-          )
-          translation_attributes = {
-            translatable_type: translatable_type,
-            translatable_id: translatable_id,
-            attribute_name: translation_params[:attribute_name],
-            locale_to: translation_params[:locale_to]
-          }
+          set_translation_attributes
+          @translation = MachineTranslation.find_by @translation_attributes
 
-          @translation = MachineTranslation.find_by translation_attributes
           # create translation if it doesn't exist
           if !@translation
             begin
-              @translation = MachineTranslationService.new.build_translation_for translation_attributes
+              @translation = MachineTranslationService.new.build_translation_for @translation_attributes
               authorize @translation
             rescue ClErrors::TransactionError => e
               if e.error_key == :translatable_blank
@@ -39,6 +27,7 @@ module MachineTranslations
           else
             authorize @translation
           end
+          
           # update translation if the original text may have changed
           if @translation.updated_at < @translation.translatable.updated_at
             MachineTranslationService.new.assign_new_translation @translation
@@ -52,7 +41,26 @@ module MachineTranslations
           render json: @translation, serializer: ::WebApi::V1::MachineTranslationSerializer
         end
 
+
         private
+
+        def set_translation_attributes
+          translatable_type = params[:translatable]
+          translatable_id = params[:"#{translatable_type.underscore}_id"]
+          raise RuntimeError, "must not be blank" if translatable_type.blank? or translatable_id.blank?
+
+          translation_params = params.require(:machine_translation).permit(
+            :attribute_name,
+            :locale_to,
+          )
+
+          @translation_attributes = {
+            translatable_type: translatable_type,
+            translatable_id: translatable_id,
+            attribute_name: translation_params[:attribute_name],
+            locale_to: translation_params[:locale_to]
+          }
+        end
 
         def secure_controller?
           false
