749674bc8b37f3f7d7b4a71a8a2f383ea52e536b
David <david@citizenlab.co>
2018-12-10 12:15:16 +0100

    e2e tests WIP

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,146 +1,24 @@
 const wp = require('@cypress/webpack-preprocessor');
 const path = require('path');
-const isDev = process.env.NODE_ENV === 'development';
-const webpack = require('webpack');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ResourceHintsWebpackPlugin = require('resource-hints-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const argv = require('yargs').argv;
-const API_HOST = process.env.API_HOST || 'localhost';
-const API_PORT = process.env.API_PORT || 4000;
-
-const config = {
-  entry: path.join(process.cwd(), 'app/root'),
-
-  output: {
-    path: path.resolve(process.cwd(), 'build'),
-    pathinfo: false,
-    publicPath: '/',
-    filename: isDev ? '[name].js' : '[name].[hash].js',
-    chunkFilename: isDev ? '[name].chunk.js' : '[name].[hash].chunk.js',
-  },
-
-  mode: isDev ? 'development' : 'production',
-
-  devtool: isDev ? 'cheap-module-eval-source-map' : false,
-
-  devServer: {
-    contentBase: path.join(process.cwd(), 'build'),
-    port: 3000,
-    host: '0.0.0.0',
-    disableHostCheck: true,
-    historyApiFallback: true,
-    proxy: {
-      '/web_api': `http://${API_HOST}:${API_PORT}`,
-      '/auth/': `http://${API_HOST}:${API_PORT}`,
-      '/widgets/': `http://${API_HOST}:3200`,
-    },
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.(tsx?)|(js)$/,
-        include: path.join(process.cwd(), 'cypress'),
-        use: {
-          loader: 'ts-loader'
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          { loader: isDev ? 'style-loader' : MiniCssExtractPlugin.loader },
-          { loader: 'css-loader' },
-        ],
-      },
-      {
-        test: /\.(svg|jpg|png|gif)$/,
-        use: [
-          { loader: 'cache-loader' },
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        loader: 'file-loader',
-      },
-      {
-        test: /\.htaccess/,
-        include: path.resolve(process.cwd(), 'app'),
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-          },
-        },
-      },
-    ],
-  },
-
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-        API_HOST: JSON.stringify(process.env.API_HOST),
-        API_PORT: JSON.stringify(process.env.API_PORT),
-        CROWDIN_PLUGIN_ENABLED: !!process.env.CROWDIN_PLUGIN_ENABLED,
-        SEGMENT_API_KEY: JSON.stringify(process.env.SEGMENT_API_KEY),
-        SENTRY_DSN: JSON.stringify(process.env.SENTRY_DSN),
-        CI: JSON.stringify(process.env.CI),
-        CIRCLECI: JSON.stringify(process.env.CIRCLECI),
-        CIRCLE_BUILD_NUM: JSON.stringify(process.env.CIRCLE_BUILD_NUM),
-        CIRCLE_SHA1: JSON.stringify(process.env.CIRCLE_SHA1),
-        CIRCLE_BRANCH: JSON.stringify(process.env.CIRCLE_BRANCH),
-      },
-    }),
-
-    new ForkTsCheckerWebpackPlugin({
-      checkSyntacticErrors: true,
-      tsconfig: path.join(process.cwd(), 'app/tsconfig.json'),
-      silent: !!argv.json, // silent when trying to profile the chunks sizes
-    }),
-
-    new CleanWebpackPlugin(['build']),
-
-    new HtmlWebpackPlugin({
-      template: 'app/index.html',
-      preload: [
-        'main.*.js',
-        'main.*.css',
-        '*.eot',
-        '*.ttf',
-        '*.woff',
-        '*.woff2',
-      ],
-      prefetch: [
-        '*.chunk.js',
-        '*.chunk.css'
-      ],
-    }),
-
-    new ResourceHintsWebpackPlugin(),
-
-    new MiniCssExtractPlugin({
-      filename: '[name].[hash].css',
-      chunkFilename: '[name].[hash].chunk.css'
-    }),
-  ],
-
-  resolve: {
-    modules: [path.resolve(process.cwd(), 'app'), 'node_modules'],
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-  },
-};
 
 module.exports = (on) => {
   on('file:preprocessor', wp({
-    webpackOptions: config,
+    webpackOptions: {
+      resolve: {
+        extensions: ['.ts', '.js'],
+      },
+      module: {
+        rules: [
+          {
+            test: /\.ts$/,
+            exclude: [/node_modules/],
+            include: path.join(process.cwd(), 'cypress'),
+            use: {
+              loader: 'ts-loader'
+            }
+          },
+        ],
+      }
+    },
   }))
 }
