eabb4123faedc88f6b49fd3a5d0dd065fe70db56
KarelCL <41987471+KarelCL@users.noreply.github.com>
2018-12-20 15:14:53 +0100

    Update messages.js

--- app/containers/Admin/ideas/all/messages.js
+++ app/containers/Admin/ideas/all/messages.js
@@ -7,6 +7,6 @@ export default defineMessages({
   },
   headerSubtitle: {
     id: 'app.containers.admin.ideas.all.headerSubtitle',
-    defaultMessage: 'Get a complete overview of all the ideas of all projects. Add themes, change the status by dragging and dropping them to one of the left columns or edit the ideas (add image, take out typo’s). If you want to use another list of the statuses or topics, hand us your personal list via support@citizenlab.co.',
+    defaultMessage: 'Get a complete overview of all the ideas of all projects. Add themes, change the status by dragging and dropping them to one of the left columns or edit the ideas. If you want to use other statuses or topics, hand us your personal list via support@citizenlab.co.',
   },
 });
