d89322dec17c8bf7e25897e6d65e3064580dbe88
Sébastien Hoorens <sebastien@citizenlab.co>
2019-01-02 15:23:14 +0100

    put back options in geotag cache key

--- engines/nlp/app/services/nlp/geotag_service.rb
+++ engines/nlp/app/services/nlp/geotag_service.rb
@@ -17,12 +17,12 @@ module NLP
         filter_by_city: filter_by_city
       }
       # TODO parameterize the choice of location between title and body
-      title_locations = Rails.cache.fetch("#{idea.cache_key}/geotag/title/#{locale}/#{options.to_s}") do
+      title_locations = Rails.cache.fetch(geotag_cache_key(idea, 'title', locale, options)) do
         @api.geotag tenant_id, title, locale, options
       end
       return title_locations.first if title_locations.present?
 
-      body_locations = Rails.cache.fetch("#{idea.cache_key}/geotag/body/#{locale}/#{options.to_s}") do
+      body_locations = Rails.cache.fetch(geotag_cache_key(idea, 'body', locale, options)) do
         @api.geotag tenant_id, idea.body_multiloc[locale], locale, options
       end
       return body_locations.first if body_locations.present?
@@ -30,6 +30,11 @@ module NLP
 
 
     private
+
+    def geotag_cache_key idea, attribute, locale, options={}
+      options_key = ::Digest::SHA256.hexdigest options.to_a.sort_by {|k,v| k.to_s}.to_s
+      "#{idea.cache_key}/geotag/#{attribute}/#{locale}/#{options_key}"
+    end
 
   end
 end
