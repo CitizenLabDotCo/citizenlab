d83d2004475bac4af17d1c71f0191f145056e922
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2019-01-03 18:46:19 +0100

    CL2-2961 Configured memcached as cache store

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :mem_cache_store, ENV.fetch('MEMCACHE_HOST')
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
