d9b3c20df1a4ab7671d2220cbf593f3586262494
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2019-01-07 17:24:15 +0100

    CL2-3015 Fixed autloading issue with onboarding controllers

--- engines/onboarding/app/controllers/onboarding/web_api/v1/campaigns_controller.rb
+++ engines/onboarding/app/controllers/onboarding/web_api/v1/campaigns_controller.rb
@@ -1,32 +1,36 @@
 module Onboarding
-  class WebApi::V1::CampaignsController < OnboardingController
+  module WebApi
+    module V1
+      class CampaignsController < OnboardingController
 
-    class Campaign < OpenStruct
-      include ActiveModel::Serialization
-      def type
-        Campaign
+        class Campaign < OpenStruct
+          include ActiveModel::Serialization
+          def type
+            Campaign
+          end
+        end
+
+
+        def current
+          authorize current_user, :update?
+          service = OnboardingService.new
+          current_campaign = service.current_campaign(current_user)
+          custom_cta = current_campaign == :custom_cta
+          campaign = Campaign.new({
+            name: current_campaign,
+            cta_message_multiloc: custom_cta ? Tenant.settings('core','custom_onboarding_message') : nil,
+            cta_button_multiloc: custom_cta ? Tenant.settings('core','custom_onboarding_button') : nil,
+            cta_button_link: custom_cta ? Tenant.settings('core','custom_onboarding_link') : nil,
+          })
+          render json: campaign
+        end
+
+        private
+
+        def secure_controller?
+          true
+        end
       end
-    end
-
-
-    def current
-      authorize current_user, :update?
-      service = OnboardingService.new
-      current_campaign = service.current_campaign(current_user)
-      custom_cta = current_campaign == :custom_cta
-      campaign = Campaign.new({
-        name: current_campaign,
-        cta_message_multiloc: custom_cta ? Tenant.settings('core','custom_onboarding_message') : nil,
-        cta_button_multiloc: custom_cta ? Tenant.settings('core','custom_onboarding_button') : nil,
-        cta_button_link: custom_cta ? Tenant.settings('core','custom_onboarding_link') : nil,
-      })
-      render json: campaign
-    end
-
-    private
-
-    def secure_controller?
-      true
     end
   end
 end
