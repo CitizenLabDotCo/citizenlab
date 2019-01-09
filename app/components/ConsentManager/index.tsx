ad5a1a6eaff9b2a0c55fdebfae28e861cf49a1ba
Sara <sara@citizenlab.co>
2019-01-09 14:57:19 +0100

    Tests index

--- app/components/ConsentManager/index.tsx
+++ app/components/ConsentManager/index.tsx
@@ -24,47 +24,47 @@ const initialPreferences = {
   functional: null
 };
 
-export default class ConsentManager extends PureComponent {
-  render() {
-    return (
-      <ConsentManagerBuilder
-        writeKey={CL_SEGMENT_API_KEY}
-        mapCustomPreferences={this.handleMapCustomPreferences}
-        initialPreferences={initialPreferences}
-      >
-        {(ConsentManagerProps) => (
-          <Container {...ConsentManagerProps} />
-        )}
-      </ConsentManagerBuilder>
-    );
+export const handleMapCustomPreferences = ({ destinations, preferences }) => {
+  const destinationPreferences = {};
+  const customPreferences = {} as CustomPreferences;
+
+  // Default unset preferences to true (for implicit consent)
+  for (const preferenceName of Object.keys(preferences)) {
+    const value = preferences[preferenceName];
+    if (typeof value === 'boolean') {
+      customPreferences[preferenceName] = value;
+    } else {
+      customPreferences[preferenceName] = true;
+    }
   }
 
-  handleMapCustomPreferences = ({ destinations, preferences }) => {
-    const destinationPreferences = {};
-    const customPreferences = {} as CustomPreferences;
-
-    // Default unset preferences to true (for implicit consent)
-    for (const preferenceName of Object.keys(preferences)) {
-      const value = preferences[preferenceName];
-      if (typeof value === 'boolean') {
-        customPreferences[preferenceName] = value;
-      } else {
-        customPreferences[preferenceName] = true;
-      }
+  for (const destination of destinations) {
+    if (ADVERTISING_CATEGORIES.find(c => c === destination.category)) {
+      destinationPreferences[destination.id] = customPreferences.advertising;
+    } else if (FUNCTIONAL_CATEGORIES.find(c => c === destination.category)) {
+      destinationPreferences[destination.id] = customPreferences.functional;
+    } else {
+      // Fallback to marketing
+      destinationPreferences[destination.id] =
+        customPreferences.analytics;
     }
-
-    for (const destination of destinations) {
-      if (ADVERTISING_CATEGORIES.find(c => c === destination.category)) {
-        destinationPreferences[destination.id] = customPreferences.advertising;
-      } else if (FUNCTIONAL_CATEGORIES.find(c => c === destination.category)) {
-        destinationPreferences[destination.id] = customPreferences.functional;
-      } else {
-        // Fallback to marketing
-        destinationPreferences[destination.id] =
-          customPreferences.analytics;
-      }
-    }
-
-    return { destinationPreferences, customPreferences };
   }
-}
+
+  return { destinationPreferences, customPreferences };
+};
+
+const ConsentManager = () => {
+  return (
+    <ConsentManagerBuilder
+      writeKey={CL_SEGMENT_API_KEY}
+      mapCustomPreferences={handleMapCustomPreferences}
+      initialPreferences={initialPreferences}
+    >
+      {(ConsentManagerProps) => (
+        <Container {...ConsentManagerProps} />
+      )}
+    </ConsentManagerBuilder>
+  );
+};
+
+export default ConsentManager;
