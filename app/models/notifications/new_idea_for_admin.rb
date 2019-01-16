aac43bf7853e61b23e51e5813a39862cec465f09
Sébastien Hoorens <sebastien@citizenlab.co>
2019-01-16 13:43:42 +0100

    added new idea for admin notification and some initial readme on howto add email campaigns

---
+++ app/models/notifications/new_idea_for_admin.rb
@@ -0,0 +1,29 @@
+module Notifications
+  class NewIdeaForAdmin < Notification
+    
+    belongs_to :initiating_user, class_name: 'User'
+
+    validates :initiating_user, presence: true
+
+    ACTIVITY_TRIGGERS = {'Idea' => {'published' => true}}
+    EVENT_NAME = 'New idea for admin'
+
+
+    def self.make_notifications_on activity
+      idea = activity.item
+      initiator = idea.author
+      
+      if initiator && !(initiator&.admin? || initiator.project_moderator(idea.project_id))
+        User.admin.or(User.project_moderator(idea.project_id)).map do |recipient|
+          self.create(
+           recipient_id: recipient.id,
+           initiating_user: initiator
+         )
+        end
+      else
+        []
+      end
+    end
+
+  end
+end
