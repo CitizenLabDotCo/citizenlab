8142c28d775a0344742b9fc5a1ba8ea3cea6dad7
Sébastien Hoorens <sebastien@citizenlab.co>
2019-01-23 10:03:56 +0100

    fixed anonymized user method and spec

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -4,11 +4,24 @@ describe UserService do
   let(:service) { UserService.new }
 
   describe "#anonymized_attributes" do
+    before(:all) do
+      Apartment::Tenant.switch!('example_org')
+      TenantTemplateService.new.apply_template('base')
+      CustomField.find_by(code: 'education').update(enabled: true)
+    end
 
-    it "anonymizes confidential parts of the user's attributes" do
-      user = create(:user)
-      attributes = service.anonymized_attributes 'en', user: user
-      User.create! attributes
+    after(:all) do
+      Apartment::Tenant.reset
+      Tenant.find_by(host: 'example.org').destroy
+      create(:test_tenant)
+    end
+
+    it "anonymizes confidential parts of the user's attributes" do 
+      10.times do 
+        user = create(:user)
+        attributes = service.anonymized_attributes 'en', user: user
+        expect(User.new(attributes)).to be_valid
+      end
     end
 
   end
