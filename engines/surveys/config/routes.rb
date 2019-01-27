2bdbfd661d06bcc54e90efaa5ffd964794d5aa8b
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2019-01-27 16:58:40 +0100

    CL2-3082 Added endpoint to xlsx export survey responses

--- engines/surveys/config/routes.rb
+++ engines/surveys/config/routes.rb
@@ -2,7 +2,8 @@ Surveys::Engine.routes.draw do
 
   namespace :web_api, :defaults => {:format => :json} do
     namespace :v1 do
-
+      get 'projects/:participation_context_id/survey_responses/as_xlsx' => 'responses#index_xlsx', defaults: {pc_class: Project}
+      get 'phases/:participation_context_id/survey_responses/as_xlsx' => 'responses#index_xlsx', defaults: {pc_class: Phase}
     end
   end
 
