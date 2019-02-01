515d3729c3f595a027b03be5bae0ad26a49519f7
Sara <sara@citizenlab.co>
2019-02-01 15:38:32 +0100

    Styling FormLocaleSwitcher

--- app/components/UI/Icon/index.tsx
+++ app/components/UI/Icon/index.tsx
@@ -819,6 +819,12 @@ const clIcons = {
       <path d="M7 0C3.1325 0 0 3.1325 0 7C0 10.8675 3.1325 14 7 14C10.8675 14 14 10.8675 14 7C14 3.1325 10.8675 0 7 0ZM7.7 10.5H6.3V6.3H7.7V10.5ZM7.7 4.9H6.3V3.5H7.7V4.9Z" />
     </svg>
   ),
+  dot: (className, title) => (
+    <svg className={className} width="7" height="7" viewBox="0 0 7 7" xmlns="http://www.w3.org/2000/svg">
+      {injectTitle(title)}
+      <circle cx="3.5" cy="3.5" r="3.5" />
+    </svg>
+  ),
 };
 
 type NormalIconNames = keyof typeof icons;
