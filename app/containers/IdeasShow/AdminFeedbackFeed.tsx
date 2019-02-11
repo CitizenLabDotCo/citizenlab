c7c3926895d949bc016c0d182cd75235378fb4dc
Brent Guffens <brentguf@users.noreply.github.com>
2019-02-11 14:22:25 +0100

    Styles for AdminFeedbackFeed

--- app/containers/IdeasShow/AdminFeedbackFeed.tsx
+++ app/containers/IdeasShow/AdminFeedbackFeed.tsx
@@ -5,7 +5,7 @@ import Button from 'components/UI/Button';
 
 // styles
 import styled from 'styled-components';
-import { colors } from 'utils/styleUtils';
+import { colors, fontSizes } from 'utils/styleUtils';
 
 // i18n
 import messages from './messages';
@@ -22,22 +22,34 @@ const AdminFeedbackPost = styled.div`
   flex-direction: column;
   background-color: rgba(236, 90, 36, 0.06);
   color: ${colors.text};
+  font-size: ${fontSizes.base}px;
+  padding: 17px 53px 27px 34px;
+  margin-bottom: 10px;
 `;
 
 const EditPostButton = styled(Button)`
   margin-left: auto;
   text-decoration: underline;
+  padding: 0;
+  margin-bottom: 10px;
 `;
 
-const Body = styled.div``;
+const Body = styled.div`
+  line-height: 23px;
+  margin-bottom: 16px;
+`;
 
-const Footer = styled.div``;
+const Footer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const Author = styled.span`
-  font-weight: bold;
+  font-weight: 600;
 `;
 
 const DatePosted = styled.span`
+  color: ${colors.label};
 `;
 
 const LoadMoreButton = styled(Button)`
@@ -65,11 +77,15 @@ export default class AdminFeedbackFeed extends React.Component<Props, State> {
             text={<FormattedMessage {...messages.editAdminFeedbackPost} />}
           />
           <Body>
-            {}
+            Hi everyone! First off, thanks to those who filled out our survey from earlier this summer. User accounts is a big, big undertaking, and we’re still in the research phase, working to determine which use-cases would be most impactful to tackle first.
+
+            Our first steps down this path will likely be in the realm of ecommerce, as we’re planning to build a login system for customers on your ecommerce site. The good news here is that nearly all of the work on this ecommerce customer portal will lay the foundation for larger work around a general user login/account system in Webflow.
+
+            So, in summary: we’re still researching, but our planned work on ecommerce will continue moving us closer to the day when a more general user login/account system is possible in Webflow.
           </Body>
           <Footer>
-            <Author>{}</Author>
-            <DatePosted>{}</DatePosted>
+            <Author>Sarah from Mobility Department</Author>
+            <DatePosted>02 jan 2019</DatePosted>
           </Footer>
         </AdminFeedbackPost>
         {true /* !querying && hasMore && */ &&
