96396a166d5e8ab408828743c7f727219e1d6c22
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2019-02-18 16:24:14 +0100

    CL2-2981 WIP Added logic for manual project sorting to sortin service

--- app/services/project_sorting_service.rb
+++ app/services/project_sorting_service.rb
@@ -1,7 +1,13 @@
 class ProjectSortingService
 
   def sort projects_sope
-    projects_sope
+    if Tenant.current.has_feature? 'manual_project_sorting'
+      projects_sope
+        .publication_status_ordered
+        .order(:ordering)
+    else
+      projects_sope
+    end
   end
 
 end
