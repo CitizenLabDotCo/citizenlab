294bde923b4afc9aeba103de585f6eeb9441d2dc
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2019-02-20 19:15:47 +0100

    CL2-2981 WIP Added recency_score to sorting db view

--- db/views/project_sort_scores_v01.sql
+++ db/views/project_sort_scores_v01.sql
@@ -1,4 +1,4 @@
-SELECT sub.id AS project_id, CONCAT(sub.status_score, sub.active_score, sub.hot_score) AS score FROM (
+SELECT sub.id AS project_id, CONCAT(sub.status_score, sub.active_score, sub.hot_score, sub.recency_score) AS score FROM (
   SELECT
     projects.id as id,
     CASE publication_status
@@ -30,11 +30,24 @@ SELECT sub.id AS project_id, CONCAT(sub.status_score, sub.active_score, sub.hot_
   END
       WHEN 'continuous' THEN
         CASE
-          WHEN (NOW()::date - created_at::date) <= 7 THEN 1
+          WHEN (NOW()::date - projects.created_at::date) <= 7 THEN 1
           ELSE 2
         END
-    END AS hot_score
+    END AS hot_score,
 
+    LPAD(
+      CASE process_type
+        WHEN 'timeline' THEN COALESCE(MIN(joined_phases.recency_diff), NOW()::date - projects.created_at::date)
+        WHEN 'continuous' THEN NOW()::date - projects.created_at::date
+      END::text,
+      5,
+      '0'
+    ) AS recency_score
   FROM 
     projects
+  LEFT OUTER JOIN 
+    (
+      SELECT project_id, LEAST(ABS(NOW()::date - start_at), ABS(NOW()::date - end_at)) AS recency_diff FROM phases
+    ) AS joined_phases ON joined_phases.project_id = projects.id
+  GROUP BY projects.id
 ) AS sub
