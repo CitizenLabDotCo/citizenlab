28905c28e5de22dc066b4c124132e0db33d141b3
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2019-02-20 18:29:46 +0100

    CL2-2981 WIP Added hot_score to sorting db view

--- db/views/project_sort_scores_v01.sql
+++ db/views/project_sort_scores_v01.sql
@@ -1,4 +1,4 @@
-SELECT sub.id AS project_id, CONCAT(sub.status_score, sub.active_score) AS score FROM (
+SELECT sub.id AS project_id, CONCAT(sub.status_score, sub.active_score, sub.hot_score) AS score FROM (
   SELECT
     projects.id as id,
     CASE publication_status
@@ -20,7 +20,20 @@ SELECT sub.id AS project_id, CONCAT(sub.status_score, sub.active_score) AS score
               ELSE 3
             END
         END
-    END AS active_score
+    END AS active_score,
+
+    CASE process_type
+      WHEN 'timeline' THEN
+  CASE
+    WHEN EXISTS(SELECT 1 FROM phases WHERE (ABS(start_at - NOW()::date) <= 7 OR ABS(end_at - NOW()::date) <= 7) AND project_id = projects.id) THEN 1
+    ELSE 2
+  END
+      WHEN 'continuous' THEN
+        CASE
+          WHEN (NOW()::date - created_at::date) <= 7 THEN 1
+          ELSE 2
+        END
+    END AS hot_score
 
   FROM 
     projects
