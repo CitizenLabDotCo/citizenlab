e2cc9cf4f6ddf0e5f3848e27fdd5c9ec7c1e812d
Sébastien Hoorens <sebastien@citizenlab.co>
2019-03-05 15:59:48 +0100

    git pls

---
+++ app/models/notifications/mention_in_official_feedback.rb
@@ -0,0 +1,47 @@
+module Notifications
+  class MentionInComment < Notification
+    
+    belongs_to :initiating_user, class_name: 'User'
+    belongs_to :official_feedback
+    belongs_to :idea
+    belongs_to :project, optional: true
+
+    validates :official_feedback_id, presence: true
+    validates :initiating_user, presence: true
+    validates :idea_id, presence: true
+
+
+    ACTIVITY_TRIGGERS = {'OfficialFeedback' => {'mentioned' => true}}
+    EVENT_NAME = 'Mention in official feedback'
+    
+
+    def self.make_notifications_on activity
+      official_feedback = activity.item
+      recipient_id = activity.payload["mentioned_user"]
+
+      official_feedback_id = official_feedback&.id
+      idea = official_feedback&.idea
+      idea_id = official_feedback&.idea_id
+      initiator_id = official_feedback&.user_id
+      project_id = idea&.project_id
+      participant_ids = [idea.author_id]
+      participant_ids += idea.votes.pluck(:user_id)
+      participant_ids += idea.comments.pluck(:author_id)
+      participant_ids.uniq!
+
+      if official_feedback_id && idea_id && recipient_id && initiator_id && (recipient_id != initiator_id) && !participant_ids.include?(recipient_id)
+        [self.create(
+           recipient_id: recipient_id,
+           initiating_user: User.find(initiator_id),
+           idea_id: idea_id,
+           official_feedback_id: official_feedback_id,
+           project_id: project_id
+         )]
+      else
+        []
+      end
+    end
+
+  end
+end
+
---
+++ engines/email_campaigns/app/models/email_campaigns/campaigns/mention_in_official_feedback.rb
@@ -0,0 +1,13 @@
+module EmailCampaigns
+  class Campaigns::MentionInOfficialFeedback < Campaigns::NotificationCampaign
+    include Consentable
+    include ActivityTriggerable
+    include RecipientConfigurable
+
+    recipient_filter :filter_notification_recipient
+
+    def activity_triggers
+      {'Notifications::MentionInOfficialFeedback' => {'created' => true}}
+    end
+  end
+end
