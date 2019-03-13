6801912434c52b155b6e122f775d72a1bf668a89
David <david@citizenlab.co>
2019-03-13 09:48:52 +0100

    fix for broken project_overview_page e2e test

--- cypress/integration/project_overview_page.ts
+++ cypress/integration/project_overview_page.ts
@@ -13,7 +13,8 @@ describe('Project overview page', () => {
     cy.wait(1000);
     cy.get('#e2e-projects-list');
     cy.get('.e2e-filter-selector-publicationstatus').contains('Archived projects');
-    cy.get('.e2e-project-card').first().find('.e2e-project-card-archived-label');
-    cy.get('.e2e-project-card').first().should('have.class', 'archived');
+    cy.get('.e2e-project-card').first().as('projectCard');
+    cy.get('@projectCard').should('have.class', 'archived');
+    cy.get('@projectCard').get('.e2e-project-card-archived-label');
   });
 });
