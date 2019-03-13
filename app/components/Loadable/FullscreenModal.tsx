46445175288915ac8e0ae7d84d28d1c4be1a4671
Brent Guffens <brentguf@users.noreply.github.com>
2019-03-13 14:49:26 +0100

    Make FullscreenModal lazy loadable

---
+++ app/components/Loadable/FullscreenModal.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Loadable from 'react-loadable';
+
+type LoadableFullscreenModalInputProps = {
+  modalOpened: boolean;
+  close: () => void;
+  modalUrl: string | null;
+  modalId: string | null;
+  modalType: string | null;
+  unauthenticatedVoteClick: () => void;
+};
+
+const LoadableFullscreenModal = Loadable.Map({
+  loading: () => null,
+  loader: {
+    FullscreenModal: () => import('components/UI/FullscreenModal'),
+    IdeasShow: () => import('containers/IdeasShow'),
+    VoteControl: () => import('components/VoteControl'),
+  },
+  render(loaded, props: LoadableFullscreenModalInputProps) {
+    const FullscreenModal = loaded.FullscreenModal.default;
+    const IdeasShow = loaded.IdeasShow.default;
+    const VoteControl = loaded.VoteControl.default;
+    const { modalOpened, close, modalUrl, modalId, modalType, unauthenticatedVoteClick } = props;
+
+    const fullscreenModalHeaderChild: JSX.Element | undefined = ((modalOpened && modalType === 'idea' && modalId) ? (
+      <VoteControl
+        ideaId={modalId}
+        unauthenticatedVoteClick={unauthenticatedVoteClick}
+        size="1"
+      />
+    ) : undefined);
+
+    return (
+      <FullscreenModal
+        opened={modalOpened}
+        close={close}
+        url={modalUrl}
+        headerChild={fullscreenModalHeaderChild}
+      >
+        {modalId && <IdeasShow ideaId={modalId} inModal={true}/>}
+      </FullscreenModal>);
+  }
+});
+
+export default LoadableFullscreenModal;
--- app/containers/App/index.tsx
+++ app/containers/App/index.tsx
@@ -29,9 +29,7 @@ import { trackPage, trackIdentification } from 'utils/analytics';
 import Meta from './Meta';
 import Navbar from 'containers/Navbar';
 import ForbiddenRoute from 'components/routing/forbiddenRoute';
-import FullscreenModal from 'components/UI/FullscreenModal';
-import IdeasShow from 'containers/IdeasShow';
-import VoteControl from 'components/VoteControl';
+import LoadableFullscreenModal from 'components/Loadable/FullscreenModal';
 
 // auth
 import HasPermission from 'components/HasPermission';
@@ -156,6 +154,9 @@ class App extends PureComponent<Props & WithRouterProps, State> {
         this.setState({ tenant, authUser });
       }),
 
+      eventEmitter.observeEvent('cardHover').subscribe(() => {
+        this.preloadIdeaModal();
+      }),
       eventEmitter.observeEvent<IModalInfo>('cardClick').subscribe(({ eventValue }) => {
         const { type, id, url } = eventValue;
         this.openModal(type, id, url);
@@ -166,6 +167,10 @@ class App extends PureComponent<Props & WithRouterProps, State> {
   componentWillUnmount() {
     this.unlisten();
     this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
+
+  preloadIdeaModal = () => {
+    LoadableFullscreenModal.preload();
   }
 
   openModal = (type: string, id: string | null, url: string | null) => {
@@ -185,13 +190,6 @@ class App extends PureComponent<Props & WithRouterProps, State> {
     const { previousPathname, tenant, modalOpened, modalType, modalId, modalUrl, visible } = this.state;
     const isAdminPage = (location.pathname.startsWith('/admin'));
     const theme = getTheme(tenant);
-    const fullscreenModalHeaderChild: JSX.Element | undefined = ((modalOpened && modalType === 'idea' && modalId) ? (
-      <VoteControl
-        ideaId={modalId}
-        unauthenticatedVoteClick={this.unauthenticatedVoteClick}
-        size="1"
-      />
-    ) : undefined);
 
     return (
       <>
@@ -201,14 +199,14 @@ class App extends PureComponent<Props & WithRouterProps, State> {
               <Container className={`${isAdminPage ? 'admin' : 'citizen'}`}>
                 <Meta />
 
-                <FullscreenModal
-                  opened={modalOpened}
+                <LoadableFullscreenModal
+                  modalOpened={modalOpened}
                   close={this.closeModal}
-                  url={modalUrl}
-                  headerChild={fullscreenModalHeaderChild}
-                >
-                  {modalId && <IdeasShow ideaId={modalId} inModal={true}/>}
-                </FullscreenModal>
+                  modalUrl={modalUrl}
+                  modalId={modalId}
+                  modalType={modalType}
+                  unauthenticatedVoteClick={this.unauthenticatedVoteClick}
+                />
 
                 <div id="modal-portal" />
 
