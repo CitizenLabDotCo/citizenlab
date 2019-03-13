994059e21fe7572bcf26cb9b4acebbefb802e3ef
Brent Guffens <brentguf@users.noreply.github.com>
2019-03-13 15:49:33 +0100

    Rename folder: i18n-test => i18n

--- app/i18n/index.ts
+++ app/i18n/index.ts
@@ -1,12 +1,13 @@
 import { DEFAULT_LOCALE } from 'containers/App/constants';
 import { addLocaleData } from 'react-intl';
 import enLocaleData from 'react-intl/locale-data/en';
+import { Locale } from 'typings';
 
 addLocaleData(enLocaleData);
 
 const enTranslationMessages = require('translations/en.json');
 
-export const formatTranslationMessages = (locale, messages) => {
+export const formatTranslationMessages = (locale: Locale, messages: { translateId: string }) => {
   const defaultFormattedMessages = locale !== DEFAULT_LOCALE
     ? formatTranslationMessages(DEFAULT_LOCALE, enTranslationMessages)
     : {};
