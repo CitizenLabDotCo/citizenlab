2033cc458b01e1e17ccf1f9e33e4b94140f3cebd
Sébastien Hoorens <sebastien@citizenlab.co>
2019-03-14 14:03:21 +0100

    minor fixes

--- app/serializers/web_api/v1/notifications/project_phase_started_serializer.rb
+++ app/serializers/web_api/v1/notifications/project_phase_started_serializer.rb
@@ -1,10 +1,9 @@
 class WebApi::V1::Notifications::ProjectPhaseStartedSerializer < WebApi::V1::Notifications::NotificationSerializer
 
   belongs_to :phase, serializer: WebApi::V1::PhaseSerializer
-  belongs_to :idea_status, serializer: WebApi::V1::IdeaStatusSerializer  
   belongs_to :project, serializer: WebApi::V1::ProjectSerializer
 
-  attributes :phase_title_multiloc, :phase_start_at, :project_title_multiloc
+  attributes :phase_title_multiloc, :phase_start_at, :project_slug, :project_title_multiloc, :created_at
 
 
   def phase_title_multiloc
@@ -13,6 +12,10 @@ class WebApi::V1::Notifications::ProjectPhaseStartedSerializer < WebApi::V1::Not
 
   def phase_start_at
     object.phase&.start_at
+  end
+
+  def project_slug
+    object.project&.slug
   end
 
   def project_title_multiloc
