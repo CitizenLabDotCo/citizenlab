4a99143131f2c4a0b74b7be86e0a789164e23379
Brent Guffens <brentguf@users.noreply.github.com>
2019-03-18 11:34:18 +0100

    Simplify centering spinner

--- app/components/UI/FullPageSpinner/index.tsx
+++ app/components/UI/FullPageSpinner/index.tsx
@@ -19,28 +19,20 @@ const FullPageContainer: any = styled.div`
   left: ${(props: Props) => props.admin ? '260px' : '0'};
   width: ${(props: Props) => props.admin ? 'calc(100% - 260px)' : '100%'}; ;
   min-height: calc(100vh - ${props => props.theme.menuHeight}px - 1px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   ${media.smallerThanMaxTablet`
     min-height: calc(100vh - ${props => props.theme.mobileMenuHeight}px - ${props => props.theme.mobileTopBarHeight}px);
   `}
 `;
 
-const SpinnerCenter = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  // Spinner is 32px width/height by default, 16px is half of that
-  margin-top: -16px;
-  margin-left: -16px;
-`;
-
 const FullPageSpinner = (props: Props) => {
   const { admin } = props;
   return (
     <FullPageContainer admin={admin}>
-      <SpinnerCenter>
-        <Spinner />
-      </SpinnerCenter>
+      <Spinner />
     </FullPageContainer>
   );
 };
