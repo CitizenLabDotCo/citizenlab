4edbabd556343cb2c3de7d2d9ee24d5ecf11df42
Brent Guffens <brentguf@users.noreply.github.com>
2019-03-20 11:21:53 +0100

    WIP: E2E ideas_overview_page

--- cypress/integration/ideas_overview_page.ts
+++ cypress/integration/ideas_overview_page.ts
@@ -1,28 +1,65 @@
-describe('Ideas overview page', () => {
-  beforeEach(() => {
-    cy.visit('/ideas');
-  });
+import { randomString } from '../support/commands';
 
+describe('Ideas overview page', () => {
   context('Project filter', () => {
+
     it('filters correctly on 1 project', () => {
+      const projectTitle = randomString();
+      const projectDescriptionPreview = randomString();
+      const projectDescription = randomString();
+      const ideaTitle = randomString();
+      const ideaContent = randomString();
+
+      cy.apiCreateProject('continuous', projectTitle, projectDescriptionPreview, projectDescription).then((project) => {
+        const projectId = project.body.data.id;
+        cy.apiCreateIdea(projectId, ideaTitle, ideaContent);
+      });
+
+      cy.visit('/ideas');
+
+      cy.wait(5000);
       cy.get('#e2e-project-filter-selector').click();
       // select project
-      cy.get('.e2e-filter-selector-dropdown-list').contains('An idea? Bring it to your council!').click();
-      // contains idea from this project
-      cy.get('#e2e-ideas-container').contains('Very New Idea');
+      cy.get('.e2e-filter-selector-dropdown-list').contains(projectTitle).click();
+      // contains only ideas from this project, including a specific check to make sure it's not just the number of ideas that's right
+      cy.get('#e2e-ideas-container').find('.e2e-idea-card').contains(ideaTitle);
     });
 
     it('filters correctly on 2 projects', () => {
-      cy.get('#e2e-project-filter-selector').click();
-      // select first project
-      cy.get('.e2e-filter-selector-dropdown-list').contains('An idea? Bring it to your council!').click();
-      // select second project
-      cy.get('.e2e-filter-selector-dropdown-list').contains('Sit voluptas sint aliquid quia aut nihil eos').click();
-      // contains idea from first project
-      cy.get('#e2e-ideas-container').contains('Very New Idea');
-      // contains idea from second project
-      cy.get('#e2e-ideas-container').contains('Recusandae illo quis porro quisquam enim.');
-    });
+      const projectTitle1 = randomString();
+      const projectTitle2 = randomString();
+      const projectDescriptionPreview1 = randomString();
+      const projectDescriptionPreview2 = randomString();
+      const projectDescription1 = randomString();
+      const projectDescription2 = randomString();
+      const ideaTitle1 = randomString();
+      const ideaTitle2 = randomString();
+      const ideaContent1 = randomString();
+      const ideaContent2 = randomString();
 
+      // create first project with one idea
+      cy.apiCreateProject('continuous', projectTitle1, projectDescriptionPreview1, projectDescription1).then((project) => {
+        const projectId = project.body.data.id;
+        cy.apiCreateIdea(projectId, ideaTitle1, ideaContent1);
+      });
+
+      // create second project with one idea
+      cy.apiCreateProject('continuous', projectTitle2, projectDescriptionPreview2, projectDescription2).then((project) => {
+        const projectId = project.body.data.id;
+        cy.apiCreateIdea(projectId, ideaTitle2, ideaContent2);
+      });
+
+      cy.visit('/ideas');
+
+      cy.wait(5000);
+
+      cy.get('#e2e-project-filter-selector').click();
+      cy.get('.e2e-filter-selector-dropdown-list').contains(projectTitle1).click();
+      cy.get('#e2e-project-filter-selector').click();
+      cy.get('.e2e-filter-selector-dropdown-list').contains(projectTitle2).click();
+
+      cy.get('#e2e-ideas-container').find('.e2e-idea-card').contains(ideaTitle1);
+      cy.get('#e2e-ideas-container').find('.e2e-idea-card').contains(ideaTitle2);
+    });
   });
 });
