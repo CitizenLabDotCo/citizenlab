f038f1bb52d90753baef50200c54bb86c8287420
Sébastien Hoorens <sebastien@citizenlab.co>
2019-03-22 11:57:45 +0100

    added periodic activity for invite not accepted since 3 days

--- spec/services/activities_service_spec.rb
+++ spec/services/activities_service_spec.rb
@@ -57,5 +57,18 @@ describe ActivitiesService do
         .not_to have_enqueued_job(LogActivityJob).with(phase, 'upcoming', nil, now.to_i)
     end
 
+    it "logs invite not accepted since 3 days activity when an invite was not accpeted since (in the tenant's timezone)" do 
+      created_at = Time.parse '2019-03-22 10:50:00 +0000'
+      timezone = 'Asia/Kamchatka'
+      invite = create(:invite, created_at: created_at)
+      settings = Tenant.current.settings
+      settings['core']['timezone'] = timezone
+      Tenant.current.update!(settings: settings)
+      now = created_at + 3.days
+
+      expect {service.create_periodic_activities_for_current_tenant(now: now)}
+        .to have_enqueued_job(LogActivityJob).with(invite, 'not_accepted_since_3_days', nil, now.to_i)
+    end
+
   end
 end
