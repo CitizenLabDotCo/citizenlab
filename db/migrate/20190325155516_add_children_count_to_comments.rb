002339c14280f318ec241c09f9b4dd35af9ee2c7
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2019-03-26 14:52:48 +0100

    CL2-3474 Removed depth from comments to fix Rails Dirty

--- db/migrate/20190325155516_add_children_count_to_comments.rb
+++ db/migrate/20190325155516_add_children_count_to_comments.rb
@@ -1,6 +1,5 @@
-class AddChildrenCountAndDepthToComments < ActiveRecord::Migration[5.2]
+class AddChildrenCountToComments < ActiveRecord::Migration[5.2]
   def change
-    add_column :comments, :depth, :integer, null: false, default: 0
     add_column :comments, :children_count, :integer, null: false, default: 0
 
     Comment.reset_column_information
