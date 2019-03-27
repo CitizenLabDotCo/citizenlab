088f8be124fc0bc7fa62536a4b76a151bd05e647
Brent Guffens <brentguf@users.noreply.github.com>
2019-03-27 11:10:13 +0100

    Add select options for default assignee menu

--- app/containers/Admin/projects/edit/permissions/IdeaAssignment.tsx
+++ app/containers/Admin/projects/edit/permissions/IdeaAssignment.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import { adopt } from 'react-adopt';
 
+// typings
+import { IOption } from 'typings';
+
+// utils
+import { isNilOrError } from 'utils/helperUtils';
+
 // resources
 import GetModerators, { GetModeratorsChildProps } from 'resources/GetModerators';
+
+// components
+import Select from 'components/UI/Select';
+
+// i18n
+import messages from './messages';
+import { injectIntl } from 'utils/cl-intl';
+import { InjectedIntlProps } from 'react-intl';
 
 interface InputProps {
   projectId: string;
@@ -12,20 +26,49 @@ interface DataProps {
   moderators: GetModeratorsChildProps;
 }
 
-interface Props extends InputProps {}
+interface Props extends InputProps, DataProps {}
 
-class IdeaAssignment extends React.PureComponent<Props> {
+class IdeaAssignment extends React.PureComponent<Props & InjectedIntlProps> {
+
+  getOptions = () => {
+    const { moderators } = this.props;
+    let moderatorOptions: IOption[] = [];
+
+    if (!isNilOrError(moderators)) {
+      moderatorOptions = moderators.map(moderator => {
+        return ({
+          value: moderator.id,
+          label: `${moderator.attributes.first_name} ${moderator.attributes.last_name}`,
+        });
+      });
+    }
+
+    return [{ value: null, label: this.props.intl.formatMessage(messages.unassigned) }, ...moderatorOptions];
+  }
+
   render() {
-    return (<div>test</div>);
+    // We want a list that contains the default option, null -- unassigned + a list of moderators.
+    // We can update the assignee with a project update
+    // After this update, we want to show who's been assigned by looking up the user through the user id and show it through the value prop
+    //
+    return (
+      <Select
+        options={this.getOptions()}
+        value={}
+        onChange={}
+      />
+    );
   }
 }
 
 const Data = adopt<DataProps, InputProps>({
-  moderators: ({ projectId }) => <GetModerators projectId={projectId} />,
+  moderators: ({ projectId, render }) => <GetModerators projectId={projectId}>{render}</GetModerators>,
 });
+
+const IdeaAssignmentWithInjectIntl = injectIntl(IdeaAssignment);
 
 export default (inputProps: InputProps) => (
   <Data {...inputProps}>
-    {dataprops => <IdeaAssignment {...inputProps} {...dataprops} />}
+    {dataprops => <IdeaAssignmentWithInjectIntl {...inputProps} {...dataprops} />}
   </Data>
 );
