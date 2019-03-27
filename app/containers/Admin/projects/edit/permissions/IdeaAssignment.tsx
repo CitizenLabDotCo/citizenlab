1f88a1b4a7dd56167c1c8157655f003b485631d2
Brent Guffens <brentguf@users.noreply.github.com>
2019-03-27 10:12:12 +0100

    Load moderators in IdeaAssignment

--- app/containers/Admin/projects/edit/permissions/IdeaAssignment.tsx
+++ app/containers/Admin/projects/edit/permissions/IdeaAssignment.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { adopt } from 'react-adopt';
 
-interface Props {}
+// resources
+import GetModerators, { GetModeratorsChildProps } from 'resources/GetModerators';
 
-export default class IdeaAssignment extends React.PureComponent<Props> {
+interface InputProps {
+  projectId: string;
+}
+
+interface DataProps {
+  moderators: GetModeratorsChildProps;
+}
+
+interface Props extends InputProps {}
+
+class IdeaAssignment extends React.PureComponent<Props> {
   render() {
     return (<div>test</div>);
   }
 }
+
+const Data = adopt<DataProps, InputProps>({
+  moderators: ({ projectId }) => <GetModerators projectId={projectId} />,
+});
+
+export default (inputProps: InputProps) => (
+  <Data {...inputProps}>
+    {dataprops => <IdeaAssignment {...inputProps} {...dataprops} />}
+  </Data>
+);
