514fcf2a5133c49cef11fdec107a2ac89422b6e0
Sébastien Hoorens <sebastien@citizenlab.co>
2019-03-29 11:19:53 +0100

    dont send new idea for admin to assignee

--- app/serializers/web_api/v1/notifications/idea_assigned_to_you_serializer.rb
+++ app/serializers/web_api/v1/notifications/idea_assigned_to_you_serializer.rb
@@ -4,11 +4,15 @@ class WebApi::V1::Notifications::IdeaAssignedToYouSerializer < WebApi::V1::Notif
   belongs_to :idea, serializer: WebApi::V1::IdeaSerializer 
   belongs_to :project, serializer: WebApi::V1::ProjectSerializer
 
-  attributes :idea_title
+  attributes :idea_title_multiloc, :idea_slug
   
 
-  def idea_title
+  def idea_title_multiloc
     object.idea&.title_multiloc
+  end
+
+  def idea_slug
+    object.idea&.slug
   end
 
 end
