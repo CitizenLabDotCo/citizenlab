b02f46c77348050df0b8b2ae2aac962f7eb30568
Brent Guffens <brentguf@users.noreply.github.com>
2019-03-29 16:02:16 +0100

    getCanonicalLink function

--- app/utils/cl-router/getCanonicalLink.tsx
+++ app/utils/cl-router/getCanonicalLink.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import{ GetTenantLocalesChildProps } from 'resources/GetTenantLocales';
-import { replacePathnameLocale } from 'services/locale';
+import { removeUrlLocale } from 'services/locale';
 
-export default function getCanonicalLink(tenantLocales: GetTenantLocalesChildProps) {
-  if (Array.isArray(tenantLocales)) {
-    const firstTenantLocale = tenantLocales[0];
-    return <link rel="canonical" href={`${location.origin}${replacePathnameLocale(location.pathname, firstTenantLocale)}`} />;
-  }
-
-  return null;
+// Make url without locale the canonical of a certain page
+// e.g. our URL = https://youth4climate.be/nl-BE/
+// this function will turn it into the following link element to put in the head element of the page:
+// <link rel="canonical" href="https://youth4climate.be/"/>
+// this lets Google know it should show 'https://youth4climate.be/' in the search results, not the version with locale
+export default function getCanonicalLink() {
+  return <link rel="canonical" href={`${location.origin}${removeUrlLocale(location.pathname)}`} />;
 }
