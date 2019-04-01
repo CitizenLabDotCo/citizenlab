64563e431d365b899910a105e4f0c451d4a1e19a
Brent Guffens <brentguf@users.noreply.github.com>
2019-04-01 15:13:53 +0200

    Only add hreflang for non-admin pages

--- app/utils/cl-router/getAlternateLinks.tsx
+++ app/utils/cl-router/getAlternateLinks.tsx
@@ -4,7 +4,9 @@ import { replacePathnameLocale } from 'services/locale';
 
 // https://github.com/nfl/react-helmet/issues/279 href comes first!
 export default function getAlternateLinks(tenantLocales: GetTenantLocalesChildProps) {
-  if (Array.isArray(tenantLocales) && tenantLocales.length > 1) {
+  const isAdminPage = location.pathname.includes('admin');
+
+  if (!isAdminPage && Array.isArray(tenantLocales) && tenantLocales.length > 1) {
     return tenantLocales.map(loc => {
       return <link href={`${location.origin}${replacePathnameLocale(location.pathname, loc)}`} rel="alternate" hrefLang={loc} key={loc} />;
     });
