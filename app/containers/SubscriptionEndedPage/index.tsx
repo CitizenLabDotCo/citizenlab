132a482476bee950661b21bd9564dde560d45dde
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2019-04-01 14:11:56 +0200

    CL2-3331 Revoked admin access for churned tenants

---
+++ app/containers/SubscriptionEndedPage/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { FormattedMessage } from 'utils/cl-intl';
+import messages from './messages';
+import ContentContainer from 'components/ContentContainer';
+
+// styling
+import styled from 'styled-components';
+import { media, colors, fontSizes } from 'utils/styleUtils';
+
+const Container = styled.div`
+  min-height: calc(100vh - ${props => props.theme.menuHeight}px - 1px);
+  display: flex;
+  flex-direction: column;
+  background: ${colors.background};
+
+  ${media.smallerThanMaxTablet`
+    min-height: calc(100vh - ${props => props.theme.mobileMenuHeight}px - ${props => props.theme.mobileTopBarHeight}px);
+  `}
+  padding-top: 60px;
+`;
+
+const Title = styled.h1`
+  color: ${colors.text};
+  font-size: ${fontSizes.xxxxl}px;
+  line-height: 40px;
+  font-weight: 500;
+  text-align: left;
+  margin: 0;
+  padding: 0;
+  padding-top: 0px;
+  padding-bottom: 40px;
+
+  ${media.smallerThanMaxTablet`
+    font-size: ${fontSizes.xxxl};
+    line-height: 34px;
+  `}
+`;
+
+const SubscriptionEndedPage = React.memo(() => (
+    <Container>
+      <ContentContainer>
+        <Title><FormattedMessage {...messages.accessDenied} /></Title>
+        <div>
+          <FormattedMessage {...messages.subscriptionEnded} />
+        </div>
+      </ContentContainer>
+    </Container>
+));
+
+export default SubscriptionEndedPage;
