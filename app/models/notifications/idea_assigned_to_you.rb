3a9c8761bae86964192e2f700cb7954ebf463813
Sébastien Hoorens <sebastien@citizenlab.co>
2019-04-03 16:38:17 +0200

    add initiator when creating assignment notification

--- app/models/notifications/idea_assigned_to_you.rb
+++ app/models/notifications/idea_assigned_to_you.rb
@@ -10,13 +10,15 @@ module Notifications
     def self.make_notifications_on activity
       idea = activity.item
       recipient = idea.assignee
+      initiator_id = activity.user_id
 
       if recipient
         [
           self.create(
            recipient_id: recipient.id,
            idea_id: idea.id,
-           project_id: idea.project_id
+           project_id: idea.project_id,
+           initiating_user_id: initiator_id
          )
         ]
       else
