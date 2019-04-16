9b966a9c56d2ebc2032d442cfdb8000c6541d466
Sébastien Hoorens <sebastien@citizenlab.co>
2019-04-16 14:07:25 +0200

    random ideas ordering using part of todays epoch as modulus for created_at epoch

--- app/services/random_ordering_service.rb
+++ app/services/random_ordering_service.rb
@@ -1,8 +1,7 @@
 class RandomOrderingService
 
   def modulus_of_the_day date=Date.today
-    # returns 36
-    date.to_time.to_i.to_s.chars.map(&:to_i).sum
+    1000 + date.to_time.to_i.to_s.chars.reverse[2..5].join.to_i
   end
 
 end
