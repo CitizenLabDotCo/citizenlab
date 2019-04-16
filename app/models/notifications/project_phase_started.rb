019d241bc10defdbbde1cf81e4c93a6f2c76c54a
Sébastien Hoorens <sebastien@citizenlab.co>
2019-04-16 11:01:08 +0200

    dont send out phase started when no action (posting, voting, survey...) is possible in phase

--- app/models/notifications/project_phase_started.rb
+++ app/models/notifications/project_phase_started.rb
@@ -12,6 +12,7 @@ module Notifications
     
 
     def self.make_notifications_on activity
+      service = ParticipationContextService.new
       phase = activity.item
 
       phase_id = phase&.id
@@ -20,12 +21,18 @@ module Notifications
       if project_id
         user_scope = User.where.not(id: User.admin.or(User.project_moderator(project_id)).ids)
         ProjectPolicy::InverseScope.new(phase.project, user_scope).resolve.map do |recipient|
-          self.create!(
-             recipient_id: recipient.id,
-             phase_id: phase_id,
-             project_id: project_id
-           )
-        end
+          if ( !service.posting_disabled_reason_for_context(phase, recipient)\
+            || !service.commenting_disabled_reason_for_context(phase, recipient)\
+            || !service.voting_disabled_reason_for_context(phase, recipient)\
+            || !service.taking_survey_disabled_reason_for_context(phase, recipient)\
+            || !service.budgeting_disabled_reason_for_context(phase, recipient) )
+            self.create!(
+               recipient_id: recipient.id,
+               phase_id: phase_id,
+               project_id: project_id
+             )
+          end
+        end.compact
       else
         []
       end
