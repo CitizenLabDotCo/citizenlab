53073718addf7776d5d906a0d74d8a906c133d6b
David <david@citizenlab.co>
2019-04-18 13:58:25 +0200

    better comments WIP

--- app/containers/IdeasShow/Comments/Comments.tsx
+++ app/containers/IdeasShow/Comments/Comments.tsx
@@ -11,7 +11,7 @@ import GetComments, { GetCommentsChildProps } from 'resources/GetComments';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  padding-bottom: 40px;
+  padding-bottom: 45px;
 `;
 
 interface InputProps {
