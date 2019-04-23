a2ca6770d515138aefe39bcf2ef172d4f5584c31
SaraBCL <38424075+SaraBCL@users.noreply.github.com>
2019-04-23 17:42:47 +0200

    adds e2e test for user deletion

---
+++ cypress/integration/profile-deletion.ts
@@ -0,0 +1,24 @@
+import { randomString, randomEmail } from '../support/commands';
+
+const firstName = randomString();
+const lastName = randomString();
+const peasantEmail = randomEmail();
+const peasantPassword = randomString();
+
+describe('Idea show page actions', () => {
+  before(() => {
+    cy.apiSignup(firstName, lastName, peasantEmail, peasantPassword);
+  });
+  beforeEach(() => {
+    cy.login(peasantEmail, peasantPassword);
+    cy.visit('/profile/edit');
+    cy.acceptCookies();
+  });
+  it('lets user delete their profile', () => {
+    cy.get('.e2e-delete-profile').find('button').click();
+    cy.wait(3000);
+    cy.get('.e2e-user-deleted-success-modal-content');
+    cy.get('.e2e-modal-close-button').click();
+    cy.get('.e2e-user-deleted-success-modal-content').should('not.exist');
+  });
+});
