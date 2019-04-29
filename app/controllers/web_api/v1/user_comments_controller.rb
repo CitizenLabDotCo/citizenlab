c3b7d57022b3340bb87acdca4c2b4f8e1610e2f9
Sébastien Hoorens <sebastien@citizenlab.co>
2019-04-29 16:44:19 +0200

    user comments paginated per idea

--- app/controllers/web_api/v1/user_comments_controller.rb
+++ app/controllers/web_api/v1/user_comments_controller.rb
@@ -3,8 +3,13 @@ class WebApi::V1::UserCommentsController < ApplicationController
   def index
     @comments = policy_scope(Comment)
       .where(author_id: params[:user_id])
+
+    @ideas = policy_scope(Idea)
+      .where(id: @comments)
       .page(params.dig(:page, :number))
       .per(params.dig(:page, :size))
+
+    @comments = @comments
       .includes(:idea)
       .left_outer_joins(:idea)
     @comments = @comments.order('ideas.published_at DESC, comments.created_at DESC')
