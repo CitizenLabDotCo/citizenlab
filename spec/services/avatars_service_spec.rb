04331367dfc759515d91d65bbd9edf90a13d1cdd
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2019-05-07 16:03:12 +0200

    CL2-3644 Added ability to request avatars for idea

--- spec/services/avatars_service_spec.rb
+++ spec/services/avatars_service_spec.rb
@@ -34,4 +34,51 @@ describe AvatarsService do
     end
   end
 
+  describe "avatars_for_idea" do
+
+    it "returns the idea and comments authors" do
+      idea1, idea2 = create_list(:idea, 2)
+      comment1, comment2 = create_list(:comment, 2, idea: idea1)
+      create(:comment, idea: idea2)
+
+      result = service.avatars_for_idea(idea1)
+
+      expect(result[:users].to_a).to match_array [idea1.author, comment1.author, comment2.author]
+      expect(result[:total_count]).to eq 3
+    end
+
+    it "does not include authors from deleted comments" do
+      idea = create(:idea)
+      comments = create(:comment, idea: idea, publication_status: 'deleted')
+
+      result = service.avatars_for_idea(idea)
+
+      expect(result[:users].to_a).to match_array [idea.author]
+      expect(result[:total_count]).to eq 1
+    end
+
+    it "does not return the voters" do
+      idea = create(:idea)
+      idea_vote = create(:vote, votable: idea)
+      comment = create(:comment, idea: idea)
+      comment_vote = create(:vote, votable: comment)
+
+      result = service.avatars_for_idea(idea)
+
+      expect(result[:users].to_a).to match_array [idea.author, comment.author]
+      expect(result[:total_count]).to eq 2
+    end
+
+    it "doesn't return the same user twice" do
+      u1 = create(:user)
+      idea = create(:idea, author: u1)
+      comments = create_list(:comment, 2, author: u1, idea: idea)
+
+      result = service.avatars_for_idea(idea)
+
+      expect(result[:users].to_a).to match_array [u1]
+      expect(result[:total_count]).to eq 1
+    end
   end
+
+end
