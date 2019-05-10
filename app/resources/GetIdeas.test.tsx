3c03c1a61be8bdef0080021ddc2ac74c522a8876
SaraBCL <38424075+SaraBCL@users.noreply.github.com>
2019-05-10 16:57:51 +0200

    Rollsback getIdeas test

--- app/resources/GetIdeas.test.tsx
+++ app/resources/GetIdeas.test.tsx
@@ -1,11 +1,9 @@
-jest.mock('services/ideas');
 // libraries
 import React from 'react';
 import { shallow } from 'enzyme';
-jest.useFakeTimers();
 
 // component to test
-import GetIdeas, { scheduler } from './GetIdeas';
+import GetIdeas from './GetIdeas';
 
 // mocks
 import * as ideasServices from 'services/ideas';
@@ -14,108 +12,91 @@ import * as ideasServices from 'services/ideas';
 const ideasStream = ideasServices.ideasStream as jest.Mock;
 const __setMockIdeas = ideasServices.__setMockIdeas;
 
+jest.mock('services/ideas');
 jest.mock('services/projects');
 
-describe('<GetIdeas />', () => {
+describe('<GetIdeas sort="new" />', () => {
 
   let child: jest.Mock;
 
   beforeEach(() => {
-    child = jest.fn(() => {});
+    child = jest.fn();
   });
 
-  // it('calls the ideasStream stream', () => {
-  //   const Wrapper = shallow(<GetIdeas>{child}</GetIdeas>);
-  //   scheduler.flush();
-  //   expect(ideasStream).toHaveBeenCalled();
-  // });
-  //
-  // it('calls the ideasStream stream whith the passed in parameters', () => {
-  //   shallow(<GetIdeas assignee="User_ID" feedbackNeeded={true}>{child}</GetIdeas>);
-  //   scheduler.flush();
-  //   expect(ideasStream.mock.calls[0][0].queryParameters.assignee).toEqual('User_ID');
-  //   expect(ideasStream.mock.calls[0][0].queryParameters.feedback_needed).toEqual(true);
-  //   expect(ideasStream.mock.calls[0][0].queryParameters.project_id).toEqual(undefined);
-  // });
-  //
-  // it('passes undefined to the child function initially', () => {
-  //   shallow(<GetIdeas>{child}</GetIdeas>);
-  //   scheduler.flush();
-  //   expect(child.mock.calls[0][0].ideasList).toBeUndefined;
-  // });
+  it('calls the ideasStream stream', () => {
+    shallow(<GetIdeas sort="new">{child}</GetIdeas>);
+    expect(ideasStream).toHaveBeenCalled();
+  });
+
+  it('calls the ideasStream stream whith the passed in parameters', () => {
+    shallow(<GetIdeas sort="new" assignee="User_ID" feedbackNeeded={true}>{child}</GetIdeas>);
+    expect(ideasStream.mock.calls[0][0].queryParameters.assignee).toEqual('User_ID');
+    expect(ideasStream.mock.calls[0][0].queryParameters.feedback_needed).toEqual(true);
+    expect(ideasStream.mock.calls[0][0].queryParameters.project_id).toEqual(undefined);
+  });
+
+  it('passes undefined to the child function initially', () => {
+    shallow(<GetIdeas sort="new">{child}</GetIdeas>);
+    expect(child.mock.calls[0][0].ideasList).toBeUndefined;
+  });
 
   it('passes the idea data to the child function received from the streams', () => {
     const mockIdeas = {
       data: ['Idea1', 'Idea2'].map(name => ideasServices.getIdea(name, name))
     };
-    console.log('before');
     __setMockIdeas(mockIdeas);
-    console.log('set');
-    const Wrapper = shallow(<GetIdeas>{child}</GetIdeas>);
-    console.log('shallow');
 
-    console.log(scheduler.active, scheduler.actions);
-    scheduler.now();
-    scheduler.flush();
-
-    console.log(child.mock.calls);
+    shallow(<GetIdeas sort="new">{child}</GetIdeas>);
     expect(child.mock.calls.find(arr => arr[0].ideasList !== undefined)[0].ideasList).toEqual(mockIdeas.data);
   });
 
-  // it('passes the idea data to the child function received from the streams', () => {
-  //   const mockIdeas = {
-  //     data: []
-  //   };
-  //   __setMockIdeas(mockIdeas);
-  //
-  //   shallow(<GetIdeas>{child}</GetIdeas>);
-  //   scheduler.flush();
-  //   expect(child.mock.calls.find(arr => arr[0].ideasList !== undefined)[0].ideasList).toEqual(mockIdeas.data);
-  // });
-  //
-  // it('passes null to the child function when it receives null from the streams', () => {
-  //   __setMockIdeas(null);
-  //   shallow(<GetIdeas>{child}</GetIdeas>);
-  //   scheduler.flush();
-  //   expect(child.mock.calls[1][0].ideasList).toBeNull;
-  // });
-  //
-  // it('passes undefined to the child function when it receives Error from the streams', () => {
-  //   const error = new Error();
-  //   __setMockIdeas(error);
-  //   shallow(<GetIdeas>{child}</GetIdeas>);
-  //   scheduler.flush();
-  //   expect(child.mock.calls[1][0].ideasList).toBeUndefined;
-  // });
-  //
-  // it('reacts to assignee filter change', () => {
-  //   const mockIdeas = {
-  //     data: []
-  //   };
-  //   __setMockIdeas(mockIdeas);
-  //   const component = shallow(<GetIdeas>{child}</GetIdeas>);
-  //   scheduler.flush();
-  //   expect(ideasStream.mock.calls[0][0].queryParameters.assignee).toEqual(undefined);
-  //   child.mock.calls[1][0].onChangeAssignee('User_ID');
-  //   scheduler.flush();
-  //
-  //   __setMockIdeas(null);
-  //   expect(ideasStream.mock.calls[1][0].queryParameters.assignee).toEqual('User_ID');
-  //   expect(child.mock.calls[2][0].ideasList).toBeNull;
-  // });
-  //
-  // it('reacts to page change', () => {
-  //   const mockIdeas = {
-  //     data: []
-  //   };
-  //   __setMockIdeas(mockIdeas);
-  //   const component = shallow(<GetIdeas>{child}</GetIdeas>);
-  //   scheduler.flush();
-  //   expect(ideasStream.mock.calls[0][0].queryParameters['page[number]']).toEqual(1);
-  //   child.mock.calls[1][0].onChangePage(2);
-  //   scheduler.flush();
-  //
-  //   __setMockIdeas(null);
-  //   expect(ideasStream.mock.calls[1][0].queryParameters['page[number]']).toEqual(2);
-  // });
+  it('passes the idea data to the child function received from the streams', () => {
+    const mockIdeas = {
+      data: []
+    };
+    __setMockIdeas(mockIdeas);
+
+    shallow(<GetIdeas sort="new">{child}</GetIdeas>);
+    expect(child.mock.calls.find(arr => arr[0].ideasList !== undefined)[0].ideasList).toEqual(mockIdeas.data);
+  });
+
+  it('passes null to the child function when it receives null from the streams', () => {
+    __setMockIdeas(null);
+    shallow(<GetIdeas sort="new">{child}</GetIdeas>);
+    expect(child.mock.calls[1][0].ideasList).toBeNull;
+  });
+
+  it('passes undefined to the child function when it receives Error from the streams', () => {
+    const error = new Error();
+    __setMockIdeas(error);
+    shallow(<GetIdeas sort="new">{child}</GetIdeas>);
+    expect(child.mock.calls[1][0].ideasList).toBeUndefined;
+  });
+
+  it('reacts to assignee filter change', () => {
+    const mockIdeas = {
+      data: []
+    };
+    __setMockIdeas(mockIdeas);
+    const component = shallow(<GetIdeas sort="new">{child}</GetIdeas>);
+    expect(ideasStream.mock.calls[0][0].queryParameters.assignee).toEqual(undefined);
+    child.mock.calls[1][0].onChangeAssignee('User_ID');
+
+    __setMockIdeas(null);
+    expect(ideasStream.mock.calls[1][0].queryParameters.assignee).toEqual('User_ID');
+    expect(child.mock.calls[2][0].ideasList).toBeNull;
+  });
+
+  it('reacts to page change', () => {
+    const mockIdeas = {
+      data: []
+    };
+    __setMockIdeas(mockIdeas);
+    const component = shallow(<GetIdeas sort="new">{child}</GetIdeas>);
+    expect(ideasStream.mock.calls[0][0].queryParameters['page[number]']).toEqual(1);
+    child.mock.calls[1][0].onChangePage(2);
+
+    __setMockIdeas(null);
+    expect(ideasStream.mock.calls[1][0].queryParameters['page[number]']).toEqual(2);
+  });
 });
