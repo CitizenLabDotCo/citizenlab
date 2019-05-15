81cd1f1da9fcba0891ab5b330a039bb6ecbd17dc
Sébastien Hoorens <sebastien@citizenlab.co>
2019-05-15 10:30:02 +0200

    execute ImageAssignmentJob in separate queue

--- app/jobs/image_assignment_job.rb
+++ app/jobs/image_assignment_job.rb
@@ -1,6 +1,6 @@
 class ImageAssignmentJob < ApplicationJob
   include SideFxHelper
-  queue_as :image_background
+  queue_as :image_assignment
 
   def perform model, image_assignments
     image_assignments.each do |field_name, field_value|
