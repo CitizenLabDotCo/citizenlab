f372f8a2e4fe69b70e07e338e53d4ef86e1b2dfb
SaraBCL <38424075+SaraBCL@users.noreply.github.com>
2019-05-16 14:44:51 +0200

    Fix assignee not always set to authUser on first load

--- app/components/admin/IdeaManager/components/TopLevelFilters/AssigneeFilter.tsx
+++ app/components/admin/IdeaManager/components/TopLevelFilters/AssigneeFilter.tsx
@@ -8,14 +8,13 @@ import { Dropdown } from 'semantic-ui-react';
 
 import GetUsers, { GetUsersChildProps } from 'resources/GetUsers';
 
-import { IOption } from 'typings';
-
 import { injectIntl } from 'utils/cl-intl';
 import { InjectedIntlProps } from 'react-intl';
 import messages from '../../messages';
 import GetAuthUser, { GetAuthUserChildProps } from 'resources/GetAuthUser';
 import { trackEventByName } from 'utils/analytics';
 import tracks from '../../tracks';
+import { memoize } from 'lodash-es';
 
 interface DataProps {
   prospectAssignees: GetUsersChildProps;
@@ -30,37 +29,23 @@ interface InputProps {
 
 interface Props extends InputProps, DataProps {}
 
-interface State {
-  assigneeOptions: IOption[];
-  prevPropsProspectAssignees: GetUsersChildProps | null;
-  prevPropsAuthUser: GetAuthUserChildProps;
-}
+interface State {}
 
 export class AssigneeFilter extends PureComponent<Props & InjectedIntlProps, State> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      assigneeOptions: [],
-      prevPropsProspectAssignees: null,
-      prevPropsAuthUser: null
-    };
-  }
+ componentDidMount() {
+   const { authUser, handleAssigneeFilterChange } = this.props;
+   !isNilOrError(authUser) && handleAssigneeFilterChange(authUser.id);
+ }
 
-  static getDerivedStateFromProps(props, state) {
-    const { prospectAssignees, authUser, intl: { formatMessage }, handleAssigneeFilterChange } = props;
-    const { prevPropsProspectAssignees, prevPropsAuthUser } = state;
-
-    if (authUser !== prevPropsAuthUser && !isNilOrError(authUser)) {
-      handleAssigneeFilterChange(authUser.id);
-    }
-
-    if (prospectAssignees !== prevPropsProspectAssignees || authUser !== prevPropsAuthUser) {
+  getAssigneeOptions = memoize(
+    (prospectAssignees, authUser) => {
+      const { intl: { formatMessage } } = this.props;
       let assigneeOptions;
 
       if (isNilOrError(prospectAssignees.usersList) || isNilOrError(authUser)) {
         assigneeOptions = [];
       } else {
-        const assigneeOptionsWithoutCurrentUser = prospectAssignees.usersList.filter(assignee => assignee.id !== authUser.id)
+        assigneeOptions = prospectAssignees.usersList.filter(assignee => assignee.id !== authUser.id)
           .map(assignee => ({
             value: assignee.id,
             text: formatMessage(messages.assignedTo, {
@@ -71,17 +56,13 @@ export class AssigneeFilter extends PureComponent<Props & InjectedIntlProps, Sta
 
         // Order of assignee filter options:
         // All ideas > Assigned to me > Unassigned > Assigned to X (other admins/mods)
-        assigneeOptions = assigneeOptionsWithoutCurrentUser;
         assigneeOptions.unshift({ value: 'unassigned', text: formatMessage(messages.unassignedIdeas), id: 'e2e-assignee-filter-unassigned' });
         assigneeOptions.unshift({ value: authUser.id, text: formatMessage(messages.assignedToMe), id: 'e2e-assignee-filter-assigned-to-user' });
         assigneeOptions.unshift({ value: 'all', text: formatMessage(messages.anyAssignment), id: 'e2e-assignee-filter-all-ideas' });
       }
-
-      return { assigneeOptions, prevPropsAuthUser: authUser, prevPropsProspectAssignees: prospectAssignees };
+      return assigneeOptions;
     }
-
-    return null;
-  }
+  );
 
   onAssigneeChange = (_event, assigneeOption) => {
     const realFiterParam = assigneeOption.value === 'all' ? undefined : assigneeOption.value;
@@ -93,13 +74,12 @@ export class AssigneeFilter extends PureComponent<Props & InjectedIntlProps, Sta
   }
 
   render() {
-    const { assigneeOptions } = this.state;
-    const { assignee } = this.props;
+    const { assignee, prospectAssignees, authUser } = this.props;
 
     return (
       <Dropdown
         id="e2e-idea-select-assignee-filter"
-        options={assigneeOptions}
+        options={this.getAssigneeOptions(prospectAssignees, authUser)}
         onChange={this.onAssigneeChange}
         value={assignee || 'all'}
         search
