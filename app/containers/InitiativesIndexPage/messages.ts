b7f36994ff84b73dc8aa746eab4bffc5ac39ca7e
SaraBCL <38424075+SaraBCL@users.noreply.github.com>
2019-05-20 19:19:07 +0200

    Intitiatives index page global layout

---
+++ app/containers/InitiativesIndexPage/messages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  metaTitle: {
+    id: 'app.containers.IdeasIndexPage.metaTitle',
+    defaultMessage: 'All Initiatives • {orgName}',
+  },
+  metaDescription: {
+    id: 'app.containers.IdeasIndexPage.metaDescription',
+    defaultMessage: 'TODO'
+  },
+});
