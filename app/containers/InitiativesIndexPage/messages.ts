5fff89f9ad3327d737c08795456346342bed255a
SaraBCL <38424075+SaraBCL@users.noreply.github.com>
2019-05-21 17:39:01 +0200

    fix intl

--- app/containers/InitiativesIndexPage/messages.ts
+++ app/containers/InitiativesIndexPage/messages.ts
@@ -2,11 +2,11 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   metaTitle: {
-    id: 'app.containers.IdeasIndexPage.metaTitle',
+    id: 'app.containers.InitiativesIndexPage.metaTitle',
     defaultMessage: 'All Initiatives • {orgName}',
   },
   metaDescription: {
-    id: 'app.containers.IdeasIndexPage.metaDescription',
-    defaultMessage: 'TODO'
+    id: 'app.containers.InitiativesIndexPage.metaDescription',
+    defaultMessage: 'Explore and support initiatives conducted in {orgName}'
   },
 });
