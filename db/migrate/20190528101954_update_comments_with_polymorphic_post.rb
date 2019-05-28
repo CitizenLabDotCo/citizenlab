94874d16eeda978b7226f21e5d93208df5e6f902
Sébastien Hoorens <sebastien@citizenlab.co>
2019-05-28 15:12:17 +0200

    try gain

--- db/migrate/20190528101954_update_comments_with_polymorphic_post.rb
+++ db/migrate/20190528101954_update_comments_with_polymorphic_post.rb
@@ -4,8 +4,8 @@ class UpdateCommentsWithPolymorphicPost < ActiveRecord::Migration[5.2]
     remove_foreign_key :ideas, column: :post_id
     add_column :comments, :post_type, :string
 
+    add_index :comments, [:post_id, :post_type]
+
     Comment.update_all(post_type: 'Idea')
   end
-
-  add_index :comments, [:post_id, :post_type]
 end
