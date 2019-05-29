a3fb159e5bcde21d53b880e18b5d3096e2a80b11
Sébastien Hoorens <sebastien@citizenlab.co>
2019-05-29 16:52:39 +0200

    fixed some comment factory with idea props...

--- spec/lib/smart_group_rules/participated_in_topic_spec.rb
+++ spec/lib/smart_group_rules/participated_in_topic_spec.rb
@@ -35,7 +35,7 @@ describe SmartGroupRules::ParticipatedInTopic do
       @user4 = create(:user)
       @idea1 = create(:idea, topics: [@topic1], author: @user1)
       @vote = create(:vote, votable: @idea1, user: @user2)
-      @comment = create(:comment, idea: @idea1, author: @user3)
+      @comment = create(:comment, post: @idea1, author: @user3)
       @idea2 = create(:idea, topics: [@topic2], author: @user3)
 
     end
