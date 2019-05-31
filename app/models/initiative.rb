cdc4798f7642764e449081357bef73ac8550821e
Sébastien Hoorens <sebastien@citizenlab.co>
2019-05-31 15:59:51 +0200

    minor

--- app/models/initiative.rb
+++ app/models/initiative.rb
@@ -1,4 +1,7 @@
 class Initiative < ApplicationRecord
   include Post
 
+  has_many :idea_images, -> { order(:ordering) }, dependent: :destroy
+  has_many :idea_files, -> { order(:ordering) }, dependent: :destroy
+
 end
