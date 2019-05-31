3d429d7f609d19b627ab697bf70f0d0ef0ee61ff
Sébastien Hoorens <sebastien@citizenlab.co>
2019-05-31 10:49:46 +0200

    machine translation policy fix

--- engines/machine_translations/app/policies/machine_translations/machine_translation_policy.rb
+++ engines/machine_translations/app/policies/machine_translations/machine_translation_policy.rb
@@ -2,7 +2,12 @@ module MachineTranslations
   class MachineTranslationPolicy < ApplicationPolicy
 
     def show?
-      "#{record.translatable_type}Policy".constantize.new(user, record.translatable).show?
+      translatable_policy = case record.translatable_type
+      when 'Idea' then IdeaPolicy
+      when 'Comment' then "#{record.translatable.post_type}CommentPolicy".constantize
+      else raise "#{record.translatable_type} has no voting policy defined"
+      end
+      translatable_policy.new(user, record.translatable).show?
     end
 
   end
