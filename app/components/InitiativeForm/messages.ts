8079266d4004a0e2c7c615d59af145b3662e073f
SaraBCL <38424075+SaraBCL@users.noreply.github.com>
2019-06-06 18:18:02 +0200

    Start work on form styling, title displayed

---
+++ app/components/InitiativeForm/messages.ts
@@ -0,0 +1,109 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  formGeneralSectionTitle: {
+    id: 'app.components.InitiativeForm.formGeneralSectionTitle',
+    defaultMessage: 'What is your initiative ?',
+  },
+  titleEmptyError: {
+    id: 'app.components.InitiativeForm.titleEmptyError',
+    defaultMessage: 'Please provide a title',
+  },
+  titleLengthError: {
+    id: 'app.components.InitiativeForm.titleLengthError',
+    defaultMessage: 'The initiative title must be at least 10 characters long',
+  },
+  descriptionEmptyError: {
+    id: 'app.components.InitiativeForm.descriptionEmptyError',
+    defaultMessage: 'Please provide a description',
+  },
+  descriptionLengthError: {
+    id: 'app.components.InitiativeForm.descriptionLengthError',
+    defaultMessage: 'The initiative description must be at least 500 characters long',
+  },
+  titleLabel: {
+    id: 'app.components.InitiativeForm.titleLabel',
+    defaultMessage: 'Title',
+  },
+  titleLabelSubtext: {
+    id: 'app.components.InitiativeForm.titleLabelSubtext',
+    defaultMessage: 'Choose a title that describes your initiative in a clear and attractive way',
+  },
+  descriptionLabel: {
+    id: 'app.components.InitiativeForm.descriptionLabel',
+    defaultMessage: 'What is your initiative about ?',
+  },
+  descriptionLabelSubtext: {
+    id: 'app.components.InitiativeForm.descriptionLabelSubtext',
+    defaultMessage: 'Give others a good understanding of your initiative (min. 500 characters)',
+  },
+  formDetailsSectionTitle: {
+    id: 'app.components.InitiativeForm.formDetailsSectionTitle',
+    defaultMessage: 'Details',
+  },
+  topicEmptyError: {
+    id: 'app.components.InitiativeForm.topicEmptyError',
+    defaultMessage: 'Please provide a topic',
+  },
+  topicsLabel: {
+    id: 'app.components.InitiativeForm.topicsLabel',
+    defaultMessage: 'Select the topic of your iniative',
+  },
+  topicsLabelSubtext: {
+    id: 'app.components.InitiativeForm.topicsLabelSubtext',
+    defaultMessage: 'You can select up to 2 topics',
+  },
+  projectsLabel: {
+    id: 'app.components.InitiativeForm.projectsLabel',
+    defaultMessage: 'Project (optional)',
+  },
+  projectsPlaceholder: {
+    id: 'app.components.InitiativeForm.projectsPlaceholder',
+    defaultMessage: 'Select a project to which your initiative belongs',
+  },
+  locationLabel: {
+    id: 'app.components.InitiativeForm.locationLabel',
+    defaultMessage: 'Select a Location',
+  },
+  locationLabelSubtext: {
+    id: 'app.components.InitiativeForm.locationLabelSubtext',
+    defaultMessage: 'Where is your initiative located ?',
+  },
+  locationPlaceholder: {
+    id: 'app.components.InitiativeForm.locationPlaceholder',
+    defaultMessage: 'Type an address',
+  },
+  // TODO
+  // imageUploadLabel: {
+  //   id: 'app.components.InitiativeForm.imageUploadLabel',
+  //   defaultMessage: 'Image (optional)',
+  // },
+  // imageUploadPlaceholder: {
+  //   id: 'app.components.InitiativeForm.imageUploadPlaceholder',
+  //   defaultMessage: 'Select an image for your initiative.<br /> This will increase the visibility of your initiative!',
+  // },
+  // imageDropzonePlaceholder: {
+  //   id: 'app.container.InitiativeForm.imageDropzonePlaceholder',
+  //   defaultMessage: 'Drop your image here',
+  // },
+  // budgetLabel: {
+  //   id: 'app.container.InitiativeForm.budgetLabel',
+  //   defaultMessage: 'Budget (in {currency}, max. {maxBudget})',
+  // },
+  // noBudgetError: {
+  //   id: 'app.container.InitiativeForm.noBudgetError',
+  //   defaultMessage: 'Please provide a budget',
+  // },
+  // budgetIsZeroError: {
+  //   id: 'app.container.InitiativeForm.budgetIsZeroError',
+  //   defaultMessage: 'Please provide a budget larger than 0',
+  // },
+  // budgetIsTooBig: {
+  //   id: 'app.container.InitiativeForm.budgetIsTooBig',
+  //   defaultMessage: `The provided budget is too big`,
+  // },
+  // fileUploadLabel: {
+  //   id: 'app.container.InitiativeForm.fileUploadLabel',
+  //   defaultMessage: 'Add files to your initiative',
+  // }
+});
