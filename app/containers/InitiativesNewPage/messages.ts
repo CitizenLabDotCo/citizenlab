91fb4de90a504a181f16c91a586fa3dd2022d7af
SaraBCL <38424075+SaraBCL@users.noreply.github.com>
2019-06-06 15:18:19 +0200

    Add route and basic layout for initiatives new

---
+++ app/containers/InitiativesNewPage/messages.ts
@@ -0,0 +1,44 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  metaTitle: {
+    id: 'app.containers.InitiativesNewPage.metaTitle',
+    defaultMessage: 'Start an Initiative • {orgName}',
+  },
+  metaDescription: {
+    id: 'app.containers.InitiativesNewPage.metaDescription',
+    defaultMessage: 'Start your own initiative and make your voice heard by '
+  },
+  header: {
+    id: 'app.containers.InitiativesNewPage.header',
+    defaultMessage: 'Start your own initiative and make your voice heard by {styledOrgName}'
+  },
+  orgName: {
+    id: 'app.containers.InitiativesNewPage.orgName',
+    defaultMessage: '{orgName}'
+  },
+  tipsTitle: {
+    id: 'app.containers.InitiativesNewPage.tipsTitle',
+    defaultMessage: 'Tips and tricks for a successful initiative'
+  },
+  tipsExplanation: {
+    id: 'app.containers.InitiativesNewPage.tipsExplanation',
+    defaultMessage: 'An initiative is a project that you, as a citizen, can start anytime. Posting it on the platform enables you to gather support from others and, at the end, being heard by Vancouver.'
+  },
+  requirmentsListTitle: {
+    id: 'app.containers.InitiativesNewPage.requirmentsListTitle',
+    defaultMessage: 'To get there, you need to:'
+  },
+  requirmentVoteTreshold: {
+    id: 'app.containers.InitiativesNewPage.requirmentVoteTreshold',
+    defaultMessage: 'Gather {voteThreshold} votes'
+  },
+  requirmentDaysLimit: {
+    id: 'app.containers.InitiativesNewPage.requirmentDaysLimit',
+    defaultMessage: 'Within {daysLimit} days'
+  },
+  eligibility: {
+    id: 'app.containers.InitiativesNewPage.eligibility',
+    defaultMessage: 'To be eligible, Vancouver requires your initiative to respect the following:'
+  },
+});
