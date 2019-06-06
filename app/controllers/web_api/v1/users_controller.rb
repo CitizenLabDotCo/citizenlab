f5520e7de5095519c53c89ed8d7618da6a61e6ed
Sébastien Hoorens <sebastien@citizenlab.co>
2019-06-06 18:22:35 +0200

    updated users comments and post counts

--- app/controllers/web_api/v1/users_controller.rb
+++ app/controllers/web_api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class WebApi::V1::UsersController < ::ApplicationController
 
   # before_action :authenticate_user, except: [:create]
-  before_action :set_user, only: [:show, :update, :destroy, :ideas_count, :comments_count]
+  before_action :set_user, only: [:show, :update, :destroy, :ideas_count, :initiatives_count, :comments_count]
   skip_after_action :verify_authorized, only: [:index_xlsx]
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -151,8 +151,26 @@ class WebApi::V1::UsersController < ::ApplicationController
     render json: {count: policy_scope(@user.ideas.published).count}, status: :ok
   end
 
+  def initiatives_count
+    render json: {count: policy_scope(@user.initiatives.published).count}, status: :ok
+  end
+
   def comments_count
-    render json: {count: policy_scope(@user.comments.where(post_type: 'Idea').published, policy_scope_class: IdeaCommentPolicy::Scope).count}, status: :ok
+    count = 0
+    published_comments = @user.comments.published
+    if !params[:post_type] || params[:post_type] == 'Idea'
+      count += policy_scope(
+        published_comments.where(post_type: 'Idea'), 
+        policy_scope_class: IdeaCommentPolicy::Scope
+        ).count
+    end
+    if !params[:post_type] || params[:post_type] == 'Initiative'
+      count += policy_scope(
+        published_comments.where(post_type: 'Initiative'), 
+        policy_scope_class: InitiativeCommentPolicy::Scope
+        ).count
+    end
+    render json: {count: count}, status: :ok
   end
 
   private
