873164eefc7358160351ce460280419ea22116ea
David <david@citizenlab.co>
2019-06-07 10:44:24 +0200

    fix for extract-intl error

--- app/containers/IdeasShowPage/messages.ts
+++ app/containers/IdeasShowPage/messages.ts
@@ -2,15 +2,15 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   noIdeaFoundHere: {
-    id: 'app.components.IdeasShow.noIdeaFoundHere',
+    id: 'app.components.IdeasShowPage.noIdeaFoundHere',
     defaultMessage: 'No idea was found here.',
   },
   goBack: {
-    id: 'app.components.IdeasShow.goBack',
+    id: 'app.components.IdeasShowPage.goBack',
     defaultMessage: 'Go back',
   },
   goBackToList: {
-    id: 'app.components.IdeasShow.goBackToList',
+    id: 'app.components.IdeasShowPage.goBackToList',
     defaultMessage: 'Go back to the list',
   },
 });
