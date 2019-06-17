1429e2a0f705218728c9d649fe5e1c09db9f9af0
David <david@citizenlab.co>
2019-06-17 14:53:03 +0200

    fixed e2e test

--- internals/webpack/webpack.config.js
+++ internals/webpack/webpack.config.js
@@ -8,7 +8,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ResourceHintsWebpackPlugin = require('resource-hints-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const SentryCliPlugin = require('@sentry/webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const argv = require('yargs').argv;
 const API_HOST = process.env.API_HOST || 'localhost';
 const API_PORT = process.env.API_PORT || 4000;
@@ -30,8 +30,6 @@ const config = {
     },
   },
 
-  stats: 'normal',
-
   mode: isDev ? 'development' : 'production',
 
   devtool: isDev ? 'cheap-module-eval-source-map' : (isProd ? 'source-map' : false),
@@ -40,7 +38,6 @@ const config = {
     contentBase: path.join(process.cwd(), 'build'),
     port: 3000,
     host: '0.0.0.0',
-    stats: 'normal',
     disableHostCheck: true,
     historyApiFallback: true,
     proxy: {
@@ -148,13 +145,13 @@ const config = {
       chunkFilename: '[name].[hash].chunk.css'
     }),
 
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'disabled',
-      generateStatsFile: true,
-      statsOptions: {
-        source: false
-      }
-    })
+    // new BundleAnalyzerPlugin({
+    //   analyzerMode: 'disabled',
+    //   generateStatsFile: true,
+    //   statsOptions: {
+    //     source: false
+    //   }
+    // })
   ],
 
   resolve: {
