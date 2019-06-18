8a6c11369f4fe997b045e42717d8b46d082c0eb5
David <david@citizenlab.co>
2019-06-18 16:32:01 +0200

    tweaked webpack splitChunks

--- internals/webpack/webpack.config.js
+++ internals/webpack/webpack.config.js
@@ -24,197 +24,29 @@ const config = {
     chunkFilename: isDev ? '[name].chunk.js' : '[name].[contenthash].chunk.js'
   },
 
-  // optimized 6
-	// optimization: {
-	// 	splitChunks: {
-	// 		cacheGroups: {
-	// 			commons: {
-	// 				test: /[\\/]node_modules[\\/]/,
-	// 				name: 'vendors',
-	// 				chunks: 'all'
-	// 			}
-	// 		}
-	// 	}
-	// },
-
-  // optimized 5
-  // optimization: {
-  //   splitChunks: {
-  //     chunks: 'all',
-  //   },
-  // },
-
-  // optimized 4
-  // optimization: {
-  //   splitChunks: {
-  //     chunks: 'all',
-  //     minSize: 30000,
-  //     maxSize: 250000,
-  //     minChunks: 1,
-  //     maxAsyncRequests: 5,
-  //     maxInitialRequests: 3,
-  //     automaticNameDelimiter: '~',
-  //     name: true,
-  //     cacheGroups: {
-  //       vendors: {
-  //         test: /[\\/]node_modules[\\/]/,
-  //         priority: -10
-  //       },
-  //       default: {
-  //         minChunks: 2,
-  //         priority: -20,
-  //         reuseExistingChunk: true
-  //       }
-  //     }
-  //   }
-  // },
-
-  // optimized 3
-  // optimization: {
-  //   runtimeChunk: true,
-  //   moduleIds: 'hashed',
-  //   splitChunks: {
-  //     hidePathInfo: true, // prevents the path from being used in the filename when using maxSize
-  //     chunks: 'initial', // default is async, set to initial and then use async inside cacheGroups instead
-  //     maxInitialRequests: Infinity, // Default is 3, make this unlimited if using HTTP/2
-  //     maxAsyncRequests: Infinity, // Default is 5, make this unlimited if using HTTP/2
-  //     // sizes are compared against source before minification
-  //     minSize: 10000, // chunk is only created if it would be bigger than minSize
-  //     maxSize: 40000, // splits chunks if bigger than 40k, added in webpack v4.15
-  //     cacheGroups: { // create separate js files for bluebird, jQuery, bootstrap, aurelia and one for the remaining node modules
-  //       default: false, // disable the built-in groups, default & vendors (vendors is overwritten below)
-
-  //       // generic 'initial/sync' vendor node module splits: separates out larger modules
-  //       vendorSplit: { // each node module as separate chunk file if module is bigger than minSize
-  //         test: /[\\/]node_modules[\\/]/,
-  //         name(module) {
-  //           // Extract the name of the package from the path segment after node_modules
-  //           const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-  //           return `vendor.${packageName.replace('@', '')}`;
-  //         },
-  //         priority: 20
-  //       },
-  //       vendors: { // picks up everything else being used from node_modules that is less than minSize
-  //         test: /[\\/]node_modules[\\/]/,
-  //         name: 'vendors',
-  //         priority: 19,
-  //         enforce: true // create chunk regardless of the size of the chunk
-  //       },
-
-  //       // generic 'async' vendor node module splits: separates out larger modules
-  //       vendorAsyncSplit: { // vendor async chunks, create each asynchronously used node module as separate chunk file if module is bigger than minSize
-  //         test: /[\\/]node_modules[\\/]/,
-  //         name(module) {
-  //           const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-  //           return `vendor.async.${packageName.replace('@', '')}`;
-  //         },
-  //         chunks: 'async',
-  //         priority: 10,
-  //         reuseExistingChunk: true,
-  //         minSize: 5000 // only create if 5k or larger
-  //       },
-  //       vendorsAsync: { // vendors async chunk, remaining asynchronously used node modules as single chunk file
-  //         test: /[\\/]node_modules[\\/]/,
-  //         name: 'vendors.async',
-  //         chunks: 'async',
-  //         priority: 9,
-  //         reuseExistingChunk: true,
-  //         enforce: true // create chunk regardless of the size of the chunk
-  //       },
-
-  //       // generic 'async' common module splits: separates out larger modules
-  //       commonAsync: { // common async chunks, each asynchronously used module as a separate chunk files
-  //         name(module) {
-  //           // Extract the name of the module from last path component. 'src/modulename/' results in 'modulename'
-  //           const moduleName = module.context.match(/[^\\/]+(?=\/$|$)/)[0];
-  //           return `common.async.${moduleName.replace('@', '')}`;
-  //         },
-  //         minChunks: 2, // Minimum number of chunks that must share a module before splitting
-  //         chunks: 'async',
-  //         priority: 1,
-  //         reuseExistingChunk: true,
-  //         minSize: 5000 // only create if 5k or larger
-  //       },
-  //       commonsAsync: { // commons async chunk, remaining asynchronously used modules as single chunk file
-  //         name: 'commons.async',
-  //         minChunks: 2, // Minimum number of chunks that must share a module before splitting
-  //         chunks: 'async',
-  //         priority: 0,
-  //         reuseExistingChunk: true,
-  //         enforce: true // create chunk regardless of the size of the chunk
-  //       }
-  //     }
-  //   }
-  // },
-
-  // optimized 2
-  // optimization: {
-  //   runtimeChunk: 'single',
-  //   splitChunks: {
-  //     chunks: 'all',
-  //     cacheGroups: {
-  //       default: {
-  //         enforce: true,
-  //         priority: 1
-  //       },
-  //       vendors: {
-  //         test: /[\\/]node_modules[\\/]/,
-  //         priority: 2,
-  //         name: 'vendors',
-  //         enforce: true,
-  //         chunks: 'all'
-  //       }
-  //     }
-  //   }
-  // },
-
-  // optimized 1
-  // optimization: {
-  //   runtimeChunk: 'single',
-  //   splitChunks: {
-  //     chunks: 'all',
-  //     maxInitialRequests: Infinity,
-  //     minSize: 0,
-  //     cacheGroups: {
-  //       vendor: {
-  //         test: /[\\/]node_modules[\\/]/,
-  //         name(module) {
-  //           // get the name. E.g. node_modules/packageName/not/this/part.js
-  //           // or node_modules/packageName
-  //           const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-
-  //           // npm package names are URL-safe, but some servers don't like @ symbols
-  //           return `npm.${packageName.replace('@', '')}`;
-  //         },
-  //       },
-  //     },
-  //   },
-  // },
-
-  // webpack default
-  // optimization: {
-  //   splitChunks: {
-  //     chunks: 'async',
-  //     minSize: 30000,
-  //     maxSize: 0,
-  //     minChunks: 1,
-  //     maxAsyncRequests: 5,
-  //     maxInitialRequests: 3,
-  //     automaticNameDelimiter: '~',
-  //     name: true,
-  //     cacheGroups: {
-  //       vendors: {
-  //         test: /[\\/]node_modules[\\/]/,
-  //         priority: -10
-  //       },
-  //       default: {
-  //         minChunks: 2,
-  //         priority: -20,
-  //         reuseExistingChunk: true
-  //       }
-  //     }
-  //   }
-  // },
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 7,
+      maxInitialRequests: 5,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  },
 
   mode: isDev ? 'development' : 'production',
 
