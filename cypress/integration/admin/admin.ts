af93e509c2b083f13117239d4af00cb4a3860c28
SaraBCL <38424075+SaraBCL@users.noreply.github.com>
2019-06-18 17:25:55 +0200

    Tests main admin route

---
+++ cypress/integration/admin/admin.ts
@@ -0,0 +1,14 @@
+describe('/admin route', () => {
+  it('redirects unauthenticated users to signin', () => {
+    cy.visit('/admin');
+    cy.wait(500);
+    cy.location('pathname').should('eq', '/en-GB/sign-in/');
+  });
+
+  it('redirects admins to dashboard', () => {
+    cy.login('admin@citizenlab.co', 'testtest');
+    cy.visit('/admin');
+    cy.wait(500);
+    cy.location('pathname').should('eq', '/en-GB/admin/dashboard/');
+  });
+});
