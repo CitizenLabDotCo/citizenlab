799f2158cb56cdde2183f8ca6bcb138bb80d1eb1
David <david@citizenlab.co>
2019-06-19 14:55:45 +0200

    tweaked webpack splitChunks

--- internals/webpack/webpack.config.js
+++ internals/webpack/webpack.config.js
@@ -44,30 +44,30 @@ const config = {
     },
   },
 
-  optimization: {
-    runtimeChunk: 'single',
-    splitChunks: {
-      chunks: 'all',
-      maxInitialRequests: 5,
-      maxAsyncRequests: 7,
-      minSize: 30000,
-      maxSize: 250000,
-      minChunks: 2,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            // get the name. E.g. node_modules/packageName/not/this/part.js
-            // or node_modules/packageName
-            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+  // optimization: {
+  //   runtimeChunk: 'single',
+  //   splitChunks: {
+  //     chunks: 'all',
+  //     maxInitialRequests: 5,
+  //     maxAsyncRequests: 7,
+  //     minSize: 30000,
+  //     maxSize: 250000,
+  //     minChunks: 2,
+  //     cacheGroups: {
+  //       vendor: {
+  //         test: /[\\/]node_modules[\\/]/,
+  //         name(module) {
+  //           // get the name. E.g. node_modules/packageName/not/this/part.js
+  //           // or node_modules/packageName
+  //           const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
 
-            // npm package names are URL-safe, but some servers don't like @ symbols
-            return `npm.${packageName.replace('@', '')}`;
-          },
-        },
-      },
-    },
-  },
+  //           // npm package names are URL-safe, but some servers don't like @ symbols
+  //           return `npm.${packageName.replace('@', '')}`;
+  //         },
+  //       },
+  //     },
+  //   },
+  // },
 
   module: {
     rules: [
