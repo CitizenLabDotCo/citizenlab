b28a19b79ea799bec885235696b856aae6fb753c
SaraBCL <38424075+SaraBCL@users.noreply.github.com>
2019-06-19 10:55:54 +0200

    Minimal testing of summary dashboard

--- app/containers/Admin/dashboard/components/TimeControl.tsx
+++ app/containers/Admin/dashboard/components/TimeControl.tsx
@@ -147,6 +147,7 @@ class TimeControl extends PureComponent<Props & InjectedIntlProps, State> {
           <StyledButton
             style="text"
             onClick={this.toggleDropdown}
+            className="e2e-open-time-presets"
           >
             {activePreset ? activePreset.label : <FormattedMessage {...messages.customDateRange} />}
             <DropdownItemIcon name="dropdown" />
@@ -156,6 +157,7 @@ class TimeControl extends PureComponent<Props & InjectedIntlProps, State> {
             top="45px"
             opened={dropdownOpened}
             onClickOutside={this.toggleDropdown}
+            className="e2e-preset-items"
             content={
               <>
                 {this.presets.map(preset => (
