687da9b1d1d7a07b3ddb4121e702cb7db4bc375c
Brent Guffens <brentguf@users.noreply.github.com>
2019-06-20 13:50:24 +0200

    Remove TTF fonts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,75 +1,51 @@
-const larsseitThinTTF = require('./assets/fonts/larsseit-thin.ttf');
 const larsseitThinWOFF = require('./assets/fonts/larsseit-thin.woff');
 const larsseitThinWOFF2 = require('./assets/fonts/larsseit-thin.woff2');
-const larsseitThinItalicTTF = require('./assets/fonts/larsseit-thinitalic.ttf');
 const larsseitThinItalicWOFF = require('./assets/fonts/larsseit-thinitalic.woff');
 const larsseitThinItalicWOFF2 = require('./assets/fonts/larsseit-thinitalic.woff2');
-const larsseitLightTTF = require('./assets/fonts/larsseit-light.ttf');
 const larsseitLightWOFF = require('./assets/fonts/larsseit-light.woff');
 const larsseitLightWOFF2 = require('./assets/fonts/larsseit-light.woff2');
-const larsseitLightItalicTTF = require('./assets/fonts/larsseit-lightitalic.ttf');
 const larsseitLightItalicWOFF = require('./assets/fonts/larsseit-lightitalic.woff');
 const larsseitLightItalicWOFF2 = require('./assets/fonts/larsseit-lightitalic.woff2');
-const larsseitRegularTTF = require('./assets/fonts/larsseit.ttf');
 const larsseitRegularWOFF = require('./assets/fonts/larsseit.woff');
 const larsseitRegularWOFF2 = require('./assets/fonts/larsseit.woff2');
-const larsseitRegularItalicTTF = require('./assets/fonts/larsseit-italic.ttf');
 const larsseitRegularItalicWOFF = require('./assets/fonts/larsseit-italic.woff');
 const larsseitRegularItalicWOFF2 = require('./assets/fonts/larsseit-italic.woff2');
-const larsseitMediumTTF = require('./assets/fonts/larsseit-medium.ttf');
 const larsseitMediumWOFF = require('./assets/fonts/larsseit-medium.woff');
 const larsseitMediumWOFF2 = require('./assets/fonts/larsseit-medium.woff2');
-const larsseitMediumItalicTTF = require('./assets/fonts/larsseit-mediumitalic.ttf');
 const larsseitMediumItalicWOFF = require('./assets/fonts/larsseit-mediumitalic.woff');
 const larsseitMediumItalicWOFF2 = require('./assets/fonts/larsseit-mediumitalic.woff2');
-const larsseitBoldTTF = require('./assets/fonts/larsseit-bold.ttf');
 const larsseitBoldWOFF = require('./assets/fonts/larsseit-bold.woff');
 const larsseitBoldWOFF2 = require('./assets/fonts/larsseit-bold.woff2');
-const larsseitBoldItalicTTF = require('./assets/fonts/larsseit-bolditalic.ttf');
 const larsseitBoldItalicWOFF = require('./assets/fonts/larsseit-bolditalic.woff');
 const larsseitBoldItalicWOFF2 = require('./assets/fonts/larsseit-bolditalic.woff2');
-const larsseitExtraBoldTTF = require('./assets/fonts/larsseit-extrabold.ttf');
 const larsseitExtraBoldWOFF = require('./assets/fonts/larsseit-extrabold.woff');
 const larsseitExtraBoldWOFF2 = require('./assets/fonts/larsseit-extrabold.woff2');
-const larsseitExtraBoldItalicTTF = require('./assets/fonts/larsseit-extrabolditalic.ttf');
 const larsseitExtraBoldItalicWOFF = require('./assets/fonts/larsseit-extrabolditalic.woff');
 const larsseitExtraBoldItalicWOFF2 = require('./assets/fonts/larsseit-extrabolditalic.woff2');
 
 export {
-  larsseitThinTTF,
   larsseitThinWOFF,
   larsseitThinWOFF2,
-  larsseitThinItalicTTF,
   larsseitThinItalicWOFF,
   larsseitThinItalicWOFF2,
-  larsseitLightTTF,
   larsseitLightWOFF,
   larsseitLightWOFF2,
-  larsseitLightItalicTTF,
   larsseitLightItalicWOFF,
   larsseitLightItalicWOFF2,
-  larsseitRegularTTF,
   larsseitRegularWOFF,
   larsseitRegularWOFF2,
-  larsseitRegularItalicTTF,
   larsseitRegularItalicWOFF,
   larsseitRegularItalicWOFF2,
-  larsseitMediumTTF,
   larsseitMediumWOFF,
   larsseitMediumWOFF2,
-  larsseitMediumItalicTTF,
   larsseitMediumItalicWOFF,
   larsseitMediumItalicWOFF2,
-  larsseitBoldTTF,
   larsseitBoldWOFF,
   larsseitBoldWOFF2,
-  larsseitBoldItalicTTF,
   larsseitBoldItalicWOFF,
   larsseitBoldItalicWOFF2,
-  larsseitExtraBoldTTF,
   larsseitExtraBoldWOFF,
   larsseitExtraBoldWOFF2,
-  larsseitExtraBoldItalicTTF,
   larsseitExtraBoldItalicWOFF,
   larsseitExtraBoldItalicWOFF2
 };
