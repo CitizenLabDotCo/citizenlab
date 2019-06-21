e841f0a29702643ec24559344a12d208665c7da1
Sébastien Hoorens <sebastien@citizenlab.co>
2019-06-21 15:17:35 +0200

    fast clustering serializer

---
+++ app/serializers/web_api/v1/fast/clustering_serializer.rb
@@ -0,0 +1,3 @@
+class WebApi::V1::Fast::ClusteringSerializer < WebApi::V1::Fast::BaseSerializer
+  attributes :title_multiloc, :structure, :created_at, :updated_at
+end
---
+++ app/serializers/web_api/v1/fast/custom_field_option_serializer.rb
@@ -0,0 +1,3 @@
+class WebApi::V1::Fast::CustomFieldOptionSerializer < WebApi::V1::Fast::BaseSerializer
+  attributes :key, :title_multiloc, :ordering, :created_at, :updated_at
+end
