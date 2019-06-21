53a4a6b820a9b3cbd1f1bd85b02c54bb2bbd8cb4
Sébastien Hoorens <sebastien@citizenlab.co>
2019-06-21 15:11:09 +0200

    fast basket serializer

--- app/serializers/web_api/v1/fast/basket_serializer.rb
+++ app/serializers/web_api/v1/fast/basket_serializer.rb
@@ -1,12 +1,11 @@
 class WebApi::V1::Fast::BasketSerializer < WebApi::V1::Fast::BaseSerializer
-  attributes, :submitted_at, :total_budget, :budget_exceeds_limit?
+  attributes :submitted_at, :total_budget, :budget_exceeds_limit?
 
   belongs_to :participation_context, polymorphic: true
   belongs_to :user
-  has_many :baskets_ideas
 
 
-  has_many :ideas do |order|
+  has_many :ideas do |object|
     object.ideas.order('baskets_ideas.created_at DESC')
   end
 
