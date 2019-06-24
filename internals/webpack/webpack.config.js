4736920011cee97fdc4ca5e434684c1c86d09274
DavidCnoops <david@citizenlab.co>
2019-06-24 16:54:39 +0200

    changed webpack splitChunks config

--- internals/webpack/webpack.config.js
+++ internals/webpack/webpack.config.js
@@ -29,31 +29,10 @@ const config = {
   },
 
   optimization: {
-    runtimeChunk: 'single',
     splitChunks: {
       chunks: 'all',
     },
   },
-
-// 	optimization: {
-// 		splitChunks: {
-// 			cacheGroups: {
-// 				commons: {
-// 					chunks: "initial",
-// 					minChunks: 2,
-// 					maxInitialRequests: 5,
-// 					minSize: 0
-// 				},
-// 				vendor: {
-// 					test: /node_modules/,
-// 					chunks: "initial",
-// 					name: "vendor",
-// 					priority: 10,
-// 					enforce: true
-// 				}
-// 			}
-// 		}
-// 	},
 
   mode: isDev ? 'development' : 'production',
 
