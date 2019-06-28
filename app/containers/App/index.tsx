ae9542a75df611d882445680b6bb2559f3dcde99
David <david@citizenlab.co>
2019-06-28 14:31:09 +0200

    fixed linter errors

--- app/containers/App/index.tsx
+++ app/containers/App/index.tsx
@@ -211,30 +211,78 @@ class App extends PureComponent<Props & WithRouterProps, State> {
   loadFonts = async () => {
     try {
       const fontModules = await Promise.all([
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-thin.woff'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-thin.woff2'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-thinitalic.woff'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-thinitalic.woff2'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-light.woff'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-light.woff2'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-lightitalic.woff'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-lightitalic.woff2'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit.woff'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit.woff2'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-italic.woff'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-italic.woff2'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-medium.woff'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-medium.woff2'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-mediumitalic.woff'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-mediumitalic.woff2'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-bold.woff'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-bold.woff2'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-bolditalic.woff'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-bolditalic.woff2'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-extrabold.woff'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-extrabold.woff2'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-extrabolditalic.woff'),
-        import(/* webpackPreload: true */ 'assets/fonts/larsseit-extrabolditalic.woff2')
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-thin.woff'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-thin.woff2'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-thinitalic.woff'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-thinitalic.woff2'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-light.woff'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-light.woff2'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-lightitalic.woff'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-lightitalic.woff2'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit.woff'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit.woff2'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-italic.woff'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-italic.woff2'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-medium.woff'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-medium.woff2'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-mediumitalic.woff'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-mediumitalic.woff2'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-bold.woff'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-bold.woff2'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-bolditalic.woff'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-bolditalic.woff2'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-extrabold.woff'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-extrabold.woff2'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-extrabolditalic.woff'
+        ),
+        import(
+          /* webpackPreload: true */ 'assets/fonts/larsseit-extrabolditalic.woff2'
+        )
       ]);
 
       const fonts = {};
