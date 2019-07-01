aae8adcc9f0f72b1bb917cbf2fb1dea6ef992906
David <david@citizenlab.co>
2019-07-01 15:33:15 +0200

    fix for broken e2e tests after service workers introduction

--- internals/webpack/webpack.config.js
+++ internals/webpack/webpack.config.js
@@ -197,29 +197,31 @@ const config = {
       chunkFilename: '[name].[contenthash].chunk.css'
     }),
 
-    // !isDev && new OfflinePlugin({
-    //   relativePaths: false,
-    //   publicPath: '/',
-    //   appShell: '/',
-    //   responseStrategy: 'cache-first',
-    //   excludes: [
-    //     '**/.*',
-    //     '**/*.map',
-    //     '**/*.gz',
-    //     '**/*.html',
-    //     '.htaccess'
-    //   ],
-    //   caches: {
-    //     main: [':rest:'],
-    //     additional: ['*.chunk.js'],
-    //   },
-    //   ServiceWorker: {
-    //     prefetchRequest: {
-    //       credentials: 'same-origin',
-    //     },
-    //   },
-    //   safeToUseOptionalCaches: true, // removes warning for about `additional` section usage
-    // }),
+    !isDev && new OfflinePlugin({
+      relativePaths: false,
+      publicPath: '/',
+      appShell: '/',
+      responseStrategy: 'cache-first',
+      excludes: [
+        '**/.*',
+        '**/*.map',
+        '**/*.gz',
+        '**/*.html',
+        '.htaccess',
+        '/__',
+        '/__/**'
+      ],
+      caches: {
+        main: [':rest:'],
+        additional: ['*.chunk.js'],
+      },
+      ServiceWorker: {
+        prefetchRequest: {
+          credentials: 'same-origin',
+        },
+      },
+      safeToUseOptionalCaches: true, // removes warning for about `additional` section usage
+    }),
 
     !isDev && new webpack.HashedModuleIdsPlugin(),
 
