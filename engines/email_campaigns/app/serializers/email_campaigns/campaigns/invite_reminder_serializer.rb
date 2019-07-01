c653e3702e36991343ca4c97084942244c193b12
Sébastien Hoorens <sebastien@citizenlab.co>
2019-07-01 10:06:23 +0200

    migrated email campaigns serializers to fast json, but payload has now changed...

--- engines/email_campaigns/app/serializers/email_campaigns/campaigns/invite_reminder_serializer.rb
+++ engines/email_campaigns/app/serializers/email_campaigns/campaigns/invite_reminder_serializer.rb
@@ -1,4 +1,4 @@
 module EmailCampaigns::Campaigns
-	class InviteReminderSerializer < ::WebApi::V1::External::InviteSerializer
+	class InviteReminderSerializer < ::WebApi::V1::Fast::External::InviteSerializer
 	end
 end
--- engines/email_campaigns/app/serializers/email_campaigns/campaigns/project_phase_upcoming_serializer.rb
+++ engines/email_campaigns/app/serializers/email_campaigns/campaigns/project_phase_upcoming_serializer.rb
@@ -2,7 +2,8 @@ module EmailCampaigns::Campaigns
   class ProjectPhaseUpcomingSerializer < NotificationSerializer
     has_many :phase, serializer: CustomPhaseSerializer
     belongs_to :project, serializer: CustomProjectSerializer
-    has_many :project_images, serializer: CustomImageSerializer
-
+    has_many :project_images, serializer: CustomImageSerializer do |object|
+      object.project&.project_images
+    end
   end
 end
