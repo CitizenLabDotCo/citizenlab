b21e584e7fe758db24ef3ba02a443822e83ffe1b
Sébastien Hoorens <sebastien@citizenlab.co>
2019-07-02 14:59:49 +0200

    migrated remaining web api serializers from engines

--- app/serializers/web_api/v1/fast/avatar_serializer.rb
+++ app/serializers/web_api/v1/fast/avatar_serializer.rb
@@ -1,8 +1,5 @@
 class WebApi::V1::Fast::AvatarSerializer < WebApi::V1::Fast::BaseSerializer
   set_type :avatar
-  set_id :id do |object|
-    "#{object.id}-avatar"
-  end
 
   attribute :avatar do |object|
     object.avatar && object.avatar.versions.map{|k, v| [k.to_s, v.url]}.to_h
--- engines/nlp/app/serializers/web_api/v1/idea_geotag_serializer.rb
+++ engines/nlp/app/serializers/web_api/v1/idea_geotag_serializer.rb
@@ -1,21 +1,21 @@
-class WebApi::V1::IdeaGeotagSerializer < ActiveModel::Serializer
-  attributes :id, :title_multiloc, :location_point_geojson, :location_description
+class WebApi::V1::IdeaGeotagSerializer < WebApi::V1::Fast::BaseSerializer
+  attribute :title_multiloc
 
-  def location_point_geojson
+  attribute :location_point_geojson do |object, params|
     if object.location_point.present?
       RGeo::GeoJSON.encode(object.location_point)
-    elsif @instance_options[:geotags][object.id]
-      geo = @instance_options[:geotags][object.id]
+    elsif params[:geotags][object.id]
+      geo = params[:geotags][object.id]
       factory = RGeo::Cartesian.factory
       RGeo::GeoJSON.encode(factory.point(geo['lon'], geo['lat']))
     end
   end
 
-  def location_description
+  attribute :location_description do |object, params|
     if object.location_description 
       object.location_description
-    elsif @instance_options[:geotags][object.id]
-      @instance_options[:geotags][object.id]['address']
+    elsif params[:geotags][object.id]
+      params[:geotags][object.id]['address']
     end
   end
 end
