5f13fb89e476280c16c500be5992fd08f5fdfee3
David <david@citizenlab.co>
2019-07-02 11:01:29 +0200

    remove webpack service worker setup (for now)

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,20 +1,20 @@
 import './commands'
 
-before(function () {
-  // run this once before all code
-  return window.caches.keys().then((cacheNames) => {
-    return Promise.all(
-      cacheNames.map((cacheName) => {
-        return window.caches.delete(cacheName);
-      })
-    );
-  });
-});
+// before(function () {
+//   // run this once before all code
+//   return window.caches.keys().then((cacheNames) => {
+//     return Promise.all(
+//       cacheNames.map((cacheName) => {
+//         return window.caches.delete(cacheName);
+//       })
+//     );
+//   });
+// });
 
-beforeEach(() => {
-  // remove service workers before each test
-  cy.unregisterServiceWorkers();
-});
+// beforeEach(() => {
+//   // remove service workers before each test
+//   cy.unregisterServiceWorkers();
+// });
 
 Cypress.on('uncaught:exception', (err, runnable) => {
   // returning false here prevents Cypress from
