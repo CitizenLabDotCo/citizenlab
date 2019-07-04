8dff59fc4b82660dbc303680f0af4c5c6a67e725
Brent Guffens <brentguf@users.noreply.github.com>
2019-07-04 12:06:33 +0200

    Correct prop for IdeaMap

--- app/components/admin/IdeaManager/components/IdeaPreview/IdeaContent.tsx
+++ app/components/admin/IdeaManager/components/IdeaPreview/IdeaContent.tsx
@@ -242,7 +242,7 @@ export class IdeaContent extends PureComponent<Props & InjectedLocalized & Injec
 
                 {ideaLocation && ideaAdress &&
                   <StyledIdeaMap
-                    adress={ideaAdress}
+                    address={ideaAdress}
                     location={ideaLocation}
                     id={idea.id}
                   />
