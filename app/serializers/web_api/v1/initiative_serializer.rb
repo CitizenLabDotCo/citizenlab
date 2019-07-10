df5a98494af08df4e8593752abfeb498876d6ebc
Sébastien Hoorens <sebastien@citizenlab.co>
2019-07-10 17:07:16 +0200

    added comment and feedback counts to initiatives serialization

--- app/serializers/web_api/v1/initiative_serializer.rb
+++ app/serializers/web_api/v1/initiative_serializer.rb
@@ -1,6 +1,6 @@
 class WebApi::V1::InitiativeSerializer < ActiveModel::Serializer
 
-  attributes :id, :title_multiloc, :body_multiloc, :author_name, :slug, :publication_status, :upvotes_count, :location_point_geojson, :location_description, :created_at, :updated_at, :published_at, :header_bg
+  attributes :id, :title_multiloc, :body_multiloc, :author_name, :slug, :publication_status, :upvotes_count, :comments_count, :official_feedbacks_count, :location_point_geojson, :location_description, :created_at, :updated_at, :published_at, :header_bg
 
   has_many :initiative_images, serializer: WebApi::V1::ImageSerializer
 
