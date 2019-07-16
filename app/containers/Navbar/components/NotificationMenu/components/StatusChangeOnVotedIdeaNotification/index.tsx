d3e1c05f984c631f71b5ea714ba0207532b20a98
Sébastien Hoorens <sebastien@citizenlab.co>
2019-07-16 17:52:03 +0200

    requested changes

--- app/containers/Navbar/components/NotificationMenu/components/StatusChangeOnVotedIdeaNotification/index.tsx
+++ app/containers/Navbar/components/NotificationMenu/components/StatusChangeOnVotedIdeaNotification/index.tsx
@@ -24,7 +24,7 @@ interface Props extends InputProps, DataProps {}
 const StatusChangeOnVotedIdeaNotification = memo<Props>(props => {
   const { notification, idea, ideaStatus } = props;
 
-  if (isNilOrError(idea) || !ideaStatus) return null;
+  if (isNilOrError(idea) || isNilOrError(ideaStatus)) return null;
 
   return (
     <NotificationWrapper
@@ -36,8 +36,7 @@ const StatusChangeOnVotedIdeaNotification = memo<Props>(props => {
       <FormattedMessage
         {...messages.statusChangeOnVotedIdea}
         values={{
-          status: <T value={ideaStatus.attributes.title_multiloc} />,
-          ideaTitle: <T value={idea.attributes.title_multiloc} />
+          status: <T value={ideaStatus.attributes.title_multiloc} />
         }}
       />
     </NotificationWrapper>
