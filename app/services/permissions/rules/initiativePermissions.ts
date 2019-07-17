52f25985312ff43b6c96a41d31d2052560f38942
Brent Guffens <brentguf@users.noreply.github.com>
2019-07-17 15:17:22 +0200

    initiativePermissions

---
+++ app/services/permissions/rules/initiativePermissions.ts
@@ -0,0 +1,20 @@
+import { definePermissionRule } from 'services/permissions/permissions';
+import { isAdmin } from '../roles';
+import { IInitiativeData } from 'services/initiatives';
+import { IUser } from 'services/users';
+
+const isAuthor = (initiative: IInitiativeData, user?: IUser) => {
+  return user && initiative.relationships.author.data && initiative.relationships.author.data.id === user.data.id;
+};
+
+definePermissionRule('initiatives', 'edit', (initiative: IInitiativeData, user: IUser) => {
+  return !!(isAuthor(initiative, user) || isAdmin(user));
+});
+
+definePermissionRule('initiatives', 'markAsSpam', () => {
+  return true;
+});
+
+definePermissionRule('initiatives', 'moderate', (_initiative: IInitiativeData, user: IUser) => {
+  return isAdmin(user);
+});
