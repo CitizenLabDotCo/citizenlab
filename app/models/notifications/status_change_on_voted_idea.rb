a5ad9df669f8908d4972fafe80f4b4b416707aff
Sébastien Hoorens <sebastien@citizenlab.co>
2019-07-17 11:27:00 +0200

    requested changes

--- app/models/notifications/status_change_on_voted_idea.rb
+++ app/models/notifications/status_change_on_voted_idea.rb
@@ -10,27 +10,22 @@ module Notifications
 
     ACTIVITY_TRIGGERS = {'Idea' => {'changed_status' => true}}
     EVENT_NAME = 'Status change on voted idea'
-    
+
 
     def self.make_notifications_on activity
       idea = activity.item
+      initiator_id = activity.user_id
 
-      idea_id = idea&.id
-      recipient_id = idea&.author_id
-      initiator_id = activity&.user_id
-      project_id = idea&.project_id
-      idea_status_id = idea&.idea_status_id
-
-      if idea_id && recipient_id && recipient_id != initiator_id
+      if idea.present?
         comment_author_ids = idea.comments.pluck(:author_id)
         idea.votes.pluck(:user_id).map do |recipient_id|
           if (recipient_id != initiator_id) && !(comment_author_ids + [idea.author_id]).include?(recipient_id)
             self.create!(
               recipient_id: recipient_id,
-              initiating_user: User.find_by(id: initiator_id),
-              idea_id: idea_id,
-              project_id: project_id,
-              idea_status_id: idea_status_id
+              initiating_user_id: initiator_id,
+              idea_id: idea.id,
+              project_id: idea.project_id,
+              idea_status_id: idea.idea_status_id
             )
           end
         end
