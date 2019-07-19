7bb65550eadda63f5fe4f4f42c6da4b357069601
Brent Guffens <brentguf@users.noreply.github.com>
2019-07-19 18:04:53 +0200

    Make TranslateButton a UI component

---
+++ app/components/UI/TranslateButton/messages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  seeTranslation: {
+    id: 'app.components.UI.TranslateButton.seeTranslation',
+    defaultMessage: 'See translation',
+  },
+  seeOriginal: {
+    id: 'app.components.UI.TranslateButton.seeOriginal',
+    defaultMessage: 'See original',
+  },
+});
