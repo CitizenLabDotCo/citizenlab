eb5bf8826a216a2aee04192b75bc1be04b1726c9
Sébastien Hoorens <sebastien@citizenlab.co>
2019-07-22 17:53:47 +0200

    fixed passing classes to fields_for and changed minimum voting threshold to 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,7 +51,7 @@ class User < ApplicationRecord
   validates :invite_status, inclusion: {in: INVITE_STATUSES}, allow_nil: true
 
   validates :custom_field_values, json: {
-    schema: lambda { CustomFieldService.new.fields_to_json_schema(CustomField.fields_for(User)) },
+    schema: lambda { CustomFieldService.new.fields_to_json_schema(CustomField.fields_for('User')) },
     message: ->(errors) { errors }
   }, if: [:custom_field_values_changed?, :active?]
 
