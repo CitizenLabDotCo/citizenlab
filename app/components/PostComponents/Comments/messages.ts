bc20ff8d8b2594d7fd4a014975387e7534113fcd
Brent Guffens <brentguf@users.noreply.github.com>
2019-07-23 12:29:42 +0200

    PostComponent/Comments messages

--- app/components/PostComponents/Comments/messages.ts
+++ app/components/PostComponents/Comments/messages.ts
@@ -1,8 +1,40 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  x: {
-    id: 'app.components.PostComponents.Comments.x',
-    defaultMessage: '',
+  noComments: {
+    id: 'app.components.PostComponents.Comments.noComments',
+    defaultMessage: 'There are no comments yet.',
+  },
+  loadingMoreComments: {
+    id: 'app.components.PostComponents.Comments.loadingMoreComments',
+    defaultMessage: 'Loading more comments...',
+  },
+  commentingDisabledProjectInactive: {
+    id: 'app.components.PostComponents.Comments.commentingDisabledProjectInactive',
+    defaultMessage: "Commenting on this idea is not possible because '{projectName}' is currently not active.",
+  },
+  commentingDisabledInContext: {
+    id: 'app.components.PostComponents.Comments.commentingDisabledInContext',
+    defaultMessage: "Commenting on ideas in '{projectName}' is currently disabled.",
+  },
+  commentingDisabledIdeaNotInCurrentPhase: {
+    id: 'app.components.PostComponents.Comments.commentingDisabledIdeaNotInCurrentPhase',
+    defaultMessage: "Commenting on this idea is not possible since it's no longer or not yet in consideration.",
+  },
+  commentingNotPermitted: {
+    id: 'app.components.PostComponents.Comments.commentingNotPermitted',
+    defaultMessage: 'Commenting on this idea is currently not allowed',
+  },
+  commentingMaybeNotPermitted: {
+    id: 'app.components.PostComponents.Comments.commentingMaybeNotPermitted',
+    defaultMessage: 'Not all users are allowed to comment. Please {signInLink} to see whether you comply.',
+  },
+  signInToComment: {
+    id: 'app.containers.PostComponents.Comments.signInToComment',
+    defaultMessage: 'Please {signInLink} to spread your wisdom.',
+  },
+  signInLinkText: {
+    id: 'app.containers.PostComponents.Comments.signInLinkText',
+    defaultMessage: 'log in',
   },
 });
