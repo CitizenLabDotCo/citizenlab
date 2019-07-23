5eaea044224549750e3ba89f2551ea7006099971
Sébastien Hoorens <sebastien@citizenlab.co>
2019-07-23 12:04:28 +0200

    removed unsafe constantizes from engines

--- engines/machine_translations/app/policies/machine_translations/machine_translation_policy.rb
+++ engines/machine_translations/app/policies/machine_translations/machine_translation_policy.rb
@@ -5,7 +5,14 @@ module MachineTranslations
       translatable_policy = case record.translatable_type
       when 'Idea' then IdeaPolicy
       when 'Initiative' then InitiativePolicy
-      when 'Comment' then "#{record.translatable.post_type}CommentPolicy".constantize
+      when 'Comment' then 
+        case record.translatable.post_type
+        when 'Idea'
+          IdeaCommentPolicy
+        when 'Initiative'
+          InitiativeCommentPolicy
+        else raise "#{record.translatable.post_type} has no known commenting policy"
+        end
       else raise "#{record.translatable_type} has no known policy"
       end
       record.translatable && translatable_policy.new(user, record.translatable).show?
