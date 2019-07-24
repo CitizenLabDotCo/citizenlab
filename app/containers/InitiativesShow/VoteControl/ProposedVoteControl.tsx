79d9b9e28cb96d365aec2dac3edaa4d04d12467b
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2019-07-24 16:16:45 +0200

    CL2-4063 Added countdown

--- app/containers/InitiativesShow/VoteControl/ProposedVoteControl.tsx
+++ app/containers/InitiativesShow/VoteControl/ProposedVoteControl.tsx
@@ -1,18 +1,23 @@
 import React, { PureComponent } from 'react';
 import styled from 'styled-components';
+import moment from 'moment';
 
 import { IInitiativeData } from 'services/initiatives';
 import { IInitiativeStatusData } from 'services/initiativeStatuses';
 import { ITenantSettings } from 'services/tenant';
-
-import T from 'components/T';
+import CountDown from './CountDown';
 
 const Container = styled.div``;
+
+const CountDownWrapper = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+`;
 
 interface InputProps {
   initiative: IInitiativeData;
   initiativeStatus: IInitiativeStatusData;
-  initiativeSettings: ITenantSettings['initiatives'];
+  initiativeSettings: NonNullable<ITenantSettings['initiatives']>;
 }
 interface DataProps {}
 
@@ -21,12 +26,20 @@ interface Props extends InputProps, DataProps {}
 interface State {}
 
 class ProposedVoteControl extends PureComponent<Props, State> {
-  render() {
-    const { initiativeStatus } = this.props;
 
+  calculateCountdownTarget = () => {
+    const { initiative, initiativeSettings: { days_limit } } = this.props;
+    const mStart = moment(initiative.attributes.published_at);
+    return mStart.add(days_limit, 'day');
+  }
+
+  render() {
+    const { initiative } = this.props;
     return (
       <Container>
-        <T value={initiativeStatus.attributes.title_multiloc} />
+        <CountDownWrapper>
+          <CountDown targetTime={this.calculateCountdownTarget()} />
+        </CountDownWrapper>
       </Container>
     );
   }
