1f4d77eb14ffb61b9888f48413de39bb95b0f08f
Brent Guffens <brentguf@users.noreply.github.com>
2019-07-24 13:21:34 +0200

    canCommentOnInitiative permission

--- app/services/permissions/rules/initiativePermissions.ts
+++ app/services/permissions/rules/initiativePermissions.ts
@@ -7,6 +7,10 @@ const isAuthor = (initiative: IInitiativeData, user?: IUser) => {
   return user && initiative.relationships.author.data && initiative.relationships.author.data.id === user.data.id;
 };
 
+export const canCommentOnInitiative = (user: IUser) => {
+  return !isAdmin(user);
+};
+
 definePermissionRule('initiatives', 'edit', (initiative: IInitiativeData, user: IUser) => {
   return !!(isAuthor(initiative, user) || isAdmin(user));
 });
