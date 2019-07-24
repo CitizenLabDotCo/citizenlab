0b5dd8ce3f081f3b5902757e016dacb350310699
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2019-07-24 23:15:27 +0200

    CL2-4066 Vote control for ineligible status

--- app/containers/InitiativesShow/VoteControl/IneligibleVoteControl.tsx
+++ app/containers/InitiativesShow/VoteControl/IneligibleVoteControl.tsx
@@ -1,18 +1,65 @@
 import React, { PureComponent } from 'react';
 import styled from 'styled-components';
+import { colors, fontSizes } from 'utils/styleUtils';
 
 import { IInitiativeData } from 'services/initiatives';
 import { IInitiativeStatusData } from 'services/initiativeStatuses';
 import { ITenantSettings } from 'services/tenant';
 
+import Icon from 'components/UI/Icon';
+import { StatusWrapper, StatusExplanation, TooltipWrapper, HelpIcon } from './SharedStyles';
+import Tooltip from 'components/UI/Tooltip';
+import ProgressBar from 'components/UI/ProgressBar';
+import Button from 'components/UI/Button';
+
 import T from 'components/T';
+import messages from './messages';
+import { FormattedMessage } from 'utils/cl-intl';
 
 const Container = styled.div``;
+
+const StatusIcon = styled(Icon)`
+  path {
+    fill: ${colors.clGreyOnGreyBackground};
+  }
+  width: 30px;
+  height: 30px;
+  margin-bottom: 20px;
+`;
+
+const StyledTooltip = styled(Tooltip)`
+  display: inline;
+`;
+
+const VoteCounter = styled.div`
+  margin-top: 15px;
+`;
+
+const VoteTexts = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding-bottom: 4px;
+`;
+
+const VoteText = styled.div`
+  font-size: ${fontSizes.base}px;
+  color: ${colors.clGreyOnGreyBackground};
+`;
+
+const StyledProgressBar = styled(ProgressBar)`
+  height: 12px;
+  width: 100%;
+`;
+
+const StyledButton = styled(Button)`
+  margin-top: 20px;
+`;
 
 interface InputProps {
   initiative: IInitiativeData;
   initiativeStatus: IInitiativeStatusData;
-  initiativeSettings: ITenantSettings['initiatives'];
+  initiativeSettings: NonNullable<ITenantSettings['initiatives']>;
 }
 interface DataProps {}
 
@@ -22,11 +69,55 @@ interface State {}
 
 class IneligibleVoteControl extends PureComponent<Props, State> {
   render() {
-    const { initiativeStatus } = this.props;
+    const { initiative, initiativeSettings: { eligibility_criteria, voting_threshold }, initiativeStatus } = this.props;
+
+    const voteCount = initiative.attributes.upvotes_count;
+    const voteLimit = voting_threshold || 1;
 
     return (
       <Container>
-        <T value={initiativeStatus.attributes.title_multiloc} />
+        <StatusWrapper>
+          <T value={initiativeStatus.attributes.title_multiloc} />
+        </StatusWrapper>
+        <StatusIcon name="halt" />
+        <StatusExplanation>
+          <FormattedMessage
+            {...messages.ineligibleStatusExplanation}
+            values={{
+              ineligibleStatusExplanationBold: <b><FormattedMessage {...messages.ineligibleStatusExplanationBold} /></b>
+            }}
+          />
+          {eligibility_criteria &&
+            <StyledTooltip
+              content={
+                <TooltipWrapper>
+                  <T value={eligibility_criteria} supportHtml />
+                </TooltipWrapper>
+              }
+              top="20"
+            >
+              <HelpIcon name="info" />
+            </StyledTooltip>
+          }
+        </StatusExplanation>
+        <VoteCounter>
+          <VoteTexts>
+            <VoteText>
+              <FormattedMessage {...messages.xVotes} values={{ count: voteCount }} />
+            </VoteText>
+            <VoteText>
+              {voteLimit}
+            </VoteText>
+          </VoteTexts>
+          <StyledProgressBar
+            progress={voteCount / voteLimit}
+            color="linear-gradient(270deg, #84939E 0%, #C8D0D6 100%)"
+            bgColor={colors.lightGreyishBlue}
+          />
+        </VoteCounter>
+        <StyledButton>
+          <FormattedMessage {...messages.readAnswer} />
+        </StyledButton>
       </Container>
     );
   }
--- app/containers/InitiativesShow/VoteControl/messages.ts
+++ app/containers/InitiativesShow/VoteControl/messages.ts
@@ -21,6 +21,14 @@ export default defineMessages({
     id: 'app.containers.InitiativesShow.VoteControl.proposedStatusExplanationBold',
     defaultMessage: 'considered by {orgName}',
   },
+  ineligibleStatusExplanation: {
+    id: 'app.containers.InitiativesShow.VoteControl.ineligibleStatusExplanation',
+    defaultMessage: 'This initiative doesn\'t meet the criteria {ineligibleStatusExplanationBold}',
+  },
+  ineligibleStatusExplanationBold: {
+    id: 'app.containers.InitiativesShow.VoteControl.ineligibleStatusExplanationBold',
+    defaultMessage: 'to be eligible',
+  },
   xVotes: {
     id: 'app.containers.InitiativesShow.VoteControl.xVotes',
     defaultMessage: '{count, plural, =0 {no votes} one {1 vote} other {# votes}}',
@@ -28,5 +36,9 @@ export default defineMessages({
   vote: {
     id: 'app.containers.InitiativesShow.VoteControl.vote',
     defaultMessage: 'Vote',
+  },
+  readAnswer: {
+    id: 'app.containers.InitiativesShow.VoteControl.readAnswer',
+    defaultMessage: 'Read answer',
   },
 });
