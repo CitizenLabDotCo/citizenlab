b15650d482c2496ec11f1e4b1f2defa0eb91f25f
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2019-07-24 11:47:28 +0200

    CL2-3851 Initial scaffolding and data loading for vote control

---
+++ app/containers/InitiativesShow/VoteControl/AnsweredVoteControl.tsx
@@ -0,0 +1,35 @@
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+
+import { IInitiativeData } from 'services/initiatives';
+import { IInitiativeStatusData } from 'services/initiativeStatuses';
+import { ITenantSettings } from 'services/tenant';
+
+import T from 'components/T';
+
+const Container = styled.div``;
+
+interface InputProps {
+  initiative: IInitiativeData;
+  initiativeStatus: IInitiativeStatusData;
+  initiativeSettings: ITenantSettings['initiatives'];
+}
+interface DataProps {}
+
+interface Props extends InputProps, DataProps {}
+
+interface State {}
+
+class AnsweredVoteControl extends PureComponent<Props, State> {
+  render() {
+    const { initiativeStatus } = this.props;
+
+    return (
+      <Container>
+        <T value={initiativeStatus.attributes.title_multiloc} />
+      </Container>
+    );
+  }
+}
+
+export default AnsweredVoteControl;
