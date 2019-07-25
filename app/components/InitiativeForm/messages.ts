4a7e4b5434d29fefdbfb4b0de0ec8770f5b95348
Sara <sara@citizenlab.co>
2019-07-25 11:46:10 +0200

    Preliminary changes to Initiatives New for Initiatives Edit

--- app/components/InitiativeForm/messages.ts
+++ app/components/InitiativeForm/messages.ts
@@ -1,6 +1,41 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
+  // Header Messages
+  header: {
+    id: 'app.containers.InitiativesNewPage.header',
+    defaultMessage: 'Start your own initiative and make your voice heard by {styledOrgName}'
+  },
+  orgName: {
+    id: 'app.containers.InitiativesNewPage.orgName',
+    defaultMessage: '{orgName}'
+  },
+  // Tips Box Messages
+  tipsTitle: {
+    id: 'app.containers.InitiativesNewPage.tipsTitle',
+    defaultMessage: 'Tips and tricks for a successful initiative'
+  },
+  tipsExplanation: {
+    id: 'app.containers.InitiativesNewPage.tipsExplanation',
+    defaultMessage: 'An initiative is a project that you, as a citizen, can start anytime. Posting it on the platform enables you to gather support from others and, at the end, being heard by {orgName}.'
+  },
+  requirmentsListTitle: {
+    id: 'app.containers.InitiativesNewPage.requirmentsListTitle',
+    defaultMessage: 'To get there, you need to:'
+  },
+  requirmentVoteTreshold: {
+    id: 'app.containers.InitiativesNewPage.requirmentVoteTreshold',
+    defaultMessage: 'Gather {voteThreshold} votes'
+  },
+  requirmentDaysLimit: {
+    id: 'app.containers.InitiativesNewPage.requirmentDaysLimit',
+    defaultMessage: 'Within {daysLimit} days'
+  },
+  eligibility: {
+    id: 'app.containers.InitiativesNewPage.eligibility',
+    defaultMessage: 'To be eligible, {orgName} requires your initiative to respect the following:'
+  },
+  // Form Messages
   formGeneralSectionTitle: {
     id: 'app.components.InitiativeForm.formGeneralSectionTitle',
     defaultMessage: 'What is your initiative ?',
