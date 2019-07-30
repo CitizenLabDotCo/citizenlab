e6d225773dc25ac58417cb42b2e309b0e6ae8e47
Brent Guffens <brentguf@users.noreply.github.com>
2019-07-30 17:25:57 +0200

    Social sharing modal for initiatives (WIP)

---
+++ app/components/PostComponents/SharingModalContent/messages.ts
@@ -0,0 +1,41 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+
+  ideaShareTitle: {
+    id: 'app.components.PostComponents.SharingModalContent.ideaShareTitle',
+    defaultMessage: 'Congratulations, your idea was successfully posted!',
+  },
+  initiativeShareTitle: {
+    id: 'app.components.PostComponents.SharingModalContent.initiativeShareTitle',
+    defaultMessage: 'Congratulations, your initiative was successfully posted!',
+  },
+  ideaShareSubtitle: {
+    id: 'app.components.PostComponents.SharingModalContent.ideaShareSubtitle',
+    defaultMessage: 'Share your idea to reach more people, receive more votes and have more impact.',
+  },
+  initiativeSubtitle: {
+    id: 'app.components.PostComponents.SharingModalContent.initiativeSubtitle',
+    defaultMessage: 'Share your initiative to reach more people, receive more votes and have more impact.',
+  },
+  twitterMessage: {
+    id: 'app.components.PostComponents.SharingModalContent.twitterMessage',
+    defaultMessage: 'Vote for {postTitle} on',
+  },
+  ideaEmailSharingSubject: {
+    id: 'app.components.PostComponents.SharingModalContent.ideaEmailSharingSubject',
+    defaultMessage: 'Support my idea: {postTitle}.',
+  },
+  initiativeEmailSharingSubject: {
+    id: 'app.components.PostComponents.SharingModalContent.initiativeEmailSharingSubject',
+    defaultMessage: 'Support my initiative: {postTitle}.',
+  },
+  ideaEmailSharingBody: {
+    id: 'app.components.PostComponents.SharingModalContent.ideaEmailSharingBody',
+    defaultMessage: 'What do you think of this idea? Vote on it and share the discussion at {postUrl} to make your voice heard!',
+  },
+  initiativeEmailSharingBody: {
+    id: 'app.components.PostComponents.SharingModalContent.initiativeEmailSharingBody',
+    defaultMessage: 'What do you think of this initiative? Vote on it and share the discussion at {postUrl} to make your voice heard!',
+  },
+});
