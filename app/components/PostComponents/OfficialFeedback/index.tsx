1e1ea4abeba56e1e089e98112c1703e502cbf966
DavidCnoops <david@citizenlab.co>
2019-08-08 10:30:19 +0200

    Fixed permissionToPost typing error

--- app/components/PostComponents/OfficialFeedback/index.tsx
+++ app/components/PostComponents/OfficialFeedback/index.tsx
@@ -1,11 +1,9 @@
 import React, { PureComponent } from 'react';
+import { isBoolean } from 'lodash-es';
 
 // components
 import OfficialFeedbackNew from './Form/OfficialFeedbackNew';
 import OfficialFeedbackFeed from './OfficialFeedbackFeed';
-
-// resources
-import { GetPermissionChildProps } from 'resources/GetPermission';
 
 // style
 import styled from 'styled-components';
@@ -15,7 +13,7 @@ const Container = styled.div``;
 interface Props {
   postId: string;
   postType: 'idea' | 'initiative';
-  permissionToPost: GetPermissionChildProps;
+  permissionToPost: boolean | undefined;
   className?: string;
 }
 
@@ -25,22 +23,26 @@ export default class OfficialFeedback extends PureComponent<Props, State> {
   render() {
     const { postId, postType, permissionToPost, className } = this.props;
 
-    return (
-      <Container className={className}>
-        {permissionToPost &&
-          <OfficialFeedbackNew
+    if (isBoolean(permissionToPost)) {
+      return (
+        <Container className={className}>
+          {permissionToPost &&
+            <OfficialFeedbackNew
+              postId={postId}
+              postType={postType}
+            />
+          }
+
+          <OfficialFeedbackFeed
             postId={postId}
             postType={postType}
+            permission={permissionToPost}
+            editingAllowed={permissionToPost}
           />
-        }
+        </Container>
+      );
+    }
 
-        <OfficialFeedbackFeed
-          postId={postId}
-          postType={postType}
-          permission={permissionToPost}
-          editingAllowed={permissionToPost}
-        />
-      </Container>
-    );
+    return null;
   }
 }
