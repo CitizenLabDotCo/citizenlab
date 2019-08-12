dbb8d811e99194e62aaf232517cc5a80c5d9140a
Sébastien Hoorens <sebastien@citizenlab.co>
2019-08-12 14:56:05 +0200

    switching branches

--- app/models/notifications/official_feedback_on_commented_idea.rb
+++ app/models/notifications/official_feedback_on_commented_idea.rb
@@ -6,9 +6,9 @@ module Notifications
     belongs_to :idea
     belongs_to :project, optional: true
 
-    validates :official_feedback_id, presence: true
+    validates :official_feedback, presence: true
     validates :initiating_user, presence: true
-    validates :idea_id, presence: true
+    validates :idea, presence: true
 
 
     ACTIVITY_TRIGGERS = {'OfficialFeedback' => {'created' => true}}
@@ -17,27 +17,22 @@ module Notifications
 
     def self.make_notifications_on activity
       official_feedback = activity.item
+      initiator_id = official_feedback.user_id
 
-      official_feedback_id = official_feedback&.id
-      idea = official_feedback&.post
-      idea_id = official_feedback&.post_id
-      initiator_id = official_feedback&.user_id
-      project_id = idea&.project_id
-
-      if (official_feedback&.post_type == 'Idea') && official_feedback_id && idea_id && initiator_id
+      if official_feedback.post_type == 'Idea' && initiator_id
         User.active
           .joins(:comments).merge(Comment.published)
           .where(comments: {post: idea})
           .distinct
           .ids
-          .select{|recipient_id| recipient_id != initiator_id && recipient_id != idea.author_id}
+          .select{|recipient_id| recipient_id != initiator_id && recipient_id != official_feedback.post.author_id}
           .map do |recipient_id|
             self.new(
               recipient_id: recipient_id,
-              initiating_user: User.find(initiator_id),
-              idea_id: idea_id,
-              official_feedback_id: official_feedback_id,
-              project_id: project_id
+              initiating_user_id: initiator_id,
+              idea: official_feedback.post,
+              official_feedback: official_feedback,
+              project_id: official_feedback.post.project_id
             )
           end
       else
@@ -47,4 +42,3 @@ module Notifications
 
   end
 end
-
