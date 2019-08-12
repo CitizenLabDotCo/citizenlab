18c51454ea9592cbc773c1a955e73e66f533d46f
Sébastien Hoorens <sebastien@citizenlab.co>
2019-08-12 14:56:05 +0200

    switching branches

--- app/models/notifications/official_feedback_on_voted_initiative.rb
+++ app/models/notifications/official_feedback_on_voted_initiative.rb
@@ -6,9 +6,9 @@ module Notifications
     belongs_to :idea
     belongs_to :project, optional: true
 
-    validates :official_feedback, presence: true
+    validates :official_feedback_id, presence: true
     validates :initiating_user, presence: true
-    validates :idea, presence: true
+    validates :idea_id, presence: true
 
 
     ACTIVITY_TRIGGERS = {'OfficialFeedback' => {'created' => true}}
@@ -17,26 +17,31 @@ module Notifications
 
     def self.make_notifications_on activity
       official_feedback = activity.item
-      initiator_id = official_feedback.user_id
 
-      if official_feedback.post_type == 'Idea' && initiator_id
+      official_feedback_id = official_feedback&.id
+      idea = official_feedback&.post
+      idea_id = official_feedback&.post_id
+      initiator_id = official_feedback&.user_id
+      project_id = idea&.project_id
+
+      if (official_feedback&.post_type == 'Idea') && official_feedback_id && idea_id && initiator_id
         comment_author_ids = User.active
           .joins(:comments).merge(Comment.published)
-          .where(comments: {post: official_feedback.post})
+          .where(comments: {post: idea})
           .distinct
           .ids
         voter_ids = User.active
-          .joins(:votes).where(votes: {votable: official_feedback.post})
+          .joins(:votes).where(votes: {votable: idea})
           .distinct
           .ids
 
-        (voter_ids - [initiator_id, *comment_author_ids, official_feedback.post.author_id]).map do |recipient_id|
+        (voter_ids - [initiator_id, *comment_author_ids, idea.author_id]).map do |recipient_id|
           self.new(
             recipient_id: recipient_id,
-            initiating_user_id: initiator_id,
-            idea: official_feedback.post,
-            official_feedback: official_feedback,
-            project_id: official_feedback.post.project_id
+            initiating_user: User.find(initiator_id),
+            idea_id: idea_id,
+            official_feedback_id: official_feedback_id,
+            project_id: project_id
             )
         end
       else
--- app/models/notifications/official_feedback_on_your_initiative.rb
+++ app/models/notifications/official_feedback_on_your_initiative.rb
@@ -1,32 +1,30 @@
 module Notifications
-  class OfficialFeedbackOnYourIdea < Notification
+  class OfficialFeedbackOnYourInitiative < Notification
     
     belongs_to :initiating_user, class_name: 'User'
     belongs_to :official_feedback
-    belongs_to :idea
-    belongs_to :project, optional: true
+    belongs_to :initiative
 
     validates :official_feedback, presence: true
     validates :initiating_user, presence: true
-    validates :idea, presence: true
+    validates :initiative, presence: true
 
 
     ACTIVITY_TRIGGERS = {'OfficialFeedback' => {'created' => true}}
-    EVENT_NAME = 'Official feedback on your idea'
+    EVENT_NAME = 'Official feedback on your initiative'
     
 
     def self.make_notifications_on activity
       official_feedback = activity.item
-      if official_feedback.post_type == 'Idea'
+      if official_feedback.post_type == 'Initiative'
         recipient_id = official_feedback.post.author_id
         initiator_id = official_feedback&.user_id
         if official_feedback && recipient_id && initiator_id && (recipient_id != initiator_id)
           [self.new(
              recipient_id: recipient_id,
              initiating_user_id: initiator_id,
-             idea: official_feedback.post,
-             official_feedback: official_feedback,
-             project_id: official_feedback.post.project_id
+             initiative: official_feedback.post,
+             official_feedback: official_feedback
            )]
         else
           []
