7244a2ca1e02c8a3331d989752c85557a1b85822
Sébastien Hoorens <sebastien@citizenlab.co>
2019-08-12 15:48:37 +0200

    official feedback campaigns

--- app/models/notifications/official_feedback_on_commented_initiative.rb
+++ app/models/notifications/official_feedback_on_commented_initiative.rb
@@ -1,44 +1,39 @@
 module Notifications
-  class OfficialFeedbackOnVotedIdea < Notification
+  class OfficialFeedbackOnCommentedInitiative < Notification
     
     belongs_to :initiating_user, class_name: 'User'
     belongs_to :official_feedback
-    belongs_to :idea
+    belongs_to :initiative
     belongs_to :project, optional: true
 
     validates :official_feedback, presence: true
     validates :initiating_user, presence: true
-    validates :idea, presence: true
+    validates :initiative, presence: true
 
 
     ACTIVITY_TRIGGERS = {'OfficialFeedback' => {'created' => true}}
-    EVENT_NAME = 'Official feedback on voted idea'
+    EVENT_NAME = 'Official feedback on commented initiative'
     
 
     def self.make_notifications_on activity
       official_feedback = activity.item
       initiator_id = official_feedback.user_id
 
-      if official_feedback.post_type == 'Idea' && initiator_id
-        comment_author_ids = User.active
+      if official_feedback.post_type == 'Initiative' && initiator_id
+        User.active
           .joins(:comments).merge(Comment.published)
-          .where(comments: {post: official_feedback.post})
+          .where(comments: {post: initiative})
           .distinct
           .ids
-        voter_ids = User.active
-          .joins(:votes).where(votes: {votable: official_feedback.post})
-          .distinct
-          .ids
-
-        (voter_ids - [initiator_id, *comment_author_ids, official_feedback.post.author_id]).map do |recipient_id|
-          self.new(
-            recipient_id: recipient_id,
-            initiating_user_id: initiator_id,
-            idea: official_feedback.post,
-            official_feedback: official_feedback,
-            project_id: official_feedback.post.project_id
+          .select{|recipient_id| recipient_id != initiator_id && recipient_id != official_feedback.post.author_id}
+          .map do |recipient_id|
+            self.new(
+              recipient_id: recipient_id,
+              initiating_user_id: initiator_id,
+              initiative: official_feedback.post,
+              official_feedback: official_feedback
             )
-        end
+          end
       else
         []
       end.compact
@@ -46,4 +41,3 @@ module Notifications
 
   end
 end
-
