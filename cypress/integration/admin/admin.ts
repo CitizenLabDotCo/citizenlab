bdfdd411d33a815e174c638132f895598cf9e39b
Brent Guffens <brentguf@users.noreply.github.com>
2019-08-14 11:46:02 +0200

    Fix failing tests URL problems

--- cypress/integration/admin/admin.ts
+++ cypress/integration/admin/admin.ts
@@ -2,13 +2,13 @@ describe('/admin route', () => {
   it('redirects unauthenticated users to signin', () => {
     cy.visit('/admin');
     cy.wait(500);
-    cy.location('pathname').should('eq', '/en-GB/sign-in/');
+    cy.location('pathname').should('eq', '/en-GB/sign-in');
   });
 
   it('redirects admins to dashboard', () => {
     cy.login('admin@citizenlab.co', 'testtest');
     cy.visit('/admin');
     cy.wait(500);
-    cy.location('pathname').should('eq', '/en-GB/admin/dashboard/');
+    cy.location('pathname').should('eq', '/en-GB/admin/dashboard');
   });
 });
