b4416117ad7519392279172f1fd1ced479a7b44f
Sébastien Hoorens <sebastien@citizenlab.co>
2019-08-20 11:24:48 +0200

    refactoring: project notifications

--- app/models/notifications/project_moderation_rights_received.rb
+++ app/models/notifications/project_moderation_rights_received.rb
@@ -1,26 +1,24 @@
 module Notifications
   class ProjectModerationRightsReceived < Notification
     
-    belongs_to :initiating_user, class_name: 'User'
+    belongs_to :initiating_user, class_name: 'User', optional: true
     belongs_to :project
 
-    validates :project_id, presence: true
-    validates :initiating_user, presence: true
+    validates :project presence: true
 
     ACTIVITY_TRIGGERS = {'User' => {'project_moderation_rights_given' => true}}
     EVENT_NAME = 'Project moderation rights received'
 
 
     def self.make_notifications_on activity
-      moderator = activity.item
-      recipient_id = moderator.id
+      recipient = activity.item
       initiator_id = activity.user_id
       project_id = activity.payload['project_id']
       
-      if project_id && recipient_id && initiator_id && (recipient_id != initiator_id)
+      if project_id && recipient
         [self.new(
-           recipient_id: moderator.id,
-           initiating_user: User.find(initiator_id),
+           recipient: recipient,
+           initiating_user_id: initiator_id,
            project_id: project_id
          )]
       else
