817ab263441ad49eb1d83f4d0f00d7bd5cda954e
Sébastien Hoorens <sebastien@citizenlab.co>
2019-08-20 10:20:33 +0200

    deleted add initiative to notification migration

--- db/migrate/20190805121513_add_initiative_to_notifications.rb
+++
@@ -1,5 +0,0 @@
-class AddInitiativeToNotifications < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :notifications, :initiative, foreign_key: true, type: :uuid, :default => nil
-  end
-end
