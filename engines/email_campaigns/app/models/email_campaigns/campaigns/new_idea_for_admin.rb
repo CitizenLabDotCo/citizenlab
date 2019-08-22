5a8d665e003f8381dd0587187ccc8056322ed327
Sébastien Hoorens <sebastien@citizenlab.co>
2019-08-22 17:45:16 +0200

    refactoring: until new initiative for admin

--- engines/email_campaigns/app/models/email_campaigns/campaigns/new_idea_for_admin.rb
+++ engines/email_campaigns/app/models/email_campaigns/campaigns/new_idea_for_admin.rb
@@ -1,5 +1,5 @@
 module EmailCampaigns
-  class Campaigns::NewIdeaForAdmin < Campaigns::NotificationCampaign
+  class Campaigns::NewIdeaForAdmin < Campaign
     include Consentable
     include ActivityTriggerable
     include RecipientConfigurable
@@ -16,6 +16,26 @@ module EmailCampaigns
 
     def activity_triggers
       {'Notifications::NewIdeaForAdmin' => {'created' => true}}
+    end
+
+    def filter_notification_recipient users_scope, activity:, time: nil
+      users_scope.where(id: activity.item.recipient.id)
+    end
+
+    def generate_commands recipient:, activity:, time: nil
+      notification = activity.item
+      [{
+        event_payload: {
+          initiating_user_first_name: notification.initiating_user&.first_name,
+          initiating_user_last_name: notification.initiating_user&.last_name,                    
+          post_published_at: notification.post.published_at.iso8601,   
+          post_title_multiloc: notification.post.title_multiloc,
+          post_author_name: notification.post.author_name,
+          comment_author_name: notification.comment.author_name,
+          comment_body_multiloc: notification.comment.body_multiloc,
+          comment_url: Frontend::UrlService.new.model_to_url(notification.comment, locale: recipient.locale)
+        }
+      }]
     end
 
     protected
