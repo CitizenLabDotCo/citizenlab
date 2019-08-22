c94e11e2d6fa47983d6c0f105c56563790265058
Sébastien Hoorens <sebastien@citizenlab.co>
2019-08-22 15:56:29 +0200

    refactoring email til mention in feedback

--- engines/email_campaigns/app/models/email_campaigns/campaigns/mention_in_comment.rb
+++ engines/email_campaigns/app/models/email_campaigns/campaigns/mention_in_comment.rb
@@ -1,5 +1,5 @@
 module EmailCampaigns
-  class Campaigns::MentionInComment < Campaigns::NotificationCampaign
+  class Campaigns::MentionInComment < Campaign
     include Consentable
     include ActivityTriggerable
     include RecipientConfigurable
@@ -12,6 +12,26 @@ module EmailCampaigns
 
     def activity_triggers
       {'Notifications::MentionInComment' => {'created' => true}}
+    end
+
+    def filter_notification_recipient users_scope, activity:, time: nil
+      users_scope.where(id: activity.item.recipient.id)
+    end
+
+    def generate_commands recipient:, activity:, time: nil
+      notification = activity.item
+      [{
+        event_payload: {
+          initiating_user_first_name: notification.initiating_user&.first_name,
+          initiating_user_last_name: notification.initiating_user&.last_name,
+          post_published_at: notification.post.published_at.iso8601,
+          post_title_multiloc: notification.post.title_multiloc,
+          post_body_multiloc: notification.post.body_multiloc,  
+          post_author_name: notification.post.author_name,
+          comment_body_multiloc: notification.comment.body_multiloc,
+          comment_url: Frontend::UrlService.new.model_to_url(notification.comment, locale: recipient.locale)
+        }
+      }]
     end
   end
 end
