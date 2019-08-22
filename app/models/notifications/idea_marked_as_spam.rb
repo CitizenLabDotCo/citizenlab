e882afba3f2ea356a00517d60e7dcfe23d51b673
Sébastien Hoorens <sebastien@citizenlab.co>
2019-08-22 10:51:33 +0200

    fixed

--- app/models/notifications/idea_marked_as_spam.rb
+++ app/models/notifications/idea_marked_as_spam.rb
@@ -15,14 +15,14 @@ module Notifications
       spam_report = activity.item
       if spam_report.spam_reportable_type == 'Idea'
         initiator_id = spam_report.user_id
-        project_id = spam_report.spam_reportable_id
+        project_id = spam_report.spam_reportable.project_id
         self.recipient_ids(initiator_id, project_id).map do |recipient_id|
           self.new(
             recipient_id: recipient_id,
             initiating_user_id: initiator_id,
             spam_report: spam_report,
             post: spam_report.spam_reportable,
-            project_id: initiating_user
+            project_id: project_id
           )
         end
       else
