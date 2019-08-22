0550eeb0283c9147d98b95854c5c7548fd6bfc31
Sébastien Hoorens <sebastien@citizenlab.co>
2019-08-22 14:58:41 +0200

    refactoring until initiative_marked_as_spam

--- engines/email_campaigns/app/models/email_campaigns/campaigns/admin_rights_received.rb
+++ engines/email_campaigns/app/models/email_campaigns/campaigns/admin_rights_received.rb
@@ -1,5 +1,5 @@
 module EmailCampaigns
-  class Campaigns::AdminRightsReceived < Campaigns::NotificationCampaign
+  class Campaigns::AdminRightsReceived < Campaign
     include ActivityTriggerable
     include Disableable
     include LifecycleStageRestrictable
@@ -10,6 +10,19 @@ module EmailCampaigns
 
     def activity_triggers
       {'Notifications::AdminRightsReceived' => {'created' => true}}
+    end
+
+    def filter_notification_recipient users_scope, activity:, time: nil
+      users_scope.where(id: activity.item.recipient.id)
+    end
+
+    def generate_commands recipient:, activity:, time: nil
+      notification = activity.item
+      [{
+        event_payload: {
+          
+        }
+      }]
     end
   end
 end
--- engines/email_campaigns/app/models/email_campaigns/campaigns/comment_marked_as_spam.rb
+++ engines/email_campaigns/app/models/email_campaigns/campaigns/comment_marked_as_spam.rb
@@ -1,5 +1,5 @@
 module EmailCampaigns
-  class Campaigns::CommentMarkedAsSpam < Campaigns::NotificationCampaign
+  class Campaigns::CommentMarkedAsSpam < Campaign
     include Consentable
     include ActivityTriggerable
     include RecipientConfigurable
@@ -16,6 +16,26 @@ module EmailCampaigns
 
     def activity_triggers
       {'Notifications::CommentMarkedAsSpam' => {'created' => true}}
+    end
+
+    def filter_notification_recipient users_scope, activity:, time: nil
+      users_scope.where(id: activity.item.recipient.id)
+    end
+
+    def generate_commands recipient:, activity:, time: nil
+      notification = activity.item
+      [{
+        event_payload: {
+          initiating_user_first_name: notification.initiating_user&.first_name,
+          initiating_user_last_name: notification.initiating_user&.last_name,
+          post_title_multiloc: notification.post.title_multiloc,
+          comment_author_name: notification.comment.author_name,
+          comment_body_multiloc: notification.comment_body_multiloc,
+          comment_url: Frontend::UrlService.new.model_to_url(notification.comment, locale: recipient.locale),
+          spam_report_reason_code: notification.spam_report.reason_code,
+          spam_report_other_reason: notification.spam_report.other_reason
+        }
+      }]
     end
   end
 end
