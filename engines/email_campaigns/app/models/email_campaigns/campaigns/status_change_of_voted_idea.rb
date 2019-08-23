1908d758a3c8b940e4aaa7bcc60ad7e2cbb1d4e2
Sébastien Hoorens <sebastien@citizenlab.co>
2019-08-23 13:58:41 +0200

    refactoring emails: status changes

--- engines/email_campaigns/app/models/email_campaigns/campaigns/status_change_of_voted_idea.rb
+++ engines/email_campaigns/app/models/email_campaigns/campaigns/status_change_of_voted_idea.rb
@@ -27,24 +27,19 @@ module EmailCampaigns
       status = idea.idea_status
       [{
         event_payload: {
-          idea: {
-            id: idea.id,
-            title_multiloc: idea.title_multiloc,
-            body_multiloc: idea.body_multiloc,
-            url: Frontend::UrlService.new.model_to_url(idea, locale: recipient.locale),
-            idea_images: idea.idea_images.map{ |image|
-              {
-                ordering: image.ordering,
-                versions: image.image.versions.map{|k, v| [k.to_s, v.url]}.to_h
-              }
-            }
-          },
-          idea_status: {
-            id: status.id,
-            title_multiloc: status.title_multiloc,
-            code: status.code,
-            color: status.color
-          }
+          post_id: idea.id,
+          post_title_multiloc: idea.title_multiloc,
+          post_body_multiloc: idea.body_multiloc,
+          post_url: Frontend::UrlService.new.model_to_url(idea, locale: recipient.locale),
+          post_images: idea.idea_images.map{ |image|
+            {
+              ordering: image.ordering,
+              versions: image.image.versions.map{|k, v| [k.to_s, v.url]}.to_h
+            },
+          idea_status_id: status.id
+          idea_status_title_multiloc: status.title_multiloc,
+          idea_status_code: status.code,
+          idea_status_color: status.color
         }
       }]
     end
