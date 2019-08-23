46cb028a6085fc48443c0588e648af5c968e67fe
Sébastien Hoorens <sebastien@citizenlab.co>
2019-08-23 11:36:21 +0200

    refactoring emails: project stuff

--- engines/email_campaigns/app/models/email_campaigns/campaigns/project_moderation_rights_received.rb
+++ engines/email_campaigns/app/models/email_campaigns/campaigns/project_moderation_rights_received.rb
@@ -12,5 +12,21 @@ module EmailCampaigns
     def activity_triggers
       {'Notifications::ProjectModerationRightsReceived' => {'created' => true}}
     end
+
+    def filter_notification_recipient users_scope, activity:, time: nil
+      users_scope.where(id: activity.item.recipient.id)
+    end
+
+    def generate_commands recipient:, activity:, time: nil
+      notification = activity.item
+      [{
+        event_payload: {
+          project_id: notification.project.id,
+          project_title_multiloc: notification.project.title_multiloc,
+          project_ideas_count: notification.project.ideas_count,
+          project_url: Frontend::UrlService.new.model_to_url(notification.project, locale: recipient.locale)
+        }
+      }]
+    end
   end
 end
