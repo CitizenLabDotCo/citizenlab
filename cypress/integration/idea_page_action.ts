bf901a98df375c5e493e08f9a8c7446a1eb4d661
DavidCnoops <david@citizenlab.co>
2019-08-29 15:01:28 +0200

    fix for initiative and idea official feedback not properly working + e2e tests

--- cypress/integration/idea_page_action.ts
+++ cypress/integration/idea_page_action.ts
@@ -40,14 +40,14 @@ describe('Idea show page actions', () => {
       cy.get('.e2e-submit-wrapper-button').should('have.class', 'disabled');
       cy.wait(2000);
 
+      cy.get('.e2e-official-feedback-post');
       cy.get('.e2e-official-feedback-post').contains(officialFeedback);
       cy.get('.e2e-official-feedback-post').contains(officialFeedback);
 
       // delete
       cy.get('.e2e-official-feedback-post').find('button').first().click();
       cy.get('.e2e-official-feedback-post').find('.e2e-action-delete').click();
-
-      cy.wait(1000);
+      cy.wait(2000);
       cy.get('.e2e-official-feedback-post').should('not.exist');
     });
   });
