c207488de014b356eb38fddb8b7a762d81e44825
Sara <sara@citizenlab.co>
2019-09-02 16:54:44 +0200

    Support a blacklist of segment destinations set on tenant object
    - updates tests accordingly
    - TODO: react to a change in tenant settings

---
+++ app/components/ConsentManager/ConsentManagerBuilderHandler.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { ConsentManagerProps, IDestination } from './';
+import { ADVERTISING_CATEGORIES, FUNCTIONAL_CATEGORIES } from './categories';
+
+interface Props extends ConsentManagerProps {
+  blacklistedDestinations: string[];
+}
+
+import Container from './Container';
+
+const getCategorizedDestinations = (destinationArray: IDestination[]) => {
+  const categorizedDestinations = {
+    advertising: [] as IDestination[],
+    functional: [] as IDestination[],
+    analytics: [] as IDestination[],
+  };
+  for (const destination of destinationArray) {
+    if (ADVERTISING_CATEGORIES.find(c => c === destination.category)) {
+      categorizedDestinations.advertising.push(destination);
+    } else if (FUNCTIONAL_CATEGORIES.find(c => c === destination.category)) {
+      categorizedDestinations.functional.push(destination);
+    } else {
+      // Fallback to analytics
+      categorizedDestinations.analytics.push(destination);
+    }
+  }
+  return categorizedDestinations;
+};
+
+const ConsentManagerBuilderHandler = ({
+  setPreferences,
+  resetPreferences,
+  saveConsent,
+  newDestinations,
+  destinations,
+  preferences,
+  blacklistedDestinations
+}: Props) => {
+
+  const filteredNewDestinations = newDestinations.filter(destination => !blacklistedDestinations.includes(destination.id));
+  const filteredDestinations = destinations.filter(destination => !blacklistedDestinations.includes(destination.id));
+  const categorizedDestinations = getCategorizedDestinations(filteredDestinations);
+  const isConsentRequired = filteredNewDestinations.length > 0;
+
+  return (
+    <Container
+      {...{
+        setPreferences,
+        resetPreferences,
+        saveConsent,
+        isConsentRequired,
+        preferences,
+        categorizedDestinations
+      }}
+    />
+  );
+};
+
+export default ConsentManagerBuilderHandler;
