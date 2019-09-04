cde896f6ae78b3534958c3b8d305b138b5368816
Brent Guffens <brentguf@users.noreply.github.com>
2019-09-04 16:58:51 +0200

    find-unused-messages script

--- internals/scripts/find-unused-messages.js
+++ internals/scripts/find-unused-messages.js
@@ -1,18 +1,13 @@
-const findInFiles = require('find-in-files');
 const translationMessages = require('../../app/translations/en.json');
+const { execSync } = require('child_process');
 
 Object.keys(translationMessages).forEach(messageKey => {
   const lastPartMessageKey = messageKey.match(/[^.]+$/)[0];
-  findInFiles.find(`messages.${lastPartMessageKey}`, 'app', /^(?!messages).*\.(js|ts|tsx)$/).then(mathces => {
-    const filesThatUseMessageKey = [];
-    for (const match in matches) {
-      const res = matches[match];
-      filesThatUseMessageKey.push(res);
-    }
 
-    if (filesThatUseMessageKey.length === 0) {
-      console.log(messageKey);
-    }
-  });
-
+  try {
+    // requires linux environment
+    execSync(`grep -rn --exclude=messages.*s 'app' -e 'messages.${lastPartMessageKey}'`);
+  } catch (err) {
+    console.log(`unused: ${messageKey}`);
+  }
 });
