81c1f54c4d70841ab081368d27cdfba0a9977160
Sébastien Hoorens <sebastien@citizenlab.co>
2019-09-05 14:29:06 +0200

    forgotten initiative email attributes: votes_needed and expires_at

--- engines/email_campaigns/app/models/email_campaigns/campaigns/initiative_assigned_to_you.rb
+++ engines/email_campaigns/app/models/email_campaigns/campaigns/initiative_assigned_to_you.rb
@@ -29,7 +29,9 @@ module EmailCampaigns
           post_body_multiloc: notification.post.body_multiloc,
           post_author_name: notification.post.author_name,
           post_published_at: notification.post.published_at.iso8601,
-          post_url: Frontend::UrlService.new.model_to_url(notification.post, locale: recipient.locale)
+          post_url: Frontend::UrlService.new.model_to_url(notification.post, locale: recipient.locale),
+          initiative_votes_needed: notification.post.votes_needed,
+          initiative_expires_at: notification.post.expires_at.iso8601
         }
       }]
     end
--- engines/email_campaigns/app/models/email_campaigns/campaigns/new_initiative_for_admin.rb
+++ engines/email_campaigns/app/models/email_campaigns/campaigns/new_initiative_for_admin.rb
@@ -29,7 +29,9 @@ module EmailCampaigns
           post_published_at: notification.post.published_at.iso8601,   
           post_title_multiloc: notification.post.title_multiloc,
           post_author_name: notification.post.author_name,
-          post_url: Frontend::UrlService.new.model_to_url(notification.post, locale: recipient.locale)
+          post_url: Frontend::UrlService.new.model_to_url(notification.post, locale: recipient.locale),
+          initiative_votes_needed: notification.post.votes_needed,
+          initiative_expires_at: notification.post.expires_at.iso8601
         }
       }]
     end
