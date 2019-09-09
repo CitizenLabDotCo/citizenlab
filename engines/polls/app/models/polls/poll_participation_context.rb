28492a14bed8176de94a9b1d6c85937165263606
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2019-09-09 15:16:56 +0200

    CL2-4242 Added validation to not allow changing participation_method

--- engines/polls/app/models/polls/poll_participation_context.rb
+++ engines/polls/app/models/polls/poll_participation_context.rb
@@ -4,10 +4,20 @@ module Polls::PollParticipationContext
   included do
     has_many :poll_questions, class_name: 'Polls::Question', as: :participation_context, dependent: :destroy
     has_many :poll_responses, class_name: 'Polls::Response', as: :participation_context, dependent: :destroy
+
+	  # for timeline projects, the phases are the participation contexts, so nothing applies
+    with_options unless: :is_timeline_project? do
+      validate :poll_questions_allowed_in_participation_method
+    end
   end
 
   def poll?
     self.participation_method == 'poll'
   end
 
+  def poll_questions_allowed_in_participation_method
+    if !poll? && poll_questions.present?
+      errors.add(:base, :cannot_contain_poll_questions, questions_count: poll_questions.size, message: 'cannot contain poll questions in the current non-poll participation context')
+    end
+  end
 end
