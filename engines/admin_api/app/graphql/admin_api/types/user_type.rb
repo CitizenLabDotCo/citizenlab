3dfb6563fd21dd94788ace19c4b7865c716ba77c
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2019-09-09 13:41:56 +0200

    Added admin API fields for kiosk

--- engines/admin_api/app/graphql/admin_api/types/user_type.rb
+++ engines/admin_api/app/graphql/admin_api/types/user_type.rb
@@ -2,11 +2,30 @@ module AdminApi
   class Types::UserType < GraphQL::Schema::Object
     description "A registered or invited person on the platform"
 
+    class UserAvatar < GraphQL::Schema::Object
+      field :small_url, String, null: false
+      def small_url
+        object.versions[:small]
+      end
+
+      field :medium_url, String, null: false
+      def medium_url
+        object.versions[:medium]
+      end
+
+      field :large_url, String, null: false
+      def large_url
+        object.versions[:large]
+      end
+    end
+
     field :id, ID, null: false
     field :first_name, String, null: true
     field :last_name, String, null: true
     field :email, String, null: true
     field :slug, String, null: true
     field :locale, String, null: true
+    field :avatar, UserAvatar, null: true
+
   end
 end
