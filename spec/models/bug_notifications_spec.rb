063954d37f133db1e93d38f1aae31ca009cbba2c
Sébastien Hoorens <sebastien@citizenlab.co>
2019-09-10 17:17:33 +0200

    much more fixed

--- spec/models/bug_notifications_spec.rb
+++ spec/models/bug_notifications_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Notification, type: :model do
     let!(:initiator) { create(:user) }
     let!(:recipient) { create(:user) }
     let!(:spam_report) { create(:spam_report) }
-    let!(:notification) { create(:comment_marked_as_spam, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient, spam_report: spam_report) }
+    let!(:notification) { create(:comment_marked_as_spam, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient, spam_report: spam_report) }
     
 
     it "deleting recipient of notification" do
@@ -42,94 +42,94 @@ RSpec.describe Notification, type: :model do
     end
 
     it "deleting parent of comment of comment on your comment notification" do
-      coyc = create(:comment_on_your_comment, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient)
+      coyc = create(:comment_on_your_comment, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient)
       parent.destroy!
     end
 
     it "deleting idea of comment on your comment notification" do
-      coyc = create(:comment_on_your_comment, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient)
+      coyc = create(:comment_on_your_comment, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient)
       idea.destroy
     end
 
     it "deleting project of comment on your comment notification" do
-      coyc = create(:comment_on_your_comment, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient)
+      coyc = create(:comment_on_your_comment, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient)
       project.destroy!
     end
 
     it "deleting spam report of comment on your comment notification" do
-      coyc = create(:comment_on_your_comment, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient)
+      coyc = create(:comment_on_your_comment, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient)
       spam_report.destroy!
     end
 
     it "deleting initiator of comment on your comment notification" do
-      coyc = create(:comment_on_your_comment, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient)
+      coyc = create(:comment_on_your_comment, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient)
       initiator.destroy!
     end
 
     it "deleting recipient of comment on your comment notification" do
-      coyc = create(:comment_on_your_comment, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient)
+      coyc = create(:comment_on_your_comment, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient)
       recipient.destroy!
     end
 
 
     it "deleting parent of comment of comment on your idea notification" do
-      coyi = create(:comment_on_your_idea, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient)
+      coyi = create(:comment_on_your_idea, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient)
       parent.destroy!
     end
 
     it "deleting idea of comment on your idea notification" do
-      coyi = create(:comment_on_your_idea, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient)
+      coyi = create(:comment_on_your_idea, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient)
       idea.destroy!
     end
 
     it "deleting project of comment on your idea notification" do
-      coyi = create(:comment_on_your_idea, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient)
+      coyi = create(:comment_on_your_idea, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient)
       project.destroy!
     end
 
     it "deleting spam report of comment on your idea notification" do
-      coyi = create(:comment_on_your_idea, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient)
+      coyi = create(:comment_on_your_idea, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient)
       spam_report.destroy!
     end
 
     it "deleting initiator of comment on your idea notification" do
-      coyi = create(:comment_on_your_idea, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient)
+      coyi = create(:comment_on_your_idea, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient)
       initiator.destroy!
     end
 
     it "deleting recipient of comment on your idea notification" do
-      coyi = create(:comment_on_your_idea, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient)
+      coyi = create(:comment_on_your_idea, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient)
       recipient.destroy!
     end
 
 
     it "deleting parent of comment of comment marked as spam notification" do
-      notif = create(:comment_marked_as_spam, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient, spam_report: spam_report)
+      notif = create(:comment_marked_as_spam, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient, spam_report: spam_report)
       parent.destroy!
     end
 
     it "deleting idea of comment marked as spam notification" do
-      notif = create(:comment_marked_as_spam, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient, spam_report: spam_report)
+      notif = create(:comment_marked_as_spam, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient, spam_report: spam_report)
       idea.destroy!
     end
 
     it "deleting project of comment marked as spam notification" do
-      notif = create(:comment_marked_as_spam, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient, spam_report: spam_report)
+      notif = create(:comment_marked_as_spam, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient, spam_report: spam_report)
       project.destroy!
     end
 
     it "deleting spam report of comment marked as spam notification" do
-      notif = create(:comment_marked_as_spam, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient, spam_report: spam_report)
+      notif = create(:comment_marked_as_spam, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient, spam_report: spam_report)
       spam_report.destroy!
     end
 
     it "deleting initiator of comment marked as spam notification" do
-      notif = create(:comment_marked_as_spam, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient, spam_report: spam_report)
+      notif = create(:comment_marked_as_spam, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient, spam_report: spam_report)
       initiator.destroy!
     end
 
     it "deleting recipient of comment marked as spam notification" do
-      notif = create(:comment_marked_as_spam, project: project, idea: idea, comment: child, initiating_user: initiator, recipient: recipient, spam_report: spam_report)
+      notif = create(:comment_marked_as_spam, project: project, post: idea, comment: child, initiating_user: initiator, recipient: recipient, spam_report: spam_report)
       recipient.destroy!
     end
 
