98119df924050e690dc60ae269825481b168c980
Sébastien Hoorens <sebastien@citizenlab.co>
2019-09-13 17:47:09 +0200

    stop wanking

---
+++ engines/polls/app/services/polls/export_service.rb
@@ -0,0 +1,32 @@
+module Polls
+	class ExportService
+
+		def generate_poll_results_xlsx participation_context, users: nil
+      multiloc_service = MultilocService.new
+      questions = Polls::Question.where(participation_context: participation_context)
+      responses = Polls::Response.where(participation_context: participation_context)
+
+      pa = Axlsx::Package.new
+      wb = pa.workbook
+      wb.styles do |s|
+        wb.add_worksheet(name: 'Poll results') do |sheet|
+          sheet.add_row [
+            'id',
+            'email',
+            *questions.map{|q| multiloc_service.t(q.title_multiloc)}
+          ], style: XlsxService.new.header_style(s)
+          responses.includes(:user).each do |response|
+            sheet.add_row [
+              response.user.id,
+              response.user.email,
+              *response.response_options.map{|r| multiloc_service.t(r.option.title_multiloc)}
+            ]
+          end
+          sheet.column_info[2].width = 65
+        end
+      end
+      pa.to_stream
+    end
+
+	end
+end
