e214f8ea40db9ecf01b8cba0626fea16b87e9d6c
Sébastien Hoorens <sebastien@citizenlab.co>
2019-09-16 16:25:08 +0200

    project copy works, updated seed and template extraction

--- engines/polls/app/services/polls/export_service.rb
+++ engines/polls/app/services/polls/export_service.rb
@@ -4,22 +4,27 @@ module Polls
 		def generate_poll_results_xlsx participation_context, users: nil
       multiloc_service = MultilocService.new
       questions = Polls::Question.where(participation_context: participation_context)
+        .order(:ordering)
       responses = Polls::Response.where(participation_context: participation_context)
+        .includes(response_options: [:option]).order(:created_at)
 
       pa = Axlsx::Package.new
       wb = pa.workbook
       wb.styles do |s|
         wb.add_worksheet(name: 'Poll results') do |sheet|
           sheet.add_row [
-            'id',
-            'email',
+            'User ID',
+            'Email',
             *questions.map{|q| multiloc_service.t(q.title_multiloc)}
           ], style: XlsxService.new.header_style(s)
           responses.includes(:user).each do |response|
             sheet.add_row [
               response.user.id,
               response.user.email,
-              *response.response_options.map{|r| multiloc_service.t(r.option.title_multiloc)}
+              *response.response_options
+                .group_by{|ro| ro.option.question.ordering}
+                .sort_by{|ordering, _| ordering}
+                .map{|_, ros| ros.map{|ro| multiloc_service.t(ro.option.title_multiloc)}.join(', ')}
             ]
           end
           sheet.column_info[2].width = 65
