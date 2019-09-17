5784fe21098408c6e55f47c8ebefa3e64e03c71e
Sébastien Hoorens <sebastien@citizenlab.co>
2019-09-17 15:05:17 +0200

    added script to fix existing tenants

--- app/services/custom_field_service.rb
+++ app/services/custom_field_service.rb
@@ -56,21 +56,20 @@ class CustomFieldService
       .update_all("custom_field_values = custom_field_values - '#{field.key}'")
   end
 
-  def delete_custom_field_option_values option
-    field = option.custom_field
+  def delete_custom_field_option_values option_key, field
     if field.input_type == 'multiselect'
       # When option is the only selection
       User
-        .where("custom_field_values->>'#{field.key}' = ?", [option.key].to_json)
+        .where("custom_field_values->>'#{field.key}' = ?", [option_key].to_json)
         .update_all("custom_field_values = custom_field_values - '#{field.key}'")
       # When option was selected amongst other values
       User
-        .where("(custom_field_values->>'#{field.key}')::jsonb ? :value", value: option.key)
-        .update_all("custom_field_values = jsonb_set(custom_field_values, '{#{field.key}}', (custom_field_values->'#{field.key}') - '#{option.key}')")
+        .where("(custom_field_values->>'#{field.key}')::jsonb ? :value", value: option_key)
+        .update_all("custom_field_values = jsonb_set(custom_field_values, '{#{field.key}}', (custom_field_values->'#{field.key}') - '#{option_key}')")
     else
       # When single select
       User
-        .where("custom_field_values->>'#{field.key}' = ?", option.key)
+        .where("custom_field_values->>'#{field.key}' = ?", option_key)
         .update_all("custom_field_values = custom_field_values - '#{field.key}'")
     end
   end
