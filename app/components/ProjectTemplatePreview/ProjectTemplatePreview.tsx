d5b35f62648b177c5e47e4995b67e53c82124242
DavidCnoops <david@citizenlab.co>
2019-09-18 16:32:45 +0200

    project templates WIP

--- app/components/ProjectTemplatePreview/ProjectTemplatePreview.tsx
+++ app/components/ProjectTemplatePreview/ProjectTemplatePreview.tsx
@@ -1,6 +1,8 @@
-import React, { memo, useCallback, useState } from 'react';
+import React, { memo, useCallback, useState, useEffect } from 'react';
 import { adopt } from 'react-adopt';
 import { isNilOrError, transformLocale } from 'utils/helperUtils';
+import * as clipboard from 'clipboard-polyfill';
+import { removeLocale } from 'utils/cl-router/updateLocationDescriptor';
 
 // resources
 import GetLocale, { GetLocaleChildProps } from 'resources/GetLocale';
@@ -40,7 +42,10 @@ const Header = styled.div`
 
 const HeaderLeft = styled.div``;
 
-const HeaderRight = styled.div``;
+const HeaderRight = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
 const Title = styled.h1`
   color: ${colors.adminTextColor};
@@ -60,6 +65,28 @@ const Subtitle = styled.h2`
   padding: 0;
   margin-top: 0;
   margin-bottom: 5px;
+`;
+
+const LinkCopied = styled.div`
+  color: ${colors.clGreenSuccess};
+  display: flex;
+  align-items: center;
+  opacity: 0;
+  transition: all 150ms ease-out;
+
+  &.visible {
+    opacity: 1;
+  }
+`;
+
+const LinkCopiedIcon = styled(Icon)`
+  fill: ${colors.clGreenSuccess};
+  height: 13px;
+  margin-right: 3px;
+`;
+
+const CopyLinkButton = styled(Button)`
+  margin-left: 20px;
 `;
 
 const MetaInfo = styled.div`
@@ -180,6 +207,8 @@ interface Props extends DataProps, InputProps { }
 
 const ProjectTemplatePreview = memo<Props>(({ locale, projectTemplateId, className }) => {
 
+  const [linkCopied, setLinkCopied] = useState(false);
+
   const graphQLLocale = !isNilOrError(locale) ? transformLocale(locale) : null;
 
   const TEMPLATE_QUERY = gql`
@@ -224,86 +253,19 @@ const ProjectTemplatePreview = memo<Props>(({ locale, projectTemplateId, classNa
     }
   `;
 
-  /*
-  "projectTemplate": {
-    "id": "fe2923ef-cbf4-44b6-894f-82145d5566df",
-    "headerImage": "http://localhost:5001/rails/active_storage/representations/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCdz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--fd5fa014b1f562850827ff575450ba5ac7f88c0a/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCam9UY21WemFYcGxYM1J2WDJacGJHeGJCMmtDaEFOcEFnUUIiLCJleHAiOm51bGwsInB1ciI6InZhcmlhdGlvbiJ9fQ==--02e9050b019b1cfc3a1ee1031bb943fabb902b22/project_template_header_1.png",
-    "departments": [
-      {
-        "id": "23db4556-7b79-4e62-9990-22c25573cbbc",
-        "titleMultiloc": {
-          "en": "Mobility",
-          "__typename": "Multiloc"
-        },
-        "__typename": "Department"
-      }
-    ],
-    "participationLevels": [
-      {
-        "id": "a94ef897-48a9-48d5-8e74-a4f6f6f9b056",
-        "titleMultiloc": {
-          "en": "Inform",
-          "__typename": "Multiloc"
-        },
-        "__typename": "ParticipationLevel"
-      },
-      {
-        "id": "3e3d8eb1-0e1f-4b47-9156-134f9e6c1f53",
-        "titleMultiloc": {
-          "en": "Involve",
-          "__typename": "Multiloc"
-        },
-        "__typename": "ParticipationLevel"
-      }
-    ],
-    "purposes": [
-      {
-        "id": "254f38bb-5bc9-4eff-b2f9-6acdcf91cf90",
-        "titleMultiloc": {
-          "en": "Engaging citizens",
-          "__typename": "Multiloc"
-        },
-        "__typename": "Purpose"
-      }
-    ],
-    "titleMultiloc": {
-      "en": "Mobility survey",
-      "__typename": "Multiloc"
-    },
-    "subtitleMultiloc": {
-      "en": "Question citizens on a specific mobility proposal",
-      "__typename": "Multiloc"
-    },
-    "descriptionMultilocs": [
-      {
-        "content": "<div class=\"trix-content\">\n  monoplast uncompassionate parasubstituted Trionychidae Fatimid Discoplacentalia aggrade heterologically gharnao neurilemmatous pseudometamerism pterygoquadrate timberyard tagwerk yuzluk overjoyful cluttery unfostered counterreply orphanhood cob imperfectious handiwork Sufiism\n</div>\n",
-        "locale": "en",
-        "__typename": "RichMultiloc"
-      }
-    ],
-    "successCases": [
-      {
-        "id": "50d4ba57-47ad-4ed9-9f25-e1e7db949e1a",
-        "href": "https://www.citizenlab.co",
-        "image": "http://localhost:5001/rails/active_storage/representations/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBDQT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--ffc2453ed528cdae871a711a06b595eca1ecefd5/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCam9UY21WemFYcGxYM1J2WDJacGJHeGJCMmxwYVRjPSIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--49b653362b9b1dd2d26a8ae723493a3581e3a0d9/success_case_image_1.png",
-        "__typename": "SuccessCase"
-      },
-      {
-        "id": "87b54854-08be-4998-b8e2-bfb1cf0ea25e",
-        "href": "https://www.citizenlab.co",
-        "image": "http://localhost:5001/rails/active_storage/representations/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBDUT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--b41ec19417a5ba81d0eb41aeafccb243b02d1b58/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCam9UY21WemFYcGxYM1J2WDJacGJHeGJCMmxwYVRjPSIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--49b653362b9b1dd2d26a8ae723493a3581e3a0d9/success_case_image_1.png",
-        "__typename": "SuccessCase"
-      }
-    ],
-    "__typename": "ProjectTemplate"
-  }
-  */
-
   const { loading, data } = useQuery(TEMPLATE_QUERY);
 
   const copyLink = useCallback(() => {
-    // empty
+    const copiedUrl = window.location.origin + removeLocale(window.location.pathname).pathname;
+    clipboard.writeText(copiedUrl);
+    setLinkCopied(true);
   }, []);
+
+  useEffect(() => {
+    if (linkCopied) {
+      setTimeout(() => setLinkCopied(false), 2000);
+    }
+  }, [linkCopied]);
 
   if (!loading && data) {
     return (
@@ -315,13 +277,17 @@ const ProjectTemplatePreview = memo<Props>(({ locale, projectTemplateId, classNa
           </HeaderLeft>
 
           <HeaderRight>
-            <Button
+            <LinkCopied className={linkCopied ? 'visible' : 'hidden'}>
+              <LinkCopiedIcon name="checkmark" />
+              <FormattedMessage {...messages.copied} />
+            </LinkCopied>
+            <CopyLinkButton
               onClick={copyLink}
               icon="link"
               style="secondary"
             >
               <FormattedMessage {...messages.copyLink} />
-            </Button>
+            </CopyLinkButton>
           </HeaderRight>
         </Header>
 
