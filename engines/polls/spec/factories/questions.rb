4e41edbf16a0de4a2d2ac36555d27424c99669c6
Sébastien Hoorens <sebastien@citizenlab.co>
2019-09-18 16:30:40 +0200

    updated specs to cover adding/removing questions/options

--- engines/polls/spec/factories/questions.rb
+++ engines/polls/spec/factories/questions.rb
@@ -1,10 +1,12 @@
 FactoryBot.define do
   factory :poll_question, class: 'Polls::Question' do
     participation_context { create(:continuous_poll_project) }
-    title_multiloc {{
-      "en" => "How many townhalls would you like to have?",
-      "nl-BE" => "Hoeveel gemeentehuizen wil je?"
-    }}
+    sequence(:title_multiloc) do |n|
+      {
+        "en" => "How many #{n} townhalls would you like to have?",
+        "nl-BE" => "Hoeveel #{n} gemeentehuizen wil je?"
+      }
+    end
 
     trait :with_options do
       after :create do |question|
