bf8e221de52a4009e7ca562bd37e2b58b2781b44
Sara <sara@citizenlab.co>
2019-09-19 14:34:04 +0200

    Fixes puntuaation in a message

--- app/containers/ProjectsShowPage/PollSection/messages.ts
+++ app/containers/ProjectsShowPage/PollSection/messages.ts
@@ -3,7 +3,7 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   formCompleted: {
     id: 'app.containers.Projects.PollForm.formCompleted',
-    defaultMessage: 'Thank you for answering this poll !',
+    defaultMessage: 'Thank you for answering this poll!',
   },
   sendAnswer: {
     id: 'app.containers.Projects.PollForm.sendAnswer',
