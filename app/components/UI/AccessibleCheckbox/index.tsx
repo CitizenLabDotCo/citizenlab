a961d40717e9fac2f45a889f3cf9a9dc97c10991
Brent Guffens <brentguf@users.noreply.github.com>
2019-09-24 16:58:28 +0200

    Outline

--- app/components/UI/AccessibleCheckbox/index.tsx
+++ app/components/UI/AccessibleCheckbox/index.tsx
@@ -34,7 +34,7 @@ const InputWrapper: any = styled.div`
   box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.15);
 
   &.focused {
-    outline: rgb(59, 153, 252) auto 5px;
+    outline: rgb(59, 153, 252) solid 2px;
   }
 
   &:hover {
