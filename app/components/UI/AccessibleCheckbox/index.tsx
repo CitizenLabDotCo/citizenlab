cf12371ae02a3f447aa2afbbf6e92df2a9300193
Brent Guffens <brentguf@users.noreply.github.com>
2019-09-24 10:24:50 +0200

    WIP

--- app/components/UI/AccessibleCheckbox/index.tsx
+++ app/components/UI/AccessibleCheckbox/index.tsx
@@ -1,61 +1,66 @@
 import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 import { colors } from 'utils/styleUtils';
-
-interface Props {
-  label?: string | JSX.Element | null | undefined;
-}
-
-type State = {};
+import Icon from 'components/UI/Icon';
 
 const Container = styled.div`
   display: flex;
   align-items: center;
 `;
 
-const InputWrapper = styled.div`
+const InputWrapper: any = styled.div`
   position: relative;
-  height: 22px;
-  width: 22px;
-`;
+  flex: 0 0 ${(props: any) => props.size};
+  width: ${(props: any) => props.size};
+  height: ${(props: any) => props.size};
+  color: #fff;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  border: solid 1px #aaa;
+  border-radius: ${(props: any) => props.theme.borderRadius};
+  background: ${(props: any) => props.checked ? colors.clGreen : '#fff'};
+  border-color: ${(props: any) => props.checked ? colors.clGreen : '#aaa'};
+  box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.15);
 
-const Input = styled.input`
-  opacity: 0;
-
-  &:focus + #checkmark::before {
+  &.focused {
     outline: rgb(59, 153, 252) auto 5px;
   }
 
-  & + #checkmark::after {
-    content: none;
-  }
-
-  &:checked + #checkmark::after {
-    content: "";
+  &:hover {
+    border-color: ${(props: any) => props.checked ? colors.clGreen : '#333'};
   }
 `;
 
-const CheckmarkBox = styled.div`
-  &::before, &::after {
+const Input = styled.input`
+  &[type='checkbox'] {
+    // opacity: 0;
     position: absolute;
+    left: -50px;
   }
+`;
 
-  &::before {
-    content: "";
-    display: inline-block;
-    height: 22px;
-    width: 22px;
-    border: 1px solid #aaa;
-    border-radius: ${(props: any) => props.theme.borderRadius};
-    box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.15);
-    background: ${(props: any) => props.checked ? colors.clGreen : '#fff'};
-    left: 50%;
-    margin-left: -11px;
-    top: 50%;
-    margin-top: -11px;
-  }
+const CheckmarkIcon = styled(Icon)`
+  position: absolute;
+  fill: #fff;
+  height: 55%;
+`;
 
-  &::after {
+const CheckmarkBox = styled.div`
+  position: absolute;
+  top: 0;
+  z-index: 10;
+  height: 22px;
+  width: 22px;
+  min-width: 22px;
+  border: 1px solid #aaa;
+  border-radius: ${(props: any) => props.theme.borderRadius};
+  box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.15);
+  background: ${(props: any) => props.checked ? colors.clGreen : '#fff'};
+
+  &.checked::after {
     content: "";
     display: inline-block;
     height: 6px;
@@ -70,21 +75,77 @@ const Label = styled.label`
   margin-left: 15px;
 `;
 
+interface DefaultProps {
+  size?: string;
+}
+
+interface Props extends DefaultProps {
+  label?: string | JSX.Element | null | undefined;
+}
+
+interface State {
+  checked: boolean;
+  inputFocused: boolean;
+}
+
 export default class AccessibleCheckbox extends PureComponent<Props, State> {
+   checkbox = React.createRef<HTMLInputElement>();
+
+  static defaultProps: DefaultProps = {
+    size: '22px'
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: false,
+      inputFocused: false
+    };
+  }
+
+  handleOnChange = () => {
+    this.setState(prevState => ({
+      checked: !prevState.checked
+    }));
+  }
+
+  handleOnFocus = () => {
+    this.setState({
+      inputFocused: true
+    });
+  }
+
+  handleOnBlur = () => {
+    this.setState({
+      inputFocused: false
+    });
+  }
+
   render() {
-    const { label } = this.props;
+    const { label, size } = this.props;
+    const { checked, inputFocused } = this.state;
 
     return (
       <Container>
-        <InputWrapper>
+        {/* <InputWrapper>
           <Input type="checkbox" id="checkbox" />
-          <CheckmarkBox id="checkmark" />
+        </InputWrapper> */}
+        <InputWrapper className={ inputFocused ? 'focused' : '' } onClick={this.handleOnChange} checked={checked} size={size}>
+          <Input
+            ref={this.checkbox}
+            id="checkbox"
+            aria-checked={checked}
+            type="checkbox"
+            checked={checked}
+            onFocus={this.handleOnFocus}
+            onBlur={this.handleOnBlur}
+          />
+          {checked && <CheckmarkIcon ariaHidden name="checkmark" />}
         </InputWrapper>
+
         {label &&
           <Label htmlFor="checkbox" id="label">
-            {/* {label} */}
-            This is a test. This is a test.
-            This is a test. This is a test.This is a test. This is a test.This is a test. This is a test.This is a test. This is a test.
+            {label}
           </Label>
         }
       </Container>
