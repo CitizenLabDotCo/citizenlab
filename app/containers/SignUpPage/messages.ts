eeb2e1c26cec9efc4ef7dc9c36b49d9f34559b9b
Brent Guffens <brentguf@users.noreply.github.com>
2019-09-25 16:39:40 +0200

    SignUpPageMeta

--- app/containers/SignUpPage/messages.ts
+++ app/containers/SignUpPage/messages.ts
@@ -5,4 +5,12 @@ export default defineMessages({
     id: 'app.containers.SignUpPage.invitationTitle',
     defaultMessage: 'Redeem your invitation',
   },
+  metaTitle: {
+    id: 'app.containers.SignUpPage.metaTitle',
+    defaultMessage: 'Log into the CitizenLab platform of {tenantName} | CitizenLab',
+  },
+  metaDescription: {
+    id: 'app.containers.SignUpPage.metaDescription',
+    defaultMessage: 'Log into the CitizenLab platform of {tenantName} to share your ideas, start an initiative or contribute to those of others.',
+  }
 });
