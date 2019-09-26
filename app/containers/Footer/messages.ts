48e5276ccc35418a24462498ed4b2ca535ac7c0a
Brent Guffens <brentguf@users.noreply.github.com>
2019-09-26 17:02:15 +0200

    New Footer component (WIP)

---
+++ app/containers/Footer/messages.ts
@@ -0,0 +1,72 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  poweredBy: {
+    id: 'app.containers.Footer.poweredBy',
+    defaultMessage: 'Powered by',
+  },
+  information: {
+    id: 'app.containers.Footer.information',
+    defaultMessage: 'Information',
+  },
+  'terms-and-conditions': {
+    id: 'app.containers.Footer.terms-and-conditions',
+    defaultMessage: 'Terms and Conditions',
+  },
+  'privacy-policy': {
+    id: 'app.containers.Footer.privacy-policy',
+    defaultMessage: 'Privacy policy',
+  },
+  cookieSettings: {
+    id: 'app.containers.Footer.cookieSettings',
+    defaultMessage: 'Cookie settings',
+  },
+  feedbackQuestion: {
+    id: 'app.containers.Footer.feedbackQuestion',
+    defaultMessage: 'Did you find what you\'re looking for?'
+  },
+  yes: {
+    id: 'app.containers.Footer.yes',
+    defaultMessage: 'Yes'
+  },
+  no: {
+    id: 'app.containers.Footer.no',
+    defaultMessage: 'No'
+  },
+  thanksForFeedback: {
+    id: 'app.containers.Footer.thanksForFeedback',
+    defaultMessage: 'Thanks for your feedback!'
+  },
+  sendFeedback: {
+    id: 'app.containers.Footer.sendFeedback',
+    defaultMessage: 'Send your feedback'
+  },
+  feedbackModalTitle: {
+    id: 'app.containers.Footer.feedbackModalTitle',
+    defaultMessage: 'What were you looking for?'
+  },
+  email: {
+    id: 'app.containers.Footer.email',
+    defaultMessage: 'Email (optional)'
+  },
+  feedback: {
+    id: 'app.containers.Footer.feedback',
+    defaultMessage: 'Let us know what you didn’t find on this page:'
+  },
+  submit: {
+    id: 'app.containers.Footer.submit',
+    defaultMessage: 'Submit'
+  },
+  moreInfo: {
+    id: 'app.containers.Footer.moreInfo',
+    defaultMessage: '... let us know more'
+  },
+  feedbackSuccessfullySubmitted: {
+    id: 'app.containers.Footer.feedbackSuccessfullySubmitted',
+    defaultMessage: 'Your feedback was successfully submitted.'
+  },
+  close: {
+    id: 'app.containers.Footer.close',
+    defaultMessage: 'Close'
+  }
+});
