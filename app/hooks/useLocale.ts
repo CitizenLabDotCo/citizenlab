d5210bd8e32b805080d6fe5a61aac1926e42f663
Brent Guffens <brentguf@users.noreply.github.com>
2019-09-27 09:57:37 +0200

    useLocale hook

---
+++ app/hooks/useLocale.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+import { Locale } from 'typings';
+import { localeStream } from 'services/locale';
+
+export default function useTenant() {
+  const [locale, setLocale] = useState<Locale | undefined | null | Error>(undefined);
+
+  useEffect(() => {
+    const subscription = localeStream().observable.subscribe((currentLocale) => {
+      setLocale(currentLocale);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  return locale;
+}
