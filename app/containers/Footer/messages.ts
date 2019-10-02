03fd81837bb4f1fb4e174e331f0ac4800f8b8de4
Brent Guffens <brentguf@users.noreply.github.com>
2019-10-02 15:26:03 +0200

    Revert message key changes

--- app/containers/Footer/messages.ts
+++ app/containers/Footer/messages.ts
@@ -2,71 +2,71 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   poweredBy: {
-    id: 'app.containers.Footer.poweredBy',
+    id: 'app.containers.footer.poweredBy',
     defaultMessage: 'Powered by',
   },
   information: {
-    id: 'app.containers.Footer.information',
+    id: 'app.containers.footer.information',
     defaultMessage: 'Information',
   },
   'terms-and-conditions': {
-    id: 'app.containers.Footer.terms-and-conditions',
+    id: 'app.containers.footer.terms-and-conditions',
     defaultMessage: 'Terms and Conditions',
   },
   'privacy-policy': {
-    id: 'app.containers.Footer.privacy-policy',
+    id: 'app.containers.footer.privacy-policy',
     defaultMessage: 'Privacy policy',
   },
   cookieSettings: {
-    id: 'app.containers.Footer.cookieSettings',
+    id: 'app.containers.footer.cookieSettings',
     defaultMessage: 'Cookie settings',
   },
   feedbackQuestion: {
-    id: 'app.containers.Footer.feedbackQuestion',
+    id: 'app.containers.footer.feedbackQuestion',
     defaultMessage: 'Did you find what you\'re looking for?'
   },
   yes: {
-    id: 'app.containers.Footer.yes',
+    id: 'app.containers.footer.yes',
     defaultMessage: 'Yes'
   },
   no: {
-    id: 'app.containers.Footer.no',
+    id: 'app.containers.footer.no',
     defaultMessage: 'No'
   },
   thanksForFeedback: {
-    id: 'app.containers.Footer.thanksForFeedback',
+    id: 'app.containers.footer.thanksForFeedback',
     defaultMessage: 'Thanks for your feedback!'
   },
   sendFeedback: {
-    id: 'app.containers.Footer.sendFeedback',
+    id: 'app.containers.footer.sendFeedback',
     defaultMessage: 'Send your feedback'
   },
   feedbackModalTitle: {
-    id: 'app.containers.Footer.feedbackModalTitle',
+    id: 'app.containers.footer.feedbackModalTitle',
     defaultMessage: 'What were you looking for?'
   },
   email: {
-    id: 'app.containers.Footer.email',
+    id: 'app.containers.footer.email',
     defaultMessage: 'Email (optional)'
   },
   feedback: {
-    id: 'app.containers.Footer.feedback',
+    id: 'app.containers.footer.feedback',
     defaultMessage: 'Let us know what you didn’t find on this page:'
   },
   submit: {
-    id: 'app.containers.Footer.submit',
+    id: 'app.containers.footer.submit',
     defaultMessage: 'Submit'
   },
   moreInfo: {
-    id: 'app.containers.Footer.moreInfo',
+    id: 'app.containers.footer.moreInfo',
     defaultMessage: '... let us know more'
   },
   feedbackSuccessfullySubmitted: {
-    id: 'app.containers.Footer.feedbackSuccessfullySubmitted',
+    id: 'app.containers.footer.feedbackSuccessfullySubmitted',
     defaultMessage: 'Your feedback was successfully submitted.'
   },
   close: {
-    id: 'app.containers.Footer.close',
+    id: 'app.containers.footer.close',
     defaultMessage: 'Close'
   }
 });
