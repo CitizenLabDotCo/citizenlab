60fef66ca23bc88f7ac85f74f99fd1a25e4bda15
brentguf <brent.guffens@gmail.com>
2019-10-04 13:49:08 +0200

    Alt text of Idea/InitiativeCard

--- app/components/IdeaCard/messages.ts
+++
@@ -1,8 +0,0 @@
-import { defineMessages } from 'react-intl';
-
-export default defineMessages({
-  imageAltText: {
-    id: 'app.components.IdeaCard.imageAltText',
-    defaultMessage: '{orgName} - main image for the idea {ideaTitle}',
-  },
-});
