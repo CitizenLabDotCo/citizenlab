fa0954316689cf6c5879d8918b5a286fbeddc7a4
Sara <sara@citizenlab.co>
2019-10-08 11:52:41 +0200

    adds auth user hook

---
+++ app/hooks/useAuthUser.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+import { authUserStream } from 'services/auth';
+import { IUser } from 'services/users';
+
+export default function useAuthUser() {
+  const [authUser, setAuthUser] = useState<IUser | undefined | null | Error>(undefined);
+
+  useEffect(() => {
+    const subscription = authUserStream().observable.subscribe((currentAuthUser) => {
+      setAuthUser(currentAuthUser);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  return authUser;
+}
