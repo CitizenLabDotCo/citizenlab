b780728374bd556617896fa665029e30ddf812d8
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2019-10-08 09:36:24 +0200

    Fixed projects export for real

--- engines/admin_api/app/services/admin_api/project_copy_service.rb
+++ engines/admin_api/app/services/admin_api/project_copy_service.rb
@@ -64,7 +64,7 @@ module AdminApi
       @refs[model_name][id] = yml_obj
     end
 
-    def yml_projects shift_timestamps: 0, new_slug: new_slug: nil, new_title_multiloc: nil
+    def yml_projects shift_timestamps: 0, new_slug: nil, new_title_multiloc: nil
       yml_project = yml_participation_context @project, shift_timestamps: shift_timestamps
       yml_project.merge!({
         'title_multiloc'               => new_title_multiloc || @project.title_multiloc,
