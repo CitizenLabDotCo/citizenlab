e451d6a5c9743e8c69c7b953c0bfdf449d4de43b
Sara <sara@citizenlab.co>
2019-10-09 12:51:06 +0200

    Adds verification modal components

---
+++ app/components/VerificationSuccessModal/messages.ts
@@ -0,0 +1,17 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  // Modal when a user just vefified its profile
+  userVerifiedTitle: {
+    id: 'app.components.VerificationSuccessModal.userVerifiedTitle',
+    defaultMessage: 'You’re now verified !'
+  },
+  userVerifiedSubtitle: {
+    id: 'app.components.VerificationSuccessModal.userVerifiedSubtitle',
+    defaultMessage: 'Your identity has been verified. You’re now a full member of the community on this platform.'
+  },
+  close: {
+    id: 'app.components.VerificationSuccessModal.close',
+    defaultMessage: 'Close'
+  },
+});
