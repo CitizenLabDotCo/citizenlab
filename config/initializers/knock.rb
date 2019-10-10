ca16da537486b036e07293d88f68515c806282e3
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2019-10-10 11:21:05 +0200

    CL2-4442 Changed JWT signature encoding to RSA256

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -28,7 +28,7 @@ Knock.setup do |config|
   ## Configure the algorithm used to encode the token
   ##
   ## Default:
-  # config.token_signature_algorithm = 'HS256'
+  config.token_signature_algorithm = 'RS256'
 
   ## Signature key
   ## -------------
@@ -36,7 +36,7 @@ Knock.setup do |config|
   ## Configure the key used to sign tokens.
   ##
   ## Default:
-  # config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
+  config.token_secret_signature_key = -> { OpenSSL::PKey::RSA.new(ENV.fetch('JWT_RS256_PRIVATE_KEY').gsub('\n', "\n")) }
 
   ## If using Auth0, uncomment the line below
   # config.token_secret_signature_key = -> { JWT.base64url_decode Rails.application.secrets.auth0_client_secret }
@@ -48,6 +48,7 @@ Knock.setup do |config|
   ##
   ## Default:
   # config.token_public_key = nil
+  config.token_public_key = OpenSSL::PKey::RSA.new(ENV.fetch('JWT_RS256_PUBLIC_KEY').gsub('\n', "\n"))
 
   ## Exception Class
   ## ---------------
