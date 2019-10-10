4e6c0b937295fd934cce3defcd0a429e9a367a4d
brentguf <brent.guffens@gmail.com>
2019-10-10 17:32:43 +0200

    Svg component props

--- app/components/UI/Icon/Svg.tsx
+++ app/components/UI/Icon/Svg.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { IconNames } from '.';
 
 interface Props {
   className?: string;
@@ -7,20 +8,24 @@ interface Props {
   // if ariaHidden is not specified, a title is 'required' (for a11y)
   title?: string | JSX.Element;
   viewBox: string;
-  fill: string;
+  fill?: string;
   ariaHidden?: boolean;
+  height?: string;
+  width?: string;
 }
 
-const Svg = React.memo(({ className, children, name, title, viewBox, fill, ariaHidden }: Props) => {
+const Svg = React.memo(({ className, children, name, title, viewBox, fill, ariaHidden, height, width }: Props) => {
   return (
     <svg
       className={className}
       role="img"
       aria-labelledby={name}
       viewBox={viewBox}
-      aria-hidden={ariaHidden}
-      fill={fill}
+      aria-hidden={ariaHidden || false}
+      fill={fill || 'none'}
       xmlns="http://www.w3.org/2000/svg"
+      height={height || '100%'}
+      width={width}
     >
       {title && <title id={name}>{title}</title>}
       {children}
