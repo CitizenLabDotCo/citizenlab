1ec8e642e5d97e251583962879ffd5f10e7f4875
brentguf <brent.guffens@gmail.com>
2019-10-10 17:32:22 +0200

    error icon

--- app/components/UI/Icon/index.tsx
+++ app/components/UI/Icon/index.tsx
@@ -48,12 +48,17 @@ const Icon = memo<Props>(({ name, className, title, colorTheme, ariaHidden }) =>
       </svg>
     ),
     error: (
-      <svg aria-hidden={ariaHidden} className={className} height="100%" viewBox="2 2 20 20">
-        {injectTitle(title)}
+      <Svg
+        className={className}
+        ariaHidden={ariaHidden}
+        title={title}
+        name={name}
+        viewBox="2 2 20 20"
+      >
         <path fill="none" d="M0 0h24v24H0V0z" />
         <path d="M11 15h2v2h-2zM11 7h2v6h-2z" />
         <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z" />
-      </svg>
+      </Svg>
     ),
     eye: (
       <svg className={className} height="100%" viewBox="1 4.5 22 15">
