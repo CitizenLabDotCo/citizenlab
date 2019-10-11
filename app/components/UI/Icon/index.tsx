260c08d4574ca8bc4f6e7d99d987d95b4d70f4c4
brentguf <brent.guffens@gmail.com>
2019-10-11 10:51:08 +0200

    idea2 icon

--- app/components/UI/Icon/index.tsx
+++ app/components/UI/Icon/index.tsx
@@ -168,10 +168,15 @@ const Icon = memo<Props>(({ name, className, title, colorTheme, ariaHidden }) =>
       </svg>
     ),
     idea2: (
-      <svg aria-hidden={ariaHidden} className={className} height="100%" viewBox="0 0 14 20">
-        {injectTitle(title)}
+      <Svg
+        className={className}
+        ariaHidden={ariaHidden}
+        title={title}
+        name={name}
+        viewBox="0 0 14 20"
+      >
         <path d="M4 19c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H4v1zM7 0C3.14 0 0 3.14 0 7c0 2.38 1.19 4.47 3 5.74V15c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V14H5v-2.3l-.85-.6A4.997 4.997 0 0 1 2 7c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z" />
-      </svg>
+      </Svg>
     ),
     user: (
       <svg role="img" aria-hidden={ariaHidden} className={className} height="100%" viewBox="2 2 20 20">
