26c6e06d9647f376249c7d90307cfee8b122a31e
brentguf <brent.guffens@gmail.com>
2019-10-17 16:09:35 +0200

    Comment info

---
+++ app/components/InitiativeCard/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  xComments: {
+    id: 'app.containers.InitiativeCard.xComments',
+    defaultMessage: '{commentsCount, plural, =0 {no comments} one {1 comment} other {# comments}}',
+  },
+});
