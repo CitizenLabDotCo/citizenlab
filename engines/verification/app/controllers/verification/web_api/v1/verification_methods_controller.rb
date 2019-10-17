07b3a68c41c8de97bcc1398d739212c1e8fa89e7
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2019-10-17 11:33:01 +0200

    Fixed autoloading of verification controllers

--- engines/verification/app/controllers/verification/web_api/v1/verification_methods_controller.rb
+++ engines/verification/app/controllers/verification/web_api/v1/verification_methods_controller.rb
@@ -1,27 +1,30 @@
 module Verification
-  class WebApi::V1::VerificationMethodsController < VerificationController
+  module WebApi
+    module V1
+      class VerificationMethodsController < VerificationController
 
-    def index
-      vm_service = VerificationService.new
+        def index
+          vm_service = VerificationService.new
 
-      @verification_methods = vm_service.active_methods_for_tenant(Tenant.current)
-      @verification_methods = policy_scope(@verification_methods, policy_scope_class: VerificationMethodPolicy::Scope)
-      @verification_methods = Kaminari.paginate_array(@verification_methods)
-        .page(params.dig(:page, :number))
-        .per(params.dig(:page, :size))
+          @verification_methods = vm_service.active_methods_for_tenant(Tenant.current)
+          @verification_methods = policy_scope(@verification_methods, policy_scope_class: VerificationMethodPolicy::Scope)
+          @verification_methods = Kaminari.paginate_array(@verification_methods)
+            .page(params.dig(:page, :number))
+            .per(params.dig(:page, :size))
 
-      render json: linked_json(
-        @verification_methods,
-        WebApi::V1::VerificationMethodSerializer,
-        params: fastjson_params
-      )
+          render json: linked_json(
+            @verification_methods,
+            WebApi::V1::VerificationMethodSerializer,
+            params: fastjson_params
+          )
+        end
+
+        private
+
+        def secure_controller?
+          false
+        end
+      end
     end
-
-    private
-
-    def secure_controller?
-      false
-    end
-
   end
 end
