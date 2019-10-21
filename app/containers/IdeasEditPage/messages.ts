c4fb2962b5a1fe5f969b59b525df0f67d9b4d9af
brentguf <brent.guffens@gmail.com>
2019-10-21 15:11:12 +0200

    Use full-width submit button for idea edit

--- app/containers/IdeasEditPage/messages.ts
+++ app/containers/IdeasEditPage/messages.ts
@@ -13,4 +13,12 @@ export default defineMessages({
     id: 'app.containers.IdeasEditPage.submitError',
     defaultMessage: 'Something went wrong. Please try again later.',
   },
+  submit: {
+    id: 'app.containers.IdeasEditPage.submit',
+    defaultMessage: 'Submit idea',
+  },
+  fileOrImageError: {
+    id: 'app.containers.IdeasEditPage.fileOrImageError',
+    defaultMessage: 'Your idea was submitted, but a file failed to be uploaded, please edit your idea to try again.',
+  },
 });
