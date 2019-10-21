2b9d76435b740478db4f464c75f17efd540c6b2a
Sébastien Hoorens <sebastien@citizenlab.co>
2019-10-21 11:12:22 +0200

    towards solution with separate function to load file

--- lib/carrierwave/base65/base65_string_io.rb
+++ lib/carrierwave/base65/base65_string_io.rb
@@ -4,7 +4,6 @@ module Carrierwave
 
       attr_reader :file_name
       attr_reader :file_extension
-      attr_reader :mime_type
 
       def initialize encoded_file, file_name, file_extension
         @file_name = file_name
@@ -13,8 +12,6 @@ module Carrierwave
         description, encoded_bytes = encoded_file.split ','
         raise ArgumentError if !encoded_bytes || encoded_bytes.eql?('(null)')
         bytes = ::Base64.decode64 encoded_bytes
-
-        @mime_type = description.split(';base64').first
 
         super bytes
       end
