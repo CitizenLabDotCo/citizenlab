cc518608058746e49e24043ce0bfbf3cf9c26341
brentguf <brent.guffens@gmail.com>
2019-10-21 15:26:12 +0200

    IdeasEditButtonBar

---
+++ app/containers/IdeasEditPage/IdeasEditButtonBar.tsx
@@ -0,0 +1,116 @@
+import React, { PureComponent } from 'react';
+import { Subscription } from 'rxjs';
+
+// components
+import Button from 'components/UI/Button';
+import Error from 'components/UI/Error';
+import ButtonBar from 'components/ButtonBar';
+
+// services
+import { globalState, IGlobalStateService, IIdeasPageGlobalState } from 'services/globalState';
+
+// i18n
+import { FormattedMessage } from 'utils/cl-intl';
+import messages from './messages';
+
+// utils
+import eventEmitter from 'utils/eventEmitter';
+
+// style
+import styled from 'styled-components';
+
+const ButtonBarInner = styled.div`
+  width: 100%;
+  max-width: 600px;
+  display: flex;
+  align-items: center;
+  padding-right: 20px;
+  padding-left: 20px;
+
+  .Button {
+    margin-right: 10px;
+  }
+
+  .Error {
+    flex: 1;
+  }
+`;
+
+interface Props {
+  form?: string;
+  id?: string;
+}
+
+interface GlobalState {
+  submitError: boolean;
+  processing: boolean;
+  fileOrImageError: boolean;
+}
+
+interface State extends GlobalState {}
+
+export default class IdeasEditButtonBar extends PureComponent<Props, State> {
+  globalState: IGlobalStateService<IIdeasPageGlobalState>;
+  subscriptions: Subscription[];
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      submitError: false,
+      processing: false,
+      fileOrImageError: false
+    };
+    this.globalState = globalState.init<IIdeasPageGlobalState>('IdeasEditPage');
+    this.subscriptions = [];
+  }
+
+  async componentDidMount() {
+    const globalState$ = this.globalState.observable;
+
+    this.subscriptions = [
+      globalState$.subscribe(({ submitError, processing, fileOrImageError }) => {
+        this.setState({ submitError, processing, fileOrImageError });
+      })
+    ];
+  }
+
+  componentWillUnmount() {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
+
+  handleOnSubmitButtonClick = () => {
+    eventEmitter.emit('IdeasEditPage', 'IdeaFormSubmitEvent', null);
+  }
+
+  render() {
+    const { processing, fileOrImageError, submitError } = this.state;
+    const { id } = this.props;
+    let { form } = this.props;
+    const submitErrorMessage = submitError
+      ? <FormattedMessage {...messages.submitError} />
+      : fileOrImageError
+        ? <FormattedMessage {...messages.fileOrImageError} />
+        : null;
+
+    form = (form || '');
+
+    return (
+      <ButtonBar>
+        <ButtonBarInner>
+          <Button
+            id={id}
+            form={form}
+            className="e2e-submit-idea-form"
+            size="1"
+            processing={processing}
+            text={<FormattedMessage {...messages.submit} />}
+            onClick={this.handleOnSubmitButtonClick}
+          />
+          {submitErrorMessage &&
+            <Error text={submitErrorMessage} marginTop="0px" showBackground={false} showIcon={true} />
+          }
+        </ButtonBarInner>
+      </ButtonBar>
+    );
+  }
+}
