11e8069d9e27a979c2072b591b428cac7a615ed7
DavidCnoops <david@citizenlab.co>
2019-10-22 15:53:24 +0200

    fix for translatedSlogan error

--- app/containers/LandingPage/SignedOutHeader.tsx
+++ app/containers/LandingPage/SignedOutHeader.tsx
@@ -198,7 +198,7 @@ class SignedOutHeader extends PureComponent<Props, State> {
       const subtitle = (headerSloganMultiLoc ? (
         <T value={headerSloganMultiLoc}>
           {translatedSlogan =>
-             translatedSlogan ? <h2>${translatedSlogan}</h2> : genericSlogan
+             translatedSlogan ? <h2>{translatedSlogan}</h2> : genericSlogan
           }
         </T>
       ) : genericSlogan);
