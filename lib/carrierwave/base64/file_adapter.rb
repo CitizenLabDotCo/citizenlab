395db5699c28590075b8e40bc5c331b9e9178f88
Sébastien Hoorens <sebastien@citizenlab.co>
2019-10-22 17:05:13 +0200

    switchinnnn againnn

--- lib/carrierwave/base64/file_adapter.rb
+++ lib/carrierwave/base64/file_adapter.rb
@@ -6,24 +6,25 @@ module Carrierwave
         mount_uploader attribute, uploader_class, options
 
         self.send(:define_method, "#{attribute}_by_content=") do |file_attributes|
+          raise ArgumentError if !file_attributes[:content] || !file_attributes[:name]
           data = file_attributes[:content]
           self.name = file_attributes[:name]
-          # TODO return arg error if something missing
           return if data.to_s.empty? || data == send(attribute).to_s
 
           send("#{attribute}_will_change!") if respond_to? "#{attribute}_will_change!"
 
           if !(data.is_a?(String) && data.strip.start_with?('data'))
-            self.file = data
+            self.send "#{attribute}=", data
             return
           end
 
           file_name = self.name.split('.')[0..-2].join('.')
           extension = self.name.split('.').last
-          self.file = Carrierwave::Base64::Base64FileStringIO.new(data.strip, file_name, extension)
+          self.send "#{attribute}=", Carrierwave::Base64::Base64FileStringIO.new(data.strip, file_name, extension)
         end
 
         self.send(:define_method, "#{attribute}_by_url=") do |file_attributes|
+          raise ArgumentError if !file_attributes[:url] || !file_attributes[:name]
           self.assign_attributes attribute => file_attributes[:name], "remote_#{attribute}_url" => file_attributes[:url]
         end
       end
