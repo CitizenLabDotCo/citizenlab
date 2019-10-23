4541e4a55c0bdd56e20d5bb6c86dd2b331d496cd
DavidCnoops <david@citizenlab.co>
2019-10-23 10:55:00 +0200

    layout improvements

--- app/containers/ProjectsShowPage/ProjectNavbar.tsx
+++ app/containers/ProjectsShowPage/ProjectNavbar.tsx
@@ -32,7 +32,7 @@ const ProjectNavbarWrapper = styled.div`
   position: sticky;
   top: ${({ theme }) => theme.menuHeight}px;
   z-index: 10;
-  background: ${({ theme }) => theme.projectNavbarBackgroundColor || '#002332'};
+  background: ${({ theme }) => theme.projectNavbarBackgroundColor || '#171717'};
   box-shadow: 1px 2px 2px rgba(0, 0, 0, 0.06);
 
   ${media.smallerThanMinTablet`
