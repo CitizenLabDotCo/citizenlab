3ad7ecd9fc722ba1a6b2f5abfd39a8017872acc0
Sara <sara@citizenlab.co>
2019-10-23 18:31:51 +0200

    fixes the issue

--- app/components/UI/Radio/index.tsx
+++ app/components/UI/Radio/index.tsx
@@ -14,7 +14,7 @@ const Wrapper = styled.label`
   }
 `;
 
-export const CustomRadio = styled.button`
+export const CustomRadio = styled.div`
   flex: 0 0 20px;
   width: 20px;
   height: 20px;
@@ -29,6 +29,7 @@ export const CustomRadio = styled.button`
   border-radius: 50%;
   border: 1px solid #a6a6a6;
   box-shadow: inset 0px 1px 2px rgba(0, 0, 0, 0.15);
+  cursor: pointer;
 
   &:not(.disabled) {
     cursor: pointer;
