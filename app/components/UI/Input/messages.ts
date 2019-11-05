9940832d40ad8d65e6c7ebdeb80b74ee127519c7
brentguf <brent.guffens@gmail.com>
2019-11-05 12:43:23 +0100

    Improved title field for SRs

---
+++ app/components/UI/Input/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  charactersLeft: {
+    id: 'app.components.Input.charactersLeft',
+    defaultMessage: '{currentCharCount} characters used out of {maxCharCount}',
+  },
+});
