d41b631c82fe5d7e80ab5c7ecce86efa7c63125b
Sara <sara@citizenlab.co>
2019-11-05 12:47:06 +0100

    Should make tests more reproducable
    3rd topic is waste and we test filtering on waste

--- cypress/integration/idea_new_page.ts
+++ cypress/integration/idea_new_page.ts
@@ -58,7 +58,7 @@ describe('Idea new page', () => {
     cy.get('#e2e-idea-description-input .ql-editor').contains(ideaContent);
 
     // add a topic
-    cy.get('.e2e-topics-picker').find('button').eq(3).click();
+    cy.get('.e2e-topics-picker').find('button').eq(4).click();
 
     // verify that the topic has been selected
     cy.get('.e2e-topics-picker').find('button.selected').should('have.length', 1);
