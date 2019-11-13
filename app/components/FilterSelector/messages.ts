56c79be3ac338e486e659d5aa7c3ac29e89f73d9
brentguf <brent.guffens@gmail.com>
2019-11-13 18:52:09 +0100

    Single filter selecter aria live (WIP)

---
+++ app/components/FilterSelector/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  a11y_selectedFilter: {
+    id: 'app.components.FilterSelector.a11y_selectedFilter',
+    defaultMessage: 'Selected filter: ',
+  },
+});
