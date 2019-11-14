d778f32f4cf0d42d552dfb23638036d155e580f5
Sébastien Hoorens <sebastien@citizenlab.co>
2019-11-14 14:36:56 +0100

    participated in project rule descriptions

--- spec/lib/smart_group_rules/participated_in_project_spec.rb
+++ spec/lib/smart_group_rules/participated_in_project_spec.rb
@@ -105,7 +105,7 @@ describe SmartGroupRules::ParticipatedInProject do
     let(:project) { create(:project, title_multiloc: {
       'en'    => 'Choose a name for the new trash can',
       'fr-FR' => 'Choisissez un nom pour la nouvelle poubelle',
-      'nl-NL' => 'Kies een naam voor de nieuwe vuilniszak'
+      'nl-NL' => 'Kies een naam voor de nieuwe vuilnisbak'
     }) }
 
     let(:participated_in_project_in_rule) {SmartGroupRules::ParticipatedInProject.from_json({
@@ -175,54 +175,64 @@ describe SmartGroupRules::ParticipatedInProject do
       I18n.load_path += Dir[Rails.root.join('spec', 'fixtures', 'locales', '*.yml')]
 
       expect(participated_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Participation in an idea with status in the garbage can',
-        'fr-FR' => 'Participation dans une idée avec statut dans la poubelle',
-        'nl-NL' => 'Participatie in een idee met status in de prullenmand'
+        'en'    => 'Participation in an idea in the project Choose a name for the new trash can',
+        'fr-FR' => 'Participation dans une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
+        'nl-NL' => 'Participatie in een idee in het project Kies een naam voor de nieuwe vuilnisbak'
       })
       expect(participated_not_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'No participation in an idea with status in the garbage can',
-        'fr-FR' => 'Pas de participation dans une idée avec statut dans la poubelle',
-        'nl-NL' => 'Geen participatie in een idea met status in de prullenmand'
+        'en'    => 'No participation in an idea in the project Choose a name for the new trash can',
+        'fr-FR' => 'Pas de participation dans une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
+        'nl-NL' => 'Geen participatie in een idee in het project Kies een naam voor de nieuwe vuilnisbak'
       })
       expect(participated_posted_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Posted an idea with status in the garbage can',
-        'fr-FR' => 'Posté une idée avec statut dans la poubelle',
-        'nl-NL' => 'Plaatste een idee met status in de prullenmand'
+        'en'    => 'Posted an idea in the project Choose a name for the new trash can',
+        'fr-FR' => 'Posté une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
+        'nl-NL' => 'Plaatste een idee in het project Kies een naam voor de nieuwe vuilnisbak'
       })
       expect(participated_not_posted_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Did not post an idea with status in the garbage can',
-        'fr-FR' => 'N\'as pas posté une idée avec statut dans la poubelle',
-        'nl-NL' => 'Plaatste geen idee met status in de prullenmand'
+        'en'    => 'Did not post an idea in the project Choose a name for the new trash can',
+        'fr-FR' => 'N\'as pas posté une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
+        'nl-NL' => 'Plaatste geen idee in het project Kies een naam voor de nieuwe vuilnisbak'
       })
       expect(participated_commented_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Commented on an idea with status in the garbage can',
-        'fr-FR' => 'Commenté sur une idée avec statut dans la poubelle',
-        'nl-NL' => 'Reageerde op een idee met status in de prullenmand'
+        'en'    => 'Commented on an idea in the project Choose a name for the new trash can',
+        'fr-FR' => 'Commenté sur une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
+        'nl-NL' => 'Reageerde op een idee in het project Kies een naam voor de nieuwe vuilnisbak'
       })
       expect(participated_not_commented_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Did not comment on an idea with status in the garbage can',
-        'fr-FR' => 'N\'as pas commenté sur une idée avec statut dans la poubelle',
-        'nl-NL' => 'Reageerde niet op een idee met status in de prullenmand'
+        'en'    => 'Did not comment on an idea in the project Choose a name for the new trash can',
+        'fr-FR' => 'N\'as pas commenté sur une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
+        'nl-NL' => 'Reageerde niet op een idee in het project Kies een naam voor de nieuwe vuilnisbak'
       })
       expect(participated_voted_idea_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Voted on an idea with status in the garbage can',
-        'fr-FR' => 'Voté pour une idée avec statut dans la poubelle',
-        'nl-NL' => 'Stemde op een idee met status in de prullenmand'
+        'en'    => 'Voted on an idea in the project Choose a name for the new trash can',
+        'fr-FR' => 'Voté pour une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
+        'nl-NL' => 'Stemde op een idee in het project Kies een naam voor de nieuwe vuilnisbak'
       })
       expect(participated_not_voted_idea_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Did not vote on an idea with status in the garbage can',
-        'fr-FR' => 'N\'as pas voté pour une idée avec statut dans la poubelle',
-        'nl-NL' => 'Stemde niet op een idee met status in de prullenmand'
+        'en'    => 'Did not vote on an idea in the project Choose a name for the new trash can',
+        'fr-FR' => 'N\'as pas voté pour une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
+        'nl-NL' => 'Stemde niet op een idee in het project Kies een naam voor de nieuwe vuilnisbak'
       })
       expect(participated_voted_comment_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Voted on a comment on an idea with status in the garbage can',
-        'fr-FR' => 'Voté pour un commentaire sur une idée avec statut dans la poubelle',
-        'nl-NL' => 'Stemde op een reactie op een idee met status in de prullenmand'
+        'en'    => 'Voted on a comment on an idea in the project Choose a name for the new trash can',
+        'fr-FR' => 'Voté pour un commentaire sur une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
+        'nl-NL' => 'Stemde op een reactie op een idee in het project Kies een naam voor de nieuwe vuilnisbak'
       })
       expect(participated_not_voted_comment_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Did not vote on a comment on an idea with status in the garbage can',
-        'fr-FR' => 'N\'as pas voté pour un commentaire sur une idée avec statut dans la poubelle',
-        'nl-NL' => 'Stemde niet op een reactie op een idee met status in de prullenmand'
+        'en'    => 'Did not vote on a comment on an idea in the project Choose a name for the new trash can',
+        'fr-FR' => 'N\'as pas voté pour un commentaire sur une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
+        'nl-NL' => 'Stemde niet op een reactie op een idee in het project Kies een naam voor de nieuwe vuilnisbak'
+      })
+      expect(participated_budgeted_in_project_in_rule.description_multiloc).to eq ({
+        'en'    => 'Assigned a budget in the project Choose a name for the new trash can',
+        'fr-FR' => 'Dépensé un budget dans le projet Choisissez un nom pour la nouvelle poubelle',
+        'nl-NL' => 'Wees een budget toe in het project Kies een naam voor de nieuwe vuilnisbak'
+      })
+      expect(participated_not_budgeted_in_project_in_rule.description_multiloc).to eq ({
+        'en'    => 'Didn\'t assign a budget in the project Choose a name for the new trash can',
+        'fr-FR' => 'N\'as pas dépensé un budget dans le projet Choisissez un nom pour la nouvelle poubelle',
+        'nl-NL' => 'Wees geen budget toe in het project Kies een naam voor de nieuwe vuilnisbak'
       })
     end
   end
