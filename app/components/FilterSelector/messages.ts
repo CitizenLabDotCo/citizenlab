0cf58394df47a23c72f2ee8272865097330eaaf5
brentguf <brent.guffens@gmail.com>
2019-11-14 11:16:31 +0100

    Delete unused message

--- app/components/FilterSelector/messages.ts
+++
@@ -1,8 +0,0 @@
-import { defineMessages } from 'react-intl';
-
-export default defineMessages({
-  a11y_selectedFilter: {
-    id: 'app.components.FilterSelector.a11y_selectedFilter',
-    defaultMessage: 'Selected filter: ',
-  },
-});
