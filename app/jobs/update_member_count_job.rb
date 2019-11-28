5cb220b222de7f98e4f462f78b2c2bb4ee52ae69
Sébastien Hoorens <sebastien@citizenlab.co>
2019-11-28 16:57:31 +0100

    minor

--- app/jobs/update_member_count_job.rb
+++ app/jobs/update_member_count_job.rb
@@ -6,9 +6,8 @@ class UpdateMemberCountJob < ApplicationJob
       group.update_memberships_count!
     else
       Group.where(membership_type: 'manual').each do |group|
-        group.update! memberships_count: Membership.where(group: group).where(user: User.active)
+        group.update! memberships_count: Membership.where(group: group).where(user: User.active).count
       end
-
       Group.where(membership_type: 'rules').each do |group|
         group.update_memberships_count!
       end
