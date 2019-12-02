5b5216a03e74ecdcc8f738ee4cef26e25a33c8fe
Sébastien Hoorens <sebastien@citizenlab.co>
2019-12-02 17:43:00 +0100

    removed require dependencies

--- engines/frontend/lib/frontend/engine.rb
+++ engines/frontend/lib/frontend/engine.rb
@@ -1,11 +1,5 @@
 module Frontend
   class Engine < ::Rails::Engine
     isolate_namespace Frontend
-
-    config.to_prepare do
-      Dir.glob(Engine.root + "app/models/**/*_decorator*.rb").each do |c|
-        require_dependency(c)
-      end
-    end
   end
 end
