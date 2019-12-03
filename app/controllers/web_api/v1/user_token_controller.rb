dada336463ee8c90d0b7e0806ee2c0213b30b072
Sébastien Hoorens <sebastien@citizenlab.co>
2019-12-03 12:04:54 +0100

    fixed schema extension loading issue by getting schema url from class to be loaded

--- app/controllers/web_api/v1/user_token_controller.rb
+++ app/controllers/web_api/v1/user_token_controller.rb
@@ -1,4 +1,4 @@
 class WebApi::V1::UserTokenController < Knock::AuthTokenController
   # Turn off CSRF
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 end
