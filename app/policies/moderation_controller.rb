ebbece04336a6394bac6f31627308383011fe9fb
Sébastien Hoorens <sebastien@citizenlab.co>
2019-12-09 16:15:04 +0100

    first version moderations endpoint

---
+++ app/policies/moderation_controller.rb
@@ -0,0 +1,18 @@
+class ModerationPolicy < ApplicationPolicy
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user  = user
+      @scope = scope
+    end
+
+    def resolve
+      if user&.admin?
+        scope.all
+      else
+        scope.none
+      end
+    end
+  end
+end
