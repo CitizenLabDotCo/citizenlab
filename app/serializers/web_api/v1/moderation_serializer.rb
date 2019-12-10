c9eb44bc66a0619f1375c00294e11f898b0c1b98
Sébastien Hoorens <sebastien@citizenlab.co>
2019-12-10 11:47:03 +0100

    various fixes and stop creating idea by creating feedback by creating status change when creating initiative

--- app/serializers/web_api/v1/moderation_serializer.rb
+++ app/serializers/web_api/v1/moderation_serializer.rb
@@ -2,4 +2,8 @@ class WebApi::V1::ModerationSerializer < WebApi::V1::BaseSerializer
   set_type :moderation
 
   attributes :moderatable_type, :context_slug, :context_type, :context_multiloc, :content_multiloc, :created_at
+
+  attribute :context_url do |object|
+    Frontend::UrlService.new.slug_to_url object.context_slug, object.context_type
+  end
 end
