b45f2f715bbbb0d504d34d66edeb4b638eecde2a
Sébastien Hoorens <sebastien@citizenlab.co>
2019-12-10 15:09:59 +0100

    complete spec + minor fixes

--- spec/acceptance/moderations_spec.rb
+++ spec/acceptance/moderations_spec.rb
@@ -12,22 +12,22 @@ resource "Moderations" do
     token = Knock::AuthToken.new(payload: @user.to_token_payload).token
     header 'Authorization', "Bearer #{token}"
     @time = Time.now
-    @m1 = create(:idea, 
+    @m3 = create(:idea, 
       title_multiloc: {'en' => 'More bicycle repairmen'}, 
       body_multiloc: {'en' => 'They are the true heroes of society'},
       published_at: @time - 1.day
       )
     @m2 = create(:comment, 
       body_multiloc: {'en' => 'I\'m glad there\'s still heroes around'},
-      post: @m1, 
+      post: @m3, 
       created_at: @time - 1.hour
       )
-    @m3 = create(:idea, 
+    @m4 = create(:idea, 
       title_multiloc: {'en' => 'Fewer bicycle repairmen'}, 
       body_multiloc: {'en' => 'They are pretentious donkeys'},
       published_at: @time - 2.days
       )
-    @m4 = create(:initiative, 
+    @m1 = create(:initiative, 
       title_multiloc: {'en' => 'Burn more leaves'}, 
       body_multiloc: {'en' => 'We must return that CO2 to our atmosphere at all cost'},
       published_at: @time - 1.minute
@@ -43,6 +43,11 @@ resource "Moderations" do
       expect(status).to eq(200)
       json_response = json_parse(response_body)
       expect(json_response[:data].size).to eq 4
+      expect(json_response[:data].map { |d| d.dig(:id) }).to eq [@m1.id, @m2.id, @m3.id, @m4.id]
+      expect(json_response[:data].map { |d| d.dig(:attributes, :moderatable_type) }).to eq ['Initiative', 'Comment', 'Idea', 'Idea']
+      expect(json_response[:data].map { |d| d.dig(:attributes, :context_multiloc).stringify_keys['en'] }).to eq ['Burn more leaves', 'More bicycle repairmen', 'More bicycle repairmen', 'Fewer bicycle repairmen']
+      expect(json_response[:data].map { |d| d.dig(:attributes, :content_multiloc).stringify_keys['en'] }).to eq ['We must return that CO2 to our atmosphere at all cost', 'I\'m glad there\'s still heroes around', 'They are the true heroes of society', 'They are pretentious donkeys']
+      expect(json_response[:data].map { |d| Time.parse(d.dig(:attributes, :created_at)).to_i }).to eq [@time - 1.minute, @time - 1.hour, @time - 1.day, @time - 2.days].map(&:to_i)
     end
   end
 end
