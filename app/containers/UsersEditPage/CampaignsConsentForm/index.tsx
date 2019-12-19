6b9ac707c053bb2cdbf42420244a75d6074ef74e
brentguf <brent.guffens@gmail.com>
2019-12-19 17:01:07 +0100

    Move trackEventName outside of ConsentForm

--- app/containers/UsersEditPage/CampaignsConsentForm/index.tsx
+++ app/containers/UsersEditPage/CampaignsConsentForm/index.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import GetCampaignConsents from 'resources/GetCampaignConsents';
 import ConsentForm from 'components/ConsentForm';
 import { isNilOrError } from 'utils/helperUtils';
+import tracks from './tracks';
 
 export default () => (
   <GetCampaignConsents>
-    {(consents) => !isNilOrError(consents) ? <ConsentForm consents={consents} /> : null}
+    {(consents) => !isNilOrError(consents) ?
+      <ConsentForm
+        trackEventName={tracks.clickChangeEmailNotificationProfileSettings.name}
+        consents={consents}
+      />
+    :
+      null
+    }
   </GetCampaignConsents>
 );
