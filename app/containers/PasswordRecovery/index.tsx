09832c57fc93b613853c8b7538202a22b80820ad
brentguf <brent.guffens@gmail.com>
2019-12-30 15:37:53 +0100

    Password recovery input colors

--- app/containers/PasswordRecovery/index.tsx
+++ app/containers/PasswordRecovery/index.tsx
@@ -197,6 +197,7 @@ class PasswordRecovery extends React.PureComponent<Props & InjectedIntlProps, St
               placeholder={emailPlaceholder}
               onChange={this.handleEmailOnChange}
               setRef={this.handleEmailInputSetRef}
+              onGreyBackground
             />
 
             {/* <Error fieldName="title_multiloc" apiErrors={this.state.errors.title_multiloc} /> */}
