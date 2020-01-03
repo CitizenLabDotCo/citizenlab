c2384f2d05f05ed2f6c66cdbee6c882020e4f0a4
brentguf <brent.guffens@gmail.com>
2020-01-03 11:25:56 +0100

    Connect image dropzone idea form w/ label

--- app/components/IdeaForm/index.tsx
+++ app/components/IdeaForm/index.tsx
@@ -507,16 +507,17 @@ class IdeaForm extends PureComponent<Props & InjectedIntlProps & WithRouterProps
         <StyledFormSection>
           <FormSectionTitle message={messages.formAttachmentsSectionTitle} />
           <FormElement id="e2e-idea-image-upload">
-            <FormLabel labelMessage={messages.imageUploadLabel} />
-            <ImagesDropzone
-              images={imageFile}
-              imagePreviewRatio={135 / 298}
-              acceptedFileTypes="image/jpg, image/jpeg, image/png, image/gif"
-              maxImageFileSize={5000000}
-              maxNumberOfImages={1}
-              onAdd={this.handleUploadOnAdd}
-              onRemove={this.handleUploadOnRemove}
-            />
+            <FormLabel labelMessage={messages.imageUploadLabel}>
+              <ImagesDropzone
+                images={imageFile}
+                imagePreviewRatio={135 / 298}
+                acceptedFileTypes="image/jpg, image/jpeg, image/png, image/gif"
+                maxImageFileSize={5000000}
+                maxNumberOfImages={1}
+                onAdd={this.handleUploadOnAdd}
+                onRemove={this.handleUploadOnRemove}
+              />
+            </FormLabel>
           </FormElement>
 
           <FormElement id="e2e-idea-file-upload">
