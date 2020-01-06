870701b1172eb2a73bf251c922d2d37fa86b7e4a
Sébastien Hoorens <sebastien@citizenlab.co>
2020-01-06 11:54:41 +0100

    destroy moderation_status when deleting moderation

--- app/models/concerns/moderatable.rb
+++ app/models/concerns/moderatable.rb
@@ -4,7 +4,7 @@ module Moderatable
   extend ActiveSupport::Concern
 
   included do
-    has_one :moderation_status, dependent: :destroy
+    has_one :moderation_status, dependent: :destroy, foreign_key: :moderatable_id, foreign_type: :moderatable_type
   end
 
 end
