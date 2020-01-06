9f6214bd5709fcdfb14376830fcb9e70b1463e75
Sébastien Hoorens <sebastien@citizenlab.co>
2020-01-06 11:32:38 +0100

    minor

--- app/serializers/web_api/v1/moderation_serializer.rb
+++ app/serializers/web_api/v1/moderation_serializer.rb
@@ -1,13 +1,9 @@
 class WebApi::V1::ModerationSerializer < WebApi::V1::BaseSerializer
   set_type :moderation
 
-  attributes :moderatable_type, :content_title_multiloc, :content_body_multiloc, :content_slug, :created_at
+  attributes :moderatable_type, :content_title_multiloc, :content_body_multiloc, :content_slug, :belongs_to, :created_at
 
   attribute :moderation_status do |object|
     ModerationStatus.where(moderatable_id: object.id, moderatable_type: object.moderatable_type).first&.status || 'unread'
-  end
-
-  attribute :belongs_to do |object, params|
-    object.belongs_to preloaded: params[:preloaded]
   end
 end
