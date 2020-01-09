f2284dddcd5384fb753ebb5e5d6da33afe5fd3e2
Sébastien Hoorens <sebastien@citizenlab.co>
2020-01-09 16:45:36 +0100

    added ordering to topics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_18_161144) do
+ActiveRecord::Schema.define(version: 2020_01_09_163736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
