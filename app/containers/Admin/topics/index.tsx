12606a82304fe63044c2ee810d302404120d4190
Sébastien Hoorens <sebastien@citizenlab.co>
2020-01-14 17:12:02 +0100

    initial version from areas

---
+++ app/containers/Admin/topics/index.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import { InjectedIntlProps } from 'react-intl';
+import { isNilOrError } from 'utils/helperUtils';
+
+import GetTopics, { GetTopicsChildProps } from 'resources/GetTopics';
+import { deleteTopic } from 'services/topics';
+
+import messages from './messages';
+import T from 'components/T';
+import { FormattedMessage, injectIntl } from 'utils/cl-intl';
+
+import { Section, SectionSubtitle, SectionTitle } from 'components/admin/Section';
+import { List, Row, TextCell } from 'components/admin/ResourceList';
+import Button from 'components/UI/Button';
+import { ButtonWrapper } from 'components/admin/PageWrapper';
+
+interface InputProps { }
+
+interface DataProps {
+  topics: GetTopicsChildProps;
+}
+
+interface Props extends InputProps, DataProps { }
+
+interface State {
+  terminologyOpened: boolean;
+}
+
+class TopicList extends React.PureComponent<Props & InjectedIntlProps, State>{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      terminologyOpened: false,
+    };
+  }
+
+  handleToggleTerminology = () => {
+    this.setState(({ terminologyOpened }) => ({ terminologyOpened: !terminologyOpened }));
+  }
+
+  handleDeleteClick = (topicId: string) => (event: React.FormEvent<any>) => {
+    const deleteMessage = this.props.intl.formatMessage(messages.topicDeletionConfirmation);
+    event.preventDefault();
+
+    if (window.confirm(deleteMessage)) {
+      deleteTopic(topicId);
+    }
+  }
+
+  render() {
+    const { topics } = this.props;
+
+    if (isNilOrError(topics)) return null;
+
+    return (
+      <Section>
+        <SectionTitle>
+          <FormattedMessage {...messages.titleTopics} />
+        </SectionTitle>
+        <SectionSubtitle>
+          <FormattedMessage {...messages.subtitleTopics} />
+        </SectionSubtitle>
+
+        <ButtonWrapper>
+          <Button
+            style="cl-blue"
+            icon="plus-circle"
+            linkTo="/admin/topics/new"
+          >
+            <FormattedMessage {...messages.addTopicButton} />
+          </Button>
+        </ButtonWrapper>
+        <List>
+          {topics.map((topic, index) => (
+            <Row key={topic.id} lastItem={(index === topics.length - 1)}>
+              <TextCell className="expand">
+                <T value={topic.attributes.title_multiloc} />
+              </TextCell>
+              <Button
+                onClick={this.handleDeleteClick(topic.id)}
+                style="text"
+                icon="delete"
+              >
+                <FormattedMessage {...messages.deleteButtonLabel} />
+              </Button>
+              <Button
+                linkTo={`/admin/topics/${topic.id}`}
+                style="secondary"
+                icon="edit"
+              >
+                <FormattedMessage {...messages.editButtonLabel} />
+              </Button>
+            </Row>
+          ))}
+        </List>
+      </Section>
+    );
+  }
+}
+
+const TopicListWithHoCs = injectIntl<Props>(TopicList);
+
+export default () => (
+  <GetTopics>
+    {topics => (<TopicListWithHoCs topics={topics} />)}
+  </GetTopics>
+);
---
+++ app/containers/Admin/topics/messages.ts
@@ -0,0 +1,80 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  subtitleTopics: {
+    id: 'app.containers.AdminPage.SettingsPage.subtitleTopics',
+    defaultMessage: 'Define the geographical topics (neighbourhoods, suburbs, …) that you can ask users who register on the platform. Topics can be linked to projects and can be used to create Smart Groups and give particular groups access to certain projects.',
+  },
+  titleTopics: {
+    id: 'app.containers.AdminPage.SettingsPage.titleTopics',
+    defaultMessage: 'Topics configuration',
+  },
+  subtitleTerminology: {
+    id: 'app.containers.AdminPage.SettingsPage.subtitleTerminology',
+    defaultMessage: 'Terminology',
+  },
+  deleteButtonLabel: {
+    id: 'app.containers.AdminPage.SettingsPage.deleteButtonLabel',
+    defaultMessage: 'Delete',
+  },
+  editButtonLabel: {
+    id: 'app.containers.AdminPage.SettingsPage.editButtonLabel',
+    defaultMessage: 'Edit',
+  },
+  addTopicButton: {
+    id: 'app.containers.AdminPage.SettingsPage.addTopicButton',
+    defaultMessage: 'Add topic',
+  },
+  topicDeletionConfirmation: {
+    id: 'app.containers.AdminPage.SettingsPage.topicDeletionConfirmation',
+    defaultMessage: 'Are you sure you want to delete this topic?',
+  },
+  fieldTitle: {
+    id: 'app.containers.AdminPage.SettingsPage.fieldTitle',
+    defaultMessage: 'Topic name',
+  },
+  fieldTitleTooltip: {
+    id: 'app.containers.AdminPage.SettingsPage.fieldTitleTooltip',
+    defaultMessage: 'The name you choose for each topic will be visible for citizens during signup and when filtering projects.',
+  },
+  fieldDescription: {
+    id: 'app.containers.AdminPage.SettingsPage.fieldDescription',
+    defaultMessage: 'Topic description',
+  },
+  fieldDescriptionTooltip: {
+    id: 'app.containers.AdminPage.SettingsPage.fieldDescriptionTooltip',
+    defaultMessage: 'This description is only for internal collaboration with other administrators, to have a clear understanding what is meant by each topic.',
+  },
+  editFormTitle: {
+    id: 'app.containers.AdminPage.SettingsPage.editFormTitle',
+    defaultMessage: 'Edit topic',
+  },
+  titleTopicTerms: {
+    id: 'app.containers.AdminPage.SettingsPage.titleTopicTerms',
+    defaultMessage: 'Topic terminology',
+  },
+  terminologyTooltip: {
+    id: 'app.containers.AdminPage.SettingsPage.terminologyTooltip',
+    defaultMessage: 'How should topics be called towards users? e.g. neighbourhoods, quarters, counties, ...',
+  },
+  topicTerm: {
+    id: 'app.containers.AdminPage.SettingsPage.topicTerm',
+    defaultMessage: 'Term for one topic (singular)',
+  },
+  topicTermPlaceholder: {
+    id: 'app.containers.AdminPage.SettingsPage.topicTermPlaceholder',
+    defaultMessage: 'topic',
+  },
+  topicsTerm: {
+    id: 'app.containers.AdminPage.SettingsPage.topicsTerm',
+    defaultMessage: 'Term for multiple topics (plural)',
+  },
+  topicsTermPlaceholder: {
+    id: 'app.containers.AdminPage.SettingsPage.topicsTermPlaceholder',
+    defaultMessage: 'topics',
+  },
+  topicsTermsSave: {
+    id: 'app.containers.AdminPage.SettingsPage.topicsTermsSave',
+    defaultMessage: 'Save',
+  }
+});
