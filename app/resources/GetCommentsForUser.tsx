d8424cea34fb17a9ab32d554a2bba45d3f9ac332
Sara <sara@citizenlab.co>
2020-01-28 17:12:30 +0100

    Fixes the issue with loading more comments.

--- app/resources/GetCommentsForUser.tsx
+++ app/resources/GetCommentsForUser.tsx
@@ -79,15 +79,15 @@ export default class GetCommentsForUser extends React.Component<Props, State> {
 
           // if we had not set loading more, we should'nt aggregate the content,
           // it's either first load for this id or a refetch
-          this.setState({
+          this.setState(({ pageNumber }) => ({
             hasMore,
             commentsList: (!loadingMore
               ? newComments.data
               : [...(!isNilOrError(commentsList) ? commentsList : []), ...newComments.data]),
             loadingMore: false,
-            pageNumber: selfLink,
-            querying: false
-          });
+            querying: false,
+            pageNumber: loadingMore ? pageNumber + 1 : pageNumber
+          }));
         }
 
       })
@@ -115,6 +115,7 @@ export default class GetCommentsForUser extends React.Component<Props, State> {
 
   render() {
     const { children } = this.props;
+    console.log(this.state);
     return (children as children)({ loadMore: this.loadMore, ...this.state });
   }
 }
