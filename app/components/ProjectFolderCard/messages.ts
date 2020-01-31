fbbb590715f9b0a6670ca7b5c9f67fb78af2777b
brentguf <brent.guffens@gmail.com>
2020-01-31 12:30:45 +0100

    Remove ProjectLabel

--- app/components/ProjectFolderCard/messages.ts
+++ app/components/ProjectFolderCard/messages.ts
@@ -2,71 +2,43 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   idea: {
-    id: 'app.components.ProjectCard.idea',
+    id: 'app.components.ProjectFolderCard.idea',
     defaultMessage: 'idea',
   },
   ideas: {
-    id: 'app.components.ProjectCard.ideas',
+    id: 'app.components.ProjectFolderCard.ideas',
     defaultMessage: 'ideas',
   },
   xIdeas: {
-    id: 'app.components.ProjectCard.xIdeas',
+    id: 'app.components.ProjectFolderCard.xIdeas',
     defaultMessage: '{ideasCount, plural, no {# ideas} one {# idea} other {# ideas}}',
   },
   xComments: {
-    id: 'app.components.ProjectCard.xComments',
+    id: 'app.components.ProjectFolderCard.xComments',
     defaultMessage: '{commentsCount, plural, no {# comments} one {# comments} other {# comments}}',
   },
   archived: {
-    id: 'app.components.ProjectCard.archived',
+    id: 'app.components.ProjectFolderCard.archived',
     defaultMessage: 'Archived',
   },
   finished: {
-    id: 'app.components.ProjectCard.finished',
+    id: 'app.components.ProjectFolderCard.finished',
     defaultMessage: 'Finished',
   },
   remaining: {
-    id: 'app.components.ProjectCard.remaining',
+    id: 'app.components.ProjectFolderCard.remaining',
     defaultMessage: '{timeRemaining} remaining',
   },
-  allocateYourBudget: {
-    id: 'app.components.ProjectCard.allocateYourBudget',
-    defaultMessage: 'Allocate your budget',
-  },
   learnMore: {
-    id: 'app.components.ProjectCard.learnMore',
+    id: 'app.components.ProjectFolderCard.learnMore',
     defaultMessage: 'Learn more',
   },
-  takeTheSurvey: {
-    id: 'app.components.ProjectCard.takeTheSurvey',
-    defaultMessage: 'Take the survey',
-  },
-  takeThePoll: {
-    id: 'app.components.ProjectCard.takeThePoll',
-    defaultMessage: 'Take the poll',
-  },
-  postYourIdea: {
-    id: 'app.components.ProjectCard.postYourIdea',
-    defaultMessage: 'Post your idea',
-  },
-  vote: {
-    id: 'app.components.ProjectCard.vote',
-    defaultMessage: 'Vote',
-  },
-  comment: {
-    id: 'app.components.ProjectCard.comment',
-    defaultMessage: 'Comment',
-  },
-  viewTheIdeas: {
-    id: 'app.components.ProjectCard.viewTheIdeas',
-    defaultMessage: 'View the ideas',
-  },
   a11y_projectTitle: {
-    id: 'app.components.ProjectCard.a11y_projectTitle',
+    id: 'app.components.ProjectFolderCard.a11y_projectTitle',
     defaultMessage: 'Project title: ',
   },
   a11y_projectDescription: {
-    id: 'app.components.ProjectCard.a11y_projectDescription',
+    id: 'app.components.ProjectFolderCard.a11y_projectDescription',
     defaultMessage: 'Project description: ',
   }
 });
