b54fb524313b181fe7f0077055758423f60e34ce
Sara <sara@citizenlab.co>
2020-02-04 16:20:05 +0100

    fixes extract_intl

--- app/containers/Admin/projects/folders/messages.ts
+++ app/containers/Admin/projects/folders/messages.ts
@@ -3,95 +3,95 @@ import {  defineMessages } from 'react-intl';
 export default defineMessages({
   // edit - index
   titleSettingsTab: {
-    id: 'app.containers.AdminPage.ProjectEdit.titleSettingsTab',
+    id: 'app.containers.AdminPage.FoldersEdit.titleSettingsTab',
     defaultMessage: 'Settings',
   },
   subtitleSettingsTab: {
-    id: 'app.containers.AdminPage.ProjectEdit.subtitleSettingsTab',
+    id: 'app.containers.AdminPage.FoldersEdit.subtitleSettingsTab',
     defaultMessage: 'Explain why grouped project belong together, define a visual identity, expose shared information.',
   },
   viewPublicProjectFolder: {
-    id: 'app.containers.AdminPage.ProjectEdit.viewPublicProjectFolder',
+    id: 'app.containers.AdminPage.FoldersEdit.viewPublicProjectFolder',
     defaultMessage: 'View Folder',
   },
   projectFolderSettingsTab: {
-    id: 'app.containers.AdminPage.ProjectEdit.projectFolderSettingsTab',
+    id: 'app.containers.AdminPage.FoldersEdit.projectFolderSettingsTab',
     defaultMessage: 'Settings',
   },
   projectFolderProjectsTab: {
-    id: 'app.containers.AdminPage.ProjectEdit.projectFolderProjectsTab',
+    id: 'app.containers.AdminPage.FoldersEdit.projectFolderProjectsTab',
     defaultMessage: 'Projects',
   },
   newProjectFolder: {
-    id: 'app.containers.AdminPage.ProjectEdit.newFolder',
+    id: 'app.containers.AdminPage.FoldersEdit.newFolder',
     defaultMessage: 'New folder',
   },
   titleInputLabel: {
-    id: 'app.containers.AdminPage.ProjectEdit.titleInputLabel',
+    id: 'app.containers.AdminPage.FoldersEdit.titleInputLabel',
     defaultMessage: 'Title',
   },
   shortDescriptionInputLabel: {
-    id: 'app.containers.AdminPage.ProjectEdit.shortDescriptionInputLabel',
+    id: 'app.containers.AdminPage.FoldersEdit.shortDescriptionInputLabel',
     defaultMessage: 'Short description',
   },
   shortDescriptionInputLabelTooltip: {
-    id: 'app.containers.AdminPage.ProjectEdit.shortDescriptionInputLabelTooltip',
+    id: 'app.containers.AdminPage.FoldersEdit.shortDescriptionInputLabelTooltip',
     defaultMessage: 'shown in the landing page',
   },
   descriptionInputLabel: {
-    id: 'app.containers.AdminPage.ProjectEdit.descriptionInputLabel',
+    id: 'app.containers.AdminPage.FoldersEdit.descriptionInputLabel',
     defaultMessage: 'Description',
   },
   headerImageInputLabel: {
-    id: 'app.containers.AdminPage.ProjectEdit.headerImageInputLabel',
+    id: 'app.containers.AdminPage.FoldersEdit.headerImageInputLabel',
     defaultMessage: 'Header image',
   },
   save: {
-    id: 'app.containers.AdminPage.SettingsPage.save',
+    id: 'app.containers.AdminPage.FoldersEdit.save',
     defaultMessage: 'Save',
   },
   saveSuccess: {
-    id: 'app.containers.AdminPage.SettingsPage.saveSuccess',
+    id: 'app.containers.AdminPage.FoldersEdit.saveSuccess',
     defaultMessage: 'Success!',
   },
   saveErrorMessage: {
-    id: 'app.containers.AdminPage.SettingsPage.saveErrorMessage',
+    id: 'app.containers.AdminPage.FoldersEdit.saveErrorMessage',
     defaultMessage: 'Something went wrong, please try again later.',
   },
   saveSuccessMessage: {
-    id: 'app.containers.AdminPage.SettingsPage.saveSuccessMessage',
+    id: 'app.containers.AdminPage.FoldersEdit.saveSuccessMessage',
     defaultMessage: 'Your changes have been saved.',
   },
   titleNewFolder: {
-    id: 'app.containers.AdminPage.SettingsPage.titleNewFolder',
+    id: 'app.containers.AdminPage.FoldersEdit.titleNewFolder',
     defaultMessage: 'Create a new folder',
   },
   subtitleNewFolder: {
-    id: 'app.containers.AdminPage.SettingsPage.subtitleNewFolder',
+    id: 'app.containers.AdminPage.FoldersEdit.subtitleNewFolder',
     defaultMessage: 'Explain why grouped project belong together, define a visual identity, expose shared information.',
   },
   inFolder: {
-    id: 'app.containers.AdminPage.SettingsPage.inFolder',
+    id: 'app.containers.AdminPage.FoldersEdit.inFolder',
     defaultMessage: 'In this folder',
   },
   otherProjects: {
-    id: 'app.containers.AdminPage.SettingsPage.otherProjects',
+    id: 'app.containers.AdminPage.FoldersEdit.otherProjects',
     defaultMessage: 'Outside this folder',
   },
   otherProjectsTooltip: {
-    id: 'app.containers.AdminPage.SettingsPage.otherProjectsTooltip',
+    id: 'app.containers.AdminPage.FoldersEdit.otherProjectsTooltip',
     defaultMessage: 'These are the all the projects of the platform that are not in a folder.',
   },
   emptyFolder: {
-    id: 'app.containers.AdminPage.SettingsPage.emptyFolder',
+    id: 'app.containers.AdminPage.FoldersEdit.emptyFolder',
     defaultMessage: 'This folder is empty, you\'re in the right place to add projects to it, though !',
   },
   addToFolder: {
-    id: 'app.containers.AdminPage.SettingsPage.addToFolder',
+    id: 'app.containers.AdminPage.FoldersEdit.addToFolder',
     defaultMessage: 'Add to folder',
   },
   removeFromFolder: {
-    id: 'app.containers.AdminPage.SettingsPage.removeFromFolder',
+    id: 'app.containers.AdminPage.FoldersEdit.removeFromFolder',
     defaultMessage: 'Remove from folder',
   },
 });
