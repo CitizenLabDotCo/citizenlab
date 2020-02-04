1e8b514432ee6f4850f3979e09f1b1f92791904e
Sébastien Hoorens <sebastien@citizenlab.co>
2020-02-04 15:20:42 +0100

    ensure project holder orderings when changing project stuff

--- app/services/project_holder_service.rb
+++ app/services/project_holder_service.rb
@@ -2,11 +2,17 @@ class ProjectHolderService
 
   def fix_project_holder_orderings!
     folder_ids = ProjectFolder.ids
-    ProjectHolderOrdering.create(folder_ids.map{|id| {project_holder_id: id, project_holder_type: 'ProjectFolder'}})
+    missing_folder_ids = folder_ids - ProjectHolderOrdering.where(project_holder_type: 'ProjectFolder').where(project_holder_id: folder_ids).ids
+    ProjectHolderOrdering.create!(missing_folder_ids.map{|id| 
+      {project_holder_id: id, project_holder_type: 'ProjectFolder'}
+    })
     ProjectHolderOrdering.where(project_holder_type: 'ProjectFolder').where.not(project_holder_id: folder_ids).destroy_all
 
     project_ids = Project.where('folder_id IS NULL').published.ids
-    ProjectHolderOrdering.create(project_ids.map{|id| {project_holder_id: id, project_holder_type: 'Project'}})
+    missing_project_ids = project_ids - ProjectHolderOrdering.where(project_holder_type: 'Project').where(project_holder_id: project_ids).ids
+    ProjectHolderOrdering.create!(missing_project_ids.map{|id| 
+      {project_holder_id: id, project_holder_type: 'Project'}
+    })
     ProjectHolderOrdering.where(project_holder_type: 'Project').where.not(project_holder_id: project_ids).destroy_all
   end
 
