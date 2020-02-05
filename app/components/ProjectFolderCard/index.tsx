edfd4e0073845c866963778d57713ac4d1056da4
brentguf <brent.guffens@gmail.com>
2020-02-05 11:26:52 +0100

    imageUrl type checking

--- app/components/ProjectFolderCard/index.tsx
+++ app/components/ProjectFolderCard/index.tsx
@@ -304,7 +304,7 @@ class ProjectCard extends PureComponent<Props & InjectedIntlProps> {
     const { projectFolder, size, layout, className } = this.props;
 
     if (!isNilOrError(projectFolder)) {
-      const imageUrl = projectFolder.attributes.header_bg.medium;
+      const imageUrl = projectFolder?.attributes?.header_bg?.medium;
       const folderUrl = getProjectFolderUrl(projectFolder);
 
       const contentHeader = (
