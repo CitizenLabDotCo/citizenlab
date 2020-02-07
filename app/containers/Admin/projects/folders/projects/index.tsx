b7f804e4e0aa9c647c51d6ef2580faa35df7c98d
brentguf <brent.guffens@gmail.com>
2020-02-07 11:50:07 +0100

    Folders/projects min-height

--- app/containers/Admin/projects/folders/projects/index.tsx
+++ app/containers/Admin/projects/folders/projects/index.tsx
@@ -24,7 +24,9 @@ import ProjectRow from '../../components/ProjectRow';
 // style
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  min-height: 60vh;
+`;
 
 const ListsContainer = styled.div``;
 
