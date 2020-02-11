51a1580c2586d2b4b0d31a021bc500a6ba1178b9
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2020-02-11 14:33:11 +0100

    Revert "CL2-4925 restricted schema access to project access"
    This reverts commit b9a257719e9b6a233db3334d5a5ee1e140aa300e.

--- spec/policies/idea_custom_field_policy_spec.rb
+++ spec/policies/idea_custom_field_policy_spec.rb
@@ -8,23 +8,10 @@ describe IdeaCustomFieldPolicy do
   let!(:project) { create(:project, custom_form: custom_form) }
   let!(:idea_custom_field) { create(:custom_field, resource: custom_form) }
 
-  context "for a mortal user with access to the project" do
+  context "for a mortal user" do
     let(:user) { create(:user) }
 
       it { should     permit(:schema)    }
-      it { should_not permit(:show)    }
-      it { should_not permit(:upsert_by_code)  }
-
-      it "should not index the custom field" do
-        expect(scope.resolve.size).to eq 0
-      end
-  end
-
-  context "for a mortal user without access to the project" do
-    let(:user) { create(:user) }
-    let!(:project) { create(:private_groups_project, custom_form: custom_form) }
-
-      it { should_not permit(:schema)    }
       it { should_not permit(:show)    }
       it { should_not permit(:upsert_by_code)  }
 
