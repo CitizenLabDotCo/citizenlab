04e4d2f575f8e78dbce76bd741c23bc5775e07f7
brentguf <brent.guffens@gmail.com>
2020-02-14 14:40:24 +0100

    PHO comment

--- app/services/projectHolderOrderings.ts
+++ app/services/projectHolderOrderings.ts
@@ -3,6 +3,11 @@ import streams from 'utils/streams';
 import { API_PATH } from 'containers/App/constants';
 const apiEndpoint = `${API_PATH}/project_holder_orderings`;
 
+/*
+  Data structure to handle the order when there's a mix of projects and folders,
+  as seen in the admin project overview when the folders feature is enabled.
+*/
+
 export interface IProjectHolderOrderingData {
   id: string;
   type: 'project_holder_orderings';
