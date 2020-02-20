baf6b3e7716256e17fa509f3b24f95fa061e8aaa
Sébastien Hoorens <sebastien@citizenlab.co>
2020-02-20 11:47:21 +0100

    fix project holders to also make for non-published top level projects

--- app/services/project_holder_service.rb
+++ app/services/project_holder_service.rb
@@ -12,7 +12,7 @@ class ProjectHolderService
     })
     ProjectHolderOrdering.where(project_holder_type: 'ProjectFolder').where.not(project_holder_id: folder_ids).destroy_all
 
-    project_ids = Project.where(folder_id: nil).published.order(:ordering).ids
+    project_ids = Project.where(folder_id: nil).order(:ordering).ids
     missing_project_ids = project_ids - ProjectHolderOrdering.where(project_holder_type: 'Project').where(project_holder_id: project_ids).pluck(:project_holder_id)
     ProjectHolderOrdering.create!(missing_project_ids.map{|id|
       {project_holder_id: id, project_holder_type: 'Project'}
