7a20592ed4317fa4033bd060c25dc5bf18d832dc
Sara <sara@citizenlab.co>
2020-03-02 15:42:10 +0100

    More actions Menu

--- .storybook/themedecorator.js
+++ .storybook/themedecorator.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { getTheme } from '../app/utils/styleUtils';
 import GlobalStyle from '../app/global-styles';
+import 'assets/css/reset.min.css';
+import 'assets/fonts/fonts.css';
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/themes/light.css';
 
 const ThemeDecorator = storyFn => (
   <ThemeProvider theme={getTheme(null)}>
