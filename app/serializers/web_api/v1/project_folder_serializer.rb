ef8eddafca451e6c259e758a59376665d2d6f08c
Sara <sara@citizenlab.co>
2020-03-10 15:39:48 +0100

    Implements project folder images ressource

--- app/serializers/web_api/v1/project_folder_serializer.rb
+++ app/serializers/web_api/v1/project_folder_serializer.rb
@@ -10,4 +10,6 @@ class WebApi::V1::ProjectFolderSerializer < WebApi::V1::BaseSerializer
     object.projects.order(:ordering)
   end
 
+  has_many :project_folder_images, serializer: WebApi::V1::ImageSerializer
+
 end
