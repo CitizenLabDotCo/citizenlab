b576d2bb86ed158c971d67b20c46ca5346a4af54
Sébastien Hoorens <sebastien@citizenlab.co>
2020-03-11 18:09:37 +0100

    initial version migration script

--- db/migrate/20200311132551_create_admin_publications.rb
+++ db/migrate/20200311132551_create_admin_publications.rb
@@ -1,5 +1,6 @@
 class CreateAdminPublications < ActiveRecord::Migration[6.0]
   def change
+    # Create admin publications
     create_table :admin_publications, id: :uuid do |t|
       t.uuid :parent_id, null: true, index: true
       t.integer :lft, null: false, index: true
@@ -13,15 +14,40 @@ class CreateAdminPublications < ActiveRecord::Migration[6.0]
       t.timestamps
     end
 
-    # ProjectHolderOrdering.order(:ordering).each.with_index do |pho, index|
-    #   # [:publication_type, :publication_id, ordering: index]
-    #   AdminPublication.create!()
-    # end
+    # Migrate existing data
+    folder_to_publication_id = {}
+    sql = %q(
+      SELECT project_holder_id, project_holder_type, ordering 
+      FROM project_holder_orderings
+      ORDER BY ordering DESC;
+    )
+    ActiveRecord::Base.connection.execute(sql).each do |pho|
+      publication = AdminPublication.create!(
+        publication_id:   pho['project_holder_id'],
+        publication_type: pho['project_holder_type'],
+        ordering:         pho['ordering']
+        )
+      if pho['project_holder_type'] == 'ProjectFolder'
+        folder_to_publication_id[pho['project_holder_id']] = publication.id
+      end
+    end
 
-    # Project.where('folder_id IS NOT NULL').each do prrt
-    #   ordering????
-    # end
+    sql = %q(
+      SELECT id, project_holder_type, ordering, folder_id
+      FROM projects
+      WHERE folder_id IS NOT NULL
+      ORDER BY ordering DESC;
+    )
+    ActiveRecord::Base.connection.execute(sql).each do |pho|
+      AdminPublication.create!(
+        publication_id:   pho['id'],
+        publication_type: 'Project',
+        ordering:         pho['ordering'],
+        parent_id: folder_to_publication_id[pho['folder_id']]
+        )
+    end
 
+    # Remove project holder modelling
     drop_table :project_holder_orderings
     remove_column :projects, :folder_id, :uuid
     remove_column :projects, :ordering, :integer
