af73ba9969b1f5b3e937545356b9a6b14a8fc03c
Sébastien Hoorens <sebastien@citizenlab.co>
2020-03-11 18:37:01 +0100

    second version migration script

--- db/migrate/20200311132551_create_admin_publications.rb
+++ db/migrate/20200311132551_create_admin_publications.rb
@@ -17,15 +17,14 @@ class CreateAdminPublications < ActiveRecord::Migration[6.0]
     # Migrate existing data
     folder_to_publication_id = {}
     sql = %q(
-      SELECT project_holder_id, project_holder_type, ordering 
+      SELECT project_holder_id, project_holder_type 
       FROM project_holder_orderings
       ORDER BY ordering DESC;
     )
     ActiveRecord::Base.connection.execute(sql).each do |pho|
       publication = AdminPublication.create!(
         publication_id:   pho['project_holder_id'],
-        publication_type: pho['project_holder_type'],
-        ordering:         pho['ordering']
+        publication_type: pho['project_holder_type']
         )
       if pho['project_holder_type'] == 'ProjectFolder'
         folder_to_publication_id[pho['project_holder_id']] = publication.id
@@ -33,7 +32,7 @@ class CreateAdminPublications < ActiveRecord::Migration[6.0]
     end
 
     sql = %q(
-      SELECT id, project_holder_type, ordering, folder_id
+      SELECT id, folder_id
       FROM projects
       WHERE folder_id IS NOT NULL
       ORDER BY ordering DESC;
@@ -42,7 +41,6 @@ class CreateAdminPublications < ActiveRecord::Migration[6.0]
       AdminPublication.create!(
         publication_id:   pho['id'],
         publication_type: 'Project',
-        ordering:         pho['ordering'],
         parent_id: folder_to_publication_id[pho['folder_id']]
         )
     end
