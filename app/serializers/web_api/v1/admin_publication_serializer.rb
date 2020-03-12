8923c02dd2beaa88b5338cbd626511fd0c29f6fd
Sébastien Hoorens <sebastien@citizenlab.co>
2020-03-12 17:16:23 +0100

    rm ordering from admin pub serializer

--- app/serializers/web_api/v1/admin_publication_serializer.rb
+++ app/serializers/web_api/v1/admin_publication_serializer.rb
@@ -1,5 +1,5 @@
 class WebApi::V1::AdminPublicationSerializer < WebApi::V1::BaseSerializer
-  attributes :ordering, :parent_id
+  attributes :parent_id
 
   belongs_to :publication, polymorphic: true
 end
