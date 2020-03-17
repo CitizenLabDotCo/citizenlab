8323ec6ddf498992f90aea1222ebeb546d4afb54
Sara <sara@citizenlab.co>
2020-03-17 17:04:07 +0100

    Show top-level projects and folders

--- app/containers/LandingPage/index.tsx
+++ app/containers/LandingPage/index.tsx
@@ -202,6 +202,7 @@ class LandingPage extends PureComponent<Props, State> {
                         publicationStatusFilter={['published', 'archived']}
                         showTitle={true}
                         layout="dynamic"
+                        folderId={null}
                       />
                     </Suspense>
                   </SectionContainer>
