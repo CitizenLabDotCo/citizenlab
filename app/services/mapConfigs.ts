959ce200a1107aa4d7d3400c68932599610507e1
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2020-03-18 16:06:56 +0100

    CL2-5073 map config service, resource and hook

---
+++ app/services/mapConfigs.ts
@@ -0,0 +1,29 @@
+import { API_PATH } from 'containers/App/constants';
+import streams from 'utils/streams';
+import { Multiloc } from 'typings';
+
+export interface IMapConfigData {
+  id: string;
+  type: string;
+  attributes: {
+    zoom_level?: string;
+    tile_provider?: string;
+    center_geojson?: GeoJSON.Point;
+    layers: {
+      title_multiloc: Multiloc;
+      geojson: GeoJSON.GeoJsonObject;
+      legend: {
+        title_multiloc: Multiloc;
+        color?: string;
+      }[];
+    }[];
+  };
+}
+
+export interface IMapConfig {
+  data: IMapConfigData;
+}
+
+export const mapConfigByProjectStream = (projectId: string) => {
+  return streams.get<IMapConfig>({ apiEndpoint: `${API_PATH}/projects/${projectId}/map_config` });
+};
