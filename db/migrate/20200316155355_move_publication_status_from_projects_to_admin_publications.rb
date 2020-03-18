cbbffef7ef442c1921f987538c335ed34135345d
Sébastien Hoorens <sebastien@citizenlab.co>
2020-03-18 18:33:11 +0100

    fixed minor issue migrations

--- db/migrate/20200316155355_move_publication_status_from_projects_to_admin_publications.rb
+++ db/migrate/20200316155355_move_publication_status_from_projects_to_admin_publications.rb
@@ -1,7 +1,5 @@
 class MovePublicationStatusFromProjectsToAdminPublications < ActiveRecord::Migration[6.0]
   def change
-    add_column :admin_publications, :publication_status, :string, null: false, default: 'published'
-
     Project.where.not(publication_status: 'published').includes(:admin_publication).each do |project|
       project.admin_publication&.update_column :publication_status, project.publication_status
     end
