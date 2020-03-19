5e43cf3c7ea78be82699f487659cb0e2579e848f
Sébastien Hoorens <sebastien@citizenlab.co>
2020-03-19 15:18:31 +0100

    added publication title description preview and children count to serializer

--- app/serializers/web_api/v1/admin_publication_serializer.rb
+++ app/serializers/web_api/v1/admin_publication_serializer.rb
@@ -1,5 +1,21 @@
 class WebApi::V1::AdminPublicationSerializer < WebApi::V1::BaseSerializer
-  attributes :parent_id, :ordering, :publication_status
+  attributes :parent_id, :ordering, :publication_status, :children_count
+
+  attribute :publication_title_multiloc do |object|
+    object.publication.title_multiloc
+  end
+
+  attribute :publication_description_multiloc do |object|
+    object.publication.description_multiloc
+  end
+
+  attribute :publication_description_preview_multiloc do |object|
+    object.publication.description_preview_multiloc
+  end
+
+  attribute :publication_slug do |object|
+    object.publication.slug
+  end
 
   belongs_to :publication, polymorphic: true
 end
