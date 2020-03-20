df5bdfc54d69c41d12e5c3dda9ca00dde0e30334
Sébastien Hoorens <sebastien@citizenlab.co>
2020-03-20 14:03:29 +0100

    added publication_visible_to to admin publication serializer

--- app/serializers/web_api/v1/admin_publication_serializer.rb
+++ app/serializers/web_api/v1/admin_publication_serializer.rb
@@ -17,6 +17,12 @@ class WebApi::V1::AdminPublicationSerializer < WebApi::V1::BaseSerializer
     object.publication.slug
   end
 
+  attribute :publication_visible_to, if: Proc.new { |object|
+    object.publication_type == Project.name
+  } do |object|
+    object.publication.visible_to
+  end
+
   attribute :visible_children_count do |object, params|
     params.dig(:visible_children_count_by_parent_id, object.id) || Pundit.policy_scope(current_user(params), Project).where(id: object.children.map(&:publication_id)).count
   end
