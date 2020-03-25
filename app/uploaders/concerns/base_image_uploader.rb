0e0dfbfc0dc6f706990b17e8ce539a4ba25c9fe0
Sébastien Hoorens <sebastien@citizenlab.co>
2020-03-25 14:10:22 +0100

    what happens when also overriding asset_host for tests

--- app/uploaders/concerns/base_image_uploader.rb
+++ app/uploaders/concerns/base_image_uploader.rb
@@ -12,21 +12,19 @@ module BaseImageUploader
     "uploads/#{tenant.id}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  unless Rails.env.test?
-    def asset_host
-      begin
-        Tenant.current.base_backend_uri
-      rescue ActiveRecord::RecordNotFound # There is no Tenant.current
+  def asset_host
+    begin
+      Tenant.current.base_backend_uri
+    rescue ActiveRecord::RecordNotFound # There is no Tenant.current
 
-        # Maybe the model we're operating on is a Tenant itself?
-        if model.kind_of? Tenant
-          model.base_backend_uri
+      # Maybe the model we're operating on is a Tenant itself?
+      if model.kind_of? Tenant
+        model.base_backend_uri
 
-        # Nope, so let's fall back to default carrierwave behavior (s3 bucket
-        # in production)
-        else
-          super
-        end
+      # Nope, so let's fall back to default carrierwave behavior (s3 bucket
+      # in production)
+      else
+        super
       end
     end
   end
