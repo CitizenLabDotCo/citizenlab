7aaee61851e5aba0015a91d80fbe718ad984b6eb
Sébastien Hoorens <sebastien@citizenlab.co>
2020-03-26 09:42:37 +0100

    miinor

--- app/uploaders/concerns/base_image_uploader.rb
+++ app/uploaders/concerns/base_image_uploader.rb
@@ -14,8 +14,8 @@ module BaseImageUploader
     "uploads/#{tenant.id}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def asset_host
-    unless Rails.env.test?
+  unless Rails.env.test?
+    def asset_host
       begin
         Tenant.current.base_backend_uri
       rescue ActiveRecord::RecordNotFound # There is no Tenant.current
