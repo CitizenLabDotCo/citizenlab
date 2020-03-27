ab16297bbb8f3c2a96ab1b55005035c5925e56f7
brentguf <brent.guffens@gmail.com>
2020-03-27 14:41:25 +0100

    Unused import

--- app/components/Map/index.tsx
+++ app/components/Map/index.tsx
@@ -1,6 +1,6 @@
 import React, { FormEvent } from 'react';
 import { adopt } from 'react-adopt';
-import { compact, /* get,*/ isNil } from 'lodash-es';
+import { compact, isNil } from 'lodash-es';
 import { isNilOrError } from 'utils/helperUtils';
 require('leaflet-simplestyle');
 
