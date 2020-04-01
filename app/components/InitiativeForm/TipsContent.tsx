1677dc5be54a16f3c8503c3d8a678de454b3a9a7
brentguf <brent.guffens@gmail.com>
2020-04-01 15:42:19 +0200

    Remove eligibility criteria from proposal tips

--- app/components/InitiativeForm/TipsContent.tsx
+++ app/components/InitiativeForm/TipsContent.tsx
@@ -1,10 +1,4 @@
 import React, { memo } from 'react';
-import { get } from 'lodash-es';
-import { isNilOrError } from 'utils/helperUtils';
-
-// components
-import T from 'components/T';
-import QuillEditedContent from 'components/UI/QuillEditedContent';
 
 // i18n
 import { FormattedMessage } from 'utils/cl-intl';
@@ -14,9 +8,6 @@ import messages from './messages';
 import styled, { withTheme } from 'styled-components';
 import { fontSizes } from 'utils/styleUtils';
 
-// hooks
-import useTenant from 'hooks/useTenant';
-
 const Container = styled.div`
   color: ${({ theme }) => theme.colorText};
   font-size: ${fontSizes.base}px;
@@ -24,40 +15,18 @@ const Container = styled.div`
   padding: 20px;
 `;
 
-const StyledQuillEditedContent = styled(QuillEditedContent)`
-  span:last-child ol, span:last-child ul {
-    margin-bottom: 0px;
-  }
-`;
-
 interface Props {
   theme: any;
 }
 
-const TipsContent = memo<Props>((props) => {
-  const tenant = useTenant();
-
-  if (!isNilOrError(tenant)) {
-    const eligibilityCriteriaMultiloc = tenant.data.attributes.settings.initiatives?.eligibility_criteria;
-
-    return (
-      <Container>
-        <p>
-          <FormattedMessage {...messages.tipsExplanation} />
-        </p>
-        {eligibilityCriteriaMultiloc &&
-          <>
-
-            <StyledQuillEditedContent textColor={props.theme.colorText}>
-              <T value={eligibilityCriteriaMultiloc}  supportHtml={true}/>
-            </StyledQuillEditedContent>
-          </>
-        }
-      </Container>
-    );
-  }
-
-  return null;
+const TipsContent = memo<Props>((_props) => {
+  return (
+    <Container>
+      <p>
+        <FormattedMessage {...messages.tipsExplanation} />
+      </p>
+    </Container>
+  );
 });
 
 export default withTheme(TipsContent);
