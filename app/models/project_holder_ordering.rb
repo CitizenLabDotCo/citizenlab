fc086b2bef230da63d79784955f68bfe83a08ca1
Sébastien Hoorens <sebastien@citizenlab.co>
2020-04-02 14:24:02 +0200

    cleanup project holder, official feedback should have post present

--- app/models/project_holder_ordering.rb
+++
@@ -1,7 +0,0 @@
-class ProjectHolderOrdering < ApplicationRecord
-  acts_as_list column: :ordering, top_of_list: 0, add_new_at: :bottom
-
-  belongs_to :project_holder, polymorphic: true
-
-  validates :project_holder, presence: true
-end
