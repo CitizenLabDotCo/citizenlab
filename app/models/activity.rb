2b97676477ffbe9a5058843848b573f3f542680f
Sébastien Hoorens <sebastien@citizenlab.co>
2020-04-02 14:11:01 +0200

    fixed item presence validation for activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,5 +2,5 @@ class Activity < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :item, polymorphic: true, optional: true
 
-  validates :action, :item_type, :item_id, presence: true
+  validates :action, :item, presence: true
 end
