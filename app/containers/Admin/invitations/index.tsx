70b625f680db10f8dbe07f29025f51b9bdbe4c30
brentguf <brent.guffens@gmail.com>
2020-04-03 09:48:48 +0200

    Solve build error

--- app/containers/Admin/invitations/index.tsx
+++ app/containers/Admin/invitations/index.tsx
@@ -9,31 +9,33 @@ import messages from './messages';
 import { InjectedIntlProps } from 'react-intl';
 import { injectIntl } from 'utils/cl-intl';
 
-export class InvitationsPage extends React.PureComponent<InjectedIntlProps> {
-  private tabs = [
+export interface Props {
+  children: JSX.Element;
+}
+
+const InvitationsPage = React.memo((props: Props & InjectedIntlProps) => {
+  const tabs = [
     { label: this.props.intl.formatMessage(messages.tabInviteUsers), url: '/admin/invitations' },
     { label: this.props.intl.formatMessage(messages.tabAllInvitations), url: '/admin/invitations/all' },
   ];
-  private resource = {
+  const resource = {
     title: this.props.intl.formatMessage(messages.invitePeople),
     subtitle: this.props.intl.formatMessage(messages.invitationSubtitle)
   };
 
-  render() {
-    const { children } = this.props;
-    return (
-      <TabbedResource
-        resource={this.resource}
-        tabs={this.tabs}
-      >
-        <HelmetIntl
-          title={messages.helmetTitle}
-          description={messages.helmetDescription}
-        />
-        {children}
-      </TabbedResource>
-    );
-  }
-}
+  const { children } = props;
+  return (
+    <TabbedResource
+      resource={resource}
+      tabs={tabs}
+    >
+      <HelmetIntl
+        title={messages.helmetTitle}
+        description={messages.helmetDescription}
+      />
+      {children}
+    </TabbedResource>
+  );
+});
 
 export default injectIntl(InvitationsPage);
