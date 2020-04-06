59f106ed2ee1d8eef98c526c016d23e5e1eb1ef6
Sébastien Hoorens <sebastien@citizenlab.co>
2020-04-06 15:56:02 +0200

    added folder graphql spec + updated bootsnap in attempt to fix email camp factory const error...

---
+++ engines/admin_api/spec/graphql/folders_spec.rb
@@ -0,0 +1,36 @@
+RSpec.describe "Graphql folder" do
+  let(:context) { {} }
+  let(:result) {
+    AdminApi::Schema.execute(
+      query_string,
+      context: context,
+      variables: variables
+    )
+  }
+
+  describe "folder" do
+    let(:query_string) { %|
+      query projectFolderQuery($id: ID!) {
+        projectFolder(id: $id) {
+          id
+          slug
+          publicationStatus
+        }
+      }
+    |}
+
+
+    let(:folder) { create(:project_folder) }
+    let(:variables) { {id: folder.id }}
+
+    it "returns all folders" do
+      response = result
+      expect(response.dig("data", "folder")).to match ({
+        "id" => folder.id,
+        "slug" => folder.slug,
+        "publicationStatus" => folder.admin_publication.publication_status
+      })
+    end
+
+  end
+end
