b8300a1218b35dd9aa54e891f4c1d931c23ad2dc
Sébastien Hoorens <sebastien@citizenlab.co>
2020-04-13 14:31:14 +0200

    fixed: hours->days and eager loading in development

--- app/services/cl2_data_listing_service.rb
+++ app/services/cl2_data_listing_service.rb
@@ -1,13 +1,15 @@
 class Cl2DataListingService
 
   def cl2_tenant_models
+    # After https://github.com/rails/rails/issues/37006.
+    # It seems that we need to use Zeitwerk::Loader.eager_load_all
+    # instead of Rails.application.eager_load!.
+    Zeitwerk::Loader.eager_load_all if Rails.env.development?
     ActiveRecord::Base.descendants.select do |claz|
       ![
-        'PgSearch::Document', 
-        'Apartment::Adapters::AbstractAdapter::SeparateDbConnectionHandler', 
-        'ApplicationRecord', 
-        'PublicApi::ApiClient', 
-        'Tenant'
+        *ActiveRecord::Base.subclasses.map(&:name),
+        PublicApi::ApiClient.name, 
+        Tenant.name
       ].include? claz.name
     end.select do |claz|
       claz.descendants.empty?
