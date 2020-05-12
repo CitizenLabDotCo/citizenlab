52eba46f6b6e3fafb2e117c766421d061160e0ea
brentguf <brent.guffens@gmail.com>
2020-05-12 10:02:10 +0200

    Add topics tab to project settings

---
+++ app/containers/Admin/projects/edit/topics/messages.ts
@@ -0,0 +1,14 @@
+import {
+  defineMessages
+} from 'react-intl';
+
+export default defineMessages({
+  titleDescription: {
+    id: 'app.containers.AdminPage.Topics.titleDescription',
+    defaultMessage: 'Topics',
+  },
+  subtitleDescription: {
+    id: 'app.containers.AdminPage.Topics.subtitleDescription',
+    defaultMessage: 'Add or remove topics that are available to this project. Topics can be managed in the topic manager.',
+  },
+});
