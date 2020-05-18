fe621639a542611d788eb992cbcd6880845ed129
brentguf <brent.guffens@gmail.com>
2020-05-18 14:32:55 +0200

    topics tab hide condition

--- app/containers/Admin/projects/edit/index.tsx
+++ app/containers/Admin/projects/edit/index.tsx
@@ -29,6 +29,7 @@ import { withRouter, WithRouterProps } from 'react-router';
 import { IProjectData } from 'services/projects';
 import { Subscription } from 'rxjs';
 import eventEmitter from 'utils/eventEmitter';
+import { flatMap } from 'rxjs/operators';
 
 const TopContainer = styled.div`
   width: 100%;
@@ -165,7 +166,7 @@ export class AdminProjectEdition extends PureComponent<Props & InjectedIntlProps
             participationMethod !== 'ideation' &&
             participationMethod !== 'budgeting'
           )
-          ||
+        ||
           (
             processType === 'timeline' &&
             !isNilOrError(phases) &&
@@ -173,6 +174,30 @@ export class AdminProjectEdition extends PureComponent<Props & InjectedIntlProps
               return phase.attributes.participation_method === 'ideation' ||
                      phase.attributes.participation_method === 'budgeting';
 
+            }).length === 0
+          )
+        ) {
+          return true;
+        }
+
+        return false;
+      },
+      topics: function isTopicsTabHidden() {
+        if (
+          !customTopicsEnabled
+        ||
+          (
+            processType === 'continuous' &&
+            participationMethod !== 'ideation' &&
+            participationMethod !== 'budgeting'
+          )
+        ||
+          (
+            processType === 'timeline' &&
+            !isNilOrError(phases) &&
+            phases.filter(phase => {
+              return phase.attributes.participation_method === 'ideation' ||
+                    phase.attributes.participation_method === 'budgeting';
             }).length === 0
           )
         ) {
@@ -237,27 +262,6 @@ export class AdminProjectEdition extends PureComponent<Props & InjectedIntlProps
           name: 'survey-results'
         });
       }
-    }
-
-    if (
-        !customTopicsEnabled
-      ||
-        (
-          processType === 'continuous' &&
-          participationMethod !== 'ideation' &&
-          participationMethod !== 'budgeting'
-        )
-      ||
-        (
-          processType === 'timeline' &&
-          !isNilOrError(phases) &&
-          phases.filter(phase => {
-            return phase.attributes.participation_method === 'ideation' ||
-                  phase.attributes.participation_method === 'budgeting';
-          }).length === 0
-        )
-    ) {
-      tabs = reject(tabs, { name: 'topics' });
     }
 
     return tabs;
