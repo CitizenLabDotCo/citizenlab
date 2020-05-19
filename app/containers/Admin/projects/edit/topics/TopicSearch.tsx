4b8454c407e9cd6204f06428a6292599ce795294
brentguf <brent.guffens@gmail.com>
2020-05-19 11:04:44 +0200

    TopicSearch cleanup WIP

--- app/containers/Admin/projects/edit/topics/TopicSearch.tsx
+++ app/containers/Admin/projects/edit/topics/TopicSearch.tsx
@@ -1,193 +1,87 @@
-// // Libraries
-// import React, { memo, useCallback } from 'react';
-// import { get } from 'lodash-es';
-// import { first } from 'rxjs/operators';
-// import { isNilOrError } from 'utils/helperUtils';
+// Libraries
+import React, { memo, useCallback } from 'react';
+import { isNilOrError } from 'utils/helperUtils';
 
-// // Services
-// import { findMembership, addMembership } from 'services/moderators';
-// import { IGroupMembershipsFoundUserData } from 'services/groupMemberships';
+// Hooks
+import useTopics from 'hooks/useTopics';
 
-// // Resources
-// import { GetModeratorsChildProps } from 'resources/GetModerators';
+// i18n
+import { InjectedIntlProps } from 'react-intl';
+import { injectIntl } from 'utils/cl-intl';
+import messages from './messages';
 
-// // Hooks
-// import useTopics from 'hooks/useTopics';
+// Components
+import Button from 'components/UI/Button';
+import MultipleSelect from 'components/UI/MultipleSelect';
 
-// // i18n
-// import { InjectedIntlProps } from 'react-intl';
-// import { injectIntl } from 'utils/cl-intl';
-// import messages from './messages';
+// Style
+import styled from 'styled-components';
+import selectStyles from 'components/UI/MultipleSelect/styles';
 
-// // Components
-// import Button from 'components/UI/Button';
-// import AsyncSelect from 'react-select/async';
-// import MultipleSelect from 'components/UI/MultipleSelect';
+// Typings
+import { IOption } from 'typings';
 
-// // Style
-// import styled from 'styled-components';
-// import selectStyles from 'components/UI/MultipleSelect/styles';
+const Container = styled.div`
+  width: 100%;
+  margin-bottom: 20px;
+`;
 
-// // Typings
-// import { IOption } from 'typings';
+const SelectGroupsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  align-items: center;
+  margin-bottom: 30px;
+`;
 
-// const Container = styled.div`
-//   width: 100%;
-//   margin-bottom: 20px;
-// `;
+const AddTopicButton = styled(Button)`
+  flex-grow: 0;
+  flex-shrink: 0;
+  margin-left: 30px;
+`;
 
-// const SelectGroupsContainer = styled.div`
-//   width: 100%;
-//   display: flex;
-//   flex-direction: row;
-//   align-items: flex-start;
-//   align-items: center;
-//   margin-bottom: 30px;
-// `;
+interface Props {
+  selectableTopicIds: string[];
+  handleAddSelectedTopic: (topicId: string) => void;
+}
 
-// const StyledAsyncSelect = styled(AsyncSelect)`
-//   min-width: 300px;
-// `;
+const TopicSearch = memo(() => {
+  const topics = useTopics();
 
-// const AddTopicButton = styled(Button)`
-//   flex-grow: 0;
-//   flex-shrink: 0;
-//   margin-left: 30px;
-// `;
+  const handleTopicSelectionChange = useCallback(() => {
 
-// interface Props {}
+  }, []);
 
-// interface State {
-//   selection: IOption[];
-//   loading: boolean;
-//   processing: boolean;
-// }
+  const getOptions = () => {
 
-// function isModerator(user: IGroupMembershipsFoundUserData) {
-//   return get(user.attributes, 'is_moderator') !== undefined;
-// }
+  };
 
-// const MembersAdd = memo(() => {
-//   const topics = useTopics();
+  if (!isNilOrError(topics)) {
 
-//   const handleTopicSelectionChange = useCallback(() => {
+    return (
+      <Container>
+        <SelectGroupsContainer>
+          <MultipleSelect
+            value={selectedTopics}
+            options={}
+            onChange={handleTopicSelectionChange}
+          />
 
-//   }, []);
+          <AddTopicButton
+            text={formatMessage(messages.addTopics)}
+            buttonStyle="cl-blue"
+            icon="plus-circle"
+            onClick={this.handleOnAddModeratorsClick}
+            disabled={!selection || selection.length === 0}
+            processing={this.state.processing}
+          />
+        </SelectGroupsContainer>
+      </Container>
+    );
+  }
 
-//   const getOptions = () => {
+  return null;
+});
 
-//   };
-
-//   if (!isNilOrError(topics)) {
-
-//     return (
-//       <Container>
-//         <SelectGroupsContainer>
-//           <MultipleSelect
-//             value={selectedTopics}
-//             options={}
-//             onChange={handleTopicSelectionChange}
-//           />
-
-//           <AddTopicButton
-//             text={formatMessage(messages.addTopics)}
-//             buttonStyle="cl-blue"
-//             icon="plus-circle"
-//             onClick={this.handleOnAddModeratorsClick}
-//             disabled={!selection || selection.length === 0}
-//             processing={this.state.processing}
-//           />
-//         </SelectGroupsContainer>
-//       </Container>
-//     );
-//   }
-
-//   return null;
-// });
-
-// extends PureComponent<Props & InjectedIntlProps, State> {
-//   constructor(props: Props & InjectedIntlProps) {
-//     super(props);
-//     this.state = {
-//       selection: [],
-//       loading: false,
-//       processing: false
-//     };
-//   }
-
-//   getOptions = (users: IGroupMembershipsFoundUserData[]) => {
-//     return users
-//       .filter((user) => {
-//         let userIsNotYetModerator = true;
-//         const { moderators } = this.props;
-
-//         if (!isNilOrError(moderators)) {
-//           moderators.forEach((moderator) => {
-//             if (moderator.id === user.id) {
-//               userIsNotYetModerator = false;
-//             }
-//           });
-//         }
-
-//         return userIsNotYetModerator;
-//       })
-//       .map((user) => {
-//         return {
-//           value: user.id,
-//           label: `${user.attributes.first_name} ${user.attributes.last_name}`,
-//           email: `${user.attributes.email}`,
-//           disabled: isModerator(user) ? get(user.attributes, 'is_moderator') : get(user.attributes, 'is_member'),
-//         };
-//       });
-//   }
-
-//   loadOptions = (inputValue: string, callback) => {
-//     if (inputValue) {
-//       this.setState({ loading: true });
-
-//       findMembership(this.props.projectId, {
-//         queryParameters: {
-//           search: inputValue
-//         }
-//       }).observable.pipe(
-//         first()
-//       ).subscribe((response) => {
-//         const options = this.getOptions(response.data);
-//         this.setState({ loading: false });
-//         callback(options);
-//       });
-//     }
-//   }
-
-//   handleOnChange = async (selection: IOption[]) => {
-//     this.setState({ selection });
-//   }
-
-//   handleOnAddModeratorsClick = async () => {
-//     const { selection } = this.state;
-
-//     if (selection && selection.length > 0) {
-//       this.setState({ processing: true });
-//       const promises = selection.map(item => addMembership(this.props.projectId, item.value));
-
-//       try {
-//         await Promise.all(promises);
-//         this.setState({ selection: [], processing: false });
-//       } catch {
-//         this.setState({ selection: [], processing: false });
-//       }
-//     }
-//   }
-
-//   // noOptionsMessage = () => {
-//   //   return this.props.intl.formatMessage(messages.noOptions);
-//   // }
-
-//   render() {
-//     const { selection } = this.state;
-//     const { formatMessage } = this.props.intl;
-
-//   }
-// }
-
-// export default injectIntl<Props>(MembersAdd);
+export default injectIntl<Props>(TopicSearch);
