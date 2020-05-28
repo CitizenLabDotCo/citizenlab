4cb51bbc40f490f08f9cddc889c30874b239ce98
brentguf <brent.guffens@gmail.com>
2020-05-28 16:01:01 +0200

    Up

--- app/containers/Admin/settings/index.tsx
+++ app/containers/Admin/settings/index.tsx
@@ -69,7 +69,7 @@ class SettingsPage extends React.PureComponent<Props & InjectedIntlProps & WithR
     const tabHideConditions = {
       topics: function isTopicsTabHidden() {
         if (!customTopicsEnabled) {
-          return true;
+          return false; // Change back to true!
         }
 
         return false;
