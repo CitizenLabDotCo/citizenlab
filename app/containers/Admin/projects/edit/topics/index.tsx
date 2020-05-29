a76452d500cd5cabcf1255985ae0bd00636ecdd3
brentguf <brent.guffens@gmail.com>
2020-05-29 14:10:47 +0200

    Decouple ProjectTopicList data loading

--- app/containers/Admin/projects/edit/topics/index.tsx
+++ app/containers/Admin/projects/edit/topics/index.tsx
@@ -5,19 +5,15 @@ import { withRouter, WithRouterProps } from 'react-router';
 
 import { SectionTitle, SectionSubtitle } from 'components/admin/Section';
 import ProjectTopicSelector from './ProjectTopicSelector';
-import ProjectTopicList from './ProjectTopicList';
+import SortableProjectTopicList from './SortableProjectTopicList';
 
 // i18n
 import { FormattedMessage } from 'utils/cl-intl';
 import messages from './messages';
 
-// hooks
-import useTopics from 'hooks/useTopics';
-
 // services
 import { addProjectTopic, deleteProjectTopic } from 'services/projects';
 import { ITopicData } from 'services/topics';
-import useProjectTopics from 'hooks/useProjectTopics';
 
 const Container = styled.div`
   min-height: 80vh;
@@ -26,17 +22,16 @@ const Container = styled.div`
 interface Props {}
 
 const Topics = memo(({ params: { projectId } }: Props & WithRouterProps) => {
-  const topics = useTopics();
-  const projectTopics = useProjectTopics({ projectId });
   const [processing, setProcessing] = useState(false);
 
   const handleRemoveSelectedTopic = useCallback((topicIdToRemove: string) => {
     deleteProjectTopic(projectId, topicIdToRemove);
   }, []);
 
-  const handleAddSelectedTopics = useCallback(async (toBeAddedtopics: ITopicData[]) => {
+  const handleAddSelectedTopics = async (topicsToAdd: ITopicData[]) => {
       setProcessing(true);
-      const promises = toBeAddedtopics.map(topic => addProjectTopic(projectId, topic.id));
+
+      const promises = topicsToAdd.map(topic => addProjectTopic(projectId, topic.id));
 
       try {
         await Promise.all(promises);
@@ -44,40 +39,25 @@ const Topics = memo(({ params: { projectId } }: Props & WithRouterProps) => {
       } catch {
         setProcessing(false);
       }
-  }, []);
+  };
 
-  if (
-    !isNilOrError(topics) &&
-    !isNilOrError(projectTopics)
-  ) {
-    const topicIds = topics
-      .map(topic => !isNilOrError(topic) ? topic.id : null)
-      .filter(topic => topic) as string[];
-    const projectTopicIds = projectTopics.map(topic => topic.id);
-    const selectableTopicIds = topicIds.filter(topicId => !projectTopicIds.includes(topicId));
-
-    return (
-      <Container>
-        <SectionTitle>
-          <FormattedMessage {...messages.titleDescription} />
-        </SectionTitle>
-        <SectionSubtitle>
-          <FormattedMessage {...messages.subtitleDescription} />
-        </SectionSubtitle>
-        <ProjectTopicSelector
-          selectableTopicIds={selectableTopicIds}
-          handleAddSelectedTopics={handleAddSelectedTopics}
-          processing={processing}
-        />
-        <ProjectTopicList
-          projectTopics={projectTopics}
-          onHandleRemoveSelectedTopic={handleRemoveSelectedTopic}
-        />
-      </Container>
-    );
-  }
-
-  return null;
+  return (
+    <Container>
+      <SectionTitle>
+        <FormattedMessage {...messages.titleDescription} />
+      </SectionTitle>
+      <SectionSubtitle>
+        <FormattedMessage {...messages.subtitleDescription} />
+      </SectionSubtitle>
+      <ProjectTopicSelector
+        handleAddSelectedTopics={handleAddSelectedTopics}
+        processing={processing}
+      />
+      <SortableProjectTopicList
+        onHandleRemoveSelectedTopic={handleRemoveSelectedTopic}
+      />
+    </Container>
+  );
 });
 
 export default withRouter(Topics);
