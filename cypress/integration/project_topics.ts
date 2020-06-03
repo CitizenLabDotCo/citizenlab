f6de24bf7517f29a4ab356e2d12f9391dbec111f
brentguf <brent.guffens@gmail.com>
2020-06-03 09:11:44 +0200

    E2E test descriptions

--- cypress/integration/project_topics.ts
+++ cypress/integration/project_topics.ts
@@ -24,8 +24,29 @@ describe('Project topics', () => {
     cy.setAdminLoginCookie();
   });
 
-  describe('', () => {
-    it('', () => {
+  describe('Topic manager', () => {
+    it('Adding a custom topic in the topic manager makes it available in the project topic settings', () => {
+    });
+
+    it('Removing a custom topic in the topic manager makes it unavailable in the project topic settings', () => {
+    });
+
+    it('Renaming a custom topic in the topic manager updates its name in the project topic settings', () => {
+    });
+
+  });
+
+  describe('Project settings', () => {
+    it('Adding a topic to a project makes it available in the idea form', () => {
+    });
+
+    it('Removing a topic from a project makes it unavailable in the idea form', () => {
+    });
+
+    it('Adding a topic to a project makes it available in the project idea manager', () => {
+    });
+
+    it('Removing a topic from a project makes it unavailable in the project idea manager', () => {
     });
 
   });
