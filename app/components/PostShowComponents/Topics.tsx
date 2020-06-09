ef6b828b68f6f72798fbbb89fdff16a6305fa3de
brentguf <brent.guffens@gmail.com>
2020-06-09 13:36:30 +0200

    Topics component correct type

--- app/components/PostShowComponents/Topics.tsx
+++ app/components/PostShowComponents/Topics.tsx
@@ -50,10 +50,11 @@ interface DataProps {
 interface Props extends InputProps, DataProps {}
 
 const Topics = memo<Props & InjectedLocalized>(({ topics, localize, className, postType }) => {
+
   if (!isNilOrError(topics) && topics.length > 0) {
     return (
       <Container id={`e2e-${postType}-topics`} className={className}>
-        {topics.map((topic: ITopicData) => {
+        {topics.filter(topic => !isNilOrError(topic)).map((topic: ITopicData) => {
           return <Topic key={topic.id} className={`e2e-${postType}-topic`}>{localize(topic.attributes.title_multiloc)}</Topic>;
         })}
       </Container>
