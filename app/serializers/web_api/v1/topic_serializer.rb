046a8dc442ddb467e049a55ab028941403d17ae0
Sébastien Hoorens <sebastien@citizenlab.co>
2020-06-17 11:07:01 +0200

    final minor fixes

--- app/serializers/web_api/v1/topic_serializer.rb
+++ app/serializers/web_api/v1/topic_serializer.rb
@@ -4,6 +4,8 @@ class WebApi::V1::TopicSerializer < WebApi::V1::BaseSerializer
   attribute :ordering_within_project, if: Proc.new { |object, params|
     params[:project_id].present?
   } do |object, params|
-    object.projects_topics.find_by(project_id: params[:project_id])&.ordering
+    object.projects_topics.select do |pt|
+      pt.project_id == params[:project_id]
+    end.first&.ordering
   end
 end
