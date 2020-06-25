dfb57b298c518dc78f725f450cf535802d5d1d8c
Adrien Dessy <adrien@citizenlab.co>
2020-06-25 23:33:04 +0200

    refactoring

--- engines/polls/app/services/polls/export_service.rb
+++ engines/polls/app/services/polls/export_service.rb
@@ -3,34 +3,39 @@ module Polls
 
 		def generate_poll_results_xlsx participation_context, responses
       multiloc_service = MultilocService.new
-      questions = Polls::Question.where(participation_context: participation_context)
+      is_anonymous = participation_context.poll_anonymous?
+      questions = Polls::Question
+        .where(participation_context: participation_context)
         .order(:ordering)
 
       pa = Axlsx::Package.new
       wb = pa.workbook
       wb.styles do |s|
         wb.add_worksheet(name: 'Poll results') do |sheet|
-          sheet.add_row [
-            *(participation_context.poll_anonymous? ? [] : ['User ID']),
-            *questions.map{|q| multiloc_service.t(q.title_multiloc)}
-          ], style: XlsxService.new.header_style(s)
-          responses.includes(:user, response_options: [:option]).each do |response|
-            user_options = questions.map do |q|
-              ros = response.response_options.select{|ro| ro.option.question_id == q.id}
-              if ros.present?
-                ros.map{|ro| multiloc_service.t(ro.option.title_multiloc)}.join(', ')
-              else
-                ''
-              end
-            end
-            sheet.add_row [
-              *(participation_context.poll_anonymous? ? [] : [response.user.id]),
-              *user_options
-            ]
+          header_row = is_anonymous ? [] : ['User ID']
+          header_row += questions.map{|q| multiloc_service.t(q.title_multiloc)}
+          sheet.add_row(header_row, style: XlsxService.new.header_style(s))
+
+          responses.includes(:user, response_options: [:option]).each do |resp_obj|
+            user_responses = questions.map { |q| extract_responses(resp_obj, q.id, multiloc_service) }
+            response_row = is_anonymous ? [] : [resp_obj.user.id]
+            response_row += user_responses
+            sheet.add_row response_row
           end
         end
       end
       pa.to_stream
+    end
+
+    private
+
+    def extract_responses(response_obj, question_id, multiloc_service)
+      resp_opts = response_obj.response_options.select{|ro| ro.option.question_id == question_id}
+      return '' if resp_opts.blank?
+
+      resp_opts
+        .map{ |ro| multiloc_service.t(ro.option.title_multiloc) }
+        .join(', ')
     end
 
 	end
