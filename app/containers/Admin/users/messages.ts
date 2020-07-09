3308cc294e82ebcffa256a2ba1f3daf278f75b4e
brentguf <brent.guffens@gmail.com>
2020-07-09 15:14:41 +0200

    BlackedOut

--- app/containers/Admin/users/messages.ts
+++ app/containers/Admin/users/messages.ts
@@ -196,5 +196,9 @@ export default defineMessages({
   smartGroupsAvailability: {
     id: 'app.containers.AdminPage.Users.UsersGroup.smartGroupsAvailability',
     defaultMessage: 'Smart groups are available on the Standard and Premium plans.',
+  },
+  learnMorePlans: {
+    id: 'app.containers.AdminPage.Users.UsersGroup.learnMorePlans',
+    defaultMessage: 'Learn more about our plans'
   }
 });
