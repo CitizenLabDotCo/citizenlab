d6ab33c573a655565003225748ea842d3357b3b4
brentguf <brent.guffens@gmail.com>
2020-07-09 15:26:24 +0200

    Refactor GroupCreationStep1 to functional component (WIP)

--- app/containers/Admin/users/GroupCreationStep1.tsx
+++ app/containers/Admin/users/GroupCreationStep1.tsx
@@ -1,9 +1,12 @@
 // Libraries
-import React from 'react';
+import React, { memo } from 'react';
 
 // Components
 import Icon from 'components/UI/Icon';
 import Button from 'components/UI/Button';
+
+// hooks
+import useFeatureFlag from 'hooks/useFeatureFlag';
 
 // i18n
 import FormattedMessage from 'utils/cl-intl/FormattedMessage';
@@ -123,60 +126,54 @@ import { IGroupData } from 'services/groups';
 export interface Props {
   onOpenStep2: (groupType: IGroupData['attributes']['membership_type']) => void;
 }
-export interface State {}
 
-export class GroupCreationStep1 extends React.PureComponent<Props & InjectedIntlProps, State> {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const GroupCreationStep1 = memo(({ intl, onOpenStep2 }: Props & InjectedIntlProps) => {
+  const formattedLink = intl.formatMessage(messages.readMoreLink);
+  const smartGroupsEnabled = useFeatureFlag('smart_groups');
 
-  createStep2Handler = (groupType: IGroupData['attributes']['membership_type']) => () => {
-    this.props.onOpenStep2(groupType);
-  }
+  const createStep2Handler = (groupType: IGroupData['attributes']['membership_type']) => () => {
+    onOpenStep2(groupType);
+  };
 
-  render() {
-    const formattedLink = this.props.intl.formatMessage(messages.readMoreLink);
-    return (
-      <Container>
-        <GroupType className="manual">
-          <GroupIcon name="database" />
-          <GroupName>
-            <FormattedMessage {...messages.step1TypeNameNormal} />
-          </GroupName>
-          <GroupDescription>
-            <DescriptionText><FormattedMessage {...messages.step1TypeDescriptionNormal} /></DescriptionText>
-            <MoreInfoLink href={formattedLink} target="_blank" ><FormattedMessage {...messages.step1LearnMoreGroups} /></MoreInfoLink>
-          </GroupDescription>
-          <Step2Button className="e2e-create-normal-group-button" buttonStyle="cl-blue" onClick={this.createStep2Handler('manual')}>
-            <FormattedMessage {...messages.step1CreateButtonNormal} />
-          </Step2Button>
-        </GroupType>
-        <GroupType className="rules">
-          <GroupIcon name="lightingBolt" />
-          <GroupName>
-            <FormattedMessage {...messages.step1TypeNameSmart} />
-          </GroupName>
-          <GroupDescription>
-          <DescriptionText><FormattedMessage {...messages.step1TypeDescriptionSmart} /></DescriptionText>
-            <MoreInfoLink href={formattedLink} target="_blank" ><FormattedMessage {...messages.step1LearnMoreGroups} /></MoreInfoLink>
-          </GroupDescription>
-          <Step2Button className="e2e-create-rules-group-button" buttonStyle="cl-blue" onClick={this.createStep2Handler('rules')}>
-            <FormattedMessage {...messages.step1CreateButtonSmart} />
-          </Step2Button>
-          <BlackedOut>
-            <LockIcon name="lock" />
-            <Copy>
-              <FormattedMessage {...messages.smartGroupsAvailability}/>
-            </Copy>
-            <LearnMoreLink href="https://www.citizenlab.co/plans" target="_blank">
-              <FormattedMessage {...messages.learnMorePlans} />
-            </LearnMoreLink>
-          </BlackedOut>
-        </GroupType>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <GroupType className="manual">
+        <GroupIcon name="database" />
+        <GroupName>
+          <FormattedMessage {...messages.step1TypeNameNormal} />
+        </GroupName>
+        <GroupDescription>
+          <DescriptionText><FormattedMessage {...messages.step1TypeDescriptionNormal} /></DescriptionText>
+          <MoreInfoLink href={formattedLink} target="_blank" ><FormattedMessage {...messages.step1LearnMoreGroups} /></MoreInfoLink>
+        </GroupDescription>
+        <Step2Button className="e2e-create-normal-group-button" buttonStyle="cl-blue" onClick={createStep2Handler('manual')}>
+          <FormattedMessage {...messages.step1CreateButtonNormal} />
+        </Step2Button>
+      </GroupType>
+      <GroupType className="rules">
+        <GroupIcon name="lightingBolt" />
+        <GroupName>
+          <FormattedMessage {...messages.step1TypeNameSmart} />
+        </GroupName>
+        <GroupDescription>
+        <DescriptionText><FormattedMessage {...messages.step1TypeDescriptionSmart} /></DescriptionText>
+          <MoreInfoLink href={formattedLink} target="_blank" ><FormattedMessage {...messages.step1LearnMoreGroups} /></MoreInfoLink>
+        </GroupDescription>
+        <Step2Button disabled={!smartGroupsEnabled} className="e2e-create-rules-group-button" buttonStyle="cl-blue" onClick={createStep2Handler('rules')}>
+          <FormattedMessage {...messages.step1CreateButtonSmart} />
+        </Step2Button>
+        <BlackedOut>
+          <LockIcon name="lock" />
+          <Copy>
+            <FormattedMessage {...messages.smartGroupsAvailability}/>
+          </Copy>
+          <LearnMoreLink href="https://www.citizenlab.co/plans" target="_blank">
+            <FormattedMessage {...messages.learnMorePlans} />
+          </LearnMoreLink>
+        </BlackedOut>
+      </GroupType>
+    </Container>
+  );
+});
 
 export default injectIntl<Props>(GroupCreationStep1);
