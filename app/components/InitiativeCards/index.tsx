9c59066409b50510fa001b96bff33f78afeea2d3
brentguf <brent.guffens@gmail.com>
2020-07-16 15:27:18 +0200

    Load more buttons tracking

--- app/components/InitiativeCards/index.tsx
+++ app/components/InitiativeCards/index.tsx
@@ -3,6 +3,10 @@ import { adopt } from 'react-adopt';
 import { get, isNumber } from 'lodash-es';
 import { isNilOrError } from 'utils/helperUtils';
 
+// tracks
+import { trackEventByName } from 'utils/analytics';
+import tracks from './tracks';
+
 // components
 import InitiativeCard from 'components/InitiativeCard';
 import InitiativesMap from 'components/InitiativesMap';
@@ -357,6 +361,7 @@ class InitiativeCards extends PureComponent<Props & InjectedIntlProps, State> {
   }
 
   loadMore = () => {
+    trackEventByName(tracks.loadMoreProposals)
     this.props.initiatives.onLoadMore();
   }
 
---
+++ app/components/InitiativeCards/tracks.ts
@@ -0,0 +1,3 @@
+export default {
+  loadMoreProposals: 'Load more proposals button clicked',
+};
