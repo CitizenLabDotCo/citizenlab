3902f83a91dd77656ff91fba9f3acb356e36f711
Sébastien Hoorens <sebastien@citizenlab.co>
2020-07-24 15:35:30 +0200

    added test cases for unauthorized xlsx exports

--- spec/acceptance/initiative_comments_spec.rb
+++ spec/acceptance/initiative_comments_spec.rb
@@ -152,6 +152,18 @@ resource "Comments" do
         expect(worksheet.count).to eq (initiatives.size + 1)
       end
     end
+
+    describe do
+      before do 
+        @user = create(:user)
+        token = Knock::AuthToken.new(payload: @user.to_token_payload).token
+        header 'Authorization', "Bearer #{token}"
+      end
+      
+      example_request '[error] XLSX export by a normal user', document: false do
+        expect(status).to eq 401
+      end
+    end
   end
 
   get "web_api/v1/comments/:id" do
