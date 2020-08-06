4b2c5207b87ba44ab721de3306daf235612cff9f
adessy <adrien@citizenlab.co>
2020-08-06 11:23:40 +0000

    CL2-CL-5691 Support HTML in description of custom fields (#1142)

--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -58,7 +58,7 @@ class CustomField < ApplicationRecord
     service = SanitizationService.new
     self.description_multiloc = service.sanitize_multiloc(
       self.description_multiloc,
-      %i{decoration link}
+      %i{decoration link list title}
     )
     self.description_multiloc = service.remove_empty_paragraphs_multiloc(self.description_multiloc)
     self.description_multiloc = service.linkify_multiloc(self.description_multiloc)
