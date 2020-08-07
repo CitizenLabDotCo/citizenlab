43ba5fc7459066b1971affe994d7cce7f733159e
Sébastien Hoorens <sebastien@citizenlab.co>
2020-08-07 18:27:18 +0200

    fixed e2e template

--- app/models/common_password.rb
+++ app/models/common_password.rb
@@ -5,11 +5,6 @@ class CommonPassword < ApplicationRecord
   acts_as_list column: :freq_ordering, top_of_list: 0, add_new_at: :bottom
 
 
-  def self.check password
-    # Returns true when the password is common
-    where(password: password).exists?
-  end
-
   def self.initialize!
     CommonPassword.destroy_all
     pwds = open(COMMON_PASSWORDS_FILE).readlines.map.with_index do |password, index|
@@ -17,6 +12,11 @@ class CommonPassword < ApplicationRecord
     end
     # Bulk insert in one query
     CommonPassword.import pwds
+  end
+
+  def self.check password
+    # Returns true when the password is common
+    where(password: password).exists?
   end
   
 end
