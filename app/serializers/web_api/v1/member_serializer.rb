ae4fc8856e0f0e5ac0450f2214ff3dc053caae55
Sébastien Hoorens <sebastien@citizenlab.co>
2020-08-13 14:31:36 +0200

    hiding private user attributes from normal endpoints

--- app/serializers/web_api/v1/member_serializer.rb
+++ app/serializers/web_api/v1/member_serializer.rb
@@ -1,5 +1,9 @@
 class WebApi::V1::MemberSerializer < WebApi::V1::BaseSerializer
-  attributes :first_name, :last_name, :slug, :email # TODO hide email for non-admins
+  attributes :first_name, :last_name, :slug
+
+  attribute :email, if: Proc.new { |object, params|
+    view_private_attributes? object, params
+  }
 
   attribute :avatar, if: Proc.new { |object|
     object.avatar
@@ -11,5 +15,10 @@ class WebApi::V1::MemberSerializer < WebApi::V1::BaseSerializer
     params[:group_id]
   } do |object, params|
   	object.member_of? params[:group_id]
+  end
+
+
+  def self.view_private_attributes? object, params={}
+    Pundit.policy!(current_user(params), object).view_private_attributes?
   end
 end
--- app/serializers/web_api/v1/moderator_serializer.rb
+++ app/serializers/web_api/v1/moderator_serializer.rb
@@ -1,5 +1,9 @@
 class WebApi::V1::ModeratorSerializer < WebApi::V1::BaseSerializer
-  attributes :first_name, :last_name, :slug, :roles, :email # TODO hide email for non-admins
+  attributes :first_name, :last_name, :slug, :roles
+
+  attribute :email, if: Proc.new { |object, params|
+    view_private_attributes? object, params
+  }
 
   attribute :avatar, if: Proc.new { |object|
     object.avatar
@@ -11,6 +15,11 @@ class WebApi::V1::ModeratorSerializer < WebApi::V1::BaseSerializer
     params[:project_id]
   } do |object, params|
     object.project_moderator? params[:project_id]
+  end
+
+
+  def self.view_private_attributes? object, params={}
+    Pundit.policy!(current_user(params), object).view_private_attributes?
   end
 
 end
