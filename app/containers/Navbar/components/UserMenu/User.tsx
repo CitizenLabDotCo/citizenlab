0842a71f587c19c1dd6947c866d4fda47abce39f
brentguf <brent.guffens@gmail.com>
2020-08-17 12:18:13 +0200

    User component for NavBar

---
+++ app/containers/Navbar/components/UserMenu/User.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled, { withTheme } from 'styled-components';
+import Avatar from 'components/Avatar';
+import UserName from 'components/UI/User';
+import VerificationBadge from './VerificationBadge';
+
+// style
+import { colors, media, fontSizes } from 'utils/styleUtils';
+
+const UserNameContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 5px;
+`;
+
+const StyledUserName = styled(UserName)`
+  margin-right: 4px;
+  white-space: nowrap;
+  font-size: ${fontSizes.base}px;
+  font-weight: 500;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  transition: all 100ms ease-out;
+
+  ${media.smallerThanMinTablet`
+    display: none;
+  `}
+`;
+
+const StyledAvatar = styled(Avatar)``;
+
+interface Props {
+  theme: any;
+  userId: string;
+  isVerified: boolean;
+}
+
+const User = ({ theme, userId, isVerified }: Props) => {
+  return (
+    <>
+      <UserNameContainer>
+        <StyledUserName
+          color={theme.navbarTextColor || theme.colorText}
+          userId={userId}
+          hideLastName
+        />
+        <VerificationBadge isVerified={isVerified} />
+      </UserNameContainer>
+
+      <StyledAvatar
+        userId={userId}
+        size="30px"
+        hasHoverEffect={false}
+        fillColor={
+          theme && theme.navbarTextColor ? theme.navbarTextColor : colors.label
+        }
+        verified
+      />
+    </>
+  );
+};
+
+export default withTheme(User);
