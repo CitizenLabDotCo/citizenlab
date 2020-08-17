3146b2726ab00b3dc6a45192d69e2db73da1df34
brentguf <brent.guffens@gmail.com>
2020-08-17 16:57:25 +0200

    Remove unknown user comp + Name for styling only

--- app/components/UI/UserName/index.tsx
+++ app/components/UI/UserName/index.tsx
@@ -8,64 +8,97 @@ import GetUser, { GetUserChildProps } from 'resources/GetUser';
 
 // components
 import Name from './Name';
-import UnknownUser from './UnknownUser';
+
+// i18n
+import { injectIntl } from 'utils/cl-intl';
+import { InjectedIntlProps } from 'react-intl';
+import messages from './messages';
 
 interface DataProps {
   user: GetUserChildProps;
 }
 
-interface UserNameProps {
-  hideLastName?: boolean;
+interface StyleProps {
   emphasize?: boolean;
   color?: string;
   canModerate?: boolean;
 }
 
-interface InputProps extends UserNameProps {
+interface InputProps extends StyleProps {
   // if user was deleted, userId can be null
   userId: string | null;
   className?: string;
   linkToProfile?: boolean;
+  hideLastName?: boolean;
 }
 
 interface Props extends InputProps, DataProps {}
 
-const UserName = (props: Props) => {
-  const { user, className, linkToProfile, ...userNameProps } = props;
+const UserName = (props: Props & InjectedIntlProps) => {
+  const {
+    intl: { formatMessage },
+    user,
+    className,
+    linkToProfile,
+    hideLastName,
+    ...styleProps
+  } = props;
 
-  if (!isNilOrError(user)) {
-    const firstName = user.attributes.first_name;
-    const lastName = user.attributes.last_name;
-    const profileLink = `/profile/${user.attributes.slug}`;
-
-    if (firstName) {
-      // Sometimes we have a user, but names don't load (only seen in dev mode)
-      // Built in this check to make sure we don't show an empty space (in production)
-      // See Name.tsx for why only firstName is required
-      const NameComponent = (
-        <Name
-          firstName={firstName}
-          lastName={lastName}
-          className={className}
-          {...userNameProps}
-        />
-      );
-
-      return linkToProfile
-        ? withProfileLink(NameComponent, profileLink, className)
-        : NameComponent;
+  const getName = () => {
+    if (!isNilOrError(user)) {
+      const firstName = user.attributes.first_name;
+      const lastName = user.attributes.last_name;
+      if (firstName) {
+        // Sometimes we have a user, but names don't load (only seen in dev mode)
+        // Built in this check to make sure we don't show an empty space (in production)
+        // See Name.tsx for why only firstName is required
+        return `${firstName} ${!hideLastName && lastName ? lastName : ''}`;
+      }
     }
-  }
 
-  return <UnknownUser className={className} />;
+    return formatMessage(messages.deletedUser);
+  };
+
+  const getProfileLink = () => {
+    if (
+      !isNilOrError(user) &&
+      // It only makes sense to link to profile when we see a name,
+      // we don't want to link to the profile of an unknown user
+      user.attributes.first_name &&
+      user.attributes.slug
+    ) {
+      return `/profile/${user.attributes.slug}`;
+    }
+
+    return null;
+  };
+
+  const name = getName();
+  const profileLink = getProfileLink();
+  const isUnknownUser = isNilOrError(user);
+
+  const NameComponent = (
+    <Name
+      name={name}
+      className={className}
+      isUnknownUser={isUnknownUser}
+      {...styleProps}
+    />
+  );
+
+  return linkToProfile && profileLink
+    ? withProfileLink(NameComponent, profileLink, className)
+    : NameComponent;
 };
 
 const Data = adopt<DataProps, InputProps>({
   user: ({ userId, render }) => <GetUser id={userId}>{render}</GetUser>,
 });
 
+const UserNameWithHOCs = injectIntl(UserName);
+
 export default (inputProps: InputProps) => (
   <Data {...inputProps}>
-    {(dataProps) => <UserName {...inputProps} {...dataProps} />}
+    {(dataProps) => <UserNameWithHOCs {...inputProps} {...dataProps} />}
   </Data>
 );
