dccea6e53c6d2e20551c23f5f86859c8e48772df
brentguf <brent.guffens@gmail.com>
2020-08-18 11:14:41 +0200

    MetaInformation skeleton

---
+++ app/containers/IdeasShow/MetaInformation/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// i18n
+import { FormattedMessage } from 'utils/cl-intl';
+import messages from './messages';
+
+// styles
+import { colors, fontSizes } from 'cl2-component-library';
+
+// i18n
+
+import Status from './Status';
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Item = styled.div`
+  border-bottom: 1px solid ${colors.separation};
+  padding-top: 25px;
+  padding-bottom: 30px;
+`;
+
+const Header = styled.h3`
+  font-size: ${fontSizes.base}px;
+  font-weight: bold;
+  color: ${(props) => props.theme.colorText};
+  margin-bottom: 15px;
+`;
+
+const MetaInformation = () => {
+  return (
+    <Container>
+      <Item>
+        <Header>
+          <FormattedMessage {...messages.currentStatus} />
+        </Header>
+      </Item>
+      <Item>
+        <Header>
+          <FormattedMessage {...messages.topics} />
+        </Header>
+      </Item>
+      <Item>
+        <Header>
+          <FormattedMessage {...messages.location} />
+        </Header>
+      </Item>
+      <Item>
+        <Header>
+          <FormattedMessage {...messages.attachments} />
+        </Header>
+      </Item>
+      <Item>
+        <Header>
+          <FormattedMessage {...messages.similarIdeas} />
+        </Header>
+      </Item>
+    </Container>
+  );
+};
+
+export default MetaInformation;
