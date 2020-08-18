9236e8da77e486db5d76526e3d0dfafc1831a399
Sara <sara@citizenlab.co>
2020-08-18 16:42:01 +0200

    Tries to get more insights on pages

--- app/containers/Admin/projects/all/index.tsx
+++ app/containers/Admin/projects/all/index.tsx
@@ -130,7 +130,7 @@ class AdminProjectsList extends PureComponent<Props, State> {
               this.unlisten = clHistory.listen(() =>
                 this.closeTemplatePreview()
               );
-              trackPage(this.url);
+              trackPage(url);
             }
 
             window.scrollTo(0, 0);
