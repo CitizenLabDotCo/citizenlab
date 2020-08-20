73d24fa6fea1039082ec9ed695d0fbb59b55ebd7
Adrien Dessy <adrien@citizenlab.co>
2020-08-20 11:11:44 +0200

    New service to adapt the display name according to the context

---
+++ app/services/user_display_name_service.rb
@@ -0,0 +1,40 @@
+# Service that adapts the user name that is shown depending on the context.
+# For now, it can be used to "shallow anonymization" of users, depending on the
+# tenant settings and the permissions of the current user.
+
+class UserDisplayNameService
+
+  # @param [Tenant] tenant
+  # @param [User] current_user
+  def initialize(tenant, current_user)
+    @tenant = tenant
+    @current_user = current_user
+  end
+
+  # @param [User] user
+  # @return [String]
+  def display_name(user)
+    [user.first_name, last_name(user)].join(' ')
+  end
+
+  # @param [User] user
+  # @return [String]
+  def last_name(user)
+    can_see_fullname_of?(user) ? user.last_name : self.class.initial(user.last_name)
+  end
+
+  private
+
+  # @param [User] user
+  # @return [Boolean]
+  def can_see_fullname_of?(user)
+    return true unless @tenant.shallow_anonymization?
+    (user == @current_user) || @current_user.admin?
+  end
+
+  # @param [String] name
+  # @return [String]
+  def self.initial(name)
+    name[0] + '.'
+  end
+end
