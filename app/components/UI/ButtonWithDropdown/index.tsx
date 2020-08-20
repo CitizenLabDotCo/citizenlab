4ba05645703c4ca9e3624fc3c3b8d61f5c7dd640
brentguf <brent.guffens@gmail.com>
2020-08-20 18:18:32 +0200

    Share button WIP

--- app/components/UI/ButtonWithDropdown/index.tsx
+++ app/components/UI/ButtonWithDropdown/index.tsx
@@ -1,16 +1,8 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import bowser from 'bowser';
 
-// typings
-import { IParticipationContextType } from 'typings';
-
 // components
-import { Dropdown, Icon } from 'cl2-component-library';
-import PBBasket from './PBBasket';
-
-// i18n
-import { FormattedMessage } from 'utils/cl-intl';
-import messages from '../messages';
+import { Dropdown } from 'cl2-component-library';
 
 // styling
 import styled from 'styled-components';
@@ -22,28 +14,6 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
-const ManageBudgetButton = styled.button`
-  height: 100%;
-  display: flex;
-  color: ${({ theme }) => theme.projectNavbarTextColor || '#fff'};
-  opacity: 0.6;
-  align-items: center;
-  white-space: nowrap;
-  cursor: pointer;
-
-  &:focus,
-  &:hover {
-    color: ${({ theme }) => theme.projectNavbarTextColor || '#fff'};
-    opacity: 1;
-  }
-`;
-
-const StyledIcon = styled(Icon)`
-  flex: 0 0 18px;
-  height: 18px;
-  margin-right: 9px;
-`;
-
 const DropdownWrapper = styled.div`
   width: 100%;
   flex: 0 0 0px;
@@ -53,62 +23,31 @@ const DropdownWrapper = styled.div`
 `;
 
 interface Props {
-  participationContextId: string | null;
-  participationContextType: IParticipationContextType;
   className?: string;
-}
-
-interface State {
+  buttonComponent: JSX.Element;
+  dropdownContent: JSX.Element;
   dropdownOpened: boolean;
+  onClickOutside: () => void;
+  dropdownWidth?: string;
 }
 
-export default class PBNavbarButton extends PureComponent<Props, State> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dropdownOpened: false,
-    };
-  }
-
-  toggleDropdown = () => {
-    this.setState(({ dropdownOpened }) => ({
-      dropdownOpened: !dropdownOpened,
-    }));
-  };
-
-  render() {
-    const {
-      participationContextType,
-      participationContextId,
-      className,
-    } = this.props;
-    const { dropdownOpened } = this.state;
-
-    return (
-      <Container className={className}>
-        <ManageBudgetButton
-          onClick={this.toggleDropdown}
-          aria-expanded={dropdownOpened}
-        >
-          <StyledIcon name="moneybag" className="moneybag" ariaHidden />
-          <FormattedMessage {...messages.manageBudget} />
-        </ManageBudgetButton>
-
-        <DropdownWrapper>
-          <Dropdown
-            top="-5px"
-            right={bowser.msie ? '-5px' : 'auto'}
-            opened={dropdownOpened}
-            onClickOutside={this.toggleDropdown}
-            content={
-              <PBBasket
-                participationContextType={participationContextType}
-                participationContextId={participationContextId}
-              />
-            }
-          />
-        </DropdownWrapper>
-      </Container>
-    );
-  }
+const ButtonWithDropdown = ({ className, buttonComponent, dropdownContent, dropdownOpened, onClickOutside, dropdownWidth }: Props) => {
+  // TODO: add aria-expanded to buttonComponent
+  return (
+    <Container className={className}>
+      {buttonComponent}
+      <DropdownWrapper>
+        <Dropdown
+          top="5px"
+          right={bowser.msie ? '-5px' : 'auto'}
+          opened={dropdownOpened}
+          onClickOutside={onClickOutside}
+          content={dropdownContent}
+          width={dropdownWidth}
+        />
+      </DropdownWrapper>
+    </Container>
+  );
 }
+
+export default ButtonWithDropdown;
