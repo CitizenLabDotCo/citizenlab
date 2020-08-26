43c1d30b358c990fc629fa88159bc3cfe4061066
Adrien Dessy <adrien@citizenlab.co>
2020-08-26 09:11:46 +0200

    tad of refactoring

--- app/controllers/web_api/v1/mentions_controller.rb
+++ app/controllers/web_api/v1/mentions_controller.rb
@@ -1,7 +1,7 @@
 class WebApi::V1::MentionsController < ApplicationController
 
   skip_after_action :verify_authorized, only: [:users]
- 
+
   def users
     service = MentionService.new
     slug_service = SlugService.new
@@ -10,13 +10,9 @@ class WebApi::V1::MentionsController < ApplicationController
     mention_pattern = slug_service.slugify(params[:mention])
 
     @users = []
-    if params[:post_id] && params[:post_type]
-      post_class = case params[:post_type]
-        when 'Idea' then Idea
-        when 'Initiative' then Initiative
-        else raise "#{params[:post_type]} is not a post type"
-      end
-      post = post_class.find(params[:post_id])
+    if (post_id = params[:post_id]) && (post_type = params[:post_type])
+      post_class = post_type_to_class(post_type)
+      post = post_class.find(post_id)
       @users = service.users_from_post mention_pattern, post, limit
     end
 
@@ -29,12 +25,24 @@ class WebApi::V1::MentionsController < ApplicationController
     end
 
     render json: WebApi::V1::UserSerializer.new(
-      @users, 
-      params: fastjson_params
-      ).serialized_json
+        @users,
+        params: fastjson_params
+    ).serialized_json
   end
 
   def secure_controller?
     false
+  end
+
+  private
+
+  # @param [String] type
+  # @@return [Class]
+  def post_type_to_class(type)
+    case type
+    when 'Idea' then Idea
+    when 'Initiative' then Initiative
+    else raise "#{type} is not a post type"
+    end
   end
 end
