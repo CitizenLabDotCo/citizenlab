3c4e5b88c7bbe2fd84c9210b773d16f3d392f478
Adrien Dessy <adrien@citizenlab.co>
2020-08-28 15:44:30 +0200

    author_id instead to avoid instantiation of the author object, also this would give an error for ideas of deleted users.

--- app/services/mention_service.rb
+++ app/services/mention_service.rb
@@ -68,7 +68,7 @@ class MentionService
   # @return [Array<User>]
   def users_from_post query, post, limit
     commenter_ids = User.joins(:comments).where(comments: {post_id: post.id}).ids.uniq
-    user_ids = commenter_ids << post.author.id
+    user_ids = commenter_ids << post.author_id
     User.where(id: user_ids).by_username(query).limit(limit).to_a
   end
 
