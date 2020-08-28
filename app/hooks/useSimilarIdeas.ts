53da40c72253e05ad442d7872335fad28a8cb08a
brentguf <brent.guffens@gmail.com>
2020-08-28 12:56:00 +0200

    useSimilarIdeas parameters

--- app/hooks/useSimilarIdeas.ts
+++ app/hooks/useSimilarIdeas.ts
@@ -1,12 +1,20 @@
 import { useState, useEffect } from 'react';
-import { IMinimalIdeaData, similarIdeasStream} from 'services/ideas';
+import { IMinimalIdeaData, similarIdeasStream } from 'services/ideas';
 
-export default function useSimilarIdeas({ ideaId, pageSize }) {
-  const [similarIdeas, setSimilarIdeas] = useState<IMinimalIdeaData[] | Error | null | undefined>(undefined);
+interface Parameters {
+  ideaId: string;
+  pageSize?: number;
+}
+
+export default function useSimilarIdeas({ ideaId, pageSize = 5 }: Parameters) {
+  const [similarIdeas, setSimilarIdeas] = useState<
+    IMinimalIdeaData[] | Error | null | undefined
+  >(undefined);
   const queryParameters = { pageSize };
 
   useEffect(() => {
-    const observable = similarIdeasStream(ideaId, { queryParameters }).observable;
+    const observable = similarIdeasStream(ideaId, { queryParameters })
+      .observable;
 
     const subscription = observable.subscribe((response) => {
       setSimilarIdeas(response.data);
