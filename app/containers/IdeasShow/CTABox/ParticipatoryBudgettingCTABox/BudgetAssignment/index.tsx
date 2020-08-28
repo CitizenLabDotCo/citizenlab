05c2125c47f9f42657dca718759f36192c8473ee
brentguf <brent.guffens@gmail.com>
2020-08-28 16:34:32 +0200

    PB CTA box styling

--- app/containers/IdeasShow/CTABox/ParticipatoryBudgettingCTABox/BudgetAssignment/index.tsx
+++ app/containers/IdeasShow/CTABox/ParticipatoryBudgettingCTABox/BudgetAssignment/index.tsx
@@ -19,7 +19,6 @@ const ControlWrapper = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  margin-bottom: 45px;
   padding: 35px;
   border: 1px solid #e0e0e0;
   ${defaultCardStyle};
