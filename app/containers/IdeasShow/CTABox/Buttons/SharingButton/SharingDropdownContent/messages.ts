c171563b82e95a80534ee4e06b9843f3fc1fa49f
brentguf <brent.guffens@gmail.com>
2020-08-31 15:15:24 +0200

    Sharing button dropdown content messages

---
+++ app/containers/IdeasShow/CTABox/Buttons/SharingButton/SharingDropdownContent/messages.ts
@@ -0,0 +1,20 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  shareOnFacebook: {
+    id: 'app.components.Sharing.shareOnFacebook',
+    defaultMessage: 'Share on Facebook',
+  },
+  shareViaMessenger: {
+    id: 'app.components.Sharing.shareViaMessenger',
+    defaultMessage: 'Share via Messenger',
+  },
+  shareOnTwitter: {
+    id: 'app.components.Sharing.shareOnTwitter',
+    defaultMessage: 'Share on Twitter',
+  },
+  shareByEmail: {
+    id: 'app.components.Sharing.shareByEmail',
+    defaultMessage: 'Share by email',
+  },
+});
