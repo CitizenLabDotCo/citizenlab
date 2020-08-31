733b0d3c5ad0b52043369c9ff709db949a57cfb9
brentguf <brent.guffens@gmail.com>
2020-08-31 11:38:02 +0200

    Extract buttons from sharing component

--- app/containers/IdeasShow/CTABox/Buttons/SharingButtonWithDropdown/SharingDropdownContent/Messenger.tsx
+++ app/containers/IdeasShow/CTABox/Buttons/SharingButtonWithDropdown/SharingDropdownContent/Messenger.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { FacebookButton } from 'react-social';
 import { isNilOrError } from 'utils/helperUtils';
 import tracks from '../tracks';
 import trackClickByEventName from './trackClickByEventName';
@@ -12,7 +11,11 @@ interface Props {
   url: string;
 }
 
-const Facebook = ({
+const handleClick = (_event) => {
+  trackClickByEventName(tracks.clickMessengerShare.name);
+};
+
+const Messenger = ({
   url,
   intl: { formatMessage },
 }: Props & InjectedIntlProps) => {
@@ -23,20 +26,21 @@ const Facebook = ({
       tenant.data.attributes.settings.facebook_login?.app_id;
 
     return (
-      <FacebookButton
-        appId={facebookAppId}
-        url={url}
-        className="sharingButton facebook first"
-        sharer={true}
-        onClick={trackClickByEventName(tracks.clickFbShare.name)}
-        aria-label={formatMessage(messages.shareOnFacebook)}
+      <a
+        className="sharingButton messenger"
+        href={`fb-messenger://share/?link=${encodeURIComponent(
+          url
+        )}&app_id=${facebookAppId}`}
+        onClick={handleClick}
+        role="button"
+        aria-label={formatMessage(messages.shareViaMessenger)}
       >
-        {/* <StyledIcon name="facebook" /> */}
-      </FacebookButton>
+        {/* <StyledIcon name="messenger" /> */}
+      </a>
     );
   }
 
   return null;
 };
 
-export default injectIntl(Facebook);
+export default injectIntl(Messenger);
--- app/containers/IdeasShow/CTABox/Buttons/SharingButtonWithDropdown/SharingDropdownContent/index.tsx
+++ app/containers/IdeasShow/CTABox/Buttons/SharingButtonWithDropdown/SharingDropdownContent/index.tsx
@@ -1,31 +1,22 @@
 import React from 'react';
 
-// libraries
-import { FacebookButton, TwitterButton } from 'react-social';
-
 // components
 import { Icon } from 'cl2-component-library';
 
 // i18n
 import messages from './messages';
-import { InjectedIntlProps } from 'react-intl';
-import { injectIntl, FormattedMessage } from 'utils/cl-intl';
-
-// analytics
-import { trackEventByName } from 'utils/analytics';
-import tracks from './tracks';
+import { FormattedMessage } from 'utils/cl-intl';
 
 // style
 import styled from 'styled-components';
 import { media, fontSizes, colors } from 'utils/styleUtils';
 import { darken } from 'polished';
 
-// utils
-import { isNilOrError } from 'utils/helperUtils';
-import useTenant from 'hooks/useTenant';
-
 // components
 import Facebook from './Facebook';
+import Messenger from './Messenger';
+import Twitter from './Twitter';
+import Email from './Email';
 
 const Container = styled.div`
   display: flex;
@@ -152,99 +143,54 @@ interface Props {
   id?: string;
 }
 
-class Sharing extends PureComponent<Props & ITracks & InjectedIntlProps> {
-  buildUrl = (medium: string) => {
-    const { utmParams, url } = this.props;
-    let resUrl = url;
-    if (utmParams) {
-      resUrl += `?utm_source=${utmParams.source}&utm_campaign=${utmParams.campaign}&utm_medium=${medium}`;
-      if (utmParams.content) {
-        resUrl += `&utm_content=${utmParams.content}`;
-      }
-    }
-    return resUrl;
-  };
-
-  render() {
-    const {
-      tenant,
-      context,
-      twitterMessage,
-      emailSubject,
-      emailBody,
-      className,
-      intl: { formatMessage },
-      location,
-      id,
-    } = this.props;
-
-    if (!isNilOrError(tenant)) {
-      const messengerButtonText = formatMessage(messages.shareViaMessenger);
-      const twitterButtonText = formatMessage(messages.shareOnTwitter);
-      const emailButtonText = formatMessage(messages.shareByEmail);
-
-      const messenger = facebookAppId ? (
-        <a
-          className="sharingButton messenger"
-          href={`fb-messenger://share/?link=${encodeURIComponent(
-            this.buildUrl('messenger')
-          )}&app_id=${facebookAppId}`}
-          onClick={trackMessengerShare}
-          role="button"
-          aria-label={messengerButtonText}
-        >
-          <StyledIcon name="messenger" />
-        </a>
-      ) : null;
-
-      const twitter = (
-        <TwitterButton
-          message={twitterMessage}
-          url={this.buildUrl('twitter')}
-          className={`sharingButton twitter ${
-            !emailSubject || !emailBody ? 'last' : ''
-          }`}
-          sharer={true}
-          onClick={trackTwitterShare}
-          aria-label={twitterButtonText}
-        >
-          <StyledIcon name="twitter" />
-        </TwitterButton>
-      );
-
-      const email =
-        emailSubject && emailBody ? (
-          <a
-            className="sharingButton last email"
-            href={`mailto:?subject=${emailSubject}&body=${emailBody}`}
-            onClick={trackEmailShare}
-            role="button"
-            aria-label={emailButtonText}
-          >
-            <StyledIcon name="email" />
-          </a>
-        ) : null;
-    }
-
-    return null;
-  }
-}
-const SharingDropdownContent = ({ id, className, context }: Props) => {
+const SharingDropdownContent = ({
+  id,
+  className,
+  context,
+  url,
+  utmParams,
+  emailBody,
+  emailSubject,
+  twitterMessage,
+}: Props) => {
   const titleMessage = {
     idea: <FormattedMessage {...messages.shareIdea} />,
     project: <FormattedMessage {...messages.shareThisProject} />,
     initiative: <FormattedMessage {...messages.shareThisInitiative} />,
     folder: <FormattedMessage {...messages.shareThisFolder} />,
   }[context];
+  const hasEmailSharing = !!(emailBody && emailSubject);
+
+  const buildUrl = (medium: string) => {
+    let resUrl = url;
+
+    if (utmParams) {
+      resUrl += `?utm_source=${utmParams.source}&utm_campaign=${utmParams.campaign}&utm_medium=${medium}`;
+
+      if (utmParams.content) {
+        resUrl += `&utm_content=${utmParams.content}`;
+      }
+    }
+
+    return resUrl;
+  };
 
   return (
     <Container id={id || ''} className={className || ''}>
       <Title>{titleMessage}</Title>
       <Buttons>
-        <Facebook />
-        {messenger}
-        {twitter}
-        {email}
+        <Facebook url={buildUrl('facebook')} />
+        {<Messenger url={buildUrl('messenger')} />}
+        {
+          <Twitter
+            url={buildUrl('twitter')}
+            twitterMessage={twitterMessage}
+            isLastItem={!hasEmailSharing}
+          />
+        }
+        {emailBody && emailSubject && (
+          <Email emailBody={emailBody} emailSubject={emailSubject} />
+        )}
       </Buttons>
     </Container>
   );
