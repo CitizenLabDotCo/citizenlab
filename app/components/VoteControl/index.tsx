934606ee0388f3c6963f15be10b9cb5855311ad8
brentguf <brent.guffens@gmail.com>
2020-09-01 09:51:45 +0200

    Only show shadow when voting is enabled

--- app/components/VoteControl/index.tsx
+++ app/components/VoteControl/index.tsx
@@ -89,7 +89,10 @@ const VoteIconContainer = styled.div<{
   }
 
   &.shadow {
-    box-shadow: 0px 4px 3px rgba(0, 0, 0, 0.05); // TODO: add to styleutils
+    ${({ votingEnabled }) =>
+      votingEnabled &&
+      `box-shadow: 0px 4px 3px rgba(0, 0, 0, 0.05); // TODO: add to styleutils
+    `}
   }
 
   ${(props) =>
