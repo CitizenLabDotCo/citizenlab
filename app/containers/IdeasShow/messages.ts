11a84ee3e286542d5f77f99dc3fec5dbc420ab1c
brentguf <brent.guffens@gmail.com>
2020-09-01 10:35:29 +0200

    Uncomment mobile sharing

--- app/containers/IdeasShow/messages.ts
+++ app/containers/IdeasShow/messages.ts
@@ -99,4 +99,17 @@ export default defineMessages({
     id: 'app.components.IdeasShow.bodyTitle',
     defaultMessage: 'Description',
   },
+  twitterMessage: {
+    id: 'app.containers.IdeasShow.twitterMessage',
+    defaultMessage: 'Vote for {ideaTitle} on',
+  },
+  emailSharingSubject: {
+    id: 'app.containers.IdeasShow.emailSharingSubject',
+    defaultMessage: 'Support my idea: {ideaTitle}.',
+  },
+  emailSharingBody: {
+    id: 'app.containers.IdeasShow.emailSharingBody',
+    defaultMessage:
+      'What do you think of this idea? Vote on it and share the discussion at {ideaUrl} to make your voice heard!',
+  },
 });
