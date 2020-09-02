eeb370c4fbdc18e2195a7bfc50abc37fa9129b79
brentguf <brent.guffens@gmail.com>
2020-09-02 15:37:51 +0200

    Buttons finish

--- app/containers/IdeasShow/CTABox/Buttons/GoToCommentsButton.tsx
+++ app/containers/IdeasShow/CTABox/Buttons/GoToCommentsButton.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import IdeaCTAButton from './IdeaCTAButton';
 
+// i18n
+import { injectIntl } from 'utils/cl-intl';
+import { InjectedIntlProps } from 'react-intl';
+import messages from './messages';
+
 interface Props {
   className?: string;
 }
 
-const GoToCommentsButton = ({ className }: Props) => {
+const GoToCommentsButton = ({
+  className,
+  intl: { formatMessage },
+}: Props & InjectedIntlProps) => {
   const onClick = () => {
     const commentInputField = document.getElementById('comments-main-title');
 
@@ -18,9 +26,10 @@ const GoToCommentsButton = ({ className }: Props) => {
     <IdeaCTAButton
       onClick={onClick}
       className={className}
-      copy="Join the discussion"
+      copy={formatMessage(messages.postComment)}
+      iconName="comments"
     />
   );
 };
 
-export default GoToCommentsButton;
+export default injectIntl(GoToCommentsButton);
---
+++ app/containers/IdeasShow/CTABox/Buttons/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  postComment: {
+    id: 'app.containers.IdeasShow.postComment',
+    defaultMessage: 'Post comment',
+  },
+});
