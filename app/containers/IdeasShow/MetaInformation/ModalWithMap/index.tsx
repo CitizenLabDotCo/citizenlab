69b096ec7e4fde455015b36322f2ead03e264917
brentguf <brent.guffens@gmail.com>
2020-09-04 10:45:00 +0200

    Modal responsiveness

--- app/containers/IdeasShow/MetaInformation/ModalWithMap/index.tsx
+++ app/containers/IdeasShow/MetaInformation/ModalWithMap/index.tsx
@@ -1,7 +1,9 @@
 import React, { lazy, Suspense } from 'react';
 import Modal from 'components/UI/Modal';
 const Map = lazy(() => import('./Map'));
+import useWindowSize from 'hooks/useWindowSize';
 import { Spinner } from 'cl2-component-library';
+import { viewportWidths } from 'utils/styleUtils';
 
 interface Props {
   position: GeoJSON.Point;
@@ -16,8 +18,15 @@ const ModalWithMap = ({
   isOpened,
   onCloseModal,
 }: Props) => {
+  const { windowWidth } = useWindowSize();
+  const smallerThanSmallTablet = windowWidth <= viewportWidths.smallTablet;
+
   return (
-    <Modal padding={'70px 30px 30px'} opened={isOpened} close={onCloseModal}>
+    <Modal
+      padding={smallerThanSmallTablet ? '70px 0 30px' : '70px 30px 30px'}
+      opened={isOpened}
+      close={onCloseModal}
+    >
       <Suspense fallback={<Spinner />}>
         <Map position={position} projectId={projectId} />
       </Suspense>
