34d36a03bbb7fde2ed0e8ad02afb525d713b0478
brentguf <brent.guffens@gmail.com>
2020-09-08 15:36:02 +0200

    Load more text WIP

--- app/components/PostShowComponents/Body.tsx
+++ app/components/PostShowComponents/Body.tsx
@@ -1,10 +1,11 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import { isNilOrError } from 'utils/helperUtils';
 import useTranslation from 'hooks/useTranslation';
 import useWindowSize from 'hooks/useWindowSize';
 
 // components
 import QuillEditedContent from 'components/UI/QuillEditedContent';
+import Button from 'components/UI/Button';
 
 // typings
 import { Locale } from 'typings';
@@ -14,6 +15,11 @@ import styled, { useTheme } from 'styled-components';
 import { viewportWidths } from 'utils/styleUtils';
 
 const Container = styled.div``;
+
+const LoadMoreTextButton = styled(Button)`
+  text-decoration: underline;
+  display: inline-block;
+`;
 
 interface Props {
   postId: string;
@@ -26,11 +32,16 @@ interface Props {
 
 const Body = memo<Props>(
   ({ postId, body, locale, translateButtonClicked, className, postType }) => {
+    const [loadMoreButtonClicked, setLoadMoreButtonClicked] = useState(false);
     const windowSize = useWindowSize();
     const theme: any = useTheme();
     const smallerThanSmallTablet = windowSize
       ? windowSize.windowWidth <= viewportWidths.smallTablet
       : false;
+
+    const loadMore = () => {
+      setLoadMoreButtonClicked(true);
+    };
 
     if (locale) {
       const translation = useTranslation({
@@ -41,9 +52,20 @@ const Body = memo<Props>(
       });
 
       if (!isNilOrError(translation)) {
-        const bodyText = translateButtonClicked
+        const originalBodyText = translateButtonClicked
           ? translation.attributes.translation
           : body;
+        const wordsArray = originalBodyText.split(' ');
+        const numberOfWords = wordsArray.length;
+        const initialWordsLimitToDisplay = 50;
+        const hasTooLongBody = numberOfWords > initialWordsLimitToDisplay;
+        const showLoadMoreButton = hasTooLongBody && !loadMoreButtonClicked;
+        const bodyTextToDisplay = showLoadMoreButton
+          ? wordsArray
+              .slice(0, initialWordsLimitToDisplay)
+              .join(' ')
+              .concat('...')
+          : originalBodyText;
 
         return (
           <Container id={`e2e-${postType}-description`} className={className}>
@@ -53,9 +75,14 @@ const Body = memo<Props>(
               fontWeight={300}
             >
               <div aria-live="polite">
-                <span dangerouslySetInnerHTML={{ __html: bodyText }} />
+                <span dangerouslySetInnerHTML={{ __html: bodyTextToDisplay }} />
               </div>
             </QuillEditedContent>
+            {showLoadMoreButton && (
+              <LoadMoreTextButton buttonStyle="text" onClick={loadMore}>
+                Load more...
+              </LoadMoreTextButton>
+            )}
           </Container>
         );
       }
