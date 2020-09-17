57fc2e53b0a48474bd35c8cee955144d22ecbdd8
Sébastien Hoorens <sebastien@citizenlab.co>
2020-09-17 14:55:31 +0200

    granular permissions for posting initiatives

--- app/policies/initiative_policy.rb
+++ app/policies/initiative_policy.rb
@@ -27,18 +27,6 @@ class InitiativePolicy < ApplicationPolicy
     )
   end
 
-  def create?
-    pcs = ParticipationContextService.new 
-    record.draft? ||
-    user&.active_admin_or_moderator?(record.project_id) ||
-    (
-      user&.active? &&
-      record.author_id == user.id &&
-      !pcs.posting_idea_disabled_reason_for_project(record.project, user) &&
-      ProjectPolicy.new(user, record.project).show?
-    )
-  end
-
   def show?
     (
       user&.active? &&
