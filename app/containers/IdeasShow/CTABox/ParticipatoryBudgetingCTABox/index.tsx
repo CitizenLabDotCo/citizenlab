4092c8ededa0de1ae7adc066e749d8bd97a456e6
brentguf <brent.guffens@gmail.com>
2020-09-17 15:36:32 +0200

    Code review prep

--- app/containers/IdeasShow/CTABox/ParticipatoryBudgetingCTABox/index.tsx
+++ app/containers/IdeasShow/CTABox/ParticipatoryBudgetingCTABox/index.tsx
@@ -11,7 +11,7 @@ const Container = styled.div`
   border-radius: 2px;
   padding-bottom: 30px;
   border-bottom: 1px solid ${colors.separation};
-  background-color: #edeff0; // TODO: add color to component library
+  background-color: #edeff0;
   padding: 25px 30px;
 `;
 
@@ -37,7 +37,6 @@ interface Props {
 const ParticipatoryBudgetingCTABox = (props: Props) => {
   const { ideaId } = props;
 
-  // TODO: a11y title
   return (
     <Container>
       <StyledBudgetAssignment {...props} />
