f12b0e41eabf614e9db9030b8c358c9d27952fd3
Sara <sara@citizenlab.co>
2020-09-21 18:13:32 +0200

    fixes extract_intl

--- app/components/InitiativeButton/messages.ts
+++ app/components/InitiativeButton/messages.ts
@@ -2,11 +2,11 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   startInitiative: {
-    id: 'app.containers.InitiativesIndexPage.startInitiative',
+    id: 'app.containers.InitiativesButton.startInitiative',
     defaultMessage: 'Start an initiative',
   },
   postingNotPermitted: {
-    id: 'app.containers.InitiativesIndexPage.postingNotPermitted',
+    id: 'app.containers.InitiativesButton.postingNotPermitted',
     defaultMessage: "You don't have the rights to post an initiative",
   },
 });
