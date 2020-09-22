9bc0abfec413fccf7f7ceca4ad1793840f37756c
brentguf <brent.guffens@gmail.com>
2020-09-22 11:34:34 +0200

    CTA box screen reader headers

--- app/containers/IdeasShow/CTABox/ParticipatoryBudgetingCTABox/BudgetAssignment/messages.ts
+++
@@ -1,8 +0,0 @@
-import { defineMessages } from 'react-intl';
-
-export default defineMessages({
-  a11y_budgetControl: {
-    id: 'app.containers.IdeasShow.a11y_budgetControl',
-    defaultMessage: 'Idea assignment and status',
-  },
-});
