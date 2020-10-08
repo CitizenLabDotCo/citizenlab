03f2950894ab05729d409b7a8f803e4a816d7cd7
Adrien Dessy <adrien@citizenlab.co>
2020-10-08 16:59:51 +0200

    fix schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_174500) do
+ActiveRecord::Schema.define(version: 2020_10_07_102916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
