10f7bcd0963f057ccc35e5a547227fc7c134a2eb
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-10-14 22:28:35 +0200

    reverted eager load line

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
+  config.eager_load = true
 
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
@@ -57,5 +57,5 @@ Rails.application.configure do
   # config.rails_semantic_logger.started    = true
   # config.rails_semantic_logger.processing = true
   # config.rails_semantic_logger.rendered   = true
-   
+
 end
