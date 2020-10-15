8c032648b4ab6cec6a6203ac95af557f0857a068
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-10-15 09:15:20 +0200

    moved bullet config to environment config file

--- config/application.rb
+++ config/application.rb
@@ -51,18 +51,6 @@ module Cl2Back
     config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
 
     config.skylight.environments = %w[staging]
-
-    config.after_initialize do
-      Bullet.enable = true
-      Bullet.rails_logger = true
-      Bullet.bullet_logger = true
-      # Bullet.sentry = true if Rails.env.staging?
-      # Bullet.raise = true if Rails.env.test?
-
-      # Bullet.slack = { webhook_url: 'http://some.slack.url', channel: '#default', username: 'notifier' }
-      # Bullet.stacktrace_includes = [ 'your_gem', 'your_middleware' ]
-      # Bullet.stacktrace_excludes = [ 'their_gem', 'their_middleware', ['my_file.rb', 'my_method'], ['my_file.rb', 16..20] ]
-    end
   end
 end
 
