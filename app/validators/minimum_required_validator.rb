cf6d901e3a33d2a920590cd9b04fef54a774724e
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-10-17 01:40:50 +0200

    added validations and specs on ideastatuses with default codes

--- app/validators/minimum_required_validator.rb
+++ app/validators/minimum_required_validator.rb
@@ -1,9 +1,11 @@
 class MinimumRequiredValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    return if !required_values.include?(value.to_sym) ||
-              include_required_values?(record.class.pluck(attribute))
+    return unless record.persisted?
 
-    record.errors.add(:required, attribute, message: 'Cannot delete a required value.')
+    other_record_values = record.class.where.not(id: record.id).pluck(attribute)
+    return if !required_values.include?(value.to_sym) || contains_required_values?(other_record_values)
+
+    record.errors.add(attribute, :required, message: 'is required and cannot be changed or deleted.')
   end
 
   private
@@ -12,7 +14,7 @@ class MinimumRequiredValidator < ActiveModel::EachValidator
     options.dig(:values).map(&:to_sym)
   end
 
-  def include_required_values?(*values)
+  def contains_required_values?(values)
     (required_values & values.map(&:to_sym)) == required_values
   end
 end
