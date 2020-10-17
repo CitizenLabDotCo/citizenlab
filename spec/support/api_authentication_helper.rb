9d88a9190929ec5296f4902744524e049088bce8
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-10-17 02:29:18 +0200

    added controller specs and policies

---
+++ spec/support/api_authentication_helper.rb
@@ -0,0 +1,14 @@
+module ApiAuthenticationHelper
+  def admin_header_token
+    header_token_for create(:admin)
+  end
+
+  def user_header_token
+    header_token_for create(:user)
+  end
+
+  def header_token_for(user)
+    token = Knock::AuthToken.new(payload: user.to_token_payload).token
+    header 'Authorization', "Bearer #{token}"
+  end
+end
