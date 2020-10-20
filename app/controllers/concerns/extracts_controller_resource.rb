f19ab17e7d8664196c0f2c8dc313d152bc50f243
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-10-21 00:38:58 +0200

    some documentation

--- app/controllers/concerns/extracts_controller_resource.rb
+++ app/controllers/concerns/extracts_controller_resource.rb
@@ -1,31 +1,85 @@
 # frozen_string_literal: true
 
-#
-# Defines a Controller Macro to easily access @resource_variables in a controller
-#
-#
-#
-#
+##
+# Defines a Controller Macro to easily access a +resource+ in a controller.
 module ExtractsControllerResource
   extend ActiveSupport::Concern
 
-  COLLECTION_METHODS = %i[index bulk_update].freeze
-  MEMBER_METHODS = %i[show create update destroy reorder].freeze
-
   included do
     extend ClassMethods
+    private_constant :COLLECTION_METHODS, :MEMBER_METHODS
   end
 
-  # ExtractsControllerResource::ClassMethods
-  module ClassMethods
-    private
+  # ActionDispatch::Routing::Resources::CANONICAL_ACTIONS
 
+  # The array of collection method symbols
+  COLLECTION_METHODS = %i[index bulk_update].freeze
+
+  # The array of member method symbols
+  MEMBER_METHODS = %i[show create update destroy reorder].freeze
+
+  ##
+  # Defines the ClassMethods used by a Controller that ExtractsControllerResource.
+  module ClassMethods
+    ##
+    # Defines a +#resource+ instance method that returns a *collection* or *member* of a class.
+    #
+    # @example Trigger the generator without passing a +resource_name+
+    #   class IdeasController < ApplicationController
+    #     include ExtractsControllerResource
+    #
+    #     resource
+    #
+    #     def index
+    #       @ideas = Idea.all
+    #       resource #=> @ideas
+    #     end
+    #
+    #
+    #     def show
+    #       @idea = Idea.find(2)
+    #       resource #=> @idea
+    #     end
+    #   end
+    #
+    # @example Trigger the generator passing a +resource_name+
+    #   class AbstractController < ApplicationController
+    #     include ExtractsControllerResource
+    #
+    #     resource :idea
+    #
+    #     def index
+    #       @ideas = Idea.all
+    #       resource #=> @ideas
+    #     end
+    #
+    #
+    #     def show
+    #       @idea = Idea.find(2)
+    #       resource #=> @idea
+    #     end
+    #   end
+    #
+    # @param resource_name [String, Symbol, nil]  The name of the +Resource+ class used in the controller's CRUD.
+    # @param options [Hash, nil] The optional arguments to generate the resource helpers.
+    # @return nil
     def resource(resource_name = nil, **options)
       @resource_name = resource_name || controller_resource_name
       @resource_options = options
 
       define_resource_methods
     end
+
+    # A helper that extracts the current action from +params+ and returns a +member+ or +collection+ variable.
+    # @param params [ActionController::Parameters]
+    # @return [ActiveRecord::CollectionProxy, <Resource>]
+    def resource_variable_from_params(params)
+      return collection_variable if collection_method?(params)
+
+      member_variable
+    end
+
+    private
 
     def collection_method?(params)
       COLLECTION_METHODS.include?(params[:action].to_sym)
@@ -56,14 +110,6 @@ module ExtractsControllerResource
 
     def collection_variable
       member_variable.pluralize
-    end
-
-    public
-
-    def resource_variable_from_params(params)
-      return collection_variable if collection_method?(params)
-
-      member_variable
     end
   end
 end
