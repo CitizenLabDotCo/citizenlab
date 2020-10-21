165a7ddb194984e3aee1d6afeca4d672514a727c
Michiel Leyman <michiel.leyman@gmail.com>
2020-10-21 11:58:20 +0200

    Call and handle delete functionality

--- app/containers/Admin/projects/components/DeleteProjectButton.tsx
+++ app/containers/Admin/projects/components/DeleteProjectButton.tsx
@@ -1,23 +1,56 @@
-import React, { memo, FormEvent } from 'react';
+import React, { memo, FormEvent, useState } from 'react';
 import Button from 'components/UI/Button';
 import HasPermission from 'components/HasPermission';
-import { IProject } from 'services/projects';
-import { injectIntl, FormattedMessage } from 'utils/cl-intl';
+import { IProject, deleteProject } from 'services/projects';
+import clHistory from 'utils/cl-router/history';
+
+// i18n
 import { InjectedIntlProps } from 'react-intl';
+import { injectIntl } from 'utils/cl-intl';
+import messages from './messages';
 
 interface Props {
   project: IProject;
 }
 
+type TDeleteRequestState = {
+  processing: boolean;
+  error: string;
+};
+
 const DeleteProjectButton = memo<Props & InjectedIntlProps>(
   ({ project, intl: { formatMessage } }) => {
-    const deleteProject = async (event: FormEvent<any>) => {
+    const [deleteRequestState, setDeleteRequestState] = useState<
+      TDeleteRequestState
+    >({
+      processing: false,
+      error: '',
+    });
+
+    const handleDeleteProject = async (event: FormEvent<any>) => {
       event.preventDefault();
+
+      if (
+        project &&
+        window.confirm(formatMessage(messages.deleteProjectConfirmation))
+      ) {
+        try {
+          setDeleteRequestState({ ...deleteRequestState, processing: true });
+          await deleteProject(project.data.id);
+          clHistory.push('/admin/projects');
+        } catch {
+          setDeleteRequestState({
+            ...deleteRequestState,
+            processing: true,
+            error: formatMessage(messages.deleteProjectError),
+          });
+        }
+      }
     };
 
     return (
       <HasPermission item={project.data} action="delete">
-        <button onClick={deleteProject}>delete</button>
+        <button onClick={handleDeleteProject}>delete</button>
       </HasPermission>
     );
   }
