89f1f8932c0b0f21325a0b94da23218bcfd32983
Michiel Leyman <michiel.leyman@gmail.com>
2020-10-23 11:06:03 +0200

    Make props optional

--- cypress/fixtures/project_folder.ts
+++ cypress/fixtures/project_folder.ts
@@ -1,16 +1,16 @@
 import { randomString } from '../support/commands';
 
 interface Props {
-  type: 'timeline' | 'continuous';
-  title: string;
-  descriptionPreview: string;
-  description: string;
+  type?: 'timeline' | 'continuous';
+  title?: string;
+  descriptionPreview?: string;
+  description?: string;
   publicationStatus?: 'draft' | 'published' | 'archived';
   projectIds?: string[];
 }
 
 export default ({ projectIds = [], ...folder }: Props) => ({
-  type: 'continuous',
+  type: folder.type || 'continuous',
   title: folder.title || randomString(),
   descriptionPreview: folder.descriptionPreview || randomString(),
   description: folder.description || randomString(),
