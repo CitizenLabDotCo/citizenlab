b0591f4fe3255941a8c48bba2912ed97d5da52bb
Sara <sara@citizenlab.co>
2020-10-26 17:31:14 +0100

    Typing fixes

--- app/utils/analytics/index.ts
+++ app/utils/analytics/index.ts
@@ -137,12 +137,12 @@ combineLatest(tenant$, authUser$, initializeTacking$).subscribe(
       (function (w, d, s, l, i) {
         w[l] = w[l] || [];
         w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-        let f = d.getElementsByTagName(s)[0],
-          j = d.createElement(s),
-          dl = l != 'dataLayer' ? '&l=' + l : '';
+        const f = d.getElementsByTagName(s)[0];
+        const j = d.createElement(s) as HTMLScriptElement;
+        const dl = l !== 'dataLayer' ? `&l=${l}` : '';
         j.async = true;
-        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode.insertBefore(j, f);
+        j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`;
+        f.parentNode?.insertBefore(j, f);
       })(window, document, 'script', 'dataLayer', 'GTM-KBM5894');
     }
     // shutdown
