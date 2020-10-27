402279ae675156542d78072b8c6754c841268cfc
Adrien Dessy <adrien@citizenlab.co>
2020-10-27 11:10:43 +0100

    specify the controller for images and files (from the main app)

--- engines/project_folders/config/routes.rb
+++ engines/project_folders/config/routes.rb
@@ -1,10 +1,10 @@
 ProjectFolders::Engine.routes.draw do
   namespace :web_api, :defaults => {:format => :json} do
     namespace :v1 do
-      resources :project_folders do
-        resources :images, defaults: {container_type: 'ProjectFolder'}
-        resources :files, defaults: {container_type: 'ProjectFolder'}
-        get 'by_slug/:slug', on: :collection, to: 'project_folders#by_slug'
+      resources :project_folders, controller: 'folders' do
+        resources :images, controller: '/web_api/v1/images', defaults: {container_type: 'ProjectFolder'}
+        resources :files, controller: '/web_api/v1/files', defaults: {container_type: 'ProjectFolder'}
+        get 'by_slug/:slug', on: :collection, to: 'folders#by_slug'
       end
     end
   end
