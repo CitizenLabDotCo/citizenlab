a7ec278fd8e9c2af7377c9568c874e280d0876a5
Sébastien Hoorens <sebastien@citizenlab.co>
2020-10-28 15:12:32 +0100

    previewing emails

---
+++ engines/email_campaigns/spec/mailers/previews/welcome_mailer_preview.rb
@@ -0,0 +1,10 @@
+class WelcomeMailerPreview < ActionMailer::Preview
+  def campaign_mail
+    command = {
+      recipient: User.first
+    }
+    campaign = EmailCampaigns::Campaigns::Welcome.first
+    
+    campaign.mailer_class.campaign_mail campaign, command
+  end
+end
