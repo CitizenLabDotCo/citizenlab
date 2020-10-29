968a4c2a8d426d7c80645c581ab22d753e54bc00
Sébastien Hoorens <sebastien@citizenlab.co>
2020-10-29 16:14:21 +0100

    help

--- config/initializers/mjml.rb
+++ config/initializers/mjml.rb
@@ -1,4 +1,4 @@
 Mjml.setup do |config|
-  config.template_language = :erb
-  config.raise_render_exception = true
+  config.template_language = :erb # Should already be default
+  config.raise_render_exception = true # For debugging purposes
 end
