3025398e2c189440fbf3a67386554f4ce47d7a09
brentguf <brent.guffens@gmail.com>
2020-10-30 12:45:24 +0100

    Remove cache prop from useUserCustomFields

--- app/hooks/useUserCustomFields.ts
+++ app/hooks/useUserCustomFields.ts
@@ -3,7 +3,6 @@ import {
   userCustomFieldsStream,
   IUserCustomFieldData,
 } from 'services/userCustomFields';
-import { isBoolean } from 'lodash-es';
 
 type IInputType = 'select' | 'multiselect' | 'checkbox' | 'number';
 
@@ -12,22 +11,20 @@ interface Props {
   cache?: boolean;
 }
 
-export default function useUserCustomFields({ inputTypes, cache }: Props) {
+export default function useUserCustomFields({ inputTypes }: Props) {
   const [userCustomFields, setUserCustomFields] = useState<
     IUserCustomFieldData[] | undefined | null | Error
   >(undefined);
-  const useCacheStream = isBoolean(cache) ? cache : true;
 
   useEffect(() => {
     const subscription = userCustomFieldsStream({
-      cacheStream: useCacheStream,
       queryParameters: { input_types: inputTypes },
     }).observable.subscribe((userCustomFields) => {
       setUserCustomFields(userCustomFields.data);
     });
 
     return () => subscription.unsubscribe();
-  }, [inputTypes, cache]);
+  }, [inputTypes]);
 
   return userCustomFields;
 }
