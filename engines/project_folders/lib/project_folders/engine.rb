07419e9a396ec442c2b1e68d4e41179c9d0edca2
Adrien Dessy <adrien@citizenlab.co>
2020-10-30 10:39:18 +0100

    project decorator to extract project-folder methods

--- engines/project_folders/lib/project_folders/engine.rb
+++ engines/project_folders/lib/project_folders/engine.rb
@@ -2,5 +2,13 @@ module ProjectFolders
   class Engine < ::Rails::Engine
     isolate_namespace ProjectFolders
     config.generators.api_only = true
+
+    def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+
+    config.to_prepare(&method(:activate).to_proc)
   end
 end
