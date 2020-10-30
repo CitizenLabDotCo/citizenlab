9d9eea8e1be0b2534c8823e0dddde8dfb6f272ab
Sébastien Hoorens <sebastien@citizenlab.co>
2020-10-30 17:31:09 +0100

    awesome footer

--- engines/email_campaigns/app/helpers/email_campaigns/campaign_helper.rb
+++ engines/email_campaigns/app/helpers/email_campaigns/campaign_helper.rb
@@ -1,8 +1,15 @@
 module EmailCampaigns
   module CampaignHelper
 
-    def formatMessage key, values={}
-      t("email_campaigns.#{@campaign.class.name.demodulize.underscore}.#{key}", values)
+    def formatMessage key, fragment: nil, escape_html: true, values: {}
+      group = fragment || @campaign.class.name.demodulize.underscore
+      # byebug if key == 'statement'
+      msg = t("email_campaigns.#{group}.#{key}", values)
+      if escape_html
+        msg
+      else
+        msg.html_safe
+      end
     end
 
     def localize multiloc
