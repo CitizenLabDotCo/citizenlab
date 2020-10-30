da7a3e0543b9e7cdb06d0c043b7c054fcabcd9d2
Sébastien Hoorens <sebastien@citizenlab.co>
2020-10-30 18:26:51 +0100

    application mailer

--- engines/email_campaigns/app/mailers/email_campaigns/application_mailer.rb
+++ engines/email_campaigns/app/mailers/email_campaigns/application_mailer.rb
@@ -1,6 +1,48 @@
 module EmailCampaigns
   class ApplicationMailer < ActionMailer::Base
+    add_template_helper(CampaignHelper)
+
     default from: ENV.fetch("DEFAULT_FROM_EMAIL", 'hello@citizenlab.co')
+    default reply_to: ENV.fetch("DEFAULT_FROM_EMAIL", 'hello@citizenlab.co')
     layout 'mailer'
+
+    def campaign_mail campaign, command
+      recipient = command[:recipient]
+      
+      @url_service = Frontend::UrlService.new
+      @cta = {}
+      @locale = recipient.locale
+      @user = recipient
+      @tenant = Tenant.current
+      @campaign = campaign
+      @command = command
+
+      I18n.with_locale(@locale) do
+        message = mail(
+          subject: subject,
+          to: recipient.email
+        ) do |format|
+          # format.text
+          format.mjml
+        end
+        if (ActionMailer::Base.delivery_method == :mailgun)
+          message.mailgun_headers = {
+            'X-Mailgun-Variables' => {
+              'cl_tenant_id' => @tenant.id,
+              'cl_campaign_id' => campaign.id,
+              'cl_user_id' => recipient.id,
+            }.to_json,
+          }
+        end
+        message
+      end
+    end
+
+
+    protected
+
+    def subject
+      ''
+    end
   end
 end
--- engines/email_campaigns/app/mailers/email_campaigns/welcome_mailer.rb
+++ engines/email_campaigns/app/mailers/email_campaigns/welcome_mailer.rb
@@ -1,49 +1,15 @@
 module EmailCampaigns
-  class WelcomeMailer < ActionMailer::Base
-    add_template_helper(CampaignHelper)
+  class WelcomeMailer < ApplicationMailer
+    
 
-    default from: ENV.fetch("DEFAULT_FROM_EMAIL", 'hello@citizenlab.co')
-    default reply_to: ENV.fetch("DEFAULT_FROM_EMAIL", 'hello@citizenlab.co')
+    protected
 
-    def campaign_mail campaign, command
-      recipient = command[:recipient]
-      multiloc_service = MultilocService.new
-      
-      @url_service = Frontend::UrlService.new
-      @cta = {}
-      @locale = recipient.locale
-      @user = recipient
-      @tenant = Tenant.current
-      @campaign = campaign
-      @command = command
-
-      I18n.with_locale(@locale) do
-        subject = I18n.t(
-          'email_campaigns.welcome.subject', 
-          organizationName: multiloc_service.t(@tenant.settings.dig('core', 'organization_name'))
-          )
-        message = mail(
-          subject: subject,
-          to: recipient.email
-        ) do |format|
-          # format.text
-          format.mjml
-        end
-        if (ActionMailer::Base.delivery_method == :mailgun)
-          message.mailgun_headers = {
-            'X-Mailgun-Variables' => {
-              'cl_tenant_id' => @tenant.id,
-              'cl_campaign_id' => campaign.id,
-              'cl_user_id' => recipient.id,
-            }.to_json,
-          }
-        end
-        message
-      end
+    def subject
+      I18n.t(
+        'email_campaigns.welcome.subject', 
+        organizationName: MultilocService.new.t(@tenant.settings.dig('core', 'organization_name'))
+        )
     end
-
-
-    private
 
   end
 end
