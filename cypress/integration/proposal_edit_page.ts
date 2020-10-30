fbfd141bccfae3c1273df3f0a365ccb683df3442
David <david@citizenlab.co>
2020-10-30 22:48:47 +0100

    another attempt at fixing proposal edit page e2e test

--- cypress/integration/proposal_edit_page.ts
+++ cypress/integration/proposal_edit_page.ts
@@ -95,13 +95,27 @@ describe('Initiative form page', () => {
     // save the form
     cy.get('.e2e-initiative-publish-button').find('.e2e-submit-form').click();
 
-    // verify updated initiative page
-    cy.get('#e2e-initiative-show', { timeout: 200000 });
+    // manually wait for form to be saved
+    cy.wait(20000);
 
-    cy.location('pathname').should(
-      'eq',
-      `/en-GB/initiatives/${initiativeTitle}`
-    );
+    // verify updated initiative page
+    cy.visit(`/initiatives/${initiativeTitle}`);
+
+    cy.get('#e2e-initiative-show');
+    cy.get('#e2e-initiative-show')
+      .find('#e2e-initiative-title')
+      .contains(newInitiativeTitle);
+    cy.get('#e2e-initiative-show')
+      .find('#e2e-initiative-description')
+      .contains(newInitiativeContent);
+    cy.get('#e2e-initiative-show')
+      .find('#e2e-initiative-topics')
+      .find('.e2e-initiative-topic')
+      .should('have.length', 1);
+    cy.get('#e2e-initiative-image');
+    cy.get('#e2e-initiative-show')
+      .find('#e2e-map-toggle')
+      .contains('Antwerpen, Belgium');
   });
 
   after(() => {
