e0a1845564c492529b9cd9bee4d71d85d2e07a10
Sara <sara@citizenlab.co>
2020-11-02 19:34:00 +0100

    Hacky tag assignment loading

--- app/services/tags.ts
+++ app/services/tags.ts
@@ -2,8 +2,6 @@ import { API_PATH } from 'containers/App/constants';
 import streams, { IStreamParams } from 'utils/streams';
 import { Multiloc } from 'typings';
 
-const apiEndpoint = `${API_PATH}/tag_suggestions`;
-
 // web_api/v1/tag_suggestions?idea_ids= [the list of ids]&locale="the local the admin uses"
 
 export interface ITag {
@@ -17,7 +15,27 @@ export interface ITag {
 export interface ITagReponseData {
   data: ITag[];
 }
+export interface ITagAssignmentReponse {
+  id: string;
+  type: 'tag_assignment';
+  attributes: {
+    assignment_method: 'automatic' | 'manual';
+    tag_id: string;
+    idea_id: string;
+  };
+}
 
 export function tagSuggestionStream(streamParams: IStreamParams | null = null) {
-  return streams.get<ITagReponseData>({ apiEndpoint, ...streamParams });
+  return streams.get<ITagReponseData>({
+    apiEndpoint: `${API_PATH}/tag_suggestions`,
+    ...streamParams,
+  });
+}
+export function tagAssignmentsSuggestionStream(
+  streamParams: IStreamParams | null = null
+) {
+  return streams.get<{ data: ITagAssignmentReponse[] }>({
+    apiEndpoint: `${API_PATH}/generate_tag_assignment`,
+    ...streamParams,
+  });
 }
