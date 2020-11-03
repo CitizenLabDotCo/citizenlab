e6ba28c00a7d94b5c1025bfbcdfbdf92ba1ab59a
brentguf <brent.guffens@gmail.com>
2020-11-03 14:02:18 +0100

    Regex error copy

--- app/containers/Admin/projects/edit/general/messages.ts
+++ app/containers/Admin/projects/edit/general/messages.ts
@@ -187,17 +187,17 @@ export default defineMessages({
   regexFirstCharError: {
     id: 'app.containers.AdminPage.ProjectEdit.regexFirstCharError',
     defaultMessage:
-      'The first character must be a letter or number. (Letters must be without special characters and not be capitalized.)',
+      'The first character must be a letter or number. (Letters must be without special characters and not capitalized.)',
   },
   regexMiddleError: {
     id: 'app.containers.AdminPage.ProjectEdit.regexMiddleError',
     defaultMessage:
-      'Characters between the first and last characters must be letters (a-z), numbers (0-9) or hyphens (-). (Consecutive hyphens (--) are forbidden. Letters must be without special characters and not be capitalized.)',
+      'Characters between the first and last characters must be letters (a-z), numbers (0-9) or hyphens (-). (Consecutive hyphens (--) are forbidden. Letters must be without special characters and not capitalized.)',
   },
   regexLastCharError: {
     id: 'app.containers.AdminPage.ProjectEdit.regexLastCharError',
     defaultMessage:
-      'The last character must be a letter or number. (Letters must be without special characters and not be capitalized.)',
+      'The last character must be a letter or number. (Letters must be without special characters and not capitalized.)',
   },
   resultingLink: {
     id: 'app.containers.AdminPage.ProjectEdit.resultingLink',
