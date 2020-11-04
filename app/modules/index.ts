4526ecd79a8db8a30c743e2716065a9d35cca7fb
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2020-11-04 17:28:55 +0100

    Pass through outlets

--- app/modules/index.ts
+++ app/modules/index.ts
@@ -2,9 +2,14 @@ import { loadModules } from 'utils/moduleUtils';
 
 import projectFolderConfiguration from './projectFolder';
 
-export default loadModules([
+export default loadModules(
+  [
+    {
+      configuration: projectFolderConfiguration,
+      enabled: true,
+    },
+  ],
   {
-    configuration: projectFolderConfiguration,
-    enabled: true,
-  },
-]);
+    'app.containers.AdminPage.projects.all.projectsAndFolders': [],
+  }
+);
