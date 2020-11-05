e3fb47ac56da4f9c7f4592e0a2421cbf000e27c7
Sara <sara@citizenlab.co>
2020-11-05 16:16:57 +0100

    suggested fixes

--- db/migrate/20201102093045_create_tag_assignments.rb
+++ db/migrate/20201102093045_create_tag_assignments.rb
@@ -1,7 +1,7 @@
 class CreateTagAssignments < ActiveRecord::Migration[5.0]
   def change
     create_table :tag_assignments do |t|
-      t.string :assignment_method, null: false
+      t.column :assignment_method, :integer, default: 0
       t.references :idea, foreign_key: true, type: :uuid, index: true
       t.references :tag, foreign_key: true, type: :uuid, index: true
 
--- engines/nlp/app/services/nlp/tag_assignment_suggestion_service.rb
+++ engines/nlp/app/services/nlp/tag_assignment_suggestion_service.rb
@@ -1,25 +1,38 @@
 module NLP
   class TagAssignmentSuggestionService
+    @full_sanitizer = ActionView::Base.full_sanitizer
 
     def suggest(ideas, tags, locale)
       @api ||= NLP::API.new ENV.fetch('CL2_NLP_HOST')
-      @documents = ideas.map { |idea|
-        {
-          text: ActionView::Base.full_sanitizer.sanitize(idea.body_multiloc[locale]),
-          doc_id: idea.id
-        }
-      }.reject{ |doc| doc[:text].blank?}
-      @candidate_labels = tags.map{ |tag|
-        {
-          text: tag.title_multiloc[locale],
-          label_id: tag.id
-        }
-      }.reject{ |doc| doc[:text].blank?}
+
+      @documents = parse_ideas ideas, locale
+      @candidate_labels = parse_tags tags, locale
+
       @documents.any? ? @api.zeroshot_classification({
         candidate_labels: @candidate_labels,
         min_confidence_treshold: 0.5,
         documents: @documents
       }.freeze) : []
+    end
+
+    private
+
+    def parse_ideas(ideas, locale)
+      ideas.map { |idea|
+        {
+          text: @full_sanitizer.sanitize(idea.body_multiloc[locale]),
+          doc_id: idea.id
+        }
+      }.reject{ |doc| doc[:text].blank? }
+    end
+
+    def parse_tags(tags, locale)
+      tags.map{ |tag|
+        {
+          text: tag.title_multiloc[locale],
+          label_id: tag.id
+        }
+      }.reject{ |doc| doc[:text].blank? }
     end
   end
 end
