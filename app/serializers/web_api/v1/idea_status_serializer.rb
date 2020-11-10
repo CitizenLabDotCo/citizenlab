aac2e0fe0cf3510a3345b03786a7d3f30241c4b5
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-11-10 12:47:51 +0100

    continue rebase

--- app/serializers/web_api/v1/idea_status_serializer.rb
+++ app/serializers/web_api/v1/idea_status_serializer.rb
@@ -1,3 +1,3 @@
 class WebApi::V1::IdeaStatusSerializer < WebApi::V1::BaseSerializer
-  attributes :title_multiloc, :color, :ordering, :code, :description_multiloc
+  attributes :title_multiloc, :color, :ordering, :code, :description_multiloc, :ideas_count
 end
