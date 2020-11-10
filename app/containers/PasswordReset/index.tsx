fef0cdef5f3f694c270f8ec69f32d8b0a469f020
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-11-10 11:15:56 +0100

    added authentication error missing translations

--- app/containers/PasswordReset/index.tsx
+++ app/containers/PasswordReset/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { isString } from 'lodash-es';
+import { Link } from 'react-router';
 
 // router
 import clHistory from 'utils/cl-router/history';
@@ -10,12 +11,15 @@ import Button from 'components/UI/Button';
 import { Helmet } from 'react-helmet';
 import ContentContainer from 'components/ContentContainer';
 import { FormLabel } from 'components/UI/FormComponents';
+import Error from 'components/UI/Error';
 
 // services
 import { resetPassword } from 'services/auth';
+import { CLError } from 'typings';
+import { addErrorPayload } from 'utils/errorUtils';
 
 // i18n
-import { InjectedIntlProps } from 'react-intl';
+import { InjectedIntlProps, FormattedMessage } from 'react-intl';
 import { injectIntl } from 'utils/cl-intl';
 
 // style
@@ -66,6 +70,11 @@ const Form = styled.form`
 
 type Props = {};
 
+interface IApiErrors {
+  token?: CLError[];
+  password?: CLError[];
+}
+
 type State = {
   token: string | null;
   password: string | null;
@@ -73,6 +82,7 @@ type State = {
   submitError: boolean;
   processing: boolean;
   success: boolean;
+  apiErrors: IApiErrors | null;
 };
 
 class PasswordReset extends React.PureComponent<
@@ -92,6 +102,7 @@ class PasswordReset extends React.PureComponent<
       submitError: false,
       processing: false,
       success: false,
+      apiErrors: null,
     };
 
     this.passwordInputElement = null;
@@ -123,6 +134,7 @@ class PasswordReset extends React.PureComponent<
     this.setState({
       passwordError: false,
       submitError: false,
+      apiErrors: null,
       password: value,
     });
   };
@@ -142,7 +154,23 @@ class PasswordReset extends React.PureComponent<
         await resetPassword(password, token);
         this.setState({ password: null, processing: false, success: true });
       } catch (error) {
-        this.setState({ processing: false, success: false, submitError: true });
+        let { errors } = error.json;
+        const resetPasswordLink = (
+          <Link to="/password-recovery">
+            <FormattedMessage {...messages.resetPassword} />
+          </Link>
+        );
+
+        errors = addErrorPayload(errors, 'token', 'invalid', {
+          link: resetPasswordLink,
+        });
+
+        this.setState({
+          processing: false,
+          success: false,
+          submitError: true,
+          apiErrors: errors,
+        });
       }
     }
   };
@@ -152,9 +180,9 @@ class PasswordReset extends React.PureComponent<
     const {
       password,
       passwordError,
-      submitError,
       processing,
       success,
+      apiErrors,
     } = this.state;
     const helmetTitle = formatMessage(messages.helmetTitle);
     const helmetDescription = formatMessage(messages.helmetDescription);
@@ -168,8 +196,6 @@ class PasswordReset extends React.PureComponent<
 
     if (passwordError) {
       errorMessage = formatMessage(messages.passwordError);
-    } else if (submitError) {
-      errorMessage = formatMessage(messages.submitError);
     }
 
     return (
@@ -197,6 +223,13 @@ class PasswordReset extends React.PureComponent<
                 onChange={this.handlePasswordOnChange}
                 setRef={this.handlePasswordInputSetRef}
               />
+              {apiErrors &&
+                Object.keys(apiErrors).map((errorField) => (
+                  <Error
+                    apiErrors={apiErrors[errorField]}
+                    fieldName={errorField}
+                  />
+                ))}
 
               <StyledButton
                 size="2"
--- app/utils/errorUtils.ts
+++ app/utils/errorUtils.ts
@@ -1,3 +1,22 @@
+import merge from 'deepmerge';
+import { remove } from 'lodash-es';
+
 export function isCLErrorJSON(error) {
   return !!(error && error.json && error.json.errors);
+}
+
+export function addErrorPayload(errors, fieldName, errorType, payload) {
+  const error = errors[fieldName].find(({ error }) => error === errorType);
+
+  if (error) {
+    errors = remove(errors, ({ error }) => error === errorType);
+
+    const errorsWithPayload = {
+      [fieldName]: [{ ...error, payload: payload }],
+    };
+
+    errors = merge(errors, errorsWithPayload);
+  }
+
+  return errors;
 }
