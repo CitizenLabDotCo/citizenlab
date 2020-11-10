7a39b697f92b5c45f90f6fc9d3071b7558365b7f
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-11-10 12:46:10 +0100

    rebase 1

--- app/models/idea_status.rb
+++ app/models/idea_status.rb
@@ -1,17 +1,18 @@
 class IdeaStatus < ApplicationRecord
   CODES = %w(proposed viewed under_consideration accepted implemented rejected custom)
+  MINIMUM_REQUIRED_CODES = %w[proposed].freeze
+  #  viewed under_consideration accepted implemented rejected custom
 
-  has_many :ideas, dependent: :nullify
-  before_destroy :remove_notifications
+  has_many :ideas
   has_many :notifications, foreign_key: :post_status_id, dependent: :nullify
 
-  validates :title_multiloc, presence: true, multiloc: {presence: true}
-  validates :code, inclusion: {in: CODES}
-  validates :code, uniqueness: true, unless: :custom?
-  validates :description_multiloc, presence: true, multiloc: {presence: true}
+  before_destroy :remove_notifications
+
+  validates :title_multiloc, presence: true, multiloc: { presence: true }
+  validates :description_multiloc, presence: true, multiloc: { presence: true }
+  validates :code, presence: true, minimum_required: { values: MINIMUM_REQUIRED_CODES }
 
   before_validation :strip_title
-
 
   def self.create_defaults
     (CODES - ['custom']).each.with_index do |code, i|
@@ -34,24 +35,20 @@ class IdeaStatus < ApplicationRecord
   end
 
   def custom?
-    self.code == 'custom'
+    code == 'custom'
   end
-
 
   private
 
   def strip_title
-    self.title_multiloc.each do |key, value|
-      self.title_multiloc[key] = value.strip
-    end
+    title_multiloc.each { |key, value| title_multiloc[key] = value.strip }
   end
 
   def remove_notifications
     notifications.each do |notification|
-      if !notification.update post_status: nil
-        notification.destroy!
-      end
+      next if notification.update(post_status: nil)
+
+      notification.destroy!
     end
   end
-
 end
