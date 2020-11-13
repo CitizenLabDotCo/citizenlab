8be8b7269c7db536adbdc0617733bd9eb5faee02
Michiel Leyman <michiel.leyman@gmail.com>
2020-11-13 16:08:53 +0100

    Remove component-level redirect

--- app/containers/Admin/projects/index.tsx
+++ app/containers/Admin/projects/index.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import HelmetIntl from 'components/HelmetIntl';
 
-import clHistory from 'utils/cl-router/history';
-import { isNilOrError } from 'utils/helperUtils';
-
-// resources
-import useAuthUser from 'hooks/useAuthUser';
-import { isAdmin, isModerator } from 'services/permissions/roles';
-
 // i18n
 import messages from './messages';
 
@@ -15,21 +8,14 @@ type Props = {
   children: React.ReactNode;
 };
 
-const ProjectDashboard = ({ children }: Props) => {
-  const authUser = useAuthUser();
-  if (isNilOrError(authUser) || !isAdmin(authUser) || !isModerator(authUser)) {
-    clHistory.push('/');
-    return null;
-  }
-  return (
-    <>
-      <HelmetIntl
-        title={messages.helmetTitle}
-        description={messages.helmetDescription}
-      />
-      {children}
-    </>
-  );
-};
+const ProjectDashboard = ({ children }: Props) => (
+  <>
+    <HelmetIntl
+      title={messages.helmetTitle}
+      description={messages.helmetDescription}
+    />
+    {children}
+  </>
+);
 
 export default ProjectDashboard;
