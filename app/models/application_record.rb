b2724f1036bd2e1a484aa53096e006a387ba45f9
Adrien Dessy <adrien@citizenlab.co>
2020-11-17 16:49:52 +0100

    simplifying the way of listing publications types
    getting rid of the reflective approach on polymorphic associations which was not perfect:
    - risks of name collisions
    - inverse assocation are not mandatory

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,28 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-
-  # @param [Symbol,String] name The name of the polymorphic type
-  # @return [Array<Class>] List of classes
-  def self.all_polymorphic_types(name)
-    @reverse_polymorphic_types ||= Hash.new { |h, k| h[k] = [] }.tap do |hash|
-      ActiveRecord::Base.descendants.each do |klass|
-        polymorphic_types_of(klass).compact.each { |type| hash[type] << klass }
-      end
-    end
-    @reverse_polymorphic_types[name.to_sym]
-  end
-
-  private
-
-  # @param [Class] klass
-  # @return [Array<Symbol>] List of polymorphic types
-  def self.polymorphic_types_of(klass)
-    klass.reflect_on_all_associations
-         .map { |assoc_reflection| assoc_reflection.options[:as] }
-         .uniq.compact
-  end
-
 end
-
-
