1d6f150787256a2e1f760cf261827176d5a19091
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2020-11-18 13:37:09 +0100

    Remove unused import

--- app/components/ProjectAndFolderCards/index.tsx
+++ app/components/ProjectAndFolderCards/index.tsx
@@ -7,7 +7,6 @@ import { stringify } from 'qs';
 
 // components
 import ProjectCard from 'components/ProjectCard';
-import ProjectFolderCard from 'modules/project_folders/components/ProjectFolderCard';
 import SelectAreas from './SelectAreas';
 import LoadingBox from './LoadingBox';
 import Button from 'components/UI/Button';
