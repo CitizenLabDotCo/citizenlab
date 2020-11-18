55d645ca71c7f4db005eb5f337f704d90af05d84
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2020-11-18 11:36:39 +0100

    Return fragment

--- app/components/Outlet/index.tsx
+++ app/components/Outlet/index.tsx
@@ -4,9 +4,14 @@ import useOutlet from 'hooks/useOutlet';
 const Outlet = ({ id, ...props }) => {
   const components = useOutlet(id);
   if (!components) return null;
-  return components.map((Component, index) => (
-    <Component key={`${id}_${index}`} {...props} />
-  ));
+
+  return (
+    <>
+      {components.map((Component, index) => (
+        <Component key={`${id}_${index}`} {...props} />
+      ))}
+    </>
+  );
 };
 
 export default Outlet;
