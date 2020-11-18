3eb75e642d58fb547f9dbea5b63bd81890ca395d
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2020-11-18 11:29:29 +0100

    Better handle no outlets

--- app/components/Outlet/index.tsx
+++ app/components/Outlet/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import useOutlet from 'hooks/useOutlet';
 
 const Outlet = ({ id, ...props }) => {
-  const outlets = useOutlet(id);
-  return outlets.map((Outlet, index) => (
-    <Outlet key={`${id}_${index}`} {...props} />
+  const components = useOutlet(id);
+  if (!components) return null;
+  return components.map((Component, index) => (
+    <Component key={`${id}_${index}`} {...props} />
   ));
 };
 
