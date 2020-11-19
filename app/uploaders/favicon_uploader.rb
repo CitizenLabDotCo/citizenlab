9a11adc486f37c8be7729b469da20444216e7a47
Adrien Dessy <adrien@citizenlab.co>
2020-11-19 13:26:11 +0100

    rake task for tenant image migration
    logo, favicon, header_bg will be removed from Tenant and will be mounted on AppConfiguration. This has a consequence on the storage directory path (bc the model class is part of the path). So we need to move (copy here) the uploaded to the new expected location).

--- app/uploaders/favicon_uploader.rb
+++ app/uploaders/favicon_uploader.rb
@@ -4,7 +4,7 @@ class FaviconUploader < CarrierWave::Uploader::Base
 
 
   def store_dir
-    tenant = model
+    tenant = Tenant.current
     "uploads/#{tenant.id}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
