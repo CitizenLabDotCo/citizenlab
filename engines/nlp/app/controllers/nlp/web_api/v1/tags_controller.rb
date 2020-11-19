9f2cdefd881f1b3041488cdde4261254c4988863
Sara <sara@citizenlab.co>
2020-11-19 16:31:46 +0100

    Adds tag index

--- engines/nlp/app/controllers/nlp/web_api/v1/tags_controller.rb
+++ engines/nlp/app/controllers/nlp/web_api/v1/tags_controller.rb
@@ -2,9 +2,9 @@ module NLP
   module WebApi
     module V1
       class TagsController < ApplicationController
+        before_action :set_tag, only: [:show]
 
         skip_after_action :verify_authorized, only: [:generate_tags]
-
 
         def generate_tags
           locale = params['locale']
@@ -16,20 +16,29 @@ module NLP
 
           render json: ::WebApi::V1::TagSerializer.new(@tags, params: fastjson_params).serialized_json, status: :ok
         end
-      end
 
-      def index
-        @tags = Tag.all
-          .page(params.dig(:page, :number))
-          .per(params.dig(:page, :size))
+        def index
+          @tags = policy_scope(Tag)
 
-        render json: linked_json(@tags, WebApi::V1::TagSerializer, params: fastjson_params)
-      end
+          @tags = @tags.search_by_all(params[:search]) if params[:search].present?
 
-      private
+          @tags = @tags
+                   .page(params.dig(:page, :number))
+                   .per(params.dig(:page, :size))
 
-      def secure_controller?
-        false
+          render json: linked_json(@tags, ::WebApi::V1::TagSerializer, params: fastjson_params)
+        end
+
+        private
+
+        def set_tag
+          @tag = Tag.find(params[:id])
+          authorize @tag
+        end
+
+        def secure_controller?
+          false
+        end
       end
     end
   end
--- engines/nlp/app/models/tag.rb
+++ engines/nlp/app/models/tag.rb
@@ -1,9 +1,15 @@
   class Tag < ApplicationRecord
+    include PgSearch
 
     has_many :tag_assignments, dependent: :destroy
     has_many :ideas, through: :tag_assignments
 
     validates :title_multiloc, presence: true, multiloc: {presence: true}
+
+
+    pg_search_scope :search_by_all,
+                    against: [:title_multiloc],
+                    using: { :tsearch => {:prefix => true} }
 
     before_validation :strip_title
 
