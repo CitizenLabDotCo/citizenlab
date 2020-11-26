6b7eeca2ba3a122868814c61e724fb83779c7056
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2020-11-26 11:13:00 +0100

    Render icon via outlet

--- app/modules/smart_groups/index.tsx
+++ app/modules/smart_groups/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ModuleConfiguration } from 'utils/moduleUtils';
 
 import useFeatureFlag from 'hooks/useFeatureFlag';
+import LightningBolt from './components/LightningBolt';
 
 const RenderOnFeatureFlag = ({ children }) => {
   const isSmartGroupsEnabled = useFeatureFlag('smart_groups');
@@ -11,8 +12,21 @@ const RenderOnFeatureFlag = ({ children }) => {
   return null;
 };
 
+const RenderOnType = ({ type, children }) => {
+  if (type === 'rules') {
+    return <>{children}</>;
+  }
+  return null;
+};
+
 const configuration: ModuleConfiguration = {
-  outlets: {},
+  outlets: {
+    'app.containers.Admin.users.GroupsListPanel.listitem.icon': ({ type }) => (
+      <RenderOnType type={type}>
+        <LightningBolt />
+      </RenderOnType>
+    ),
+  },
   routes: {
     citizen: [],
     admin: [],
