c761d318b6ef2d1f453447eb14465ef23edba9ec
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-11-26 13:32:57 +0100

    tests fixed

--- engines/roles/spec/support/matchers/have_one_role_matcher.rb
+++ engines/roles/spec/support/matchers/have_one_role_matcher.rb
@@ -16,11 +16,11 @@ module HaveOneRoleMatcher
     end
 
     def failure_message
-      "shit"
+      'Role methods not present'
     end
 
     def failure_message_when_negated
-      "shit"
+      'Role methods not present'
     end
   end
 
