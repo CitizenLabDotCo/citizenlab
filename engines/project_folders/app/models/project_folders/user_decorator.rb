f7df2944614bca8d012986b13ada2422062c6e77
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-11-26 14:40:33 +0100

    added folder specs

--- engines/project_folders/app/models/project_folders/user_decorator.rb
+++ engines/project_folders/app/models/project_folders/user_decorator.rb
@@ -1,7 +1,10 @@
 module ProjectFolders::UserDecorator
   def self.prepended(base)
     base.class_eval do
-      has_many_roles :project_folder_moderator, through: :admin_publication_moderator, class: 'ProjectFolders::Folder', source: :publication
+      has_many_roles :project_folder_moderator,
+                     through: :admin_publication_moderator,
+                     class: 'ProjectFolders::Folder',
+                     source: :publication
     end
   end
 end
