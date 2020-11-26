1feb8ebe9fa632c2eb4e00c0dae2f2097f75648b
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-11-26 14:28:59 +0100

    first refactor try of remove_role and moderatable_ids

--- app/services/side_fx_project_service.rb
+++ app/services/side_fx_project_service.rb
@@ -59,7 +59,7 @@ class SideFxProjectService
 
   def remove_moderators project_id
     User.project_moderator(project_id).all.each do |moderator|
-      moderator.delete_role 'project_moderator', project_id: project_id
+      moderator.remove_role 'project_moderator', project_id: project_id
       moderator.save!
     end
   end
