57a9dec990a43788ffce2d2af06cd447732520fe
Sébastien Hoorens <sebastien@citizenlab.co>
2020-11-27 16:28:41 +0100

    first working version

--- engines/email_campaigns/app/mailers/email_campaigns/application_mailer.rb
+++ engines/email_campaigns/app/mailers/email_campaigns/application_mailer.rb
@@ -2,7 +2,10 @@ module EmailCampaigns
   class ApplicationMailer < ActionMailer::Base
     add_template_helper CampaignHelper
 
-    default from: ENV.fetch("DEFAULT_FROM_EMAIL", 'hello@citizenlab.co')
+    def self.sender_email
+      ENV.fetch("DEFAULT_FROM_EMAIL", 'hello@citizenlab.co')
+    end
+    default from: self.sender_email
     default reply_to: ENV.fetch("DEFAULT_FROM_EMAIL", 'hello@citizenlab.co')
     layout 'mailer'
 
@@ -18,9 +21,12 @@ module EmailCampaigns
       @command = command
 
       I18n.with_locale(@locale) do
+        sender_name = @multiloc_service.t(@tenant.settings.dig('core','organization_name'), @user)
+        recipient_name = UserDisplayNameService.new(@tenant, recipient).display_name recipient
         message = mail(
           subject: subject,
-          to: recipient.email
+          from: %("#{sender_name}" <#{self.class.sender_email}>),
+          to: %("#{recipient_name}" <#{recipient.email}>) 
         ) do |format|
           format.mjml
         end
@@ -43,5 +49,6 @@ module EmailCampaigns
     def subject
       raise NotImplementedError 
     end
+
   end
 end
