a4cc1fb411c18ff343256c831b986a36ee0a739b
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-12-02 00:25:46 +0100

    more work on project folder moderators

--- app/containers/Admin/projects/all/Lists/ModeratorProjectList.tsx
+++ app/containers/Admin/projects/all/Lists/ModeratorProjectList.tsx
@@ -10,6 +10,7 @@ import GetAdminPublications, {
   GetAdminPublicationsChildProps,
 } from 'resources/GetAdminPublications';
 import GetFeatureFlag from 'resources/GetFeatureFlag';
+import GetAuthUser, { GetAuthUserChildProps } from 'resources/GetAuthUser';
 
 // components
 import { List, Row } from 'components/admin/ResourceList';
