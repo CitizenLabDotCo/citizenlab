32341a78422af5821cdfdc017707acce5d9ae1d9
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-12-04 14:47:26 +0100

    input term added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_160903) do
+ActiveRecord::Schema.define(version: 2020_12_04_134337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -658,6 +658,7 @@ ActiveRecord::Schema.define(version: 2020_11_27_160903) do
     t.boolean "downvoting_enabled", default: true, null: false
     t.integer "ideas_count", default: 0, null: false
     t.string "ideas_order"
+    t.string "input_term", default: "idea"
     t.index ["project_id"], name: "index_phases_on_project_id"
   end
 
@@ -779,6 +780,7 @@ ActiveRecord::Schema.define(version: 2020_11_27_160903) do
     t.uuid "custom_form_id"
     t.boolean "downvoting_enabled", default: true, null: false
     t.string "ideas_order"
+    t.string "input_term", default: "idea"
     t.index ["custom_form_id"], name: "index_projects_on_custom_form_id"
     t.index ["slug"], name: "index_projects_on_slug", unique: true
   end
