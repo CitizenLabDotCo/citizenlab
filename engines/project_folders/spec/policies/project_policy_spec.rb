d61fa56fa00adfd89f77e7eaa4945172c6646001
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-12-07 11:54:32 +0100

    fixed policy spec

--- engines/project_folders/spec/policies/project_policy_spec.rb
+++ engines/project_folders/spec/policies/project_policy_spec.rb
@@ -4,11 +4,20 @@ ProjectPolicy.prepend ProjectFolders::MonkeyPatches::ProjectPolicy
 
 describe ProjectPolicy do
   subject { ProjectPolicy.new(user, project) }
-  let!(:project) { create(:project) }
 
-  context 'for an project folder moderator' do
-    let(:user) { build(:project_folder_moderator) }
+  context 'for a project contained within a folder the user moderates' do
+    let!(:project) { create(:project, admin_publication_attributes: { parent_id: project_folder.admin_publication.id }) }
+    let!(:project_folder) { create(:project_folder) }
+    let(:user) { build(:project_folder_moderator, project_folder: project_folder) }
 
     it { should permit(:create) }
+  end
+
+  context 'for a project not contained within a folder the user moderates' do
+    let!(:project) { create(:project) }
+    let!(:project_folder) { create(:project_folder) }
+    let(:user) { build(:project_folder_moderator, project_folder: project_folder) }
+
+    it { should_not permit(:create) }
   end
 end
