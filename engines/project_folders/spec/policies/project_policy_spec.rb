fde4418652786f2aafe8b1241a55bf6c9a0537cc
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-12-07 11:50:01 +0100

    attempt fixing project policy in engine

--- engines/project_folders/spec/policies/project_policy_spec.rb
+++ engines/project_folders/spec/policies/project_policy_spec.rb
@@ -1,13 +1,10 @@
 require 'rails_helper'
 
+ProjectPolicy.prepend ProjectFolders::MonkeyPatches::ProjectPolicy
+
 describe ProjectPolicy do
   subject { ProjectPolicy.new(user, project) }
-  let(:scope) { ProjectPolicy::Scope.new(user, Project) }
-  let(:inverse_scope) { ProjectPolicy::InverseScope.new(project, User) }
-
   let!(:project) { create(:project) }
-
-  ProjectPolicy.prepend ProjectFolders::MonkeyPatches::ProjectPolicy
 
   context 'for an project folder moderator' do
     let(:user) { build(:project_folder_moderator) }
