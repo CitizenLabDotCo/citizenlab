75f6e1211ec52db85b6a24ab66bf34df68430bb5
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-12-08 10:21:48 +0100

    removed unused route

--- engines/project_folders/config/routes.rb
+++ engines/project_folders/config/routes.rb
@@ -2,9 +2,7 @@ ProjectFolders::Engine.routes.draw do
   namespace :web_api, defaults: { format: :json } do
     namespace :v1 do
       resources :project_folders, controller: 'folders' do
-        resources :moderators, except: %i[update] do
-          get :users_search, on: :collection
-        end
+        resources :moderators, except: %i[update]
 
         resources :images, controller: '/web_api/v1/images', defaults: {container_type: 'ProjectFolder'}
         resources :files, controller: '/web_api/v1/files', defaults: {container_type: 'ProjectFolder'}
