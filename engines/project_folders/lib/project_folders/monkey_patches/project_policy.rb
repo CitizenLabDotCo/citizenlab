db671b3c39f3b3df544b867a7e4f97e279bd5521
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-12-08 10:37:30 +0100

    fixed policy patch spec

--- engines/project_folders/lib/project_folders/monkey_patches/project_policy.rb
+++ engines/project_folders/lib/project_folders/monkey_patches/project_policy.rb
@@ -4,7 +4,7 @@ module ProjectFolders
       def create?
         super || (
           record&.admin_publication&.parent&.publication &&
-          user.project_folder_moderator?(record.admin_publication.parent.publication)
+          user.project_folder_moderator?(record.admin_publication.parent.publication.id)
         )
       end
     end
