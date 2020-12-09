5601ab9e3b6aa3e4604403b66e570420d18f1583
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2020-12-09 17:34:47 +0100

    Trigger code on lifecycle methods
    # Conflicts:
    #	app/modules/index.ts

--- app/modules/google_analytics/index.ts
+++ app/modules/google_analytics/index.ts
@@ -6,6 +6,7 @@ import { combineLatest } from 'rxjs';
 import { currentTenantStream } from 'services/tenant';
 import { initializeFor, shutdownFor } from 'utils/analytics';
 import { isNilOrError } from 'utils/helperUtils';
+import { ModuleConfiguration } from 'utils/moduleUtils';
 
 declare module 'components/ConsentManager/destinations' {
   export interface IDestinationMap {
@@ -20,67 +21,73 @@ const destinationConfig: IDestinationConfig = {
   name: () => 'Google Analytics',
 };
 
-registerDestination(destinationConfig);
+const configuration: ModuleConfiguration = {
+  beforeMountApplication: () => {
+    // Initialize
+    combineLatest([
+      currentTenantStream().observable,
+      initializeFor('google_analytics'),
+    ]).subscribe(([tenant, _]) => {
+      if (isNilOrError(tenant)) return;
 
-// Initialize
-combineLatest([
-  currentTenantStream().observable,
-  initializeFor('google_analytics'),
-]).subscribe(([tenant, _]) => {
-  if (isNilOrError(tenant)) return;
+      const currdate: any = new Date();
+      const gaNewElem: any = {};
+      const gaElems: any = {};
 
-  const currdate: any = new Date();
-  const gaNewElem: any = {};
-  const gaElems: any = {};
+      /* tslint:disable:no-string-literal */
+      /* tslint:disable:semicolon */
+      /* tslint:disable:no-unused-expression */
+      // This code is from Google, so let's not modify it too much, just add gaNewElem and gaElems:
 
-  /* tslint:disable:no-string-literal */
-  /* tslint:disable:semicolon */
-  /* tslint:disable:no-unused-expression */
-  // This code is from Google, so let's not modify it too much, just add gaNewElem and gaElems:
-
-  (function (i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    (i[r] =
-      i[r] ||
-      function () {
-        (i[r].q = i[r].q || []).push(arguments);
+      (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        (i[r] =
+          i[r] ||
+          function () {
+            (i[r].q = i[r].q || []).push(arguments);
+            // tslint:disable-next-line: no-parameter-reassignment
+          }),
+          (i[r].l = 1 * currdate);
         // tslint:disable-next-line: no-parameter-reassignment
-      }),
-      (i[r].l = 1 * currdate);
-    // tslint:disable-next-line: no-parameter-reassignment
-    (a = s.createElement(o)),
-      // tslint:disable-next-line: no-parameter-reassignment
-      (m = s.getElementsByTagName(o)[0]);
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m);
-  })(
-    window,
-    document,
-    'script',
-    '//www.google-analytics.com/analytics.js',
-    'ga',
-    gaNewElem,
-    gaElems
-  );
-  /* tslint:enable:no-unused-expression */
-  /* tslint:enable:semicolon */
-  /* tslint:enable:no-string-literal */
+        (a = s.createElement(o)),
+          // tslint:disable-next-line: no-parameter-reassignment
+          (m = s.getElementsByTagName(o)[0]);
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m);
+      })(
+        window,
+        document,
+        'script',
+        '//www.google-analytics.com/analytics.js',
+        'ga',
+        gaNewElem,
+        gaElems
+      );
+      /* tslint:enable:no-unused-expression */
+      /* tslint:enable:semicolon */
+      /* tslint:enable:no-string-literal */
 
-  (window as any).ga(
-    'create',
-    tenant.data.attributes.settings.google_analytics?.tracking_id,
-    'auto'
-  );
-  (window as any).ga('send', 'pageview');
-});
+      (window as any).ga(
+        'create',
+        tenant.data.attributes.settings.google_analytics?.tracking_id,
+        'auto'
+      );
+      (window as any).ga('send', 'pageview');
+    });
 
-// Shutdown
-combineLatest([
-  currentTenantStream().observable,
-  shutdownFor('google_analytics'),
-]).subscribe(([tenant, _]) => {
-  window[
-    `ga-disable-${tenant.data.attributes.settings.google_analytics?.tracking_id}`
-  ];
-});
+    // Shutdown
+    combineLatest([
+      currentTenantStream().observable,
+      shutdownFor('google_analytics'),
+    ]).subscribe(([tenant, _]) => {
+      window[
+        `ga-disable-${tenant.data.attributes.settings.google_analytics?.tracking_id}`
+      ];
+    });
+
+    registerDestination(destinationConfig);
+  },
+};
+
+export default configuration;
--- app/modules/index.ts
+++ app/modules/index.ts
@@ -1,15 +1,36 @@
 import { loadModules } from 'utils/moduleUtils';
 
 import projectFoldersConfiguration from './project_folders';
-import './google_tag_manager';
-import './intercom';
-import './satismeter';
-import './google_analytics';
-import './segment';
+
+import googleTagManagerConfiguration from './google_tag_manager';
+import googleAnalyticsConfiguration from './google_analytics';
+import intercomConfiguration from './intercom';
+import satismeterConfiguration from './satismeter';
+import segmentConfiguration from './segment';
 
 export default loadModules([
   {
     configuration: projectFoldersConfiguration,
+    enabled: true,
+  },
+  {
+    configuration: googleTagManagerConfiguration,
+    enabled: true,
+  },
+  {
+    configuration: googleAnalyticsConfiguration,
+    enabled: true,
+  },
+  {
+    configuration: intercomConfiguration,
+    enabled: true,
+  },
+  {
+    configuration: satismeterConfiguration,
+    enabled: true,
+  },
+  {
+    configuration: segmentConfiguration,
     enabled: true,
   },
 ]);
