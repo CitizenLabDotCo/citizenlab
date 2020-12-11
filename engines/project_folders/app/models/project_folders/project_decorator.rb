d6325fdd912f4e1ac9ecf08aa6385c8937d86882
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-12-11 15:56:11 +0100

    simplify project decorator

--- engines/project_folders/app/models/project_folders/project_decorator.rb
+++ engines/project_folders/app/models/project_folders/project_decorator.rb
@@ -1,27 +1,6 @@
 module ProjectFolders::ProjectDecorator
-  def self.prepended(base)
-    base.class_eval do
-      after_validation :assign_moderators_from_folder, if: :folder
-      after_validation :remove_moderators_from_folder, if: :folder
-    end
-
-    def folder
-      admin_publication.parent&.publication
-    end
-  end
-
-  def assign_moderators_from_folder
-    (folder.moderators - moderators).each do |user|
-      user.add_role('project_moderator', { project_id: id })
-      user.save
-    end
-  end
-
-  def remove_moderators_from_folder
-    (moderators - folder.moderators).each do |user|
-      user.delete_role('project_moderator', { project_id: id })
-      user.save
-    end
+  def folder
+    admin_publication.parent&.publication
   end
 end
 
