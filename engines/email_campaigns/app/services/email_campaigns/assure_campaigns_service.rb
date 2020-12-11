b653906db339a07ee2037f8274851e46e7af9cff
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-12-11 17:08:09 +0100

    removed unused changes

--- engines/email_campaigns/app/services/email_campaigns/assure_campaigns_service.rb
+++ engines/email_campaigns/app/services/email_campaigns/assure_campaigns_service.rb
@@ -24,12 +24,14 @@ module EmailCampaigns
       # This makes it possible to temporarily load the records without STI errors
       EmailCampaigns::Campaign.class_eval { self.inheritance_column = :_type_disabled }
 
-      ActiveRecord::Base.transaction do
-        # these have to be deleted manually since EmailCampaigns::Campaign
-        # does not have Trackable and RecipientConfigurable included.
+      ActiveRecord::Base.transaction do	      ActiveRecord::Base.transaction do
+        # These have to be deleted manually since EmailCampaigns::Campaign	        # these have to be deleted manually since EmailCampaigns::Campaign
+        # does not have Trackable and RecipientConfigurable included.	        # does not have Trackable and RecipientConfigurable included.
+
         EmailCampaigns::CampaignGroup.where(id: unsupported_ids).destroy_all
+        EmailCampaigns::CampaignsGroup.where(campaign_id: unsupported_ids).destroy_all
         EmailCampaigns::Delivery.where(campaign_id: unsupported_ids).destroy_all
-        EmailCampaigns::Campaign.where(campaign_id: unsupported_ids).destroy_all
+        EmailCampaigns::Campaign.where(id: unsupported_ids).destroy_all
       end
 
       # Make everything back to normal
