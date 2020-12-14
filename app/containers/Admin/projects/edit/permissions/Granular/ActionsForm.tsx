865ef7593106ac9563152fd0fde066cb536d9053
brentguf <brent.guffens@gmail.com>
2020-12-14 14:48:43 +0100

    permissionAction correct inputTerm

--- app/containers/Admin/projects/edit/permissions/Granular/ActionsForm.tsx
+++ app/containers/Admin/projects/edit/permissions/Granular/ActionsForm.tsx
@@ -1,13 +1,17 @@
 import React, { memo } from 'react';
 import styled from 'styled-components';
 import { isEmpty } from 'lodash-es';
+import { isNilOrError } from 'utils/helperUtils';
 
+// services
 import {
   IPermissionData,
   IGlobalPermissionAction,
   IPCPermissionAction,
 } from 'services/actionPermissions';
+import { getInputTerm } from 'services/participationContexts';
 
+// components
 import ActionForm from './ActionForm';
 
 // i18n
@@ -17,7 +21,7 @@ import { getInputTermMessage } from 'utils/i18n';
 
 // hooks
 import useProject from 'hooks/useProject';
-import { isNilOrError } from 'utils/helperUtils';
+import usePhases from 'hooks/usePhases';
 
 const ActionPermissionWrapper = styled.div`
   margin-bottom: 30px;
@@ -51,6 +55,7 @@ type Props = PostTypeProps & SharedProps;
 const ActionsForm = memo(
   ({ permissions, postType, onChange, projectId }: Props) => {
     const project = useProject({ projectId });
+    const phases = usePhases(projectId);
     const handlePermissionChange = (permission: IPermissionData) => (
       permittedBy: IPermissionData['attributes']['permitted_by'],
       groupIds: string[]
@@ -62,14 +67,24 @@ const ActionsForm = memo(
       permissionAction: IPCPermissionAction | IGlobalPermissionAction
     ) => {
       if (postType === 'idea' && !isNilOrError(project)) {
-        const projectInputTerm = project.attributes.input_term;
+        const inputTerm = getInputTerm(
+          project.attributes.process_type === 'continuous'
+            ? 'project'
+            : 'phase',
+          project,
+          phases
+        );
 
         return {
-          posting_idea: getInputTermMessage(projectInputTerm, {
+          posting_idea: getInputTermMessage(inputTerm, {
             idea: messages.permissionAction_post_idea,
           }),
-          voting_idea: messages.permissionAction_vote_ideas,
-          commenting_idea: messages.permissionAction_comment_ideas,
+          voting_idea: getInputTermMessage(inputTerm, {
+            idea: messages.permissionAction_vote_ideas,
+          }),
+          commenting_idea: getInputTermMessage(inputTerm, {
+            idea: messages.permissionAction_comment_ideas,
+          }),
           taking_survey: messages.permissionAction_take_survey,
           taking_poll: messages.permissionAction_take_poll,
         }[permissionAction];
