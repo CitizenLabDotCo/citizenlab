1adc7790955aa2780823f7a100ff84c01c6f02f3
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-12-16 09:51:01 +0100

    fix policy

--- engines/project_folders/lib/project_folders/monkey_patches/project_policy.rb
+++ engines/project_folders/lib/project_folders/monkey_patches/project_policy.rb
@@ -8,9 +8,11 @@ module ProjectFolders
                                          .includes(:admin_publication)
                                          .pluck('admin_publications.id')
 
+            all_ids = user.moderatable_project_ids + filter_for_normal_user(normal_user_result, user)
+
             scope.includes(:admin_publication)
                  .where(admin_publications: { parent_id: folder_publication_ids })
-                 .or(scope.includes(:admin_publication).where(projects: { id: user.moderatable_project_ids }))
+                 .or(scope.includes(:admin_publication).where(projects: { id: all_ids }))
           else
             super
           end
