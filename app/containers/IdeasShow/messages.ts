bf8fe93e09c20bb58d26a3221f758747768b23df
brentguf <brent.guffens@gmail.com>
2020-12-17 08:37:19 +0100

    Vote control

--- app/containers/IdeasShow/messages.ts
+++ app/containers/IdeasShow/messages.ts
@@ -47,13 +47,13 @@ export default defineMessages({
     id: 'app.components.IdeasShow.reportAsSpam',
     defaultMessage: 'Report as spam',
   },
-  editIdea: {
-    id: 'app.components.IdeasShow.editIdea',
-    defaultMessage: 'Edit idea',
+  editPost: {
+    id: 'app.components.IdeasShow.editPost',
+    defaultMessage: 'Edit',
   },
-  deleteIdea: {
-    id: 'app.components.IdeasShow.deleteIdea',
-    defaultMessage: 'Delete idea',
+  deletePost: {
+    id: 'app.components.IdeasShow.deletePost',
+    defaultMessage: 'Delete',
   },
   deleteIdeaConfirmation: {
     id: 'app.components.IdeasShow.deleteIdeaConfirmation',
