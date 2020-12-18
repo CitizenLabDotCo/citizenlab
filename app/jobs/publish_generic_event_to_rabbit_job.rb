9d73b11ffc07c62c512be7a8af5d126b96d44458
Adrien Dessy <adrien@citizenlab.co>
2020-12-18 10:50:20 +0100

    refactoring publish_raw_event_to_rabbit_job.rb

---
+++ app/jobs/publish_generic_event_to_rabbit_job.rb
@@ -0,0 +1,34 @@
+class PublishGenericEventToRabbitJob < ApplicationJob
+  queue_as :default
+
+  def perform(event, routing_key)
+    event = add_tenant_properties(event)
+
+    with_channel do |channel|
+      exchange = channel.topic('cl2back')
+      exchange.publish(
+          event.to_json,
+          app_id: 'cl2-back',
+          content_type: 'application/json',
+          routing_key: routing_key
+      )
+    end
+  end
+
+  private
+
+  def add_tenant_properties(event)
+    tenant_properties = TrackingService.new.tenant_properties(Tenant.current)
+    event.merge(tenant_properties)
+  rescue ActiveRecord::RecordNotFound
+    # TODO Shouldn't we at least log something?
+    # Tenant can't be found, so we don't add anything
+  end
+
+  def with_channel
+    channel = BUNNY_CON.create_channel
+    yield channel
+    channel.close
+  end
+
+end
