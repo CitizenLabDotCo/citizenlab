a3096ca42265f3d074823adbeef0c944459a1923
brentguf <brent.guffens@gmail.com>
2020-12-18 10:35:48 +0100

    Posts page title/subtitle

--- app/containers/Admin/ideas/index.tsx
+++ app/containers/Admin/ideas/index.tsx
@@ -29,8 +29,8 @@ const IdeasPage = memo(
       'custom_idea_statuses'
     );
     const resource = {
-      title: formatMessage(messages.pageTitle),
-      subtitle: formatMessage(messages.pageSubtitle),
+      title: formatMessage(messages.postManagerPageTitle),
+      subtitle: formatMessage(messages.postManagerPageSubtitle),
     };
 
     const getTabs = () => {
