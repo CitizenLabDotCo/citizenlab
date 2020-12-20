80ec66b7a0e7dd2e35b197d3472f747e2cc80db9
brentguf <brent.guffens@gmail.com>
2020-12-20 20:59:32 +0100

    viewTheIdeas + postYourIdea

--- app/components/ProjectCard/messages.ts
+++ app/components/ProjectCard/messages.ts
@@ -39,9 +39,29 @@ export default defineMessages({
     id: 'app.components.ProjectCard.takeThePoll',
     defaultMessage: 'Take the poll',
   },
-  postYourIdea: {
-    id: 'app.components.ProjectCard.postYourIdea',
-    defaultMessage: 'Post your idea',
+  submitYourIdea: {
+    id: 'app.components.ProjectCard.submitYourIdea',
+    defaultMessage: 'Submit your idea',
+  },
+  submitYourOption: {
+    id: 'app.components.ProjectCard.submitYourOption',
+    defaultMessage: 'Submit your option',
+  },
+  submitYourProject: {
+    id: 'app.components.ProjectCard.submitYourProject',
+    defaultMessage: 'Submit your project',
+  },
+  submitYourQuestion: {
+    id: 'app.components.ProjectCard.submitYourQuestion',
+    defaultMessage: 'Submit your question',
+  },
+  submitYourIssue: {
+    id: 'app.components.ProjectCard.submitYourIssue',
+    defaultMessage: 'Submit your issue',
+  },
+  submitYourContribution: {
+    id: 'app.components.ProjectCard.submitYourContribution',
+    defaultMessage: 'Submit your contribution',
   },
   vote: {
     id: 'app.components.ProjectCard.vote',
@@ -54,6 +74,26 @@ export default defineMessages({
   viewTheIdeas: {
     id: 'app.components.ProjectCard.viewTheIdeas',
     defaultMessage: 'View the ideas',
+  },
+  viewTheOptions: {
+    id: 'app.components.ProjectCard.viewTheOptions',
+    defaultMessage: 'View the options',
+  },
+  viewTheProjects: {
+    id: 'app.components.ProjectCard.viewTheProjects',
+    defaultMessage: 'View the projects',
+  },
+  viewTheQuestions: {
+    id: 'app.components.ProjectCard.viewTheQuestions',
+    defaultMessage: 'View the questions',
+  },
+  viewTheIssues: {
+    id: 'app.components.ProjectCard.viewTheIssues',
+    defaultMessage: 'View the issues',
+  },
+  viewTheContributions: {
+    id: 'app.components.ProjectCard.viewTheContributions',
+    defaultMessage: 'View the contributions',
   },
   a11y_projectTitle: {
     id: 'app.components.ProjectCard.a11y_projectTitle',
