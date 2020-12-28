d426c3b101b0c1c5ce53e0da9fcbf09a6a303c12
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-12-28 17:28:00 +0100

    fix monkey patch loading

--- engines/project_folders/lib/project_folders.rb
+++ engines/project_folders/lib/project_folders.rb
@@ -1,8 +1,4 @@
 require 'project_folders/engine'
 
 module ProjectFolders
-  # Your code goes here...
-  extend ActiveSupport::Autoload
-
-  autoload :MonkeyPatches, 'project_folders/monkey_patches'
 end
