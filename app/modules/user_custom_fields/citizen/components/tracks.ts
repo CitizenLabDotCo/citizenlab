29875c73ab8baa4d8037b1b7696c027060829cd7
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-01-06 15:27:01 +0100

    Move custom fields tracking variables to own file

---
+++ app/modules/user_custom_fields/citizen/components/tracks.ts
@@ -0,0 +1,7 @@
+export default {
+  signUpCustomFieldsStepEntered: 'Sign up - custom fields step entered',
+  signUpCustomFieldsStepExited: 'Sign up - custom fields step exited',
+  signUpCustomFieldsStepCompleted: 'Sign up - custom fields step completed',
+  signUpCustomFieldsStepSkipped: 'Sign up - custom fields step skipped',
+  signUpCustomFieldsStepFailed: 'Sign up - custom fields step failed',
+};
