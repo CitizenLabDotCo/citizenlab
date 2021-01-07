f1239fe2fc0ea15ea1793c5e66c29757c384ec9a
Adrien Dessy <adrien@citizenlab.co>
2021-01-07 10:58:53 +0100

    tapping it

--- app/jobs/publish_activity_to_rabbit_job.rb
+++ app/jobs/publish_activity_to_rabbit_job.rb
@@ -15,15 +15,16 @@ class PublishActivityToRabbitJob < ApplicationJob
 
   def event_from(activity)
     service = TrackingService.new
+
     event = {
         event: service.activity_event_name(activity),
         timestamp: activity.acted_at,
         **service.activity_properties(activity),
         **service.environment_properties,
         item_content: service.activity_item_content(activity)
-    }
-    event[:user_id] = activity.user_id if activity.user_id
-    event
+    }.tap do |event|
+      event[:user_id] = activity.user_id if activity.user_id
+    end
   end
 
 end
