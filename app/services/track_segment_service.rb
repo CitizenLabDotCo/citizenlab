44f477b4601b26b5d8ae4f1e8ab9c01f8698eb3b
Adrien Dessy <adrien@citizenlab.co>
2021-01-07 13:51:55 +0100

    formatting

--- app/services/track_segment_service.rb
+++ app/services/track_segment_service.rb
@@ -27,35 +27,35 @@ class TrackSegmentService
     )
   end
 
-  def identify_tenant user, tenant
+  def identify_tenant(user, tenant)
     traits = {
-      name: tenant.name,
-      website: "https://#{tenant.host}",
-      avatar: tenant&.logo&.medium&.url,
-      createdAt: tenant.created_at,
-      tenantLocales: tenant.settings.dig('core', 'locales'),
-      **TrackingService.new.tenant_properties(tenant)
+        name: tenant.name,
+        website: "https://#{tenant.host}",
+        avatar: tenant&.logo&.medium&.url,
+        createdAt: tenant.created_at,
+        tenantLocales: tenant.settings.dig('core', 'locales'),
+        **TrackingService.new.tenant_properties(tenant)
     }
 
     Analytics && tenant && Analytics.group(
-      user_id: user.id,
-      group_id: tenant.id,
-      traits: traits,
-      integrations: integrations(user)
+        user_id: user.id,
+        group_id: tenant.id,
+        traits: traits,
+        integrations: integrations(user)
     )
   end
 
-  def track activity, tenant
+  def track(activity, tenant)
     service = TrackingService.new
     event = {
-      event: service.activity_event_name(activity),
-      timestamp: activity.acted_at,
-      properties: {
-        source: 'cl2-back',
-        **service.activity_properties(activity),
-        **service.environment_properties,
-        item_content: service.activity_item_content(activity)
-      }
+        event: service.activity_event_name(activity),
+        timestamp: activity.acted_at,
+        properties: {
+            source: 'cl2-back',
+            **service.activity_properties(activity),
+            **service.environment_properties,
+            item_content: service.activity_item_content(activity)
+        }
     }
 
     if activity.user_id
@@ -64,24 +64,24 @@ class TrackSegmentService
         event[:integrations] = integrations(activity.user)
       end
     else
-    	event[:anonymous_id] = SecureRandom.base64
+      event[:anonymous_id] = SecureRandom.base64
     end
 
     if tenant
       event[:properties] = {
-        **event[:properties],
-        **service.tenant_properties(tenant)
+          **event[:properties],
+          **service.tenant_properties(tenant)
       }
     end
 
     Analytics.track(event)
   end
 
-  def integrations user
+  def integrations(user)
     {
-      All: true,
-      Intercom: [:admin, :project_moderator].include?(user.highest_role),
-      SatisMeter: [:admin, :project_moderator].include?(user.highest_role),
+        All: true,
+        Intercom: [:admin, :project_moderator].include?(user.highest_role),
+        SatisMeter: [:admin, :project_moderator].include?(user.highest_role),
     }
   end
 end
