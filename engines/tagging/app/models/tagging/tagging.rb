8a8dbe73370de277d9507e72a21c7b0f570d00ba
Sara <sara@citizenlab.co>
2021-01-07 22:40:55 +0100

    modify tagging controller generate behaviour
    creates tags, tags ideas with pending, sends request and receives taskId

--- engines/tagging/app/models/tagging/tagging.rb
+++ engines/tagging/app/models/tagging/tagging.rb
@@ -1,9 +1,9 @@
 module Tagging
   class Tagging < ApplicationRecord
-    enum assignment_method: %i[automatic manual]
+    enum assignment_method: %i[automatic manual pending]
 
     belongs_to :idea
-    belongs_to :tag
+    belongs_to :tag, optional: true
 
     accepts_nested_attributes_for :tag
     validates_uniqueness_of :tag_id, scope: :idea_id
