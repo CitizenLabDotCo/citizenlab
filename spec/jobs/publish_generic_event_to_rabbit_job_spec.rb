3214fc315b7090d95db09e2b356624c2467d5f60
Adrien Dessy <adrien@citizenlab.co>
2021-01-11 15:29:25 +0100

    regression test for CL2-6174

---
+++ spec/jobs/publish_generic_event_to_rabbit_job_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe PublishGenericEventToRabbitJob, type: :job do
+
+  subject(:job) { PublishGenericEventToRabbitJob.new }
+
+  describe '#perform' do
+
+    let(:routing_key) { "routing_key" }
+    let(:generic_event) { {field_1: "value1", field_2: "value2"} }
+
+    it "actually publishes the event to RabbitMQ" do
+      expect(job).to receive(:publish_to_rabbitmq) do |_bunny, event, actual_routing_key|
+        expect(event).to include(generic_event)
+        expect(actual_routing_key).to eq(routing_key)
+      end
+
+      job.perform(generic_event, routing_key)
+    end
+
+    it "actually publishes the event to RabbitMQ when there is no current tenant" do
+      expect(job).to receive(:publish_to_rabbitmq) do |_bunny, event, actual_routing_key|
+        expect(event).to include(generic_event)
+        expect(actual_routing_key).to eq(routing_key)
+      end
+
+      Apartment::Tenant.switch('public') do
+        job.perform(generic_event, routing_key)
+      end
+    end
+
+  end
+end
