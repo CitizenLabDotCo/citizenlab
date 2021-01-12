ee4d978235948f8f0f2b42b900b60ca6355a4475
Sébastien Hoorens <sebastien@citizenlab.co>
2021-01-12 12:48:56 +0100

    migrated idea published email

--- engines/email_campaigns/spec/mailers/idea_published_mailer_spec.rb
+++ engines/email_campaigns/spec/mailers/idea_published_mailer_spec.rb
@@ -1,24 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe EmailCampaigns::IdeaPublishedMailer, type: :mailer do
-  describe 'Idea published' do
-    before do
-      EmailCampaigns::Campaigns::IdeaPublished.create!
-      @recipient = create(:user, locale: 'en')
-      EmailCampaigns::UnsubscriptionToken.create!(user_id: @recipient.id)
-
-      @idea = create(:idea, author: @recipient)
-    end
+  describe 'campaign_mail' do
+    let!(:recipient) { create(:user, locale: 'en') }
+    let!(:campaign) { EmailCampaigns::Campaigns::IdeaPublished.create! }
+    let(:mail) { described_class.with(command: command, campaign: campaign).campaign_mail.deliver_now }
+    
+    let!(:idea) { create(:idea, author: recipient) }
 
     let(:command) do
       {
-        recipient: @recipient,
+        recipient: recipient,
         event_payload: {
-          post_id: @idea.id,
-          post_title_multiloc: @idea.title_multiloc,
-          post_body_multiloc: @idea.body_multiloc,
-          post_url: Frontend::UrlService.new.model_to_url(@idea, locale: @recipient.locale),
-          post_images: @idea.idea_images.map do |image|
+          post_id: idea.id,
+          post_title_multiloc: idea.title_multiloc,
+          post_body_multiloc: idea.body_multiloc,
+          post_url: Frontend::UrlService.new.model_to_url(idea, locale: recipient.locale),
+          post_images: idea.idea_images.map do |image|
             {
               ordering: image.ordering,
               versions: image.image.versions.map { |k, v| [k.to_s, v.url] }.to_h
@@ -27,14 +25,18 @@ RSpec.describe EmailCampaigns::IdeaPublishedMailer, type: :mailer do
         }
       }
     end
-    let(:mail) { described_class.campaign_mail(EmailCampaigns::Campaigns::IdeaPublished.first, command).deliver_now }
+
+    before do
+      EmailCampaigns::UnsubscriptionToken.create!(user_id: recipient.id)
+    end
+
 
     it 'renders the subject' do
       expect(mail.subject).to start_with('Your idea on the platform of')
     end
 
     it 'renders the receiver email' do
-      expect(mail.to).to eq([@recipient.email])
+      expect(mail.to).to eq([recipient.email])
     end
 
     it 'renders the sender email' do
@@ -46,7 +48,7 @@ RSpec.describe EmailCampaigns::IdeaPublishedMailer, type: :mailer do
     end
 
     it 'assigns go to idea CTA' do
-      idea_url = Frontend::UrlService.new.model_to_url(@idea, locale: @recipient.locale)
+      idea_url = Frontend::UrlService.new.model_to_url(idea, locale: recipient.locale)
       expect(mail.body.encoded).to match(idea_url)
     end
   end
