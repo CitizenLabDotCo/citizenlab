a668a5d06e4d2b9deb9a67c66c552e8bfe011571
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-01-12 12:52:46 +0100

    status_change_of_commented_idea

--- engines/email_campaigns/app/mailers/email_campaigns/status_change_of_commented_idea_mailer.rb
+++ engines/email_campaigns/app/mailers/email_campaigns/status_change_of_commented_idea_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module EmailCampaigns
-  class StatusChangeOfYourIdeaMailer < ApplicationMailer
+  class StatusChangeOfCommentedIdeaMailer < ApplicationMailer
     private
 
     def subject
