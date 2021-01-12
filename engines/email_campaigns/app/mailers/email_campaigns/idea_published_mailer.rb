cd1ff9243c6feac4a9653e45edc1d90f9ec5469a
Sébastien Hoorens <sebastien@citizenlab.co>
2021-01-12 12:48:56 +0100

    migrated idea published email

--- engines/email_campaigns/app/mailers/email_campaigns/idea_published_mailer.rb
+++ engines/email_campaigns/app/mailers/email_campaigns/idea_published_mailer.rb
@@ -3,10 +3,27 @@ module EmailCampaigns
     protected
 
     def subject
-      I18n.t(
-        'email_campaigns.idea_published.subject',
-        organizationName: MultilocService.new.t(@tenant.settings.dig('core', 'organization_name'))
-      )
+      I18n.t('email_campaigns.idea_published.subject', organizationName: organization_name)
+    end
+
+    private
+
+    def header_title
+      format_message('main_header')
+    end
+
+    def header_message
+      if @tenant.name == 'Stad Leuven'
+        '<p style="margin-bottom: 20px;">
+           Bedankt om je idee te delen. We houden je verder op de hoogte van de volgende stappen binnen dit project.
+         </p>'.html_safe
+      else
+        format_message('message_next_steps', values: { firstName: recipient_first_name, organizationName: organization_name })
+      end
+    end
+
+    def preheader
+      format_message('preheader', values: { firstName: recipient_first_name, organizationName: organization_name })
     end
   end
 end
--- engines/email_campaigns/spec/mailers/previews/email_campaigns/idea_published_mailer_preview.rb
+++ engines/email_campaigns/spec/mailers/previews/email_campaigns/idea_published_mailer_preview.rb
@@ -20,7 +20,7 @@ module EmailCampaigns
       }
       campaign = EmailCampaigns::Campaigns::IdeaPublished.first
 
-      campaign.mailer_class.campaign_mail campaign, command
+      campaign.mailer_class.with(campaign: campaign, command: command).campaign_mail
     end
   end
 end
