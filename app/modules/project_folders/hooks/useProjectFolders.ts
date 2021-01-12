c73cb242b107d0d38c344875662e478e21ab9351
brentguf <brent.guffens@gmail.com>
2021-01-12 11:38:22 +0100

    useProjectFolders

--- app/modules/project_folders/hooks/useProjectFolders.ts
+++ app/modules/project_folders/hooks/useProjectFolders.ts
@@ -1,39 +1,58 @@
-import { useState, useEffect } from 'react';
-import { isNilOrError } from 'utils/helperUtils';
+import { useState, useEffect, useCallback } from 'react';
 import {
   IProjectFolderData,
   projectFoldersStream,
 } from 'modules/project_folders/services/projectFolders';
 
-interface Options {
-  filterIds?: string[];
+interface Props {
+  projectFolderIds?: string[];
+  pageNumber?: number;
+  pageSize?: number;
 }
 
-function useProjectFolders(options = <Options>{ filterIds: [] }) {
-  const [projectFolders, setProjectFolders] = useState<IProjectFolderData[]>(
-    []
+function useProjectFolders(props: Props) {
+  const [pageNumber, setPageNumber] = useState(props.pageNumber);
+  const [pageSize, setPageSize] = useState(props.pageSize);
+  const [projectFolders, setProjectFolders] = useState<
+    IProjectFolderData[] | undefined | null | Error
+  >(undefined);
+  const [projectFolderIds, setProjectFolderIds] = useState(
+    props.projectFolderIds
   );
 
+  const onPageNumberChange = useCallback((newPageNumber: number) => {
+    setPageNumber(newPageNumber);
+  }, []);
+
+  const onPageSizeChange = useCallback((newPageSize: number) => {
+    setPageSize(newPageSize);
+    setPageNumber(1);
+  }, []);
+
+  const onProjectFolderIdsChange = useCallback((projectFolderIds: string[]) => {
+    setProjectFolderIds([...projectFolderIds]);
+  }, []);
+
   useEffect(() => {
-    const { filterIds } = options;
-    let streamPayload;
-
-    if (!isNilOrError(filterIds) && filterIds?.length > 0) {
-      streamPayload = filterIds
-        ? { queryParameters: { filter_ids: filterIds } }
-        : null;
-    }
-
-    const subscription = projectFoldersStream(
-      streamPayload
-    ).observable.subscribe((streamFolders) => {
-      setProjectFolders(streamFolders.data);
+    const subscription = projectFoldersStream({
+      queryParameters: {
+        'page[number]': pageNumber || 1,
+        'page[size]': pageSize,
+        filter_ids: projectFolderIds,
+      },
+    }).observable.subscribe((response) => {
+      setProjectFolders(response.data);
     });
 
-    return () => subscription?.unsubscribe();
-  }, [options.filterIds]);
+    return () => subscription.unsubscribe();
+  }, [projectFolderIds, pageNumber, pageSize]);
 
-  return { projectFolders };
+  return {
+    projectFolders,
+    onPageNumberChange,
+    onPageSizeChange,
+    onProjectFolderIdsChange,
+  };
 }
 
 export default useProjectFolders;
