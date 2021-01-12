991e13dc0e84b58e19ec0072761683d8a159df55
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-01-12 13:40:04 +0100

    fix tests

--- engines/email_campaigns/spec/mailers/status_change_of_your_idea_mailer_spec.rb
+++ engines/email_campaigns/spec/mailers/status_change_of_your_idea_mailer_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe EmailCampaigns::StatusChangeOfYourIdeaMailer, type: :mailer do
     let!(:recipient) { create(:user, locale: 'en') }
     let!(:campaign) { EmailCampaigns::Campaigns::StatusChangeOfYourIdea.create! }
     let(:mail) { described_class.with(command: command, campaign: campaign).campaign_mail.deliver_now }
+    let(:idea) { create(:idea) }
+    let(:status) { idea.status }
 
     let(:command) do
       {
