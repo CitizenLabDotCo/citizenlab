acd82ca65e355ec063a41adc2d8e67e616fbe804
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-01-13 15:16:26 +0100

    Refactor to function component

--- app/modules/user_custom_fields/citizen/components/CustomFieldsStep.tsx
+++ app/modules/user_custom_fields/citizen/components/CustomFieldsStep.tsx
@@ -1,6 +1,5 @@
-import React, { PureComponent, FormEvent } from 'react';
+import React, { FormEvent, FC, memo, useEffect, useState } from 'react';
 import { isNilOrError } from 'utils/helperUtils';
-import { adopt } from 'react-adopt';
 import { isObject } from 'lodash-es';
 
 // components
@@ -11,12 +10,6 @@ import UserCustomFieldsForm from 'modules/user_custom_fields/citizen/components/
 
 // services
 import { completeRegistration } from 'services/users';
-
-// resources
-import GetAuthUser, { GetAuthUserChildProps } from 'resources/GetAuthUser';
-import GetUserCustomFieldsSchema, {
-  GetUserCustomFieldsSchemaChildProps,
-} from 'modules/user_custom_fields/resources/GetUserCustomFieldsSchema';
 
 // i18n
 import { InjectedIntlProps } from 'react-intl';
@@ -35,11 +28,13 @@ import tracks from './tracks';
 import styled from 'styled-components';
 
 // typings
-import { CLErrorsJSON } from 'typings';
 import {
   TSignUpSteps,
   TSignUpStepConfigurationObject,
 } from 'components/SignUpIn/SignUp';
+
+import useAuthUser from 'hooks/useAuthUser';
+import useUserCustomFieldsSchema from 'modules/user_custom_fields/hooks/useUserCustomFieldsSchema';
 
 const Loading = styled.div`
   padding-top: 15px;
@@ -95,111 +90,81 @@ type InputProps = {
   step: TSignUpSteps;
 };
 
-interface DataProps {
-  authUser: GetAuthUserChildProps;
-  userCustomFieldsSchema: GetUserCustomFieldsSchemaChildProps;
-}
+interface Props extends InputProps, InjectedIntlProps {}
 
-interface Props extends InputProps, DataProps {}
+//   componentWillMount() {
+//     trackEventByName(tracks.signUpCustomFieldsStepExited);
+//   }
 
-type State = {
-  processing: boolean;
-  unknownError: string | null;
-  apiErrors: CLErrorsJSON | null;
-};
+const CustomFieldsStep: FC<Props & InjectedIntlProps> = memo(
+  ({ onData, intl: { formatMessage }, onCompleted, step }) => {
+    const [processing, setProcessing] = useState<boolean>(false);
+    const [unknownError, setUnknownError] = useState<string | null>();
 
-class CustomFieldsStep extends PureComponent<Props & InjectedIntlProps, State> {
-  constructor(props: Props & InjectedIntlProps) {
-    super(props);
-    this.state = {
-      processing: false,
-      unknownError: null,
-      apiErrors: null,
-    };
-  }
+    const authUser = useAuthUser();
+    const userCustomFieldsSchema = useUserCustomFieldsSchema();
 
-  componentDidMount() {
-    trackEventByName(tracks.signUpCustomFieldsStepEntered);
-    const { onData } = this.props;
-    const { formatMessage } = this.props.intl;
+    useEffect(() => {
+      trackEventByName(tracks.signUpCustomFieldsStepEntered);
+    }, []);
 
-    onData({
-      key: 'custom-fields',
-      configuration: {
-        position: 4,
-        stepName: formatMessage(messages.completeYourProfile),
-        helperText: (tenant) =>
-          tenant?.attributes?.settings?.core.custom_fields_signup_helper_text,
-        // isEnabled: ({ customFieldsSchema }) => {
-        //   return (
-        //     !isNilOrError(customFieldsSchema) &&
-        //     customFieldsSchema.hasCustomFields
-        //   );
-        // },
-        isEnabled: () => true,
-        isActive: (authUser) => !authUser?.attributes.registration_completed_at,
-      },
-    });
-  }
-
-  componentWillMount() {
-    trackEventByName(tracks.signUpCustomFieldsStepExited);
-  }
-
-  handleOnSubmitButtonClick = (event: FormEvent) => {
-    event.preventDefault();
-    eventEmitter.emit('customFieldsSubmitEvent');
-  };
-
-  handleCustomFieldsFormOnSubmit = async (formData) => {
-    const { formatMessage } = this.props.intl;
-    const { authUser } = this.props;
-
-    if (authUser && isObject(formData)) {
-      try {
-        this.setState({
-          processing: true,
-          unknownError: null,
-        });
-
-        await completeRegistration(formData);
-
-        this.setState({ processing: false });
-        trackEventByName(tracks.signUpCustomFieldsStepCompleted);
-        this.props.onCompleted();
-      } catch (error) {
-        trackEventByName(tracks.signUpCustomFieldsStepFailed, { error });
-        this.setState({
-          processing: false,
-          unknownError: formatMessage(messages.unknownError),
+    useEffect(() => {
+      if (!isNilOrError(userCustomFieldsSchema)) {
+        onData({
+          key: 'custom-fields',
+          configuration: {
+            position: 4,
+            stepName: formatMessage(messages.completeYourProfile),
+            helperText: (tenant) =>
+              tenant?.attributes?.settings?.core
+                .custom_fields_signup_helper_text,
+            isEnabled: () => userCustomFieldsSchema.hasCustomFields,
+            isActive: (authUser) =>
+              !authUser?.attributes.registration_completed_at,
+          },
         });
       }
-    }
-  };
+    }, [userCustomFieldsSchema]);
 
-  skipStep = async (event: FormEvent) => {
-    event.preventDefault();
+    const handleOnSubmitButtonClick = (event: FormEvent) => {
+      event.preventDefault();
+      eventEmitter.emit('customFieldsSubmitEvent');
+    };
 
-    const { authUser } = this.props;
+    const handleCustomFieldsFormOnSubmit = async (formData) => {
+      if (!isNilOrError(authUser) && isObject(formData)) {
+        try {
+          setProcessing(true);
+          setUnknownError(null);
 
-    trackEventByName(tracks.signUpCustomFieldsStepSkipped);
+          await completeRegistration(formData);
 
-    if (!isNilOrError(authUser)) {
-      await completeRegistration({});
-    }
+          setProcessing(false);
+          trackEventByName(tracks.signUpCustomFieldsStepCompleted);
+          onCompleted();
+        } catch (error) {
+          trackEventByName(tracks.signUpCustomFieldsStepFailed, { error });
+          setProcessing(false);
+          setUnknownError(formatMessage(messages.unknownError));
+        }
+      }
+    };
 
-    this.props.onCompleted();
-  };
+    const skipStep = async (event: FormEvent) => {
+      event.preventDefault();
 
-  render() {
-    const { step } = this.props;
+      trackEventByName(tracks.signUpCustomFieldsStepSkipped);
+
+      if (!isNilOrError(authUser)) {
+        await completeRegistration({});
+      }
+
+      onCompleted();
+    };
+
     if (step !== 'custom-fields') {
       return null;
     }
-
-    const { formatMessage } = this.props.intl;
-    const { processing, unknownError } = this.state;
-    const { authUser, userCustomFieldsSchema } = this.props;
 
     if (isNilOrError(authUser) || isNilOrError(userCustomFieldsSchema)) {
       return (
@@ -213,8 +178,8 @@ class CustomFieldsStep extends PureComponent<Props & InjectedIntlProps, State> {
       return (
         <Container id="e2e-signup-custom-fields-container">
           <UserCustomFieldsForm
-            formData={authUser.attributes.custom_field_values}
-            onSubmit={this.handleCustomFieldsFormOnSubmit}
+            formData={authUser.data.attributes.custom_field_values}
+            onSubmit={handleCustomFieldsFormOnSubmit}
           />
 
           <ButtonWrapper>
@@ -222,12 +187,12 @@ class CustomFieldsStep extends PureComponent<Props & InjectedIntlProps, State> {
               id="e2e-signup-custom-fields-submit-btn"
               processing={processing}
               text={formatMessage(messages.completeSignUp)}
-              onClick={this.handleOnSubmitButtonClick}
+              onClick={handleOnSubmitButtonClick}
             />
             {!userCustomFieldsSchema.hasRequiredFields && (
               <SkipButton
                 className="e2e-signup-custom-fields-skip-btn"
-                onClick={this.skipStep}
+                onClick={skipStep}
               >
                 {formatMessage(messages.skip)}
               </SkipButton>
@@ -240,17 +205,8 @@ class CustomFieldsStep extends PureComponent<Props & InjectedIntlProps, State> {
 
     return null;
   }
-}
+);
 
 const CustomFieldsWithHoC = injectIntl<Props>(CustomFieldsStep);
 
-const Data = adopt<DataProps, InputProps>({
-  authUser: <GetAuthUser />,
-  userCustomFieldsSchema: <GetUserCustomFieldsSchema />,
-});
-
-export default (inputProps: InputProps) => (
-  <Data {...inputProps}>
-    {(dataprops) => <CustomFieldsWithHoC {...inputProps} {...dataprops} />}
-  </Data>
-);
+export default CustomFieldsWithHoC;
