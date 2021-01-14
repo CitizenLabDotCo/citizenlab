443aa8af87c152e861d1e4dc66d381556ad85e52
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-01-14 10:45:43 +0100

    subject change

--- engines/email_campaigns/app/mailers/email_campaigns/comment_on_your_idea_mailer.rb
+++ engines/email_campaigns/app/mailers/email_campaigns/comment_on_your_idea_mailer.rb
@@ -3,19 +3,22 @@ module EmailCampaigns
     protected
 
     def subject
-      I18n.t('email_campaigns.comment_on_your_idea.subject', organizationName: organization_name)
+      format_message('subject', values: { organizationName: organization_name })
     end
 
     def header_title
-      format_message('main_header', values: {authorName: event.comment_author_name})
+      format_message('main_header', values: { authorName: event.comment_author_name })
     end
 
     def header_message
-      format_message('event_description', values: {
-        authorNameFull: event.comment_author_name,
-        authorName: event.initiating_user_first_name,
-        ideaTitle: localize_for_recipient(event.post_title_multiloc)
-      })
+      format_message(
+        'event_description',
+        values: {
+          authorNameFull: event.comment_author_name,
+          authorName: event.initiating_user_first_name,
+          ideaTitle: localize_for_recipient(event.post_title_multiloc)
+        }
+      )
     end
 
     def preheader
