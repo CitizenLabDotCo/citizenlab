69ca4ad184b05bdebbbadfe5df5fe2a8e3895da2
Sara <sara@citizenlab.co>
2021-01-14 13:02:57 +0100

    Refactors message receiver

--- engines/tagging/app/services/tagging/automatic_tagging_service.rb
+++ engines/tagging/app/services/tagging/automatic_tagging_service.rb
@@ -1,38 +1,42 @@
 module Tagging
   class AutomaticTaggingService
-    def add_tags(body)
-      switch_tenant(body) do
-        save_tagging body["result"]["data"]["final_predictions"] if body["status"] == 'SUCCESS'
+    def save_tags_from_prediction(body)
+      if body['status'] == 'SUCCESS'
+        switch_tenant(body) do
+          prediction = body['result']['data']['final_predictions']
+          parse_prediction prediction do |tag_id, document_id, confidence_score|
+            create_tag tag_id, document_id, confidence_score
+          end
+          Tagging.pending.where(idea_id: prediction&.map { |document| document['id'] })&.delete_all
+          TagService.new.remove_unused_tags unless processing?
+        end
       end
     end
 
     def cancel_tasks
-      if Tagging.pending&.map { |e| e.task_id }.uniq.map { |task_id|
-          r = NLP::TasksService.new.cancel(task_id)
-          # for now we'll be very optimistic and just act as if cancelling always work.
-          # Tagging.pending.where(task_id: task_id).destroy_all if r== 200
-          r
-        }.all? { |r|
-          r == 200
-        }
-        Tagging.pending.destroy_all
+      Tagging.pending.destroy_all
+      if Tagging.pending&.map { |e| e.task_id }.uniq.map do |task_id|
+           NLP::TasksService.new.cancel(task_id)
+           # for now we'll be very optimistic and just act as if cancelling always work.
+           # Tagging.pending.where(task_id: task_id).destroy_all if r== 200
+         end.all? do |r|
+           r == 200
+         end
         TagService.new.remove_unused_tags
         200
       else
-        Tagging.pending.destroy_all
         500
       end
     end
 
     def processing?
-      Tagging.pending.length > 0
+      !Tagging.pending.empty?
     end
-
 
     private
 
-    def switch_tenant body
-      tenant_id = body["result"]["data"]["tenant_id"]
+    def switch_tenant(body)
+      tenant_id = body['result']['data']['tenant_id']
       if tenant_id
         Apartment::Tenant.switch(Tenant.find(tenant_id).schema_name) do
           yield
@@ -42,34 +46,21 @@ module Tagging
       end
     end
 
-    def save_tagging(suggestion)
+    def parse_prediction(suggestion)
       suggestion&.each do |document|
-        Tagging.pending.find_by(idea_id: document["id"])&.delete
-
-
-        document["predicted_labels"].each do |label|
-          Tagging.create(
-            tag_id: label["id"],
-            idea_id: document["id"],
-            assignment_method: :automatic,
-            confidence_score: label["confidence"]
-          )
+        document['predicted_labels'].each do |label|
+          yield label['id'], document['id'], label['confidence']
         end
       end
+    end
 
-      p '/n'
-      p '/n'
-      p '/n'
-      p '/n'
-      p '/n'
-      p '/n'
-      p 'processing?'
-      p processing?
-      p '/n'
-      p '/n'
-      p '/n'
-
-      TagService.new.remove_unused_tags unless processing?
+    def create_tag(tag_id, document_id, confidence_score)
+      Tagging.create(
+        tag_id: tag_id,
+        idea_id: document_id,
+        assignment_method: :automatic,
+        confidence_score: confidence_score
+      )
     end
   end
 end
