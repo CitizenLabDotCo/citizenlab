680c5fb931b5c5e03a593ac4c4ffd8087102a831
brentguf <brent.guffens@gmail.com>
2021-01-15 16:18:02 +0100

    isProjectFolderModerator types

--- app/modules/project_folders/permissions/roles.ts
+++ app/modules/project_folders/permissions/roles.ts
@@ -1,5 +1,5 @@
 import { IProjectFolderData } from 'modules/project_folders/services/projectFolders';
-import { IUserData } from 'services/users';
+import { IUserData, IRole } from 'services/users';
 
 export type IProjectFolderModerator = {
   type: 'project_folder_moderator';
@@ -13,15 +13,15 @@ export const isProjectFolderModerator = (
   return !!(
     user &&
     user.attributes?.roles &&
-    user.attributes?.roles?.find((role) => {
+    user.attributes?.roles?.find((role: IRole | IProjectFolderModerator) => {
       if (projectFolderId) {
         return (
-          role.project_folder_id === projectFolderId &&
-          role.type === 'project_folder_moderator'
+          role.type === 'project_folder_moderator' &&
+          role.project_folder_id === projectFolderId
         );
       }
 
-      return r.type === 'project_folder_moderator';
+      return role.type === 'project_folder_moderator';
     })
   );
 };
