1ed9d77a5b20ffb65345935a3247310c613e9cc6
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-01-19 15:23:24 +0100

    initial commit

--- Gemfile
+++ Gemfile
@@ -65,7 +65,7 @@ gem "active_model_serializers", "~> 0.10.8"
 # See https://github.com/nsarno/knock/issues/250
 # Installs v2.2 which is not available on rubygems.org
 gem 'knock', git: 'https://github.com/nsarno/knock', branch: 'master', ref: '9214cd027422df8dc31eb67c60032fbbf8fc100b'
-gem "sidekiq", "~> 6.1"
+gem 'que'
 
 gem 'activerecord-postgis-adapter', '~> 6.0.0'
 gem "activerecord-import", '~> 1.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_170635) do
+ActiveRecord::Schema.define(version: 2021_01_19_144531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -815,6 +815,17 @@ ActiveRecord::Schema.define(version: 2020_12_17_170635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["tenant_id"], name: "index_public_api_api_clients_on_tenant_id"
+  end
+
+  create_table "que_jobs", primary_key: ["queue", "priority", "run_at", "job_id"], comment: "3", force: :cascade do |t|
+    t.integer "priority", limit: 2, default: 100, null: false
+    t.datetime "run_at", default: -> { "now()" }, null: false
+    t.bigserial "job_id", null: false
+    t.text "job_class", null: false
+    t.json "args", default: [], null: false
+    t.integer "error_count", default: 0, null: false
+    t.text "last_error"
+    t.text "queue", default: "", null: false
   end
 
   create_table "spam_reports", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
