feada00c3d375008180fa57eb81a4d9c1580fe35
brentguf <brent.guffens@gmail.com>
2021-01-19 11:09:32 +0100

    Declare module for IProjectFolderModeratorRole

--- app/modules/project_folders/permissions/roles.ts
+++ app/modules/project_folders/permissions/roles.ts
@@ -1,10 +1,12 @@
 import { IUserData, IRole } from 'services/users';
 import { isAdmin } from 'services/permissions/roles';
 
-export type IProjectFolderModerator = {
-  type: 'project_folder_moderator';
-  project_folder_id: string;
-};
+declare module 'services/users' {
+  type IProjectFolderModeratorRole = {
+    type: 'project_folder_moderator';
+    project_folder_id: string;
+  };
+}
 
 export function isProjectFolderModerator(
   user: IUserData,
@@ -13,24 +15,21 @@ export function isProjectFolderModerator(
   if (isAdmin({ data: user })) {
     return true;
   } else {
-    return !!user.attributes?.roles?.find(
-      (role: IRole | IProjectFolderModerator) => {
-        if (projectFolderId) {
-          return (
-            role.type === 'project_folder_moderator' &&
-            role.project_folder_id === projectFolderId
-          );
-        }
-
-        return role.type === 'project_folder_moderator';
+    return !!user.attributes?.roles?.find((role: IRole) => {
+      if (projectFolderId) {
+        return (
+          role.type === 'project_folder_moderator' &&
+          role.project_folder_id === projectFolderId
+        );
       }
-    );
+
+      return role.type === 'project_folder_moderator';
+    });
   }
 }
 
 export function hasProjectFolderModeratorRole(user: IUserData) {
   return !!user.attributes.roles?.find(
-    (role: IRole | IProjectFolderModerator) =>
-      role.type === 'project_folder_moderator'
+    (role: IRole) => role.type === 'project_folder_moderator'
   );
 }
