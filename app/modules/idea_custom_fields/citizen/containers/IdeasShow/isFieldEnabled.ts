5ccf65b2806e96aeafe561cb256493c3d733a9a9
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-01-20 14:47:06 +0100

    Move isFieldEnabled

--- app/modules/idea_custom_fields/citizen/containers/IdeasShow/isFieldEnabled.ts
+++ app/modules/idea_custom_fields/citizen/containers/IdeasShow/isFieldEnabled.ts
@@ -1,7 +1,7 @@
 import {
   IIdeaCustomFieldsSchemas,
   CustomFieldCodes,
-} from 'services/ideaCustomFields';
+} from 'modules/idea_custom_fields/services/ideaCustomFields';
 import { Locale } from 'typings';
 
 export default function isFieldEnabled(
