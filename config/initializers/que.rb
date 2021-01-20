d75982e5fe28446b8f707a49158efc2d263a3f4d
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-01-20 11:45:08 +0100

    removed useless comments

--- config/initializers/que.rb
+++ config/initializers/que.rb
@@ -1,10 +1,1 @@
-# require 'que/connection'
 require 'apartment_que'
-
-# module QueConnectionPatch
-#   def execute(command, params = {})
-#     byebug
-#   end
-# end
-
-# ActiveJob::QueueAdapters::QueAdapter.prepend(QueConnectionPatch)
