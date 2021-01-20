62c3870886af4154b4ff8dc574390a74b6a764b4
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-01-20 11:52:17 +0100

    added comments to reference patched files

--- lib/apartment_que/monkey_patches/active_job/base.rb
+++ lib/apartment_que/monkey_patches/active_job/base.rb
@@ -1,5 +1,6 @@
 module ApartmentQue
   module MonkeyPatches
+    # patching https://github.com/rails/rails/blob/1eeb26c8527beffc8207af819b5432aa91341595/activejob/lib/active_job/base.rb
     module ActiveJob::Base
       def self.prepended(base)
         base.class_eval do
--- lib/apartment_que/monkey_patches/active_job/queue_adapters/que_adapter.rb
+++ lib/apartment_que/monkey_patches/active_job/queue_adapters/que_adapter.rb
@@ -1,5 +1,6 @@
 module ApartmentQue
   module MonkeyPatches
+    # patching https://github.com/rails/rails/blob/1eeb26c8527beffc8207af819b5432aa91341595/activejob/lib/active_job/queue_adapters/que_adapter.rb
     module ActiveJob::QueueAdapters::QueAdapter
       module JobWrapper
         def run(job_data)
