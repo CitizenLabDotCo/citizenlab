9526b813197905bceaf2c4ad583351c90bbc5790
Adrien Dessy <adrien@citizenlab.co>
2021-01-21 22:07:36 +0100

    simplified: actually we cannot modify the host through the AppConfigurationController.
    we are also logging duplicate activities for the app configuration and tenant for now. to be further simplified once subscribers to those events have been refactored.

--- app/services/side_fx_app_configuration_service.rb
+++ app/services/side_fx_app_configuration_service.rb
@@ -5,17 +5,11 @@ class SideFxAppConfigurationService
   def before_update(app_config, current_user); end
 
   def after_update(app_config, current_user)
-    update_time = app_config.updated_at.to_i
-    LogActivityJob.perform_later(app_config, 'changed', current_user, update_time)
-
-    if app_config.host_previously_changed?
-      payload = {changes: app_config.host_previous_change}
-      LogActivityJob.perform_later(app_config, 'changed_host', current_user, update_time, payload: payload)
-    end
+    log_activity(app_config, 'changed', current_user)
 
     if ( lifecycle_change = get_lifecycle_change(app_config) )
       payload = {changes: lifecycle_change}
-      LogActivityJob.perform_later(app_config, 'changed_lifecycle_stage', current_user, update_time, payload: payload)
+      log_activity(app_config, 'changed_lifecycle_stage', current_user, payload)
     end
 
     update_group_by_identify
@@ -39,4 +33,16 @@ class SideFxAppConfigurationService
     diff[0] != diff[1] ? diff : nil
   end
 
+  # @param [AppConfiguration] app_config
+  # @param [String] action
+  # @param [User] user
+  # @param [Hash,nil] payload
+  def log_activity(app_config, action, user, payload=nil)
+    tenant = Tenant.find_by(host: app_config.host)
+    update_time = app_config.updated_at.to_i
+    options = {payload: payload}.compact
+
+    LogActivityJob.perform_later(app_config, action, user, update_time, options)
+    LogActivityJob.perform_later(tenant, action, user, update_time, options) # MT_TODO To be removed once event subscribers have benn adapted.
+  end
 end
--- spec/services/side_fx_app_configuration_spec.rb
+++ spec/services/side_fx_app_configuration_spec.rb
@@ -3,43 +3,36 @@ require "rails_helper"
 describe 'SideFxAppConfigurationService' do
   let(:service) { SideFxAppConfigurationService.new }
   let(:current_user) { create(:user) }
+  let(:tenant) { Tenant.current }
+  let(:config) { AppConfiguration.instance }
 
   it "has an #before_update method" do
+    # only test for the presence of the method bc it does nothing for now.
     service.before_update(AppConfiguration.instance, current_user)
   end
 
   describe "after_update" do
     it "logs a 'changed' action job when the configuration has changed" do
-      config = AppConfiguration.instance
       settings = config.settings
       settings['core']['organization_name'] = {'en' => "New name"}
       config.update!(settings: settings)
 
       expect { service.after_update(config, current_user) }
-        .to have_enqueued_job(LogActivityJob)
-        .with(config, 'changed', current_user, config.updated_at.to_i)
-    end
-
-    it "logs a 'changed_host' action job when the configuration host has changed" do
-      config = AppConfiguration.instance
-      old_host = config.host
-      config.update!(host: 'some-domain.net')
-
-      expect {service.after_update(config, current_user)}
-        .to have_enqueued_job(LogActivityJob)
-        .with(config, 'changed_host', current_user, config.updated_at.to_i, payload: {changes: [old_host, "some-domain.net"]})
+        .to  have_enqueued_job(LogActivityJob).with(config, 'changed', current_user, config.updated_at.to_i, {})
+        .and have_enqueued_job(LogActivityJob).with(tenant, 'changed', current_user, config.updated_at.to_i, {})
     end
 
     it "logs a 'changed_lifecycle_stage' action job when the lifecycle has changed" do
-      config = AppConfiguration.instance
       settings = config.settings
       old_lifecycle_stage = settings.dig('core', 'lifecycle_stage')
       settings['core']['lifecycle_stage'] = "churned"
       config.update!(settings: settings)
+      options = { payload: { changes: [old_lifecycle_stage, "churned"] } }
 
+      updated_at = config.updated_at.to_i
       expect {service.after_update(config, current_user)}
-        .to have_enqueued_job(LogActivityJob)
-        .with(config, 'changed_lifecycle_stage', current_user, config.updated_at.to_i, payload: {changes: [old_lifecycle_stage, "churned"]})
+        .to  have_enqueued_job(LogActivityJob).with(config, 'changed_lifecycle_stage', current_user, updated_at, options)
+        .and have_enqueued_job(LogActivityJob).with(tenant, 'changed_lifecycle_stage', current_user, updated_at, options)
     end
   end
 end
