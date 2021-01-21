4368c947238a2245b4f286836f71fcb1395ce32a
Sara <sara@citizenlab.co>
2021-01-21 18:42:56 +0100

    Small css fixes, quick and direty ordering

--- app/containers/Admin/processing/TagWrapper.tsx
+++ app/containers/Admin/processing/TagWrapper.tsx
@@ -5,12 +5,19 @@ import { Spinner, Tag } from 'cl2-component-library';
 
 import useLocalize from 'hooks/useLocalize';
 import useTag from 'hooks/useTag';
+import styled from 'styled-components';
 
 type TagProps = React.ComponentProps<typeof Tag>;
 
 interface Props extends Omit<TagProps, 'text'> {
   tagId: string | null;
 }
+
+const StyledSpinner = styled(Spinner)`
+  display: inline-flex;
+  width: auto;
+  margin: 2px;
+`;
 
 export default memo<Props>(({ tagId, ...tagProps }) => {
   const localize = useLocalize();
@@ -27,7 +34,7 @@ export default memo<Props>(({ tagId, ...tagProps }) => {
       );
     }
   } else {
-    return <Spinner color="#666" size="20px" />;
+    return <StyledSpinner color="#666" size="20px" />;
   }
 
   return null;
