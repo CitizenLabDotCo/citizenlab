1b5ef0e7539b264040211f4a04a9641035324bb0
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-01-25 16:21:08 +0100

    remove freeze step from que

--- lib/active_job_que_extension.rb
+++ lib/active_job_que_extension.rb
@@ -12,4 +12,16 @@ module ActiveJobQueExtension
       end
     end
   end
+
+  # Removing the freeze step from
+  # https://github.com/que-rb/que/blob/77c6b92952b821898c393239ce0e4047b17d7dae/lib/que/active_job/extensions.rb#L14
+  def perform(*args)
+    Que.internal_log(:active_job_perform, self) { { args: args } }
+
+    _run(
+      args: que_filter_args(
+        args.map { |a| a.is_a?(Hash) ? a.deep_symbolize_keys : a }
+      )
+    )
+  end
 end
