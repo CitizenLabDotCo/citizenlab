8fd9a12fe8015cb6a94da749e9448f8c4d4ae56e
Sara <sara@citizenlab.co>
2021-01-25 17:39:45 +0100

    Fixes to tests after changing service apis

--- engines/nlp/spec/acceptance/tagging_suggestion_service_spec.rb
+++ engines/nlp/spec/acceptance/tagging_suggestion_service_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
 describe NLP::TaggingSuggestionService do
-  let(:service) { NLP::TaggingSuggestionService.new }
+  let(:api) { NLP::API.new ENV.fetch('CL2_NLP_HOST') }
+  let(:service) { NLP::TaggingSuggestionService.new(api) }
 
   before do
     Tagging::Tag.create(title_multiloc: {'en' => 'label', 'fr-BE' => 'label'})
@@ -10,14 +11,13 @@ describe NLP::TaggingSuggestionService do
     @tags = Tagging::Tag.all()
     @ideas = create_list(:idea, 2, title_multiloc: {'en' => 'I\'m an idea.'}, body_multiloc: {'en' => 'But I\'m not ideal.'}).push(create(:idea, title_multiloc: {'en' => 'I\'m an idea.', 'fr-BE': 'Je suis une idée.'}, body_multiloc: {'en' => 'But I\'m not ideal.', 'fr-BE' => 'Mais je ne suis pas idéale.'}))
     @tenant_id = Tenant.current.id
-    @api ||= NLP::API.new ENV.fetch('CL2_NLP_HOST')
   end
 
   describe "suggest" do
     it "parses args and pass down" do
-      allow(@api).to receive(:zeroshot_classification).and_return(nil)
-      service.suggest(@ideas, @tags, 'en', @api)
-      expect(@api).to have_received(:zeroshot_classification).with(
+      allow(api).to receive(:zeroshot_classification).and_return(nil)
+      service.suggest(@ideas, @tags, 'en')
+      expect(api).to have_received(:zeroshot_classification).with(
         {:candidate_labels=>
           [{:label_id=> @tags[0].id, :text=>"label"},
           {:label_id=> @tags[1].id, :text=>"item"}],
@@ -34,9 +34,9 @@ describe NLP::TaggingSuggestionService do
       )
     end
     it "parses args and pass down" do
-      allow(@api).to receive(:zeroshot_classification).and_return(nil)
-      service.suggest(@ideas, @tags, 'fr-BE', @api)
-      expect(@api).to have_received(:zeroshot_classification).with(
+      allow(api).to receive(:zeroshot_classification).and_return(nil)
+      service.suggest(@ideas, @tags, 'fr-BE')
+      expect(api).to have_received(:zeroshot_classification).with(
         {:candidate_labels=>
           [{:label_id=> @tags[0].id, :text=>"label"}],
          :documents=>
