50550b2875c869a4d1ac97dacfdc026e27430f28
brentguf <brent.guffens@gmail.com>
2021-01-28 13:10:24 +0100

    More replacement of QuillMultiloc by QuillMultilocWithLocaleSwitcher

--- app/containers/Admin/projects/edit/timeline/edit.tsx
+++ app/containers/Admin/projects/edit/timeline/edit.tsx
@@ -31,7 +31,7 @@ import { convertUrlToUploadFileObservable } from 'utils/fileTools';
 // Components
 import { Label } from 'cl2-component-library';
 import InputMultiloc from 'components/UI/InputMultiloc';
-import QuillMultiloc from 'components/UI/QuillEditor/QuillMultiloc';
+import QuillMultilocWithLocaleSwitcher from 'components/UI/QuillEditor/QuillMultilocWithLocaleSwitcher';
 import Error from 'components/UI/Error';
 import DateRangePicker from 'components/admin/DateRangePicker';
 import SubmitWrapper from 'components/admin/SubmitWrapper';
@@ -465,7 +465,7 @@ class AdminProjectTimelineEdit extends PureComponent<
               </SectionField>
 
               <SectionField className="fullWidth">
-                <QuillMultiloc
+                <QuillMultilocWithLocaleSwitcher
                   id="description"
                   label={this.quillMultilocLabel}
                   valueMultiloc={phaseAttrs.description_multiloc}
