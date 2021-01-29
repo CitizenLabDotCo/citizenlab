2337c9f17a98c79baf013f1ef719cca0f26e3e5c
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-01-29 10:59:13 +0100

    Add Outlet

---
+++ app/modules/granular_permissions/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ModuleConfiguration } from 'utils/moduleUtils';
+
+const configuration: ModuleConfiguration = {
+  outlets: {
+    'app.containers.Admin.project.edit.permissions': () => <div>hello</div>,
+  },
+};
+
+export default configuration;
