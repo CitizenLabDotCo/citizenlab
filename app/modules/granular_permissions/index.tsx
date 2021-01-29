0c56fb26578b69e51b6794d9c4c4cd43d3078070
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-01-29 11:19:28 +0100

    Wrap with feature flag

--- app/modules/granular_permissions/index.tsx
+++ app/modules/granular_permissions/index.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import useFeatureFlag from 'hooks/useFeatureFlag';
+import React, { ReactNode } from 'react';
 import { ModuleConfiguration } from 'utils/moduleUtils';
 import Granular from './admin/containers/Granular';
+
+type RenderOnFeatureFlagProps = {
+  children: ReactNode;
+};
+
+const RenderOnFeatureFlag = ({ children }: RenderOnFeatureFlagProps) => {
+  const isGranularPermissionsEnabled = useFeatureFlag('granular_permissions');
+  if (isGranularPermissionsEnabled) {
+    return <>{children}</>;
+  }
+  return null;
+};
 
 const configuration: ModuleConfiguration = {
   outlets: {
     'app.containers.Admin.project.edit.permissions': (props) => (
-      <Granular {...props} />
+      <RenderOnFeatureFlag>
+        <Granular {...props} />
+      </RenderOnFeatureFlag>
     ),
   },
 };
