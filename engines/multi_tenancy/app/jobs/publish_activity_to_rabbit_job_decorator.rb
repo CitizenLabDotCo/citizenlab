a4e02fe9a7f13d1c48652d475948c0e3686a6e9b
Adrien Dessy <adrien@citizenlab.co>
2021-02-02 10:15:53 +0100

    Patch #event_from(activity) to inject extra properties from the multi-tenancy engine

---
+++ engines/multi_tenancy/app/jobs/publish_activity_to_rabbit_job_decorator.rb
@@ -0,0 +1,10 @@
+module MultiTenancy
+  module PublishActivityToRabbitJobDecorator
+    def event_from(activity)
+      event = super
+      event.merge(TrackingService.new.environment_properties)
+    end
+  end
+end
+
+::PublishActivityToRabbitJob.prepend(MultiTenancy::PublishActivityToRabbitJobDecorator)
