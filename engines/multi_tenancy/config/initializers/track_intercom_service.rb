a3c11b27951e457eb23a866527a7acbdfa9b8c3c
Adrien Dessy <adrien@citizenlab.co>
2021-02-02 13:49:27 +0100

    Fix missing namespacing

--- engines/multi_tenancy/config/initializers/track_intercom_service.rb
+++ engines/multi_tenancy/config/initializers/track_intercom_service.rb
@@ -1,19 +1,19 @@
-
+require 'pry' ; binding.pry
 TrackIntercomService.setup do |config|
 
   config.user_attributes_builder = lambda do |user|
     TrackIntercomService::Helpers
       .default_user_attributes(user)
-      .merge(TrackingTenantService.new.tenant_properties)
+      .merge(MultiTenancy::TrackingTenantService.new.tenant_properties)
   end
 
   config.activity_attributes_builder = lambda do |activity|
-    tracking_service = TrackingTenantService.new
+    tracking_service = MultiTenancy::TrackingTenantService.new
     TrackIntercomService::Helpers
       .default_activity_attributes(activity)
       .merge(tracking_service.tenant_properties)
       .merge(tracking_service.environment_properties)
   end
 
-  config.tenant_attributes_builder = TrackingTenantService.new.method(:tenant_properties)
+  config.tenant_attributes_builder = MultiTenancy::TrackingTenantService.new.method(:tenant_properties)
 end
