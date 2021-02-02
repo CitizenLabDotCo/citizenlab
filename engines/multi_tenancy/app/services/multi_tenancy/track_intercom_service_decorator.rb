a0f1dcdb37db52901f8c903257a01efb191ca473
Adrien Dessy <adrien@citizenlab.co>
2021-02-02 15:00:27 +0100

    Fix: add company to contact only when tracking is enabled.

--- engines/multi_tenancy/app/services/multi_tenancy/track_intercom_service_decorator.rb
+++ engines/multi_tenancy/app/services/multi_tenancy/track_intercom_service_decorator.rb
@@ -3,8 +3,8 @@ module MultiTenancy
     # Patches +identify_user+ to link the user with an intercom company
     # (tenant) if possible.
     def identify_user(user)
-      contact = super
-      add_company_to_contact(contact, Tenant.current)
+      contact = super # returns nil if the tracking is off (in general or for that user in particular).
+      add_company_to_contact(contact, Tenant.current) if contact
     rescue Intercom::ResourceNotFound
       # Ignored: we don't add the company when there is not current tenant.
     end
