0c425554078d84d39c26504acf7a6c5543ddf5be
brentguf <brent.guffens@gmail.com>
2021-02-02 14:02:44 +0100

    Props for wrapper component

--- app/components/UI/PasswordInput/index.tsx
+++ app/components/UI/PasswordInput/index.tsx
@@ -1,38 +1,5 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { Input, Button, Icon } from 'cl2-component-library';
-import useLocale from 'hooks/useLocale';
-import { isNilOrError } from 'utils/helperUtils';
-import { ScreenReaderOnly } from 'utils/a11y';
-// i18n
-import { injectIntl } from 'utils/cl-intl';
-import { InjectedIntlProps } from 'react-intl';
-import messages from './messages';
-
-const Container = styled.div`
-  position: relative;
-  display: flex;
-`;
-
-const StyledInput = styled(Input)``;
-
-const EyeIcon = styled(Icon)`
-  width: 22px;
-  height: 15px;
-`;
-
-const EyeClosedIcon = styled(Icon)`
-  width: 22px;
-  height: 19px;
-  margin-bottom: -1px;
-`;
-
-const ShowPasswordButton = styled(Button)`
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  right: 0;
-`;
+import React from 'react';
+import PasswordInput from './PasswordInput';
 
 interface Props {
   id: string;
@@ -45,73 +12,28 @@ interface Props {
   placeholder?: string;
 }
 
-const PasswordInput = ({
+const Component = ({
   id,
   value,
   error,
-  autocomplete,
-  placeholder,
   onChange,
   onBlur,
+  autocomplete,
+  placeholder,
   setRef,
-  intl: { formatMessage },
-}: Props & InjectedIntlProps) => {
-  const locale = useLocale();
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleOnChange = (password: string) => {
-    onChange(password);
-  };
-  const handleOnBlur = () => {
-    if (onBlur) {
-      onBlur();
-    }
-  };
-  const handleOnClick = () => {
-    setShowPassword(!showPassword);
-  };
-
-  if (!isNilOrError(locale)) {
-    return (
-      <Container>
-        <StyledInput
-          type={showPassword ? 'text' : 'password'}
-          id={id}
-          value={value}
-          error={error}
-          onChange={handleOnChange}
-          onBlur={handleOnBlur}
-          autocomplete={autocomplete}
-          placeholder={placeholder}
-          setRef={setRef}
-        />
-        <ShowPasswordButton
-          locale={locale}
-          onClick={handleOnClick}
-          buttonStyle="text"
-          height={'100%'}
-        >
-          {showPassword ? (
-            <EyeIcon name="eye" title={formatMessage(messages.hidePassword)} />
-          ) : (
-            <EyeClosedIcon
-              name="eyeClosed"
-              title={formatMessage(messages.showPassword)}
-            />
-          )}
-        </ShowPasswordButton>
-        <ScreenReaderOnly aria-live="polite">
-          {formatMessage(
-            showPassword
-              ? messages.a11y_passwordVisible
-              : messages.a11y_passwordHidden
-          )}
-        </ScreenReaderOnly>
-      </Container>
-    );
-  }
-
-  return null;
+}: Props) => {
+  return (
+    <PasswordInput
+      id={id}
+      value={value}
+      error={error}
+      onChange={onChange}
+      onBlur={onBlur}
+      autocomplete={autocomplete}
+      placeholder={placeholder}
+      setRef={setRef}
+    />
+  );
 };
 
-export default injectIntl(PasswordInput);
+export default Component;
