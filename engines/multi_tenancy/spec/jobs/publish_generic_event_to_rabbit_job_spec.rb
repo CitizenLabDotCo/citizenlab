d7d7145ecd1ecbf00802b75b9df8d5a80988a6b5
Adrien Dessy <adrien@citizenlab.co>
2021-02-02 19:26:59 +0100

    Add test for injection of tenant properties in RabbitMQ events.

--- engines/multi_tenancy/spec/jobs/publish_generic_event_to_rabbit_job_spec.rb
+++ engines/multi_tenancy/spec/jobs/publish_generic_event_to_rabbit_job_spec.rb
@@ -10,14 +10,14 @@ RSpec.describe PublishGenericEventToRabbitJob, type: :job do
     let(:routing_key) { "routing_key" }
     let(:generic_event) { {field_1: "value1", field_2: "value2"} }
 
-    it "actually publishes the event to RabbitMQ" do
+    it "include tenant properties in RabbitMQ events" do
       tenant_properties = MultiTenancy::TrackingTenantService.new.tenant_properties
+
       expect(job).to receive(:publish_to_rabbitmq) do |_bunny, event, _routing_key|
         expect(event).to include(tenant_properties)
       end
 
       job.perform(generic_event, routing_key, bunny)
     end
-
   end
 end
