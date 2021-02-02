a829235cf8463634b596a696ad50334656ff231a
Adrien Dessy <adrien@citizenlab.co>
2021-02-02 13:02:50 +0100

    Replace TrackService patch with a tracking service dedicated to tenants

--- engines/multi_tenancy/config/initializers/track_segment_service.rb
+++ engines/multi_tenancy/config/initializers/track_segment_service.rb
@@ -3,15 +3,13 @@ TrackSegmentService.setup do |config|
   config.user_traits_builder = lambda do |user|
     TrackSegmentService::Helpers
       .default_user_traits(user)
-      .merge(TrackingService.new.tenant_properties)
+      .merge(TrackingTenantService.new.tenant_properties)
   end
 
   config.activity_traits_builder = lambda do |activity|
-    tracking_service = TrackingService.new
     TrackSegmentService::Helpers
       .default_activity_traits(activity)
-      .merge(tracking_service.activity_properties(activity))
-      .merge(tracking_service.environment_properties)
+      .merge(TrackingTenantService.new.environment_properties)
   end
 
   config.tenant_traits_builder = lambda do |tenant|
@@ -22,7 +20,7 @@ TrackSegmentService.setup do |config|
       createdAt: tenant.created_at,
       avatar: configuration.logo&.medium&.url,
       tenantLocales: configuration.settings('core', 'locales'),
-      **TrackingService.new.tenant_properties(tenant)
+      **TrackingTenantService.new.tenant_properties(tenant)
     }
   end
 end
