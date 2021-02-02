42d51f3623581c2a5f2128809213e5f420aa7efb
Adrien Dessy <adrien@citizenlab.co>
2021-02-02 18:52:38 +0100

    Refactor PublishActivityToRabbitJob specs

---
+++ engines/multi_tenancy/spec/jobs/publish_activity_to_rabbit_job_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe PublishActivityToRabbitJob, type: :job do
+  
+  subject(:job) { PublishActivityToRabbitJob.new }
+
+  describe '#perform' do
+
+    it "generates an event including environment properties" do
+      user = create(:user)
+      comment = create(:comment)
+      activity = create(:activity, item: comment, action: 'created', user: user)
+
+      expect(PublishGenericEventToRabbitJob).to receive(:perform_now) do |event, _routing_key|
+        expect(event).to include(MultiTenancy::TrackingTenantService.new.environment_properties)
+      end
+
+      job.perform activity
+    end
+
+  end
+end
+
