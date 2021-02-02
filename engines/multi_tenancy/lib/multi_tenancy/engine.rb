0e8f7a901f1039edcb4ba498ce50424a8e81ab53
Adrien Dessy <adrien@citizenlab.co>
2021-02-02 12:09:10 +0100

    Apply Patches (decorators) once at application startup and every time the code is reloaded.

--- engines/multi_tenancy/lib/multi_tenancy/engine.rb
+++ engines/multi_tenancy/lib/multi_tenancy/engine.rb
@@ -1,5 +1,13 @@
 module MultiTenancy
   class Engine < ::Rails::Engine
     config.generators.api_only = true
+
+    def self.apply_patches
+      Dir.glob(::File.join(::File.dirname(__FILE__), '../../app/**/*_decorator*.rb')).sort.each do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+
+    config.to_prepare(&method(:apply_patches).to_proc)
   end
 end
