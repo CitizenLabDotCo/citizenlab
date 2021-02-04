8b12624386e64e9b3c4930c61d05e6a5876392f7
brentguf <brent.guffens@gmail.com>
2021-02-04 16:54:32 +0100

    messages

--- app/components/UI/PasswordInput/messages.ts
+++ app/components/UI/PasswordInput/messages.ts
@@ -2,68 +2,72 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   a11y_passwordVisible: {
-    id: 'app.components.Modal.a11y_passwordVisible',
+    id: 'app.components.PasswordInput.a11y_passwordVisible',
     defaultMessage: 'Password visible',
   },
   a11y_passwordHidden: {
-    id: 'app.components.Modal.a11y_passwordHidden',
+    id: 'app.components.PasswordInput.a11y_passwordHidden',
     defaultMessage: 'Password hidden',
   },
   hidePassword: {
-    id: 'app.components.Modal.hidePassword',
+    id: 'app.components.PasswordInput.hidePassword',
     defaultMessage: 'Hide password',
   },
   showPassword: {
-    id: 'app.components.Modal.showPassword',
+    id: 'app.components.PasswordInput.showPassword',
     defaultMessage: 'Show password',
   },
   strength1Password: {
-    id: 'app.components.Modal.strength1Password',
+    id: 'app.components.PasswordInput.strength1Password',
     defaultMessage: 'Weak',
   },
   strength2Password: {
-    id: 'app.components.Modal.strength2Password',
+    id: 'app.components.PasswordInput.strength2Password',
     defaultMessage: 'Reasonable',
   },
   strength3Password: {
-    id: 'app.components.Modal.strength3Password',
+    id: 'app.components.PasswordInput.strength3Password',
     defaultMessage: 'Decent',
   },
   strength4Password: {
-    id: 'app.components.Modal.strength4Password',
+    id: 'app.components.PasswordInput.strength4Password',
     defaultMessage: 'Strong',
   },
   strength5Password: {
-    id: 'app.components.Modal.strength5Password',
+    id: 'app.components.PasswordInput.strength5Password',
     defaultMessage: 'Very strong',
   },
   a11y_strength1Password: {
-    id: 'app.components.Modal.a11y_strength1Password',
+    id: 'app.components.PasswordInput.a11y_strength1Password',
     defaultMessage: 'Weak password strength',
   },
   a11y_strength2Password: {
-    id: 'app.components.Modal.a11y_strength2Password',
+    id: 'app.components.PasswordInput.a11y_strength2Password',
     defaultMessage: 'Reasonable password strength',
   },
   a11y_strength3Password: {
-    id: 'app.components.Modal.a11y_strength3Password',
+    id: 'app.components.PasswordInput.a11y_strength3Password',
     defaultMessage: 'Decent password strength',
   },
   a11y_strength4Password: {
-    id: 'app.components.Modal.a11y_strength4Password',
+    id: 'app.components.PasswordInput.a11y_strength4Password',
     defaultMessage: 'Strong password strength',
   },
   a11y_strength5Password: {
-    id: 'app.components.Modal.a11y_strength5Password',
+    id: 'app.components.PasswordInput.a11y_strength5Password',
     defaultMessage: 'Very strong password strength',
   },
   initialPasswordStrengthCheckerMessage: {
-    id: 'app.components.Modal.initialPasswordStrengthCheckerMessage',
+    id: 'app.components.PasswordInput.initialPasswordStrengthCheckerMessage',
     defaultMessage: 'Too short',
   },
   minimumPasswordLengthErrorMessage: {
-    id: 'app.components.Modal.minimumPasswordLengthErrorMessage',
+    id: 'app.components.PasswordInput.minimumPasswordLengthErrorMessage',
     defaultMessage:
       'Your password needs to be at least {minimumPasswordLength} characters long.',
+  },
+  emptyPasswordError: {
+    id: 'app.components.PasswordInput.emptyPasswordError',
+    defaultMessage: 'The password field cannot be empty',
   },
 });
