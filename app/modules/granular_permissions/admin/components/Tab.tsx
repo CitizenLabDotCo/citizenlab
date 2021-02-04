84ef2a87106910346232df19f74bd7cd0a80d93b
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-02-04 09:52:13 +0100

    Make naming a bit clearer

--- app/modules/granular_permissions/admin/components/Tab.tsx
+++ app/modules/granular_permissions/admin/components/Tab.tsx
@@ -4,7 +4,10 @@ import { ITab, MessageDescriptor } from 'typings';
 import messages from './messages';
 
 type Props = {
-  onData: (data: { after?: string; configuration: ITab }) => void;
+  onData: (data: {
+    insertAfterTabName?: string;
+    tabConfiguration: ITab;
+  }) => void;
   formatMessage: (
     messageDescriptor: MessageDescriptor,
     values?: { [key: string]: MessageValue } | undefined
@@ -15,13 +18,13 @@ const Tab: FC<Props> = ({ onData, formatMessage }) => {
   useEffect(
     () =>
       onData({
-        configuration: {
+        tabConfiguration: {
           label: formatMessage(messages.permissionTab),
           name: 'permissions',
           url: '/admin/initiatives/permissions',
           feature: 'granular_permissions',
         },
-        after: 'manage',
+        insertAfterTabName: 'manage',
       }),
     []
   );
