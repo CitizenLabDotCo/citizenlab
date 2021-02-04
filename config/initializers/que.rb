0ce2683d66a4acad57b0e81861988eb5d7c161ac
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-02-04 21:27:50 +0100

    decrease to 6

--- config/initializers/que.rb
+++ config/initializers/que.rb
@@ -1,3 +1,3 @@
 Que::Job.tap do |config|
-  config.retry_interval = proc { |count| (count**8) + 15 + (rand(30) * (count + 1)) }
+  config.retry_interval = proc { |count| (count**6) + 15 + (rand(30) * (count + 1)) }
 end
