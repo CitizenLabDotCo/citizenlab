8f0a410996840f8c9014a0a44453b547fc81ac2e
Adrien Dessy <adrien@citizenlab.co>
2021-02-08 16:31:05 +0100

    Refactor TenantSerializer to get attributes from AppConfiguration

--- engines/multi_tenancy/app/serializers/web_api/v1/external/tenant_serializer.rb
+++ engines/multi_tenancy/app/serializers/web_api/v1/external/tenant_serializer.rb
@@ -1,11 +1,18 @@
 class WebApi::V1::External::TenantSerializer < ActiveModel::Serializer
   attributes :id, :name, :host, :settings, :style, :logo, :header_bg
+  delegate :host, :settings, :style, to: :configuration
 
   def logo
-    object.logo && object.logo.versions.map{|k, v| [k.to_s, v.url]}.to_h
+    configuration.logo && configuration.logo.versions.map{|k, v| [k.to_s, v.url]}.to_h
   end
 
   def header_bg
-    object.header_bg && object.header_bg.versions.map{|k, v| [k.to_s, v.url]}.to_h
+    configuration.header_bg && configuration.header_bg.versions.map{|k, v| [k.to_s, v.url]}.to_h
+  end
+
+  private
+
+  def configuration
+    @configuration ||= instance_options[:app_configuration] || object.configuration
   end
 end
