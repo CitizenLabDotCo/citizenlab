64983adb1c2a3cc7cba01cda2dbd3d35c6994cbd
Adrien Dessy <adrien@citizenlab.co>
2021-02-09 16:57:55 +0100

    Make `current_user` nil by default in SideFxAppConfigurationService

--- app/services/side_fx_app_configuration_service.rb
+++ app/services/side_fx_app_configuration_service.rb
@@ -1,14 +1,15 @@
-class SideFxAppConfigurationService
+# frozen_string_literal: true
 
+class SideFxAppConfigurationService
   include SideFxHelper
 
-  def before_update(app_config, current_user); end
+  def before_update(app_config, current_user = nil) end
 
-  def after_update(app_config, current_user)
+  def after_update(app_config, current_user = nil)
     log_activity(app_config, 'changed', current_user)
 
-    if ( lifecycle_change = get_lifecycle_change(app_config) )
-      payload = {changes: lifecycle_change}
+    if (lifecycle_change = get_lifecycle_change(app_config))
+      payload = { changes: lifecycle_change }
       log_activity(app_config, 'changed_lifecycle_stage', current_user, payload)
     end
 
@@ -29,7 +30,7 @@ class SideFxAppConfigurationService
     old_settings = app_config.settings_previous_change[0]
     new_settings = app_config.settings
 
-    diff = [old_settings, new_settings].map{|s| s&.dig('core', 'lifecycle_stage')}
+    diff = [old_settings, new_settings].map { |s| s&.dig('core', 'lifecycle_stage') }
     diff[0] != diff[1] ? diff : nil
   end
 
@@ -37,10 +38,10 @@ class SideFxAppConfigurationService
   # @param [String] action
   # @param [User] user
   # @param [Hash,nil] payload
-  def log_activity(app_config, action, user, payload=nil)
+  def log_activity(app_config, action, user, payload = nil)
     tenant = Tenant.find_by(host: app_config.host)
     update_time = app_config.updated_at.to_i
-    options = {payload: payload}.compact
+    options = { payload: payload }.compact
 
     LogActivityJob.perform_later(app_config, action, user, update_time, options)
     LogActivityJob.perform_later(tenant, action, user, update_time, options) # MT_TODO To be removed once event subscribers have benn adapted.
