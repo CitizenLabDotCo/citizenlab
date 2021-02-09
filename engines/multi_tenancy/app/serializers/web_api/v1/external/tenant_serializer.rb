2ca3e795237390c8f616ef1cf702aa56ee2d71ef
Adrien Dessy <adrien@citizenlab.co>
2021-02-09 11:55:48 +0100

    Add favicon to serialized tenants and some documentation

--- engines/multi_tenancy/app/serializers/web_api/v1/external/tenant_serializer.rb
+++ engines/multi_tenancy/app/serializers/web_api/v1/external/tenant_serializer.rb
@@ -1,18 +1,36 @@
-class WebApi::V1::External::TenantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :host, :settings, :style, :logo, :header_bg
-  delegate :host, :settings, :style, to: :configuration
+# frozen_string_literal: true
 
-  def logo
-    configuration.logo && configuration.logo.versions.map{|k, v| [k.to_s, v.url]}.to_h
-  end
 
-  def header_bg
-    configuration.header_bg && configuration.header_bg.versions.map{|k, v| [k.to_s, v.url]}.to_h
-  end
+# This serializer is defined directly in the +WebApi+ namespace instead of the
+# +MultiTenancy+ namespace (`MultiTenancy::WebApi::...`) because +Notification+
+# (and maybe other places) is using dynamic dispatching to pick the right
+# serializer:
+#   serializer = "WebApi::V1::External::#{self.class.name}Serializer".constantize
+# So this serializer needs to be defined alongside the other serializer of the
+# core application.
+module WebApi
+  module V1
+    module External
+      class TenantSerializer < ActiveModel::Serializer
+        attributes :id, :name, :host, :settings, :style, :logo, :header_bg, :favicon
+        delegate :host, :settings, :style, to: :configuration
 
-  private
+        def logo
+          configuration.logo && configuration.logo.versions.map { |k, v| [k.to_s, v.url] }.to_h
+        end
 
-  def configuration
-    @configuration ||= instance_options[:app_configuration] || object.configuration
+        def header_bg
+          configuration.header_bg && configuration.header_bg.versions.map { |k, v| [k.to_s, v.url] }.to_h
+        end
+
+        def favicon
+          configuration.favicon && configuration.favicon.versions.map { |k, v| [k.to_s, v.url] }.to_h
+        end
+
+        def configuration
+          @configuration ||= instance_options[:app_configuration] || object.configuration
+        end
+      end
+    end
   end
 end
