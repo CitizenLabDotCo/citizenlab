70f27e07c6e8fd26bb207d88a6a7ed27ffa15d17
David <david@citizenlab.co>
2021-02-10 11:42:41 +0100

    mapping WIP

---
+++ app/containers/Admin/projects/edit/map/events.ts
@@ -0,0 +1,19 @@
+import { map, publishReplay, refCount } from 'rxjs/operators';
+import eventEmitter from 'utils/eventEmitter';
+import { IMapLayerAttributes } from 'services/mapLayers';
+
+enum events {
+  layersUpdated = 'layersUpdated',
+}
+
+export function updateLayers(layers: IMapLayerAttributes[]) {
+  eventEmitter.emit<IMapLayerAttributes[]>(events.layersUpdated, layers);
+}
+
+export const layers$ = eventEmitter
+  .observeEvent<IMapLayerAttributes[]>(events.layersUpdated)
+  .pipe(
+    map((event) => event.eventValue),
+    publishReplay(1),
+    refCount()
+  );
