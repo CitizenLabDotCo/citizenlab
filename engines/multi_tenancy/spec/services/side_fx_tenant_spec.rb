1e090423db85e10496287580dbc0f1600813d69d
Adrien Dessy <adrien@citizenlab.co>
2021-02-10 17:03:14 +0100

    Fix tests
    "logs a 'changed_lifecycle_stage' action job when the tenant has changed" is now tested in spec/services/side_fx_app_configuration_spec.rb:16

--- engines/multi_tenancy/spec/services/side_fx_tenant_spec.rb
+++ engines/multi_tenancy/spec/services/side_fx_tenant_spec.rb
@@ -8,7 +8,7 @@ describe MultiTenancy::SideFxTenantService do
     it "logs a 'created' action" do
       tenant = Tenant.current
       expect {service.after_create(tenant, current_user)}.
-        to have_enqueued_job(LogActivityJob).with(tenant, 'created', current_user, tenant.updated_at.to_i)
+        to have_enqueued_job(LogActivityJob).with(tenant, 'created', current_user, tenant.created_at.to_i)
     end
 
   end
@@ -33,17 +33,6 @@ describe MultiTenancy::SideFxTenantService do
       expect {service.after_update(tenant, current_user)}.
         to have_enqueued_job(LogActivityJob).with(tenant, 'changed_host', current_user, tenant.updated_at.to_i, payload: {changes: [old_host, "some-domain.net"]})
     end
-
-    it "logs a 'changed_lifecycle_stage' action job when the tenant has changed" do
-      tenant = Tenant.current
-      settings = tenant.settings
-      old_lifecycle_stage = settings['core']['lifecycle_stage']
-      settings['core']['lifecycle_stage'] = "churned"
-      tenant.update!(settings: settings)
-      expect {service.after_update(tenant, current_user)}.
-        to have_enqueued_job(LogActivityJob).with(tenant, 'changed_lifecycle_stage', current_user, tenant.updated_at.to_i, payload: {changes: [old_lifecycle_stage, "churned"]})
-    end
-
   end
 
   describe "before_destroy" do
