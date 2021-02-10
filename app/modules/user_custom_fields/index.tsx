7613c13a84697696d81e611234eab898558f16a5
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-02-10 11:15:08 +0100

    Only render form when there is a userCustomFieldsSchema

--- app/modules/user_custom_fields/index.tsx
+++ app/modules/user_custom_fields/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { isNilOrError } from 'utils/helperUtils';
 import { ModuleConfiguration } from 'utils/moduleUtils';
 import CustomFieldGraphs from './admin/components/CustomFieldGraphs';
 import RegistrationFieldsToGraphs from './admin/components/RegistrationFieldsToGraphs';
 import Tab from './admin/components/Tab';
 import CustomFieldsStep from './citizen/components/CustomFieldsStep';
 import UserCustomFieldsForm from './citizen/components/UserCustomFieldsForm';
+import useUserCustomFieldsSchema from './hooks/useUserCustomFieldsSchema';
+
+const RenderOnCustomFields = ({ children }) => {
+  const userCustomFieldsSchema = useUserCustomFieldsSchema();
+
+  const hasCustomFields =
+    !isNilOrError(userCustomFieldsSchema) &&
+    userCustomFieldsSchema.hasCustomFields;
+
+  if (!hasCustomFields) return null;
+
+  return <>{children}</>;
+};
 
 const configuration: ModuleConfiguration = {
   routes: {
@@ -21,7 +35,11 @@ const configuration: ModuleConfiguration = {
       <CustomFieldsStep {...props} />
     ),
     'app.containers.Admin.dashboard.reports.ProjectReport.graphs': CustomFieldGraphs,
-    'app.containers.UserEditPage.ProfileForm.forms': UserCustomFieldsForm,
+    'app.containers.UserEditPage.ProfileForm.forms': (props) => (
+      <RenderOnCustomFields>
+        <UserCustomFieldsForm {...props} />
+      </RenderOnCustomFields>
+    ),
     'app.containers.Admin.settings.SettingsPage': (props) => <Tab {...props} />,
   },
 };
