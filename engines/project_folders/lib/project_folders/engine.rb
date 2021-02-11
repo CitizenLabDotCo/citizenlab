423faac9c1ff754fd29ca533c16a801666e115d7
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-02-11 13:50:56 +0100

    added select to project folders

--- engines/project_folders/lib/project_folders/engine.rb
+++ engines/project_folders/lib/project_folders/engine.rb
@@ -48,6 +48,11 @@ module ProjectFolders
 
       ::Seo::ApplicationController.outlet 'seo.sitemap' do |locals|
         folders = ProjectFolders::Folder
+                  .select(
+                    :'project_folders_folders.id', :'project_folders_folders.slug',
+                    :'project_folders_folders.updated_at', :'admin_publications.publication_status',
+                    :'admin_publications.publication_type', :'admin_publications.publication_id'
+                  )
                   .includes(:admin_publication)
                   .where(admin_publications: { publication_status: %w[published archived] })
         { partial: 'seo/sitemap', locals: { folders: folders, **locals } }
