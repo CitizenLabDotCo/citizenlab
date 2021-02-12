769e21f0fb0707578d1c4aa1bf628e9d88c3ee1b
Adrien Dessy <adrien@citizenlab.co>
2021-02-12 11:51:37 +0100

    Fix rails version in migration.

--- db/migrate/20201116092907_add_style_to_app_configuration.custom_style.rb
+++ db/migrate/20201116092907_add_style_to_app_configuration.custom_style.rb
@@ -1,5 +1,5 @@
 # This migration comes from custom_style (originally 20210101120000)
-class AddStyleToAppConfiguration < ActiveRecord::Migration[5.2]
+class AddStyleToAppConfiguration < ActiveRecord::Migration[6.0]
   def change
     add_column :app_configurations, :style, :jsonb, default: {}
   end
