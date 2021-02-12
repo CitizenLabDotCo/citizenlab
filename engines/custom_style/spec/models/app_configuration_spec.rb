c75ab4575cba1704c631d3bee4ba61496c8a255e
Adrien Dessy <adrien@citizenlab.co>
2021-02-12 14:01:15 +0100

    Fix tests: import the group of shared examples.

--- engines/custom_style/spec/models/app_configuration_spec.rb
+++ engines/custom_style/spec/models/app_configuration_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require_relative './style_settings_spec.rb'
 
 RSpec.describe AppConfiguration, type: :model do
   it_behaves_like 'StyleSettings'
