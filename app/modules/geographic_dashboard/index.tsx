52f633f4a94ad6a8efdd34b73f2cd66c2ad6fccc
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-02-15 10:12:46 +0100

    waiting for tabs injection function

---
+++ app/modules/geographic_dashboard/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface RenderOnTypeProps {}
+// {
+//   label: formatMessage(messages.tabMap),
+//   url: '/admin/dashboard/map',
+//   feature: 'geographic_dashboard',
+// },
+// {
+//   path: 'map',
+//   component: Loadable({
+//     loader: () => import('./map'),
+//     loading: () => null,
+//   }),
+// },
+const configuration: ModuleConfiguration = {
+  routes: {},
+  outlets: {},
+};
+
+export default configuration;
