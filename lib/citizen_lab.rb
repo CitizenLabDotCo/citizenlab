9af5f942742039479bc6c1a9e4cd6c6988f7eb7a
Adrien Dessy <adrien@citizenlab.co>
2021-02-15 13:08:12 +0100

    Add source comment.

--- lib/citizen_lab.rb
+++ lib/citizen_lab.rb
@@ -2,6 +2,8 @@
 
 require 'pathname'
 
+# Heavily inspired by:
+# https://gitlab.com/gitlab-org/gitlab-foss/-/blob/master/lib/gitlab.rb
 module CitizenLab
   def self.root
     Pathname.new(File.expand_path('..', __dir__))
