5bbc4bc0027e7b69394f7e3ff1694039fc274084
Adrien Dessy <adrien@citizenlab.co>
2021-02-15 12:55:19 +0100

    Add missing #root method

--- lib/citizen_lab.rb
+++ lib/citizen_lab.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
+require 'pathname'
+
 module CitizenLab
+  def self.root
+    Pathname.new(File.expand_path('..', __dir__))
+  end
+
   def self.ee?
     @is_ee ||= root.join('engines/multi_tenancy').exist? && %w[true 1].include?(ENV['CITIZENLAB_EE'].to_s)
   end
