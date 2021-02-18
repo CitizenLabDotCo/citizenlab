cbfb2ada4d9338fafc89665fdb8b51778b7021bc
Adrien Dessy <adrien@citizenlab.co>
2021-02-18 09:31:31 +0100

    Add name attribute to a settings specification

---
+++ lib/citizen_lab/mixins/settings_provider.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module CitizenLab
+  module Mixins
+    module SettingsSpecification
+      # @return [String] Name of the settings group
+      def name
+        raise NotImplementedError
+      end
+
+      # @return [String]
+      def settings_json_schema_str
+        @settings_json_schema_str ||= JSON.dump(settings_json_schema)
+      end
+
+      def settings_json_schema
+        @settings_json_schema ||= JSON.parse(settings_json_schema_str)
+      end
+    end
+  end
+end
+
+
