6349c7e4812146b01260e2f565608b0834eedb95
Adrien Dessy <adrien@citizenlab.co>
2021-02-18 17:12:48 +0100

    Comment the migration content.
    We'll migrate in two steps:
    
    - an empty migration first for the existing deployments (this will add it to the migration table).
    - uncomment the migration code for future deployments.

--- db/migrate/20201116092907_add_style_to_app_configuration.custom_style.rb
+++ db/migrate/20201116092907_add_style_to_app_configuration.custom_style.rb
@@ -1,6 +1,8 @@
 # This migration comes from custom_style (originally 20210101120000)
 class AddStyleToAppConfiguration < ActiveRecord::Migration[6.0]
   def change
-    add_column :app_configurations, :style, :jsonb, default: {}
+    # Running it as an empty migration on existing deployments.
+    # The comment will be removed afterwards.
+    # add_column :app_configurations, :style, :jsonb, default: {}
   end
 end
