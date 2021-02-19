2223ad36029a7bca68446394d050e7755254293f
Adrien Dessy <adrien@citizenlab.co>
2021-02-19 11:51:08 +0100

    Formatting

--- engines/custom_style/app/models/custom_style/style_settings.rb
+++ engines/custom_style/app/models/custom_style/style_settings.rb
@@ -30,13 +30,13 @@ module CustomStyle
         style_json_schema['properties'].map do |prop, descriptor|
           {
             name: prop,
-            description: descriptor["description"]
+            description: descriptor['description']
           }
         end
       end
 
       def style_json_schema_str
-        style_schema_filepath = Rails.root.join('engines', 'custom_style', 'config', 'schemas', 'style.schema.json')
+        style_schema_filepath = Rails.root.join('engines/custom_style/config/schemas/style.schema.json')
         @style_json_schema_str ||= File.read(style_schema_filepath)
       end
 
