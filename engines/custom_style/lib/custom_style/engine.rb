5ab957c26b5a4192db15a8cabf4141c487de207d
Adrien Dessy <adrien@citizenlab.co>
2021-02-19 11:45:34 +0100

    Remove the reload of patches/extensions.
    Those are now applied directly in the patched/extended module.

--- engines/custom_style/lib/custom_style/engine.rb
+++ engines/custom_style/lib/custom_style/engine.rb
@@ -2,16 +2,5 @@ module CustomStyle
   class Engine < ::Rails::Engine
     isolate_namespace CustomStyle
     config.generators.api_only = true
-
-    def self.apply_patches
-      patches_paths =  Dir.glob(File.join(::File.dirname(__FILE__), '../../app/**/extensions/**/*.rb'))
-      patches_paths += Dir.glob(File.join(::File.dirname(__FILE__), '../../app/**/patches/**/*.rb'))
-
-      patches_paths.sort.each do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-    end
-
-    config.to_prepare(&method(:apply_patches))
   end
 end
