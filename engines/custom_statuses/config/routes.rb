a5299e2155f438cdad2bde58cafedb96306dbca5
Sébastien Hoorens <sebastien@citizenlab.co>
2021-02-23 18:31:47 +0100

    incredibly minor

--- engines/custom_statuses/config/routes.rb
+++ engines/custom_statuses/config/routes.rb
@@ -1,7 +1,7 @@
 CustomStatuses::Engine.routes.draw do
   namespace :web_api, :defaults => {:format => :json} do
     namespace :v1 do
-      resources :idea_statuses, only: [:create, :update, :destroy]
+      resources :idea_statuses, only: %i[create update destroy]
     end
   end
 end
