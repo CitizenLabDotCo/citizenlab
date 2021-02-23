06866ad5c0fedb3b336cf142f652b437f9a00f70
Adrien Dessy <adrien@citizenlab.co>
2021-02-23 10:58:15 +0100

    Test AppConfiguration::Settings

--- spec/models/app_configuration_spec.rb
+++ spec/models/app_configuration_spec.rb
@@ -40,39 +40,44 @@ RSpec.describe AppConfiguration, type: :model do
     tenant_core_settings = Tenant.current.settings['core']
     expect(tenant_core_settings).to include(core_settings_update)
   end
+end
 
-  # TODO rewrite test
-  # describe '.extend_settings' do
-  #
-  #   before(:each) do
-  #     described_class.instance_variable_set(:@extensions_settings_specs, {})
-  #   end
-  #
-  #   let(:settings_spec) do
-  #     Module.new do
-  #       def self.settings_name
-  #         'my_feature'
-  #       end
-  #
-  #       def self.settings_json_schema
-  #         {
-  #           type: 'object',
-  #           additionalProperties: false,
-  #           required: %w[allowed enabled],
-  #           properties: {
-  #             allowed: { type: 'boolean', default: true },
-  #             enabled: { type: 'boolean', default: true }
-  #           }
-  #         }.deep_stringify_keys
-  #       end
-  #     end
-  #   end
-  #
-  #   it 'extends the settings json schema' do
-  #     described_class.extend_settings(settings_spec)
-  #     expect(
-  #       described_class.settings_json_schema.dig('properties', settings_spec.settings_name)
-  #     ).to match(settings_spec.json_schema)
-  #   end
-  # end
+RSpec.describe AppConfiguration::Settings do
+  before { described_class.send(:reset) }
+
+  context 'without extension features' do
+    describe '.json_schema' do
+      it { expect(described_class.json_schema).to match(described_class.core_settings_json_schema) }
+      it { expect(described_class.extension_features_specs).to be_empty }
+    end
+  end
+
+  context 'with one extension feature' do
+    let(:feature_spec) do
+      Module.new do
+        extend CitizenLab::Mixins::FeatureSpecification
+
+        # rubocop:disable Style/SingleLineMethods, Layout/EmptyLineBetweenDefs
+        def self.feature_name; 'dummy_feature' end
+        def self.feature_title; 'Dummy feature' end
+        def self.feature_description; 'Oh my... such a good feature.' end
+        # rubocop:enable Style/SingleLineMethods, Layout/EmptyLineBetweenDefs
+
+        add_setting 'dummy setting', required: true, schema: {'type' => 'boolean'}
+      end
+    end
+
+    before { described_class.add_feature(feature_spec) }
+
+    describe '.json_schema' do
+      let(:json_schema) { described_class.json_schema }
+
+      it { expect(json_schema).to include(described_class.core_settings_json_schema) }
+      it { expect(json_schema.dig('properties', feature_spec.feature_name)).to eq(feature_spec.json_schema) }
+    end
+
+    describe '.extension_features_specs' do
+      it { expect(described_class.extension_features_specs.length).to eq(1) }
+    end
+  end
 end
