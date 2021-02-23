a0dc7cf3e104cc9336cec960bcaeb46c18841a71
Sébastien Hoorens <sebastien@citizenlab.co>
2021-02-23 15:20:35 +0100

    split up idea status api in engine and main app

--- engines/custom_statuses/config/routes.rb
+++ engines/custom_statuses/config/routes.rb
@@ -1,3 +1,7 @@
 CustomStatuses::Engine.routes.draw do
-
+  namespace :web_api, :defaults => {:format => :json} do
+    namespace :v1 do
+      resources :idea_statuses, only: [:create, :update, :destroy]
+    end
+  end
 end
