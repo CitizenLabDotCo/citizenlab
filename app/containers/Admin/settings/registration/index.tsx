d0ac7d2145d9f6d8139c74017e3d2ca8495564ad
brentguf <brent.guffens@gmail.com>
2021-02-24 17:55:29 +0100

    Add sign-up form field to registration WIP

--- app/containers/Admin/settings/registration/index.tsx
+++ app/containers/Admin/settings/registration/index.tsx
@@ -1,12 +1,18 @@
 import React, { PureComponent } from 'react';
 import AllCustomFields from './CustomFields/All';
-import messages from '../messages';
 import {
   Section,
   SectionTitle,
+  SectionField,
   SectionDescription,
+  SubSectionTitle,
 } from 'components/admin/Section';
+import InputMultilocWithLocaleSwitcher from 'components/UI/InputMultilocWithLocaleSwitcher';
+import { IconTooltip } from 'cl2-component-library';
+
+// i18n
 import { FormattedMessage } from 'utils/cl-intl';
+import messages from '../messages';
 
 type Props = {};
 
@@ -23,6 +29,33 @@ class SettingsRegistrationTab extends PureComponent<Props, State> {
           <FormattedMessage {...messages.subtitleRegistration} />
         </SectionDescription>
         <AllCustomFields />
+        <Section key={'project_header'}>
+          <SubSectionTitle>
+            <FormattedMessage {...messages.project_header} />
+            <IconTooltip
+              content={
+                <FormattedMessage {...messages.project_header_tooltip} />
+              }
+            />
+          </SubSectionTitle>
+          {/* <SectionField>
+            <InputMultilocWithLocaleSwitcher
+              type="text"
+              valueMultiloc={
+                get(
+                  attributesDiff,
+                  'settings.core.currently_working_on_text'
+                ) ||
+                get(
+                  tenant,
+                  'data.attributes.settings.core.currently_working_on_text'
+                )
+              }
+              onChange={this.handleProjectHeaderOnChange}
+              errorMultiloc={subtitleError}
+            />
+          </SectionField> */}
+        </Section>
       </Section>
     );
   }
