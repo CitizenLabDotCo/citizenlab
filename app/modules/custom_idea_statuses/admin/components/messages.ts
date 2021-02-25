cc62192666da06b38a41603e5e4c93afe6666f00
brentguf <brent.guffens@gmail.com>
2021-02-25 15:00:44 +0100

    IdeaStatusForm copy

--- app/modules/custom_idea_statuses/admin/components/messages.ts
+++ app/modules/custom_idea_statuses/admin/components/messages.ts
@@ -5,4 +5,80 @@ export default defineMessages({
     id: 'app.containers.Admin.ideas.tabStatuses',
     defaultMessage: 'Statuses',
   },
+  fieldTitle: {
+    id: 'app.containers.admin.ideaStatuses.form.fieldTitle',
+    defaultMessage: 'Status Name',
+  },
+  fieldDescription: {
+    id: 'app.containers.admin.ideaStatuses.form.fieldDescription',
+    defaultMessage: 'Status Description',
+  },
+  fieldColor: {
+    id: 'app.containers.admin.ideaStatuses.form.fieldColor',
+    defaultMessage: 'Color',
+  },
+  category: {
+    id: 'app.containers.admin.ideaStatuses.form.category',
+    defaultMessage: 'Category',
+  },
+  categoryDescription: {
+    id: 'app.containers.admin.ideaStatuses.form.categoryDescription',
+    defaultMessage:
+      'Please select the category that best represents your status. This selection will help our analytics tool to more accurately process and analyze posts.',
+  },
+  proposedFieldCodeTitle: {
+    id: 'app.containers.admin.ideaStatuses.form.proposedFieldCodeTitle',
+    defaultMessage: 'Proposed',
+  },
+  viewedFieldCodeTitle: {
+    id: 'app.containers.admin.ideaStatuses.form.viewedFieldCodeTitle',
+    defaultMessage: 'Viewed',
+  },
+  under_considerationFieldCodeTitle: {
+    id:
+      'app.containers.admin.ideaStatuses.form.under_considerationFieldCodeTitle',
+    defaultMessage: 'Under Consideration',
+  },
+  acceptedFieldCodeTitle: {
+    id: 'app.containers.admin.ideaStatuses.form.acceptedFieldCodeTitle',
+    defaultMessage: 'Approved',
+  },
+  implementedFieldCodeTitle: {
+    id: 'app.containers.admin.ideaStatuses.form.implementedFieldCodeTitle',
+    defaultMessage: 'Implemented',
+  },
+  rejectedFieldCodeTitle: {
+    id: 'app.containers.admin.ideaStatuses.form.rejectedFieldCodeTitle',
+    defaultMessage: 'Not Selected',
+  },
+  customFieldCodeTitle: {
+    id: 'app.containers.admin.ideaStatuses.form.customFieldCodeTitle',
+    defaultMessage: 'Other',
+  },
+  proposedFieldCodeDescription: {
+    id: 'app.containers.admin.ideaStatuses.form.proposedFieldCodeDescription',
+    defaultMessage: 'Successfully submitted as a proposal for consideration',
+  },
+  viewedFieldCodeDescription: {
+    id: 'app.containers.admin.ideaStatuses.form.viewedFieldCodeDescription',
+    defaultMessage: 'Viewed but not yet processed',
+  },
+  under_considerationFieldCodeDescription: {
+    id:
+      'app.containers.admin.ideaStatuses.form.under_considerationFieldCodeDescription',
+    defaultMessage: 'Considered for implementation or next steps',
+  },
+  acceptedFieldCodeDescription: {
+    id: 'app.containers.admin.ideaStatuses.form.acceptedFieldCodeDescription',
+    defaultMessage: 'Selected for implementation or next steps',
+  },
+  implementedFieldCodeDescription: {
+    id:
+      'app.containers.admin.ideaStatuses.form.implementedFieldCodeDescription',
+    defaultMessage: 'Successfully implemented',
+  },
+  rejectedFieldCodeDescription: {
+    id: 'app.containers.admin.ideaStatuses.form.rejectedFieldCodeDescription',
+    defaultMessage: 'Ineligible or not selected to move forward',
+  },
 });
--- app/modules/custom_idea_statuses/admin/containers/messages.ts
+++ app/modules/custom_idea_statuses/admin/containers/messages.ts
@@ -1,10 +1,6 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  // required: {
-  //   id: 'app.containers.admin.ideaStatuses.all.required',
-  //   defaultMessage: 'Required',
-  // },
   deleteButtonLabel: {
     id: 'app.containers.admin.ideaStatuses.all.deleteButtonLabel',
     defaultMessage: 'Delete',
@@ -13,18 +9,14 @@ export default defineMessages({
     id: 'app.containers.admin.ideaStatuses.all.editButtonLabel',
     defaultMessage: 'Edit',
   },
-  // systemField: {
-  //   id: 'app.containers.admin.ideaStatuses.all.systemField',
-  //   defaultMessage: 'Default',
-  // },
   addIdeaStatus: {
     id: 'app.containers.admin.ideaStatuses.all.addIdeaStatus',
     defaultMessage: 'Add status',
   },
-  // editIdeaStatus: {
-  //   id: 'app.containers.admin.ideaStatuses.all.editIdeaStatus',
-  //   defaultMessage: 'Edit status',
-  // },
+  editIdeaStatus: {
+    id: 'app.containers.admin.ideaStatuses.all.editIdeaStatus',
+    defaultMessage: 'Edit status',
+  },
   titleIdeaStatuses: {
     id: 'app.containers.admin.ideaStatuses.all.titleIdeaStatuses',
     defaultMessage: 'Statuses',
@@ -52,80 +44,4 @@ export default defineMessages({
     id: 'app.containers.admin.ideaStatuses.all.lockedStatusTooltip',
     defaultMessage: 'This status cannot be deleted or moved.',
   },
-  // fieldTitle: {
-  //   id: 'app.containers.admin.ideaStatuses.form.fieldTitle',
-  //   defaultMessage: 'Status Name',
-  // },
-  // fieldDescription: {
-  //   id: 'app.containers.admin.ideaStatuses.form.fieldDescription',
-  //   defaultMessage: 'Status Description',
-  // },
-  // fieldColor: {
-  //   id: 'app.containers.admin.ideaStatuses.form.fieldColor',
-  //   defaultMessage: 'Color',
-  // },
-  // category: {
-  //   id: 'app.containers.admin.ideaStatuses.form.category',
-  //   defaultMessage: 'Category',
-  // },
-  // categoryDescription: {
-  //   id: 'app.containers.admin.ideaStatuses.form.categoryDescription',
-  //   defaultMessage:
-  //     'Please select the category that best represents your status. This selection will help our analytics tool to more accurately process and analyze posts.',
-  // },
-  // proposedFieldCodeTitle: {
-  //   id: 'app.containers.admin.ideaStatuses.form.proposedFieldCodeTitle',
-  //   defaultMessage: 'Proposed',
-  // },
-  // viewedFieldCodeTitle: {
-  //   id: 'app.containers.admin.ideaStatuses.form.viewedFieldCodeTitle',
-  //   defaultMessage: 'Viewed',
-  // },
-  // under_considerationFieldCodeTitle: {
-  //   id:
-  //     'app.containers.admin.ideaStatuses.form.under_considerationFieldCodeTitle',
-  //   defaultMessage: 'Under Consideration',
-  // },
-  // acceptedFieldCodeTitle: {
-  //   id: 'app.containers.admin.ideaStatuses.form.acceptedFieldCodeTitle',
-  //   defaultMessage: 'Approved',
-  // },
-  // implementedFieldCodeTitle: {
-  //   id: 'app.containers.admin.ideaStatuses.form.implementedFieldCodeTitle',
-  //   defaultMessage: 'Implemented',
-  // },
-  // rejectedFieldCodeTitle: {
-  //   id: 'app.containers.admin.ideaStatuses.form.rejectedFieldCodeTitle',
-  //   defaultMessage: 'Not Selected',
-  // },
-  // customFieldCodeTitle: {
-  //   id: 'app.containers.admin.ideaStatuses.form.customFieldCodeTitle',
-  //   defaultMessage: 'Other',
-  // },
-  // proposedFieldCodeDescription: {
-  //   id: 'app.containers.admin.ideaStatuses.form.proposedFieldCodeDescription',
-  //   defaultMessage: 'Successfully submitted as a proposal for consideration',
-  // },
-  // viewedFieldCodeDescription: {
-  //   id: 'app.containers.admin.ideaStatuses.form.viewedFieldCodeDescription',
-  //   defaultMessage: 'Viewed but not yet processed',
-  // },
-  // under_considerationFieldCodeDescription: {
-  //   id:
-  //     'app.containers.admin.ideaStatuses.form.under_considerationFieldCodeDescription',
-  //   defaultMessage: 'Considered for implementation or next steps',
-  // },
-  // acceptedFieldCodeDescription: {
-  //   id: 'app.containers.admin.ideaStatuses.form.acceptedFieldCodeDescription',
-  //   defaultMessage: 'Selected for implementation or next steps',
-  // },
-  // implementedFieldCodeDescription: {
-  //   id:
-  //     'app.containers.admin.ideaStatuses.form.implementedFieldCodeDescription',
-  //   defaultMessage: 'Successfully implemented',
-  // },
-  // rejectedFieldCodeDescription: {
-  //   id: 'app.containers.admin.ideaStatuses.form.rejectedFieldCodeDescription',
-  //   defaultMessage: 'Ineligible or not selected to move forward',
-  // },
 });
