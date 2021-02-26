56689324d1d4f70ff9cf2f3a658f28f9d3e06814
Sébastien Hoorens <sebastien@citizenlab.co>
2021-02-26 18:06:08 +0100

    fixed spec

--- engines/custom_statuses/spec/lib/settings_spec.rb
+++ engines/custom_statuses/spec/lib/settings_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe CustomStatuses::Settings do
+RSpec.describe AppConfiguration::Settings do
   let(:custom_statuses_feature_name) { CustomStatuses::FeatureSpecification.feature_name }
 
   it do
-    expect(described_class.json_schema['properties']).to 
-      include(custom_statuses_feature_name) 
+    expect(described_class.json_schema['properties']).to include(custom_statuses_feature_name) 
   end
 end
