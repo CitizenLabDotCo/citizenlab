6b03abaf6704a7bb5901bd9d769757b5aee376e3
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-02-27 02:57:09 +0100

    fix finde

--- lib/finder/base.rb
+++ lib/finder/base.rb
@@ -59,6 +59,7 @@ module Finder
 
     def _filter_records
       params.each do |param, value|
+        value = [nil] if value == []
         next unless respond_to?("#{param}_condition", true) && value.present?
 
         send("#{param}_condition", value)
