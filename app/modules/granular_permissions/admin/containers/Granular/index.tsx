093e9bcd3b506d9bb4b5fb541472739cf1807d60
brentguf <brent.guffens@gmail.com>
2021-03-02 17:53:41 +0100

    Granular fixes

--- app/modules/granular_permissions/admin/containers/Granular/index.tsx
+++ app/modules/granular_permissions/admin/containers/Granular/index.tsx
@@ -1,43 +1,40 @@
 import React, { Component } from 'react';
 import { IProjectData } from 'services/projects';
+import styled from 'styled-components';
 
 // i18n
 import { FormattedMessage } from 'utils/cl-intl';
 import messages from './messages';
 
 // components
-import { SubSectionTitle } from 'components/admin/Section';
 import Timeline from './Timeline';
 import Continuous from './Continuous';
-import {
-  StyledSection,
-  SubSection,
-} from 'containers/Admin/projects/edit/permissions';
+import { SubSection, SubSectionTitle } from 'components/admin/Section';
+
+const StyledSubSection = styled(SubSection)`
+  margin-bottom: 50px;
+`;
 
 interface Props {
   project: IProjectData;
 }
 
-class Granular extends Component<Props> {
-  render() {
-    const { project } = this.props;
-    const projectId = project.id;
-    return (
-      <SubSection>
-        <StyledSection>
-          <SubSectionTitle>
-            <FormattedMessage {...messages.granularPermissionsTitle} />
-          </SubSectionTitle>
-          {project && project.attributes.process_type === 'timeline' && (
-            <Timeline projectId={projectId} />
-          )}
-          {project && project.attributes.process_type === 'continuous' && (
-            <Continuous projectId={projectId} />
-          )}
-        </StyledSection>
-      </SubSection>
-    );
-  }
-}
+const Granular = ({ project }: Props) => {
+  const projectId = project.id;
+
+  return (
+    <StyledSubSection>
+      <SubSectionTitle>
+        <FormattedMessage {...messages.granularPermissionsTitle} />
+      </SubSectionTitle>
+      {project && project.attributes.process_type === 'timeline' && (
+        <Timeline projectId={projectId} />
+      )}
+      {project && project.attributes.process_type === 'continuous' && (
+        <Continuous projectId={projectId} />
+      )}
+    </StyledSubSection>
+  );
+};
 
 export default Granular;
