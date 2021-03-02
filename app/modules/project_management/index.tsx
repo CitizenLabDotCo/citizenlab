cc3698d35876f6819a1629ad1f125311763b4ee5
brentguf <brent.guffens@gmail.com>
2021-03-02 19:06:57 +0100

    Module config init

---
+++ app/modules/project_management/index.tsx
@@ -0,0 +1,28 @@
+import useFeatureFlag from 'hooks/useFeatureFlag';
+import React, { ReactNode } from 'react';
+import { ModuleConfiguration } from 'utils/moduleUtils';
+import ProjectManagement from './admin/containers/ProjectManagement';
+
+type RenderOnFeatureFlagProps = {
+  children: ReactNode;
+};
+
+const RenderOnFeatureFlag = ({ children }: RenderOnFeatureFlagProps) => {
+  const isEnabled = useFeatureFlag('project_management');
+  if (isEnabled) {
+    return <>{children}</>;
+  }
+  return null;
+};
+
+const configuration: ModuleConfiguration = {
+  outlets: {
+    'app.containers.Admin.project.edit.permissions': (props) => (
+      <RenderOnFeatureFlag>
+        <ProjectManagement {...props} />
+      </RenderOnFeatureFlag>
+    ),
+  },
+};
+
+export default configuration;
