1a79d6f089546d10b3431ada2a64ebb0c101258b
David <david@citizenlab.co>
2021-03-02 20:28:05 +0100

    mapping module WIP

---
+++ app/modules/custom_maps/admin/components/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  mapTab: {
+    id: 'app.containers.AdminPage.ProjectEdit.mapTab',
+    defaultMessage: 'Map',
+  },
+});
