0ef8fdcf477a36eb8b5a0d9cd74c1d3bf54fe102
Sara <sara@citizenlab.co>
2021-03-02 13:53:59 +0100

    Removes comments from the nlp dumps

--- engines/nlp/app/services/nlp/tenant_dump_service.rb
+++ engines/nlp/app/services/nlp/tenant_dump_service.rb
@@ -42,33 +42,10 @@ module NLP
           areas:                idea.areas.map{|ar| ar.id},
           upvotes_count:        idea.upvotes_count,
           downvotes_count:      idea.downvotes_count,
-          comments:             encode_comments(idea),
           updated_at:           idea.updated_at.iso8601,
           published_at:         idea.published_at.iso8601
         }
         d
-      end
-    end
-
-    def encode_comments idea
-      children_map = idea.comments.map{|c| [c.id,[]]}.to_h
-      idea.comments.each do |c|
-        if c.parent_id
-          children_map[c.parent_id] += [c]
-        end
-      end
-      loop_encode_comments idea.comments.where(parent_id: nil), children_map
-    end
-
-    def loop_encode_comments comments, children_map
-      comments.sort_by{|c| c.created_at.to_i}.map do |c|
-        {
-          id:            c.id,
-          created_at:    c.created_at.iso8601,
-          body_multiloc: c.body_multiloc,
-          author_name:   c.author_name,
-          comments:      loop_encode_comments(children_map[c.id], children_map)
-        }
       end
     end
 
