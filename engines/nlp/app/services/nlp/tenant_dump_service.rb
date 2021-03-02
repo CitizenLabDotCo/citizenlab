b3f14afd6da332156cd0380bfc3ed818c56e828e
Sara <sara@citizenlab.co>
2021-03-02 15:02:12 +0100

    Puts back code to re-enable comments easily when needed

--- engines/nlp/app/services/nlp/tenant_dump_service.rb
+++ engines/nlp/app/services/nlp/tenant_dump_service.rb
@@ -49,6 +49,29 @@ module NLP
       end
     end
 
+    def encode_comments idea
+      children_map = idea.comments.map{|c| [c.id,[]]}.to_h
+      idea.comments.each do |c|
+        if c.parent_id
+          children_map[c.parent_id] += [c]
+        end
+      end
+      loop_encode_comments idea.comments.where(parent_id: nil), children_map
+    end
+
+    def loop_encode_comments comments, children_map
+      comments.sort_by{|c| c.created_at.to_i}.map do |c|
+        {
+          id:            c.id,
+          created_at:    c.created_at.iso8601,
+          body_multiloc: c.body_multiloc,
+          author_name:   c.author_name,
+          comments:      loop_encode_comments(children_map[c.id], children_map)
+        }
+      end
+    end
+
+
     def encode_areas
       Area.all.map do |area|
         d = {
