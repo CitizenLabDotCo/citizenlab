b0adf16619493fbf40f487e0b315fce288760f9c
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-03 11:37:17 +0100

    added feature spec

--- engines/geographic_dashboard/lib/geographic_dashboard/engine.rb
+++ engines/geographic_dashboard/lib/geographic_dashboard/engine.rb
@@ -1,5 +1,10 @@
 module GeographicDashboard
   class Engine < ::Rails::Engine
     isolate_namespace GeographicDashboard
+
+    config.to_prepare do
+      require 'geographic_dashboard/feature_specification'
+      AppConfiguration::Settings.add_feature(GeographicDashboard::FeatureSpecification)
+    end
   end
 end
