ffb1f060581925a9f616dedff4fea53241fc95f6
brentguf <brent.guffens@gmail.com>
2021-03-03 14:13:45 +0100

    Delete Moderators comp

--- app/modules/project_management/admin/components/messages.ts
+++ app/modules/project_management/admin/components/messages.ts
@@ -93,24 +93,6 @@ export default defineMessages({
     id: 'app.containers.AdminPage.groups.permissions.moderatorsNotFound',
     defaultMessage: 'Moderators not found',
   },
-  moderatorsSectionTitle: {
-    id: 'app.containers.AdminPage.ProjectEdit.moderatorsSectionTitle',
-    defaultMessage: 'Who can moderate this project?',
-  },
-  projectManagerTooltipContent: {
-    id: 'app.containers.AdminPage.ProjectEdit.projectManagerTooltipContent',
-    defaultMessage:
-      'Project managers can edit projects, manage inputs and email participants. You can {moderationInfoCenterLink} to find more information about the rights assigned to project managers.',
-  },
-  moderationInfoCenterLinkText: {
-    id: 'app.containers.AdminPage.ProjectEdit.moderationInfoCenterLinkText',
-    defaultMessage: 'visit our Help Center',
-  },
-  moreInfoModeratorLink: {
-    id: 'app.containers.AdminPage.ProjectEdit.moreInfoModeratorLink',
-    defaultMessage:
-      'http://support.citizenlab.co/en-your-citizenlab-platform-step-by-step/set-up/pointing-out-the-right-project-moderators',
-  },
   // inputAssignmentSectionTitle: {
   //   id: 'app.containers.AdminPage.ProjectEdit.inputAssignmentSectionTitle',
   //   defaultMessage: 'Who is responsible for processing the inputs?',
