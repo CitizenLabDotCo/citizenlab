44dda594cda270cfb8f0a97d9795e0cb63274e93
Sébastien Hoorens <sebastien@citizenlab.co>
2021-03-03 15:18:06 +0100

    keep app config style in schema

--- db/migrate/20201116092907_add_style_to_app_configuration.custom_style.rb
+++ db/migrate/20201116092907_add_style_to_app_configuration.custom_style.rb
@@ -3,6 +3,6 @@ class AddStyleToAppConfiguration < ActiveRecord::Migration[6.0]
   def change
     # Running it as an empty migration on existing deployments.
     # The comment will be removed afterwards.
-    # add_column :app_configurations, :style, :jsonb, default: {}
+    add_column :app_configurations, :style, :jsonb, default: {}
   end
 end
