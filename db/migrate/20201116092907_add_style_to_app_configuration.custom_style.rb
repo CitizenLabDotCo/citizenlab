a67a717ddc2217637366bef403b70d17987725a8
adessy <adrien@citizenlab.co>
2021-03-03 15:01:24 +0000

    Update 20201116092907_add_style_to_app_configuration.custom_style.rb
    Remove comment

--- db/migrate/20201116092907_add_style_to_app_configuration.custom_style.rb
+++ db/migrate/20201116092907_add_style_to_app_configuration.custom_style.rb
@@ -1,8 +1,6 @@
 # This migration comes from custom_style (originally 20210101120000)
 class AddStyleToAppConfiguration < ActiveRecord::Migration[6.0]
   def change
-    # Running it as an empty migration on existing deployments.
-    # The comment will be removed afterwards.
     add_column :app_configurations, :style, :jsonb, default: {}
   end
 end
