b76e1159af0c37c8885e66018e35cf664f03b006
brentguf <brent.guffens@gmail.com>
2021-03-03 13:48:32 +0100

    ProjectManagement

--- app/modules/project_management/admin/containers/ProjectManagement.tsx
+++ app/modules/project_management/admin/containers/ProjectManagement.tsx
@@ -2,23 +2,64 @@ import React from 'react';
 import styled from 'styled-components';
 
 // components
-import { Section } from 'components/admin/Section';
-import Moderators from '../components/Moderators';
+import { Section, SubSectionTitle } from 'components/admin/Section';
+
+// i18n
+import { FormattedMessage, injectIntl } from 'utils/cl-intl';
+import messages from './messages';
+import { InjectedIntlProps } from 'react-intl';
+
+// components
+import { IconTooltip } from 'cl2-component-library';
+import ModeratorList from '../components/ModeratorList';
+import UserSearch from '../components/UserSearch';
 
 const ModeratorSubSection = styled(Section)`
-  margin-bottom: 20px;
+  margin-bottom: 25px;
+`;
+
+const StyledA = styled.a`
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 interface Props {
   projectId: string;
 }
 
-const ProjectManagement = ({ projectId }: Props) => {
+const ProjectManagement = ({
+  projectId,
+  intl: { formatMessage },
+}: Props & InjectedIntlProps) => {
   return (
     <ModeratorSubSection>
-      <Moderators projectId={projectId} />
+      <SubSectionTitle>
+        <FormattedMessage {...messages.moderatorsSectionTitle} />
+        <IconTooltip
+          content={
+            <FormattedMessage
+              {...messages.projectManagerTooltipContent}
+              values={{
+                moderationInfoCenterLink: (
+                  <StyledA
+                    href={formatMessage(messages.moreInfoModeratorLink)}
+                    target="_blank"
+                  >
+                    <FormattedMessage
+                      {...messages.moderationInfoCenterLinkText}
+                    />
+                  </StyledA>
+                ),
+              }}
+            />
+          }
+        />
+      </SubSectionTitle>
+      <UserSearch projectId={projectId} />
+      <ModeratorList projectId={projectId} />
     </ModeratorSubSection>
   );
 };
 
-export default ProjectManagement;
+export default injectIntl(ProjectManagement);
