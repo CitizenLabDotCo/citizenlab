afce4e83d343d51b6c8c5d17c15e48eeed0b326d
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-03 10:42:56 +0100

    added touch true option in counter culture

--- app/models/ideas_phase.rb
+++ app/models/ideas_phase.rb
@@ -1,21 +1,20 @@
 class IdeasPhase < ApplicationRecord
   belongs_to :idea
   belongs_to :phase
-  counter_culture :phase, column_name: 'ideas_count'
+  counter_culture :phase, column_name: 'ideas_count', touch: true
 
   validates :idea, :phase, presence: true
   validates :phase_id, uniqueness: {scope: :idea_id}
   validate :idea_and_phase_same_project
   validate :phase_is_ideation
 
-
-  private 
+  private
 
   def idea_and_phase_same_project
     unless idea.project_id == phase.project_id
       self.errors.add(
-        :base, 
-        :idea_and_phase_not_same_project, 
+        :base,
+        :idea_and_phase_not_same_project,
         message: 'The idea and the phase do not belong to the same project'
       )
     end
@@ -24,8 +23,8 @@ class IdeasPhase < ApplicationRecord
   def phase_is_ideation
     unless phase.can_contain_ideas?
       self.errors.add(
-        :phase_id, 
-        :phase_not_ideation, 
+        :phase_id,
+        :phase_not_ideation,
         message: 'You can\'t add an idea to a non-ideation phase'
       )
     end
