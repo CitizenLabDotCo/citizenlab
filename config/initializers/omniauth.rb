5bc2de3c269b9ca2ce33cf7ede21954037c8e3bb
Adrien Dessy <adrien@citizenlab.co>
2021-03-03 17:50:06 +0100

    CL2-6319 Patch OmniAuth::Strategies::OpenIDConnect to allow dynamic specification of the issuer.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -22,10 +22,22 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, :setup => FACEBOOK_SETUP_PROC
   provider :google_oauth2, :setup => GOOGLE_SETUP_PROC, name: 'google'
   provider :azure_activedirectory, :setup => AZURE_AD_SETUP_PROC
-  provider :openid_connect, :setup => FRANCECONNECT_SETUP_PROC, name: 'franceconnect'
+  provider :openid_connect, :setup => FRANCECONNECT_SETUP_PROC, name: 'franceconnect', issuer: OmniauthMethods::FranceConnect.new.method(:issuer)
   provider :openid_connect, :setup => BOSA_FAS_SETUP_PROC, name: 'bosa_fas'
 end
 
 OmniAuth.config.full_host = -> (_env) {
   AppConfiguration.instance&.base_backend_uri
 }
+
+
+module OpenIDConnectPatch
+  # Patch +OmniAuth::Strategies::OpenIDConnect+ to allow dynamic specification of the issuer.
+  def issuer
+    return options.issuer.call(env) if options.issuer && options.issuer.respond_to?(:call)
+
+    super
+  end
+end
+
+OmniAuth::Strategies::OpenIDConnect.prepend(OpenIDConnectPatch)
