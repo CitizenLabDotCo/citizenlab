dbb8557c9c64e6cad632d36e8b3aed4904350516
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2021-03-04 22:45:37 +0100

    Moved franceconnect settings into engine

--- engines/id_franceconnect/lib/id_franceconnect/engine.rb
+++ engines/id_franceconnect/lib/id_franceconnect/engine.rb
@@ -5,6 +5,8 @@ module IdFranceconnect
     isolate_namespace IdFranceconnect
 
     config.to_prepare do
+      AppConfiguration::Settings.add_feature(IdFranceconnect::FeatureSpecification)
+
       fc = FranceconnectOmniauth.new
       AuthenticationService.add_method('franceconnect', fc)
       Verification::VerificationService.add_method(fc)
