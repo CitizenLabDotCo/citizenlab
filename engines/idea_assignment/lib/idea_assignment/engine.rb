4994fae63da5c174815b937d3049088e04950869
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-04 15:58:44 +0100

    require factories

--- engines/idea_assignment/lib/idea_assignment/engine.rb
+++ engines/idea_assignment/lib/idea_assignment/engine.rb
@@ -1,6 +1,18 @@
+# frozen_string_literal: true
+
+# rubocop:disable Lint/SuppressedException
+begin
+  require 'factory_bot_rails'
+rescue LoadError
+end
+# rubocop:enable Lint/SuppressedException
+
 module IdeaAssignment
   class Engine < ::Rails::Engine
     isolate_namespace IdeaAssignment
+
+    factories_path = File.expand_path('../../spec/factories', __dir__)
+    config.factory_bot.definition_file_paths += [factories_path] if defined?(FactoryBotRails)
 
     config.to_prepare do
       ::NotificationToSerializerMapper.add_to_map(
