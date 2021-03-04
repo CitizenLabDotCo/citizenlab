a33aaa31ec99812460345bf33086d0d489db2d36
David <david@citizenlab.co>
2021-03-04 14:44:08 +0100

    mapping WIP

--- app/utils/dateUtils.ts
+++ app/utils/dateUtils.ts
@@ -3,26 +3,31 @@ import { isString } from 'lodash-es';
 
 export function pastPresentOrFuture(input: string | [string, string]) {
   if (isString(input)) {
-    const isoDate = moment(input).format('YYYY-MM-DD');
+    const isoDate = getIsoDate(input);
     const currentIsoDate = moment().format('YYYY-MM-DD');
 
     if (isoDate === currentIsoDate) {
       return 'present';
-    } else if (moment(currentIsoDate).isAfter(isoDate)) {
+    } else if (moment(currentIsoDate, 'YYYY-MM-DD').isAfter(isoDate)) {
       return 'past';
     }
 
     return 'future';
   } else {
-    const startIsoDate = moment(input[0]).format('YYYY-MM-DD');
-    const endIsoDate = moment(input[1]).format('YYYY-MM-DD');
+    const startIsoDate = getIsoDate(input[0]);
+    const endIsoDate = getIsoDate(input[1]);
     const currentIsoDate = moment().format('YYYY-MM-DD');
 
     if (
-      moment(currentIsoDate).isBetween(startIsoDate, endIsoDate, 'days', '[]')
+      moment(currentIsoDate, 'YYYY-MM-DD').isBetween(
+        startIsoDate,
+        endIsoDate,
+        'days',
+        '[]'
+      )
     ) {
       return 'present';
-    } else if (moment(currentIsoDate).isAfter(endIsoDate)) {
+    } else if (moment(currentIsoDate, 'YYYY-MM-DD').isAfter(endIsoDate)) {
       return 'past';
     }
 
@@ -31,11 +36,11 @@ export function pastPresentOrFuture(input: string | [string, string]) {
 }
 
 export function getIsoDate(date: string) {
-  return moment(date).format('YYYY-MM-DD');
+  return moment(new Date(date)).format('YYYY-MM-DD');
 }
 
 export function getDaysRemainingUntil(date: string): number {
-  return moment(date).diff(moment({ hours: 0 }), 'days');
+  return moment(new Date(date)).diff(moment({ hours: 0 }), 'days');
 }
 
 export function convertSecondsToDDHHMM(seconds: number) {
