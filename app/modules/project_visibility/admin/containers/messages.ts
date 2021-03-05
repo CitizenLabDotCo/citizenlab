8768b672c356dc31833c05950af1d7e327d2d075
brentguf <brent.guffens@gmail.com>
2021-03-05 09:39:15 +0100

    Admin container index messages

---
+++ app/modules/project_visibility/admin/containers/messages.ts
@@ -0,0 +1,36 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  permissionsEveryoneLabel: {
+    id: 'app.containers.admin.project.permissions.permissionsEveryoneLabel',
+    defaultMessage: 'Everyone',
+  },
+  permissionsAdministrators: {
+    id: 'app.containers.admin.project.permissions.permissionsAdministrators',
+    defaultMessage: 'Administrators',
+  },
+  permissionsSelectionLabel: {
+    id: 'app.containers.admin.project.permissions.permissionsSelectionLabel',
+    defaultMessage: 'Selection',
+  },
+  viewingRightsTitle: {
+    id: 'app.containers.admin.project.permissions.viewingRightsTitle',
+    defaultMessage: 'Who can see this project?',
+  },
+  save: {
+    id: 'app.containers.AdminPage.groups.permissions.save',
+    defaultMessage: 'Save',
+  },
+  saveSuccess: {
+    id: 'app.containers.AdminPage.groups.permissions.saveSuccess',
+    defaultMessage: 'Success!',
+  },
+  saveErrorMessage: {
+    id: 'app.containers.AdminPage.groups.permissions.saveErrorMessage',
+    defaultMessage: 'Something went wrong, please try again later.',
+  },
+  saveSuccessMessage: {
+    id: 'app.containers.AdminPage.groups.permissions.saveSuccessMessage',
+    defaultMessage: 'Your changes have been saved.',
+  },
+});
