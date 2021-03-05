f7d699ce6b1fbb46204f1224bd498b9cb60fabad
brentguf <brent.guffens@gmail.com>
2021-03-05 08:29:32 +0100

    Module index WIP

---
+++ app/modules/project_visibility/index.tsx
@@ -0,0 +1,28 @@
+import useFeatureFlag from 'hooks/useFeatureFlag';
+import React, { ReactNode } from 'react';
+import { ModuleConfiguration } from 'utils/moduleUtils';
+import ProjectVisibility from './admin/containers/index';
+
+type RenderOnFeatureFlagProps = {
+  children: ReactNode;
+};
+
+const RenderOnFeatureFlag = ({ children }: RenderOnFeatureFlagProps) => {
+  const isEnabled = useFeatureFlag('project_visibility');
+  if (isEnabled) {
+    return <>{children}</>;
+  }
+  return null;
+};
+
+const configuration: ModuleConfiguration = {
+  outlets: {
+    'app.containers.Admin.project.edit.permissions': (props) => (
+      <RenderOnFeatureFlag>
+        <ProjectVisibility {...props} />
+      </RenderOnFeatureFlag>
+    ),
+  },
+};
+
+export default configuration;
