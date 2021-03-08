31aff6f7024d7f2ff811acc2cf157edfe7a4c67e
Sara <sara@citizenlab.co>
2021-03-08 15:04:08 +0100

    When we don't know where the data goes, no need to load the tracking code.

--- app/modules/matomo/index.ts
+++ app/modules/matomo/index.ts
@@ -63,6 +63,13 @@ const configuration: ModuleConfiguration = {
       initializeFor('matomo'),
     ]).subscribe(([appConfiguration, user, _]) => {
       if (!MATOMO_HOST) return;
+
+      if (
+        !appConfiguration.data.attributes.settings.matomo?.product_site_id &&
+        appConfiguration.data.attributes.settings.matomo?.tenant_site_id
+      )
+        return;
+
       /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
       window._paq = window._paq || [];
 
