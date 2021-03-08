606dbb1080911d0465696112d13b37468e956c45
Adrien Dessy <adrien@citizenlab.co>
2021-03-08 10:21:20 +0100

    CL2-6319 Introduce Permission#denied?(user)

--- spec/factories/permissions.rb
+++ spec/factories/permissions.rb
@@ -4,5 +4,17 @@ FactoryBot.define do
     permitted_by { 'groups' }
     permission_scope { create(:continuous_project, participation_method: 'ideation') }
     groups { [] }
+
+    trait :by_everyone do
+      permitted_by { 'everyone' }
+    end
+
+    trait :by_admins_moderators do
+      permitted_by { 'admins_moderators' }
+    end
+
+    trait :by_users do
+      permitted_by { 'users' }
+    end
   end
 end
