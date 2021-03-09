b5d3522241e149a28afc16a786f41605b8d93ab0
Adrien Dessy <adrien@citizenlab.co>
2021-03-09 16:51:59 +0100

    CL2-6319 Remove duplicate tests

--- spec/services/permissions_service_spec.rb
+++ spec/services/permissions_service_spec.rb
@@ -1,132 +1,34 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 describe PermissionsService do
-  let(:service) { PermissionsService.new }
+  let(:service) { described_class.new }
 
-  describe "posting_initiative_disabled_reason" do
+  describe '#denied?' do
+    let(:action) { 'posting_initiative' }
+    let(:permission) { Permission.find_by(permission_scope: nil, action: action) }
+    let(:user) { create(:user) }
+
     before do
       service.update_global_permissions
     end
 
-    it "returns nil when posting is allowed" do
-      permission = Permission.find_by(permission_scope: nil, action: 'posting_initiative')
+    it 'returns nil when action is allowed' do
       groups = create_list(:group, 2)
+      groups.first.add_member(user).save!
       permission.update!(permitted_by: 'groups', group_ids: groups.map(&:id))
-      user = create(:user)
-      group = groups.first
-      group.add_member user
-      group.save!
-      expect(service.posting_initiative_disabled_reason(user)).to be_nil
+      expect(service.denied?(user, action)).to be_nil
     end
 
-    it "returns `not_signed_in` when user needs to be signed in" do
-      permission = Permission.find_by(permission_scope: nil, action: 'posting_initiative')
+    it 'returns `not_signed_in` when user needs to be signed in' do
       permission.update!(permitted_by: 'users')
-      expect(service.posting_initiative_disabled_reason(nil)).to eq 'not_signed_in'
+      expect(service.denied?(nil, action)).to eq 'not_signed_in'
     end
 
-    it "returns `not_permitted` when posting is not permitted" do
-      permission = Permission.find_by(permission_scope: nil, action: 'posting_initiative')
-      permission.update!(permitted_by: 'groups', 
-        group_ids: create_list(:group, 2).map(&:id)
-        )
-      expect(service.posting_initiative_disabled_reason(create(:user))).to eq 'not_permitted'
-    end
-  end
-
-  describe "commenting_initiative_disabled_reason" do
-    before do
-      service.update_global_permissions
-    end
-
-    it "returns nil when the commenting is allowed" do
-      expect(service.commenting_initiative_disabled_reason(create(:user))).to be_nil
-    end
-
-    it "returns `not_signed_in` when user needs to be signed in" do
-      permission = Permission.find_by(permission_scope: nil, action: 'commenting_initiative')
-      permission.update!(permitted_by: 'users')
-      expect(service.commenting_initiative_disabled_reason(nil)).to eq 'not_signed_in'
-    end
-
-    it "returns `not_permitted` when commenting is not permitted for the user" do
-      permission = Permission.find_by(permission_scope: nil, action: 'commenting_initiative')
-      permission.update!(permitted_by: 'groups', 
-        group_ids: create_list(:group, 2).map(&:id)
-        )
-      expect(service.commenting_initiative_disabled_reason(create(:user))).to eq 'not_permitted'
-    end
-  end
-
-  describe "voting_initiative_disabled_reason" do
-    before do
-      service.update_global_permissions
-    end
-
-    it "returns nil when the voting is allowed" do
-      expect(service.voting_initiative_disabled_reason(create(:user))).to be_nil
-    end
-
-    it "returns `not_signed_in` when user needs to be signed in" do
-      permission = Permission.find_by(permission_scope: nil, action: 'voting_initiative')
-      permission.update!(permitted_by: 'users')
-      expect(service.voting_initiative_disabled_reason(nil)).to eq 'not_signed_in'
-    end
-
-    it "returns `not_permitted` when voting is not permitted for the user" do
-      permission = Permission.find_by(permission_scope: nil, action: 'voting_initiative')
-      permission.update!(permitted_by: 'groups', 
-        group_ids: create_list(:group, 2).map(&:id)
-        )
-      expect(service.voting_initiative_disabled_reason(create(:user))).to eq 'not_permitted'
-    end
-  end
-
-  describe "cancelling_votes_disabled_reason_for_initiative" do
-    before do
-      service.update_global_permissions
-    end
-
-    it "returns nil when the voting is allowed" do
-      expect(service.cancelling_votes_disabled_reason_for_initiative(create(:user))).to be_nil
-    end
-
-    it "returns `not_signed_in` when user needs to be signed in" do
-      permission = Permission.find_by(permission_scope: nil, action: 'voting_initiative')
-      permission.update!(permitted_by: 'users')
-      expect(service.cancelling_votes_disabled_reason_for_initiative(nil)).to eq 'not_signed_in'
-    end
-
-    it "returns `not_permitted` when voting is not permitted for the user" do
-      permission = Permission.find_by(permission_scope: nil, action: 'voting_initiative')
-      permission.update!(permitted_by: 'groups', 
-        group_ids: create_list(:group, 2).map(&:id)
-        )
-      expect(service.cancelling_votes_disabled_reason_for_initiative(create(:user))).to eq 'not_permitted'
-    end
-  end
-
-  describe "voting_disabled_reason_for_initiative_comment" do
-    before do
-      service.update_global_permissions
-    end
-
-    it "returns nil when the commenting is allowed" do
-      expect(service.voting_disabled_reason_for_initiative_comment(create(:user))).to be_nil
-    end
-
-    it "returns `not_signed_in` when user needs to be signed in" do
-      permission = Permission.find_by(permission_scope: nil, action: 'commenting_initiative')
-      permission.update!(permitted_by: 'users')
-      expect(service.voting_disabled_reason_for_initiative_comment(nil)).to eq 'not_signed_in'
-    end
-
-    it "returns `not_permitted` when commenting is not permitted for the user" do
-      permission = Permission.find_by(permission_scope: nil, action: 'commenting_initiative')
-      permission.update!(permitted_by: 'groups', 
-        group_ids: create_list(:group, 2).map(&:id)
-        )
-      expect(service.voting_disabled_reason_for_initiative_comment(create(:user))).to eq 'not_permitted'
+    it 'returns `not_permitted` when user is not in authorized groups' do
+      permission.update!(permitted_by: 'groups', group_ids: create_list(:group, 2).map(&:id))
+      expect(service.denied?(user, action)).to eq 'not_permitted'
     end
   end
 end
