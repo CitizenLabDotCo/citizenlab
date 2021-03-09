ed01f627c3920fb73732401661260e1045675b3c
brentguf <brent.guffens@gmail.com>
2021-03-09 15:50:51 +0100

    Remove duplicate strings

--- app/containers/Admin/projects/edit/permissions/messages.ts
+++ app/containers/Admin/projects/edit/permissions/messages.ts
@@ -60,24 +60,6 @@ export default defineMessages({
     id: 'app.containers.AdminPage.groups.permissions.saveSuccessMessage',
     defaultMessage: 'Your changes have been saved.',
   },
-  moderatorsSectionTitle: {
-    id: 'app.containers.AdminPage.ProjectEdit.moderatorsSectionTitle',
-    defaultMessage: 'Who can moderate this project?',
-  },
-  projectManagerTooltipContent: {
-    id: 'app.containers.AdminPage.ProjectEdit.projectManagerTooltipContent',
-    defaultMessage:
-      'Project managers can edit projects, manage inputs and email participants. You can {moderationInfoCenterLink} to find more information about the rights assigned to project managers.',
-  },
-  moderationInfoCenterLinkText: {
-    id: 'app.containers.AdminPage.ProjectEdit.moderationInfoCenterLinkText',
-    defaultMessage: 'visit our Help Center',
-  },
-  moreInfoModeratorLink: {
-    id: 'app.containers.AdminPage.ProjectEdit.moreInfoModeratorLink',
-    defaultMessage:
-      'http://support.citizenlab.co/en-your-citizenlab-platform-step-by-step/set-up/pointing-out-the-right-project-moderators',
-  },
   inputAssignmentSectionTitle: {
     id: 'app.containers.AdminPage.ProjectEdit.inputAssignmentSectionTitle',
     defaultMessage: 'Who is responsible for processing the inputs?',
