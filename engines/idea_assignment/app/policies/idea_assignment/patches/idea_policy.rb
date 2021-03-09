a2298dfabc751663be3cd3e40c0d3398acd9abd9
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-09 16:37:47 +0100

    fix policies

--- engines/idea_assignment/app/policies/idea_assignment/patches/idea_policy.rb
+++ engines/idea_assignment/app/policies/idea_assignment/patches/idea_policy.rb
@@ -2,7 +2,9 @@ module IdeaAssignment
   module Patches
     module IdeaPolicy
       def permitted_attributes
-        super.push(:assignee_id) if admin_or_project_moderator?
+        super.tap do |attributes|
+          attributes.push(:assignee_id) if admin_or_project_moderator?
+        end
       end
     end
   end
