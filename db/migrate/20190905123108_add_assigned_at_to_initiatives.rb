5b59a138e2dec56f13e07f196b2c8885a11b54b2
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-09 14:17:11 +0100

    split assigned_at migrations

---
+++ db/migrate/20190905123108_add_assigned_at_to_initiatives.rb
@@ -0,0 +1,13 @@
+class AddAssignedAtToInitiatives < ActiveRecord::Migration[5.2]
+  def change
+    add_column :initiatives, :assigned_at, :timestamp
+
+    now = Time.zone.now
+    Initiative.where.not(assignee_id: nil).each do |initiative|
+      assigned_at = Activity.where(item: initiative,
+                                   action: 'changed_assignee').order(created_at: :desc).first&.created_at
+      assigned_at ||= now
+      initiative.update_columns assigned_at: assigned_at
+    end
+  end
+end
