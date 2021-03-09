d56a1e16e74fe21c4a0c4d0f208f09f12ea7b39c
brentguf <brent.guffens@gmail.com>
2021-03-09 15:53:09 +0100

    Delete duplicate strings

--- app/containers/Admin/projects/edit/permissions/messages.ts
+++ app/containers/Admin/projects/edit/permissions/messages.ts
@@ -17,32 +17,9 @@ export default defineMessages({
     id: 'app.containers.admin.project.permissions.viewingRightsTitle',
     defaultMessage: 'Who can see this project?',
   },
-  add: {
-    id: 'app.containers.AdminPage.groups.permissions.add',
-    defaultMessage: 'Add',
-  },
-  groupsMultipleSelectPlaceholder: {
-    id:
-      'app.containers.AdminPage.groups.permissions.groupsMultipleSelectPlaceholder',
-    defaultMessage: 'Select one or more groups',
-  },
-  members: {
-    id: 'app.containers.AdminPage.groups.permissions.members',
-    defaultMessage:
-      '{count, plural, =0 {No members} one {1 member} other {{count} members}}',
-  },
-  deleteButtonLabel: {
-    id: 'app.containers.AdminPage.groups.permissions.deleteButtonLabel',
-    defaultMessage: 'Delete',
-  },
   editButtonLabel: {
     id: 'app.containers.AdminPage.groups.permissions.editButtonLabel',
     defaultMessage: 'Edit',
-  },
-  groupDeletionConfirmation: {
-    id: 'app.containers.AdminPage.groups.permissions.groupDeletionConfirmation',
-    defaultMessage:
-      'Are you sure you want to remove this group from the project?',
   },
   save: {
     id: 'app.containers.AdminPage.groups.permissions.save',
