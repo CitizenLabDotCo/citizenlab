cec4ece7f178096b6b2627e892c4f1febc7b14c7
brentguf <brent.guffens@gmail.com>
2021-03-10 14:39:58 +0100

    admin messages

---
+++ app/modules/admin_project_templates/admin/messages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  fromATemplate: {
+    id: 'app.containers.AdminPage.ProjectEdit.fromATemplate',
+    defaultMessage: 'From a template',
+  },
+  fromScratch: {
+    id: 'app.containers.AdminPage.ProjectEdit.fromScratch',
+    defaultMessage: 'From scratch',
+  },
+});
