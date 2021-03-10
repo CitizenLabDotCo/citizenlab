bd10cceee5c48f70d1d7f9124000a58a90c0d77b
brentguf <brent.guffens@gmail.com>
2021-03-10 11:03:28 +0100

    Messages

--- app/modules/admin_project_templates/admin/messages.ts
+++ app/modules/admin_project_templates/admin/messages.ts
@@ -1,24 +1,6 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  overviewPageTitle: {
-    id: 'app.containers.AdminPage.ProjectDashboard.overviewPageTitle',
-    defaultMessage: 'Projects',
-  },
-  overviewPageSubtitle: {
-    id: 'app.containers.AdminPage.ProjectDashboard.overviewPageSubtitle',
-    defaultMessage:
-      'Create as many projects as you want and edit them at any time. Drag and drop them to change the order in which you want to see them on your homepage.',
-  },
-  overviewPageSubtitleModerator: {
-    id:
-      'app.containers.AdminPage.ProjectDashboard.overviewPageSubtitleModerator',
-    defaultMessage: 'Here are the projects on the platform you are moderating.',
-  },
-  createAProject: {
-    id: 'app.containers.AdminPage.ProjectEdit.createAProject',
-    defaultMessage: 'Create a project',
-  },
   fromATemplate: {
     id: 'app.containers.AdminPage.ProjectEdit.fromATemplate',
     defaultMessage: 'From a template',
@@ -26,57 +8,5 @@ export default defineMessages({
   fromScratch: {
     id: 'app.containers.AdminPage.ProjectEdit.fromScratch',
     defaultMessage: 'From scratch',
-  },
-  departments: {
-    id: 'app.containers.AdminPage.ProjectEdit.departments',
-    defaultMessage: 'Departments',
-  },
-  purposes: {
-    id: 'app.containers.AdminPage.ProjectEdit.purposes',
-    defaultMessage: 'Purposes',
-  },
-  participationLevels: {
-    id: 'app.containers.AdminPage.ProjectEdit.participationLevels',
-    defaultMessage: 'Participation levels',
-  },
-  searchPlaceholder: {
-    id: 'app.containers.AdminPage.ProjectEdit.searchPlaceholder',
-    defaultMessage: 'Search the templates',
-  },
-  useTemplate: {
-    id: 'app.containers.AdminPage.ProjectEdit.useTemplate',
-    defaultMessage: 'Use template',
-  },
-  moreDetails: {
-    id: 'app.containers.AdminPage.ProjectEdit.moreDetails',
-    defaultMessage: 'More details',
-  },
-  loadMoreTemplates: {
-    id: 'app.containers.AdminPage.ProjectEdit.loadMoreTemplates',
-    defaultMessage: 'Load more templates',
-  },
-  published: {
-    id: 'app.containers.AdminPage.ProjectDashboard.published',
-    defaultMessage: 'Published',
-  },
-  draft: {
-    id: 'app.containers.AdminPage.ProjectEdit.draft',
-    defaultMessage: 'Draft',
-  },
-  archived: {
-    id: 'app.containers.AdminPage.ProjectEdit.archived',
-    defaultMessage: 'Archived',
-  },
-  noTemplatesFound: {
-    id: 'app.containers.AdminPage.ProjectEdit.noTemplatesFound',
-    defaultMessage: 'No templates found',
-  },
-  createAProjectFromATemplate: {
-    id: 'app.containers.AdminPage.ProjectEdit.createAProjectFromATemplate',
-    defaultMessage: 'Create a project from a template',
-  },
-  existingProjects: {
-    id: 'app.containers.AdminPage.projects.all.existingProjects',
-    defaultMessage: 'Existing projects',
   },
 });
