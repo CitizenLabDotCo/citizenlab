526b3ad3355fe0cd567aa67ef298320bb3a83d6f
brentguf <brent.guffens@gmail.com>
2021-03-10 12:18:56 +0100

    No feature flagging in module index

--- app/modules/admin_project_templates/index.tsx
+++ app/modules/admin_project_templates/index.tsx
@@ -1,25 +1,11 @@
-import useFeatureFlag from 'hooks/useFeatureFlag';
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { ModuleConfiguration } from 'utils/moduleUtils';
 import AdminProjectTemplates from './admin/containers';
-type RenderOnFeatureFlagProps = {
-  children: ReactNode;
-};
-
-const RenderOnFeatureFlag = ({ children }: RenderOnFeatureFlagProps) => {
-  const isEnabled = useFeatureFlag('admin_project_templates');
-  if (isEnabled) {
-    return <>{children}</>;
-  }
-  return null;
-};
 
 const configuration: ModuleConfiguration = {
   outlets: {
     'app.containers.Admin.project.edit.permissions': (props) => (
-      <RenderOnFeatureFlag>
-        <AdminProjectTemplates />
-      </RenderOnFeatureFlag>
+      <AdminProjectTemplates />
     ),
   },
 };
