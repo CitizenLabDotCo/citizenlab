f99d3e9ca4b577f05db0910c4abd7668ffbb9312
Adrien Dessy <adrien@citizenlab.co>
2021-03-10 13:41:36 +0100

    CL2-6319 Add the feature specification for granular permissions

--- engines/granular_permissions/lib/granular_permissions/engine.rb
+++ engines/granular_permissions/lib/granular_permissions/engine.rb
@@ -2,5 +2,10 @@ module GranularPermissions
   class Engine < ::Rails::Engine
     isolate_namespace GranularPermissions
     config.generators.api_only = true
+
+    config.to_prepare do
+      require 'granular_permissions/feature_specification'
+      AppConfiguration::Settings.add_feature(GranularPermissions::FeatureSpecification)
+    end
   end
 end
