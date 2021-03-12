99af49c42b42740b8a84f73a00290ddf04912adb
brentguf <brent.guffens@gmail.com>
2021-03-12 12:33:24 +0100

    Remove messages diff

--- app/containers/Admin/projects/edit/permissions/messages.ts
+++ app/containers/Admin/projects/edit/permissions/messages.ts
@@ -1,14 +1,6 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  participationAccessRightsTitle: {
-    id: 'app.containers.AdminPage.ProjectEdit.participationAccessRightsTitle',
-    defaultMessage: 'Participation',
-  },
-  moderationRightsTitle: {
-    id: 'app.containers.AdminPage.ProjectEdit.moderationRightsTitle',
-    defaultMessage: 'Moderation',
-  },
   permissionsEveryoneLabel: {
     id: 'app.containers.admin.project.permissions.permissionsEveryoneLabel',
     defaultMessage: 'Everyone',
@@ -20,5 +12,13 @@ export default defineMessages({
   permissionsSelectionLabel: {
     id: 'app.containers.admin.project.permissions.permissionsSelectionLabel',
     defaultMessage: 'Selection',
+  },
+  participationAccessRightsTitle: {
+    id: 'app.containers.AdminPage.ProjectEdit.participationAccessRightsTitle',
+    defaultMessage: 'Participation',
+  },
+  moderationRightsTitle: {
+    id: 'app.containers.AdminPage.ProjectEdit.moderationRightsTitle',
+    defaultMessage: 'Moderation',
   },
 });
