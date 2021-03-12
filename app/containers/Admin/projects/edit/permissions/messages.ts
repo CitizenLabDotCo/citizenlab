5cf6261fbd6d12b235a69d3a29700cfa8db98c30
brentguf <brent.guffens@gmail.com>
2021-03-12 12:14:00 +0100

    Remove unused copy

--- app/containers/Admin/projects/edit/permissions/messages.ts
+++ app/containers/Admin/projects/edit/permissions/messages.ts
@@ -1,48 +1,6 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  editButtonLabel: {
-    id: 'app.containers.AdminPage.groups.permissions.editButtonLabel',
-    defaultMessage: 'Edit',
-  },
-  inputAssignmentSectionTitle: {
-    id: 'app.containers.AdminPage.ProjectEdit.inputAssignmentSectionTitle',
-    defaultMessage: 'Who is responsible for processing the inputs?',
-  },
-  inputAssignmentTooltipText: {
-    id: 'app.containers.AdminPage.ProjectEdit.inputAssignmentTooltipText',
-    defaultMessage:
-      'All new inputs in this project will be assigned to this person. The assignee can be changed in the {ideaManagerLink}.',
-  },
-  inputManagerLinkText: {
-    id: 'app.containers.AdminPage.ProjectEdit.inputManagerLinkText',
-    defaultMessage: 'input manager',
-  },
-  unassigned: {
-    id: 'app.containers.AdminPage.ProjectEdit.unassigned',
-    defaultMessage: 'Unassigned',
-  },
-  groupDeletionConfirmation: {
-    id: 'app.containers.AdminPage.groups.permissions.groupDeletionConfirmation',
-    defaultMessage:
-      'Are you sure you want to remove this group from the project?',
-  },
-  save: {
-    id: 'app.containers.AdminPage.groups.permissions.save',
-    defaultMessage: 'Save',
-  },
-  saveSuccess: {
-    id: 'app.containers.AdminPage.groups.permissions.saveSuccess',
-    defaultMessage: 'Success!',
-  },
-  saveErrorMessage: {
-    id: 'app.containers.AdminPage.groups.permissions.saveErrorMessage',
-    defaultMessage: 'Something went wrong, please try again later.',
-  },
-  saveSuccessMessage: {
-    id: 'app.containers.AdminPage.groups.permissions.saveSuccessMessage',
-    defaultMessage: 'Your changes have been saved.',
-  },
   participationAccessRightsTitle: {
     id: 'app.containers.AdminPage.ProjectEdit.participationAccessRightsTitle',
     defaultMessage: 'Participation',
