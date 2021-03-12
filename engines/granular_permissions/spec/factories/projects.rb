927077f0b7c8b548d6c04eb0b5447e59c612453c
Adrien Dessy <adrien@citizenlab.co>
2021-03-12 09:39:33 +0100

    CL2-6319 Remove debug instructions

--- engines/granular_permissions/spec/factories/projects.rb
+++ engines/granular_permissions/spec/factories/projects.rb
@@ -3,7 +3,6 @@
 FactoryBot.modify do
   factory :project do
     after(:create) do |project|
-      require 'pry' ; binding.pry
       PermissionsService.new.update_permissions_for_scope(project)
     end
   end
