a58e144660e51c15e671cb0675f5378a669d6847
brentguf <brent.guffens@gmail.com>
2021-03-12 11:53:45 +0100

    Add missing copy

--- app/containers/Admin/projects/edit/permissions/messages.ts
+++ app/containers/Admin/projects/edit/permissions/messages.ts
@@ -30,4 +30,16 @@ export default defineMessages({
     id: 'app.containers.AdminPage.ProjectEdit.moderationRightsTitle',
     defaultMessage: 'Moderation',
   },
+  permissionsEveryoneLabel: {
+    id: 'app.containers.admin.project.permissions.permissionsEveryoneLabel',
+    defaultMessage: 'Everyone',
+  },
+  permissionsAdministrators: {
+    id: 'app.containers.admin.project.permissions.permissionsAdministrators',
+    defaultMessage: 'Administrators',
+  },
+  permissionsSelectionLabel: {
+    id: 'app.containers.admin.project.permissions.permissionsSelectionLabel',
+    defaultMessage: 'Selection',
+  },
 });
