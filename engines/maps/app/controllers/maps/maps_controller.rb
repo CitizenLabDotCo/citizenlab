f246a893c9ad21e01b27522b9f08cbf2c9d2cebf
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-13 15:48:05 +0100

    fix module name

---
+++ engines/maps/app/controllers/maps/maps_controller.rb
@@ -0,0 +1,13 @@
+module CustomMaps
+  class MapsController < ::ApplicationController
+    before_action :set_project
+    skip_before_action :authenticate_user, only: %i[show]
+
+    private
+
+    def set_project
+      @project = Project.find(params[:project_id])
+      authorize @project
+    end
+  end
+end
