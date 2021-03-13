bafa3e1fd700f81c91e0d21eee9419149d7cb786
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-13 15:54:34 +0100

    fix rules service

---
+++ engines/smart_groups/app/services/concerns/smart_groups/rulable_service.rb
@@ -0,0 +1,31 @@
+module SmartGroups
+  module RulableService
+    extend ActiveSupport::Concern
+
+    included do
+      delegate :rules_by_type, :rules, :each_rule, :rule_by_type, to: :class
+    end
+
+    class_methods do
+      def rules
+        @rules ||= []
+      end
+
+      def add_rules(*rule_classes)
+        rules.push(rule_classes)
+      end
+
+      def rules_by_type
+        rules.index_by(&:rule_type)
+      end
+
+      def each_rule
+        rules_by_type.values.each
+      end
+
+      def rule_by_type(rule_type)
+        rules_by_type[rule_type]
+      end
+    end
+  end
+end
