8fc8dd78a748e652a2806fc38640c3e7055cd335
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-13 16:04:25 +0100

    load factories

--- engines/custom_maps/lib/custom_maps/engine.rb
+++ engines/custom_maps/lib/custom_maps/engine.rb
@@ -1,6 +1,17 @@
+# frozen_string_literal: true
+
+# rubocop:disable Lint/SuppressedException
+begin
+  require 'factory_bot_rails'
+rescue LoadError
+end
+# rubocop:enable Lint/SuppressedException
 module CustomMaps
   class Engine < ::Rails::Engine
     isolate_namespace CustomMaps
+
+    factories_path = File.expand_path('../../spec/factories', __dir__)
+    config.factory_bot.definition_file_paths += [factories_path] if defined?(FactoryBotRails)
 
     config.to_prepare do
       require 'custom_maps/feature_specification'
