e78018e560d808db863e49516dc3d10501135e3e
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-14 22:06:36 +0100

    fix verified test

--- engines/verification/lib/verification/engine.rb
+++ engines/verification/lib/verification/engine.rb
@@ -1,11 +1,12 @@
+require 'verification/smart_groups/rules/verified'
+
 module Verification
   class Engine < ::Rails::Engine
     isolate_namespace Verification
 
     config.to_prepare do
       if defined? ::SmartGroups::RulesService
-        require 'verification/smart_groups/rules/verified'
-        ::SmartGroups::RulesService.add_rule(SmartGroups::Rules::Verified)
+        ::SmartGroups::RulesService.add_rule(::Verification::SmartGroups::Rules::Verified)
       end
     end
   end
