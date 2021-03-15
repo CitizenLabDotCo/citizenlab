af9c11681dc646c60d5021e8c6c10af5d0f083dd
Adrien Dessy <adrien@citizenlab.co>
2021-03-15 23:47:04 +0100

    CL2-6319 Refactor granular-permissions specs

--- spec/services/permissions_service_spec.rb
+++ spec/services/permissions_service_spec.rb
@@ -5,8 +5,29 @@ require 'rails_helper'
 describe PermissionsService do
   let(:service) { described_class.new }
 
+  before(:all) do
+    # rubocop:disable RSpec/BeforeAfterAll
+    @scope_types = PermissionsService.instance_variable_get(:@scope_spec_hash)
+
+    # rubocop:disable Style/SingleLineMethods Layout/EmptyLineBetweenDefs
+    dummy_global_scope = Module.new do
+      def self.actions(_scope = nil) %w[action] end
+      def self.scope_type; nil end
+      def self.scope_class; nil end
+    end
+    # rubocop:enable Style/SingleLineMethods Layout/EmptyLineBetweenDefs
+
+    PermissionsService.clear_scope_types
+    PermissionsService.register_scope_type(dummy_global_scope)
+  end
+
+  after(:all) do
+    # Restore registered scope-types as they were before the tests.
+    PermissionsService.instance_variable_set(:@scope_spec_hash, @scope_types)
+  end
+
   describe '#denied?' do
-    let(:action) { 'posting_initiative' }
+    let(:action) { 'action' }
     let(:permission) { Permission.find_by(permission_scope: nil, action: action) }
     let(:user) { create(:user) }
 
