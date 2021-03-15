630797b2ebc2a26c25ebd0fc23d549a78ac3db38
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-15 23:36:03 +0100

    load factory bot

--- plugins/citizen_lab/lib/citizen_lab/engine_config.rb
+++ plugins/citizen_lab/lib/citizen_lab/engine_config.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
+# rubocop:disable Lint/SuppressedException
+begin
+  require 'factory_bot_rails'
+rescue LoadError
+end
+# rubocop:enable Lint/SuppressedException
+
 module CitizenLab
   module EngineConfig
     def self.included(base)
@@ -19,6 +28,9 @@ module CitizenLab
         else
           config.eager_load_paths << "#{config.root}/lib"
         end
+
+        factories_path = File.expand_path('../../spec/factories', __dir__)
+        config.factory_bot.definition_file_paths += [factories_path] if defined?(FactoryBotRails)
 
         config.to_prepare(&method(:add_feature_spec).to_proc)
 
