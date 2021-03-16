b0921f17fb7e9664b23572f236ebd7b7bed1a3bd
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2021-03-16 16:34:57 +0100

    Corrected typos and translation keys

--- app/modules/id_id_card_lookup/messages.ts
+++ app/modules/id_id_card_lookup/messages.ts
@@ -2,39 +2,39 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   takenFormError: {
-    id: 'app/modules/id_id_card_lookup.takenFormError',
+    id: 'app.modules.id_id_card_lookup.takenFormError',
     defaultMessage: 'Already taken.',
   },
   emptyFieldError: {
-    id: 'app/modules/id_id_card_lookup.emptyFieldError',
+    id: 'app.modules.id_id_card_lookup.emptyFieldError',
     defaultMessage: 'This field cannot be empty.',
   },
   noMatchFormError: {
-    id: 'app/modules/id_id_card_lookup.noMatchFormError',
+    id: 'app.modules.id_id_card_lookup.noMatchFormError',
     defaultMessage: 'No match was found.',
   },
   somethingWentWrongError: {
-    id: 'app/modules/id_id_card_lookup.somethingWentWrongError',
+    id: 'app.modules.id_id_card_lookup.somethingWentWrongError',
     defaultMessage: "We can't verify you because something went wrong",
   },
   invalidCardIdError: {
-    id: 'app/modules/id_id_card_lookup.invalidCardIdError',
+    id: 'app.modules.id_id_card_lookup.invalidCardIdError',
     defaultMessage: 'This id is not valid.',
   },
   verifyYourIdentity: {
-    id: 'app/modules/id_id_card_lookup.verifyYourIdentity',
+    id: 'app.modules.id_id_card_lookup.verifyYourIdentity',
     defaultMessage: 'Verify your identity',
   },
   showHelp: {
-    id: 'app/modules/id_id_card_lookup.showHelp',
+    id: 'app.modules.id_id_card_lookup.showHelp',
     defaultMessage: 'Where can I find my ID Serial Number?',
   },
   submit: {
-    id: 'app/modules/id_id_card_lookup.submit',
+    id: 'app.modules.id_id_card_lookup.submit',
     defaultMessage: 'Submit',
   },
   cancel: {
-    id: 'app/modules/id_id_card_lookup.cancel',
+    id: 'app.modules.id_id_card_lookup.cancel',
     defaultMessage: 'Cancel',
   },
 });
