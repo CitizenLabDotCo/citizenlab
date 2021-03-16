2f99d442db52fb856298a9347547c3c731a17f84
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-16 11:21:47 +0100

    remove engineconfig mixing include

--- engines/idea_custom_fields/lib/idea_custom_fields/engine.rb
+++ engines/idea_custom_fields/lib/idea_custom_fields/engine.rb
@@ -1,5 +1,31 @@
+# frozen_string_literal: true
+
+# rubocop:disable Lint/SuppressedException
+begin
+  require 'factory_bot_rails'
+rescue LoadError
+end
+# rubocop:enable Lint/SuppressedException
+
 module IdeaCustomFields
   class Engine < ::Rails::Engine
-    include ::CitizenLab::EngineConfig
+    isolate_namespace IdeaCustomFields
+
+    def self.add_feature_spec
+      return unless defined?(IdeaCustomFields::FeatureSpecification)
+
+      AppConfiguration::Settings.add_feature(IdeaCustomFields::FeatureSpecification)
+    end
+
+    if Rails.env.development? || Rails.env.test?
+      config.autoload_paths << "#{config.root}/lib"
+    else
+      config.eager_load_paths << "#{config.root}/lib"
+    end
+
+    factories_path = File.expand_path('../../spec/factories', __dir__)
+    config.factory_bot.definition_file_paths += [factories_path] if defined?(FactoryBotRails)
+
+    config.to_prepare(&method(:add_feature_spec).to_proc)
   end
 end
