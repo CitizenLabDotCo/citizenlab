dc1cca0f62ade1e556a660a4163be85bd252e626
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2021-03-16 19:51:52 +0100

    OS-94 Fix slow tests

--- spec/acceptance/seedfile_spec.rb
+++ spec/acceptance/seedfile_spec.rb
@@ -35,7 +35,7 @@ describe "seedfile", slow_test: true do
       expect(CustomFieldOption.count).to be > 0
       expect(CustomField.with_resource_type('CustomForm').count).to be > 0
       expect(Invite.count).to be > 0
-      expect(Verification::IdCard.count).to be 10
+      expect(IdIdCardLookup::IdCard.count).to be 10
       expect(EmailCampaigns::UnsubscriptionToken.count).to be > 0
       expect(Maps::MapConfig.count).to be 1
       expect(Maps::Layer.count).to be 2
