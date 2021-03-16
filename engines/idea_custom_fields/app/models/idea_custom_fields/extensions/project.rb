2726a73c910ab01a5fad212f5ab3ff1cffe19c26
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-16 14:15:26 +0100

    add class name to belongs to custom form

--- engines/idea_custom_fields/app/models/idea_custom_fields/extensions/project.rb
+++ engines/idea_custom_fields/app/models/idea_custom_fields/extensions/project.rb
@@ -3,7 +3,10 @@ module IdeaCustomFields
     module Project
       def self.included(base)
         base.class_eval do
-          belongs_to :custom_form, optional: true, dependent: :destroy
+          belongs_to :custom_form,
+                     class_name: 'IdeaCustomFields::CustomForm',
+                     optional: true,
+                     dependent: :destroy
         end
       end
     end
