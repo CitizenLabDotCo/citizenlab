808fad68f961b36af314c3c7289ecf027c6383a1
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-17 13:50:07 +0100

    remove fallback from outlets

--- app/components/Outlet/index.tsx
+++ app/components/Outlet/index.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import { isEmpty } from 'lodash-es';
 import useOutlet from 'hooks/useOutlet';
 import { OutletsPropertyMap } from 'utils/moduleUtils';
-import { isUndefinedOrError } from 'utils/helperUtils';
-
-type BaseProps = {
-  fallback?: JSX.Element;
-};
 
 type CustomPropsMap = {
   [P in keyof OutletsPropertyMap]: { id: P } & OutletsPropertyMap[P];
@@ -14,15 +9,11 @@ type CustomPropsMap = {
 
 type Props = CustomPropsMap[keyof CustomPropsMap];
 
-const Outlet = ({ id, fallback, ...props }: Props & BaseProps) => {
+const Outlet = ({ id, ...props }: Props) => {
   const components = useOutlet(id);
 
   if (!components || isEmpty(components)) {
-    if (isUndefinedOrError(fallback)) {
-      return null;
-    }
-
-    return fallback;
+    return null;
   }
 
   return (
