243fb7149913d74f44c4684b79c4004766444814
brentguf <brent.guffens@gmail.com>
2021-03-18 10:16:04 +0100

    Remove old ProjectTemplatePreview folder

--- app/components/ProjectTemplatePreview/tracks.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  linkCopied: 'Project template preview page link copied',
-  useTemplateModalCreateProjectButtonClicked:
-    "Use template modal 'Create project' button clicked",
-};
