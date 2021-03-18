b072af4ce31e83782f7a3323e11a7bd15d3abfe3
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-03-18 15:36:20 +0100

    Move all verification components to module

--- app/modules/verification/citizen/VerificationSignUpSteps.tsx
+++ app/modules/verification/citizen/VerificationSignUpSteps.tsx
@@ -1,20 +1,25 @@
 import React, { useEffect } from 'react';
-import VerificationSteps from 'components/Verification/VerificationSteps';
+import VerificationSteps from './VerificationSteps';
 import { SignUpStepOutletProps } from 'utils/moduleUtils';
 import { injectIntl } from 'utils/cl-intl';
 import { InjectedIntlProps } from 'react-intl';
 import { trackEventByName } from 'utils/analytics';
 import tracks from './tracks';
+import messages from './messages';
 
 type Props = SignUpStepOutletProps & InjectedIntlProps;
 
-const VerificationSignUpSteps = ({ metaData, ...props }: Props) => {
+const VerificationSignUpSteps = ({
+  metaData,
+  intl: { formatMessage },
+  ...props
+}: Props) => {
   useEffect(() => {
     props.onData({
       key: 'verification',
       configuration: {
         position: 3,
-        stepName: 'temp', //formatMessage(messages.verifyYourIdentity),
+        stepName: formatMessage(messages.verifyYourIdentity),
         onSkipped: () => trackEventByName(tracks.signUpVerificationStepSkipped),
         onError: () => trackEventByName(tracks.signUpVerificationStepFailed),
         onCompleted: () =>
