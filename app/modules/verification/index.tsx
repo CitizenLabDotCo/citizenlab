0e46614787727215aec53728ceafa05d153a4545
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-03-18 12:50:05 +0100

    Add empty configuration file

---
+++ app/modules/verification/index.tsx
@@ -0,0 +1,10 @@
+import { ModuleConfiguration } from 'utils/moduleUtils';
+
+const configuration: ModuleConfiguration = {
+  routes: {
+    admin: [],
+  },
+  outlets: {},
+};
+
+export default configuration;
