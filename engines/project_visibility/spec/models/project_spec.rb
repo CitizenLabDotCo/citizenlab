40bd62ad5db9c518d68f36c706702b6ae5e75b68
Adrien Dessy <adrien@citizenlab.co>
2021-03-19 08:38:55 +0100

    OS-151 Fix filename

--- engines/project_visibility/spec/models/project_spec.rb
+++ engines/project_visibility/spec/models/project_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  describe "visible_to" do
+  describe 'visible_to' do
     it "gets set to 'public' when not specified on create" do
       project = create(:project, visible_to: nil)
       expect(project.visible_to).to eq 'public'
