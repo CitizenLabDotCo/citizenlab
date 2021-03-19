229966580bddfc5803d59a0ad92c018f402faaaf
Sébastien Hoorens <sebastien@citizenlab.co>
2021-03-19 11:57:01 +0100

    added text images to project folders

--- engines/project_folders/app/services/project_folders/side_fx_service.rb
+++ engines/project_folders/app/services/project_folders/side_fx_service.rb
@@ -4,10 +4,12 @@ module ProjectFolders
     include SideFxHelper
 
     def after_create folder, user
+      folder.update!(description_multiloc: TextImageService.new.swap_data_images(folder, :description_multiloc))
       LogActivityJob.perform_later(folder, 'created', user, folder.created_at.to_i)
     end
 
     def after_update folder, user
+      folder.description_multiloc = TextImageService.new.swap_data_images(folder, :description_multiloc)
       LogActivityJob.perform_later(folder, 'changed', user, folder.updated_at.to_i)
     end
 
