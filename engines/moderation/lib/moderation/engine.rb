64e58a48746690ecfbcf13d83836ac84065bf60b
Sébastien Hoorens <sebastien@citizenlab.co>
2021-03-22 17:06:36 +0100

    load factories from engine

--- engines/moderation/lib/moderation/engine.rb
+++ engines/moderation/lib/moderation/engine.rb
@@ -2,6 +2,9 @@ module Moderation
   class Engine < ::Rails::Engine
     isolate_namespace Moderation
 
+    factories_path = File.expand_path('../../spec/factories', __dir__)
+    config.factory_bot.definition_file_paths += [factories_path] if defined?(FactoryBotRails)
+
     config.to_prepare do
       require 'moderation/feature_specification'
       AppConfiguration::Settings.add_feature ::Moderation::FeatureSpecification
