94901bf44550164eef51bd6fc9847388124f4aef
Adrien Dessy <adrien@citizenlab.co>
2021-03-22 10:01:23 +0100

    OS-151 Use qualified namespacing to fix tests

--- engines/project_permissions/app/policies/project_permissions/patches/group_policy.rb
+++ engines/project_permissions/app/policies/project_permissions/patches/group_policy.rb
@@ -7,7 +7,7 @@ module ProjectPermissions
         def resolve_for_active
           return super if user.admin?
 
-          moderated_projects = Project.where(id: user.moderatable_project_ids)
+          moderated_projects = ::Project.where(id: user.moderatable_project_ids)
           return scope.all if moderated_projects.any? { |p| p.visible_to == 'public' }
 
           group_ids = groups_associated_to(moderated_projects)
@@ -16,10 +16,10 @@ module ProjectPermissions
 
         def groups_associated_to(projects)
           group_projects = projects.select { |p| p.visible_to == 'groups' }
-          return Group.none if group_projects.blank? # skip the DB query if we can
+          return ::Group.none if group_projects.blank? # skip the DB query if we can
 
-          Group.joins(:groups_projects)
-               .where(groups_projects: { project_id: group_projects })
+          ::Group.joins(:groups_projects)
+                 .where(groups_projects: { project_id: group_projects })
         end
       end
 
@@ -32,13 +32,13 @@ module ProjectPermissions
       def show_to_moderator?
         # No need to check that the user is a moderator with +user.project_moderator?+.
         # We just rely on the fact that the list of 'moderatable' projects will be empty.
-        projects = Project.where(id: user.moderatable_project_ids)
+        projects = ::Project.where(id: user.moderatable_project_ids)
 
         if projects.any? { |p| p.visible_to == 'public' }
           true
         else
           grp_projects = projects.select { |p| p.visible_to == 'groups' }
-          grp_projects.blank? ? false : GroupsProject.exists?(project_id: grp_projects, group_id: record.id)
+          grp_projects.blank? ? false : ::GroupsProject.exists?(project_id: grp_projects, group_id: record.id)
         end
       end
     end
