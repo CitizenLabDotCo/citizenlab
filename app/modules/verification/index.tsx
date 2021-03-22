57220ba45276fd50c2dc23eeb2caf985d3e69c7e
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-03-22 11:55:37 +0100

    Render modal through outlet

--- app/modules/verification/index.tsx
+++ app/modules/verification/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import ErrorBoundary from 'components/ErrorBoundary';
+import React, { Suspense } from 'react';
 import { ModuleConfiguration } from 'utils/moduleUtils';
 import VerificationBadge from './citizen/components/VerificationBadge';
+import VerificationModal from './citizen/components/VerificationModal';
 import VerificationSignUpSteps from './citizen/components/VerificationSignUpSteps';
 import VerificationStatus from './citizen/components/VerificationStatus';
 
@@ -15,6 +17,13 @@ const configuration: ModuleConfiguration = {
     'app.containers.UserEditPage.content': () => <VerificationStatus />,
     'app.containers.Navbar.UserMenu.UserNameContainer': (props) => (
       <VerificationBadge {...props} />
+    ),
+    'app.containers.App.modals': ({ onMounted }) => (
+      <ErrorBoundary>
+        <Suspense fallback={null}>
+          <VerificationModal onMounted={() => onMounted('verification')} />
+        </Suspense>
+      </ErrorBoundary>
     ),
   },
 };
