18889ba41087d9db0d7d6ff2aac887d70def5308
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-23 10:38:00 +0100

    add license in gemspec and remove useless method in project extension

--- engines/idea_assignment/app/models/idea_assignment/extensions/project.rb
+++ engines/idea_assignment/app/models/idea_assignment/extensions/project.rb
@@ -6,16 +6,6 @@ module IdeaAssignment
           belongs_to :default_assignee, class_name: 'User', optional: true
         end
       end
-
-      def assignee_can_moderate_project
-        return unless assignee && project && !::ProjectPolicy.new(assignee, project).moderate?
-
-        errors.add(
-          :assignee_id,
-          :assignee_can_not_moderate_project,
-          message: 'The assignee can not moderate the project of this idea'
-        )
-      end
     end
   end
 end
