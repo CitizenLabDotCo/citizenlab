4a47939ff8ce9ef20ada8fce83f6f633ef0020bc
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-23 12:56:01 +0100

    add permission policy

--- spec/policies/permission_policy_spec.rb
+++ spec/policies/permission_policy_spec.rb
@@ -6,20 +6,19 @@ describe PermissionPolicy do
   subject { described_class.new(user, permission) }
 
   let(:scope) { PermissionPolicy::Scope.new(user, Permission) }
-  let(:group) { create(:group) }
 
   before(:all) do # rubocop:disable RSpec/BeforeAfterAll
     @scope_types = PermissionsService.instance_variable_get(:@scope_spec_hash)
 
+    # rubocop:disable Style/SingleLineMethods
     dummy_global_scope = Module.new do
-      def self.actions(_scope = nil)
-        %w[a1 a2]
-      end
+      def self.actions(_scope = nil) %w[a1 a2] end
 
       def self.scope_type; nil end
 
       def self.scope_class; nil end
     end
+    # rubocop:enable Style/SingleLineMethods
 
     PermissionsService.clear_scope_types
     PermissionsService.register_scope_type(dummy_global_scope)
@@ -36,6 +35,7 @@ describe PermissionPolicy do
 
     it { is_expected.not_to permit(:show)         }
     it { is_expected.not_to permit(:update)       }
+    it { is_expected.to permit(:participation_conditions) }
 
     it 'indexes some permissions' do
       expect(scope.resolve.size).to eq 1
@@ -46,28 +46,27 @@ describe PermissionPolicy do
     let(:user) { create(:user) }
     let!(:permission) { create(:global_permission, :by_everyone, action: 'a1') }
 
-    before do
-      group.members << user
-    end
-
     it { is_expected.not_to permit(:show)         }
     it { is_expected.not_to permit(:update)       }
+    it { is_expected.to permit(:participation_conditions) }
 
     it 'indexes some permissions' do
       expect(scope.resolve.size).to eq 1
     end
   end
 
-  context 'for an admin' do
-    let(:user) { create(:admin) }
-    let!(:permission) { create(:permission, permitted_by: 'admins_moderators') }
+  context 'for a member of a group with granular permissions' do
+    let(:user) { create(:user) }
+    let(:group) { create(:group) }
+    let!(:permission) { create(:global_permission, permitted_by: 'groups', groups: [group], action: 'a1') }
 
     before do
       group.members << user
     end
 
-    it { is_expected.to permit(:show)             }
-    it { is_expected.to permit(:update)           }
+    it { is_expected.not_to permit(:show)         }
+    it { is_expected.not_to permit(:update)       }
+    it { is_expected.to permit(:participation_conditions) }
 
     it 'indexes some permissions' do
       expect(scope.resolve.size).to eq 1
@@ -78,8 +77,9 @@ describe PermissionPolicy do
     let(:user) { create(:admin) }
     let!(:permission) { create(:global_permission, :by_admins_moderators, action: 'a1') }
 
-    it { is_expected.not_to permit(:show)         }
-    it { is_expected.not_to permit(:update)       }
+    it { is_expected.to permit(:show)             }
+    it { is_expected.to permit(:update)           }
+    it { is_expected.to permit(:participation_conditions) }
 
     it 'indexes the permission' do
       expect(scope.resolve.size).to eq 1
