44f95b75cd612124aa8c1d2fa7dee9cd23dcc058
Adrien Dessy <adrien@citizenlab.co>
2021-03-24 09:23:29 +0100

    OS-152 Move ModeratorSerializer & Controller to project-permissions engine

---
+++ engines/project_permissions/app/serializers/project_permissions/web_api/v1/moderator_serializer.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module ProjectPermissions
+  module WebApi
+    module V1
+      class ModeratorSerializer < ::WebApi::V1::BaseSerializer
+        attributes :first_name, :last_name, :slug, :roles
+
+        attribute :email, if: Proc.new { |object, params|
+          view_private_attributes? object, params
+        }
+
+        attribute :avatar, if: Proc.new { |object|
+          object.avatar
+        } do |object|
+          object.avatar.versions.map { |k, v| [k.to_s, v.url] }.to_h
+        end
+
+        attribute :is_moderator, if: Proc.new { |object, params|
+          params[:project_id]
+        } do |object, params|
+          object.project_moderator? params[:project_id]
+        end
+
+        def self.view_private_attributes? object, params = {}
+          Pundit.policy!(current_user(params), object).view_private_attributes?
+        end
+      end
+    end
+  end
+end
