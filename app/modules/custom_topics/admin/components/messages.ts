396293a13b2eace975c8f6652b0ab2f110177a65
brentguf <brent.guffens@gmail.com>
2021-03-24 09:07:44 +0100

    Use same copy keys

--- app/modules/custom_topics/admin/components/messages.ts
+++ app/modules/custom_topics/admin/components/messages.ts
@@ -2,11 +2,11 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   projectEditTopicsTab: {
-    id: 'app.containers.AdminPage.ProjectEdit.projectEditTopicsTab',
+    id: 'app.containers.AdminPage.ProjectEdit.topicsTab',
     defaultMessage: 'Topics',
   },
   settingsTopicsTab: {
-    id: 'app.containers.AdminPage.SettingsPage.settingsTopicsTab',
+    id: 'app.containers.AdminPage.SettingsPage.tabTopics',
     defaultMessage: 'Topic manager',
   },
 });
