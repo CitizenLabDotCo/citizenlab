4768a78a9c67dbfaf14939d75f8de9a76a852dd6
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-03-25 16:03:34 +0100

    Move CommentTranslateButton

--- app/modules/machine_translations/citizen/components/CommentTranslateButton.tsx
+++ app/modules/machine_translations/citizen/components/CommentTranslateButton.tsx
@@ -6,14 +6,14 @@ import FeatureFlag from 'components/FeatureFlag';
 
 // i18n
 import { FormattedMessage } from 'utils/cl-intl';
-import messages from './messages';
+import messages from '../../../../components/PostShowComponents/Comments/messages';
 
 // events
-import { commentTranslateButtonClicked } from './events';
+import { commentTranslateButtonClicked } from '../../../../components/PostShowComponents/Comments/events';
 
 // analytics
 import { trackEventByName } from 'utils/analytics';
-import tracks from './tracks';
+import tracks from '../../../../components/PostShowComponents/Comments/tracks';
 
 // style
 import styled from 'styled-components';
