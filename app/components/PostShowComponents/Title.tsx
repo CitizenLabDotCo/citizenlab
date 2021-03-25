e9ce43531823cb8b00d5c6d04d96725f5d5df423
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-03-25 15:20:45 +0100

    Split up component

--- app/components/PostShowComponents/Title.tsx
+++ app/components/PostShowComponents/Title.tsx
@@ -12,12 +12,62 @@ import { Locale } from 'typings';
 // styling
 import styled from 'styled-components';
 import { media, fontSizes } from 'utils/styleUtils';
+import useFeatureFlag from 'hooks/useFeatureFlag';
 
 const Container = styled.div<{ align: 'left' | 'center' }>`
   width: ${({ align }) => (align === 'left' ? '100%' : 'auto')};
 `;
 
-const Title = styled.h1<{
+const parseTranslation = (
+  translation: GetMachineTranslationChildProps,
+  title
+) => {
+  if (!isNilOrError(translation)) {
+    return translation.attributes.translation;
+  }
+
+  return title;
+};
+
+const PostShowTitle = ({
+  locale,
+  translateButtonClicked,
+  postId,
+  postType,
+  color,
+  align,
+  title,
+}) => {
+  if (locale && translateButtonClicked) {
+    return (
+      <GetMachineTranslation
+        attributeName="title_multiloc"
+        localeTo={locale}
+        id={postId}
+        context={postType}
+      >
+        {(translation) => (
+          <Title
+            id={`e2e-${postType}-title`}
+            color={color}
+            align={align}
+            aria-live="polite"
+          >
+            {parseTranslation(translation, title)}
+          </Title>
+        )}
+      </GetMachineTranslation>
+    );
+  }
+
+  return (
+    <Title id={`e2e-${postType}-title`} color={color} align={align}>
+      {title}
+    </Title>
+  );
+};
+
+export const Title = styled.h1<{
   color: string | undefined;
   align: 'left' | 'center';
 }>`
@@ -49,17 +99,6 @@ interface Props {
   align?: 'left' | 'center';
 }
 
-const parseTranslation = (
-  translation: GetMachineTranslationChildProps,
-  title
-) => {
-  if (!isNilOrError(translation)) {
-    return translation.attributes.translation;
-  }
-
-  return title;
-};
-
 const PostTitle = memo<Props>(
   ({
     postId,
@@ -71,26 +110,20 @@ const PostTitle = memo<Props>(
     color,
     align = 'center',
   }) => {
+    const isMachineTranslationsEnabled = useFeatureFlag('machine_translations');
+
     return (
       <Container className={className} align={align}>
-        {locale && translateButtonClicked ? (
-          <GetMachineTranslation
-            attributeName="title_multiloc"
-            localeTo={locale}
-            id={postId}
-            context={postType}
-          >
-            {(translation) => (
-              <Title
-                id={`e2e-${postType}-title`}
-                color={color}
-                align={align}
-                aria-live="polite"
-              >
-                {parseTranslation(translation, title)}
-              </Title>
-            )}
-          </GetMachineTranslation>
+        {isMachineTranslationsEnabled ? (
+          <PostShowTitle
+            title={title}
+            postId={postId}
+            postType={postType}
+            color={color}
+            align={align}
+            translateButtonClicked={translateButtonClicked}
+            locale={locale}
+          />
         ) : (
           <Title id={`e2e-${postType}-title`} color={color} align={align}>
             {title}
