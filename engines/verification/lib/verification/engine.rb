97803c31f6c008e838f2eb9a9dc38e5f0eb1060c
Adrien Dessy <adrien@citizenlab.co>
2021-03-25 14:14:17 +0100

    OS-93 Register 'verification' feature flag from the verification engine

--- engines/verification/lib/verification/engine.rb
+++ engines/verification/lib/verification/engine.rb
@@ -1,5 +1,16 @@
+# frozen_string_literal: true
+
 module Verification
   class Engine < ::Rails::Engine
     isolate_namespace Verification
+
+    def self.register_feature
+      require 'verification/feature_specification'
+      AppConfiguration::Settings.add_feature(Verification::FeatureSpecification)
+    end
+
+    config.to_prepare do
+      Verification::Engine.register_feature
+    end
   end
 end
---
+++ engines/verification/lib/verification/feature_specification.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'citizen_lab/mixins/feature_specification'
+
+module Verification
+  module FeatureSpecification
+    extend CitizenLab::Mixins::FeatureSpecification
+
+    def self.feature_name
+      'verification'
+    end
+
+    def self.feature_title
+      'Verification'
+    end
+
+    def self.feature_description
+      'Use a pre-defined list to verify user identities during registration.'
+    end
+
+    add_setting 'verification_methods', required: true, schema: {
+      "title": 'Verification Methods',
+      "private": true,
+      "type": 'array',
+      "default": [],
+      "items": {
+        "anyOf": VerificationService.new.all_methods.map do |method|
+          {
+            type: 'object',
+            title: method.name,
+            required: ['name', *method.config_parameters],
+            properties: {
+              name: { type: 'string', enum: [method.name], default: method.name, readOnly: true },
+              **method.config_parameters.map do |cp|
+                parameter_schema = method.respond_to?(:config_parameters_schema) && method.config_parameters_schema[cp]
+                [cp, parameter_schema || { type: 'string', private: 'true' }]
+              end.to_h
+            }
+          }
+        end
+      }
+    }
+  end
+end
