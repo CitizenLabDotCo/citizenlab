6f936b52e96131fa4a30c8711e717792fe1c16ce
Sébastien Hoorens <sebastien@citizenlab.co>
2021-03-26 20:39:16 +0100

    user custom field options route refering back to generic custom field options controller

--- back/engines/commercial/user_custom_fields/config/routes.rb
+++ back/engines/commercial/user_custom_fields/config/routes.rb
@@ -5,10 +5,17 @@ UserCustomFields::Engine.routes.draw do
         resources :custom_fields, controller: 'user_custom_fields' do
           patch 'reorder', on: :member
           get 'schema', on: :collection
-          resources :custom_field_options do
+          resources :custom_field_options, controller: '/web_api/v1/custom_field_options' do
             patch 'reorder', on: :member
           end
         end
+      end
+
+      scope 'stats' do
+        route_params = {controller: 'stats_users'}
+
+        get 'users_by_custom_field/:custom_field_id', action: :users_by_custom_field, **route_params
+        get 'users_by_custom_field_as_xlsx/:custom_field_id', action: :users_by_custom_field_as_xlsx, **route_params
       end
     end
   end
