031c843a56aec98bb08dffb03a74b0e65679cf8c
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-27 20:59:27 +0100

    fix migrations

--- back/db/migrate/20180424190023_add_membership_types_to_groups.rb
+++ back/db/migrate/20180424190023_add_membership_types_to_groups.rb
@@ -1,6 +1,10 @@
 class AddMembershipTypesToGroups < ActiveRecord::Migration[5.1]
-  def change
+  def up
     add_column :groups, :membership_type, :string
     Group.update_all(membership_type: 'manual')
+  end
+
+  def down
+    remove_column :groups, :membership_type
   end
 end
