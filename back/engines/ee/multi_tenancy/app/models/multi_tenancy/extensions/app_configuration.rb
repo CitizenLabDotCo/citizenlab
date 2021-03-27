197d879196b720bc41e0a9ee0eb6554d4a259016
adessy <adrien@citizenlab.co>
2021-03-27 09:19:05 +0000

    OS-168 Move AppConfiguration<>Tenant sync to MultiTenancy engine (#2)

--- back/engines/ee/multi_tenancy/app/models/multi_tenancy/extensions/app_configuration.rb
+++ back/engines/ee/multi_tenancy/app/models/multi_tenancy/extensions/app_configuration.rb
@@ -5,6 +5,12 @@ module MultiTenancy
     module AppConfiguration
       def self.included(base)
         base.extend ClassMethods
+        base.class_eval do
+          attr_accessor :tenant_sync_enabled
+
+          after_save :update_tenant, if: :tenant_sync_enabled
+          after_initialize :custom_initialization
+        end
       end
 
       # Returns the corresponding tenant.
@@ -12,6 +18,27 @@ module MultiTenancy
       # @return [Tenant]
       def tenant
         Tenant.find_by(id: id)
+      end
+
+      def custom_initialization
+        @tenant_sync_enabled = true
+      end
+
+      def disable_tenant_sync
+        self.tenant_sync_enabled = false
+        self
+      end
+
+      def update_tenant
+        tenant = Tenant.current
+        attrs_delta = tenant.send(:attributes_delta, self, tenant)
+        return if attrs_delta.blank?
+
+        tenant.attributes = attrs_delta
+        tenant.remove_logo! if logo_previously_changed? && logo.blank?
+        tenant.remove_favicon! if favicon_previously_changed? && favicon.blank?
+        tenant.remove_header_bg! if header_bg_previously_changed? && header_bg.blank?
+        tenant.disable_config_sync.save
       end
 
       module ClassMethods
