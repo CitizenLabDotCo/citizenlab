613550ef3efba785b954d8d8e82816038d1f32db
Adrien Dessy <adrien@citizenlab.co>
2021-03-27 15:17:37 +0100

    OS-167 Fix spec_helper.rb for essential version
    There is no tenant for essential version.

--- back/engines/free/email_campaigns/spec/support/spec_helper.rb
+++ back/engines/free/email_campaigns/spec/support/spec_helper.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+puts "hello"
 RSpec.configure do |config|
   config.before(:each, type: :mailer) do
     I18n.load_path += Dir[Rails.root.join('engines/free/email_campaigns/spec/fixtures/locales/mailers.*.yml')]
