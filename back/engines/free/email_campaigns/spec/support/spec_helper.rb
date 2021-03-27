b6f1139559706f00c753b772afcd5c2917de7543
Adrien Dessy <adrien@citizenlab.co>
2021-03-27 17:20:35 +0100

    OS-167 Remove debug instruction

--- back/engines/free/email_campaigns/spec/support/spec_helper.rb
+++ back/engines/free/email_campaigns/spec/support/spec_helper.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-puts "hello"
 RSpec.configure do |config|
   config.before(:each, type: :mailer) do
     I18n.load_path += Dir[Rails.root.join('engines/free/email_campaigns/spec/fixtures/locales/mailers.*.yml')]
