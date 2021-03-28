562e182baec3516345506cc56059f77453c27055
Adrien Dessy <adrien@citizenlab.co>
2021-03-28 09:09:27 +0200

    OS-159 Micro-change to trigger pipeline

--- back/config/initializers/carrierwave.rb
+++ back/config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ require 'carrierwave/file_base64/adapter'
 
 CarrierWave.configure { |config| config.enable_processing = false } if Rails.env.test? || Rails.env.cucumber?
 
+# Adds the mount_base64_file_uploader class method to ActiveRecord.
 ActiveSupport.on_load :active_record do
-  # Adds the mount_base64_file_uploader class method to ActiveRecord.
   ActiveRecord::Base.extend Carrierwave::FileBase64::Adapter
 end
