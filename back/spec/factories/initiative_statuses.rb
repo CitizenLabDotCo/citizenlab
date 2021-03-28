7a5cf415af82f8c562968e18474f6de6de935086
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2021-03-28 20:54:53 +0200

    OS-158 Removed dependency on base template for iniative_status_change_spec

--- back/spec/factories/initiative_statuses.rb
+++ back/spec/factories/initiative_statuses.rb
@@ -11,5 +11,30 @@ FactoryBot.define do
       "en" => "This initiative has made Joke cry",
       "nl-BE" => "Het initiatief heeft Joke doen wenen"
     }}
+
+    factory :initiative_status_proposed do
+      code { 'proposed' }
+      title_multiloc { {'en' => 'proposed'} }
+    end
+
+    factory :initiative_status_expired do
+      code { 'expired' }
+      title_multiloc { {'en' => 'expired'} }
+    end
+
+    factory :initiative_status_threshold_reached do
+      code { 'threshold_reached' }
+      title_multiloc { {'en' => 'threshold_reached'} }
+    end
+
+    factory :initiative_status_answered do
+      code { 'answered' }
+      title_multiloc { {'en' => 'answered'} }
+    end
+
+    factory :initiative_status_ineligible do
+      code { 'ineligible' }
+      title_multiloc { {'en' => 'ineligible'} }
+    end
   end
 end
