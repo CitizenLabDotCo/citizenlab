87216393e96f903b1beae0fa0e74fdc8204a6306
adessy <adrien@citizenlab.co>
2021-03-28 10:14:44 +0200

    OS-161 Move factories/tenants.rb & tenant_spec.rb to citizenlab-ee (#4)

--- back/engines/ee/multi_tenancy/lib/multi_tenancy/engine.rb
+++ back/engines/ee/multi_tenancy/lib/multi_tenancy/engine.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
+# rubocop:disable Lint/SuppressedException
+begin
+  require 'factory_bot_rails'
+rescue LoadError
+  # Ignoring
+end
+# rubocop:enable Lint/SuppressedException
+
 module MultiTenancy
   class Engine < ::Rails::Engine
     config.generators.api_only = true
+
+    # Sharing the factories to make them accessible from to the main app / other engines.
+    factories_path = File.expand_path('../../spec/factories', __dir__)
+    config.factory_bot.definition_file_paths += [factories_path] if defined?(FactoryBotRails)
 
     def self.reload_extensions
       module_paths =  Dir.glob(File.join(::File.dirname(__FILE__), '../../app/**/extensions/**/*.rb'))
