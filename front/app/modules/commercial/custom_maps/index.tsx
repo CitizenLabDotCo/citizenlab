1bb8be0a41a961bd35f8a85231c8c98b15a96cde
brentguf <brent.guffens@gmail.com>
2021-03-29 20:01:40 +0200

    Add RenderOnTabHideCondition for custom_maps

--- front/app/modules/commercial/custom_maps/index.tsx
+++ front/app/modules/commercial/custom_maps/index.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { ModuleConfiguration } from 'utils/moduleUtils';
 import Tab from './admin/components/Tab';
 import LeafletConfig from './shared/components/Map/LeafletConfig';
 import Legend from './shared/components/Map/Legend';
+import { isNilOrError } from 'utils/helperUtils';
+import { IProjectData } from 'services/projects';
+import { IPhaseData } from 'services/phases';
+
+type RenderOnShowTabConditionProps = {
+  project: IProjectData;
+  phases: IPhaseData[] |null;
+  children: ReactNode;
+};
+
+const RenderOnHideTabCondition = (props: RenderOnShowTabConditionProps) => {
+  const { project, phases, children } = props;
+  const processType = project.attributes.process_type;
+  const participationMethod = project.attributes.participation_method;
+  const hideTabCondition = (
+    processType === 'continuous' &&
+    participationMethod !== 'ideation' &&
+    participationMethod !== 'budgeting') ||
+    (processType === 'timeline' &&
+      !isNilOrError(phases) &&
+      phases.filter((phase) => {
+        return (
+          phase.attributes.participation_method === 'ideation' ||
+          phase.attributes.participation_method === 'budgeting'
+        );
+      }).length === 0
+    );
+
+  if (hideTabCondition) {
+    return null;
+  };
+
+  return <>{children}</>
+}
 
 const configuration: ModuleConfiguration = {
   routes: {
@@ -18,7 +52,13 @@ const configuration: ModuleConfiguration = {
   outlets: {
     'app.components.Map.leafletConfig': (props) => <LeafletConfig {...props} />,
     'app.components.Map.Legend': (props) => <Legend {...props} />,
-    'app.containers.Admin.projects.edit': (props) => <Tab {...props} />,
+    'app.containers.Admin.projects.edit': (props) => {
+      return (
+        <RenderOnHideTabCondition project={props.project} phases={props.phases}>
+          <Tab {...props} />,
+        </RenderOnHideTabCondition>
+      )
+    }
   },
 };
 
