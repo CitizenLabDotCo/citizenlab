63af70f25696e5b36a946e64c9cd9a54426033a0
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-29 21:30:30 +0200

    remove models from idea_custom_fields

--- back/engines/commercial/idea_custom_fields/app/models/idea_custom_fields/custom_form.rb
+++
@@ -1,6 +0,0 @@
-module IdeaCustomFields
-  class IdeaCustomFields::CustomForm < ApplicationRecord
-    has_one :project, foreign_key: 'idea_custom_fields_custom_form_id'
-    has_many :custom_fields, as: :resource, dependent: :destroy
-  end
-end
--- back/engines/commercial/idea_custom_fields/app/models/idea_custom_fields/patches/custom_field.rb
+++
@@ -1,9 +0,0 @@
-module IdeaCustomFields
-  module Patches
-    module CustomField
-      def fieldable_types
-        super + ['IdeaCustomFields::CustomForm']
-      end
-    end
-  end
-end
