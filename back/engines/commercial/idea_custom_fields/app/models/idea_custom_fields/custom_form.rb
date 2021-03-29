7013cc17a0634a08a86ba931d62b1ca45956693b
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-29 17:01:50 +0200

    rename foreign key

--- back/engines/commercial/idea_custom_fields/app/models/idea_custom_fields/custom_form.rb
+++ back/engines/commercial/idea_custom_fields/app/models/idea_custom_fields/custom_form.rb
@@ -1,6 +1,6 @@
 module IdeaCustomFields
   class IdeaCustomFields::CustomForm < ApplicationRecord
-    has_one :project
+    has_one :project, foreign_key: 'idea_custom_fields_custom_form_id'
     has_many :custom_fields, as: :resource, dependent: :destroy
   end
 end
