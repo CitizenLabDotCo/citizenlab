eb982bd4f02abdb6e055d9b3040142756c40f079
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-03-29 17:03:29 +0200

    Fix for failing test

--- front/app/modules/commercial/user_custom_fields/admin/components/RegistrationFieldsToGraphs.test.tsx
+++ front/app/modules/commercial/user_custom_fields/admin/components/RegistrationFieldsToGraphs.test.tsx
@@ -10,7 +10,7 @@ import {
   Props,
 } from './RegistrationFieldsToGraphs';
 import { mockGetUserCustomFields } from '../../resources/__mocks__/GetUserCustomFields';
-import { mockUsersByRegFields } from 'services/__mocks__/stats';
+import { mockUsersByRegFields } from 'modules/commercial/user_custom_fields/services/__mocks__/userCustomFieldsStats';
 import { localizeProps } from 'utils/testUtils/localizeProps';
 import { intl } from 'utils/cl-intl';
 
