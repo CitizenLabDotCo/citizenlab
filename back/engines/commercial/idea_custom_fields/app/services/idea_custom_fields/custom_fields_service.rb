f28e8933200b537d7ad64fdcab4711e53d681eef
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-29 22:39:01 +0200

    fix order of methods in custom fields service

--- back/engines/commercial/idea_custom_fields/app/services/idea_custom_fields/custom_fields_service.rb
+++
@@ -1,8 +0,0 @@
-module IdeaCustomFields
-  class CustomFieldsService
-    def find_or_build_field custom_form, code
-      custom_form&.custom_fields&.find_by(code: code) ||
-        IdeaCustomFieldsService.call(custom_form).find { |bicf| bicf.code == code }
-    end
-  end
-end
