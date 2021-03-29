c85c31e37a0b603bacb3cb7f09d52eae34202026
adessy <adrien@citizenlab.co>
2021-03-29 14:14:17 +0200

    OS-176 Fix OS front-end url service + various bug fixes

--- back/engines/commercial/project_folders/spec/models/folder_spec.rb
+++ back/engines/commercial/project_folders/spec/models/folder_spec.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe ProjectFolders::Folder, type: :model do
-  describe "Default factory" do
-    it "is valid" do
+  describe 'Default factory' do
+    it 'is valid' do
       expect(build(:project_folder)).to be_valid
     end
   end
 
-  describe "Folder without admin publication" do
-    it "is invalid" do
+  describe 'Folder without admin publication' do
+    it 'is invalid' do
       folder = create(:project_folder)
       AdminPublication.where(publication: folder).first.destroy!
       expect(folder.reload).to be_invalid
