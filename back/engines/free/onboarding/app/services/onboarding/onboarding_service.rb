565eb0a07f16815ffdabaa6424fe261ef9199e90
Adrien Dessy <adrien@citizenlab.co>
2021-03-29 20:32:41 +0200

    OS-191 Rubocop

--- back/engines/free/onboarding/app/services/onboarding/onboarding_service.rb
+++ back/engines/free/onboarding/app/services/onboarding/onboarding_service.rb
@@ -1,16 +1,19 @@
+# frozen_string_literal: true
+
 module Onboarding
   class OnboardingService
+    CAMPAIGNS = %w[verification complete_profile custom_cta default].freeze
 
-    CAMPAIGNS = %w(verification complete_profile custom_cta default)
-
-    def current_campaign user
+    def current_campaign(user)
       raise ArgumentError unless user
+
       dismissals = CampaignDismissal.where(user: user).pluck(:campaign_name)
-      if Tenant.current.feature_activated?('verification') && !user.verified && !dismissals.include?('verification')
+      if Tenant.current.feature_activated?('verification') && !user.verified && dismissals.exclude?('verification')
         :verification
-      elsif profile_incomplete?(user) && !dismissals.include?('complete_profile')
+      elsif profile_incomplete?(user) && dismissals.exclude?('complete_profile')
         :complete_profile
-      elsif !MultilocService.new.is_empty?(Tenant.settings('core', 'custom_onboarding_message')) && !dismissals.include?('custom_cta')
+      elsif !MultilocService.new.is_empty?(Tenant.settings('core',
+                                                           'custom_onboarding_message')) && dismissals.exclude?('custom_cta')
         :custom_cta
       else
         :default
@@ -19,13 +22,12 @@ module Onboarding
 
     private
 
-    def profile_incomplete? user
+    def profile_incomplete?(user)
       MultilocService.new.is_empty?(user.bio_multiloc) ||
         user.avatar.blank? ||
         CustomField.with_resource_type('User').enabled.any? do |cf|
           user.custom_field_values[cf.key].nil?
         end
     end
-
   end
 end
