b8af7a0c2962298c5c998cdafad21e9a13373f68
Adrien Dessy <adrien@citizenlab.co>
2021-03-29 13:25:01 +0200

    OS-176 Fix NameError: uninitialized constant Frontend::UrlService::ProjectFolders

--- back/engines/commercial/project_folders/app/services/project_folders/patches/frontend/url_service.rb
+++ back/engines/commercial/project_folders/app/services/project_folders/patches/frontend/url_service.rb
@@ -1,7 +1,15 @@
+# frozen_string_literal: true
+
 module ProjectFolders
-  module Extensions
+  module Patches
     module Frontend
       module UrlService
+        def model_to_url(model_instance, options = {})
+          return "#{home_url(options)}/folders/#{model_instance.slug}" if model_instance.is_a?(::ProjectFolders::Folder)
+
+          super
+        end
+
         def admin_project_folder_url(project_folder_id, locale: nil)
           locale ||= AppConfiguration.instance.settings('core', 'locales').first
           "#{AppConfiguration.instance.base_frontend_uri}/#{locale}/admin/projects/folders/#{project_folder_id}"
