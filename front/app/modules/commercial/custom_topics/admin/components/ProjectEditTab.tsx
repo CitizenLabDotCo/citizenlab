0e6080ba6d57623f94f54bfc96046796ffea49cd
brentguf <brent.guffens@gmail.com>
2021-03-29 20:21:49 +0200

    Correct format onData arguments ProjectEditTab

--- front/app/modules/commercial/custom_topics/admin/components/ProjectEditTab.tsx
+++ front/app/modules/commercial/custom_topics/admin/components/ProjectEditTab.tsx
@@ -1,11 +1,8 @@
 import { ProjectTabOptions } from 'containers/Admin/projects/edit';
 import { FC, useEffect } from 'react';
 import { InjectedIntlProps } from 'react-intl';
-import { IPhaseData } from 'services/phases';
-import { IProjectData } from 'services/projects';
 import { InsertConfigurationOptions, ITab } from 'typings';
 import { injectIntl } from 'utils/cl-intl';
-import { isNilOrError } from 'utils/helperUtils';
 import messages from './messages';
 
 type Props = {
@@ -19,35 +16,13 @@ const ProjectEditTab: FC<Props & InjectedIntlProps> = ({
   const tabName = 'topics';
   useEffect(() => {
     onData({
-      tabOptions: {
-        configuration: {
-          label: formatMessage(messages.topicsTab),
-          name: tabName,
-          url: 'topics',
-          feature: 'custom_topics',
-        },
-        insertBeforeName: 'phases',
+      configuration: {
+        label: formatMessage(messages.topicsTab),
+        name: tabName,
+        url: 'topics',
+        feature: 'custom_topics',
       },
-      tabHideConditions: {
-        [tabName]: (project: IProjectData, phases: IPhaseData[] | null) => {
-          const processType = project.attributes.process_type;
-          const participationMethod = project.attributes.participation_method;
-
-          return (
-            (processType === 'continuous' &&
-              participationMethod !== 'ideation' &&
-              participationMethod !== 'budgeting') ||
-            (processType === 'timeline' &&
-              !isNilOrError(phases) &&
-              phases.filter((phase) => {
-                return (
-                  phase.attributes.participation_method === 'ideation' ||
-                  phase.attributes.participation_method === 'budgeting'
-                );
-              }).length === 0)
-          );
-        },
-      },
+      insertBeforeName: 'phases',
     });
   }, []);
 
