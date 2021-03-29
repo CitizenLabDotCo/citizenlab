68879e1704d3f37e03344d3ec2a20361d54b57ff
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-29 14:48:11 +0200

    remove self.table_name

--- back/engines/commercial/idea_custom_fields/app/models/idea_custom_fields/custom_form.rb
+++ back/engines/commercial/idea_custom_fields/app/models/idea_custom_fields/custom_form.rb
@@ -1,7 +1,5 @@
 module IdeaCustomFields
   class IdeaCustomFields::CustomForm < ApplicationRecord
-    self.table_name = 'custom_forms'
-
     has_one :project
     has_many :custom_fields, as: :resource, dependent: :destroy
   end
