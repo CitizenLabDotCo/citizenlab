e02ee5a2a0d4ae7cd83bbfba32c44d889ea705d3
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-30 18:59:34 +0200

    rename more translation keys

--- front/app/modules/commercial/project_folders/admin/components/ProjectFolderTitle/messages.ts
+++ front/app/modules/commercial/project_folders/admin/components/ProjectFolderTitle/messages.ts
@@ -2,7 +2,7 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   projectsAndFolders: {
-    id: 'app.containers.AdminPage.projects.all.projectsAndFolders',
+    id: 'app.containers.AdminPage.projects.all.adminPublications',
     defaultMessage: 'Projects and folders',
   },
 });
