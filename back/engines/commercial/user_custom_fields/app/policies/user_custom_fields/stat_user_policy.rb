9af92c76c8c71fa455e41f40e4270b7c22d5c234
Sébastien Hoorens <sebastien@citizenlab.co>
2021-03-30 10:54:20 +0200

    moved other custom field stats to engine

--- back/engines/commercial/user_custom_fields/app/policies/user_custom_fields/stat_user_policy.rb
+++ back/engines/commercial/user_custom_fields/app/policies/user_custom_fields/stat_user_policy.rb
@@ -1,6 +1,38 @@
 module UserCustomFields
   class StatUserPolicy < ApplicationPolicy
+    def users_by_gender?
+      user&.active? && (user.admin? || user.project_moderator?)
+    end
+
+    def users_by_birthyear?
+      user&.active? && (user.admin? || user.project_moderator?)
+    end
+
+    def users_by_domicile?
+      user&.active? && (user.admin? || user.project_moderator?)
+    end
+
+    def users_by_education?
+      user&.active? && (user.admin? || user.project_moderator?)
+    end
+
     def users_by_custom_field?
+      user&.active? && (user.admin? || user.project_moderator?)
+    end
+
+    def users_by_gender_as_xlsx?
+      user&.active? && (user.admin? || user.project_moderator?)
+    end
+
+    def users_by_birthyear_as_xlsx?
+      user&.active? && (user.admin? || user.project_moderator?)
+    end
+
+    def users_by_domicile_as_xlsx?
+      user&.active? && (user.admin? || user.project_moderator?)
+    end
+
+    def users_by_education_as_xlsx?
       user&.active? && (user.admin? || user.project_moderator?)
     end
 
--- back/engines/commercial/user_custom_fields/config/routes.rb
+++ back/engines/commercial/user_custom_fields/config/routes.rb
@@ -14,7 +14,16 @@ UserCustomFields::Engine.routes.draw do
       scope 'stats' do
         route_params = {controller: 'stats_users'}
 
+        get 'users_by_gender', **route_params
+        get 'users_by_birthyear', **route_params
+        get 'users_by_domicile', **route_params
+        get 'users_by_education', **route_params
         get 'users_by_custom_field/:custom_field_id', action: :users_by_custom_field, **route_params
+
+        get 'users_by_gender_as_xlsx', **route_params
+        get 'users_by_birthyear_as_xlsx', **route_params
+        get 'users_by_domicile_as_xlsx', **route_params
+        get 'users_by_education_as_xlsx', **route_params
         get 'users_by_custom_field_as_xlsx/:custom_field_id', action: :users_by_custom_field_as_xlsx, **route_params
       end
     end
