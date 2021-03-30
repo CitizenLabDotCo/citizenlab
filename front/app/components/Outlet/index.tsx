e162e19581822e56027557c07e4b61bb6abf30ed
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-03-30 13:08:40 +0200

    Tweak Outlet to also render when there are no outletComponent

--- front/app/components/Outlet/index.tsx
+++ front/app/components/Outlet/index.tsx
@@ -1,16 +1,17 @@
 import React, { memo, useContext } from 'react';
 import { OutletsPropertyMap, OutletId } from 'utils/moduleUtils';
 import { OutletsContext } from 'containers/OutletsProvider';
-import { isEmpty } from 'lodash-es';
 
 type CustomPropsMap = {
   [P in keyof OutletsPropertyMap]: { id: P } & OutletsPropertyMap[P];
 };
 
 type CustomOutletProps = CustomPropsMap[keyof CustomPropsMap];
+
 export type OutletRenderProps = (
   renderProps: JSX.Element[]
 ) => JSX.Element | null;
+
 type Children = OutletRenderProps;
 type InputProps = {
   children?: Children;
@@ -26,19 +27,15 @@ type Props = InputProps & CustomOutletProps;
 const Outlet = memo(({ children, id, ...props }: Props) => {
   const outletComponents = useOutlet(id);
 
-  if (outletComponents && !isEmpty(outletComponents)) {
-    const componentsToRender = outletComponents.map((Component, index) => (
-      <Component key={`${id}_${index}`} {...props} />
-    ));
+  const componentsToRender = outletComponents.map((Component, index) => (
+    <Component key={`${id}_${index}`} {...props} />
+  ));
 
-    if (children) {
-      return children(componentsToRender);
-    } else {
-      return <>{componentsToRender}</>;
-    }
+  if (children) {
+    return children(componentsToRender);
   }
 
-  return null;
+  return <>{componentsToRender}</>;
 });
 
 export default Outlet;
