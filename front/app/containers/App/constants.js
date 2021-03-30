5fbc2e730f3a36a705a7fdac9f1e5ed9506ac69b
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2021-03-30 18:46:55 +0200

    Restored wrong constants.js changed

--- front/app/containers/App/constants.js
+++ front/app/containers/App/constants.js
@@ -97,7 +97,7 @@ exports.appLocalePairs = {
   'pt-BR': 'Português (Brasil)',
   'ar-SA': 'عربى',
   mi: 'Māori',
-  'sr-Latn': 'Serbian (Latin)',
+  'sr-Latn': 'Srpski (latinica)',
 };
 exports.appGraphqlLocalePairs = {
   en: 'en',
@@ -107,6 +107,7 @@ exports.appGraphqlLocalePairs = {
   nb: 'nb',
   da: 'da',
   es: 'es',
+  mi: 'mi',
   enGb: 'en-GB',
   enCa: 'en-CA',
   frBe: 'fr-BE',
@@ -124,8 +125,7 @@ exports.appGraphqlLocalePairs = {
   roRo: 'ro-RO',
   ptBr: 'pt-BR',
   arSa: 'ar',
-  mi: 'mi',
-  srLatn: 'sr-Latn',
+  srLatn: 'Srpski (latinica)',
 };
 exports.shortenedAppLocalePairs = {
   en: 'English',
@@ -147,7 +147,7 @@ exports.shortenedAppLocalePairs = {
   'pt-BR': 'Português',
   'ar-SA': 'عربى',
   mi: 'Māori',
-  'sr-Latn': 'Serbian',
+  'sr-Latn': 'Srpski',
 };
 // see https://www.ge.com/digital/documentation/predix-services/c_custom_locale_support.html
 exports.appLocalesMomentPairs = {
