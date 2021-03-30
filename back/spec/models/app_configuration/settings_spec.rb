5794765e9f3602d839d85ba5ffcf0ef5c61e608c
Adrien Dessy <adrien@citizenlab.co>
2021-03-30 10:28:08 +0200

    OS-194 Move Tenant-AppConfiguration sync tests to the multi-tenancy engine

--- back/spec/models/app_configuration/settings_spec.rb
+++ back/spec/models/app_configuration/settings_spec.rb
@@ -2,45 +2,6 @@
 
 require 'rails_helper'
 
-RSpec.describe AppConfiguration, type: :model do
-  it 'name is synced with tenant' do
-    Tenant.current.update(name: 'Karhide')
-    expect(described_class.instance.name).to eq('Karhide')
-
-    described_class.instance.update(name: 'Orgoreyn')
-    expect(Tenant.current.name).to eq('Orgoreyn')
-  end
-
-  it 'host is synced with tenant' do
-    Tenant.current.update(host: 'karhide.org')
-    Apartment::Tenant.switch!('karhide_org')
-    expect(described_class.instance.host).to eq('karhide.org')
-
-    described_class.instance.update(host: 'orgoreyn.org')
-    expect { Apartment::Tenant.switch!('karhide_org') }.to raise_error(Apartment::TenantNotFound)
-    Apartment::Tenant.switch!('orgoreyn_org')
-    expect(described_class.instance.host).to eq('orgoreyn.org')
-  end
-
-  it 'settings are synced with tenant' do
-    core_settings_update = {
-      organization_type: 'large_city',
-      lifecycle_stage: 'trial',
-      currency: 'USD',
-      color_main: '#FFFFFF'
-    }.stringify_keys!
-
-    config = described_class.instance
-    settings = config.settings
-    settings['core'].merge!(core_settings_update)
-    config.settings = settings
-    config.save!
-
-    tenant_core_settings = Tenant.current.settings['core']
-    expect(tenant_core_settings).to include(core_settings_update)
-  end
-end
-
 RSpec.describe AppConfiguration::Settings do
   before { described_class.send(:reset) }
 
