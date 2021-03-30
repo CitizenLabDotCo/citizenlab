e75e3980948156081dd78ac6736f584ec4d5e642
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-30 15:08:05 +0200

    fix taggings

--- back/app/controllers/web_api/v1/ideas_controller.rb
+++ back/app/controllers/web_api/v1/ideas_controller.rb
@@ -4,7 +4,7 @@ class WebApi::V1::IdeasController < ApplicationController
   skip_after_action :verify_authorized, only: [:index_xlsx, :index_mini, :index_idea_markers, :filter_counts]
   after_action :verify_policy_scoped, only: %i[index index_mini]
   before_action :authorize_project_or_ideas,
-                only: %i[index_xlsx index_with_tags_xlsx]
+                only: %i[index_xlsx]
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def index
@@ -60,20 +60,6 @@ class WebApi::V1::IdeasController < ApplicationController
 
     I18n.with_locale(current_user&.locale) do
       xlsx = XlsxService.new.generate_ideas_xlsx @ideas, view_private_attributes: Pundit.policy!(current_user, User).view_private_attributes?
-      send_data xlsx, type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', filename: 'ideas.xlsx'
-    end
-  end
-
-  def index_with_tags_xlsx
-    @result = IdeasFinder.find(
-      params,
-      scope: policy_scope(Idea).where(publication_status: 'published'),
-      includes: %i[author topics areas project idea_status idea_files]
-    )
-    @ideas = @result.records
-
-    I18n.with_locale(current_user&.locale) do
-      xlsx = XlsxService.new.generate_ideas_xlsx @ideas, view_private_attributes: Pundit.policy!(current_user, User).view_private_attributes?, with_tags: true
       send_data xlsx, type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', filename: 'ideas.xlsx'
     end
   end
