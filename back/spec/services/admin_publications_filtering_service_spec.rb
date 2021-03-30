d1bef00fa1154159d3f6b9950b043c05894d0612
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-30 12:52:34 +0200

    fix filtering service

--- back/spec/services/admin_publications_filtering_service_spec.rb
+++ back/spec/services/admin_publications_filtering_service_spec.rb
@@ -3,16 +3,12 @@ require 'rails_helper'
 describe AdminPublicationsFilteringService do
   subject(:result) { described_class.new.filter(base_scope, options) }
 
-  before do
-    AdminPublication.destroy_all
-  end
+  before { AdminPublication.destroy_all }
 
   let!(:tree_mock) { MockAdminPublicationsTree.call }
 
   shared_examples 'when a normal user searching from the landing page' do
-    let(:base_scope) do
-      Pundit.policy_scope(create(:user), AdminPublication.includes(:parent))
-    end
+    let(:base_scope) { Pundit.policy_scope(create(:user), AdminPublication.includes(:parent)) }
 
     it 'includes truly empty parents' do
       expect(result.ids).to include(*tree_mock.empty_parents.where(publication_status: %w[archived published]).ids)
