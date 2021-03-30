ef80c9b2ef471b8c5ce914ca310aafdc792ac430
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-30 15:33:20 +0200

    extend idea with tags

---
+++ back/engines/commercial/tagging/app/models/tagging/extensions/idea.rb
@@ -0,0 +1,13 @@
+module Tagging
+  module Extensions
+    module Idea
+      def self.prepended(base)
+        base.class_eval do
+          has_many :tagging_taggings
+          has_many :tagging_tags, through: :tagging_taggings
+          has_and_belongs_to_many :tagging_pending_tasks, class_name: 'Tagging::PendingTask', join_table: :tagging_pending_tasks_ideas
+        end
+      end
+    end
+  end
+end
