0dca0b6056c2e17d780dd4aa8cb0d2b42247ee43
adessy <adrien@citizenlab.co>
2021-03-30 20:37:49 +0200

    OS-217 Fix acceptance/app_configuration_spec.rb (#80)

--- back/app/uploaders/favicon_uploader.rb
+++ back/app/uploaders/favicon_uploader.rb
@@ -1,26 +1,28 @@
-class FaviconUploader < BaseImageUploader
+# frozen_string_literal: true
 
+class FaviconUploader < BaseImageUploader
   def store_dir
-    "uploads/#{model.tenant.id}/favicon/#{model.id}"
+    'uploads/favicon'
   end
-  
+
   version :large do
-    process resize_to_fit: [152,152]
+    process resize_to_fit: [152, 152]
     process convert: :png
   end
 
   version :medium do
-    process resize_to_fit: [32,32]
+    process resize_to_fit: [32, 32]
     process convert: :png
   end
 
   version :small do
-    process resize_to_fit: [16,16]
+    process resize_to_fit: [16, 16]
     process convert: :png
   end
 
   def extension_allowlist
-    %w(jpg jpeg gif png ico svg)
+    %w[jpg jpeg gif png ico svg]
   end
-
 end
+
+FaviconUploader.prepend_if_ee('MultiTenancy::Patches::FaviconUploader')
