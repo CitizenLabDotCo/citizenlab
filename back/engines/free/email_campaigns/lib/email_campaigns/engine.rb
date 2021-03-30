dccccdc5381bdf9a68ca770b434498afcbf7db50
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-03-30 19:37:53 +0200

    added factories

--- back/engines/free/email_campaigns/lib/email_campaigns/engine.rb
+++ back/engines/free/email_campaigns/lib/email_campaigns/engine.rb
@@ -1,5 +1,18 @@
+# frozen_string_literal: true
+
+# rubocop:disable Lint/SuppressedException
+begin
+  require 'factory_bot_rails'
+rescue LoadError
+end
+# rubocop:enable Lint/SuppressedException
+
 module EmailCampaigns
   class Engine < ::Rails::Engine
     isolate_namespace EmailCampaigns
+
+    # Sharing the factories to make them accessible from to the main app / other engines.
+    factories_path = File.expand_path('../../spec/factories', __dir__)
+    config.factory_bot.definition_file_paths += [factories_path] if defined?(FactoryBotRails)
   end
 end
