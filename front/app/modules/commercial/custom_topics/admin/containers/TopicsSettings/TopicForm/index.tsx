6a3b21911be97453180b7002c8cae64f2b07a080
brentguf <brent.guffens@gmail.com>
2021-04-01 14:12:32 +0200

    Try React Hook Form for TopicForm (WIP)

--- front/app/modules/commercial/custom_topics/admin/containers/TopicsSettings/TopicForm/index.tsx
+++ front/app/modules/commercial/custom_topics/admin/containers/TopicsSettings/TopicForm/index.tsx
@@ -1,64 +1,55 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 // i18n
-import { injectIntl, FormattedMessage } from 'utils/cl-intl';
+import { injectIntl } from 'utils/cl-intl';
 import messages from '../messages';
 import { InjectedIntlProps } from 'react-intl';
 
-import { Form, Field, InjectedFormikProps } from 'formik';
-import FormikInputMultilocWithLocaleSwitcher from 'components/UI/FormikInputMultilocWithLocaleSwitcher';
-import FormikSubmitWrapper from 'components/admin/FormikSubmitWrapper';
-
-import { Section, SectionField } from 'components/admin/Section';
+import InputMultilocWithLocaleSwitcher from 'components/UI/InputMultilocWithLocaleSwitcher';
 import Error from 'components/UI/Error';
 
 // typings
 import { Multiloc } from 'typings';
 
-export interface Props {}
-
-export interface FormValues {
+interface IFormValues {
   title_multiloc: Multiloc;
   description_multiloc: Multiloc;
 }
 
-class TopicForm extends React.Component<
-  InjectedFormikProps<Props & InjectedIntlProps, FormValues>
-> {
-  render() {
-    const {
-      isSubmitting,
-      errors,
-      isValid,
-      touched,
-      status,
-      intl: { formatMessage },
-    } = this.props;
-
-    return (
-      <Form>
-        <Section>
-          <SectionField>
-            <Field
-              name="title_multiloc"
-              component={FormikInputMultilocWithLocaleSwitcher}
-              label={<FormattedMessage {...messages.fieldTopicTitle} />}
-              labelTooltipText={formatMessage(messages.fieldTopicTitleTooltip)}
-              id="e2e-topic-name"
-            />
-            {touched.title_multiloc && (
-              <Error
-                fieldName="title_multiloc"
-                apiErrors={errors.title_multiloc as any}
-              />
-            )}
-          </SectionField>
-        </Section>
-
-        <FormikSubmitWrapper {...{ isValid, isSubmitting, status, touched }} />
-      </Form>
-    );
-  }
+export interface Props {
+  handleOnSubmit: (formValues: IFormValues) => void;
 }
 
-export default injectIntl<Props>(TopicForm);
+const TopicForm = ({
+  intl: { formatMessage },
+  handleOnSubmit,
+}: Props & InjectedIntlProps) => {
+  const { register, handleSubmit, errors } = useForm<IFormValues>();
+  const onSubmit = (formValues: IFormValues) => {
+    handleOnSubmit(formValues);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <InputMultilocWithLocaleSwitcher
+          id="e2e-topic-name"
+          name="title_multiloc"
+          label={formatMessage(messages.fieldTopicTitle)}
+          labelTooltipText={formatMessage(messages.fieldTopicTitleTooltip)}
+          register={register}
+        />
+        {errors.title_multiloc && (
+          <Error
+            fieldName="title_multiloc"
+            apiErrors={errors.title_multiloc as any}
+          />
+        )}
+        <input type="submit" />
+      </form>
+    </>
+  );
+};
+
+export default injectIntl(TopicForm);
