e73ca5c3621eede8d53314b7e9be67ee11b6f360
Adrien Dessy <adrien@citizenlab.co>
2021-04-02 15:38:48 +0200

    OS-246 Remove unnecessary method
    'public' is already the default in the table definition

--- back/engines/free/project_permissions/app/models/project_permissions/patches/project.rb
+++ back/engines/free/project_permissions/app/models/project_permissions/patches/project.rb
@@ -8,12 +8,7 @@ module ProjectPermissions
       def self.included(base)
         base.class_eval do
           validates :visible_to, presence: true, inclusion: { in: VISIBLE_TOS }
-          before_validation :set_visible_to, on: :create
         end
-      end
-
-      def set_visible_to
-        self.visible_to ||= 'public'
       end
     end
   end
