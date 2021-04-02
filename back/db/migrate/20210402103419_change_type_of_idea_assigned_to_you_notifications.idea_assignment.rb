c681522339e96793a4bf539c6c4a2fbd3490ea0e
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-04-02 12:52:57 +0200

    fix notification in engine

--- back/db/migrate/20210402103419_change_type_of_idea_assigned_to_you_notifications.idea_assignment.rb
+++ back/db/migrate/20210402103419_change_type_of_idea_assigned_to_you_notifications.idea_assignment.rb
@@ -1,6 +1,8 @@
 class ChangeTypeOfIdeaAssignedToYouNotifications < ActiveRecord::Migration[6.0]
   def up
-    Notification.where(type: 'Notifications::IdeaAssignedToYou')
-                .update_all(type: 'IdeaAssignment::Notifications::IdeaAssignedToYou')
+    Notification.disable_inheritance do
+      Notification.where(type: 'Notifications::IdeaAssignedToYou')
+                  .update_all(type: 'IdeaAssignment::Notifications::IdeaAssignedToYou')
+    end
   end
 end
