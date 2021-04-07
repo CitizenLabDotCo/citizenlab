ea7e5c411bb948b947853291d63bdcf4cd8bf7c8
Adrien Dessy <adrien@citizenlab.co>
2021-04-07 16:42:13 +0200

    OS-262 Refactor project_file_policy_spec.rb

--- back/spec/policies/project_file_policy_spec.rb
+++ back/spec/policies/project_file_policy_spec.rb
@@ -46,19 +46,6 @@ describe ProjectFilePolicy do
         expect(scope.resolve.size).to eq 1
       end
     end
-
-    context "for a moderator" do
-      let(:user) { create(:moderator, project: project) }
-
-      it { should permit(:show)    }
-      it { should permit(:create)  }
-      it { should permit(:update)  }
-      it { should permit(:destroy) }
-
-      it "should index the file"  do
-        expect(scope.resolve.size).to eq 1
-      end
-    end
 	end
 
 	 context "on a file in a private admins project" do 
