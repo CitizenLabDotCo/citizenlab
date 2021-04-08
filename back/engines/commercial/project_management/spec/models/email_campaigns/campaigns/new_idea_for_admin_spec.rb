268778c615f75cf2aa1f8381b45927d75e67bbf5
Adrien Dessy <adrien@citizenlab.co>
2021-04-08 18:24:00 +0200

    OS-262 Fix: "undefined campaign"

--- back/engines/commercial/project_management/spec/models/email_campaigns/campaigns/new_idea_for_admin_spec.rb
+++ back/engines/commercial/project_management/spec/models/email_campaigns/campaigns/new_idea_for_admin_spec.rb
@@ -4,19 +4,21 @@ require 'rails_helper'
 
 skip_reason = defined?(EmailCampaigns::Engine) ? nil : 'email_campaigns engine is not installed'
 
-RSpec.describe EmailCampaigns::Campaigns::NewIdeaForAdmin, type: :model, skip: skip_reason do
-  it 'keeps moderators' do
-    idea = create(:idea)
-    moderator = create(:moderator, project: idea.project)
-    _other_moderator = create(:moderator)
+RSpec.describe 'EmailCampaigns::Campaigns::NewIdeaForAdmin', type: :model, skip: skip_reason do
+  let(:campaign) { build(:new_idea_for_admin_campaign) }
 
-    idea_published = create(:activity, item: idea, action: 'published')
-    expect(campaign.apply_recipient_filters(activity: idea_published)).to match([moderator])
+  describe 'apply_recipient_filters' do
+    it 'keeps moderators' do
+      idea = create(:idea)
+      moderator = create(:moderator, project: idea.project)
+      _other_moderator = create(:moderator)
+
+      idea_published = create(:activity, item: idea, action: 'published')
+      expect(campaign.apply_recipient_filters(activity: idea_published)).to match([moderator])
+    end
   end
 
   describe 'apply_recipient_filters' do
-    let(:campaign) { build(:new_idea_for_admin_campaign) }
-
     it 'filters out everyone if the author is moderator' do
       project = create(:project)
       moderator = create(:moderator, project: project)
