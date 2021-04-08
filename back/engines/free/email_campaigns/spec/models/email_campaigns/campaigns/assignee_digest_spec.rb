736fec20149203e41b38415eb6bb1015cf4e6f62
Adrien Dessy <adrien@citizenlab.co>
2021-04-08 11:41:00 +0200

    OS-262 Refactor assignee_digest_spec.rb

--- back/engines/free/email_campaigns/spec/models/email_campaigns/campaigns/assignee_digest_spec.rb
+++ back/engines/free/email_campaigns/spec/models/email_campaigns/campaigns/assignee_digest_spec.rb
@@ -1,28 +1,27 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe EmailCampaigns::Campaigns::AssigneeDigest, type: :model do
-  describe "AssigneeDigest Campaign default factory" do
-    it "is valid" do
-      expect(build(:assignee_digest_campaign)).to be_valid
-    end
+  describe 'AssigneeDigest Campaign default factory' do
+    it { expect(build(:assignee_digest_campaign)).to be_valid }
   end
 
-  describe "apply_recipient_filters" do
+  describe '#apply_recipient_filters' do
     let(:campaign) { build(:assignee_digest_campaign) }
 
-    it "filters out invitees" do
+    it 'filters out invitees' do
       admin = create(:admin)
-      invitee = create(:invited_user, roles: [{type: 'admin'}])
+      _invitee = create(:invited_user, roles: [{ type: 'admin' }])
 
       expect(campaign.apply_recipient_filters).to match([admin])
     end
 
-    it "filters out normal users" do
+    it 'filters out normal users' do
       admin = create(:admin)
-      moderator = create(:moderator)
-      user = create(:user)
+      _user = create(:user)
 
-      expect(campaign.apply_recipient_filters.map(&:id)).to match_array([admin.id, moderator.id])
+      expect(campaign.apply_recipient_filters.map(&:id)).to match_array([admin.id])
     end
   end
 end
