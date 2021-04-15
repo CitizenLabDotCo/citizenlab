11fb59804acfc6666a4ccc2a36eb75f1cf7a2ed3
Adrien Dessy <adrien@citizenlab.co>
2021-04-15 17:23:50 +0200

    OS-262 Refactor stat_vote_policy.rb

--- back/app/policies/stat_vote_policy.rb
+++ back/app/policies/stat_vote_policy.rb
@@ -9,91 +9,105 @@ class StatVotePolicy < ApplicationPolicy
     end
 
     def resolve
-      if user&.active? && user.admin?
-        scope.all
-      else user&.active? && user.project_moderator?
-        projects = ProjectPolicy::Scope.new(user, Project.all).moderatable
-        scope
-          .where(votable_type: 'Idea')
-          .joins("JOIN ideas ON ideas.id = votes.votable_id")
-          .where(ideas: {project_id: projects})
-      end
+      return scope.none unless user&.active?
+
+      resolve_for_active
+    end
+
+    private
+
+    def resolve_for_active
+      user.admin? ? scope.all : scope.none
     end
   end
 
   def votes_count?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_birthyear?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_domicile?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_education?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_gender?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_custom_field?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_time?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_time_cumulative?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_topic?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_project?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_birthyear_as_xlsx?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_domicile_as_xlsx?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_education_as_xlsx?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_gender_as_xlsx?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_custom_field_as_xlsx?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_time_as_xlsx?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_time_cumulative_as_xlsx?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_topic_as_xlsx?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
   end
 
   def votes_by_project_as_xlsx?
-    user&.active? && (user.admin? || user.project_moderator?)
+    show_stats?
+  end
+
+  private
+
+  def show_stats?
+    return unless user&.active?
+
+    show_stats_to_active?
+  end
+
+  def show_stats_to_active?
+    user.admin?
   end
 end
+
+StatVotePolicy.prepend_if_ee('ProjectManagement::Patches::StatVotePolicy')
