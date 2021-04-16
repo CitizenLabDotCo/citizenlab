6254738f286398479b366a79a330683d0348e0dd
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-04-16 16:30:37 +0200

    remove stat idea policy to ensure ideas are filtered equally per user role, independelty of controller

--- back/app/policies/stat_idea_policy.rb
+++ back/app/policies/stat_idea_policy.rb
@@ -1,25 +1,4 @@
 class StatIdeaPolicy < ApplicationPolicy
-
-  class Scope
-    attr_reader :user, :scope
-
-    def initialize(user, scope)
-      @user  = user
-      @scope = scope
-    end
-
-    def resolve
-      if user&.active? && user.admin?
-        scope.all
-      elsif user&.active? && user.project_moderator?
-        projects = ProjectPolicy::Scope.new(user, Project.all).moderatable
-        scope.where(project: projects)
-      else
-        scope.none
-      end
-    end
-  end
-
   def ideas_count?
     user&.active? && (user.admin? || user.project_moderator?)
   end
@@ -71,5 +50,4 @@ class StatIdeaPolicy < ApplicationPolicy
   def ideas_by_time_cumulative_as_xlsx?
     user&.active? && (user.admin? || user.project_moderator?)
   end
-
 end
