b369365e5800a50313b0f183f4f1455b4ff32d75
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-04-23 15:08:38 +0200

    code generator

--- back/engines/ee/multi_tenancy/app/services/multi_tenancy/patches/user_confirmation/code_generator.rb
+++ back/engines/ee/multi_tenancy/app/services/multi_tenancy/patches/user_confirmation/code_generator.rb
@@ -2,14 +2,14 @@ module MultiTenancy
   module UserConfirmation
     module CodeGenerator
       def call
-        result.code = '4532' && return if e2e?
+        result.code = '1234' && return if e2e?
         super
       end
 
       private
 
       def e2e?
-        AppConfiguration.instance.host == 'e2e.stg.citizenlab.co'
+        AppConfiguration.instance.host == 'e2e.stg.citizenlab.co' || Rails.env.development?
       end
     end
   end
