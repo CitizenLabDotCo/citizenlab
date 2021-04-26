f17c630aee9cbe21b834ac75be4f6fee8db6f934
Adrien Dessy <adrien@citizenlab.co>
2021-04-26 18:46:13 +0200

    OS-276 Refactor the user & moderator side fx services

--- back/engines/commercial/idea_assignment/app/models/idea_assignment/extensions/user.rb
+++ back/engines/commercial/idea_assignment/app/models/idea_assignment/extensions/user.rb
@@ -8,7 +8,32 @@ module IdeaAssignment
                    class_name: 'Project',
                    foreign_key: :default_assignee_id,
                    dependent: :nullify
+
+          after_update :clean_assignments, if: :saved_change_to_roles?
+          after_destroy :clear_assignments
         end
+      end
+
+      def clean_assignments
+        lost_roles = saved_change_to_roles[1] - saved_change_to_roles[0]
+        _clean_assignments(lost_roles)
+      end
+
+      def clear_assignments
+        _clean_assignments(roles)
+      end
+
+      def _clean_assignments(lost_roles)
+        return if (lost_roles.pluck('type') & %w[admin project_moderator]).empty?
+
+        moderatable_projects = ::ProjectPolicy::Scope.new(user, Project).moderatable
+        user.assigned_ideas
+            .where.not(project: moderatable_projects)
+            .update(assignee_id: nil, updated_at: DateTime.now)
+
+        user.default_assigned_projects
+            .where.not(id: moderatable_projects)
+            .update(default_assignee_id: nil, updated_at: DateTime.now)
       end
     end
   end
