a3b9d4e32c3be39444f3ed02df9e2f5dc33ff6ba
Sébastien Hoorens <sebastien@citizenlab.co>
2021-04-26 19:40:07 +0200

    added specs for searching blocked words + minor fix

--- back/app/services/profanity_service.rb
+++ back/app/services/profanity_service.rb
@@ -4,11 +4,10 @@ class ProfanityService
       locale.split('-').first
     end.uniq.flat_map do |lang|
       regex = Rails.cache.fetch("#{lang}/blocked_words", expires_in: 1.hour) do
-        blocked_words = open(Rails.root.join("config/blocked_words/#{lang}.txt")).readlines
-        blocked_words = blocked_words.map(&:strip).map{|w| normalize_text w}
+        blocked_words = fetch_blocked_words(lang).map{|w| normalize_text w}
         /(#{blocked_words.map{|word| Regexp.escape(word)}.join('|')})/
       end
-      normalize_text(text).enum_for(:scan, regex).map do |word| 
+      normalize_text(text).enum_for(:scan, regex).map(&:join).map do |word| 
         { 
           word: word,
           position: Regexp.last_match.begin(0),
@@ -18,9 +17,15 @@ class ProfanityService
     end
   end
 
+  private
+
   def normalize_text text
     # We could also consider removing accents and
     # substituting digits by resembling letters.
     text.downcase
+  end
+
+  def fetch_blocked_words lang
+    open(Rails.root.join("config/blocked_words/#{lang}.txt")).readlines.map(&:strip)
   end
 end
