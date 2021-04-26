74389ec3058d482a797532d278a2450d883f76c5
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-04-26 15:27:38 +0200

    CL2-6427 simplify code generation logic

--- back/engines/free/user_confirmation/app/services/user_confirmation/code_generator.rb
+++ back/engines/free/user_confirmation/app/services/user_confirmation/code_generator.rb
@@ -3,16 +3,7 @@ module UserConfirmation
     include Callable
 
     def call
-      loop do
-        code = random_code
-
-        result.code = code unless existing_codes.include?(code)
-        break if result.code
-      end
-    end
-
-    def existing_codes
-      @existing_codes ||= User.pluck(:email_confirmation_code)
+      random_code
     end
 
     def random_code
