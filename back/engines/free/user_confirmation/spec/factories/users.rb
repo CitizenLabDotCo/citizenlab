cde01325f063981130435a8168acd64b5c075dca
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-04-27 10:41:40 +0200

    CL2-6427 another try at fixing factories

--- back/engines/free/user_confirmation/spec/factories/users.rb
+++ back/engines/free/user_confirmation/spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user_with_confirmation, parent: :user do
-    after(:build) do |user, evaluator|
-      user.reset_confirmation_code
+    after(:create) do |user, evaluator|
+      user.reset_confirmation_code!
     end
   end
 end
