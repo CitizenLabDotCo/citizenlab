01910f125415970f9129e93c7ed619ad634fb02d
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-04-27 12:39:58 +0200

    CL2-6427 fix reset code spec

--- back/engines/free/user_confirmation/spec/interactors/user_confirmation/reset_user_confirmation_code_spec.rb
+++ back/engines/free/user_confirmation/spec/interactors/user_confirmation/reset_user_confirmation_code_spec.rb
@@ -5,20 +5,6 @@ RSpec.describe UserConfirmation::ResetUserConfirmationCode do
 
   let(:context) { {} }
 
-  context 'when the email confirmation code is correct' do
-    before do
-      context[:user] = create(:user_with_confirmation)
-    end
-
-    it 'is successful' do
-      expect(result).to be_a_success
-    end
-
-    it 'enqueues a code expiration job' do
-      expect { result }.to enqueue_job(UserConfirmation::ExpireConfirmationCodeJob)
-    end
-  end
-
   context 'when the confirmation code generation mechanism is broken' do
     before do
       context[:user] = create(:user_with_confirmation)
