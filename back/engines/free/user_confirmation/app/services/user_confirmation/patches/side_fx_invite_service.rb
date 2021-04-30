f7f3f3f034c61c49814265a5fb74543c0c277ea2
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-04-30 11:56:25 +0200

    CL2-6427 add some feature flags in the BE

--- back/engines/free/user_confirmation/app/services/user_confirmation/patches/side_fx_invite_service.rb
+++ back/engines/free/user_confirmation/app/services/user_confirmation/patches/side_fx_invite_service.rb
@@ -3,6 +3,8 @@ module UserConfirmation
     module SideFxInviteService
       def before_accept(invite)
         super
+        return unless AppConfiguration.instance.feature_activated?('user_confirmation')
+
         invite.invitee.email_confirmed_at ||= invite.accepted_at
       end
     end
--- back/engines/free/user_confirmation/app/services/user_confirmation/patches/side_fx_user_service.rb
+++ back/engines/free/user_confirmation/app/services/user_confirmation/patches/side_fx_user_service.rb
@@ -3,6 +3,8 @@ module UserConfirmation
     module SideFxUserService
       def after_create(user, current_user)
         super
+        return unless AppConfiguration.instance.feature_activated?('user_confirmation')
+
         SendConfirmationCode.call(user: user) if user.requires_confirmation?
       end
     end
