14fced6afa6818a2f508c50056570b5ef3a1db0b
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-05-03 09:13:32 +0200

    CL2-6423 initial setup for confirmation required in the DB

--- back/db/migrate/20210430154637_add_confirmation_required_field_to_users.user_confirmation.rb
+++ back/db/migrate/20210430154637_add_confirmation_required_field_to_users.user_confirmation.rb
@@ -1,5 +1,15 @@
 class AddConfirmationRequiredFieldToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :confirmation_required, :boolean
+  def up
+    add_column :users, :confirmation_required, :boolean, default: true, null: false
+
+    users_that_do_not_require_confirmation = User.all.reject(&:will_require_confirmation?)
+
+    users_that_do_not_require_confirmation.each do |user|
+      user.update(confirmation_required: false)
+    end
+  end
+
+  def down
+    remove_column :users, :confirmation_required, :boolean, default: true, null: false
   end
 end
--- back/engines/free/user_confirmation/app/models/user_confirmation/extensions/user.rb
+++ back/engines/free/user_confirmation/app/models/user_confirmation/extensions/user.rb
@@ -3,6 +3,8 @@ module UserConfirmation
     module User
       def self.included(base)
         base.class_eval do
+          private :confirmation_required
+
           with_options if: -> { AppConfiguration.instance.feature_activated?('user_confirmation') } do
             validates :email_confirmation_code, format: { with: USER_CONFIRMATION_CODE_PATTERN }, allow_nil: true
             validates :email_confirmation_retry_count, numericality: { less_than_or_equal_to: ENV.fetch('EMAIL_CONFIRMATION_MAX_RETRIES', 5) }
@@ -10,24 +12,43 @@ module UserConfirmation
 
             before_validation :reset_confirmation_code, unless: :email_confirmation_code, if: :email_changed?
             before_validation :reset_confirmed_at, on: :update, if: :email_changed?
-            before_validation :reset_confirmation_required, on: :create
-
-            private :confirmation_required
+            before_validation :reset_confirmation_required
           end
         end
       end
 
+      public
+
+      #
+      # <Used to check upon update or create, if a user should have to confirm their account>
+      #
+      # @return [<Boolean>] <True if the user requires confirmation>
+      #
       def will_require_confirmation?
         !(active? || invited? || confirmed? || registered_with_phone? || highest_role != :user)
       end
 
+      #
+      # <The reader for the private `#confirmation_required` attribute.>
+      #
+      # @return [<Boolean>] <True if the user has not yet confirmed their account after creation or an update to it's details.>
+      #
       def confirmation_required?
         AppConfiguration.instance.feature_activated?('user_confirmation') &&
           confirmation_required
       end
 
+      #
+      # <Returns true if the user has performed confirmation of it's account.>
+      #
+      # @return [<Boolean>] <True has confirmed it's account.>
+      #
       def confirmed?
         email_confirmed_at.present?
+      end
+
+      def reset_confirmation_required
+        self.confirmation_required = will_require_confirmation?
       end
 
       def confirm!
@@ -45,10 +66,6 @@ module UserConfirmation
         reset_confirmation_code
         increment_confirmation_code_reset_count
         save!
-      end
-
-      def reset_confirmation_required
-        self.confirmation_required = will_require_confirmation?
       end
 
       def increment_confirmation_retry_count!
