6dbed1130cda28263e7e3ffc02f53292fc79854d
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-05-05 19:10:28 +0200

    CL2-6423 remove data migration from migration files

--- back/engines/free/user_confirmation/db/migrate/20210430154637_add_confirmation_required_field_to_users.rb
+++ back/engines/free/user_confirmation/db/migrate/20210430154637_add_confirmation_required_field_to_users.rb
@@ -1,17 +1,9 @@
 class AddConfirmationRequiredFieldToUsers < ActiveRecord::Migration[6.0]
   def up
     add_column :users, :confirmation_required, :boolean, default: true, null: false
-    confirm_active_users
   end
 
   def down
     remove_column :users, :confirmation_required, :boolean, default: true, null: false
-  end
-
-  def confirm_active_users
-    active_users = User.active
-    user_ids = active_users.reject(&:should_require_confirmation?).map(&:id)
-
-    User.where(id: user_ids).update_all(confirmation_required: false)
   end
 end
