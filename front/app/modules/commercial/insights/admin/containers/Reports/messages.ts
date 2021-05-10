c8c1d1a8db2b77b6972d8b4f419716eb4cb040a2
Iva <iva@citizenlab.co>
2021-05-10 12:04:34 +0300

    Remove duplicate translation keys

--- front/app/modules/commercial/insights/admin/containers/Reports/messages.ts
+++ front/app/modules/commercial/insights/admin/containers/Reports/messages.ts
@@ -2,131 +2,131 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   selectAProject: {
-    id: 'app.containers.Admin.Reports.selectAProject',
+    id: 'app.containers.Admin.Insights.Reports.selectAProject',
     defaultMessage: 'What project are you interested in ?',
   },
   sectionWho: {
-    id: 'app.containers.Admin.Reports.sectionWho',
+    id: 'app.containers.Admin.Insights.Reports.sectionWho',
     defaultMessage: 'Who participated ?',
   },
   sectionWhatInput: {
-    id: 'app.containers.Admin.Reports.sectionWhatInput',
+    id: 'app.containers.Admin.Insights.Reports.sectionWhatInput',
     defaultMessage: 'What input did you collect?',
   },
   participantsOverTimeTitle: {
-    id: 'app.containers.Admin.Reports.participantsOverTimeTitle',
+    id: 'app.containers.Admin.Insights.Reports.participantsOverTimeTitle',
     defaultMessage: 'Participants',
   },
   timelineType: {
-    id: 'app.containers.Admin.Reports.timelineType',
+    id: 'app.containers.Admin.Insights.Reports.timelineType',
     defaultMessage: 'Timeline',
   },
   continuousType: {
-    id: 'app.containers.Admin.Reports.continuousType',
+    id: 'app.containers.Admin.Insights.Reports.continuousType',
     defaultMessage: 'Continuous',
   },
   projectType: {
-    id: 'app.containers.Admin.Reports.projectType',
+    id: 'app.containers.Admin.Insights.Reports.projectType',
     defaultMessage: 'Project type : {projectType}',
   },
   fromTo: {
-    id: 'app.containers.Admin.Reports.fromTo',
+    id: 'app.containers.Admin.Insights.Reports.fromTo',
     defaultMessage: 'from {from} to {to}',
   },
   noPhase: {
-    id: 'app.containers.Admin.Reports.noPhase',
+    id: 'app.containers.Admin.Insights.Reports.noPhase',
     defaultMessage: 'No phase created for this project',
   },
   ideationAndFeedback: {
-    id: 'app.containers.Admin.Reports.ideationAndFeedback',
+    id: 'app.containers.Admin.Insights.Reports.ideationAndFeedback',
     defaultMessage: 'ideation and feedback',
   },
   information: {
-    id: 'app.containers.Admin.Reports.information',
+    id: 'app.containers.Admin.Insights.Reports.information',
     defaultMessage: 'information',
   },
   survey: {
-    id: 'app.containers.Admin.Reports.survey',
+    id: 'app.containers.Admin.Insights.Reports.survey',
     defaultMessage: 'survey',
   },
   budgeting: {
-    id: 'app.containers.Admin.Reports.budgeting',
+    id: 'app.containers.Admin.Insights.Reports.budgeting',
     defaultMessage: 'budgeting',
   },
   poll: {
-    id: 'app.containers.Admin.Reports.poll',
+    id: 'app.containers.Admin.Insights.Reports.poll',
     defaultMessage: 'poll',
   },
   volunteering: {
-    id: 'app.containers.Admin.Reports.volunteering',
+    id: 'app.containers.Admin.Insights.Reports.volunteering',
     defaultMessage: 'volunteering',
   },
   customFieldTitleExport: {
-    id: 'app.containers.Admin.Reports.customFieldTitleExport',
+    id: 'app.containers.Admin.Insights.Reports.customFieldTitleExport',
     defaultMessage: '{fieldName}_repartition',
   },
   participationPerTopic: {
-    id: 'app.containers.Admin.Reports.participationPerTopic',
+    id: 'app.containers.Admin.Insights.Reports.participationPerTopic',
     defaultMessage: 'Participation per topic',
   },
   noData: {
-    id: 'app.containers.Admin.Reports.noData',
+    id: 'app.containers.Admin.Insights.Reports.noData',
     defaultMessage: 'No data available with the current filters.',
   },
   inputs: {
-    id: 'app.containers.Admin.Reports.inputs',
+    id: 'app.containers.Admin.Insights.Reports.inputs',
     defaultMessage: 'Inputs',
   },
   votes: {
-    id: 'app.containers.Admin.Reports.votes',
+    id: 'app.containers.Admin.Insights.Reports.votes',
     defaultMessage: 'Votes',
   },
   comments: {
-    id: 'app.containers.Admin.Reports.comments',
+    id: 'app.containers.Admin.Insights.Reports.comments',
     defaultMessage: 'Comments',
   },
   commentsByTimeTitle: {
-    id: 'app.containers.Admin.Reports.commentsByTimeTitle',
+    id: 'app.containers.Admin.Insights.Reports.commentsByTimeTitle',
     defaultMessage: 'Comments',
   },
   fiveInputsWithMostVotes: {
-    id: 'app.containers.Admin.Reports.fiveInputsWithMostVotes',
+    id: 'app.containers.Admin.Insights.Reports.fiveInputsWithMostVotes',
     defaultMessage: 'Top 5 inputs by votes',
   },
   total: {
-    id: 'app.containers.Admin.Reports.total',
+    id: 'app.containers.Admin.Insights.Reports.total',
     defaultMessage: 'Total',
   },
   totalForPeriod: {
-    id: 'app.containers.Admin.Reports.totalForPeriod',
+    id: 'app.containers.Admin.Insights.Reports.totalForPeriod',
     defaultMessage: 'This {period}',
   },
   perPeriod: {
-    id: 'app.containers.Admin.Reports.perPeriod',
+    id: 'app.containers.Admin.Insights.Reports.perPeriod',
     defaultMessage: 'Per {period}',
   },
   numberOfVotesUp: {
-    id: 'app.containers.Admin.Reports.numberOfVotesUp',
+    id: 'app.containers.Admin.Insights.Reports.numberOfVotesUp',
     defaultMessage: 'Upvotes',
   },
   numberOfVotesDown: {
-    id: 'app.containers.Admin.Reports.numberOfVotesDown',
+    id: 'app.containers.Admin.Insights.Reports.numberOfVotesDown',
     defaultMessage: 'Downvotes',
   },
   day: {
-    id: 'app.containers.Admin.Reports.day',
+    id: 'app.containers.Admin.Insights.Reports.day',
     defaultMessage: 'day',
   },
   week: {
-    id: 'app.containers.Admin.Reports.week',
+    id: 'app.containers.Admin.Insights.Reports.week',
     defaultMessage: 'week',
   },
   month: {
-    id: 'app.containers.Admin.Reports.month',
+    id: 'app.containers.Admin.Insights.Reports.month',
     defaultMessage: 'month',
   },
   inputsByStatusTitle: {
-    id: 'app.containers.Admin.Reports.inputsByStatusTitle',
+    id: 'app.containers.Admin.Insights.Reports.inputsByStatusTitle',
     defaultMessage: 'Inputs by status',
   },
 });
