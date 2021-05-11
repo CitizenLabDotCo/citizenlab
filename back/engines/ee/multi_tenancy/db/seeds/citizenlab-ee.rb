b6c6efcc2c1d3fe3632169dcc8120133a6f5a948
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2021-05-11 16:53:34 +0200

    Added auth0 verification method to seeds and create_tenant

--- back/engines/ee/multi_tenancy/db/seeds/citizenlab-ee.rb
+++ back/engines/ee/multi_tenancy/db/seeds/citizenlab-ee.rb
@@ -384,6 +384,13 @@ if ['public','example_org'].include? Apartment::Tenant.current
           {
             name: 'franceconnect'
           },
+          {
+            name: 'auth0',
+            client_id: 'fake_client_id',
+            client_secret: 'fake_client_secret',
+            domain: 'fake_domain',
+            method_name_multiloc: {en: 'Verify with Auth0'}
+          },
         ],
       },
       volunteering: {
