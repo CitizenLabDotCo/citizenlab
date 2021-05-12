766f6d395632caf6364302943d90bb5d9718ab84
Adrien Dessy <adrien@citizenlab.co>
2021-05-12 15:24:48 +0200

    OS-262 Refactor moderator out of projects_filtering_service.rb

---
+++ back/engines/commercial/project_management/app/services/project_management/patches/projects_filtering_service.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module ProjectManagement
+  module Patches
+    module ProjectsFilteringService
+      def self.included(base)
+        base.class_eval do
+          add_filter('by_moderator') do |scope, options|
+            next scope unless options.key? :moderator
+
+            moderator = options[:moderator] # nil means the user is not logged in
+            ProjectPolicy::Scope.new(moderator, scope).moderatable
+          end
+        end
+      end
+    end
+  end
+end
