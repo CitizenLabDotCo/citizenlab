ea8591073f2d7fc2fe9f783fb9944e1e469b8996
Sébastien Hoorens <sebastien@citizenlab.co>
2021-05-20 15:36:56 +0200

    match blocked words in between spaces

--- back/app/services/profanity_service.rb
+++ back/app/services/profanity_service.rb
@@ -1,9 +1,33 @@
 class ProfanityService
+
+  IGNORE_SPECIAL_CHARS = '.?¿!,:;\'"(){}[]#@_'
+
+
   def search_blocked_words text
     AppConfiguration.instance.settings.dig('core', 'locales').map do |locale|
       locale.split('-').first
     end.uniq.flat_map do |lang|
-      regex = Rails.cache.fetch("#{lang}/blocked_words", expires_in: 1.hour) do
+      blocked_words = Rails.cache.fetch("#{lang}/blocked_words_list", expires_in: 1.hour) do
+        fetch_blocked_words(lang).map{|w| normalize_text w}
+      end
+      words = without_special_chars(normalize_text(text)).split ' '
+      blocked_words.select do |blocked_word|
+        words.include? blocked_word
+      end.map
+        { 
+          word: blocked_word
+          language: lang
+        }
+      end
+    end
+  end
+
+  # Keeping this when we would go back to regex solution
+  def search_blocked_occurences text
+    AppConfiguration.instance.settings.dig('core', 'locales').map do |locale|
+      locale.split('-').first
+    end.uniq.flat_map do |lang|
+      regex = Rails.cache.fetch("#{lang}/blocked_words_regex", expires_in: 1.hour) do
         blocked_words = fetch_blocked_words(lang).map{|w| normalize_text w}
         /(#{blocked_words.map{|word| Regexp.escape(word)}.join('|')})/
       end
@@ -23,6 +47,10 @@ class ProfanityService
     # We could also consider removing accents and
     # substituting digits by resembling letters.
     text.downcase
+  end
+
+  def without_special_chars text
+    text.tr(IGNORE_SPECIAL_CHARS,'')
   end
 
   def fetch_blocked_words lang
