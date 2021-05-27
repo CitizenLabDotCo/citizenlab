ca033a3e2c67c71b5a25f0d6352258192f416f87
brentguf <brent.guffens@gmail.com>
2021-05-27 14:58:49 +0200

    Move InappropriateContentWarning to module

---
+++ front/app/modules/commercial/flag_inappropriate_content/admin/components/messages.ts
@@ -0,0 +1,10 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  warningText: {
+    id:
+      'app.modules.commercial.flag_inappropriate_content.admin.components.warningText',
+    defaultMessage:
+      'Warning: possible inappropriate or offensive content detected',
+  },
+});
