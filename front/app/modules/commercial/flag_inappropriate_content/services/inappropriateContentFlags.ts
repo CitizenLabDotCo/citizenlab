856e2e57ee16479d80e2dd5cac0c12ef38ea77b8
brentguf <brent.guffens@gmail.com>
2021-05-31 12:12:02 +0200

    inappropriateContentFlagByIdStream

--- front/app/modules/commercial/flag_inappropriate_content/services/inappropriateContentFlags.ts
+++ front/app/modules/commercial/flag_inappropriate_content/services/inappropriateContentFlags.ts
@@ -1,7 +1,7 @@
 import { API_PATH } from 'containers/App/constants';
 import streams from 'utils/streams';
 
-interface IInappropriateContentFlagData {
+export interface IInappropriateContentFlagData {
   flaggable_type: string;
   flaggable_id: string;
   deleted_at: string;
@@ -14,9 +14,12 @@ export interface IInappropriateContentFlag {
 
 const apiEndpoint = `${API_PATH}/inappropriate_content_flag`;
 
-export function removeInappropriateContentFlag(flaggableId: string) {
-  return streams.delete(
-    `${apiEndpoint}/${flaggableId}#mark_as_deleted`,
-    flaggableId
-  );
+export function inappropriateContentFlagByIdStream(flagId: string) {
+  return streams.get<IInappropriateContentFlag>({
+    apiEndpoint: `${apiEndpoint}/${flagId}`,
+  });
+}
+
+export function removeInappropriateContentFlag(flagId: string) {
+  return streams.delete(`${apiEndpoint}/${flagId}#mark_as_deleted`, flagId);
 }
