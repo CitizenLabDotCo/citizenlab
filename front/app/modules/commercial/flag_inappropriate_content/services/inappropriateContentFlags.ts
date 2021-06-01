54be59a72b9ca12561f171c4f56759b574ee28b5
brentguf <brent.guffens@gmail.com>
2021-06-01 16:32:55 +0200

    IInappropriateContentFlagData comment

--- front/app/modules/commercial/flag_inappropriate_content/services/inappropriateContentFlags.ts
+++ front/app/modules/commercial/flag_inappropriate_content/services/inappropriateContentFlags.ts
@@ -4,8 +4,12 @@ import { IRelationship } from 'typings';
 
 type TReasonCode = 'inappropriate' | 'wrong' | 'other';
 
-interface IInappropriateContentFlagData {
+export interface IInappropriateContentFlagData {
   attributes: {
+    // We want to keep the flag alive to potentially readd it.
+    // Therefore, to mark a "removed" flag in the front-end,
+    // we check if the reason_code is set to null,
+    // which is what happens when removeInappropriateContentFlag below is called
     reason_code: TReasonCode | null;
     deleted_at: string | null;
     toxicity_label: string | null;
