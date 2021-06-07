dbd3288dcfb9c86d67698c74ec333d4bc3465868
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2021-06-07 18:22:24 +0200

    Fixed double question mark typo

--- back/app/services/side_fx_idea_service.rb
+++ back/app/services/side_fx_idea_service.rb
@@ -94,7 +94,7 @@ class SideFxIdeaService
 
   def scrape_facebook(idea)
     url = Frontend::UrlService.new.model_to_url(idea)
-    url_with_utm = "#{url}??utm_source=share_idea&utm_campaign=share_content&utm_medium=facebook"
+    url_with_utm = "#{url}?utm_source=share_idea&utm_campaign=share_content&utm_medium=facebook"
     Seo::ScrapeFacebookJob.perform_later(url_with_utm)
   end
 end
