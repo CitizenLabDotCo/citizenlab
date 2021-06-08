894c4109842aee704d7dd1c502729aaaa50fdddc
Sara <sara@citizenlab.co>
2021-06-08 19:27:24 +0200

    Fixes existing tests

--- front/app/modules/commercial/insights/admin/containers/Insights/Edit/InputsTable/InputsTable.test.tsx
+++ front/app/modules/commercial/insights/admin/containers/Insights/Edit/InputsTable/InputsTable.test.tsx
@@ -10,53 +10,57 @@ import InputsTable from './';
 
 const viewId = '1';
 
-let mockInputData = [
-  {
-    id: '4e9ac1f1-6928-45e9-9ac9-313e86ad636f',
-    type: 'input',
-    relationships: {
-      source: {
-        data: {
-          id: '4e9ac1f1-6928-45e9-9ac9-313e86ad636f',
-          type: 'idea',
+let mockInputData = {
+  currentPage: 1,
+  lastPage: 2,
+  list: [
+    {
+      id: '4e9ac1f1-6928-45e9-9ac9-313e86ad636f',
+      type: 'input',
+      relationships: {
+        source: {
+          data: {
+            id: '4e9ac1f1-6928-45e9-9ac9-313e86ad636f',
+            type: 'idea',
+          },
+        },
+        categories: {
+          data: [
+            {
+              id: '94a649b5-23fe-4d47-9165-9beceef2dcad',
+              type: 'category',
+            },
+            {
+              id: '94a649b5-23fe-4d47-9165-9becedfg45sd',
+              type: 'category',
+            },
+          ],
+        },
+        suggested_categories: {
+          data: [],
         },
       },
-      categories: {
-        data: [
-          {
-            id: '94a649b5-23fe-4d47-9165-9beceef2dcad',
-            type: 'category',
-          },
-          {
-            id: '94a649b5-23fe-4d47-9165-9becedfg45sd',
-            type: 'category',
-          },
-        ],
-      },
-      suggested_categories: {
-        data: [],
-      },
     },
-  },
-  {
-    id: '54438f73-12f4-4b16-84f3-a55bd118de7e',
-    type: 'input',
-    relationships: {
-      source: {
-        data: {
-          id: '54438f73-12f4-4b16-84f3-a55bd118de7e',
-          type: 'idea',
+    {
+      id: '54438f73-12f4-4b16-84f3-a55bd118de7e',
+      type: 'input',
+      relationships: {
+        source: {
+          data: {
+            id: '54438f73-12f4-4b16-84f3-a55bd118de7e',
+            type: 'idea',
+          },
+        },
+        categories: {
+          data: [],
+        },
+        suggested_categories: {
+          data: [],
         },
       },
-      categories: {
-        data: [],
-      },
-      suggested_categories: {
-        data: [],
-      },
     },
-  },
-];
+  ],
+};
 
 const mockIdeaData = {
   id: '2',
@@ -85,6 +89,7 @@ jest.mock('react-router', () => {
         return <Component {...props} params={{ viewId }} />;
       };
     },
+    location: {},
   };
 });
 
@@ -118,12 +123,12 @@ describe('Insights Input Table', () => {
 
     expect(spy).toHaveBeenCalledWith(
       viewId,
-      mockInputData[0].id,
-      mockInputData[0].relationships.categories.data[0].id
+      mockInputData.list[0].id,
+      mockInputData.list[0].relationships.categories.data[0].id
     );
   });
   it('renders table empty state when there are no inputs', () => {
-    mockInputData = [];
+    mockInputData = { list: [] };
     render(<InputsTable />);
     expect(
       screen.getByTestId('insightsInputsTableEmptyState')
