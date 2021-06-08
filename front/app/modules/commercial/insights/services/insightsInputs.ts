b249a1cc775b8e90999599d0531db94f661ab521
Iva <iva@citizenlab.co>
2021-06-08 14:40:02 +0300

    Remove unnecessary type

--- front/app/modules/commercial/insights/services/insightsInputs.ts
+++ front/app/modules/commercial/insights/services/insightsInputs.ts
@@ -3,20 +3,6 @@ import streams, { IStreamParams } from 'utils/streams';
 import { IRelationship } from 'typings';
 import { uuidRegExp } from 'utils/helperUtils';
 
-// This will be imported from the categories service once we merge everything
-export interface IInsightsCategoryData {
-  id: string;
-  type: string;
-  attributes: {
-    name: string;
-  };
-  relationships?: {
-    view: {
-      data: IRelationship;
-    };
-  };
-}
-
 export interface IInsightsInputData {
   id: string;
   type: string;
