a5e766c75c288ef7c505b6f333706f624d31f430
Sébastien Hoorens <sebastien@citizenlab.co>
2021-06-09 18:24:29 +0200

    automaticall list serializers for notifications

--- back/engines/commercial/flag_inappropriate_content/app/serializers/flag_inappropriate_content/web_api/v1/notifications/inappropriate_content_flagged_serializer.rb
+++ back/engines/commercial/flag_inappropriate_content/app/serializers/flag_inappropriate_content/web_api/v1/notifications/inappropriate_content_flagged_serializer.rb
@@ -1,5 +1,5 @@
 module FlagInappropriateContent
-  class WebApi::V1::InappropriateContentFlaggedSerializer < ::WebApi::V1::Notifications::NotificationSerializer
+  class WebApi::V1::Notifications::InappropriateContentFlaggedSerializer < ::WebApi::V1::Notifications::NotificationSerializer
     attribute :flaggable_url do |object|
       Frontend::UrlService.new.model_to_url object.inappropriate_content_flag.flaggable
     end
---
+++ back/engines/commercial/project_folders/app/serializers/project_folders/web_api/v1/notifications/project_folder_moderation_rights_received_serializer.rb
@@ -0,0 +1,7 @@
+class ProjectFolders::WebApi::V1::Notifications::ProjectFolderModerationRightsReceivedSerializer < ::WebApi::V1::Notifications::NotificationSerializer
+  attribute :project_folder_id
+
+  attribute :project_folder_title_multiloc do |object|
+    object.project_folder&.title_multiloc
+  end
+end
