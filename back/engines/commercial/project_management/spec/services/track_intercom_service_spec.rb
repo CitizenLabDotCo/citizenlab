43a042b6ba1d215297ae06c3e6493c68da3f92cd
Adrien Dessy <adrien@citizenlab.co>
2021-06-09 14:59:58 +0200

    OS-262 Fix intercom service specs

--- back/engines/commercial/project_management/spec/services/track_intercom_service_spec.rb
+++ back/engines/commercial/project_management/spec/services/track_intercom_service_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 describe TrackIntercomService do
-  let(:intercom) { instance_double(INTERCOM_CLIENT) }
+  let(:intercom) { double(INTERCOM_CLIENT).as_null_object }
   let(:service) { described_class.new(intercom) }
 
   describe 'identify_user' do
@@ -12,12 +12,15 @@ describe TrackIntercomService do
 
       # rubocop:disable RSpec/ExampleLength, RSpec/MultipleExpectations
       it "includes 'isProjectModerator' custom attribute" do
-        contacts_api = double
+        contacts_api = double('contacts_api').as_null_object
         expect(intercom).to receive(:contacts).twice.and_return(contacts_api)
+
+        zero_contacts = OpenStruct.new(count: 0)
+        expect(contacts_api).to receive(:search).and_return(zero_contacts)
 
         expect(contacts_api).to receive(:create) do |attributes|
           expect(attributes.dig(:custom_attributes, :isProjectModerator)).to eq(false)
-        end
+        end.and_return(double('contact').as_null_object)
 
         service.identify_user(user)
       end
@@ -29,12 +32,11 @@ describe TrackIntercomService do
 
       # rubocop:disable RSpec/ExampleLength, RSpec/MultipleExpectations
       it "includes 'isProjectModerator' custom attribute" do
-        contacts_api = double
+        contacts_api = double('contacts_api').as_null_object
         expect(intercom).to receive(:contacts).twice.and_return(contacts_api)
 
-        contact = double.as_null_object
-        expect(contacts_api).to receive(:search)
-          .and_return(double({ count: 1, :[] => contact }))
+        contact = double('contact').as_null_object
+        expect(contacts_api).to receive(:search).and_return(double({ count: 1, :[] => contact }))
 
         # The main assertion:
         expect(contact).to receive(:custom_attributes=).with(hash_including(isProjectModerator: false))
