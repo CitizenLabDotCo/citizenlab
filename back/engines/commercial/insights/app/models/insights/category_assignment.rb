854c112cd311c045a33c1da7dcdc303a915a8adf
Adrien Dessy <adrien@citizenlab.co>
2021-06-10 15:05:19 +0200

    IN-383 Touch view when modifying its categories or assignments

--- back/engines/commercial/insights/app/models/insights/category_assignment.rb
+++ back/engines/commercial/insights/app/models/insights/category_assignment.rb
@@ -9,5 +9,13 @@ module Insights
     validates :category, presence: true
     validates :input, presence: true
     validates :input_id, uniqueness: { scope: %i[input_type category], message: 'Assignment already exists'}
+
+    # Updates 'updated_at' of the view when actions are taken within the view.
+    after_destroy :touch_view
+    after_save :touch_view
+
+    def touch_view
+      category.view.touch if previous_changes.present?
+    end
   end
 end
