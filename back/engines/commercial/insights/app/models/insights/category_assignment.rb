4b82faeaf2de2847bfc13db3372c9488e914460d
Adrien Dessy <adrien@citizenlab.co>
2021-06-11 08:26:37 +0200

    IN-404 Validate input types in category assignments

--- back/engines/commercial/insights/app/models/insights/category_assignment.rb
+++ back/engines/commercial/insights/app/models/insights/category_assignment.rb
@@ -2,13 +2,18 @@
 
 module Insights
   class CategoryAssignment < ::ApplicationRecord
+    INPUT_TYPES = ['Idea'].freeze
+
     belongs_to :category
     belongs_to :input, polymorphic: true
 
-    validates :input_type, inclusion: { in: ['Idea'] }
+    # When modifying validators & callbacks here, make sure to update the batch
+    # insert/update in Insights::CategoryAssignmentsService accordingly, as
+    # they bypass the callbacks (including the validation).
+    validates :input_type, inclusion: { in: INPUT_TYPES }
     validates :category, presence: true
     validates :input, presence: true
-    validates :input_id, uniqueness: { scope: %i[input_type category], message: 'Assignment already exists'}
+    validates :input_id, uniqueness: { scope: %i[input_type category], message: 'Assignment already exists' }
 
     # Updates 'updated_at' of the view when actions are taken within the view.
     after_destroy :touch_view
