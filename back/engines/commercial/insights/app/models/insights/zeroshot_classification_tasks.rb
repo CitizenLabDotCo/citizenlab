3d83d29bda81d0eff48c84e4851a228e6d9b57e7
Adrien Dessy <adrien@citizenlab.co>
2021-06-21 11:25:15 +0200

    IN-438 Creating models for zeroshot classification tasks

---
+++ back/engines/commercial/insights/app/models/insights/zeroshot_classification_tasks.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Insights
+  class ZeroshotClassificationTasks < ::ApplicationRecord
+    has_and_belongs_to_many :categories, join_table: 'insights_zeroshot_classification_tasks_categories', foreign_key: :task_id
+    has_many :inputs_tasks, class_name: 'Insights::ZeroshotClassificationTasksInputs', foreign_key: :task_id, dependent: :destroy
+
+    validates :task_id, presence: true, uniqueness: true
+
+    def inputs
+      inputs_tasks.map(&:input)
+    end
+  end
+end
