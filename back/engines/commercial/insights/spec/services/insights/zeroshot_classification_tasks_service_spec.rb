3b112c2b306628355b489b892355f43b7d3a9b2a
Adrien Dessy <adrien@citizenlab.co>
2021-06-21 18:02:54 +0200

    IN-438 Add test for ZeroshotClassificationTasksService#delete_task

--- back/engines/commercial/insights/spec/services/insights/zeroshot_classification_tasks_service_spec.rb
+++ back/engines/commercial/insights/spec/services/insights/zeroshot_classification_tasks_service_spec.rb
@@ -13,11 +13,20 @@ describe Insights::ZeroshotClassificationTasksService do
       task_id = 'some-task-id'
       task = service.create_task(task_id, inputs, categories)
 
-      aggregate_failures "check created task" do
+      aggregate_failures 'check created task' do
         expect(task.task_id).to eq(task_id)
         expect(task.inputs).to match(inputs)
         expect(task.categories).to match(categories)
       end
+    end
+  end
+
+  describe '#delete_task' do
+    let(:task) { create(:zsc_task) }
+
+    it 'deletes the task' do
+      service.delete_task(task.task_id)
+      expect { task.reload }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
