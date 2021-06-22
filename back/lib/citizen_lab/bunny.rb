77b7d9f9a7748e840b628a4dfa4c8e8b408e91b1
Adrien Dessy <adrien@citizenlab.co>
2021-06-22 16:31:57 +0200

    IN-442 Improve documentation
    IN-442 #time 0w 0d 0h 1m Improve documentation

--- back/lib/citizen_lab/bunny.rb
+++ back/lib/citizen_lab/bunny.rb
@@ -7,6 +7,7 @@ module CitizenLab
     # @param [String] rabbitmq_uri
     # @param [Integer] max_retries
     # @param [Integer] retry_delay in seconds
+    # @return [Bunny::Session] A bunny session with the connection already started
     def self.connect(rabbitmq_uri, max_retries: 10, retry_delay: 5)
       retries ||= 0
       ::Bunny.new(rabbitmq_uri, automatically_recover: true, continuation_timeout: 20_000).start
