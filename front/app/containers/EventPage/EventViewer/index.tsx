a7c0365fd60113614081d7c99c5d5ff907672fd8
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2021-06-24 10:54:29 +0200

    [CL2-6566] added 'no upcoming events' illustration

--- front/app/containers/EventPage/EventViewer/index.tsx
+++ front/app/containers/EventPage/EventViewer/index.tsx
@@ -2,16 +2,39 @@ import React, { memo } from 'react';
 
 // components
 import TopBar from './TopBar';
-import NoEventsPicture from './NoEventsPicture.svg';
+
+// svg
+import noEventsIllustration from './NoEventsPicture.svg';
 
 // styling
 import styled from 'styled-components';
+import { colors, fontSizes } from 'utils/styleUtils';
 
 const PlaceHolder = styled.div`
   background-color: green;
   width: 100%;
   height: 237px;
   margin-top: 33px;
+`;
+
+const NoEventsContainer = styled.figure`
+  position: relative;
+  width: 100%;
+  margin: 78px 0px 125px 0px;
+`;
+
+const NoEventsIllustration = styled.img`
+  width: 345px;
+  height: 286px;
+  display: block;
+  margin: 0px auto;
+`;
+
+const NoEventsText = styled.figcaption`
+  margin: 37px auto 0px auto;
+  text-align: center;
+  color: ${colors.label};
+  font-size: ${fontSizes.xl}px;
 `;
 
 interface Props {
@@ -27,7 +50,13 @@ const EventViewer = memo<Props>(({ title, events }) => {
       {events.length > 0 &&
         events.map((e) => <PlaceHolder key={e}>{e}</PlaceHolder>)}
 
-      {events.length === 0 && <NoEventsPicture />}
+      {events.length === 0 && (
+        <NoEventsContainer>
+          <NoEventsIllustration src={noEventsIllustration} />
+
+          <NoEventsText>There are no upcoming events</NoEventsText>
+        </NoEventsContainer>
+      )}
     </>
   );
 });
