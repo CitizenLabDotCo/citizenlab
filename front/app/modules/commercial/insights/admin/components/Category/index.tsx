ea31b195afca50575d6a66d8e7cb23c04ad8253b
Iva <iva@citizenlab.co>
2021-06-29 14:04:44 +0300

    Remove confirmation message

--- front/app/modules/commercial/insights/admin/components/Category/index.tsx
+++ front/app/modules/commercial/insights/admin/components/Category/index.tsx
@@ -4,34 +4,25 @@ import { isNilOrError } from 'utils/helperUtils';
 import useCategory from 'modules/commercial/insights/hooks/useInsightsCategory';
 import Tag from 'modules/commercial/insights/admin/components/Tag';
 import { deleteInsightsInputCategory } from 'modules/commercial/insights/services/insightsInputs';
-import messages from './messages';
-
-import { injectIntl } from 'utils/cl-intl';
-import { InjectedIntlProps } from 'react-intl';
 
 import { withRouter, WithRouterProps } from 'react-router';
 
 export type CategoryProps = {
   id: string;
   inputId: string;
-} & WithRouterProps &
-  InjectedIntlProps;
+} & WithRouterProps;
 
-const Category = ({
-  id,
-  inputId,
-  params: { viewId },
-  intl: { formatMessage },
-}: CategoryProps) => {
+const Category = ({ id, inputId, params: { viewId } }: CategoryProps) => {
   const category = useCategory(viewId, id);
 
   if (isNilOrError(category)) {
     return null;
   }
   const handleRemoveCategory = () => {
-    const deleteMessage = formatMessage(messages.deleteCategoryConfirmation);
-    if (window.confirm(deleteMessage)) {
+    try {
       deleteInsightsInputCategory(viewId, inputId, id);
+    } catch {
+      // Do nothing
     }
   };
 
@@ -44,4 +35,4 @@ const Category = ({
   );
 };
 
-export default withRouter(injectIntl(Category));
+export default withRouter(Category);
