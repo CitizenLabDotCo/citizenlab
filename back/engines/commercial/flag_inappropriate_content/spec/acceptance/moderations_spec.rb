6727d8e13c223f856dd7e52878aa9b18617682e8
Sébastien Hoorens <sebastien@citizenlab.co>
2021-06-30 16:25:56 +0200

    added is_flagged filter for moderations endpoint

--- back/engines/commercial/flag_inappropriate_content/spec/acceptance/moderations_spec.rb
+++ back/engines/commercial/flag_inappropriate_content/spec/acceptance/moderations_spec.rb
@@ -9,6 +9,7 @@ resource 'Moderations' do
       parameter :number, 'Page number'
       parameter :size, 'Number of moderations per page'
     end
+    parameter :is_flagged, 'Filter moderations that have been flagged or that are not flagged (boolean)', required: false
 
     context 'when moderator' do
       before do
@@ -20,6 +21,7 @@ resource 'Moderations' do
         @idea = create(:idea, project: @project)
         @comment = create(:comment, post: @idea)
         @flag = create(:inappropriate_content_flag, flaggable: @comment, toxicity_label: 'insult')
+        @other_idea = create(:idea, project: @project)
       end
       
       example_request 'Moderations include inappropriate content flag' do
@@ -29,6 +31,28 @@ resource 'Moderations' do
         expect(json_response[:included].map{|d| d.dig(:attributes, :toxicity_label)}).to include('insult')
         idea_data = json_response[:data].find{|d| d[:id] == @idea.id}
         expect(idea_data[:relationships].keys).not_to include(:inappropriate_content_flag)
+      end
+
+      describe do
+        let(:is_flagged) { true }
+
+        example_request 'Filter moderations that are flagged' do
+          expect(status).to eq(200)
+          json_response = json_parse(response_body)
+          expect(json_response[:data].size).to eq 1
+          expect(json_response[:data].map{|d| d[:id]}).to match_array [@comment.id]
+        end
+      end
+
+      describe do
+        let(:is_flagged) { false }
+
+        example_request 'Filter moderations that are not flagged' do
+          expect(status).to eq(200)
+          json_response = json_parse(response_body)
+          expect(json_response[:data].size).to eq 2
+          expect(json_response[:data].map{|d| d[:id]}).to match_array [@idea.id, @other_idea.id]
+        end
       end
     end
   end
