d057c3bce8fd10ec0bbec807f92a3ab35042da7e
Adrien Dessy <adrien@citizenlab.co>
2021-07-02 11:43:57 +0200

    IN-447 Add tests for create_classification_tasks_job.rb
    IN-447 #comment + add suggestion_service param for dependency injection
    + do not trigger request when there is no input/categories
    
    IN-447 #time 0w 0d 1h 38m Add tests for create_classification_tasks_job.rb

--- back/engines/commercial/insights/app/jobs/insights/create_classification_tasks_job.rb
+++ back/engines/commercial/insights/app/jobs/insights/create_classification_tasks_job.rb
@@ -5,14 +5,17 @@ module Insights
     queue_as :insights
 
     # +view+ must be provided if +inputs+ or +categories+ is not specified.
-    # 
+    #
     # @param [Enumerable<Insights::Category>, nil] categories
     # @param [Insights::View, nil] view
-    def run(inputs: nil, categories: nil, view: nil)
+    def run(inputs: nil, categories: nil, view: nil, suggestion_service: nil)
       inputs ||= view.scope.ideas
       categories ||= view.categories
+      suggestion_service ||= CategorySuggestionsService.new
 
-      CategorySuggestionsService.new.classify(inputs, categories)
+      return if inputs.blank? || categories.blank?
+
+      suggestion_service.classify(inputs, categories)
     end
   end
 end
