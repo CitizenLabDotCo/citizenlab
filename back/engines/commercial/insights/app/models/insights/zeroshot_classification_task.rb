c1ad76f1dccfb1c2ff8910c77197f040295d4d50
Adrien Dessy <adrien@citizenlab.co>
2021-07-05 16:32:26 +0200

    IN-447 Add acceptance tests for delete + get routes to /insights/.../tasks/category_suggestions

--- back/engines/commercial/insights/app/models/insights/zeroshot_classification_task.rb
+++ back/engines/commercial/insights/app/models/insights/zeroshot_classification_task.rb
@@ -3,14 +3,23 @@
 module Insights
   class ZeroshotClassificationTask < ::ApplicationRecord
     has_and_belongs_to_many :categories, join_table: 'insights_zeroshot_classification_tasks_categories', foreign_key: :task_id
-    has_many :tasks_inputs, 
-             class_name: 'Insights::ZeroshotClassificationTaskInput', 
-             foreign_key: :task_id, dependent: :destroy
+    has_many :tasks_inputs,
+      class_name: 'Insights::ZeroshotClassificationTaskInput',
+      foreign_key: :task_id, dependent: :destroy
 
     validates :task_id, presence: true, uniqueness: true
 
     def inputs
       tasks_inputs.map(&:input)
+    end
+
+    def add_inputs(inputs)
+      tasks_inputs_attrs = inputs.map { |i| { input: i } }
+      tasks_inputs.create(tasks_inputs_attrs)
+    end
+
+    def add_input(input)
+      add_inputs([input])
     end
   end
 end
--- back/engines/commercial/insights/spec/acceptance/tasks/category_suggestions_spec.rb
+++ back/engines/commercial/insights/spec/acceptance/tasks/category_suggestions_spec.rb
@@ -4,15 +4,77 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
 resource 'Category-suggestion tasks' do
+
+  shared_examples 'unauthorized requests' do
+    context 'when visitor' do
+      example 'unauthorized', document: false do
+        do_request
+        expect(status).to eq(401)
+      end
+    end
+
+    context 'when normal user' do
+      before { user_header_token }
+
+      example 'unauthorized', document: false do
+        do_request
+        expect(status).to eq(401)
+      end
+    end
+  end
+
   before { header 'Content-Type', 'application/json' }
+
+  let(:view) { create(:view) }
+  let(:view_id) { view.id }
+
+  get 'web_api/v1/insights/views/:view_id/tasks/category_suggestions' do
+    parameter :inputs, 'An array of input identifiers'
+    parameter :categories, 'An array of category identifiers'
+
+    context 'when admin' do
+      before { admin_header_token }
+
+      let(:c1) { create(:category, view: view) }
+      let(:c2) { create(:category, view: view) }
+
+      let!(:tasks_c1) { create_list(:zsc_task, 2, categories: [c1]) }
+      let!(:tasks_c2) { create_list(:zsc_task, 1, categories: [c2]) }
+      let!(:other_task) { create(:zsc_task) } # task in another view
+
+      example_request 'returns pending tasks' do
+        expect(status).to eq(200)
+        expected_tasks = tasks_c1 + tasks_c2
+        expect(json_response_body[:data].pluck(:id)).to match(expected_tasks.pluck(:id))
+      end
+
+      example 'returns pending tasks for a subset of categories', document: false do
+        do_request(categories: [c1.id])
+        expect(status).to eq(200)
+        expect(json_response_body[:data].pluck(:id)).to match(tasks_c1.pluck(:id))
+      end
+
+      example 'returns pending tasks for a subset of inputs', document: false do
+        task_1, task_2 = expected_tasks = [tasks_c1.first, tasks_c2.first]
+        input_1, input_2 = inputs = create_list(:idea, 2, project: view.scope) # we need inputs that belong to the view
+        
+        task_1.add_input(input_1)
+        task_2.add_input(input_2)
+
+        do_request(inputs: inputs.pluck(:id))
+
+        expect(status).to eq(200)
+        expect(json_response_body[:data].pluck(:id)).to match(expected_tasks.pluck(:id))
+      end
+
+    end
+
+    include_examples 'unauthorized requests'
+  end
 
   post 'web_api/v1/insights/views/:view_id/tasks/category_suggestions' do
     parameter :inputs, 'An array of input identifiers'
     parameter :categories, 'An array of category identifiers'
-
-    let(:view) { create(:view) }
-
-    let(:view_id) { view.id }
 
     context 'when admin' do
       before { admin_header_token }
@@ -45,5 +107,37 @@ resource 'Category-suggestion tasks' do
         expect(status).to eq(202)
       end
     end
+
+    include_examples 'unauthorized requests'
+  end
+
+  delete 'web_api/v1/insights/views/:view_id/tasks/category_suggestions' do
+    context 'when admin' do
+      before do
+        admin_header_token
+        create_list(:zsc_task, 2, categories: [create(:category, view: view)])
+        create(:zsc_task)
+      end
+
+      example 'deletes all the tasks (of the view)' do
+        expect { do_request }.to change(Insights::ZeroshotClassificationTask, :count).from(3).to(1)
+        expect(status).to eq(200)
+      end
+    end
+  end
+
+  delete 'web_api/v1/insights/views/:view_id/tasks/category_suggestions/:task_id' do
+    context 'when admin' do
+      before { admin_header_token }
+
+      example 'deletes a task' do
+        task = create(:zsc_task, categories: [create(:category, view: view)])
+        do_request(task_id: task.id)
+        expect(status).to eq(200)
+        expect { task.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    include_examples 'unauthorized requests'
   end
 end
