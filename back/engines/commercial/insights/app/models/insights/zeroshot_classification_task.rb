ca46f3532e86869af9aecd6e4fdbfb4a9e170050
Adrien Dessy <adrien@citizenlab.co>
2021-07-06 09:14:21 +0200

    IN-447 Add documentation to zeroshot_classification_task.rb
    IN-447 #time 0w 0d 0h 7m Add documentation to zeroshot_classification_task.rb

--- back/engines/commercial/insights/app/models/insights/zeroshot_classification_task.rb
+++ back/engines/commercial/insights/app/models/insights/zeroshot_classification_task.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
 module Insights
+  # Prefer using +.create_task+ instead of the regular +.create+.
+  # It will take care of creating +Insights::ZeroshotClassificationTaskInput+
+  # records from inputs directly.
   class ZeroshotClassificationTask < ::ApplicationRecord
     has_and_belongs_to_many :categories, join_table: 'insights_zeroshot_classification_tasks_categories', foreign_key: :task_id
     has_many :tasks_inputs,
-      class_name: 'Insights::ZeroshotClassificationTaskInput',
-      foreign_key: :task_id, dependent: :destroy
+             class_name: 'Insights::ZeroshotClassificationTaskInput',
+             foreign_key: :task_id, dependent: :destroy
 
     validates :task_id, presence: true, uniqueness: true
 
+    # @param [String] task_id
+    # @param [Insights::Category] categories
+    # @return [ZeroshotClassificationTask]
     def self.create_task(task_id, inputs, categories)
       ZeroshotClassificationTask.transaction do
         task = ZeroshotClassificationTask.create(task_id: task_id, categories: categories)
