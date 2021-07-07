65d3b944966d3f8d0c2efe818492a18fa9183b60
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-07-07 09:40:57 +0200

    [CL2-6597] abstract location_point to a new concern

---
+++ back/app/models/concerns/locatable.rb
@@ -0,0 +1,18 @@
+module Locatable
+  extend ActiveSupport::Concern
+
+  included do
+    scope :with_bounding_box, (Proc.new do |coordinates|
+      x1,y1,x2,y2 = eval(coordinates)
+      where("ST_Intersects(ST_MakeEnvelope(?, ?, ?, ?), location_point)", x1, y1, x2, y2)
+    end)
+  end
+
+  def location_point_geojson
+    RGeo::GeoJSON.encode(location_point) if location_point.present?
+  end
+
+  def location_point_geojson= geojson_point
+    self.location_point = RGeo::GeoJSON.decode(geojson_point)
+  end
+end
