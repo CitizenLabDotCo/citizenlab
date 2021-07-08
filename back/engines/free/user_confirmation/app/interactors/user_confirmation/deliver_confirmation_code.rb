17eee1488ee280838ec8a7ccd8aa02117a086de1
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-07-08 13:12:52 +0200

    [CL2-6596] improve loggin when confirmation code sent is not recorded

--- back/engines/free/user_confirmation/app/interactors/user_confirmation/deliver_confirmation_code.rb
+++ back/engines/free/user_confirmation/app/interactors/user_confirmation/deliver_confirmation_code.rb
@@ -2,18 +2,22 @@ module UserConfirmation
   class DeliverConfirmationCode < ApplicationInteractor
     delegate :user, to: :context
 
-    def call
-      unless user.registered_with_email?
-        fail_with_error! :registration_method, :invalid, message: 'Confirmation is currently working for emails only.'
-      end
+    after do
+      LogActivityJob.perform_later(user, 'confirmation_code_sent', user, Time.now.to_i)
 
+      next unless user.email_confirmation_code_sent_at.nil?
+
+      Raven.capture_exception("Confirmation_code_sent_at not recorded.", tags: { type: 'Interactor', app: AppConfig.instance.host, user_id: user.id })
+    end
+
+    def call
       if Rails.env.test?
         ConfirmationsMailer.with(user: user).send_confirmation_code.deliver_now
       else
         ConfirmationsMailer.with(user: user).send_confirmation_code.deliver_later(priority: 1)
       end
 
-      user.update!(email_confirmation_code_sent_at: Time.zone.now)
+      user.update_attribute(email_confirmation_code_sent_at: Time.zone.now)
     end
   end
 end
