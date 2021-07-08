fb31da58fe9052328907be73e0a60b8ea1b6677a
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2021-07-08 11:42:34 +0200

    [CL2-6597] added some specs

--- back/app/serializers/web_api/v1/event_serializer.rb
+++ back/app/serializers/web_api/v1/event_serializer.rb
@@ -1,17 +1,9 @@
 class WebApi::V1::EventSerializer < WebApi::V1::BaseSerializer
-  attributes :title_multiloc, :location_multiloc, :start_at, :end_at, :created_at, :updated_at, :location_point_geojson
+  attributes :title_multiloc, :location_multiloc, :start_at, :end_at, :created_at, :updated_at, :location_point_geojson, :latitude, :longitude
 
   attribute :description_multiloc do |object|
     TextImageService.new.render_data_images object, :description_multiloc
   end
 
   belongs_to :project
-
-  def latitude
-    RGeo::GeoJSON.encode(object.location_point)&.dig('coordinates',1)
-  end
-
-  def longitude
-    RGeo::GeoJSON.encode(object.location_point)&.dig('coordinates',0)
-  end
 end
