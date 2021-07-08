6edccfbed8b4c0fcc1816b7f602d86d5b7677c76
Iva <iva@citizenlab.co>
2021-07-08 12:43:01 +0300

    Add views tests

---
+++ front/app/modules/commercial/insights/hooks/useInsightsInput.test.ts
@@ -0,0 +1,77 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useInsightsInput from './useInsightsInput';
+import { Observable, Subscription } from 'rxjs';
+import { waitFor } from 'utils/testUtils/rtl';
+import { insightsInputStream } from 'modules/commercial/insights/services/insightsInputs';
+
+const viewId = '1';
+const inputId = '4e9ac1f1-6928-45e9-9ac9-313e86ad636f';
+
+const mockInput = {
+  data: {
+    id: '4e9ac1f1-6928-45e9-9ac9-313e86ad636f',
+    type: 'input',
+    relationships: {
+      source: {
+        data: {
+          id: '4e9ac1f1-6928-45e9-9ac9-313e86ad636f',
+          type: 'idea',
+        },
+      },
+      categories: {
+        data: [],
+      },
+      suggested_categories: {
+        data: [],
+      },
+    },
+  },
+};
+
+let mockObservable = new Observable((subscriber) => {
+  subscriber.next(setTimeout(() => mockInput, 0));
+});
+
+jest.mock('modules/commercial/insights/services/insightsInputs', () => {
+  return {
+    insightsInputStream: jest.fn(() => {
+      return {
+        observable: mockObservable,
+      };
+    }),
+  };
+});
+
+describe('useInsightsInput', () => {
+  it('should call insightsInputStream with correct arguments', async () => {
+    renderHook(() => useInsightsInput(viewId, inputId));
+    expect(insightsInputStream).toHaveBeenCalledWith(viewId, inputId);
+  });
+  it('should return data when data', async () => {
+    const { result } = renderHook(() => useInsightsInput(viewId, inputId));
+    expect(result.current).toBe(undefined); // initially, the hook returns undefined
+    waitFor(() => expect(result.current).toBe(mockInput.data));
+  });
+  it('should return error when error', () => {
+    const error = new Error();
+    mockObservable = new Observable((subscriber) => {
+      subscriber.next({ data: new Error() });
+    });
+    const { result } = renderHook(() => useInsightsInput(viewId, inputId));
+    expect(result.current).toStrictEqual(error);
+  });
+  it('should return null when data is null', () => {
+    mockObservable = new Observable((subscriber) => {
+      subscriber.next({ data: null });
+    });
+    const { result } = renderHook(() => useInsightsInput(viewId, inputId));
+    expect(result.current).toBe(null);
+  });
+  it('should unsubscribe on unmount', () => {
+    spyOn(Subscription.prototype, 'unsubscribe');
+    const { unmount } = renderHook(() => useInsightsInput(viewId, inputId));
+
+    unmount();
+    expect(Subscription.prototype.unsubscribe).toHaveBeenCalledTimes(1);
+  });
+});
--- front/app/modules/commercial/insights/hooks/useInsightsView.test.ts
+++ front/app/modules/commercial/insights/hooks/useInsightsView.test.ts
@@ -1,29 +1,29 @@
 import { renderHook } from '@testing-library/react-hooks';
-import useInsightsCategory from './useInsightsCategory';
+import useInsightsView from './useInsightsView';
 import { Observable, Subscription } from 'rxjs';
 import { waitFor } from 'utils/testUtils/rtl';
-import { insightsCategoryStream } from 'modules/commercial/insights/services/insightsCategories';
+import { insightsViewStream } from 'modules/commercial/insights/services/insightsViews';
 
-const viewId = '1';
-const categoryId = '1aa8a788-3aee-4ada-a581-6d934e49784b';
+const viewId = '1aa8a788-3aee-4ada-a581-6d934e49784b';
 
-const mockCategory = {
+const mockView = {
   data: {
     id: '1aa8a788-3aee-4ada-a581-6d934e49784b',
-    type: 'category',
+    type: 'view',
     attributes: {
       name: 'Test',
+      updated_at: '2021-05-18T16:07:27.123Z',
     },
   },
 };
 
 let mockObservable = new Observable((subscriber) => {
-  subscriber.next(setTimeout(() => mockCategory, 0));
+  subscriber.next(setTimeout(() => mockView, 0));
 });
 
-jest.mock('modules/commercial/insights/services/insightsCategories', () => {
+jest.mock('modules/commercial/insights/services/insightsViews', () => {
   return {
-    insightsCategoryStream: jest.fn(() => {
+    insightsViewStream: jest.fn(() => {
       return {
         observable: mockObservable,
       };
@@ -31,42 +31,34 @@ jest.mock('modules/commercial/insights/services/insightsCategories', () => {
   };
 });
 
-describe('useInsightsCategory', () => {
-  it('should call insightsCategoryStream with correct arguments', async () => {
-    renderHook(() => useInsightsCategory(viewId, categoryId));
-    expect(insightsCategoryStream).toHaveBeenCalledWith(viewId, categoryId);
+describe('useInsightsView', () => {
+  it('should call insightsViewStream with correct arguments', async () => {
+    renderHook(() => useInsightsView(viewId));
+    expect(insightsViewStream).toHaveBeenCalledWith(viewId);
   });
   it('should return data when data', async () => {
-    const { result } = renderHook(() =>
-      useInsightsCategory(viewId, categoryId)
-    );
+    const { result } = renderHook(() => useInsightsView(viewId));
     expect(result.current).toBe(undefined); // initially, the hook returns undefined
-    waitFor(() => expect(result.current).toBe(mockCategory.data));
+    waitFor(() => expect(result.current).toBe(mockView.data));
   });
   it('should return error when error', () => {
     const error = new Error();
     mockObservable = new Observable((subscriber) => {
       subscriber.next({ data: new Error() });
     });
-    const { result } = renderHook(() =>
-      useInsightsCategory(viewId, categoryId)
-    );
+    const { result } = renderHook(() => useInsightsView(viewId));
     expect(result.current).toStrictEqual(error);
   });
   it('should return null when data is null', () => {
     mockObservable = new Observable((subscriber) => {
       subscriber.next({ data: null });
     });
-    const { result } = renderHook(() =>
-      useInsightsCategory(viewId, categoryId)
-    );
+    const { result } = renderHook(() => useInsightsView(viewId));
     expect(result.current).toBe(null);
   });
   it('should unsubscribe on unmount', () => {
     spyOn(Subscription.prototype, 'unsubscribe');
-    const { unmount } = renderHook(() =>
-      useInsightsCategory(viewId, categoryId)
-    );
+    const { unmount } = renderHook(() => useInsightsView(viewId));
 
     unmount();
     expect(Subscription.prototype.unsubscribe).toHaveBeenCalledTimes(1);
