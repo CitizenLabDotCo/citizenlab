7e6b2e8d59224a308d5555260b30d283c6f5cf0e
Sara <sara@citizenlab.co>
2021-07-09 15:47:59 +0200

    Fixes updated_at of view when changing processed flags

--- back/engines/commercial/insights/app/models/insights/processed_flag.rb
+++ back/engines/commercial/insights/app/models/insights/processed_flag.rb
@@ -5,7 +5,7 @@ module Insights
     self.table_name = 'insights_processed_flags'
 
     belongs_to :input, polymorphic: true
-    belongs_to :view
+    belongs_to :view, touch: true
 
     validates :input, presence: true
     validates :input_id, uniqueness: { scope: %i[input_type view], message: 'Flag already exists' }
