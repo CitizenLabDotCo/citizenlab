3bd7f1320ea4dd8e4636d5904a2ab4cd3cba2e5a
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2021-07-14 11:47:22 +0200

    [CL2-6569] added cards (WIP)

--- front/app/containers/LandingPage/EventsWidget/index.tsx
+++ front/app/containers/LandingPage/EventsWidget/index.tsx
@@ -2,21 +2,35 @@ import React from 'react';
 
 // components
 import TopBar from './TopBar';
+import EventCard from 'components/EventCard';
 
 // hooks
-// import useEvents from 'hooks/useEvents';
+import useEvents from 'hooks/useEvents';
 
 // styling
 import styled from 'styled-components';
+
+// other
+import { isNilOrError } from 'utils/helperUtils';
 
 const EventsWidgetContainer = styled.div`
   padding: 48px 0 124px 0;
 `;
 
 export default () => {
+  const { events } = useEvents({
+    futureOnly: true,
+    pageSize: 3,
+  });
+
   return (
     <EventsWidgetContainer>
       <TopBar />
+
+      {!isNilOrError(events) &&
+        events.map((event) => {
+          return <EventCard event={event} key={event.id} />;
+        })}
     </EventsWidgetContainer>
   );
 };
