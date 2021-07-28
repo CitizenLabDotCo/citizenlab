3ab348a05a3941e92bd475973be4b826cb5868bb
Iva <iva@citizenlab.co>
2021-07-28 14:27:20 +0300

    Add tags

--- front/app/modules/commercial/insights/admin/containers/Insights/Detect/index.tsx
+++ front/app/modules/commercial/insights/admin/containers/Insights/Detect/index.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { withRouter, WithRouterProps } from 'react-router';
+import useDetectedCategories from 'modules/commercial/insights/hooks/useInsightsDetectedCategories';
+import PageWrapper from 'components/admin/PageWrapper';
+import Tag from 'modules/commercial/insights/admin/components/Tag';
+import { isNilOrError } from 'utils/helperUtils';
 
-const Detect = () => {
-  return <div>Detect</div>;
+const Detect = ({ params: { viewId } }: WithRouterProps) => {
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+  const detectedCategories = useDetectedCategories(viewId);
+
+  if (isNilOrError(detectedCategories)) {
+    return null;
+  }
+
+  const handleCategoryClick = (selectedName: string) => () => {
+    if (selectedCategories.includes(selectedName)) {
+      setSelectedCategories(
+        selectedCategories.filter((name) => name !== selectedName)
+      );
+    } else {
+      setSelectedCategories([...selectedCategories, selectedName]);
+    }
+  };
+
+  return (
+    <PageWrapper>
+      {detectedCategories.names.map((name) => (
+        <Tag
+          label={name}
+          variant={selectedCategories.includes(name) ? 'primary' : 'default'}
+          key={name}
+          size="large"
+          onIconClick={handleCategoryClick(name)}
+        />
+      ))}
+    </PageWrapper>
+  );
 };
 
-export default Detect;
+export default withRouter(Detect);
