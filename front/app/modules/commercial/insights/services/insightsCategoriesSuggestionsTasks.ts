4ec7c1eeb8bd73172d456341b6495c40d5178891
Iva <iva@citizenlab.co>
2021-07-28 13:04:28 +0300

    Improve refetching

--- front/app/modules/commercial/insights/services/insightsCategoriesSuggestionsTasks.ts
+++ front/app/modules/commercial/insights/services/insightsCategoriesSuggestionsTasks.ts
@@ -3,7 +3,7 @@ import streams, { IStreamParams } from 'utils/streams';
 import { IRelationship } from 'typings';
 
 import { of } from 'rxjs';
-import { delay, repeat, takeWhile } from 'rxjs/operators';
+import { delay, repeat, takeWhile, skip } from 'rxjs/operators';
 
 const getInsightsCategorySuggestionsTasksEndpoint = (viewId: string) =>
   `insights/views/${viewId}/tasks/category_suggestions`;
@@ -50,6 +50,8 @@ export async function insightsTriggerCategoriesSuggestionsTasks(
   categories?: string[],
   inputs?: string[]
 ) {
+  const pollingStream = of({}).pipe(delay(3000), repeat());
+
   const response = await streams.add(
     `${API_PATH}/${getInsightsCategorySuggestionsTasksEndpoint(
       insightsViewId
@@ -60,28 +62,29 @@ export async function insightsTriggerCategoriesSuggestionsTasks(
     }
   );
 
-  const pollingSubscription = of({})
-    .pipe(delay(3000), repeat())
-    .subscribe(() => {
-      streams.fetchAllWith({
-        partialApiEndpoint: [
-          `insights/views/${insightsViewId}/tasks/category_suggestions`,
-        ],
-      });
+  // Refetch pending tasks at an interval
+  const subscription = pollingStream.subscribe(() => {
+    streams.fetchAllWith({
+      partialApiEndpoint: [
+        `insights/views/${insightsViewId}/tasks/category_suggestions`,
+      ],
     });
+  });
 
   insightsCategoriesSuggestionsTasksStream(insightsViewId, {
     queryParameters: { categories, inputs },
   })
     .observable.pipe(
-      // Only poll while there are remaining tasks
+      // Ignore the first emitted value coming from the hook
+      skip(1),
+      // Poll while there are pending tasks
       takeWhile((response) => {
-        // Refetch inputs when no tasks remain
         if (response.data.length === 0) {
+          // Refetch inputs when there are no pending tasks
           streams.fetchAllWith({
             partialApiEndpoint: [`insights/views/${insightsViewId}/inputs`],
           });
-          pollingSubscription.unsubscribe();
+          subscription.unsubscribe();
         }
         return response.data.length > 0;
       })
