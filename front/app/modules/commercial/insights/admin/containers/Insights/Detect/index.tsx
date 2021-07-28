20687f529debc138376235371fb1b113d8db4690
Iva <iva@citizenlab.co>
2021-07-28 16:08:00 +0300

    Add categories logic

--- front/app/modules/commercial/insights/admin/containers/Insights/Detect/index.tsx
+++ front/app/modules/commercial/insights/admin/containers/Insights/Detect/index.tsx
@@ -1,20 +1,33 @@
 import React, { useState } from 'react';
 import { withRouter, WithRouterProps } from 'react-router';
+
+// hooks
 import useDetectedCategories from 'modules/commercial/insights/hooks/useInsightsDetectedCategories';
+
+// components
 import PageWrapper from 'components/admin/PageWrapper';
 import Tag from 'modules/commercial/insights/admin/components/Tag';
-import { isNilOrError } from 'utils/helperUtils';
 import GoBackButton from 'components/UI/GoBackButton';
-import clHistory from 'utils/cl-router/history';
 import PageTitle from 'components/admin/PageTitle';
-import { injectIntl } from 'utils/cl-intl';
-import { InjectedIntlProps } from 'react-intl';
-import messages from '../messages';
-import styled from 'styled-components';
 import TopBar from '../../../components/TopBar';
 import Button from 'components/UI/Button';
 
+// utils
+import { isNilOrError } from 'utils/helperUtils';
+import clHistory from 'utils/cl-router/history';
+
+// intl
+import { injectIntl } from 'utils/cl-intl';
+import { InjectedIntlProps } from 'react-intl';
+import messages from '../messages';
+
+// styles
+import styled from 'styled-components';
 import { fontSizes } from 'utils/styleUtils';
+
+// services
+import { addInsightsCategory } from 'modules/commercial/insights/services/insightsCategories';
+
 const Container = styled.div`
   padding: 60px;
 `;
@@ -52,12 +65,30 @@ const Detect = ({
   params: { viewId },
   intl: { formatMessage },
 }: WithRouterProps & InjectedIntlProps) => {
+  const [processing, setProcessing] = useState(false);
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
   const detectedCategories = useDetectedCategories(viewId);
 
   if (isNilOrError(detectedCategories)) {
     return null;
   }
+
+  const goBack = () => {
+    clHistory.push(`/admin/insights/${viewId}/edit`);
+  };
+
+  const handleAddCategories = async () => {
+    setProcessing(true);
+    for (const name of selectedCategories) {
+      try {
+        await addInsightsCategory(viewId, name);
+      } catch {
+        // Do nothing
+      }
+    }
+    setProcessing(false);
+    goBack();
+  };
 
   const handleCategorySelection = (selectedName: string) => () => {
     if (selectedCategories.includes(selectedName)) {
@@ -67,10 +98,6 @@ const Detect = ({
     } else {
       setSelectedCategories([...selectedCategories, selectedName]);
     }
-  };
-
-  const goBack = () => {
-    clHistory.push(`/admin/insights/${viewId}/edit`);
   };
 
   return (
@@ -101,6 +128,8 @@ const Detect = ({
             <Button
               buttonStyle="admin-dark"
               disabled={selectedCategories.length === 0}
+              processing={processing}
+              onClick={handleAddCategories}
             >
               {selectedCategories.length === 1
                 ? formatMessage(messages.detectCategoriesAddCategory)
