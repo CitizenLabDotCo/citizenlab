1cf4b8779a51c93220afb3a85602f72f2c7f2d93
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2021-07-29 09:44:37 +0200

    Fixed Jira issue key detection

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,33 +1,29 @@
 import { warn, message, danger } from "danger";
 
-// Check whether the changelog was modified
+// Changelog modification
 const hasChangelog = danger.git.modified_files.includes("CHANGELOG.md");
 if (!hasChangelog) {
-  warn("No changelog entry was added");
+  warn("The changelog hasn't been modified");
 }
 
-// Check whether PR and branch have reference to Jira issue
-const JiraKeys = ["CL2", "OS", "WOR", "IN", "TEC"].join("|");
-const prTitle = danger.github.pr.title;
-const branchName = danger.github.pr.head.ref;
-const jiraIssueRegex = new RegExp(`(${JiraKeys})-[0-9]+`, "g");
+// PR title and branch Jira key reference
+const extractJiraKeys = (str: string): string[] => {
+  const jiraIssueRegex = new RegExp(`((CL2|OS|WOR|IN|TEC)-[0-9]+)`, "g");
+  return Array.from(str.matchAll(jiraIssueRegex)).map((m) => m[0]);
+};
 
-const matchPr = jiraIssueRegex.exec(prTitle);
-if (!matchPr) {
+const prKeys = extractJiraKeys(danger.github.pr.title);
+if (prKeys.length === 0) {
   warn("The PR title contains no Jira issue key (case-sensitive)");
-} else {
-  const jiraKey = matchPr[0];
-  message(
-    `<a href="https://citizenlab.atlassian.net/browse/${jiraKey}">${jiraKey}</a>`
-  );
 }
 
-const matchBranch = jiraIssueRegex.exec(branchName);
-if (!matchBranch) {
+const branchKeys = extractJiraKeys(danger.github.pr.head.ref);
+if (branchKeys.length === 0) {
   warn("The branch name contains no Jira issue key (case-sensitive)");
-} else {
-  const jiraKey = matchBranch[0];
-  message(
-    `<a href="https://citizenlab.atlassian.net/browse/${jiraKey}">${jiraKey}</a>`
-  );
 }
+
+new Set([...branchKeys, ...prKeys]).forEach((jiraKey) => {
+  message(
+    `Jira issue: <a href="https://citizenlab.atlassian.net/browse/${jiraKey}">${jiraKey}</a>`
+  );
+});
