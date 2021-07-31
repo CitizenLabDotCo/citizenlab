1b83f6471c2c79e7bd458af0e49eefb092c7e999
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2021-07-31 17:39:45 +0200

    CL2-6650 Added Gent Rrn verification method BE

--- back/engines/commercial/id_gent_rrn/app/lib/id_gent_rrn/gent_rrn_verification.rb
+++ back/engines/commercial/id_gent_rrn/app/lib/id_gent_rrn/gent_rrn_verification.rb
@@ -9,11 +9,11 @@ module IdGentRrn
     end
 
     def id
-      "8a6d6f7e-a451-41ea-8e0a-0021439923a0"
+      '8a6d6f7e-a451-41ea-8e0a-0021439923a0'
     end
 
     def name
-      "gent_rrn"
+      'gent_rrn'
     end
 
     def config_parameters
@@ -24,82 +24,52 @@ module IdGentRrn
       [:rrn]
     end
 
-    def verify_sync rrn:
-      raise Verification::VerificationService::ParameterInvalidError.new("run") unless run_valid?(run)
-      raise Verification::VerificationService::ParameterInvalidError.new("id_serial") unless id_serial_valid?(id_serial)
+    def verify_sync(rrn:)
+      cleaned_rrn = clean_rrn(rrn)
+      raise Verification::VerificationService::ParameterInvalidError, 'rrn' unless rnn_valid?(cleaned_rrn)
 
-      GentRrn_valid_citizen!(run, id_serial)
+      validate_citizen!(cleaned_rrn)
     end
 
     private
 
-    def GentRrn_valid_citizen! run, id_serial
-      client = Savon.client(SHARED_SAVON_CONFIG.merge({
-        wsse_auth: [config[:api_username], config[:api_password]]
-      }))
+    def validate_citizen!(rrn)
+      api = WijkBudgetApi.new(config[:api_key])
+      response = api.verificatie(rrn)
 
-      response = client.call(
-        :get_data_document,
-        message: {
-          "typens:RUTEmpresa" => config[:rut_empresa],
-          "typens:DVEmpresa" => 'k',
-          "typens:CodTipoDocumento" => 'C',
-          "typens:NumRUN" => clean_run(run),
-          "typens:NumSerie" => clean_id_serial(id_serial),
+      if response.success?
+        body = response.parsed_response
+
+        raise RuntimeError if body['ErrCodes'].include? 'ERR10'
+        raise Verification::VerificationService::NotEntitledError if body['ErrCodes'].include? 'ERR11'
+        raise Verification::VerificationService::NotEntitledError if body['ErrCodes'].include? 'ERR12'
+
+        raise Verification::VerificationService::NoMatchError unless body['isValid']
+
+        {
+          uid: rrn
         }
-      )
-
-      valid_response!(response.body[:get_data_document_response].slice(:ind_vigencia, :ind_bloqueo, :estado_respuesta))
-      {
-        uid: response.body[:get_data_document_response][:num_serie]
-      }
-    end
-
-    # A transaction is successful if it meets one of the following rules:
-    # 
-    # 1:
-    # Any person with IndVigencia = S can vote.
-    # <type:IndVigencia>S</type:IndVigencia>
-    # 
-    # 2:
-    # If IndVigencia is not in 'S', check other rules.  Here are the exceptions that allow you to vote:
-    #     2.1
-    #     <type:IndVigencia>N</type:IndVigencia>
-    #     +
-    #     <type:IndBloqueo>NO BLOQUEADO</type:IndBloqueo>
-    #     2.2:
-    #     <type:IndVigencia>N</type:IndVigencia>
-    #     +
-    #     <type:IndBloqueo>RENOVACION</type:IndBloqueo>
-    #     2.3
-    #     <type:IndVigencia>N</type:IndVigencia>
-    #     +
-    #     <type:IndBloqueo>TEMPORAL</type:IndBloqueo>
-    # **Both IndVigencia and IndBloqueo conditions must be met.
-
-    # For all other combinations that do not fit in these 4 alternatives, they must be blocked from voting.
-    def valid_response! ind_vigencia: nil, ind_bloqueo: nil, estado_respuesta:
-      if estado_respuesta != '000'
-        raise Verification::VerificationService::NoMatchError.new
-      elsif not(ind_vigencia == "S" || (ind_vigencia == "N" && ["NO BLOQUEADO", "RENOVACION", "TEMPORAL"].include?(ind_bloqueo)))
-        raise Verification::VerificationService::NotEntitledError.new
+      else
+        raise RuntimeError
       end
     end
 
-    def run_valid? run
-      run.present? && /^\d{1,2}\.\d{3}\.\d{3}[-][0-9kK]{1}$/.match?(run.strip)
+    def rnn_valid?(rrn)
+      return false if rrn.blank?
+      return false if rrn.length != 11
+
+      unique_part = rrn[0..8]
+      check_digit = rrn[-2..-1].to_i
+
+      vanilla_check = 97 - (unique_part.to_i % 97)
+      y2k_check = 97 - ("2#{unique_part}".to_i % 97)
+      return false if check_digit != vanilla_check && check_digit != y2k_check
+
+      true
     end
 
-    def clean_run run
-      run.strip.split('-').first.tr('^0-9', '')
-    end
-
-    def id_serial_valid? id_serial
-      id_serial.strip.present?
-    end
-
-    def clean_id_serial id_serial
-      id_serial.tr('^0-9a-zA-Z', '')
+    def clean_rrn(rrn)
+      rrn.tr('^0-9', '')
     end
   end
 end
--- back/engines/commercial/id_gent_rrn/spec/acceptance/gent_rrn_verification_spec.rb
+++ back/engines/commercial/id_gent_rrn/spec/acceptance/gent_rrn_verification_spec.rb
@@ -2,186 +2,173 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 require 'savon/mock/spec_helper'
 
-resource "Verifications" do
-
-  explanation "A Verifications is an attempt from a user to get verified"
-  include Savon::SpecHelper
+resource 'Verifications' do
+  explanation 'A Verifications is an attempt from a user to get verified'
 
   before do
     @user = create(:user)
     token = Knock::AuthToken.new(payload: @user.to_token_payload).token
     header 'Authorization', "Bearer #{token}"
-    header "Content-Type", "application/json"
+    header 'Content-Type', 'application/json'
     configuration = AppConfiguration.instance
     settings = configuration.settings
     settings['verification'] = {
       allowed: true,
       enabled: true,
-      verification_methods: [{ name: 'GentRrn', api_username: 'fake_username', api_password: 'fake_password', rut_empresa: 'fake_rut_empresa' }],
+      verification_methods: [{ name: 'gent_rrn', api_key: 'fake_api_key' }]
     }
     configuration.save!
   end
 
-  post "web_api/v1/verification_methods/GentRrn/verification" do
+  post 'web_api/v1/verification_methods/gent_rrn/verification' do
     with_options scope: :verification do
-      parameter :run, "The RUN number of the citizen", required: true
-      parameter :id_serial, "The ID card serial number of the citizen", required: true
+      parameter :rrn, 'The rrn (rijksregister nummer) the user wants to validate their identity with', required: true
     end
-
-    before do
-      stub_request(:get, "https://terceros.sidiv.registrocivil.cl:8443/InteroperabilityPlateform/TercerosGentRrnProxyService?wsdl")
-        .to_return(status: 200, body: File.read("engines/commercial/id_gent_rrn/spec/fixtures/GentRrn_wsdl.xml"), headers: {})
-      savon.mock!
-    end
-    after do
-      savon.unmock!
-    end
-
-    # Uncomment this and fill out the credentials to do a real, non-mocked test
-    #
-    # describe do
-    #   before do
-    #     configuration = AppConfiguration.instance
-    #     settings = configuration.settings
-    #     settings['verification'] = {
-    #       allowed: true,
-    #       enabled: true,
-    #       verification_methods: [{
-    #         name: 'GentRrn',
-    #         api_username:'usr_im_penalolen_01',
-    #         api_password: 'realpasswordhere',
-    #         rut_empresa: 'realempresahere'
-    #       }],
-    #     }
-    #     configuration.save!
-    #     savon.unmock!
-    #   end
-    #   let(:run) { "14.533.402-0" }
-    #   let(:id_serial) { "518.137.850" }
-    #   example_request "Verify with GentRrn for real" do
-    #     expect(status).to eq(201)
-    #     expect(@user.reload.verified).to be true
-    #     expect(@user.verifications.first).to have_attributes({
-    #       method_name: "GentRrn",
-    #       user_id: @user.id,
-    #       active: true,
-    #       hashed_uid: '6260dc3f1756c9f3aa431798d855a98c4383ba64c73fd3e87e47f839fc7f112e'
-    #     })
-    #   end
-    # end
 
     describe do
-      let(:run) { "12.025.365-6" }
-      let(:id_serial) { "A001529382" }
-      example "Verify with GentRrn" do
-        savon.expects(:get_data_document)
-             .with(message: {
-               "typens:RUTEmpresa" => 'fake_rut_empresa',
-               "typens:DVEmpresa" => 'k',
-               "typens:CodTipoDocumento" => 'C',
-               "typens:NumRUN" => '12025365',
-               "typens:NumSerie" => 'A001529382',
-             })
-             .returns(File.read("engines/commercial/id_gent_rrn/spec/fixtures/get_data_document_match.xml"))
+      let(:rrn) { '85102317223' }
+
+      example 'Verify with a valid rrn' do
+        stub_request(:get, "https://unknown.domain/services/wijkbudget-api/v1/api/WijkBudget/verificatie/#{rrn}")
+          .to_return(status: 200, body: {
+            'rrn' => '85102317223',
+            'isValid' => true,
+            'wijkNr' => '5',
+            'ErrCodes' => []
+          }.to_json, headers: { 'Content-Type' => 'application/json' })
+
         do_request
         expect(status).to eq(201)
         expect(@user.reload.verified).to be true
         expect(@user.verifications.first).to have_attributes({
-          method_name: "GentRrn",
-          user_id: @user.id,
-          active: true,
-          hashed_uid: '6a35b8e317fad56c885efc7397d8c14b6c4008f549abc5ae27222e8b2e3380c5'
-        })
+                                                               method_name: 'gent_rrn',
+                                                               user_id: @user.id,
+                                                               active: true,
+                                                               hashed_uid: '2a77e70b71b206c4c9dcf263250847b101180a8303938ed88caa8e60bb5a5fcf'
+                                                             })
       end
     end
 
     describe do
-      let(:run) { "11.111.111-1" }
-      let(:id_serial) { "A001529382" }
-      example "[error] Verify with GentRrn without a match" do
-        savon.expects(:get_data_document)
-             .with(message: {
-               "typens:RUTEmpresa" => 'fake_rut_empresa',
-               "typens:DVEmpresa" => 'k',
-               "typens:CodTipoDocumento" => 'C',
-               "typens:NumRUN" => '11111111',
-               "typens:NumSerie" => 'A001529382',
-             })
-             .returns(File.read("engines/commercial/id_gent_rrn/spec/fixtures/get_data_document_no_match.xml"))
+      let(:rrn) { '85-10-23-172.23 ' }
+
+      example 'Verify with a valid rrn in non-normalized form' do
+        stub_request(:get, 'https://unknown.domain/services/wijkbudget-api/v1/api/WijkBudget/verificatie/85102317223')
+          .to_return(status: 200, body: {
+            'rrn' => '85102317223',
+            'isValid' => true,
+            'wijkNr' => '5',
+            'ErrCodes' => []
+          }.to_json, headers: { 'Content-Type' => 'application/json' })
+
         do_request
-        expect(status).to eq (422)
-        json_response = json_parse(response_body)
-        expect(json_response).to eq ({ :errors => { :base => [{ :error => "no_match" }] } })
+        expect(status).to eq(201)
+        expect(@user.reload.verified).to be true
+        expect(@user.verifications.first).to have_attributes({
+                                                               method_name: 'gent_rrn',
+                                                               user_id: @user.id,
+                                                               active: true,
+                                                               hashed_uid: '2a77e70b71b206c4c9dcf263250847b101180a8303938ed88caa8e60bb5a5fcf'
+                                                             })
       end
     end
 
     describe do
-      let(:run) { "11.111.111-1" }
-      let(:id_serial) { "A.001.529.382" }
-      example "[error] Verify with GentRrn with a match that's not entitled to verification" do
-        savon.expects(:get_data_document)
-             .with(message: {
-               "typens:RUTEmpresa" => 'fake_rut_empresa',
-               "typens:DVEmpresa" => 'k',
-               "typens:CodTipoDocumento" => 'C',
-               "typens:NumRUN" => '11111111',
-               "typens:NumSerie" => 'A001529382',
-             })
-             .returns(File.read("engines/commercial/id_gent_rrn/spec/fixtures/get_data_document_match_no_citizen.xml"))
+      let(:rrn) { '82110750220' }
+
+      example '[error] Verify with a rrn of citizen not living in Ghent' do
+        stub_request(:get, "https://unknown.domain/services/wijkbudget-api/v1/api/WijkBudget/verificatie/#{rrn}")
+          .to_return(status: 200, body: {
+            "rrn": '82110750220',
+            "isValid": true,
+            "wijkNr": '24',
+            "ErrCodes": ['ERR11']
+          }.to_json, headers: { 'Content-Type' => 'application/json' })
         do_request
-        expect(status).to eq (422)
+        expect(status).to eq(422)
+        expect(@user.reload.verified).to be false
         json_response = json_parse(response_body)
-        expect(json_response).to eq ({ :errors => { :base => [{ :error => "not_entitled" }] } })
+        expect(json_response).to eq({ errors: { base: [{ error: 'not_entitled' }] } })
       end
     end
 
     describe do
-      let(:run) { "125.326.452-1" }
-      let(:id_serial) { "A001529382" }
-      example_request "[error] Verify with GentRrn using invalid run" do
-        expect(status).to eq (422)
+      let(:rrn) { '11010115780' }
+
+      example '[error] Verify with a rrn of citizen less than 14 year old' do
+        stub_request(:get, "https://unknown.domain/services/wijkbudget-api/v1/api/WijkBudget/verificatie/#{rrn}")
+          .to_return(status: 200, body: {
+            "rrn": '11010115780',
+            "isValid": false,
+            "wijkNr": '',
+            "ErrCodes": ['ERR12']
+          }.to_json, headers: { 'Content-Type' => 'application/json' })
+        do_request
+        expect(status).to eq(422)
+        expect(@user.reload.verified).to be false
         json_response = json_parse(response_body)
-        expect(json_response).to eq ({ :errors => { :run => [{ :error => "invalid" }] } })
+        expect(json_response).to eq({ errors: { base: [{ error: 'not_entitled' }] } })
       end
     end
 
     describe do
-      let(:run) { "12.025.365-6" }
-      let(:id_serial) { "" }
-      example_request "[error] Verify with GentRrn using invalid id_serial" do
-        expect(status).to eq (422)
+      let(:rrn) { '172639283' }
+
+      example '[error] Verify with an invalid rrn' do
+        do_request
+        expect(status).to eq(422)
+        expect(@user.reload.verified).to be false
         json_response = json_parse(response_body)
-        expect(json_response).to eq ({ :errors => { :id_serial => [{ :error => "invalid" }] } })
+        expect(json_response).to eq({ errors: { rrn: [{ error: 'invalid' }] } })
+      end
+    end
+
+    describe do
+      let(:rrn) { '85102317223' }
+
+      example '[error] Verify with a non-matching rrn' do
+        stub_request(:get, "https://unknown.domain/services/wijkbudget-api/v1/api/WijkBudget/verificatie/#{rrn}")
+          .to_return(status: 200, body: {
+            "rrn": '85102317223',
+            "isValid": false,
+            "wijkNr": '',
+            "ErrCodes": []
+          }.to_json, headers: { 'Content-Type' => 'application/json' })
+        do_request
+        expect(status).to eq(422)
+        expect(@user.reload.verified).to be false
+        json_response = json_parse(response_body)
+        expect(json_response).to eq({ errors: { base: [{ error: 'no_match' }] } })
       end
     end
 
     describe do
       before do
         other_user = create(:user)
-        @run = "12.025.365-6"
-        @id_serial = "A001529382"
-        savon.expects(:get_data_document)
-             .with(message: :any)
-             .returns(File.read("engines/commercial/id_gent_rrn/spec/fixtures/get_data_document_match.xml"))
-
+        @rrn = '85102317223'
+        stub_request(:get, "https://unknown.domain/services/wijkbudget-api/v1/api/WijkBudget/verificatie/#{rrn}")
+          .to_return(status: 200, body: {
+            'rrn' => '85102317223',
+            'isValid' => true,
+            'wijkNr' => '5',
+            'ErrCodes' => []
+          }.to_json, headers: { 'Content-Type' => 'application/json' })
         Verification::VerificationService.new.verify_sync(
           user: other_user,
-          method_name: "GentRrn",
-          verification_parameters: { run: @run, id_serial: @id_serial }
+          method_name: 'gent_rrn',
+          verification_parameters: { rrn: @rrn }
         )
       end
-      let(:run) { @run }
-      let(:id_serial) { @id_serial }
-      example "[error] Verify with GentRrn using credentials that are already taken" do
-        savon.expects(:get_data_document)
-             .with(message: :any)
-             .returns(File.read("engines/commercial/id_gent_rrn/spec/fixtures/get_data_document_match.xml"))
+
+      let(:rrn) { @rrn }
+
+      example '[error] Verify with an rrn that has already been taken' do
         do_request
-        expect(status).to eq (422)
+        expect(status).to eq(422)
+        expect(@user.reload.verified).to be false
         json_response = json_parse(response_body)
-        expect(json_response).to eq ({ :errors => { :base => [{ :error => "taken" }] } })
+        expect(json_response).to eq({ errors: { base: [{ error: 'taken' }] } })
       end
     end
   end
-
 end
