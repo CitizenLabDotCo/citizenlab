79c8cb43cdb3694330835c425a63d33d6c782646
Simon Tharby <simontharby@gmail.com>
2021-08-03 19:16:09 +0100

    Rubocop for 20+ offenses in volunteering/xlsx_service.rb
    Still 3 remaining offenses, which need some thought or
    significant refactoring:
    
    engines/free/volunteering/app/services/volunteering/
    xlsx_service.rb:5:5: C: Style/ClassVars: Replace class var
    @@multiloc_service with a class instance var.
        @@multiloc_service = MultilocService.new
        ^^^^^^^^^^^^^^^^^^
    engines/free/volunteering/app/services/volunteering/
    xlsx_service.rb:7:5: C: Metrics/AbcSize: Assignment Branch
    Condition size for generate_xlsx is too high.
    [<10, 26, 3> 28.02/17]
        def generate_xlsx( ...
        ^^^^^^^^^^^^^^^^^^
    engines/free/volunteering/app/services/volunteering/
    xlsx_service.rb:7:5: C: Metrics/MethodLength: Method has
    too many lines. [18/10]
        def generate_xlsx( ...

--- back/engines/free/volunteering/app/services/volunteering/xlsx_service.rb
+++ back/engines/free/volunteering/app/services/volunteering/xlsx_service.rb
@@ -1,32 +1,42 @@
+# frozen_string_literal: true
+
 module Volunteering
   class XlsxService
-
     @@multiloc_service = MultilocService.new
 
-    def generate_xlsx pc, volunteers, view_private_attributes: false
+    def generate_xlsx(
+      participation_context,
+      volunteers,
+      view_private_attributes: false
+    )
       xlsx_service = ::XlsxService.new
       columns = [
-        {header: 'first_name', f: -> (v) { v.user.first_name }},
-        {header: 'last_name',  f: -> (v) { v.user.last_name }},
-        {header: 'email',      f: -> (v) { v.user.email }},
-        {header: 'date',       f: -> (v) { v.created_at }, skip_sanitization: true}
+        { header: 'first_name', f: ->(v) { v.user.first_name } },
+        { header: 'last_name',  f: ->(v) { v.user.last_name } },
+        { header: 'email',      f: ->(v) { v.user.email } },
+        { header: 'date',       f: ->(v) { v.created_at }, skip_sanitization: true }
       ]
-      if !view_private_attributes
+
+      unless view_private_attributes
         columns.select! do |c|
-          !%w(email).include?(c[:header])
+          %w[email].exclude?(c[:header])
         end
       end
+
       pa = Axlsx::Package.new
-      pc.causes.order(:ordering).each do |cause|
+
+      participation_context.causes.order(:ordering).each do |cause|
         sheetname = sanitize_title @@multiloc_service.t(cause.title_multiloc)
         xlsx_service.generate_sheet pa.workbook, sheetname, columns, volunteers.where(cause: cause)
       end
+
       pa.to_stream
     end
 
     private
 
-    # Sheet names, derived from Cause titles, can only be 31 characters long, and cannot contain the characters \ , / , * , ? , : , [ , ].
+    # Sheet names, derived from Cause titles, can only be 31 characters long,
+    # and cannot contain the characters \ , / , * , ? , : , [ , ].
     # We are being strict and removing any character that is not alphanumeric or a space.
     def sanitize_title(title)
       title[0..30].gsub(/[^A-Za-z0-9 ]/, '')
