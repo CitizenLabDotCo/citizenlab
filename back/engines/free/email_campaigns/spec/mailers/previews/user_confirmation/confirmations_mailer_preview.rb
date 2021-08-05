d58ba82eb6eb6e364abb5ccdc5502d4180c0d061
Sergey Kucher <s.e.kucher@gmail.com>
2021-08-05 17:54:31 +0500

    CL2-6627 remove preheader from user confirmation mailer
    https://citizenlab.atlassian.net/browse/CL2-6627

--- back/engines/free/email_campaigns/spec/mailers/previews/user_confirmation/confirmations_mailer_preview.rb
+++ back/engines/free/email_campaigns/spec/mailers/previews/user_confirmation/confirmations_mailer_preview.rb
@@ -2,7 +2,6 @@ module UserConfirmation
   class ConfirmationsMailerPreview < ActionMailer::Preview
     def send_confirmation_code
       user = User.first
-      user.reset_email_confirmation_code
       user.save
       UserConfirmation::ConfirmationsMailer.with(user: user).send_confirmation_code
     end
