9d5f4999be07a5a32ae3dcdc2a835bd140da356c
Adrien Dessy <adrien@citizenlab.co>
2021-08-06 11:23:45 +0200

    IN-474 Add specs and factory for TNA task_view's
    IN-474 #time 0w 0d 1h 53m Add specs and factory for TNA task_view's

---
+++ back/engines/commercial/insights/spec/models/insights/text_network_analysis_task_view_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Insights::TextNetworkAnalysisTaskView do
+
+  describe 'factory' do
+    subject(:task_view) { build(:tna_task_view) }
+
+    it { is_expected.to be_valid }
+  end
+
+  describe 'validations' do
+    subject(:task_view) { build(:tna_task_view) }
+
+    it { is_expected.to validate_presence_of(:task) }
+    it { is_expected.to validate_presence_of(:view) }
+    it { is_expected.to validate_presence_of(:language) }
+  end
+end
