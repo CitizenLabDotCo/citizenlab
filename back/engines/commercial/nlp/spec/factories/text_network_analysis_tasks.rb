2fb4f98cf3d5b22d56f970f9caaed26cc10e3dea
Adrien Dessy <adrien@citizenlab.co>
2021-08-09 12:12:31 +0200

    IN-474 Add tests for NLP::TextNetworkAnalysisTask and improvements to the factory
    IN-474 #time 0w 0d 0h 21m Add tests for NLP::TextNetworkAnalysisTask and improvements to the factory

--- back/engines/commercial/nlp/spec/factories/text_network_analysis_tasks.rb
+++ back/engines/commercial/nlp/spec/factories/text_network_analysis_tasks.rb
@@ -3,8 +3,9 @@
 require 'nlp/testing/dummy_task_handler'
 
 FactoryBot.define do
-  factory :tna_task, class: 'NLP::TextNetworkAnalysisTask' do
-    sequence :task_id
+  factory :text_network_analysis_task, aliases: [:tna_task], class: 'NLP::TextNetworkAnalysisTask' do
+    sequence(:task_id) { |n| "tna-task-#{n}" }
+
     handler_class { NLP::Testing::DummyTaskHandler }
   end
 end
