b2bfc828e03ec3ee1b9e67fece4a5b65e5afd3a4
Adrien Dessy <adrien@citizenlab.co>
2021-08-12 15:43:22 +0200

    IN-515 Add tests for the text-network FE adapter
    IN-515 #comment + refactoring to make testing easier
    
    IN-515 #time 0w 0d 0h 1m Add tests for the text-network FE adapter

--- back/engines/commercial/insights/app/services/insights/front_end_format_text_network.rb
+++ back/engines/commercial/insights/app/services/insights/front_end_format_text_network.rb
@@ -14,11 +14,15 @@ module Insights
     end
 
     def nodes
-      @nodes ||= keyword_nodes + cluster_nodes
+      @nodes ||= self.class.nodes(@network)
     end
 
-    def cluster_nodes
-      @cluster_nodes ||= @network.communities.map do |community|
+    def self.nodes(network)
+      keyword_nodes(network) + cluster_nodes(network)
+    end
+
+    def self.cluster_nodes(network)
+      network.communities.map do |community|
         {
           id: community.id,
           name: community_name(community),
@@ -28,41 +32,43 @@ module Insights
       end
     end
 
-    def community_name(community, n = 3)
+    def self.community_name(community, n = 3)
       community.children.take(n).map(&:name).join(', ')
     end
 
-    def keyword_nodes
-      @keyword_nodes ||= @network.communities.flat_map do |community|
-        community.children.map { |node| format_node(node, community.id) }
+    def self.keyword_nodes(network)
+      network.communities.flat_map do |community|
+        community.children.map do |node|
+          {
+            id: node.id,
+            name: node.name,
+            val: node.importance_score,
+            cluster_id: community.id
+          }
+        end
       end
     end
 
-    def format_node(node, cluster_id = nil)
-      {
-        id: node.id,
-        name: node.name,
-        val: node.importance_score,
-        cluster_id: cluster_id
-      }
-    end
-
     def links
-      @links ||= cluster_membership_links + inter_cluster_links
+      @links ||= self.class.links(@network)
     end
 
-    def cluster_membership_links
-      @network.communities.flat_map do |community|
+    def self.links(network)
+        cluster_membership_links(network) + inter_cluster_links(network)
+    end
+
+    def self.cluster_membership_links(network)
+      network.communities.flat_map do |community|
         community.children.map { |node| { source: community.id, target: node.id } }
       end
     end
 
-    def inter_cluster_links
-      links_index = @network.links.group_by do |link|
+    def self.inter_cluster_links(network)
+      links_index = network.links.group_by do |link|
         [link.from_id, link.to_id].sort
       end
 
-      community_pairs = @network.communities.combination(2).select do |c1, c2|
+      community_pairs = network.communities.combination(2).select do |c1, c2|
         c1.children_ids.product(c2.children_ids).any? do |id_pair|
           links_index[id_pair.sort]
         end
