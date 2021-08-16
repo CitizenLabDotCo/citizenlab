38efbb869db5d3b96cbdda81124e68ceae414f2b
brentguf <brent.guffens@gmail.com>
2021-08-16 17:05:52 +0200

    Don't add empty quotes for non default locales

--- front/internals/scripts/extract-intl.js
+++ front/internals/scripts/extract-intl.js
@@ -95,15 +95,17 @@ const extractFromFile = async (fileName) => {
       for (const message of metadata['react-intl'].messages) {
         for (const locale of locales) {
           const oldLocaleMapping = oldLocaleMappings[locale][message.id];
-          // Merge old translations into the babel extracted instances where react-intl is used
-          const newMsg = (locale === constants.DEFAULT_LOCALE) ? message.defaultMessage : '';
 
           // We don't allow duplicate definitions, so let's throw an error if we already came accross this one
           if (localeMappings[locale][message.id]) {
             throw new Error(`Duplicate definition found for id '${message.id}'`);
           }
 
-          localeMappings[locale][message.id] = (oldLocaleMapping) ? oldLocaleMapping : newMsg;
+          if (oldLocaleMapping) {
+            localeMappings[locale][message.id] = oldLocaleMapping;
+          } else if (locale === constants.DEFAULT_LOCALE) {
+            localeMappings[locale][message.id] = message.defaultMessage;
+          }
         }
       }
     }
