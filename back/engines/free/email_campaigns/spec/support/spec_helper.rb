a04f4d25d8c010839818e9b9962dce28b8c526d8
Sébastien Hoorens <sebastien@citizenlab.co>
2021-08-16 11:30:07 +0200

    loading spec translations

--- back/engines/free/email_campaigns/spec/support/spec_helper.rb
+++ back/engines/free/email_campaigns/spec/support/spec_helper.rb
@@ -1,7 +1,5 @@
-# frozen_string_literal: true
-
-# RSpec.configure do |config|
-#   config.before(:each, type: :mailer) do
-#     I18n.load_path += Dir[Rails.root.join('engines/*/*/spec/fixtures/locales/mailers.*.yml')]
-#   end
-# end
+RSpec.configure do |config|
+  config.before(:each, type: :mailer) do
+    I18n.load_path += Dir[Rails.root.join('engines/*/*/spec/fixtures/locales/mailers.*.yml')]
+  end
+end
