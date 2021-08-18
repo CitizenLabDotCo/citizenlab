c4f37156b4699cd3696e9eaa4577951b02347d76
brentguf <brent.guffens@gmail.com>
2021-08-18 14:42:17 +0200

    areas mockData

---
+++ front/app/hooks/fixtures/areas.ts
@@ -0,0 +1,91 @@
+// copied from https://developers.citizenlab.co/api-docs/frontweb_api/master/areas/list_all_areas.html
+
+const areas = [
+  {
+    id: '26910e4b-1ceb-45cf-8ecb-7f064cd42d60',
+    type: 'area',
+    attributes: {
+      title_multiloc: {
+        en: 'Westside',
+        'nl-BE': 'Westerbuurt',
+      },
+      description_multiloc: {
+        en:
+          '<p>A <i>calm</i> space to relax, where the city meets the woods.</p>',
+        'nl-BE':
+          '<p>Een <i>kalme</i> buurt om te relaxen, waar de stad en het bos samensmelten.</p>',
+      },
+      ordering: 0,
+    },
+  },
+  {
+    id: '03e5b332-7d00-4eb9-aecc-5a13a3f7b68f',
+    type: 'area',
+    attributes: {
+      title_multiloc: {
+        en: 'Westside',
+        'nl-BE': 'Westerbuurt',
+      },
+      description_multiloc: {
+        en:
+          '<p>A <i>calm</i> space to relax, where the city meets the woods.</p>',
+        'nl-BE':
+          '<p>Een <i>kalme</i> buurt om te relaxen, waar de stad en het bos samensmelten.</p>',
+      },
+      ordering: 1,
+    },
+  },
+  {
+    id: '7d56a50e-a39e-4103-a161-f4f9b770f05b',
+    type: 'area',
+    attributes: {
+      title_multiloc: {
+        en: 'Westside',
+        'nl-BE': 'Westerbuurt',
+      },
+      description_multiloc: {
+        en:
+          '<p>A <i>calm</i> space to relax, where the city meets the woods.</p>',
+        'nl-BE':
+          '<p>Een <i>kalme</i> buurt om te relaxen, waar de stad en het bos samensmelten.</p>',
+      },
+      ordering: 2,
+    },
+  },
+  {
+    id: '1b8d27c5-f36e-402b-8419-3029127ea442',
+    type: 'area',
+    attributes: {
+      title_multiloc: {
+        en: 'Westside',
+        'nl-BE': 'Westerbuurt',
+      },
+      description_multiloc: {
+        en:
+          '<p>A <i>calm</i> space to relax, where the city meets the woods.</p>',
+        'nl-BE':
+          '<p>Een <i>kalme</i> buurt om te relaxen, waar de stad en het bos samensmelten.</p>',
+      },
+      ordering: 3,
+    },
+  },
+  {
+    id: 'ff10b236-944b-455b-a018-0677ec462c3c',
+    type: 'area',
+    attributes: {
+      title_multiloc: {
+        en: 'Westside',
+        'nl-BE': 'Westerbuurt',
+      },
+      description_multiloc: {
+        en:
+          '<p>A <i>calm</i> space to relax, where the city meets the woods.</p>',
+        'nl-BE':
+          '<p>Een <i>kalme</i> buurt om te relaxen, waar de stad en het bos samensmelten.</p>',
+      },
+      ordering: 4,
+    },
+  },
+];
+
+export default areas;
