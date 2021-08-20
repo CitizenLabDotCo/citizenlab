2341ca513f70dec111c1b9a79610d2c99e4324e7
Simon Tharby <simontharby@gmail.com>
2021-08-20 17:10:16 +0100

    Add multi-value predicates for projects + related specs
    Now have 1 failing test in:
    back/engines/commercial/smart_groups/spec/services/side_fx_project_spec.rb

--- back/engines/commercial/smart_groups/app/lib/smart_groups/rules/participated_in_project.rb
+++ back/engines/commercial/smart_groups/app/lib/smart_groups/rules/participated_in_project.rb
@@ -4,13 +4,14 @@ module SmartGroups::Rules
     include DescribableRule
 
     PREDICATE_VALUES = %w(in not_in posted_in not_posted_in commented_in not_commented_in voted_idea_in not_voted_idea_in voted_comment_in not_voted_comment_in budgeted_in not_budgeted_in volunteered_in not_volunteered_in)
+    MULTIVALUE_PREDICATES = %w(in posted_in commented_in voted_idea_in voted_comment_in)
     VALUELESS_PREDICATES = []
 
     attr_accessor :predicate, :value
 
     validates :predicate, presence: true
     validates :predicate, inclusion: { in: PREDICATE_VALUES }
-    validates :value, presence: true, inclusion: { in: -> (record) { Project.ids } }
+    validates :value, presence: true
 
     def self.to_json_schema
       [
@@ -25,7 +26,7 @@ module SmartGroups::Rules
             },
             "predicate" => {
               "type": "string",
-              "enum": PREDICATE_VALUES - VALUELESS_PREDICATES,
+              "enum": PREDICATE_VALUES - (VALUELESS_PREDICATES + MULTIVALUE_PREDICATES),
             },
             "value" => {
               "description" => "The id of a project",
@@ -33,6 +34,30 @@ module SmartGroups::Rules
             }
           },
         },
+        {
+          "type": "object",
+          "required" => ["ruleType", "predicate", "value"],
+          "additionalProperties" => false,
+          "properties" => {
+            "ruleType" => {
+              "type" => "string",
+              "enum" => [rule_type],
+            },
+            "predicate" => {
+              "type": "string",
+              "enum": MULTIVALUE_PREDICATES,
+            },
+            "value" => {
+              "description" => "The ids of some of the projects",
+              "type" => "array",
+              "items" => {
+                "type" => "string"
+              },
+              "uniqueItems" => true,
+              "minItems" => 1
+            }
+          },
+        }
       ]
     end
 
@@ -54,46 +79,46 @@ module SmartGroups::Rules
 
       case predicate
       when 'in'
-        participants = participants_service.projects_participants([Project.find(value)])
+        participants = participants_service.projects_participants(Project.where(id: value))
         users_scope.where(id: participants)
       when 'not_in'
-        participants = participants_service.projects_participants([Project.find(value)])
+        participants = participants_service.projects_participants(Project.where(id: value))
         users_scope.where.not(id: participants)
       when 'posted_in'
-        participants = participants_service.projects_participants([Project.find(value)], actions: [:posting])
+        participants = participants_service.projects_participants(Project.where(id: value), actions: [:posting])
         users_scope.where(id: participants)
       when 'not_posted_in'
-        participants = participants_service.projects_participants([Project.find(value)], actions: [:posting])
+        participants = participants_service.projects_participants(Project.where(id: value), actions: [:posting])
         users_scope.where.not(id: participants)
       when 'commented_in'
-        participants = participants_service.projects_participants([Project.find(value)], actions: [:commenting])
+        participants = participants_service.projects_participants(Project.where(id: value), actions: [:commenting])
         users_scope.where(id: participants)
       when 'not_commented_in'
-        participants = participants_service.projects_participants([Project.find(value)], actions: [:commenting])
+        participants = participants_service.projects_participants(Project.where(id: value), actions: [:commenting])
         users_scope.where.not(id: participants)
       when 'voted_idea_in'
-        participants = participants_service.projects_participants([Project.find(value)], actions: [:idea_voting])
+        participants = participants_service.projects_participants(Project.where(id: value), actions: [:idea_voting])
         users_scope.where(id: participants)
       when 'not_voted_idea_in'
-        participants = participants_service.projects_participants([Project.find(value)], actions: [:idea_voting])
+        participants = participants_service.projects_participants(Project.where(id: value), actions: [:idea_voting])
         users_scope.where.not(id: participants)
       when 'voted_comment_in'
-        participants = participants_service.projects_participants([Project.find(value)], actions: [:comment_voting])
+        participants = participants_service.projects_participants(Project.where(id: value), actions: [:comment_voting])
         users_scope.where(id: participants)
       when 'not_voted_comment_in'
-        participants = participants_service.projects_participants([Project.find(value)], actions: [:comment_voting])
+        participants = participants_service.projects_participants(Project.where(id: value), actions: [:comment_voting])
         users_scope.where.not(id: participants)
       when 'budgeted_in'
-        participants = participants_service.projects_participants([Project.find(value)], actions: [:budgeting])
+        participants = participants_service.projects_participants(Project.where(id: value), actions: [:budgeting])
         users_scope.where(id: participants)
       when 'not_budgeted_in'
-        participants = participants_service.projects_participants([Project.find(value)], actions: [:budgeting])
+        participants = participants_service.projects_participants(Project.where(id: value), actions: [:budgeting])
         users_scope.where.not(id: participants)
       when 'volunteered_in'
-        participants = participants_service.projects_participants([Project.find(value)], actions: [:volunteering])
+        participants = participants_service.projects_participants(Project.where(id: value), actions: [:volunteering])
         users_scope.where(id: participants)
       when 'not_volunteered_in'
-        participants = participants_service.projects_participants([Project.find(value)], actions: [:volunteering])
+        participants = participants_service.projects_participants(Project.where(id: value), actions: [:volunteering])
         users_scope.where.not(id: participants)
       else
         raise "Unsupported predicate #{predicate}"
@@ -101,7 +126,13 @@ module SmartGroups::Rules
     end
 
     def description_value locale
-      Project.find(value).title_multiloc[locale]
+      if self.value.is_a? Array
+        value.map do |v|
+          Project.find(v).title_multiloc[locale]
+        end.join ', '
+      else
+        Project.find(value).title_multiloc[locale]
+      end
     end
 
   end
--- back/engines/commercial/smart_groups/spec/rules/participated_in_project_spec.rb
+++ back/engines/commercial/smart_groups/spec/rules/participated_in_project_spec.rb
@@ -5,23 +5,36 @@ describe SmartGroups::Rules::ParticipatedInProject do
   let(:valid_json_rule) {{
     'ruleType' => 'participated_in_project',
     'predicate' => 'in',
-    'value' => create(:project).id
+    'value' => create_list(:project, 2).map(&:id)
   }}
   let(:valid_rule) { SmartGroups::Rules::ParticipatedInProject.from_json(valid_json_rule) }
 
   describe "from_json" do
-
     it "successfully parses a valid json" do
       expect(valid_rule.predicate).to eq valid_json_rule['predicate']
       expect(valid_rule.value).to eq valid_json_rule['value']
     end
-
   end
 
   describe "validations" do
-    it "successfully validate the valid rule" do
+    it "accepts a rule with a mutli-value predicate and an array of values" do
       expect(valid_rule).to be_valid
       expect(build(:smart_group, rules: [valid_json_rule])).to be_valid
+    end
+    it "rejects a rule with a mutli-value predicate and a single value" do
+      rule = valid_json_rule.tap{|r| r['predicate']='in'; r['value']=Project.first.id}
+      expect(build(:smart_group, rules: [rule])).to be_invalid
+    end
+
+    it "accepts a rule with a single-value predicate and a single value" do
+      rule = valid_json_rule.tap{|r| r['predicate']='not_in'; r['value']=Project.first.id}
+      expect(SmartGroups::Rules::ParticipatedInProject.from_json(rule)).to be_valid
+      expect(build(:smart_group, rules: [rule])).to be_valid
+    end
+
+    it "rejects a rule with a single-value predicate and an array of values" do
+      rule = valid_json_rule.tap{|r| r['predicate']='not_in'}
+      expect(build(:smart_group, rules: [valid_json_rule])).to be_invalid
     end
   end
 
@@ -41,7 +54,7 @@ describe SmartGroups::Rules::ParticipatedInProject do
     end
 
     it "correctly filters on 'in' predicate" do
-      rule = SmartGroups::Rules::ParticipatedInProject.new('in', @project1.id)
+      rule = SmartGroups::Rules::ParticipatedInProject.new('in', [@project1.id])
       expect(rule.filter(User)).to match_array [@user1, @user2, @user3]
     end
 
@@ -51,8 +64,8 @@ describe SmartGroups::Rules::ParticipatedInProject do
     end
 
     it "correctly filters on 'posted_in' predicate" do
-      rule = SmartGroups::Rules::ParticipatedInProject.new('posted_in', @project1.id)
-      expect(rule.filter(User)).to match_array [@user1]
+      rule = SmartGroups::Rules::ParticipatedInProject.new('posted_in', [@project1.id, @project2.id])
+      expect(rule.filter(User)).to match_array [@user1, @user3]
     end
 
     it "correctly filters on 'not_posted_in' predicate" do
@@ -61,7 +74,7 @@ describe SmartGroups::Rules::ParticipatedInProject do
     end
 
     it "correctly filters on 'commented_in' predicate" do
-      rule = SmartGroups::Rules::ParticipatedInProject.new('commented_in', @project1.id)
+      rule = SmartGroups::Rules::ParticipatedInProject.new('commented_in', [@project1.id])
       expect(rule.filter(User)).to match_array [@user3]
     end
 
@@ -71,7 +84,7 @@ describe SmartGroups::Rules::ParticipatedInProject do
     end
 
     it "correctly filters on 'voted_idea_in' predicate" do
-      rule = SmartGroups::Rules::ParticipatedInProject.new('voted_idea_in', @project1.id)
+      rule = SmartGroups::Rules::ParticipatedInProject.new('voted_idea_in', [@project1.id])
       expect(rule.filter(User)).to match_array [@user2]
     end
 
@@ -81,7 +94,7 @@ describe SmartGroups::Rules::ParticipatedInProject do
     end
 
     it "correctly filters on 'voted_comment_in' predicate" do
-      rule = SmartGroups::Rules::ParticipatedInProject.new('voted_comment_in', @project1.id)
+      rule = SmartGroups::Rules::ParticipatedInProject.new('voted_comment_in', [@project1.id])
       expect(rule.filter(User)).to match_array []
     end
 
@@ -91,7 +104,7 @@ describe SmartGroups::Rules::ParticipatedInProject do
     end
 
     it "correctly filters on 'budgeted_in' predicate" do
-      rule = SmartGroups::Rules::ParticipatedInProject.new('budgeted_in', @project1.id)
+      rule = SmartGroups::Rules::ParticipatedInProject.new('budgeted_in', [@project1.id])
       expect(rule.filter(User)).to match_array []
     end
 
@@ -101,7 +114,7 @@ describe SmartGroups::Rules::ParticipatedInProject do
     end
 
     it "correctly filters on 'volunteered_in' predicate" do
-      rule = SmartGroups::Rules::ParticipatedInProject.new('volunteered_in', @project1.id)
+      rule = SmartGroups::Rules::ParticipatedInProject.new('volunteered_in', [@project1.id])
       expect(rule.filter(User)).to match_array []
     end
 
@@ -112,137 +125,139 @@ describe SmartGroups::Rules::ParticipatedInProject do
   end
 
   describe "description_multiloc" do
-    let(:project) { create(:project, title_multiloc: {
-      'en'    => 'Choose a name for the new trash can',
-      'fr-FR' => 'Choisissez un nom pour la nouvelle poubelle',
-      'nl-NL' => 'Kies een naam voor de nieuwe vuilnisbak'
+    let(:project1) { create(:project, title_multiloc: {
+      'en'    => 'beer',
+      'fr-FR' => 'bière',
+      'nl-NL' => 'bier'
+    }) }
+    let(:project2) { create(:project, title_multiloc: {
+      'en'    => 'delayed',
+      'fr-FR' => 'retardé',
+      'nl-NL' => 'uitgesteld'
     }) }
 
     let(:participated_in_project_in_rule) {SmartGroups::Rules::ParticipatedInProject.from_json({
       'ruleType'      => 'participated_in_project',
       'predicate'     => 'in',
-      'value'         => project.id
+      'value'         => [project1.id, project2.id]
     })}
     let(:participated_not_in_project_in_rule) {SmartGroups::Rules::ParticipatedInProject.from_json({
       'ruleType'      => 'participated_in_project',
       'predicate'     => 'not_in',
-      'value'         => project.id
+      'value'         => project1.id
     })}
     let(:participated_posted_in_project_in_rule) {SmartGroups::Rules::ParticipatedInProject.from_json({
       'ruleType'      => 'participated_in_project',
       'predicate'     => 'posted_in',
-      'value'         => project.id
+      'value'         => [project1.id, project2.id]
     })}
     let(:participated_not_posted_in_project_in_rule) {SmartGroups::Rules::ParticipatedInProject.from_json({
       'ruleType'      => 'participated_in_project',
       'predicate'     => 'not_posted_in',
-      'value'         => project.id
+      'value'         => project1.id
     })}
     let(:participated_commented_in_project_in_rule) {SmartGroups::Rules::ParticipatedInProject.from_json({
       'ruleType'      => 'participated_in_project',
       'predicate'     => 'commented_in',
-      'value'         => project.id
+      'value'         => [project1.id, project2.id]
     })}
     let(:participated_not_commented_in_project_in_rule) {SmartGroups::Rules::ParticipatedInProject.from_json({
       'ruleType'      => 'participated_in_project',
       'predicate'     => 'not_commented_in',
-      'value'         => project.id
+      'value'         => project1.id
     })}
     let(:participated_voted_idea_in_project_in_rule) {SmartGroups::Rules::ParticipatedInProject.from_json({
       'ruleType'      => 'participated_in_project',
       'predicate'     => 'voted_idea_in',
-      'value'         => project.id
+      'value'         => [project1.id, project2.id]
     })}
     let(:participated_not_voted_idea_in_project_in_rule) {SmartGroups::Rules::ParticipatedInProject.from_json({
       'ruleType'      => 'participated_in_project',
       'predicate'     => 'not_voted_idea_in',
-      'value'         => project.id
+      'value'         => project1.id
     })}
     let(:participated_voted_comment_in_project_in_rule) {SmartGroups::Rules::ParticipatedInProject.from_json({
       'ruleType'      => 'participated_in_project',
       'predicate'     => 'voted_comment_in',
-      'value'         => project.id
+      'value'         => [project1.id, project2.id]
     })}
     let(:participated_not_voted_comment_in_project_in_rule) {SmartGroups::Rules::ParticipatedInProject.from_json({
       'ruleType'      => 'participated_in_project',
       'predicate'     => 'not_voted_comment_in',
-      'value'         => project.id
+      'value'         => project1.id
     })}
     let(:participated_budgeted_in_project_in_rule) {SmartGroups::Rules::ParticipatedInProject.from_json({
       'ruleType'      => 'participated_in_project',
       'predicate'     => 'budgeted_in',
-      'value'         => project.id
+      'value'         => [project1.id, project2.id]
     })}
     let(:participated_not_budgeted_in_project_in_rule) {SmartGroups::Rules::ParticipatedInProject.from_json({
       'ruleType'      => 'participated_in_project',
       'predicate'     => 'not_budgeted_in',
-      'value'         => project.id
+      'value'         => project1.id
     })}
 
 
     it "successfully translates different combinations of rules" do
-      # Stubbing the translations so the specs don't depend on those.
-      I18n.load_path += Dir[Rails.root.join('spec', 'fixtures', 'locales', '*.yml')]
-
       expect(participated_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Participation in an idea in the project Choose a name for the new trash can',
-        'fr-FR' => 'Participation dans une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
-        'nl-NL' => 'Participatie in een idee in het project Kies een naam voor de nieuwe vuilnisbak'
+        'en'    => 'Participation in an idea in one of the following projects beer, delayed',
+        'fr-FR' => "Participation à une idée dans l'un des projets bière, retardé",
+        'nl-NL' => 'Participatie in een idee in een van de volgende projecten bier, uitgesteld'
       })
       expect(participated_not_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'No participation in an idea in the project Choose a name for the new trash can',
-        'fr-FR' => 'Pas de participation dans une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
-        'nl-NL' => 'Geen participatie in een idee in het project Kies een naam voor de nieuwe vuilnisbak'
+        'en'    => 'No participation in an idea in the project beer',
+        'fr-FR' => 'Pas de participation dans une idée dans le projet bière',
+        'nl-NL' => 'Geen participatie in een idee in het project bier'
       })
       expect(participated_posted_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Posted an idea in the project Choose a name for the new trash can',
-        'fr-FR' => 'Posté une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
-        'nl-NL' => 'Plaatste een idee in het project Kies een naam voor de nieuwe vuilnisbak'
+        'en'    => 'Posted an idea in one of the following projects beer, delayed',
+        'fr-FR' => 'Posté une idée dans l\'un des projets bière, retardé',
+        'nl-NL' => 'Plaatste een idee in een van de volgende projecten bier, uitgesteld'
       })
       expect(participated_not_posted_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Did not post an idea in the project Choose a name for the new trash can',
-        'fr-FR' => 'N\'as pas posté une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
-        'nl-NL' => 'Plaatste geen idee in het project Kies een naam voor de nieuwe vuilnisbak'
+        'en'    => 'Did not post an idea in the project beer',
+        'fr-FR' => 'N\'as pas posté une idée dans le projet bière',
+        'nl-NL' => 'Plaatste geen idee in het project bier'
       })
       expect(participated_commented_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Commented on an idea in the project Choose a name for the new trash can',
-        'fr-FR' => 'Commenté sur une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
-        'nl-NL' => 'Reageerde op een idee in het project Kies een naam voor de nieuwe vuilnisbak'
+        'en'    => 'Commented on an idea in one of the following projects beer, delayed',
+        'fr-FR' => 'Commenté sur une idée dans l\'un des projets bière, retardé',
+        'nl-NL' => 'Reageerde op een idee in een van de volgende projecten bier, uitgesteld'
       })
       expect(participated_not_commented_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Did not comment on an idea in the project Choose a name for the new trash can',
-        'fr-FR' => 'N\'as pas commenté sur une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
-        'nl-NL' => 'Reageerde niet op een idee in het project Kies een naam voor de nieuwe vuilnisbak'
+        'en'    => 'Did not comment on an idea in the project beer',
+        'fr-FR' => 'N\'as pas commenté sur une idée dans le projet bière',
+        'nl-NL' => 'Reageerde niet op een idee in het project bier'
       })
       expect(participated_voted_idea_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Voted on an idea in the project Choose a name for the new trash can',
-        'fr-FR' => 'Voté pour une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
-        'nl-NL' => 'Stemde op een idee in het project Kies een naam voor de nieuwe vuilnisbak'
+        'en'    => 'Voted on an idea in one of the following projects beer, delayed',
+        'fr-FR' => 'Voté pour une idée dans l\'un des projets bière, retardé',
+        'nl-NL' => 'Stemde op een idee in een van de volgende projecten bier, uitgesteld'
       })
       expect(participated_not_voted_idea_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Did not vote on an idea in the project Choose a name for the new trash can',
-        'fr-FR' => 'N\'as pas voté pour une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
-        'nl-NL' => 'Stemde niet op een idee in het project Kies een naam voor de nieuwe vuilnisbak'
+        'en'    => 'Did not vote on an idea in the project beer',
+        'fr-FR' => 'N\'as pas voté pour une idée dans le projet bière',
+        'nl-NL' => 'Stemde niet op een idee in het project bier'
       })
       expect(participated_voted_comment_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Voted on a comment on an idea in the project Choose a name for the new trash can',
-        'fr-FR' => 'Voté pour un commentaire sur une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
-        'nl-NL' => 'Stemde op een reactie op een idee in het project Kies een naam voor de nieuwe vuilnisbak'
+        'en'    => 'Voted on a comment on an idea in one of the following projects beer, delayed',
+        'fr-FR' => 'Voté pour un commentaire sur une idée dans l\'un des projets bière, retardé',
+        'nl-NL' => 'Stemde op een reactie op een idee in een van de volgende projecten bier, uitgesteld'
       })
       expect(participated_not_voted_comment_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Did not vote on a comment on an idea in the project Choose a name for the new trash can',
-        'fr-FR' => 'N\'as pas voté pour un commentaire sur une idée dans le projet Choisissez un nom pour la nouvelle poubelle',
-        'nl-NL' => 'Stemde niet op een reactie op een idee in het project Kies een naam voor de nieuwe vuilnisbak'
+        'en'    => 'Did not vote on a comment on an idea in the project beer',
+        'fr-FR' => 'N\'as pas voté pour un commentaire sur une idée dans le projet bière',
+        'nl-NL' => 'Stemde niet op een reactie op een idee in het project bier'
       })
       expect(participated_budgeted_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Assigned a budget in the project Choose a name for the new trash can',
-        'fr-FR' => 'Dépensé un budget dans le projet Choisissez un nom pour la nouvelle poubelle',
-        'nl-NL' => 'Wees een budget toe in het project Kies een naam voor de nieuwe vuilnisbak'
+        'en'    => 'Assigned a budget in one of the following projects beer, delayed',
+        'fr-FR' => 'Dépensé un budget dans l\'un des projets bière, retardé',
+        'nl-NL' => 'Wees een budget toe in een van de volgende projecten bier, uitgesteld'
       })
       expect(participated_not_budgeted_in_project_in_rule.description_multiloc).to eq ({
-        'en'    => 'Didn\'t assign a budget in the project Choose a name for the new trash can',
-        'fr-FR' => 'N\'as pas dépensé un budget dans le projet Choisissez un nom pour la nouvelle poubelle',
-        'nl-NL' => 'Wees geen budget toe in het project Kies een naam voor de nieuwe vuilnisbak'
+        'en'    => 'Didn\'t assign a budget in the project beer',
+        'fr-FR' => 'N\'as pas dépensé un budget dans le projet bière',
+        'nl-NL' => 'Wees geen budget toe in het project bier'
       })
     end
   end
