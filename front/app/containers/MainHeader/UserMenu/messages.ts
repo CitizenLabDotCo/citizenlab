9c30f98849f4d19c037dd3b030d9fddd62fd2a9d
brentguf <brent.guffens@gmail.com>
2021-08-24 13:49:25 +0200

    UserMenu messages

---
+++ front/app/containers/MainHeader/UserMenu/messages.ts
@@ -0,0 +1,25 @@
+/*
+ * Navbar Messages
+ *
+ * This contains all the text for the Navbar component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  myProfile: {
+    id: 'app.containers.app.navbar.myProfile',
+    defaultMessage: 'My profile',
+  },
+  editProfile: {
+    id: 'app.containers.app.navbar.editProfile',
+    defaultMessage: 'Settings',
+  },
+  signOut: {
+    id: 'app.containers.app.navbar.signOut',
+    defaultMessage: 'Sign out',
+  },
+  admin: {
+    id: 'app.containers.app.navbar.admin',
+    defaultMessage: 'Admin',
+  },
+});
