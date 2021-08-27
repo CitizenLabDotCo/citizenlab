160092cda49ad297a1a8c64a9a9353b6de03b663
Sébastien Hoorens <sebastien@citizenlab.co>
2021-08-27 17:01:06 +0200

    updated aws-fog

--- back/spec/models/idea_file_spec.rb
+++ back/spec/models/idea_file_spec.rb
@@ -15,10 +15,11 @@ RSpec.describe IdeaFile, type: :model do
 
       file = create(:idea_file)
 
-      file.remote_file_url = 'https://seboslovakia.citizenlab.co/uploads/4365d891-7fad-4416-b8e3-fc9109a3027d/project_file/file/17f56e9e-b32c-4c30-bd76-688ff42f413e/Visio%CC%81n_2028_de_Metro_Plan_Estrate%CC%81gico_A.pdf'
+      # file.remote_file_url = 'https://seboslovakia.citizenlab.co/uploads/4365d891-7fad-4416-b8e3-fc9109a3027d/project_file/file/17f56e9e-b32c-4c30-bd76-688ff42f413e/Visio%CC%81n_2028_de_Metro_Plan_Estrate%CC%81gico_A.pdf'
       # file.remote_file_url = 'https://kortrijkspreekt.be/uploads/94774368-0e9d-406c-a765-457d58242666/project_file/file/1401b697-5bc6-4ddd-940e-61ee1211d0f6/OT_Q2020-2215_Schaal_1_250.pdf'
       # file.file = Rails.root.join('file://spec/fixtures/Visio%CC%81n_2028_de_Metro_Plan_Estrate%CC%81gico_A.pdf').open
 
+      file.remote_file_url = 'https://seboslovakia.citizenlab.co/uploads/4365d891-7fad-4416-b8e3-fc9109a3027d/project_folders/file/file/ea40a7c7-194a-4e86-a067-b5efc0cd5655/Vision_2028_de_Metro_Plan_Estrategico_A.pdf'
       expect{file.save!}.not_to raise_error
     end
   end
