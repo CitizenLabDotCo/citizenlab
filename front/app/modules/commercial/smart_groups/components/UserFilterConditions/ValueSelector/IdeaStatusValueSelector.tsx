cc5f491bd3a49abcdddd7de2db64dc09de43ceb2
brentguf <brent.guffens@gmail.com>
2021-08-31 14:32:37 +0200

    IdeaStatusValueSelector: useLocalize

--- front/app/modules/commercial/smart_groups/components/UserFilterConditions/ValueSelector/IdeaStatusValueSelector.tsx
+++ front/app/modules/commercial/smart_groups/components/UserFilterConditions/ValueSelector/IdeaStatusValueSelector.tsx
@@ -1,8 +1,8 @@
 import React, { memo } from 'react';
 import { IOption } from 'typings';
 import useIdeaStatuses from 'hooks/useIdeaStatuses';
+import useLocalize from 'hooks/useLocalize';
 import { Select } from 'cl2-component-library';
-import localize, { InjectedLocalized } from 'utils/localize';
 import { isNilOrError } from 'utils/helperUtils';
 
 interface Props {
@@ -10,34 +10,33 @@ interface Props {
   onChange: (string) => void;
 }
 
-const IdeaStatusValueSelector = memo(
-  ({ value, onChange, localize }: Props & InjectedLocalized) => {
-    const ideaStatuses = useIdeaStatuses();
-    const generateOptions = (): IOption[] => {
-      if (!isNilOrError(ideaStatuses)) {
-        return ideaStatuses.map((ideaStatus) => {
-          return {
-            value: ideaStatus.id,
-            label: localize(ideaStatus.attributes.title_multiloc),
-          };
-        });
-      } else {
-        return [];
-      }
-    };
+const IdeaStatusValueSelector = memo(({ value, onChange }: Props) => {
+  const ideaStatuses = useIdeaStatuses();
+  const localize = useLocalize();
+  const generateOptions = (): IOption[] => {
+    if (!isNilOrError(ideaStatuses)) {
+      return ideaStatuses.map((ideaStatus) => {
+        return {
+          value: ideaStatus.id,
+          label: localize(ideaStatus.attributes.title_multiloc),
+        };
+      });
+    } else {
+      return [];
+    }
+  };
 
-    const handleOnChange = (option: IOption) => {
-      onChange(option.value);
-    };
+  const handleOnChange = (option: IOption) => {
+    onChange(option.value);
+  };
 
-    return (
-      <Select
-        value={value}
-        options={generateOptions()}
-        onChange={handleOnChange}
-      />
-    );
-  }
-);
+  return (
+    <Select
+      value={value}
+      options={generateOptions()}
+      onChange={handleOnChange}
+    />
+  );
+});
 
-export default localize(IdeaStatusValueSelector);
+export default IdeaStatusValueSelector;
