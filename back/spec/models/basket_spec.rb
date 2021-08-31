ab678e1368a945554c99859bfaa7f362481bc030
Simon Tharby <simontharby@gmail.com>
2021-08-31 15:28:01 +0100

    Use immediate evaluation of basket declaration, not lazy
    This gives a cleaner error message if the basket fails validation,
    which is what this test checks for.

--- back/spec/models/basket_spec.rb
+++ back/spec/models/basket_spec.rb
@@ -70,13 +70,13 @@ RSpec.describe Basket, type: :model do
   end
 
   context "when the basket's project is updated to non-budgeting participation method" do
+    let!(:basket) { create(:basket, ideas: [idea], participation_context: project, submitted_at: Time.now) }
     let(:project) { create(:continuous_budgeting_project, min_budget: 200) }
     let(:idea) { create(:idea, budget: 100, project: project) }
 
     # Check the basket remains valid and thus won't fail data consistency checks, as would be the case,
     # for example, if we enforce validation that the participation_context is budgeting.
     it "the basket remains valid" do
-      basket = create(:basket, ideas: [idea], participation_context: project, submitted_at: Time.now)
       project.update!(participation_method: "ideation")
       basket.reload
       expect(basket).to be_valid
