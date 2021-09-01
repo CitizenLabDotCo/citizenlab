05d268a30714b5bb859b8f0814f0193d50450856
Simon Tharby <simontharby@gmail.com>
2021-09-01 14:15:43 +0100

    ProjectValueSelector: export props (build error)

--- front/app/modules/commercial/smart_groups/components/UserFilterConditions/ValueSelector/ProjectValueSelector.tsx
+++ front/app/modules/commercial/smart_groups/components/UserFilterConditions/ValueSelector/ProjectValueSelector.tsx
@@ -6,7 +6,7 @@ import { Select } from 'cl2-component-library';
 import useLocalize from 'hooks/useLocalize';
 import { isNilOrError } from 'utils/helperUtils';
 
-interface Props {
+export interface Props {
   rule: TRule;
   value: string;
   onChange: (value: string) => void;
