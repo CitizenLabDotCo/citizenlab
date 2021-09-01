878cc4f3619e607bb012564bba6d09ac59e578dc
Simon Tharby <simontharby@gmail.com>
2021-09-01 09:34:05 +0100

    ProjectValueSelector: refactor to functional component

--- front/app/modules/commercial/smart_groups/components/UserFilterConditions/ValueSelector/ProjectValueSelector.tsx
+++ front/app/modules/commercial/smart_groups/components/UserFilterConditions/ValueSelector/ProjectValueSelector.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { TRule } from '../rules';
 import { IOption } from 'typings';
 import GetProjects, { GetProjectsChildProps } from 'resources/GetProjects';
@@ -13,38 +13,34 @@ interface Props {
   projects: GetProjectsChildProps;
 }
 
-class ProjectValueSelector extends React.PureComponent<
-  Props & InjectedLocalized
-> {
-  generateOptions = (): IOption[] => {
-    const { projects, localize } = this.props;
+const ProjectValueSelector = memo(
+  ({ value, onChange, projects, localize }: Props & InjectedLocalized) => {
+    const generateOptions = (): IOption[] => {
+      if (!isNilOrError(projects) && projects.projectsList) {
+        return projects.projectsList.map((project) => {
+          return {
+            value: project.id,
+            label: localize(project.attributes.title_multiloc),
+          };
+        });
+      } else {
+        return [];
+      }
+    };
 
-    if (!isNilOrError(projects) && projects.projectsList) {
-      return projects.projectsList.map((project) => ({
-        value: project.id,
-        label: localize(project.attributes.title_multiloc),
-      }));
-    } else {
-      return [];
-    }
-  };
-
-  handleOnChange = (option: IOption) => {
-    this.props.onChange(option.value);
-  };
-
-  render() {
-    const { value } = this.props;
+    const handleOnChange = (option: IOption) => {
+      onChange(option.value);
+    };
 
     return (
       <Select
         value={value}
-        options={this.generateOptions()}
-        onChange={this.handleOnChange}
+        options={generateOptions()}
+        onChange={handleOnChange}
       />
     );
   }
-}
+);
 
 const ProjectValueSelectorWithHOC = localize(ProjectValueSelector);
 
