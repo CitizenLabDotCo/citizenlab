c6cebc5be0ffbc37b8a016afe6fafdc89ae42c33
Simon Tharby <simontharby@gmail.com>
2021-09-01 14:03:23 +0100

    ProjectValueSelector: useLocalize

--- front/app/modules/commercial/smart_groups/components/UserFilterConditions/ValueSelector/ProjectValueSelector.tsx
+++ front/app/modules/commercial/smart_groups/components/UserFilterConditions/ValueSelector/ProjectValueSelector.tsx
@@ -3,7 +3,7 @@ import { TRule } from '../rules';
 import { IOption } from 'typings';
 import GetProjects, { GetProjectsChildProps } from 'resources/GetProjects';
 import { Select } from 'cl2-component-library';
-import localize, { InjectedLocalized } from 'utils/localize';
+import useLocalize from 'hooks/useLocalize';
 import { isNilOrError } from 'utils/helperUtils';
 
 interface Props {
@@ -13,41 +13,36 @@ interface Props {
   projects: GetProjectsChildProps;
 }
 
-const ProjectValueSelector = memo(
-  ({ value, onChange, projects, localize }: Props & InjectedLocalized) => {
-    const generateOptions = (): IOption[] => {
-      if (!isNilOrError(projects) && projects.projectsList) {
-        return projects.projectsList.map((project) => {
-          return {
-            value: project.id,
-            label: localize(project.attributes.title_multiloc),
-          };
-        });
-      } else {
-        return [];
-      }
-    };
+const ProjectValueSelector = memo(({ value, onChange, projects }: Props) => {
+  const localize = useLocalize();
+  const generateOptions = (): IOption[] => {
+    if (!isNilOrError(projects) && projects.projectsList) {
+      return projects.projectsList.map((project) => {
+        return {
+          value: project.id,
+          label: localize(project.attributes.title_multiloc),
+        };
+      });
+    } else {
+      return [];
+    }
+  };
 
-    const handleOnChange = (option: IOption) => {
-      onChange(option.value);
-    };
+  const handleOnChange = (option: IOption) => {
+    onChange(option.value);
+  };
 
-    return (
-      <Select
-        value={value}
-        options={generateOptions()}
-        onChange={handleOnChange}
-      />
-    );
-  }
-);
-
-const ProjectValueSelectorWithHOC = localize(ProjectValueSelector);
+  return (
+    <Select
+      value={value}
+      options={generateOptions()}
+      onChange={handleOnChange}
+    />
+  );
+});
 
 export default (inputProps: Props) => (
   <GetProjects publicationStatuses={['draft', 'published', 'archived']}>
-    {(projects) => (
-      <ProjectValueSelectorWithHOC {...inputProps} projects={projects} />
-    )}
+    {(projects) => <ProjectValueSelector {...inputProps} projects={projects} />}
   </GetProjects>
 );
