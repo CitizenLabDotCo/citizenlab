e830acd95559d09c8cc8a40cac66ec043bdabd38
brentguf <brent.guffens@gmail.com>
2021-09-03 11:42:21 +0200

    Custom button for 'More' item to deal with padding

--- front/app/containers/MobileNavbar/ShowFullMenuButton.tsx
+++ front/app/containers/MobileNavbar/ShowFullMenuButton.tsx
@@ -1,33 +1,47 @@
 import React from 'react';
 import { NavigationItem, NavigationLabel } from './';
-import Button from 'components/UI/Button';
+import { Icon } from 'cl2-component-library';
 import { FormattedMessage } from 'utils/cl-intl';
 import messages from './messages';
 import { colors } from 'utils/styleUtils';
-import { useTheme } from 'styled-components';
+import styled from 'styled-components';
+import { darken } from 'polished';
 
 interface Props {
   onClick: () => void;
   isFullMenuOpened: boolean;
 }
 
+const StyledButton = styled.button<{ isFullMenuOpened: boolean }>`
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${({ isFullMenuOpened, theme }) =>
+    isFullMenuOpened ? theme.colorMain : colors.label};
+
+  &:hover {
+    color: ${({ isFullMenuOpened, theme }) =>
+      darken(0.2, isFullMenuOpened ? theme.colorMain : colors.label)};
+  }
+`;
+const StyledIcon = styled(Icon)<{ isFullMenuOpened: boolean }>`
+  width: 17px;
+  height: 17px;
+  margin-right: 3px;
+  fill: ${({ isFullMenuOpened, theme }) =>
+    isFullMenuOpened ? theme.colorMain : colors.label};
+`;
+
 const ShowFullMenuButton = ({ onClick, isFullMenuOpened }: Props) => {
-  const theme: any = useTheme();
   return (
     <NavigationItem>
-      <Button
-        height="100%"
-        padding="0"
-        icon="more-options"
-        buttonStyle="text"
-        onClick={onClick}
-        textColor={isFullMenuOpened ? theme.colorMain : colors.label}
-        iconColor={isFullMenuOpened ? theme.colorMain : colors.label}
-      >
+      <StyledButton onClick={onClick} isFullMenuOpened={isFullMenuOpened}>
+        <StyledIcon name="more-options" isFullMenuOpened={isFullMenuOpened} />
         <NavigationLabel>
           <FormattedMessage {...messages.showMore} />
         </NavigationLabel>
-      </Button>
+      </StyledButton>
     </NavigationItem>
   );
 };
