8c32e8697b46a817414af7265586e1b29cbc8e00
Adrien Dessy <adrien@citizenlab.co>
2021-09-06 18:13:51 +0200

    Rubocop

--- back/spec/factories/app_configurations.rb
+++ back/spec/factories/app_configurations.rb
@@ -1,67 +1,69 @@
+# frozen_string_literal: true
+
 require 'faker'
 
 FactoryBot.define do
   factory :app_configuration do
-    host { "localhost" }
-    settings {
+    host { 'localhost' }
+    settings do
       {
-        "core" => {
-          "enabled" => true,
-          "allowed" => true,
-          "organization_type" => "medium_city",
-          "organization_name" => {
-            "en" => Faker::Address.city,
-            "nl-BE" => Faker::Address.city,
-            "fr-FR" => Faker::Address.city
+        'core' => {
+          'enabled' => true,
+          'allowed' => true,
+          'organization_type' => 'medium_city',
+          'organization_name' => {
+            'en' => Faker::Address.city,
+            'nl-BE' => Faker::Address.city,
+            'fr-FR' => Faker::Address.city
           },
-          "timezone" => "Brussels",
-          "currency" => "EUR",
-          "locales" => ["en","nl-BE","fr-FR"],
-          "color_main" => "#335533",
-          "color_secondary" => Faker::Color.hex_color,
-          "color_text" => Faker::Color.hex_color,
+          'timezone' => 'Brussels',
+          'currency' => 'EUR',
+          'locales' => %w[en nl-BE fr-FR],
+          'color_main' => '#335533',
+          'color_secondary' => Faker::Color.hex_color,
+          'color_text' => Faker::Color.hex_color
         },
-        "initiatives" => {
-          "enabled" => true,
-          "allowed" => true,
-          "voting_threshold" => 300,
-          "days_limit" => 90
+        'initiatives' => {
+          'enabled' => true,
+          'allowed' => true,
+          'voting_threshold' => 300,
+          'days_limit' => 90
         }
       }
-    }
+    end
 
     initialize_with { AppConfiguration.send(:new, **attributes) }
   end
 
-  factory :test_app_configuration, class: AppConfiguration do
-    host { "example.org" }
-    settings {
+  factory :test_app_configuration, class: 'AppConfiguration' do
+    host { 'example.org' }
+    settings do
       {
-          "core" => {
-              "allowed" => true,
-              "enabled" => true,
-              "organization_type" => "medium_city",
-              "organization_name" => {
-                  "en" => "Liege",
-                  "nl-NL" => "Luik",
-                  "fr-FR" => "Liege"
-              },
-              "lifecycle_stage": "active",
-              "locales" => ["en","fr-FR","nl-NL"],
-              "timezone" => "Brussels",
-              "currency" => "EUR",
-              "color_main" => "#335533",
-              "color_secondary" => Faker::Color.hex_color,
-              "color_text" => Faker::Color.hex_color,
+        'core' => {
+          'allowed' => true,
+          'enabled' => true,
+          'organization_type' => 'medium_city',
+          'organization_name' => {
+            'en' => 'Liege',
+            'nl-NL' => 'Luik',
+            'fr-FR' => 'Liege'
           },
-          "initiatives" => {
-              "enabled" => true,
-              "allowed" => true,
-              "voting_threshold" => 300,
-              "days_limit" => 90
-          }
+          "lifecycle_stage": 'active',
+          'locales' => %w[en fr-FR nl-NL],
+          'timezone' => 'Brussels',
+          'currency' => 'EUR',
+          'color_main' => '#335533',
+          'color_secondary' => Faker::Color.hex_color,
+          'color_text' => Faker::Color.hex_color
+        },
+        'initiatives' => {
+          'enabled' => true,
+          'allowed' => true,
+          'voting_threshold' => 300,
+          'days_limit' => 90
+        }
       }
-    }
+    end
 
     initialize_with { AppConfiguration.send(:new, **attributes) }
   end
