082eb0a233c2dca7ac50387daac4e63e2658c91f
Adrien Dessy <adrien@citizenlab.co>
2021-09-13 12:08:35 +0200

    IN-428 Rework and fix xlsx_service_spec.rb (view_private_attributes is a keyword param)
    IN-428 #time 0w 0d 0h 34m Rework and fix xlsx_service_spec.rb (view_private_attributes is a keyword param)

--- back/engines/commercial/insights/spec/services/insights/xlsx_service_spec.rb
+++ back/engines/commercial/insights/spec/services/insights/xlsx_service_spec.rb
@@ -1,56 +1,56 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 require 'rubyXL'
 
-
-describe XlsxService do
+RSpec.describe XlsxService do
   let(:service) { Insights::XlsxService.new }
 
-  describe "generate_inputs_xlsx" do
-
+  # rubocop:disable RSpec/MultipleMemoizedHelpers
+  describe 'generate_inputs_xlsx' do
     let(:view) { create(:view) }
-    before do
-      create_list(:idea, 3, project: view.scope).tap do |ideas|
-        ideas.first.author.destroy! # should be able to handle ideas without author
-        ideas.first.reload
-      end
-    end
-    let(:ideas) { Idea.where(project: view.scope)}
-
+    let(:ideas) { view.scope.ideas }
     let(:categories) { create_list(:category, 2, view: view) }
-    let(:bad_category) { create(:category) }
-
-    before{ Insights::CategoryAssignmentsService.new.add_assignments_batch(ideas, categories | [bad_category]) }
-
-    let(:xlsx) { service.generate_inputs_xlsx(ideas, categories, true) }
+    let(:xlsx) { service.generate_inputs_xlsx(ideas, categories, view_private_attributes: true) }
     let(:workbook) { RubyXL::Parser.parse_buffer(xlsx) }
     let(:worksheet) { workbook.worksheets[0] }
 
-    it "exports a valid excel file" do
-      expect{ workbook }.to_not raise_error
+    before do
+      ideas = create_list(:idea, 3, project: view.scope)
+      ideas.first.author.destroy! # should be able to handle ideas without author
+
+      other_category = create(:category) # category from another view
+      Insights::CategoryAssignmentsService.new.add_assignments_batch(ideas, categories.push(other_category))
     end
 
-    it "contains a row for every idea" do
-      expect(worksheet.sheet_data.size).to eq (ideas.size + 1)
+    it 'exports a valid excel file' do
+      expect { workbook }.not_to raise_error
     end
 
-    it "contains a column for every category" do
-      category_one_col = worksheet.map {|col| col.cells[3].value}
-      header, *category_one = category_one_col
+    it 'contains a row for every idea' do
+      expect(worksheet.sheet_data.size).to eq(ideas.size + 1)
+    end
 
-      category_two_col = worksheet.map {|col| col.cells[4].value}
-      header, *category_two = category_two_col
+    it 'contains a column for every category' do
+      category_one_col = worksheet.map { |col| col.cells[3].value }
+      _header, *category_one = category_one_col
+
+      category_two_col = worksheet.map { |col| col.cells[4].value }
+      _header, *category_two = category_two_col
+
       aggregate_failures do
-        expect(category_one).to match ['approved', 'approved', 'approved']
-        expect(category_two).to match ['approved', 'approved', 'approved']
+        expect(category_one).to match %w[approved approved approved]
+        expect(category_two).to match %w[approved approved approved]
       end
     end
 
-    describe do
-      let(:xlsx) { service.generate_inputs_xlsx(ideas, categories, false) }
+    context 'when not allowed to see private attributes' do
+      let(:xlsx) { service.generate_inputs_xlsx(ideas, categories, view_private_attributes: false) }
 
-      it "hides private attributes" do
+      it 'hides private attributes' do
         expect(worksheet[0].cells.map(&:value)).not_to include 'author_email'
       end
     end
   end
+  # rubocop:enable RSpec/MultipleMemoizedHelpers
 end
