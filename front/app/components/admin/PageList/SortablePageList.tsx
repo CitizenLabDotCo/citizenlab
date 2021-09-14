7d57ac53165e46c52f66e598556d9afae846859f
luucvanderzee <luuc_v_d_zee@hotmail.com>
2021-09-14 17:38:38 +0200

    _

--- front/app/components/admin/PageList/SortablePageList.tsx
+++ front/app/components/admin/PageList/SortablePageList.tsx
@@ -2,40 +2,41 @@ import React from 'react';
 import { SortableList, SortableRow } from 'components/admin/ResourceList';
 import { ChildProps as Props } from '.'
 import PageRow from './PageRow'
-import { IPageData } from 'services/pages';
 
 export default ({
   pagesData,
   pagesPermissions
-}: Props) => (
-  <SortableList
-
-  >
-    {({ itemsList, handleDragRow, handleDropRow }) => {
-      return (
-        <>
-          {itemsList.map((item, i: number) => {
-            return (
-              <SortableRow
-                key={item.id}
-                id={item.id}
-                index={i}
-                moveRow={handleDragRow}
-                dropRow={handleDropRow}
-                lastItem={
-                  i === itemsList.length - 1
-                }
-              >
-                <PageRow
-                  pageData={item}
-                  pagePermissions={pagesPermissions[i]}
-                />
-              </SortableRow>
-            );
-          }
-          )}
-        </>
-      );
-    }}
-  </SortableList>
-)
+}: Props) => {
+  return (
+    <SortableList
+      items={pagesData}
+    >
+      {({ itemsList, handleDragRow, handleDropRow }) => {
+        return (
+          <>
+            {itemsList.map((item, i: number) => {
+              return (
+                <SortableRow
+                  key={item.id}
+                  id={item.id}
+                  index={i}
+                  moveRow={handleDragRow}
+                  dropRow={handleDropRow}
+                  lastItem={
+                    i === itemsList.length - 1
+                  }
+                >
+                  <PageRow
+                    pageData={item}
+                    pagePermissions={pagesPermissions[i]}
+                  />
+                </SortableRow>
+              );
+            }
+            )}
+          </>
+        );
+      }}
+    </SortableList>
+  )
+}
