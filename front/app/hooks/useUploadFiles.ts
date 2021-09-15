c96e6c6630dffb9bbf1b55dfd27b1f91e0fe4619
brentguf <brent.guffens@gmail.com>
2021-09-15 18:07:00 +0200

    useUploadFiles (WIP)

--- front/app/hooks/useUploadFiles.ts
+++ front/app/hooks/useUploadFiles.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { isNilOrError } from 'utils/helperUtils';
-import { combineLatest, of } from 'rxjs';
+import { combineLatest, of, Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { projectFilesStream } from 'services/projectFiles';
 import { phaseFilesStream } from 'services/phaseFiles';
@@ -31,24 +31,28 @@ function useUploadFiles({
       idea: ideaFilesStream,
       initiative: initiativeFilesStream,
     }[resourceType];
-    const observable = streamFn(resourceId).observable.pipe(
-      switchMap((files) => {
-        const filesData = files?.data;
-        if (filesData && filesData.length > 0) {
-          return combineLatest(
-            filesData.map((file) =>
-              convertUrlToUploadFileObservable(
-                file.attributes.file.url,
-                file.id,
-                file.attributes.name
-              )
-            )
-          );
-        }
+    let observable: Observable<(UploadFile | null)[] | null> = of(null);
 
-        return of(null);
-      })
-    );
+    if (resourceId) {
+      observable = streamFn(resourceId).observable.pipe(
+        switchMap((files) => {
+          const filesData = files?.data;
+          if (filesData && filesData.length > 0) {
+            return combineLatest(
+              filesData.map((file) =>
+                convertUrlToUploadFileObservable(
+                  file.attributes.file.url,
+                  file.id,
+                  file.attributes.name
+                )
+              )
+            );
+          }
+
+          return of(null);
+        })
+      );
+    }
 
     const subscription = observable.subscribe((files) => {
       setUploadFiles(
