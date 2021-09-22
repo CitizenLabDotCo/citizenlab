08ad0df8572e336370ee87571d9c3bf1b721b869
Adrien Dessy <adrien@citizenlab.co>
2021-09-22 13:51:31 +0200

    IN-475 Use singleton class notation to define class methods
    IN-475 #time 0w 0d 1h 2m Use singleton class notation to define class methods

--- back/engines/commercial/insights/app/services/insights/front_end_format_text_network.rb
+++ back/engines/commercial/insights/app/services/insights/front_end_format_text_network.rb
@@ -32,96 +32,99 @@ module Insights
       @nodes ||= self.class.nodes(@network, @keyword_size_range, @cluster_size_range)
     end
 
-    # @param [NLP::TextNetwork] network
-    # @return [Array<Hash>]
-    def self.nodes(
-      network, keyword_size_range = DEFAULT_KEYWORD_SIZE_RANGE, cluster_size_range = DEFAULT_CLUSTER_SIZE_RANGE
-    )
-      keyword_nodes(network, keyword_size_range) + cluster_nodes(network, cluster_size_range)
-    end
-
-    # @param [NLP::TextNetwork] network
-    # @param [Array(Numeric, Numeric)] val_range range of the +val+ attribute after rescaling
-    # @return [Array<Hash>]
-    def self.cluster_nodes(network, val_range = DEFAULT_CLUSTER_SIZE_RANGE)
-      nodes = network.communities.map do |community|
-        {
-          id: community.id,
-          name: community_name(community),
-          val: community.importance_score,
-          cluster_id: nil
-        }
-      end
-
-      rescale_node_vals(nodes, val_range)
-    end
-
-    # @param [NLP::TextNetwork::Community] community
-    # @param [Integer] n
-    # @return [String]
-    def self.community_name(community, n = 3)
-      community.children
-               .sort_by(&:importance_score).reverse
-               .take(n)
-               .map(&:name).join(', ')
-    end
-
-    # @param [NLP::TextNetwork] network
-    # @param [Array(Numeric, Numeric)] val_range range of the +val+ attribute after rescaling
-    # @return [Array<Hash>]
-    def self.keyword_nodes(network, val_range = DEFAULT_KEYWORD_SIZE_RANGE)
-      nodes = network.communities.flat_map do |community|
-        community.children.map do |node|
-          {
-            id: node.id,
-            name: node.name,
-            val: node.importance_score,
-            cluster_id: community.id
-          }
-        end
-      end
-
-      rescale_node_vals(nodes, val_range)
-    end
-
-    def self.rescale_node_vals(nodes, output_range)
-      input_range = nodes.pluck(:val).minmax
-      scaler = Insights::MinMaxScaler.new(input_range, output_range)
-      nodes.each { |node| node[:val] = scaler.transform(node[:val]) }
-    end
-
     def links
       @links ||= self.class.links(@network)
     end
 
-    # @param [NLP::TextNetwork] network
-    # @return [Array<{Symbol=>String}>]
-    def self.links(network)
-      cluster_membership_links(network) + inter_cluster_links(network)
-    end
+    class << self
 
-    # @param [NLP::TextNetwork] network
-    # @return [Array<{Symbol=>String}>]
-    def self.cluster_membership_links(network)
-      network.communities.flat_map do |community|
-        community.children.map { |node| { source: community.id, target: node.id } }
-      end
-    end
-
-    # @param [NLP::TextNetwork] network
-    # @return [Array<{Symbol=>String}>]
-    def self.inter_cluster_links(network)
-      links_index = network.links.group_by do |link|
-        [link.from_id, link.to_id].sort
+      # @param [NLP::TextNetwork] network
+      # @return [Array<Hash>]
+      def nodes(
+        network, keyword_size_range = DEFAULT_KEYWORD_SIZE_RANGE, cluster_size_range = DEFAULT_CLUSTER_SIZE_RANGE
+      )
+        keyword_nodes(network, keyword_size_range) + cluster_nodes(network, cluster_size_range)
       end
 
-      community_pairs = network.communities.combination(2).select do |c1, c2|
-        c1.children_ids.product(c2.children_ids).any? do |id_pair|
-          links_index[id_pair.sort]
+      # @param [NLP::TextNetwork] network
+      # @param [Array(Numeric, Numeric)] val_range range of the +val+ attribute after rescaling
+      # @return [Array<Hash>]
+      def cluster_nodes(network, val_range = DEFAULT_CLUSTER_SIZE_RANGE)
+        nodes = network.communities.map do |community|
+          {
+            id: community.id,
+            name: community_name(community),
+            val: community.importance_score,
+            cluster_id: nil
+          }
+        end
+
+        rescale_node_vals(nodes, val_range)
+      end
+
+      # @param [NLP::TextNetwork::Community] community
+      # @param [Integer] n
+      # @return [String]
+      def community_name(community, n = 3)
+        community.children
+                 .sort_by(&:importance_score).reverse
+                 .take(n)
+                 .map(&:name).join(', ')
+      end
+
+      # @param [NLP::TextNetwork] network
+      # @param [Array(Numeric, Numeric)] val_range range of the +val+ attribute after rescaling
+      # @return [Array<Hash>]
+      def keyword_nodes(network, val_range = DEFAULT_KEYWORD_SIZE_RANGE)
+        nodes = network.communities.flat_map do |community|
+          community.children.map do |node|
+            {
+              id: node.id,
+              name: node.name,
+              val: node.importance_score,
+              cluster_id: community.id
+            }
+          end
+        end
+
+        rescale_node_vals(nodes, val_range)
+      end
+
+      def rescale_node_vals(nodes, output_range)
+        input_range = nodes.pluck(:val).minmax
+        scaler = Insights::MinMaxScaler.new(input_range, output_range)
+        nodes.each { |node| node[:val] = scaler.transform(node[:val]) }
+      end
+
+      # @param [NLP::TextNetwork] network
+      # @return [Array<{Symbol=>String}>]
+      def links(network)
+        cluster_membership_links(network) + inter_cluster_links(network)
+      end
+
+      # @param [NLP::TextNetwork] network
+      # @return [Array<{Symbol=>String}>]
+      def cluster_membership_links(network)
+        network.communities.flat_map do |community|
+          community.children.map { |node| { source: community.id, target: node.id } }
         end
       end
 
-      community_pairs.map { |c1, c2| { source: c1.id, target: c2.id } }
+      # @param [NLP::TextNetwork] network
+      # @return [Array<{Symbol=>String}>]
+      def inter_cluster_links(network)
+        links_index = network.links.group_by do |link|
+          [link.from_id, link.to_id].sort
+        end
+
+        community_pairs = network.communities.combination(2).select do |c1, c2|
+          c1.children_ids.product(c2.children_ids).any? do |id_pair|
+            links_index[id_pair.sort]
+          end
+        end
+
+        community_pairs.map { |c1, c2| { source: c1.id, target: c2.id } }
+      end
     end
   end
 end
