c21465ef7c4fc1a80f997e43253d898c05e83378
luucvanderzee <luuc_v_d_zee@hotmail.com>
2021-09-23 17:48:21 +0200

    Refactor: GetPages -> usePages (WIP)

---
+++ front/app/hooks/usePages.ts
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import { isEqual } from 'lodash-es';
+import { Subscription, BehaviorSubject, of, combineLatest } from 'rxjs';
+import { map, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { IPageData, listPages, pageByIdStream } from 'services/pages';
+
+interface IParams {
+  ids?: string[];
+}
+
+interface State {
+  pages: IPageData[] | undefined | null | Error;
+}
+
+export type GetPagesChildProps = IPageData[] | undefined | null | Error;
+
+export default function usePages(params?: IParams) {
+  const [pages, setPages] = useState<IPageData[] | undefined | null | Error>(
+    undefined
+  );
+}
+
+// export default class GetPages extends React.Component<Props, State> {
+//   private inputProps$: BehaviorSubject<InputProps>;
+//   private subscriptions: Subscription[];
+
+//   constructor(props: Props) {
+//     super(props);
+//     this.state = {
+//       pages: undefined,
+//     };
+//   }
+
+//   componentDidMount() {
+//     const { ids } = this.props;
+
+//     this.inputProps$ = new BehaviorSubject({ ids });
+
+//     this.subscriptions = [
+//       this.inputProps$
+//         .pipe(
+//           distinctUntilChanged((prev, next) => isEqual(prev, next)),
+//           switchMap(({ ids }) => {
+//             if (ids) {
+//               if (ids.length > 0) {
+//                 return combineLatest(
+//                   ids.map((id) =>
+//                     pageByIdStream(id).observable.pipe(
+//                       map((topic) => topic.data)
+//                     )
+//                   )
+//                 );
+//               }
+
+//               return of(null);
+//             }
+
+//             return listPages().observable.pipe(map((pages) => pages.data));
+//           })
+//         )
+//         .subscribe((pages) => {
+//           this.setState({ pages });
+//         }),
+//     ];
+//   }
+
+//   componentDidUpdate() {
+//     this.inputProps$.next({ ids: this.props.ids });
+//   }
+
+//   componentWillUnmount() {
+//     this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+//   }
+
+//   render() {
+//     const { children } = this.props;
+//     const { pages } = this.state;
+//     return (children as children)(pages);
+//   }
+// }
