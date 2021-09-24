d583f18a783404dd2e5024bdddd85f2c0e1877bc
brentguf <brent.guffens@gmail.com>
2021-09-24 12:16:24 +0200

    Remove unused import

--- front/app/components/UI/FormikFileUploader/index.tsx
+++ front/app/components/UI/FormikFileUploader/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import FileUploader from 'components/UI/FileUploader';
 import { FieldProps } from 'formik';
 import { UploadFile } from 'typings';
