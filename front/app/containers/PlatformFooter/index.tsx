33e38bcf7ec1df96a9db3d2f1a44093c1b62aeb2
brentguf <brent.guffens@gmail.com>
2021-09-28 18:12:01 +0200

    PlatformFooter to functional components

--- front/app/containers/PlatformFooter/index.tsx
+++ front/app/containers/PlatformFooter/index.tsx
@@ -1,6 +1,5 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import { reportError } from 'utils/loggingUtils';
-import { adopt } from 'react-adopt';
 import { isNilOrError } from 'utils/helperUtils';
 import { isEmpty } from 'lodash-es';
 
@@ -28,15 +27,6 @@ import tracks from './tracks';
 import { removeUrlLocale } from 'services/locale';
 import { FOOTER_PAGES, TFooterPage } from 'services/pages';
 
-// resources
-import GetLocale, { GetLocaleChildProps } from 'resources/GetLocale';
-import GetWindowSize, {
-  GetWindowSizeChildProps,
-} from 'resources/GetWindowSize';
-import GetAppConfiguration, {
-  GetAppConfigurationChildProps,
-} from 'resources/GetAppConfiguration';
-
 // style
 import styled, { css } from 'styled-components';
 import { transparentize } from 'polished';
@@ -47,7 +37,12 @@ import {
   viewportWidths,
   isRtl,
 } from 'utils/styleUtils';
-import GetFeatureFlag from 'resources/GetFeatureFlag';
+
+// hooks
+import useAppConfiguration from 'hooks/useAppConfiguration';
+import useWindowSize from 'hooks/useWindowSize';
+import useLocale from 'hooks/useLocale';
+import useFeatureFlag from 'hooks/useFeatureFlag';
 
 const Container = styled.footer<{ insideModal?: boolean }>`
   display: flex;
@@ -345,26 +340,10 @@ const CitizenLabLogo = styled(Icon)`
   }
 `;
 
-interface InputProps {
+interface Props {
   showShortFeedback?: boolean;
   className?: string;
   insideModal?: boolean;
-}
-
-interface DataProps {
-  locale: GetLocaleChildProps;
-  windowSize: GetWindowSizeChildProps;
-  appConfiguration: GetAppConfigurationChildProps;
-  customizedA11yHrefEnabled: boolean;
-}
-
-interface Props extends DataProps, InputProps {}
-
-interface State {
-  shortFeedbackButtonClicked: boolean;
-  feedbackModalOpen: boolean;
-  feedbackSubmitting: boolean;
-  feedbackSubmitted: boolean;
 }
 
 type TMessagesMap = { [key in TFooterPage]: MessageDescriptor };
@@ -378,25 +357,27 @@ const MESSAGES_MAP: TMessagesMap = {
   'accessibility-statement': messages.accessibilityStatement,
 };
 
-class PlatformFooter extends PureComponent<Props, State> {
-  static defaultProps = {
-    showShortFeedback: true,
-  };
+const PlatformFooter = ({
+  showShortFeedback = true,
+  className,
+  insideModal,
+}: Props) => {
+  const appConfiguration = useAppConfiguration();
+  const windowSize = useWindowSize();
+  const locale = useLocale();
+  const customizedA11yHrefEnabled = useFeatureFlag(
+    'custom_accessibility_statement_link'
+  );
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      shortFeedbackButtonClicked: false,
-      feedbackModalOpen: false,
-      feedbackSubmitting: false,
-      feedbackSubmitted: false,
-    };
-  }
+  const [shortFeedbackButtonClicked, setShortFeedbackButtonClicked] = useState(
+    false
+  );
+  const [feedbackModalOpen, setFeedbackModalOpen] = useState(false);
+  const [feedbackSubmitting, setFeedbackSubmitting] = useState(false);
+  const [feedbackSubmitted, setFeedbackSubmitted] = useState(false);
 
-  handleFeedbackButtonClick = (answer: 'yes' | 'no') => () => {
-    this.setState({
-      shortFeedbackButtonClicked: true,
-    });
+  const handleFeedbackButtonClick = (answer: 'yes' | 'no') => () => {
+    setShortFeedbackButtonClicked(true);
 
     // tracking
     if (answer === 'yes') {
@@ -404,257 +385,207 @@ class PlatformFooter extends PureComponent<Props, State> {
       postProductFeedback({
         question: 'found_what_youre_looking_for?',
         page: removeUrlLocale(location.pathname),
-        locale: this.props.locale || undefined,
+        locale: !isNilOrError(locale) ? locale : undefined,
         answer: 'yes',
       }).catch((err) => {
         reportError(err);
       });
     } else if (answer === 'no') {
       trackEventByName(tracks.clickShortFeedbackNo);
-      this.openFeedbackModal();
+      setFeedbackModalOpen(true);
     }
   };
 
-  openFeedbackModal = () => {
-    this.setState({ feedbackModalOpen: true });
+  const closeFeedbackModal = () => {
+    setFeedbackModalOpen(false);
   };
 
-  closeFeedbackModal = () => {
-    this.setState({ feedbackModalOpen: false });
+  const closeFeedbackModalSuccess = () => {
+    setFeedbackModalOpen(false);
   };
 
-  closeFeedbackModalSuccess = () => {
-    this.setState({ feedbackModalOpen: false });
+  const handleFeedbackOnSubmit = (submitting: boolean) => {
+    setFeedbackSubmitting(submitting);
   };
 
-  handleFeedbackOnSubmit = (submitting: boolean) => {
-    this.setState({ feedbackSubmitting: submitting });
+  const handleFeedbackSubmitted = () => {
+    setFeedbackSubmitted(true);
   };
 
-  handleFeedbackSubmitted = () => {
-    this.setState({ feedbackSubmitted: true });
-  };
-
-  closeFeedbackModalCancel = () => {
-    this.setState({ feedbackModalOpen: false });
+  const closeFeedbackModalCancel = () => {
+    setFeedbackSubmitting(false);
 
     postProductFeedback({
       question: 'found_what_youre_looking_for?',
       page: removeUrlLocale(location.pathname),
-      locale: this.props.locale || undefined,
+      locale: !isNilOrError(locale) ? locale : undefined,
       answer: 'no',
     }).catch((err) => reportError(err));
   };
 
-  shortFeedbackFormOnSubmit = () => {
+  const shortFeedbackFormOnSubmit = () => {
     eventEmitter.emit('ShortFeedbackFormSubmitEvent');
   };
 
-  openConsentManager = () => {
+  const openConsentManager = () => {
     eventEmitter.emit('openConsentManager');
   };
 
-  getHasCustomizedA11yFooterLink = () => {
-    const { customizedA11yHrefEnabled, appConfiguration } = this.props;
-
+  const getHasCustomizedA11yFooterLink = () => {
     return (
       !isNilOrError(appConfiguration) &&
       customizedA11yHrefEnabled &&
       !isEmpty(
-        appConfiguration.attributes.settings.custom_accessibility_statement_link
-          .url
+        appConfiguration.data.attributes.settings
+          .custom_accessibility_statement_link.url
       )
     );
   };
 
-  getCustomizedA11yHref = () => {
-    const { appConfiguration } = this.props;
-
-    if (
-      isNilOrError(appConfiguration) ||
-      !this.getHasCustomizedA11yFooterLink()
-    ) {
+  const getCustomizedA11yHref = () => {
+    if (isNilOrError(appConfiguration) || !getHasCustomizedA11yFooterLink()) {
       return null;
     }
 
-    return appConfiguration.attributes.settings
+    return appConfiguration.data.attributes.settings
       .custom_accessibility_statement_link.url;
   };
 
-  render() {
-    const {
-      shortFeedbackButtonClicked,
-      feedbackModalOpen,
-      feedbackSubmitting,
-      feedbackSubmitted,
-    } = this.state;
-    const {
-      showShortFeedback,
-      className,
-      windowSize,
-      insideModal,
-    } = this.props;
-    const smallerThanSmallTablet = windowSize
-      ? windowSize <= viewportWidths.smallTablet
-      : false;
-    const hasCustomizedA11yFooterLink = this.getHasCustomizedA11yFooterLink();
-    const customizedA11yHref = this.getCustomizedA11yHref();
+  const smallerThanSmallTablet =
+    windowSize.windowWidth <= viewportWidths.smallTablet;
+  const hasCustomizedA11yFooterLink = getHasCustomizedA11yFooterLink();
+  const customizedA11yHref = getCustomizedA11yHref();
 
-    return (
-      <Container
-        insideModal={insideModal}
-        id="hook-footer"
-        className={className}
-      >
-        {showShortFeedback && (
-          <>
-            <ShortFeedbackContainer>
-              <ShortFeedback>
-                {shortFeedbackButtonClicked ? (
-                  feedbackModalOpen ? (
-                    <ThankYouNote>
-                      <FormattedMessage {...messages.moreInfo} />
-                    </ThankYouNote>
-                  ) : (
-                    <ThankYouNote>
-                      <FormattedMessage {...messages.thanksForFeedback} />
-                    </ThankYouNote>
-                  )
+  return (
+    <Container insideModal={insideModal} id="hook-footer" className={className}>
+      {showShortFeedback && (
+        <>
+          <ShortFeedbackContainer>
+            <ShortFeedback>
+              {shortFeedbackButtonClicked ? (
+                feedbackModalOpen ? (
+                  <ThankYouNote>
+                    <FormattedMessage {...messages.moreInfo} />
+                  </ThankYouNote>
                 ) : (
-                  <>
-                    <FeedbackQuestion>
-                      <FormattedMessage {...messages.feedbackQuestion} />
-                    </FeedbackQuestion>
-                    <FeedbackButtons>
-                      <FeedbackButton
-                        onClick={this.handleFeedbackButtonClick('yes')}
-                      >
-                        <FormattedMessage {...messages.yes} />
-                      </FeedbackButton>
-                      <FeedbackButton
-                        className="hasLeftMargin"
-                        onClick={this.handleFeedbackButtonClick('no')}
-                      >
-                        <FormattedMessage {...messages.no} />
-                      </FeedbackButton>
-                    </FeedbackButtons>
-                  </>
+                  <ThankYouNote>
+                    <FormattedMessage {...messages.thanksForFeedback} />
+                  </ThankYouNote>
+                )
+              ) : (
+                <>
+                  <FeedbackQuestion>
+                    <FormattedMessage {...messages.feedbackQuestion} />
+                  </FeedbackQuestion>
+                  <FeedbackButtons>
+                    <FeedbackButton onClick={handleFeedbackButtonClick('yes')}>
+                      <FormattedMessage {...messages.yes} />
+                    </FeedbackButton>
+                    <FeedbackButton
+                      className="hasLeftMargin"
+                      onClick={handleFeedbackButtonClick('no')}
+                    >
+                      <FormattedMessage {...messages.no} />
+                    </FeedbackButton>
+                  </FeedbackButtons>
+                </>
+              )}
+            </ShortFeedback>
+          </ShortFeedbackContainer>
+
+          <Modal
+            width={500}
+            opened={feedbackModalOpen}
+            close={closeFeedbackModalCancel}
+            className="e2e-feedback-modal"
+            closeOnClickOutside={false}
+            header={<FormattedMessage {...messages.feedbackModalTitle} />}
+            footer={
+              <ShortFeedbackFormModalFooter>
+                {!feedbackSubmitted ? (
+                  <Button
+                    onClick={shortFeedbackFormOnSubmit}
+                    processing={feedbackSubmitting}
+                  >
+                    <FormattedMessage {...messages.submit} />
+                  </Button>
+                ) : (
+                  <Button buttonStyle="secondary" onClick={closeFeedbackModal}>
+                    <FormattedMessage {...messages.close} />
+                  </Button>
                 )}
-              </ShortFeedback>
-            </ShortFeedbackContainer>
+              </ShortFeedbackFormModalFooter>
+            }
+          >
+            <ShortFeedbackForm
+              closeModal={closeFeedbackModalSuccess}
+              submitting={handleFeedbackOnSubmit}
+              successfullySubmitted={handleFeedbackSubmitted}
+            />
+          </Modal>
+        </>
+      )}
 
-            <Modal
-              width={500}
-              opened={feedbackModalOpen}
-              close={this.closeFeedbackModalCancel}
-              className="e2e-feedback-modal"
-              closeOnClickOutside={false}
-              header={<FormattedMessage {...messages.feedbackModalTitle} />}
-              footer={
-                <ShortFeedbackFormModalFooter>
-                  {!feedbackSubmitted ? (
-                    <Button
-                      onClick={this.shortFeedbackFormOnSubmit}
-                      processing={feedbackSubmitting}
-                    >
-                      <FormattedMessage {...messages.submit} />
-                    </Button>
-                  ) : (
-                    <Button
-                      buttonStyle="secondary"
-                      onClick={this.closeFeedbackModal}
-                    >
-                      <FormattedMessage {...messages.close} />
-                    </Button>
-                  )}
-                </ShortFeedbackFormModalFooter>
-              }
-            >
-              <ShortFeedbackForm
-                closeModal={this.closeFeedbackModalSuccess}
-                submitting={this.handleFeedbackOnSubmit}
-                successfullySubmitted={this.handleFeedbackSubmitted}
+      <FooterContainer className={showShortFeedback ? 'showShortFeedback' : ''}>
+        <PagesNav>
+          <PagesNavList>
+            {FOOTER_PAGES.map((slug: TFooterPage, index) => {
+              return (
+                <React.Fragment key={slug}>
+                  <PagesNavListItem>
+                    {slug === 'accessibility-statement' &&
+                    hasCustomizedA11yFooterLink &&
+                    customizedA11yHref ? (
+                      <StyledA
+                        href={customizedA11yHref}
+                        target={hasCustomizedA11yFooterLink && '_blank'}
+                        className={index === 0 ? 'first' : ''}
+                      >
+                        <FormattedMessage {...MESSAGES_MAP[slug]} />
+                      </StyledA>
+                    ) : (
+                      <StyledLink
+                        to={`/pages/${slug}`}
+                        className={index === 0 ? 'first' : ''}
+                      >
+                        <FormattedMessage {...MESSAGES_MAP[slug]} />
+                      </StyledLink>
+                    )}
+                  </PagesNavListItem>
+                </React.Fragment>
+              );
+            })}
+            <PagesNavListItem>
+              <StyledButton onClick={openConsentManager}>
+                <FormattedMessage {...messages.cookieSettings} />
+              </StyledButton>
+            </PagesNavListItem>
+            <PagesNavListItem>
+              <StyledLink to="/site-map">
+                <FormattedMessage {...messages.siteMap} />
+              </StyledLink>
+            </PagesNavListItem>
+          </PagesNavList>
+        </PagesNav>
+
+        <Right>
+          <PoweredBy>
+            <PoweredByText>
+              <FormattedMessage {...messages.poweredBy} />
+            </PoweredByText>
+            <CitizenlabLink href="https://www.citizenlab.co/" target="_blank">
+              <CitizenLabLogo
+                name="citizenlab-footer-logo"
+                title="CitizenLab"
               />
-            </Modal>
-          </>
-        )}
+            </CitizenlabLink>
+          </PoweredBy>
 
-        <FooterContainer
-          className={showShortFeedback ? 'showShortFeedback' : ''}
-        >
-          <PagesNav>
-            <PagesNavList>
-              {FOOTER_PAGES.map((slug: TFooterPage, index) => {
-                return (
-                  <React.Fragment key={slug}>
-                    <PagesNavListItem>
-                      {slug === 'accessibility-statement' &&
-                      hasCustomizedA11yFooterLink &&
-                      customizedA11yHref ? (
-                        <StyledA
-                          href={customizedA11yHref}
-                          target={hasCustomizedA11yFooterLink && '_blank'}
-                          className={index === 0 ? 'first' : ''}
-                        >
-                          <FormattedMessage {...MESSAGES_MAP[slug]} />
-                        </StyledA>
-                      ) : (
-                        <StyledLink
-                          to={`/pages/${slug}`}
-                          className={index === 0 ? 'first' : ''}
-                        >
-                          <FormattedMessage {...MESSAGES_MAP[slug]} />
-                        </StyledLink>
-                      )}
-                    </PagesNavListItem>
-                  </React.Fragment>
-                );
-              })}
-              <PagesNavListItem>
-                <StyledButton onClick={this.openConsentManager}>
-                  <FormattedMessage {...messages.cookieSettings} />
-                </StyledButton>
-              </PagesNavListItem>
-              <PagesNavListItem>
-                <StyledLink to="/site-map">
-                  <FormattedMessage {...messages.siteMap} />
-                </StyledLink>
-              </PagesNavListItem>
-            </PagesNavList>
-          </PagesNav>
+          <StyledSendFeedback showFeedbackText={smallerThanSmallTablet} />
+        </Right>
+      </FooterContainer>
+    </Container>
+  );
+};
 
-          <Right>
-            <PoweredBy>
-              <PoweredByText>
-                <FormattedMessage {...messages.poweredBy} />
-              </PoweredByText>
-              <CitizenlabLink href="https://www.citizenlab.co/" target="_blank">
-                <CitizenLabLogo
-                  name="citizenlab-footer-logo"
-                  title="CitizenLab"
-                />
-              </CitizenlabLink>
-            </PoweredBy>
-
-            <StyledSendFeedback showFeedbackText={smallerThanSmallTablet} />
-          </Right>
-        </FooterContainer>
-      </Container>
-    );
-  }
-}
-
-const Data = adopt<Props>({
-  locale: <GetLocale />,
-  windowSize: <GetWindowSize />,
-  appConfiguration: <GetAppConfiguration />,
-  customizedA11yHrefEnabled: (
-    <GetFeatureFlag name="custom_accessibility_statement_link" />
-  ),
-});
-
-export default (inputProps: InputProps) => (
-  <Data>
-    {(dataProps) => <PlatformFooter {...inputProps} {...dataProps} />}
-  </Data>
-);
+export default PlatformFooter;
