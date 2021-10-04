eaa17525fda7c570cbbf2bd3634d1b635e250f14
luucvanderzee <luuc_v_d_zee@hotmail.com>
2021-10-04 17:04:21 +0200

    _

--- front/app/containers/Admin/settings/navigation/core/index.tsx
+++ front/app/containers/Admin/settings/navigation/core/index.tsx
@@ -25,38 +25,39 @@ const PagesOverview = () => {
   const pages = usePages();
   const navbarItems = useNavbarItems();
 
+  if (isNilOrError(pages) || isNilOrError(navbarItems)) return null;
+
   const { visibleNavbarItems, otherNavbarItems } = generateNavbarItems(
     navbarItems,
     pages
   );
 
-  if (!isNilOrError(pages)) {
-    return (
-      <>
-        <UpgradeBox />
+  console.log(visibleNavbarItems);
+  console.log(otherNavbarItems);
 
-        <StyledPageList
-          title={<FormattedMessage {...messages.navigationItems} />}
-          pages={pages.slice(0, 8)}
-          pagesPermissions={Array(pages.length)
-            .fill(0)
-            .map((_, i) => ({ isDefaultPage: i < 2 }))}
-          sortable={true}
-          lockFirstNItems={2}
-        />
+  return (
+    <>
+      <UpgradeBox />
 
-        <PageList
-          title={<FormattedMessage {...messages.hiddenFromNavigation} />}
-          pages={pages.slice(8, 16)}
-          pagesPermissions={Array(pages.length)
-            .fill(0)
-            .map(() => ({ hasAddButton: true }))}
-        />
-      </>
-    );
-  }
+      <StyledPageList
+        title={<FormattedMessage {...messages.navigationItems} />}
+        pages={pages.slice(0, 8)}
+        pagesPermissions={Array(pages.length)
+          .fill(0)
+          .map((_, i) => ({ isDefaultPage: i < 2 }))}
+        sortable={true}
+        lockFirstNItems={2}
+      />
 
-  return null;
+      <PageList
+        title={<FormattedMessage {...messages.hiddenFromNavigation} />}
+        pages={pages.slice(8, 16)}
+        pagesPermissions={Array(pages.length)
+          .fill(0)
+          .map(() => ({ hasAddButton: true }))}
+      />
+    </>
+  );
 };
 
 export default PagesOverview;
