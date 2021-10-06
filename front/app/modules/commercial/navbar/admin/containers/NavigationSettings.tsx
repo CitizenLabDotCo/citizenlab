8f6d7cc6ed1e3ef66438353d4900da4c1db4a282
luucvanderzee <luuc_v_d_zee@hotmail.com>
2021-10-06 10:47:49 +0200

    Added text to top of navigation settings page

--- front/app/modules/commercial/navbar/admin/containers/NavigationSettings.tsx
+++ front/app/modules/commercial/navbar/admin/containers/NavigationSettings.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Box } from 'cl2-component-library';
 
+// styling
+import styled from 'styled-components';
+import { colors } from 'utils/styleUtils';
+
 // hooks
 import usePages from 'hooks/usePages';
 import useNavbarItems from 'hooks/useNavbarItems';
@@ -9,9 +13,28 @@ import useNavbarItems from 'hooks/useNavbarItems';
 import VisibleNavbarItemList from '../components/NavbarItemList/VisibleNavbarItemList';
 import HiddenNavbarItemList from '../components/NavbarItemList/HiddenNavbarItemList';
 
+// i18n
+import { FormattedMessage } from 'utils/cl-intl';
+import messages from './messages';
+
 // utils
 import { isNilOrError } from 'utils/helperUtils';
 import generateNavbarItems from './generateNavbarItems';
+
+const PageTitle = styled.div`
+  font-size: 25px;
+  font-weight: 700;
+  color: ${colors.adminTextColor};
+  margin-bottom: 28px;
+`;
+
+const PageSubtitle = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+  color: ${colors.label};
+  margin-bottom: 58px;
+`;
 
 const NavigationSettings = () => {
   const pages = usePages();
@@ -19,15 +42,22 @@ const NavigationSettings = () => {
 
   if (isNilOrError(pages) || isNilOrError(navbarItems)) return null;
 
+  // temporary solution until backend is fixed
   const { visibleNavbarItems, hiddenNavbarItems } = generateNavbarItems(
     navbarItems,
     pages
   );
 
-  console.log(visibleNavbarItems);
-
   return (
     <>
+      <PageTitle>
+        <FormattedMessage {...messages.pageTitle} />
+      </PageTitle>
+
+      <PageSubtitle>
+        <FormattedMessage {...messages.pageSubtitle} />
+      </PageSubtitle>
+
       <Box mb="44px">
         <VisibleNavbarItemList
           navbarItems={visibleNavbarItems}
---
+++ front/app/modules/commercial/navbar/admin/containers/messages.ts
@@ -0,0 +1,13 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  pageTitle: {
+    id: 'app.modules.navbar.containers.pageTitle',
+    defaultMessage: 'Navigation bar and platform pages',
+  },
+  pageSubtitle: {
+    id: 'app.modules.navbar.containers.pageSubtitle',
+    defaultMessage:
+      'Your navigation bar can display up to four pages in addition to the Home and projects pages. You can rename menu items, re-order and add new pages  with your own content.',
+  },
+});
