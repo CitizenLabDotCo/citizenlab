c983fabba88332017e8dd6a3b51d2b74d66d27ce
brentguf <brent.guffens@gmail.com>
2021-10-11 14:51:43 +0200

    Remove enabled status check in FormikSubmitWrapper

--- front/app/components/admin/FormikSubmitWrapper/index.tsx
+++ front/app/components/admin/FormikSubmitWrapper/index.tsx
@@ -37,7 +37,7 @@ const FormikSubmitWrapper = ({
   ...props
 }: Props) => {
   const getStatus = () => {
-    if (status === 'enabled' && !isEmpty(touched)) {
+    if (!isEmpty(touched)) {
       return 'enabled';
     } else if (status === 'error') {
       return 'error';
