a71d8753d77216a349e0fd6876f9f2ce506946e1
luucvanderzee <luuc_v_d_zee@hotmail.com>
2021-10-11 15:10:28 +0200

    Update copy navigation settings page

--- front/app/modules/commercial/navbar/admin/containers/messages.ts
+++ front/app/modules/commercial/navbar/admin/containers/messages.ts
@@ -8,6 +8,6 @@ export default defineMessages({
   pageSubtitle: {
     id: 'app.modules.navbar.admin.containers.pageSubtitle',
     defaultMessage:
-      'Your navigation bar can display up to four pages in addition to the Home and projects pages. You can rename menu items, re-order and add new pages  with your own content.',
+      'Your navigation bar can display up to five pages in addition to the Home and projects pages. You can rename menu items, re-order and add new pages  with your own content.',
   },
 });
