ef901771856d0c481d584bf76df74d4105c09594
brentguf <brent.guffens@gmail.com>
2021-10-12 11:03:48 +0200

    IPhaseAttributes

--- front/app/services/phases.ts
+++ front/app/services/phases.ts
@@ -16,31 +16,7 @@ const apiEndpoint = `${API_PATH}/phases`;
 export interface IPhaseData {
   id: string;
   type: string;
-  attributes: {
-    title_multiloc: Multiloc;
-    description_multiloc: Multiloc;
-    input_term: InputTerm;
-    start_at: string;
-    end_at: string;
-    created_at: string;
-    updated_at: string;
-    participation_method: ParticipationMethod;
-    posting_enabled: boolean;
-    commenting_enabled: boolean;
-    voting_enabled: boolean;
-    voting_method: 'limited' | 'unlimited';
-    upvoting_limited_max: number;
-    downvoting_enabled: boolean;
-    downvoting_limited_max: number;
-    presentation_mode: 'card' | 'map';
-    min_budget?: number;
-    max_budget?: number;
-    survey_service?: SurveyServices;
-    survey_embed_url?: string;
-    poll_anonymous?: boolean;
-    ideas_count: number;
-    ideas_order?: IdeaDefaultSortMethod;
-  };
+  attributes: IPhaseAttributes;
   relationships: {
     permissions: {
       data: IRelationship[];
@@ -52,6 +28,32 @@ export interface IPhaseData {
       data: IRelationship | null;
     };
   };
+}
+
+export interface IPhaseAttributes {
+  title_multiloc: Multiloc;
+  description_multiloc: Multiloc;
+  input_term: InputTerm;
+  start_at: string;
+  end_at: string;
+  created_at: string;
+  updated_at: string;
+  participation_method: ParticipationMethod;
+  posting_enabled: boolean;
+  commenting_enabled: boolean;
+  voting_enabled: boolean;
+  voting_method: 'limited' | 'unlimited';
+  upvoting_limited_max: number;
+  downvoting_enabled: boolean;
+  downvoting_limited_max: number;
+  presentation_mode: 'card' | 'map';
+  min_budget?: number;
+  max_budget?: number;
+  survey_service?: SurveyServices;
+  survey_embed_url?: string;
+  poll_anonymous?: boolean;
+  ideas_count: number;
+  ideas_order?: IdeaDefaultSortMethod;
 }
 
 export interface IPhase {
