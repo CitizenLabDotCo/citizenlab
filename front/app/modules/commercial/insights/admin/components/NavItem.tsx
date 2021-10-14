813a2752e56530a4674002d0115d8af99c8aec8e
Iva <iva@citizenlab.co>
2021-10-14 12:01:52 +0300

    Remove reports from dashboard

--- front/app/modules/commercial/insights/admin/components/NavItem.tsx
+++ front/app/modules/commercial/insights/admin/components/NavItem.tsx
@@ -2,32 +2,35 @@ import { NavItem } from 'containers/Admin/sideBar';
 import { FC, useEffect } from 'react';
 import { getUrlLocale } from 'services/locale';
 import { InsertConfigurationOptions } from 'typings';
+import useFeatureFlag from 'hooks/useFeatureFlag';
 
 type Props = {
   onData: (data: InsertConfigurationOptions<NavItem>) => void;
 };
 
 const NavItemComponent: FC<Props> = ({ onData }) => {
-  useEffect(
-    () =>
-      onData({
-        configuration: {
-          name: 'insights',
-          link: '/admin/insights',
-          iconName: 'processing',
-          message: 'insights',
-          featureName: 'insights_manual_flow',
-          isActive: (pathName) =>
-            pathName.startsWith(
-              `${
-                getUrlLocale(pathName) ? `/${getUrlLocale(pathName)}` : ''
-              }/admin/insights`
-            ),
-        },
-        insertAfterName: 'projects',
-      }),
-    [onData]
-  );
+  const projectReports = useFeatureFlag('project_reports');
+
+  useEffect(() => {
+    onData({
+      configuration: {
+        name: 'insights',
+        link: `/admin/insights${projectReports ? '/reports' : ''}`,
+        iconName: 'processing',
+        message: 'insights',
+        featureName: projectReports
+          ? 'project_reports'
+          : 'insights_manual_flow',
+        isActive: (pathName) =>
+          pathName.startsWith(
+            `${
+              getUrlLocale(pathName) ? `/${getUrlLocale(pathName)}` : ''
+            }/admin/insights`
+          ),
+      },
+      insertAfterName: 'projects',
+    });
+  }, [onData, projectReports]);
   return null;
 };
 
