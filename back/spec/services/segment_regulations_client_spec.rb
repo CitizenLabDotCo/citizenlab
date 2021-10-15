657531727dee0a882137b0ebb7a51cbfab515fd2
Adrien Dessy <adrien@citizenlab.co>
2021-10-15 18:39:40 +0200

    IN-577 Fix filename

---
+++ back/spec/services/segment_regulations_client_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe SegmentRegulationsClient do
+  subject(:service) { SegmentRegulationsClient.new(authorization_token: token) }
+
+  let(:token) { 'dummy-authorization-token' }
+
+  describe '#delete' do
+    let(:user_ids) { %w[uuid-user-1 uuid-user-2 uuid-user-3] }
+
+    it "sends a request to Segment to create a 'Delete' regulation" do
+      allow(HTTParty).to receive(:post)
+      service.delete(user_ids)
+
+      expect(HTTParty).to have_received(:post) do |url, body:, headers:|
+        expect(url).to eq("https://platform.segmentapis.com/v1beta/workspaces/citizen-lab/regulations")
+
+        body = JSON.parse(body)
+        expect(body).to match({
+          "regulation_type"=>"Delete",
+          "attributes"=>{"name"=>"userId", "values"=>match_array(user_ids.shuffle)}}
+        )
+
+        expect(headers).to eq( {
+          Authorization: "Bearer #{token}",
+          "Content-Type": "application/json"}
+        )
+      end
+    end
+  end
+end
