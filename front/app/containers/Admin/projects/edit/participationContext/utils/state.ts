9fcda55b321a169a6508576400bd3f34a48a6a91
brentguf <brent.guffens@gmail.com>
2021-10-17 21:17:17 +0200

    getStateFromParticipationMethod simplified

--- front/app/containers/Admin/projects/edit/participationContext/utils/state.ts
+++ front/app/containers/Admin/projects/edit/participationContext/utils/state.ts
@@ -6,31 +6,25 @@ import { IParticipationContextConfig } from '..';
 
 export const getStateFromParticipationMethod = (
   participation_method: ParticipationMethod
-) =>
-  ({
+) => {
+  const ideation = participation_method === 'ideation';
+  const budgeting = participation_method === 'budgeting';
+  const survey = participation_method === 'survey';
+  const ideationOrBudgeting = ideation || budgeting;
+
+  return {
     participation_method,
-    posting_enabled: participation_method === 'ideation' ? true : null,
-    commenting_enabled:
-      participation_method === 'ideation' ||
-      participation_method === 'budgeting'
-        ? true
-        : null,
-    voting_enabled: participation_method === 'ideation' ? true : null,
-    upvoting_method: participation_method === 'ideation' ? 'unlimited' : null,
+    posting_enabled: ideation ? true : null,
+    commenting_enabled: ideationOrBudgeting ? true : null,
+    voting_enabled: ideation ? true : null,
+    upvoting_method: ideation ? 'unlimited' : null,
     voting_limited_max: null,
-    downvoting_enabled: participation_method === 'ideation' ? true : null,
-    presentation_mode:
-      participation_method === 'ideation' ||
-      participation_method === 'budgeting'
-        ? 'card'
-        : null,
+    downvoting_enabled: ideation ? true : null,
+    presentation_mode: ideationOrBudgeting ? 'card' : null,
     survey_embed_url: null,
-    survey_service: participation_method === 'survey' ? 'typeform' : null,
-    min_budget: participation_method === 'budgeting' ? 0 : null,
-    max_budget: participation_method === 'budgeting' ? 1000 : null,
-    ideas_order:
-      participation_method === 'ideation' ||
-      participation_method === 'budgeting'
-        ? ideaDefaultSortMethodFallback
-        : null,
-  } as IParticipationContextConfig);
+    survey_service: survey ? 'typeform' : null,
+    min_budget: budgeting ? 0 : null,
+    max_budget: budgeting ? 1000 : null,
+    ideas_order: ideationOrBudgeting ? ideaDefaultSortMethodFallback : null,
+  } as IParticipationContextConfig;
+};
