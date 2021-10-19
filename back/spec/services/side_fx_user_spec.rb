0af43fc0a5176179add6e8f4c0046bbaa174128a
Adrien Dessy <adrien@citizenlab.co>
2021-10-19 17:23:59 +0200

    IN-577 Change example names to something more suitable
    IN-577 #time 0w 0d 0h 28m Change example names to something more suitable

--- back/spec/services/side_fx_user_spec.rb
+++ back/spec/services/side_fx_user_spec.rb
@@ -85,12 +85,12 @@ describe SideFxUserService do
       expect { service.after_destroy(user, current_user) }.to have_enqueued_job(UpdateMemberCountJob)
     end
 
-    it 'removes PII data from Intercom' do
+    it 'successfully enqueues PII data deletion job for Intercom' do
       expect { service.after_destroy(user, current_user) }
         .to have_enqueued_job(RemoveUserFromIntercomJob).with(user.id)
     end
 
-    it 'removes PII data from Segment' do
+    it 'successfully enqueues PII data deletion job for Segment' do
       expect { service.after_destroy(user, current_user) }
         .to have_enqueued_job(RemoveUsersFromSegmentJob).with([user.id])
     end
