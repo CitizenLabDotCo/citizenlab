5c9acd55e55f0c22b923fa7356da03335fc4f04b
Adrien Dessy <adrien@citizenlab.co>
2021-10-20 18:22:36 +0200

    IN-610 Rubocop
    IN-610 #time 0w 0d 0h 1m Rubocop

--- back/app/jobs/application_job.rb
+++ back/app/jobs/application_job.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationJob < ActiveJob::Base
   include Bullet::ActiveJob if Rails.env.development?
   include Que::ActiveJob::JobExtensions
--- back/app/jobs/delete_user_job.rb
+++ back/app/jobs/delete_user_job.rb
@@ -4,7 +4,7 @@ class DeleteUserJob < ApplicationJob
   self.priority = 90 # pretty low priority (lowest is 100)
 
   # @param [User,String] user user or user identifier
-  # @param [User,NilClass] current_user 
+  # @param [User,NilClass] current_user
   def run(user, current_user = nil)
     user = User.find(user) unless user.respond_to?(:id)
     user.destroy!
