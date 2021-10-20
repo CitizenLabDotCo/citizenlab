b33f4f709c2a7a7b143d6f56939f909a88fa0859
Adrien Dessy <adrien@citizenlab.co>
2021-10-20 18:21:36 +0200

    IN-610 Set default priority for job to medium
    IN-610 #time 0w 0d 0h 5m Set default priority for job to medium

--- back/app/jobs/application_job.rb
+++ back/app/jobs/application_job.rb
@@ -4,4 +4,7 @@ class ApplicationJob < ActiveJob::Base
   include ActiveJobQueExtension
 
   perform_retries true
+
+  # Otherwise the default priority would be 100, which is the lowest priority.
+  self.priority = 50
 end
