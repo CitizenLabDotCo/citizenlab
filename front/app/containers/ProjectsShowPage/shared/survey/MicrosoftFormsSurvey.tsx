9a0c83de25b50cd964ae426f12e37fe0a81495f5
Iva <iva@citizenlab.co>
2021-10-21 10:31:34 +0300

    Increase survey width

--- front/app/containers/ProjectsShowPage/shared/survey/MicrosoftFormsSurvey.tsx
+++ front/app/containers/ProjectsShowPage/shared/survey/MicrosoftFormsSurvey.tsx
@@ -4,9 +4,8 @@ import { Box } from 'cl2-component-library';
 
 const StyledIframe = styled.iframe`
   display: block;
-  border: none;
   height: 600px;
-  flex-basis: 640px;
+  width: 100%;
   border: 1px solid #ccc;
 `;
 
