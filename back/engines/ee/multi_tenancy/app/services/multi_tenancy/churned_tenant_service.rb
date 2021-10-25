2a6059c0ea850a6bc417ec804eda5810e47b4e6f
Adrien Dessy <adrien@citizenlab.co>
2021-10-26 00:07:49 +0200

    Namespace ChurnedTenantService + some rework

--- back/engines/ee/multi_tenancy/app/services/multi_tenancy/churned_tenant_service.rb
+++ back/engines/ee/multi_tenancy/app/services/multi_tenancy/churned_tenant_service.rb
@@ -2,29 +2,32 @@
 
 # This service provides a few helper functions to deal with the cleaning of
 # churned tenants.
-class ChurnedTenantService
-  attr_reader :pii_retention_period
+module MultiTenancy
+  class ChurnedTenantService
+    attr_reader :pii_retention_period
 
-  # @param [Integer,NilClass] pii_retention_period How long before user PII
-  #   data are removed from churned tenants (in days since churn).
-  def initialize(pii_retention_period: nil)
-    @pii_retention_period = pii_retention_period || ENV['CHURNED_TENANT_PII_RETENTION_PERIOD']&.to_i
-  end
-
-  def remove_expired_pii(tenants = nil)
-    return unless @pii_retention_period
-
-    tenants = (tenants || Tenant.all).churned
-    tenants.each do |tenant|
-      tenant.switch { User.destroy_all_async } if pii_expired?(tenant)
+    # @param [Integer,NilClass] pii_retention_period How long before user PII
+    #   data are removed from churned tenants (in days since churn).
+    def initialize(pii_retention_period: nil)
+      @pii_retention_period = pii_retention_period || ENV['CHURNED_TENANT_PII_RETENTION_PERIOD']&.to_i
     end
-  end
 
-  private
+    # @param [Tenant::ActiveRecord_Relation,NilClass] tenants
+    def remove_expired_pii(tenants = nil)
+      return unless @pii_retention_period
 
-  def pii_expired?(tenant)
-    today = Date.today
-    last_update_date = tenant.updated_at.to_date
-    (today - last_update_date) > pii_retention_period
+      tenants = (tenants || Tenant.all).churned
+      tenants.each do |tenant|
+        churn_date = tenant.updated_at.to_date
+        tenant.switch { User.destroy_all_async } if pii_expired?(churn_date)
+      end
+    end
+
+    private
+
+    def pii_expired?(churn_date)
+      today = Date.today
+      (today - churn_date) > pii_retention_period
+    end
   end
 end
