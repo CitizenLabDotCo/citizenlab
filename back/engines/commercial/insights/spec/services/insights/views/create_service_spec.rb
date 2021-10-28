4990c5a12f7f4b7e4d47b3f94ae4ea3e4af8d658
Adrien Dessy <adrien@citizenlab.co>
2021-10-28 13:11:44 +0200

    IN-622 Rework pundit authorization in create_service.rb
    IN-622 #time 0w 0d 0h 42m Rework pundit authorization in create_service.rb

---
+++ back/engines/commercial/insights/spec/services/insights/views/create_service_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Insights::Views::CreateService do
+  subject(:service) { described_class.new(current_user, params) }
+
+  let(:params) { { scope_id: view_scope.id, name: view_name } }
+  let(:view_name) { 'view-name' }
+  let_it_be(:view_scope) { create(:project) }
+  let_it_be(:current_user) { create(:admin) }
+
+  describe '#execute' do
+    it 'creates a new view' do
+      view = nil
+      expect { view = service.execute }.to change { Insights::View.count }.by(1)
+      expect(view.name).to eq(view_name)
+      expect(view.scope).to eq(view_scope)
+    end
+
+    # rubocop:disable RSpec/MultipleExpectations
+    it 'enqueues a LogActivityJob job' do
+      view = nil
+      expect { view = service.execute }
+        .to enqueue_job(LogActivityJob).with do |item, action, user, acted_at, _options|
+        expect(item).to eq(view)
+        expect(action).to eq('created')
+        expect(user).to eq(current_user)
+        expect(acted_at).to eq(view.created_at.to_i)
+      end
+    end
+    # rubocop:enable RSpec/MultipleExpectations
+
+    it 'authorizes the current user' do
+      service.execute
+      expect(service.send(:pundit_policy_authorized?)).to eq(true)
+    end
+  end
+end
