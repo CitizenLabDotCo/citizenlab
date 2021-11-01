153a987bc435e9c56b66009966b630d3194654a9
Alexander <alexander@citizenlab.co>
2021-11-01 16:16:22 +0300

    [CL2-6800] Add spec. Find by email case insensitively

--- back/engines/commercial/admin_api/app/jobs/admin_api/bulk_delete_users_job.rb
+++ back/engines/commercial/admin_api/app/jobs/admin_api/bulk_delete_users_job.rb
@@ -1,7 +1,7 @@
 class AdminApi::BulkDeleteUsersJob < ApplicationJob
   def run(emails)
     emails.each do |email|
-      user = User.find_by!(email: email)
+      user = User.find_by_cimail(email) || raise(ActiveRecord::RecordNotFound)
       DeleteUserJob.perform_later(user)
     rescue StandardError => e
       Sentry.capture_exception(e)
