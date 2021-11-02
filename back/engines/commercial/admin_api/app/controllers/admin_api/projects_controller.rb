06e8bc831b8f358c1b351e64e56d63605f05cac3
Adrien Dessy <adrien@citizenlab.co>
2021-11-02 16:00:10 +0100

    IN-629 Use short notation for 'rescue'
    IN-629 #time 0w 0d 1h 30m Use short notation for 'rescue'

--- back/engines/commercial/admin_api/app/controllers/admin_api/projects_controller.rb
+++ back/engines/commercial/admin_api/app/controllers/admin_api/projects_controller.rb
@@ -16,17 +16,16 @@ module AdminApi
       options = template_export_params.to_h.symbolize_keys
       options[:shift_timestamps] = options[:shift_timestamps].to_i if options[:shift_timestamps]
       template = ProjectCopyService.new.export project, **options
-      render json: {template_yaml: template.to_yaml}
+      render json: { template_yaml: template.to_yaml }
     end
 
     def template_import
-      begin
-        template = YAML.load(template_import_params[:template_yaml])
-        ProjectCopyService.new.import template
-      rescue Exception => e
-        Sentry.capture_exception e
-        raise ClErrors::TransactionError.new(error_key: :bad_template)
-      end
+      template = YAML.load(template_import_params[:template_yaml])
+      ProjectCopyService.new.import template
+    rescue Exception => e
+      Sentry.capture_exception e
+      raise ClErrors::TransactionError.new(error_key: :bad_template)
+    else
       head :ok
     end
 
