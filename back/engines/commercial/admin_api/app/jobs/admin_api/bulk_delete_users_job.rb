2c10adeef094e41987836956b95c2c99da7b87e6
Alexander <alexander@citizenlab.co>
2021-11-02 15:43:13 +0300

    [CL2-6800] Specify user email in Not Found Error

--- back/engines/commercial/admin_api/app/jobs/admin_api/bulk_delete_users_job.rb
+++ back/engines/commercial/admin_api/app/jobs/admin_api/bulk_delete_users_job.rb
@@ -1,7 +1,7 @@
 class AdminApi::BulkDeleteUsersJob < ApplicationJob
   def run(emails)
     emails.each do |email|
-      user = User.find_by_cimail(email) || raise(ActiveRecord::RecordNotFound)
+      user = User.find_by_cimail(email) || raise("No user with email #{email} found")
       DeleteUserJob.perform_later(user)
     rescue StandardError => e
       Sentry.capture_exception(e)
