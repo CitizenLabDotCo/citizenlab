0a380825d371b30b1552e6d5ee16104d21f2998c
Adrien Dessy <adrien@citizenlab.co>
2021-11-02 16:14:30 +0100

    IN-629 Dump the whole tenant when a template is imported (overkill but simple & safe)
    IN-629 #time 0w 0d 0h 5m Dump the whole tenant when a template is imported (overkill but simple & safe)

--- back/engines/commercial/admin_api/app/controllers/admin_api/projects_controller.rb
+++ back/engines/commercial/admin_api/app/controllers/admin_api/projects_controller.rb
@@ -23,6 +23,7 @@ module AdminApi
     def template_import
       template = YAML.safe_load(template_import_params[:template_yaml])
       ProjectCopyService.new.import(template)
+      NLP::TenantDumpService.new.dump(Tenant.current) if defined?(NLP)
     rescue StandardError => e
       Sentry.capture_exception(e)
       raise ClErrors::TransactionError.new(error_key: :bad_template)
