0b58e5a238e99538f29ab35e76bd028803b516fc
Sebastien Hoorens <sebastien@citizenlab.co>
2021-11-10 20:24:12 +0100

    reverting factories and deleted crate page service spec

--- back/spec/factories/pages.rb
+++ back/spec/factories/pages.rb
@@ -1,35 +1,14 @@
 FactoryBot.define do
   factory :page do
+    code { 'custom' }
     title_multiloc {{
-      "en" => Faker::Lorem.sentence,
-      "nl-BE" => Faker::Lorem.sentence
+      'en' => Faker::Lorem.sentence,
+      'nl-BE' => Faker::Lorem.sentence
     }}
     body_multiloc {{
-      "en" => Faker::Lorem.paragraphs.map{|p| "<p>#{p}</p>"}.join,
-      "nl-BE" => Faker::Lorem.paragraphs.map{|p| "<p>#{p}</p>"}.join
+      'en' => Faker::Lorem.paragraphs.map{|p| "<p>#{p}</p>"}.join,
+      'nl-BE' => Faker::Lorem.paragraphs.map{|p| "<p>#{p}</p>"}.join
     }}
     sequence(:slug) { |n| "#{Faker::Internet.slug.parameterize}-#{n}".gsub('_', '-') }
-
-    navbar_item { build(:navbar_item, page: nil) }
-
-    trait :skip_validation do
-      to_create { |instance| instance.save(validate: false) }
-    end
-
-    trait :home do
-      skip_validation
-
-      slug { 'home' }
-      body_multiloc { {} }
-      navbar_item { build(:navbar_item, type: 'home', visible: true, ordering: 0) }
-    end
-
-    trait :projects do
-      skip_validation
-
-      slug { 'projects' }
-      body_multiloc { {} }
-      navbar_item { build(:navbar_item, type: 'projects', visible: true, ordering: 1) }
-    end
   end
 end
