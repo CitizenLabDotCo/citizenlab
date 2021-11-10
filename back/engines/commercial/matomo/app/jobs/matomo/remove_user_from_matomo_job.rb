879f22565c8061e00db54a6f8580b7f25c3b8b2d
Adrien Dessy <adrien@citizenlab.co>
2021-11-10 17:08:30 +0100

    IN-573 Typos

--- back/engines/commercial/matomo/app/jobs/matomo/remove_user_from_matomo_job.rb
+++ back/engines/commercial/matomo/app/jobs/matomo/remove_user_from_matomo_job.rb
@@ -2,12 +2,12 @@
 
 module Matomo
   class RemoveUserFromMatomoJob < ApplicationJob
-    self.priority = 70 # lower priority than default, but still higher than DeleteUserJob
+    self.priority = 70 # Lower priority than default, but still higher than DeleteUserJob.
 
     def run(user_id)
       Matomo::Client.new.delete_user_data(user_id)
     rescue Matomo::Client::MissingBaseUriError
-      # Ignore, assuming that matomo was not configured.
+      # Ignore, assuming that Matomo was not configured.
     rescue Matomo::Client::MissingAuthorizationTokenError
       # If Matomo is configured, an authorization token should also be provided.
       raise if ENV.key?('MATOMO_HOST')
