9256b8fedb15feff03a03880a6fb398b573818fa
Simon Tharby <simontharby@gmail.com>
2021-11-11 09:52:51 +0000

    Rename filter

--- back/app/services/areas_filtering_service.rb
+++ back/app/services/areas_filtering_service.rb
@@ -1,7 +1,7 @@
 class AreasFilteringService
   include Filterer
 
-  add_filter('remove_not_allowed_parents') do |scope, options|
+  add_filter('only_selected') do |scope, options|
     next scope unless ['true', true, '1'].include? options[:only_selected]
 
     scope # No-op
