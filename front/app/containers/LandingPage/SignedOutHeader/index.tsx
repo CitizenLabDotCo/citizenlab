365d3af70a02ffed8ce42ce9022627d135047fe5
brentguf <brent.guffens@gmail.com>
2021-11-18 12:17:22 +0100

    Use new setting in front

--- front/app/containers/LandingPage/SignedOutHeader/index.tsx
+++ front/app/containers/LandingPage/SignedOutHeader/index.tsx
@@ -1,17 +1,29 @@
+import useAppConfiguration from 'hooks/useAppConfiguration';
 import React from 'react';
 import Layout1 from './Layout1';
 import Layout2 from './Layout2';
 import Layout3 from './Layout3';
+import { isNilOrError } from 'utils/helperUtils';
 
 interface Props {}
 
 const SignedOutHeaderIndex = ({}: Props) => {
-  const l = 2;
-  return {
-    1: <Layout1 />,
-    2: <Layout2 />,
-    3: <Layout3 />,
-  }[l];
+  const appConfiguration = useAppConfiguration();
+
+  if (!isNilOrError(appConfiguration)) {
+    const homepageBannerLayout =
+      appConfiguration.data.attributes.settings.core.homepage_banner_layout;
+
+    if (homepageBannerLayout) {
+      return {
+        layout_1: <Layout1 />,
+        layout_2: <Layout2 />,
+        layout_3: <Layout3 />,
+      }[homepageBannerLayout];
+    }
+  }
+
+  return null;
 };
 
 export default SignedOutHeaderIndex;
