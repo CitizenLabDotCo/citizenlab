455030036df8d097819d9f57664724bcf5265ac3
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-11-23 15:10:42 +0100

    Add dropzone component

--- front/app/components/Form/ImageControl.tsx
+++ front/app/components/Form/ImageControl.tsx
@@ -3,6 +3,8 @@ import { Box } from 'cl2-component-library';
 import { RankedTester, rankWith, scopeEndsWith } from '@jsonforms/core';
 import React from 'react';
 import { FormLabelStyled } from 'components/UI/FormComponents';
+import ImagesDropzone from 'components/UI/ImagesDropzone';
+
 
 interface InputControlProps {
   data: any;
@@ -15,17 +17,23 @@ interface InputControlProps {
 
 const ImageControl = (props: InputControlProps) => {
   const { uischema } = props;
+
+  const handleUploadOnAdd = () => { };
+  const handleUploadOnRemove = () => { };
+  const imageFile = null
+
+
   return (
     <Box id="e2e-idea-image-input" width="100%" marginBottom="40px">
       <FormLabelStyled>{uischema.label}</FormLabelStyled>
-      {/* <Input
-        type="text"
-        value={data}
-        onChange={(value) => handleChange(path, value)}
-        maxCharCount={schema?.maxLength}
-        error={didBlur ? errors : undefined}
-        onBlur={() => setDidBlur(true)}
-      /> */}
+      <ImagesDropzone
+        id="idea-image-dropzone"
+        images={imageFile}
+        imagePreviewRatio={135 / 298}
+        acceptedFileTypes="image/jpg, image/jpeg, image/png, image/gif"
+        onAdd={handleUploadOnAdd}
+        onRemove={handleUploadOnRemove}
+      />
     </Box>
   );
 };
