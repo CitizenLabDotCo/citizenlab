14f820b615309ba741ead7b969529e962e01b957
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-11-23 14:41:50 +0100

    Set up basic renderers

---
+++ front/app/components/Form/AttachmentsControl.tsx
@@ -0,0 +1,35 @@
+import { withJsonFormsControlProps } from '@jsonforms/react';
+import { Box } from 'cl2-component-library';
+import { RankedTester, rankWith, scopeEndsWith } from '@jsonforms/core';
+import React from 'react';
+import { FormLabelStyled } from 'components/UI/FormComponents';
+
+interface InputControlProps {
+  data: any;
+  handleChange(path: string, value: any): void;
+  path: string;
+  errors: string;
+  schema: any;
+  uischema: any;
+}
+
+const AttachmentsControl = (props: InputControlProps) => {
+  const { uischema } = props;
+  return (
+    <Box id="e2e-idea-image-input" width="100%" marginBottom="40px">
+      <FormLabelStyled>{uischema.label}</FormLabelStyled>
+      {/* <Input
+        type="text"
+        value={data}
+        onChange={(value) => handleChange(path, value)}
+        maxCharCount={schema?.maxLength}
+        error={didBlur ? errors : undefined}
+        onBlur={() => setDidBlur(true)}
+      /> */}
+    </Box>
+  );
+};
+
+export default withJsonFormsControlProps(AttachmentsControl);
+
+export const attachmentsControlTester: RankedTester = rankWith(4, scopeEndsWith('attachments'));
