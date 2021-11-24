ba140c634cb1077fa257156aa04aeceb11a2a157
Sebastien Hoorens <sebastien@citizenlab.co>
2021-11-24 12:29:47 +0100

    also fall back to title translations for custom navbaritems

--- back/app/controllers/web_api/v1/nav_bar_items_controller.rb
+++ back/app/controllers/web_api/v1/nav_bar_items_controller.rb
@@ -5,7 +5,7 @@ class WebApi::V1::NavBarItemsController < ApplicationController
   after_action :verify_policy_scoped, only: :index
 
   def index
-    @items = policy_scope(NavBarItem).order(:ordering)
+    @items = policy_scope(NavBarItem).includes(:static_page).order(:ordering)
     render json: WebApi::V1::NavBarItemSerializer.new(@items, params: fastjson_params).serialized_json
   end
 
