523a02f07fbce3ce14209f6730ee5deb35e3b632
Adrien Dessy <adrien@citizenlab.co>
2021-11-26 10:31:47 +0100

    [IN-662] Add `deleted_at` column to `tenants` table

---
+++ back/db/migrate/20211806161357_add_deleted_at_to_tenants.multi_tenancy.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+# This migration comes from multi_tenancy (originally 20211806161357)
+
+class AddDeletedAtToTenants < ActiveRecord::Migration[6.1]
+  def change
+    add_column :tenants, :deleted_at, :datetime, null: true, default: nil
+  end
+end
