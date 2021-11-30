d8a8c77d71d4a041b5531bd22a2eee3b46f89b0a
Philipp Preß <ntraum@fastmail.com>
2021-11-30 17:57:08 +0100

    Test that vote to user association is optional

--- back/spec/models/vote_spec.rb
+++ back/spec/models/vote_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
   context "associations" do
-    it { should belong_to(:user) }
+    it { should belong_to(:user).optional }
     it { should belong_to(:votable) }
   end
 
