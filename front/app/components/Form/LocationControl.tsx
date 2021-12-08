ca312432dc0be6a0a6ea6bf923b0c9ea42d0ecc8
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-12-08 09:57:55 +0100

    Add location input

--- front/app/components/Form/LocationControl.tsx
+++ front/app/components/Form/LocationControl.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 
 import { withJsonFormsControlProps } from '@jsonforms/react';
-import { Box } from 'cl2-component-library';
+import { Box, LocationInput } from 'cl2-component-library';
 import { RankedTester, rankWith, scopeEndsWith } from '@jsonforms/core';
 import { FormLabelStyled } from 'components/UI/FormComponents';
 
 interface LocationControlProps {
   uischema: any;
+  data: any;
+  handleChange(path: string, value: any): void;
+  path: string;
 }
 
 const LocationControl = (props: LocationControlProps) => {
-  const { uischema } = props;
+  const { uischema, data, handleChange, path } = props;
 
   return (
     <Box id="e2e-idea-location-input" width="100%" marginBottom="40px">
       <FormLabelStyled>{uischema.label}</FormLabelStyled>
+      <LocationInput
+        className="e2e-initiative-location-input"
+        value={data}
+        onChange={(location) => handleChange(path, location)}
+        placeholder={''}
+      />
     </Box>
   );
 };
