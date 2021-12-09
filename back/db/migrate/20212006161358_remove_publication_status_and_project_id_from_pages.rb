f377a909fd9c9d2abd03de3bd58bc623cd919f59
Sebastien Hoorens <sebastien@citizenlab.co>
2021-12-09 12:29:25 +0100

    final fixes (pages -> static pages when migrations moved after renaming)

--- back/db/migrate/20212006161358_remove_publication_status_and_project_id_from_pages.rb
+++ back/db/migrate/20212006161358_remove_publication_status_and_project_id_from_pages.rb
@@ -1,6 +1,6 @@
 class RemovePublicationStatusAndProjectIdFromPages < ActiveRecord::Migration[6.1]
   def change
-    remove_column :pages, :publication_status, :string, default: 'published'
-    remove_column :pages, :project_id, :uuid
+    remove_column :static_pages, :publication_status, :string, default: 'published'
+    remove_column :static_pages, :project_id, :uuid
   end
 end
