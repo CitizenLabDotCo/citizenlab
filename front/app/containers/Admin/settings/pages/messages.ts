3c248a0538ea6e87f93f835adb28c5c2043ef438
luucvanderzee <luuc_v_d_zee@hotmail.com>
2021-12-10 10:48:38 +0100

    Change back crowdin keys (already newly translated)

--- front/app/containers/Admin/settings/pages/messages.ts
+++ front/app/containers/Admin/settings/pages/messages.ts
@@ -2,16 +2,16 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   defaultPagesTitle: {
-    id: 'app.containers.AdminPage.PagesEdition.fixedPagesTitle',
+    id: 'app.containers.AdminPage.PagesEdition.defaultPagesTitle',
     defaultMessage: 'Platform pages',
   },
   defaultPagesSubtitle: {
-    id: 'app.containers.AdminPage.PagesEdition.fixedPagesSubtitle',
+    id: 'app.containers.AdminPage.PagesEdition.defaultPagesSubtitle',
     defaultMessage:
       "Edit your platform's About and FAQ pages. Other pages, including the terms and conditions and privacy policy, can be edited in the {policiesLink} tab.",
   },
   defaultPagesSubtitleLink: {
-    id: 'app.containers.AdminPage.PagesEdition.fixedPagesSubtitleLink',
+    id: 'app.containers.AdminPage.PagesEdition.defaultPagesSubtitleLink',
     defaultMessage: 'Policies',
   },
 });
