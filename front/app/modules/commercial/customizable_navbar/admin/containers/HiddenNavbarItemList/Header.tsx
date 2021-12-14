d9039fba63502abd4c2d165bdc931dd99ae9c14d
luucvanderzee <luuc_v_d_zee@hotmail.com>
2021-12-14 11:00:10 +0100

    Fix HiddenNavbarItemList header

--- front/app/modules/commercial/customizable_navbar/admin/containers/HiddenNavbarItemList/Header.tsx
+++ front/app/modules/commercial/customizable_navbar/admin/containers/HiddenNavbarItemList/Header.tsx
@@ -3,14 +3,20 @@ import React from 'react';
 // components
 import { SubSectionTitle } from 'components/admin/Section';
 import Button from 'components/UI/Button';
+import { Box } from 'cl2-component-library';
 
 // i18n
 import { FormattedMessage } from 'utils/cl-intl';
 import messages from '../messages';
 
 export default () => (
-  <SubSectionTitle>
-    <FormattedMessage {...messages.hiddenFromNavigation} />
-    <Button buttonStyle="cl-blue" linkTo="admin/settings/navigation/new-page" />
-  </SubSectionTitle>
+  <Box display="flex" justifyContent="space-between" mb="17px">
+    <SubSectionTitle>
+      <FormattedMessage {...messages.hiddenFromNavigation} />
+    </SubSectionTitle>
+
+    <Button buttonStyle="cl-blue" linkTo="admin/settings/navigation/new-page">
+      <FormattedMessage {...messages.addPageButton} />
+    </Button>
+  </Box>
 );
--- front/app/modules/commercial/customizable_navbar/services/navbar.ts
+++ front/app/modules/commercial/customizable_navbar/services/navbar.ts
@@ -1,7 +1,7 @@
 import { Multiloc } from 'typings';
 import streams from 'utils/streams';
 import { apiEndpoint, INavbarItem, TNavbarItemCode } from 'services/navbar';
-import { IItemNotInNavbar } from '../admin/containers/getItemsNotInNavbar';
+import { IItemNotInNavbar } from '../admin/containers/HiddenNavbarItemList/getItemsNotInNavbar';
 
 interface INavbarItemAdd {
   code: TNavbarItemCode;
