043bbca97f9f7dba7faa2e1f6724b989c5023d1f
Alexander <alexander@citizenlab.co>
2021-12-17 19:50:24 +0300

    [NO TICKET] fix: skip SSRF protection downloading text images on localhost

--- back/app/uploaders/text_image_uploader.rb
+++ back/app/uploaders/text_image_uploader.rb
@@ -1,1 +1,9 @@
-class TextImageUploader < BaseImageUploader; end
+class TextImageUploader < BaseImageUploader
+  class CustomDownloader < CarrierWave::Downloader::Base
+    def skip_ssrf_protection?(uri)
+      uri.hostname == 'localhost' && uri.port == 4000
+    end
+  end
+
+  self.downloader = CustomDownloader
+end
