ac2820cd6e178756571389e474ce4c07c368fe04
Simon Tharby <simontharby@gmail.com>
2021-12-21 11:23:28 +0000

    Add test - failing as expected
    Currently failing as we are not yet returninga relative path to
    the respective Idea, but rather an absolute URL = the bug.

---
+++ back/engines/commercial/flag_inappropriate_content/spec/serializers/inappropriate_content_flagged_serializer_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe FlagInappropriateContent::WebApi::V1::Notifications::InappropriateContentFlaggedSerializer do
+  it 'includes a relative path to the respective Idea' do
+    flagged = create(:inappropriate_content_flagged)
+    hash = described_class.new(flagged).serializable_hash
+    slug = Idea.find(flagged.inappropriate_content_flag.flaggable.post_id).slug
+
+    expect(hash[:data][:attributes][:flaggable_url]).to eq("ideas/#{slug}")
+  end  
+end
