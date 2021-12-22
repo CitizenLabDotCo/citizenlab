c3efee19a7e126a0f841c334fc4e0a5b31356844
Simon Tharby <simontharby@gmail.com>
2021-12-22 09:10:28 +0000

    Use string, not variable, in test assertion

--- back/engines/commercial/flag_inappropriate_content/spec/serializers/flag_inappropriate_content/web_api/v1/notifications/inappropriate_content_flagged_serializer_spec.rb
+++ back/engines/commercial/flag_inappropriate_content/spec/serializers/flag_inappropriate_content/web_api/v1/notifications/inappropriate_content_flagged_serializer_spec.rb
@@ -3,11 +3,13 @@
 require 'rails_helper'
 
 describe FlagInappropriateContent::WebApi::V1::Notifications::InappropriateContentFlaggedSerializer do
-  it 'includes a relative path to the respective Idea' do
-    flagged = create(:inappropriate_content_flagged)
+  it 'includes a path to the respective Idea' do
+    idea = create(:idea, slug: 'my-idea')
+    comment = create(:comment, idea: idea)
+    flag = create(:inappropriate_content_flag, flaggable: comment)
+    flagged = create(:inappropriate_content_flagged, inappropriate_content_flag: flag)
     hash = described_class.new(flagged).serializable_hash
-    slug = Idea.find(flagged.inappropriate_content_flag.flaggable.post_id).slug
 
-    expect(hash[:data][:attributes][:flaggable_path]).to eq("ideas/#{slug}")
+    expect(hash[:data][:attributes][:flaggable_path]).to eq("ideas/my-idea")
   end
 end
