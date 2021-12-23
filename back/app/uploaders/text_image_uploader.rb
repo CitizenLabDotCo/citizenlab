fbbca77a98f102296d30cceb85c06001ab38d4c5
luucvanderzee <luuc_v_d_zee@hotmail.com>
2021-12-23 11:25:21 +0100

    Revert temporary change to text_image_uploader.rb

--- back/app/uploaders/text_image_uploader.rb
+++ back/app/uploaders/text_image_uploader.rb
@@ -1,9 +1,1 @@
-class TextImageUploader < BaseImageUploader
-  class CustomDownloader < CarrierWave::Downloader::Base
-    def skip_ssrf_protection?(uri)
-      uri.hostname == 'localhost' && uri.port == 4000
-    end
-  end
-
-  self.downloader = CustomDownloader
-end
+class TextImageUploader < BaseImageUploader; end
