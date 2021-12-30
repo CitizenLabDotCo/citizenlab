a35181e4198d5a9412ac7199c10ef5bc8b2ca131
Simon Tharby <simontharby@gmail.com>
2021-12-30 16:18:10 +0000

    Use gsub to interpolate email values into json params

--- back/spec/requests/rack_attack_spec.rb
+++ back/spec/requests/rack_attack_spec.rb
@@ -15,25 +15,26 @@ describe "Rack::Attack", type: :request do
 
   it "limits login requests from same IP to 10 in 20 seconds" do
     headers = { "CONTENT_TYPE" => "application/json" }
+    users = create_list(:user, 12)
 
+    # Use a different email for each request, to avoid testing limit by email
     10.times do |i|
-      # Use a different email for each request, to avoid testing limit by email
-      post "/web_api/v1/user_token", :params => '{ "auth": { "email": "a#{i + 1}@b.com", "password": "test123456" } }', :headers => headers
+      post "/web_api/v1/user_token", :params => '{ "auth": { "email": "INSERT", "password": "test123456" } }'.gsub("INSERT", "#{users[i].email}"), :headers => headers
     end
     expect(status).to eq(404) # Not found
 
-    post "/web_api/v1/user_token", :params => '{ "auth": { "email": "a11@b.com", "password": "test123456" } }', :headers => headers
+    post "/web_api/v1/user_token", :params => '{ "auth": { "email": "INSERT", "password": "test123456" } }'.gsub("INSERT", "#{users[10].email}"), :headers => headers
     expect(status).to eq(429) # Too many requests
 
     travel_to(20.seconds.from_now) do
-      post "/web_api/v1/user_token", :params => '{ "auth": { "email": "a12@b.com", "password": "test123456" } }', :headers => headers
+      post "/web_api/v1/user_token", :params => '{ "auth": { "INSERT": "a12@b.com", "password": "test123456" } }'.gsub("INSERT", "#{users[11].email}"), :headers => headers
       expect(status).to eq(404) # Not found
     end
   end
   
   it "limits login requests for same email to 10 in 20 seconds" do
+    # Use a different IP for each request, to avoid testing limit by IP
     10.times do |i|
-      # Use a different IP for each request, to avoid testing limit by IP
       headers = { "CONTENT_TYPE" => "application/json", "REMOTE_ADDR" => "1.2.3.#{i + 1}" }
       post "/web_api/v1/user_token", :params => '{ "auth": { "email": "a@b.com", "password": "test123456" } }', :headers => headers
     end
@@ -53,11 +54,11 @@ describe "Rack::Attack", type: :request do
   it "limits account creation requests from same IP to 10 in 20 seconds" do
     headers = { "CONTENT_TYPE" => "application/json" }
 
+    # Use a different email for each request, to emulate multiple account creation attempts
     10.times do |i|
-      # Use a different email for each request, to emulate multiple account creation attempts
-      post "/web_api/v1/users", :params => '{ "user": { "email": "a#{i + 1}@b.com", "password": "test123456", "locale": "en", "first_name": "Jane", "last_name": "Doe" } }', :headers => headers
+      post "/web_api/v1/users", :params => '{ "user": { "email": "INSERT", "password": "test123456", "locale": "en", "first_name": "Jane", "last_name": "Doe" } }'.gsub("INSERT", "a#{i + 1}@b.com"), :headers => headers
     end
-    expect(status).to eq(422) # Unprocessable Entity
+    expect(status).to eq(201) # Created
 
     post "/web_api/v1/users", :params => '{ "user": { "email": "a11@b.com", "password": "test123456", "locale": "en", "first_name": "Jane", "last_name": "Doe" } }', :headers => headers
     expect(status).to eq(429) # Too many requests
@@ -70,30 +71,20 @@ describe "Rack::Attack", type: :request do
 
   it "limits password reset email requests from same IP to 10 in 20 seconds" do
     headers = { "CONTENT_TYPE" => "application/json" }
+    users = create_list(:user, 12)
 
-    12.times do |i|
-      create(:user, email: "a#{i + 1}@b.com")
+    # Use a different email for each request, to avoid testing limit by email
+    10.times do |i|
+      post "/web_api/v1/users/reset_password_email", :params => '{ "user": { "email": "INSERT" } }'.gsub("INSERT", "#{users[i].email}"), :headers => headers
     end
 
-    # For some reason, looping and interpolating an email of "a#{i + 1}@b.com" fails with 429 - Too many requests,
-    # maybe because it fits 1 'same email' request in 20 seconds rule.
-    post "/web_api/v1/users/reset_password_email", :params => '{ "user": { "email": "a1@b.com" } }', :headers => headers
-    post "/web_api/v1/users/reset_password_email", :params => '{ "user": { "email": "a2@b.com" } }', :headers => headers
-    post "/web_api/v1/users/reset_password_email", :params => '{ "user": { "email": "a3@b.com" } }', :headers => headers
-    post "/web_api/v1/users/reset_password_email", :params => '{ "user": { "email": "a4@b.com" } }', :headers => headers
-    post "/web_api/v1/users/reset_password_email", :params => '{ "user": { "email": "a5@b.com" } }', :headers => headers
-    post "/web_api/v1/users/reset_password_email", :params => '{ "user": { "email": "a6@b.com" } }', :headers => headers
-    post "/web_api/v1/users/reset_password_email", :params => '{ "user": { "email": "a7@b.com" } }', :headers => headers
-    post "/web_api/v1/users/reset_password_email", :params => '{ "user": { "email": "a8@b.com" } }', :headers => headers
-    post "/web_api/v1/users/reset_password_email", :params => '{ "user": { "email": "a9@b.com" } }', :headers => headers
-    post "/web_api/v1/users/reset_password_email", :params => '{ "user": { "email": "a10@b.com" } }', :headers => headers
     expect(status).to eq(202) # Accepted
 
-    post "/web_api/v1/users/reset_password_email", :params => '{ "user": { "email": "a11@b.com" } }', :headers => headers
+    post "/web_api/v1/users/reset_password_email", :params => '{ "user": { "email": "INSERT" } }'.gsub("INSERT", "#{users[10].email}"), :headers => headers
     expect(status).to eq(429) # Too many requests
 
     travel_to(20.seconds.from_now) do
-      post "/web_api/v1/users/reset_password_email", :params => '{ "user": { "email": "a12@b.com" } }', :headers => headers
+      post "/web_api/v1/users/reset_password_email", :params => '{ "user": { "email": "INSERT" } }'.gsub("INSERT", "#{users[11].email}"), :headers => headers
       expect(status).to eq(202) # Accepted
     end
   end
