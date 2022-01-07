eb9daf1df6466a58251c27f81bf9142cc9ba526c
Iva <iva@citizenlab.co>
2022-01-07 12:07:59 +0200

    Change cookie policy

--- front/app/containers/CookiePolicy/CookiePolicy.test.tsx
+++ front/app/containers/CookiePolicy/CookiePolicy.test.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
-import 'jest-styled-components';
+import { render, screen, fireEvent } from 'utils/testUtils/rtl';
+import CookiePolicy from '../CookiePolicy';
+import eventEmitter from 'utils/eventEmitter';
 
 jest.mock('utils/cl-intl');
+
+jest.mock('utils/eventEmitter');
 jest.mock('modules', () => ({ streamsToReset: [] }));
+jest.mock('components/Fragment', () => ({ children }) => {
+  return <div>{children}</div>;
+});
 
-import CookiePolicy from './index';
-import { shallowWithIntl } from '../../utils/testUtils/withIntl';
-
-describe('<CookiePolicy />', () => {
+describe('CookiePolicy', () => {
   it('renders correctly', () => {
-    const wrapper = shallowWithIntl(<CookiePolicy />);
-    expect(wrapper).toMatchSnapshot();
+    render(<CookiePolicy />);
+    expect(screen.getByTestId('cookiePolicy')).toBeInTheDocument();
+  });
+  it('calls event emitter correctly on view preferences', () => {
+    render(<CookiePolicy />);
+    const button = screen.getByTestId('viewPreferencesButton');
+    fireEvent.click(button);
+    expect(eventEmitter.emit).toHaveBeenCalledWith('openConsentManager');
+  });
+  it('calls event emitter correctly on manage preferences', () => {
+    render(<CookiePolicy />);
+    const button = screen.getByTestId('managePreferencesButton');
+    fireEvent.click(button);
+    expect(eventEmitter.emit).toHaveBeenCalledWith('openConsentManager');
   });
 });
