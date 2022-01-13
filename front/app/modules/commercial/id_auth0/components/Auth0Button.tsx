7595d405910bbe967e5445802c38ed1930e0d7e4
brentguf <brent.guffens@gmail.com>
2022-01-13 21:58:33 +0100

    AuthButton refactor WIP

--- front/app/modules/commercial/id_auth0/components/Auth0Button.tsx
+++ front/app/modules/commercial/id_auth0/components/Auth0Button.tsx
@@ -1,40 +1,44 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { AUTH_PATH } from 'containers/App/constants';
 import { removeUrlLocale } from 'services/locale';
 import { getJwt } from 'utils/auth/jwt';
-
-// typings
-import { IDAuth0Method } from 'services/verificationMethods';
+import { isNilOrError } from 'utils/helperUtils';
+import { TVerificationMethodName } from 'services/verificationMethods';
 
 // components
 import VerificationMethodButton from 'modules/commercial/verification/citizen/components/VerificationMethodButton';
 
 // i18n
 import T from 'components/T';
+import useVerificationMethod from 'hooks/useVerificationMethod';
 
 interface Props {
-  method: IDAuth0Method;
-  last: boolean;
+  onClick: (methodName: TVerificationMethodName) => void;
 }
 
-const Auth0Button = ({ method, last }: Props) => {
-  const handleOnClick = useCallback(() => {
+const Auth0Button = ({ onClick }: Props) => {
+  const verificationMethod = useVerificationMethod('auth0');
+  const handleOnClick = () => {
+    onClick('auth0');
     const jwt = getJwt();
     window.location.href = `${AUTH_PATH}/auth0?token=${jwt}&pathname=${removeUrlLocale(
       window.location.pathname
     )}`;
-  }, []);
+  };
 
-  return (
-    <VerificationMethodButton
-      key={method.id}
-      id="e2e-auth0-button"
-      className={last ? 'last' : ''}
-      onClick={handleOnClick}
-    >
-      <T value={method.attributes.method_name_multiloc} />
-    </VerificationMethodButton>
-  );
+  if (!isNilOrError(verificationMethod)) {
+    return (
+      <VerificationMethodButton
+        id="e2e-auth0-button"
+        className={last ? 'last' : ''}
+        onClick={handleOnClick}
+      >
+        <T value={verificationMethod.attributes.method_name_multiloc} />
+      </VerificationMethodButton>
+    );
+  }
+
+  return null;
 };
 
 export default Auth0Button;
--- front/app/modules/commercial/id_auth0/index.tsx
+++ front/app/modules/commercial/id_auth0/index.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { ModuleConfiguration } from 'utils/moduleUtils';
 import './services/verificationMethods';
 import Auth0Button from './components/Auth0Button';
-import { IDAuth0Method } from 'services/verificationMethods';
 
+// note dependency on
 const configuration: ModuleConfiguration = {
   outlets: {
-    'app.components.VerificationModal.button': (props) => {
-      if (props.method.attributes.name !== 'auth0') return null;
-      return <Auth0Button {...props} method={props.method as IDAuth0Method} />;
+    'app.components.VerificationModal.buttons': (props) => {
+      return <Auth0Button {...props} />;
     },
   },
 };
