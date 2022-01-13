cb77af1b978d039517f8b988cf9726af6ad02d41
Adrien Dessy <adrien@citizenlab.co>
2022-01-13 11:51:21 +0100

    IN-689 Flatten the insights text networks

--- back/engines/commercial/insights/app/services/insights/front_end_format_text_network.rb
+++ back/engines/commercial/insights/app/services/insights/front_end_format_text_network.rb
@@ -9,7 +9,6 @@ module Insights
   # to work with for the front-end.
   class FrontEndFormatTextNetwork
     DEFAULT_KEYWORD_SIZE_RANGE = [1, 5].freeze
-    DEFAULT_CLUSTER_SIZE_RANGE = [100, 500].freeze
     MAX_NB_CLUSTERS = 10
     MAX_NB_KW_PER_CLUSTER = 25 # max number of keyword by cluster
 
@@ -19,25 +18,24 @@ module Insights
     def initialize(
       view,
       keyword_size_range: DEFAULT_KEYWORD_SIZE_RANGE,
-      cluster_size_range: DEFAULT_CLUSTER_SIZE_RANGE,
       max_nb_clusters: MAX_NB_CLUSTERS,
       max_nb_kw_per_cluster: MAX_NB_KW_PER_CLUSTER
     )
       @id = "network-#{view.id}"
       @keyword_size_range = keyword_size_range
-      @cluster_size_range = cluster_size_range
 
       @network = NLP::TextNetwork.merge(
         # Namespacing networks wrt to the language to avoid id collisions.
         *view.text_networks.map { |tn| tn.network.namespace(tn.language) }
       )
+
       @network.prune_communities(max_nb_clusters)
       @network.shrink_communities(max_nb_kw_per_cluster)
     end
 
     # @return [Array<Hash>]
     def nodes
-      @nodes ||= self.class.nodes(@network, @keyword_size_range, @cluster_size_range)
+      @nodes ||= self.class.nodes(@network, @keyword_size_range)
     end
 
     def links
@@ -48,37 +46,8 @@ module Insights
 
       # @param [NLP::TextNetwork] network
       # @return [Array<Hash>]
-      def nodes(
-        network, keyword_size_range = DEFAULT_KEYWORD_SIZE_RANGE, cluster_size_range = DEFAULT_CLUSTER_SIZE_RANGE
-      )
-        keyword_nodes(network, keyword_size_range) + cluster_nodes(network, cluster_size_range)
-      end
-
-      # @param [NLP::TextNetwork] network
-      # @param [Array(Numeric, Numeric)] val_range range of the +val+ attribute after rescaling
-      # @return [Array<Hash>]
-      def cluster_nodes(network, val_range = DEFAULT_CLUSTER_SIZE_RANGE)
-        nodes = network.communities.map.with_index do |community, i|
-          {
-            id: community.id,
-            name: community_name(community),
-            val: community.importance_score,
-            cluster_id: nil,
-            color_index: i
-          }
-        end
-
-        rescale_node_vals(nodes, val_range)
-      end
-
-      # @param [NLP::TextNetwork::Community] community
-      # @param [Integer] n
-      # @return [String]
-      def community_name(community, n = 3)
-        community.children
-                 .sort_by(&:importance_score).reverse
-                 .take(n)
-                 .map(&:name).join(', ')
+      def nodes(network, keyword_size_range = DEFAULT_KEYWORD_SIZE_RANGE)
+        keyword_nodes(network, keyword_size_range)
       end
 
       # @param [NLP::TextNetwork] network
@@ -109,31 +78,7 @@ module Insights
       # @param [NLP::TextNetwork] network
       # @return [Array<{Symbol=>String}>]
       def links(network)
-        cluster_membership_links(network) + inter_cluster_links(network)
-      end
-
-      # @param [NLP::TextNetwork] network
-      # @return [Array<{Symbol=>String}>]
-      def cluster_membership_links(network)
-        network.communities.flat_map do |community|
-          community.children.map { |node| { source: community.id, target: node.id } }
-        end
-      end
-
-      # @param [NLP::TextNetwork] network
-      # @return [Array<{Symbol=>String}>]
-      def inter_cluster_links(network)
-        links_index = network.links.group_by do |link|
-          [link.from_id, link.to_id].sort
-        end
-
-        community_pairs = network.communities.combination(2).select do |c1, c2|
-          c1.children_ids.product(c2.children_ids).any? do |id_pair|
-            links_index[id_pair.sort]
-          end
-        end
-
-        community_pairs.map { |c1, c2| { source: c1.id, target: c2.id } }
+        network.links.map { |link| { source: link.from_id, target: link.to_id } }
       end
     end
   end
--- back/engines/commercial/insights/spec/services/insights/front_end_format_text_network_spec.rb
+++ back/engines/commercial/insights/spec/services/insights/front_end_format_text_network_spec.rb
@@ -19,10 +19,7 @@ RSpec.describe Insights::FrontEndFormatTextNetwork do
 
     let(:options) do
       # picking very unlikely ranges to make sure sizes are rescaled
-      {
-        keyword_size_range: [217, 221],
-        cluster_size_range: [953, 960]
-      }
+      { keyword_size_range: [217, 221] }
     end
     let(:languages) { %w[en fr] }
     let!(:insights_networks) { languages.map { |l| create(:insights_text_network, view: view, language: l) } }
@@ -44,28 +41,21 @@ RSpec.describe Insights::FrontEndFormatTextNetwork do
       expect(vals).to all(be_between(*options[:keyword_size_range]))
     end
 
-    it "rescales 'val' attribute of cluster nodes" do
-      vals = nodes.reject { |node| node[:cluster_id] }.pluck(:val)
-      expect(vals).to all(be_between(*options[:cluster_size_range]))
-    end
-
-    it 'has consistent color information' do
-      keyword_nodes = nodes.select { |n| n[:cluster_id] }
-      cluster_nodes = nodes.reject { |n| n[:cluster_id] }
-
-      color_by_cluster = cluster_nodes.map { |n| [n[:id], n[:color_index]] }.to_h
-      expected_keyword_colors = keyword_nodes.map { |n| color_by_cluster[n[:cluster_id]] }
-      cluster_colors = color_by_cluster.values
-
-      aggregate_failures('check colors') do
-        expect(cluster_colors).not_to include(nil)
-        # all cluster colors should be different
-        expect(cluster_colors.uniq.count).to eq(cluster_colors.count)
-        expect(keyword_nodes.pluck(:color_index)).to eq(expected_keyword_colors)
+    it 'has a one-to-one relationship between clusters and colors' do
+      # Compute mapping between clusters (ids) and their colors.
+      cluster_colors = nodes.group_by { |n| n[:cluster_id] }.transform_values do |nodes|
+        nodes.pluck(:color_index).uniq
       end
+
+      aggregate_failures('check clusters have only 1 color') do
+        expect(cluster_colors.values.map(&:size)).to all(eq(1))
+      end
+
+      # Clusters have different colors.
+      colors = cluster_colors.values.flatten
+      expect(colors).to eq(colors.uniq)
     end
 
-    # rubocop:disable RSpec/ExampleLength
     it 'abides to the limits on the nb of nodes' do
       view = create(:view).tap do |view|
         nlp_network = build(:nlp_text_network, nb_nodes: 20, nb_communities: 5)
@@ -73,15 +63,14 @@ RSpec.describe Insights::FrontEndFormatTextNetwork do
       end
 
       fe_network = described_class.new(view, max_nb_clusters: 3, max_nb_kw_per_cluster: 2)
-      keyword_nodes, cluster_nodes = fe_network.nodes.partition { |node| node[:cluster_id] }
-      kw_counts = keyword_nodes.group_by { |n| n[:cluster_id] }.transform_values(&:count)
+      counts_by_cluster = fe_network.nodes.group_by { |n| n[:cluster_id] }
+                                    .transform_values(&:count)
 
       aggregate_failures('checking nb of nodes') do
-        expect(kw_counts.values).to all eq(2)
-        expect(cluster_nodes.count).to eq(3)
+        expect(counts_by_cluster.values).to all eq(2)
+        expect(counts_by_cluster.size).to eq(3)
       end
     end
-    # rubocop:enable RSpec/ExampleLength
   end
 
   describe '#links' do
@@ -105,9 +94,9 @@ RSpec.describe Insights::FrontEndFormatTextNetwork do
 
     describe 'size (nb of nodes)' do
       where(:network, :expected_size) do
-        build(:nlp_text_network, nb_nodes: 1, nb_communities: 1) | 2
-        build(:nlp_text_network, nb_nodes: 3, nb_communities: 2) | 5
-        build(:nlp_text_network, nb_nodes: 2, nb_communities: 2) | 4
+        build(:nlp_text_network, nb_nodes: 1, nb_communities: 1) | 1
+        build(:nlp_text_network, nb_nodes: 3, nb_communities: 2) | 3
+        build(:nlp_text_network, nb_nodes: 2, nb_communities: 2) | 2
       end
 
       with_them do
@@ -139,91 +128,18 @@ RSpec.describe Insights::FrontEndFormatTextNetwork do
     end
   end
 
-  describe '.cluster_nodes' do
-    subject(:nodes) { described_class.cluster_nodes(network) }
-
-    let(:network) { build(:nlp_text_network, nb_nodes: 3, nb_communities: 2) }
-
-    it { expect(nodes.size).to eq(2) }
-
-    it 'returns correctly formatted nodes' do
-      community = network.communities.first
-
-      expected_node = {
-        id: community.id,
-        name: anything,
-        val: be_between(100, 500),
-        cluster_id: nil,
-        color_index: be_an(Integer)
-      }
-
-      expect(nodes).to include(expected_node)
-    end
-  end
-
   describe '.links size' do
     using RSpec::Parameterized::TableSyntax
     subject { described_class.links(network).size }
 
     where(:network, :min_size, :max_size) do
-      build(:nlp_text_network, nb_nodes: 1, nb_communities: 1) | 1 | 1
-      build(:nlp_text_network, nb_nodes: 3, nb_communities: 1) | 3 | 3
-      build(:nlp_text_network, nb_nodes: 2, nb_communities: 2) | 2 | 3
-      build(:nlp_text_network, nb_nodes: 4, nb_communities: 3) | 4 | 10 # 4 memberships + 3*2 cluster combinations
+      build(:nlp_text_network, nb_nodes: 1, nb_communities: 1) # a single node
+      build(:nlp_text_network, nb_nodes: 5, nb_communities: 1) # all nodes as a single community
+      build(:nlp_text_network, nb_nodes: 4, nb_communities: 2) # 2 nodes / community
     end
 
     with_them do
-      it { is_expected.to be >= min_size }
-      it { is_expected.to be <= max_size }
-    end
-  end
-
-  describe '.cluster_membership_links' do
-    subject(:links) { described_class.cluster_membership_links(network) }
-
-    let(:network) { build(:nlp_text_network, nb_nodes: 3, nb_communities: 2) }
-
-    it { expect(links.size).to eq(3) }
-
-    it 'returns correctly formatted links' do
-      community = network.communities.first
-
-      expected_link = {
-        source: community.id,
-        target: community.children.first.id
-      }
-
-      expect(links).to include(expected_link)
-    end
-  end
-
-  describe '.inter_cluster_links' do
-    subject(:links) { described_class.inter_cluster_links(fully_connected_network) }
-
-    let(:nb_communities) { 4 }
-    let(:nb_nodes) { 5 }
-    let(:fully_connected_network) do
-      build(
-        :nlp_text_network,
-        nb_nodes: nb_nodes,
-        nb_links: nb_nodes * (nb_nodes - 1),
-        nb_communities: nb_communities
-      )
-    end
-
-    specify do
-      # The network is fully connected => the graph of clusters is also fully connected.
-      expected_nb_links = nb_communities * (nb_communities - 1) / 2
-      expect(links.size).to eq(expected_nb_links)
-    end
-
-    it 'returns correctly formatted links' do
-      c1, c2 = fully_connected_network.communities.take(2)
-
-      link1 = { source: c1.id, target: c2.id }
-      link2 = { source: c2.id, target: c1.id }
-
-      expect(links).to include(link1).or include(link2)
+      it { is_expected.to eq(network.links.size) }
     end
   end
 end
