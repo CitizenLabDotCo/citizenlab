b7d48621c4039c1ffe068f44444b1b9e6758a7e5
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-01-21 11:14:50 +0100

    Fix tab styling

--- front/app/components/ProjectAndFolderCards/components/Header/Tabs.tsx
+++ front/app/components/ProjectAndFolderCards/components/Header/Tabs.tsx
@@ -3,6 +3,7 @@ import React, { useMemo } from 'react';
 // styling
 import styled from 'styled-components';
 import { fontSizes, isRtl } from 'utils/styleUtils';
+import { rgba } from 'polished';
 
 // typings
 import { IStatusCounts } from 'hooks/useAdminPublicationsStatusCounts';
@@ -22,14 +23,26 @@ const TabsContainer = styled.div`
 `;
 
 const Tab = styled.div<{ active: boolean }>`
-  ${({ active }) => (active ? 'border-bottom: 3px solid #ce4040' : '')};
+  box-sizing: content-box;
+  ${({ active, theme }) =>
+    active ? `border-bottom: 3px solid ${theme.colorMain}` : ''};
   display: flex;
   flex-direction: row;
   align-items: center;
   font-size: ${fontSizes.large}px;
-  color: ${({ active }) => (active ? '#ce4040' : '')};
-
+  color: ${({ active, theme }) => (active ? theme.colorMain : 'black')};
   padding: 0px 15px;
+
+  ${({ active, theme }) =>
+    !active
+      ? `
+        border-bottom: 3px solid transparent;
+        &:hover {
+          border-bottom: 3px solid ${rgba(theme.colorMain, 0.3)};
+        }
+        cursor: pointer;
+      `
+      : ''}
 `;
 
 interface Props {
@@ -38,15 +51,24 @@ interface Props {
   onChangeTab: (tab: PublicationTab) => void;
 }
 
-const Tabs = ({ currentTab, statusCounts }: Props) => {
+const Tabs = ({ currentTab, statusCounts, onChangeTab }: Props) => {
   const availableTabs = useMemo(() => {
     return getAvailableTabs(statusCounts);
   }, [statusCounts]);
 
+  const handleClickTab = (tab: PublicationTab) => () => {
+    if (currentTab === tab) return;
+    onChangeTab(tab);
+  };
+
   return (
     <TabsContainer>
       {availableTabs.map((tab) => (
-        <Tab active={currentTab === tab} key={tab}>
+        <Tab
+          active={currentTab === tab}
+          key={tab}
+          onClick={handleClickTab(tab)}
+        >
           {tab} ({statusCounts[tab]})
         </Tab>
       ))}
