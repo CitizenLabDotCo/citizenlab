06ec5ef113590b47fc155ec92a4add3b4c51eae3
Simon Tharby <simontharby@gmail.com>
2022-01-24 16:09:34 +0000

    Use correct value for published count in non-ee test.

--- back/spec/acceptance/admin_publications_spec.rb
+++ back/spec/acceptance/admin_publications_spec.rb
@@ -298,12 +298,11 @@ resource "AdminPublication" do
 
         json_response = json_parse(response_body)
         expect(json_response[:status_counts][:draft]).to eq nil
+        expect(json_response[:status_counts][:published]).to eq 2
          
         if CitizenLab.ee?
-          expect(json_response[:status_counts][:published]).to eq 2
           expect(json_response[:status_counts][:archived]).to eq 1
         else
-          expect(json_response[:status_counts][:published]).to eq 1
           expect(json_response[:status_counts][:archived]).to eq 2
         end
       end
