c8a94ea05fcef08ce507b201e76b2b2cf0b3431d
Iva <iva@citizenlab.co>
2022-01-24 18:23:20 +0200

    Update changelog

--- front/app/components/CityLogoSection/messages.ts
+++ front/app/components/CityLogoSection/messages.ts
@@ -6,7 +6,7 @@ export default defineMessages({
     defaultMessage: 'More information about {orgName}',
   },
   logoAltText: {
-    id: 'app.components.CityLogoSection.logoAltText',
-    defaultMessage: '{orgName} logo',
+    id: 'app.components.CityLogoSection.logoImgAltText',
+    defaultMessage: 'Logo of {orgName}',
   },
 });
