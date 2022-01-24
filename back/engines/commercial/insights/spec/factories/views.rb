8ef0e156efcd4091f544906071d75718509538c2
Adrien Dessy <adrien@citizenlab.co>
2022-01-24 17:47:54 +0100

    IN-693 Add temporary support for (legacy) scope option to the :view factory

--- back/engines/commercial/insights/spec/factories/views.rb
+++ back/engines/commercial/insights/spec/factories/views.rb
@@ -3,6 +3,13 @@
 FactoryBot.define do
   factory :view, class: 'Insights::View' do
     sequence(:name) { |n| "view_#{n}" }
-    data_sources { [association(:data_source, view: instance)] }
+    scope { nil }
+    data_sources do
+      if scope.present?
+        [association(:data_source, view: instance, origin: scope)]
+      else
+        [association(:data_source, view: instance)]
+      end
+    end
   end
 end
