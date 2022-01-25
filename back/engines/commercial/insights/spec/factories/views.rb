2041a035cc40d882efff574c1511d62a313f74ad
Adrien Dessy <adrien@citizenlab.co>
2022-01-25 17:49:10 +0100

    IN-693 Introduce minimal empty_view factory

--- back/engines/commercial/insights/spec/factories/views.rb
+++ back/engines/commercial/insights/spec/factories/views.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :view, class: 'Insights::View' do
+  factory :empty_view, class: 'Insights::View' do
     sequence(:name) { |n| "view_#{n}" }
 
-    transient do
-      scope { nil }
-    end
+    factory :view, class: 'Insights::View' do
+      transient do
+        scope { nil }
+      end
 
-    data_sources do
-      if scope.present?
-        [association(:data_source, view: instance, origin: scope)]
-      else
-        [association(:data_source, view: instance)]
+      data_sources do
+        if scope.present?
+          [association(:data_source, view: instance, origin: scope)]
+        else
+          [association(:data_source, view: instance)]
+        end
       end
     end
   end
