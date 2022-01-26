67b419bb0e814b294cbd27b2b745be6674bc148d
Adrien Dessy <adrien@citizenlab.co>
2022-01-26 12:21:57 +0100

    IN-693 Improve documentation

--- back/engines/commercial/insights/app/models/insights/data_source.rb
+++ back/engines/commercial/insights/app/models/insights/data_source.rb
@@ -23,10 +23,13 @@ module Insights
   class DataSource < ::ApplicationRecord
     belongs_to :origin, polymorphic: true
     belongs_to :view, class_name: 'Insights::View'
-    # dependent: :destroy (which is equivalent to an :after_destroy callback)
-    # was triggering an error when deleting a view or a data source.
+    # We use an explicit +before_destroy+ callback because
+    #   dependent: :destroy
+    # (which is equivalent to an :after_destroy callback) makes the deletion of 
+    # views or data sources fail with:
     #   ActiveRecord::RecordNotDestroyed: 
     #   Failed to destroy the record
+    # I don't have a clear explanation why this happens.
     before_destroy { view.destroy }
 
     validates :view, :origin, presence: true
