7aab9905975d9108fc0b431d4dc09590ffc16dc0
brentguf <brent.guffens@gmail.com>
2022-01-26 17:59:51 +0100

    Make GoBackButton responsive + screen reader-friendly

--- front/app/containers/IdeasShow/GoBackButton.tsx
+++ front/app/containers/IdeasShow/GoBackButton.tsx
@@ -1,11 +1,17 @@
 import React, { memo } from 'react';
 import clHistory from 'utils/cl-router/history';
-import { Button } from '@citizenlab/cl2-component-library';
+import {
+  Button,
+  useWindowSize,
+  viewportWidths,
+  Box,
+} from '@citizenlab/cl2-component-library';
 import useProject from 'hooks/useProject';
 import useLocale from 'hooks/useLocale';
 import useLocalize from 'hooks/useLocalize';
 import { isNilOrError } from 'utils/helperUtils';
 import eventEmitter from 'utils/eventEmitter';
+import { ScreenReaderOnly } from 'utils/a11y';
 
 interface Props {
   projectId: string;
@@ -16,6 +22,7 @@ interface Props {
 const GoBackButton = memo(({ projectId, className, insideModal }: Props) => {
   const project = useProject({ projectId });
   const locale = useLocale();
+  const { windowWidth } = useWindowSize();
   const localize = useLocalize();
 
   const onGoBack = (event: React.MouseEvent) => {
@@ -30,6 +37,8 @@ const GoBackButton = memo(({ projectId, className, insideModal }: Props) => {
     }
   };
 
+  const showButtonText = windowWidth <= viewportWidths.smallTablet;
+
   if (!isNilOrError(project) && !isNilOrError(locale)) {
     return (
       <Button
@@ -43,7 +52,12 @@ const GoBackButton = memo(({ projectId, className, insideModal }: Props) => {
         padding="0"
         textDecorationHover="underline"
       >
-        {localize(project.attributes.title_multiloc)}
+        <Box display={showButtonText ? 'none' : 'block'} aria-hidden>
+          {localize(project.attributes.title_multiloc)}
+        </Box>
+        <ScreenReaderOnly>
+          {localize(project.attributes.title_multiloc)}
+        </ScreenReaderOnly>
       </Button>
     );
   }
