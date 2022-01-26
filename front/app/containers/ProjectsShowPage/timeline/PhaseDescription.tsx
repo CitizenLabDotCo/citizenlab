cf83f1a53833208872b9d765a1fcfd372d6b2506
brentguf <brent.guffens@gmail.com>
2022-01-26 11:11:57 +0100

    Remove aria-live from PhaseDescription

--- front/app/containers/ProjectsShowPage/timeline/PhaseDescription.tsx
+++ front/app/containers/ProjectsShowPage/timeline/PhaseDescription.tsx
@@ -73,7 +73,6 @@ const PhaseDescription = ({
 
   return (
     <Container
-      aria-live="polite"
       className={`e2e-phase-description ${className || ''}`}
       role="tabpanel"
       tabIndex={0}
