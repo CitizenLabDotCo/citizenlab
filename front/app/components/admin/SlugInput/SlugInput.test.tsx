ca42aa11b5c0de40327a15a7630395b6ec246ec3
Philipp Preß <ntraum@fastmail.com>
2022-01-27 11:16:39 +0100

    Attempt to add a unit test for SlugInput

---
+++ front/app/components/admin/SlugInput/SlugInput.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import SlugInput from '.';
+
+jest.mock('hooks/useAppConfiguration', () => () => ({
+  data: { attributes: { name: 'orgName', host: 'localhost' } },
+}));
+jest.mock('hooks/useLocale', () => jest.fn(() => 'en'));
+
+describe('SlugInput', () => {
+  let onChange: jest.Mock;
+
+  // beforeEach(() => {
+  //   onChange = jest.fn();
+  // });
+
+  it('shows a preview URL', () => {
+    const resource = 'folder';
+    const showError = true;
+    const slug = 'my-folder';
+    let errors = {};
+
+    render(
+      <SlugInput
+        slug={slug}
+        resource={resource}
+        apiErrors={errors}
+        showSlugErrorMessage={showError}
+        handleSlugOnChange={onChange}
+      />
+    );
+  });
+});
