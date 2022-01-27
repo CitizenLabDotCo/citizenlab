9bfc8040f08f52ad52f6e9d3232651b44ae33489
Simon Tharby <simontharby@gmail.com>
2022-01-27 12:21:32 +0000

    Stub class in old migration

--- back/db/migrate/20200527094026_add_ordering_to_projects_topics.rb
+++ back/db/migrate/20200527094026_add_ordering_to_projects_topics.rb
@@ -1,10 +1,18 @@
 class AddOrderingToProjectsTopics < ActiveRecord::Migration[6.0]
-  def change
+  class StubProjectsTopic
+    self.table_name = 'projects_topics'
+  end
+
+  def up
     add_timestamps :projects_topics
     add_column :projects_topics, :ordering, :integer
 
-    ProjectsTopic.order(:created_at).each.with_index do |projects_topic, index|
+    StubProjectsTopic.order(:created_at).each.with_index do |projects_topic, index|
       projects_topic.update_column :ordering, index
     end
+  end
+
+  def down
+    :projects_topics, :ordering, :integer
   end
 end
