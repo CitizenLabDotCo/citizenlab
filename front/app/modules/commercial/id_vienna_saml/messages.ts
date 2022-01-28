848420e543c708055d656024113b99778f03ddb5
Philipp Preß <ntraum@fastmail.com>
2022-01-28 18:11:23 +0100

    Remove unused messages for now

--- front/app/modules/commercial/id_vienna_saml/messages.ts
+++
@@ -1,8 +0,0 @@
-import { defineMessages } from 'react-intl';
-
-export default defineMessages({
-  loginText: {
-    id: 'app.modules.id_vienna_saml.login_text',
-    defaultMessage: 'Login via StandardPortal',
-  },
-});
