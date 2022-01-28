33b60975baa25bc3a8e52ca40a8d2c37dfad7beb
Sara <sara@citizenlab.co>
2022-01-28 18:14:18 +0100

    Remove deadcode

--- front/app/utils/JSONFormUtils.ts
+++ front/app/utils/JSONFormUtils.ts
@@ -17,18 +17,6 @@ export function getFieldNameFromPath(val: string) {
 export const sanitizeForClassname = (val: string) =>
   val.replaceAll(/#|\/|\./g, '');
 
-export function getLocationNameFromInstancePath(val: string) {
-  const pathChuncks = val.split('/');
-  if (pathChuncks.length <= 1) {
-    return val;
-  } else {
-    return (
-      pathChuncks.find((chunck) => chunck.endsWith('_multiloc')) ||
-      pathChuncks[pathChuncks.length - 1]
-    );
-  }
-}
-
 export const getLabel = (uischema, schema, path) =>
   isString(uischema?.label)
     ? uischema.label
