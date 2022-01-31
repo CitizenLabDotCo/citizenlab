44af6f0b288a6e03f9d6d63da3391e405d579b00
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-01-31 09:59:04 +0100

    Attribution: change CC to v4

--- front/app/modules/commercial/custom_maps/utils/tileProviderDefaultOptions.ts
+++ front/app/modules/commercial/custom_maps/utils/tileProviderDefaultOptions.ts
@@ -17,5 +17,5 @@ export const BASEMAP_AT_DEFAULT_OPTIONS = {
   maxZoom: 18,
   subdomains: ['maps', 'maps1', 'maps2', 'maps3'],
   attribution:
-    'Karte: <a href="http://basemap.at" target="_blank">basemap.at</a>, <a href="http://creativecommons.org/licenses/by/3.0/at/deed.de" target="_blank">CC-BY 3.0</a>',
+    'Karte: <a href="http://basemap.at" target="_blank">basemap.at</a>, <a href="https://creativecommons.org/licenses/by/4.0/deed.de" target="_blank">CC-BY 4.0</a>',
 };
