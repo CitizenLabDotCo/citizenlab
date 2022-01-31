bfe676993dabbcc725783dfd7966c62a7fbfae65
Simon Tharby <simontharby@gmail.com>
2022-01-31 16:31:05 +0000

    improve use of reverisble in migration

--- back/db/migrate/20200527094026_add_ordering_to_projects_topics.rb
+++ back/db/migrate/20200527094026_add_ordering_to_projects_topics.rb
@@ -3,19 +3,16 @@ class AddOrderingToProjectsTopics < ActiveRecord::Migration[6.0]
     self.table_name = 'projects_topics'
   end
 
-  reversible do |dir|
-    dir.up do
-      add_timestamps :projects_topics
-      add_column :projects_topics, :ordering, :integer
+  def change
+    add_timestamps :projects_topics
+    add_column :projects_topics, :ordering, :integer
 
-      StubProjectsTopic.order(:created_at).each.with_index do |projects_topic, index|
-        projects_topic.update_column :ordering, index
+    reversible do |dir|
+      dir.up do
+        StubProjectsTopic.order(:created_at).each.with_index do |projects_topic, index|
+          projects_topic.update_column :ordering, index
+        end
       end
-    end
-
-    dir.down do
-      remove_column :projects_topics, :ordering
-      remove_timestamps :projects_topics
     end
   end
 end
