d17e42bcbba1221595a0763c69eec20fd885f488
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-02-02 16:21:25 +0100

    Again new id

--- front/app/components/ProjectAndFolderCards/components/Topbar/messages.tsx
+++ front/app/components/ProjectAndFolderCards/components/Topbar/messages.tsx
@@ -2,27 +2,27 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   published: {
-    id: 'app.components.ProjectFolderCards.components.Topbar.published',
+    id: 'app.components.ProjectFolderCards.components.Topbar2.published',
     defaultMessage: 'Active',
   },
   archived: {
-    id: 'app.components.ProjectFolderCards.components.Topbar.archived',
+    id: 'app.components.ProjectFolderCards.components.Topbar2.archived',
     defaultMessage: 'Archived',
   },
   draft: {
-    id: 'app.components.ProjectFolderCards.components.Topbar.draft',
+    id: 'app.components.ProjectFolderCards.components.Topbar2.draft',
     defaultMessage: 'Draft',
   },
   all: {
-    id: 'app.components.ProjectFolderCards.components.Topbar.all',
+    id: 'app.components.ProjectFolderCards.components.Topbar2.all',
     defaultMessage: 'All',
   },
   a11y_publicationStatusTabs: {
-    id: 'app.components.ProjectFolderCards.components.Topbar.a11y_publicationStatusTabs',
+    id: 'app.components.ProjectFolderCards.components.Topbar2.a11y_publicationStatusTabs',
     defaultMessage: 'Tabs to switch between active and archived projects',
   },
   a11y_tab: {
-    id: 'app.components.ProjectFolderCards.components.Topbar.a11y_tab',
+    id: 'app.components.ProjectFolderCards.components.Topbar2.a11y_tab',
     defaultMessage: 'Tab {tab}: {count} projects',
   },
 });
