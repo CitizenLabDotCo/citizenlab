0b28f6d0b7224340f4af06cb615b6af4a2612169
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-02-03 10:11:23 +0100

    _

--- front/app/components/EventCard/messages.ts
+++ front/app/components/EventCard/messages.ts
@@ -25,10 +25,6 @@ export default defineMessages({
     id: 'app.components.EventCard.a11y_lessContentVisible',
     defaultMessage: 'Less event information became visble.',
   },
-  attending: {
-    id: 'app.components.EventCard.attending',
-    defaultMessage: '{count} attending',
-  },
   attend: {
     id: 'app.components.EventCard.attend',
     defaultMessage: 'Attend',
