de0a613604c201a3593993e88c920bb0ce700b7c
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-02-03 16:54:26 +0100

    Fix typos in model spec

--- back/spec/models/news_post_spec.rb
+++ back/spec/models/news_post_spec.rb
@@ -9,17 +9,17 @@ RSpec.describe NewsPost, type: :model do
 
   describe "body sanitizer" do
     it "sanitizes script tags in the body" do
-      comment = create(:news_post, body_multiloc: {
+      post = create(:news_post, body_multiloc: {
         "en" => "<p>Test</p><script>This should be removed!</script>"
       })
-      expect(comment.body_multiloc).to eq({"en" => "<p>Test</p>This should be removed!"})
+      expect(post.body_multiloc).to eq({"en" => "<p>Test</p>This should be removed!"})
     end
   end
 
   describe "slug" do
     it "should generate a slug on creation" do
-      idea = create(:initiative, slug: nil)
-      expect(idea.slug).to be_present
+      post = create(:news_post, slug: nil)
+      expect(post.slug).to be_present
     end
   end
 end
