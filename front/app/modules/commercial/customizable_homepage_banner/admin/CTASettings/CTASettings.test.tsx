e2cda70d92f75e8a018603b3851154960f95f0eb
Alexander <alexander@citizenlab.co>
2022-02-03 18:55:24 +0200

    Mock missing components. Add missing props

--- front/app/modules/commercial/customizable_homepage_banner/admin/CTASettings/CTASettings.test.tsx
+++ front/app/modules/commercial/customizable_homepage_banner/admin/CTASettings/CTASettings.test.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import { render, screen } from 'utils/testUtils/rtl';
+import {
+  CTASignedOutType,
+  CTASignedInType,
+  THomepageBannerLayout,
+} from 'services/appConfiguration';
 
 import CTASettings from './';
 
+jest.mock('utils/cl-router/Link', () => 'Link');
+jest.mock('utils/cl-intl');
+jest.mock(
+  'components/UI/InputMultilocWithLocaleSwitcher',
+  () => 'InputMultilocWithLocaleSwitcher'
+);
+jest.mock('hooks/useAppConfigurationLocales', () => jest.fn(() => ['en']));
+
 describe('<CTASettings />', () => {
-  it('renders the correct input', () => {
+  it('renders the correct labels', () => {
     const props = {
       latestAppConfigSettings: {
         customizable_homepage_banner: {
-          cta_signed_out_type: 'no_button',
-          cta_signed_in_type: 'no_button',
+          allowed: true,
+          enabled: true,
+          layout: 'full_width_layout' as THomepageBannerLayout,
+          cta_signed_out_type: 'no_button' as CTASignedOutType,
+          cta_signed_in_type: 'no_button' as CTASignedInType,
         },
       },
-      handleOnChange: (value) => (settingKey, settingValue) => {},
-      errors: null,
+      handleOnChange: (_value) => (_settingKey, _settingValue) => {},
+      errors: {},
     };
 
     render(<CTASettings {...props} />);
