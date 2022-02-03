6ae94db231008c90806b93cec13398b10d028936
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-02-03 14:47:00 +0100

    Add model validations and sanitizations

--- back/app/models/news_post.rb
+++ back/app/models/news_post.rb
@@ -10,4 +10,34 @@
 #  updated_at     :datetime         not null
 #
 class NewsPost < ApplicationRecord
+  has_many :text_images, as: :imageable, dependent: :destroy
+  accepts_nested_attributes_for :text_images
+
+  validates :title_multiloc, presence: true, multiloc: { presence: true }
+  validates :body_multiloc, presence: true, multiloc: { presence: true }
+  validates :slug, presence: true, uniqueness: true
+
+  before_validation :generate_slug, on: :create
+  before_validation :strip_title
+  before_validation :sanitize_body_multiloc
+
+  def generate_slug
+    self.slug ||= SlugService.new.generate_slug self, title_multiloc.values.first
+  end
+
+  def strip_title
+    title_multiloc.each do |key, value|
+      title_multiloc[key] = value.strip
+    end
+  end
+
+  def sanitize_body_multiloc
+    service = SanitizationService.new
+    self.body_multiloc = service.sanitize_multiloc(
+      body_multiloc,
+      %i[title alignment list decoration link image video]
+    )
+    self.body_multiloc = service.remove_multiloc_empty_trailing_tags body_multiloc
+    self.body_multiloc = service.linkify_multiloc body_multiloc
+  end
 end
