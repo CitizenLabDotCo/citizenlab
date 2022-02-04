e38f95e19df03d5b53fbd984c15133dfcf19dc2a
Amanda Anderson <amanda.anderson@citizenlab.co>
2022-02-04 12:09:00 +0100

    Add user test for registered visitors label

--- front/app/modules/commercial/customizable_homepage_banner/admin/CTASettings/CTASettings.test.tsx
+++ front/app/modules/commercial/customizable_homepage_banner/admin/CTASettings/CTASettings.test.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { render, screen } from 'utils/testUtils/rtl';
+import {
+  CTASignedOutType,
+  CTASignedInType,
+  THomepageBannerLayout,
+} from 'services/appConfiguration';
+
 import CTASettings from './';
 
 jest.mock('utils/cl-router/Link', () => 'Link');
@@ -10,25 +16,31 @@ jest.mock(
 );
 jest.mock('hooks/useAppConfigurationLocales', () => jest.fn(() => ['en']));
 
-const latestAppConfigSettings = {
-  customizable_homepage_banner: {
-    cta_signed_out_type: 'no_button',
-    cta_signed_in_type: 'no_button',
+const props = {
+  latestAppConfigSettings: {
+    customizable_homepage_banner: {
+      allowed: true,
+      enabled: true,
+      layout: 'full_width_layout' as THomepageBannerLayout,
+      cta_signed_out_type: 'no_button' as CTASignedOutType,
+      cta_signed_in_type: 'no_button' as CTASignedInType,
+    },
   },
+  handleOnChange: (_value) => (_settingKey, _settingValue) => {},
+  errors: {},
 };
 
-const handleOnChange = (value) => (settingKey, settingValue) => {};
-
-const errors = null;
-
-const props = { latestAppConfigSettings, handleOnChange, errors };
-console.log(props);
-
-describe('TestDescribe', () => {
-  it('Test1', () => {
+describe('<CTASettings />', () => {
+  it('Non-registered visitors label', () => {
     render(<CTASettings {...props} />);
     expect(
       screen.getByText('Button for non-registered visitors')
+    ).toBeInTheDocument();
+  });
+  it('Registered visitors label', () => {
+    render(<CTASettings {...props} />);
+    expect(
+      screen.getByText('Button for registered visitors')
     ).toBeInTheDocument();
   });
 });
