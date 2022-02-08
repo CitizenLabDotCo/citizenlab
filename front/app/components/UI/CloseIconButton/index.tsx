cc727dcc40a6ba7d33c0cde4f77833cccb55bdc2
brentguf <brent.guffens@gmail.com>
2022-02-08 17:47:20 +0100

    CloseIconButton component

---
+++ front/app/components/UI/CloseIconButton/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { IconButton } from '@citizenlab/cl2-component-library';
+// i18n
+import { injectIntl } from 'utils/cl-intl';
+import { InjectedIntlProps } from 'react-intl';
+import messages from './messages';
+
+interface Props {
+  onClick: () => void;
+  a11y_buttonActionDescription: string;
+}
+
+const CloseIconButton = ({
+  onClick,
+  a11y_buttonActionDescription,
+  intl: { formatMessage },
+}: Props & InjectedIntlProps) => {
+  return (
+    <IconButton
+      iconName="close"
+      onClick={onClick}
+      a11y_buttonActionDescription={
+        a11y_buttonActionDescription ||
+        formatMessage(messages.a11y_closeButtonActionDescriptionFallBack)
+      }
+    />
+  );
+};
+
+export default injectIntl(CloseIconButton);
---
+++ front/app/components/UI/CloseIconButton/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  a11y_closeButtonActionDescriptionFallBack: {
+    id: 'app.components.UI.CloseIconButton.a11y_closeButtonActionDescriptionFallBack',
+    defaultMessage: 'Please provide a title',
+  },
+});
