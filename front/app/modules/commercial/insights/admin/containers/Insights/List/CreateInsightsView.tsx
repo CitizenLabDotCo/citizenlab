dbcc9bca7a844221dff9b868f803b6bd542a6b82
Iva <iva@citizenlab.co>
2022-02-09 16:11:40 +0200

    Add animation on collapse/expand

--- front/app/modules/commercial/insights/admin/containers/Insights/List/CreateInsightsView.tsx
+++ front/app/modules/commercial/insights/admin/containers/Insights/List/CreateInsightsView.tsx
@@ -9,11 +9,12 @@ import { FormattedMessage } from 'utils/cl-intl';
 import messages from '../messages';
 
 // components
-import { Input, Box } from '@citizenlab/cl2-component-library';
+import { Input, Box, Icon } from '@citizenlab/cl2-component-library';
 import Button from 'components/UI/Button';
 import { SectionField } from 'components/admin/Section';
 import Error from 'components/UI/Error';
 import Checkbox from 'components/UI/Checkbox';
+import { CSSTransition } from 'react-transition-group';
 
 // resources
 import { adopt } from 'react-adopt';
@@ -24,6 +25,7 @@ import GetProjects, {
 
 // hooks
 import useLocalize from 'hooks/useLocalize';
+import useProjectFolders from 'modules/commercial/project_folders/hooks/useProjectFolders';
 
 // services
 import { addInsightsView } from 'modules/commercial/insights/services/insightsViews';
@@ -31,7 +33,6 @@ import { addInsightsView } from 'modules/commercial/insights/services/insightsVi
 // utils
 import { isNilOrError } from 'utils/helperUtils';
 import clHistory from 'utils/cl-router/history';
-import { compact } from 'lodash-es';
 
 // typings
 import { CLErrors } from 'typings';
@@ -55,11 +56,76 @@ interface InputProps {
   closeCreateModal: () => void;
 }
 
+const timeout = 400;
+
+const ArrowIcon = styled(Icon)`
+  flex: 0 0 12px;
+  width: 12px;
+  height: 12px;
+  transform: rotate(90deg);
+  transition: all 0.2s linear;
+  margin-left: 5px;
+
+  &.open {
+    transform: rotate(0deg);
+  }
+`;
+
+const AnimatedFieldset = styled.fieldset`
+  border: none;
+  margin: 0;
+  padding: 0;
+  opacity: 0;
+  display: none;
+  transition: all ${timeout}ms cubic-bezier(0.165, 0.84, 0.44, 1);
+  will-change: opacity, height;
+
+  &.collapse-enter {
+    opacity: 0;
+    max-height: 0px;
+    overflow: hidden;
+    display: block;
+
+    &.collapse-enter-active {
+      opacity: 1;
+      max-height: 350px;
+      overflow: hidden;
+      display: block;
+    }
+  }
+
+  &.collapse-enter-done {
+    opacity: 1;
+    overflow: visible;
+    display: block;
+  }
+
+  &.collapse-exit {
+    opacity: 1;
+    max-height: 350px;
+    overflow: hidden;
+    display: block;
+
+    &.collapse-exit-active {
+      opacity: 0;
+      max-height: 0px;
+      overflow: hidden;
+      display: block;
+    }
+  }
+
+  &.collapse-exit-done {
+    display: none;
+  }
+`;
+
 export const CreateInsightsView = ({
   projects,
   closeCreateModal,
 }: DataProps & InputProps) => {
   const localize = useLocalize();
+  const { projectFolders } = useProjectFolders({});
+  console.log(projectFolders);
   const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState<CLErrors | undefined>();
 
@@ -70,6 +136,7 @@ export const CreateInsightsView = ({
   };
 
   const [selectedProjectsIds, setSelectedProjectsIds] = useState<string[]>([]);
+  const [expandedFoldersIds, setExpandedFoldersIds] = useState<string[]>([]);
 
   const handleSubmit = async () => {
     if (name && selectedProjectsIds.length) {
@@ -101,10 +168,14 @@ export const CreateInsightsView = ({
       setSelectedProjectsIds([...selectedProjectsIds, projectId]);
     }
   };
-  const folderIds = compact(
-    projects?.projectsList?.map((project) => project.attributes.folder_id)
-  );
-  console.log(folderIds);
+  const toggleExpandFolder = (folderId: string) => {
+    if (expandedFoldersIds.includes(folderId)) {
+      setExpandedFoldersIds(expandedFoldersIds.filter((id) => id !== folderId));
+    } else {
+      setExpandedFoldersIds([...expandedFoldersIds, folderId]);
+    }
+  };
+
   return (
     <Box
       w="100%"
@@ -133,7 +204,11 @@ export const CreateInsightsView = ({
         <Box>
           {projects.projectsList?.map((project) =>
             project.attributes.folder_id ? null : (
-              <Box py="15px" borderBottom={`1px solid ${colors.separation}`}>
+              <Box
+                key={project.id}
+                py="15px"
+                borderBottom={`1px solid ${colors.separation}`}
+              >
                 <Checkbox
                   size="20px"
                   onChange={() => toggleSelectProject(project.id)}
@@ -143,6 +218,77 @@ export const CreateInsightsView = ({
               </Box>
             )
           )}
+          {!isNilOrError(projectFolders) &&
+            projectFolders.map((folder) => (
+              <Box key={folder.id}>
+                <Box
+                  py="15px"
+                  borderBottom={`1px solid ${colors.separation}`}
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Checkbox
+                    size="20px"
+                    onChange={() => toggleSelectProject(folder.id)}
+                    label={localize(folder.attributes.title_multiloc)}
+                    checked={selectedProjectsIds.includes(folder.id)}
+                  />
+                  <Button
+                    onClick={() => toggleExpandFolder(folder.id)}
+                    buttonStyle="text"
+                    type="button"
+                    ariaExpanded={expandedFoldersIds.includes(folder.id)}
+                    padding="0px"
+                  >
+                    {expandedFoldersIds.includes(folder.id) ? (
+                      <FormattedMessage {...messages.createModalCollapse} />
+                    ) : (
+                      <FormattedMessage {...messages.createModalExpand} />
+                    )}
+                    <ArrowIcon
+                      name="dropdown"
+                      className={
+                        expandedFoldersIds.includes(folder.id) ? 'open' : ''
+                      }
+                      ariaHidden
+                    />
+                  </Button>
+                </Box>
+                <Box>
+                  <CSSTransition
+                    classNames="collapse"
+                    in={expandedFoldersIds.includes(folder.id)}
+                    appear={expandedFoldersIds.includes(folder.id)}
+                    timeout={timeout}
+                    mounOnEnter={false}
+                    unmountOnExit={false}
+                    enter={true}
+                    exit={true}
+                  >
+                    <AnimatedFieldset>
+                      {projects.projectsList
+                        ?.filter(
+                          (project) =>
+                            project.attributes.folder_id === folder.id
+                        )
+                        .map((project) => (
+                          <Box key={project.id} py="15px" ml="36px">
+                            <Checkbox
+                              size="20px"
+                              onChange={() => toggleSelectProject(project.id)}
+                              label={localize(
+                                project.attributes.title_multiloc
+                              )}
+                              checked={selectedProjectsIds.includes(project.id)}
+                            />
+                          </Box>
+                        ))}
+                    </AnimatedFieldset>
+                  </CSSTransition>
+                </Box>
+              </Box>
+            ))}
         </Box>
         <Box display="flex" justifyContent="center" mt="60px">
           <Button
