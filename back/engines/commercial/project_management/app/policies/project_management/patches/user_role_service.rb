92fc9501014b4bf6edfdfc81ae3865f93ac2c518
Sebastien Hoorens <sebastien@citizenlab.co>
2022-02-10 19:07:12 +0100

    bugfixes user role service

--- back/engines/commercial/project_management/app/policies/project_management/patches/user_role_service.rb
+++ back/engines/commercial/project_management/app/policies/project_management/patches/user_role_service.rb
@@ -4,11 +4,12 @@ module ProjectManagement
   module Patches
     module UserRoleService
       def can_moderate_project?(project, user)
-        super || user.project_moderator?(project.id)
+        super || (project.id && user.project_moderator?(project.id))
       end
 
       def moderators_for_project(project, scope = ::User)
-        super.or scope.project_moderator(project.id)
+        moderator_scope = project.id ? scope.project_moderator(project.id) : scope.none
+        super.or moderator_scope
       end
 
       # @param [User] user
