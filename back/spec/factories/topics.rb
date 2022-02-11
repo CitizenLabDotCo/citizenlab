3ab138f2bd16ee2a4384cfb886995f493eab94b4
Adrien Dessy <adrien@citizenlab.co>
2022-02-11 15:36:03 +0100

    IN-716 Remove hardcode title/desc/code from topic factory

--- back/spec/factories/topics.rb
+++ back/spec/factories/topics.rb
@@ -1,39 +1,37 @@
 FactoryBot.define do
   factory :topic do
-    title_multiloc {{
-      "en" => "Health",
-      "nl-BE" => "Gezondheid"
-    }}
-    description_multiloc {{
-      "en" => "<p>How are the people feeling?</p><p>What is the expected agespan?</p>",
-      "nl-BE" => "<p>Hoe voelen de mensen zich?</p><p>Wat is de levensverwachting?</p>"
-    }}
+    transient do
+      locales { %w[en nl-BE] }
+    end
+
+    sequence(:title_multiloc) { |n| locales.index_with { |l| "Topic #{n} (#{l})" } }
+    sequence(:description_multiloc) { |n| locales.index_with { |l| "Topic-#{n} description (#{l})" } }
+    sequence(:code) { |n| Topic::DEFAULT_CODES[n - 1] }
+
     icon { "medical" }
-    code { Topic::DEFAULT_CODES.first }
 
     factory :topic_nature do
-      title_multiloc {{ 'en' => 'Nature' }}
+      title_multiloc { { 'en' => 'Nature' } }
     end
 
     factory :topic_waste do
-      title_multiloc {{ 'en' => 'Waste' }}
+      title_multiloc { { 'en' => 'Waste' } }
     end
 
     factory :topic_sustainability do
-      title_multiloc {{ 'en' => 'Sustainability' }}
+      title_multiloc { { 'en' => 'Sustainability' } }
     end
 
     factory :topic_mobility do
-      title_multiloc {{ 'en' => 'Mobility' }}
+      title_multiloc { { 'en' => 'Mobility' } }
     end
 
     factory :topic_technology do
-      title_multiloc {{ 'en' => 'Technology' }}
+      title_multiloc { { 'en' => 'Technology' } }
     end
 
     factory :topic_economy do
-      title_multiloc {{ 'en' => 'Economy' }}
+      title_multiloc { { 'en' => 'Economy' } }
     end
   end
-
 end
