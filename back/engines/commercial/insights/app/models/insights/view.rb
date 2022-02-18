e4e1a12a977d07576356885027c461cea0e71d9a
Adrien Dessy <adrien@citizenlab.co>
2022-02-18 12:00:53 +0100

    IN-696 Add helper source_projects association to insights views

--- back/engines/commercial/insights/app/models/insights/view.rb
+++ back/engines/commercial/insights/app/models/insights/view.rb
@@ -16,10 +16,14 @@
 module Insights
   class View < ::ApplicationRecord
     has_many :data_sources, class_name: 'Insights::DataSource', dependent: :destroy
+    has_many :source_projects, through: :data_sources, source: :origin, source_type: 'Project'
+
     has_many :categories, -> { order(position: :desc) }, class_name: 'Insights::Category', dependent: :destroy
     has_many :category_assignments, through: :categories, source: :assignments
+
     has_many :text_networks, class_name: 'Insights::TextNetwork', dependent: :destroy
     has_many :tna_tasks_views, class_name: 'Insights::TextNetworkAnalysisTaskView', dependent: :destroy
+
     has_many(
       :processed_flags,
       class_name: 'Insights::ProcessedFlag',
