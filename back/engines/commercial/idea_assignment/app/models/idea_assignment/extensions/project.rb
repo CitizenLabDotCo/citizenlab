8811b3218e0f47816d5a9880a96da85c16b60bee
Sebastien Hoorens <sebastien@citizenlab.co>
2022-02-21 17:30:56 +0100

    validate default assignee + spec for assignee service

--- back/engines/commercial/idea_assignment/app/models/idea_assignment/extensions/project.rb
+++ back/engines/commercial/idea_assignment/app/models/idea_assignment/extensions/project.rb
@@ -4,6 +4,17 @@ module IdeaAssignment
       def self.included(base)
         base.class_eval do
           belongs_to :default_assignee, class_name: 'User', optional: true
+          validate :assignee_can_moderate_project
+        end
+      end
+
+      def assignee_can_moderate_project
+        if default_assignee && project && !UserRoleService.new.can_moderate_project?(project, default_assignee)
+          errors.add(
+            :default_assignee_id,
+            :assignee_can_not_moderate_project,
+            message: 'The default assignee can not moderate the project'
+          )
         end
       end
     end
