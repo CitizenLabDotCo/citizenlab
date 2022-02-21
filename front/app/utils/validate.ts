011b6f1c683702a82e42bea332be1b1506449f05
brentguf <brent.guffens@gmail.com>
2022-02-21 15:48:43 +0100

    isValidPhoneNumber validation

--- front/app/utils/validate.ts
+++ front/app/utils/validate.ts
@@ -1,3 +1,8 @@
-export function isValidEmail(email) {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+export function isValidEmail(email: string) {
+  return email.length > 0 && /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+export function isValidPhoneNumber(phone: string) {
+  // current back-end implementation, see phone_service.rb
+  return phone.length > 5 && /^\+?[0-9\.x\-\s\(\)]+$/.test(phone);
 }
