55d8d186d3ecf225cad9ade27815df95a7954c40
brentguf <brent.guffens@gmail.com>
2022-02-22 10:18:10 +0100

    getHasEmailOrPhoneNumberValidationError

--- front/app/utils/validate.ts
+++ front/app/utils/validate.ts
@@ -6,3 +6,27 @@ export function isValidPhoneNumber(phone: string) {
   // current back-end implementation, see phone_service.rb
   return phone.length > 5 && /^\+?[0-9\.x\-\s\(\)]+$/.test(phone);
 }
+
+export function getHasEmailOrPhoneNumberValidationError(
+  emailOrPhoneNumber: string | null,
+  phoneLoginEnabled: boolean
+) {
+  if (emailOrPhoneNumber === null) {
+    return {
+      hasPhoneNumberValidationError: true,
+      hasEmailValidationError: true,
+    };
+  }
+
+  const hasPhoneNumberValidationError =
+    phoneLoginEnabled && isValidPhoneNumber(emailOrPhoneNumber);
+  const hasEmailValidationError =
+    (!phoneLoginEnabled ||
+      (phoneLoginEnabled && hasPhoneNumberValidationError)) &&
+    !isValidEmail(emailOrPhoneNumber);
+
+  return {
+    hasPhoneNumberValidationError,
+    hasEmailValidationError,
+  };
+}
