add46d627661b17b3548f96a3fa56c69e111ea1c
Philipp Preß <ntraum@fastmail.com>
2022-02-22 17:13:53 +0100

    [CL-208] Remove unused map serializers

--- back/engines/commercial/admin_api/app/serializers/layer_serializer.rb
+++
@@ -1,5 +0,0 @@
-class LayerSerializer
-  include FastJsonapi::ObjectSerializer
-
-  attributes :title_multiloc, :geojson, :default_enabled, :marker_svg_url, :ordering, :id
-end
--- back/engines/commercial/admin_api/app/serializers/map_config_serializer.rb
+++
@@ -1,8 +0,0 @@
-class MapConfigSerializer
-  include FastJsonapi::ObjectSerializer
-
-  attributes :project_id, :zoom_level, :tile_provider
-  attribute :center, &:center_geojson
-
-  has_many :layers
-end
