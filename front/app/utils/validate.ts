9071b00ecb90e63c4c1ee5edd2ca27322f7e2c90
brentguf <brent.guffens@gmail.com>
2022-02-23 14:25:25 +0100

    Simplify validation functions

--- front/app/utils/validate.ts
+++ front/app/utils/validate.ts
@@ -1,17 +1,8 @@
-function isValidEmail(email: string) {
+export function isValidEmail(email: string) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
 
-function isValidPhoneNumber(phoneNumber: string) {
+export function isValidPhoneNumber(phoneNumber: string) {
   // current back-end implementation, see phone_service.rb
   return phoneNumber.length > 5 && /^\+?[0-9\.x\-\s\(\)]+$/.test(phoneNumber);
-}
-
-export function getHasEmailValidationError(email: string | null) {
-  if (email === null) return true;
-  return !isValidEmail(email);
-}
-export function getHasPhoneNumberValidationError(phoneNumber: string | null) {
-  if (phoneNumber === null) return true;
-  return !isValidPhoneNumber(phoneNumber);
 }
