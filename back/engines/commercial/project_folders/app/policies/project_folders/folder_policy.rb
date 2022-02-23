6213f1999c2cc75d69b973881c023b8fe3c3044f
Sebastien Hoorens <sebastien@citizenlab.co>
2022-02-23 19:57:17 +0100

    small improvement

--- back/engines/commercial/project_folders/app/policies/project_folders/folder_policy.rb
+++ back/engines/commercial/project_folders/app/policies/project_folders/folder_policy.rb
@@ -25,7 +25,7 @@ module ProjectFolders
 
     def show?
       return true if user && UserRoleService.new.can_moderate?(record, user)
-      return false if %w[published archived].include?(record.admin_publication.publication_status)
+      return false if %w[published archived].exclude?(record.admin_publication.publication_status)
 
       ProjectPolicy::Scope.new(user, record.projects).resolve.exists?
     end
