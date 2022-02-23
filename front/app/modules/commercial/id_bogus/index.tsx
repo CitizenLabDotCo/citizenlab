323dbee64be5151c40b4bad08add6f985187de87
brentguf <brent.guffens@gmail.com>
2022-02-23 16:16:52 +0100

    Implement module index of 'bogus' verification method/button

--- front/app/modules/commercial/id_bogus/index.tsx
+++ front/app/modules/commercial/id_bogus/index.tsx
@@ -6,9 +6,32 @@ import VerificationFormBogus from './components/VerificationFormBogus';
 
 const configuration: ModuleConfiguration = {
   outlets: {
-    'app.components.VerificationModal.buttonss': (props) => {
-      if (props.method.attributes.name !== 'bogus') return null;
-      return <BogusButton {...props} />;
+    'app.components.VerificationModal.buttons': ({
+      verificationMethods,
+      onClick,
+    }) => {
+      const method = verificationMethods.find(
+        (vm) => vm.attributes.name === 'bogus'
+      );
+
+      if (method) {
+        const last =
+          verificationMethods
+            .map((vm) => vm.attributes.name)
+            .indexOf('bogus') ===
+          verificationMethods.length - 1;
+        const onMethodSelected = () => onClick('bogus');
+
+        return (
+          <BogusButton
+            method={method}
+            last={last}
+            onMethodSelected={onMethodSelected}
+          />
+        );
+      }
+
+      return null;
     },
     'app.components.VerificationModal.methodStep': (props) => {
       if (props.method.attributes.name !== 'bogus') return null;
