b5fbf06d1d0a4f1dffc86cf84e08b0276fe8794f
brentguf <brent.guffens@gmail.com>
2022-02-25 15:17:48 +0100

    id_auth0 button

--- front/app/modules/commercial/id_auth0/index.tsx
+++ front/app/modules/commercial/id_auth0/index.tsx
@@ -2,22 +2,32 @@ import React from 'react';
 import { ModuleConfiguration } from 'utils/moduleUtils';
 import { IDAuth0Method } from 'services/verificationMethods';
 import Auth0Button from './components/Auth0Button';
+import { isLastVerificationMethod } from 'modules/commercial/verification';
 
+const verificationMethodName = 'auth0';
 // note dependency on
 const configuration: ModuleConfiguration = {
   outlets: {
     'app.components.VerificationModal.buttons': ({
       verificationMethods,
+      onClick,
       ...props
     }) => {
-      const auth0VerificationMethod = verificationMethods.find(
-        (vm) => vm.attributes.name === 'auth0'
+      const method = verificationMethods.find(
+        (vm) => vm.attributes.name === verificationMethodName
       );
 
-      if (auth0VerificationMethod) {
+      if (method) {
+        const last = isLastVerificationMethod(
+          verificationMethodName,
+          verificationMethods
+        );
+        const onMethodSelected = () => onClick(verificationMethodName);
         return (
           <Auth0Button
-            verificationMethod={auth0VerificationMethod as IDAuth0Method}
+            verificationMethod={method as IDAuth0Method}
+            onClick={onMethodSelected}
+            last={last}
             {...props}
           />
         );
