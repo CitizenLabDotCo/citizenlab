ad111dec38f42bd2b53e15f33b32f6987452baa9
brentguf <brent.guffens@gmail.com>
2022-02-28 13:19:06 +0100

    clave_unica

--- front/app/modules/commercial/id_clave_unica/index.tsx
+++ front/app/modules/commercial/id_clave_unica/index.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 import { ModuleConfiguration } from 'utils/moduleUtils';
 import './services/verificationMethods';
 import ClaveUnicaButton from './components/ClaveUnicaButton';
+import { isLastVerificationMethod } from 'modules/commercial/verification';
 
+const verificationMethodName = 'clave_unica';
 const configuration: ModuleConfiguration = {
   outlets: {
-    'app.components.VerificationModal.buttons': (props) => {
-      if (props.method.attributes.name !== 'clave_unica') return null;
-      return <ClaveUnicaButton {...props} />;
+    'app.components.VerificationModal.buttons': ({ verificationMethods }) => {
+      const method = verificationMethods.find(
+        (vm) => vm.attributes.name === verificationMethodName
+      );
+
+      if (method) {
+        const last = isLastVerificationMethod(
+          verificationMethodName,
+          verificationMethods
+        );
+        return <ClaveUnicaButton last={last} method={method} />;
+      }
+
+      return null;
     },
   },
 };
