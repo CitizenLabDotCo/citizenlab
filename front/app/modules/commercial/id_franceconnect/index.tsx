391ea49dd67b8f340e9600dfdbf4cf66280b7c47
brentguf <brent.guffens@gmail.com>
2022-02-28 15:15:55 +0100

    france_connect

--- front/app/modules/commercial/id_franceconnect/index.tsx
+++ front/app/modules/commercial/id_franceconnect/index.tsx
@@ -2,12 +2,34 @@ import React from 'react';
 import { ModuleConfiguration } from 'utils/moduleUtils';
 import './services/verificationMethods';
 import VerificationFranceConnectButton from './components/VerificationFranceConnectButton';
+import { isLastVerificationMethod } from 'modules/commercial/verification';
 
+const verificationMethodName = 'franceconnect';
 const configuration: ModuleConfiguration = {
   outlets: {
-    'app.components.VerificationModal.buttons': (props) => {
-      if (props.method.attributes.name !== 'franceconnect') return null;
-      return <VerificationFranceConnectButton {...props} />;
+    'app.components.VerificationModal.buttons': ({
+      verificationMethods,
+      ...otherProps
+    }) => {
+      const method = verificationMethods.find(
+        (vm) => vm.attributes.name === verificationMethodName
+      );
+      if (method) {
+        const last = isLastVerificationMethod(
+          verificationMethodName,
+          verificationMethods
+        );
+
+        return (
+          <VerificationFranceConnectButton
+            method={method}
+            last={last}
+            {...otherProps}
+          />
+        );
+      }
+
+      return null;
     },
   },
 };
