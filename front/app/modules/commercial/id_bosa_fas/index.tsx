39c8ee85b80f84ef49dbcf87ce1c4effb543bca5
brentguf <brent.guffens@gmail.com>
2022-02-28 13:21:01 +0100

    bosa_fas

--- front/app/modules/commercial/id_bosa_fas/index.tsx
+++ front/app/modules/commercial/id_bosa_fas/index.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 import { ModuleConfiguration } from 'utils/moduleUtils';
 import './services/verificationMethods';
 import BosaFasButton from './components/BosaFasButton';
+import { isLastVerificationMethod } from 'modules/commercial/verification';
 
+const verificationMethodName = 'clave_unica';
 const configuration: ModuleConfiguration = {
   outlets: {
-    'app.components.VerificationModal.buttons': (props) => {
-      if (props.method.attributes.name !== 'bosa_fas') return null;
-      return <BosaFasButton {...props} />;
+    'app.components.VerificationModal.buttons': ({ verificationMethods }) => {
+      const method = verificationMethods.find(
+        (vm) => vm.attributes.name === verificationMethodName
+      );
+
+      if (method) {
+        const last = isLastVerificationMethod(
+          verificationMethodName,
+          verificationMethods
+        );
+        return <BosaFasButton last={last} method={method} />;
+      }
+
+      return null;
     },
   },
 };
