2c1b55b4123c9522d143a90095f855414fcdc991
Amanda Anderson <amanda.anderson@citizenlab.co>
2022-03-10 16:27:18 +0100

    Add toggle functionality

--- front/app/modules/commercial/content_builder/admin/components/contentBuilderToggle.tsx
+++ front/app/modules/commercial/content_builder/admin/components/contentBuilderToggle.tsx
@@ -1,8 +1,9 @@
 import QuillMultilocWithLocaleSwitcher from 'components/UI/QuillEditor/QuillMultilocWithLocaleSwitcher';
-import React from 'react';
+import React, { useState } from 'react';
 import { withRouter, WithRouterProps } from 'react-router';
 import { Locale, Multiloc } from 'typings';
 import Link from 'utils/cl-router/Link';
+import { Toggle } from '@citizenlab/cl2-component-library';
 
 type ContentBuilderToggleProps = {
   valueMultiloc: Multiloc | undefined | null;
@@ -18,18 +19,31 @@ const ContentBuilderToggle = ({
   label,
   labelTooltipText,
 }: ContentBuilderToggleProps) => {
+  const [contentBuilderLinkVisible, setContentBuilderLinkVisible] =
+    useState(false);
   const route = `${pathname}/content-builder`;
+  const toggleContentBuilderLinkVisible = () => {
+    setContentBuilderLinkVisible(!contentBuilderLinkVisible);
+  };
+
   return (
     <>
-      <Link to={route}>Advanced Editor</Link>
-      <QuillMultilocWithLocaleSwitcher
-        id="project-description"
-        valueMultiloc={valueMultiloc}
-        onChange={onChange}
-        label={label}
-        labelTooltipText={labelTooltipText}
-        withCTAButton
-      />
+      <Toggle
+        checked={contentBuilderLinkVisible}
+        label="Label"
+        onChange={toggleContentBuilderLinkVisible}
+      ></Toggle>
+      {contentBuilderLinkVisible && <Link to={route}>Advanced Editor</Link>}
+      {!contentBuilderLinkVisible && (
+        <QuillMultilocWithLocaleSwitcher
+          id="project-description"
+          valueMultiloc={valueMultiloc}
+          onChange={onChange}
+          label={label}
+          labelTooltipText={labelTooltipText}
+          withCTAButton
+        />
+      )}
     </>
   );
 };
