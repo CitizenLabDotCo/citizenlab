6f59c97a7714364ebeb6d512edd3de9b56db4286
Simon Tharby <simontharby@gmail.com>
2022-03-15 09:30:10 +0000

    CL-456 Merge latest branch version + remove redundant code

--- front/app/containers/Admin/messaging/texting/index.tsx
+++ front/app/containers/Admin/messaging/texting/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 //import { isUndefined } from 'lodash-es';
-import GetCampaigns, { GetCampaignsChildProps } from 'resources/GetCampaigns';
-import { ICampaignData, updateCampaign } from 'services/campaigns';
-import { isNilOrError } from 'utils/helperUtils';
+//import GetCampaigns, { GetCampaignsChildProps } from 'resources/GetCampaigns';
+//import { ICampaignData, updateCampaign } from 'services/campaigns';
+//import { isNilOrError } from 'utils/helperUtils';
 //import T from 'components/T';
 //import { Toggle } from '@citizenlab/cl2-component-library';
 import {
@@ -14,31 +14,12 @@ import {
 //import styled from 'styled-components';
 // i18n
 //import { FormattedMessage, injectIntl } from 'utils/cl-intl';
-import { injectIntl } from 'utils/cl-intl';
-import { InjectedIntlProps } from 'react-intl';
+// import { injectIntl } from 'utils/cl-intl';
+// import { InjectedIntlProps } from 'react-intl';
 //import messages from '../messages';
 
-// const StyledWarning = styled(Warning)`
-//   max-width: 600px;
-//   margin-bottom: 30px;
-// `;
-
-type DataProps = GetCampaignsChildProps;
-
-type Props = DataProps;
-
-class TextingCampaigns extends React.PureComponent<Props & InjectedIntlProps> {
-  handleOnEnabledToggle = (campaign: ICampaignData) => () => {
-    updateCampaign(campaign.id, {
-      enabled: !campaign.attributes.enabled,
-    });
-  };
-
+class TextingCampaigns extends React.PureComponent {
   render() {
-    const { campaigns } = this.props;
-
-    if (isNilOrError(campaigns)) return null;
-
     return (
       <>
         <TextsList>
@@ -49,10 +30,4 @@ class TextingCampaigns extends React.PureComponent<Props & InjectedIntlProps> {
   }
 }
 
-const TextingCampaignsWithIntl = injectIntl<Props>(TextingCampaigns);
-
-export default (inputProps: Props) => (
-  <GetCampaigns withoutCampaignNames={['manual']} pageSize={250}>
-    {(campaigns) => <TextingCampaignsWithIntl {...inputProps} {...campaigns} />}
-  </GetCampaigns>
-);
+export default TextingCampaigns;
