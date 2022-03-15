9f1fc03617b315d49a303d334ca92b5d974860b8
Simon Tharby <simontharby@gmail.com>
2022-03-15 09:39:13 +0000

    CL-456 Npm linting

--- front/app/containers/Admin/messaging/texting/index.tsx
+++ front/app/containers/Admin/messaging/texting/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-//import { isUndefined } from 'lodash-es';
-//import GetCampaigns, { GetCampaignsChildProps } from 'resources/GetCampaigns';
-//import { ICampaignData, updateCampaign } from 'services/campaigns';
-//import { isNilOrError } from 'utils/helperUtils';
-//import T from 'components/T';
-//import { Toggle } from '@citizenlab/cl2-component-library';
+// import { isUndefined } from 'lodash-es';
+// import GetCampaigns, { GetCampaignsChildProps } from 'resources/GetCampaigns';
+// import { ICampaignData, updateCampaign } from 'services/campaigns';
+// import { isNilOrError } from 'utils/helperUtils';
+// import T from 'components/T';
+// import { Toggle } from '@citizenlab/cl2-component-library';
 import {
   List as TextsList,
-  //Row,
-  //TextCell,
+  // Row,
+  // TextCell,
 } from 'components/admin/ResourceList';
-//import Warning from 'components/UI/Warning';
-//import styled from 'styled-components';
+// import Warning from 'components/UI/Warning';
+// import styled from 'styled-components';
 // i18n
-//import { FormattedMessage, injectIntl } from 'utils/cl-intl';
+// import { FormattedMessage, injectIntl } from 'utils/cl-intl';
 // import { injectIntl } from 'utils/cl-intl';
 // import { InjectedIntlProps } from 'react-intl';
-//import messages from '../messages';
+// import messages from '../messages';
 
 class TextingCampaigns extends React.PureComponent {
   render() {
