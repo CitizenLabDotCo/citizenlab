7455acad21be6ab06faac98d1e5df780ca93d2da
Amanda Anderson <amanda.anderson@citizenlab.co>
2022-03-15 09:00:25 +0100

    Add test for Quill component visibility

--- front/app/modules/commercial/content_builder/admin/components/ContentBuilderToggle.test.tsx
+++ front/app/modules/commercial/content_builder/admin/components/ContentBuilderToggle.test.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { WithRouterProps } from 'react-router';
+import { Multiloc } from 'typings';
 import { render, screen } from 'utils/testUtils/rtl';
 import ContentBuilderToggle from './ContentBuilderToggle';
 
-jest.mock('services/locale');
-jest.mock('services/avatars');
-jest.mock('utils/cl-intl');
 jest.mock('services/appConfiguration');
 jest.mock('utils/cl-router/history');
-jest.mock('components/UI/QuillEditor/QuillMultilocWithLocaleSwitcher');
-jest.mock('components/UI/QuillEditor');
-jest.mock('components/UI/QuillEditedContent', () => 'QuillEditedContent');
+jest.mock('hooks/useLocale', () => jest.fn(() => 'en'));
+jest.mock('hooks/useAppConfigurationLocales', () => jest.fn(() => ['en']));
 
 const getRouterProps = (projectId) =>
   ({
@@ -25,14 +22,15 @@ const getRouterProps = (projectId) =>
 const dummyFunction = () => {};
 
 const routerProps = getRouterProps('projectID');
+const multiloc = 'en' as Multiloc;
 
 describe('ContentBuilderToggle', () => {
   it('Confirm link shown appropriately when builder option toggled', () => {
     render(
       <ContentBuilderToggle
-        valueMultiloc={undefined}
+        valueMultiloc={multiloc}
         onChange={dummyFunction}
-        label={'Label'}
+        label={'QuillLabel'}
         labelTooltipText={'LabelTooltipText'}
         toggleLabel={'ToggleLabel'}
         toggleTooltipText={'ToggleTooltipText'}
@@ -41,10 +39,28 @@ describe('ContentBuilderToggle', () => {
         {...routerProps}
       />
     );
-    screen.debug();
     const toggle = screen.getByRole('checkbox');
     expect(screen.queryByText('LinkText')).not.toBeInTheDocument();
     toggle.click();
     expect(screen.queryByText('LinkText')).toBeInTheDocument();
+  });
+  it('Confirm Quill editor shown appropriately when builder option toggled', () => {
+    render(
+      <ContentBuilderToggle
+        valueMultiloc={multiloc}
+        onChange={dummyFunction}
+        label={'QuillLabel'}
+        labelTooltipText={'LabelTooltipText'}
+        toggleLabel={'ToggleLabel'}
+        toggleTooltipText={'ToggleTooltipText'}
+        linkText={'LinkText'}
+        onMount={dummyFunction}
+        {...routerProps}
+      />
+    );
+    const toggle = screen.getByRole('checkbox');
+    expect(screen.queryByText('QuillLabel')).toBeInTheDocument();
+    toggle.click();
+    expect(screen.queryByText('QuillLabel')).not.toBeInTheDocument();
   });
 });
