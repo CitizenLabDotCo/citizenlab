937eb972f4406f7450717bb8fc47051399b264a7
Amanda Anderson <amanda.anderson@citizenlab.co>
2022-03-16 15:29:33 +0100

    Update styling

--- front/app/modules/commercial/content_builder/admin/containers/index.tsx
+++ front/app/modules/commercial/content_builder/admin/containers/index.tsx
@@ -6,26 +6,28 @@ import Button from 'components/UI/Button';
 
 // Styling
 import styled from 'styled-components';
+import { colors } from 'utils/styleUtils';
+import { Box } from '@citizenlab/cl2-component-library';
 
-const TopContainer = styled.div`
+const TopContainer = styled(Box)`
   border-style: solid;
   border-width: 1px;
-  border-color: Gainsboro;
+  border-color: ${colors.separation};
   width: 100%;
   display: flex;
   background: white;
   align-items: center;
 `;
 
-const HeaderContainerLeft = styled.div`
+const HeaderContainerLeft = styled(Box)`
   padding: 15px;
   width: 210px;
 `;
 
-const HeaderContainerRight = styled.div`
+const HeaderContainerRight = styled(Box)`
   border-left-style: solid;
   border-left-width: 1px;
-  border-left-color: Gainsboro;
+  border-left-color: ${colors.separation};
   padding: 15px;
   flex-grow: 1;
   align-items: center;
@@ -33,16 +35,16 @@ const HeaderContainerRight = styled.div`
   gap: 10px;
 `;
 
-const ProjectDescriptionContainer = styled.div`
+const ProjectDescriptionContainer = styled(Box)`
   flex-grow: 2;
 `;
 
 const ProjectTitle = styled.p`
   margin-bottom: 8px;
-  color: Gray;
+  color: ${colors.adminSecondaryTextColor};
 `;
 
-const BuilderTitle = styled.h3`
+const BuilderTitle = styled.h1`
   margin: 0px;
 `;
 
@@ -50,28 +52,20 @@ const dummy = () => {};
 
 const ContentBuilderPage = () => {
   return (
-    <>
-      <TopContainer>
-        <HeaderContainerLeft>
-          <GoBackButton onClick={dummy} />
-        </HeaderContainerLeft>
-        <HeaderContainerRight>
-          <ProjectDescriptionContainer>
-            <ProjectTitle>An idea? Bring it to your council</ProjectTitle>
-            <BuilderTitle>Project Description</BuilderTitle>
-          </ProjectDescriptionContainer>
-          <Button
-            buttonStyle="primary"
-            disabled={false}
-            processing={false}
-            onClick={dummy}
-          >
-            Save
-          </Button>
-        </HeaderContainerRight>
-      </TopContainer>
-      <h1>Content Builder</h1>
-    </>
+    <TopContainer>
+      <HeaderContainerLeft>
+        <GoBackButton onClick={dummy} />
+      </HeaderContainerLeft>
+      <HeaderContainerRight>
+        <ProjectDescriptionContainer>
+          <ProjectTitle>An idea? Bring it to your council</ProjectTitle>
+          <BuilderTitle>Project Description</BuilderTitle>
+        </ProjectDescriptionContainer>
+        <Button buttonStyle="primary" onClick={dummy}>
+          Save
+        </Button>
+      </HeaderContainerRight>
+    </TopContainer>
   );
 };
 
