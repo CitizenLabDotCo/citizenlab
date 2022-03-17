fafee088f3eac2a010c36e6f0a2e050cc2c2fec3
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-03-17 10:06:11 +0000

    Fix broken unit tests in show.test.ts

--- front/app/components/ProjectAndFolderCards/components/Topbar/show.test.ts
+++ front/app/components/ProjectAndFolderCards/components/Topbar/show.test.ts
@@ -3,76 +3,96 @@ import { getShowFilters } from './show';
 describe('getShowFilters', () => {
   describe('desktop', () => {
     it('shows filters if there are admin publications', () => {
-      const result = getShowFilters(
-        false,
-        true,
-        { all: 2, published: 2 },
-        [],
-        []
-      );
+      const result = getShowFilters({
+        smallerThanXlPhone: false,
+        hasPublications: true,
+        statusCounts: { all: 2, published: 2 },
+        selectedTopics: [],
+        selectedAreas: [],
+      });
+
       expect(result).toBe(true);
     });
 
     it('does not show filters if there are no admin publications', () => {
-      const result = getShowFilters(false, false, { all: 0 }, [], []);
+      const result = getShowFilters({
+        smallerThanXlPhone: false,
+        hasPublications: false,
+        statusCounts: { all: 0 },
+        selectedTopics: [],
+        selectedAreas: [],
+      });
+
       expect(result).toBe(false);
     });
 
     it('always shows filters if any topics or areas selected', () => {
-      const result1 = getShowFilters(
-        false,
-        true,
-        { all: 2, published: 2 },
-        ['test'],
-        []
-      );
+      const result1 = getShowFilters({
+        smallerThanXlPhone: false,
+        hasPublications: true,
+        statusCounts: { all: 2, published: 2 },
+        selectedTopics: ['test'],
+        selectedAreas: [],
+      });
+
       expect(result1).toBe(true);
 
-      const result2 = getShowFilters(false, false, { all: 0 }, ['test'], []);
+      const result2 = getShowFilters({
+        smallerThanXlPhone: false,
+        hasPublications: false,
+        statusCounts: { all: 0 },
+        selectedTopics: ['test'],
+        selectedAreas: [],
+      });
+
       expect(result2).toBe(true);
     });
   });
 
   describe('phone', () => {
     it('shows filters if current tab has admin publications', () => {
-      const result = getShowFilters(
-        true,
-        true,
-        { all: 2, published: 2 },
-        [],
-        []
-      );
+      const result = getShowFilters({
+        smallerThanXlPhone: true,
+        hasPublications: true,
+        statusCounts: { all: 2, published: 2 },
+        selectedTopics: [],
+        selectedAreas: [],
+      });
+
       expect(result).toBe(true);
     });
 
     it('does not show filters if current tab no admin publications', () => {
-      const result = getShowFilters(
-        true,
-        false,
-        { all: 2, published: 2 },
-        [],
-        []
-      );
+      const result = getShowFilters({
+        smallerThanXlPhone: true,
+        hasPublications: false,
+        statusCounts: { all: 2, published: 2 },
+        selectedTopics: [],
+        selectedAreas: [],
+      });
+
       expect(result).toBe(false);
     });
 
     it('always shows filters if any topics or areas selected', () => {
-      const result1 = getShowFilters(
-        true,
-        true,
-        { all: 2, published: 2 },
-        ['test'],
-        []
-      );
+      const result1 = getShowFilters({
+        smallerThanXlPhone: true,
+        hasPublications: true,
+        statusCounts: { all: 2, published: 2 },
+        selectedTopics: ['test'],
+        selectedAreas: [],
+      });
+
       expect(result1).toBe(true);
 
-      const result2 = getShowFilters(
-        true,
-        false,
-        { all: 2, published: 2 },
-        ['test'],
-        []
-      );
+      const result2 = getShowFilters({
+        smallerThanXlPhone: true,
+        hasPublications: false,
+        statusCounts: { all: 2, published: 2 },
+        selectedTopics: ['test'],
+        selectedAreas: [],
+      });
+
       expect(result2).toBe(true);
     });
   });
