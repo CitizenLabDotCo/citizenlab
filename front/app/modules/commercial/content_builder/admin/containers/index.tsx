6ae2afc4c9a3236e24704948d27486751a88881b
Amanda Anderson <amanda.anderson@citizenlab.co>
2022-03-18 16:25:47 +0100

    Add padding

--- front/app/modules/commercial/content_builder/admin/containers/index.tsx
+++ front/app/modules/commercial/content_builder/admin/containers/index.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Frame, Element } from '@craftjs/core';
+import { Box } from '@citizenlab/cl2-component-library';
 
 const ContentBuilderPage = () => {
   return (
-    <div className="page-container">
-      <Frame>
-        <Element
-          is="div"
-          canvas
-          style={{
-            padding: '4px',
-            minHeight: '300px',
-            backgroundColor: '#fff',
-          }}
-        />
-      </Frame>
-    </div>
+    <Box paddingTop="100px">
+      <div className="page-container">
+        <Frame>
+          <Element
+            is="div"
+            canvas
+            style={{
+              padding: '4px',
+              minHeight: '300px',
+              backgroundColor: '#fff',
+            }}
+          />
+        </Frame>
+      </div>
+    </Box>
   );
 };
 
