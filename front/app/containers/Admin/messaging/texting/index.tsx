574f723522fda0b2071e5753fde6706c5f08f478
lucas <lucasquinnmathis@gmail.com>
2022-03-21 11:25:48 +0100

    [CL-456] Clean up placeholder page for upcoming texting feature

--- front/app/containers/Admin/messaging/texting/index.tsx
+++ front/app/containers/Admin/messaging/texting/index.tsx
@@ -1,29 +1,13 @@
 import React from 'react';
-// import { isUndefined } from 'lodash-es';
-// import GetCampaigns, { GetCampaignsChildProps } from 'resources/GetCampaigns';
-// import { ICampaignData, updateCampaign } from 'services/campaigns';
-// import { isNilOrError } from 'utils/helperUtils';
-// import T from 'components/T';
-// import { Toggle } from '@citizenlab/cl2-component-library';
-import {
-  List as TextsList,
-  // Row,
-  // TextCell,
-} from 'components/admin/ResourceList';
-// import Warning from 'components/UI/Warning';
-// import styled from 'styled-components';
-// i18n
-// import { FormattedMessage, injectIntl } from 'utils/cl-intl';
-// import { injectIntl } from 'utils/cl-intl';
-// import { InjectedIntlProps } from 'react-intl';
-// import messages from '../messages';
+
+import { List as TextsList } from 'components/admin/ResourceList';
 
 class TextingCampaigns extends React.PureComponent {
   render() {
     return (
       <>
         <TextsList>
-          <p>Imagine I am a list of texts.</p>
+          <p>Texting main page placeholder</p>
         </TextsList>
       </>
     );
