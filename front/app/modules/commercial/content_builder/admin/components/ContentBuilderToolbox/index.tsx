afb02994e6c79ac682a7d9298de9d3d6a61e142b
Amanda Anderson <amanda.anderson@citizenlab.co>
2022-03-23 10:22:18 +0100

    Add content builder icon list

--- front/app/modules/commercial/content_builder/admin/components/ContentBuilderToolbox/index.tsx
+++ front/app/modules/commercial/content_builder/admin/components/ContentBuilderToolbox/index.tsx
@@ -9,34 +9,92 @@ import Text from '../CraftComponents/Text';
 import { injectIntl } from 'utils/cl-intl';
 import { InjectedIntlProps } from 'react-intl';
 import messages from '../../messages';
-import { Box } from '@citizenlab/cl2-component-library';
+import { Box, Icon } from '@citizenlab/cl2-component-library';
+
+// Styling
+import styled from 'styled-components';
+import { colors } from 'utils/styleUtils';
+
+const ToolboxSection = styled(Box)`
+  border-style: solid;
+  border-right-style: none;
+  border-top-style: none;
+  border-width: 1px;
+  border-color: ${colors.border};
+  width: 100%;
+`;
+
+const ComponentToolboxItem = styled(Box)`
+  margin-bottom: 16px;
+  width: 100%;
+  display: flex;
+  gap: 20px;
+  padding-left: 10px;
+`;
+
+const ToolboxItemText = styled.p`
+  color: ${colors.adminDarkestBackground};
+  font-size: 15px;
+`;
+
+const ToolboxHeader = styled.h1`
+  padding-top: 10px;
+  font-size: 16px;
+  padding-left: 10px;
+  color: ${colors.adminDarkestBackground};
+`;
+
+const ToolboxIcon = styled(Icon)`
+  width: 18px;
+  height: 18px;
+`;
 
 const ContentBuilderToolbox = ({
   intl: { formatMessage },
 }: InjectedIntlProps) => {
   const { connectors } = useEditor();
   return (
-    <Box marginTop="20px">
-      <button
-        ref={(ref) =>
-          ref &&
-          connectors.create(
-            ref,
-            <Element canvas is={Container} id="container" />
-          )
-        }
-      >
-        {formatMessage(messages.oneColumn)}
-      </button>
-      <button
-        ref={(ref) =>
-          ref &&
-          connectors.create(ref, <Element text="Text" is={Text} id="text" />)
-        }
-      >
-        {formatMessage(messages.text)}
-      </button>
-    </Box>
+    <>
+      <ToolboxSection>
+        <ToolboxHeader>Sections</ToolboxHeader>
+        <ComponentToolboxItem
+          ref={(ref) =>
+            ref &&
+            connectors.create(
+              ref,
+              <Element canvas is={Container} id="container" />
+            )
+          }
+        >
+          <ToolboxIcon name="column1" />
+          <ToolboxItemText>{formatMessage(messages.oneColumn)}</ToolboxItemText>
+        </ComponentToolboxItem>
+        <ComponentToolboxItem
+          ref={(ref) =>
+            ref &&
+            connectors.create(
+              ref,
+              <Element canvas is={Container} id="container" />
+            )
+          }
+        >
+          <ToolboxIcon name="column2" />
+          <ToolboxItemText>{formatMessage(messages.twoColumn)}</ToolboxItemText>
+        </ComponentToolboxItem>
+      </ToolboxSection>
+      <ToolboxSection>
+        <ToolboxHeader>Elements</ToolboxHeader>
+        <ComponentToolboxItem
+          ref={(ref) =>
+            ref &&
+            connectors.create(ref, <Element text="Text" is={Text} id="text" />)
+          }
+        >
+          <ToolboxIcon name="text" />
+          <ToolboxItemText>{formatMessage(messages.text)}</ToolboxItemText>
+        </ComponentToolboxItem>
+      </ToolboxSection>
+    </>
   );
 };
 
