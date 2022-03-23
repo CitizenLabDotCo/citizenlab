a47991586b20afd28364130ec708fd9ff639bb75
Sebastien Hoorens <sebastien@citizenlab.co>
2022-03-23 17:25:07 +0100

    fixing some rubocop suggestions, to test if ticket number is now added to commit message

--- back/app/services/cl2_data_listing_service.rb
+++ back/app/services/cl2_data_listing_service.rb
@@ -19,28 +19,28 @@ class Cl2DataListingService
     # After https://github.com/rails/rails/issues/37006.
     # It seems that we need to use Zeitwerk::Loader.eager_load_all
     # instead of Rails.application.eager_load!.
-    Zeitwerk::Loader.eager_load_all if Rails.env.development? || (File.basename($0) == 'rake')
+    Zeitwerk::Loader.eager_load_all if Rails.env.development? || (File.basename($PROGRAM_NAME) == 'rake')
     views = ActiveRecord::Base.connection.execute(
       "SELECT table_name FROM information_schema.tables WHERE table_type = 'VIEW'"
-      ).map{|r| r['table_name']}
+    ).map { |r| r['table_name'] }
     ActiveRecord::Base.descendants.select do |claz|
-      ![
+      [
         *ActiveRecord::Base.subclasses.map(&:name),
         PublicApi::ApiClient.name,
         Tenant.name
-      ].include? claz.name
+      ].exclude? claz.name
     end.select do |claz|
-      !views.include? claz.table_name
+      views.exclude? claz.table_name
     end
   end
 
-  def timestamp_attributes model_class
+  def timestamp_attributes(model_class)
     model_class.column_names.select do |column_name|
       column_name.ends_with? '_at'
     end
   end
 
-  def multiloc_attributes model_class
+  def multiloc_attributes(model_class)
     model_class.column_names.select do |column_name|
       column_name.ends_with? '_multiloc'
     end
