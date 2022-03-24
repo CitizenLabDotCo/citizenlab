7789ede6e2b5cfce373bcf05cddc5b22cf220ca1
Amanda Anderson <amanda.anderson@citizenlab.co>
2022-03-24 14:56:19 +0100

    Add temp test for failing tests WIP

---
+++ front/app/modules/commercial/content_builder/admin/components/ContentBuilderToolbox/ContentBuilderToolbox.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'utils/testUtils/rtl';
+
+describe('ContentBuilderToolbox', () => {
+  it('dummy test', () => {
+    render(<h1>Temp</h1>);
+  });
+});
