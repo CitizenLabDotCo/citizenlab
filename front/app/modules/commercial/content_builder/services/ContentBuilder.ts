b30297192a727d38f0836decc0b96438172f55ba
Amanda Anderson <amanda.anderson@citizenlab.co>
2022-03-28 15:29:17 +0200

    [CL-539] [CL-539] Add data ID to update and delete

--- front/app/modules/commercial/content_builder/services/ContentBuilder.ts
+++ front/app/modules/commercial/content_builder/services/ContentBuilder.ts
@@ -1,17 +1,16 @@
 import { API_PATH } from 'containers/App/constants';
-import streams, { IStreamParams } from 'utils/streams';
+import streams from 'utils/streams';
 
 export interface IContentLayoutData {
-  // Should these be nested within an Attribute?
-  craftjs_jsonmultiloc: JSON;
-  content_buildable: {
-    type: 'project';
-    id: string;
+  type: 'project';
+  id: string;
+  attributes: {
+    craftjs_jsonmultiloc: JSON;
+    code: string;
+    enabled: boolean;
+    created_at: string;
+    updated_at: string;
   };
-  code: string;
-  enabled: boolean;
-  created_at: string;
-  updated_at: string;
 }
 
 export interface IContentLayout {
@@ -27,18 +26,23 @@ export function BuilderContentStream(projectId: string, code: string) {
 export function updateBuilderContent(
   projectId: string,
   code: string,
+  dataId: string,
   object: IContentLayout
 ) {
   return streams.update<IContentLayout>(
     `${API_PATH}/projects/${projectId}/content_builder_layouts/${code}/upsert`,
-    projectId,
+    dataId,
     { layout: object }
   );
 }
 
-export function deleteBuilderContent(projectId: string, code: string) {
+export function deleteBuilderContent(
+  projectId: string,
+  code: string,
+  dataId: string
+) {
   return streams.delete(
     `${API_PATH}/projects/${projectId}/content_builder_layouts/${code}`,
-    projectId
+    dataId
   );
 }
