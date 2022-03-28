4efce4905c7fb53329ca09490858485929c4112d
brentguf <brentguf@users.noreply.github.com>
2022-03-28 14:30:19 +0200

    Comment out ViennaSamlButton messages

--- front/app/modules/commercial/id_vienna_saml/components/messages.ts
+++ front/app/modules/commercial/id_vienna_saml/components/messages.ts
@@ -1,8 +1,10 @@
-import { defineMessages } from 'react-intl';
+// This button is temporarily commented out
+// https://citizenlab.atlassian.net/browse/CL-528
+// import { defineMessages } from 'react-intl';
 
-export default defineMessages({
-  continueWithStandardPortal: {
-    id: 'app.modules.commercial.id_vienna_saml.components.continueWithStandardPortal',
-    defaultMessage: 'Continue with StandardPortal',
-  },
-});
+// export default defineMessages({
+//   continueWithStandardPortal: {
+//     id: 'app.modules.commercial.id_vienna_saml.components.continueWithStandardPortal',
+//     defaultMessage: 'Continue with StandardPortal',
+//   },
+// });
