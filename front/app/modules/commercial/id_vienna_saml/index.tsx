5e886f65bf7f95ca5a12783bfbddbdd269f99ca1
brentguf <brentguf@users.noreply.github.com>
2022-03-28 13:10:16 +0200

    Comment out ViennaSamlButton

--- front/app/modules/commercial/id_vienna_saml/index.tsx
+++ front/app/modules/commercial/id_vienna_saml/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
+// import React from 'react';
 import { ModuleConfiguration } from 'utils/moduleUtils';
-import ViennaSamlButton from './components/ViennaSamlButton';
-import FeatureFlag from 'components/FeatureFlag';
+// import ViennaSamlButton from './components/ViennaSamlButton';
+// import FeatureFlag from 'components/FeatureFlag';
 
 const configuration: ModuleConfiguration = {
   outlets: {
-    'app.components.SignUpIn.AuthProviders.ContainerEnd': (props) => {
-      return (
-        <FeatureFlag name="vienna_login">
-          <ViennaSamlButton {...props} />
-        </FeatureFlag>
-      );
-    },
+    // 'app.components.SignUpIn.AuthProviders.ContainerEnd': (props) => {
+    //   return (
+    //     <FeatureFlag name="vienna_login">
+    //       <ViennaSamlButton {...props} />
+    //     </FeatureFlag>
+    //   );
+    // },
   },
 };
 
