08d172d676f9b0ea3fd69fc4ddfe9922b6e5d38c
Koen De Hondt <koen@dehondt.dev>
2022-03-29 08:42:33 +0200

    [CL-403] Use expect instead of allow to describe the contract between 2 classes

--- back/engines/commercial/content_builder/spec/policies/content_builder/layout_policy_spec.rb
+++ back/engines/commercial/content_builder/spec/policies/content_builder/layout_policy_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe ContentBuilder::LayoutPolicy do
   subject(:policy) { described_class.new(user, layout) }
 
   let(:layout) { create(:layout) }
+  let(:user_role_service) { policy.send(:user_role_service) }
 
   context 'for a visitor' do
     let(:user) { nil }
@@ -25,7 +26,7 @@ RSpec.describe ContentBuilder::LayoutPolicy do
     let(:user) { instance_double User, active?: true }
 
     before do
-      allow(policy.send(:user_role_service)).to receive(
+      allow(user_role_service).to receive(
         :can_moderate?
       ).with(
         layout.content_buildable,
@@ -37,16 +38,32 @@ RSpec.describe ContentBuilder::LayoutPolicy do
       let(:can_moderate) { false }
 
       it { is_expected.to permit(:show) }
-      it { is_expected.not_to permit(:upsert) }
-      it { is_expected.not_to permit(:destroy) }
+
+      it 'forbids upsert' do
+        expect(user_role_service).to receive(:can_moderate?)
+        expect(policy).not_to permit(:upsert)
+      end
+
+      it 'forbids destroy' do
+        expect(user_role_service).to receive(:can_moderate?)
+        expect(policy).not_to permit(:destroy)
+      end
     end
 
     context 'for moderators' do
       let(:can_moderate) { true }
 
       it { is_expected.to permit(:show) }
-      it { is_expected.to permit(:upsert) }
-      it { is_expected.to permit(:destroy) }
+
+      it 'permits upsert' do
+        expect(user_role_service).to receive(:can_moderate?)
+        expect(policy).to permit(:upsert)
+      end
+
+      it 'permits destroy' do
+        expect(user_role_service).to receive(:can_moderate?)
+        expect(policy).to permit(:destroy)
+      end
     end
   end
 end
