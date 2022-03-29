82e408919bfc56e0039fdcedac2fb08d45d1ad6d
brentguf <brentguf@users.noreply.github.com>
2022-03-29 09:53:21 +0200

    Vienna Saml button message comment

--- front/app/modules/commercial/id_vienna_saml/components/messages.ts
+++ front/app/modules/commercial/id_vienna_saml/components/messages.ts
@@ -2,6 +2,8 @@
 // https://citizenlab.atlassian.net/browse/CL-528
 // import { defineMessages } from 'react-intl';
 
+// If you uncomment this, the string needs to be translated again (in German only)
+
 // export default defineMessages({
 //   continueWithStandardPortal: {
 //     id: 'app.modules.commercial.id_vienna_saml.components.continueWithStandardPortal',
