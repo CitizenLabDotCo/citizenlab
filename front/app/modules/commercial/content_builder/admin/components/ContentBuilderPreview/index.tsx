5f35c18115ea453d576d6ce0416331bebc403023
Iva <iva@citizenlab.co>
2022-04-01 11:26:29 +0300

    [CL-404] Add project info to module

--- front/app/modules/commercial/content_builder/admin/components/ContentBuilderPreview/index.tsx
+++ front/app/modules/commercial/content_builder/admin/components/ContentBuilderPreview/index.tsx
@@ -2,16 +2,24 @@ import React, { useEffect } from 'react';
 import { withRouter, WithRouterProps } from 'react-router';
 
 // Hooks
+import useProject from 'hooks/useProject';
 import useFeatureFlag from 'hooks/useFeatureFlag';
 
 // Components
 import { Box } from '@citizenlab/cl2-component-library';
+import ProjectInfo from 'containers/ProjectsShowPage/shared/header/ProjectInfo';
+import { isNilOrError } from 'utils/helperUtils';
 
 type ContentBuilderPreviewProps = {
   onMount: () => void;
 } & WithRouterProps;
 
-const ContentBuilderPreview = ({ onMount }: ContentBuilderPreviewProps) => {
+const ContentBuilderPreview = ({
+  onMount,
+  params: { slug },
+}: ContentBuilderPreviewProps) => {
+  const project = useProject({ projectSlug: slug });
+
   const featureEnabled = useFeatureFlag({ name: 'content_builder' });
 
   useEffect(() => {
@@ -23,7 +31,14 @@ const ContentBuilderPreview = ({ onMount }: ContentBuilderPreviewProps) => {
     return null;
   }
 
-  return <Box>preview</Box>;
+  if (isNilOrError(project)) return null;
+
+  return (
+    <Box>
+      <div>preview</div>
+      <ProjectInfo projectId={project.id} />
+    </Box>
+  );
 };
 
 export default withRouter(ContentBuilderPreview);
