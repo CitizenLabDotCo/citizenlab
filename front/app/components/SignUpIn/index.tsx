d0faf3c86d444141058433b189052065583c53d2
brentguf <brentguf@users.noreply.github.com>
2022-04-04 18:28:03 +0200

    Various small component simplifications

--- front/app/components/SignUpIn/index.tsx
+++ front/app/components/SignUpIn/index.tsx
@@ -1,22 +1,19 @@
-import React, { memo } from 'react';
+import React from 'react';
 
 // hooks
 import useAppConfiguration from 'hooks/useAppConfiguration';
-import { useWindowSize } from '@citizenlab/cl2-component-library';
+import { useWindowSize, Box } from '@citizenlab/cl2-component-library';
 
 // component
-import SignUp from 'components/SignUpIn/SignUp';
-import SignIn from 'components/SignUpIn/SignIn';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
 
 // styling
-import styled from 'styled-components';
 import { ContextShape } from 'components/Verification/verificationModalEvents';
 
 // utils
 import { isNilOrError } from 'utils/helperUtils';
-import { openSignUpInModal } from 'components/SignUpIn/events';
-
-const Container = styled.div``;
+import { openSignUpInModal } from './events';
 
 export type TSignUpInFlow = 'signup' | 'signin';
 
@@ -51,48 +48,49 @@ function getNewFlow(flow: TSignUpInFlow) {
   }
 }
 
-const SignUpIn = memo<Props>(
-  ({
-    metaData,
-    customSignInHeader,
-    customSignUpHeader,
-    onSignUpInCompleted,
-    className,
+const SignUpIn = ({
+  metaData,
+  customSignInHeader,
+  customSignUpHeader,
+  onSignUpInCompleted,
+  className,
+}: Props) => {
   const appConfiguration = useAppConfiguration();
+  const { windowHeight } = useWindowSize();
 
-    const onToggleSelectedMethod = () => {
-      const flow = getNewFlow(metaData.flow);
-      openSignUpInModal({
-        ...metaData,
-        flow,
-      });
-    };
+  const onToggleSelectedMethod = () => {
+    const flow = getNewFlow(metaData.flow);
+    openSignUpInModal({
+      ...metaData,
+      flow,
+    });
+  };
 
   if (!isNilOrError(appConfiguration)) {
-        <Container className={className}>
-          {metaData.flow === 'signup' ? (
-            <SignUp
-              metaData={metaData}
-              windowHeight={windowHeight}
-              customHeader={customSignUpHeader}
-              onSignUpCompleted={onSignUpInCompleted}
-              onGoToSignIn={onToggleSelectedMethod}
-            />
-          ) : (
-            <SignIn
-              metaData={metaData}
-              windowHeight={windowHeight}
-              customHeader={customSignInHeader}
-              onSignInCompleted={onSignUpInCompleted}
-              onGoToSignUp={onToggleSelectedMethod}
-            />
-          )}
-        </Container>
-      );
-    }
-
-    return null;
+    return (
+      <Box className={className}>
+        {metaData.flow === 'signup' ? (
+          <SignUp
+            metaData={metaData}
+            windowHeight={windowHeight}
+            customHeader={customSignUpHeader}
+            onSignUpCompleted={onSignUpInCompleted}
+            onGoToSignIn={onToggleSelectedMethod}
+          />
+        ) : (
+          <SignIn
+            metaData={metaData}
+            windowHeight={windowHeight}
+            customHeader={customSignInHeader}
+            onSignInCompleted={onSignUpInCompleted}
+            onGoToSignUp={onToggleSelectedMethod}
+          />
+        )}
+      </Box>
+    );
   }
-);
+
+  return null;
+};
 
 export default SignUpIn;
