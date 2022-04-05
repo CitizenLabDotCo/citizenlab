4fca4645b60b0f65160837167adc60f6e8ec0daa
Simon Tharby <simontharby@gmail.com>
2022-04-05 10:56:21 +0100

    CL-476 Use Box not styled main component

--- front/app/containers/EventsPage/index.tsx
+++ front/app/containers/EventsPage/index.tsx
@@ -6,11 +6,12 @@ import SectionContainer from 'components/SectionContainer';
 import ContentContainer from 'components/ContentContainer';
 import UpcomingEvents from './UpcomingEvents';
 import PastEvents from './PastEvents';
+import { Box } from '@citizenlab/cl2-component-library';
 
 // styling
 import styled from 'styled-components';
 
-const Container = styled.main``;
+//const Container = styled.main``;
 
 const StyledContentContainer = styled(ContentContainer)`
   max-width: calc(${(props) => props.theme.maxPageWidth}px - 100px);
@@ -22,13 +23,13 @@ export default () => (
   <>
     <EventsPageMeta />
 
-    <Container>
+    <Box as="main">
       <SectionContainer>
         <StyledContentContainer>
           <UpcomingEvents />
           <PastEvents />
         </StyledContentContainer>
       </SectionContainer>
-    </Container>
+    </Box>
   </>
 );
