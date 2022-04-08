d5dd1da1db5a1b148e5b0f7a6d3f8866cb4736b0
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-04-08 18:24:56 +0200

    _

--- front/app/modules/commercial/user_custom_fields/admin/components/CustomFieldGraphs/index.tsx
+++ front/app/modules/commercial/user_custom_fields/admin/components/CustomFieldGraphs/index.tsx
@@ -10,36 +10,54 @@ import { isNilOrError } from 'utils/helperUtils';
 // hooks
 import useUserCustomFields from '../../../hooks/useUserCustomFields';
 
+// typings
+import { ParticipationMethod } from 'services/participationContexts';
+import { IProjectData } from 'services/projects';
+
+interface Props {
+  startAt: string;
+  endAt: string;
+  participationMethods: ParticipationMethod[];
+  project: IProjectData;
+}
+
 const CustomFieldGraphs = ({
   participationMethods,
   startAt,
   endAt,
   project,
-}) => {
+}: Props) => {
   const userCustomFields = useUserCustomFields({
     inputTypes: ['select', 'multiselect', 'checkbox', 'number'],
   });
 
-  return (
+  if (
     participationMethods !== ['information'] &&
     startAt &&
     endAt &&
-    !isNilOrError(userCustomFields) &&
-    userCustomFields.map(
-      (customField) =>
-        // only show enabled fields, only supported number field is birthyear.
-        customField.attributes.enabled &&
-        (customField.attributes.input_type === 'number'
-          ? customField.attributes.code === 'birthyear'
-          : true) && (
-          <CustomFieldGraph
-            customField={customField}
-            currentProject={project.id}
-            key={customField.id}
-          />
-        )
-    )
-  );
+    !isNilOrError(userCustomFields)
+  ) {
+    return (
+      <>
+        {userCustomFields.map(
+          (customField) =>
+            // only show enabled fields, only supported number field is birthyear.
+            customField.attributes.enabled &&
+            (customField.attributes.input_type === 'number'
+              ? customField.attributes.code === 'birthyear'
+              : true) && (
+              <CustomFieldGraph
+                customField={customField}
+                currentProject={project.id}
+                key={customField.id}
+              />
+            )
+        )}
+      </>
+    );
+  }
+
+  return null;
 };
 
 export default CustomFieldGraphs;
