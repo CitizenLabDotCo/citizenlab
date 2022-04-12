76853d37539cb60542e38064466117ff2bfdab4f
Sebastien Hoorens <sebastien@citizenlab.co>
2022-04-12 11:26:43 +0200

    adding options to error request matcher

--- back/spec/support/json_response_matchers.rb
+++ back/spec/support/json_response_matchers.rb
@@ -1,5 +1,10 @@
-RSpec::Matchers.define :include_response_error do |attribute, error_key|
+RSpec::Matchers.define :include_response_error do |attribute, error_key, options = nil|
   match do |response|
-    response.dig(:errors, attribute)&.select { |error| error[:error].to_s == error_key.to_s }.present?
+    matching_error = response.dig(:errors, attribute)&.find do |error|
+      error[:error].to_s == error_key.to_s
+    end
+    return false if !matching_error
+
+    !options || matching_error.except(:error) == options
   end
 end
