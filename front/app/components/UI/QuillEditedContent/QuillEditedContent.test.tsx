66a6b7eb6978f72c014347985efc49b44f64dbd4
lucas <lucasquinnmathis@gmail.com>
2022-04-12 18:21:39 +0200

    [CL-482] Add test for QuillEditedContent tabindex functionality

---
+++ front/app/components/UI/QuillEditedContent/QuillEditedContent.test.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { render, screen, waitFor } from 'utils/testUtils/rtl';
+import QuillEditedContent from '.';
+import 'jest-styled-components';
+
+describe('QuillEditedContent component', () => {
+  it('Should change tabIndex on children properly', async () => {
+    // multiple rerenders to simulate a user showing/hiding the contentnt multiple times
+
+    const { rerender } = render(
+      <QuillEditedContent>
+        <a data-testid="link">link</a>
+        <button data-testid="button">button</button>
+        <iframe data-testid="iframe">
+          <div>iframe content</div>
+        </iframe>
+      </QuillEditedContent>
+    );
+
+    rerender(
+      <QuillEditedContent disableTabbing={false}>
+        <a data-testid="link">link</a>
+        <button data-testid="button">button</button>
+        <iframe data-testid="iframe">
+          <div>iframe content</div>
+        </iframe>
+      </QuillEditedContent>
+    );
+
+    await waitFor(() => {
+      expect(screen.getByTestId('link')).toHaveAttribute('tabindex', '0');
+      expect(screen.getByTestId('button')).toHaveAttribute('tabindex', '0');
+      expect(screen.getByTestId('iframe')).toHaveAttribute('tabindex', '0');
+    });
+
+    rerender(
+      <QuillEditedContent disableTabbing={true}>
+        <a data-testid="link">link</a>
+        <button data-testid="button">button</button>
+        <iframe data-testid="iframe">
+          <div>iframe content</div>
+        </iframe>
+      </QuillEditedContent>
+    );
+
+    await waitFor(() => {
+      expect(screen.getByTestId('link')).toHaveAttribute('tabindex', '-1');
+      expect(screen.getByTestId('button')).toHaveAttribute('tabindex', '-1');
+      expect(screen.getByTestId('iframe')).toHaveAttribute('tabindex', '-1');
+    });
+
+    rerender(
+      <QuillEditedContent disableTabbing={false}>
+        <a data-testid="link">link</a>
+        <button data-testid="button">button</button>
+        <iframe data-testid="iframe">
+          <div>iframe content</div>
+        </iframe>
+      </QuillEditedContent>
+    );
+
+    await waitFor(() => {
+      expect(screen.getByTestId('link')).toHaveAttribute('tabindex', '0');
+      expect(screen.getByTestId('button')).toHaveAttribute('tabindex', '0');
+      expect(screen.getByTestId('iframe')).toHaveAttribute('tabindex', '0');
+    });
+  });
+});
