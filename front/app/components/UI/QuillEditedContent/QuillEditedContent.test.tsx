359ff90a5f152dcbc983062db21cdf4b70847a2e
lucas <lucasquinnmathis@gmail.com>
2022-04-13 10:44:42 +0200

    [CL-482] Update QuillEditedContent test

--- front/app/components/UI/QuillEditedContent/QuillEditedContent.test.tsx
+++ front/app/components/UI/QuillEditedContent/QuillEditedContent.test.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { render, screen, waitFor } from 'utils/testUtils/rtl';
 import QuillEditedContent from '.';
-import 'jest-styled-components';
 
 describe('QuillEditedContent component', () => {
   it('Should change tabIndex on children properly', async () => {
     // multiple rerenders to simulate a user showing/hiding the contentnt multiple times
 
     const { rerender } = render(
-      <QuillEditedContent>
-        <a href="www.google.com" data-testid="link">
-          link
-        </a>
-        <button data-testid="button">button</button>
-        <iframe data-testid="iframe">
+      <QuillEditedContent disableTabbing={true}>
+        <a href="www.google.com">link</a>
+        <button>button</button>
+        <iframe title="youtube embed">
           <div>iframe content</div>
         </iframe>
       </QuillEditedContent>
@@ -21,56 +18,59 @@ describe('QuillEditedContent component', () => {
 
     rerender(
       <QuillEditedContent disableTabbing={false}>
-        <a href="www.google.com" data-testid="link">
-          link
-        </a>
-        <button data-testid="button">button</button>
-        <iframe data-testid="iframe">
+        <a href="www.google.com">link</a>
+        <button>button</button>
+        <iframe title="youtube embed">
           <div>iframe content</div>
         </iframe>
       </QuillEditedContent>
     );
 
     await waitFor(() => {
-      expect(screen.getByTestId('link')).toHaveAttribute('tabindex', '0');
-      expect(screen.getByTestId('button')).toHaveAttribute('tabindex', '0');
-      expect(screen.getByTestId('iframe')).toHaveAttribute('tabindex', '0');
+      expect(screen.getByRole('link')).toHaveAttribute('tabindex', '0');
+      expect(screen.getByRole('button')).toHaveAttribute('tabindex', '0');
+      expect(screen.getByTitle('youtube embed')).toHaveAttribute(
+        'tabindex',
+        '0'
+      );
     });
 
     rerender(
       <QuillEditedContent disableTabbing={true}>
-        <a href="www.google.com" data-testid="link">
-          link
-        </a>
-        <button data-testid="button">button</button>
-        <iframe data-testid="iframe">
+        <a href="www.google.com">link</a>
+        <button>button</button>
+        <iframe title="youtube embed">
           <div>iframe content</div>
         </iframe>
       </QuillEditedContent>
     );
 
     await waitFor(() => {
-      expect(screen.getByTestId('link')).toHaveAttribute('tabindex', '-1');
-      expect(screen.getByTestId('button')).toHaveAttribute('tabindex', '-1');
-      expect(screen.getByTestId('iframe')).toHaveAttribute('tabindex', '-1');
+      expect(screen.getByRole('link')).toHaveAttribute('tabindex', '-1');
+      expect(screen.getByRole('button')).toHaveAttribute('tabindex', '-1');
+      expect(screen.getByTitle('youtube embed')).toHaveAttribute(
+        'tabindex',
+        '-1'
+      );
     });
 
     rerender(
       <QuillEditedContent disableTabbing={false}>
-        <a href="www.google.com" data-testid="link">
-          link
-        </a>
-        <button data-testid="button">button</button>
-        <iframe data-testid="iframe">
+        <a href="www.google.com">link</a>
+        <button>button</button>
+        <iframe title="youtube embed">
           <div>iframe content</div>
         </iframe>
       </QuillEditedContent>
     );
 
     await waitFor(() => {
-      expect(screen.getByTestId('link')).toHaveAttribute('tabindex', '0');
-      expect(screen.getByTestId('button')).toHaveAttribute('tabindex', '0');
-      expect(screen.getByTestId('iframe')).toHaveAttribute('tabindex', '0');
+      expect(screen.getByRole('link')).toHaveAttribute('tabindex', '0');
+      expect(screen.getByRole('button')).toHaveAttribute('tabindex', '0');
+      expect(screen.getByTitle('youtube embed')).toHaveAttribute(
+        'tabindex',
+        '0'
+      );
     });
   });
 });
