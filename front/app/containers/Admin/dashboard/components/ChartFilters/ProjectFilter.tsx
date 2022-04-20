dafce2a8c85a5afee0eda32950d5e478bb4c0e3e
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-04-20 17:25:07 +0100

    Create ProjectFilter

--- front/app/containers/Admin/dashboard/components/ChartFilters/ProjectFilter.tsx
+++ front/app/containers/Admin/dashboard/components/ChartFilters/ProjectFilter.tsx
@@ -1,59 +1,109 @@
-// import React from 'react';
+import React from 'react';
 
-// // resources
-// import GetProjects, {
-//   GetProjectsChildProps,
-//   PublicationStatus,
-// } from 'resources/GetProjects';
+// resources
+import GetProjects, {
+  GetProjectsChildProps,
+  PublicationStatus,
+} from 'resources/GetProjects';
 
-// // components
-// import { Box, Select } from '@citizenlab/cl2-component-library';
-// import { HiddenLabel } from 'utils/a11y';
+// hooks
+import useLocalize, { Localize } from 'hooks/useLocalize';
 
-// // typings
-// import { IOption } from 'typings';
+// components
+import { Box, Select } from '@citizenlab/cl2-component-library';
+import { HiddenLabel } from 'utils/a11y';
 
-// // i18n
-// import { FormattedMessage } from 'utils/cl-intl';
-// import messages from '../../messages';
+// typings
+import { IOption } from 'typings';
+import { IProjectData } from 'services/projects';
 
-// interface DataProps {
-//   projects: any;
-// }
+// i18n
+import { injectIntl } from 'utils/cl-intl';
+import { InjectedIntlProps } from 'react-intl';
+import messages from '../../messages';
 
-// interface InputProps {
-//   currentProjectFilter?: string | null;
-//   onProjectFilter: ((filter: IOption) => void);
-// }
+// utils
+import { isNilOrError } from 'utils/helperUtils';
 
-// interface Props extends DataProps, InputProps {}
+interface DataProps {
+  projects: GetProjectsChildProps;
+}
 
-// const ProjectFilter = ({
-//   projects,
-//   currentProjectFilter,
-//   onProjectFilter,
-// }: Props) => {
-//   const projectFilterOptions = projects; // TODO
+interface InputProps {
+  currentProjectFilter?: string | null;
+  onlyModerator?: boolean;
+  onProjectFilter: (filter: IOption) => void;
+}
 
-//   return (
-//     <Box width="32%">
-//       <HiddenLabel>
-//         <FormattedMessage {...messages.hiddenLabelProjectFilter} />
-//         <Select
-//           id="projectFilter"
-//           onChange={onProjectFilter}
-//           value={currentProjectFilter || ''}
-//           options={projectFilterOptions}
-//         />
-//       </HiddenLabel>
-//     </Box>
-//   )
-// }
+interface Props extends DataProps, InputProps {}
 
-// export default (props: InputProps) => (
-//   <GetProjects>
-//     {() => (
-//       <ProjectFilter {...props} />
-//     )}
-//   </GetProjects>
-// );
+const generateProjectOptions = (
+  projectsList: IProjectData[],
+  onlyModerator: boolean | undefined,
+  localize: Localize,
+  formatMessage: InjectedIntlProps['intl']['formatMessage']
+): IOption[] => {
+  let projectOptions = projectsList.map((project) => ({
+    value: project.id,
+    label: localize(project.attributes.title_multiloc),
+  }));
+
+  if (!onlyModerator) {
+    projectOptions = [
+      { value: '', label: formatMessage(messages.allProjects) },
+      ...projectOptions,
+    ];
+  }
+
+  return projectOptions;
+};
+
+const ProjectFilter = ({
+  projects: { projectsList },
+  currentProjectFilter,
+  onlyModerator,
+  onProjectFilter,
+  intl: { formatMessage },
+}: Props & InjectedIntlProps) => {
+  const localize = useLocalize();
+
+  if (isNilOrError(projectsList)) return null;
+
+  const projectFilterOptions = generateProjectOptions(
+    projectsList,
+    onlyModerator,
+    localize,
+    formatMessage
+  );
+
+  return (
+    <Box width="32%">
+      <HiddenLabel>
+        {formatMessage(messages.hiddenLabelProjectFilter)}
+        <Select
+          id="projectFilter"
+          onChange={onProjectFilter}
+          value={currentProjectFilter || ''}
+          options={projectFilterOptions}
+        />
+      </HiddenLabel>
+    </Box>
+  );
+};
+
+const publicationStatuses: PublicationStatus[] = [
+  'draft',
+  'published',
+  'archived',
+];
+
+const ProjectFilterWithIntl = injectIntl(ProjectFilter);
+
+export default (props: InputProps) => (
+  <GetProjects
+    publicationStatuses={publicationStatuses}
+    filterCanModerate={true}
+  >
+    {(projects) => <ProjectFilterWithIntl projects={projects} {...props} />}
+  </GetProjects>
+);
