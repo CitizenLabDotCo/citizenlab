73abbd3105578182dfe16fd27626e4860d660330
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-04-20 11:20:28 +0100

    Refactor SelectableResourceByTopicChart

--- front/app/containers/Admin/dashboard/summary/charts/SelectableResourceByTopicChart.tsx
+++ front/app/containers/Admin/dashboard/summary/charts/SelectableResourceByTopicChart.tsx
@@ -1,15 +1,12 @@
 // libraries
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { map, orderBy } from 'lodash-es';
 
 // components
 import SelectableResourceChart from './SelectableResourceChart';
 
-// i18n
-import messages from '../../messages';
-import { injectIntl } from 'utils/cl-intl';
-import { InjectedIntlProps } from 'react-intl';
-import localize, { InjectedLocalized } from 'utils/localize';
+// hooks
+import useLocalize from 'hooks/useLocalize';
 
 // typings
 import {
@@ -22,7 +19,7 @@ import {
 } from 'services/stats';
 import { IResource } from '..';
 import { IResolution } from 'components/admin/ResolutionControl';
-import { IGraphFormat, IOption } from 'typings';
+import { IOption } from 'typings';
 
 interface QueryProps {
   startAt: string | null | undefined;
@@ -37,7 +34,6 @@ interface InputProps {
   onResourceByTopicChange: (option: IOption) => void;
   currentResourceByTopic: IResource;
   resourceOptions: IOption[];
-  topicOptions: IOption[];
   startAt: string | null | undefined;
   endAt: string | null;
   resolution: IResolution;
@@ -49,14 +45,28 @@ interface InputProps {
 
 interface Props extends InputProps, QueryProps {}
 
-interface PropsWithHoCs extends Props, InjectedIntlProps, InjectedLocalized {}
+const getCurrentStream = (currentResourceByTopic: IResource) => {
+  if (currentResourceByTopic === 'ideas') {
+    return ideasByTopicStream;
+  } else if (currentResourceByTopic === 'comments') {
+    return commentsByTopicStream;
+  } else {
+    return votesByTopicStream;
+  }
+};
 
-class SelectableResourceByTopicChart extends PureComponent<PropsWithHoCs> {
-  convertToGraphFormat = (
+const SelectableResourceByTopicChart = ({
+  currentResourceByTopic,
+  onResourceByTopicChange,
+  currentTopicFilter,
+  ...otherProps
+}: Props) => {
+  const localize = useLocalize();
+
+  const convertToGraphFormat = (
     data: IIdeasByTopic | IVotesByTopic | ICommentsByTopic
   ) => {
     const { series, topics } = data;
-    const { localize, currentResourceByTopic } = this.props;
     const dataKey =
       currentResourceByTopic === 'votes' ? 'total' : currentResourceByTopic;
 
@@ -70,81 +80,18 @@ class SelectableResourceByTopicChart extends PureComponent<PropsWithHoCs> {
     return sortedByValue.length > 0 ? sortedByValue : null;
   };
 
-  convertSerie = (serie: IGraphFormat | null) => {
-    const {
-      currentTopicFilter,
-      currentResourceByTopic,
-      topicOptions,
-      intl: { formatMessage },
-    } = this.props;
+  return (
+    <SelectableResourceChart
+      onResourceByXChange={onResourceByTopicChange}
+      currentSelectedResource={currentResourceByTopic}
+      stream={getCurrentStream(currentResourceByTopic)}
+      convertToGraphFormat={convertToGraphFormat}
+      currentFilter={currentTopicFilter}
+      currentTopicFilter={currentTopicFilter}
+      byWhat="Topic"
+      {...otherProps}
+    />
+  );
+};
 
-    if (serie && currentResourceByTopic) {
-      const selectedTopic = serie.find(
-        (item) => item.code === currentTopicFilter
-      );
-      const selectedCount = selectedTopic ? selectedTopic.value : 0;
-
-      let selectedName;
-      if (selectedTopic) {
-        selectedName = selectedTopic.name;
-      } else {
-        const foundOption = topicOptions.find(
-          (option) => option.value === currentTopicFilter
-        );
-        selectedName = foundOption
-          ? foundOption.label
-          : formatMessage(messages.selectedTopic);
-      }
-
-      const convertedSerie = serie
-        .filter((item) => item.code !== currentTopicFilter)
-        .map((item) => {
-          const { value, name, ...rest } = item;
-          const shortenedName =
-            name.length > 60 ? `${name.substring(0, 61)}...` : name;
-          return { value: value - selectedCount, name: shortenedName, ...rest };
-        });
-
-      return { convertedSerie, selectedCount, selectedName };
-    }
-
-    return { convertedSerie: serie, selectedCount: null, selectedName: null };
-  };
-
-  getCurrentStream(currentResourceByTopic) {
-    if (currentResourceByTopic === 'ideas') {
-      return ideasByTopicStream;
-    } else if (currentResourceByTopic === 'comments') {
-      return commentsByTopicStream;
-    } else {
-      return votesByTopicStream;
-    }
-  }
-
-  render() {
-    const {
-      currentResourceByTopic,
-      currentTopicFilter,
-      onResourceByTopicChange,
-    } = this.props;
-
-    return (
-      <SelectableResourceChart
-        {...this.props}
-        onResourceByXChange={onResourceByTopicChange}
-        currentSelectedResource={currentResourceByTopic}
-        stream={this.getCurrentStream(currentResourceByTopic)}
-        convertToGraphFormat={this.convertToGraphFormat}
-        convertSerie={this.convertSerie}
-        currentFilter={currentTopicFilter}
-        byWhat="Topic"
-      />
-    );
-  }
-}
-
-export default localize<Props>(
-  injectIntl<Props & InjectedLocalized>(
-    SelectableResourceByTopicChart as any
-  ) as any
-);
+export default SelectableResourceByTopicChart;
