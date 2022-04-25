cbeb4c9f175bbbc0ec7bd404ac458dfb794ae8f3
Amanda Anderson <amanda.anderson@citizenlab.co>
2022-04-25 16:42:18 +0200

    [CL-632] [CL-632] Replace import path

--- front/app/modules/commercial/content_builder/admin/components/CraftComponents/AboutBox/index.tsx
+++ front/app/modules/commercial/content_builder/admin/components/CraftComponents/AboutBox/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // components
-import ProjectInfoSideBar from '/home/aander-cl/CitizenLabLocal/citizenlab/front/app/containers/ProjectsShowPage/shared/header/ProjectInfoSideBar';
+import ProjectInfoSideBar from '../../../../../../../../../front/app/containers/ProjectsShowPage/shared/header/ProjectInfoSideBar';
 import { Box } from '@citizenlab/cl2-component-library';
 import useProject from 'hooks/useProject';
 import { isNil } from 'lodash-es';
