d42206f2807c7a95daede4abab5a36a58618b188
lucas <lucasquinnmathis@gmail.com>
2022-04-26 10:29:34 +0200

    [CL-152] Update dateUtils helper functions and tests

--- front/app/utils/dateUtils.test.ts
+++ front/app/utils/dateUtils.test.ts
@@ -1,13 +1,17 @@
-import { pastPresentOrFuture, getIsoDate } from './dateUtils';
+import {
+  pastPresentOrFuture,
+  getIsoDate,
+  getIsoDateForToday,
+} from './dateUtils';
 import moment from 'moment';
 import 'moment-timezone';
 
-// test date is 1am June 15 UTC time
+// test date is 1AM, June 15, UTC time (Z)
 const testDateStr = '2020-06-15T01:00:00Z';
 
-// the local time in Santiago should be on the 14th, since they are -0400
-// hour offset from UTC time
-describe('in Santiago time zone', () => {
+// the local time in Tokyo should be June 14th 2022, 9pm,
+// since Tokyo is -0400 hour offset from UTC time
+describe('in America/Santiago time zone', () => {
   beforeAll(() => {
     jest.useFakeTimers('modern');
     jest.setSystemTime(new Date(testDateStr));
@@ -18,63 +22,78 @@ describe('in Santiago time zone', () => {
     jest.useRealTimers();
   });
 
-  describe('getISODate', () => {
+  describe('general time safety checks', () => {
+    it('gets the day and time correctly for the America/Santiago timezone', () => {
+      expect(
+        new Date().toLocaleString('en-US', { timeZone: 'America/Santiago' })
+      ).toBe('6/14/2020, 9:00:00 PM');
+    });
+  });
+
+  describe('getIsoDate', () => {
     it('returns the correct date formatted', () => {
-      const result = getIsoDate('2020-06-15');
-      expect(result).toEqual('2020-06-15');
+      expect(getIsoDate('2020-06-15')).toEqual('2020-06-15');
+    });
+  });
+
+  describe('getIsoDateForToday', () => {
+    it('returns the correct date formatted', () => {
+      expect(getIsoDateForToday()).toEqual('2020-06-14');
     });
   });
 
   describe('pastPresentOrFuture', () => {
     describe('with a single input date', () => {
       it('should accurately report a past date', () => {
-        const result = pastPresentOrFuture('2020-06-13');
-        expect(result).toEqual('past');
+        expect(pastPresentOrFuture('2020-06-13')).toEqual('past');
       });
 
       it('should accurately report a present date', () => {
-        const result = pastPresentOrFuture('2020-06-14');
-        expect(result).toEqual('present');
+        expect(pastPresentOrFuture('2020-06-14')).toEqual('present');
       });
 
       it('should accurately report a future date', () => {
-        const result = pastPresentOrFuture('2020-06-15');
-        expect(result).toEqual('future');
+        expect(pastPresentOrFuture('2020-06-15')).toEqual('future');
       });
     });
 
     describe('with an input date range', () => {
       it('should accurately report a past date', () => {
-        const result = pastPresentOrFuture(['2020-06-10', '2020-06-13']);
-        expect(result).toEqual('past');
+        expect(pastPresentOrFuture(['2020-06-10', '2020-06-13'])).toEqual(
+          'past'
+        );
       });
 
       it('should accurately report a present date when in range', () => {
-        const result = pastPresentOrFuture(['2020-06-10', '2020-06-20']);
-        expect(result).toEqual('present');
+        expect(pastPresentOrFuture(['2020-06-10', '2020-06-20'])).toEqual(
+          'present'
+        );
       });
 
       it('should accurately report a present date, including start date', () => {
-        const result = pastPresentOrFuture(['2020-06-14', '2020-06-20']);
-        expect(result).toEqual('present');
+        expect(pastPresentOrFuture(['2020-06-14', '2020-06-20'])).toEqual(
+          'present'
+        );
       });
 
       it('should accurately report a present date, including end date', () => {
-        const result = pastPresentOrFuture(['2020-06-10', '2020-06-14']);
-        expect(result).toEqual('present');
+        expect(pastPresentOrFuture(['2020-06-10', '2020-06-14'])).toEqual(
+          'present'
+        );
       });
 
       it('should accurately report a future date', () => {
-        const result = pastPresentOrFuture(['2020-06-15', '2020-06-20']);
-        expect(result).toEqual('future');
+        expect(pastPresentOrFuture(['2020-06-15', '2020-06-20'])).toEqual(
+          'future'
+        );
       });
     });
   });
 });
 
-// the local time in Tokyo should be on the 15th, since they are +0900
-// hour offset from UTC time
-describe('in Tokyo time zone', () => {
+// the local time in Tokyo should be June 15th 2022, 10am,
+// since Tokyo is +0900 hour offset from UTC time
+describe('in Asia/Tokyo time zone', () => {
   beforeAll(() => {
     jest.useFakeTimers('modern');
     jest.setSystemTime(new Date(testDateStr));
@@ -85,55 +104,70 @@ describe('in Tokyo time zone', () => {
     jest.useRealTimers();
   });
 
-  describe('getISODate', () => {
+  describe('general time safety checks', () => {
+    it('gets the day and time correctly for the Asia/Tokyo timezone', () => {
+      expect(
+        new Date().toLocaleString('en-US', { timeZone: 'Asia/Tokyo' })
+      ).toBe('6/15/2020, 10:00:00 AM');
+    });
+  });
+
+  describe('getIsoDateForToday', () => {
     it('returns the correct date formatted', () => {
-      const result = getIsoDate('2020-06-15');
-      expect(result).toEqual('2020-06-15');
+      expect(getIsoDateForToday()).toEqual('2020-06-15');
+    });
+  });
+
+  describe('getIsoDate', () => {
+    it('returns the correct date formatted', () => {
+      expect(getIsoDate('2020-06-15')).toEqual('2020-06-15');
     });
   });
 
   describe('pastPresentOrFuture', () => {
     describe('with a single input date', () => {
       it('should accurately report a past date', () => {
-        const result = pastPresentOrFuture('2020-06-14');
-        expect(result).toEqual('past');
+        expect(pastPresentOrFuture('2020-06-14')).toEqual('past');
       });
 
       it('should accurately report a present date', () => {
-        const result = pastPresentOrFuture('2020-06-15');
-        expect(result).toEqual('present');
+        expect(pastPresentOrFuture('2020-06-15')).toEqual('present');
       });
 
       it('should accurately report a future date', () => {
-        const result = pastPresentOrFuture('2020-06-16');
-        expect(result).toEqual('future');
+        expect(pastPresentOrFuture('2020-06-16')).toEqual('future');
       });
     });
 
     describe('with an input date range', () => {
       it('should accurately report a past date', () => {
-        const result = pastPresentOrFuture(['2020-06-10', '2020-06-14']);
-        expect(result).toEqual('past');
+        expect(pastPresentOrFuture(['2020-06-10', '2020-06-14'])).toEqual(
+          'past'
+        );
       });
 
       it('should accurately report a present date when in range', () => {
-        const result = pastPresentOrFuture(['2020-06-10', '2020-06-20']);
-        expect(result).toEqual('present');
+        expect(pastPresentOrFuture(['2020-06-10', '2020-06-20'])).toEqual(
+          'present'
+        );
       });
 
       it('should accurately report a present date, including start date', () => {
-        const result = pastPresentOrFuture(['2020-06-15', '2020-06-20']);
-        expect(result).toEqual('present');
+        expect(pastPresentOrFuture(['2020-06-15', '2020-06-20'])).toEqual(
+          'present'
+        );
       });
 
       it('should accurately report a present date, including end date', () => {
-        const result = pastPresentOrFuture(['2020-06-10', '2020-06-15']);
-        expect(result).toEqual('present');
+        expect(pastPresentOrFuture(['2020-06-10', '2020-06-15'])).toEqual(
+          'present'
+        );
       });
 
       it('should accurately report a future date', () => {
-        const result = pastPresentOrFuture(['2020-06-16', '2020-06-20']);
-        expect(result).toEqual('future');
+        expect(pastPresentOrFuture(['2020-06-16', '2020-06-20'])).toEqual(
+          'future'
+        );
       });
     });
   });
--- front/app/utils/dateUtils.ts
+++ front/app/utils/dateUtils.ts
@@ -1,10 +1,20 @@
 import moment from 'moment';
 import { isString } from 'lodash-es';
 
+export function getIsoDateForToday(): string {
+  // this is based on the user's timezone in moment, so
+  // will return based on the current day of the user's browser
+  return moment().format('YYYY-MM-DD');
+}
+
+// this function is exclusively used to compare phase start/ends,
+// which are created and stored with the YYYY-MM-DD format (no time of day)
 export function pastPresentOrFuture(input: string | [string, string]) {
+  const currentIsoDate = getIsoDateForToday();
+
+  // if input is a string representing one date
   if (isString(input)) {
     const isoDate = getIsoDate(input);
-    const currentIsoDate = moment().format('YYYY-MM-DD');
 
     if (isoDate === currentIsoDate) {
       return 'present';
@@ -13,29 +23,30 @@ export function pastPresentOrFuture(input: string | [string, string]) {
     }
 
     return 'future';
-  } else {
-    const startIsoDate = getIsoDate(input[0]);
-    const endIsoDate = getIsoDate(input[1]);
-    const currentIsoDate = moment().format('YYYY-MM-DD');
-
-    if (
-      moment(currentIsoDate, 'YYYY-MM-DD').isBetween(
-        startIsoDate,
-        endIsoDate,
-        'days',
-        '[]'
-      )
-    ) {
-      return 'present';
-    } else if (moment(currentIsoDate, 'YYYY-MM-DD').isAfter(endIsoDate)) {
-      return 'past';
-    }
-
-    return 'future';
   }
+
+  // if input is an array with start and end dates
+  const startIsoDate = getIsoDate(input[0]);
+  const endIsoDate = getIsoDate(input[1]);
+
+  if (
+    moment(currentIsoDate, 'YYYY-MM-DD').isBetween(
+      startIsoDate,
+      endIsoDate,
+      'days',
+      '[]'
+    )
+  ) {
+    return 'present';
+  } else if (moment(currentIsoDate, 'YYYY-MM-DD').isAfter(endIsoDate)) {
+    return 'past';
+  }
+
+  return 'future';
 }
 
 export function getIsoDate(date: string) {
+  // by using moment.utc, we ignore timezone offsets which could cause bugs
   return moment.utc(new Date(date)).format('YYYY-MM-DD');
 }
 
