c00fea7d4a514c38792e7eef8dc70339a988444d
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-04-27 12:46:49 +0100

    Add field title

--- front/app/modules/commercial/representativeness/admin/components/ChartCard/Header.tsx
+++ front/app/modules/commercial/representativeness/admin/components/ChartCard/Header.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
-const Header = () => {
-  return <></>;
+// hooks
+import useLocalize from 'hooks/useLocalize';
+
+// components
+import { Box, Title } from '@citizenlab/cl2-component-library';
+
+// typings
+import { Multiloc } from 'typings';
+
+interface Props {
+  title: Multiloc;
+}
+
+const Header = ({ title }: Props) => {
+  const localize = useLocalize();
+
+  return (
+    <Box p="20px 40px 0px 40px" display="flex" justifyContent="space-between">
+      <Title variant="h3" as="h2">
+        {localize(title)}
+      </Title>
+      <Box display="flex" alignItems="center"></Box>
+    </Box>
+  );
 };
 
 export default Header;
--- front/app/modules/commercial/representativeness/admin/components/ChartCard/index.tsx
+++ front/app/modules/commercial/representativeness/admin/components/ChartCard/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 // hooks
 import { useTheme } from 'styled-components';
-import useLocalize from 'hooks/useLocalize';
 
 // components
-import { Box, Title } from '@citizenlab/cl2-component-library';
+import { Box } from '@citizenlab/cl2-component-library';
+import Header from './Header';
 import MultiBarChart from 'components/admin/Graphs/MultiBarChart';
 import { DEFAULT_BAR_CHART_MARGIN } from 'components/admin/Graphs/constants';
 
@@ -21,17 +21,10 @@ interface Props {
 
 const ChartCard = ({ customField }: Props) => {
   const { newBarFill, secondaryNewBarFill }: any = useTheme();
-  const localize = useLocalize();
-
-  console.log(customField);
 
   return (
     <Box width="100%" background="white">
-      <Box pl="40px" pt="20px">
-        <Title variant="h3" as="h2">
-          {localize(customField.attributes.title_multiloc)}
-        </Title>
-      </Box>
+      <Header title={customField.attributes.title_multiloc} />
       <MultiBarChart
         height={300}
         data={TEST_GENDER_DATA}
