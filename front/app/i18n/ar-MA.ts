23b4695f039e16c2d4811fc7f1f13527ed256637
Simon Tharby <simontharby@gmail.com>
2022-04-27 14:43:46 -0300

    CL-686 Create new language init file

---
+++ front/app/i18n/ar-MA.ts
@@ -0,0 +1,15 @@
+import { addLocaleData } from 'react-intl';
+import ma from 'react-intl/locale-data/hr';
+
+import { formatTranslationMessages } from './';
+
+addLocaleData(ma);
+
+const arMATranslationMessages = require('translations/ar-MA.json');
+const arMAAdminTranslationMessages = require('translations/admin/ar-MA.json');
+const translationMessages = formatTranslationMessages('ar-MA', {
+  ...arMATranslationMessages,
+  ...arMAAdminTranslationMessages,
+});
+
+export default translationMessages;
