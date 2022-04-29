a3c4a3a16a532853d26863450e4b1f015c2b8fa5
lucas <lucasquinnmathis@gmail.com>
2022-04-29 14:36:46 +0200

    [TEC-11] WIP use browser history

--- front/app/utils/cl-router/history.ts
+++ front/app/utils/cl-router/history.ts
@@ -3,7 +3,6 @@ import { LocationDescriptor } from 'history';
 import { localeStream } from 'services/locale';
 import updateLocationDescriptor from 'utils/cl-router/updateLocationDescriptor';
 // tslint:disable-next-line:no-vanilla-routing
-import { browserHistory } from 'react-router';
 
 // overrides push and replace methods so they update the location with the current locale from the locale stream
 function historyMethod(
@@ -15,12 +14,12 @@ function historyMethod(
     .observable.pipe(first())
     .subscribe((locale) => {
       // calls the vanilla react-router method with updated location
-      browserHistory[method](updateLocationDescriptor(location, locale));
+      history[method](updateLocationDescriptor(location, locale));
     });
 }
 
 export default {
-  ...browserHistory,
+  ...history,
   push: (location: LocationDescriptor): void => historyMethod('push', location),
   replace: (location: LocationDescriptor): void =>
     historyMethod('replace', location),
