2ba53738a83c7bf81d9d0a15116e150977ce1cfa
Sebastien Hoorens <sebastien@citizenlab.co>
2022-05-05 15:46:33 +0200

    [CL-732] navbaritem title_multiloc with fallback as separate method rather than overriding

--- back/app/models/nav_bar_item.rb
+++ back/app/models/nav_bar_item.rb
@@ -39,8 +39,8 @@ class NavBarItem < ActiveRecord::Base
     code == 'custom'
   end
 
-  def title_multiloc
-    super.presence || fallback_title_multiloc
+  def title_multiloc_with_fallback
+    title_multiloc.presence || fallback_title_multiloc
   end
 
   private
