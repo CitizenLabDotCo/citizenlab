daca060b7e991e8de1f94943f16351361335fbcc
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-05-06 11:30:19 +0100

    Fix background image positioning and color

--- front/app/modules/commercial/representativeness/admin/containers/Dashboard/EmptyState.tsx
+++ front/app/modules/commercial/representativeness/admin/containers/Dashboard/EmptyState.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 // images
-import EmptyStateImage from '../../assets/EmptyStateImage.svg';
 import EmptyProjectsImageSrc from 'assets/img/landingpage/no_projects_image.svg';
+import EmptyStateImage from '../../assets/EmptyStateImage.svg';
 
 // components
 import { Box, Image, Title, Text } from '@citizenlab/cl2-component-library';
@@ -11,19 +11,29 @@ import Button from 'components/UI/Button';
 // styling
 import styled from 'styled-components';
 
+const StyledBackgroundImage = styled(Image)`
+  opacity: 0.5;
+`;
+
 const StyledImage = styled(Image)`
   filter: drop-shadow(0px 0px 12px rgba(0, 0, 0, 0.25));
 `;
 
 const EmptyState = () => {
   return (
-    <Box background="white" position="absolute" mb="36px" height="578px">
-      <Image alt="" src={EmptyProjectsImageSrc} width="100%" height="auto" />
-      <Box position="absolute" top="0px" left="0px" width="100%">
+    <Box background="white" mb="36px" position="relative">
+      <StyledBackgroundImage
+        alt=""
+        src={EmptyProjectsImageSrc}
+        width="100%"
+        height="100%"
+        position="absolute"
+      />
+      <Box pt="46px" pb="64px">
         <Box
           ml="5%"
           width="90%"
-          mt="46px"
+          mt="0px"
           background="rgba(255, 12, 12, 0.12)"
           border="1px solid #CF4040"
           px="24px"
@@ -43,7 +53,11 @@ const EmptyState = () => {
         </Box>
 
         <Box display="flex" justifyContent="center" mt="23px">
-          <StyledImage src={EmptyStateImage} alt="" width="516px" />
+          <StyledImage
+            src={EmptyStateImage}
+            alt="Representativeness dashboard preview"
+            width="516px"
+          />
         </Box>
       </Box>
     </Box>
