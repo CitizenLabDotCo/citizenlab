9dce73078536326a9c36a82027450492780bad88
Amanda Anderson <amanda.anderson@citizenlab.co>
2022-05-06 15:49:14 +0200

    [CL-634] Replace match with test

--- front/app/modules/commercial/content_builder/admin/components/CraftComponents/Iframe/index.tsx
+++ front/app/modules/commercial/content_builder/admin/components/CraftComponents/Iframe/index.tsx
@@ -150,9 +150,10 @@ Iframe.craft = {
 const isValidUrl = (url: string) => {
   // Used this reference for generating a valid URL regex:
   // https://tutorial.eyehunts.com/js/url-regex-validation-javascript-example-code/
-  let invalidUrl = !url.match(
-    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g
-  );
+  const validUrlRegex = [
+    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g,
+  ];
+  let invalidUrl = !validUrlRegex.some((rx) => rx.test(url));
   if (invalidUrl) {
     return [false, 'invalidUrl'];
   }
