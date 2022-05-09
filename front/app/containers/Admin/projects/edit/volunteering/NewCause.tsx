decad1e0bbda017990c568948d9a495044436ff5
lucas <lucasquinnmathis@gmail.com>
2022-05-09 15:30:59 +0200

    WIP revert one file to master

--- front/app/containers/Admin/projects/edit/volunteering/NewCause.tsx
+++ front/app/containers/Admin/projects/edit/volunteering/NewCause.tsx
@@ -1,7 +1,6 @@
 import React, { memo, useCallback, useState } from 'react';
 import { isEmpty } from 'lodash-es';
-import CLWithRouter, { ICLComponentWithRouterProp } from 'utils/withRouter';
-// import { withRouter, WithRouterProps } from 'react-router';
+import { withRouter, WithRouterProps } from 'react-router';
 import clHistory from 'utils/cl-router/history';
 
 // Services
@@ -48,172 +47,170 @@ interface IFormValues {
   image: UploadFile | null;
 }
 
-const NewCause = memo<Props & InjectedIntlProps & ICLComponentWithRouterProp>(
-  (props) => {
-    const {
-      intl: { formatMessage },
-    } = props;
+const NewCause = memo<Props & InjectedIntlProps & WithRouterProps>((props) => {
+  const {
+    intl: { formatMessage },
+  } = props;
 
-    const [touched, setTouched] = useState(false);
-    const [processing, setProcessing] = useState(false);
-    const [errors, setErrors] = useState<{ [key: string]: any }>({});
-    const [formValues, setFormValues] = useState<IFormValues>({
-      title_multiloc: null,
-      description_multiloc: null,
+  const [touched, setTouched] = useState(false);
+  const [processing, setProcessing] = useState(false);
+  const [errors, setErrors] = useState<{ [key: string]: any }>({});
+  const [formValues, setFormValues] = useState<IFormValues>({
+    title_multiloc: null,
+    description_multiloc: null,
+    image: null,
+  });
+
+  const projectId = props.params.projectId;
+  const phaseId = props.params.phaseId;
+  const participationContextType = phaseId ? 'phase' : 'project';
+  const participationContextId = phaseId || projectId;
+
+  const handleTitleOnChange = useCallback((title_multiloc: Multiloc) => {
+    setTouched(true);
+    setFormValues((prevFormValues) => ({
+      ...prevFormValues,
+      title_multiloc,
+    }));
+  }, []);
+
+  const handleDescriptionOnChange = useCallback(
+    (description_multiloc: Multiloc, _locale: Locale) => {
+      setTouched(true);
+      setFormValues((prevFormValues) => ({
+        ...prevFormValues,
+        description_multiloc,
+      }));
+    },
+    []
+  );
+
+  const handleImageOnAdd = useCallback((images: UploadFile[]) => {
+    setTouched(true);
+    setFormValues((prevFormValues) => ({
+      ...prevFormValues,
+      image: images[0],
+    }));
+  }, []);
+
+  const handleImageOnRemove = useCallback(() => {
+    setTouched(true);
+    setFormValues((prevFormValues) => ({
+      ...prevFormValues,
       image: null,
-    });
+    }));
+  }, []);
 
-    const projectId = props.params.projectId;
-    const phaseId = props.params.phaseId;
-    const participationContextType = phaseId ? 'phase' : 'project';
-    const participationContextId = phaseId || projectId;
+  const handleOnSubmit = useCallback(() => {
+    const { title_multiloc, description_multiloc, image } = formValues;
 
-    const handleTitleOnChange = useCallback((title_multiloc: Multiloc) => {
-      setTouched(true);
-      setFormValues((prevFormValues) => ({
-        ...prevFormValues,
-        title_multiloc,
-      }));
-    }, []);
+    if (!processing && title_multiloc && description_multiloc) {
+      setProcessing(true);
+      setErrors({});
 
-    const handleDescriptionOnChange = useCallback(
-      (description_multiloc: Multiloc, _locale: Locale) => {
-        setTouched(true);
-        setFormValues((prevFormValues) => ({
-          ...prevFormValues,
-          description_multiloc,
-        }));
-      },
-      []
-    );
-
-    const handleImageOnAdd = useCallback((images: UploadFile[]) => {
-      setTouched(true);
-      setFormValues((prevFormValues) => ({
-        ...prevFormValues,
-        image: images[0],
-      }));
-    }, []);
-
-    const handleImageOnRemove = useCallback(() => {
-      setTouched(true);
-      setFormValues((prevFormValues) => ({
-        ...prevFormValues,
-        image: null,
-      }));
-    }, []);
-
-    const handleOnSubmit = useCallback(() => {
-      const { title_multiloc, description_multiloc, image } = formValues;
-
-      if (!processing && title_multiloc && description_multiloc) {
-        setProcessing(true);
-        setErrors({});
-
-        let PCType;
-        switch (participationContextType) {
-          case 'project':
-            PCType = 'Project';
-            break;
-          case 'phase':
-            PCType = 'Phase';
-            break;
-        }
-        addCause({
-          description_multiloc,
-          title_multiloc,
-          participation_context_type: PCType,
-          participation_context_id: participationContextId,
-          image: image?.base64,
-        })
-          .then(() => {
-            setProcessing(false);
-            setErrors({});
-            setTouched(false);
-            clHistory.push(`/admin/projects/${projectId}/volunteering`);
-          })
-          .catch((errorResponse) => {
-            setProcessing(false);
-            setErrors(errorResponse?.json?.errors || {});
-          });
+      let PCType;
+      switch (participationContextType) {
+        case 'project':
+          PCType = 'Project';
+          break;
+        case 'phase':
+          PCType = 'Phase';
+          break;
       }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [formValues, processing]);
+      addCause({
+        description_multiloc,
+        title_multiloc,
+        participation_context_type: PCType,
+        participation_context_id: participationContextId,
+        image: image?.base64,
+      })
+        .then(() => {
+          setProcessing(false);
+          setErrors({});
+          setTouched(false);
+          clHistory.push(`/admin/projects/${projectId}/volunteering`);
+        })
+        .catch((errorResponse) => {
+          setProcessing(false);
+          setErrors(errorResponse?.json?.errors || {});
+        });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [formValues, processing]);
 
-    return (
-      <Container>
-        <SectionTitle>
-          <FormattedMessage {...messages.newCauseTitle} />
-        </SectionTitle>
-        <SectionDescription>
-          <FormattedMessage {...messages.newCauseSubtitle} />
-        </SectionDescription>
+  return (
+    <Container>
+      <SectionTitle>
+        <FormattedMessage {...messages.newCauseTitle} />
+      </SectionTitle>
+      <SectionDescription>
+        <FormattedMessage {...messages.newCauseSubtitle} />
+      </SectionDescription>
 
-        <Section>
-          <SectionField>
-            <InputMultilocWithLocaleSwitcher
-              type="text"
-              id="cause-title"
-              valueMultiloc={formValues.title_multiloc}
-              onChange={handleTitleOnChange}
-              label={formatMessage(messages.causeTitleLabel)}
-            />
-            <Error
-              fieldName="title_multiloc"
-              apiErrors={errors?.title_multiloc}
-            />
-          </SectionField>
+      <Section>
+        <SectionField>
+          <InputMultilocWithLocaleSwitcher
+            type="text"
+            id="cause-title"
+            valueMultiloc={formValues.title_multiloc}
+            onChange={handleTitleOnChange}
+            label={formatMessage(messages.causeTitleLabel)}
+          />
+          <Error
+            fieldName="title_multiloc"
+            apiErrors={errors?.title_multiloc}
+          />
+        </SectionField>
 
-          <SectionField>
-            <QuillMultilocWithLocaleSwitcher
-              id="cause-description"
-              valueMultiloc={formValues.description_multiloc}
-              onChange={handleDescriptionOnChange}
-              label={formatMessage(messages.causeDescriptionLabel)}
-              labelTooltipText={formatMessage(messages.causeDescriptionTooltip)}
-              withCTAButton
-            />
-            <Error
-              fieldName="description_multiloc"
-              apiErrors={errors?.description_multiloc}
-            />
-          </SectionField>
-          <SectionField>
-            <Label>
-              <FormattedMessage {...messages.causeImageLabel} />
-            </Label>
-            <ImagesDropzone
-              acceptedFileTypes="image/jpg, image/jpeg, image/png, image/gif"
-              images={formValues.image ? [formValues.image] : null}
-              imagePreviewRatio={120 / 480}
-              maxImagePreviewWidth="500px"
-              onAdd={handleImageOnAdd}
-              onRemove={handleImageOnRemove}
-            />
-          </SectionField>
-        </Section>
+        <SectionField>
+          <QuillMultilocWithLocaleSwitcher
+            id="cause-description"
+            valueMultiloc={formValues.description_multiloc}
+            onChange={handleDescriptionOnChange}
+            label={formatMessage(messages.causeDescriptionLabel)}
+            labelTooltipText={formatMessage(messages.causeDescriptionTooltip)}
+            withCTAButton
+          />
+          <Error
+            fieldName="description_multiloc"
+            apiErrors={errors?.description_multiloc}
+          />
+        </SectionField>
+        <SectionField>
+          <Label>
+            <FormattedMessage {...messages.causeImageLabel} />
+          </Label>
+          <ImagesDropzone
+            acceptedFileTypes="image/jpg, image/jpeg, image/png, image/gif"
+            images={formValues.image ? [formValues.image] : null}
+            imagePreviewRatio={120 / 480}
+            maxImagePreviewWidth="500px"
+            onAdd={handleImageOnAdd}
+            onRemove={handleImageOnRemove}
+          />
+        </SectionField>
+      </Section>
 
-        <ButtonContainer>
-          <Button
-            buttonStyle="admin-dark"
-            onClick={handleOnSubmit}
-            processing={processing}
-            disabled={!touched}
-          >
-            <FormattedMessage {...messages.saveCause} />
-          </Button>
+      <ButtonContainer>
+        <Button
+          buttonStyle="admin-dark"
+          onClick={handleOnSubmit}
+          processing={processing}
+          disabled={!touched}
+        >
+          <FormattedMessage {...messages.saveCause} />
+        </Button>
 
-          {!isEmpty(errors) && (
-            <Error
-              text={formatMessage(messages.causeErrorMessage)}
-              showBackground={false}
-              showIcon={false}
-            />
-          )}
-        </ButtonContainer>
-      </Container>
-    );
-  }
-);
+        {!isEmpty(errors) && (
+          <Error
+            text={formatMessage(messages.causeErrorMessage)}
+            showBackground={false}
+            showIcon={false}
+          />
+        )}
+      </ButtonContainer>
+    </Container>
+  );
+});
 
-export default CLWithRouter(injectIntl(NewCause));
+export default withRouter(injectIntl(NewCause));
