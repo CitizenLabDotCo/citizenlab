d928e7b7e6538e67092e17d6514ee5419efa5cc7
Koen De Hondt <koen@dehondt.dev>
2022-05-10 09:51:47 +0200

    [CL-740] Elaborate UrlSanitizationService spec

--- back/spec/services/url_sanitization_service_spec.rb
+++ back/spec/services/url_sanitization_service_spec.rb
@@ -6,64 +6,80 @@ describe UrlSanitizationService do
   describe 'whitelist verification' do
 
     # ---- TODO ----
-    # two cases for HTML specifically
-    # Correct URL, and wrong URL
     # Use Stubbing
     # E.g. allow_any_instance_of(urlService).to receive(:whitelist).an_return(true);
 
-    it 'allows Microsoft Forms url to pass through whitelist' do
-      expect(service.url_whitelisted?("https://customervoice.microsoft.com/Pages/ResponsePage.aspx?id=Zl36w")).to be true;
+    it 'checks Microsoft Forms url passes through whitelist' do
+      expect(service.url_whitelisted?('https://customervoice.microsoft.com/something')).to be true
+      expect(service.url_whitelisted?('https://customervoice.microsoft.com')).to be false
+      expect(service.url_whitelisted?('https://microsoft.com/something')).to be false
     end
-    it 'allows Typeform url to pass through whitelist' do
-      expect(service.url_whitelisted?("")).to be true;
+    it 'checks Typeform url passes through whitelist' do
+      expect(service.url_whitelisted?('https://citizenlabco.typeform.com/to/something')).to be true
+      expect(service.url_whitelisted?('https://citizenlabco.typeform.com')).to be false
+      expect(service.url_whitelisted?('https://typeform.com/to/something')).to be false
     end
-    it 'allows SurveyMonkey url to pass through whitelist' do
-      expect(service.url_whitelisted?("")).to be true;
+    it 'checks SurveyMonkey url passes through whitelist' do
+      expect(service.url_whitelisted?('https://widget.surveymonkey.com/collect/website/js/something.js')).to be true
+      expect(service.url_whitelisted?('https://widget.surveymonkey.com')).to be false
     end
-    it 'allows Google Forms url to pass through whitelist' do
-      expect(service.url_whitelisted?("")).to be true;
+    it 'checks Google Forms url passes through whitelist' do
+      expect(service.url_whitelisted?('https://docs.google.com/forms/d/e/something/viewform?embedded=true')).to be true
+      expect(service.url_whitelisted?('https://docs.google.com/forms/bogus/e/something/viewform?embedded=true')).to be false
     end
-    it 'allows Enalyzer url to pass through whitelist' do
-      expect(service.url_whitelisted?("")).to be true;
+    it 'checks Enalyzer url passes through whitelist' do
+      expect(service.url_whitelisted?('https://surveys.enalyzer.com/?pid=something')).to be true
+      expect(service.url_whitelisted?('https://surveys.enalyzer.com?pid=something')).to be true
+      expect(service.url_whitelisted?('https://surveys.enalyzer.com?bogus=something')).to be false
     end
-    it 'allows SurveyXact url to pass through whitelist' do
-      expect(service.url_whitelisted?("")).to be true;
+    it 'checks SurveyXact url passes through whitelist' do
+      expect(service.url_whitelisted?('https://www.survey-xact.dk/LinkCollector?key=something')).to be true
+      expect(service.url_whitelisted?('https://www.survey-xact.dk/LinkCollector?bogus=something')).to be false
     end
-    it 'allows Qualtrics url to pass through whitelist' do
-      expect(service.url_whitelisted?("")).to be true;
+    it 'checks Qualtrics url passes through whitelist' do
+      expect(service.url_whitelisted?('https://subdomain.qualtrics.com/jfe/form/something')).to be true
+      expect(service.url_whitelisted?('https://subdomain.qualtrics.com/jfe/bogus/something')).to be false
+      expect(service.url_whitelisted?('https://qualtrics.com/jfe/form/something')).to be false
     end
-    it 'allows SmartSurvey url to pass through whitelist' do
-      expect(service.url_whitelisted?("")).to be true;
+    it 'checks SmartSurvey url passes through whitelist' do
+      expect(service.url_whitelisted?('https://www.smartsurvey.co.uk/something')).to be true
     end
-    it 'allows Eventbrite url to pass through whitelist' do
-      expect(service.url_whitelisted?("")).to be true;
+    it 'checks Eventbrite url passes through whitelist' do
+      expect(service.url_whitelisted?('https://www.eventbrite.com/static/widgets/something')).to be true
     end
-    it 'allows Tableau url to pass through whitelist' do
-      expect(service.url_whitelisted?("")).to be true;
+    it 'checks Tableau url passes through whitelist' do
+      expect(service.url_whitelisted?('https://public.tableau.com/something')).to be true
     end
-    it 'allows DataStudio url to pass through whitelist' do
-      expect(service.url_whitelisted?("")).to be true;
+    it 'checks DataStudio url passes through whitelist' do
+      expect(service.url_whitelisted?('https://subdomain.datastudio.google.com/embed/something')).to be true
     end
-    it 'allows PowerBI url to pass through whitelist' do
-      expect(service.url_whitelisted?("")).to be true;
+    it 'checks PowerBI url passes through whitelist' do
+      expect(service.url_whitelisted?('https://app.powerbi.com/something')).to be true
+      expect(service.url_whitelisted?('https://app.powerbi.com')).to be false
     end
-    it 'allows Data Studio url to pass through whitelist' do
-      expect(service.url_whitelisted?("")).to be true;
+    it 'checks Constant Contact url passes through whitelist' do
+      expect(service.url_whitelisted?('https://subdomain.static.ctctcdn.com/js/something},')).to be true
+      expect(service.url_whitelisted?('https://subdomain.static.ctctcdn.com/bogus/something},')).to be false
     end
-    it 'allows Constant Contact url to pass through whitelist' do
-      expect(service.url_whitelisted?("")).to be true;
+    it 'checks Instagram url passes through whitelist' do
+      expect(service.url_whitelisted?('https://instagram.com/something')).to be true
+      expect(service.url_whitelisted?('https://instagram.com')).to be false
     end
-    it 'allows Instagram url to pass through whitelist' do
-      expect(service.url_whitelisted?("")).to be true;
+    it 'checks Twitter url passes through whitelist' do
+      expect(service.url_whitelisted?('https://platform.twitter.com/something')).to be true
+      expect(service.url_whitelisted?('https://platform.twitter.com')).to be false
     end
-    it 'allows Twitter url to pass through whitelist' do
-      expect(service.url_whitelisted?("")).to be true;
+    it 'checks Facebook url passes through whitelist' do
+      expect(service.url_whitelisted?('https://facebook.com/something')).to be true
+      expect(service.url_whitelisted?('https://facebook.com')).to be false
     end
-    it 'allows Facebook url to pass through whitelist' do
-      expect(service.url_whitelisted?("")).to be true;
+    it 'checks Konveio url passes through whitelist' do
+      expect(service.url_whitelisted?('https://name.konveio.com/something')).to be true
+      expect(service.url_whitelisted?('https://name.konveio.com')).to be false
     end
-    it 'allows Konveio url to pass through whitelist' do
-      expect(service.url_whitelisted?("")).to be true;
+    it 'checks ArcGis url passes through whitelist' do
+      expect(service.url_whitelisted?('https://subdomain.arcgis.com/something')).to be true
+      expect(service.url_whitelisted?('https://subdomain.arcgis.com')).to be false
     end
   end
 end
