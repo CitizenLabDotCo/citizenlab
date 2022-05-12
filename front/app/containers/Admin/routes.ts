90731a33f4462cc9c014641a2554e68da9d7819b
brentguf <brentguf@users.noreply.github.com>
2022-05-12 16:20:56 +0200

    Remove irrelevant comments

--- front/app/containers/Admin/routes.ts
+++ front/app/containers/Admin/routes.ts
@@ -1,7 +1,3 @@
-// These are the pages you can go to. (within the admin)
-// They are all wrapped in the App component, which should contain the navbar etc
-// See http://blog.mxstbr.com/2016/01/react-apps-with-pages for more information
-// about the code splitting business
 import dashboardRoutes from './dashboard/routes';
 import initiativesRoutes from './initiatives/routes';
 import usersRoutes from './users/routes';
