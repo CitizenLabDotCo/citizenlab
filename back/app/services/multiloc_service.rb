42cacb90f9b3caf98bcc2ec3bf466b0e8c790670
Sebastien Hoorens <sebastien@citizenlab.co>
2022-05-16 15:21:20 +0200

    [DISP-76] fixed query issue (appconfiguration) with multiloc service which caused slow users xlsx exports

--- back/app/services/multiloc_service.rb
+++ back/app/services/multiloc_service.rb
@@ -1,8 +1,12 @@
 class MultilocService
+  def initialize(app_configuration: nil)
+    @app_configuration = app_configuration
+  end
+
   def t(translations, user = nil)
     return nil unless translations
 
-    locales = AppConfiguration.instance.settings('core', 'locales')
+    locales = app_configuration.settings('core', 'locales')
     user_locale = user&.locale || I18n.locale.to_s
     result = ([user_locale] + locales + translations.keys).each do |locale|
       break translations[locale] if translations[locale]
@@ -11,7 +15,7 @@ class MultilocService
   end
 
   def i18n_to_multiloc(key, locales: nil)
-    (locales || AppConfiguration.instance.settings('core', 'locales')).each_with_object({}) do |locale, result|
+    (locales || app_configuration.settings('core', 'locales')).each_with_object({}) do |locale, result|
       I18n.with_locale(locale) do
         result[locale] = I18n.t(key, raise: true)
       end
@@ -19,7 +23,7 @@ class MultilocService
   end
 
   def block_to_multiloc
-    AppConfiguration.instance.settings('core', 'locales').each_with_object({}) do |locale, result|
+    app_configuration.settings('core', 'locales').each_with_object({}) do |locale, result|
       I18n.with_locale(locale) do
         result[locale] = yield(locale)
       end
@@ -28,5 +32,11 @@ class MultilocService
 
   def is_empty?(multiloc)
     !multiloc || multiloc.values.all?(&:blank?)
+  end
+
+  private
+
+  def app_configuration
+    @app_configuration || AppConfiguration.instance
   end
 end
