85ecc9635efedd654f1e8f2860c249b7a11899fc
lucas <lucasquinnmathis@gmail.com>
2022-05-16 11:59:28 +0200

    Create browser history in custom CL history util

--- front/app/utils/cl-router/history.ts
+++ front/app/utils/cl-router/history.ts
@@ -3,8 +3,9 @@ import { localeStream } from 'services/locale';
 import updateLocationDescriptor from 'utils/cl-router/updateLocationDescriptor';
 // tslint:disable-next-line:no-vanilla-routing
 
-import { Location } from 'history';
-import { rootHistory } from 'root';
+import { Location, BrowserHistory, createBrowserHistory } from 'history';
+
+const historyObject: BrowserHistory = createBrowserHistory();
 
 // overrides push and replace methods so they update the location with the current locale from the locale stream
 function historyMethod(
@@ -16,15 +17,15 @@ function historyMethod(
     .observable.pipe(first())
     .subscribe((locale) => {
       // calls the vanilla react-router method with updated location
-      rootHistory[method](updateLocationDescriptor(location, locale));
+      historyObject[method](updateLocationDescriptor(location, locale));
     });
 }
 
 export default {
-  ...rootHistory,
+  ...historyObject,
   push: (location: Partial<Location> | string): void =>
     historyMethod('push', location),
   replace: (location: Partial<Location> | string): void =>
     historyMethod('replace', location),
-  goBack: () => rootHistory.back(),
+  goBack: () => historyObject.back(),
 };
