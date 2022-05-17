c4fa705399a9e6459484e74c485c94d8dbb2e7f6
Sebastien Hoorens <sebastien@citizenlab.co>
2022-05-17 18:47:27 +0200

    [CL-504] minor rubocop fix

--- back/engines/commercial/idea_custom_fields/app/serializers/idea_custom_fields/extensions/web_api/v1/idea_serializer.rb
+++ back/engines/commercial/idea_custom_fields/app/serializers/idea_custom_fields/extensions/web_api/v1/idea_serializer.rb
@@ -9,7 +9,7 @@ module IdeaCustomFields
             base.class_eval do
               attribute :custom_field_values, if: proc {
                 AppConfiguration.instance.feature_activated? 'idea_custom_fields'
-               } do |idea|
+              } do |idea|
                 visible_field_values = CustomFieldService.remove_hidden_custom_fields idea.custom_field_values
                 CustomFieldService.remove_disabled_custom_fields visible_field_values
               end
