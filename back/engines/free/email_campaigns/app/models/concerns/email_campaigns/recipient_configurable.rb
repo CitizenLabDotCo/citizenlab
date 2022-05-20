f028e2f468493c7328264b9198f35a74c1d7e44f
Koen De Hondt <koen@dehondt.dev>
2022-05-20 17:14:20 +0200

    [CL-667] Adapt condition after earlier change to use flat_map
    Originally user_ids could be nil. In the previous commit it is always an array.

--- back/engines/free/email_campaigns/app/models/concerns/email_campaigns/recipient_configurable.rb
+++ back/engines/free/email_campaigns/app/models/concerns/email_campaigns/recipient_configurable.rb
@@ -13,7 +13,7 @@ module EmailCampaigns
 
     def filter_users_in_groups(users_scope, activity: nil, time: nil)
       user_ids = groups.flat_map(&:member_ids).uniq
-      if user_ids
+      if user_ids.any?
         users_scope
           .active
           .where(id: groups.flat_map(&:members).uniq)
