b996895238cc742ec23eb43016d7a94cf6a7d5f2
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-05-20 17:17:12 +0100

    Add test

--- back/engines/commercial/geographic_dashboard/spec/acceptance/geotag_ideas_spec.rb
+++ back/engines/commercial/geographic_dashboard/spec/acceptance/geotag_ideas_spec.rb
@@ -20,27 +20,52 @@ resource 'GeotagIdeas' do
   end
 
   get '/web_api/v1/ideas/geotagged' do
-    let!(:ideas) do
-      create_list(:idea, 2, title_multiloc: { 'nl-BE' => 'Een fietspad in het Prinsenbos van Grimbergen' })
-    end
-
-    before do
-      allow_any_instance_of(GeographicDashboard::GeotagService).to receive(:geotag).and_return(geocoded)
-      do_request
-    end
-
     with_options scope: :page do
       parameter :number, 'Page number'
       parameter :size, 'Number of campaigns per page'
     end
 
-    example 'Returns the right status code' do
-      expect(status).to eq 200
+    context "When ideas have same locale" do
+      let!(:ideas) do
+        create_list(:idea, 2, title_multiloc: { 'nl-BE' => 'Een fietspad in het Prinsenbos van Grimbergen' })
+      end
+
+      before do
+        allow_any_instance_of(NLP::Api).to receive(:geotag).with(
+          instance_of(String), instance_of(String), instance_of(String), anything
+        ).and_return([geocoded])
+        do_request
+      end
+
+      example 'Returns the right status code' do
+        expect(status).to eq 200
+      end
+
+      example 'List all ideas geotagged' do
+        json_response = json_parse(response_body)
+        expect(json_response[:data].size).to eq 2
+      end
     end
 
-    example 'List all ideas geotagged' do
-      json_response = json_parse(response_body)
-      expect(json_response[:data].size).to eq 2
+    context "When ideas have different locales" do
+      let!(:ideas) do
+        [
+          create(:idea, title_multiloc: { 'nl-BE' => 'Een fietspad in het Prinsenbos van Grimbergen' }),
+          create(:idea, title_multiloc: { 'en' => 'A bicycle lane in the Prinsenbos of Grimbergen' })
+        ]
+      end
+
+      before do
+        allow_any_instance_of(NLP::Api).to receive(:geotag).with(
+          instance_of(String), instance_of(String), instance_of(String), anything
+        ).and_return([geocoded])
+        do_request
+      end
+
+      example 'List all ideas geotagged (different locales)' do
+        json_response = json_parse(response_body)
+        expect(json_response[:data].size).to eq 2
+      end
     end
   end
 end
