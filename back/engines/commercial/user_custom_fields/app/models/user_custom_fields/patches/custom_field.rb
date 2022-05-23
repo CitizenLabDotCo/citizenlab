7f4a54ca87034b59b81045fbb95276100a11c9fc
Adrien Dessy <adrien@citizenlab.co>
2022-05-23 09:27:28 +0200

    CL-610 Clarify TODO comment [skip ci]

--- back/engines/commercial/user_custom_fields/app/models/user_custom_fields/patches/custom_field.rb
+++ back/engines/commercial/user_custom_fields/app/models/user_custom_fields/patches/custom_field.rb
@@ -3,7 +3,9 @@
 module UserCustomFields::Patches::CustomField
   def self.included(base)
     base.class_eval do
-      # TODO: Refactor CustomFields to use STI and add the following only to user custom fields.
+      # TODO: Refactor CustomFields to use STI with two types: UserCustomField & FormCustomField
+      # `UserCustomField` would replace this patch (and live in the `user_custom_fields` engine)
+      # and `FormCustomField` would belong to the core.
       has_many(
         :ref_distributions,
         class_name: 'UserCustomFields::Representativeness::RefDistribution',
