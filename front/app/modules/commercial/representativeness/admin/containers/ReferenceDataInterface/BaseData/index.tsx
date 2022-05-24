b98d0f9fb09fd7e2a320ae1cda581cb4d79df086
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-05-24 17:43:09 +0100

    Add child routes for tabs

---
+++ front/app/modules/commercial/representativeness/admin/containers/ReferenceDataInterface/BaseData/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+  <>
+    <h1>Base data</h1>
+  </>
+);
---
+++ front/app/modules/commercial/representativeness/admin/containers/ReferenceDataInterface/DashboardItems/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+  <>
+    <h1>Dashboard items</h1>
+  </>
+);
