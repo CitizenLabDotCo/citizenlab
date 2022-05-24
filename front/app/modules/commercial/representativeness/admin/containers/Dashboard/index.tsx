30bfd6dba2799a44d3b255b6733d523823353b39
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-05-24 10:37:23 +0100

    Remove dummy router param used for epic review

--- front/app/modules/commercial/representativeness/admin/containers/Dashboard/index.tsx
+++ front/app/modules/commercial/representativeness/admin/containers/Dashboard/index.tsx
@@ -10,34 +10,29 @@ import ChartFilters from '../../components/ChartFilters';
 import EmptyState from './EmptyState';
 import ChartCards from './ChartCards';
 
-// TODO remove router stuff
-import { withRouter, WithRouterProps } from 'react-router';
+const SHOW_EMPTY = false;
 
-const RepresentativenessDashboard = withRouter(
-  ({ location: { search } }: WithRouterProps) => {
-    const [currentProjectFilter, setCurrentProjectFilter] = useState<string>();
+const RepresentativenessDashboard = () => {
+  const [currentProjectFilter, setCurrentProjectFilter] = useState<string>();
 
-    const onProjectFilter = ({ value }) => {
-      setCurrentProjectFilter(value);
-    };
+  const onProjectFilter = ({ value }) => {
+    setCurrentProjectFilter(value);
+  };
 
-    const showEmpty = search === '?showEmpty=true';
+  return (
+    <>
+      <Box width="100%" mb="36px">
+        <Header />
+        <ChartFilters
+          currentProjectFilter={currentProjectFilter}
+          onProjectFilter={onProjectFilter}
+        />
+      </Box>
 
-    return (
-      <>
-        <Box width="100%" mb="36px">
-          <Header />
-          <ChartFilters
-            currentProjectFilter={currentProjectFilter}
-            onProjectFilter={onProjectFilter}
-          />
-        </Box>
-
-        <Box>{showEmpty ? <EmptyState /> : <ChartCards />}</Box>
-      </>
-    );
-  }
-);
+      <Box>{SHOW_EMPTY ? <EmptyState /> : <ChartCards />}</Box>
+    </>
+  );
+};
 
 const RepresentativenessDashboardFeatureFlagWrapper = () => {
   const customFieldsActive = useFeatureFlag({ name: 'user_custom_fields' });
