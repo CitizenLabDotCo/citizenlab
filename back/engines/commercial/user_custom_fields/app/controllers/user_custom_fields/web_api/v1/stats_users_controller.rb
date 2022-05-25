9b0a3569553d6ce1a8de4a3e8627a10767cb0181
Adrien Dessy <adrien@citizenlab.co>
2022-05-25 17:13:10 +0200

    CL-844 Make a few methods private

--- back/engines/commercial/user_custom_fields/app/controllers/user_custom_fields/web_api/v1/stats_users_controller.rb
+++ back/engines/commercial/user_custom_fields/app/controllers/user_custom_fields/web_api/v1/stats_users_controller.rb
@@ -4,10 +4,6 @@ module UserCustomFields
   module WebApi
     module V1
       class StatsUsersController < ::WebApi::V1::StatsController
-        def user_counts
-          count_users_by_custom_field(find_users, custom_field)
-        end
-
         def users_by_gender
           render json: { series: { users: user_counts } }
         end
@@ -59,48 +55,6 @@ module UserCustomFields
         def users_by_education_as_xlsx
           xlsx = XlsxService.new.generate_field_stats_xlsx user_counts, 'education', 'users'
           send_data xlsx, type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', filename: 'users_by_education.xlsx'
-        end
-
-        def count_users_by_custom_field(users, custom_field)
-          field_values = select_field_values(users, custom_field)
-
-          # Warning: The method +count+ cannot be used here because it introduces a SQL syntax
-          # error while rewriting the SELECT clause. This is because the 'field_value' column is a
-          # computed column and it does not seem to be supported properly by the +count+ method.
-          counts = field_values.order('field_value')
-                               .group('field_value')
-                               .select('COUNT(*) as count')
-                               .to_a.pluck(:field_value, :count).to_h
-
-          counts['_blank'] = counts.delete(nil) || 0
-          counts
-        end
-
-        # Returns an ActiveRecord::Relation of all the custom field values for the given users.
-        # It returns a view (result set) with a single column named 'field_value'. Essentially,
-        # something that looks like:
-        #   SELECT ... AS field_value FROM ...
-        #
-        # Each user results in one or multiple rows, depending on the type of custom field.
-        # Custom fields with multiple values (e.g. multiselect) are returned as multiple rows.
-        # If the custom field has no value for a given user, the resulting row contains NULL.
-        def select_field_values(users, custom_field)
-          case custom_field.input_type
-          when 'select', 'checkbox', 'number'
-            users.select("custom_field_values->'#{custom_field.key}' as field_value")
-          when 'multiselect'
-            users.joins(<<~SQL.squish).select('cfv.field_value as field_value')
-              LEFT JOIN (
-                SELECT
-                  jsonb_array_elements(custom_field_values->'#{custom_field.key}') as field_value,
-                  id as user_id
-                FROM users
-              ) as cfv
-              ON users.id = cfv.user_id
-            SQL
-          else
-            raise NotSupportedFieldTypeError
-          end
         end
 
         def users_by_custom_field
@@ -158,6 +112,52 @@ module UserCustomFields
         def custom_field_key_from_path
           request.path.split('/').last
                  .match(/^users_by_(?<key>gender|education|birthyear|domicile)/)[:key]
+        end
+
+        def user_counts
+          count_users_by_custom_field(find_users, custom_field)
+        end
+
+        def count_users_by_custom_field(users, custom_field)
+          field_values = select_field_values(users, custom_field)
+
+          # Warning: The method +count+ cannot be used here because it introduces a SQL syntax
+          # error while rewriting the SELECT clause. This is because the 'field_value' column is a
+          # computed column and it does not seem to be supported properly by the +count+ method.
+          counts = field_values.order('field_value')
+                               .group('field_value')
+                               .select('COUNT(*) as count')
+                               .to_a.pluck(:field_value, :count).to_h
+
+          counts['_blank'] = counts.delete(nil) || 0
+          counts
+        end
+
+        # Returns an ActiveRecord::Relation of all the custom field values for the given users.
+        # It returns a view (result set) with a single column named 'field_value'. Essentially,
+        # something that looks like:
+        #   SELECT ... AS field_value FROM ...
+        #
+        # Each user results in one or multiple rows, depending on the type of custom field.
+        # Custom fields with multiple values (e.g. multiselect) are returned as multiple rows.
+        # If the custom field has no value for a given user, the resulting row contains NULL.
+        def select_field_values(users, custom_field)
+          case custom_field.input_type
+          when 'select', 'checkbox', 'number'
+            users.select("custom_field_values->'#{custom_field.key}' as field_value")
+          when 'multiselect'
+            users.joins(<<~SQL.squish).select('cfv.field_value as field_value')
+              LEFT JOIN (
+                SELECT
+                  jsonb_array_elements(custom_field_values->'#{custom_field.key}') as field_value,
+                  id as user_id
+                FROM users
+              ) as cfv
+              ON users.id = cfv.user_id
+            SQL
+          else
+            raise NotSupportedFieldTypeError
+          end
         end
 
         def do_authorize
