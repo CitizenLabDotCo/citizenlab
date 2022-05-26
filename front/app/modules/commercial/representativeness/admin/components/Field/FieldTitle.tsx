d7ef6f1e86c027f264642f35b703741fbf3a97c9
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-05-26 10:47:15 +0100

    Rewrite field using Accordion (WIP)

---
+++ front/app/modules/commercial/representativeness/admin/components/Field/FieldTitle.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import Tippy from '@tippyjs/react';
+
+// hooks
+import useLocalize from 'hooks/useLocalize';
+
+// components
+import { Box, Toggle, Title, Text } from '@citizenlab/cl2-component-library';
+
+// styling
+import styled from 'styled-components';
+
+// i18n
+import messages from './messages';
+import { FormattedMessage } from 'utils/cl-intl';
+
+// typings
+import { Multiloc } from 'typings';
+
+const StyledText = styled(Text)`
+  font-weight: 500;
+`;
+
+const TooltipContent = styled.p`
+  color: white;
+  margin: 12px 16px;
+  font-size: 14px;
+  font-weight: 500px;
+`;
+
+interface Props {
+  enabled: boolean;
+  titleMultiloc: Multiloc;
+  isDefault: boolean;
+  onToggleEnabled: (event: React.FormEvent) => void;
+}
+
+const FieldTitle = ({
+  enabled,
+  titleMultiloc,
+  isDefault,
+  onToggleEnabled,
+}: Props) => {
+  const localize = useLocalize();
+
+  const handleToggle = (event: React.FormEvent) => {
+    event.stopPropagation();
+    onToggleEnabled(event);
+  };
+
+  return (
+    <Box py="20px" display="flex" justifyContent="space-between" width="100%">
+      <Tippy
+        placement="top"
+        content={
+          <TooltipContent>
+            {enabled ? (
+              <FormattedMessage {...messages.shownOnDashboard} />
+            ) : (
+              <FormattedMessage {...messages.hiddenOnDashboard} />
+            )}
+          </TooltipContent>
+        }
+      >
+        <Box display="flex" alignItems="center">
+          <Toggle checked={enabled} onChange={handleToggle} />
+        </Box>
+      </Tippy>
+      <Title variant="h4" as="h3" mt="0px" mb="0px" ml="12px">
+        {localize(titleMultiloc)}
+      </Title>
+
+      {isDefault && (
+        <StyledText mt="0px" mb="0px" variant="bodyS" color="adminTextColor">
+          <FormattedMessage {...messages.defaultField} />
+        </StyledText>
+      )}
+    </Box>
+  );
+};
+
+export default FieldTitle;
--- front/app/modules/commercial/representativeness/admin/components/Field/index.tsx
+++ front/app/modules/commercial/representativeness/admin/components/Field/index.tsx
@@ -1,25 +1,8 @@
 import React from 'react';
-import Tippy from '@tippyjs/react';
-
-// hooks
-import useLocalize from 'hooks/useLocalize';
 
 // components
-import {
-  Box,
-  Toggle,
-  Title,
-  Text,
-  Icon,
-} from '@citizenlab/cl2-component-library';
-
-// styling
-import styled from 'styled-components';
-import { colors } from 'utils/styleUtils';
-
-// i18n
-import messages from './messages';
-import { FormattedMessage } from 'utils/cl-intl';
+import { Accordion, Box } from '@citizenlab/cl2-component-library';
+import FieldTitle from './FieldTitle';
 
 // typings
 import { Multiloc } from 'typings';
@@ -31,73 +14,27 @@ interface Props {
   onToggleEnabled: (event: React.FormEvent) => void;
 }
 
-const StyledText = styled(Text)`
-  font-weight: 500;
-`;
-
-const StyledIcon = styled(Icon)`
-  transform: translateY(-1px);
-`;
-
-const TooltipContent = styled.p`
-  color: white;
-  margin: 12px 16px;
-  font-size: 14px;
-  font-weight: 500px;
-`;
-
 const Field = ({
   enabled,
   titleMultiloc,
   isDefault,
   onToggleEnabled,
 }: Props) => {
-  const localize = useLocalize();
-
   return (
-    <Box
-      display="flex"
-      justifyContent="space-between"
-      alignItems="center"
-      py="20px"
-      borderTop={`1px solid ${colors.separation}`}
-    >
-      <Box display="flex" alignItems="center">
-        <Tippy
-          placement="top"
-          content={
-            <TooltipContent>
-              {enabled ? (
-                <FormattedMessage {...messages.shownOnDashboard} />
-              ) : (
-                <FormattedMessage {...messages.hiddenOnDashboard} />
-              )}
-            </TooltipContent>
-          }
-        >
-          <Box display="flex" alignItems="center">
-            <Toggle checked={enabled} onChange={onToggleEnabled} />
-          </Box>
-        </Tippy>
-        <Title variant="h4" as="h3" mt="0px" mb="0px" ml="12px">
-          {localize(titleMultiloc)}
-        </Title>
-      </Box>
-
-      <Box display="flex" alignItems="center">
-        {isDefault && (
-          <StyledText mt="0px" mb="0px" variant="bodyS" color="adminTextColor">
-            <FormattedMessage {...messages.defaultField} />
-          </StyledText>
-        )}
-        <StyledIcon
-          name="chevron-right"
-          height="12px"
-          fill={colors.label}
-          ml="16px"
-        />
-      </Box>
-    </Box>
+    <>
+      <Accordion
+        title={
+          <FieldTitle
+            enabled={enabled}
+            titleMultiloc={titleMultiloc}
+            isDefault={isDefault}
+            onToggleEnabled={onToggleEnabled}
+          />
+        }
+      >
+        <Box>SUPPP</Box>
+      </Accordion>
+    </>
   );
 };
 
