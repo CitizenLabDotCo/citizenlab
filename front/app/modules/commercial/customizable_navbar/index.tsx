49231e9d80ebbd4717f40539d0dfb3f879c2d8ff
brentguf <brentguf@users.noreply.github.com>
2022-05-30 11:54:48 +0200

    Fix navigation index rendering

--- front/app/modules/commercial/customizable_navbar/index.tsx
+++ front/app/modules/commercial/customizable_navbar/index.tsx
@@ -26,7 +26,7 @@ const configuration: ModuleConfiguration = {
         element: <CustomNavbarContainer />,
         children: [
           {
-            index: true,
+            path: '',
             element: <CustomNavbarSettingsComponent />,
           },
           {
