2195b83c58daf1a64755b2dbf93bdf92ddaaf0ca
brentguf <brentguf@users.noreply.github.com>
2022-05-31 19:01:01 +0200

    Type children prop, don't use FC

--- front/app/components/UI/Loading/index.tsx
+++ front/app/components/UI/Loading/index.tsx
@@ -1,15 +1,14 @@
-import React, { FC, Suspense } from 'react';
+import React, { Suspense } from 'react';
 import FullPageSpinner from '../FullPageSpinner';
 
 interface Props {
   // If we're in the admin, the loader will take into account
   // the admin navbar's width.
   admin?: boolean;
-  // possibly add children prop and remove FC
-  // https://medium.com/raccoons-group/why-you-probably-shouldnt-use-react-fc-to-type-your-react-components-37ca1243dd13
+  children: React.ReactNode;
 }
 
-const Loading: FC<Props> = ({ admin = false, children }) => {
+const Loading = ({ admin = false, children }: Props) => {
   return (
     <Suspense fallback={<FullPageSpinner admin={admin} />}>{children}</Suspense>
   );
