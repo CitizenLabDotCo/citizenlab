2dd3ebdd855d074b289e761a3ac2a2703295bbb9
Adrien Dessy <adrien@citizenlab.co>
2022-05-31 10:10:32 +0200

    CL-610 Change TODO as suggested in review
    > I would write "TODO tech debt:" at the beginning of this comment, because for the reader it is not clear whether it is a to-do for you, or a general to-do, i.e. tech debt.

--- back/engines/commercial/user_custom_fields/app/models/user_custom_fields/patches/custom_field.rb
+++ back/engines/commercial/user_custom_fields/app/models/user_custom_fields/patches/custom_field.rb
@@ -3,7 +3,7 @@
 module UserCustomFields::Patches::CustomField
   def self.included(base)
     base.class_eval do
-      # TODO: Refactor CustomFields to use STI with two types: UserCustomField & FormCustomField
+      # TODO: (tech debt) Refactor CustomFields to use STI with two types: UserCustomField & FormCustomField
       # `UserCustomField` would replace this patch (and live in the `user_custom_fields` engine)
       # and `FormCustomField` would belong to the core.
       has_many(
