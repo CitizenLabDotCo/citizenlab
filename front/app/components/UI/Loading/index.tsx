228360dc14036b7b7db04620cfe37b90481eb24a
brentguf <brentguf@users.noreply.github.com>
2022-05-31 15:50:07 +0200

    Remove LoadableLoadingCitizen/Admin

--- front/app/components/UI/Loading/index.tsx
+++ front/app/components/UI/Loading/index.tsx
@@ -1,26 +1,5 @@
 import React, { FC, Suspense } from 'react';
 import FullPageSpinner from '../FullPageSpinner';
-import { LoadingComponentProps } from 'react-loadable';
-
-export const LoadableLoadingCitizen = (props: LoadingComponentProps) => {
-  if (process.env.NODE_ENV === 'development' && props.error) {
-    // eslint-disable-next-line no-console
-    console.log(props.error);
-  }
-  if (props.pastDelay) {
-    return <FullPageSpinner />;
-  }
-
-  return null;
-};
-
-export const LoadableLoadingAdmin = (props: LoadingComponentProps) => {
-  if (props.pastDelay) {
-    return <FullPageSpinner admin={true} />;
-  }
-
-  return null;
-};
 
 interface Props {
   admin?: boolean;
