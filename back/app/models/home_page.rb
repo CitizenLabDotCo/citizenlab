42c55987e8ec2d9d7ec97be01b090545b0830d0a
Philipp Preß <ntraum@fastmail.com>
2022-06-03 11:28:37 +0200

    Only validate info sections when they are enabled

--- back/app/models/home_page.rb
+++ back/app/models/home_page.rb
@@ -31,14 +31,14 @@
 #  updated_at                               :datetime         not null
 #
 class HomePage < ApplicationRecord
-  before_validation :sanitize_top_info_section_multiloc, if: :top_info_section_enabled?
-  before_validation :sanitize_bottom_info_section_multiloc, if: :bottom_info_section_enabled?
+  before_validation :sanitize_top_info_section_multiloc, if: :top_info_section_enabled
+  before_validation :sanitize_bottom_info_section_multiloc, if: :bottom_info_section_enabled
 
   validates :top_info_section_enabled, inclusion: [true, false]
-  validates :top_info_section_multiloc, presence: true, multiloc: { html: true, presence: true }
+  validates :top_info_section_multiloc, presence: true, multiloc: { html: true, presence: true }, if: :top_info_section_enabled
 
   validates :bottom_info_section_enabled, inclusion: [true, false]
-  validates :bottom_info_section_multiloc, presence: true, multiloc: { html: true, presence: true }
+  validates :bottom_info_section_multiloc, presence: true, multiloc: { html: true, presence: true }, if: :bottom_info_section_enabled
 
   validates :events_enabled, inclusion: [true, false]
   validates :projects_enabled, inclusion: [true, false]
