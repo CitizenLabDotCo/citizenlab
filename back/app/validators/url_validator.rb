e2f59d876155c2211bdb63b49e4e05ff1b87a37b
Philipp Preß <ntraum@fastmail.com>
2022-06-03 15:17:16 +0200

    Fix code comment format

--- back/app/validators/url_validator.rb
+++ back/app/validators/url_validator.rb
@@ -4,7 +4,7 @@
 class UrlValidator < ActiveModel::EachValidator
   URL_PATTERN = %r{^$|^(http(s)?://.+)}.freeze
 
-  # Validates that the attribute is a valid URL. Allowed are URLs starting with `https://`  or `http://`.
+  # Validates that the attribute is a valid URL. Allowed are URLs starting with `https://` or `http://`.
   def validate_each(record, attribute, value)
     return if value.nil?
 
