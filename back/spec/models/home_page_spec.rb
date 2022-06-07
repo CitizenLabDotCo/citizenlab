f036f9f8728aa1b4cdf6d85d0593b4c80493616d
Philipp Preß <ntraum@fastmail.com>
2022-06-07 10:55:34 +0200

    Remove unnecessary save

--- back/spec/models/home_page_spec.rb
+++ back/spec/models/home_page_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe HomePage, type: :model do
     it 'only allows once instance of homepage to exist' do
       create(:home_page)
       second_home_page = build(:home_page)
-      expect(second_home_page.save).to be(false)
+      expect(second_home_page).to be_invalid
       expect(second_home_page.errors[:base]).not_to be_empty
     end
   end
