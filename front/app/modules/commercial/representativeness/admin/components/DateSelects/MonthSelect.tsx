bf5fb1dd0bca9e57b4154f8be5d241e0df0272a7
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-06-09 15:31:17 +0100

    Implement MonthSelect

---
+++ front/app/modules/commercial/representativeness/admin/components/DateSelects/MonthSelect.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+// components
+import { Select, SelectProps } from '@citizenlab/cl2-component-library';
+
+// i18n
+import messages from './messages';
+import { injectIntl } from 'utils/cl-intl';
+import { InjectedIntlProps } from 'react-intl';
+
+// utils
+import { capitalize } from 'lodash-es';
+
+// typings
+import { MessageDescriptor } from 'typings';
+
+type MonthNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
+interface Month {
+  monthNumber: MonthNumber;
+  monthMessage: MessageDescriptor;
+}
+
+const MONTHS: Month[] = [
+  { monthNumber: 1, monthMessage: messages.january },
+  { monthNumber: 2, monthMessage: messages.february },
+  { monthNumber: 3, monthMessage: messages.march },
+  { monthNumber: 4, monthMessage: messages.april },
+  { monthNumber: 5, monthMessage: messages.may },
+  { monthNumber: 6, monthMessage: messages.june },
+  { monthNumber: 7, monthMessage: messages.july },
+  { monthNumber: 8, monthMessage: messages.august },
+  { monthNumber: 9, monthMessage: messages.september },
+  { monthNumber: 10, monthMessage: messages.october },
+  { monthNumber: 11, monthMessage: messages.november },
+  { monthNumber: 12, monthMessage: messages.december },
+];
+
+interface IMonthSelectOption {
+  value: MonthNumber;
+  label: string;
+}
+
+type OmittedSelectProps = 'onChange' | 'value' | 'options';
+
+interface Props extends Omit<SelectProps, OmittedSelectProps> {
+  onChange: (option: IMonthSelectOption) => void;
+  value: IMonthSelectOption | MonthNumber;
+}
+
+const MonthSelect = ({
+  intl: { formatMessage },
+  ...otherProps
+}: Props & InjectedIntlProps) => {
+  const options: IMonthSelectOption[] = MONTHS.map(
+    ({ monthNumber, monthMessage }) => ({
+      value: monthNumber,
+      label: capitalize(formatMessage(monthMessage)),
+    })
+  );
+
+  return <Select options={options} {...otherProps} />;
+};
+
+export default injectIntl(MonthSelect);
---
+++ front/app/modules/commercial/representativeness/admin/components/DateSelects/messages.ts
@@ -0,0 +1,52 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  january: {
+    id: 'app.containers.AdminPage.DashboardPage.components.DateSelects.january',
+    defaultMessage: 'january',
+  },
+  february: {
+    id: 'app.containers.AdminPage.DashboardPage.components.DateSelects.february',
+    defaultMessage: 'february',
+  },
+  march: {
+    id: 'app.containers.AdminPage.DashboardPage.components.DateSelects.march',
+    defaultMessage: 'march',
+  },
+  april: {
+    id: 'app.containers.AdminPage.DashboardPage.components.DateSelects.april',
+    defaultMessage: 'april',
+  },
+  may: {
+    id: 'app.containers.AdminPage.DashboardPage.components.DateSelects.may',
+    defaultMessage: 'may',
+  },
+  june: {
+    id: 'app.containers.AdminPage.DashboardPage.components.DateSelects.june',
+    defaultMessage: 'june',
+  },
+  july: {
+    id: 'app.containers.AdminPage.DashboardPage.components.DateSelects.july',
+    defaultMessage: 'july',
+  },
+  august: {
+    id: 'app.containers.AdminPage.DashboardPage.components.DateSelects.august',
+    defaultMessage: 'august',
+  },
+  september: {
+    id: 'app.containers.AdminPage.DashboardPage.components.DateSelects.september',
+    defaultMessage: 'september',
+  },
+  october: {
+    id: 'app.containers.AdminPage.DashboardPage.components.DateSelects.october',
+    defaultMessage: 'october',
+  },
+  november: {
+    id: 'app.containers.AdminPage.DashboardPage.components.DateSelects.november',
+    defaultMessage: 'november',
+  },
+  december: {
+    id: 'app.containers.AdminPage.DashboardPage.components.DateSelects.december',
+    defaultMessage: 'december',
+  },
+});
