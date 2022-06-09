50404dad15fe7f77856a3e42f6ecab06ca3f8083
Adrien Dessy <adrien@citizenlab.co>
2022-06-09 10:52:51 +0200

    CL-844 Improve comment

--- back/engines/commercial/user_custom_fields/app/serializers/user_custom_fields/web_api/v1/user_custom_field_serializer.rb
+++ back/engines/commercial/user_custom_fields/app/serializers/user_custom_fields/web_api/v1/user_custom_field_serializer.rb
@@ -5,13 +5,14 @@ module UserCustomFields
     module V1
       class UserCustomFieldSerializer < ::WebApi::V1::CustomFieldSerializer
         # Warning: This creates an N+1 query problem.
-        # If the performance becomes an issue, +current_ref_distribution+ should be implemented as
-        # an association on CustomField, either through a +belongs_to+ or an +has_one+.
-        # * +belongs_to+ would probably be the best option, but it's more work to implement.
-        #   (It would certainly require a join table and we would have to implement the lifecycle
-        #   logic to keep that table up to date.)
-        # * +has_one+ is possible also and cheaper to implement, but it may result in unexpected
-        #   behavior when combined into more complex queries.
+        # If the performance becomes an issue, +current_ref_distribution+ method should
+        # be rewritten as an association in the +CustomField+ *model*, either through a
+        # +belongs_to+ or an +has_one+.
+        # * +belongs_to+ would probably be the best option, but it's more work to
+        #   implement. (It would certainly require a join table and we would have to
+        #   implement the lifecycle logic to keep that table up to date.)
+        # * +has_one+ is possible also and cheaper to implement, but it may result in
+        #   unexpected behavior when combined into more complex queries.
         #   See: https://stackoverflow.com/questions/15824041/rails-associations-has-one-latest-record.
         has_one(
           :current_ref_distribution,
