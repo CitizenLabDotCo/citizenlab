69a92a744ace67b45d3b0c6b15bc0c261f4a7412
Philipp Preß <ntraum@fastmail.com>
2022-06-10 15:47:39 +0200

    More initializers from Rails 7

--- back/config/boot.rb
+++ back/config/boot.rb
@@ -2,21 +2,5 @@
 
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-# To benchmark boot time, uncomment following monkey patch
-# from  https://mildlyinternet.com/code/profiling-rails-boot-time.html
-#
-# require "benchmark"
-#
-# def require(file_name)
-#   result = nil
-#   time = Benchmark.realtime do
-#     result = super
-#   end
-#   if time > 0.1
-#     puts "#{time} #{file_name}"
-#   end
-#   result
-# end
-
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup'
+require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
