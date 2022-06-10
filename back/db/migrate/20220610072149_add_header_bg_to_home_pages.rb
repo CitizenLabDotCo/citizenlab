d6cf6f38a278df8ed5ff4fc45af97eb7ed007aab
Philipp Preß <ntraum@fastmail.com>
2022-06-10 14:20:38 +0200

    Fix Rubocop offenses

--- back/db/migrate/20220610072149_add_header_bg_to_home_pages.rb
+++ back/db/migrate/20220610072149_add_header_bg_to_home_pages.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddHeaderBgToHomePages < ActiveRecord::Migration[6.1]
   def change
     add_column :home_pages, :header_bg, :string
--- back/spec/uploaders/header_bg_uploader_spec.rb
+++ back/spec/uploaders/header_bg_uploader_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'carrierwave/test/matchers'
 require 'rails_helper'
 
@@ -5,7 +7,12 @@ RSpec.describe HeaderBgUploader do
   include CarrierWave::Test::Matchers
 
   let(:uploader) { described_class.new(home_page) }
+
+  # rubocop:disable RSpec/VerifiedDoubles
+  # Yes in general it's much more robust to use VerifiedDoubles, but in this test case we really just need any kind of
+  # object which we can use with the uploader. It does not need to based on a real class.
   let(:home_page) { double('home_page', header_bg: nil, id: '812451ad-9ff4-47c3-966a-5ebd935f0f73') }
+  # rubocop:enable RSpec/VerifiedDoubles
 
   before do
     described_class.enable_processing = true
