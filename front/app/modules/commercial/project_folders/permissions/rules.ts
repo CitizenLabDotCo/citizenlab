266b2ef2fdab79d06632c76a41df7e4dcd4c9d93
lucas <lucasquinnmathis@gmail.com>
2022-06-13 18:55:05 +0200

    Remove permissions check for admin folders

--- front/app/modules/commercial/project_folders/permissions/rules.ts
+++ front/app/modules/commercial/project_folders/permissions/rules.ts
@@ -1,32 +1,9 @@
 import { IProjectFolderData } from './../services/projectFolders';
-import {
-  definePermissionRule,
-  IRouteItem,
-} from 'services/permissions/permissions';
-import { isProjectFolderModerator, moderatesFolder } from './roles';
+import { definePermissionRule } from 'services/permissions/permissions';
+import { moderatesFolder } from './roles';
 import { isAdmin } from 'services/permissions/roles';
-import {
-  canAccessRoute,
-  isAdminRoute,
-  MODERATOR_ROUTES,
-} from 'services/permissions/rules/routePermissions';
+
 import { IUser } from 'services/users';
-import { IAppConfigurationData } from 'services/appConfiguration';
-
-const canUserAccessAdminFolderRoute = (
-  item: IRouteItem,
-  user: IUser | null,
-  tenant: IAppConfigurationData
-) => {
-  return (
-    canAccessRoute(item, user, tenant) ||
-    (isAdminRoute(item.path) &&
-      (user ? isProjectFolderModerator(user.data) : false) &&
-      MODERATOR_ROUTES.includes(item.path))
-  );
-};
-
-definePermissionRule('route', 'access', canUserAccessAdminFolderRoute);
 
 definePermissionRule(
   'project_folder',
