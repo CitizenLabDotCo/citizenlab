87dee9362e59eda400a059d5ae0d045df924f31e
brentguf <brentguf@users.noreply.github.com>
2022-06-13 17:48:08 +0200

    Undo idRegexp change

--- front/app/services/permissions/rules/routePermissions.ts
+++ front/app/services/permissions/rules/routePermissions.ts
@@ -30,7 +30,7 @@ export const isModeratedProjectRoute = (
   item: IRouteItem,
   user: IUser | null
 ) => {
-  const idRegexp = /^\/admin\/projects\/([a-z0-9-]+)\/?/;
+  const idRegexp = /^\/admin\/projects\/([a-z0-9-]+)\//;
   const matches = idRegexp.exec(item.path);
   const pathProjectId = matches && matches[1];
   return (pathProjectId && isProjectModerator(user, pathProjectId)) || false;
