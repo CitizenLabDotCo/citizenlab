f6cc2320f3f8e280eb869914687d589c8eb071bb
Alexander <alexander@citizenlab.co>
2022-06-14 12:06:28 +0200

    [CL-870] tmp2

--- front/app/modules/commercial/customizable_navbar/admin/containers/index.tsx
+++ front/app/modules/commercial/customizable_navbar/admin/containers/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import useFeatureFlag from 'hooks/useFeatureFlag';
 import { Outlet as RouterOutlet } from 'react-router-dom';
 
-export const NAVIGATION_PATH = '/admin/settings/navigation';
+export const NAVIGATION_PATH = '/admin/pages-menu';
 
 export default () => {
   const featureEnabled = useFeatureFlag({ name: 'customizable_navbar' });
