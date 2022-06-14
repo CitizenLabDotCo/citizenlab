23b1e733e10c604a0225f0e41e18d6fa27aaf1ff
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-06-14 14:03:24 +0100

    [CL-767] ChartCard Header: only show xlsx endpoint link if showing table

--- front/app/modules/commercial/representativeness/admin/components/ChartCard/Header.tsx
+++ front/app/modules/commercial/representativeness/admin/components/ChartCard/Header.tsx
@@ -109,10 +109,8 @@ const Header = ({
       <Box mr="12px">
         <ReportExportMenu
           name={title}
-          currentProjectFilter={
-            viewState === 'table' ? projectFilter : undefined
-          }
-          xlsxEndpoint={xlsxEndpoint}
+          currentProjectFilter={projectFilter}
+          xlsxEndpoint={viewState === 'table' ? xlsxEndpoint : undefined}
           svgNode={viewState === 'chart' ? svgNode : undefined}
         />
       </Box>
