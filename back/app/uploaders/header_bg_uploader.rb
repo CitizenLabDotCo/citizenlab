028c42bbafc1f5174c610023c549cae418f23b9d
Philipp Preß <ntraum@fastmail.com>
2022-06-14 09:58:08 +0200

    [CL-909] Remove custom `store_dir` for HeaderBgUploader
    We don't need it anymore, see https://github.com/CitizenLabDotCo/citizenlab/pull/2151#issuecomment-1153653877 for details.
    
    Also https://github.com/CitizenLabDotCo/citizenlab/pull/2151#issuecomment-1154011221:
    
    > I'm not sure if we can trust old me on this one. But I could find traces of a differentiated treatment between store_dir that predates those commits (in the old cl2-back repository). I'd say the main reason behind this difference was that in some cases (maybe via the admin API) the code of the uploader was run outside the context of a tenant. But I guess it should not be the case anymore if it's moved to this new HomePage model.

--- back/app/uploaders/header_bg_uploader.rb
+++ back/app/uploaders/header_bg_uploader.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
-# Uploads a header background image in large, medium, small sizes and stores them in a directory based on the
-# AppConfiguration id and model id.
+# Uploads a header background image in large, medium, small sizes.
 class HeaderBgUploader < BaseImageUploader
-  def store_dir
-    File.join('uploads', AppConfiguration.instance.id, 'header-background', model.id)
-  end
-
   version :large do
     process safe_resize_to_fill_for_gif: [1920, 640]
   end
