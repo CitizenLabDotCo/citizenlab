2ddf9b84fa5c975a87f9ab7aa86340e2ae3e3ee7
Alexander <alexander@citizenlab.co>
2022-06-15 12:18:19 +0200

    [CL-870] render RouterOutlet in tabbedResources

--- front/app/modules/commercial/customizable_navbar/admin/containers/NavigationSettings/index.tsx
+++ front/app/modules/commercial/customizable_navbar/admin/containers/NavigationSettings/index.tsx
@@ -1,29 +1,18 @@
 import React from 'react';
 import { Box } from '@citizenlab/cl2-component-library';
-import { injectIntl } from 'utils/cl-intl';
-import { InjectedIntlProps } from 'react-intl';
 
 // components
 import VisibleNavbarItemList from './VisibleNavbarItemList';
 import HiddenNavbarItemList from './HiddenNavbarItemList';
 
-// i18n
-import messages from './messages';
-import TabbedResource from 'components/admin/TabbedResource';
-
-const NavigationSettings = ({ intl: { formatMessage } }: InjectedIntlProps) => (
-  <TabbedResource
-    resource={{
-      title: formatMessage(messages.pageHeader),
-      subtitle: formatMessage(messages.pageSubtitle),
-    }}
-  >
+const NavigationSettings = () => (
+  <>
     <Box mb="44px">
       <VisibleNavbarItemList />
     </Box>
 
     <HiddenNavbarItemList />
-  </TabbedResource>
+  </>
 );
 
-export default injectIntl(NavigationSettings);
+export default NavigationSettings;
