b6b41bfd96022f1ad461bb880ad047959bc2455f
Amanda Anderson <amanda.anderson@citizenlab.co>
2022-06-16 12:42:50 +0200

    [CL-885] working version without two column settings WIP

--- front/app/modules/commercial/content_builder/admin/components/CraftSections/InfoWithAccordions/index.tsx
+++ front/app/modules/commercial/content_builder/admin/components/CraftSections/InfoWithAccordions/index.tsx
@@ -4,12 +4,11 @@ import React from 'react';
 import { Box } from '@citizenlab/cl2-component-library';
 
 // craft
-import { UserComponent, Element } from '@craftjs/core';
+import { UserComponent } from '@craftjs/core';
 import Accordion from '../../CraftComponents/Accordion';
 import { TwoColumn } from '../../CraftComponents/TwoColumn';
 import Text from '../../CraftComponents/Text';
 import AboutBox from '../../CraftComponents/AboutBox';
-import Container from '../../CraftComponents/Container';
 
 const InfoWithAccordions: UserComponent = ({
   projectId,
@@ -18,7 +17,7 @@ const InfoWithAccordions: UserComponent = ({
   accordionText,
 }) => {
   return (
-    <Element is={Container} canvas id="infoWithAccordions">
+    <div>
       <TwoColumn
         columnLayout="2-1"
         rightChildren={<AboutBox projectId={projectId} />}
@@ -43,7 +42,7 @@ const InfoWithAccordions: UserComponent = ({
           </Box>
         }
       />
-    </Element>
+    </div>
   );
 };
 
