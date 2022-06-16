99dee10408fbc28691a12637d60da7aa350fdd11
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-06-16 14:18:50 +0100

    [CL-851] Remove reordering functionality

--- front/app/modules/commercial/representativeness/admin/components/Field/Options/index.tsx
+++ front/app/modules/commercial/representativeness/admin/components/Field/Options/index.tsx
@@ -7,15 +7,11 @@ import useLocalize from 'hooks/useLocalize';
 
 // components
 import { Box, Text } from '@citizenlab/cl2-component-library';
-import { SortableList, SortableRow } from 'components/admin/ResourceList';
 import OptionToggle from './OptionToggle';
 import OptionInput from './OptionInput';
 
 // utils
 import { isNilOrError } from 'utils/helperUtils';
-
-// typings
-import { IUserCustomFieldOptionData } from 'modules/commercial/user_custom_fields/services/userCustomFieldOptions';
 
 interface Props {
   fieldId: string;
@@ -51,36 +47,23 @@ const Options = ({ fieldId }: Props) => {
   if (isNilOrError(userCustomFieldOptions)) return null;
 
   const renderFn = () => (
-    <SortableList items={userCustomFieldOptions} onReorder={console.log}>
-      {({ itemsList, handleDragRow, handleDropRow }) => (
+    <>
+      {userCustomFieldOptions.map(({ id, attributes }) => (
         <>
-          {itemsList.map(
-            ({ id, attributes }: IUserCustomFieldOptionData, index: number) => (
-              <SortableRow
-                key={id}
-                id={id}
-                index={index}
-                moveRow={handleDragRow}
-                dropRow={handleDropRow}
-                noStyling
-              >
-                <Box pl="8px" display="flex" alignItems="center" width="50%">
-                  <OptionToggle optionId={id} />
+          <Box pl="8px" display="flex" alignItems="center" width="50%">
+            <OptionToggle optionId={id} />
 
-                  <Text ml="12px" variant="bodyM" color="adminTextColor">
-                    {localize(attributes.title_multiloc)}
-                  </Text>
-                </Box>
+            <Text ml="12px" variant="bodyM" color="adminTextColor">
+              {localize(attributes.title_multiloc)}
+            </Text>
+          </Box>
 
-                <Box ml="-20px" display="flex" alignItems="center" width="50%">
-                  <OptionInput optionId={id} />
-                </Box>
-              </SortableRow>
-            )
-          )}
+          <Box ml="-20px" display="flex" alignItems="center" width="50%">
+            <OptionInput optionId={id} />
+          </Box>
         </>
-      )}
-    </SortableList>
+      ))}
+    </>
   );
 
   return (
