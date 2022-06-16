3d6c9e86d8a0aa4baca12d95a82b0169147056c0
Philipp Preß <ntraum@fastmail.com>
2022-06-16 11:00:36 +0200

    [CL-895] Skip pending example

--- back/spec/models/pin_spec.rb
+++ back/spec/models/pin_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Pin, type: :model do
     expect(described_class.create!(page: home_page, admin_publication: project.admin_publication)).to be_persisted
   end
 
-  it 'can relate to a folder' do
+  it 'can relate to a folder', skip: 'cannot reference project_folder factory because it lives in a separate engine' do
     expect(described_class.create!(page: home_page, admin_publication: project_folder.admin_publication)).to be_persisted
   end
 
