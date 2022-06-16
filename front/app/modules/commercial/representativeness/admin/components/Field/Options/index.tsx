66ba21feade30021e89820335a32d1dc9d07bdb5
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-06-16 14:30:21 +0100

    [CL-851] Add submit button

--- front/app/modules/commercial/representativeness/admin/components/Field/Options/index.tsx
+++ front/app/modules/commercial/representativeness/admin/components/Field/Options/index.tsx
@@ -9,6 +9,7 @@ import useLocalize from 'hooks/useLocalize';
 import { Box, Text } from '@citizenlab/cl2-component-library';
 import OptionToggle from './OptionToggle';
 import OptionInput from './OptionInput';
+import FormikSubmitWrapper from 'components/admin/FormikSubmitWrapper';
 
 // utils
 import { isNilOrError } from 'utils/helperUtils';
@@ -46,7 +47,12 @@ const Options = ({ fieldId }: Props) => {
 
   if (isNilOrError(userCustomFieldOptions)) return null;
 
-  const renderFn = ({ handleSubmit }: FormikProps<FormValues>) => (
+  const renderFn = ({
+    isSubmitting,
+    touched,
+    status,
+    handleSubmit,
+  }: FormikProps<FormValues>) => (
     <form onSubmit={handleSubmit}>
       {userCustomFieldOptions.map(({ id, attributes }) => (
         <>
@@ -63,6 +69,12 @@ const Options = ({ fieldId }: Props) => {
           </Box>
         </>
       ))}
+
+      <FormikSubmitWrapper
+        isSubmitting={isSubmitting}
+        status={status}
+        touched={touched}
+      />
     </form>
   );
 
