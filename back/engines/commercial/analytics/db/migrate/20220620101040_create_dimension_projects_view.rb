8f74c2fe7ca08c157721135e667a51a00e9dcea6
jamesspeake <james@speake.org>
2022-06-22 10:39:55 +0100

    [CL-976] Fixed issue with migration name

--- back/engines/commercial/analytics/db/migrate/20220620101040_create_dimension_projects_view.rb
+++ back/engines/commercial/analytics/db/migrate/20220620101040_create_dimension_projects_view.rb
@@ -1,4 +1,4 @@
-class CreateDimensionProjects < ActiveRecord::Migration[6.1]
+class CreateDimensionProjectsView < ActiveRecord::Migration[6.1]
   def change
     create_view :analytics_dimension_projects, materialized: true
 
