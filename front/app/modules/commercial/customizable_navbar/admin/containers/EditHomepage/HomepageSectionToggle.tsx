8f369ddf6738fac11b2889b31ee3fd9420dc10a6
brentguf <brentguf@users.noreply.github.com>
2022-06-22 16:54:52 +0200

    HomepageSectionToggle components + props

--- front/app/modules/commercial/customizable_navbar/admin/containers/EditHomepage/HomepageSectionToggle.tsx
+++ front/app/modules/commercial/customizable_navbar/admin/containers/EditHomepage/HomepageSectionToggle.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
+// components
+import {
+  IconTooltip,
+  Toggle,
+  Box,
+  Title,
+} from '@citizenlab/cl2-component-library';
+import { Row } from 'components/admin/ResourceList';
+import AdminEditButton from './AdminEditButton';
 
-interface Props {}
+const StyledToggle = styled(Toggle)`
+  margin-right: 20px;
+`;
 
-const HomepageSectionToggle = ({}: Props) => {
-  return <div>Hello, world!</div>;
+interface Props {
+  onChangeSectionToggle: () => void;
+  onClickEditButton: () => void;
+}
+
+const HomepageSectionToggle = ({
+  onChangeSectionToggle,
+  onClickEditButton,
+}: Props) => {
+  return (
+    <Row>
+      <Box display="flex" alignItems="center">
+        <StyledToggle checked onChange={onChangeSectionToggle} />
+        {/*
+      Note: I think we want a default font-weigt of 600, not 700 for this component.
+      Also, the margin-top is more than margin-bottom (for an h3).
+      */}
+        <Title variant="h3" mr="10px">
+          Hero banner
+        </Title>
+        <IconTooltip content="" />
+      </Box>
+      <AdminEditButton onClick={onClickEditButton} />
+    </Row>
+  );
 };
 
 export default HomepageSectionToggle;
--- front/app/modules/commercial/customizable_navbar/admin/containers/EditHomepage/index.tsx
+++ front/app/modules/commercial/customizable_navbar/admin/containers/EditHomepage/index.tsx
@@ -2,14 +2,34 @@ import React from 'react';
 import HomepageSectionToggle from './HomepageSectionToggle';
 
 const EditHomepage = () => {
+  const handleOnChangeToggle = () => {};
+  const handleOnClick = () => {};
   return (
     <div>
-      <HomepageSectionToggle />
-      <HomepageSectionToggle />
-      <HomepageSectionToggle />
-      <HomepageSectionToggle />
-      <HomepageSectionToggle />
-      <HomepageSectionToggle />
+      <HomepageSectionToggle
+        onChangeSectionToggle={handleOnChangeToggle}
+        onClickEditButton={handleOnClick}
+      />
+      <HomepageSectionToggle
+        onChangeSectionToggle={handleOnChangeToggle}
+        onClickEditButton={handleOnClick}
+      />
+      <HomepageSectionToggle
+        onChangeSectionToggle={handleOnChangeToggle}
+        onClickEditButton={handleOnClick}
+      />
+      <HomepageSectionToggle
+        onChangeSectionToggle={handleOnChangeToggle}
+        onClickEditButton={handleOnClick}
+      />
+      <HomepageSectionToggle
+        onChangeSectionToggle={handleOnChangeToggle}
+        onClickEditButton={handleOnClick}
+      />
+      <HomepageSectionToggle
+        onChangeSectionToggle={handleOnChangeToggle}
+        onClickEditButton={handleOnClick}
+      />
     </div>
   );
 };
