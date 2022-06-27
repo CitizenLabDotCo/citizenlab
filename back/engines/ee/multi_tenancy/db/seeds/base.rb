b7424f5076abd4ed9a5d75578016cc7fda555cc6
Philipp <philipp@citizenlab.co>
2022-06-27 10:54:25 +0200

    Update back/engines/ee/multi_tenancy/db/seeds/base.rb
    Co-authored-by: Simon Tharby <simontharby@gmail.com>

--- back/engines/ee/multi_tenancy/db/seeds/base.rb
+++ back/engines/ee/multi_tenancy/db/seeds/base.rb
@@ -3,7 +3,7 @@
 module MultiTenancy
   module Seeds
     # Base class for seeding classes that seed specific records.
-    # The {#runner} has gives access to seeding helper methods, defaults and other config.
+    # The {#runner} has access to seeding helper methods, defaults and other config.
     # Child classes should overwrite the {#run} method, where the actual seeding then place.
     class Base
       attr_reader :runner
