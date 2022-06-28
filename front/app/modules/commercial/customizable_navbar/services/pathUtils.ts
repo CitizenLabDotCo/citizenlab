de86c7f8aa40fd9c9cf42fa69dd9aa59ea2949de
brentguf <brentguf@users.noreply.github.com>
2022-06-28 10:26:27 +0200

    adminPagesMenuPagesEditHomePath change to accomodate move to core

--- front/app/modules/commercial/customizable_navbar/services/pathUtils.ts
+++ front/app/modules/commercial/customizable_navbar/services/pathUtils.ts
@@ -3,8 +3,11 @@ const NAVIGATION_PATH = '/admin/pages-menu';
 function navigationPath(restOfPath: string) {
   return `${NAVIGATION_PATH}${restOfPath}`;
 }
+
 function adminPagesMenuPagesEditHomePath() {
-  return navigationPath('/pages/edit/home-page');
+  // could use type checking, I initially edited it to /pages/edit...,
+  // which resulted in a 404
+  return navigationPath('/edit/home-page');
 }
 
 export { adminPagesMenuPagesEditHomePath };
