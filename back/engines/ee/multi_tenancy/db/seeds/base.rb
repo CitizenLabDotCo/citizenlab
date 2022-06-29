f5e56aa166bab6b38a04ba2c45580cd46fc954fe
Philipp Preß <ntraum@fastmail.com>
2022-06-29 09:31:24 +0200

    Fix attr_reader not being private

--- back/engines/ee/multi_tenancy/db/seeds/base.rb
+++ back/engines/ee/multi_tenancy/db/seeds/base.rb
@@ -6,14 +6,16 @@ module MultiTenancy
     # The {#runner} has access to seeding helper methods, defaults and other config.
     # Child classes should overwrite the {#run} method, where the actual seeding takes place.
     class Base
-      attr_reader :runner
-
       # @param runner [MultiTenancy::Seeds::Runner]
       def initialize(runner:)
         @runner = runner
       end
 
       def run; end
+
+      private
+
+      attr_reader :runner
     end
   end
 end
