a15912a02355fd5be10cd818ca8847806da6deba
brentguf <brentguf@users.noreply.github.com>
2022-06-30 22:49:18 +0200

    IHomepageSettings WIP

--- front/app/services/homePages.ts
+++ front/app/services/homePages.ts
@@ -11,7 +11,32 @@ export interface IHomepageSectionMap {
 
 export type THomepageSection = IHomepageSectionMap[keyof IHomepageSectionMap];
 
-export interface IHomepageSettings {}
+
+export interface IHomepageSettings {
+  top_info_section_enabled: boolean;
+  top_info_section_multiloc: Multiloc;
+  bottom_info_section_enabled: boolean;
+  bottom_info_section_multiloc: Multiloc;
+  events_widget: boolean;
+  projects_enabled: boolean;
+  projects_header_multiloc: Multiloc;
+  banner_avatars_enabled: boolean;
+  customizable_homepage_banner: boolean;
+  banner_layout;
+  banner_signed_in_header_multiloc: Multiloc;
+  cta_signed_in_text_multiloc: Multiloc;
+  cta_signed_in_type;
+  cta_signed_in_url: string;
+  banner_signed_out_header_multiloc: Multiloc;
+  banner_signed_out_subheader_multiloc: Multiloc;
+  banner_signed_out_header_overlay_color;
+  banner_signed_out_header_overlay_opacity;
+  cta_signed_out_text_multiloc: Multiloc;
+  cta_signed_out_type;
+  cta_signed_out_url: string;
+  header_bg;
+  pinned_admin_publication_ids: string[];
+}
 
 export function homepageSettingsStream() {
   return streams.get<IHomepageSettings>({
