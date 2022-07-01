af98d3ce310a7d840beeccb9034a8e95b9aa5725
Edwin Kato <katedwin88@gmail.com>
2022-07-01 09:35:09 +0300

    [CL-1034] Fix lint issues on the backend

--- back/app/services/url_validation_service.rb
+++ back/app/services/url_validation_service.rb
@@ -1,40 +1,38 @@
 # frozen_string_literal: true
 
 class UrlValidationService
-  # Changed the format of these to them similar to those in the frontend
-  # This will make it easy to maintain them.
   URL_WHITELIST = [
-    /^(https?):\/\/(.+\.)typeform\.com\/to\//,
-    /^(https?):\/\/surveys.enalyzer.com\/?\?pid=.*/,
-    /^(https?):\/\/(www\.)?survey-xact\.dk\/LinkCollector\?key=.*/i,
-    /^(https?):\/\/(.+\.)qualtrics\.com\/jfe\/form\//,
-    /^(https?):\/\/(www\.)?smartsurvey\.co\.uk\//,
-    /^(https?):\/\/(.+\.)(microsoft|office)\.com\//,
-    /^(https?):\/\/(www\.)?eventbrite\.([-A-Z0-9+&@#\/%=~_|!:,.;]+)/i,
-    /^(https?):\/\/(www\.)?(arcgis|arcg)\.([-A-Z0-9+&@#\/%=~_|!:,.;]+)/i,
-    /^(https?):\/\/public\.tableau\.com\/([-A-Z0-9+&@#\/%=~_|!:,.;?]+)/i,
-    /^(https?):\/\/datastudio\.google\.com\/embed\//,
-    /^(https?):\/\/app\.powerbi\.com\//,
-    /^(https?):\/\/static\.ctctcdn\.com\/js\//,
-    /^(https?):\/\/(www\.)?instagram\.com\//,
-    /^(https?):\/\/platform\.twitter\.com\//,
-    /^(https?):\/\/.+\.konveio\.com\//,
-    /^(https?):\/\/(www\.)?facebook\.com\//,
-    /^(https?):\/\/(.+\.)(welcomesyourfeedback.net|snapsurveys.com)\//,
-    /^(https?):\/\/([-A-Z0-9.]+)\.surveymonkey\.([-A-Z0-9+&@#\/%=~_|!:,.;]+)/i,
-    /^(https?):\/\/docs\.google\.com\/(document|spreadsheets|forms|presentation)\/d\/(.*?)\/.*?/,
-    /^(https?):\/\/(www\.)?google\.com\/maps(\/[a-z])?\/embed\?([^&]*)=([-A-Z0-9+&@#\/%=~_|!:,.;]+)/i,
-    /^(https?):\/\/([-A-Z0-9.]+)\.slideshare(\.(net|com))\/slideshow\/embed_code\/key\/([-A-Z0-9+&@#\/%=~_|!:,.;]+)/i,
-    /^(https?):\/\/(www\.)?onedrive\.live\.([-A-Z0-9+&@#\/%=~_|!:,.;?]+)/i
+    %r{\A(https?)://(.+\.)typeform\.com/to/},
+    %r{\A(https?):\/\/surveys.enalyzer.com\/?\?pid=.*},
+    %r{\A(https?):\/\/(www\.)?survey-xact\.dk\/LinkCollector\?key=.*}i,
+    %r{\A(https?)://(.+\.)qualtrics\.com/jfe/form/},
+    %r{\A(https?)://(www\.)?smartsurvey\.co\.uk/},
+    %r{\A(https?)://(.+\.)(microsoft|office)\.com/},
+    %r{\A(https?)://(www\.)?eventbrite\.([-A-Z0-9+&@#/%=~_|!:,.;]+)}i,
+    %r{\A(https?)://(www\.)?(arcgis|arcg)\.([-A-Z0-9+&@#/%=~_|!:,.;]+)}i,
+    %r{\A(https?)://public\.tableau\.com/([-A-Z0-9+&@#/%=~_|!:,.;?]+)}i,
+    %r{\A(https?)://datastudio\.google\.com/embed/},
+    %r{\A(https?)://app\.powerbi\.com/},
+    %r{\A(https?)://static\.ctctcdn\.com/js/},
+    %r{\A(https?)://(www\.)?instagram\.com/},
+    %r{\A(https?)://platform\.twitter\.com/},
+    %r{\A(https?)://.+\.konveio\.com/},
+    %r{\A(https?)://(www\.)?facebook\.com/},
+    %r{\A(https?)://(.+\.)(welcomesyourfeedback.net|snapsurveys.com)/},
+    %r{\A(https?)://([-A-Z0-9.]+)\.surveymonkey\.([-A-Z0-9+&@#/%=~_|!:,.;]+)}i,
+    %r{\A(https?):\/\/docs\.google\.com\/(document|spreadsheets|forms|presentation)\/d\/(.*?)\/.*?},
+    %r{\A(https?)://(www\.)?google\.com/maps(/[a-z])?/embed\?([^&]*)=([-A-Z0-9+&@#/%=~_|!:,.;]+)}i,
+    %r{\A(https?)://([-A-Z0-9.]+)\.slideshare(\.(net|com))/slideshow/embed_code/key/([-A-Z0-9+&@#/%=~_|!:,.;]+)}i,
+    %r{\A(https?)://(www\.)?onedrive\.live\.([-A-Z0-9+&@#/%=~_|!:,.;?]+)}i
   ].freeze
 
   VIDEO_WHITELIST = [
-    /^(https?):\/\/(?:www\.)?youtu(?:be\.com\/(?:watch\?v=|embed\/)|\.be\/)([\w\-_]*)/,
-    /^(https?):\/\/(?:www\.)?(?:player\.vimeo\.com\/video|vimeo\.com)\/(\d+)(?:|\/\?)/,
-    /^(https?):\/\/(.+)?(wistia\.com|wi\.st)\/.*\//,
-    /^(https?):\/\/(?:www\.)?dailymotion\.com\/embed\/video\/?(.+)/,
-    /^(https?):\/\/?media\.videotool\.dk\/?\?vn=[\w-]+/,
-    /^(https?):\/\/(?:www\.)?dreambroker\.com\/channel\/([\w-]+)\/iframe\//
+    %r{\A(?:http(?:s?):)?//(?:www\.)?youtu(?:be\.com/(?:watch\?v=|embed/)|\.be/)([\w\-_]*)},
+    %r{\A(?:http(?:s?):)?//(?:www\.)?(?:player\.vimeo\.com/video|vimeo\.com)/(\d+)(?:|/\?)},
+    %r{\A(?:http(?:s?):)?//(.+)?(wistia.com|wi.st).*/},
+    %r{\A(?:http(?:s?):)?//(?:www\.)?dailymotion\.com/embed/video/?(.+)},
+    %r{\A(https?://)?media\.videotool\.dk/?\?vn=[\w-]+},
+    %r{\A(https?://)(?:www\.)?dreambroker\.com/channel/([\w-]+)/iframe/([\w\-\#/]+)}
   ].freeze
 
   def whitelisted?(url)
