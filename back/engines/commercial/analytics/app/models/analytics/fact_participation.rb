4793b4fb08804a7e670926b74b673338555b53d5
jamesspeake <james@speake.org>
2022-07-05 15:11:02 +0100

    [CL-1081] Updated annotations on models

--- back/engines/commercial/analytics/app/models/analytics/fact_participation.rb
+++ back/engines/commercial/analytics/app/models/analytics/fact_participation.rb
@@ -2,20 +2,20 @@
 #
 # Table name: analytics_fact_participations
 #
-#  id              :uuid
+#  id              :uuid             primary key
 #  user_id         :uuid
 #  project_id      :uuid
 #  type_id         :uuid
-#  created_date_id :uuid
+#  created_date    :date
 #  votes_count     :integer
 #  upvotes_count   :integer
 #  downvotes_count :integer
 #
 # Indexes
 #
-#  index_analytics_fact_participations_on_created_date_id  (created_date_id)
-#  index_analytics_fact_participations_on_id               (id)
-#  index_analytics_fact_participations_on_project_id       (project_id)
+#  index_analytics_fact_participations_on_created_date  (created_date)
+#  index_analytics_fact_participations_on_id            (id)
+#  index_analytics_fact_participations_on_project_id    (project_id)
 #
 module Analytics
   class FactParticipation < Analytics::ApplicationRecord
