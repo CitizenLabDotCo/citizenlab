4fd7a6c748e5e01280bdd66315a85307f493ae9b
brentguf <brentguf@users.noreply.github.com>
2022-07-12 10:12:56 +0200

    Add comments to hook

--- front/app/hooks/useHomepageSettingsFeatureFlag.ts
+++ front/app/hooks/useHomepageSettingsFeatureFlag.ts
@@ -4,15 +4,24 @@ import { isNilOrError } from 'utils/helperUtils';
 import useHomepageSettings from './useHomepageSettings';
 import useAppConfiguration from './useAppConfiguration';
 
+// Enabled values for sections that have a corresponding
+// setting in appConfiguration.ts
 type TFeatureSectionEnabledSetting = Extract<
   THomepageEnabledSetting,
   'events_widget_enabled' | 'customizable_homepage_banner_enabled'
 >;
 
+// Enabled values for sections that DON'T have a corresponding
+// setting in appConfiguration.ts (are regular sections)
 type TRegularSectionEnabledSetting = Exclude<
   THomepageEnabledSetting,
   TFeatureSectionEnabledSetting
 >;
+
+// If we deal with a section whose allowed value needs to be checked
+// in appConfiguration, we requre the appConfiguration setting name.
+// Otherwise, we just need the enabled value that can be found in homepageSettings.
+type Parameters = FeatureSectionParameters | RegularSectionParameters;
 
 type FeatureSectionParameters = {
   homepageEnabledSetting: TFeatureSectionEnabledSetting;
@@ -27,18 +36,19 @@ type RegularSectionParameters = {
   appConfigSettingName?: never;
 };
 
-type Parameters = FeatureSectionParameters | RegularSectionParameters;
-
 export default function useHomepageSettingsFeatureFlag({
   homepageEnabledSetting,
   appConfigSettingName,
 }: Parameters) {
   const homepageSettings = useHomepageSettings();
   const appConfig = useAppConfiguration();
+  // It only makes sense to have appConfigSetting if there's an appConfigSettingName
   const appConfigSetting =
     appConfigSettingName && !isNilOrError(appConfig)
       ? appConfig.data.attributes.settings?.[appConfigSettingName]
       : null;
+  // If there's no appConfigSetting, a homepageSetting
+  // is always allowed.
   const isAllowed =
     !appConfigSettingName ||
     (!isNilOrError(appConfigSetting) ? appConfigSetting.allowed : false);
