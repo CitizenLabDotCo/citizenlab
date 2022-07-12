923497d7dff7db09a48c94dc08c4ca602663727f
jamesspeake <james@speake.org>
2022-07-12 11:55:35 +0100

    [CL-1040] Added rudimentary multi-tenancy - needs testing though

--- back/engines/commercial/analytics/app/models/analytics/application_record.rb
+++ back/engines/commercial/analytics/app/models/analytics/application_record.rb
@@ -3,7 +3,11 @@
 module Analytics
   class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
+
     connects_to database: { writing: :analytics, reading: :analytics }
+
+    # Set the schema to the schema that has already been set by Apartment for the main connection
+    connection.schema_search_path = ActiveRecord::Base.connection.schema_search_path
 
     def readonly?
       true
