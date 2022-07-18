945e823dfa3fb95e379dd8a438a88b2a4076c52b
lucas <lucasquinnmathis@gmail.com>
2022-07-18 10:05:21 +0200

    Don't spread object

--- front/app/containers/Admin/pages-menu/containers/HeroBannerForm/index.tsx
+++ front/app/containers/Admin/pages-menu/containers/HeroBannerForm/index.tsx
@@ -134,9 +134,7 @@ const HeroBannerForm = ({ intl: { formatMessage } }: InjectedIntlProps) => {
 
       setIsLoading(true);
       try {
-        await updateHomepageSettings({
-          ...diffedValues,
-        });
+        await updateHomepageSettings(diffedValues);
         setApiErrors(null);
         setIsLoading(false);
       } catch (error) {
