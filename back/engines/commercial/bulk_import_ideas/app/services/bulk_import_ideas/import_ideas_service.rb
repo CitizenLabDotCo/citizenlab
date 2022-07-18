1ac96c5ae9a6c659e62a092d7fb4dcf97ab2c4ab
Sebastien Hoorens <sebastien@citizenlab.co>
2022-07-18 17:24:03 +0200

    specs for different fields for idea bulk import + fix

--- back/engines/commercial/bulk_import_ideas/app/services/bulk_import_ideas/import_ideas_service.rb
+++ back/engines/commercial/bulk_import_ideas/app/services/bulk_import_ideas/import_ideas_service.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 module BulkImportIdeas
+  class Error < StandardError; end
+
   class ImportIdeasService
     DEFAULT_MAX_IDEAS = 500
 
     def import_ideas(idea_rows, max_ideas: DEFAULT_MAX_IDEAS)
-      raise "The maximal amount of #{max_ideas} ideas has been exceeded" if idea_rows.size > max_ideas
+      raise Error, "The maximal amount of #{max_ideas} ideas has been exceeded" if idea_rows.size > max_ideas
 
       ActiveRecord::Base.transaction do
         idea_rows.each do |idea_row|
@@ -31,7 +33,7 @@ module BulkImportIdeas
 
       idea = Idea.new idea_attributes
       # Later iteration: parseable error using idea.errors.details
-      raise "The resulting idea is not valid: #{idea.errors.messages}" unless idea.valid?
+      raise Error, "The resulting idea is not valid: #{idea.errors.messages}" unless idea.valid?
 
       idea.save!
 
@@ -41,19 +43,19 @@ module BulkImportIdeas
     end
 
     def add_title_multiloc(idea_row, idea_attributes)
-      raise 'Idea with empty title' if idea_row[:title_multiloc].blank?
+      raise Error, 'Idea with empty title' if idea_row[:title_multiloc].blank?
 
       idea_attributes[:title_multiloc] = idea_row[:title_multiloc]
     end
 
     def add_body_multiloc(idea_row, idea_attributes)
-      raise 'Idea with empty body' if idea_row[:body_multiloc].blank?
+      raise Error, 'Idea with empty body' if idea_row[:body_multiloc].blank?
 
       idea_attributes[:body_multiloc] = idea_row[:body_multiloc]
     end
 
     def add_project(idea_row, idea_attributes)
-      raise 'Idea without project' if idea_row[:project_title].blank?
+      raise Error, 'Idea without project' if idea_row[:project_title].blank?
 
       project_title = idea_row[:project_title].downcase.strip
       # Later iteration: Only load necessary attributes? Preload all projects before importing all?
@@ -64,16 +66,16 @@ module BulkImportIdeas
           .map { |v| v.downcase.strip }
           .include? project_title
       end
-      raise "No project with title #{idea_row[:project_title]} exists" if project.blank?
+      raise Error, "No project with title \"#{idea_row[:project_title]}\" exists" if project.blank?
 
       idea_attributes[:project] = project
     end
 
     def add_author(idea_row, idea_attributes)
-      raise 'Idea without user email' if idea_row[:user_email].blank?
+      raise Error, 'Idea without user email' if idea_row[:user_email].blank?
 
       author = User.find_by_cimail idea_row[:user_email]
-      raise "No user exists with email #{idea_row[:user_email]}" if author.blank?
+      raise Error, "No user exists with email \"#{idea_row[:user_email]}\"" if author.blank?
 
       idea_attributes[:author] = author
     end
@@ -85,7 +87,7 @@ module BulkImportIdeas
       begin
         published_at = Date.parse idea_row[:published_at]
       rescue StandardError => _e
-        raise "Idea with invalid publication date format: #{idea_row[:published_at]}"
+        raise Error, "Idea with invalid publication date format \"#{idea_row[:published_at]}\""
       end
 
       idea_attributes[:published_at] = published_at
@@ -109,14 +111,14 @@ module BulkImportIdeas
     def add_phase(idea_row, idea_attributes)
       return if idea_row[:phase_rank].blank?
 
-      phase_rank = idea_data[:phase_rank].to_i
+      phase_rank = idea_row[:phase_rank].to_i
       project_phases = Phase.where(project: idea_attributes[:project])
       if phase_rank > project_phases.size
-        raise "There are only #{project_phases.size} phases in project #{idea_row[:project_title]} (requested phase: #{phase_rank})"
+        raise Error, "There are only #{project_phases.size} phases in project \"#{idea_row[:project_title]}\" (requested phase: #{phase_rank})"
       end
 
       phase = project_phases.order(:start_at).all[phase_rank - 1]
-      raise "No phase #{phase_rank} found in project #{idea_row[:project_title]}" if phase.blank?
+      raise Error, "No phase #{phase_rank} found in project \"#{idea_row[:project_title]}\"" if phase.blank?
 
       idea_attributes[:phases] = [phase]
     end
@@ -133,7 +135,7 @@ module BulkImportIdeas
             .map(&:downcase)
             .include? topic_title
         end
-      end.select(&:present?).uniq(&:id)
+      end.select(&:present?).map(&:id).uniq
 
       idea_attributes[:topic_ids] = topics_ids
     end
@@ -144,7 +146,7 @@ module BulkImportIdeas
       begin
         IdeaImage.create!(remote_image_url: idea_row[:image_url], idea: idea)
       rescue StandardError => _e
-        raise "No image could be downloaded from #{idea_row[:image_url]}, make sure the URL is valid and ends with a file extension such as .png or .jpg"
+        raise Error, "No image could be downloaded from #{idea_row[:image_url]}, make sure the URL is valid and ends with a file extension such as .png or .jpg"
       end
     end
   end
--- back/engines/commercial/bulk_import_ideas/spec/services/import_ideas_service_spec.rb
+++ back/engines/commercial/bulk_import_ideas/spec/services/import_ideas_service_spec.rb
@@ -44,4 +44,143 @@ describe BulkImportIdeas::ImportIdeasService do
     expect(idea2.title_multiloc).to eq({ 'en' => 'My idea title 2' })
     expect(idea2.body_multiloc).to eq({ 'en' => 'My idea description 2' })
   end
+
+  it 'imports ideas with publication info' do
+    create :user, email: 'userimport@citizenlab.co'
+    create :project, title_multiloc: { 'en' => 'Project title' }
+
+    idea_rows = [
+      {
+        title_multiloc: { 'en' => 'My idea title' },
+        body_multiloc: { 'en' => 'My idea description' },
+        project_title: 'Project title',
+        user_email: 'userimport@citizenlab.co',
+        published_at: '2022-07-18'
+      }
+    ]
+
+    service.import_ideas idea_rows
+
+    expect(Idea.count).to eq 1
+    idea = Idea.first
+    expect(idea.published_at).to eq Date.parse('2022-07-18')
+    expect(idea.publication_status).to eq 'published'
+  end
+
+  it 'imports ideas with location info' do
+    create :user, email: 'userimport@citizenlab.co'
+    create :project, title_multiloc: { 'en' => 'Project title' }
+
+    idea_rows = [
+      {
+        title_multiloc: { 'en' => 'My idea title' },
+        body_multiloc: { 'en' => 'My idea description' },
+        project_title: 'Project title',
+        user_email: 'userimport@citizenlab.co',
+        latitude: 50.5035,
+        longitude: 6.0944,
+        location_description: 'Panorama sur les Hautes Fagnes / Hohes Venn'
+      }
+    ]
+
+    service.import_ideas idea_rows
+
+    expect(Idea.count).to eq 1
+    idea = Idea.first
+    expect(RGeo::GeoJSON.encode(idea.location_point)&.dig('coordinates', 1)).to eq 50.5035
+    expect(RGeo::GeoJSON.encode(idea.location_point)&.dig('coordinates', 0)).to eq 6.0944
+    expect(idea.location_description).to eq 'Panorama sur les Hautes Fagnes / Hohes Venn'
+  end
+
+  it 'imports ideas in a phase' do
+    create :user, email: 'userimport@citizenlab.co'
+    project = create :project_with_phases, phases_count: 2, title_multiloc: { 'en' => 'Project title' }
+
+    idea_rows = [
+      {
+        title_multiloc: { 'en' => 'My idea title' },
+        body_multiloc: { 'en' => 'My idea description' },
+        project_title: 'Project title',
+        user_email: 'userimport@citizenlab.co',
+        phase_rank: 2
+      }
+    ]
+
+    service.import_ideas idea_rows
+
+    expect(Idea.count).to eq 1
+    idea = Idea.first
+    expect(idea.phase_ids).to eq [project.phases.order(:start_at).last.id]
+  end
+
+  it 'imports ideas with topics' do
+    create :user, email: 'userimport@citizenlab.co'
+    create :project, title_multiloc: { 'en' => 'Project title' }
+    create :topic
+    topic1 = create :topic, title_multiloc: { 'en' => 'Topic 1' }
+    topic2 = create :topic, title_multiloc: { 'nl-BE' => 'Project twee', 'en' => 'Topic 2' }
+
+    idea_rows = [
+      {
+        title_multiloc: { 'en' => 'My idea title' },
+        body_multiloc: { 'en' => 'My idea description' },
+        project_title: 'Project title',
+        user_email: 'userimport@citizenlab.co',
+        topic_titles: ['Topic 1', 'Topic 2']
+      }
+    ]
+
+    service.import_ideas idea_rows
+
+    expect(Idea.count).to eq 1
+    idea = Idea.first
+    expect(idea.topic_ids).to match_array [topic1.id, topic2.id]
+  end
+
+  it 'imports ideas with images', pending: true do
+    WebMock.disable_net_connect!
+    stub_request(:get, 'https://images.com/image.png')
+      .to_return(body: 'abc')
+
+    create :user, email: 'userimport@citizenlab.co'
+    create :project, title_multiloc: { 'en' => 'Project title' }
+
+    idea_rows = [
+      {
+        title_multiloc: { 'en' => 'My idea title' },
+        body_multiloc: { 'en' => 'My idea description' },
+        project_title: 'Project title',
+        user_email: 'userimport@citizenlab.co',
+        image_url: 'https://images.com/image.png'
+      }
+    ]
+
+    service.import_ideas idea_rows
+
+    expect(Idea.count).to eq 1
+    idea = Idea.first
+    expect(idea.idea_images.count).to eq 1
+  end
+
+  it 'does not accept invalid import data' do
+    create :user, email: 'userimport@citizenlab.co'
+    create :project, title_multiloc: { 'en' => 'Project title' }
+
+    idea_rows = [
+      {
+        title_multiloc: { 'en' => 'My idea title' },
+        body_multiloc: { 'en' => 'My idea description' },
+        project_title: 'Project title',
+        user_email: 'userimport@citizenlab.co'
+      },
+      {
+        title_multiloc: { 'en' => 'My idea title' },
+        body_multiloc: { 'en' => 'My idea description' },
+        project_title: 'Non-existing project',
+        user_email: 'userimport@citizenlab.co'
+      }
+    ]
+    expect { service.import_ideas idea_rows }.to raise_error BulkImportIdeas::Error
+    expect(Idea.count).to eq 0
+  end
 end
