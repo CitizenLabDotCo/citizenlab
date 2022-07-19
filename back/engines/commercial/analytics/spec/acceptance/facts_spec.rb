e8b66f9db7dd94faf05faa4713be64b9d0f5398a
Fran <francisco.pensa.web@gmail.com>
2022-07-19 09:26:07 -0300

    [CL-423] Improve analytics testing

--- back/engines/commercial/analytics/spec/acceptance/facts_spec.rb
+++ back/engines/commercial/analytics/spec/acceptance/facts_spec.rb
@@ -15,36 +15,37 @@ resource 'Posts' do
       Universal API for querying posts from the analytics database
     DESC
 
-    with_options scope: :query do
-      parameter :query, 'The query object.', required: true
+    with_options required: true do
+      parameter :query, 'The query object.'
     end
 
     context 'when admin' do
       before { admin_header_token }
 
-      let(:query) do {
-          groups: {
-            key: "type.name",
-            aggregations: {
-              votes_count: "sum"
+      let(:query) do
+        {
+          dimensions: {
+            type: {
+              name: 'idea'
             }
           }
         }
       end
-      let(:expected_response) do [
+      let(:expected_response) do
+        [
           {
-              "sum_votes_count": 470,
-              "type.name": "idea"
+            sum_votes_count: 470,
+            'type.name': 'idea'
           },
           {
-              "sum_votes_count": 156,
-              "type.name": "initiative"
+            sum_votes_count: 156,
+            'type.name': 'initiative'
           }
         ]
       end
 
       example_request 'makes a query' do
-        expect(status).to eq(201)
+        expect(status).to eq(200)
         expect(json_response).to match(expected_response)
       end
     end
