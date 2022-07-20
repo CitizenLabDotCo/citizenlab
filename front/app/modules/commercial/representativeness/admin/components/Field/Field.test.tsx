d8a60ee1cce61fbb13d7c5a810a99952bb18a70e
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-07-20 11:15:55 +0100

    [CL-1110] Sanity tests for Field

--- front/app/modules/commercial/representativeness/admin/components/Field/Field.test.tsx
+++ front/app/modules/commercial/representativeness/admin/components/Field/Field.test.tsx
@@ -1,30 +1,50 @@
-// import React from 'react';
-// import Field from '.';
-// import { render } from 'utils/testUtils/rtl'
+import React from 'react';
+import Field from '.';
+import { render, screen } from 'utils/testUtils/rtl';
 
 jest.mock('services/appConfiguration');
+jest.mock('services/locale');
 jest.mock('hooks/useLocalize');
 jest.mock('utils/cl-intl');
 
 let mockUserCustomFieldOptions;
-const selectUserCustomFieldOptions = {
-  id: 'field1',
-  attributes: {},
-};
+const selectUserCustomFieldOptions = [
+  { id: 'option1', attributes: { title_multiloc: { en: 'Option 1' } } },
+  { id: 'option2', attributes: { title_multiloc: { en: 'Option 2' } } },
+  { id: 'option3', attributes: { title_multiloc: { en: 'Option 3' } } },
+];
+const birthyearUserCustomFieldOptions = [];
+
 jest.mock(
   'modules/commercial/user_custom_fields/hooks/useUserCustomFieldOptions',
   () => () => mockUserCustomFieldOptions
 );
 
-let mockReferenceDistribution;
+const mockReferenceDistribution = {
+  referenceDataUploaded: false,
+  referenceDistribution: null,
+};
+
 jest.mock(
   '../../hooks/useReferenceDistribution',
   () => () => mockReferenceDistribution
 );
 
 let mockUserCustomField;
-const selectField = { attributes: { key: null } };
-const birthyearField = { attributes: { key: 'birthyear' } };
+const selectField = {
+  attributes: {
+    key: null,
+    code: 'code',
+    title_multiloc: { en: 'Select field' },
+  },
+};
+const birthyearField = {
+  attributes: {
+    key: 'birthyear',
+    code: 'birthyear',
+    title_multiloc: { en: 'Age' },
+  },
+};
 
 jest.mock(
   'modules/commercial/user_custom_fields/hooks/useUserCustomField',
@@ -37,11 +57,22 @@ describe('<Field />', () => {
       mockUserCustomField = selectField;
       mockUserCustomFieldOptions = selectUserCustomFieldOptions;
     });
+
+    it('renders', () => {
+      render(<Field userCustomFieldId="field1" />);
+      expect(screen.getByText('Select field')).toBeInTheDocument();
+    });
   });
 
   describe('birthyear field', () => {
     beforeEach(() => {
       mockUserCustomField = birthyearField;
+      mockUserCustomFieldOptions = birthyearUserCustomFieldOptions;
+    });
+
+    it('renders', () => {
+      render(<Field userCustomFieldId="field1" />);
+      expect(screen.getByText('Age')).toBeInTheDocument();
     });
   });
 });
