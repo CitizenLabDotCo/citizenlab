d2c7f43cebfa04217bb9b2d4ce4227e8c4a13185
Sebastien Hoorens <sebastien@citizenlab.co>
2022-07-20 17:54:53 +0200

    download template import file + make the import tab look somewhat like the design

--- front/app/containers/Admin/ideas/import/index.tsx
+++ front/app/containers/Admin/ideas/import/index.tsx
@@ -4,13 +4,54 @@ import { InjectedIntlProps } from 'react-intl';
 import { UploadFile, CLErrors } from 'typings';
 
 import { injectIntl } from 'utils/cl-intl';
-// import messages from '../messages';
+
+import { API_PATH } from 'containers/App/constants';
+import { saveAs } from 'file-saver';
+import { requestBlob } from 'utils/request';
 
 // components
 import FileUploader from 'components/UI/FileUploader';
+import { SectionField, SectionTitle } from 'components/admin/Section';
+import Button from 'components/UI/Button';
 
 // resources
 import { addIdeaImportFile } from 'services/ideaFiles';
+
+// styling
+import styled from 'styled-components';
+import { colors, fontSizes } from 'utils/styleUtils';
+import { darken } from 'polished';
+
+const StyledSectionTitle = styled(SectionTitle)`
+  margin-bottom: 15px;
+  font-size: ${fontSizes.l}px;
+  font-weight: bold;
+`;
+
+const SectionDescription = styled.div`
+  font-size: ${fontSizes.base}px;
+`;
+
+const SectionParagraph = styled.p`
+  a {
+    color: ${colors.clBlue};
+    text-decoration: underline;
+
+    &:hover {
+      color: ${darken(0.2, colors.clBlue)};
+      text-decoration: underline;
+    }
+  }
+`;
+
+const FlexWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const DownloadButton = styled(Button)`
+  margin-bottom: 15px;
+`;
 
 const Import = ({ intl: { formatMessage } }: InjectedIntlProps) => {
   const [files, setFiles] = useState<UploadFile[]>([]);
@@ -40,14 +81,53 @@ const Import = ({ intl: { formatMessage } }: InjectedIntlProps) => {
     }
   };
 
+  const downloadExampleFile = async (event) => {
+    event.preventDefault();
+    const blob = await requestBlob(
+      `${API_PATH}/import_ideas/example_xlsx`,
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    );
+    saveAs(blob, 'example.xlsx');
+  };
+
   return (
-    <FileUploader
-      id={'bulk_idea_import'}
-      onFileRemove={() => {}}
-      onFileAdd={handleFileOnAdd}
-      apiErrors={apiErrors}
-      files={files}
-    />
+    <>
+      <h1>Import inputs</h1>
+      <SectionDescription><p>Import existing inputs from an Excel file.</p></SectionDescription>
+      <SectionField>
+        <StyledSectionTitle>
+          1. Download and fill out the template
+        </StyledSectionTitle>
+        <SectionDescription>
+          <FlexWrapper>
+            <DownloadButton
+              buttonStyle="secondary"
+              icon="download"
+              onClick={downloadExampleFile}
+            >
+              Download template
+            </DownloadButton>
+          </FlexWrapper>
+          <SectionParagraph>
+            <span><a href="https://support.citizenlab.co/en/articles/1502238-how-to-bulk-import-ideas-and-input-or-locations-into-your-project" target="_blank" rel="noreferrer">Visit the support page</a> if you want more info about all supported columns in the import template.</span>
+          </SectionParagraph>
+          <SectionParagraph>
+            Important: In order to send the invitations correctly, no column can be removed from the import template. Leave unused columns empty.
+          </SectionParagraph>
+        </SectionDescription>
+
+        <StyledSectionTitle>
+          2. Upload your completed template file
+        </StyledSectionTitle>
+        <FileUploader
+          id={'bulk_idea_import'}
+          onFileRemove={() => {}}
+          onFileAdd={handleFileOnAdd}
+          apiErrors={apiErrors}
+          files={files}
+        />
+      </SectionField>
+    </>
   );
 };
 
