be261973464b8676154bdcde0940c3256201621a
Fran <francisco.pensa.web@gmail.com>
2022-07-21 19:20:38 -0300

    [CL-1216] fix too many sql queries

--- back/engines/commercial/insights/app/services/insights/xlsx_service.rb
+++ back/engines/commercial/insights/app/services/insights/xlsx_service.rb
@@ -2,23 +2,27 @@
 
 module Insights
   class XlsxService
+    def build_assignments_hash(category_assignments)
+      assignments_hash = {}
+      if category_assignments
+        assignments_hash = category_assignments.map { |assignment| [assignment.category_id, assignment.approved ? 'approved' : 'suggested'] }.to_h
+      end
+      assignments_hash
+    end
+
     def generate_inputs_xlsx(inputs, categories, view_private_attributes: false)
       columns = xlsx_service.generate_idea_xlsx_columns(inputs, view_private_attributes: view_private_attributes)
+
+      values = inputs.includes(:insights_category_assignments).select('id').map { |input| [input.id, build_assignments_hash(input.insights_category_assignments)] }.to_h
 
       category_cols = categories.map do |category|
         {
           header: category.name,
-          f: lambda do |input|
-            assignment = input.insights_category_assignments.find_by(category_id: category.id)
-            return unless assignment
-
-            assignment.approved ? 'approved' : 'suggested'
-          end
+          f: ->(input) { values[input.id][category.id] }
         }
       end
 
       columns.insert(3, *category_cols)
-      inputs = inputs.includes(:insights_category_assignments)
       xlsx_service.generate_xlsx('Inputs export', columns, inputs)
     end
 
