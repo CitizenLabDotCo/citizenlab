a6b4916e483d87bcbac4ccf5f29049ac4ff5d1aa
Fran <francisco.pensa.web@gmail.com>
2022-07-22 10:14:48 -0300

    [CL-1216] make method private & fix lint

--- back/engines/commercial/insights/app/services/insights/xlsx_service.rb
+++ back/engines/commercial/insights/app/services/insights/xlsx_service.rb
@@ -2,18 +2,18 @@
 
 module Insights
   class XlsxService
-    def build_assignments_hash(category_assignments)
-      assignments_hash = {}
-      if category_assignments
-        assignments_hash = category_assignments.map { |assignment| [assignment.category_id, assignment.approved ? 'approved' : 'suggested'] }.to_h
-      end
-      assignments_hash
-    end
-
     def generate_inputs_xlsx(inputs, categories, view_private_attributes: false)
       columns = xlsx_service.generate_idea_xlsx_columns(inputs, view_private_attributes: view_private_attributes)
 
-      values = inputs.includes(:insights_category_assignments).select('id').map { |input| [input.id, build_assignments_hash(input.insights_category_assignments)] }.to_h
+      values = inputs
+        .includes(:insights_category_assignments)
+        .select('id')
+        .to_h do |input|
+          [
+            input.id,
+            build_assignments_hash(input.insights_category_assignments)
+          ]
+        end
 
       category_cols = categories.map do |category|
         {
@@ -27,6 +27,20 @@ module Insights
     end
 
     private
+
+    def build_assignments_hash(category_assignments)
+      assignments_hash = {}
+      if category_assignments
+        assignments_hash = category_assignments
+          .to_h do |assignment|
+            [
+              assignment.category_id,
+              assignment.approved ? 'approved' : 'suggested'
+            ]
+          end
+      end
+      assignments_hash
+    end
 
     def xlsx_service
       @xlsx_service ||= ::XlsxService.new
