bbf4d169d70503079b9822816878001fc4b4b983
Iva <iva@citizenlab.co>
2022-07-27 13:03:31 +0300

    [TEC-35] Add test for quill

--- front/app/components/UI/RHFQuillMultilocWithLocaleSwitcher/RHFQuillMultilocWithLocaleSwitcher.test.tsx
+++ front/app/components/UI/RHFQuillMultilocWithLocaleSwitcher/RHFQuillMultilocWithLocaleSwitcher.test.tsx
@@ -1,69 +1,53 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from 'utils/testUtils/rtl';
-import RHFInputMultilocWithLocaleSwitcher from './';
+import RHFQuillMultilocWithLocaleSwitcher from './';
 import { useForm, FormProvider } from 'react-hook-form';
 import { object } from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import validateMultiloc from 'utils/yup/validateMultiloc';
 
 const schema = object({
-  title: validateMultiloc('Error message'),
+  description: validateMultiloc('Error message'),
 });
 
 jest.mock('utils/cl-intl');
 jest.mock('hooks/useAppConfigurationLocales', () =>
   jest.fn(() => ['en', 'nl-NL'])
 );
+jest.mock('hooks/useLocale');
 
 const onSubmit = jest.fn();
 
-const Form = () => {
+const Form = ({ defaultValue }: { defaultValue?: Record<string, string> }) => {
   const methods = useForm({
     resolver: yupResolver(schema),
+    defaultValues: { description: defaultValue },
   });
 
   return (
     <FormProvider {...methods}>
       <form onSubmit={methods.handleSubmit((formData) => onSubmit(formData))}>
-        <RHFInputMultilocWithLocaleSwitcher
-          name="title"
-          placeholder="title"
-          type="text"
-        />
+        <RHFQuillMultilocWithLocaleSwitcher name="description" />
         <button type="submit">Submit</button>
       </form>
     </FormProvider>
   );
 };
 
-describe('RHFInputMultilocWithLocaleSwitcher', () => {
+describe('RHFQuillMultilocWithLocaleSwitcher', () => {
   it('renders', () => {
-    render(<Form />);
-    expect(screen.getByPlaceholderText(/title/i)).toBeInTheDocument();
+    const { container } = render(<Form />);
+    expect(container.querySelector('.ql-editor')).toBeInTheDocument();
   });
   it('submits correct data from input', async () => {
-    render(<Form />);
-    const valueEN = 'en title';
-    const valueNL = 'nl title';
-
-    fireEvent.change(screen.getByPlaceholderText(/title/i), {
-      target: {
-        value: valueEN,
-      },
-    });
-
-    fireEvent.click(screen.getByText(/nl-NL/i));
-
-    fireEvent.change(screen.getByPlaceholderText(/title/i), {
-      target: {
-        value: valueNL,
-      },
-    });
+    const defaultValue = { en: 'en description', 'nl-NL': 'nl description' };
+    render(<Form defaultValue={defaultValue} />);
 
     fireEvent.click(screen.getByText(/submit/i));
+
     await waitFor(() =>
       expect(onSubmit).toHaveBeenCalledWith({
-        title: { en: valueEN, 'nl-NL': valueNL },
+        description: defaultValue,
       })
     );
   });
