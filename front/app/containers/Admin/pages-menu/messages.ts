9b983242a4e773c131dfc25eefc3bd6143c0f197
Alexander <alexander@citizenlab.co>
2022-07-27 10:46:26 +0200

    [CL-1137] Finalize i18n. Rename pages-menu ids

--- front/app/containers/Admin/pages-menu/messages.ts
+++ front/app/containers/Admin/pages-menu/messages.ts
@@ -2,15 +2,15 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   pagesAndMenuTitle: {
-    id: 'app.containers.Admin.pagesAndMenu.title',
+    id: 'app.containers.Admin.pages-menu.title',
     defaultMessage: 'Pages & menu',
   },
   homeTitle: {
-    id: 'app.containers.Admin.pagesAndMenu.homeTitle',
+    id: 'app.containers.Admin.pages-menu.homeTitle',
     defaultMessage: 'Home',
   },
   pageHeader: {
-    id: 'app.containers.admin.pages-menu.pageHeader',
+    id: 'app.containers.Admin.pages-menu.pageHeader',
     defaultMessage: 'Pages',
   },
 });
