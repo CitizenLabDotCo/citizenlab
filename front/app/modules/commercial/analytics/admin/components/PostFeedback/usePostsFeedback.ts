4fea24c9d5e4e92300ce32641a8ac5cd592b9e41
Fran <francisco.pensa.web@gmail.com>
2022-07-29 12:22:33 -0300

    [CL-1180] allow chart without project id

--- front/app/modules/commercial/analytics/admin/components/PostFeedback/usePostsFeedback.ts
+++ front/app/modules/commercial/analytics/admin/components/PostFeedback/usePostsFeedback.ts
@@ -2,13 +2,8 @@ import { useState, useEffect } from 'react';
 import { postsAnalyticsStream } from '../../../services/analyticsFacts';
 import { sum, roundPercentage } from 'utils/math';
 
-const query = (projectId) => ({
-  query: {
-    dimensions: {
-      project: {
-        id: projectId,
-      },
-    },
+const query = (projectId) => {
+  const groups = {
     groups: {
       key: 'project.id',
       aggregations: {
@@ -18,77 +13,83 @@ const query = (projectId) => ({
         feedback_time_taken: 'avg',
       },
     },
-  },
-});
+  };
+
+  const dimensions = !projectId
+    ? {}
+    : { dimensions: { project: { id: projectId } } };
+
+  const query = { query: { ...groups, ...dimensions } };
+
+  return query;
+};
 
 type Response = {
+  'project.id'?: string;
   sum_feedback_none: number;
   sum_feedback_official: number;
   sum_feedback_status_change: number;
-  'project.id': string;
   avg_feedback_time_taken: number;
 };
 
 export default function usePostsWithFeedback(projectId) {
   const [postsWithFeedback, setPostsWithFeedback] = useState<any>(undefined);
   useEffect(() => {
-    if (projectId) {
-      postsAnalyticsStream<Response>(query(projectId)).then((results) => {
-        if (results && results.data.length > 0) {
-          const {
-            sum_feedback_none,
-            sum_feedback_official,
-            sum_feedback_status_change,
-            avg_feedback_time_taken,
-          } = results.data[0];
+    postsAnalyticsStream<Response>(query(projectId)).then((results) => {
+      if (results && results.data.length > 0) {
+        const {
+          sum_feedback_none,
+          sum_feedback_official,
+          sum_feedback_status_change,
+          avg_feedback_time_taken,
+        } = results.data[0];
 
-          const feedback_count = sum([
-            sum_feedback_official,
-            sum_feedback_status_change,
-          ]);
-          const total = sum([feedback_count, sum_feedback_none]);
+        const feedback_count = sum([
+          sum_feedback_official,
+          sum_feedback_status_change,
+        ]);
+        const total = sum([feedback_count, sum_feedback_none]);
 
-          const serie = [
-            { name: 'sum_feedback', value: feedback_count, color: '#40B8C5' },
-            {
-              name: 'sum_no_feedback',
-              value: sum_feedback_none,
-              color: '#E0E0E0',
-            },
-          ];
+        const serie = [
+          { name: 'sum_feedback', value: feedback_count, color: '#40B8C5' },
+          {
+            name: 'sum_no_feedback',
+            value: sum_feedback_none,
+            color: '#E0E0E0',
+          },
+        ];
 
-          const feedbackPercent = feedback_count / total;
-          const avgTime = Math.round(avg_feedback_time_taken / 86400);
+        const feedbackPercent = feedback_count / total;
+        const avgTime = Math.round(avg_feedback_time_taken / 86400);
 
-          const progressBars = [
-            {
-              name: 'Status changed',
-              label: `Status changed: ${sum_feedback_status_change} (${roundPercentage(
-                sum_feedback_status_change,
-                total
-              )}%)`,
-              value: sum_feedback_status_change,
-              total,
-            },
-            {
-              name: 'Official update',
-              label: `Official update: ${sum_feedback_official} (${roundPercentage(
-                sum_feedback_official,
-                total
-              )}%)`,
-              value: sum_feedback_official,
-              total,
-            },
-          ];
-          setPostsWithFeedback({
-            serie,
-            feedbackPercent,
-            avgTime,
-            progressBars,
-          });
-        }
-      });
-    }
+        const progressBars = [
+          {
+            name: 'Status changed',
+            label: `Status changed: ${sum_feedback_status_change} (${roundPercentage(
+              sum_feedback_status_change,
+              total
+            )}%)`,
+            value: sum_feedback_status_change,
+            total,
+          },
+          {
+            name: 'Official update',
+            label: `Official update: ${sum_feedback_official} (${roundPercentage(
+              sum_feedback_official,
+              total
+            )}%)`,
+            value: sum_feedback_official,
+            total,
+          },
+        ];
+        setPostsWithFeedback({
+          serie,
+          feedbackPercent,
+          avgTime,
+          progressBars,
+        });
+      }
+    });
   }, [projectId]);
 
   return postsWithFeedback;
