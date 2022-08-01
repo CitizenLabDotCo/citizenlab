e30bf379caadcb89324373d249b270218109609e
Edwin Kato <katedwin88@gmail.com>
2022-08-01 13:05:28 +0300

    Fix duplicate messages key

--- front/app/modules/commercial/native_surveys/admin/components/SurveyBuilderTopBar/messages.ts
+++ front/app/modules/commercial/native_surveys/admin/components/SurveyBuilderTopBar/messages.ts
@@ -1,28 +1,16 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  openForSubmissions: {
-    id: 'app.containers.AdminPage.ProjectEdit.survey.openForSubmissions',
-    defaultMessage: 'Open for submissions',
-  },
   survey: {
-    id: 'app.containers.AdminPage.ProjectEdit.survey.surveyText',
+    id: 'app.containers.AdminPage.ProjectEdit.survey.topBar.surveyText',
     defaultMessage: 'Survey',
   },
   saveSurvey: {
-    id: 'app.containers.AdminPage.ProjectEdit.survey.saveSurvey',
+    id: 'app.containers.AdminPage.ProjectEdit.survey.topBar.saveSurvey',
     defaultMessage: 'Save',
   },
-  downloadSurveyResults: {
-    id: 'app.containers.AdminPage.ProjectEdit.survey.downloadSurveyResults',
-    defaultMessage: 'Download survey results ({count})',
-  },
-  editSurveyContent: {
-    id: 'app.containers.AdminPage.ProjectEdit.survey.editSurveyContent',
-    defaultMessage: 'Edit survey content',
-  },
   viewSurvey: {
-    id: 'app.containers.AdminPage.ProjectEdit.survey.viewSurvey',
+    id: 'app.containers.AdminPage.ProjectEdit.survey.topBar.viewSurvey',
     defaultMessage: 'View survey',
   },
 });
