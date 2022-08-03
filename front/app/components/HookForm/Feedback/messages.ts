d2a4847b062f4746a77a75caa375cb5b3afd4f6b
Iva <iva@citizenlab.co>
2022-08-03 13:43:10 +0300

    [TEC-35] Edit messages

--- front/app/components/HookForm/Feedback/messages.ts
+++ front/app/components/HookForm/Feedback/messages.ts
@@ -2,20 +2,20 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   errorTitle: {
-    id: 'app.components.RHFFeedback.errorTitle',
+    id: 'app.components.HookForm.Feedback.errorTitle',
     defaultMessage: 'There is a problem',
   },
   submissionErrorTitle: {
-    id: 'app.components.RHFFeedback.submissionErrorTitle',
-    defaultMessage: 'There was a problem',
+    id: 'app.components.HookForm.Feedback.submissionErrorTitle',
+    defaultMessage: 'There was a problem on our end, sorry',
   },
   submissionErrorMessage: {
-    id: 'app.components.RHFFeedback.submissionErrorMessage',
+    id: 'app.components.HookForm.Feedback.submissionErrorMessage',
     defaultMessage:
       'Please try again. If the issue persists, please contact us.',
   },
   successMessage: {
-    id: 'app.components.RHFFeedback.successMessage',
+    id: 'app.components.HookForm.Feedback.successMessage',
     defaultMessage: 'Form successfully submitted',
   },
 });
