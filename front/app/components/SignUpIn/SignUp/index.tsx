1145a183f898496b30d4f16351b13977ca3f76a8
brentguf <brentguf@users.noreply.github.com>
2022-08-04 08:18:22 +0200

    Min height for signup

--- front/app/components/SignUpIn/SignUp/index.tsx
+++ front/app/components/SignUpIn/SignUp/index.tsx
@@ -55,6 +55,12 @@ const Container = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+  min-height: calc(
+    100vh -
+      ${(props) => {
+        return props.theme.menuHeight + props.theme.footerHeight;
+      }}px
+  );
 `;
 
 const SignUpHelperText = styled(QuillEditedContent)`
