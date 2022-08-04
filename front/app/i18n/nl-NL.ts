cdd3152d9963becb703708ab1179fc3066bb1cde
Iva <iva@citizenlab.co>
2022-08-04 13:41:23 +0300

    [TEC-29] Remove addLocaleData

--- front/app/i18n/nl-NL.ts
+++ front/app/i18n/nl-NL.ts
@@ -1,9 +1,4 @@
-import { addLocaleData } from 'react-intl';
-import nlLocaleData from 'react-intl/locale-data/nl';
-
 import { formatTranslationMessages } from './';
-
-addLocaleData(nlLocaleData);
 
 const nlNLTranslationMessages = require('translations/nl-NL.json');
 const nlNLAdminTranslationMessages = require('translations/admin/nl-NL.json');
--- front/app/i18n/sv-SE.ts
+++ front/app/i18n/sv-SE.ts
@@ -1,9 +1,4 @@
-import { addLocaleData } from 'react-intl';
-import svSE from 'react-intl/locale-data/sv';
-
 import { formatTranslationMessages } from './';
-
-addLocaleData(svSE);
 
 const svSETranslationMessages = require('translations/sv-SE.json');
 const svSEAdminTranslationMessages = require('translations/admin/sv-SE.json');
