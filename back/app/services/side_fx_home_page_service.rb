a0df6fdb1d79c9348da229c720579f409fb9a31b
Alexander <alexander@citizenlab.co>
2022-08-04 17:58:20 +0200

    [CL-5] Transform homepage images from input base64 to refs

---
+++ back/app/services/side_fx_home_page_service.rb
@@ -0,0 +1,13 @@
+class SideFxHomePageService
+  include SideFxHelper
+
+  def before_update(home_page, _ = nil)
+    if home_page.top_info_section_multiloc
+      home_page.top_info_section_multiloc = TextImageService.new.swap_data_images home_page, :top_info_section_multiloc
+    end
+
+    if home_page.bottom_info_section_multiloc
+      home_page.bottom_info_section_multiloc = TextImageService.new.swap_data_images home_page, :bottom_info_section_multiloc
+    end
+  end
+end
