b632de692ac3ff311688de9a42ebf01fb0265a2d
Adrien Dessy <adrien@citizenlab.co>
2022-08-05 10:48:02 +0200

    CL-1092 Refactor UserCustomFields::StatUserPolicy

--- back/engines/commercial/user_custom_fields/app/policies/user_custom_fields/stat_user_policy.rb
+++ back/engines/commercial/user_custom_fields/app/policies/user_custom_fields/stat_user_policy.rb
@@ -3,51 +3,57 @@
 module UserCustomFields
   class StatUserPolicy < ApplicationPolicy
     def users_by_gender?
-      user&.active? && (user&.admin? || user&.project_moderator?)
+      active_admin_or_project_moderator?
     end
 
     def users_by_age?
-      user&.active? && (user&.admin? || user&.project_moderator?)
+      active_admin_or_project_moderator?
     end
 
     def users_by_birthyear?
-      user&.active? && (user&.admin? || user&.project_moderator?)
+      active_admin_or_project_moderator?
     end
 
     def users_by_domicile?
-      user&.active? && (user&.admin? || user&.project_moderator?)
+      active_admin_or_project_moderator?
     end
 
     def users_by_education?
-      user&.active? && (user&.admin? || user&.project_moderator?)
+      active_admin_or_project_moderator?
     end
 
     def users_by_custom_field?
-      user&.active? && (user&.admin? || user&.project_moderator?)
+      active_admin_or_project_moderator?
     end
 
     def users_by_gender_as_xlsx?
-      user&.active? && (user&.admin? || user&.project_moderator?)
+      active_admin_or_project_moderator?
     end
 
     def users_by_age_as_xlsx?
-      user&.active? && (user&.admin? || user&.project_moderator?)
+      active_admin_or_project_moderator?
     end
 
     def users_by_birthyear_as_xlsx?
-      user&.active? && (user&.admin? || user&.project_moderator?)
+      active_admin_or_project_moderator?
     end
 
     def users_by_domicile_as_xlsx?
-      user&.active? && (user&.admin? || user&.project_moderator?)
+      active_admin_or_project_moderator?
     end
 
     def users_by_education_as_xlsx?
-      user&.active? && (user&.admin? || user&.project_moderator?)
+      active_admin_or_project_moderator?
     end
 
     def users_by_custom_field_as_xlsx?
-      user&.active? && (user&.admin? || user&.project_moderator?)
+      active_admin_or_project_moderator?
+    end
+
+    private
+
+    def active_admin_or_project_moderator?
+      active? && (admin? || user&.project_moderator?)
     end
   end
 end
