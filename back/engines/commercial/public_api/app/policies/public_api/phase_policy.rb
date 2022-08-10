510930b4005e22ac9d395dfa57c5b6b42574b2b2
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2022-08-10 14:28:53 +0200

    CL-1306 Added based phases endpoints to public api

---
+++ back/engines/commercial/public_api/app/policies/public_api/phase_policy.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module PublicApi
+  class PhasePolicy < PublicApiPolicy
+    class Scope
+      attr_reader :api_client, :scope
+
+      def initialize(api_client, scope)
+        @api_client = api_client
+        @scope = scope
+      end
+
+      def resolve
+        # We base this on the same scope as a non-authenticated user
+        ::PhasePolicy::Scope.new(nil, scope).resolve
+      end
+    end
+
+    def show?
+      # We base this on the same rules a non-authenticated user
+      ::PhasePolicy.new(nil, record).show?
+    end
+  end
+end
---
+++ back/engines/commercial/public_api/app/serializers/public_api/v1/phase_serializer.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class PublicApi::V1::PhaseSerializer < ActiveModel::Serializer
+  @@multiloc_service = MultilocService.new
+
+  attributes :id,
+    :title,
+    :start_at,
+    :end_at
+
+  def title
+    @@multiloc_service.t(object.title_multiloc)
+  end
+end
