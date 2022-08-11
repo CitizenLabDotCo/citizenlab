04526c4a820f006be1290f35f97b44c624be6693
Adrien Dessy <adrien@citizenlab.co>
2022-08-12 00:08:24 +0200

    CL-959 Re-implement the data migration as a background job

--- back/db/migrate/20220810084347_add_custom_field_option_to_areas.rb
+++ back/db/migrate/20220810084347_add_custom_field_option_to_areas.rb
@@ -12,7 +12,7 @@ class AddCustomFieldOptionToAreas < ActiveRecord::Migration[6.1]
 
     reversible do |dir|
       dir.up do
-        create_custom_field_options if domicile_custom_field_id
+        ::UserCustomFields::DataMigrations::CreateDomicileOptionsJob.perform_later if domicile_custom_field_id
       end
     end
   end
@@ -21,55 +21,7 @@ class AddCustomFieldOptionToAreas < ActiveRecord::Migration[6.1]
 
   def domicile_custom_field_id
     @domicile_custom_field_id ||= execute(<<-SQL.squish).first&.[]('id')
-          SELECT id FROM custom_fields WHERE key = 'domicile'
-    SQL
-  end
-
-  def generate_key(area_attrs)
-    title_multiloc = JSON.parse(area_attrs['title_multiloc'])
-    area_name = title_multiloc.values.first.parameterize.snakecase
-    "area-#{area_attrs['ordering']}-#{area_name}"
-  end
-
-  def create_custom_field_options
-    areas = execute('SELECT * FROM areas')
-
-    areas.each do |area|
-      option_id = create_domicile_option(generate_key(area), area['title_multiloc'], area['ordering'])
-      update_area_foreign_key(area['id'], option_id)
-    end
-
-    create_somewhere_else_option(areas.count)
-  end
-
-  def create_domicile_option(key, title_multiloc, ordering)
-    insert_option_query = <<~SQL.squish
-      INSERT INTO custom_field_options (custom_field_id, key, title_multiloc, ordering, created_at, updated_at)
-      VALUES ($1, $2, $3, $4, NOW(), NOW())
-    SQL
-
-    result = exec_insert(
-      insert_option_query,
-      'SQL',
-      [domicile_custom_field_id, key, title_multiloc, ordering]
-    )
-
-    result.first&.[]('id')
-  end
-
-  def create_somewhere_else_option(ordering)
-    title_multiloc = CL2_SUPPORTED_LOCALES.index_with do |locale|
-      I18n.t('custom_field_options.domicile.outside', locale: locale)
-    end.compact
-    key = "area-#{ordering}-somewhere_else"
-    create_domicile_option(key, title_multiloc.to_json, ordering)
-  end
-
-  def update_area_foreign_key(area_id, option_id)
-    exec_update <<~SQL.squish, 'SQL', [option_id, area_id]
-      UPDATE areas
-      SET custom_field_option_id = $1
-      WHERE id = $2
+      SELECT id FROM custom_fields WHERE key = 'domicile'
     SQL
   end
 
