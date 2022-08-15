e343458060ca53d091780384d492d5e477bdefec
jamesspeake <james@speake.org>
2022-08-15 11:12:42 +0100

    [CL-423] Updated models and added factories to ensure data in place and get structure of tests working

--- back/engines/commercial/analytics/app/models/analytics/application_view.rb
+++ back/engines/commercial/analytics/app/models/analytics/application_view.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 module Analytics
-  class ApplicationRecord < ActiveRecord::Base
+  class ApplicationView < ActiveRecord::Base
     self.abstract_class = true
     # connects_to database: { writing: :analytics, reading: :analytics }
+    def readonly?
+      true
+    end
   end
 end
--- back/engines/commercial/analytics/lib/analytics/engine.rb
+++ back/engines/commercial/analytics/lib/analytics/engine.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
+require 'factory_bot_rails'
+
 module Analytics
   class Engine < ::Rails::Engine
     isolate_namespace Analytics
+
+    # Sharing the factories to make them accessible to the main app / other engines.
+    factories_path = File.expand_path('../../spec/factories', __dir__)
+    config.factory_bot.definition_file_paths += [factories_path] if defined?(FactoryBotRails)
 
     config.to_prepare do
       require 'analytics/feature_specification'
