f8d36b13b89af8ed51554127aeecc9f807133e6e
Edwin Kato <katedwin88@gmail.com>
2022-08-15 18:52:27 +0300

    [CL-1338] Remove unused Box

--- front/app/containers/Admin/formBuilder/components/FormBuilderToolbox/ToolboxItem.tsx
+++ front/app/containers/Admin/formBuilder/components/FormBuilderToolbox/ToolboxItem.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 // Components
-import { Box, Icon, IconNames, Text } from '@citizenlab/cl2-component-library';
+import { Icon, IconNames, Text } from '@citizenlab/cl2-component-library';
 import Button from 'components/UI/Button';
 
 import { colors } from 'utils/styleUtils';
@@ -19,16 +19,13 @@ const AddIcon = styled(Icon).attrs({ name: 'plus', ml: '20px' })`
   fill: ${colors.adminSecondaryTextColor};
 `;
 
-const StyledBox = styled(Box)`
-  &:hover {
-    background-color: ${colors.emailBg};
-    transition: background-color 80ms ease-out 0s;
-  }
-`;
-
 const StyledButton = styled(Button)`
   ${AddIcon} {
     visibility: hidden;
+  }
+  &:hover {
+    background-color: ${colors.emailBg};
+    transition: background-color 80ms ease-out 0s;
   }
   &:last-child:hover ${AddIcon} {
     visibility: visible;
@@ -37,23 +34,21 @@ const StyledButton = styled(Button)`
 
 const ToolboxItem = ({ icon, label, onClick }: Props) => {
   return (
-    <StyledBox width="100%" display="flex" alignItems="center">
-      <StyledButton
-        buttonStyle="text"
-        onClick={onClick}
-        icon={icon}
-        iconColor={colors.adminTextColor}
-        iconSize="20px"
-        width="100%"
-        m="0px"
-        px="0px"
-      >
-        <Text color="text" as="span">
-          {label}
-        </Text>
-        <AddIcon />
-      </StyledButton>
-    </StyledBox>
+    <StyledButton
+      buttonStyle="text"
+      onClick={onClick}
+      icon={icon}
+      iconColor={colors.adminTextColor}
+      iconSize="20px"
+      width="100%"
+      m="0px"
+      px="0px"
+    >
+      <Text color="text" as="span">
+        {label}
+      </Text>
+      <AddIcon />
+    </StyledButton>
   );
 };
 
