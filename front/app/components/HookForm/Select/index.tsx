1d6c958fd07b9e4e623825885cb3b01f4c777db1
Iva <iva@citizenlab.co>
2022-08-16 21:14:19 +0300

    [TEC-35] Transform input field form

---
+++ front/app/components/HookForm/Select/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  Select as SelectComponent,
+  SelectProps,
+} from '@citizenlab/cl2-component-library';
+import Error, { TFieldName } from 'components/UI/Error';
+import { Controller, useFormContext } from 'react-hook-form';
+import { CLError } from 'typings';
+
+export interface Props extends Omit<SelectProps, 'onChange'> {
+  name: string;
+}
+
+const Select = ({ name, ...rest }: Props) => {
+  const {
+    formState: { errors },
+    control,
+  } = useFormContext();
+
+  const defaultValue = false;
+
+  const validationError = errors[name]?.message as string | undefined;
+
+  const apiError =
+    (errors[name]?.error as string | undefined) &&
+    ([errors[name]] as unknown as CLError[]);
+
+  return (
+    <>
+      <Controller
+        name={name}
+        control={control}
+        defaultValue={defaultValue}
+        render={({ field: { ref: _ref, ...field } }) => {
+          return <SelectComponent id={name} {...field} {...rest} />;
+        }}
+      />
+      {validationError && (
+        <Error
+          marginTop="8px"
+          marginBottom="8px"
+          text={validationError}
+          scrollIntoView={false}
+        />
+      )}
+      {apiError && (
+        <Error
+          fieldName={name as TFieldName}
+          apiErrors={apiError}
+          marginTop="8px"
+          marginBottom="8px"
+          scrollIntoView={false}
+        />
+      )}
+    </>
+  );
+};
+
+export default Select;
---
+++ front/app/components/HookForm/TextAreaMultilocWithLocaleSwitcher/index.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import TextAreaMultilocWithLocaleSwitcherComponent, {
+  Props as TextAreaMultilocWithLocaleSwitcherProps,
+} from 'components/UI/TextAreaMultilocWithLocaleSwitcher';
+import Error, { TFieldName } from 'components/UI/Error';
+import { Controller, FieldError, useFormContext } from 'react-hook-form';
+import { CLError, Locale } from 'typings';
+import useAppConfigurationLocales from 'hooks/useAppConfigurationLocales';
+import { isNilOrError } from 'utils/helperUtils';
+
+type TextAreaProps = Props & {
+  name: string;
+};
+
+export interface Props
+  extends Omit<
+    TextAreaMultilocWithLocaleSwitcherProps,
+    'onChange' | 'valueMultiloc'
+  > {
+  name: string;
+}
+
+const TextAreaMultilocWithLocaleSwitcher = ({
+  name,
+  ...rest
+}: TextAreaProps) => {
+  const {
+    formState: { errors },
+    control,
+  } = useFormContext();
+
+  const locales = useAppConfigurationLocales();
+
+  if (isNilOrError(locales)) {
+    return null;
+  }
+
+  const defaultValue: Partial<Record<Locale, any>> = locales.reduce(
+    (acc, curr) => ((acc[curr] = ''), acc),
+    {}
+  );
+
+  // Select the first error messages from the field's multiloc validation error
+  const validationError = Object.values(
+    (errors[name] as Record<Locale, FieldError> | undefined) || {}
+  )[0]?.message;
+
+  const apiError =
+    (errors[name]?.error as string | undefined) &&
+    ([errors[name]] as unknown as CLError[]);
+
+  return (
+    <>
+      <Controller
+        name={name}
+        control={control}
+        defaultValue={defaultValue}
+        render={({ field: { ref: _ref, ...field } }) => {
+          return (
+            <TextAreaMultilocWithLocaleSwitcherComponent
+              id={name}
+              {...field}
+              {...rest}
+              valueMultiloc={{ ...defaultValue, ...field.value }}
+            />
+          );
+        }}
+      />
+      {validationError && (
+        <Error
+          marginTop="8px"
+          marginBottom="8px"
+          text={validationError}
+          scrollIntoView={false}
+        />
+      )}
+      {apiError && (
+        <Error
+          fieldName={name as TFieldName}
+          apiErrors={apiError}
+          marginTop="8px"
+          marginBottom="8px"
+          scrollIntoView={false}
+        />
+      )}
+    </>
+  );
+};
+
+export default TextAreaMultilocWithLocaleSwitcher;
