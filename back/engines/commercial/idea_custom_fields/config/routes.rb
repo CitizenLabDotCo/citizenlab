8fc487b7cfccfd85cb6e3bb93763d0b76e110833
Sebastien Hoorens <sebastien@citizenlab.co>
2022-08-18 17:50:53 +0200

    added container type to custom fields routes

--- back/engines/commercial/idea_custom_fields/config/routes.rb
+++ back/engines/commercial/idea_custom_fields/config/routes.rb
@@ -4,11 +4,18 @@ IdeaCustomFields::Engine.routes.draw do
   namespace :web_api, defaults: { format: :json } do
     namespace :v1 do
       namespace :admin do
-        resources :projects, only: [] do
-          resources :custom_fields, only: %i[index show], controller: 'idea_custom_fields' do
-            patch 'by_code/:code', action: 'upsert_by_code', on: :collection
-            patch 'update/:id', action: 'update', on: :collection
-            patch 'update_all', on: :collection
+        { projects: 'Project', phases: 'Phase' }.each do |resource, container_type|
+          resources resource, only: [] do
+            resources(
+              :custom_fields,
+              only: %i[index show],
+              controller: 'idea_custom_fields',
+              defaults: { container_type: container_type }
+            ) do
+              patch 'by_code/:code', action: 'upsert_by_code', on: :collection
+              patch 'update/:id', action: 'update', on: :collection
+              patch 'update_all', on: :collection
+            end
           end
         end
       end
