474d825d30202589d07696eff9b6fcaba855baf8
Koen De Hondt <koen@dehondt.dev>
2022-08-19 13:51:56 +0200

    [CL-1380] Add ParticipationContext#can_contain_input?, which was missing from an earlier commit

--- back/spec/models/phase_spec.rb
+++ back/spec/models/phase_spec.rb
@@ -11,6 +11,29 @@ RSpec.describe Phase, type: :model do
     end
   end
 
+  describe '#can_contain_input?' do
+    expected_results = {
+      'information' => false,
+      'ideation' => true,
+      'survey' => false,
+      'budgeting' => true,
+      'poll' => false,
+      'volunteering' => false,
+      'native_survey' => true
+    }
+    # Written this way so that additional participation methods will make this spec fail.
+    ::ParticipationContext::PARTICIPATION_METHODS.each do |participation_method|
+      expected_result = expected_results[participation_method]
+      context "for #{participation_method}" do
+        let(:phase) { build(:phase, participation_method: participation_method) }
+
+        it "returns #{expected_result}" do
+          expect(phase.can_contain_input?).to be expected_result
+        end
+      end
+    end
+  end
+
   describe 'description sanitizer' do
     it 'sanitizes script tags in the description' do
       phase = create(:phase, description_multiloc: {
