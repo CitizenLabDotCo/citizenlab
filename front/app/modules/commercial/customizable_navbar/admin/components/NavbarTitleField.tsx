2e3c32adc634a5cd57584a753b8fccfe3c05f03f
Alexander <alexander@citizenlab.co>
2022-08-19 09:11:48 +0200

    [CL-1308] Import correct component

--- front/app/modules/commercial/customizable_navbar/admin/components/NavbarTitleField.tsx
+++ front/app/modules/commercial/customizable_navbar/admin/components/NavbarTitleField.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { InputMultilocWithLocaleSwitcher } from '@citizenlab/cl2-component-library';
+import InputMultilocWithLocaleSwitcher from 'components/HookForm/InputMultilocWithLocaleSwitcher';
 import { InjectedIntlProps } from 'react-intl';
 import { injectIntl } from 'utils/cl-intl';
 import messages from './messages';
@@ -10,9 +10,6 @@ const NavbarTitleField = ({ intl: { formatMessage } }: InjectedIntlProps) => {
       label={formatMessage(messages.navbarItemTitle)}
       type="text"
       name="nav_bar_item_title_multiloc"
-      // ToDO: remove next two lines
-      valueMultiloc={undefined}
-      locales={[]}
     />
   );
 };
