62ec2510b04f1ac7e708e06dd35151209a09e684
Simon Tharby <simontharby@gmail.com>
2022-08-22 18:29:33 +0100

    [CL-1152] More fixes to get more existing BE specs green

--- back/engines/commercial/admin_api/app/graphql/admin_api/types/page_type.rb
+++ back/engines/commercial/admin_api/app/graphql/admin_api/types/page_type.rb
@@ -6,7 +6,7 @@ module AdminApi
 
     field :id, ID, null: false
     field :title_multiloc, Types::MultilocType, null: false
-    field :body_multiloc, Types::MultilocType, null: false
+    field :top_info_section_multiloc, Types::MultilocType, null: false
     field :slug, String, null: false
     field :updated_at, String, null: false
     field :created_at, String, null: false
