eb4bdc10873fea66013bace9786883d2c5f7c991
Sebastien Hoorens <sebastien@citizenlab.co>
2022-08-22 14:36:03 +0200

    get custom fields of a phase

--- back/engines/commercial/idea_custom_fields/config/routes.rb
+++ back/engines/commercial/idea_custom_fields/config/routes.rb
@@ -4,18 +4,26 @@ IdeaCustomFields::Engine.routes.draw do
   namespace :web_api, defaults: { format: :json } do
     namespace :v1 do
       namespace :admin do
-        { projects: 'Project', phases: 'Phase' }.each do |resource, container_type|
-          resources resource, only: [] do
-            resources(
-              :custom_fields,
-              only: %i[index show],
-              controller: 'idea_custom_fields',
-              defaults: { container_type: container_type }
-            ) do
-              patch 'by_code/:code', action: 'upsert_by_code', on: :collection
-              patch 'update/:id', action: 'update', on: :collection
-              patch 'update_all', on: :collection
-            end
+        resources :projects, only: [] do
+          resources(
+            :custom_fields,
+            only: %i[index show],
+            controller: 'idea_custom_fields',
+            defaults: { container_type: 'Project' }
+          ) do
+            patch 'by_code/:code', action: 'upsert_by_code', on: :collection
+            patch 'update/:id', action: 'update', on: :collection
+            patch 'update_all', on: :collection
+          end
+        end
+        resources :phases, only: [] do
+          resources(
+            :custom_fields,
+            only: %i[index show],
+            controller: 'idea_custom_fields',
+            defaults: { container_type: 'Phase' }
+          ) do
+            patch 'update_all', on: :collection
           end
         end
       end
