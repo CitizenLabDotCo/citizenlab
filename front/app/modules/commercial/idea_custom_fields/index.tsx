861c0a5d4f0092b70e5bb120a297ce674d156141
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2022-08-23 23:54:41 +0200

    CL-1361 WIP Simplified conditional form input tab rendering

--- front/app/modules/commercial/idea_custom_fields/index.tsx
+++ front/app/modules/commercial/idea_custom_fields/index.tsx
@@ -1,8 +1,6 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { ModuleConfiguration } from 'utils/moduleUtils';
 import Tab from './admin/components/Tab';
-import { IProjectData } from 'services/projects';
-import { IPhaseData } from 'services/phases';
 import {
   getAllParticipationMethods,
   getMethodConfig,
@@ -12,29 +10,6 @@ import { some } from 'lodash-es';
 const AdminProjectIdeaEditFormComponent = React.lazy(
   () => import('./admin/containers/projects/edit/ideaform')
 );
-
-type RenderIfSimpleFormEditorProps = {
-  project: IProjectData;
-  phases: IPhaseData[] | null;
-  children: ReactNode;
-};
-
-const RenderIfSimpleFormEditor = (props: RenderIfSimpleFormEditorProps) => {
-  const { project, phases, children } = props;
-
-  const allParticipationMethods = getAllParticipationMethods(project, phases);
-
-  const showTab = some(
-    allParticipationMethods,
-    (method) => getMethodConfig(method).formEditor === 'simpleFormEditor'
-  );
-
-  if (showTab) {
-    return <>{children}</>;
-  }
-
-  return null;
-};
 
 const configuration: ModuleConfiguration = {
   routes: {
@@ -47,11 +22,19 @@ const configuration: ModuleConfiguration = {
   },
   outlets: {
     'app.containers.Admin.projects.edit': (props) => {
-      return (
-        <RenderIfSimpleFormEditor project={props.project} phases={props.phases}>
-          <Tab {...props} />
-        </RenderIfSimpleFormEditor>
+      const { project, phases } = props;
+      const allParticipationMethods = getAllParticipationMethods(
+        project,
+        phases
       );
+      const showTab = some(
+        allParticipationMethods,
+        (method) => getMethodConfig(method).formEditor === 'simpleFormEditor'
+      );
+      if (showTab) {
+        return <Tab {...props} />;
+      }
+      return null;
     },
   },
 };
