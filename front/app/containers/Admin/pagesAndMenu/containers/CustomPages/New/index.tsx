1974975de9b3a3a699ee9b52d74a61e25bec05c6
brentguf <brentguf@users.noreply.github.com>
2022-08-24 15:51:40 +0200

    Add breadcrumbs + meta title placeholder

--- front/app/containers/Admin/pagesAndMenu/containers/CustomPages/New/index.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/CustomPages/New/index.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
 import CustomPageSettingsForm from '../CustomPageSettingsForm';
+import { Box } from '@citizenlab/cl2-component-library';
+import Breadcrumbs from 'components/UI/Breadcrumbs';
+import { pagesAndMenuBreadcrumb } from 'containers/Admin/pagesAndMenu/breadcrumbs';
+import HelmetIntl from 'components/HelmetIntl';
+import messages from '../messages';
+import { InjectedIntlProps } from 'react-intl';
+import { injectIntl } from 'utils/cl-intl';
 
-const CustomPagesNewSettings = () => {
-  return <CustomPageSettingsForm />;
+const CustomPagesNewSettings = ({
+  intl: { formatMessage },
+}: InjectedIntlProps) => {
+  return (
+    <>
+      <HelmetIntl
+        title={messages.newCustomPageMetaTitle}
+        description={messages.newCustomPageMetaDescription}
+      />
+      <Box mb="16px">
+        <Breadcrumbs
+          breadcrumbs={[
+            {
+              label: formatMessage(pagesAndMenuBreadcrumb.label),
+              linkTo: pagesAndMenuBreadcrumb.linkTo,
+            },
+            { label: formatMessage(messages.newCustomPagePageTitle) },
+          ]}
+        />
+      </Box>
+      <CustomPageSettingsForm />;
+    </>
+  );
 };
 
-export default CustomPagesNewSettings;
+export default injectIntl(CustomPagesNewSettings);
