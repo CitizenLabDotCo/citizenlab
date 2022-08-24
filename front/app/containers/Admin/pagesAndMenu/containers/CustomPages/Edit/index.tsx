9ba08c7819ffd038cc7ea4809c2fc76b067b4a8c
brentguf <brentguf@users.noreply.github.com>
2022-08-24 15:42:40 +0200

    Add breadcrumbs for custom page edit page

--- front/app/containers/Admin/pagesAndMenu/containers/CustomPages/Edit/index.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/CustomPages/Edit/index.tsx
@@ -5,19 +5,43 @@ import messages from '../messages';
 import { InjectedIntlProps } from 'react-intl';
 import { injectIntl } from 'utils/cl-intl';
 import HelmetIntl from 'components/HelmetIntl';
+import { Box } from '@citizenlab/cl2-component-library';
+import Breadcrumbs from 'components/UI/Breadcrumbs';
+import { pagesAndMenuBreadcrumb } from 'containers/Admin/pagesAndMenu/breadcrumbs';
+// import useLocalize from 'hooks/useLocalize';
+// import { isNilOrError } from 'utils/helperUtils';
 
 const CustomPagesEditSettings = ({
   intl: { formatMessage },
 }: InjectedIntlProps) => {
+  // const localize = useLocalize();
+  // const customPage = useCustomPage(customPageId);
+
+  // if (!isNilOrError(customPage)) {
+  // const pageTitleMultiloc = customPage.attributes.title_multiloc;
+
   return (
     <>
       <HelmetIntl
         title={messages.editCustomPageMetaTitle}
         description={messages.editCustomPageMetaDescription}
       />
+      <Box mb="16px">
+        <Breadcrumbs
+          breadcrumbs={[
+            {
+              label: formatMessage(pagesAndMenuBreadcrumb.label),
+              linkTo: pagesAndMenuBreadcrumb.linkTo,
+            },
+            // { label: localize(pageTitleMultiloc) },
+          ]}
+        />
+      </Box>
       <TabbedResource
         resource={{
           title: formatMessage(messages.editCustomPagePageTitle),
+          // The page title will be the final value here
+          // title: localize(pageTitleMultiloc),
         }}
         tabs={[
           {
@@ -37,6 +61,9 @@ const CustomPagesEditSettings = ({
       </TabbedResource>
     </>
   );
+  // }
+
+  // return null;
 };
 
 export default injectIntl(CustomPagesEditSettings);
