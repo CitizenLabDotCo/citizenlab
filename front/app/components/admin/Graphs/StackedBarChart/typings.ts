c6e7585f9043ebd124523fb1eb348a2cfaec13a5
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-08-24 16:02:56 +0100

    [CL-1307] Add mouse handlers to StackedBarChart props

--- front/app/components/admin/Graphs/StackedBarChart/typings.ts
+++ front/app/components/admin/Graphs/StackedBarChart/typings.ts
@@ -1,8 +1,14 @@
 import { Props as MultiBarChartProps } from '../MultiBarChart/typings';
 import { KeyOfType, BaseMapping, Cell } from '../typings';
 
-export interface Props<Row> extends Omit<MultiBarChartProps<Row>, 'mapping'> {
+export interface Props<Row>
+  extends Omit<
+    MultiBarChartProps<Row>,
+    'mapping' | 'onMouseOver' | 'onMouseOut'
+  > {
   mapping: Mapping<Row>;
+  onMouseOver?: (payload: Payload<Row>, event: React.MouseEvent) => void;
+  onMouseOut?: (payload: Payload<Row>, event: React.MouseEvent) => void;
 }
 
 interface Payload<Row> {
