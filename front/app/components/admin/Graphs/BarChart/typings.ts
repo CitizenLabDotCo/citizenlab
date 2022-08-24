54b222ad2d53fc7922e6025466f45ffc37b531a6
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-08-24 16:00:10 +0100

    [CL-1307] Make BarChart mapping also inherit from base mapping

--- front/app/components/admin/Graphs/BarChart/typings.ts
+++ front/app/components/admin/Graphs/BarChart/typings.ts
@@ -3,7 +3,7 @@ import {
   Props as MultiBarChartProps,
   Bars as MultiBarChartBars,
 } from '../MultiBarChart/typings';
-import { KeyOfType } from '../typings';
+import { KeyOfType, BaseMapping } from '../typings';
 
 export interface Props<Row>
   extends Omit<
@@ -21,11 +21,9 @@ export interface Payload<Row> {
   rowIndex: number;
 }
 
-export interface Mapping<Row> {
+export interface Mapping<Row> extends BaseMapping<Payload<Row>> {
   category: KeyOfType<Row, string>;
   length: KeyOfType<Row, number>;
-  fill?: (payload: Payload<Row>) => string;
-  opacity?: (payload: Payload<Row>) => number;
 }
 
 export interface Bars extends Omit<MultiBarChartBars, 'names'> {
