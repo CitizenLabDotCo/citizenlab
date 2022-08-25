400d7728ceb27c231e6acdc027262ba1f53d6381
Fran <francisco.pensa.web@gmail.com>
2022-08-25 11:52:32 -0300

    [CL-423] add validation tests

--- back/engines/commercial/analytics/spec/services/analytics/query_validator_service_spec.rb
+++ back/engines/commercial/analytics/spec/services/analytics/query_validator_service_spec.rb
@@ -5,12 +5,48 @@ require 'query'
 
 describe Analytics::QueryValidatorService do
   describe 'validate query object with json schema' do
-    it 'fails on new top level attributes' do
-      query_param = ActionController::Parameters.new(query: { other: 'value' })
+    it 'fails on empty query' do
+      query_param = ActionController::Parameters.new({})
       query = Analytics::Query.new(query_param)
 
       validator = described_class.new(query)
       expect(validator.valid).to be false
+      expect(validator.messages).to include 'The property \'#/\' did not contain a required property of \'fact\' in schema file:///cl2_back/engines/commercial/analytics/app/services/analytics/query_schema.json'
+    end
+
+    it 'fails on inexistent query attribute' do
+      query_param = ActionController::Parameters.new(fact: 'post', other: 'value')
+      query = Analytics::Query.new(query_param)
+
+      validator = described_class.new(query)
+      expect(validator.valid).to be false
+      expect(validator.messages).to include 'The property \'#/\' contains additional properties ["other"] outside of the schema when none are allowed in schema file:///cl2_back/engines/commercial/analytics/app/services/analytics/query_schema.json'
+    end
+
+    it 'fails on inexistent column reference' do
+      query_param = ActionController::Parameters.new(fact: 'post', fields: 'some_field')
+      query = Analytics::Query.new(query_param)
+
+      validator = described_class.new(query)
+      expect(validator.valid).to be false
+      expect(validator.messages).to include 'Fields column some_field does not exist in fact table.'
+    end
+
+    it 'fails on invalid dates' do
+      query_param = ActionController::Parameters.new(
+        fact: 'post',
+        fields: 'id',
+        dimensions: {
+          created_date: {
+            date: { from: 'xxxx', to: '2022-01-01' }
+          }
+        }
+      )
+      query = Analytics::Query.new(query_param)
+
+      validator = described_class.new(query)
+      expect(validator.valid).to be false
+      expect(validator.messages).to include 'Invalid \'from\' date in created_date dimension.'
     end
   end
 end
