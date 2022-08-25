767b11d2752cfcccea27cdee9318d0a3a1bd76df
Fran <francisco.pensa.web@gmail.com>
2022-08-25 17:10:10 -0300

    [CL-423] update analytics models base class

--- back/engines/commercial/analytics/app/models/analytics/application_view.rb
+++ back/engines/commercial/analytics/app/models/analytics/application_view.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Analytics
-  class ApplicationView < ActiveRecord::Base
+  class ApplicationView < ApplicationRecord
     self.abstract_class = true
     # connects_to database: { writing: :analytics, reading: :analytics }
     def readonly?
