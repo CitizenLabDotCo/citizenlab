78f4cdb2d3d28bb29d7f8508bcb244121acc1a15
Fran <francisco.pensa.web@gmail.com>
2022-08-26 13:32:53 -0300

    [CL-423] update lock and schema files

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_26_015244) do
+ActiveRecord::Schema.define(version: 2022_08_26_025846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
