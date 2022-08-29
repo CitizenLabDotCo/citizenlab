419870dd42c73216c690c7c43e5c9bf4d828e9b7
Fran <francisco.pensa.web@gmail.com>
2022-08-29 09:39:23 -0300

    [CL-1180] wip remove main directory migrations and views

--- back/db/migrate/20220826025843_create_dimension_projects_view.analytics.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-# This migration comes from analytics (originally 20220624101040)
-
-class CreateDimensionProjectsView < ActiveRecord::Migration[6.1]
-  def change
-    create_view :analytics_dimension_projects
-  end
-end
--- back/db/migrate/20220826025846_create_fact_participations_view.analytics.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-# This migration comes from analytics (originally 20220624143207)
-
-class CreateFactParticipationsView < ActiveRecord::Migration[6.1]
-  def change
-    create_view :analytics_fact_participations
-  end
-end
