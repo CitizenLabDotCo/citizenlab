6753f0a44a1e5736999e1e060c9552f3d00d4652
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-08-30 15:44:58 +0100

    [CL-1307] Improve request parameter typings

---
+++ front/app/utils/streams/streams.test.ts
@@ -0,0 +1,13 @@
+import { Streams } from '.';
+
+let streams: Streams;
+
+describe('streams.get', () => {
+  beforeEach(() => {
+    streams = new Streams();
+  })
+
+  it('', () => {
+    // TODO
+  })
+})
