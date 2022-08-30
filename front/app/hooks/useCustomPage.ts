422fd809005976daaf9db1359d2dc2b2bf486add
lucas <lucasquinnmathis@gmail.com>
2022-08-30 11:46:24 +0200

    Additional types

--- front/app/hooks/useCustomPage.ts
+++ front/app/hooks/useCustomPage.ts
@@ -1,22 +1,21 @@
 import { useState, useEffect } from 'react';
-import {
-  customPageByIdStream,
-} from 'services/customPages';
+import { customPageByIdStream, ICustomPageData } from 'services/customPages';
 import { isNilOrError } from 'utils/helperUtils';
 
 export default function useCustomPage(customPageId: string) {
-  const [customPage, setCustomPage] = useState(null);
+  const [customPage, setCustomPage] = useState<ICustomPageData | null | Error>(
+    null
+  );
 
   useEffect(() => {
-    const subscription = customPageByIdStream(customPageId).observable.subscribe(
-      (returnedCustomPage) => {
-        isNilOrError(returnedCustomPage)
-        // @ts-ignore
-          ? setCustomPage(returnedCustomPage)
-        // @ts-ignore
-          : setCustomPage(returnedCustomPage.data);
-      }
-    );
+    const subscription = customPageByIdStream(
+      customPageId
+    ).observable.subscribe((returnedCustomPage) => {
+      const apiCustomPage = !isNilOrError(returnedCustomPage)
+        ? returnedCustomPage.data
+        : returnedCustomPage;
+      setCustomPage(apiCustomPage);
+    });
 
     return () => subscription.unsubscribe();
   }, [customPageId]);
--- front/app/services/customPages.ts
+++ front/app/services/customPages.ts
@@ -1,11 +1,46 @@
 import { API_PATH } from 'containers/App/constants';
 import streams from 'utils/streams';
-import { Multiloc } from 'typings';
+import { Multiloc, ImageSizes } from 'typings';
+import { THomepageBannerLayout } from './homepageSettings';
 
-interface ICustomPage {
-  data: { id: string };
+export interface ICustomPage {
+  data: ICustomPageData;
 }
 
+export interface ICustomPageData {
+  id: string;
+  attributes: ICustomPagesAttributes;
+  // type: ''
+  // relationships:
+}
+
+export interface ICustomPagesAttributes {
+  top_info_section_enabled: boolean;
+  top_info_section_multiloc: Multiloc;
+  slug: string;
+  banner_enabled: boolean;
+  banner_layout: THomepageBannerLayout | null;
+  banner_overlay_color: string | null;
+  banner_overlay_opacity: number | null;
+  banner_cta_button_multiloc: Multiloc;
+  banner_cta_button_type: 'customized_button' | 'no_button' | null;
+  banner_cta_button_url: string | null;
+  banner_header_multiloc: Multiloc;
+  banner_subheader_multiloc: Multiloc;
+  files_section_enabled: boolean;
+  projects_enabled: boolean;
+  // check
+  projects_filter_type: 'area' | 'projects';
+  events_widget_enabled: boolean;
+  bottom_info_section_enabled: boolean;
+  bottom_info_section_multiloc: Multiloc;
+  header_bg: ImageSizes | null;
+  // check on which are possible
+  code: string;
+  // not sure about these
+  // pinned_admin_publication_ids: string[],
+  // static_page_file_id: string,
+}
 
 const customPagesEndpoint = `${API_PATH}/static_pages`;
 
@@ -13,11 +48,9 @@ export function createCustomPageStream(pageData: { title_multiloc: Multiloc }) {
   return streams.add<ICustomPage>(`${customPagesEndpoint}`, pageData);
 }
 
-export function customPageByIdStream(
-  customPageId: string,
-) {
-  return streams.get({
-    apiEndpoint: `${customPagesEndpoint}/${customPageId}`
+export function customPageByIdStream(customPageId: string) {
+  return streams.get<ICustomPage>({
+    apiEndpoint: `${customPagesEndpoint}/${customPageId}`,
   });
 }
 
