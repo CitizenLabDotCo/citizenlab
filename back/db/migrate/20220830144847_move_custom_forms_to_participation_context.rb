d96e68d3ad56ef968b36d9e83004c24383c9b94f
Koen De Hondt <koen@dehondt.dev>
2022-08-30 16:45:37 +0200

    [CL-1219] Add comments to the migration

--- back/db/migrate/20220830144847_move_custom_forms_to_participation_context.rb
+++ back/db/migrate/20220830144847_move_custom_forms_to_participation_context.rb
@@ -11,6 +11,11 @@ class MoveCustomFormsToParticipationContext < ActiveRecord::Migration[6.1]
         WHERE projects.custom_form_id = t.id
       )
     SQL
+    # We identified that some tenants in production have forms that are not
+    # associated with a project. That is problematic, because `participation_context_id`
+    # cannot be nil in a polymorphic association.
+    # Forms without projects are inconsistent data and they are useless,
+    # so we delete them, together with their fields and field options.
     delete_custom_form_results = ActiveRecord::Base.connection.execute <<~SQL.squish
       SELECT id
       FROM custom_forms
@@ -23,7 +28,8 @@ class MoveCustomFormsToParticipationContext < ActiveRecord::Migration[6.1]
         DELETE FROM custom_forms
         WHERE id IN (#{sql_form_ids})
       SQL
-
+      # If there are custom fields associated with a custom form that has to be deleted,
+      # we have to delete them as well.
       delete_custom_field_results = ActiveRecord::Base.connection.execute <<~SQL.squish
         SELECT id
         FROM custom_fields
@@ -32,6 +38,8 @@ class MoveCustomFormsToParticipationContext < ActiveRecord::Migration[6.1]
       delete_custom_field_ids = delete_custom_field_results.pluck 'id'
       if delete_custom_field_ids.present?
         sql_field_ids = delete_custom_field_ids.map { |id| "'#{id}'" }.join(', ')
+        # If there are custom field options associated with a custom field that has
+        # to be deleted, we have to delete them as well.
         ActiveRecord::Base.connection.execute <<~SQL.squish
           DELETE FROM custom_field_options
           WHERE custom_field_id IN (#{sql_field_ids})
