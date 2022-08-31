a4a69f556123ce43877bd15f8d47559d868e5944
Simon Tharby <simontharby@gmail.com>
2022-08-31 16:18:45 +0100

    [CL-900] Add header image to citizenlab seed

--- back/db/seeds/citizenlab.rb
+++ back/db/seeds/citizenlab.rb
@@ -547,7 +547,9 @@ open_idea_project.project_images.create!(remote_image_url: 'https://res.cloudina
 open_idea_project.set_default_topics!
 
 # Create settings for Home Page.
-HomePage.create!
+HomePage.create!({
+  header_bg: Rails.root.join('spec/fixtures/header.jpg').open
+})
 
 User.find_each do |user|
   EmailCampaigns::UnsubscriptionToken.create!(user_id: user.id)
