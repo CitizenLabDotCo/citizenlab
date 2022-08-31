bb5782670874a11e3c5108ce70305d848e01fad8
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-08-31 09:49:11 +0100

    [CL-1356] _

--- back/db/migrate/20220829144852_create_build_feedbacks_view.analytics.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-# This migration comes from analytics (originally 20220624102332)
-
-class CreateBuildFeedbacksView < ActiveRecord::Migration[6.1]
-  def change
-    create_view :analytics_build_feedbacks
-  end
-end
