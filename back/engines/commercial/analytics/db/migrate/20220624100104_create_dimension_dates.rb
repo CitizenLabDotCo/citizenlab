89ac1f3ac6f9427c2c211ea6389baea1db10b851
Fran <francisco.pensa.web@gmail.com>
2022-08-31 14:06:55 -0300

    [CL-423] update table migrations

--- back/engines/commercial/analytics/db/migrate/20220624100104_create_dimension_dates.rb
+++ back/engines/commercial/analytics/db/migrate/20220624100104_create_dimension_dates.rb
@@ -1,16 +1,12 @@
 # frozen_string_literal: true
 
 class CreateDimensionDates < ActiveRecord::Migration[6.1]
-  def up
+  def change
     create_table :analytics_dimension_dates, id: false do |t|
       t.date :date, primary_key: true
       t.string :year
       t.string :month
       t.string :day
     end
-  end
-
-  def down
-    drop_table :analytics_dimension_dates
   end
 end
