3db5b16ff9641270f7e74f300612d496568385d7
Edwin Kato <katedwin88@gmail.com>
2022-08-31 11:07:37 +0300

    Clean up null return in form builder settings

--- front/app/containers/Admin/formBuilder/components/FormBuilderSettings/index.tsx
+++ front/app/containers/Admin/formBuilder/components/FormBuilderSettings/index.tsx
@@ -31,6 +31,11 @@ interface Props {
 
 const FormBuilderSettings = ({ field, onDelete, onClose }: Props) => {
   const locales = useAppConfigurationLocales();
+
+  if (isNilOrError(locales)) {
+    return null;
+  }
+
   let translatedStringKey: MessageDescriptor | null = null;
   switch (field.input_type) {
     case 'text':
@@ -41,79 +46,76 @@ const FormBuilderSettings = ({ field, onDelete, onClose }: Props) => {
       break;
   }
 
-  if (!isNilOrError(locales) && locales !== undefined) {
-    return (
-      <Box
-        position="fixed"
-        right="0"
-        top={`${stylingConsts.menuHeight}px`}
-        zIndex="99999"
-        px="20px"
-        w="400px"
-        h="100%"
-        background="white"
-        boxShadow="-2px 0px 1px 0px rgba(0, 0, 0, 0.06)"
-      >
-        <Box position="absolute" right="8px" mb="20px">
-          <CloseIconButton
-            a11y_buttonActionMessage={messages.close}
-            onClick={onClose}
-            iconColor={colors.label}
-            iconColorOnHover={'#000'}
-          />
-        </Box>
-        {translatedStringKey && (
-          <SectionTitle>
-            <FormattedMessage {...translatedStringKey} />
-          </SectionTitle>
-        )}
-        <SectionField>
-          <InputMultilocWithLocaleSwitcher
-            name={`customFields.${field.index}.title_multiloc`}
-            label={<FormattedMessage {...messages.questionTitle} />}
-            type="text"
-          />
-        </SectionField>
-        <SectionField>
-          <InputMultilocWithLocaleSwitcher
-            name={`customFields.${field.index}.description_multiloc`}
-            label={<FormattedMessage {...messages.questionDescription} />}
-            type="text"
-          />
-        </SectionField>
-        <SectionField>
-          <Toggle
-            name={`customFields.${field.index}.required`}
-            label={
-              <Text as="span" color="adminTextColor" variant="bodyM" my="0px">
-                <FormattedMessage {...messages.required} />
-              </Text>
-            }
-          />
-        </SectionField>
-        {field.input_type === 'multiselect' && (
-          <ConfigMultiselectWithLocaleSwitcher // TODO: Abstract logic to somewhere else? Could be messy with more field types.
-            name={`customFields.${field.index}.options`}
-            locales={locales}
-          />
-        )}
-        <Box display="flex" justifyContent="space-between">
-          <Button
-            icon="delete"
-            buttonStyle="primary-outlined"
-            borderColor={colors.red500}
-            textColor={colors.red500}
-            iconColor={colors.red500}
-            onClick={() => onDelete(field.index)}
-            minWidth="160px"
-          >
-            <FormattedMessage {...messages.delete} />
-          </Button>
-        </Box>
+  return (
+    <Box
+      position="fixed"
+      right="0"
+      top={`${stylingConsts.menuHeight}px`}
+      zIndex="99999"
+      px="20px"
+      w="400px"
+      h="100%"
+      background="white"
+      boxShadow="-2px 0px 1px 0px rgba(0, 0, 0, 0.06)"
+    >
+      <Box position="absolute" right="8px" mb="20px">
+        <CloseIconButton
+          a11y_buttonActionMessage={messages.close}
+          onClick={onClose}
+          iconColor={colors.label}
+          iconColorOnHover={'#000'}
+        />
       </Box>
-    );
-  }
-  return null;
+      {translatedStringKey && (
+        <SectionTitle>
+          <FormattedMessage {...translatedStringKey} />
+        </SectionTitle>
+      )}
+      <SectionField>
+        <InputMultilocWithLocaleSwitcher
+          name={`customFields.${field.index}.title_multiloc`}
+          label={<FormattedMessage {...messages.questionTitle} />}
+          type="text"
+        />
+      </SectionField>
+      <SectionField>
+        <InputMultilocWithLocaleSwitcher
+          name={`customFields.${field.index}.description_multiloc`}
+          label={<FormattedMessage {...messages.questionDescription} />}
+          type="text"
+        />
+      </SectionField>
+      <SectionField>
+        <Toggle
+          name={`customFields.${field.index}.required`}
+          label={
+            <Text as="span" color="adminTextColor" variant="bodyM" my="0px">
+              <FormattedMessage {...messages.required} />
+            </Text>
+          }
+        />
+      </SectionField>
+      {field.input_type === 'multiselect' && (
+        <ConfigMultiselectWithLocaleSwitcher // TODO: Abstract logic to somewhere else? Could be messy with more field types.
+          name={`customFields.${field.index}.options`}
+          locales={locales}
+        />
+      )}
+      <Box display="flex" justifyContent="space-between">
+        <Button
+          icon="delete"
+          buttonStyle="primary-outlined"
+          borderColor={colors.red500}
+          textColor={colors.red500}
+          iconColor={colors.red500}
+          onClick={() => onDelete(field.index)}
+          minWidth="160px"
+        >
+          <FormattedMessage {...messages.delete} />
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default FormBuilderSettings;
