c3a64c1eb214841ca5de66452f8f1f78dd3b53a7
Simon Tharby <simontharby@gmail.com>
2022-08-31 11:43:56 +0100

    [CL-900] Removae useless call to before_update

--- back/app/controllers/web_api/v1/app_configurations_controller.rb
+++ back/app/controllers/web_api/v1/app_configurations_controller.rb
@@ -10,7 +10,6 @@ class WebApi::V1::AppConfigurationsController < ApplicationController
   def update
     update_configuration!(app_configuration, config_params)
     side_fx_service = SideFxAppConfigurationService.new
-    side_fx_service.before_update(app_configuration, current_user)
 
     if app_configuration.save
       side_fx_service.after_update(app_configuration, current_user)
