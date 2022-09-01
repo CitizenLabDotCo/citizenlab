859ec6a9500459bc0e86dcd11b1f9d0e040d1dcb
Fran <francisco.pensa.web@gmail.com>
2022-09-01 15:54:09 -0300

    [CL-423] remove comment

--- back/engines/commercial/analytics/lib/analytics/engine.rb
+++ back/engines/commercial/analytics/lib/analytics/engine.rb
@@ -14,15 +14,5 @@ module Analytics
       require 'analytics/feature_specification'
       AppConfiguration::Settings.add_feature(Analytics::FeatureSpecification)
     end
-
-    # Allow migrations to be created and run within the app
-    # initializer :append_migrations do |app|
-    #   unless app.root.to_s.match root.to_s
-    #     config.paths["db/migrate"].expanded.each do |expanded_path|
-    #       app.config.paths["db/migrate"] << expanded_path
-    #     end
-    #
-    #   end
-    # end
   end
 end
