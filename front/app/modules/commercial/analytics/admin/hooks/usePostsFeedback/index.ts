52856f5e07b4849fbde0e53d1f701afbce360aa8
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-09-05 16:48:19 +0100

    [CL-1587] Revert query change bc it's already on other branch and done properly there

--- front/app/modules/commercial/analytics/admin/hooks/usePostsFeedback/index.ts
+++ front/app/modules/commercial/analytics/admin/hooks/usePostsFeedback/index.ts
@@ -119,18 +119,6 @@ const query = ({ projectId, startAt, endAt }: QueryProps): Query => {
       'status.title_multiloc': 'first',
       'status.color': 'first',
     },
-    ...(projectId
-      ? {
-          dimensions: { project: { id: projectId } },
-        }
-      : {}),
-    ...(startAt && endAt
-      ? {
-          created_date: {
-            date: { from: toDate(startAt), to: toDate(endAt) },
-          },
-        }
-      : {}),
   };
 
   return { query: [queryFeedback, queryStatus] };
