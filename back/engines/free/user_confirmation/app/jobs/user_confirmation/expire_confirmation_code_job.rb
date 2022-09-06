1ce02e904fd1871ed42a07b87b8bed0b83e94731
Simon Tharby <simontharby@gmail.com>
2022-09-06 10:17:14 +0100

    [CL-1612] Simplify comment

--- back/engines/free/user_confirmation/app/jobs/user_confirmation/expire_confirmation_code_job.rb
+++ back/engines/free/user_confirmation/app/jobs/user_confirmation/expire_confirmation_code_job.rb
@@ -3,7 +3,7 @@
 module UserConfirmation
   class ExpireConfirmationCodeJob < ApplicationJob
     def run(user_id, code_to_expire)
-      # We first check if the record exists to cover the case where the user has been deleted.
+      # First check if the user record still exists.
       user = User.find_by(id: user_id)
       return if user.nil? || user.confirmed? || user.email_confirmation_code != code_to_expire
 
