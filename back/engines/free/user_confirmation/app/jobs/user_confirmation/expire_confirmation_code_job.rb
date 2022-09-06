c5ebd16ddf9a68c7d7dbac1a324cdd69cb237f72
Simon Tharby <simontharby@gmail.com>
2022-09-06 09:34:05 +0100

    [CL-1612] Pass reference not object & check record exists

--- back/engines/free/user_confirmation/app/jobs/user_confirmation/expire_confirmation_code_job.rb
+++ back/engines/free/user_confirmation/app/jobs/user_confirmation/expire_confirmation_code_job.rb
@@ -2,8 +2,10 @@
 
 module UserConfirmation
   class ExpireConfirmationCodeJob < ApplicationJob
-    def run(user, code_to_expire)
-      return if user.confirmed? || user.email_confirmation_code != code_to_expire
+    def run(user_id, code_to_expire)
+      # We first check if the record exists to cover the case where the user has been deleted.
+      user = User.find_by(id: user_id)
+      return if user.nil? || user.confirmed? || user.email_confirmation_code != code_to_expire
 
       user.reset_confirmation_code
       user.save
