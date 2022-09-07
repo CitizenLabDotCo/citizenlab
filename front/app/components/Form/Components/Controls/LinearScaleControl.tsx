8dd21a9ea9192dc999edec222baf48335cee0858
Amanda Anderson <amanda.anderson@citizenlab.co>
2022-09-07 15:54:02 +0200

    [CL-1546] Revert to working in front office WIP

--- front/app/components/Form/Components/Controls/LinearScaleControl.tsx
+++ front/app/components/Form/Components/Controls/LinearScaleControl.tsx
@@ -6,7 +6,7 @@ import { ControlProps } from '@jsonforms/core';
 import { getLabel, sanitizeForClassname } from 'utils/JSONFormUtils';
 
 // Components
-import { Box, Text, Radio, Label } from '@citizenlab/cl2-component-library';
+import { Box, Text, Radio } from '@citizenlab/cl2-component-library';
 import { FormLabel } from 'components/UI/FormComponents';
 import VerificationIcon from '../VerificationIcon';
 import ErrorDisplay from '../ErrorDisplay';
@@ -22,7 +22,7 @@ const LinearScaleControl = ({
   id,
 }: ControlProps) => {
   const maximum = schema?.properties?.rating?.maximum;
-  const minimumLabel = uischema.options?.minimum_label;
+
   return (
     <>
       <FormLabel
@@ -34,23 +34,23 @@ const LinearScaleControl = ({
       />
       <Box display="flex" flexDirection="row" gap="8px" overflow="visible">
         <Box alignSelf="flex-end">
-          <Text mr={minimumLabel ? '4px' : '0px'} fontWeight="bold">
-            {minimumLabel}
+          <Text mr="8px" fontWeight="bold">
+            {uischema.options?.minimum_label}
           </Text>
         </Box>
         <>
           {[...Array(maximum)].map((x, i) => (
             <Box key={i} style={{ lineHeight: '0px' }}>
-              <Box ml="4px" mt="16px">
-                <Label htmlFor={`${x}-radio-${i}`}>{i + 1}</Label>
-              </Box>
+              <Text mb="4px" ml="4px">
+                {i + 1}
+              </Text>
               <br />
               <Radio
                 name="linear_scale"
                 currentValue={data?.rating}
                 value={i + 1}
                 key={i}
-                id={`${x}-radio-${i}`}
+                id={x}
                 onChange={(value) => handleChange(path, { rating: value })}
               />
             </Box>
