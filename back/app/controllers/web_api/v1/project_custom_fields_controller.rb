c6c75fc06595cd5d6a7b7f412334c4c02b99800b
Koen De Hondt <koen@dehondt.dev>
2022-09-07 23:44:20 +0200

    [CL-1381] Rename IdeaCustomFieldsController to ProjectCustomFieldsController

---
+++ back/app/controllers/web_api/v1/project_custom_fields_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class WebApi::V1::ProjectCustomFieldsController < ApplicationController
+  skip_after_action :verify_policy_scoped
+  skip_after_action :verify_authorized
+  skip_before_action :authenticate_user
+
+  def schema
+    render json: CustomFieldService.new.ui_and_json_multiloc_schemas(AppConfiguration.instance, custom_fields)
+  end
+
+  def json_forms_schema
+    render json: JsonFormsService.new.input_ui_and_json_multiloc_schemas(custom_fields, current_user)
+  end
+
+  private
+
+  def project
+    @project ||= Project.find params[:project_id]
+  end
+
+  def custom_fields
+    @custom_fields ||= IdeaCustomFieldsService.new(custom_form).all_fields
+  end
+
+  def custom_form
+    project.custom_form || CustomForm.new(participation_context: project)
+  end
+end
