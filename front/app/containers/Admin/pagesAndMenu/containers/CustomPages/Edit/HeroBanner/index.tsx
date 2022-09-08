2f8f764aa7898b3289645554f32a632a094fd9ca
brentguf <brentguf@users.noreply.github.com>
2022-09-08 11:10:34 +0200

    Data loading and saving Edit custom page form

--- front/app/containers/Admin/pagesAndMenu/containers/CustomPages/Edit/HeroBanner/index.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/CustomPages/Edit/HeroBanner/index.tsx
@@ -1,12 +1,8 @@
-import React, { useState } from 'react';
-import { ICustomPagesAttributes } from 'services/customPages';
-
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import useCustomPage from 'hooks/useCustomPage';
-import { updateCustomPage } from 'services/customPages';
-import GenericHeroBannerForm, {
-  HeroBannerInputSettings,
-} from '../../../GenericHeroBannerForm';
+import { updateCustomPage, ICustomPageAttributes } from 'services/customPages';
+import GenericHeroBannerForm from '../../../GenericHeroBannerForm';
 import { isNilOrError } from 'utils/helperUtils';
 // change
 import messages from '../../../GenericHeroBannerForm/messages';
@@ -26,9 +22,19 @@ const EditCustomPageHeroBannerForm = ({
 }: InjectedIntlProps) => {
   const [isLoading, setIsLoading] = useState(false);
   const [formStatus, setFormStatus] = useState<ISubmitState>('disabled');
+  const [localSettings, setLocalSettings] =
+    useState<ICustomPageAttributes | null>(null);
 
   const { customPageId } = useParams() as { customPageId: string };
   const customPage = useCustomPage(customPageId);
+
+  useEffect(() => {
+    if (!isNilOrError(customPage)) {
+      setLocalSettings({
+        ...customPage.attributes,
+      });
+    }
+  }, [customPage]);
 
   if (isNilOrError(customPage)) {
     return null;
@@ -36,27 +42,10 @@ const EditCustomPageHeroBannerForm = ({
 
   const { attributes } = customPage;
 
-  const handleSave = async (newSettings) => {
-    if (!newSettings) return;
-
-    // necessary because the CTA module uses
-    const mappedSettings = {
-      ...newSettings,
-      ...(newSettings.banner_cta_signed_out_url && {
-        banner_cta_button_url: newSettings.banner_cta_signed_out_url,
-      }),
-      ...(newSettings.banner_cta_signed_out_type && {
-        banner_cta_button_type: newSettings.banner_cta_signed_out_type,
-      }),
-      ...(newSettings.banner_cta_signed_out_text_multiloc && {
-        banner_cta_button_multiloc:
-          newSettings.banner_cta_signed_out_text_multiloc,
-      }),
-    };
-
+  const handleSave = async () => {
     // only update the page settings if they have changed
     const diffedValues = {};
-    forOwn(mappedSettings, (value, key) => {
+    forOwn(localSettings, (value, key) => {
       if (!isEqual(value, customPage.attributes[key])) {
         diffedValues[key] = value;
       }
@@ -103,9 +92,8 @@ const EditCustomPageHeroBannerForm = ({
         { label: formatMessage(messages.heroBannerTitle) },
       ]}
       title={formatMessage(messages.heroBannerTitle)}
-      inputSettings={mappedInputSettings}
       setFormStatus={setFormStatus}
-      hideSignedInHeaderField
+      
     />
   );
 };
@@ -113,10 +101,10 @@ const EditCustomPageHeroBannerForm = ({
 export default injectIntl(EditCustomPageHeroBannerForm);
 
 export interface CustomPageHeroBannerInputSettings {
-  banner_layout: ICustomPagesAttributes['banner_layout'];
-  banner_overlay_opacity: ICustomPagesAttributes['banner_overlay_opacity'];
-  banner_overlay_color: ICustomPagesAttributes['banner_overlay_color'];
-  banner_header_multiloc: ICustomPagesAttributes['banner_header_multiloc'];
-  banner_subheader_multiloc: ICustomPagesAttributes['banner_header_multiloc'];
-  header_bg: ICustomPagesAttributes['header_bg'];
+  banner_layout: ICustomPageAttributes['banner_layout'];
+  banner_overlay_opacity: ICustomPageAttributes['banner_overlay_opacity'];
+  banner_overlay_color: ICustomPageAttributes['banner_overlay_color'];
+  banner_header_multiloc: ICustomPageAttributes['banner_header_multiloc'];
+  banner_subheader_multiloc: ICustomPageAttributes['banner_header_multiloc'];
+  header_bg: ICustomPageAttributes['header_bg'];
 }
