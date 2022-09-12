7fd5970b4ace9dad000ce2b977e829854a9e7a1e
jamesspeake <james@speake.org>
2022-09-12 11:51:56 +0200

    [CL-1598] Refactored all type dimension Factories into one file

--- back/engines/commercial/analytics/spec/factories/dimension_type.rb
+++ back/engines/commercial/analytics/spec/factories/dimension_type.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  factory :dimension_type_idea, class: 'Analytics::DimensionType' do
+    name { 'idea' }
+    parent { 'post' }
+  end
   factory :dimension_type_initiative, class: 'Analytics::DimensionType' do
     name { 'initiative' }
     parent { 'post' }
