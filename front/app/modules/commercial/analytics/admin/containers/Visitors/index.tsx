2a687546f6ee871cb3a816be5ab3d3902323cf4b
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-09-20 09:31:50 +0200

    [CL-1657] _

--- front/app/modules/commercial/analytics/admin/containers/Visitors/index.tsx
+++ front/app/modules/commercial/analytics/admin/containers/Visitors/index.tsx
@@ -1,14 +1,28 @@
 import React, { useState } from 'react';
+import moment, { Moment } from 'moment';
 
 // components
 import { Box } from '@citizenlab/cl2-component-library';
 import ChartFilters from 'containers/Admin/dashboard/overview/ChartFilters';
 
+// typings
+import { IResolution } from 'components/admin/ResolutionControl';
+
 const Visitors = () => {
+  const [startAtMoment, setStartAtMoment] = useState<Moment | null | undefined>();
+  const [endAtMoment, setEndAtMoment] = useState<Moment | null>(moment());
+  const [projectFilter, setProjectFilter] = useState<string | undefined>();
+  const [resolution, setResolution] = useState<IResolution>('month');
+
   return (
     <Box width="100%">
       <ChartFilters 
-
+        startAtMoment={startAtMoment}
+        endAtMoment={endAtMoment}
+        currentProjectFilter={projectFilter}
+        resolution={resolution}
+        onProjectFilter={setProjectFilter}
+        onChangeResolution={setResolution}
       />
     </Box>
   )
