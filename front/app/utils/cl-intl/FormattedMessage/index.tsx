cb485ef8e38c9baee9dda1651e699de93b696ab9
Amanda Anderson <amanda.anderson@citizenlab.co>
2022-09-21 09:52:02 +0200

    [CL-1301] Refactor FormatMessage

--- front/app/utils/cl-intl/FormattedMessage/index.tsx
+++ front/app/utils/cl-intl/FormattedMessage/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Subscription, combineLatest } from 'rxjs';
 // eslint-disable-next-line no-restricted-imports
 import { FormattedMessage as OriginalFormattedMessage } from 'react-intl';
@@ -9,13 +9,6 @@ import { isNilOrError } from 'utils/helperUtils';
 import styled from 'styled-components';
 import { isRtl } from 'utils/styleUtils';
 
-type State = {
-  tenantName: string | null;
-  orgType: string | null;
-  orgName: string | null;
-  loaded: boolean;
-};
-
 type Props = OriginalFormattedMessage.Props;
 
 const RtlBox = styled.span`
@@ -24,27 +17,19 @@ const RtlBox = styled.span`
   `}
 `;
 
-export default class FormattedMessage extends React.PureComponent<
-  Props,
-  State
-> {
-  subscriptions: Subscription[];
+const FormattedMessage = ({ values, ...props }: Props) => {
+  const [tenantName, setTenantName] = useState('');
+  const [orgType, setOrgType] = useState('');
+  const [orgName, setOrgName] = useState('');
+  const [loaded, setLoaded] = useState(false);
+  const [subscriptions, setSubscriptions] = useState<Subscription[]>([]);
 
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      tenantName: null,
-      orgType: null,
-      orgName: null,
-      loaded: false,
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
+    // ComponentDidMount
     const locale$ = localeStream().observable;
     const currentTenant$ = currentAppConfigurationStream().observable;
 
-    this.subscriptions = [
+    setSubscriptions([
       combineLatest([locale$, currentTenant$]).subscribe(([locale, tenant]) => {
         if (!isNilOrError(locale) && !isNilOrError(tenant)) {
           const tenantLocales = tenant.data.attributes.settings.core.locales;
@@ -56,40 +41,46 @@ export default class FormattedMessage extends React.PureComponent<
           );
           const orgType =
             tenant.data.attributes.settings.core.organization_type;
-          this.setState({ tenantName, orgName, orgType, loaded: true });
+          setTenantName(tenantName);
+          setOrgName(orgName);
+          setOrgType(orgType);
+          setLoaded(true);
         }
       }),
-    ];
-  }
+    ]);
 
-  componentWillUnmount() {
-    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
-  }
+    console.log('Variables: ', tenantName, orgName, loaded, orgType);
+    console.log('Subscriptions: ', subscriptions);
 
-  render() {
-    if (this.state.loaded) {
-      const { tenantName, orgType, orgName } = this.state;
-      const values = this.props.values || {};
+    return () => {
+      // ComponentWillUnmount
+      // subscriptions.forEach((subscription) => subscription.unsubscribe());
+    };
+  }, []);
 
-      if (tenantName) {
-        values.tenantName = tenantName;
-      }
-
-      if (orgType) {
-        values.orgType = orgType;
-      }
-
-      if (orgName) {
-        values.orgName = orgName;
-      }
-
-      return (
-        <RtlBox>
-          <OriginalFormattedMessage {...this.props} values={values} />
-        </RtlBox>
-      );
+  if (loaded && values !== undefined) {
+    if (tenantName) {
+      values.tenantName = tenantName;
     }
 
-    return null;
+    if (orgType) {
+      values.orgType = orgType;
+    }
+
+    if (orgName) {
+      values.orgName = orgName;
+    }
+
+    console.log('Props: ', props);
+    console.log('Values: ', values);
+    return (
+      <RtlBox>
+        <OriginalFormattedMessage {...props} values={values} />
+      </RtlBox>
+    );
   }
-}
+
+  return null;
+};
+
+export default FormattedMessage;
