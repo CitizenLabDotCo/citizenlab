f599b6e949e11697876c1733ef851ecc62600081
Koen De Hondt <koen@dehondt.dev>
2022-09-22 10:50:20 +0200

    [CL-1381] Ensure that idea slugs are generated when there is no active participation context

--- back/lib/participation_method/native_survey.rb
+++ back/lib/participation_method/native_survey.rb
@@ -4,11 +4,13 @@ module ParticipationMethod
   class NativeSurvey < Base
     # Survey responses do not have a fixed field that can be used
     # to generate a slug, so it has to be set after creation.
-    # Use the id as the slug.
+    # Use the id as the basis for the slug.
     def assign_slug!(input)
       return if input.slug # Slugs never change.
 
-      input.update_column :slug, input.id
+      new_slug = SlugService.new.generate_slug input, input.id
+      input.update_column :slug, new_slug
+    end
     end
   end
 end
