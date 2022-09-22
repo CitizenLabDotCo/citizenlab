c50873743c37dc635b6bc2a7d381290358b8d417
Amanda Anderson <amanda.anderson@citizenlab.co>
2022-09-22 11:56:52 +0200

    [CL-1237] Connect FE to new form submission count endpoint

---
+++ front/app/services/formSubmissionCount.ts
@@ -0,0 +1,34 @@
+import { API_PATH } from 'containers/App/constants';
+import { ProcessType } from 'services/projects';
+import streams, { IStreamParams } from 'utils/streams';
+
+export interface IFormSubmissionCountData {
+  totalSubmissions: string;
+}
+
+export interface FormSubmissionCountData {
+  totalSubmissions: string;
+}
+
+export interface IFormSubmissionCount {
+  data: IFormSubmissionCountData;
+}
+
+export function formSubmissionCountStream(
+  projectId: string,
+  projectType: ProcessType | undefined,
+  phaseId?: string | null,
+  streamParams: IStreamParams | null = null
+) {
+  if (projectType === 'timeline') {
+    return streams.get<IFormSubmissionCount>({
+      apiEndpoint: `${API_PATH}/phases/${phaseId}/submission_count`,
+      ...streamParams,
+    });
+  } else {
+    return streams.get<IFormSubmissionCount>({
+      apiEndpoint: `${API_PATH}/projects/${projectId}/submission_count`,
+      ...streamParams,
+    });
+  }
+}
