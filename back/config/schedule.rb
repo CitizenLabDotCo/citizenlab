fbc3942332be635bc02c61671e6f307be4da5abc
Adrien Dessy <adrien@citizenlab.co>
2022-09-23 11:40:36 +0200

    CL-1599 Define CRON job to import matomo visit data

---
+++ back/config/schedule.rb
@@ -0,0 +1,6 @@
+# Use this file to easily define all of your cron jobs.
+# Learn more: http://github.com/javan/whenever
+
+every 6.hours do
+  runner 'Analytics::ImportLatestMatomoDataJob.perform_for_all_tenants'
+end
