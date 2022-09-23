24901b5963f693eda69937765506df2bbc5a4c8f
jamesspeake <james@speake.org>
2022-09-23 12:57:35 +0100

    [CL-1598] Added initial model tests

---
+++ back/engines/commercial/analytics/spec/factories/dimension_channels.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :dimension_channel_website, class: 'Analytics::DimensionChannel' do
+    name_multiloc { { 'en': 'Website', 'fr-BE': 'Website', 'nl-BE': 'Website' } }
+  end
+end
