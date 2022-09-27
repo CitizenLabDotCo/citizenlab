ddd2f5e81cf97ea12119f6fb64613b4380c6afc0
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-09-27 17:59:42 +0100

    [CL-1657] _

--- front/app/modules/commercial/analytics/admin/hooks/useVisitorsData/parse/parseMonths.ts
+++ front/app/modules/commercial/analytics/admin/hooks/useVisitorsData/parse/parseMonths.ts
@@ -31,67 +31,54 @@ const parseMonths = (
 export default parseMonths;
 
 export const getEmptyMonthsBefore = (startAtMoment: Moment, firstDateInData: Moment) => {
-  if (startAtMoment.isSameOrAfter(firstDateInData)) return [];
+  const months = interpolateMonths(startAtMoment, firstDateInData.subtract({ month: 1 }))
+  if (months === null) return [];
 
-  const startMonth = startAtMoment.month() + 1;
-  const startYear = startAtMoment.year();
-  const firstMonthInData = firstDateInData.month() + 1;
-  const firstYearInData = firstDateInData.year();
-
-  if (startYear === firstYearInData) {
-    return createEmptyMonths(startMonth, firstMonthInData, startYear)
-  }
-
-  return range(startYear, firstYearInData + 1).reduce((acc, year) => {
-    if (year === startYear) {
-      return createEmptyMonths(startMonth, 13, year)
-    }
-
-    if (year === firstYearInData) {
-      return [
-        ...acc,
-        ...createEmptyMonths(1, firstMonthInData, year)
-      ]
-    }
-
-    return [...acc, ...createEmptyMonths(1, 13, year)]
-  }, [])
+  return months.map(createEmptyMonth)
 }
 
 export const getEmptyMonthsAfter = (endAtMoment: Moment, lastDateInData: Moment) => {
-  if (lastDateInData.isSameOrAfter(endAtMoment)) return [];
+  const months = interpolateMonths(lastDateInData.add({ month: 1 }), endAtMoment)
+  if (months === null) return [];
 
-  const endMonth = endAtMoment.month() + 1;
-  const endYear = endAtMoment.year();
-  const lastMonthInData = lastDateInData.month() + 1;
-  const lastYearInData = lastDateInData.year();
+  return months.map(createEmptyMonth)
+}
 
-  if (endYear === lastYearInData) {
-    return createEmptyMonths(lastMonthInData + 1, endMonth + 1, endYear)
-  }
+const interpolateMonths = (from: Moment, to: Moment) => {
+  if (from.isSameOrAfter(to)) return null;
 
-  return range(lastYearInData, endYear + 1).reduce((acc, year) => {
-    if (year === lastYearInData) {
-      return createEmptyMonths(lastMonthInData + 1, 13, year)
+  const fromMonth = from.month() + 1;
+  const fromYear = from.year();
+  const toMonth = to.month() + 1;
+  const toYear = to.year();
+
+  if (fromYear === toYear) return monthRange(fromMonth, toMonth, fromYear);
+
+  return range(fromYear, toYear + 1).reduce((acc, year) => {
+    if (year === fromYear) {
+      return monthRange(fromMonth, 13, year)
     }
 
-    if (year === endYear) {
+    if (year === toYear) {
       return [
         ...acc,
-        ...createEmptyMonths(1, endMonth + 1, year)
+        ...monthRange(1, toMonth + 1, year)
       ]
     }
 
-    return [...acc, ...createEmptyMonths(1, 13, year)];
+    return [...acc, ...monthRange(1, 13, year)];
   }, [])
 }
 
-const createEmptyMonths = (startMonth: number, endMonth: number, year: number) =>
-  range(startMonth, endMonth).map((month) => ({
-    visitors: 0,
-    visits: 0,
-    date: getFirstDayOfMonth(year, month)
-  }))
+const monthRange = (from: number, to: number, year: number) => {
+  return range(from, to).map((month) => moment(getFirstDayOfMonth(year, month)))
+}
+
+const createEmptyMonth = (moment: Moment) => ({
+  visitors: 0,
+  visits: 0,
+  date: moment.format('YYYY-MM-DD')
+})
 
 export const getFirstDayOfMonth = (year: number, month: number) => {
   return `${year}-${month < 10 ? `0${month}` : month}-01`
