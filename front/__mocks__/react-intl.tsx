db7b463105eafabe67d33ccc265a2e773c00bde0
Iva <iva@citizenlab.co>
2022-09-27 16:12:29 +0300

    [CL-1718] Remove commented code

--- front/__mocks__/react-intl.tsx
+++ front/__mocks__/react-intl.tsx
@@ -27,63 +27,6 @@ const intlReplacement = {
   formatMessage: formatMessageReplacement,
 };
 
-// const injectIntlReplacement = (Node) => {
-//   const renderWrapped = (props) => <Node {...props} intl={intl} />;
-//   renderWrapped.displayName = Node.displayName || Node.name || 'Component';
-//   return renderWrapped;
-// };
-
-// Intl.injectIntl = injectIntlReplacement;
-
-// // Override components by implementing the real components
-// //  providing them the context they need in order to function
-// const {
-//   IntlProvider,
-//   FormattedDate,
-//   FormattedTime,
-//   FormattedRelative,
-//   FormattedNumber,
-//   FormattedPlural,
-//   FormattedMessage,
-//   FormattedHTMLMessage,
-// } = Intl;
-
-// Intl.FormattedDate = (props) => (
-//   <IntlProvider {...defaultProps}>
-//     <FormattedDate {...props} />
-//   </IntlProvider>
-// );
-// Intl.FormattedTime = (props) => (
-//   <IntlProvider {...defaultProps}>
-//     <FormattedTime {...props} />
-//   </IntlProvider>
-// );
-// Intl.FormattedRelative = (props) => (
-//   <IntlProvider {...defaultProps}>
-//     <FormattedRelative {...props} />
-//   </IntlProvider>
-// );
-// Intl.FormattedNumber = (props) => (
-//   <IntlProvider {...defaultProps}>
-//     <FormattedNumber {...props} />
-//   </IntlProvider>
-// );
-// Intl.FormattedPlural = (props) => (
-//   <IntlProvider {...defaultProps}>
-//     <FormattedPlural {...props} />
-//   </IntlProvider>
-// );
-// Intl.FormattedMessage = (props) => (
-//   <IntlProvider {...defaultProps}>
-//     <FormattedMessage {...props} />
-//   </IntlProvider>
-// );
-// Intl.FormattedHTMLMessage = (props) => (
-//   <IntlProvider {...defaultProps}>
-//     <FormattedHTMLMessage {...props} />
-//   </IntlProvider>
-// );
-
 // Set displayName so that snapshots don't use "Uknown" as component name
 Intl.FormattedDate.displayName = 'FormattedDate';
 Intl.FormattedTime.displayName = 'FormattedTime';
