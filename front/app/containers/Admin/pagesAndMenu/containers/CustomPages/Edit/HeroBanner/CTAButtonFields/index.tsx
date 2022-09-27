8a5b43b544b3c5e8937f27429c674a9b8b64d48a
brentguf <brentguf@users.noreply.github.com>
2022-09-27 21:00:14 +0200

    Simplify CTAButtonFields (one level less deep)

--- front/app/containers/Admin/pagesAndMenu/containers/CustomPages/Edit/HeroBanner/CTAButtonFields/index.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/CustomPages/Edit/HeroBanner/CTAButtonFields/index.tsx
@@ -1,13 +1,13 @@
+import { Box, Radio } from '@citizenlab/cl2-component-library';
 import { SectionField, SubSectionTitle } from 'components/admin/Section';
 import React from 'react';
 import { TCustomPageCTAType } from 'services/customPages';
 import { Multiloc } from 'typings';
-import SettingRadioButtons from './SettingRadioButtons';
+import { FormattedMessage } from 'utils/cl-intl';
+import CustomizedButtonSettings from './CustomizedButtonSettings';
+import messages from './messages';
 
-const CTA_SIGNED_IN_TYPES: TCustomPageCTAType[] = [
-  'no_button',
-  'customized_button',
-];
+const CTA_TYPES: TCustomPageCTAType[] = ['no_button', 'customized_button'];
 
 interface Props {
   ctaType: TCustomPageCTAType;
@@ -31,18 +31,39 @@ const CTAButtonFields = ({
   return (
     <SectionField>
       <SubSectionTitle>{title}</SubSectionTitle>
-      <SettingRadioButtons
-        ctaTypes={CTA_SIGNED_IN_TYPES}
-        ctaType={ctaType}
-        signInStatus={'signed_in'}
-        ctaButtonMultiloc={ctaButtonMultiloc}
-        ctaButtonUrl={ctaButtonUrl}
-        handleCTAButtonTypeOnChange={handleCTAButtonTypeOnChange}
-        handleCTAButtonTextMultilocOnChange={
-          handleCTAButtonTextMultilocOnChange
-        }
-        handleCTAButtonUrlOnChange={handleCTAButtonUrlOnChange}
-      />
+      {CTA_TYPES.map((option: TCustomPageCTAType) => (
+        <div data-cy={`e2e-cta-settings-signed_in-${option}`} key={option}>
+          <Radio
+            key={`cta-type-${option}`}
+            onChange={handleCTAButtonTypeOnChange}
+            currentValue={ctaType}
+            value={option}
+            label={
+              <FormattedMessage
+                {...{
+                  customized_button: messages.customized_button,
+                  no_button: messages.no_button,
+                }[option]}
+              />
+            }
+            name={`cta_signed_in_type`}
+            id={`signed_in-${option}`}
+          />
+          {option === 'customized_button' && ctaType === 'customized_button' && (
+            <Box ml="28px">
+              <CustomizedButtonSettings
+                buttonMultiloc={ctaButtonMultiloc}
+                buttonUrl={ctaButtonUrl}
+                handleCTAButtonTextMultilocOnChange={
+                  handleCTAButtonTextMultilocOnChange
+                }
+                handleCTAButtonUrlOnChange={handleCTAButtonUrlOnChange}
+                key={`customized-button-settings-${option}`}
+              />
+            </Box>
+          )}
+        </div>
+      ))}
     </SectionField>
   );
 };
