5efa401dc63312b18656d2424e171df822576fbc
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2022-09-27 21:24:32 +0200

    CL-1553 linter fixes

--- front/app/modules/commercial/impact_tracking/index.ts
+++ front/app/modules/commercial/impact_tracking/index.ts
@@ -4,18 +4,13 @@ import request from 'utils/request';
 import { API_PATH } from 'containers/App/constants';
 
 const trackSessionStarted = () => {
-  request(
-    `${API_PATH}/sessions`,
-    {},
-    { method: 'POST' },
-    null
-  );
+  request(`${API_PATH}/sessions`, {}, { method: 'POST' }, null);
 };
 
 const configuration: ModuleConfiguration = {
   beforeMountApplication: () => {
     trackSessionStarted();
-  }
+  },
 };
 
 export default configuration;
--- front/cypress/e2e/impact_tracking.cy.ts
+++ front/cypress/e2e/impact_tracking.cy.ts
@@ -1,20 +1,18 @@
-
 describe('Impact tracking: Session tracking', () => {
-
   it('Does a POST request to /sessions as a normal user', () => {
-    cy.intercept('POST', '**/web_api/v1/sessions').as('createSession')
+    cy.intercept('POST', '**/web_api/v1/sessions').as('createSession');
     cy.goToLandingPage();
     cy.wait('@createSession').then((interception) => {
-      expect(interception.response?.statusCode).to.eq(201)
-    })
+      expect(interception.response?.statusCode).to.eq(201);
+    });
   });
 
   it('Does a POST request to /sessions as an admin', () => {
     cy.setAdminLoginCookie();
-    cy.intercept('POST', '**/web_api/v1/sessions').as('createSession')
+    cy.intercept('POST', '**/web_api/v1/sessions').as('createSession');
     cy.goToLandingPage();
     cy.wait('@createSession').then((interception) => {
-      expect(interception.response?.statusCode).to.eq(201)
-    })
-  })
+      expect(interception.response?.statusCode).to.eq(201);
+    });
+  });
 });
