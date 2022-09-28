257ebadea6d26461af9a0ba6fde3de9927fa48b1
brentguf <brentguf@users.noreply.github.com>
2022-09-28 11:49:12 +0200

    Remove SectionField

--- front/app/containers/Admin/pagesAndMenu/containers/CustomPages/Edit/HeroBanner/CTAButtonFields/index.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/CustomPages/Edit/HeroBanner/CTAButtonFields/index.tsx
@@ -57,53 +57,49 @@ const CTAButtonFields = ({
             />
             {option === 'customized_button' && ctaType === 'customized_button' && (
               <Box ml="28px">
-                <SectionField>
-                  <Box mb="20px">
-                    <InputMultilocWithLocaleSwitcher
-                      data-testid="inputMultilocLocaleSwitcher"
-                      type="text"
-                      valueMultiloc={ctaButtonMultiloc}
-                      label={
-                        <FormattedMessage
-                          {...messages.customized_button_text_label}
-                        />
-                      }
-                      onChange={handleCTAButtonTextMultilocOnChange}
-                      // we need it null and not {} to make an error disappear after entering a valid value
-                    />
-                    {hasCTAMultilocError && (
-                      <Error
-                        text={
-                          <FormattedMessage
-                            {...messages.customPageCtaButtonTextError}
-                          />
-                        }
-                      />
-                    )}
-                  </Box>
-                  <Label htmlFor="buttonConfigInput">
-                    <FormattedMessage
-                      {...messages.customized_button_url_label}
-                    />
-                  </Label>
-                  <Input
-                    id="buttonConfigInput"
-                    data-testid="buttonConfigInput"
+                <Box mb="20px">
+                  <InputMultilocWithLocaleSwitcher
+                    data-testid="inputMultilocLocaleSwitcher"
                     type="text"
-                    placeholder="https://..."
-                    onChange={handleCTAButtonUrlOnChange}
-                    value={ctaButtonUrl}
+                    valueMultiloc={ctaButtonMultiloc}
+                    label={
+                      <FormattedMessage
+                        {...messages.customized_button_text_label}
+                      />
+                    }
+                    onChange={handleCTAButtonTextMultilocOnChange}
+                    // we need it null and not {} to make an error disappear after entering a valid value
                   />
-                  {hasCTAUrlError && (
+                  {hasCTAMultilocError && (
                     <Error
                       text={
                         <FormattedMessage
-                          {...messages.customPageCtaButtonUrlError}
+                          {...messages.customPageCtaButtonTextError}
                         />
                       }
                     />
                   )}
-                </SectionField>
+                </Box>
+                <Label htmlFor="buttonConfigInput">
+                  <FormattedMessage {...messages.customized_button_url_label} />
+                </Label>
+                <Input
+                  id="buttonConfigInput"
+                  data-testid="buttonConfigInput"
+                  type="text"
+                  placeholder="https://..."
+                  onChange={handleCTAButtonUrlOnChange}
+                  value={ctaButtonUrl}
+                />
+                {hasCTAUrlError && (
+                  <Error
+                    text={
+                      <FormattedMessage
+                        {...messages.customPageCtaButtonUrlError}
+                      />
+                    }
+                  />
+                )}
               </Box>
             )}
           </div>
