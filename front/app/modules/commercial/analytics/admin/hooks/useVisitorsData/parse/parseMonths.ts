c751ccde7c3ec2211b273ce009939d6c649fc1c7
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-09-28 09:28:35 +0100

    [CL-1657] Fix unit tests

--- front/app/modules/commercial/analytics/admin/hooks/useVisitorsData/parse/parseMonths.ts
+++ front/app/modules/commercial/analytics/admin/hooks/useVisitorsData/parse/parseMonths.ts
@@ -6,43 +6,51 @@ import { TimeSeriesResponse } from '../typings';
 const parseMonths = (
   responseTimeSeries: TimeSeriesResponse,
   startAtMoment: Moment | null | undefined,
-  endAtMoment: Moment | null | undefined,
+  endAtMoment: Moment | null | undefined
 ): any => {
   const firstDateInData = getFirstDateInData(responseTimeSeries);
   const lastDateInData = getLastDateInData(responseTimeSeries);
 
   const emptyMonthsBefore = startAtMoment
     ? getEmptyMonthsBefore(startAtMoment, firstDateInData)
-    : []
+    : [];
 
   const emptyMonthsAfter = endAtMoment
     ? getEmptyMonthsAfter(endAtMoment, lastDateInData)
-    : []
+    : [];
 
   const parsedTimeSeries = parseTimeSeries(responseTimeSeries);
 
-  return [
-    ...emptyMonthsBefore,
-    ...parsedTimeSeries,
-    ...emptyMonthsAfter
-  ]
-}
+  return [...emptyMonthsBefore, ...parsedTimeSeries, ...emptyMonthsAfter];
+};
 
 export default parseMonths;
 
-export const getEmptyMonthsBefore = (startAtMoment: Moment, firstDateInData: Moment) => {
-  const months = interpolateMonths(startAtMoment, firstDateInData.subtract({ month: 1 }))
+export const getEmptyMonthsBefore = (
+  startAtMoment: Moment,
+  firstDateInData: Moment
+) => {
+  const months = interpolateMonths(
+    startAtMoment,
+    firstDateInData.subtract({ month: 1 })
+  );
   if (months === null) return [];
 
-  return months.map(createEmptyMonth)
-}
+  return months.map(createEmptyMonth);
+};
 
-export const getEmptyMonthsAfter = (endAtMoment: Moment, lastDateInData: Moment) => {
-  const months = interpolateMonths(lastDateInData.add({ month: 1 }), endAtMoment)
+export const getEmptyMonthsAfter = (
+  endAtMoment: Moment,
+  lastDateInData: Moment
+) => {
+  const months = interpolateMonths(
+    lastDateInData.add({ month: 1 }),
+    endAtMoment
+  );
   if (months === null) return [];
 
-  return months.map(createEmptyMonth)
-}
+  return months.map(createEmptyMonth);
+};
 
 const interpolateMonths = (from: Moment, to: Moment) => {
   if (from.isSameOrAfter(to)) return null;
@@ -52,53 +60,52 @@ const interpolateMonths = (from: Moment, to: Moment) => {
   const toMonth = to.month() + 1;
   const toYear = to.year();
 
-  if (fromYear === toYear) return monthRange(fromMonth, toMonth, fromYear);
+  if (fromYear === toYear) return monthRange(fromMonth, toMonth + 1, fromYear);
 
   return range(fromYear, toYear + 1).reduce((acc, year) => {
     if (year === fromYear) {
-      return monthRange(fromMonth, 13, year)
+      return monthRange(fromMonth, 13, year);
     }
 
     if (year === toYear) {
-      return [
-        ...acc,
-        ...monthRange(1, toMonth + 1, year)
-      ]
+      return [...acc, ...monthRange(1, toMonth + 1, year)];
     }
 
     return [...acc, ...monthRange(1, 13, year)];
-  }, [])
-}
+  }, []);
+};
 
 const monthRange = (from: number, to: number, year: number) => {
-  return range(from, to).map((month) => moment(getFirstDayOfMonth(year, month)))
-}
+  return range(from, to).map((month) =>
+    moment(getFirstDayOfMonth(year, month))
+  );
+};
 
 const createEmptyMonth = (moment: Moment) => ({
   visitors: 0,
   visits: 0,
-  date: moment.format('YYYY-MM-DD')
-})
+  date: moment.format('YYYY-MM-DD'),
+});
 
 export const getFirstDayOfMonth = (year: number, month: number) => {
-  return `${year}-${month < 10 ? `0${month}` : month}-01`
-}
+  return `${year}-${month < 10 ? `0${month}` : month}-01`;
+};
 
 const parseTimeSeries = (responseTimeSeries: TimeSeriesResponse) => {
   const timeSeriesWithMoments = responseTimeSeries.map((row) => ({
     moment: moment(getDate(row)),
     visitors: row.count_visitor_id,
-    visits: row.count
-  }))
+    visits: row.count,
+  }));
 
-  const sortedTimeSeries = timeSeriesWithMoments.sort(momentAscending)
+  const sortedTimeSeries = timeSeriesWithMoments.sort(momentAscending);
 
   return sortedTimeSeries.map(({ moment, visitors, visits }) => ({
     date: moment.format('YYYY-MM-DD'),
     visits,
-    visitors
+    visitors,
   }));
-}
+};
 
 interface TimeSeriesWithMomentsRow {
   moment: Moment;
@@ -109,4 +116,4 @@ interface TimeSeriesWithMomentsRow {
 const momentAscending = (
   a: TimeSeriesWithMomentsRow,
   b: TimeSeriesWithMomentsRow
-) => a.moment.isBefore(b.moment) ? -1 : 1
+) => (a.moment.isBefore(b.moment) ? -1 : 1);
