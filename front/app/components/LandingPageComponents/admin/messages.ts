03104d97dc1c56a5aa42c79111745e4e42605ed4
brentguf <brentguf@users.noreply.github.com>
2022-09-29 10:36:43 +0200

    Remove duplicate unused message

--- front/app/components/LandingPageComponents/admin/messages.ts
+++ front/app/components/LandingPageComponents/admin/messages.ts
@@ -13,10 +13,6 @@ export default defineMessages({
     id: 'app.containers.AdminPage.SettingsPage.sign_up_button',
     defaultMessage: 'Sign up',
   },
-  signed_in: {
-    id: 'app.containers.AdminPage.SettingsPage.signed_in',
-    defaultMessage: 'Button for registered visitors',
-  },
   customized_button_text_label: {
     id: 'app.containers.AdminPage.SettingsPage.customized_button_text_label',
     defaultMessage: 'Button text',
