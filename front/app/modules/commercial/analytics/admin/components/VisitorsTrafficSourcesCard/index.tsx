a54493bf9b13fed1999d50d0ef017911b4c10eae
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-09-30 17:17:23 +0100

    [CL-1660] Scaffolding traffic sources card

---
+++ front/app/modules/commercial/analytics/admin/components/VisitorsTrafficSourcesCard/index.tsx
@@ -0,0 +1,85 @@
+import React, { useRef } from 'react';
+
+// hooks
+import useVisitorsTrafficSourcesData from '../../hooks/useVisitorsTrafficSourcesData';
+
+// components
+import GraphCard from 'components/admin/GraphCard';
+import PieChart from 'components/admin/Graphs/PieChart';
+import { Box } from '@citizenlab/cl2-component-library';
+
+// i18n
+import messages from './messages';
+import { injectIntl } from 'utils/cl-intl';
+import { InjectedIntlProps } from 'react-intl';
+
+// typings
+import { IResolution } from 'components/admin/ResolutionControl';
+import { Moment } from 'moment';
+import { isNilOrError } from 'utils/helperUtils';
+import { LegendItem } from 'components/admin/Graphs/_components/Legend/typings';
+
+interface Props {
+  startAtMoment: Moment | null | undefined;
+  endAtMoment: Moment | null;
+  projectFilter: string | undefined;
+  resolution: IResolution;
+}
+
+const VisitorsCard = ({
+  startAtMoment,
+  endAtMoment,
+  projectFilter,
+  resolution,
+  intl: { formatMessage },
+}: Props & InjectedIntlProps) => {
+  const graphRef = useRef();
+  const { pieData, xlsxData } = useVisitorsTrafficSourcesData();
+
+  const legend = pieData.map(
+    (row): LegendItem => ({
+      icon: 'circle',
+      color: row.color,
+      label: row.name,
+    })
+  );
+
+  const cardTitle = formatMessage(messages.visitorsTrafficSources);
+  const startAt = startAtMoment?.toISOString();
+  const endAt = endAtMoment?.toISOString();
+
+  return (
+    <GraphCard
+      title={cardTitle}
+      exportMenu={{
+        name: cardTitle,
+        svgNode: graphRef,
+        xlsxData: isNilOrError(xlsxData) ? undefined : xlsxData,
+        startAt,
+        endAt,
+        currentProjectFilter: projectFilter,
+        resolution,
+      }}
+    >
+      <Box width="100%" maxWidth="300px" height="initial">
+        <PieChart
+          height={140}
+          data={pieData}
+          mapping={{
+            angle: 'value',
+            name: 'name',
+          }}
+          legend={{
+            items: legend,
+            marginTop: 15,
+            maintainGraphSize: true,
+            position: 'right-center',
+          }}
+          innerRef={graphRef}
+        />
+      </Box>
+    </GraphCard>
+  );
+};
+
+export default injectIntl(VisitorsCard);
---
+++ front/app/modules/commercial/analytics/admin/components/VisitorsTrafficSourcesCard/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  visitorsTrafficSources: {
+    id: 'app.modules.commercial.analytics.admin.components.VisitorsTrafficSourcesCard.visitorsTrafficSources',
+    defaultMessage: 'Traffic sources',
+  },
+});
