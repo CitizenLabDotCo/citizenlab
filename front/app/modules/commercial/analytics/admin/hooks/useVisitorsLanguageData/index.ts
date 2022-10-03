9957fc09a8453e334ffacc33ecb9e54ba192379a
Fran <francisco.pensa.web@gmail.com>
2022-10-03 11:59:41 -0300

    [CL-1328] add missing useEffect deps

--- front/app/modules/commercial/analytics/admin/hooks/useVisitorsLanguageData/index.ts
+++ front/app/modules/commercial/analytics/admin/hooks/useVisitorsLanguageData/index.ts
@@ -79,7 +79,7 @@ export default function useVisitorsData(
     );
 
     return () => subscription.unsubscribe();
-  }, []);
+  }, [projectId, startAtMoment, endAtMoment, formatMessage]);
 
   return { pieData, xlsxData };
 }
