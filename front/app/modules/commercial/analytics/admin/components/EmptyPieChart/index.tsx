c5f30fd4483bee91ed826d3b8e3b69ffece82ad8
Fran <francisco.pensa.web@gmail.com>
2022-10-03 15:54:50 -0300

    [CL-1328] fix empty state

--- front/app/modules/commercial/analytics/admin/components/EmptyPieChart/index.tsx
+++ front/app/modules/commercial/analytics/admin/components/EmptyPieChart/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 // components
 import { Box, Image } from '@citizenlab/cl2-component-library';
+import { NoDataContainer } from 'components/admin/GraphWrappers';
 
 import EmptyPieChartImageSrc from './empty.svg';
 import { colors } from 'utils/styleUtils';
@@ -10,11 +11,17 @@ import { colors } from 'utils/styleUtils';
 import messages from 'components/admin/Graphs/messages';
 import { FormattedMessage } from 'utils/cl-intl';
 
-const EmptyPieChart = (
-  <Box color={colors.coolGrey500}>
-    <Image src={EmptyPieChartImageSrc} alt="Pie chart placeholder" marginRight="48px" />
-    <FormattedMessage {...messages.noDataShort} />
-  </Box>
+const EmptyPieChart = () => (
+  <NoDataContainer>
+    <Box color={colors.coolGrey500}>
+      <Image
+        src={EmptyPieChartImageSrc}
+        alt="Pie chart placeholder"
+        marginRight="48px"
+      />
+      <FormattedMessage {...messages.noDataShort} />
+    </Box>
+  </NoDataContainer>
 );
 
 export default EmptyPieChart;
--- front/app/modules/commercial/analytics/admin/components/VisitorsLanguageCard/index.tsx
+++ front/app/modules/commercial/analytics/admin/components/VisitorsLanguageCard/index.tsx
@@ -7,7 +7,6 @@ import useVisitorsLanguageData from '../../hooks/useVisitorsLanguageData';
 import GraphCard from 'components/admin/GraphCard';
 import PieChart from 'components/admin/Graphs/PieChart';
 import { Box } from '@citizenlab/cl2-component-library';
-import EmptyState from 'components/admin/Graphs/_components/EmptyState';
 import EmptyPieChart from '../EmptyPieChart';
 
 // i18n
@@ -20,6 +19,9 @@ import { IResolution } from 'components/admin/ResolutionControl';
 import { Moment } from 'moment';
 import { isNilOrError } from 'utils/helperUtils';
 import { LegendItem } from 'components/admin/Graphs/_components/Legend/typings';
+
+// utils
+import { hasNoData } from 'components/admin/Graphs/utils';
 
 interface Props {
   startAtMoment: Moment | null | undefined;
