ac3232ed2faa6c6e32d4807f17d588db86e15eda
Simon Tharby <simontharby@gmail.com>
2022-10-05 15:32:18 +0100

    [CL-1777] Set files_section_enabled: true in static_pages seed

--- back/engines/ee/multi_tenancy/db/seeds/static_pages.rb
+++ back/engines/ee/multi_tenancy/db/seeds/static_pages.rb
@@ -10,7 +10,8 @@ module MultiTenancy
           StaticPage.create!({
             title_multiloc: runner.create_for_some_locales { Faker::Lorem.sentence },
             top_info_section_multiloc: runner.create_for_some_locales { Faker::Lorem.paragraphs.map { |p| "<p>#{p}</p>" }.join },
-            top_info_section_enabled: true
+            top_info_section_enabled: true,
+            files_section_enabled: true
           })
         end
       end
