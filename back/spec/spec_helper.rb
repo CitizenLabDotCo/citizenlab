532e8b010901f65fc5b8e411ed362444b1b52f56
Adrien Dessy <adrien@citizenlab.co>
2022-10-05 13:24:40 +0200

    CL-1599 Add missing import

--- back/spec/spec_helper.rb
+++ back/spec/spec_helper.rb
@@ -206,6 +206,7 @@ RSpec.configure do |config|
   config.filter_run_excluding template_test: true
 end
 
+require 'vcr'
 VCR.configure do |config|
   config.hook_into :webmock
   config.allow_http_connections_when_no_cassette = true
