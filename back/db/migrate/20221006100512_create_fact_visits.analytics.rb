9f56ccc2ae19e93c8b04aa593b992bddf3656804
jamesspeake <james@speake.org>
2022-10-06 15:28:13 +0100

    [CL-1598] Enforced dimensions not null and moved migrations to later date stamp

--- back/db/migrate/20221006100512_create_fact_visits.analytics.rb
+++ back/db/migrate/20221006100512_create_fact_visits.analytics.rb
@@ -25,9 +25,9 @@ class CreateFactVisits < ActiveRecord::Migration[6.1]
 
       # Dimension FKs
       t.belongs_to :dimension_user, null: true, type: :uuid, index: { name: 'i_v_user' } # No FK as users is a view
-      t.belongs_to :dimension_referrer_type, type: :uuid, foreign_key: { to_table: 'analytics_dimension_referrer_types' }, index: { name: 'i_v_referrer_type' }
-      t.belongs_to :dimension_date_first_action, type: :date, foreign_key: { to_table: 'analytics_dimension_dates', primary_key: 'date' }, index: { name: 'i_v_first_action' }
-      t.belongs_to :dimension_date_last_action, type: :date, foreign_key: { to_table: 'analytics_dimension_dates', primary_key: 'date' }, index: { name: 'i_v_last_action' }
+      t.belongs_to :dimension_referrer_type, null: false, type: :uuid, foreign_key: { to_table: 'analytics_dimension_referrer_types' }, index: { name: 'i_v_referrer_type' }
+      t.belongs_to :dimension_date_first_action, null: false, type: :date, foreign_key: { to_table: 'analytics_dimension_dates', primary_key: 'date' }, index: { name: 'i_v_first_action' }
+      t.belongs_to :dimension_date_last_action, null: false, type: :date, foreign_key: { to_table: 'analytics_dimension_dates', primary_key: 'date' }, index: { name: 'i_v_last_action' }
 
       # Fact data
       t.integer :duration, null: false
