fcc72fa0c3b627ea81f01b028726c2c25b0ded8e
jamesspeake <james@speake.org>
2022-10-06 13:26:09 +0100

    [CL-1598] Refactor of populate dimensions after review

--- back/engines/commercial/analytics/app/services/analytics/populate_dimensions_service.rb
+++ back/engines/commercial/analytics/app/services/analytics/populate_dimensions_service.rb
@@ -13,42 +13,42 @@ module Analytics
     private
 
       def populate_dates
-        dates_count = Analytics::DimensionDate.count
         from = Time.zone.today
         to = from + 6.months
 
-        if dates_count == 0
+        if Analytics::DimensionDate.none?
           first_idea = Idea.order(:created_at).limit(1).pluck(:created_at)
           first_initiative = Initiative.order(:created_at).limit(1).pluck(:created_at)
           first_post = [first_idea, first_initiative].min[0]
           unless first_post.nil?
-            from = first_post.to_datetime
+            from = first_post.year > 2020 ? first_post.to_date : Date.parse('2020-01-01')
           end
         else
-          last_date = Analytics::DimensionDate.order(date: :desc).limit(1).pluck(:date)
-          from = last_date[0].to_datetime + 1.day
+          last_date = Analytics::DimensionDate.order(date: :desc).first.date
+          from = last_date + 1.day
         end
 
         (from..to).each do |date|
-          new_date = Analytics::DimensionDate.new
-          new_date.date = date
-          new_date.week = date.beginning_of_week.to_date
-          new_date.month = "#{date.year}-#{date.strftime('%m')}"
-          new_date.year = date.year
-          new_date.save
+          Analytics::DimensionDate.create!(
+            date: date,
+            week: date.beginning_of_week.to_date,
+            month: "#{date.year}-#{date.strftime('%m')}",
+            year: date.year
+          )
         end
       end
 
       def populate_types
-        post_types = 'idea%post|initiative%post|comment%nil|vote%nil'
-        types = post_types.split('|').map { |type| type.split '%' }
-        types.each do |name, parent|
-          next if Analytics::DimensionType.exists?(name: name)
+        types = [
+          { name: 'idea', parent: 'post' },
+          { name: 'initiative', parent: 'post' },
+          { name: 'comment', parent: nil },
+          { name: 'vote', parent: nil }
+        ]
+        types.each do |type|
+          next if Analytics::DimensionType.exists?(name: type[:name])
 
-          new_type = Analytics::DimensionType.new
-          new_type.name = name
-          new_type.parent = parent == 'nil' ? nil : parent
-          new_type.save
+          Analytics::DimensionType.create!(name: type[:name], parent: type[:parent])
         end
       end
 
@@ -62,12 +62,17 @@ module Analytics
       end
 
       def populate_referrer_types
-        referrer_types = 'website%Websites|social%Social Networks|search%Search Engines|campaigns%Campaigns|direct%Direct Entry'
-        types = referrer_types.split('|').map { |type| type.split '%' }
-        types.each do |key, name|
-          next if Analytics::DimensionReferrerType.exists?(key: key)
+        types = [
+          { key: 'website', name: 'Websites' },
+          { key: 'social', name: 'Social Networks' },
+          { key: 'search', name: 'Search Engines' },
+          { key: 'campaigns', name: 'Campaigns' },
+          { key: 'direct', name: 'Direct Entry' }
+        ]
+        types.each do |type|
+          next if Analytics::DimensionReferrerType.exists?(key: type[:key])
 
-          Analytics::DimensionReferrerType.create!(key: key, name: name)
+          Analytics::DimensionReferrerType.create!(key: type[:key], name: type[:name])
         end
       end
   end
