66cc082701222dffee5bbdb1a67461d5d98d9657
Alexander <alexander@citizenlab.co>
2022-10-11 18:27:38 +0200

    [CL-1768] Remove custom sparse toggles to enable some navbar items

--- front/app/modules/free/customizable_navbar/index.tsx
+++ front/app/modules/free/customizable_navbar/index.tsx
@@ -1,7 +1,5 @@
 import React, { lazy } from 'react';
 import { ModuleConfiguration } from 'utils/moduleUtils';
-import ModuleActive from './admin/components/ModuleActive';
-import PoliciesSubtitle from './admin/components/PoliciesSubtitle';
 import PagesMenu from './admin/containers';
 import NavbarTitleField from './admin/components/NavbarTitleField';
 
@@ -30,25 +28,10 @@ const configuration: ModuleConfiguration = {
     ],
   },
   outlets: {
-    'app.containers.Admin.settings.customize.AllInput': (props) => (
-      <ModuleActive {...props} />
-    ),
-    'app.containers.Admin.initiatives.settings.EnableSwitch': (props) => (
-      <ModuleActive {...props} />
-    ),
-    'app.containers.Admin.settings.policies.start': (props) => (
-      <ModuleActive {...props} />
-    ),
-    'app.containers.Admin.settings.policies.subTitle': () => (
-      <PoliciesSubtitle />
-    ),
     'app.containers.Admin.pages-menu.index': () => <PagesMenu />,
     'app.containers.Admin.pages-menu.NavigationSettings': () => (
       <NavigationSettings />
     ),
-    'app.containers.Admin.pages-menu.containers.EditPageForm.index.onMount': (
-      props
-    ) => <ModuleActive {...props} />,
     'app.components.PageForm.index.top': (props) => (
       <NavbarTitleField {...props} />
     ),
