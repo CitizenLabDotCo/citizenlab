8c93a2c613051c080b369b0cc974b64f779130c9
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-10-11 18:01:43 +0100

    [CL-1825] _

--- front/app/components/admin/Table/Row.tsx
+++ front/app/components/admin/Table/Row.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 interface Props {
   children: React.ReactNode;
   className?: string;
+  ref?: React.RefObject<any>;
 }
 
-const Row = ({ children, className }: Props) => (
-  <tr className={className}>{children}</tr>
+const Row = ({ children, className, ref }: Props) => (
+  <tr className={className} ref={ref}>
+    {children}
+  </tr>
 );
 
 export default Row;
