2fd68d8109af6cf80dbf117948ac03177effa292
brentguf <brentguf@users.noreply.github.com>
2022-10-13 09:59:15 +0200

    Remove styled component so type checking on Button works

--- front/app/modules/commercial/verification/citizen/components/VerificationMethodButton.tsx
+++ front/app/modules/commercial/verification/citizen/components/VerificationMethodButton.tsx
@@ -1,12 +1,5 @@
-import { Button, ButtonProps } from '@citizenlab/cl2-component-library';
-import useLocale from 'hooks/useLocale';
+import { Box, Button, ButtonProps } from '@citizenlab/cl2-component-library';
 import React, { ReactNode } from 'react';
-import styled from 'styled-components';
-import { isNilOrError } from 'utils/helperUtils';
-
-const MethodButton = styled(Button)<{ last: boolean }>`
-  margin-bottom: ${({ last }) => (last ? '0px' : '15px')};
-`;
 
 interface Props extends Partial<ButtonProps> {
   children: ReactNode;
@@ -14,22 +7,20 @@ interface Props extends Partial<ButtonProps> {
 }
 
 const VerificationMethodButton = (props: Props) => {
-  const locale = useLocale();
-  if (isNilOrError(locale)) return null;
-
   return (
-    <MethodButton
-      icon="shield-check"
-      iconSize="22px"
-      buttonStyle="white"
-      fullWidth={true}
-      justify="left"
-      whiteSpace="wrap"
-      locale={locale}
-      {...props}
-    >
-      {props.children}
-    </MethodButton>
+    <Box mb={props.last ? '0px' : '15px'}>
+      <Button
+        icon="shield-check"
+        iconSize="22px"
+        buttonStyle="white"
+        fullWidth={true}
+        justify="left"
+        whiteSpace="wrap"
+        {...props}
+      >
+        {props.children}
+      </Button>
+    </Box>
   );
 };
 
