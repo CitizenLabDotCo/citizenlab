f70c9df673f4110c0da3fb81b7882472760736f6
Simon Tharby <simontharby@gmail.com>
2022-10-13 12:04:17 +0100

    [CL-1811] Add oostende_rrn to schema.json and config.ee.json

--- back/engines/commercial/id_oostende_rrn/spec/acceptance/oostende_rrn_verification_spec.rb
+++ back/engines/commercial/id_oostende_rrn/spec/acceptance/oostende_rrn_verification_spec.rb
@@ -22,7 +22,7 @@ resource 'Verifications' do
       enabled: true,
       verification_methods: [
         {
-          name: 'oostende_rnn',
+          name: 'oostende_rrn',
           api_key: 'fake_api_key',
           environment: 'qa',
           custom_field_key: @custom_field.key,
