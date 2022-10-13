ef2fad33a85d9efa4bb0fb9901544e7745017482
brentguf <brentguf@users.noreply.github.com>
2022-10-13 10:26:49 +0200

    Review fixes

--- front/app/modules/commercial/verification/citizen/components/VerificationMethodButton.tsx
+++ front/app/modules/commercial/verification/citizen/components/VerificationMethodButton.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, ButtonProps } from '@citizenlab/cl2-component-library';
+import { Button, ButtonProps } from '@citizenlab/cl2-component-library';
 import React, { ReactNode } from 'react';
 
 interface Props extends Partial<ButtonProps> {
@@ -8,19 +8,17 @@ interface Props extends Partial<ButtonProps> {
 
 const VerificationMethodButton = (props: Props) => {
   return (
-    <Box mb={props.last ? '0px' : '15px'}>
-      <Button
-        icon="shield-check"
-        iconSize="22px"
-        buttonStyle="white"
-        fullWidth={true}
-        justify="left"
-        whiteSpace="wrap"
-        {...props}
-      >
-        {props.children}
-      </Button>
-    </Box>
+    <Button
+      icon="shield-check"
+      buttonStyle="white"
+      fullWidth={true}
+      justify="left"
+      whiteSpace="wrap"
+      mb={props.last ? '0px' : '15px'}
+      {...props}
+    >
+      {props.children}
+    </Button>
   );
 };
 
