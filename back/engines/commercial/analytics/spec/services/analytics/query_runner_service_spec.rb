fd3b2d0e8b5b739115c196b42a4e188c1651b7a9
jamesspeake <james@speake.org>
2022-10-14 16:55:26 +0100

    [CL-1834] Added some service tests to cover paging

--- back/engines/commercial/analytics/spec/services/analytics/query_runner_service_spec.rb
+++ back/engines/commercial/analytics/spec/services/analytics/query_runner_service_spec.rb
@@ -74,8 +74,7 @@ describe Analytics::QueryRunnerService do
       query_param = ActionController::Parameters.new(
         fact: 'post',
         fields: 'id',
-        sort: { id: 'ASC' },
-        limit: 3
+        sort: { id: 'ASC' }
       )
       query = Analytics::Query.new(query_param)
 
@@ -83,8 +82,56 @@ describe Analytics::QueryRunnerService do
       results = runner.run(query)
       posts = (ideas + initiatives)
         .sort_by { |p| p[:id] }
-        .map { |p| { 'id' => p.id } }[0, 3]
+        .map { |p| { 'id' => p.id } }
       expect(results).to eq(posts)
+    end
+
+    context 'result limiting and paging' do
+      before_all do
+        create_list(:idea, 10)
+      end
+
+      it 'returns the first page of 2 items from 10 posts' do
+        query_param = ActionController::Parameters.new(
+          fact: 'post',
+          fields: 'id',
+          sort: { id: 'ASC' },
+          page: { size: 2, number: 1 }
+        )
+        runner = described_class.new
+        results = runner.run(Analytics::Query.new(query_param))
+
+        expect(results.length).to eq(2)
+        expect(results.first['id']).to eq(Idea.order(:id).first.id)
+      end
+
+      it 'returns the second page of 3 items from 10 posts' do
+        query_param = ActionController::Parameters.new(
+          fact: 'post',
+          fields: 'id',
+          sort: { id: 'ASC' },
+          page: { size: 3, number: 2 }
+        )
+        runner = described_class.new
+        results = runner.run(Analytics::Query.new(query_param))
+
+        expect(results.length).to eq(3)
+        expect(results.first['id']).to eq(Idea.order(:id).fourth.id)
+        expect(results.second['id']).to eq(Idea.order(:id).fifth.id)
+      end
+
+      it 'returns nothing if the page number is too high for the size' do
+        query_param = ActionController::Parameters.new(
+          fact: 'post',
+          fields: 'id',
+          sort: { id: 'ASC' },
+          page: { size: 5, number: 3 }
+        )
+        runner = described_class.new
+        results = runner.run(Analytics::Query.new(query_param))
+
+        expect(results).to eq([])
+      end
     end
   end
 end
