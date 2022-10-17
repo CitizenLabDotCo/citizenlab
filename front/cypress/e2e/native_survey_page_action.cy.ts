16a9004d26c8e57c51d41486999728800001a43a
Amanda Anderson <amanda.anderson@citizenlab.co>
2022-10-17 14:57:13 +0200

    [CL-1708] Update test

--- front/cypress/e2e/native_survey_page_action.cy.ts
+++ front/cypress/e2e/native_survey_page_action.cy.ts
@@ -131,7 +131,9 @@ describe('Native survey project page actions', () => {
   it('tests actions when unregistered users may submit survey responses', () => {
     cy.setAdminLoginCookie();
     cy.visit(`admin/projects/${projectIdContinuous}/permissions`);
-    cy.contains('Everyone').first().click({ force: true });
+    cy.get('#e2e-granular-permissions').within(() => {
+      cy.contains('Everyone').click({ force: true });
+    });
     cy.logout();
     cy.visit(`/projects/${projectSlugContinous}`);
     cy.get('[data-testid="e2e-project-survey-button"]').click({ force: true });
