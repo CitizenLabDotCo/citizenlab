ea47211d1835967c436f96382e9543518702dd38
Simon Tharby <simontharby@gmail.com>
2022-10-18 20:12:59 +0100

    [CL-1811] Ensure tenant actually switched in fix for test

--- back/engines/ee/multi_tenancy/spec/services/side_fx_app_configuration_spec.rb
+++ back/engines/ee/multi_tenancy/spec/services/side_fx_app_configuration_spec.rb
@@ -3,21 +3,10 @@
 require 'rails_helper'
 
 describe 'SideFxAppConfigurationService' do
-  # Avoid problems with CI failures, where the host change applied in one of the tests here seems to
-  # persist and break subsequent tests (but not locally), by using a separate tenant here and then
-  # switching back to the default tenant.
-  before do
-    Apartment::Tenant.switch!(test_tenant.schema_name) if CitizenLab.ee? # Switch into a test tenant
-  end
-
-  after do
-    Apartment::Tenant.switch!(current_tenant.schema_name) if CitizenLab.ee? # Switch back to the default tenant
-  end
-
   let(:service) { SideFxAppConfigurationService.new }
   let(:current_user) { create(:user) }
-  let(:test_tenant) { create(:tenant) }
-  let(:current_tenant) { Tenant.current }
+  let!(:test_tenant) { create(:tenant) }
+  let!(:current_tenant) { Tenant.current }
   let(:config) { AppConfiguration.instance }
 
   it 'has an #before_update method' do
@@ -33,7 +22,7 @@ describe 'SideFxAppConfigurationService' do
 
       expect { service.after_update(config, current_user) }
         .to  have_enqueued_job(LogActivityJob).with(config, 'changed', current_user, config.updated_at.to_i, {})
-        .and have_enqueued_job(LogActivityJob).with(test_tenant, 'changed', current_user, config.updated_at.to_i, {}) # TODO_MT to be removed
+        .and have_enqueued_job(LogActivityJob).with(current_tenant, 'changed', current_user, config.updated_at.to_i, {}) # TODO_MT to be removed
     end
 
     it "logs a 'changed_lifecycle_stage' action job when the lifecycle has changed" do
@@ -47,19 +36,32 @@ describe 'SideFxAppConfigurationService' do
       expect { service.after_update(config, current_user) }
         .to  have_enqueued_job(LogActivityJob).with(config, 'changed_lifecycle_stage', current_user, updated_at,
           options)
-        .and have_enqueued_job(LogActivityJob).with(test_tenant, 'changed_lifecycle_stage', current_user, updated_at, options) # TODO_MT to be removed
+        .and have_enqueued_job(LogActivityJob).with(current_tenant, 'changed_lifecycle_stage', current_user, updated_at, options) # TODO_MT to be removed
     end
 
-    it "logs a 'changed_host' action job when the host has changed" do
-      old_host = config.host
-      new_host = 'seboslovakia.citizenlab.co'
-      config.update! host: new_host
+    context 'when tenant host is changed' do
+      # Avoid problems with CI failures, where the host change applied in one of the tests here seems to
+      # persist and break subsequent tests (on CI), by using a separate tenant here and then switching
+      # back to the default tenant.
+      before do
+        Apartment::Tenant.switch!(test_tenant.schema_name) if CitizenLab.ee? # Switch into a test tenant
+      end
 
-      options = { payload: { changes: [old_host, new_host] } }
-      updated_at = config.updated_at.to_i
-      expect { service.after_update(config, current_user) }
-        .to  have_enqueued_job(LogActivityJob).with(config, 'changed_host', current_user, updated_at, options)
-        .and have_enqueued_job(LogActivityJob).with(test_tenant, 'changed_host', current_user, updated_at, options)
+      after do
+        Apartment::Tenant.switch!(current_tenant.schema_name) if CitizenLab.ee? # Switch back to the default tenant
+      end
+
+      it "logs a 'changed_host' action job when the host has changed" do
+        old_host = config.host
+        new_host = 'seboslovakia.citizenlab.co'
+        config.update! host: new_host
+
+        options = { payload: { changes: [old_host, new_host] } }
+        updated_at = config.updated_at.to_i
+        expect { service.after_update(config, current_user) }
+          .to  have_enqueued_job(LogActivityJob).with(config, 'changed_host', current_user, updated_at, options)
+          .and have_enqueued_job(LogActivityJob).with(test_tenant, 'changed_host', current_user, updated_at, options)
+      end
     end
   end
 end
