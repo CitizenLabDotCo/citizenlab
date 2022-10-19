1df685b1f7138e0d6eadae8addebab7f1fbb6196
Sebastien Hoorens <sebastien@citizenlab.co>
2022-10-19 17:42:24 +0200

    allow survey responses without author, initial solution on policy

--- back/app/policies/idea_policy.rb
+++ back/app/policies/idea_policy.rb
@@ -34,8 +34,8 @@ class IdeaPolicy < ApplicationPolicy
 
   def create?
     return true if record.draft?
-    return false unless active?
-    return true if UserRoleService.new.can_moderate_project? record.project, user
+    return true if user && UserRoleService.new.can_moderate_project?(record.project, user)
+    return false if !active? && record.participation_method_on_creation.sign_in_required_for_posting?
 
     reason = ParticipationContextService.new.posting_idea_disabled_reason_for_project(record.project, user)
     raise_not_authorized(reason) if reason
@@ -77,6 +77,8 @@ class IdeaPolicy < ApplicationPolicy
   private
 
   def owner?
+    return true unless user
+
     record.author_id == user.id
   end
 end
--- back/lib/participation_method/base.rb
+++ back/lib/participation_method/base.rb
@@ -33,6 +33,10 @@ module ParticipationMethod
       false
     end
 
+    def sign_in_required_for_posting?
+      true
+    end
+
     def extra_fields_category_translation_key
       'custom_forms.categories.extra.title'
     end
