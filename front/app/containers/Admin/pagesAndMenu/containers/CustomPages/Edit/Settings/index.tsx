caf4dea34dc90c1cb20bea0aab3500e8c07e7826
lucas <lucasquinnmathis@gmail.com>
2022-10-25 11:32:01 +0200

    Changes to navbar item controller and FE editing

--- front/app/containers/Admin/pagesAndMenu/containers/CustomPages/Edit/Settings/index.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/CustomPages/Edit/Settings/index.tsx
@@ -5,26 +5,39 @@ import { useParams } from 'react-router-dom';
 import { isNilOrError } from 'utils/helperUtils';
 import { updateCustomPage } from 'services/customPages';
 import { FormValues } from 'containers/Admin/pagesAndMenu/containers/CustomPages/CustomPageSettingsForm';
+import streams from 'utils/streams';
+import { apiEndpoint as navbarItemsEndpoint } from 'services/navbar';
 
 const EditCustomPageSettings = () => {
   const { customPageId } = useParams() as { customPageId: string };
   const customPage = useCustomPage({ customPageId });
 
-  const handleOnSubmit = async (formValues: FormValues) => {
-    await updateCustomPage(customPageId, formValues);
-  };
-
   if (!isNilOrError(customPage)) {
+    const hasNavbarItem = !!customPage.relationships.nav_bar_item.data?.id;
+
+    const handleOnSubmit = async (formValues: FormValues) => {
+      await updateCustomPage(customPageId, formValues);
+      // navbar items are a separate stream, so manually refresh on title update
+      // to reflect changes in the user's navbar
+      if (hasNavbarItem) {
+        streams.fetchAllWith({
+          apiEndpoint: [navbarItemsEndpoint],
+        });
+      }
+    };
+
     return (
       <CustomPageSettingsForm
         mode="edit"
         defaultValues={{
           title_multiloc: customPage.attributes.title_multiloc,
-          nav_bar_item_title_multiloc:
-            customPage.attributes.nav_bar_item_title_multiloc,
+          ...(hasNavbarItem && {
+            nav_bar_item_title_multiloc:
+              customPage.attributes.nav_bar_item_title_multiloc,
+          }),
           slug: customPage.attributes.slug,
         }}
-        showNavBarItemTitle={!!customPage.relationships.nav_bar_item.data?.id}
+        showNavBarItemTitle={hasNavbarItem}
         onSubmit={handleOnSubmit}
       />
     );
