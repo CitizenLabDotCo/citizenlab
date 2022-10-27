f9a3905bc691007b7871879bf623808b3007f76b
brentguf <brentguf@users.noreply.github.com>
2022-10-27 09:15:42 +0200

    Remove dead code

--- front/app/containers/InitiativesShowPage/index.tsx
+++ front/app/containers/InitiativesShowPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React from 'react';
 import { isError } from 'lodash-es';
 import { isNilOrError } from 'utils/helperUtils';
 import { withRouter, WithRouterProps } from 'utils/cl-router/withRouter';
