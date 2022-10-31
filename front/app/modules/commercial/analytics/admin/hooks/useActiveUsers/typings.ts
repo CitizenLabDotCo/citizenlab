2572791b78180d225056dbdf6bb03ca1df03e7b2
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-10-31 14:59:55 +0000

    [CL-1659] Add repsonse typings

--- front/app/modules/commercial/analytics/admin/hooks/useActiveUsers/typings.ts
+++ front/app/modules/commercial/analytics/admin/hooks/useActiveUsers/typings.ts
@@ -4,5 +4,38 @@ export type QueryParameters = ProjectId & Dates & Resolution;
 
 // Response
 export interface Response {
-  data: any;
+  data: [
+    TimeSeriesResponse | [],
+    ActiveUsersRow,
+    ActiveUsersRow,
+    VisitorsRow,
+    VisitorsRow
+  ];
+}
+
+type TimeSeriesResponse = TimeSeriesResponseRow[];
+
+export type TimeSeriesResponseRow =
+  | TimeSeriesResponseMonth
+  | TimeSeriesResponseWeek
+  | TimeSeriesResponseDay;
+
+interface ActiveUsersRow {
+  count_dimension_user_id: number;
+}
+
+interface TimeSeriesResponseMonth extends ActiveUsersRow {
+  'dimension_date_created.month': string;
+}
+
+interface TimeSeriesResponseWeek extends ActiveUsersRow {
+  'dimension_date_created.week': string;
+}
+
+interface TimeSeriesResponseDay extends ActiveUsersRow {
+  'dimension_date_created.date': string;
+}
+
+interface VisitorsRow {
+  count_visitor_id: number;
 }
