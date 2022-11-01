0917c17293d9ddacdc8650e019fea16706264a6a
Amanda Anderson <amanda.anderson@citizenlab.co>
2022-11-01 14:59:09 +0100

    [CL-1841] Add e2e tet

--- front/cypress/e2e/content_builder/content_builder_toggle.cy.ts
+++ front/cypress/e2e/content_builder/content_builder_toggle.cy.ts
@@ -33,11 +33,29 @@ describe('Content builder toggle', () => {
   });
 
   after(() => {
-    cy.apiRemoveProject(projectId);
+    // cy.apiRemoveProject(projectId);
   });
 
   it('shows original description by default', () => {
+    //
+    // Attach a project file
+    cy.visit(`admin/projects/${projectId}`);
+    cy.scrollTo('bottom');
+    cy.acceptCookies();
+    cy.get('#e2e-project-file-uploader').selectFile(
+      'cypress/fixtures/example.pdf'
+    );
+    // Submit project
+    cy.get('.e2e-submit-wrapper-button').click();
+    cy.wait(2000);
+    cy.contains('Your form has been saved!').should('be.visible');
+
+    // Go to project page
+    cy.visit(`/projects/${projectSlug}`);
+    // Check that original project description is visible
     cy.contains('Original project description.').should('be.visible');
+    // Check that attachment is present
+    cy.contains('example.pdf').should('exist');
   });
 
   it('shows original description when content builder is enabled but there is no content yet', () => {
@@ -47,5 +65,31 @@ describe('Content builder toggle', () => {
       .click({ force: true });
     cy.visit(`/projects/${projectSlug}`);
     cy.contains('Original project description.').should('be.visible');
+  });
+
+  it('continue to show attachments after description added using content builder', () => {
+    cy.intercept('**/content_builder_layouts/project_description/upsert').as(
+      'saveContentBuilder'
+    );
+
+    cy.visit(`/admin/content-builder/projects/${projectId}/description`);
+
+    // Add content using content builder
+    cy.get('#e2e-draggable-text').dragAndDrop('#e2e-content-builder-frame', {
+      position: 'inside',
+    });
+
+    cy.get('#e2e-text-box').click();
+    cy.get('#quill-editor').click();
+    cy.get('#quill-editor').type('Edited text.', { force: true });
+
+    cy.get('#e2e-content-builder-topbar-save').click();
+    cy.wait('@saveContentBuilder');
+
+    cy.visit(`/projects/${projectSlug}`);
+    cy.contains('Edited text.').should('be.visible');
+
+    // Check that attachment is present
+    cy.contains('example.pdf').should('exist');
   });
 });
