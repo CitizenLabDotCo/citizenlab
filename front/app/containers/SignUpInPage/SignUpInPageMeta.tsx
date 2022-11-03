ab19bcb7651eb8e75f9fd837e25dfc1abb016921
brentguf <brentguf@users.noreply.github.com>
2022-11-03 16:15:41 +0100

    SignUpInPageMeta tech debt

--- front/app/containers/SignUpInPage/SignUpInPageMeta.tsx
+++ front/app/containers/SignUpInPage/SignUpInPageMeta.tsx
@@ -1,22 +1,14 @@
 // libraries
-import React, { memo } from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet';
-import { adopt } from 'react-adopt';
-import { withRouter, WithRouterProps } from 'utils/cl-router/withRouter';
-
-// i18n
+import { useLocation } from 'react-router-dom';
 import messages from './messages';
-import { injectIntl } from 'utils/cl-intl';
-import { WrappedComponentProps } from 'react-intl';
 
-// resources
-import GetAppConfigurationLocales, {
-  GetAppConfigurationLocalesChildProps,
-} from 'resources/GetAppConfigurationLocales';
-import GetAppConfiguration, {
-  GetAppConfigurationChildProps,
-} from 'resources/GetAppConfiguration';
-import GetLocale, { GetLocaleChildProps } from 'resources/GetLocale';
+// hooks
+import useLocale from 'hooks/useLocale';
+import useAppConfigurationLocales from 'hooks/useAppConfigurationLocales';
+import useAppConfiguration from 'hooks/useAppConfiguration';
+import { useIntl } from 'utils/cl-intl';
 
 // utils
 import { isNilOrError, endsWith } from 'utils/helperUtils';
@@ -24,68 +16,47 @@ import { getLocalized } from 'utils/i18n';
 import getAlternateLinks from 'utils/cl-router/getAlternateLinks';
 import getCanonicalLink from 'utils/cl-router/getCanonicalLink';
 
-interface DataProps {
-  tenantLocales: GetAppConfigurationLocalesChildProps;
-  tenant: GetAppConfigurationChildProps;
-  locale: GetLocaleChildProps;
-}
+const SignUpInPageMeta = () => {
+  const { pathname } = useLocation();
+  const { formatMessage } = useIntl();
+  const appConfig = useAppConfiguration();
+  const locales = useAppConfigurationLocales();
+  const locale = useLocale();
 
-interface Props extends DataProps {}
+  if (
+    !isNilOrError(locales) &&
+    !isNilOrError(locale) &&
+    !isNilOrError(appConfig)
+  ) {
+    const method = endsWith(pathname, 'sign-in') ? 'signin' : 'signup';
+    const organizationNameMultiLoc =
+      appConfig.attributes.settings.core.organization_name;
+    const tenantName = getLocalized(organizationNameMultiLoc, locale, locales);
+    const pageMetaTitle = formatMessage(
+      method === 'signin' ? messages.signInMetaTitle : messages.signUpMetaTitle,
+      { tenantName }
+    );
+    const pageMetaDescription = formatMessage(
+      method === 'signin'
+        ? messages.signInPageMetaDescription
+        : messages.signUpPageMetaDescription
+    );
 
-const SignUpInPageMeta = memo<Props & WrappedComponentProps & WithRouterProps>(
-  ({ intl, location: { pathname }, tenantLocales, tenant, locale }) => {
-    if (
-      !isNilOrError(tenantLocales) &&
-      !isNilOrError(locale) &&
-      !isNilOrError(tenant)
-    ) {
-      const { formatMessage } = intl;
-      const method = endsWith(pathname, 'sign-in') ? 'signin' : 'signup';
-      const organizationNameMultiLoc =
-        tenant.attributes.settings.core.organization_name;
-      const tenantName = getLocalized(
-        organizationNameMultiLoc,
-        locale,
-        tenantLocales
-      );
-      const pageMetaTitle = formatMessage(
-        method === 'signin'
-          ? messages.signInMetaTitle
-          : messages.signUpMetaTitle,
-        { tenantName }
-      );
-      const pageMetaDescription = formatMessage(
-        method === 'signin'
-          ? messages.signInPageMetaDescription
-          : messages.signUpPageMetaDescription
-      );
-
-      return (
-        <Helmet>
-          <title>{pageMetaTitle}</title>
-          {getCanonicalLink()}
-          {getAlternateLinks(tenantLocales)}
-          <meta name="title" content={pageMetaTitle} />
-          <meta name="description" content={pageMetaDescription} />
-          <meta property="og:title" content={pageMetaTitle} />
-          <meta property="og:description" content={pageMetaDescription} />
-          <meta property="og:url" content={window.location.href} />
-        </Helmet>
-      );
-    }
-
-    return null;
+    return (
+      <Helmet>
+        <title>{pageMetaTitle}</title>
+        {getCanonicalLink()}
+        {getAlternateLinks(locales)}
+        <meta name="title" content={pageMetaTitle} />
+        <meta name="description" content={pageMetaDescription} />
+        <meta property="og:title" content={pageMetaTitle} />
+        <meta property="og:description" content={pageMetaDescription} />
+        <meta property="og:url" content={window.location.href} />
+      </Helmet>
+    );
   }
-);
 
-const SignUpInPageMetaWithHoC = withRouter(injectIntl(SignUpInPageMeta));
+  return null;
+};
 
-const Data = adopt<DataProps>({
-  tenantLocales: <GetAppConfigurationLocales />,
-  tenant: <GetAppConfiguration />,
-  locale: <GetLocale />,
-});
-
-export default () => (
-  <Data>{(dataProps) => <SignUpInPageMetaWithHoC {...dataProps} />}</Data>
-);
+export default SignUpInPageMeta;
