0f3c797948fdd3f7a265bc88dde4f0351653084b
Adrien Dessy <adrien@citizenlab.co>
2022-11-04 18:19:08 +0100

    CL-1881 Rework the analytics_fact_project_statuses view
    Rework the view to make it easier for us down the road.
    
    The new view only lists the last status. The main reason for this change is that, in its current state, the analytics API does not allow us to query the last status of each project (it has a minimal 'first' aggregation method but data in each group cannot be sorted).
    
    Another small change is that *draft* timeline projects whose last phase is complete are no longer considered complete.
    
    We also dropped the 'created' and 'deleted' statuses. The 'created' was pretty much useless anyway (it could be inferred from other status changes). The 'deleted' status is kind of interesting to know, but including it removes deleted projects from the list of finished projects (since we only consider the last status). Since we don't show any information about deleted projects in UI, I decided to drop it for now.

--- back/engines/commercial/analytics/db/views/analytics_fact_project_statuses_v01.sql
+++ back/engines/commercial/analytics/db/views/analytics_fact_project_statuses_v01.sql
@@ -1,32 +1,28 @@
-WITH regular_project_statuses AS -- project statuses that do not need to be calculated
-         (SELECT item_id as project_id, action as status, acted_at as timestamp
+WITH last_project_statuses AS -- project statuses that do not need to be calculated
+         (SELECT DISTINCT ON (item_id) item_id as project_id, action as status, acted_at as timestamp
           FROM activities
           WHERE item_type = 'Project'
-            AND action IN ('created', 'draft', 'published', 'archived', 'deleted')),
-
-     last_statuses_for_continuous_projects AS
-         (SELECT DISTINCT ON (project_id) rps.*
-          FROM regular_project_statuses rps JOIN projects p ON rps.project_id = p.id
-          WHERE p.process_type = 'continuous'
-          ORDER BY project_id, timestamp DESC),
+            AND action IN ('draft', 'published', 'archived', 'deleted')
+          ORDER BY item_id, acted_at DESC),
 
      finished_statuses_for_continuous_projects AS
          (SELECT project_id, 'finished' as status, timestamp
-          FROM last_statuses_for_continuous_projects
-          where status = 'archived'),
+          FROM last_project_statuses lps JOIN projects p ON lps.project_id = p.id
+          where p.process_type = 'continuous' AND lps.status = 'archived'),
 
-     finished_status_for_timeline_projects AS
+     finished_statuses_for_timeline_projects AS
          -- The project is considered finished at the beginning of the day following the end
          -- date of the last phase.
-         (SELECT project_id, 'finished' as status, (MAX(end_at) + 1)::timestamp as timestamp
-          FROM phases
-          GROUP BY project_id
-          HAVING MAX(end_at) < NOW()),
+         (SELECT phases.project_id, 'finished' as status, (MAX(phases.end_at) + 1)::timestamp as timestamp
+          FROM phases JOIN projects ON phases.project_id = projects.id
+          WHERE projects.process_type != 'draft' -- a project cannot be finished if it's a draft
+          GROUP BY phases.project_id
+          HAVING MAX(phases.end_at) < NOW()),
 
      project_statuses AS (
-         SELECT * FROM regular_project_statuses
+         SELECT * FROM last_project_statuses
          UNION
-         SELECT * FROM finished_status_for_timeline_projects
+         SELECT * FROM finished_statuses_for_timeline_projects
          UNION
          SELECT * FROM finished_statuses_for_continuous_projects
      )
@@ -34,4 +30,3 @@ WITH regular_project_statuses AS -- project statuses that do not need to be calc
 SELECT *, timestamp::DATE as date
 FROM project_statuses
 ORDER BY timestamp DESC;
-
