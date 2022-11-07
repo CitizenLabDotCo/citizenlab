73f9ae8df72ce1612f3c03afd40450987271dc21
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-11-07 18:33:33 +0000

    [CL-1984] Fix build error because of keys utility

--- front/app/utils/helperUtils.ts
+++ front/app/utils/helperUtils.ts
@@ -193,7 +193,8 @@ export function isDesktop(windowWidth: number) {
   return windowWidth > viewportWidths.tablet;
 }
 
-export const keys = <T>(obj: T) => Object.keys(obj as object) as Array<keyof T>;
+export const keys = <T extends object>(obj: T) =>
+  Object.keys(obj) as Array<keyof T>;
 export const get = <T, K extends keyof T>(obj: T, key: K) => obj[key];
 
 export const reduceErrors =
