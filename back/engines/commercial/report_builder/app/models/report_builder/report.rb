037c65fc229dfc968491ce1d6a64ac6db385a569
Adrien Dessy <adrien@citizenlab.co>
2022-11-10 10:04:23 +0100

    CL-2012 Add model, factory and minimal specs for reports

---
+++ back/engines/commercial/report_builder/app/models/report_builder/report.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: report_builder_reports
+#
+#  id         :uuid             not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_report_builder_reports_on_name  (name) UNIQUE
+#
+module ReportBuilder
+  class Report < ::ApplicationRecord
+    has_one(
+      :layout,
+      class_name: 'ContentBuilder::Layout', as: :content_buildable,
+      dependent: :destroy,
+      required: true
+    )
+
+    validates :name, presence: true, uniqueness: true
+  end
+end
