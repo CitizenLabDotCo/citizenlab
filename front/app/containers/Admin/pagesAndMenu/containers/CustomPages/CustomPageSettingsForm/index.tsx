a156774f0c9282753241f60b1c70f20a6028cf91
lucas <lucasquinnmathis@gmail.com>
2022-11-11 09:24:13 +0100

    Use topics and areas to fill in selects

--- front/app/containers/Admin/pagesAndMenu/containers/CustomPages/CustomPageSettingsForm/index.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/CustomPages/CustomPageSettingsForm/index.tsx
@@ -16,7 +16,13 @@ import Button from 'components/UI/Button';
 import InputMultilocWithLocaleSwitcher from 'components/HookForm/InputMultilocWithLocaleSwitcher';
 import SlugInput from 'components/HookForm/SlugInput';
 import Tabs from 'components/HookForm/Tabs';
+import MultipleSelect from 'components/HookForm/MultipleSelect';
+import Select from 'components/HookForm/Select';
 import { Box, IconTooltip, Text } from '@citizenlab/cl2-component-library';
+
+// hooks
+import useTopics from 'hooks/useTopics';
+import useAreas from 'hooks/useAreas';
 
 // utils
 import { handleHookFormSubmissionError } from 'utils/errorUtils';
@@ -28,6 +34,7 @@ import { injectIntl } from 'utils/cl-intl';
 
 // types
 import { Multiloc } from 'typings';
+import { IAreaData } from 'services/areas';
 
 export interface FormValues {
   title_multiloc: Multiloc;
@@ -47,7 +54,7 @@ interface Props {
 const linkedProjectsTabs = [
   {
     name: 'none',
-    label: 'none',
+    label: 'None',
   },
   {
     name: 'byTag',
@@ -99,6 +106,29 @@ const CustomPageSettingsForm = ({
     }
   };
 
+  const mapAreasToOptions = (areas: IAreaData[]) => {
+    return areas.map((area) => {
+      return {
+        value: area.id,
+        label: area.attributes.title_multiloc.en || 'test',
+      };
+    });
+  };
+
+  const mapTopicsToOptions = (topics) => {
+    return topics.map((topic) => {
+      return {
+        value: topic.id,
+        label: topic.attributes.title_multiloc.en || 'test',
+      };
+    });
+  };
+
+  const areas = useAreas();
+  const topics = useTopics();
+
+  console.log([areas, topics]);
+
   return (
     <FormProvider {...methods}>
       <form
@@ -134,7 +164,7 @@ const CustomPageSettingsForm = ({
               <SlugInput slug={slug} pathnameWithoutSlug="pages" />
             )}
             {/* // should be behind a feature flag */}
-            <Box mb="20px">
+            <Box mb="30px">
               <Box display="flex" justifyContent="flex-start">
                 <Text>Linked Projects</Text>
                 <IconTooltip ml="40px" content="Link some projects" />
@@ -146,6 +176,35 @@ const CustomPageSettingsForm = ({
                   console.log(e);
                 }}
               />
+              {methods.getValues('linkedProjectsType') === 'byTag' && (
+                <Box mb="20px">
+                  <MultipleSelect
+                    name="tag_ids"
+                    placeholder={'add tags'}
+                    options={mapTopicsToOptions(topics)}
+                    label={
+                      <>
+                        Add some tags
+                        <IconTooltip content={'add some tags'} />
+                      </>
+                    }
+                  />
+                </Box>
+              )}
+              {methods.getValues('linkedProjectsType') === 'byArea' && (
+                <Box mb="20px">
+                  <Select
+                    name="selected_area"
+                    options={mapAreasToOptions(areas)}
+                    label={
+                      <>
+                        select area
+                        <IconTooltip content={'choose an area'} />
+                      </>
+                    }
+                  />
+                </Box>
+              )}
             </Box>
             <Box display="flex">
               <Button
