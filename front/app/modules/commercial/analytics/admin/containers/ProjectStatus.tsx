6a8d2bfd7c002054069612dcab5733e92ce9d20e
jamesspeake <james@speake.org>
2022-11-15 14:17:48 +0000

    [CL-1312] Disabled until translations arrive

--- front/app/modules/commercial/analytics/admin/containers/ProjectStatus.tsx
+++ front/app/modules/commercial/analytics/admin/containers/ProjectStatus.tsx
@@ -5,5 +5,6 @@ import { StatCardProps } from '../hooks/useStatCard/typings';
 import ProjectStatusCard from '../components/ProjectStatusCard';
 
 export default ({ ...props }: StatCardProps) => {
+  return null; // Disabled until translations arrive
   return <ProjectStatusCard {...props} />;
 };
