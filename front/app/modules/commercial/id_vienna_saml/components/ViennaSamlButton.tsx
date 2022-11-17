8af12d93ffc5e796d17d722cacd2d3e56b2bf9fc
Philipp Preß <ntraum@fastmail.com>
2022-11-17 15:16:19 +0100

    Use standard components

--- front/app/modules/commercial/id_vienna_saml/components/ViennaSamlButton.tsx
+++ front/app/modules/commercial/id_vienna_saml/components/ViennaSamlButton.tsx
@@ -16,7 +16,7 @@ import { TSignUpInFlow } from 'components/SignUpIn';
 // styling
 import styled from 'styled-components';
 import { fontSizes } from 'utils/styleUtils';
-import { Box } from '@citizenlab/cl2-component-library';
+import { Box, Text } from '@citizenlab/cl2-component-library';
 
 const Container = styled(Box)`
   display: flex;
@@ -29,7 +29,7 @@ const TextContainer = styled(Box)`
   flex-direction: column;
 `;
 
-const SignUpSubHeader = styled.span`
+const SignUpSubHeader = styled(Text)`
   font-size: ${fontSizes.s}px;
 `;
 
@@ -67,7 +67,7 @@ const ViennaSamlButton = ({ onContinue, flow }: Props) => {
           />
 
           {flow === 'signup' && (
-            <SignUpSubHeader>
+            <SignUpSubHeader as="span">
               <FormattedMessage
                 {...messages.signUpWithStandardPortalSubHeader}
               />
