57bc4e6bea607cb72f56859dc294c4837c5ed468
Koen De Hondt <koen@dehondt.dev>
2022-11-18 13:58:03 +0100

    [CL-2003] Remove some code now that we have validation in place

--- back/app/services/form_logic_service.rb
+++ back/app/services/form_logic_service.rb
@@ -131,21 +131,16 @@ class FormLogicService
   end
 
   def target_field_rules
-    # Be robust against any missing data in the logic,
-    # because the logic json is not validated in any way.
     @target_field_rules ||= fields.each_with_object({}) do |field, accu|
       rules = field.logic['rules']
       next if rules.blank?
 
       rules.each do |rule|
-        next unless rule.key?('if') && rule.key?('then')
-
-        value = rule['if']
-        actions = rule['then']
-        actions.each do |action|
+        rule['then'].each do |action|
           effect = action['effect']
           case effect
           when 'hide', 'show'
+            value = rule['if']
             target_id = action['target_id']
             accu[target_id] ||= []
             accu[target_id] << ui_schema_rule_for(effect, field, value)
