e40853fb7c19e88604b6876fd1e91a4fbfeed46a
Koen De Hondt <koen@dehondt.dev>
2022-11-18 09:59:10 +0100

    [CL-2003] Ensure that all specs use a page as the first field

--- back/spec/services/form_logic_service_spec.rb
+++ back/spec/services/form_logic_service_spec.rb
@@ -3,41 +3,37 @@
 require 'rails_helper'
 
 describe FormLogicService do
-  let(:form_logic) { described_class.new fields }
-  let(:target_page) { create :custom_field_page, :for_custom_form, resource: field.resource }
-  let(:fields) { [field, target_page] }
+  subject(:form_logic) { described_class.new fields }
+
+  let(:front_page) { create(:custom_field_page, :for_custom_form, resource: field.resource).tap(&:move_to_top) }
+  let(:target_page1) { create :custom_field_page, :for_custom_form, resource: field.resource }
+  let(:fields) { [front_page, field, target_page1] }
   let(:logic) { {} }
 
   # TODO
   # - one value attribute present, others nil
 
   describe '#valid?' do
-    let(:field) do
-      create(
-        :custom_field_linear_scale,
-        :for_custom_form,
-        logic: logic
-      )
-    end
+    let(:field) { create(:custom_field_linear_scale, :for_custom_form, logic: logic) }
 
     context 'returns true when logic has a good structure' do
       let(:target_page2) { create :custom_field_page, :for_custom_form, resource: field.resource }
       let(:target_page3) { create :custom_field_page, :for_custom_form, resource: field.resource }
       let(:target_page4) { create :custom_field_page, :for_custom_form, resource: field.resource }
-      let(:fields) { [field, target_page, target_page2, target_page3, target_page4] }
+      let(:fields) { [front_page, field, target_page1, target_page2, target_page3, target_page4] }
 
       it 'returns true' do
         [
           {},
           { 'rules' => [] },
-          { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => target_page.id }] }] },
-          { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'hide', 'target_id' => target_page.id }] }] },
+          { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => target_page1.id }] }] },
+          { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'hide', 'target_id' => target_page1.id }] }] },
           { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'submit_survey' }] }] },
           { 'rules' => [
             {
               'if' => 123,
               'then' => [
-                { 'effect' => 'show', 'target_id' => target_page.id },
+                { 'effect' => 'show', 'target_id' => target_page1.id },
                 { 'effect' => 'hide', 'target_id' => target_page2.id }
               ]
             },
@@ -60,7 +56,7 @@ describe FormLogicService do
 
     context 'returns false when logic has a bad structure' do
       let(:target_page2) { create :custom_field_page, :for_custom_form, resource: field.resource }
-      let(:fields) { [field, target_page, target_page2] }
+      let(:fields) { [front_page, field, target_page1, target_page2] }
 
       it 'returns false' do
         [
@@ -70,9 +66,9 @@ describe FormLogicService do
           { 'rules' => [{ 'test' => {} }] },
           { 'rules' => [{ 'if' => 123, 'then' => [{}], 'bad' => {} }] },
           { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'show' }] }] },
-          { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => target_page.id, 'bad' => 'bad' }] }] },
-          { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'bad', 'target_id' => target_page.id }] }] },
-          { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'submit_survey', 'target_id' => target_page.id }] }] },
+          { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => target_page1.id, 'bad' => 'bad' }] }] },
+          { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'bad', 'target_id' => target_page1.id }] }] },
+          { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'submit_survey', 'target_id' => target_page1.id }] }] },
           { 'rules' => [
             {
               'if' => 123,
@@ -102,11 +98,11 @@ describe FormLogicService do
         [
           { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => create(:custom_field_page).id }] }] },
           { 'rules' => [{ 'if' => 123, 'then' => [
-            { 'effect' => 'show', 'target_id' => target_page.id },
+            { 'effect' => 'show', 'target_id' => target_page1.id },
             { 'effect' => 'hide', 'target_id' => create(:custom_field_page).id }
           ] }] },
           { 'rules' => [
-            { 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => target_page.id }] },
+            { 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => target_page1.id }] },
             { 'if' => 321, 'then' => [{ 'effect' => 'show', 'target_id' => create(:custom_field_page).id }] }
           ] }
         ].each do |bad_logic|
@@ -119,14 +115,13 @@ describe FormLogicService do
     end
 
     context 'when logic has target pages that occur before the source field' do
-      let(:front_page) { create(:custom_field_page, :for_custom_form, resource: field.resource).tap(&:move_to_top) }
-      let(:fields) { [front_page, field, target_page] }
+      let(:fields) { [front_page, field, target_page1] }
 
       it 'returns false' do
         [
           { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => front_page.id }] }] },
           { 'rules' => [{ 'if' => 123, 'then' => [
-            { 'effect' => 'show', 'target_id' => target_page.id },
+            { 'effect' => 'show', 'target_id' => target_page1.id },
             { 'effect' => 'hide', 'target_id' => front_page.id }
           ] }] }
         ].each do |bad_logic|
@@ -140,13 +135,13 @@ describe FormLogicService do
 
     context 'when logic has target fields that are not pages' do
       let(:end_field) { create :custom_field, :for_custom_form, resource: field.resource }
-      let(:fields) { [field, target_page, end_field] }
+      let(:fields) { [front_page, field, target_page1, end_field] }
 
       it 'returns false' do
         [
           { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => end_field.id }] }] },
           { 'rules' => [{ 'if' => 123, 'then' => [
-            { 'effect' => 'show', 'target_id' => target_page.id },
+            { 'effect' => 'show', 'target_id' => target_page1.id },
             { 'effect' => 'hide', 'target_id' => end_field.id }
           ] }] }
         ].each do |bad_logic|
@@ -163,7 +158,7 @@ describe FormLogicService do
 
       it 'returns false' do
         field.update!(
-          logic: { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => target_page.id }] }] }
+          logic: { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => target_page1.id }] }] }
         )
 
         expect(form_logic.valid?).to be false
