13f4feab236ff91f4ce32198baada62384d304f2
Koen De Hondt <koen@dehondt.dev>
2022-11-18 11:06:10 +0100

    [CL-2003] Improve and elaborate spec

--- back/spec/services/form_logic_service_spec.rb
+++ back/spec/services/form_logic_service_spec.rb
@@ -5,23 +5,32 @@ require 'rails_helper'
 describe FormLogicService do
   subject(:form_logic) { described_class.new fields }
 
-  let(:front_page) { create(:custom_field_page, :for_custom_form, resource: field.resource).tap(&:move_to_top) }
-  let(:target_page1) { create :custom_field_page, :for_custom_form, resource: field.resource }
-  let(:fields) { [front_page, field, target_page1] }
-  let(:logic) { {} }
-
   # TODO
   # - one value attribute present, others nil
 
   describe '#valid?' do
-    let(:field) { create(:custom_field_linear_scale, :for_custom_form, logic: logic) }
+    let(:form) { create(:custom_form) }
+    let(:fields) do
+      # Guarantee correct values for `ordering` attribute.
+      [
+        create(:custom_field_page, :for_custom_form, resource: form),
+        create(:custom_field_linear_scale, :for_custom_form),
+        create(:custom_field_page, :for_custom_form, resource: form),
+        create(:custom_field, :for_custom_form, resource: form),
+        create(:custom_field_page, :for_custom_form, resource: form),
+        create(:custom_field_page, :for_custom_form, resource: form),
+        create(:custom_field_page, :for_custom_form, resource: form)
+      ]
+    end
+    let(:front_page) { fields[0] }
+    let(:field1) { fields[1] }
+    let(:target_page1) { fields[2] }
+    let(:field2) { fields[3] }
+    let(:target_page2) { fields[4] }
+    let(:target_page3) { fields[5] }
+    let(:target_page4) { fields[6] }
 
-    context 'returns true when logic has a good structure' do
-      let(:target_page2) { create :custom_field_page, :for_custom_form, resource: field.resource }
-      let(:target_page3) { create :custom_field_page, :for_custom_form, resource: field.resource }
-      let(:target_page4) { create :custom_field_page, :for_custom_form, resource: field.resource }
-      let(:fields) { [front_page, field, target_page1, target_page2, target_page3, target_page4] }
-
+    context 'when logic has a good structure' do
       it 'returns true' do
         [
           {},
@@ -46,18 +55,46 @@ describe FormLogicService do
             }
           ] }
         ].each do |good_logic|
-          field.update! logic: good_logic
+          field1.update! logic: good_logic
 
           expect(form_logic.valid?).to be true
-          expect(field.errors).to be_empty
+          expect(field1.errors).to be_empty
+          expect(field2.errors).to be_empty
         end
       end
     end
 
-    context 'returns false when logic has a bad structure' do
-      let(:target_page2) { create :custom_field_page, :for_custom_form, resource: field.resource }
-      let(:fields) { [front_page, field, target_page1, target_page2] }
+    context 'when all logic in multiple fields have good structure' do
+      let(:logic_for_field1) do
+        { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => target_page1.id }] }] }
+      end
+      let(:logic_for_field2) do
+        { 'rules' => [
+          {
+            'if' => 123,
+            'then' => [{ 'effect' => 'submit_survey' }]
+          },
+          {
+            'if' => 456,
+            'then' => [
+              { 'effect' => 'show', 'target_id' => target_page3.id },
+              { 'effect' => 'hide', 'target_id' => target_page4.id }
+            ]
+          }
+        ] }
+      end
 
+      it 'returns true' do
+        field1.update! logic: logic_for_field1
+        field2.update! logic: logic_for_field2
+
+        expect(form_logic.valid?).to be true
+        expect(field1.errors).to be_empty
+        expect(field2.errors).to be_empty
+      end
+    end
+
+    context 'when logic has a bad structure' do
       it 'returns false' do
         [
           { 'bad' => [] },
@@ -86,10 +123,40 @@ describe FormLogicService do
             }
           ] }
         ].each do |bad_logic|
-          field.update! logic: bad_logic
+          field1.update! logic: bad_logic
           expect(form_logic.valid?).to be false
-          # expect(field.errors.details).to eq "todo"
+          # expect(field1.errors.details).to eq "todo"
         end
+      end
+    end
+
+    context 'when some logic in multiple fields has bad structure' do
+      let(:logic_for_field1) do
+        { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => target_page1.id }] }] }
+      end
+      let(:logic_for_field2) do
+        { 'rules' => [
+          {
+            'if' => 123,
+            'then' => [{ 'effect' => 'submit_survey' }]
+          },
+          {
+            'if' => 456,
+            'then' => [
+              { 'effect' => 'show' }, # Missing target ID
+              { 'effect' => 'hide', 'target_id' => target_page4.id }
+            ]
+          }
+        ] }
+      end
+
+      it 'returns true' do
+        field1.update! logic: logic_for_field1
+        field2.update! logic: logic_for_field2
+
+        expect(form_logic.valid?).to be false
+        expect(field1.errors).to be_empty
+        # expect(field2.errors.details).to eq "todo"
       end
     end
 
@@ -106,17 +173,15 @@ describe FormLogicService do
             { 'if' => 321, 'then' => [{ 'effect' => 'show', 'target_id' => create(:custom_field_page).id }] }
           ] }
         ].each do |bad_logic|
-          field.update! logic: bad_logic
+          field1.update! logic: bad_logic
 
           expect(form_logic.valid?).to be false
-          # expect(field.errors.details).to eq "todo"
+          # expect(field1.errors.details).to eq "todo"
         end
       end
     end
 
-    context 'when logic has target pages that occur before the source field' do
-      let(:fields) { [front_page, field, target_page1] }
-
+    context 'when logic has target pages that occur before the source field1' do
       it 'returns false' do
         [
           { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => front_page.id }] }] },
@@ -125,44 +190,39 @@ describe FormLogicService do
             { 'effect' => 'hide', 'target_id' => front_page.id }
           ] }] }
         ].each do |bad_logic|
-          field.update! logic: bad_logic
+          field1.update! logic: bad_logic
 
           expect(form_logic.valid?).to be false
-          # expect(field.errors.details).to eq "todo"
+          # expect(field1.errors.details).to eq "todo"
         end
       end
     end
 
     context 'when logic has target fields that are not pages' do
-      let(:end_field) { create :custom_field, :for_custom_form, resource: field.resource }
-      let(:fields) { [front_page, field, target_page1, end_field] }
-
       it 'returns false' do
         [
-          { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => end_field.id }] }] },
+          { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => field2.id }] }] },
           { 'rules' => [{ 'if' => 123, 'then' => [
             { 'effect' => 'show', 'target_id' => target_page1.id },
-            { 'effect' => 'hide', 'target_id' => end_field.id }
+            { 'effect' => 'hide', 'target_id' => field2.id }
           ] }] }
         ].each do |bad_logic|
-          field.update! logic: bad_logic
+          field1.update! logic: bad_logic
 
           expect(form_logic.valid?).to be false
-          # expect(field.errors.details).to eq "todo"
+          # expect(field1.errors.details).to eq "todo"
         end
       end
     end
 
-    context 'when the source field is a page' do
-      let(:field) { create :custom_field_page, :for_custom_form }
-
+    context 'when the source field1 is a page' do
       it 'returns false' do
-        field.update!(
-          logic: { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => target_page1.id }] }] }
+        front_page.update!(
+          logic: { 'rules' => [{ 'if' => 123, 'then' => [{ 'effect' => 'show', 'target_id' => target_page2.id }] }] }
         )
 
         expect(form_logic.valid?).to be false
-        # expect(field.errors.details).to eq "todo"
+        # expect(field1.errors.details).to eq "todo"
       end
     end
   end
