d80d1c4cf3311ef3efa72df6f262cdec853faa3b
Simon Tharby <simontharby@gmail.com>
2022-11-21 15:19:24 +0000

    [CL-1959] Batch user emails into one Sentry error

--- back/engines/commercial/admin_api/app/jobs/admin_api/bulk_delete_users_job.rb
+++ back/engines/commercial/admin_api/app/jobs/admin_api/bulk_delete_users_job.rb
@@ -2,11 +2,23 @@
 
 class AdminApi::BulkDeleteUsersJob < ApplicationJob
   def run(emails)
+    emails_of_not_found_users = []
+
     emails.each do |email|
-      ErrorReporter.handle do
-        user = User.find_by_cimail(email) || raise("No user with email #{email} found")
+      user = User.find_by_cimail(email)
+
+      if user
         DeleteUserJob.perform_later(user)
+      else
+        emails_of_not_found_users << email
       end
+    end
+
+    if emails_of_not_found_users.count.positive?
+      ErrorReporter.report_msg(
+        'One or more users not found with given email(s). See extra for email(s).',
+        extra: { 'emails_of_not_found_users': emails_of_not_found_users.to_s }
+      )
     end
   end
 end
