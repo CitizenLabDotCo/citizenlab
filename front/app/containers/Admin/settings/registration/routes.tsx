b3fccaec61c4d8b56f157e37fdc54c7479e96915
luucvanderzee <luuc_v_d_zee@hotmail.com>
2022-11-21 16:34:42 +0000

    [CL-2086] Fix routes

--- front/app/containers/Admin/settings/registration/routes.tsx
+++ front/app/containers/Admin/settings/registration/routes.tsx
@@ -3,38 +3,6 @@ import PageLoading from 'components/UI/PageLoading';
 
 const AdminSettingsRegistration = lazy(() => import('.'));
 
-const AdminCustomFieldsContainer = React.lazy(() => import('./CustomFields'));
-const AdminNewCustomFieldComponent = React.lazy(
-  () => import('./CustomFields/RegistrationCustomFieldNew')
-);
-const AdminCustomFieldEditComponent = React.lazy(
-  () => import('./CustomFields/RegistrationCustomFieldEdit')
-);
-const AdminCustomFieldRegistrationSettingsComponent = React.lazy(
-  () =>
-    import(
-      './CustomFields/RegistrationCustomFieldEdit/RegistrationCustomFieldSettings'
-    )
-);
-const AdminCustomFieldRegistrationOptionsComponent = React.lazy(
-  () =>
-    import(
-      './CustomFields/RegistrationCustomFieldEdit/RegistrationCustomFieldOptions'
-    )
-);
-const AdminCustomFieldRegistrationOptionsNewComponent = React.lazy(
-  () =>
-    import(
-      './CustomFields/RegistrationCustomFieldEdit/RegistrationCustomFieldOptionsNew'
-    )
-);
-const AdminCustomFieldRegistrationOptionsEditComponent = React.lazy(
-  () =>
-    import(
-      './CustomFields/RegistrationCustomFieldEdit/RegistrationCustomFieldOptionsEdit'
-    )
-);
-
 export default () => ({
   path: 'registration',
   element: (
@@ -42,38 +10,4 @@ export default () => ({
       <AdminSettingsRegistration />
     </PageLoading>
   ),
-  children: [
-    {
-      path: 'custom-fields',
-      element: <AdminCustomFieldsContainer />,
-      children: [
-        {
-          path: 'new',
-          element: <AdminNewCustomFieldComponent />,
-        },
-        {
-          path: ':userCustomFieldId',
-          element: <AdminCustomFieldEditComponent />,
-          children: [
-            {
-              path: 'field-settings',
-              element: <AdminCustomFieldRegistrationSettingsComponent />,
-            },
-            {
-              path: 'options',
-              element: <AdminCustomFieldRegistrationOptionsComponent />,
-            },
-            {
-              path: 'options/new',
-              element: <AdminCustomFieldRegistrationOptionsNewComponent />,
-            },
-            {
-              path: 'options/:userCustomFieldOptionId',
-              element: <AdminCustomFieldRegistrationOptionsEditComponent />,
-            },
-          ],
-        },
-      ],
-    },
-  ],
 });
