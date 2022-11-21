cd5b6b775fb74b75730bb4e5424d40d92017925c
lucas <lucasquinnmathis@gmail.com>
2022-11-21 17:07:30 +0100

    Update import in project folder go back test

--- front/app/modules/commercial/project_folders/citizen/components/ProjectFolderGoBackButton/ProjectFolderGoBackButton.test.tsx
+++ front/app/modules/commercial/project_folders/citizen/components/ProjectFolderGoBackButton/ProjectFolderGoBackButton.test.tsx
@@ -18,9 +18,7 @@ const projectFolderData = {
 };
 
 jest.mock('utils/cl-router/history');
-jest.mock('../../../hooks/useProjectFolder', () =>
-  jest.fn(() => projectFolderData)
-);
+jest.mock('hooks/useProjectFolder', () => jest.fn(() => projectFolderData));
 jest.mock('hooks/useLocale');
 jest.mock('hooks/useLocalize');
 
