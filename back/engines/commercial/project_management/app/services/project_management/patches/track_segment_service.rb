71b7d3864fb4692e37532cd41b5ea17fca9dfcdd
jamesspeake <james@speake.org>
2022-11-22 16:17:06 +0000

    [CL-2101] Initial commit of move of track_segment_service patch to core

--- back/engines/commercial/project_management/app/services/project_management/patches/track_segment_service.rb
+++ back/engines/commercial/project_management/app/services/project_management/patches/track_segment_service.rb
@@ -1,21 +1,18 @@
 # frozen_string_literal: true
 
-module ProjectManagement
-  module Patches
-    module TrackSegmentService
-      def user_traits(user)
-        super.merge(isProjectModerator: user.project_moderator?)
-      end
-
-      # @param [Symbol] role
-      def intercom_integration_enabled?(role)
-        super || role == :project_moderator
-      end
-
-      # @param [Symbol] role
-      def satismeter_integration_enabled?(role)
-        super || role == :project_moderator
-      end
-    end
-  end
-end
+# module ProjectManagement
+#   module Patches
+#     module TrackSegmentService
+#
+#       # @param [Symbol] role
+#       def intercom_integration_enabled?(role)
+#         super || role == :project_moderator
+#       end
+#
+#       # @param [Symbol] role
+#       def satismeter_integration_enabled?(role)
+#         super || role == :project_moderator
+#       end
+#     end
+#   end
+# end
