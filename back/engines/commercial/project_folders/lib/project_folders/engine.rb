6d5d61b0375a9d4f17e045a9feb79bfda8a9387b
Koen De Hondt <koen@dehondt.dev>
2022-11-23 14:37:02 +0100

    [CL-2100] Move engine code to add project folders to the site map to a core initializer

--- back/engines/commercial/project_folders/lib/project_folders/engine.rb
+++ back/engines/commercial/project_folders/lib/project_folders/engine.rb
@@ -22,20 +22,6 @@ module ProjectFolders
     config.to_prepare do
       require 'project_folders/feature_specification'
       AppConfiguration::Settings.add_feature(ProjectFolders::FeatureSpecification)
-
-      # Adding project folders to the sitemap.
-      if defined? ::Seo::ApplicationController
-        ::Seo::ApplicationController.outlet 'seo.sitemap' do |locals|
-          folders = ProjectFolders::Folder
-            .select(
-              :'project_folders_folders.id', :'project_folders_folders.slug',
-              :'project_folders_folders.updated_at', :'admin_publications.publication_status',
-              :'admin_publications.publication_type', :'admin_publications.publication_id'
-            )
-            .includes(:admin_publication).where(admin_publications: { publication_status: %w[published archived] })
-          { partial: 'seo/sitemap', locals: { folders: folders, **locals } }
-        end
-      end
     end
   end
 end
