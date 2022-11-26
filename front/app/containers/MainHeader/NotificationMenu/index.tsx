65c847a06b39b49874d172990fa91d449ef4e527
brentguf <brentguf@users.noreply.github.com>
2022-11-26 15:55:29 +0100

    Refactor to functional component

--- front/app/containers/MainHeader/NotificationMenu/index.tsx
+++ front/app/containers/MainHeader/NotificationMenu/index.tsx
@@ -1,87 +1,47 @@
-import React, { lazy, Suspense } from 'react';
-import { adopt } from 'react-adopt';
+import React, { lazy, Suspense, useState } from 'react';
 import { isNilOrError } from 'utils/helperUtils';
-
-import styled from 'styled-components';
-
 import { trackEventByName } from 'utils/analytics';
 import tracks from './tracks';
-
 import NotificationCount from './components/NotificationCount';
 const NotificationsDropdown = lazy(() => import('./NotificationsDropdown'));
-
 import { markAllAsRead } from 'services/notifications';
-import GetAuthUser, { GetAuthUserChildProps } from 'resources/GetAuthUser';
+import useAuthUser from 'hooks/useAuthUser';
+import { Box } from '@citizenlab/cl2-component-library';
 
-const Container = styled.div`
-  position: relative;
-`;
+const NotificationMenu = () => {
+  const authUser = useAuthUser();
+  const [dropdownOpened, setDropdownOpened] = useState(false);
 
-interface InputProps {}
-
-interface DataProps {
-  authUser: GetAuthUserChildProps;
-}
-
-interface Props extends InputProps, DataProps {}
-
-interface State {
-  dropdownOpened: boolean;
-}
-
-export class NotificationMenu extends React.PureComponent<Props, State> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dropdownOpened: false,
-    };
-  }
-
-  toggleDropdown = () => {
-    this.setState(({ dropdownOpened }) => {
-      if (!dropdownOpened) {
-        trackEventByName(tracks.clickOpenNotifications.name);
-      } else {
-        markAllAsRead();
-        trackEventByName(tracks.clickCloseNotifications.name);
-      }
-
-      return { dropdownOpened: !dropdownOpened };
-    });
-  };
-
-  render() {
-    const { dropdownOpened } = this.state;
-    const { authUser } = this.props;
-
-    if (!isNilOrError(authUser)) {
-      return (
-        <Container>
-          <NotificationCount
-            count={authUser.attributes.unread_notifications}
-            onToggleDropdown={this.toggleDropdown}
-            dropdownOpened={dropdownOpened}
-          />
-          <Suspense fallback={null}>
-            <NotificationsDropdown
-              dropdownOpened={dropdownOpened}
-              toggleDropdown={this.toggleDropdown}
-            />
-          </Suspense>
-        </Container>
-      );
+  const toggleDropdown = () => {
+    if (!dropdownOpened) {
+      trackEventByName(tracks.clickOpenNotifications.name);
+    } else {
+      markAllAsRead();
+      trackEventByName(tracks.clickCloseNotifications.name);
     }
 
-    return null;
+    setDropdownOpened((dropdownOpened) => !dropdownOpened);
+  };
+
+  if (!isNilOrError(authUser)) {
+    return (
+      <Box position="relative">
+        <NotificationCount
+          count={authUser.attributes.unread_notifications}
+          onToggleDropdown={toggleDropdown}
+          dropdownOpened={dropdownOpened}
+        />
+        <Suspense fallback={null}>
+          <NotificationsDropdown
+            dropdownOpened={dropdownOpened}
+            toggleDropdown={toggleDropdown}
+          />
+        </Suspense>
+      </Box>
+    );
   }
-}
 
-const Data = adopt<DataProps, InputProps>({
-  authUser: <GetAuthUser />,
-});
+  return null;
+};
 
-export default (inputProps: InputProps) => (
-  <Data>
-    {(dataProps) => <NotificationMenu {...inputProps} {...dataProps} />}
-  </Data>
-);
+export default NotificationMenu;
