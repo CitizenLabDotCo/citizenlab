95c321b5215203226f32f6c7ee1b465f3bc04188
Philipp Preß <ntraum@fastmail.com>
2022-11-29 07:31:28 +0100

    Rename key

--- front/app/components/AuthProviders/messages.ts
+++ front/app/components/AuthProviders/messages.ts
@@ -84,8 +84,8 @@ export default defineMessages({
     id: 'app.containers.SignUp.viennaConsentUserName',
     defaultMessage: 'User name',
   },
-  viennaThePrivacyPolicy: {
-    id: 'app.containers.SignUp.viennaThePrivacyPolicy',
+  viennaDataProtection: {
+    id: 'app.containers.SignUp.viennaDataProtection',
     defaultMessage: 'the vienna privacy policy',
   },
   iHaveReadAndAgreeToVienna: {
