e5018c120b1ef8887c0ab4cdf1e7e1172135f9af
brentguf <brentguf@users.noreply.github.com>
2022-11-30 10:17:09 +0100

    SlugInput margins

--- front/app/components/HookForm/SlugInput/index.tsx
+++ front/app/components/HookForm/SlugInput/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Input from 'components/HookForm/Input';
 import messages from './messages';
 import { FormattedMessage } from 'utils/cl-intl';
-import { Box, Text } from '@citizenlab/cl2-component-library';
+import { Text } from '@citizenlab/cl2-component-library';
 import Warning from 'components/UI/Warning';
 import { isNilOrError } from 'utils/helperUtils';
 import useLocale from 'hooks/useLocale';
@@ -36,18 +36,16 @@ const SlugInput = ({
           name="slug"
           type="text"
         />
-        <Text>
-          <b>
+        <Text mb={showWarningMessage ? '16px' : '0'}>
+          <i>
             <FormattedMessage {...messages.resultingPageURL} />
-          </b>
+          </i>
           : {previewUrl}
         </Text>
         {showWarningMessage && (
-          <Box mb="16px">
-            <Warning>
-              <FormattedMessage {...messages.brokenURLWarning} />
-            </Warning>
-          </Box>
+          <Warning>
+            <FormattedMessage {...messages.brokenURLWarning} />
+          </Warning>
         )}
       </>
     );
