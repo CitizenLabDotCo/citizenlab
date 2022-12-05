3fc32763f9f79676ce58278e21f0d550de2b6b74
Simon Tharby <simontharby@gmail.com>
2022-12-05 09:41:42 +0000

    [CL-2174] Move test to model spec and simplify

--- back/spec/models/notifications/project_phase_upcoming_spec.rb
+++ back/spec/models/notifications/project_phase_upcoming_spec.rb
@@ -4,17 +4,40 @@ require 'rails_helper'
 
 RSpec.describe Notifications::ProjectPhaseUpcoming, type: :model do
   describe 'make_notifications_on' do
-    it 'makes a notification on created comment activity' do
-      admin = create :admin
-      phase = create :phase
-      activity = create :activity, item: phase, action: 'upcoming'
+    let!(:admin) { create :admin }
+    let!(:phase) { create :phase }
+    let!(:activity) { create :activity, item: phase, action: 'upcoming' }
 
+    it 'makes a notification on created comment activity' do
       notifications = described_class.make_notifications_on activity
       expect(notifications.first).to have_attributes(
         recipient_id: admin.id,
         phase_id: phase.id,
         project_id: phase.project_id
       )
+    end
+
+    it 'only notifies admins and moderators who can moderate project' do
+      project = create(:project_with_current_phase, phases: [phase])
+      other_project = create(:project_with_current_phase)
+      project_folder = create(:project_folder, projects: [project])
+      other_project_folder = create(:project_folder, projects: [other_project])
+
+      regular_user = create(:user)
+      moderator_of_project = create(:project_moderator, projects: [project])
+      moderator_of_other_project = create(:project_moderator, projects: [other_project])
+      moderator_of_project_folder = create(:project_folder_moderator, project_folders: [project_folder])
+      moderator_of_other_project_folder = create(:project_folder_moderator, project_folders: [other_project_folder])
+
+      notifications = described_class.make_notifications_on activity
+      notification_ids = notifications.pluck(:recipient_id)
+
+      expect(notification_ids).to include moderator_of_project.id
+      expect(notification_ids).to include moderator_of_project_folder.id
+      expect(notification_ids).to include admin.id
+      expect(notification_ids).not_to include regular_user.id
+      expect(notification_ids).not_to include moderator_of_other_project.id
+      expect(notification_ids).not_to include moderator_of_other_project_folder.id
     end
   end
 end
