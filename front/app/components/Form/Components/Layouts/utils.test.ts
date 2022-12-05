bd96430429cc6ac5f0b79a3b955f80e5c376fafa
Edwin Kato <katedwin88@gmail.com>
2022-12-05 17:17:17 +0300

    Add test for isPageCategorization

--- front/app/components/Form/Components/Layouts/utils.test.ts
+++ front/app/components/Form/Components/Layouts/utils.test.ts
@@ -2,6 +2,7 @@ import Ajv from 'ajv';
 import {
   getPageSchema,
   PageCategorization,
+  isPageCategorization,
 } from 'components/Form/Components/Layouts/utils';
 
 const customAjv = new Ajv({ useDefaults: 'empty', removeAdditional: true });
@@ -228,5 +229,65 @@ describe('getPageSchema', () => {
       customAjv
     );
     expect(pageSchema).toEqual(expectedPageSchema);
+  });
+});
+
+describe('isPageCategorization', () => {
+  it('should return true if the categorization is a page', () => {
+    const uiSchema = {
+      type: 'Categorization',
+      label: 'Testlabel',
+      options: {
+        id: 'extra',
+      },
+      elements: [
+        {
+          type: 'Page',
+          label: 'Testlabel',
+          options: {
+            id: 'extra',
+          },
+          elements: [],
+        },
+      ],
+    };
+
+    const jsonSchema = {
+      type: 'object',
+      additionalProperties: false,
+      properties: {},
+      required: ['short_answer'],
+    };
+    const isPage = isPageCategorization(uiSchema, jsonSchema);
+    expect(isPage).toEqual(true);
+  });
+
+  it('should return false when the structure has no page in it', () => {
+    const uiSchema = {
+      type: 'Categorization',
+      label: 'Testlabel',
+      options: {
+        id: 'extra',
+      },
+      elements: [
+        {
+          type: 'Control',
+          label: 'Testlabel',
+          options: {
+            id: 'extra',
+          },
+          elements: [],
+        },
+      ],
+    };
+
+    const jsonSchema = {
+      type: 'object',
+      additionalProperties: false,
+      properties: {},
+      required: ['short_answer'],
+    };
+    const isPage = isPageCategorization(uiSchema, jsonSchema);
+    expect(isPage).toEqual(false);
   });
 });
