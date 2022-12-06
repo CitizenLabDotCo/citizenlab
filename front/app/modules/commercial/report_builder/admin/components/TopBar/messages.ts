310c36cc41baa5bda2dd346cc3a08859189f507b
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2022-12-06 19:07:14 +0000

    [CL-2219] Add TopBar

---
+++ front/app/modules/commercial/report_builder/admin/components/TopBar/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  reportCreator: {
+    id: 'app.modules.commercial.report_builder.admin.components.TopBar.reportCreator',
+    defaultMessage: 'Report creator',
+  },
+});
--- front/app/modules/commercial/report_builder/admin/containers/index.tsx
+++ front/app/modules/commercial/report_builder/admin/containers/index.tsx
@@ -1,7 +1,15 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, {
+  useState,
+  useRef,
+  useCallback,
+  useEffect,
+  useMemo,
+} from 'react';
+import { useLocation } from 'react-router-dom';
 
 // hooks
 import useFeatureFlag from 'hooks/useFeatureFlag';
+import useLocale from 'hooks/useLocale';
 
 // components
 import { Box } from '@citizenlab/cl2-component-library';
@@ -9,6 +17,10 @@ import { Box } from '@citizenlab/cl2-component-library';
 // craft
 import FullscreenContentBuilder from 'components/admin/ContentBuilder/FullscreenContentBuilder';
 import Editor from '../components/Editor';
+import TopBar from '../components/TopBar';
+
+// utils
+import { isNilOrError } from 'utils/helperUtils';
 
 // typings
 import { ContentBuilderErrors } from 'components/admin/ContentBuilder/typings';
@@ -17,10 +29,25 @@ import { Locale } from 'typings';
 
 const ReportBuilder = () => {
   const iframeRef = useRef<HTMLIFrameElement | null>(null);
+  const [previewEnabled, setPreviewEnabled] = useState(false);
   const [contentBuilderErrors, setContentBuilderErrors] =
     useState<ContentBuilderErrors>({});
   const [imageUploading, setImageUploading] = useState(false);
   const [selectedLocale, setSelectedLocale] = useState<Locale | undefined>();
+  const [draftData, setDraftData] = useState<Record<string, SerializedNodes>>();
+  const locale = useLocale();
+
+  useEffect(() => {
+    if (!isNilOrError(locale)) {
+      setSelectedLocale(locale);
+    }
+  }, [locale]);
+
+  const localesWithError = useMemo(() => {
+    return Object.values(contentBuilderErrors)
+      .filter((node) => node.hasError)
+      .map((node) => node.selectedLocale);
+  }, [contentBuilderErrors]);
 
   const handleErrors = useCallback((newErrors: ContentBuilderErrors) => {
     setContentBuilderErrors((contentBuilderErrors) => ({
@@ -42,6 +69,30 @@ const ReportBuilder = () => {
       iframeRef.current.contentWindow.postMessage(nodes, window.location.href);
   }, []);
 
+  const handleSelectedLocaleChange = useCallback(
+    ({
+      locale,
+      editorData,
+    }: {
+      locale: Locale;
+      editorData: SerializedNodes;
+    }) => {
+      if (selectedLocale && selectedLocale !== locale) {
+        setDraftData({ ...draftData, [selectedLocale]: editorData });
+      }
+
+      iframeRef.current &&
+        iframeRef.current.contentWindow &&
+        iframeRef.current.contentWindow.postMessage(
+          { selectedLocale: locale },
+          window.location.href
+        );
+
+      setSelectedLocale(locale);
+    },
+    []
+  );
+
   return (
     <FullscreenContentBuilder
       onErrors={handleErrors}
@@ -53,6 +104,15 @@ const ReportBuilder = () => {
         onNodesChange={handleEditorChange}
         key={selectedLocale}
       >
+        <TopBar
+          localesWithError={localesWithError}
+          hasPendingState={imageUploading}
+          previewEnabled={previewEnabled}
+          setPreviewEnabled={setPreviewEnabled}
+          selectedLocale={selectedLocale}
+          onSelectLocale={handleSelectedLocaleChange}
+          draftEditorData={draftData}
+        />
         <Box p="20px">Hello world!</Box>
       </Editor>
     </FullscreenContentBuilder>
@@ -61,7 +121,12 @@ const ReportBuilder = () => {
 
 const ReportBuilderWrapper = () => {
   const reportBuilderEnabled = useFeatureFlag({ name: 'report_builder' });
-  if (!reportBuilderEnabled) return null;
+  const { pathname } = useLocation();
+
+  const renderReportBuilder =
+    reportBuilderEnabled && pathname.includes('admin/report-builder');
+
+  if (!renderReportBuilder) return null;
 
   return <ReportBuilder />;
 };
