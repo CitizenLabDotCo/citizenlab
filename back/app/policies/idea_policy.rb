bb08a6b353314c5f70d3aa8df42893289a75fc13
Sebastien Hoorens <sebastien@citizenlab.co>
2022-12-06 13:04:43 +0100

    policy spec for ideas with posting disabled reasons

--- back/app/policies/idea_policy.rb
+++ back/app/policies/idea_policy.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class IdeaPolicy < ApplicationPolicy
+  EXCLUDED_REASONS_FOR_UPDATE = %w[posting_disabled posting_limited_max_reached].freeze
   class Scope
     attr_reader :user, :scope
 
@@ -59,13 +60,12 @@ class IdeaPolicy < ApplicationPolicy
   def update?
     return false if record.participation_method_on_creation.never_update?
 
-    bypassable_reasons = %w[posting_disabled posting_limited_max_reached]
     pcs = ParticipationContextService.new
     pcs_posting_reason = pcs.posting_idea_disabled_reason_for_project(record.project, user)
     return true if record.draft? || (user && UserRoleService.new.can_moderate_project?(record.project, user))
     return false unless active? && owner? && ProjectPolicy.new(user, record.project).show?
 
-    raise_not_authorized(pcs_posting_reason) if pcs_posting_reason && bypassable_reasons.exclude?(pcs_posting_reason)
+    raise_not_authorized(pcs_posting_reason) if pcs_posting_reason && EXCLUDED_REASONS_FOR_UPDATE.exclude?(pcs_posting_reason)
     true
   end
 
