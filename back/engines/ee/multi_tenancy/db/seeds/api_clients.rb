bcf18b4c0d7c4cb7186ecc5278325cf678dcdf97
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2022-12-06 15:46:22 +0100

    CL-1979 Reorganized .env file and replaced code secrets

--- back/engines/ee/multi_tenancy/db/seeds/api_clients.rb
+++ back/engines/ee/multi_tenancy/db/seeds/api_clients.rb
@@ -8,7 +8,7 @@ module MultiTenancy
       def run
         PublicApi::ApiClient.create!(
           id: '42cb419a-b1f8-4600-8c4e-fd45cca4bfd9',
-          secret: 'Hx7C27lxV7Qszw-zCg9UT-GFRQuxJNffllTpeU262CGabllbyTYwOmpizCygtPIZSwg'
+          secret: 'Hx7C27lxV7Qszw-zCg9UT-GFRQuxJNffllTpeU262CGabllbyTYwOmpizCygtPIZSwg' #gitleaks:allow
         )
       end
     end
