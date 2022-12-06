023ba55295aa53be7d4d1294324957b0acedc541
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2022-12-06 17:16:42 +0100

    CL-1979 Split up env files in secret <> safe

--- scripts/enable_ee.sh
+++ scripts/enable_ee.sh
@@ -6,8 +6,19 @@ echo "Copied citizenlab.config.ee.json"
 cp -r back/engines/ee ../citizenlab/back/engines/.
 echo "Copied back/engines/ee"
 
-cp .env-back ../citizenlab/.
+if [ ! -f ".env-back-secret" ]
+then
+  echo "ERROR: Generating .env-back failed because citizenlab-ee/.env-back-secret is missing. Get its content from LastPass ('citizenlab .env-back-secret' note) and retry."
+  exit 1
+fi
+cat .env-back-safe .env-back-secret | tee ../citizenlab/.env-back > /dev/null
 echo "Copied .env-back"
 
-cp .env-front ../citizenlab/.
+
+if [ ! -f ".env-front-secret" ]
+then
+  echo "ERROR: Generating .env-front failed because citizenlab-ee/.env-front-secret is missing. Get its content from LastPass ('citizenlab .env-front-secret' note) and retry."
+  exit 1
+fi
+cat .env-front-safe .env-front-secret | tee ../citizenlab/.env-front > /dev/null
 echo "Copied .env-front"
