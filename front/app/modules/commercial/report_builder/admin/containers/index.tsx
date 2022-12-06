3c7a98e4b4875aa2234d04e3d4d741a43ea2b022
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2022-12-06 17:19:11 +0000

    [CL-2219] Add Editor

--- front/app/modules/commercial/report_builder/admin/containers/index.tsx
+++ front/app/modules/commercial/report_builder/admin/containers/index.tsx
@@ -1,18 +1,26 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 
 // hooks
 import useFeatureFlag from 'hooks/useFeatureFlag';
 
+// components
+import { Box } from '@citizenlab/cl2-component-library';
+
 // craft
 import FullscreenContentBuilder from 'components/admin/ContentBuilder/FullscreenContentBuilder';
+import Editor from '../components/Editor';
 
 // typings
 import { ContentBuilderErrors } from 'components/admin/ContentBuilder/typings';
+import { SerializedNodes } from '@craftjs/core';
+import { Locale } from 'typings';
 
 const ReportBuilder = () => {
+  const iframeRef = useRef<HTMLIFrameElement | null>(null);
   const [contentBuilderErrors, setContentBuilderErrors] =
     useState<ContentBuilderErrors>({});
   const [imageUploading, setImageUploading] = useState(false);
+  const [selectedLocale, setSelectedLocale] = useState<Locale | undefined>();
 
   const handleErrors = useCallback((newErrors: ContentBuilderErrors) => {
     setContentBuilderErrors((contentBuilderErrors) => ({
@@ -28,12 +36,26 @@ const ReportBuilder = () => {
     });
   }, []);
 
+  const handleEditorChange = useCallback((nodes: SerializedNodes) => {
+    iframeRef.current &&
+      iframeRef.current.contentWindow &&
+      iframeRef.current.contentWindow.postMessage(nodes, window.location.href);
+  }, []);
+
   return (
     <FullscreenContentBuilder
       onErrors={handleErrors}
       onDeleteElement={handleDeleteElement}
       onUploadImage={setImageUploading}
-    ></FullscreenContentBuilder>
+    >
+      <Editor
+        isPreview={false}
+        onNodesChange={handleEditorChange}
+        key={selectedLocale}
+      >
+        <Box p="20px">Hello world!</Box>
+      </Editor>
+    </FullscreenContentBuilder>
   );
 };
 
