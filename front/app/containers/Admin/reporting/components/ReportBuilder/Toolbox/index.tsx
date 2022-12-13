881b03cedb90d85fe39f0642f860b237a927c2dc
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2022-12-13 15:55:46 +0000

    [CL-2232] Cleanup

--- front/app/containers/Admin/reporting/components/ReportBuilder/Toolbox/index.tsx
+++ front/app/containers/Admin/reporting/components/ReportBuilder/Toolbox/index.tsx
@@ -4,10 +4,6 @@ import React from 'react';
 import Container from 'components/admin/ContentBuilder/Toolbox/Container';
 import SectionTitle from 'components/admin/ContentBuilder/Toolbox/SectionTitle';
 
-// TODO remove
-import TwoColumn from 'components/admin/ContentBuilder/Widgets/TwoColumn';
-import DraggableElement from 'components/admin/ContentBuilder/Toolbox/DraggableElement';
-
 // types
 import { Locale } from 'typings';
 
@@ -22,12 +18,6 @@ const ContentBuilderToolbox = (
   return (
     <Container>
       <SectionTitle>Widgets</SectionTitle>
-      <DraggableElement
-        id="e2e-draggable-two-column"
-        component={<TwoColumn columnLayout="1-1" />}
-        icon="layout-2column-1"
-        label={'Two column'}
-      />
     </Container>
   );
 };
