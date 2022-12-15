90c0e1a34ab4b5e76834fd6e9e7da556caf4f05c
brentguf <brentguf@users.noreply.github.com>
2022-12-15 15:22:32 +0100

    Unused import

--- front/app/containers/Admin/pagesAndMenu/containers/GenericHeroBannerForm/BannerImageFields/OverlayControls.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/GenericHeroBannerForm/BannerImageFields/OverlayControls.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useMemo } from 'react';
 import styled, { useTheme } from 'styled-components';
 import {
   Box,
