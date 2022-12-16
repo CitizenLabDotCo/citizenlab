6083efe483a7c2cc94b81d98bd24b21815bbecab
brentguf <brentguf@users.noreply.github.com>
2022-12-16 14:51:55 +0100

    Fix test

--- front/app/containers/MainHeader/NotificationMenu/NotificationMenu.test.tsx
+++ front/app/containers/MainHeader/NotificationMenu/NotificationMenu.test.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 // component to test
-import { NotificationMenu } from './';
+import NotificationMenu from './';
 import { GetAuthUserChildProps } from 'resources/GetAuthUser';
 
 // mock utilities
