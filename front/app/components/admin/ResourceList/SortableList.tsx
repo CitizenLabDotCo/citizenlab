b7e7f980f17de6ae97389db77f284e3b813d2164
brentguf <brentguf@users.noreply.github.com>
2022-12-18 12:13:50 +0100

    Enfore strict types in navbar files

--- front/app/components/admin/ResourceList/SortableList.tsx
+++ front/app/components/admin/ResourceList/SortableList.tsx
@@ -38,7 +38,7 @@ export interface SortableListState {
 }
 
 export class SortableList extends Component<InputProps, SortableListState> {
-  constructor(props) {
+  constructor(props: InputProps) {
     super(props);
     this.state = {
       itemsWhileDragging: null,
