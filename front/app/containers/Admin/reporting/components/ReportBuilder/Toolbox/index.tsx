ccab9da4d936b1d627dea70598cb4c29c8e7ab20
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2022-12-19 12:02:15 +0000

    Scroll toolbox on overflow

--- front/app/containers/Admin/reporting/components/ReportBuilder/Toolbox/index.tsx
+++ front/app/containers/Admin/reporting/components/ReportBuilder/Toolbox/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 // components
 import Container from 'components/admin/ContentBuilder/Toolbox/Container';
-import { Title, Accordion } from '@citizenlab/cl2-component-library';
+import { Box, Title, Accordion } from '@citizenlab/cl2-component-library';
 import TwoColumn from '../../../components/ReportBuilder/Widgets/TwoColumn';
 import WhiteSpace from 'components/admin/ContentBuilder/Widgets/WhiteSpace';
 import Text from 'components/admin/ContentBuilder/Widgets/Text';
@@ -52,137 +52,139 @@ const ReportBuilderToolbox = ({ reportId }: ReportBuilderToolboxProps) => {
 
   return (
     <Container>
-      <Accordion
-        isOpenByDefault={true}
-        title={
-          <SectionTitle>
-            <FormattedMessage {...contentBuilderMessages.layout} />
-          </SectionTitle>
-        }
-      >
-        <DraggableElement
-          id="e2e-draggable-two-column"
-          component={<TwoColumn columnLayout="1-1" />}
-          icon="layout-2column-1"
-          label={formatMessage(TwoColumn.craft.custom.title)}
-        />
-        <DraggableElement
-          id="e2e-draggable-white-space"
-          component={<WhiteSpace size="small" />}
-          icon="layout-white-space"
-          label={formatMessage(WhiteSpace.craft.custom.title)}
-        />
-      </Accordion>
+      <Box height="100%" overflowX="scroll" pb="100px">
+        <Accordion
+          isOpenByDefault={true}
+          title={
+            <SectionTitle>
+              <FormattedMessage {...contentBuilderMessages.layout} />
+            </SectionTitle>
+          }
+        >
+          <DraggableElement
+            id="e2e-draggable-two-column"
+            component={<TwoColumn columnLayout="1-1" />}
+            icon="layout-2column-1"
+            label={formatMessage(TwoColumn.craft.custom.title)}
+          />
+          <DraggableElement
+            id="e2e-draggable-white-space"
+            component={<WhiteSpace size="small" />}
+            icon="layout-white-space"
+            label={formatMessage(WhiteSpace.craft.custom.title)}
+          />
+        </Accordion>
 
-      <Accordion
-        isOpenByDefault={true}
-        title={
-          <SectionTitle>
-            <FormattedMessage {...contentBuilderMessages.content} />
-          </SectionTitle>
-        }
-      >
-        <DraggableElement
-          id="e2e-draggable-about-report"
-          component={<AboutReportWidget reportId={reportId} />}
-          icon="section-image-text"
-          label={formatMessage(aboutMessages.aboutThisReport)}
-        />
-        <DraggableElement
-          id="e2e-draggable-text"
-          component={<Text text={formatMessage(textMessages.textValue)} />}
-          icon="text"
-          label={formatMessage(Text.craft.custom.title)}
-        />
-        <DraggableElement
-          id="e2e-draggable-image"
-          component={<Image alt="" />}
-          icon="image"
-          label={formatMessage(Image.craft.custom.title)}
-        />
-      </Accordion>
+        <Accordion
+          isOpenByDefault={true}
+          title={
+            <SectionTitle>
+              <FormattedMessage {...contentBuilderMessages.content} />
+            </SectionTitle>
+          }
+        >
+          <DraggableElement
+            id="e2e-draggable-about-report"
+            component={<AboutReportWidget reportId={reportId} />}
+            icon="section-image-text"
+            label={formatMessage(aboutMessages.aboutThisReport)}
+          />
+          <DraggableElement
+            id="e2e-draggable-text"
+            component={<Text text={formatMessage(textMessages.textValue)} />}
+            icon="text"
+            label={formatMessage(Text.craft.custom.title)}
+          />
+          <DraggableElement
+            id="e2e-draggable-image"
+            component={<Image alt="" />}
+            icon="image"
+            label={formatMessage(Image.craft.custom.title)}
+          />
+        </Accordion>
 
-      <Accordion
-        isOpenByDefault={true}
-        title={
-          <SectionTitle>
-            <FormattedMessage {...reportBuilderMessages.chartsSection} />
-          </SectionTitle>
-        }
-      >
-        <DraggableElement
-          id="e2e-draggable-visitors-timeline-widget"
-          component={
-            <AnalyticsChartWidget
-              chartType="VisitorsCard"
-              title={formatMessage(chartMessages.visitorTimeline)}
-              projectId={undefined}
-              startAt={undefined}
-              endAt={chartEndDate}
-            />
+        <Accordion
+          isOpenByDefault={true}
+          title={
+            <SectionTitle>
+              <FormattedMessage {...reportBuilderMessages.chartsSection} />
+            </SectionTitle>
           }
-          icon="chart-bar"
-          label={formatMessage(chartMessages.visitorTimeline)}
-        />
-        <DraggableElement
-          id="e2e-draggable-visitors-traffic-sources-widget"
-          component={
-            <AnalyticsChartWidget
-              chartType="VisitorsTrafficSourcesCard"
-              title={formatMessage(chartMessages.trafficSources)}
-              projectId={undefined}
-              startAt={undefined}
-              endAt={chartEndDate}
-            />
+        >
+          <DraggableElement
+            id="e2e-draggable-visitors-timeline-widget"
+            component={
+              <AnalyticsChartWidget
+                chartType="VisitorsCard"
+                title={formatMessage(chartMessages.visitorTimeline)}
+                projectId={undefined}
+                startAt={undefined}
+                endAt={chartEndDate}
+              />
+            }
+            icon="chart-bar"
+            label={formatMessage(chartMessages.visitorTimeline)}
+          />
+          <DraggableElement
+            id="e2e-draggable-visitors-traffic-sources-widget"
+            component={
+              <AnalyticsChartWidget
+                chartType="VisitorsTrafficSourcesCard"
+                title={formatMessage(chartMessages.trafficSources)}
+                projectId={undefined}
+                startAt={undefined}
+                endAt={chartEndDate}
+              />
+            }
+            icon="chart-bar"
+            label={formatMessage(chartMessages.trafficSources)}
+          />
+          <DraggableElement
+            id="e2e-draggable-users-by-gender-widget"
+            component={
+              <AnalyticsChartWidget
+                chartType="GenderChart"
+                title={formatMessage(chartMessages.usersByGender)}
+                projectId={undefined}
+                startAt={undefined}
+                endAt={chartEndDate}
+              />
+            }
+            icon="chart-bar"
+            label={formatMessage(chartMessages.usersByGender)}
+          />
+          <DraggableElement
+            id="e2e-draggable-users-by-age-widget"
+            component={
+              <AnalyticsChartWidget
+                chartType="AgeChart"
+                title={formatMessage(chartMessages.usersByAge)}
+                projectId={undefined}
+                startAt={undefined}
+                endAt={chartEndDate}
+              />
+            }
+            icon="chart-bar"
+            label={formatMessage(chartMessages.usersByAge)}
+          />
+          {
+            // TODO: CL-2307 Only show this if there are surveys in the platform
+            // TODO: Add in the default project / phase
           }
-          icon="chart-bar"
-          label={formatMessage(chartMessages.trafficSources)}
-        />
-        <DraggableElement
-          id="e2e-draggable-users-by-gender-widget"
-          component={
-            <AnalyticsChartWidget
-              chartType="GenderChart"
-              title={formatMessage(chartMessages.usersByGender)}
-              projectId={undefined}
-              startAt={undefined}
-              endAt={chartEndDate}
-            />
-          }
-          icon="chart-bar"
-          label={formatMessage(chartMessages.usersByGender)}
-        />
-        <DraggableElement
-          id="e2e-draggable-users-by-age-widget"
-          component={
-            <AnalyticsChartWidget
-              chartType="AgeChart"
-              title={formatMessage(chartMessages.usersByAge)}
-              projectId={undefined}
-              startAt={undefined}
-              endAt={chartEndDate}
-            />
-          }
-          icon="chart-bar"
-          label={formatMessage(chartMessages.usersByAge)}
-        />
-        {
-          // TODO: CL-2307 Only show this if there are surveys in the platform
-          // TODO: Add in the default project / phase
-        }
-        <DraggableElement
-          id="e2e-draggable-survey-results-widget"
-          component={
-            <SurveyResultsWidget
-              title={formatMessage(surveyResultMessages.surveyResults)}
-              projectId=""
-              phaseId={undefined}
-            />
-          }
-          icon="survey"
-          label={formatMessage(surveyResultMessages.surveyResults)}
-        />
-      </Accordion>
+          <DraggableElement
+            id="e2e-draggable-survey-results-widget"
+            component={
+              <SurveyResultsWidget
+                title={formatMessage(surveyResultMessages.surveyResults)}
+                projectId=""
+                phaseId={undefined}
+              />
+            }
+            icon="survey"
+            label={formatMessage(surveyResultMessages.surveyResults)}
+          />
+        </Accordion>
+      </Box>
     </Container>
   );
 };
