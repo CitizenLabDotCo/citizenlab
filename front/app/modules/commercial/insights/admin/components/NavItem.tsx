0d479978dfd6f3f0cec1443bf986d2a771333de5
Iva <iva@citizenlab.co>
2023-01-04 14:46:15 +0200

    [CL-2353] Fix errors

--- front/app/modules/commercial/insights/admin/components/NavItem.tsx
+++ front/app/modules/commercial/insights/admin/components/NavItem.tsx
@@ -1,29 +1,24 @@
 import { NavItem } from 'containers/Admin/sideBar';
 import { FC, useEffect } from 'react';
 import { InsertConfigurationOptions } from 'typings';
-import useFeatureFlag from 'hooks/useFeatureFlag';
 
 type Props = {
   onData: (data: InsertConfigurationOptions<NavItem>) => void;
 };
 
 const NavItemComponent: FC<Props> = ({ onData }) => {
-  const insightsManualFlow = useFeatureFlag({ name: 'insights_manual_flow' });
-
   useEffect(() => {
     onData({
       configuration: {
         name: 'insights',
-        link: `/admin/insights${insightsManualFlow ? '' : '/reports'}`,
+        link: `/admin/insights'/reports`,
         iconName: 'sidebar-reporting',
         message: 'insights',
-        featureNames: insightsManualFlow
-          ? ['insights_manual_flow']
-          : ['project_reports'],
+        featureNames: ['project_reports'],
       },
       insertAfterName: 'projects',
     });
-  }, [onData, insightsManualFlow]);
+  }, [onData]);
   return null;
 };
 
