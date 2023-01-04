ccf10525742f6fd70562aaa4b1ed1d4c92265458
Adrien Dessy <adrien@citizenlab.co>
2023-01-04 12:06:30 +0100

    [CL-2361] Remove CustomTopics::FeatureSpecification

--- back/engines/commercial/custom_topics/lib/custom_topics/engine.rb
+++ back/engines/commercial/custom_topics/lib/custom_topics/engine.rb
@@ -14,10 +14,5 @@ module CustomTopics
     # Sharing the factories to make them accessible to the main app / other engines.
     factories_path = File.expand_path('../../spec/factories', __dir__)
     config.factory_bot.definition_file_paths += [factories_path] if defined?(FactoryBotRails)
-
-    config.to_prepare do
-      require 'custom_topics/feature_specification'
-      AppConfiguration::Settings.add_feature(CustomTopics::FeatureSpecification)
-    end
   end
 end
