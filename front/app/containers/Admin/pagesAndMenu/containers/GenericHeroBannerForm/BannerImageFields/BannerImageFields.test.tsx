2fa14321d44add7fe13c64f04cdc63bc55a39ff6
brentguf <brentguf@users.noreply.github.com>
2023-01-04 15:43:42 +0100

    Restructure BannerImageFields tests

--- front/app/containers/Admin/pagesAndMenu/containers/GenericHeroBannerForm/BannerImageFields/BannerImageFields.test.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/GenericHeroBannerForm/BannerImageFields/BannerImageFields.test.tsx
@@ -19,19 +19,91 @@ const props = {
   onOverlayChange: jest.fn(),
 };
 
-describe('BannerImageFields', () => {
-  describe('Layout preview selector', () => {
-    it('shows when there is a saved image', async () => {
-      render(<BannerImageFields {...props} />);
+describe('Layout preview selector', () => {
+  it('does not show when there is no image', async () => {
+    render(<BannerImageFields {...props} headerBg={null} />);
 
-      await waitFor(() => {
-        const select = screen.getByRole('combobox', {
-          name: /Show preview for/,
-        });
-        expect(select).toBeInTheDocument();
+    await waitFor(() => {
+      const select = screen.queryByRole('combobox', {
+        name: /Show preview for/,
+      });
+      expect(select).not.toBeInTheDocument();
+    });
+  });
+
+  it('shows when there is a saved image', async () => {
+    render(<BannerImageFields {...props} />);
+
+    await waitFor(() => {
+      const select = screen.getByRole('combobox', {
+        name: /Show preview for/,
+      });
+      expect(select).toBeInTheDocument();
+    });
+  });
+
+  it('shows when there is an unsaved image', async () => {
+    const { container } = render(
+      <BannerImageFields {...props} headerBg={null} />
+    );
+
+    // select local image
+    const file = new File(['file'], 'file.png', {
+      type: 'image/png',
+    });
+    act(() => {
+      fireEvent.change(container.querySelector('#header-dropzone'), {
+        target: { files: [file] },
       });
     });
 
+    await waitFor(() => {
+      const select = screen.getByRole('combobox', {
+        name: /Show preview for/,
+      });
+      expect(select).toBeInTheDocument();
+    });
+  });
+
+  describe('when layout is fixed-ratio layout', () => {
+    it('does not show when there is a saved image', async () => {
+      render(
+        <BannerImageFields {...props} bannerLayout="fixed_ratio_layout" />
+      );
+
+      await waitFor(() => {
+        const select = screen.queryByRole('combobox', {
+          name: /Show preview for/,
+        });
+        expect(select).toBeNull();
+      });
+    });
+
+    it('does not show when there is an unsaved image', async () => {
+      render(
+        <BannerImageFields {...props} bannerLayout="fixed_ratio_layout" />
+      );
+
+      await waitFor(() => {
+        const select = screen.queryByRole('combobox', {
+          name: /Show preview for/,
+        });
+        expect(select).toBeNull();
+      });
+    });
+  });
+});
+
+describe('Image overlay controls', () => {
+  it('does not show if there is no image', async () => {
+    render(<BannerImageFields {...props} headerBg={null} />);
+
+    await waitFor(() => {
+      const overlayInput = screen.queryByLabelText('Image overlay color');
+      expect(overlayInput).toBeNull();
+    });
+  });
+  describe('when layout is full-width banner layout', () => {
     it('shows when there is an unsaved image', async () => {
       const { container } = render(
         <BannerImageFields {...props} headerBg={null} />
@@ -48,55 +120,12 @@ describe('BannerImageFields', () => {
       });
 
       await waitFor(() => {
-        const select = screen.getByRole('combobox', {
-          name: /Show preview for/,
-        });
-        expect(select).toBeInTheDocument();
+        const overlayInput = screen.getByLabelText('Image overlay color');
+        expect(overlayInput).toBeInTheDocument();
       });
     });
 
-    it('does not show when there is no unsaved nor saved image', async () => {
-      render(<BannerImageFields {...props} headerBg={null} />);
-
-      await waitFor(() => {
-        const select = screen.queryByRole('combobox', {
-          name: /Show preview for/,
-        });
-        expect(select).not.toBeInTheDocument();
-      });
-    });
-
-    describe('Fixed-ratio banner layout', () => {
-      it('does not show when there is a saved image', async () => {
-        render(
-          <BannerImageFields {...props} bannerLayout="fixed_ratio_layout" />
-        );
-
-        await waitFor(() => {
-          const select = screen.queryByRole('combobox', {
-            name: /Show preview for/,
-          });
-          expect(select).toBeNull();
-        });
-      });
-
-      it('does not show when there is an unsaved image', async () => {
-        render(
-          <BannerImageFields {...props} bannerLayout="fixed_ratio_layout" />
-        );
-
-        await waitFor(() => {
-          const select = screen.queryByRole('combobox', {
-            name: /Show preview for/,
-          });
-          expect(select).toBeNull();
-        });
-      });
-    });
-  });
-
-  describe('Image overlay controls', () => {
-    it('shows overlay controls correctly', async () => {
+    it('shows when there is a saved image', async () => {
       render(<BannerImageFields {...props} />);
 
       await waitFor(() => {
@@ -104,157 +133,210 @@ describe('BannerImageFields', () => {
         expect(overlayInput).toBeInTheDocument();
       });
     });
+  });
 
-    it('does not show if we have no saved image yet', async () => {
-      render(<BannerImageFields {...props} headerBg={null} />);
+  describe('when layout is two-row layout', () => {
+    const bannerLayout = 'two_row_layout';
+
+    it('does not show when there is an unsaved image', async () => {
+      const { container } = render(
+        <BannerImageFields
+          {...props}
+          bannerLayout={bannerLayout}
+          headerBg={null}
+        />
+      );
+
+      // select local image
+      const file = new File(['file'], 'file.png', {
+        type: 'image/png',
+      });
+      act(() => {
+        fireEvent.change(container.querySelector('#header-dropzone'), {
+          target: { files: [file] },
+        });
+      });
 
       await waitFor(() => {
         const overlayInput = screen.queryByLabelText('Image overlay color');
+        expect(overlayInput).not.toBeInTheDocument();
+      });
+    });
+
+    it('does not show when there is a saved image', async () => {
+      render(<BannerImageFields {...props} bannerLayout={bannerLayout} />);
+
+      await waitFor(() => {
+        const overlayInput = screen.queryByLabelText('Image overlay color');
+        expect(overlayInput).not.toBeInTheDocument();
+      });
+    });
+  });
+
+  describe('when layout is two-column layout', () => {
+    const bannerLayout = 'two_column_layout';
+
+    it('does not show when there is an unsaved image', async () => {
+      const { container } = render(
+        <BannerImageFields
+          {...props}
+          bannerLayout={bannerLayout}
+          headerBg={null}
+        />
+      );
+
+      // select local image
+      const file = new File(['file'], 'file.png', {
+        type: 'image/png',
+      });
+      act(() => {
+        fireEvent.change(container.querySelector('#header-dropzone'), {
+          target: { files: [file] },
+        });
+      });
+
+      await waitFor(() => {
+        const overlayInput = screen.queryByLabelText('Image overlay color');
+        expect(overlayInput).not.toBeInTheDocument();
+      });
+    });
+
+    it('does not show when there is a saved image', async () => {
+      render(<BannerImageFields {...props} bannerLayout={bannerLayout} />);
+
+      await waitFor(() => {
+        const overlayInput = screen.queryByLabelText('Image overlay color');
+        expect(overlayInput).not.toBeInTheDocument();
+      });
+    });
+  });
+
+  describe('when layout is fixed-ratio layout', () => {
+    const bannerLayout = 'fixed_ratio_layout';
+
+    it('does not show when there is an unsaved image', async () => {
+      const { container } = render(
+        <BannerImageFields
+          {...props}
+          headerBg={null}
+          bannerLayout={bannerLayout}
+        />
+      );
+
+      const file = new File(['file'], 'file.png', {
+        type: 'image/png',
+      });
+
+      act(() => {
+        fireEvent.change(container.querySelector('#header-dropzone'), {
+          target: { files: [file] },
+        });
+      });
+
+      await waitFor(() => {
+        const overlayInput = container.querySelector('Image overlay color');
         expect(overlayInput).toBeNull();
       });
     });
-  });
 
-  describe('Two-column layout', () => {
-    const bannerLayout = 'two_column_layout';
+    it('shows when there is a saved image', async () => {
+      render(<BannerImageFields {...props} bannerLayout={bannerLayout} />);
 
-    describe('Image overlay controls', () => {
-      it('does not show', async () => {
-        render(<BannerImageFields {...props} bannerLayout={bannerLayout} />);
-
-        await waitFor(() => {
-          const overlayInput = screen.queryByLabelText('Image overlay color');
-          expect(overlayInput).not.toBeInTheDocument();
-        });
+      await waitFor(() => {
+        const overlayInput = screen.getByLabelText('Image overlay color');
+        expect(overlayInput).toBeInTheDocument();
       });
     });
   });
+});
 
-  describe('Two-row layout', () => {
-    const bannerLayout = 'two_row_layout';
+describe('Image cropper', () => {
+  it('does not show if there is no image', async () => {
+    const { container } = render(
+      <BannerImageFields {...props} headerBg={null} />
+    );
 
-    describe('Image overlay controls', () => {
-      it('does not show', async () => {
-        render(<BannerImageFields {...props} bannerLayout={bannerLayout} />);
+    await waitFor(() => {
+      const cropContainer = container.querySelector('.reactEasyCrop_Container');
 
-        await waitFor(() => {
-          const overlayInput = screen.queryByLabelText('Image overlay color');
-          expect(overlayInput).not.toBeInTheDocument();
-        });
-      });
+      expect(cropContainer).not.toBeInTheDocument();
     });
   });
 
-  describe('Fixed-ratio layout', () => {
+  it('does not show with a saved image', async () => {
+    const { container } = render(
+      <BannerImageFields {...props} headerBg={null} />
+    );
+
+    await waitFor(() => {
+      const cropContainer = container.querySelector('.reactEasyCrop_Container');
+
+      expect(cropContainer).not.toBeInTheDocument();
+    });
+  });
+
+  it('does not show with an unsaved image', async () => {
+    const { container } = render(
+      <BannerImageFields {...props} headerBg={null} />
+    );
+
+    // select local image
+    const file = new File(['file'], 'file.png', {
+      type: 'image/png',
+    });
+    act(() => {
+      fireEvent.change(container.querySelector('#header-dropzone'), {
+        target: { files: [file] },
+      });
+    });
+
+    await waitFor(() => {
+      const cropContainer = container.querySelector('.reactEasyCrop_Container');
+
+      expect(cropContainer).not.toBeInTheDocument();
+    });
+  });
+
+  describe('when layout is fixed-ratio layout', () => {
     const bannerLayout = 'fixed_ratio_layout';
 
-    describe('Image overlay controls', () => {
-      it('shows overlay controls when we have a saved picture', async () => {
-        render(<BannerImageFields {...props} bannerLayout={bannerLayout} />);
+    it('does not show when there is a saved image', async () => {
+      const { container } = render(
+        <BannerImageFields {...props} bannerLayout={bannerLayout} />
+      );
 
-        await waitFor(() => {
-          const overlayInput = screen.queryByLabelText('Image overlay color');
-          expect(overlayInput).toBeInTheDocument();
-        });
-      });
-
-      it('does not show when there is no saved image yet', async () => {
-        render(
-          <BannerImageFields
-            {...props}
-            headerBg={null}
-            bannerLayout={bannerLayout}
-          />
+      await waitFor(() => {
+        const cropContainer = container.querySelector(
+          '.reactEasyCrop_Container'
         );
-        await waitFor(() => {
-          const overlayInput = screen.queryByLabelText('Image overlay color');
-          expect(overlayInput).toBeNull();
-        });
-      });
-
-      it('does not show when we have selected a picture that is not saved yet', async () => {
-        const { container } = render(
-          <BannerImageFields
-            {...props}
-            headerBg={null}
-            bannerLayout={bannerLayout}
-          />
-        );
-
-        const file = new File(['file'], 'file.png', {
-          type: 'image/png',
-        });
-
-        act(() => {
-          fireEvent.change(container.querySelector('#header-dropzone'), {
-            target: { files: [file] },
-          });
-        });
-
-        await waitFor(() => {
-          const overlayInput = container.querySelector('Image overlay color');
-          expect(overlayInput).toBeNull();
-        });
+        expect(cropContainer).not.toBeInTheDocument();
       });
     });
 
-    describe('Image cropper', () => {
-      it('does not show when there is no image', async () => {
-        const { container } = render(
-          <BannerImageFields
-            {...props}
-            bannerLayout={bannerLayout}
-            headerBg={null}
-          />
-        );
+    it('shows when there is an unsaved image', async () => {
+      const { container } = render(
+        <BannerImageFields
+          {...props}
+          bannerLayout={bannerLayout}
+          headerBg={null}
+        />
+      );
 
-        await waitFor(() => {
-          const cropContainer = container.querySelector(
-            '.reactEasyCrop_Container'
-          );
+      const file = new File(['file'], 'file.png', {
+        type: 'image/png',
+      });
 
-          expect(cropContainer).not.toBeInTheDocument();
+      act(() => {
+        fireEvent.change(container.querySelector('#header-dropzone'), {
+          target: { files: [file] },
         });
       });
 
-      it('does not show when image is saved', async () => {
-        const { container } = render(
-          <BannerImageFields {...props} bannerLayout={bannerLayout} />
+      await waitFor(() => {
+        const cropContainer = container.querySelector(
+          '.reactEasyCrop_Container'
         );
-
-        await waitFor(() => {
-          const cropContainer = container.querySelector(
-            '.reactEasyCrop_Container'
-          );
-          expect(cropContainer).not.toBeInTheDocument();
-        });
-      });
-
-      it('shows when image is uploaded but not saved', async () => {
-        const { container } = render(
-          <BannerImageFields
-            {...props}
-            bannerLayout={bannerLayout}
-            headerBg={null}
-          />
-        );
-
-        const file = new File(['file'], 'file.png', {
-          type: 'image/png',
-        });
-
-        act(() => {
-          fireEvent.change(container.querySelector('#header-dropzone'), {
-            target: { files: [file] },
-          });
-        });
-
-        await waitFor(() => {
-          const cropContainer = container.querySelector(
-            '.reactEasyCrop_Container'
-          );
-          expect(cropContainer).toBeInTheDocument();
-        });
+        expect(cropContainer).toBeInTheDocument();
       });
     });
   });
