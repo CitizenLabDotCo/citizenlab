a2c6146b6bbf26602df11051b5fb4047d9f4b149
Adrien Dessy <adrien@citizenlab.co>
2023-01-04 11:06:32 +0100

    [CL-2375] Fix Rubocop offenses

--- back/app/serializers/web_api/v1/project_serializer.rb
+++ back/app/serializers/web_api/v1/project_serializer.rb
@@ -14,7 +14,7 @@ class WebApi::V1::ProjectSerializer < WebApi::V1::BaseSerializer
     :slug,
     :visible_to,
     :created_at,
-    :updated_at,
+    :updated_at
   )
 
   attribute :folder_id do |project|
