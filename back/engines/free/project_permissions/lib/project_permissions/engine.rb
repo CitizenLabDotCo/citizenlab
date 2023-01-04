db5851b88254630dc71cb0a5c76b1f5ceabdea29
Adrien Dessy <adrien@citizenlab.co>
2023-01-04 10:47:53 +0100

    [CL-2375] Remove ProjectPermissions::FeatureSpecification

--- back/engines/free/project_permissions/lib/project_permissions/engine.rb
+++ back/engines/free/project_permissions/lib/project_permissions/engine.rb
@@ -15,14 +15,5 @@ module ProjectPermissions
     # Sharing the factories to make them accessible to the main app / other engines.
     factories_path = File.expand_path('../../spec/factories', __dir__)
     config.factory_bot.definition_file_paths += [factories_path] if defined?(FactoryBotRails)
-
-    def self.register_features
-      require 'project_permissions/feature_specification'
-      AppConfiguration::Settings.add_feature(ProjectPermissions::FeatureSpecification)
-    end
-
-    config.to_prepare do
-      ProjectPermissions::Engine.register_features
-    end
   end
 end
