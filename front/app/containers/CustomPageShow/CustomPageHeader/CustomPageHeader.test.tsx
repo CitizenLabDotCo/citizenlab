e6ad81dfae01fed5a124125817bcc0dc8a7f3769
Alexander <alexander@citizenlab.co>
2023-01-04 14:18:38 +0100

    [CL-2216] Add more tests

--- front/app/containers/CustomPageShow/CustomPageHeader/CustomPageHeader.test.tsx
+++ front/app/containers/CustomPageShow/CustomPageHeader/CustomPageHeader.test.tsx
@@ -1,44 +1,57 @@
 import React from 'react';
 import { ICustomPageData } from 'services/customPages';
+import { THomepageBannerLayout } from 'services/homepageSettings';
 import { render, screen } from 'utils/testUtils/rtl';
 import CustomPageHeader from '.';
 
 jest.mock('services/locale');
 jest.mock('services/appConfiguration');
 
-const mockPageData = {
-  id: '1',
-  attributes: {
-    banner_layout: 'fixed_ratio_layout',
-    banner_header_multiloc: { en: 'Header' },
-    banner_subheader_multiloc: { en: 'Subheader' },
-    header_bg: { large: 'https://example.com/image.png' },
-  },
-} as unknown as ICustomPageData;
+const headerBgUrl = 'https://example.com/image.png';
+
+function mockPageData(bannerLayout: THomepageBannerLayout) {
+  return {
+    id: '1',
+    attributes: {
+      banner_layout: bannerLayout,
+      banner_header_multiloc: { en: 'Header' },
+      banner_subheader_multiloc: { en: 'Subheader' },
+      header_bg: { large: headerBgUrl },
+    },
+  } as unknown as ICustomPageData;
+}
 
 describe('<CustomPageHeader />', () => {
-  it('renders with pageData', () => {
-    render(<CustomPageHeader pageData={mockPageData} />);
-    expect(screen.getByText('Header')).toBeInTheDocument();
-    expect(screen.getByText('Subheader')).toBeInTheDocument();
-    expect(screen.getByTestId('header-image-background')).toHaveStyle(
-      `background-image: url(${mockPageData.attributes.header_bg?.large})`
+  it('renders full width banner layout', () => {
+    render(
+      <CustomPageHeader pageData={mockPageData('full_width_banner_layout')} />
     );
-    expect(screen.getByTestId('fixed-ratio-layout')).toBeInTheDocument();
-  });
-
-  it('renders with pageData for full_width_banner_layout', () => {
-    const mockPageData2 = {
-      id: '1',
-      attributes: {
-        ...mockPageData.attributes,
-        banner_layout: 'full_width_banner_layout',
-      },
-    } as unknown as ICustomPageData;
-
-    render(<CustomPageHeader pageData={mockPageData2} />);
     expect(screen.getByText('Header')).toBeInTheDocument();
     expect(screen.getByText('Subheader')).toBeInTheDocument();
     expect(screen.getByTestId('full-width-banner-layout')).toBeInTheDocument();
+  });
+
+  it('renders two column layout', () => {
+    render(<CustomPageHeader pageData={mockPageData('two_column_layout')} />);
+    expect(screen.getByText('Header')).toBeInTheDocument();
+    expect(screen.getByText('Subheader')).toBeInTheDocument();
+    expect(screen.getByTestId('two-column-layout')).toBeInTheDocument();
+  });
+
+  it('renders two row layout fixed ratio layout', () => {
+    render(<CustomPageHeader pageData={mockPageData('two_row_layout')} />);
+    expect(screen.getByText('Header')).toBeInTheDocument();
+    expect(screen.getByText('Subheader')).toBeInTheDocument();
+    expect(screen.getByTestId('two-row-layout')).toBeInTheDocument();
+  });
+
+  it('renders fixed ratio layout', () => {
+    render(<CustomPageHeader pageData={mockPageData('fixed_ratio_layout')} />);
+    expect(screen.getByText('Header')).toBeInTheDocument();
+    expect(screen.getByText('Subheader')).toBeInTheDocument();
+    expect(screen.getByTestId('header-image-background')).toHaveStyle(
+      `background-image: url(${headerBgUrl})`
+    );
+    expect(screen.getByTestId('fixed-ratio-layout')).toBeInTheDocument();
   });
 });
