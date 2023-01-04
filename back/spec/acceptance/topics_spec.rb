2214a12828491da8fbe65a2caac17da4652123ac
Adrien Dessy <adrien@citizenlab.co>
2023-01-04 15:56:08 +0100

    [CL-2361] Migrate custom-topics specs to the core

--- back/spec/acceptance/topics_spec.rb
+++ back/spec/acceptance/topics_spec.rb
@@ -6,12 +6,7 @@ require_relative './shared/publication_filtering_model'
 
 resource 'Topics' do
   explanation 'E.g. mobility, health, culture...'
-
-  before do
-    header 'Content-Type', 'application/json'
-    @code1, @code2 = Topic::CODES.take(2)
-    @topics = create_list(:topic, 2, code: @code1) + create_list(:topic, 3, code: @code2)
-  end
+  header 'Content-Type', 'application/json'
 
   get 'web_api/v1/topics' do
     with_options scope: :page do
@@ -19,6 +14,11 @@ resource 'Topics' do
       parameter :size, 'Number of topics per page'
     end
     parameter :code, 'Filter by code', required: false
+
+    before do
+      @code1, @code2 = Topic::CODES.take(2)
+      @topics = create_list(:topic, 2, code: @code1) + create_list(:topic, 3, code: @code2)
+    end
 
     example_request 'List all topics' do
       expect(status).to eq(200)
@@ -68,12 +68,114 @@ resource 'Topics' do
   end
 
   get 'web_api/v1/topics/:id' do
-    let(:id) { @topics.first.id }
+    let(:topic) { create(:topic) }
+    let(:id) { topic.id }
 
     example_request 'Get one topic by id' do
-      expect(status).to eq 200
-      json_response = json_parse(response_body)
-      expect(json_response.dig(:data, :id)).to eq @topics.first.id
+      assert_status(200)
+      expect(response_data[:id]).to eq(id)
+    end
+  end
+
+  context 'when admin' do
+    before do
+      @admin = create(:admin)
+      token = Knock::AuthToken.new(payload: @admin.to_token_payload).token
+      header 'Authorization', "Bearer #{token}"
+    end
+
+    post 'web_api/v1/topics' do
+      with_options scope: :topic do
+        parameter :title_multiloc, 'The title of the topic, as a multiloc string', required: true
+        parameter :description_multiloc, 'The description of the topic, as a multiloc string', required: false
+      end
+      ValidationErrorHelper.new.error_fields(self, Topic)
+
+      let(:topic) { build(:topic) }
+      let(:title_multiloc) { topic.title_multiloc }
+
+      example_request 'Create a topic' do
+        expect(response_status).to eq 201
+        json_response = json_parse(response_body)
+        expect(json_response.dig(:data, :attributes, :title_multiloc).stringify_keys).to match title_multiloc
+      end
+    end
+
+    patch 'web_api/v1/topics/:id' do
+      with_options scope: :topic do
+        parameter :title_multiloc, 'The title of the topic, as a multiloc string'
+        parameter :description_multiloc, 'The description of the topic, as a multiloc string'
+      end
+      ValidationErrorHelper.new.error_fields(self, Topic)
+
+      let(:topic) { create(:custom_topic) }
+      let(:id) { topic.id }
+      let(:title_multiloc) { { 'en' => 'Comedy' } }
+      let(:description_multiloc) { { 'en' => 'Stuff that tends to make you laugh' } }
+
+      example_request 'Update a topic' do
+        expect(response_status).to eq 200
+        json_response = json_parse(response_body)
+        expect(json_response.dig(:data, :attributes, :title_multiloc).stringify_keys).to match title_multiloc
+        expect(json_response.dig(:data, :attributes, :description_multiloc).stringify_keys).to match description_multiloc
+      end
+
+      context do
+        let(:topic) { create(:custom_topic, code: 'mobility', title_multiloc: { 'en' => 'Drama' }) }
+        let(:id) { topic.id }
+
+        example_request 'Rename a default topic does not work', example: false do
+          expect(response_status).to eq 200
+          json_response = json_parse(response_body)
+          expect(json_response.dig(:data, :attributes, :title_multiloc).stringify_keys).to match({ 'en' => 'Drama' })
+        end
+      end
+    end
+
+    patch 'web_api/v1/topics/:id/reorder' do
+      with_options scope: :topic do
+        parameter :ordering, 'The position, starting from 0, where the field should be at. Fields after will move down.', required: true
+      end
+
+      let(:id) { create(:topic).id }
+      let(:ordering) { 1 }
+
+      example_request 'Reorder a topic globally' do
+        expect(response_status).to eq 200
+        json_response = json_parse(response_body)
+        expect(json_response.dig(:data, :attributes, :ordering)).to eq ordering
+      end
+
+      context do
+        let(:topic) { create(:topic, code: 'mobility') }
+        let(:id) { topic.id }
+
+        example_request 'Reorder a default topic' do
+          expect(response_status).to eq 200
+        end
+      end
+    end
+
+    delete 'web_api/v1/topics/:id' do
+      let(:topic) { create(:custom_topic) }
+      let!(:id) { topic.id }
+
+      example 'Delete a topic' do
+        old_count = Topic.count
+        do_request
+        expect(response_status).to eq 200
+        expect { Topic.find(id) }.to raise_error(ActiveRecord::RecordNotFound)
+        expect(Topic.count).to eq(old_count - 1)
+      end
+
+      context do
+        let(:topic) { create(:topic, code: 'mobility') }
+        let(:id) { topic.id }
+
+        example_request '[error] Delete a default topic' do
+          expect(response_status).to eq 401
+        end
+      end
     end
   end
 end
