272b01119f52f4f15082bff74a54adcb61c00d11
Iva <iva@citizenlab.co>
2023-01-05 14:11:41 +0200

    Change name and add test

--- front/app/utils/cl-router/useUrlQuery.ts
+++ front/app/utils/cl-router/useUrlQuery.ts
@@ -1,8 +1,7 @@
 import { useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 
-export default function useQuery() {
+export default function useURLQuery() {
   const { search } = useLocation();
-
   return useMemo(() => new URLSearchParams(search), [search]);
 }
