6b8143c0cd170ae702b782c3d942fa0bdb7ad074
Alexander <alexander@citizenlab.co>
2023-01-09 17:42:19 +0100

    [CL-2525] Fix tests

--- front/app/components/UI/RemoveImageButton/RemoveImageButton.test.tsx
+++ front/app/components/UI/RemoveImageButton/RemoveImageButton.test.tsx
@@ -5,7 +5,7 @@ import RemoveImageButton from './';
 describe('RemoveImageButton', () => {
   it('renders button with default text for screen reader', () => {
     render(<RemoveImageButton onClick={jest.fn()} />);
-    expect(screen.getByRole('button')).toHaveTextContent('Remove image');
+    expect(screen.getByRole('button')).toHaveTextContent('Remove');
   });
 
   it('renders button with specified text for screen reader', () => {
