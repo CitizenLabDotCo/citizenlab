CitizenLabDotCo/citizenlab

commit c79e606fa6590d9c8bb1689acbfb28e7d44cd159

[CL-2216] Add more tests for HomePage/SignedOutHeader

front/app/containers/HomePage/SignedOutHeader/TwoRowLayout.tsx
@@ -15,7 +15,7 @@ const TwoRowLayout = ({ homepageSettings }: Props) => {
   const headerImage = homepageSettings.attributes.header_bg?.large;
 
   return (
-    <>
+    <div data-testid="two-row-layout">
       {headerImage && (
         <HeaderImage
           src={headerImage}
@@ -31,7 +31,7 @@ const TwoRowLayout = ({ homepageSettings }: Props) => {
           <HeaderContent fontColors="dark" />
         </Container>
       </ContentContainer>
-    </>
+    </div>
   );
 };
 

front/app/containers/CustomPageShow/CustomPageHeader/CustomPageHeader.test.tsx
@@ -22,7 +22,7 @@ function mockPageData(bannerLayout: THomepageBannerLayout) {
 }
 
 describe('<CustomPageHeader />', () => {
-  it('renders full width banner layout', () => {
+  it('renders full_width_banner_layout', () => {
     render(
       <CustomPageHeader pageData={mockPageData('full_width_banner_layout')} />
     );
@@ -31,21 +31,21 @@ describe('<CustomPageHeader />', () => {
     expect(screen.getByTestId('full-width-banner-layout')).toBeInTheDocument();
   });
 
-  it('renders two column layout', () => {
+  it('renders two_column_layout', () => {
     render(<CustomPageHeader pageData={mockPageData('two_column_layout')} />);
     expect(screen.getByText('Header')).toBeInTheDocument();
     expect(screen.getByText('Subheader')).toBeInTheDocument();
     expect(screen.getByTestId('two-column-layout')).toBeInTheDocument();
   });
 
-  it('renders two row layout fixed ratio layout', () => {
+  it('renders two_row_layout fixed ratio layout', () => {
     render(<CustomPageHeader pageData={mockPageData('two_row_layout')} />);
     expect(screen.getByText('Header')).toBeInTheDocument();
     expect(screen.getByText('Subheader')).toBeInTheDocument();
     expect(screen.getByTestId('two-row-layout')).toBeInTheDocument();
   });
 
-  it('renders fixed ratio layout', () => {
+  it('renders fixed_ratio_layout', () => {
     render(<CustomPageHeader pageData={mockPageData('fixed_ratio_layout')} />);
     expect(screen.getByText('Header')).toBeInTheDocument();
     expect(screen.getByText('Subheader')).toBeInTheDocument();

front/app/containers/HomePage/SignedOutHeader/SignedOutHeader.test.tsx
@@ -21,7 +21,36 @@ jest.mock('hooks/useHomepageSettings', () => {
 });
 
 describe('<SignedOutHeader />', () => {
-  it('renders with HomepageSettings for logged out users', () => {
+  it('renders full_width_banner_layout', () => {
+    mockHomepageSettings.attributes.banner_layout = 'full_width_banner_layout';
+
+    render(<SignedOutHeader />);
+    expect(screen.getByText('Signed out header')).toBeInTheDocument();
+    expect(screen.getByText('Signed out subhead')).toBeInTheDocument();
+    expect(screen.getByTestId('full-width-banner-layout')).toBeInTheDocument();
+  });
+
+  it('renders two_column_layout', () => {
+    mockHomepageSettings.attributes.banner_layout = 'two_column_layout';
+
+    render(<SignedOutHeader />);
+    expect(screen.getByText('Signed out header')).toBeInTheDocument();
+    expect(screen.getByText('Signed out subhead')).toBeInTheDocument();
+    expect(screen.getByTestId('two-column-layout')).toBeInTheDocument();
+  });
+
+  it('renders two_row_layout', () => {
+    mockHomepageSettings.attributes.banner_layout = 'two_row_layout';
+
+    render(<SignedOutHeader />);
+    expect(screen.getByText('Signed out header')).toBeInTheDocument();
+    expect(screen.getByText('Signed out subhead')).toBeInTheDocument();
+    expect(screen.getByTestId('two-row-layout')).toBeInTheDocument();
+  });
+
+  it('renders fixed_ratio_layout', () => {
+    mockHomepageSettings.attributes.banner_layout = 'fixed_ratio_layout';
+
     render(<SignedOutHeader />);
     expect(screen.getByText('Signed out header')).toBeInTheDocument();
     expect(screen.getByText('Signed out subhead')).toBeInTheDocument();
@@ -29,14 +58,5 @@ describe('<SignedOutHeader />', () => {
       `background-image: url(${mockHomepageSettings.attributes.header_bg?.large})`
     );
     expect(screen.getByTestId('fixed-ratio-layout')).toBeInTheDocument();
-  });
-
-  it('renders with HomepageSettings for full_width_banner_layout', () => {
-    mockHomepageSettings.attributes.banner_layout = 'full_width_banner_layout';
-
-    render(<SignedOutHeader />);
-    expect(screen.getByText('Signed out header')).toBeInTheDocument();
-    expect(screen.getByText('Signed out subhead')).toBeInTheDocument();
-    expect(screen.getByTestId('full-width-banner-layout')).toBeInTheDocument();
   });
 });

front/app/containers/HomePage/SignedOutHeader/TwoColumnLayout.tsx
@@ -14,7 +14,10 @@ const TwoColumnLayout = ({ homepageSettings }: Props) => {
   const headerImage = homepageSettings.attributes.header_bg?.large;
 
   return (
-    <Container data-cy="e2e-two-column-layout-container">
+    <Container
+      data-testid="two-column-layout"
+      data-cy="e2e-two-column-layout-container"
+    >
       {headerImage && (
         <HeaderImage
           src={headerImage}

commit e84d51421c47303ee6ce221b1a487ea9f7edaf60

[CL-2551] Display two-row banner in full width
It was broken in c79e606

front/app/containers/HomePage/SignedOutHeader/TwoRowLayout.tsx
@@ -6,16 +6,21 @@ import {
 import HeaderContent from 'containers/HomePage/SignedOutHeader/HeaderContent';
 import React from 'react';
 import { IHomepageSettingsData } from 'services/homepageSettings';
+import styled from 'styled-components';
 
 interface Props {
   homepageSettings: IHomepageSettingsData;
 }
 
+const TwoRowLayoutContainer = styled.div`
+  width: 100%;
+`;
+
 const TwoRowLayout = ({ homepageSettings }: Props) => {
   const headerImage = homepageSettings.attributes.header_bg?.large;
 
   return (
-    <div data-testid="two-row-layout">
+    <TwoRowLayoutContainer data-testid="two-row-layout">
       {headerImage && (
         <HeaderImage
           src={headerImage}
@@ -31,7 +36,7 @@ const TwoRowLayout = ({ homepageSettings }: Props) => {
           <HeaderContent fontColors="dark" />
         </Container>
       </ContentContainer>
-    </div>
+    </TwoRowLayoutContainer>
   );
 };
 