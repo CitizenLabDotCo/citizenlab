7b8ddc66689101b5f92e12e825be84f782329130
Adrien Dessy <adrien@citizenlab.co>
2023-01-11 17:22:33 +0100

    [CL-2556] Remove tests from missing endpoints
    Age endpoint have not been ported to the core and will be removed from OSS version.

--- back/spec/policies/stat_user_field_policy_spec.rb
+++ back/spec/policies/stat_user_field_policy_spec.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 require 'rails_helper'
 
@@ -8,13 +7,11 @@ RSpec.describe StatUserFieldPolicy do
   context 'when the user has admin rights' do
     let_it_be(:user) { build(:admin) }
 
-    it { is_expected.to permit(:users_by_age) }
     it { is_expected.to permit(:users_by_birthyear) }
     it { is_expected.to permit(:users_by_domicile) }
     it { is_expected.to permit(:users_by_education) }
     it { is_expected.to permit(:users_by_custom_field) }
     it { is_expected.to permit(:users_by_gender_as_xlsx) }
-    it { is_expected.to permit(:users_by_age_as_xlsx) }
     it { is_expected.to permit(:users_by_birthyear_as_xlsx) }
     it { is_expected.to permit(:users_by_domicile_as_xlsx) }
     it { is_expected.to permit(:users_by_education_as_xlsx) }
@@ -24,13 +21,11 @@ RSpec.describe StatUserFieldPolicy do
   context 'when user is a normal user' do
     let_it_be(:user) { build(:user) }
 
-    it { is_expected.not_to permit(:users_by_age) }
     it { is_expected.not_to permit(:users_by_birthyear) }
     it { is_expected.not_to permit(:users_by_domicile) }
     it { is_expected.not_to permit(:users_by_education) }
     it { is_expected.not_to permit(:users_by_custom_field) }
     it { is_expected.not_to permit(:users_by_gender_as_xlsx) }
-    it { is_expected.not_to permit(:users_by_age_as_xlsx) }
     it { is_expected.not_to permit(:users_by_birthyear_as_xlsx) }
     it { is_expected.not_to permit(:users_by_domicile_as_xlsx) }
     it { is_expected.not_to permit(:users_by_education_as_xlsx) }
@@ -40,7 +35,6 @@ RSpec.describe StatUserFieldPolicy do
   context 'when user is a visitor' do
     let_it_be(:user) { nil }
 
-    it { is_expected.not_to permit(:users_by_age) }
     it { is_expected.not_to permit(:users_by_birthyear) }
     it { is_expected.not_to permit(:users_by_domicile) }
     it { is_expected.not_to permit(:users_by_education) }
