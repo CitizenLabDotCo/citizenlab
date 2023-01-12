caa99cd607136db90ee0da82d519848986c63aea
Adrien Dessy <adrien@citizenlab.co>
2023-01-12 16:36:03 +0100

    [CL-2560] Remove folders of verification engines

--- back/engines/commercial/id_id_card_lookup/db/migrate/20191210194054_create_id_cards.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class CreateIdCards < ActiveRecord::Migration[5.2]
-  def change
-    create_table :verification_id_cards, id: :uuid do |t|
-      t.string :hashed_card_id, index: true, unique: true
-    end
-  end
-end
