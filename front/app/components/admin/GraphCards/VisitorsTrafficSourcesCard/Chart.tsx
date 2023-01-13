243914f624ee099493116cc588d19b923c1ce817
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-01-13 11:24:09 +0000

    [CL-2214] Enable tooltip on traffic sources

--- front/app/components/admin/GraphCards/VisitorsTrafficSourcesCard/Chart.tsx
+++ front/app/components/admin/GraphCards/VisitorsTrafficSourcesCard/Chart.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 
 // components
-import { Box } from '@citizenlab/cl2-component-library';
 import PieChart from 'components/admin/Graphs/PieChart';
 import renderTooltip from './renderTooltip';
-import ReferrerListLink from './RefferListLink';
 
 // typings
 import { PieRow } from './useVisitorReferrerTypes/typings';
@@ -13,16 +11,10 @@ import { LegendItem } from 'components/admin/Graphs/_components/Legend/typings';
 interface Props {
   pieData: PieRow[];
   innerRef?: React.RefObject<any>;
-  onOpenModal?: () => void;
-  showReferrers?: boolean;
+  narrow?: boolean;
 }
 
-const Chart = ({
-  pieData,
-  innerRef,
-  onOpenModal = () => {},
-  showReferrers = true,
-}: Props) => {
+const Chart = ({ pieData, innerRef, narrow = false }: Props) => {
   const [hoverIndex, setHoverIndex] = useState<number | undefined>();
 
   const onMouseOver = ({ rowIndex }) => {
@@ -41,44 +33,30 @@ const Chart = ({
     })
   );
 
-  const referrersLink = showReferrers ? (
-    <Box ml="20px" mt="40px">
-      <ReferrerListLink onOpenModal={onOpenModal} />
-    </Box>
-  ) : (
-    <></>
-  );
-
   return (
-    <>
-      <Box width="100%" height="initial" display="flex" alignItems="center">
-        <PieChart
-          width={164}
-          height={164}
-          data={pieData}
-          mapping={{
-            angle: 'value',
-            name: 'name',
-            opacity: ({ rowIndex }) => {
-              if (hoverIndex === undefined) return 1;
-              return hoverIndex === rowIndex ? 1 : 0.3;
-            },
-          }}
-          tooltip={renderTooltip()}
-          legend={{
-            items: legend,
-            marginLeft: 50,
-            maintainGraphSize: true,
-            position: 'right-center',
-          }}
-          innerRef={innerRef}
-          onMouseOver={onMouseOver}
-          onMouseOut={onMouseOut}
-        />
-      </Box>
-
-      {referrersLink}
-    </>
+    <PieChart
+      width={164}
+      height={164}
+      data={pieData}
+      mapping={{
+        angle: 'value',
+        name: 'name',
+        opacity: ({ rowIndex }) => {
+          if (hoverIndex === undefined) return 1;
+          return hoverIndex === rowIndex ? 1 : 0.3;
+        },
+      }}
+      tooltip={renderTooltip()}
+      legend={{
+        items: legend,
+        marginLeft: narrow ? 10 : 50,
+        maintainGraphSize: true,
+        position: 'right-center',
+      }}
+      innerRef={innerRef}
+      onMouseOver={onMouseOver}
+      onMouseOut={onMouseOut}
+    />
   );
 };
 
