1b52ea9bf99c0c099391c1c88ee7bba5536eac27
Adrien Dessy <adrien@citizenlab.co>
2023-01-13 13:02:20 +0100

    CL-2593 Remove 'machine_translations' engines

--- back/app/models/idea.rb
+++ back/app/models/idea.rb
@@ -280,6 +280,5 @@ end
 Idea.include_if_ee 'FlagInappropriateContent::Concerns::Flaggable'
 Idea.include_if_ee 'Insights::Concerns::Input'
 Idea.include_if_ee 'Moderation::Concerns::Moderatable'
-Idea.include_if_ee 'MachineTranslations::Concerns::Translatable'
 Idea.include_if_ee 'IdeaAssignment::Extensions::Idea'
 Idea.include_if_ee 'IdeaCustomFields::Extensions::Idea'
