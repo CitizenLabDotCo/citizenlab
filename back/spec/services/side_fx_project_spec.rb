ed9f6e29a5ad3d5e064bd6f35342ff8f9e09c764
Simon Tharby <simontharby@gmail.com>
2023-01-14 20:01:20 +0000

    [CL-2542] Tests of side_fx.after_copy

--- back/spec/services/side_fx_project_spec.rb
+++ back/spec/services/side_fx_project_spec.rb
@@ -88,4 +88,92 @@ describe SideFxProjectService do
       service.after_delete_inputs project, user
     end
   end
+
+  describe 'after_copy' do
+    it 'associates correct groups with group permissions of copied continuous ideation project' do
+      groups = create_list(:group, 3)
+      source_project = create(:project, participation_method: 'ideation')
+
+      permission1 = build(
+        :permission,
+        action: 'posting_idea',
+        permission_scope_type: 'Project',
+        permission_scope_id: source_project.id,
+        permitted_by: 'groups',
+        groups: groups
+      )
+      # Skip validation to avoid Validation failed: Action has already been taken
+      permission1.save!(validate: false)
+
+      # permission2 is associated with the copied project, but has no groups associated with it,
+      # since we need side_fx.after_copy to add the necessary groups_permissions.
+      permission2 = build(
+        :permission,
+        action: 'posting_idea',
+        permission_scope_type: 'Project',
+        permission_scope_id: project.id,
+        permitted_by: 'groups'
+      )
+      # Skip validation to avoid Validation failed: Action is not included in the list
+      permission2.save!(validate: false)
+
+      service.after_copy source_project, project
+      expect(project.permissions.first.groups).to eq source_project.permissions.first.groups
+    end
+
+    it 'associates correct groups with group permissions of copied ideation phase' do
+      groups = create_list(:group, 3)
+      source_project = create(:project_with_active_ideation_phase)
+      copied_project = create(:project_with_active_ideation_phase)
+
+      permission1 = build(
+        :permission,
+        action: 'posting_idea',
+        permission_scope_type: 'Phase',
+        permission_scope_id: source_project.phases.first.id,
+        permitted_by: 'groups',
+        groups: groups
+      )
+      # Skip validation to avoid Validation failed: Action has already been taken
+      permission1.save!(validate: false)
+
+      # permission2 is associated with the copied project phase, but has no groups associated with it,
+      # since we need side_fx.after_copy to add the necessary groups_permissions.
+      permission2 = build(
+        :permission,
+        action: 'posting_idea',
+        permission_scope_type: 'Phase',
+        permission_scope_id: copied_project.phases.first.id,
+        permitted_by: 'groups'
+      )
+      # Skip validation to avoid Validation failed: Action is not included in the list
+      permission2.save!(validate: false)
+
+      # This prevents flakiness, whereby either source or copied project phase would have no groups
+      # associated with the permission evaluated in the expectation, in approx 1 out of 3 test runs.
+      source_project.phases.first.permissions = [permission1]
+      copied_project.phases.first.permissions = [permission2]
+
+      service.after_copy source_project, copied_project
+      expect(copied_project.phases.first.permissions.first.groups).to eq source_project.phases.first.permissions.first.groups
+    end
+
+    it 'associates areas of source project with copied project' do
+      area1 = create(:area)
+      area2 = create(:area)
+      source_project = build(:project, areas: [area1, area2])
+
+      service.after_copy source_project, project
+      expect(project.areas).to eq source_project.areas
+    end
+
+    it 'associates topics of source project with copied project' do
+      topic1 = create(:topic)
+      topic2 = create(:topic)
+      source_project = build(:project, topics: [topic1, topic2])
+
+      service.after_copy source_project, project
+      expect(project.topics).to eq source_project.topics
+    end
+  end
 end
