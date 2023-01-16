8943af1da6aaeb5779b052ef6b03b3bb61da1c36
Adrien Dessy <adrien@citizenlab.co>
2023-01-16 16:51:35 +0100

    CL-2593 Remove 'content_builder' engine

--- back/app/models/project.rb
+++ back/app/models/project.rb
@@ -316,4 +316,3 @@ class Project < ApplicationRecord
 end
 
 Project.include_if_ee('Insights::Patches::Project')
-Project.include_if_ee('ContentBuilder::Patches::Project')
--- back/engines/commercial/content_builder/lib/content_builder.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require 'content_builder/version'
-require 'content_builder/engine'
-
-module ContentBuilder; end
