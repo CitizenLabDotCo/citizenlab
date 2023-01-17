63060938b31f07285de9d47a2ab040a0b7c74022
Amanda Anderson <amanda.anderson@citizenlab.co>
2023-01-17 14:36:47 +0100

    [CL-2442] Add handling for delete button

--- front/app/components/FormBuilder/components/FormBuilderSettings/ContentSettings/index.tsx
+++ front/app/components/FormBuilder/components/FormBuilderSettings/ContentSettings/index.tsx
@@ -123,22 +123,31 @@ export const ContentSettings = ({
             buttonStyle="secondary"
             onClick={onClose}
             minWidth="160px"
+            width={
+              !isNilOrError(field.isDeleteEnabled) && !field.isDeleteEnabled
+                ? '100%'
+                : 'auto'
+            }
           >
             <FormattedMessage {...messages.done} />
           </Button>
-          <Button
-            icon="delete"
-            buttonStyle="primary-outlined"
-            borderColor={colors.error}
-            textColor={colors.error}
-            iconColor={colors.error}
-            onClick={() => onDelete(field.index)}
-            minWidth="160px"
-            data-cy="e2e-delete-field"
-            disabled={isDeleteDisabled}
-          >
-            <FormattedMessage {...messages.delete} />
-          </Button>
+          {(!isNilOrError(field.isDeleteEnabled)
+            ? field.isDeleteEnabled
+            : true) && (
+            <Button
+              icon="delete"
+              buttonStyle="primary-outlined"
+              borderColor={colors.error}
+              textColor={colors.error}
+              iconColor={colors.error}
+              onClick={() => onDelete(field.index)}
+              minWidth="160px"
+              data-cy="e2e-delete-field"
+              disabled={isDeleteDisabled}
+            >
+              <FormattedMessage {...messages.delete} />
+            </Button>
+          )}
         </Box>
       </Box>
     );
