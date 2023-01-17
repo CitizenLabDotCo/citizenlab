1ac354ca0cc3107d83f29e95a3fe19f76ed16c61
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-01-17 14:40:55 +0000

    [CL-2549] Replace PreferenceDialog enzyme tests by jest

--- front/app/components/ConsentManager/PreferenceDialog.test.tsx
+++ front/app/components/ConsentManager/PreferenceDialog.test.tsx
@@ -1,9 +1,6 @@
-// @ts-nocheck
-// libraries
 import React from 'react';
-import { shallow } from 'enzyme';
-
-import 'jest-styled-components';
+import { render } from 'utils/testUtils/rtl';
+import { CategorizedDestinations } from './typings';
 
 // component to test
 import PreferencesDialog from './PreferencesDialog';
@@ -14,46 +11,14 @@ jest.mock('services/appConfiguration');
 jest.mock('modules', () => ({ streamsToReset: [] }));
 
 describe('<ConsentManager />', () => {
-  let onChange: Jest.Mock;
+  let onChange;
 
-  const categoryDestinations = {
-    analytics: [
-      {
-        name: 'Google Analytics',
-        description:
-          'Google Analytics is the most popular marketing tool for the web. It’s free and provides a wide range of features. It’s especially good at measuring traffic sources and ad campaigns.',
-        category: 'Analytics',
-        website: 'http://google.com/analytics',
-        id: 'Google Analytics',
-      },
-      {
-        name: 'MarketingTool',
-        description:
-          'MarketingTool is the most popular marketing tool for the web. It’s free and provides a wide range of features. It’s especially good at measuring traffic sources and ad campaigns.',
-        category: 'Tag Managers',
-        website: 'http://random.com/marketing',
-        id: 'MarketingTool',
-      },
-    ],
-    advertising: [
-      {
-        name: 'AdvertisingTool',
-        description: 'Advertising BS',
-        category: 'Advertising',
-        website: 'http://random.com/advertising',
-        id: 'AdvertisingTool',
-      },
-    ],
-    functional: [
-      {
-        name: 'FunctionalTool',
-        description: 'Actually might be handy',
-        category: 'Security & Fraud',
-        website: 'http://random.com/securitycookie',
-        id: 'FunctionalTool',
-      },
-    ],
+  const categoryDestinations: CategorizedDestinations = {
+    analytics: ['google_analytics', 'google_tag_manager'],
+    advertising: ['segment'],
+    functional: ['intercom'],
   };
+
   const preferences = {
     analytics: true,
     advertising: false,
@@ -65,17 +30,20 @@ describe('<ConsentManager />', () => {
   });
 
   it('renders correctly when there are destinations', () => {
-    const wrapper = shallow(
+    const { container } = render(
       <PreferencesDialog
         onChange={onChange}
         categoryDestinations={categoryDestinations}
         preferences={preferences}
       />
     );
-    expect(wrapper).toMatchSnapshot();
+
+    const categoryCards = container.querySelectorAll('.e2e-category');
+    expect(categoryCards).toHaveLength(4);
   });
+
   it('renders correctly when there are no destinations', () => {
-    const wrapper = shallow(
+    const { container } = render(
       <PreferencesDialog
         onChange={onChange}
         categoryDestinations={{
@@ -86,6 +54,8 @@ describe('<ConsentManager />', () => {
         preferences={preferences}
       />
     );
-    expect(wrapper).toMatchSnapshot();
+
+    const categoryCards = container.querySelectorAll('.e2e-category');
+    expect(categoryCards).toHaveLength(1);
   });
 });
