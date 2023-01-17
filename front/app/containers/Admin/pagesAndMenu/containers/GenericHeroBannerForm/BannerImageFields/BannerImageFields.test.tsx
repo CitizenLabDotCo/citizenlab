486b0b8dc39769d1e8b88f910bd1b49c67f96889
brentguf <brentguf@users.noreply.github.com>
2023-01-17 09:46:41 +0100

    Wrap with parent describe

--- front/app/containers/Admin/pagesAndMenu/containers/GenericHeroBannerForm/BannerImageFields/BannerImageFields.test.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/GenericHeroBannerForm/BannerImageFields/BannerImageFields.test.tsx
@@ -18,167 +18,179 @@ const props = {
   onOverlayChange: jest.fn(),
 };
 
-describe('Layout preview selector', () => {
-  it('does not show when there is no image', () => {
-    render(<BannerImageFields {...props} headerBg={null} />);
-
-    const select = screen.queryByLabelText('Show preview for');
-    expect(select).not.toBeInTheDocument();
-  });
-
-  it('shows when there is an unsaved image', async () => {
-    render(<BannerImageFields {...props} headerBg={null} />);
-
-    await uploadLocalImageForHeroBanner();
-    const select = screen.getByLabelText('Show preview for');
-    expect(select).toBeInTheDocument();
-  });
-
-  describe('when layout is fixed-ratio layout', () => {
-    it('does not show when there is an unsaved image', async () => {
-      render(
-        <BannerImageFields
-          {...props}
-          bannerLayout={'fixed_ratio_layout'}
-          headerBg={null}
-        />
-      );
-
-      await uploadLocalImageForHeroBanner();
-      const select = screen.queryByRole('select', {
-        name: /Show preview for/,
-      });
-      expect(select).not.toBeInTheDocument();
-    });
-  });
-});
-
-describe('Image overlay toggle', () => {
-  describe('when layout is full-width banner layout', () => {
-    it('does not show if there is no image', () => {
+describe('BannerImageFields', () => {
+  describe('Layout preview selector', () => {
+    it('does not show when there is no image', () => {
       render(<BannerImageFields {...props} headerBg={null} />);
 
-      const overlayInput = screen.queryByLabelText('Enable overlay');
-      expect(overlayInput).not.toBeInTheDocument();
+      const select = screen.queryByLabelText('Show preview for');
+      expect(select).not.toBeInTheDocument();
     });
 
     it('shows when there is an unsaved image', async () => {
       render(<BannerImageFields {...props} headerBg={null} />);
 
       await uploadLocalImageForHeroBanner();
-      expect(screen.getByLabelText('Enable overlay')).toBeInTheDocument();
+      const select = screen.getByLabelText('Show preview for');
+      expect(select).toBeInTheDocument();
+    });
+
+    describe('when layout is fixed-ratio layout', () => {
+      it('does not show when there is an unsaved image', async () => {
+        render(
+          <BannerImageFields
+            {...props}
+            bannerLayout={'fixed_ratio_layout'}
+            headerBg={null}
+          />
+        );
+
+        await uploadLocalImageForHeroBanner();
+        const select = screen.queryByRole('select', {
+          name: /Show preview for/,
+        });
+        expect(select).not.toBeInTheDocument();
+      });
     });
   });
 
-  describe('when layout is two-row layout', () => {
-    const bannerLayout = 'two_row_layout';
+  describe('Image overlay toggle', () => {
+    describe('when layout is full-width banner layout', () => {
+      it('does not show if there is no image', () => {
+        render(<BannerImageFields {...props} headerBg={null} />);
 
-    it('does not show when there is an unsaved image', async () => {
-      render(
-        <BannerImageFields
-          {...props}
-          bannerLayout={bannerLayout}
-          headerBg={null}
-        />
-      );
+        const overlayInput = screen.queryByLabelText('Enable overlay');
+        expect(overlayInput).not.toBeInTheDocument();
+      });
 
-      await uploadLocalImageForHeroBanner();
-      expect(screen.queryByLabelText('Enable overlay')).not.toBeInTheDocument();
-    });
+      it('shows when there is an unsaved image', async () => {
+        render(<BannerImageFields {...props} headerBg={null} />);
 
-    it('does not show when there is a saved image', () => {
-      render(<BannerImageFields {...props} bannerLayout={bannerLayout} />);
-
-      expect(screen.queryByLabelText('Enable overlay')).not.toBeInTheDocument();
-    });
-  });
-
-  describe('when layout is two-column layout', () => {
-    const bannerLayout = 'two_column_layout';
-
-    it('does not show when there is an unsaved image', async () => {
-      render(
-        <BannerImageFields
-          {...props}
-          bannerLayout={bannerLayout}
-          headerBg={null}
-        />
-      );
-
-      await uploadLocalImageForHeroBanner();
-      expect(screen.queryByLabelText('Enable overlay')).not.toBeInTheDocument();
-    });
-
-    it('does not show when there is a saved image', () => {
-      render(<BannerImageFields {...props} bannerLayout={bannerLayout} />);
-
-      expect(screen.queryByLabelText('Enable overlay')).not.toBeInTheDocument();
-    });
-  });
-
-  describe('when layout is fixed-ratio layout', () => {
-    const bannerLayout = 'fixed_ratio_layout';
-
-    it('does not show when there is an unsaved image', () => {
-      render(
-        <BannerImageFields
-          {...props}
-          bannerLayout={bannerLayout}
-          headerBg={null}
-        />
-      );
-
-      expect(screen.queryByLabelText('Enable overlay')).not.toBeInTheDocument();
-    });
-
-    it('shows when there is a saved image', () => {
-      render(<BannerImageFields {...props} bannerLayout={bannerLayout} />);
-
-      waitFor(() =>
-        expect(screen.getByLabelText('Enable overlay')).toBeInTheDocument()
-      );
-    });
-  });
-});
-
-describe('Image cropper', () => {
-  describe('when layout is fixed-ratio', () => {
-    const bannerLayout = 'fixed_ratio_layout';
-
-    it('shows when there is an unsaved image', () => {
-      render(
-        <BannerImageFields
-          {...props}
-          bannerLayout={bannerLayout}
-          headerBg={null}
-        />
-      );
-
-      uploadLocalImageForHeroBanner().then(() => {
-        expect(screen.getByTestId('image-cropper')).toBeInTheDocument();
+        await uploadLocalImageForHeroBanner();
+        expect(screen.getByLabelText('Enable overlay')).toBeInTheDocument();
       });
     });
 
-    it('does not show when there is a saved image', () => {
-      render(<BannerImageFields {...props} bannerLayout={bannerLayout} />);
+    describe('when layout is two-row layout', () => {
+      const bannerLayout = 'two_row_layout';
 
-      expect(screen.queryByTestId('image-cropper')).not.toBeInTheDocument();
+      it('does not show when there is an unsaved image', async () => {
+        render(
+          <BannerImageFields
+            {...props}
+            bannerLayout={bannerLayout}
+            headerBg={null}
+          />
+        );
+
+        await uploadLocalImageForHeroBanner();
+        expect(
+          screen.queryByLabelText('Enable overlay')
+        ).not.toBeInTheDocument();
+      });
+
+      it('does not show when there is a saved image', () => {
+        render(<BannerImageFields {...props} bannerLayout={bannerLayout} />);
+
+        expect(
+          screen.queryByLabelText('Enable overlay')
+        ).not.toBeInTheDocument();
+      });
+    });
+
+    describe('when layout is two-column layout', () => {
+      const bannerLayout = 'two_column_layout';
+
+      it('does not show when there is an unsaved image', async () => {
+        render(
+          <BannerImageFields
+            {...props}
+            bannerLayout={bannerLayout}
+            headerBg={null}
+          />
+        );
+
+        await uploadLocalImageForHeroBanner();
+        expect(
+          screen.queryByLabelText('Enable overlay')
+        ).not.toBeInTheDocument();
+      });
+
+      it('does not show when there is a saved image', () => {
+        render(<BannerImageFields {...props} bannerLayout={bannerLayout} />);
+
+        expect(
+          screen.queryByLabelText('Enable overlay')
+        ).not.toBeInTheDocument();
+      });
+    });
+
+    describe('when layout is fixed-ratio layout', () => {
+      const bannerLayout = 'fixed_ratio_layout';
+
+      it('does not show when there is an unsaved image', () => {
+        render(
+          <BannerImageFields
+            {...props}
+            bannerLayout={bannerLayout}
+            headerBg={null}
+          />
+        );
+
+        expect(
+          screen.queryByLabelText('Enable overlay')
+        ).not.toBeInTheDocument();
+      });
+
+      it('shows when there is a saved image', () => {
+        render(<BannerImageFields {...props} bannerLayout={bannerLayout} />);
+
+        waitFor(() =>
+          expect(screen.getByLabelText('Enable overlay')).toBeInTheDocument()
+        );
+      });
     });
   });
 
-  describe('when layout is not fixed-ratio', () => {
-    it('does not show with an unsaved image', () => {
-      render(<BannerImageFields {...props} headerBg={null} />);
+  describe('Image cropper', () => {
+    describe('when layout is fixed-ratio', () => {
+      const bannerLayout = 'fixed_ratio_layout';
 
-      uploadLocalImageForHeroBanner().then(() => {
+      it('shows when there is an unsaved image', () => {
+        render(
+          <BannerImageFields
+            {...props}
+            bannerLayout={bannerLayout}
+            headerBg={null}
+          />
+        );
+
+        uploadLocalImageForHeroBanner().then(() => {
+          expect(screen.getByTestId('image-cropper')).toBeInTheDocument();
+        });
+      });
+
+      it('does not show when there is a saved image', () => {
+        render(<BannerImageFields {...props} bannerLayout={bannerLayout} />);
+
         expect(screen.queryByTestId('image-cropper')).not.toBeInTheDocument();
       });
     });
 
-    it('does not show when there is a saved image', () => {
-      render(<BannerImageFields {...props} />);
+    describe('when layout is not fixed-ratio', () => {
+      it('does not show with an unsaved image', () => {
+        render(<BannerImageFields {...props} headerBg={null} />);
 
-      expect(screen.queryByTestId('image-cropper')).not.toBeInTheDocument();
+        uploadLocalImageForHeroBanner().then(() => {
+          expect(screen.queryByTestId('image-cropper')).not.toBeInTheDocument();
+        });
+      });
+
+      it('does not show when there is a saved image', () => {
+        render(<BannerImageFields {...props} />);
+
+        expect(screen.queryByTestId('image-cropper')).not.toBeInTheDocument();
+      });
     });
   });
 });
