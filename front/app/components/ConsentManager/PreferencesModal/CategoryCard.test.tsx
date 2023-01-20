d7e5e02f931fc0eb77c6ef056298d5d2011dd5a5
brentguf <brentguf@users.noreply.github.com>
2023-01-20 18:37:08 +0100

    use getByRole

--- front/app/components/ConsentManager/PreferencesModal/CategoryCard.test.tsx
+++ front/app/components/ConsentManager/PreferencesModal/CategoryCard.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from 'utils/testUtils/rtl';
+import { fireEvent, render, screen } from 'utils/testUtils/rtl';
 import { IDestination, registerDestination } from '../destinations';
 
 // component to test
@@ -9,6 +9,13 @@ import CategoryCard from './CategoryCard';
 jest.mock('services/appConfiguration');
 jest.mock('utils/cl-intl');
 jest.mock('modules', () => ({ streamsToReset: [] }));
+
+const getRadioInputs = () => {
+  const allowButton = screen.getByRole('radio', { name: 'Allow' });
+  const disallowButton = screen.getByRole('radio', { name: 'Disallow' });
+
+  return { allowButton, disallowButton };
+};
 
 describe('<CategoryCard />', () => {
   const category = 'analytics';
@@ -24,7 +31,7 @@ describe('<CategoryCard />', () => {
   const handleChange = jest.fn();
 
   it('renders correctly when allowed', () => {
-    const { container } = render(
+    render(
       <CategoryCard
         key={category}
         category={category}
@@ -34,15 +41,13 @@ describe('<CategoryCard />', () => {
       />
     );
 
-    const allowButton = container.querySelector('#analytics-radio-true');
-    const disallowButton = container.querySelector('#analytics-radio-false');
-
+    const { allowButton, disallowButton } = getRadioInputs();
     expect(allowButton).toBeChecked();
     expect(disallowButton).not.toBeChecked();
   });
 
   it('renders correctly when disallowed', () => {
-    const { container } = render(
+    render(
       <CategoryCard
         key={category}
         category={category}
@@ -52,15 +57,14 @@ describe('<CategoryCard />', () => {
       />
     );
 
-    const allowButton = container.querySelector('#analytics-radio-true');
-    const disallowButton = container.querySelector('#analytics-radio-false');
+    const { allowButton, disallowButton } = getRadioInputs();
 
     expect(allowButton).not.toBeChecked();
     expect(disallowButton).toBeChecked();
   });
 
   it('lets you change your preference', () => {
-    const { container } = render(
+    render(
       <CategoryCard
         key={category}
         category={category}
@@ -69,14 +73,13 @@ describe('<CategoryCard />', () => {
         onChange={handleChange}
       />
     );
+    const { allowButton, disallowButton } = getRadioInputs();
 
     // when you change the value these onchange handlers should be called
-    const allowButton = container.querySelector('#analytics-radio-true');
     fireEvent.click(allowButton);
     expect(handleChange).toHaveBeenCalledTimes(1);
     expect(handleChange).toHaveBeenCalledWith('analytics', true);
 
-    const disallowButton = container.querySelector('#analytics-radio-false');
     fireEvent.click(disallowButton);
     expect(handleChange).toHaveBeenCalledTimes(2);
     expect(handleChange).toHaveBeenCalledWith('analytics', false);
