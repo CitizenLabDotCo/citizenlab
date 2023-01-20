8eea6fe08eddacea4200240e7964dfb6b38aff72
jamesspeake <james@speake.org>
2023-01-20 14:02:19 +0000

    [CL-2645] fix

--- back/app/serializers/web_api/v1/custom_field_serializer.rb
+++ back/app/serializers/web_api/v1/custom_field_serializer.rb
@@ -12,9 +12,10 @@ class WebApi::V1::CustomFieldSerializer < WebApi::V1::BaseSerializer
     object.resource_type == 'User'
   }
 
+  # Is this best on the serializer or could we add it to the model?
   attribute :field_constraints do |object|
     @participation_method = Factory.instance.participation_method_for object.resource.participation_context
-    @participation_method.field_constraints[object.key] || {}
+    @participation_method.field_constraints[object.code] || {}
   end
 
   attributes :maximum, :minimum_label_multiloc, :maximum_label_multiloc, if: proc { |object, _params|
