097cc375cfa7cddae72a7aa5bc38ddd208d57f75
Edwin Kato <katedwin88@gmail.com>
2023-01-23 15:52:54 +0300

    Fix duplicate keys

--- front/app/containers/Admin/projects/project/inputForm/InflexibleForm/messages.ts
+++ front/app/containers/Admin/projects/project/inputForm/InflexibleForm/messages.ts
@@ -2,56 +2,56 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   inputForm: {
-    id: 'app.containers.AdminPage.ProjectIdeaForm.inputForm',
+    id: 'app.containers.AdminPage.ProjectIdeaForm.inFlexibleForm.inputForm',
     defaultMessage: 'Input form',
   },
   postDescription: {
-    id: 'app.containers.AdminPage.ProjectIdeaForm.postDescription',
+    id: 'app.containers.AdminPage.ProjectIdeaForm.inFlexibleForm.postDescription',
     defaultMessage:
       'Specify what information should be provided, add short descriptions or instructions to guide participant responses and specify whether each field is optional or required',
   },
   save: {
-    id: 'app.containers.AdminPage.ProjectIdeaForm.save',
+    id: 'app.containers.AdminPage.ProjectIdeaForm.inFlexibleForm.save',
     defaultMessage: 'Save',
   },
   saved: {
-    id: 'app.containers.AdminPage.ProjectIdeaForm.saved',
+    id: 'app.containers.AdminPage.ProjectIdeaForm.inFlexibleForm.saved',
     defaultMessage: 'Saved!',
   },
   saveSuccessMessage: {
-    id: 'app.containers.AdminPage.ProjectIdeaForm.saveSuccessMessage',
+    id: 'app.containers.AdminPage.ProjectIdeaForm.inFlexibleForm.saveSuccessMessage',
     defaultMessage: 'Your changes have been saved successfully.',
   },
   errorMessage: {
-    id: 'app.containers.AdminPage.ProjectIdeaForm.errorMessage',
+    id: 'app.containers.AdminPage.ProjectIdeaForm.inFlexibleForm.errorMessage',
     defaultMessage: 'Something went wrong, please try again later',
   },
   descriptionLabel: {
-    id: 'app.containers.AdminPage.ProjectIdeaForm.descriptionLabel',
+    id: 'app.containers.AdminPage.ProjectIdeaForm.inFlexibleForm.descriptionLabel',
     defaultMessage: 'Field description',
   },
   collapseAll: {
-    id: 'app.containers.AdminPage.ProjectIdeaForm.collapseAll',
+    id: 'app.containers.AdminPage.ProjectIdeaForm.inFlexibleForm.collapseAll',
     defaultMessage: 'Collapse all fields',
   },
   expandAll: {
-    id: 'app.containers.AdminPage.ProjectIdeaForm.expandAll',
+    id: 'app.containers.AdminPage.ProjectIdeaForm.inFlexibleForm.expandAll',
     defaultMessage: 'Expand all fields',
   },
   enabled: {
-    id: 'app.containers.AdminPage.ProjectIdeaForm.enabled',
+    id: 'app.containers.AdminPage.ProjectIdeaForm.inFlexibleForm.enabled',
     defaultMessage: 'Enabled',
   },
   required: {
-    id: 'app.containers.AdminPage.ProjectIdeaForm.required',
+    id: 'app.containers.AdminPage.ProjectIdeaForm.inFlexibleForm.required',
     defaultMessage: 'Required',
   },
   enabledTooltipContent: {
-    id: 'app.containers.AdminPage.ProjectIdeaForm.enabledTooltipContent',
+    id: 'app.containers.AdminPage.ProjectIdeaForm.inFlexibleForm.enabledTooltipContent',
     defaultMessage: 'Include this field.',
   },
   requiredTooltipContent: {
-    id: 'app.containers.AdminPage.ProjectIdeaForm.requiredTooltipContent',
+    id: 'app.containers.AdminPage.ProjectIdeaForm.inFlexibleForm.requiredTooltipContent',
     defaultMessage: 'Require this field to be filled in.',
   },
 });
