d22f411e06e85107ea2857440116fb5d70b62f0f
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-01-23 10:42:00 +0000

    [CL-2658] _

---
+++ front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/MostVotedIdeasWidget/ProjectInfo.tsx
@@ -0,0 +1,28 @@
+// import React from 'react';
+
+// // hooks
+// import useProject from 'hooks/useProject';
+// import useLocalize from 'hooks/useLocalize';
+
+// // utils
+// import { isNilOrError } from 'utils/helperUtils';
+
+// interface Props {
+//   projectId: string;
+// }
+
+// const Header = ({ projectId }: Props) => {
+//   const localize = useLocalize();
+//   const project = useProject({ projectId });
+
+//   if (isNilOrError(project)) return null;
+
+//   const
+
+//   const projectTitle = localize(project.attributes.title_multiloc);
+//   const ideasCount = project.attributes.ideas_count;
+// };
+
+// const
+
+// export default Header;
