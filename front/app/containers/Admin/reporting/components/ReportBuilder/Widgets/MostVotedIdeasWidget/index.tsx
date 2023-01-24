8fb13215a810e63983949af383c75577c84f82c8
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-01-24 16:00:04 +0000

    [CL-2658] Add border radius to widget

--- front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/MostVotedIdeasWidget/index.tsx
+++ front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/MostVotedIdeasWidget/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+// styling
+import { BORDER } from '../constants';
+import { stylingConsts } from 'utils/styleUtils';
+
 // components
 import { Box } from '@citizenlab/cl2-component-library';
 import Header from './Header';
@@ -8,9 +12,6 @@ import Settings from './Settings';
 
 // i18n
 import messages from './messages';
-
-// utils
-import { BORDER } from '../constants';
 
 // typings
 import { Props } from './typings';
@@ -23,7 +24,12 @@ const MostVotedIdeasWidget = ({
   collapseLongText,
 }: Props) => {
   return (
-    <Box border={BORDER} mt="4px" mb="4px">
+    <Box
+      border={BORDER}
+      mt="4px"
+      mb="4px"
+      borderRadius={stylingConsts.borderRadius}
+    >
       <Header title={title} projectId={projectId} phaseId={phaseId} />
       {projectId && (
         <Ideas
