ce7dfa47d894c63af3e5b507562e426ad79d6419
brentguf <brentguf@users.noreply.github.com>
2023-01-24 09:43:00 +0100

    Remove unneeded styles

--- front/app/containers/Admin/projects/project/general/components/ProjectImageDropzone.tsx
+++ front/app/containers/Admin/projects/project/general/components/ProjectImageDropzone.tsx
@@ -1,20 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
-
-// components
 import ImagesDropzone from 'components/UI/ImagesDropzone';
-
-// typings
 import { UploadFile } from 'typings';
-
 import { CARD_IMAGE_ASPECT_RATIO } from 'services/projects';
-
-// Would have loved to put this in styling.ts, but
-// that results in some arcane typescript error
-// (see https://stackoverflow.com/q/43900035)
-const StyledImagesDropzone = styled(ImagesDropzone)`
-  margin-top: 2px;
-`;
 
 interface Props {
   projectImages: UploadFile[] | null;
@@ -27,7 +14,7 @@ export default ({
   handleProjectImagesOnAdd,
   handleProjectImageOnRemove,
 }: Props) => (
-  <StyledImagesDropzone
+  <ImagesDropzone
     images={projectImages}
     imagePreviewRatio={1 / CARD_IMAGE_ASPECT_RATIO}
     maxImagePreviewWidth="240px"
