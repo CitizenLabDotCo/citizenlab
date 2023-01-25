be11980d5191ad7879b711a516d157dc643775c1
brentguf <brentguf@users.noreply.github.com>
2023-01-25 08:19:04 +0100

    Test notifications loading (WIP)

--- front/app/containers/MainHeader/NotificationMenu/NotificationMenu.test.tsx
+++ front/app/containers/MainHeader/NotificationMenu/NotificationMenu.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { IUserAttributes } from 'services/users';
 import { render, screen, userEvent } from 'utils/testUtils/rtl';
 import NotificationMenu from '.';
+import { TNotificationData } from 'services/notifications';
 
 jest.mock('services/appConfiguration');
 jest.mock('services/auth');
@@ -19,6 +20,30 @@ let mockUserData: {
 };
 
 jest.mock('hooks/useAuthUser', () => jest.fn(() => mockUserData));
+jest.mock('resources/GetNotifications', () => mockNotifications);
+// TNotificationData[]
+
+let mockNotifications: {
+  list:
+    | {
+        id: string;
+        type: string;
+        attributes: TNotificationData;
+      }[]
+    | null;
+} = {
+  list: [
+    {
+      id: '1' as const,
+      type: 'notification' as const,
+      attributes: {
+        type: 'admin_rights_received',
+        // read_at: null,
+        // created_at: '2022-10-22T00:00:00.000Z',
+      },
+    },
+  ],
+};
 
 describe('NotificationMenu', () => {
   it('Opens and closes the dropdown when clicking the notifications icon', async () => {
@@ -42,5 +67,11 @@ describe('NotificationMenu', () => {
 
     const notificationsIcon = screen.queryByRole('button');
     expect(notificationsIcon).toBeNull();
+  });
+
+  it('Shows notifications when we have notifications', () => {});
+
+  it('It shows a fallback message when we have no notifications', () => {
+    mockNotifications = null;
   });
 });
