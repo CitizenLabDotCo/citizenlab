6bb3f5237d34ceda6e02e8420316674fa7ee48d0
Alexander <alexander@citizenlab.co>
2023-01-27 10:29:54 +0100

    [CL-2603] Set default values for signed out banner color/opacity in seeds

--- back/engines/commercial/multi_tenancy/db/seeds/home_pages.rb
+++ back/engines/commercial/multi_tenancy/db/seeds/home_pages.rb
@@ -9,7 +9,9 @@ module MultiTenancy
         # Since we add a default home_pages record from the base template,
         # we simply update that record to add a header image to the homepage.
         HomePage.first.update!({
-          header_bg: Rails.root.join('spec/fixtures/header.jpg').open
+          header_bg: Rails.root.join('spec/fixtures/header.jpg').open,
+          banner_signed_out_header_overlay_color: AppConfiguration.first.settings.dig('core', 'color_main'),
+          banner_signed_out_header_overlay_opacity: 90
         })
       end
     end
