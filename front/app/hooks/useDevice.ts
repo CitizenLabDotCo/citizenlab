0323fc1aa9d1b0bace6de913fcf3eaeb3df04c53
brentguf <brentguf@users.noreply.github.com>
2023-01-29 09:27:46 +0100

    getBreakpoint

--- front/app/hooks/useDevice.ts
+++ front/app/hooks/useDevice.ts
@@ -1,4 +1,7 @@
-import { useBreakpoint } from '@citizenlab/cl2-component-library';
+import {
+  useBreakpoint,
+  viewportWidths,
+} from '@citizenlab/cl2-component-library';
 import { TDevice } from 'components/admin/SelectPreviewDevice';
 
 export default function useDevice() {
@@ -12,4 +15,16 @@ export default function useDevice() {
   })();
 
   return device;
+}
+
+const mediaQueries = {
+  phone: `(max-width: ${viewportWidths.phone}px)`,
+  tablet: `(max-width: ${viewportWidths.tablet}px)`,
+  smallDesktop: `(max-width: ${viewportWidths.smallDesktop}px)`,
+};
+
+type BreakpointType = keyof typeof mediaQueries;
+
+export function getBreakpoint(breakpointType: BreakpointType) {
+  return window.matchMedia(mediaQueries[breakpointType]).matches;
 }
