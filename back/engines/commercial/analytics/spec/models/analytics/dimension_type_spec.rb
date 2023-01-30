296b4932d187893e96db0044238be887d4eb95af
Fran <francisco.pensa.web@gmail.com>
2023-01-30 16:25:59 -0300

    [CL-2659] fix backend tests

--- back/engines/commercial/analytics/spec/models/analytics/dimension_type_spec.rb
+++ back/engines/commercial/analytics/spec/models/analytics/dimension_type_spec.rb
@@ -8,6 +8,5 @@ RSpec.describe Analytics::DimensionType, type: :model do
   describe 'validations' do
     it { is_expected.to be_valid }
     it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_uniqueness_of(:name) }
   end
 end
