16096c3b30ab122c20cebe4244ee2ea0e3ef7418
Edwin Kato <katedwin88@gmail.com>
2023-01-31 11:18:15 +0300

    Add unit test to cover item before

---
+++ front/app/utils/moduleUtils.test.ts
@@ -0,0 +1,43 @@
+import { insertConfiguration } from 'utils/moduleUtils';
+
+jest.mock('components/UI/PageLoading', () => ({
+  PageLoading: 'PageLoading',
+}));
+
+describe('insertConfiguration', () => {
+  const initialTabsWithScratch = [
+    {
+      name: 'scratch',
+      label: 'scratch label',
+      icon: 'blank-paper',
+    },
+  ];
+  const templateItemToInsert = {
+    name: 'template',
+    label: 'template label',
+    icon: 'template',
+  };
+
+  describe('insertBeforeName', () => {
+    it('inserts the configuration before item when specified', () => {
+      const module = {
+        configuration: templateItemToInsert,
+        insertBeforeName: 'scratch',
+      };
+      const result = insertConfiguration(module)(initialTabsWithScratch);
+
+      expect(result).toEqual([
+        {
+          name: 'template',
+          label: 'template label',
+          icon: 'template',
+        },
+        {
+          name: 'scratch',
+          label: 'scratch label',
+          icon: 'blank-paper',
+        },
+      ]);
+    });
+  });
+});
