55041a7eb8d9299aabfb20e7bc1faa7c006fd497
Iva <iva@citizenlab.co>
2023-01-31 11:20:01 +0200

    [CL-2727] Add user_id column

---
+++ back/db/migrate/20230131091656_add_user_id_to_sessions.impact_tracking.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+# This migration comes from impact_tracking (originally 20230131110900)
+
+class AddUserIdToSessions < ActiveRecord::Migration[6.1]
+    def change
+      add_column :impact_tracking_sessions, :user_id, :uuid
+    end
+end
