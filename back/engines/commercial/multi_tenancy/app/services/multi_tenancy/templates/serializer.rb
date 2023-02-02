cb70bf650f3249c5e20e9a2f217ad519904a39b7
Simon Tharby <simontharby@gmail.com>
2023-02-02 15:40:57 +0000

    [CL-2746] Add content_builder layouts & images to serializer

--- back/engines/commercial/multi_tenancy/app/services/multi_tenancy/templates/serializer.rb
+++ back/engines/commercial/multi_tenancy/app/services/multi_tenancy/templates/serializer.rb
@@ -2,6 +2,7 @@
 
 module MultiTenancy
   module Templates
+    # rubocop:disable Metrics/ClassLength
     class Serializer < ::TemplateService
       def initialize(tenant)
         super()
@@ -65,13 +66,44 @@ module MultiTenancy
             'volunteering/volunteer'               => yml_volunteering_volunteers,
             'custom_maps/map_config'               => yml_maps_map_configs,
             'custom_maps/layer'                    => yml_maps_layers,
-            'custom_maps/legend_item'              => yml_maps_legend_items
+            'custom_maps/legend_item'              => yml_maps_legend_items,
+            'content_builder/layout'               => yml_content_builder_layouts,
+            'content_builder/layout_image'         => yml_content_builder_layout_images
           }
         end
         { 'models' => models }
       end
 
       private
+
+      def yml_content_builder_layouts
+        ContentBuilder::Layout.all.map do |layout|
+          yml_layout = {
+            'content_buildable_ref' => lookup_ref(layout.content_buildable_id, :project),
+            'content_buildable_type' => layout.content_buildable_type,
+            'code' => layout.code,
+            'enabled' => layout.enabled,
+            'craftjs_jsonmultiloc' => layout.craftjs_jsonmultiloc,
+            'created_at' => layout.created_at.to_s,
+            'updated_at' => layout.updated_at.to_s
+          }
+          store_ref yml_layout, layout.id, :content_builder_layout
+          yml_layout
+        end
+      end
+
+      def yml_content_builder_layout_images
+        ContentBuilder::LayoutImage.all.map do |image|
+          yml_layout_image = {
+            'code' => image.code,
+            'remote_image_url' => image.image_url,
+            'created_at' => image.created_at.to_s,
+            'updated_at' => image.updated_at.to_s
+          }
+          store_ref yml_layout_image, image.id, :content_builder_layout_image
+          yml_layout_image
+        end
+      end
 
       def yml_home_pages
         HomePage.all.map do |hp|
@@ -1040,3 +1072,4 @@ module MultiTenancy
     end
   end
 end
+# rubocop:enable Metrics/ClassLength
