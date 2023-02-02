f5733f24f332464f8d6c1e1ca500d5ba4d3f8cb5
brentguf <brentguf@users.noreply.github.com>
2023-02-02 16:12:07 +0100

    Use right image ratio for project/folder card preview

--- front/app/containers/Admin/projects/project/general/components/ProjectCardImageDropzone.tsx
+++ front/app/containers/Admin/projects/project/general/components/ProjectCardImageDropzone.tsx
@@ -2,32 +2,22 @@ import React from 'react';
 import ImagesDropzone from 'components/UI/ImagesDropzone';
 import { UploadFile } from 'typings';
 import { CARD_IMAGE_ASPECT_RATIO } from 'services/projectImages';
-import { TDevice } from 'components/admin/SelectPreviewDevice';
 
 interface Props {
   images: UploadFile[] | null;
   onAddImages: (projectImages: UploadFile[]) => void;
   onRemoveImage: (projectImageToRemove: UploadFile) => void;
-  previewDevice: TDevice;
 }
 
 const ProjectCardImageDropzone = ({
   images,
   onAddImages,
   onRemoveImage,
-  previewDevice,
 }: Props) => {
-  const imagePreviewRatioPerDevice: { [key in TDevice]: number } = {
-    phone: 1 / CARD_IMAGE_ASPECT_RATIO,
-    tablet: 1,
-    desktop: 1,
-  };
-  const imagePreviewRatio = imagePreviewRatioPerDevice[previewDevice];
-
   return (
     <ImagesDropzone
       images={images}
-      imagePreviewRatio={imagePreviewRatio}
+      imagePreviewRatio={1 / CARD_IMAGE_ASPECT_RATIO}
       acceptedFileTypes={{
         'image/*': ['.jpg', '.jpeg', '.png', '.gif'],
       }}
