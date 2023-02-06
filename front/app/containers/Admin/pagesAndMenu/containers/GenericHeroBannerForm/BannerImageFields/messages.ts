cb8362673d46dc18f234013d7aa2b44c59f81b29
brentguf <brentguf@users.noreply.github.com>
2023-02-06 16:21:57 +0100

    Remove duplicate message ids 2

--- front/app/containers/Admin/pagesAndMenu/containers/GenericHeroBannerForm/BannerImageFields/messages.ts
+++ front/app/containers/Admin/pagesAndMenu/containers/GenericHeroBannerForm/BannerImageFields/messages.ts
@@ -2,17 +2,17 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   imageSupportPageURL: {
-    id: 'app.components.Admin.ImageCropper.imageSupportPageURL',
+    id: 'pagesAndMenu.GenericHeroBannerForm.BannerImageFields.imageSupportPageURL',
     defaultMessage:
       'https://support.citizenlab.co/en/articles/1346397-what-are-the-recommended-dimensions-and-sizes-of-the-platform-images',
   },
   imageTooltip: {
-    id: 'app.components.Admin.ImageCropper.imageTooltip',
+    id: 'pagesAndMenu.GenericHeroBannerForm.BannerImageFields.imageTooltip',
     defaultMessage:
       'For more information on recommended image resolutions, {supportPageLink}.',
   },
   imageSupportPageText: {
-    id: 'app.components.Admin.ImageCropper.imageSupportPageText',
+    id: 'pagesAndMenu.GenericHeroBannerForm.BannerImageFields.imageSupportPageText',
     defaultMessage: 'visit our support center',
   },
 });
