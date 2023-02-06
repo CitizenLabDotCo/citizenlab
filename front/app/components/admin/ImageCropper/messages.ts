70004e9d0dc77be0f62b356792581555fa5c0344
brentguf <brentguf@users.noreply.github.com>
2023-02-06 15:54:07 +0100

    Move tooltip to place of usage

--- front/app/components/admin/ImageCropper/messages.ts
+++ front/app/components/admin/ImageCropper/messages.ts
@@ -10,18 +10,4 @@ export default defineMessages({
     id: 'app.components.Admin.ImageCropper.infoLinkText',
     defaultMessage: 'recommended ratio',
   },
-  imageSupportPageURL: {
-    id: 'app.components.Admin.ImageCropper.imageSupportPageURL',
-    defaultMessage:
-      'https://support.citizenlab.co/en/articles/1346397-what-are-the-recommended-dimensions-and-sizes-of-the-platform-images',
-  },
-  imageTooltip: {
-    id: 'app.components.Admin.ImageCropper.imageTooltip',
-    defaultMessage:
-      'For more information on recommended image resolutions, {supportPageLink}.',
-  },
-  imageSupportPageText: {
-    id: 'app.components.Admin.ImageCropper.imageSupportPageText',
-    defaultMessage: 'visit our support center',
-  },
 });
---
+++ front/app/containers/Admin/pagesAndMenu/containers/GenericHeroBannerForm/BannerImageFields/messages.ts
@@ -0,0 +1,18 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  imageSupportPageURL: {
+    id: 'app.components.Admin.ImageCropper.imageSupportPageURL',
+    defaultMessage:
+      'https://support.citizenlab.co/en/articles/1346397-what-are-the-recommended-dimensions-and-sizes-of-the-platform-images',
+  },
+  imageTooltip: {
+    id: 'app.components.Admin.ImageCropper.imageTooltip',
+    defaultMessage:
+      'For more information on recommended image resolutions, {supportPageLink}.',
+  },
+  imageSupportPageText: {
+    id: 'app.components.Admin.ImageCropper.imageSupportPageText',
+    defaultMessage: 'visit our support center',
+  },
+});
