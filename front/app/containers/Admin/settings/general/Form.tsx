69d1ca1fd24a05d0016f5563603d27cef0d5f858
brentguf <brentguf@users.noreply.github.com>
2023-02-07 23:13:55 +0100

    Extract Form from general settings

---
+++ front/app/containers/Admin/settings/general/Form.tsx
@@ -0,0 +1,157 @@
+import React from 'react';
+import styled from 'styled-components';
+import { isNilOrError } from 'utils/helperUtils';
+import { object } from 'yup';
+import { FormProvider, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { appLocalePairs } from 'containers/App/constants';
+
+// i18n
+import { FormattedMessage, useIntl } from 'utils/cl-intl';
+import messages from '../messages';
+
+// components
+import {
+  IconTooltip,
+  Label,
+  Button,
+  Box,
+} from '@citizenlab/cl2-component-library';
+import {
+  Section,
+  SectionTitle,
+  SubSectionTitle,
+  SectionField,
+  SectionDescription,
+} from 'components/admin/Section';
+import InputMultilocWithLocaleSwitcher from 'components/HookForm/InputMultilocWithLocaleSwitcher';
+import Input from 'components/HookForm/Input';
+import MultipleSelect from 'components/HookForm/MultipleSelect';
+
+// services
+import { IAppConfigurationSettingsCore } from 'services/appConfiguration';
+
+// Utils
+import { handleHookFormSubmissionError } from 'utils/errorUtils';
+import validateMultilocForEveryLocale from 'utils/yup/validateMultilocForEveryLocale';
+import useAppConfiguration from 'hooks/useAppConfiguration';
+
+const StyledSection = styled(Section)`
+  margin-bottom: 50px;
+`;
+
+interface FormValues {
+  organization_name: IAppConfigurationSettingsCore['organization_name'];
+  locales: IAppConfigurationSettingsCore['locales'];
+  organization_site: IAppConfigurationSettingsCore['organization_site'];
+}
+
+interface Props {
+  defaultValues: FormValues;
+  onSubmit: (formValues: FormValues) => Promise<void>;
+}
+
+const Component = ({ defaultValues, onSubmit }: Props) => {
+  const { formatMessage } = useIntl();
+  const appConfig = useAppConfiguration();
+  const schema = object({
+    organization_name: validateMultilocForEveryLocale(
+      formatMessage(messages.organizationNameMultilocError)
+    ),
+  });
+  const methods = useForm({
+    mode: 'onBlur',
+    defaultValues,
+    resolver: yupResolver(schema),
+  });
+
+  const onFormSubmit = async (formValues: FormValues) => {
+    try {
+      await onSubmit(formValues);
+    } catch (error) {
+      handleHookFormSubmissionError(error, methods.setError);
+    }
+  };
+
+  // May need memoization
+  const getLocaleOptions = () => {
+    return Object.entries(appLocalePairs).map(([label, locale]) => ({
+      label,
+      value: locale,
+    }));
+  };
+
+  if (!isNilOrError(appConfig)) {
+    const organizationType =
+      appConfig.attributes.settings.core.organization_type;
+
+    return (
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(onFormSubmit)}>
+          <SectionTitle>
+            <FormattedMessage {...messages.titleBasic} />
+          </SectionTitle>
+          <StyledSection>
+            <SubSectionTitle>
+              <FormattedMessage {...messages.platformConfiguration} />
+            </SubSectionTitle>
+            <SectionDescription>
+              <FormattedMessage {...messages.subtitleBasic} />
+            </SectionDescription>
+
+            <SectionField>
+              <InputMultilocWithLocaleSwitcher
+                type="text"
+                id="organization_name"
+                name="organization_name"
+                label={
+                  <FormattedMessage
+                    {...messages.organizationName}
+                    values={{ type: organizationType }}
+                  />
+                }
+              />
+            </SectionField>
+
+            <SectionField>
+              <Label>
+                <FormattedMessage {...messages.languages} />
+                <IconTooltip
+                  content={<FormattedMessage {...messages.languagesTooltip} />}
+                />
+              </Label>
+              <MultipleSelect
+                name="locales"
+                placeholder=""
+                options={getLocaleOptions()}
+              />
+            </SectionField>
+
+            <SectionField>
+              <Label>
+                <FormattedMessage {...messages.urlTitle} />
+                <IconTooltip
+                  content={formatMessage(messages.urlTitleTooltip)}
+                />
+              </Label>
+              <Input
+                type="text"
+                name="organization_name"
+                placeholder="https://..."
+              />
+            </SectionField>
+            <Box display="flex">
+              <Button type="submit" processing={methods.formState.isSubmitting}>
+                {formatMessage(messages.save)}
+              </Button>
+            </Box>
+          </StyledSection>
+        </form>
+      </FormProvider>
+    );
+  }
+
+  return null;
+};
+
+export default Component;
--- front/app/containers/Admin/settings/general/index.tsx
+++ front/app/containers/Admin/settings/general/index.tsx
@@ -1,45 +1,20 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { get, map, merge, set } from 'lodash-es';
 import { isNilOrError } from 'utils/helperUtils';
-import { object } from 'yup';
-import { FormProvider, useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-
-// typings
-import { CLError, Multiloc, IOption } from 'typings';
 
 // i18n
 import { FormattedMessage, useIntl } from 'utils/cl-intl';
-import { appLocalePairs } from 'containers/App/constants';
 import messages from '../messages';
 
 // components
-import InputMultilocWithLocaleSwitcher from 'components/UI/InputMultilocWithLocaleSwitcher';
-import {
-  Input,
-  IconTooltip,
-  Label,
-  Success,
-  Error,
-  Toggle,
-  Button,
-  Box,
-} from '@citizenlab/cl2-component-library';
-import MultipleSelect from 'components/UI/MultipleSelect';
-import {
-  Section,
-  SectionTitle,
-  SubSectionTitle,
-  SectionField,
-  SectionDescription,
-} from 'components/admin/Section';
+import { Success, Error, Toggle } from '@citizenlab/cl2-component-library';
+import { Section, SubSectionTitle } from 'components/admin/Section';
 import Outlet from 'components/Outlet';
+import Form from './Form';
 
 // services
 import {
   updateAppConfiguration,
-  IUpdatedAppConfigurationProperties,
   TAppConfigurationSettingWithEnabled,
   IAppConfigurationSettingsCore,
   updateAppConfigurationCore,
@@ -47,8 +22,6 @@ import {
 
 // Utils
 import useAppConfiguration from 'hooks/useAppConfiguration';
-import { handleHookFormSubmissionError } from 'utils/errorUtils';
-import validateMultilocForEveryLocale from 'utils/yup/validateMultilocForEveryLocale';
 
 const StyledSection = styled(Section)`
   margin-bottom: 50px;
@@ -82,81 +55,14 @@ interface FormValues {
   organization_site: IAppConfigurationSettingsCore['organization_site'];
 }
 
-interface Props {
-  defaultValues: FormValues;
-}
-
-const SettingsGeneralTab = ({ defaultValues }: Props) => {
+const SettingsGeneralTab = () => {
   const appConfiguration = useAppConfiguration();
-  const [hasUrlError, setHasUrlError] = useState(false);
   const [settingsUpdatedSuccessFully, setSettingsUpdatedSuccessFully] =
     useState(false);
   const [settingsSavingError, setSettingsSavingError] = useState(false);
-  const [attributesDiff, setAttributesDiff] =
-    useState<IUpdatedAppConfigurationProperties>({});
-
   const { formatMessage } = useIntl();
 
-  const schema = object({
-    organization_name: validateMultilocForEveryLocale(
-      formatMessage(messages.organizationNameMultilocError)
-    ),
-  });
-  const methods = useForm({
-    mode: 'onBlur',
-    defaultValues,
-    resolver: yupResolver(schema),
-  });
-
-  const onFormSubmit = async (formValues: FormValues) => {
-    try {
-      await save(formValues);
-    } catch (error) {
-      handleHookFormSubmissionError(error, methods.setError);
-    }
-  };
-
-  const handleCoreMultilocSettingOnChange =
-    (propertyName: string) => (multiloc: Multiloc) => {
-      setAttributesDiff((attributesDiff) => ({
-        ...attributesDiff,
-        settings: {
-          ...get(attributesDiff, 'settings', {}),
-          core: {
-            ...get(attributesDiff, 'settings.core', {}),
-            [propertyName]: multiloc,
-          },
-        },
-      }));
-    };
-
-  const handleLocalesOnChange = (selectedLocaleOptions: IOption[]) => {
-    setAttributesDiff((attributesDiff) => ({
-      ...attributesDiff,
-      settings: {
-        ...get(attributesDiff, 'settings', {}),
-        core: {
-          ...get(attributesDiff, 'settings.core', {}),
-          locales: selectedLocaleOptions.map((option) => option.value),
-        },
-      },
-    }));
-  };
-
-  const handleUrlOnChange = (url: string) => {
-    setAttributesDiff((attributesDiff) => ({
-      ...attributesDiff,
-      settings: {
-        ...get(attributesDiff, 'settings', {}),
-        core: {
-          ...get(attributesDiff, 'settings.core', {}),
-          organization_site: url,
-        },
-      },
-    }));
-  };
-
-  const save = async (formValues: FormValues) => {
+  const handleOnSubmit = async (formValues: FormValues) => {
     await updateAppConfigurationCore(formValues);
     // if (isCLErrorJSON(e)) {
     //   const errors = e.json.errors;
@@ -178,23 +84,6 @@ const SettingsGeneralTab = ({ defaultValues }: Props) => {
     //   }
     // }
   };
-
-  const getLocaleOptions = () => {
-    return map(appLocalePairs, (label, locale) => ({
-      label,
-      value: locale,
-    }));
-  };
-
-  const localesToOptions = (locales) => {
-    return locales.map((locale) => ({
-      value: locale,
-      label: appLocalePairs[locale],
-    }));
-  };
-
-  const handleOrganizatioNameOnChange =
-    handleCoreMultilocSettingOnChange('organization_name');
 
   const onToggleBlockProfanitySetting = () => {
     if (
@@ -255,119 +144,22 @@ const SettingsGeneralTab = ({ defaultValues }: Props) => {
   };
 
   if (!isNilOrError(appConfiguration)) {
-    const updatedLocales = get(attributesDiff, 'settings.core.locales');
-
-    let appConfigAttrs = appConfiguration
-      ? merge({}, appConfiguration.attributes, attributesDiff)
-      : merge({}, attributesDiff);
-
-    // Prevent merging the arrays of locales
-    if (updatedLocales) {
-      appConfigAttrs = set(
-        appConfigAttrs,
-        'settings.core.locales',
-        updatedLocales
-      );
-    }
-
-    const appConfigLocales: string[] | null = get(
-      appConfigAttrs,
-      'settings.core.locales',
-      null
-    );
-    const organizationType: string | null = get(
-      appConfigAttrs,
-      'settings.core.organization_type',
-      null
-    );
-    const appConfigSite: string | null = get(
-      appConfigAttrs,
-      'settings.core.organization_site',
-      null
-    );
-    const organizationNameMultiloc: Multiloc | null = get(
-      appConfigAttrs,
-      'settings.core.organization_name',
-      null
-    );
-    const localeOptions = getLocaleOptions();
-    const selectedLocaleOptions = localesToOptions(appConfigLocales);
     const profanityBlockerSetting =
       appConfiguration.attributes.settings.blocking_profanity;
 
+    const { organization_name, organization_site, locales } =
+      appConfiguration.attributes.settings.core;
+
     return (
       <>
-        <FormProvider {...methods}>
-          <form onSubmit={methods.handleSubmit(onFormSubmit)}>
-            <SectionTitle>
-              <FormattedMessage {...messages.titleBasic} />
-            </SectionTitle>
-            <StyledSection>
-              <SubSectionTitle>
-                <FormattedMessage {...messages.platformConfiguration} />
-              </SubSectionTitle>
-              <SectionDescription>
-                <FormattedMessage {...messages.subtitleBasic} />
-              </SectionDescription>
-
-              <SectionField>
-                <InputMultilocWithLocaleSwitcher
-                  type="text"
-                  id="organization_name"
-                  label={
-                    <FormattedMessage
-                      {...messages.organizationName}
-                      values={{ type: organizationType }}
-                    />
-                  }
-                  valueMultiloc={organizationNameMultiloc}
-                  onChange={handleOrganizatioNameOnChange}
-                />
-              </SectionField>
-
-              <SectionField>
-                <Label>
-                  <FormattedMessage {...messages.languages} />
-                  <IconTooltip
-                    content={
-                      <FormattedMessage {...messages.languagesTooltip} />
-                    }
-                  />
-                </Label>
-                <MultipleSelect
-                  placeholder=""
-                  value={selectedLocaleOptions}
-                  onChange={handleLocalesOnChange}
-                  options={localeOptions}
-                />
-              </SectionField>
-
-              <SectionField>
-                <Label>
-                  <FormattedMessage {...messages.urlTitle} />
-                  <IconTooltip
-                    content={formatMessage(messages.urlTitleTooltip)}
-                  />
-                </Label>
-                <Input
-                  type="text"
-                  placeholder="https://..."
-                  onChange={handleUrlOnChange}
-                  value={appConfigSite}
-                  error={hasUrlError ? formatMessage(messages.urlError) : null}
-                />
-              </SectionField>
-              <Box display="flex">
-                <Button
-                  type="submit"
-                  processing={methods.formState.isSubmitting}
-                >
-                  {formatMessage(messages.save)}
-                </Button>
-              </Box>
-            </StyledSection>
-          </form>
-        </FormProvider>
+        <Form
+          defaultValues={{
+            organization_name,
+            organization_site,
+            locales,
+          }}
+          onSubmit={handleOnSubmit}
+        />
         <StyledSection>
           <SubSectionTitle>
             <FormattedMessage {...messages.contentModeration} />
