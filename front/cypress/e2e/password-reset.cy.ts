34771e21dac85ef80e9769252508896c263fa702
Edwin Kato <katedwin88@gmail.com>
2023-02-07 13:08:14 +0300

    [CL-737] Correct test for non-existent email

--- front/cypress/e2e/password-reset.cy.ts
+++ front/cypress/e2e/password-reset.cy.ts
@@ -7,15 +7,15 @@ const tryReset = (email: string) => {
   cy.get('.e2e-submit-reset').find('button').click();
 };
 
-describe('profile deletion', () => {
+describe('password reset', () => {
   beforeEach(() => {
     cy.visit('/en/password-recovery');
     cy.acceptCookies();
   });
-  it("prints an error if the email doesn't exist", () => {
+  it("shows no error if email doesn't exist", () => {
     tryReset(someEmail);
-    cy.get('.e2e-input-error').should('exist');
-    cy.get('.e2e-success-reset').should('not.exist');
+    cy.get('.e2e-input-error').should('not.exist');
+    cy.get('.e2e-success-reset').should('exist');
   });
   it('is successful if the email does exist', () => {
     tryReset('admin@citizenlab.co');
