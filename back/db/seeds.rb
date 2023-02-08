4b112ebcd902d4c960d850d106c9a6586e5ac92b
Alexander <alexander@citizenlab.co>
2023-02-08 11:31:25 +0100

    Make it easier to use seeds runner in Rails console
    So, it's possible to use this code to create only projects without the rest
    MultiTenancy::Seeds::Projects.new(runner: MultiTenancy::Seeds::Runner.new).run

--- back/db/seeds.rb
+++ back/db/seeds.rb
@@ -2,6 +2,8 @@
 
 if CitizenLab.ee?
   load Rails.root.join('engines/commercial/multi_tenancy/db/seeds/runner.rb')
+  seeds_runner = MultiTenancy::Seeds::Runner.new
+  seeds_runner.execute
 else
   load Rails.root.join('db/seeds/citizenlab.rb')
 end
