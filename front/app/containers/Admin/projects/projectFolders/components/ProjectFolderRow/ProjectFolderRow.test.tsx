de9cdd209816e8226ac2fe110deac7d4c4932b81
Edwin Kato <katedwin88@gmail.com>
2023-02-13 12:23:06 +0300

    [CL-2769] Add moderator tests on ProjectFolderRow component

--- front/app/containers/Admin/projects/projectFolders/components/ProjectFolderRow/ProjectFolderRow.test.tsx
+++ front/app/containers/Admin/projects/projectFolders/components/ProjectFolderRow/ProjectFolderRow.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ProjectFolderRow, { Props } from '.';
 import { render, screen } from 'utils/testUtils/rtl';
 import { IAdminPublicationContent } from 'hooks/useAdminPublications';
+import { IUserData } from 'services/users';
 
 const publication: IAdminPublicationContent = {
   id: '1',
@@ -30,10 +31,27 @@ const publication: IAdminPublicationContent = {
 };
 
 // Needed to render moreActionsMenu
+const mockUserData: IUserData = {
+  id: 'userId',
+  type: 'user',
+  attributes: {
+    first_name: 'Stewie',
+    last_name: 'McKenzie',
+    locale: 'en',
+    slug: 'stewie-mckenzie',
+    highest_role: 'admin',
+    bio_multiloc: {},
+    roles: [{ type: 'admin' }],
+    registration_completed_at: '',
+    created_at: '',
+    updated_at: '',
+    unread_notifications: 0,
+    invite_status: null,
+    confirmation_required: false,
+  },
+};
 jest.mock('hooks/useAuthUser', () => {
-  return () => ({
-    id: 'userId',
-  });
+  return () => mockUserData;
 });
 
 const props: Props = {
@@ -42,12 +60,12 @@ const props: Props = {
 
 describe('ProjectFolderRow', () => {
   describe('When user is an admin', () => {
-    // it('shows the edit button', () => {
-    //   render(<ProjectFolderRow {...props} />);
+    it('shows the edit button', () => {
+      render(<ProjectFolderRow {...props} />);
 
-    //   const editButton = screen.getByRole('button', { name: 'Edit' });
-    //   expect(editButton).toBeInTheDocument();
-    // });
+      const editButton = screen.getByRole('button', { name: 'Manage' });
+      expect(editButton).toBeInTheDocument();
+    });
 
     it('shows the MoreActionsMenu', () => {
       render(<ProjectFolderRow {...props} />);
@@ -58,20 +76,41 @@ describe('ProjectFolderRow', () => {
   });
 
   describe('When user is a folder moderator', () => {
-    // it('shows the edit button for a folder the user is a moderator of', () => {
-    //   render(<ProjectFolderRow {...props} />);
-    //   const editButton = screen.getByRole('button', { name: 'Edit' });
-    //   expect(editButton).toBeInTheDocument();
-    // });
-    // it('shows a disabled edit button for a folder the user is not a moderator of', () => {
-    //   render(<ProjectFolderRow {...props} />);
-    //   const editButton = screen.getByRole('button', { name: 'Edit' });
-    //   expect(editButton).toBeDisabled();
-    // });
-    // it('does not show the MoreActionsMenu', () => {
-    //   render(<ProjectFolderRow {...props} />);
-    //   const moreActionsMenu = screen.queryByTestId('folderMoreActionsMenu');
-    //   expect(moreActionsMenu).toBeNull();
-    // });
+    beforeAll(() => {
+      mockUserData.attributes.highest_role = 'project_moderator';
+    });
+
+    it('shows the edit button for a folder the user is a moderator of', () => {
+      mockUserData.attributes.roles = [
+        {
+          type: 'project_folder_moderator',
+          project_folder_id: 'publicationId',
+        },
+      ];
+
+      render(<ProjectFolderRow {...props} />);
+      const editButton = screen.getByRole('button', { name: 'Manage' });
+      expect(editButton).toBeInTheDocument();
+    });
+
+    it('shows a disabled edit button for a folder the user is not a moderator of', () => {
+      mockUserData.attributes.roles = [
+        { type: 'project_folder_moderator', project_folder_id: 'testId' },
+      ];
+
+      render(<ProjectFolderRow {...props} />);
+      const editButton = screen.getByTestId('edit-button');
+      expect(editButton).toHaveAttribute('disabled');
+    });
+
+    it('does not show the MoreActionsMenu', () => {
+      mockUserData.attributes.roles = [
+        { type: 'project_folder_moderator', project_folder_id: 'folderId' },
+      ];
+
+      render(<ProjectFolderRow {...props} />);
+      const moreOptions = screen.queryByTestId('moreOptionsButton');
+      expect(moreOptions).toBeNull();
+    });
   });
 });
