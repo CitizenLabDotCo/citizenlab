b7bd5747702691b56071a7f30515f5c267c31e2e
Fran <francisco.pensa.web@gmail.com>
2023-02-14 13:45:03 -0300

    [CL-2659] update test

--- front/cypress/e2e/report_builder/widgets/comments_by_time_widget.cy.ts
+++ front/cypress/e2e/report_builder/widgets/comments_by_time_widget.cy.ts
@@ -1,6 +1,26 @@
+import { randomString } from '../../../support/commands';
+
 describe('Report builder Comments By Time widget', () => {
   beforeEach(() => {
     cy.setAdminLoginCookie();
+
+    cy.apiCreateProject({
+      type: 'continuous',
+      title: randomString(),
+      descriptionPreview: randomString(),
+      description: randomString(),
+      publicationStatus: 'published',
+      participationMethod: 'ideation',
+    }).then((project) => {
+      const projectId = project.body.data.id;
+      cy.wrap(projectId).as('projectId');
+      cy.apiCreateIdea(projectId, randomString(), randomString()).then(
+        (idea) => {
+          cy.apiAddComment(idea.body.data.id, 'idea', randomString());
+        }
+      );
+    });
+
     cy.apiCreateReportBuilder().then((report) => {
       const reportId = report.body.data.id;
       cy.wrap(reportId).as('reportId');
@@ -14,6 +34,9 @@ describe('Report builder Comments By Time widget', () => {
   afterEach(() => {
     cy.get<string>('@reportId').then((reportId) => {
       cy.apiRemoveReportBuilder(reportId);
+    });
+    cy.get<string>('@projectId').then((projectId) => {
+      cy.apiRemoveProject(projectId);
     });
   });
 
@@ -30,11 +53,19 @@ describe('Report builder Comments By Time widget', () => {
       .clear()
       .type('New Widget Title');
 
-    // Confirms that button displays and functions correctly on live page
+    // Set project filter
+    cy.get('#e2e-report-builder-project-filter-box select').select(
+      this.projectId
+    );
+
+    // Confirms that the widget displays correctly on live report
     cy.get('#e2e-content-builder-topbar-save').click();
     cy.wait('@saveReportLayout');
     cy.visit(`/admin/reporting/report-builder/${this.reportId}/viewer`);
+    cy.get('.recharts-surface:first').trigger('mouseover');
+    cy.pause();
     cy.contains('New Widget Title').should('exist');
+    cy.contains('Total : 1').should('be.visible');
   });
 
   it('deletes Comments By Time widget correctly', function () {
