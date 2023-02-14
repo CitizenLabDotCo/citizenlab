f54ad0778e79446d949b8b0b7a9f05760d192e6a
brentguf <brentguf@users.noreply.github.com>
2023-02-14 18:00:43 +0100

    Test for regular user

--- front/app/containers/HomePage/HomePage.test.tsx
+++ front/app/containers/HomePage/HomePage.test.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import { IUserData } from 'services/users';
 import clHistory from 'utils/cl-router/history';
 import { render, userEvent } from 'utils/testUtils/rtl';
 import HomePage, { adminRedirectPath } from '.';
 
-const mockUser = {
+const mockUserData: IUserData = {
   id: 'userId',
+  type: 'user',
   attributes: {
+    first_name: 'Stewie',
+    last_name: 'McKenzie',
+    locale: 'en',
+    slug: 'stewie-mckenzie',
+    highest_role: 'admin',
+    bio_multiloc: {},
     roles: [{ type: 'admin' }],
+    registration_completed_at: '',
+    created_at: '',
+    updated_at: '',
+    unread_notifications: 0,
+    invite_status: null,
+    confirmation_required: false,
   },
 };
-
 jest.mock('hooks/useHomepageSettings');
 jest.mock('hooks/useAuthUser', () => {
-  return () => mockUser;
+  return () => mockUserData;
 });
 jest.mock('hooks/useAppConfiguration', () => {
   return jest.fn(() => ({
@@ -36,5 +49,16 @@ describe('HomePage', () => {
     await user.keyboard('a');
 
     expect(clHistory.push).toHaveBeenCalledWith(adminRedirectPath);
+  });
+
+  it('Does not redirect if you do not have admin access', async () => {
+    mockUserData.attributes.roles = [];
+    mockUserData.attributes.highest_role = 'user';
+
+    render(<HomePage />);
+    const user = userEvent.setup();
+    await user.keyboard('a');
+
+    expect(clHistory.push).not.toHaveBeenCalledWith(adminRedirectPath);
   });
 });
