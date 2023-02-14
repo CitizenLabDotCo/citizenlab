72c7a90f9651c6c8faa42d64a1b92a0c64a7140e
brentguf <brentguf@users.noreply.github.com>
2023-02-14 17:41:51 +0100

    Remove unnecessary appConfig mockData

--- front/app/containers/HomePage/HomePage.test.tsx
+++ front/app/containers/HomePage/HomePage.test.tsx
@@ -3,16 +3,6 @@ import clHistory from 'utils/cl-router/history';
 import { render, userEvent } from 'utils/testUtils/rtl';
 import HomePage, { adminRedirectPath } from '.';
 
-const mockHomepageSettings = {
-  attributes: {
-    banner_layout: 'full_width_banner_layout',
-    banner_signed_out_header_multiloc: { en: 'Signed out header' },
-    banner_signed_out_subheader_multiloc: { en: 'Signed out subhead' },
-    banner_signed_in_header_multiloc: { en: 'Signed in header' },
-    header_bg: { large: 'https://example.com/image.png' },
-  },
-};
-
 const mockUser = {
   id: 'userId',
   attributes: {
@@ -20,9 +10,7 @@ const mockUser = {
   },
 };
 
-jest.mock('hooks/useHomepageSettings', () => {
-  return jest.fn(() => mockHomepageSettings);
-});
+jest.mock('hooks/useHomepageSettings');
 jest.mock('hooks/useAuthUser', () => {
   return () => mockUser;
 });
