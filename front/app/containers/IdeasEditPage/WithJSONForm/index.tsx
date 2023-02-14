be35c0a92f7c9dbe5686415bdbfa16411a78367f
Amanda Anderson <amanda.anderson@citizenlab.co>
2023-02-14 17:24:43 +0100

    [CL-2830] Clean up code and organize imports

--- front/app/containers/IdeasEditPage/WithJSONForm/index.tsx
+++ front/app/containers/IdeasEditPage/WithJSONForm/index.tsx
@@ -1,33 +1,39 @@
 import React, { useCallback, useContext, useEffect } from 'react';
-import { PreviousPathnameContext } from 'context';
 
-import { WithRouterProps } from 'utils/cl-router/withRouter';
-import clHistory from 'utils/cl-router/history';
+// components
+import PageContainer from 'components/UI/PageContainer';
+import FullPageSpinner from 'components/UI/FullPageSpinner';
+import { Box } from '@citizenlab/cl2-component-library';
+import ideaFormMessages from 'containers/IdeasNewPage/messages';
+import Form, { AjvErrorGetter, ApiErrorGetter } from 'components/Form';
+import GoBackToIdeaPage from 'containers/IdeasEditPage/GoBackToIdeaPage';
+import IdeasEditMeta from '../IdeasEditMeta';
 
-import { isError, isNilOrError } from 'utils/helperUtils';
+// services
+import { deleteIdeaImage } from 'services/ideaImages';
+import { usePermission } from 'services/permissions';
+import { updateIdea } from 'services/ideas';
+
+// hooks
+import useIdea from 'hooks/useIdea';
 import useAuthUser from 'hooks/useAuthUser';
 import useProject from 'hooks/useProject';
 import useInputSchema from 'hooks/useInputSchema';
 import useIdeaImages from 'hooks/useIdeaImages';
 import useResourceFiles from 'hooks/useResourceFiles';
 
+// intl
 import { FormattedMessage } from 'utils/cl-intl';
 import messages from '../messages';
-import ideaFormMessages from 'containers/IdeasNewPage/messages';
 
-import Form, { AjvErrorGetter, ApiErrorGetter } from 'components/Form';
-
-import PageContainer from 'components/UI/PageContainer';
-import FullPageSpinner from 'components/UI/FullPageSpinner';
-import { Box } from '@citizenlab/cl2-component-library';
-import { updateIdea } from 'services/ideas';
-import useIdea from 'hooks/useIdea';
-import IdeasEditMeta from '../IdeasEditMeta';
-import { usePermission } from 'services/permissions';
-import { getFieldNameFromPath } from 'utils/JSONFormUtils';
-import { deleteIdeaImage } from 'services/ideaImages';
-import GoBackToIdeaPage from 'containers/IdeasEditPage/GoBackToIdeaPage';
+// utils
 import { getLocationGeojson } from '../utils';
+import { omit } from 'lodash-es';
+import { isError, isNilOrError } from 'utils/helperUtils';
+import { WithRouterProps } from 'utils/cl-router/withRouter';
+import clHistory from 'utils/cl-router/history';
+import { getFieldNameFromPath } from 'utils/JSONFormUtils';
+import { PreviousPathnameContext } from 'context';
 
 const IdeasEditPageWithJSONForm = ({ params: { ideaId } }: WithRouterProps) => {
   const previousPathName = useContext(PreviousPathnameContext);
@@ -114,12 +120,11 @@ const IdeasEditPageWithJSONForm = ({ params: { ideaId } }: WithRouterProps) => {
       data
     );
 
+    const isImageNew =
+      idea_images_attributes !== initialFormData?.idea_images_attributes;
+
     // Delete a remote image only on submission
-    if (
-      idea_images_attributes !== initialFormData?.idea_images_attributes &&
-      initialFormData?.idea_images_attributes !== undefined &&
-      initialFormData?.idea_images_attributes.length !== 0
-    ) {
+    if (isImageNew && initialFormData?.idea_images_attributes !== undefined) {
       try {
         deleteIdeaImage(ideaId, initialFormData?.idea_images_attributes[0].id);
       } catch (e) {
@@ -127,30 +132,21 @@ const IdeasEditPageWithJSONForm = ({ params: { ideaId } }: WithRouterProps) => {
       }
     }
 
-    if (idea_images_attributes !== initialFormData?.idea_images_attributes) {
-      const idea = await updateIdea(ideaId, {
-        ...ideaWithoutFiles,
-        idea_images_attributes,
-        location_point_geojson,
-        project_id: project?.id,
-        publication_status: 'published',
-      });
+    const payload = {
+      ...ideaWithoutFiles,
+      idea_images_attributes,
+      location_point_geojson,
+      project_id: project?.id,
+      publication_status: 'published',
+    };
 
-      clHistory.push({
-        pathname: `/ideas/${idea.data.attributes.slug}`,
-      });
-    } else {
-      const idea = await updateIdea(ideaId, {
-        ...ideaWithoutFiles,
-        location_point_geojson,
-        project_id: project?.id,
-        publication_status: 'published',
-      });
-
-      clHistory.push({
-        pathname: `/ideas/${idea.data.attributes.slug}`,
-      });
-    }
+    const idea = await updateIdea(
+      ideaId,
+      isImageNew ? payload : omit(payload, 'idea_images_attributes')
+    );
+    clHistory.push({
+      pathname: `/ideas/${idea.data.attributes.slug}`,
+    });
   };
 
   const getApiErrorMessage: ApiErrorGetter = useCallback(
