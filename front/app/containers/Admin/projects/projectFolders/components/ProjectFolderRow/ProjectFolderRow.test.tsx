ebd2d540cbfe8a0fe61435786b36eb1f1a3f84e1
brentguf <brentguf@users.noreply.github.com>
2023-02-15 11:04:19 +0100

    Add new props to test

--- front/app/containers/Admin/projects/projectFolders/components/ProjectFolderRow/ProjectFolderRow.test.tsx
+++ front/app/containers/Admin/projects/projectFolders/components/ProjectFolderRow/ProjectFolderRow.test.tsx
@@ -56,6 +56,9 @@ jest.mock('hooks/useAuthUser', () => {
 
 const props: Props = {
   publication,
+  toggleFolder: jest.fn,
+  folderOpen: true,
+  hasProjects: true,
 };
 
 describe('ProjectFolderRow', () => {
