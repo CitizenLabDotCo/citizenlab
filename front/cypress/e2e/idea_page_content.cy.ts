f061253f330b55672d4a7407ed2f6bc57f1bbd95
Alexander <alexander@citizenlab.co>
2023-02-17 11:12:55 +0100

    Revert "Skip flaky test"
    This reverts commit 4f374ca0293b37679510939b901026de92f69d9e.

--- front/cypress/e2e/idea_page_content.cy.ts
+++ front/cypress/e2e/idea_page_content.cy.ts
@@ -94,7 +94,7 @@ describe('Idea Page', () => {
     const ideaTitle = randomString();
     const ideaContent = randomString();
 
-    before(() => {
+    beforeEach(() => {
       cy.apiCreateProject({
         type: 'continuous',
         title: projectTitle,
@@ -112,16 +112,14 @@ describe('Idea Page', () => {
         });
     });
 
-    it.skip('shows idea sharing modal if new idea', () => {
-      cy.visit(
-        `/ideas/${ideaTitle}?new_idea_id=7df35672-5b1b-442a-8e0a-8933e8dc47f2`
-      );
-      cy.get('#e2e-modal-container').should('exist');
+    it('shows idea sharing modal if new idea', () => {
+      cy.visit(`/ideas/${ideaTitle}?new_idea_id=${ideaId}`);
+      cy.get('#e2e-modal-container', { timeout: 20000 }).should('exist');
       cy.get('.e2e-modal-close-button').click();
       cy.get('#e2e-modal-container').should('not.exist');
     });
 
-    after(() => {
+    afterEach(() => {
       cy.apiRemoveIdea(ideaId);
       cy.apiRemoveProject(projectId);
     });
