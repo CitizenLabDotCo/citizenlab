da71605471dabfd7f82e6d58fec58ee72cf3ddf9
Simon Tharby <simontharby@gmail.com>
2023-02-18 17:54:20 +0000

    [CL-2901] Add LogActivityJob for every deleted orphaned image

--- back/app/jobs/delete_orphaned_text_images_job.rb
+++ back/app/jobs/delete_orphaned_text_images_job.rb
@@ -1,19 +1,28 @@
 # frozen_string_literal: true
 
 class DeleteOrphanedTextImagesJob < ApplicationJob
+  include SideFxHelper
+
   self.priority = 90 # pretty low priority (lowest is 100)
 
   # @param [Project,NilClass] resource
   # @param [User,NilClass] current_user
-  def run(resource, _current_user)
+  def run(resource, user)
     return unless resource.class.exists?(resource.id)
 
     resource_as_json = resource.to_json
 
     resource.text_images.each do |image|
-      text_image.destroy! unless resource_as_json.include?(image.text_reference)
+      next if resource_as_json.include?(image.text_reference)
 
-      # Add LogActivityJob
+      image.destroy!
+
+      image_data = image.attributes
+      LogActivityJob.perform_later(
+        ['TextImage', image.id].join('/'), 'deleted_orphan',
+        user, Time.now.to_i,
+        payload: { text_image: image_data }
+      )
     end
   end
 end
