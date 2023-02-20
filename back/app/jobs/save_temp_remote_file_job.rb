5e5e201e0a4cf6ec2205ef0bcadf3079db9df900
Alexander <alexander@citizenlab.co>
2023-02-20 23:23:00 +0100

    [CL-2842] Make SaveTempRemoteFileJob more important than default

--- back/app/jobs/save_temp_remote_file_job.rb
+++ back/app/jobs/save_temp_remote_file_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SaveTempRemoteFileJob < ApplicationJob
-  self.priority = 100
+  self.priority = 30 # So, it's more important than default 50
 
   def run(model, remote_url_field_names)
     CarrierwaveTempRemote.save_files!(model, remote_url_field_names)
