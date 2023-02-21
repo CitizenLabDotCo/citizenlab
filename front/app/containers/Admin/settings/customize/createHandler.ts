581a04190690e796885fa0d7bd1cb4dfa59926f8
Iva <iva@citizenlab.co>
2023-02-21 15:16:00 +0200

    [CL-2896] Delete dead code

--- front/app/containers/Admin/settings/customize/createHandler.ts
+++ front/app/containers/Admin/settings/customize/createHandler.ts
@@ -1,40 +1,5 @@
 import { get } from 'lodash-es';
-import { UploadFile, Multiloc } from 'typings';
-
-type TUploadType = 'logo' | 'header_bg';
-
-const setErrors = (state, type: TUploadType) => ({
-  logoError: type === 'logo' ? null : state.logoError,
-  headerError: type === 'header_bg' ? null : state.headerError,
-});
-
-export function createAddUploadHandler(type: TUploadType, setState) {
-  return (newImage: UploadFile[]) => {
-    setState((state) => ({
-      ...state,
-      ...setErrors(state, type),
-      [type]: [newImage[0]],
-      attributesDiff: {
-        ...(state.attributesDiff || {}),
-        [type]: newImage[0].base64,
-      },
-    }));
-  };
-}
-
-export function createRemoveUploadHandler(type: TUploadType, setState) {
-  return () => {
-    setState((state) => ({
-      ...state,
-      ...setErrors(state, type),
-      [type]: null,
-      attributesDiff: {
-        ...(state.attributesDiff || {}),
-        [type]: null,
-      },
-    }));
-  };
-}
+import { Multiloc } from 'typings';
 
 export function createCoreMultilocHandler(property: string, setState) {
   return (multiloc: Multiloc) => {
