78e15e61d2b7e34d283d4e77312f09f58b55da56
brentguf <brentguf@users.noreply.github.com>
2023-02-22 11:39:09 +0100

    Disable creating new campaigns for non-admins

--- front/app/containers/Admin/messaging/CustomEmails/All/NewCampaignButton.tsx
+++ front/app/containers/Admin/messaging/CustomEmails/All/NewCampaignButton.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 import Button from 'components/UI/Button';
 import { FormattedMessage } from 'utils/cl-intl';
 import messages from '../../messages';
+import useAuthUser from 'hooks/useAuthUser';
+import { isAdmin } from 'services/permissions/roles';
+import { isNilOrError } from 'utils/helperUtils';
 
 const NewCampaignButton = () => {
+  const authUser = useAuthUser();
+
+  if (isNilOrError(authUser)) {
+    return null;
+  }
+
+  const canCreateNewCampaign = isAdmin({ data: authUser });
+
   return (
     <Button
       buttonStyle="cl-blue"
       icon="plus-circle"
       linkTo="/admin/messaging/emails/custom/new"
+      disabled={!canCreateNewCampaign}
     >
       <FormattedMessage {...messages.addCampaignButton} />
     </Button>
