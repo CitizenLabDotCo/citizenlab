881b36a68f123c6e4612b02039ca5d0b0153d7e3
Adrien Dessy <adrien@citizenlab.co>
2023-02-27 11:30:24 +0100

    [CL-2913] Raise an error explicitly for unsupported connection adapters
    Each connection adapter should provide its own implementation for `ActiveRecord::ConnectionAdapters::AbstractAdapter#generate_advisory_lock_id`.

--- back/lib/gem_extensions/active_record/connection_adapters/abstract_adapter.rb
+++ back/lib/gem_extensions/active_record/connection_adapters/abstract_adapter.rb
@@ -4,7 +4,15 @@ module GemExtensions
   module ActiveRecord
     module ConnectionAdapters
       module AbstractAdapter
-        def generate_advisory_lock_id; end
+        def generate_advisory_lock_id
+          # Each connection adapter must provide its own implementation.
+          # For instance, this could look like to:
+          #   def generate_advisory_lock_id
+          #     db_name_hash = Zlib.crc32(current_database)
+          #     Migrator::MIGRATOR_SALT * db_name_hash
+          #   end
+          raise NotImplementedError
+        end
       end
     end
   end
