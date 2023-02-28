479dcaebbc967fbff6dfc29ee2bd5ef4358dc0b0
Edwin Kato <katedwin88@gmail.com>
2023-02-28 05:28:18 +0300

    [CL-2987] Move intiative images types to types file

---
+++ front/app/api/initiative_images/types.ts
@@ -0,0 +1,24 @@
+import { Keys } from 'utils/cl-react-query/types';
+import initiativeImagesKeys from './keys';
+import { ImageSizes } from 'typings';
+
+export type InitiativeImagesKeys = Keys<typeof initiativeImagesKeys>;
+
+export interface IInitiativeImageData {
+  id: string;
+  type: string;
+  attributes: {
+    versions: ImageSizes;
+    ordering: number;
+    created_at: string;
+    updated_at: string;
+  };
+}
+
+export interface IInitiativeImage {
+  data: IInitiativeImageData;
+}
+
+export interface IInitiativeImages {
+  data: IInitiativeImageData[];
+}
