6a831807e2dab4867b3956a01b4d032be8d8e60d
brentguf <brentguf@users.noreply.github.com>
2023-02-28 14:48:32 +0100

    useIdea corrections

--- front/app/api/ideas/useIdea.ts
+++ front/app/api/ideas/useIdea.ts
@@ -4,14 +4,14 @@ import fetcher from 'utils/cl-react-query/fetcher';
 import ideasKeys from './keys';
 import { IIdea, IdeasKeys } from './types';
 
-const fetchIdeaStatus = (id: string) =>
-  fetcher<IIdea>({ path: `/idea_statuses/${id}`, action: 'get' });
+const fetchIdea = (id: string) =>
+  fetcher<IIdea>({ path: `/ideas/${id}`, action: 'get' });
 
-const useIdeaStatus = (id: string) => {
+const useIdea = (id: string) => {
   return useQuery<IIdea, CLErrors, IIdea, IdeasKeys>({
     queryKey: ideasKeys.item(id),
-    queryFn: () => fetchIdeaStatus(id),
+    queryFn: () => fetchIdea(id),
   });
 };
 
-export default useIdeaStatus;
+export default useIdea;
