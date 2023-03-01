810c3b8fe2e9c5bf097d51845941718ee888a87a
Edwin Kato <katedwin88@gmail.com>
2023-03-01 08:41:57 +0300

    [CL-2986] Change InitiativeContent component to functional component

--- front/app/components/admin/PostManager/components/LazyPostPreview/Initiative/InitiativeContent.tsx
+++ front/app/components/admin/PostManager/components/LazyPostPreview/Initiative/InitiativeContent.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { isNilOrError } from 'utils/helperUtils';
 import { adopt } from 'react-adopt';
 import { get } from 'lodash-es';
@@ -104,8 +104,6 @@ const DaysLeft = styled.div`
   margin-bottom: 20px;
 `;
 
-interface State {}
-
 export interface InputProps {
   initiativeId: string;
   closePreview: () => void;
@@ -121,15 +119,18 @@ interface DataProps {
 
 interface Props extends InputProps, DataProps {}
 
-export class InitiativeContent extends PureComponent<
-  Props & InjectedLocalized & WrappedComponentProps,
-  State
-> {
-  handleClickDelete = () => {
-    const { initiative, closePreview } = this.props;
-    const message = this.props.intl.formatMessage(
-      messages.deleteInitiativeConfirmation
-    );
+const InitiativeContent = ({
+  initiative,
+  localize,
+  initiativeImages,
+  initiativeFiles,
+  handleClickEdit,
+  locale,
+  closePreview,
+  intl,
+}: Props & InjectedLocalized & WrappedComponentProps) => {
+  const handleClickDelete = () => {
+    const message = intl.formatMessage(messages.deleteInitiativeConfirmation);
 
     if (!isNilOrError(initiative)) {
       if (window.confirm(message)) {
@@ -139,127 +140,110 @@ export class InitiativeContent extends PureComponent<
     }
   };
 
-  render() {
-    const {
-      initiative,
-      localize,
-      initiativeImages,
-      initiativeFiles,
-      handleClickEdit,
-      locale,
-    } = this.props;
+  if (!isNilOrError(initiative) && !isNilOrError(locale)) {
+    const initiativeId = initiative.id;
+    const initiativeTitle = localize(initiative.attributes.title_multiloc);
+    const initiativeImageLarge =
+      !isNilOrError(initiativeImages) && initiativeImages.length > 0
+        ? get(initiativeImages[0], 'attributes.versions.large', null)
+        : null;
+    const initiativeGeoPosition =
+      initiative.attributes.location_point_geojson || null;
+    const initiativeAddress =
+      initiative.attributes.location_description || null;
+    const daysLeft = getPeriodRemainingUntil(initiative.attributes.expires_at);
 
-    if (!isNilOrError(initiative) && !isNilOrError(locale)) {
-      const initiativeId = initiative.id;
-      const initiativeTitle = localize(initiative.attributes.title_multiloc);
-      const initiativeImageLarge =
-        !isNilOrError(initiativeImages) && initiativeImages.length > 0
-          ? get(initiativeImages[0], 'attributes.versions.large', null)
-          : null;
-      const initiativeGeoPosition =
-        initiative.attributes.location_point_geojson || null;
-      const initiativeAddress =
-        initiative.attributes.location_description || null;
-      const daysLeft = getPeriodRemainingUntil(
-        initiative.attributes.expires_at
-      );
+    return (
+      <Container>
+        <Top>
+          <Button
+            icon="edit"
+            buttonStyle="text"
+            textColor={colors.primary}
+            onClick={handleClickEdit}
+          >
+            <FormattedMessage {...messages.edit} />
+          </Button>
+          <Button
+            icon="delete"
+            buttonStyle="text"
+            textColor={colors.primary}
+            onClick={handleClickDelete}
+          >
+            <FormattedMessage {...messages.delete} />
+          </Button>
+        </Top>
+        <Content>
+          <StyledTitle
+            postId={initiativeId}
+            title={initiativeTitle}
+            postType="initiative"
+          />
+          <Row>
+            <Left>
+              {initiativeImageLarge && (
+                <Image
+                  src={initiativeImageLarge}
+                  alt=""
+                  className="e2e-initiativeImage"
+                />
+              )}
 
-      return (
-        <Container>
-          <Top>
-            <Button
-              icon="edit"
-              buttonStyle="text"
-              textColor={colors.primary}
-              onClick={handleClickEdit}
-            >
-              <FormattedMessage {...messages.edit} />
-            </Button>
-            <Button
-              icon="delete"
-              buttonStyle="text"
-              textColor={colors.primary}
-              onClick={this.handleClickDelete}
-            >
-              <FormattedMessage {...messages.delete} />
-            </Button>
-          </Top>
-          <Content>
-            <StyledTitle
-              postId={initiativeId}
-              title={initiativeTitle}
-              postType="initiative"
-            />
-            <Row>
-              <Left>
-                {initiativeImageLarge && (
-                  <Image
-                    src={initiativeImageLarge}
-                    alt=""
-                    className="e2e-initiativeImage"
+              <PostedBy
+                authorId={get(initiative, 'relationships.author.data.id', null)}
+                showAboutInitiatives={false}
+              />
+
+              <StyledBody
+                postId={initiativeId}
+                postType="initiative"
+                body={localize(initiative.attributes.body_multiloc)}
+                locale={locale}
+              />
+
+              {initiativeGeoPosition && initiativeAddress && (
+                <StyledMap
+                  address={initiativeAddress}
+                  position={initiativeGeoPosition}
+                />
+              )}
+
+              {initiativeFiles && !isNilOrError(initiativeFiles) && (
+                <Box mb="25px">
+                  <FileAttachments files={initiativeFiles} />
+                </Box>
+              )}
+
+              <StyledOfficialFeedback
+                postId={initiativeId}
+                postType="initiative"
+                // If the user has access to the post preview,
+                // it means they are in the admin and therefore have permission
+                permissionToPost
+              />
+
+              <StyledComments postId={initiativeId} postType="initiative" />
+            </Left>
+            <Right>
+              <VotePreview>
+                <DaysLeft>
+                  <FormattedMessage
+                    {...messages.xDaysLeft}
+                    values={{ x: daysLeft }}
                   />
-                )}
+                </DaysLeft>
+                <VoteIndicator initiativeId={initiativeId} />
+              </VotePreview>
 
-                <PostedBy
-                  authorId={get(
-                    initiative,
-                    'relationships.author.data.id',
-                    null
-                  )}
-                  showAboutInitiatives={false}
-                />
-
-                <StyledBody
-                  postId={initiativeId}
-                  postType="initiative"
-                  body={localize(initiative.attributes.body_multiloc)}
-                  locale={locale}
-                />
-
-                {initiativeGeoPosition && initiativeAddress && (
-                  <StyledMap
-                    address={initiativeAddress}
-                    position={initiativeGeoPosition}
-                  />
-                )}
-
-                {initiativeFiles && !isNilOrError(initiativeFiles) && (
-                  <Box mb="25px">
-                    <FileAttachments files={initiativeFiles} />
-                  </Box>
-                )}
-
-                <StyledOfficialFeedback
-                  postId={initiativeId}
-                  postType="initiative"
-                  // If the user has access to the post preview,
-                  // it means they are in the admin and therefore have permission
-                  permissionToPost
-                />
-
-                <StyledComments postId={initiativeId} postType="initiative" />
-              </Left>
-              <Right>
-                <VotePreview>
-                  <DaysLeft>
-                    <FormattedMessage
-                      {...messages.xDaysLeft}
-                      values={{ x: daysLeft }}
-                    />
-                  </DaysLeft>
-                  <VoteIndicator initiativeId={initiativeId} />
-                </VotePreview>
-
-                <FeedbackSettings initiativeId={initiativeId} />
-              </Right>
-            </Row>
-          </Content>
-        </Container>
-      );
-    }
-    return null;
+              <FeedbackSettings initiativeId={initiativeId} />
+            </Right>
+          </Row>
+        </Content>
+      </Container>
+    );
   }
-}
+  return null;
+};
 
 const Data = adopt<DataProps, InputProps>({
   initiative: ({ initiativeId, render }) => (
