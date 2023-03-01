b51ee0aa759a74fa18fc8b558dd455462c81e316
brentguf <brentguf@users.noreply.github.com>
2023-03-01 09:11:27 +0100

    Delete dead code

--- front/app/services/ideaVotes.ts
+++ front/app/services/ideaVotes.ts
@@ -58,16 +58,6 @@ export function voteStream(
   });
 }
 
-export function votesStream(
-  ideaId: string,
-  streamParams: IStreamParams | null = null
-) {
-  return streams.get<IIdeaVotes>({
-    apiEndpoint: `${API_PATH}/ideas/${ideaId}/votes`,
-    ...streamParams,
-  });
-}
-
 export async function addVote(
   ideaId: string,
   object: INewVoteProperties,
