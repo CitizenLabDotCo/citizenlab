b5c683a377702e2ba4600b2712ed4a65eac52eea
brentguf <brentguf@users.noreply.github.com>
2023-03-02 21:47:53 +0100

    Fix test

--- front/app/containers/IdeasShow/Buttons/IdeaSharingButton.test.tsx
+++ front/app/containers/IdeasShow/Buttons/IdeaSharingButton.test.tsx
@@ -5,10 +5,6 @@ import SharingButtonComponent from './SharingButtonComponent';
 import translationMessages from 'i18n/en';
 import { ideaData as mockIdeaData } from 'api/ideas/__mocks__/useIdeaById';
 
-jest.mock('api/ideas/useIdeaById', () => {
-  return jest.fn(() => ({ data: { data: mockIdeaData[0] } }));
-});
-
 const mockProjectData = {
   id: '2',
   type: 'project',
@@ -25,6 +21,10 @@ jest.mock('services/projects');
 jest.mock('services/auth');
 
 jest.mock('hooks/useProject', () => jest.fn(() => mockProjectData));
+
+jest.mock('api/ideas/useIdeaById', () => {
+  return jest.fn(() => ({ data: { data: mockIdeaData[0] } }));
+});
 
 describe('IdeaSharingButton', () => {
   it('should result in correct sharing URL', () => {
@@ -46,7 +46,7 @@ describe('IdeaSharingButton', () => {
     screen.getByLabelText('Share via WhatsApp').click();
     expect(screen.getByRole('link')).toHaveAttribute(
       'href',
-      'https://api.whatsapp.com/send?phone=&text=Support%20this%20idea%3A%20Test%20Idea https://demo.stg.citizenlab.co/ideas/undefined?utm_source=share_idea&utm_campaign=share_content&utm_medium=whatsapp&utm_content=522ae8cc-a5ed-4d31-9aa0-470904934ec6'
+      `https://api.whatsapp.com/send?phone=&text=Support%20this%20idea%3A%20Idea%201%20title https://demo.stg.citizenlab.co/ideas/${mockIdeaData[0].attributes.slug}?utm_source=share_idea&utm_campaign=share_content&utm_medium=whatsapp&utm_content=522ae8cc-a5ed-4d31-9aa0-470904934ec6`
     );
   });
 });
