874e7895ce5b7bf332e0d2bb8f630300a250197c
brentguf <brentguf@users.noreply.github.com>
2023-03-02 09:10:32 +0100

    useUpdateIdea test

---
+++ front/app/api/ideas/useUpdateIdea.test.ts
@@ -0,0 +1,57 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+
+import useUpdateIdea from './useUpdateIdea';
+import { ideaData } from './__mocks__/useIdeaById';
+
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+
+import createQueryClientWrapper from 'utils/testUtils/queryClientWrapper';
+
+const apiPath = '*idea_statuses/:id';
+const server = setupServer(
+  rest.patch(apiPath, (_req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ data: ideaData[0] }));
+  })
+);
+
+describe('useUpdateIdea', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
+  it('mutates data correctly', async () => {
+    const { result, waitFor } = renderHook(() => useUpdateIdea(), {
+      wrapper: createQueryClientWrapper(),
+    });
+
+    act(() => {
+      result.current.mutate({
+        id: 'id',
+        requestBody: { title_multiloc: { en: 'name' } },
+      });
+    });
+
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+    expect(result.current.data?.data).toEqual(ideaData[0]);
+  });
+
+  it('returns error correctly', async () => {
+    server.use(
+      rest.patch(apiPath, (_req, res, ctx) => {
+        return res(ctx.status(500));
+      })
+    );
+
+    const { result, waitFor } = renderHook(() => useUpdateIdea(), {
+      wrapper: createQueryClientWrapper(),
+    });
+    act(() => {
+      result.current.mutate({
+        id: 'id',
+        requestBody: { title_multiloc: { en: 'name' } },
+      });
+    });
+    await waitFor(() => expect(result.current.isError).toBe(true));
+    expect(result.current.error).toBeDefined();
+  });
+});
