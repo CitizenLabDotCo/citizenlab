0a93405bef468f16c8fc7eb89566b14eea17d36e
brentguf <brentguf@users.noreply.github.com>
2023-03-03 15:37:50 +0100

    useDeleteIdea hook

---
+++ front/app/api/ideas/useDeleteIdea.test.ts
@@ -0,0 +1,51 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+
+import useDeleteIdea from './useDeleteIdea';
+
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+
+import createQueryClientWrapper from 'utils/testUtils/queryClientWrapper';
+const apiPath = '*ideas/:id';
+
+const server = setupServer(
+  rest.delete(apiPath, (_req, res, ctx) => {
+    return res(ctx.status(200));
+  })
+);
+
+describe('useDeleteIdea', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
+  it('mutates data correctly', async () => {
+    const { result, waitFor } = renderHook(() => useDeleteIdea(), {
+      wrapper: createQueryClientWrapper(),
+    });
+
+    act(() => {
+      result.current.mutate('id');
+    });
+
+    await waitFor(() => expect(result.current.data).not.toBe(undefined));
+  });
+
+  it('returns error correctly', async () => {
+    server.use(
+      rest.delete(apiPath, (_req, res, ctx) => {
+        return res(ctx.status(500));
+      })
+    );
+
+    const { result, waitFor } = renderHook(() => useDeleteIdea(), {
+      wrapper: createQueryClientWrapper(),
+    });
+
+    act(() => {
+      result.current.mutate('id');
+    });
+
+    await waitFor(() => expect(result.current.isError).toBe(true));
+    expect(result.current.error).toBeDefined();
+  });
+});
