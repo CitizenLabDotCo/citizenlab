962cc6c3c6d901342365ee55ade3bb13d9437a01
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-03-07 19:06:21 +0000

    [CL-2809] _

--- front/app/api/permissions/useParticipationConditions.ts
+++ front/app/api/permissions/useParticipationConditions.ts
@@ -1,9 +1,9 @@
-import fetcher from 'utils/cl-react-query/fetcher';
-import { ParticipationConditionsResponse } from './types';
+// import fetcher from 'utils/cl-react-query/fetcher';
+// import { ParticipationConditionsResponse } from './types';
 
-export const fetchParticipationConditions = ({ type }: Params) => {
-  fetcher<ParticipationConditionsResponse>({
-    path: `/app_configuration`,
-    action: 'get',
-  });
-};
+// export const fetchParticipationConditions = ({ type }: Params) => {
+//   fetcher<ParticipationConditionsResponse>({
+//     path: `/app_configuration`,
+//     action: 'get',
+//   });
+// };
