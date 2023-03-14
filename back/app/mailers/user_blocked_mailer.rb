b380fcd0dfeea9297429c554a03bc9e8fe339c44
Simon Tharby <simontharby@gmail.com>
2023-03-14 10:43:40 +0000

    [CL-3085] Working inline link to T&C

--- back/app/mailers/user_blocked_mailer.rb
+++ back/app/mailers/user_blocked_mailer.rb
@@ -15,19 +15,7 @@ class UserBlockedMailer < ApplicationMailer
     t('.subject', organizationName: organization_name)
   end
 
-  def message_user_blocked_with_reason
-    t('.message_user_blocked_with_reason', organizationName: organization_name)
-  end
-
-  def message_user_blocked_without_reason
-    t('.message_user_blocked_with_reason', organizationName: organization_name, termsAndConditionsUrl: terms_conditions_url)
-  end
-
   def header_logo_only?
     true
-  end
-
-  def link_to_terms_and_conditions
-    Frontend::UrlService.new.terms_conditions_url
   end
 end
