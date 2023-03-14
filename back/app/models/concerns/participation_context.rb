0df22f5e1e3c24eeaa94f0f4067027516fcdcb47
jamesspeake <james@speake.org>
2023-03-14 15:38:35 +0000

    [CL-1928] changed name

--- back/app/models/concerns/participation_context.rb
+++ back/app/models/concerns/participation_context.rb
@@ -18,7 +18,7 @@ module ParticipationContext
   POSTING_METHODS       = %w[unlimited limited].freeze
   VOTING_METHODS        = %w[unlimited limited].freeze
   IDEAS_ORDERS          = %w[trending random popular -new new].freeze
-  IDEAS_ORDERS_EXCLUDE  = %w[trending popular].freeze
+  IDEAS_ORDERS_BUDGETING_EXCLUDE = %w[trending popular].freeze
   INPUT_TERMS           = %w[idea question contribution project issue option].freeze
   DEFAULT_INPUT_TERM    = 'idea'
 
@@ -73,7 +73,7 @@ module ParticipationContext
       with_options if: :budgeting? do
         validates :min_budget, presence: true
         validates :max_budget, presence: true
-        validates :ideas_order, exclusion: { in: IDEAS_ORDERS_EXCLUDE }, allow_nil: true
+        validates :ideas_order, exclusion: { in: IDEAS_ORDERS_BUDGETING_EXCLUDE }, allow_nil: true
       end
       validates :min_budget,
         numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: :max_budget,
