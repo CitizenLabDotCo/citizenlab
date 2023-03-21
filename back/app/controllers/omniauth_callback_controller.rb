92c883d63c033a99927bf1bf5feae8041ac40216
jamesspeake <james@speake.org>
2023-03-21 17:13:54 +0000

    [CL-3082] Allow half registered users requiring confirmation to be confirmed when they then login with SSO.

--- back/app/controllers/omniauth_callback_controller.rb
+++ back/app/controllers/omniauth_callback_controller.rb
@@ -174,7 +174,7 @@ class OmniauthCallbackController < ApplicationController
 
     attrs = authver_method.updateable_user_attrs
     update_hash = authver_method.profile_to_user_attrs(auth).slice(*attrs).compact
-    user.confirm
+    user.confirm # confirm user email if not already confirmed
 
     if authver_method.overwrite_user_attrs?
       user.update!(update_hash)
