a464d9c865a680e7b852ddfc75d8a7a1c59205de
brentguf <brentguf@users.noreply.github.com>
2023-03-22 16:53:37 +0100

    Disable delete button if user is not an admin

--- front/app/containers/Admin/projects/project/permissions/components/ModeratorListRow.tsx
+++ front/app/containers/Admin/projects/project/permissions/components/ModeratorListRow.tsx
@@ -60,7 +60,7 @@ const ModeratorListRow = ({ isLastItem, moderator, projectId }: Props) => {
         onClick={handleDeleteClick}
         buttonStyle="text"
         icon="delete"
-        disabled={isAdmin({ data: moderator })}
+        disabled={!isAdmin({ data: moderator })}
       >
         {formatMessage(messages.deleteModeratorLabel)}
       </Button>
