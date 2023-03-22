22dfa591aa9dd05d7d24c5f65d307703241d036d
Fran <francisco.pensa.web@gmail.com>
2023-03-22 09:14:06 -0300

    [CL-3118] update disabled account page

--- front/app/containers/DisabledAccount/index.tsx
+++ front/app/containers/DisabledAccount/index.tsx
@@ -1,19 +1,54 @@
 import React from 'react';
 import moment from 'moment';
+import { adopt } from 'react-adopt';
 
 // components
 import { Box } from '@citizenlab/cl2-component-library';
 import { Title } from 'components/smallForm';
 import { useSearchParams } from 'react-router-dom';
+import Link from 'utils/cl-router/Link';
 
 // i18n
 import { FormattedMessage } from 'utils/cl-intl';
 import messages from './messages';
+import { getLocalized } from 'utils/i18n';
 
-export default () => {
-  const [searchParams, _] = useSearchParams();
+// resources
+import GetAppConfiguration, {
+  GetAppConfigurationChildProps,
+} from 'resources/GetAppConfiguration';
+import GetLocale, { GetLocaleChildProps } from 'resources/GetLocale';
+import GetAppConfigurationLocales, {
+  GetAppConfigurationLocalesChildProps,
+} from 'resources/GetAppConfigurationLocales';
+// utils
+import { isNilOrError } from 'utils/helperUtils';
+
+interface DataProps {
+  tenantLocales: GetAppConfigurationLocalesChildProps;
+  tenant: GetAppConfigurationChildProps;
+  locale: GetLocaleChildProps;
+}
+
+const DisabledAccount = ({ tenant, locale, tenantLocales }: DataProps) => {
+  const [searchParams] = useSearchParams();
+
+  if (isNilOrError(locale) || isNilOrError(tenant)) return null;
+
   const parsedDate = moment(searchParams.get('date')).format('LL');
+  const organizationNameMultiLoc =
+    tenant.attributes.settings.core.organization_name;
+  const organizationName = getLocalized(
+    organizationNameMultiLoc,
+    locale,
+    tenantLocales
+  );
 
+  const TermsAndConditions = (
+    <Link to="/pages/terms-and-conditions">
+      <FormattedMessage {...messages.termsAndConditions} />
+    </Link>
+  );
   return (
     <Box
       display="flex"
@@ -28,7 +63,13 @@ export default () => {
       <Title style={{ paddingTop: '26px' }}>
         <FormattedMessage {...messages.title} />
       </Title>
-      <FormattedMessage {...messages.text} />
+      <FormattedMessage
+        {...messages.text}
+        values={{
+          organizationName,
+          TermsAndConditions,
+        }}
+      />
       <br />
       <FormattedMessage
         {...messages.bottomText}
@@ -37,3 +78,15 @@ export default () => {
     </Box>
   );
 };
+
+const Data = adopt<DataProps>({
+  tenantLocales: <GetAppConfigurationLocales />,
+  tenant: <GetAppConfiguration />,
+  locale: <GetLocale />,
+});
+
+const WrappedDisabledAccount = () => (
+  <Data>{(dataprops) => <DisabledAccount {...dataprops} />}</Data>
+);
+
+export default WrappedDisabledAccount;
--- front/app/containers/DisabledAccount/messages.ts
+++ front/app/containers/DisabledAccount/messages.ts
@@ -6,9 +6,13 @@ export default defineMessages({
     defaultMessage: 'Your account has been temporarily disabled',
   },
   text: {
-    id: 'app.containers.DisabledAccount.text',
+    id: 'app.containers.DisabledAccount.text1',
     defaultMessage:
-      'Your account on Greenville Participation Platform has been temporarily disabled for a violation of the community guideline. For more information on this, you can consult the Terms & conditions.',
+      'Your account on {organizationName} Platform has been temporarily disabled for a violation of the community guideline. For more information on this, you can consult the {TermsAndConditions}.',
+  },
+  termsAndConditions: {
+    id: 'app.containers.DisabledAccount.termsAndConditions',
+    defaultMessage: 'terms & conditions',
   },
   bottomText: {
     id: 'app.containers.DisabledAccount.bottomText',
