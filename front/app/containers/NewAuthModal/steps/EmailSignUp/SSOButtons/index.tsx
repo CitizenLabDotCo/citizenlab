b3825e0a9bbba9ad9b392c31703c9f4d38c5b4da
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-03-23 20:15:18 +0000

    [CL-2809] _

--- front/app/containers/NewAuthModal/steps/EmailSignUp/SSOButtons/index.tsx
+++ front/app/containers/NewAuthModal/steps/EmailSignUp/SSOButtons/index.tsx
@@ -4,7 +4,15 @@ import React from 'react';
 import { Box } from '@citizenlab/cl2-component-library';
 import SSOButton from './SSOButton';
 
+// hooks
+import useFeatureFlag from 'hooks/useFeatureFlag';
+
 const SSOButtons = () => {
+  const googleLoginEnabled = useFeatureFlag({ name: 'google_login' });
+  // azureAdLoginEnabled: <GetFeatureFlag name="azure_ad_login" />,
+  // facebookLoginEnabled: <GetFeatureFlag name="facebook_login" />,
+  // franceconnectLoginEnabled: <GetFeatureFlag name="franceconnect_login" />,
+
   return (
     <Box mt="32px">
       <SSOButton ssoProvider="google" />
