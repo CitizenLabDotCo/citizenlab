0de624e2d878e572550ce5751ecb946276225986
Sebastien Hoorens <sebastien@citizenlab.co>
2023-03-24 10:33:07 +0100

    include custom fields inclusion in permission spec

--- back/engines/commercial/granular_permissions/spec/acceptance/permissions_spec.rb
+++ back/engines/commercial/granular_permissions/spec/acceptance/permissions_spec.rb
@@ -35,7 +35,7 @@ resource 'Permissions' do
         expect(json_response[:data].size).to eq Permission.available_actions(@project).size
       end
 
-      example_request 'List all permissions efficiently include custom fields', document: false do
+      example_request 'List all permissions efficiently include custom fields', document: true do
         permission = @project.permissions.first
         field2 = create :custom_field
         field1 = create :custom_field
