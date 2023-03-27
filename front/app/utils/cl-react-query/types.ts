9792a53b54ca8940543364b35189e18db0375fbf
Iva <iva@citizenlab.co>
2023-03-27 19:10:18 +0300

    [CL-3175] Add relevant query key types

--- front/app/utils/cl-react-query/types.ts
+++ front/app/utils/cl-react-query/types.ts
@@ -1,3 +1,18 @@
 export type Keys<KeysObject extends Record<string, any>> = ReturnType<
   KeysObject[keyof KeysObject]
 >;
+
+interface KeyDefinition {
+  type: string;
+  operation?: 'list' | 'item';
+  variant?: string;
+  parameters?: Record<string, any>;
+}
+
+export interface QueryKeys {
+  all: () => KeyDefinition[];
+  lists?: (parameters?: Record<string, any>) => [KeyDefinition];
+  list?: (parameters?: Record<string, any>) => [KeyDefinition];
+  items?: (parameters?: Record<string, any>) => [KeyDefinition];
+  item?: (parameters?: Record<string, any>) => [KeyDefinition];
+}
