0e5b0e6491e95aa12d58e7db326f6ed1b42f9a8e
Edwin Kato <katedwin88@gmail.com>
2023-03-28 10:52:55 +0300

    [CL-3160] Show seat info under project managers when billing is off

--- front/app/containers/Admin/projects/project/permissions/containers/ProjectManagement/index.tsx
+++ front/app/containers/Admin/projects/project/permissions/containers/ProjectManagement/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// components
-import { Section, SubSectionTitle } from 'components/admin/Section';
-
 // i18n
 import { FormattedMessage, injectIntl } from 'utils/cl-intl';
 import messages from './messages';
 import { WrappedComponentProps } from 'react-intl';
 
 // components
-import { IconTooltip } from '@citizenlab/cl2-component-library';
+import { IconTooltip, Box } from '@citizenlab/cl2-component-library';
 import ModeratorList from '../../components/ModeratorList';
 import UserSearch from '../../components/UserSearch';
+import SeatInfo from 'components/SeatInfo';
+import { Section, SubSectionTitle } from 'components/admin/Section';
+
+// hooks
+import useFeatureFlag from 'hooks/useFeatureFlag';
 
 const ModeratorSubSection = styled(Section)`
   margin-bottom: 30px;
@@ -32,6 +34,10 @@ const ProjectManagement = ({
   projectId,
   intl: { formatMessage },
 }: Props & WrappedComponentProps) => {
+  const hasSeatBasedBillingEnabled = useFeatureFlag({
+    name: 'seat_based_billing',
+  });
+
   return (
     <ModeratorSubSection>
       <SubSectionTitle>
@@ -58,6 +64,11 @@ const ProjectManagement = ({
       </SubSectionTitle>
       <UserSearch projectId={projectId} />
       <ModeratorList projectId={projectId} />
+      {!hasSeatBasedBillingEnabled && (
+        <Box width="516px">
+          <SeatInfo seatType="collaborator" />
+        </Box>
+      )}
     </ModeratorSubSection>
   );
 };
