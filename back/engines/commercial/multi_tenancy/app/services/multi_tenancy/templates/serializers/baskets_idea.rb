43c66981071af25a3e0ac95b0950bb8daec2cd2f
Adrien Dessy <adrien@citizenlab.co>
2023-03-29 15:10:53 +0200

    [CL-3184] Add template serializer for baskets-ideas

---
+++ back/engines/commercial/multi_tenancy/app/services/multi_tenancy/templates/serializers/baskets_idea.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module MultiTenancy
+  module Templates
+    module Serializers
+      class BasketsIdea < Base
+        ref_attributes %i[basket idea]
+      end
+    end
+  end
+end
