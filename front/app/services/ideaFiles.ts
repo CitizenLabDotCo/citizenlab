59bae71ef56c5a1717cb358a81aed67abbb6d432
Iva <iva@citizenlab.co>
2023-03-29 14:15:41 +0300

    [CL-3169] Delete service types

--- front/app/services/ideaFiles.ts
+++ front/app/services/ideaFiles.ts
@@ -1,31 +1,8 @@
 import { API_PATH } from 'containers/App/constants';
 import streams from 'utils/streams';
 
-export interface IIdeaFileData {
-  id: string;
-  type: string;
-  attributes: {
-    file: {
-      url: string;
-    };
-    ordering: string | null;
-    name: string;
-    size: number;
-    created_at: string;
-    updated_at: string;
-  };
-}
-
-export interface IIdeaFile {
-  data: IIdeaFileData;
-}
-
-export interface IIdeaFiles {
-  data: IIdeaFileData[];
-}
-
 export function addIdeaImportFile(base64: string) {
-  return streams.add<IIdeaFile>(`${API_PATH}/import_ideas/bulk_create_xlsx`, {
+  return streams.add(`${API_PATH}/import_ideas/bulk_create_xlsx`, {
     import_ideas: { xlsx: base64 },
   });
 }
