7ef2a1d1bbfa975732f48a53deeb014b0b60c506
Adrien Dessy <adrien@citizenlab.co>
2023-03-29 20:34:23 +0200

    [CL-3184] Add template serializer for users

---
+++ back/engines/commercial/multi_tenancy/app/services/multi_tenancy/templates/serializers/user.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module MultiTenancy
+  module Templates
+    module Serializers
+      # Roles are not included because project ids of moderators would become invalid.
+      # Pending invitations are cleared out.
+      # TODO: Properly copy project moderator roles and domicile.
+      class User < Base
+        attributes %i[
+          bio_multiloc
+          block_reason
+          cl1_migrated
+          email
+          first_name
+          last_name
+          locale
+          password_digest
+          verified
+        ]
+
+        attribute(:block_start_at) { |user| serialize_timestamp(user.block_start_at) }
+        attribute(:registration_completed_at) { |user| serialize_timestamp(user.registration_completed_at) }
+
+        attribute :custom_field_values do |user|
+          user.custom_field_values.delete_if { |k, v| v.nil? || (k == 'domicile') }
+        end
+
+        attribute(:password, if: proc { |user| user.password_digest.blank? }) do
+          SecureRandom.urlsafe_base64(32)
+        end
+
+        upload_attribute :avatar
+      end
+    end
+  end
+end
