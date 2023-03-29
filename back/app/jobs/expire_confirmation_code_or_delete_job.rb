d315a674814e275ea77187946ff210db3da7eb19
jamesspeake <james@speake.org>
2023-03-29 08:59:51 +0100

    [CL-3034] Added deletion of users with no password and no completed_at date

--- back/app/jobs/expire_confirmation_code_or_delete_job.rb
+++ back/app/jobs/expire_confirmation_code_or_delete_job.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
-class ExpireConfirmationCodeJob < ApplicationJob
+class ExpireConfirmationCodeOrDeleteJob < ApplicationJob
   def run(user_id, code_to_expire)
     # First check if the user record still exists.
     user = User.find_by(id: user_id)
     return if user.nil? || !user.confirmation_required? || user.email_confirmation_code != code_to_expire
 
     user.reset_confirmation_code
-    user.save
+    user.save!
+
+    return unless user.no_password? && user.registration_completed_at.nil?
+
+    DeleteUserJob.perform_later(user)
   end
 end
