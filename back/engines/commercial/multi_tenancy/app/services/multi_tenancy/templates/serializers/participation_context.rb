7f686ab709a4d84816d4e1f9850aea6bd20f789d
Adrien Dessy <adrien@citizenlab.co>
2023-03-29 13:19:41 +0200

    [CL-3184] Add template serializer for participation contexts

---
+++ back/engines/commercial/multi_tenancy/app/services/multi_tenancy/templates/serializers/participation_context.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module MultiTenancy
+  module Templates
+    module Serializers
+      class ParticipationContext < Base
+        attributes %i[
+          commenting_enabled
+          downvoting_enabled
+          downvoting_limited_max
+          downvoting_method
+          ideas_order
+          input_term
+          max_budget
+          min_budget
+          participation_method
+          poll_anonymous
+          posting_enabled
+          posting_limited_max
+          posting_method
+          presentation_mode
+          upvoting_limited_max
+          upvoting_method
+          voting_enabled
+        ]
+
+        attribute(:survey_embed_url, if: :survey?)
+        attribute(:survey_service, if: :survey?)
+      end
+    end
+  end
+end
