fa83301ca20e09e7d8eaf7d8511e9c9d1af2b3ae
Iva <iva@citizenlab.co>
2023-03-29 14:30:04 +0300

    [CL-3169] Convert bulk import

--- front/app/services/ideaFiles.ts
+++
@@ -1,8 +0,0 @@
-import { API_PATH } from 'containers/App/constants';
-import streams from 'utils/streams';
-
-export function addIdeaImportFile(base64: string) {
-  return streams.add(`${API_PATH}/import_ideas/bulk_create_xlsx`, {
-    import_ideas: { xlsx: base64 },
-  });
-}
