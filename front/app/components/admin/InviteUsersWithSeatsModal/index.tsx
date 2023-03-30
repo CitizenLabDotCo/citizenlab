d5b17baeccdaea738244da6a0d51bb5c90c54761
Edwin Kato <katedwin88@gmail.com>
2023-03-30 18:45:27 +0300

    [CL-3197] Show success modal in invitations page

--- front/app/components/admin/InviteUsersWithSeatsModal/index.tsx
+++ front/app/components/admin/InviteUsersWithSeatsModal/index.tsx
@@ -8,10 +8,18 @@ import SeatInfo, {
   TSeatType,
 } from 'components/SeatInfo';
 import Error from 'components/UI/Error';
+import SeatChangeSuccess from 'components/admin/SeatChangeSuccess';
 
 // Translation
 import { FormattedMessage, useIntl } from 'utils/cl-intl';
 import messages from './messages';
+
+// hooks
+import useAppConfiguration from 'api/app_configuration/useAppConfiguration';
+import useSeats from 'api/seats/useSeats';
+
+// Utils
+import { isNil } from 'utils/helperUtils';
 
 interface InviteUsersWithSeatsModalProps {
   showModal: boolean;
@@ -31,6 +39,24 @@ const InviteUsersWithSeatsModal = ({
   const { formatMessage } = useIntl();
   const [hasAcknowledged, setHasAcknowledged] = useState(false);
   const [showWarning, setShowWarning] = useState(false);
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
+  const { data: appConfiguration } = useAppConfiguration();
+  const { data: seats } = useSeats();
+  if (!appConfiguration || !seats) return null;
+
+  const maximumSeatNumber = {
+    admin:
+      appConfiguration?.data.attributes.settings.core.maximum_admins_number,
+    collaborator:
+      appConfiguration?.data.attributes.settings.core.maximum_moderators_number,
+  }[seatType];
+  const currentSeatNumber = {
+    admin: seats.data.attributes.admins_number,
+    collaborator: seats.data.attributes.project_moderators_number,
+  }[seatType];
+  const hasExceededSetNumber =
+    !isNil(maximumSeatNumber) && currentSeatNumber >= maximumSeatNumber;
+
   const seatTypeMessages: SeatTypeMessageDescriptor = {
     admin: messages.admin,
     collaborator: messages.collaborator,
@@ -55,61 +81,67 @@ const InviteUsersWithSeatsModal = ({
       setShowWarning(true);
       return;
     }
-    closeModal();
     inviteUsers();
+    setShowSuccessModal(true);
   };
 
+  const header = !showSuccessModal ? (
+    <Text color="primary" my="8px" fontSize="l" fontWeight="bold" px="2px">
+      {formatMessage(seatTypeModalTitles[seatType])}
+    </Text>
+  ) : undefined;
+
   return (
-    <Modal
-      opened={showModal}
-      close={closeModal}
-      header={
-        <Text color="primary" my="8px" fontSize="l" fontWeight="bold" px="2px">
-          {formatMessage(seatTypeModalTitles[seatType])}
-        </Text>
-      }
-    >
-      <Box display="flex" flexDirection="column" width="100%" p="32px">
-        <Text color="textPrimary" fontSize="m" my="0px">
-          <FormattedMessage
-            {...messages.infoMessage}
-            values={{
-              noOfUsers: noOfSeatsToAdd,
-              seatType: formatMessage(seatTypeMessages[seatType]),
-            }}
-          />
-        </Text>
-        <Box py="32px">
-          <SeatInfo seatType={seatType} />
-        </Box>
-
-        <Checkbox
-          checked={hasAcknowledged}
-          onChange={onChange}
-          label={
-            <Text color="blue500" my="0px">
-              {formatMessage(messages.billingAcknowledgement, {
-                seatTypes: formatMessage(seatTypesMessages[seatType]),
-              })}
-            </Text>
-          }
+    <Modal opened={showModal} close={closeModal} header={header}>
+      {showSuccessModal ? (
+        <SeatChangeSuccess
+          closeModal={closeModal}
+          hasPurchasedMoreSeats={hasExceededSetNumber}
+          seatType="collaborator"
         />
-
-        {showWarning && (
-          <Box mt="12px">
-            <Error text={formatMessage(messages.acceptWarning)} />
+      ) : (
+        <Box display="flex" flexDirection="column" width="100%" p="32px">
+          <Text color="textPrimary" fontSize="m" my="0px">
+            <FormattedMessage
+              {...messages.infoMessage}
+              values={{
+                noOfUsers: noOfSeatsToAdd,
+                seatType: formatMessage(seatTypeMessages[seatType]),
+              }}
+            />
+          </Text>
+          <Box py="32px">
+            <SeatInfo seatType={seatType} />
           </Box>
-        )}
 
-        <Box display="flex" mt="32px">
-          <Button
-            onClick={handleConfirmClick}
-            data-testid="confirm-button-text"
-          >
-            {formatMessage(messages.confirmButtonText)}
-          </Button>
+          <Checkbox
+            checked={hasAcknowledged}
+            onChange={onChange}
+            label={
+              <Text color="blue500" my="0px">
+                {formatMessage(messages.billingAcknowledgement, {
+                  seatTypes: formatMessage(seatTypesMessages[seatType]),
+                })}
+              </Text>
+            }
+          />
+
+          {showWarning && (
+            <Box mt="12px">
+              <Error text={formatMessage(messages.acceptWarning)} />
+            </Box>
+          )}
+
+          <Box display="flex" mt="32px">
+            <Button
+              onClick={handleConfirmClick}
+              data-testid="confirm-button-text"
+            >
+              {formatMessage(messages.confirmButtonText)}
+            </Button>
+          </Box>
         </Box>
-      </Box>
+      )}
     </Modal>
   );
 };
