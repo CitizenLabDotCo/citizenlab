6849b0b437dae0553167baf7d3fdea94922ed8cc
Simon Tharby <simontharby@gmail.com>
2023-03-31 16:05:34 +0100

    [CL-1829] Remove leadin whitespace

--- back/engines/free/email_campaigns/app/models/email_campaigns/campaigns/invite_reminder.rb
+++ back/engines/free/email_campaigns/app/models/email_campaigns/campaigns/invite_reminder.rb
@@ -46,7 +46,7 @@ module EmailCampaigns
     end
 
     def filter_recipient(users_scope, activity:, time: nil)
-        users_scope.where(id: activity.item.invitee_id)
+      users_scope.where(id: activity.item.invitee_id)
     end
 
     def generate_commands(recipient:, activity:)
