7570436ca9d9665816ef577760629177d92ab2c1
brentguf <brentguf@users.noreply.github.com>
2023-04-05 10:59:46 +0200

    Remove unused messages 1

--- front/app/components/ConsentManager/messages.ts
+++ front/app/components/ConsentManager/messages.ts
@@ -26,10 +26,6 @@ export default defineMessages({
     id: 'app.components.ConsentManager.Banner.accept',
     defaultMessage: 'Accept',
   },
-  ariaButtonClose: {
-    id: 'app.components.ConsentManager.Banner.ariaButtonClose2',
-    defaultMessage: 'Reject policy and close banner',
-  },
   title: {
     id: 'app.components.ConsentManager.Modal.PreferencesDialog.title',
     defaultMessage: 'Your cookie preferences',
