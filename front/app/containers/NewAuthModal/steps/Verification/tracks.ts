250bf2e3d956f22ddd8b7a3d87940d94e53dd634
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-04-11 18:47:08 +0100

    [CL-3103] Copy over verification markup

---
+++ front/app/containers/NewAuthModal/steps/Verification/tracks.ts
@@ -0,0 +1,5 @@
+export default {
+  signUpVerificationStepSkipped: 'Sign up - verification step skipped',
+  signUpVerificationStepFailed: 'Sign up - verification step failed',
+  signUpVerificationStepCompleted: 'Sign up - verification step completed',
+};
