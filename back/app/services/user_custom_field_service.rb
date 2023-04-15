3f62e7dac4a2fdb8911af729c8f7a26605406ab7
Simon Tharby <simontharby@gmail.com>
2023-04-15 20:22:54 +0100

    [CL-3333] Naive log when delete field - not performant

--- back/app/services/user_custom_field_service.rb
+++ back/app/services/user_custom_field_service.rb
@@ -1,10 +1,26 @@
 # frozen_string_literal: true
 
 class UserCustomFieldService
-  def delete_custom_field_values(field)
-    User
-      .where("custom_field_values \? '#{field.key}'")
-      .update_all("custom_field_values = custom_field_values - '#{field.key}'")
+  def delete_custom_field_values(field, current_user)
+    users = User.where("custom_field_values ? '#{field.key}'")
+    log = {}
+
+    users.each do |user|
+      log[user.id] = { field.key.to_s => user.custom_field_values[field.key] }
+    end
+
+    LogActivityJob.perform_now(
+      field,
+      'deletion_initiated',
+      current_user,
+      Time.now.to_i,
+      payload: {
+        explanation: 'if this deletion succeeded, these users lost this data from custom_field_values',
+        log_user_ids_deleted_custom_field_values: log
+      }
+    )
+
+    users.update_all("custom_field_values = custom_field_values - '#{field.key}'")
   end
 
   def delete_custom_field_option_values(option_key, field)
