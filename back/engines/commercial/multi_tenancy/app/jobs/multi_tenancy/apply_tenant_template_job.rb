3c8be310dd5f6c49dcde78a88451385bc3e9b8d3
Adrien Dessy <adrien@citizenlab.co>
2023-04-15 18:02:15 +0200

    [CL-3184] Adapt ApplyTenantTemplateJob to use the tenant template system

--- back/engines/commercial/multi_tenancy/app/jobs/multi_tenancy/apply_tenant_template_job.rb
+++ back/engines/commercial/multi_tenancy/app/jobs/multi_tenancy/apply_tenant_template_job.rb
@@ -10,10 +10,7 @@ module MultiTenancy
 
       side_fx_tenant.before_apply_template(tenant, template)
       side_fx_tenant.around_apply_template(tenant, template) do
-        tenant.switch do
-          deserializer = ::MultiTenancy::Templates::TenantDeserializer.new
-          deserializer.resolve_and_apply_template(template, external_subfolder: 'release')
-        end
+        tenant.switch { MultiTenancy::Templates::ApplyService.new.apply(template) }
       end
       side_fx_tenant.after_apply_template(tenant, template)
     end
