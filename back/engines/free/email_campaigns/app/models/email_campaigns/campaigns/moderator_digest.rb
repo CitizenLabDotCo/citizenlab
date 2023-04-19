86402c129a3d0efce06993fe77d923e4aedce8f1
Simon Tharby <simontharby@gmail.com>
2023-04-19 12:15:25 +0100

    [CL-3338] Multiple fixes

--- back/engines/free/email_campaigns/app/models/email_campaigns/campaigns/moderator_digest.rb
+++ back/engines/free/email_campaigns/app/models/email_campaigns/campaigns/moderator_digest.rb
@@ -71,10 +71,14 @@ module EmailCampaigns
         statistics = statistics project
         next unless nonzero_statistics? statistics
 
+        project_name = project.title_multiloc[recipient.locale] || project.title_multiloc[I18n.default_locale]
+        puts "project_name: #{project_name.inspect}"
         top_ideas = top_ideas project, name_service
         idea_ids = top_ideas.pluck(:id)
         {
           event_payload: {
+            project: project,
+            project_name: project_name,
             statistics: statistics,
             top_ideas: top_ideas,
             has_new_ideas: top_ideas.any?
