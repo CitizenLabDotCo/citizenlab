752a097fecf3a9c47d9fddd8db6231ce220ee3c4
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-04-19 10:06:00 +0100

    [CL-3373] Fix duplicate message error

--- front/app/containers/Authentication/steps/VerificationSuccess/messages.ts
+++ front/app/containers/Authentication/steps/VerificationSuccess/messages.ts
@@ -2,11 +2,11 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   userVerifiedTitle: {
-    id: 'app.containers.NewAuthModal.steps.VerificationSuccess',
+    id: 'app.containers.NewAuthModal.steps.VerificationSuccess.userVerifiedTitle',
     defaultMessage: "You're now verified !",
   },
   userVerifiedSubtitle: {
-    id: 'app.containers.NewAuthModal.steps.VerificationSuccess',
+    id: 'app.containers.NewAuthModal.steps.VerificationSuccess.userVerifiedSubtitle',
     defaultMessage:
       "Your identity has been verified. You're now a full member of the community on this platform.",
   },
