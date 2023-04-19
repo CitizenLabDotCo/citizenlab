6a38fc61517778b1345633363a419e89922d4457
brentguf <brentguf@users.noreply.github.com>
2023-04-19 12:57:22 +0200

    Remove unused msgs

--- front/app/components/admin/InviteUsersWithSeatsModal/messages.ts
+++ front/app/components/admin/InviteUsersWithSeatsModal/messages.ts
@@ -1,14 +1,6 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  giveManagerRights: {
-    id: 'app.containers.admin.inviteUsersWithSeatsModal.giveManagerRights',
-    defaultMessage: 'Give manager rights',
-  },
-  giveAdminRights: {
-    id: 'app.containers.admin.inviteUsersWithSeatsModal.giveAdminRights',
-    defaultMessage: 'Give admin rights',
-  },
   confirmSeatUsageChange: {
     id: 'app.containers.admin.inviteUsersWithSeatsModal.confirmSeatUsageChange',
     defaultMessage: 'Confirm impact on seat usage',
@@ -17,17 +9,6 @@ export default defineMessages({
     id: 'app.containers.admin.inviteUsersWithSeatsModal.infoMessage1',
     defaultMessage:
       'You have reached the limit of available seats within your plan. {additionalSeats} will be added over the limit.',
-  },
-  admin: {
-    id: 'app.containers.admin.inviteUsersWithSeatsModal.admin',
-    defaultMessage: 'admin',
-  },
-  manager: {
-    id: 'app.containers.admin.inviteUsersWithSeatsModal.manager',
-    defaultMessage: 'manager',
-  },
-  billingAcknowledgement: {
-    id: 'app.containers.admin.inviteUsersWithSeatsModal.billingAcknowledgement',
   },
   additionalAdminSeats: {
     id: 'app.containers.admin.inviteUsersWithSeatsModal.additionalAdminSeats',
@@ -38,14 +19,6 @@ export default defineMessages({
     id: 'app.containers.admin.inviteUsersWithSeatsModal.additionalManagerSeats',
     defaultMessage:
       '{seats, plural, one {1 additional manager seat} other {# additional manager seats}}',
-  },
-  managers: {
-    id: 'app.containers.admin.inviteUsersWithSeatsModal.managers',
-    defaultMessage: 'managers',
-  },
-  acceptWarning: {
-    id: 'app.containers.admin.inviteUsersWithSeatsModal.acceptWarning',
-    defaultMessage: 'Accept the condition to proceed',
   },
   additionalAdminAndManagerSeats: {
     id: 'app.containers.admin.inviteUsersWithSeatsModal.additionalAdminAndManagerSeats',
