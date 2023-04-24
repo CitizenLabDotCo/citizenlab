4a9238805866aec07062a36a2f273746811313cf
Iva <iva@citizenlab.co>
2023-04-24 17:30:22 +0300

    [CL-3409] Add hooks tests

---
+++ front/app/api/areas/useDeleteArea.test.ts
@@ -0,0 +1,51 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+
+import useDeleteArea from './useDeleteArea';
+
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+
+import createQueryClientWrapper from 'utils/testUtils/queryClientWrapper';
+const apiPath = '*areas/:id';
+
+const server = setupServer(
+  rest.delete(apiPath, (_req, res, ctx) => {
+    return res(ctx.status(200));
+  })
+);
+
+describe('useDeleteArea', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
+  it('mutates data correctly', async () => {
+    const { result, waitFor } = renderHook(() => useDeleteArea(), {
+      wrapper: createQueryClientWrapper(),
+    });
+
+    act(() => {
+      result.current.mutate('id');
+    });
+
+    await waitFor(() => expect(result.current.data).not.toBe(undefined));
+  });
+
+  it('returns error correctly', async () => {
+    server.use(
+      rest.delete(apiPath, (_req, res, ctx) => {
+        return res(ctx.status(500));
+      })
+    );
+
+    const { result, waitFor } = renderHook(() => useDeleteArea(), {
+      wrapper: createQueryClientWrapper(),
+    });
+
+    act(() => {
+      result.current.mutate('id');
+    });
+
+    await waitFor(() => expect(result.current.isError).toBe(true));
+    expect(result.current.error).toBeDefined();
+  });
+});
