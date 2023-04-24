095b1c40d292f9b2c3597e0e3c9be4d4aba92634
Sebastien Hoorens <sebastien@citizenlab.co>
2023-04-24 12:57:09 +0200

    spec for template issue schema validation + validate on user create/update and invite accept

--- back/app/controllers/web_api/v1/users_controller.rb
+++ back/app/controllers/web_api/v1/users_controller.rb
@@ -132,7 +132,7 @@ class WebApi::V1::UsersController < ::ApplicationController
 
     SideFxUserService.new.before_create(@user, current_user)
 
-    if @user.save
+    if @user.save(context: :form_submission)
       SideFxUserService.new.after_create(@user, current_user)
       render json: WebApi::V1::UserSerializer.new(
         @user,
