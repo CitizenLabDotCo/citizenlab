e7d87cee69bb00de1b6d633c4e08745711a5e1cc
jamesspeake <james@speake.org>
2023-04-25 22:02:52 +0100

    [CL-3387] Fixed tests

--- back/engines/commercial/granular_permissions/spec/acceptance/permissions_spec.rb
+++ back/engines/commercial/granular_permissions/spec/acceptance/permissions_spec.rb
@@ -31,8 +31,7 @@ resource 'Permissions' do
 
       example_request 'List all permissions of a project' do
         assert_status 200
-        json_response = json_parse response_body
-        expect(json_response[:data].size).to eq Permission.enabled_actions(@project).size
+        expect(response_data.size).to eq Permission.enabled_actions(@project).size
       end
 
       example 'List all permissions of a project when voting has been disabled' do
@@ -40,8 +39,7 @@ resource 'Permissions' do
 
         do_request
         assert_status 200
-        json_response = json_parse response_body
-        expect(json_response[:data].size).to eq Permission.enabled_actions(@project).size
+        expect(response_data.size).to eq Permission.enabled_actions(@project).size
       end
 
       example_request 'List all permissions efficiently include custom fields', document: true do
