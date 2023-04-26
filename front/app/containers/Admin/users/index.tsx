834db8c919d002080c47c16425e7cfe3253c683d
Iva <iva@citizenlab.co>
2023-04-26 14:08:29 +0300

    [CL-3430] Transform add form

--- front/app/containers/Admin/users/index.tsx
+++ front/app/containers/Admin/users/index.tsx
@@ -1,11 +1,7 @@
-import React, { PureComponent } from 'react';
-import { withRouter, WithRouterProps } from 'utils/cl-router/withRouter';
+import React, { useState } from 'react';
 import { Outlet as RouterOutlet } from 'react-router-dom';
 
 // Resources
-import GetFeatureFlag, {
-  GetFeatureFlagChildProps,
-} from 'resources/GetFeatureFlag';
 
 // components
 import HelmetIntl from 'components/HelmetIntl';
@@ -15,11 +11,6 @@ import GroupCreationStep1 from './GroupCreationStep1';
 import NormalGroupForm, { NormalFormValues } from './NormalGroupForm';
 
 // Global state
-import {
-  globalState,
-  IAdminNoPadding,
-  IGlobalStateService,
-} from 'services/globalState';
 
 // Styling
 import styled from 'styled-components';
@@ -65,57 +56,35 @@ import messages from './messages';
 import { IGroupData, addGroup, MembershipType } from 'services/groups';
 
 import Outlet from 'components/Outlet';
-
-interface DataProps {
-  isVerificationEnabled: GetFeatureFlagChildProps;
-}
-
-interface Props extends DataProps {}
+import useFeatureFlag from 'hooks/useFeatureFlag';
 
 export type GroupCreationModal = false | 'step1' | MembershipType;
 
-interface State {
-  groupCreationModal: GroupCreationModal;
-}
+const UsersPage = () => {
+  const [groupCreationModal, setGroupCreationModal] =
+    useState<GroupCreationModal>(false);
+  const isVerificationEnabled = useFeatureFlag({ name: 'verification' });
 
-class UsersPage extends PureComponent<Props & WithRouterProps, State> {
-  globalState: IGlobalStateService<IAdminNoPadding>;
-
-  constructor(props: Props & WithRouterProps) {
-    super(props);
-    this.globalState = globalState.init('AdminNoPadding', { enabled: true });
-    this.state = {
-      groupCreationModal: false,
-    };
-  }
-
-  componentDidMount() {
-    this.globalState.set({ enabled: true });
-  }
-
-  componentWillUnmount() {
-    this.globalState.set({ enabled: false });
-  }
-
-  openGroupCreationModal = () => {
-    this.setState({ groupCreationModal: 'step1' });
+  const openGroupCreationModal = () => {
+    setGroupCreationModal('step1');
   };
 
-  closeGroupCreationModal = () => {
-    this.setState({ groupCreationModal: false });
+  const closeGroupCreationModal = () => {
+    setGroupCreationModal(false);
   };
 
-  openStep2 = (groupType: IGroupData['attributes']['membership_type']) => {
-    this.setState({ groupCreationModal: groupType });
+  const openStep2 = (
+    groupType: IGroupData['attributes']['membership_type']
+  ) => {
+    setGroupCreationModal(groupType);
   };
 
-  handleSubmitForm = async (values: NormalFormValues) => {
+  const handleSubmitForm = async (values: NormalFormValues) => {
     await addGroup({ ...values });
-    this.closeGroupCreationModal();
+    closeGroupCreationModal();
   };
 
-  renderModalHeader = () => {
-    const { groupCreationModal } = this.state;
+  const renderModalHeader = () => {
     if (groupCreationModal === 'step1') {
       return <FormattedMessage {...messages.modalHeaderStep1} />;
     }
@@ -130,66 +99,52 @@ class UsersPage extends PureComponent<Props & WithRouterProps, State> {
     );
   };
 
-  render() {
-    if (!this.props.location) return null;
+  return (
+    <>
+      <HelmetIntl
+        title={messages.helmetTitle}
+        description={messages.helmetDescription}
+      />
 
-    const { groupCreationModal } = this.state;
-
-    return (
-      <>
-        <HelmetIntl
-          title={messages.helmetTitle}
-          description={messages.helmetDescription}
+      <Wrapper>
+        <LeftPanel
+          className="e2e-left-panel"
+          onCreateGroup={openGroupCreationModal}
         />
+        <ChildWrapper id="e2e-users-container">
+          <RouterOutlet />
+        </ChildWrapper>
+      </Wrapper>
 
-        <Wrapper>
-          <LeftPanel
-            className="e2e-left-panel"
-            onCreateGroup={this.openGroupCreationModal}
-          />
-          <ChildWrapper id="e2e-users-container">
-            <RouterOutlet />
-          </ChildWrapper>
-        </Wrapper>
+      <Modal
+        header={renderModalHeader()}
+        opened={groupCreationModal !== false}
+        close={closeGroupCreationModal}
+      >
+        <>
+          {groupCreationModal === 'step1' && (
+            <GroupCreationStep1 onOpenStep2={openStep2} />
+          )}
 
-        <Modal
-          header={this.renderModalHeader()}
-          opened={groupCreationModal !== false}
-          close={this.closeGroupCreationModal}
-        >
-          <>
-            {groupCreationModal === 'step1' && (
-              <GroupCreationStep1 onOpenStep2={this.openStep2} />
-            )}
-
-            {groupCreationModal === 'manual' && (
-              <NormalGroupForm
-                defaultValues={{
-                  membership_type: 'manual',
-                }}
-                onSubmit={this.handleSubmitForm}
-              />
-            )}
-
-            <Outlet
-              id="app.containers.Admin.users.form"
-              type={groupCreationModal}
-              onSubmit={this.handleSubmitForm}
-              isVerificationEnabled={this.props.isVerificationEnabled}
+          {groupCreationModal === 'manual' && (
+            <NormalGroupForm
+              defaultValues={{
+                membership_type: 'manual',
+              }}
+              onSubmit={handleSubmitForm}
             />
-          </>
-        </Modal>
-      </>
-    );
-  }
-}
+          )}
 
-const UsersPageWithHocs = withRouter(UsersPage);
+          <Outlet
+            id="app.containers.Admin.users.form"
+            type={groupCreationModal}
+            onSubmit={handleSubmitForm}
+            isVerificationEnabled={isVerificationEnabled}
+          />
+        </>
+      </Modal>
+    </>
+  );
+};
 
-export default () => (
-  <GetFeatureFlag name="verification">
-    {(isVerificationEnabled) => (
-      <UsersPageWithHocs isVerificationEnabled={isVerificationEnabled} />
-    )}
-  </GetFeatureFlag>
-);
+export default UsersPage;
