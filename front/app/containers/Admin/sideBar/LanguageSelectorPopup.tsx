c26a968589146006f4427751263766b6c1c4f3cf
Edwin Kato <katedwin88@gmail.com>
2023-04-27 16:59:42 +0300

    [CL-3412] Add LanguageSelectorPopup

---
+++ front/app/containers/Admin/sideBar/LanguageSelectorPopup.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+// components
+import { Box } from '@citizenlab/cl2-component-library';
+import { Popup } from 'semantic-ui-react';
+
+// i18n
+import { useIntl } from 'utils/cl-intl';
+import { shortenedAppLocalePairs } from 'containers/App/constants';
+import messages from './messages';
+
+// hooks
+import useLocale from 'hooks/useLocale';
+import useAppConfiguration from 'api/app_configuration/useAppConfiguration';
+
+// services
+import { updateLocale } from 'services/locale';
+
+// styles
+import { ItemMenu } from './styles';
+
+// utils
+import { isNilOrError } from 'utils/helperUtils';
+
+export const LanguageSelectorPopup = () => {
+  const { formatMessage } = useIntl();
+  const { data: appConfig } = useAppConfiguration();
+  const locale = useLocale();
+
+  if (isNilOrError(appConfig) || isNilOrError(locale)) {
+    return null;
+  }
+
+  const tenantLocales = appConfig.data.attributes.settings.core.locales;
+
+  return (
+    <Popup
+      trigger={
+        <Box display="flex" justifyContent="space-between" width="100%">
+          {formatMessage({ ...messages.language })}
+        </Box>
+      }
+      on="click"
+      position="right center"
+      positionFixed
+      offset={[0, 30]}
+      basic
+      wide
+    >
+      <Box width="172px">
+        <>
+          {tenantLocales.map((tenantLocale) => (
+            <ItemMenu
+              key={tenantLocale}
+              buttonStyle="text"
+              onClick={() => updateLocale(tenantLocale, appConfig)}
+            >
+              <Box display="flex" justifyContent="space-between" w="100%">
+                {shortenedAppLocalePairs[tenantLocale]}
+              </Box>
+            </ItemMenu>
+          ))}
+        </>
+      </Box>
+    </Popup>
+  );
+};
+
+export default LanguageSelectorPopup;
