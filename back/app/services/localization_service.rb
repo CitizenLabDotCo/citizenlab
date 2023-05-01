5e8ed8a1f037f21f7e73741353f85f5af970e3a6
Simon Tharby <simontharby@gmail.com>
2023-05-01 14:33:02 +0100

    [CL-3420] Correct a typo

--- back/app/services/localization_service.rb
+++ back/app/services/localization_service.rb
@@ -3,7 +3,7 @@
 # Custom localization formats, separated into this file mainly to make testing easier.
 class LocalizationService
   # @param [Time] time object with hour of day specified. e.g. Time.new(-2000, 1, 1, 18, 0, 0), where 18 is the hour.
-  # @return [String] localized hour of day in display format. e.g. '6 PM' or '18:00' or '18h'.
+  # @return [String] localized hour of day in desired display format. e.g. '6 PM' or '18:00' or '18h'.
   def hour_of_day(time_obj)
     (I18n.l time_obj, format: :hour_of_day).lstrip
   end
