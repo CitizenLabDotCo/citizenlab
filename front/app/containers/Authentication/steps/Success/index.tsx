9f47757ac9545db0c40841d904976350007db23b
Amanda Anderson <anderson_amanda@mail.com>
2023-05-02 09:30:06 +0200

    Fix sign-up-custom-fields test

--- front/app/containers/Authentication/steps/Success/index.tsx
+++ front/app/containers/Authentication/steps/Success/index.tsx
@@ -23,7 +23,12 @@ const Success = ({ status, onContinue }: Props) => {
   const loading = status === 'pending';
 
   return (
-    <Box display="flex" flexDirection="column" alignItems="center">
+    <Box
+      id="e2e-sign-up-success-modal"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+    >
       <Image src={SuccessImageSrc} alt="" />
       <Title variant="h4" as="h2" mt="24px" mb="0">
         {formatMessage(messages.allDone)}
