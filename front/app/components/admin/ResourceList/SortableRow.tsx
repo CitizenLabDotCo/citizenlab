f08df829286b4335ed5bd83d3444051441f940bf
brentguf <brentguf@users.noreply.github.com>
2023-05-08 11:50:57 +0200

    Convert SortableRow

--- front/app/components/admin/ResourceList/SortableRow.tsx
+++ front/app/components/admin/ResourceList/SortableRow.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
-import { findDOMNode } from 'react-dom';
-import { DragSource, DropTarget } from 'react-dnd-cjs';
+import { useDrag, useDrop } from 'react-dnd';
 import { Row } from 'components/admin/ResourceList';
 import { Icon } from '@citizenlab/cl2-component-library';
+import { Identifier, XYCoord } from 'dnd-core';
 
 const DragHandle = styled.div`
   cursor: move;
@@ -11,11 +11,7 @@ const DragHandle = styled.div`
   height: 100%;
 `;
 
-// TODO: type checking doesn't work for this component
-export interface Props {
-  connectDragSource: any;
-  connectDropTarget: any;
-  isDragging: boolean;
+interface Props {
   index: number;
   id: string;
   className?: string;
@@ -25,100 +21,119 @@ export interface Props {
   children?: React.ReactNode;
 }
 
+interface DragItem {
+  index: number;
+  id: string;
+  type: string;
+}
+
+const itemType = 'ROW';
+
 const SortableRow = ({
-  connectDropTarget,
-  connectDragSource,
-  isDragging,
   isLastItem,
   className,
   children,
+  id,
+  index,
+  dropRow,
+  moveRow,
 }: Props) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [{ handlerId }, drop] = useDrop<
+    DragItem,
+    void,
+    { handlerId: Identifier | null }
+  >({
+    accept: itemType,
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      };
+    },
+    hover(item: DragItem, monitor) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+
+      // Get vertical middle
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset();
+
+      // Get pixels to the top
+      const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top;
+
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+
+      // Time to actually perform the action
+      moveRow(dragIndex, hoverIndex);
+
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex;
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    type: itemType,
+    item: {
+      id,
+      index,
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+    end: (item, _monitor) => {
+      const { id, index } = item;
+      dropRow(id, index);
+    },
+  });
+
   const opacity = isDragging ? 0 : 1;
 
-  if (!children) {
-    return null;
-  }
+  drag(drop(ref));
 
-  return connectDropTarget(
-    connectDragSource(
-      <div style={{ opacity }} className={className}>
-        <Row isLastItem={isLastItem}>
-          <DragHandle className="sortablerow-draghandle">
-            <Icon width="12px" name="sort" />
-          </DragHandle>
-          {children}
-        </Row>
-      </div>
-    )
-  );
+  return children ? (
+    <div
+      style={{ opacity }}
+      className={className}
+      ref={ref}
+      data-handler-id={handlerId}
+    >
+      <Row isLastItem={isLastItem}>
+        <DragHandle className="sortablerow-draghandle">
+          <Icon width="12px" name="sort" />
+        </DragHandle>
+        {children}
+      </Row>
+    </div>
+  ) : null;
 };
 
-const dragSource = {
-  beginDrag(props) {
-    return {
-      id: props.id,
-      index: props.index,
-    };
-  },
-};
-
-const dropTarget = {
-  hover(props, monitor, component) {
-    const fromIndex = monitor.getItem().index;
-    const toIndex = props.index;
-
-    // Don't replace items with themselves
-    if (fromIndex === toIndex) {
-      return;
-    }
-
-    // Determine rectangle on screen
-    // eslint-disable-next-line react/no-find-dom-node
-    const domNode = findDOMNode(component);
-    const hoverBoundingRect = (domNode as Element).getBoundingClientRect();
-
-    // Get vertical middle
-    const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-
-    // Determine mouse position
-    const clientOffset = monitor.getClientOffset();
-
-    // Get pixels to the top
-    const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-
-    // Only perform the move when the mouse has crossed half of the items height
-    // When dragging downwards, only move when the cursor is below 50%
-    // When dragging upwards, only move when the cursor is above 50%
-
-    // Dragging downwards
-    if (fromIndex < toIndex && hoverClientY < hoverMiddleY) {
-      return;
-    }
-
-    // Dragging upwards
-    if (fromIndex > toIndex && hoverClientY > hoverMiddleY) {
-      return;
-    }
-    // Time to actually perform the action
-    props.moveRow(fromIndex, toIndex);
-
-    // Note: we're mutating the monitor item here!
-    // Generally it's better to avoid mutations,
-    // but it's good here for the sake of performance
-    // to avoid expensive index searches.
-    monitor.getItem().index = toIndex;
-  },
-  drop(props, monitor) {
-    const { id, index } = monitor.getItem();
-    props.dropRow(id, index);
-  },
-};
-
-export default DropTarget('ROW', dropTarget, (connect) => ({
-  connectDropTarget: connect.dropTarget(),
-}))(
-  DragSource('ROW', dragSource, (connect, monitor) => ({
-    connectDragSource: connect.dragSource(),
-    isDragging: monitor.isDragging(),
-  }))(SortableRow)
-);
+export default SortableRow;
