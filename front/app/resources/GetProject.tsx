a750de60305fab41b495f1ded73de5b7606de2c2
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-05-08 15:21:29 +0530

    [CL-3553] Replace GetProject internal logic by useProject

--- front/app/resources/GetProject.tsx
+++ front/app/resources/GetProject.tsx
@@ -1,20 +1,5 @@
-import React from 'react';
-import { isString } from 'lodash-es';
-import { Subscription, BehaviorSubject, of } from 'rxjs';
-import { distinctUntilChanged, switchMap, tap } from 'rxjs/operators';
-import { isNilOrError } from 'utils/helperUtils';
-import shallowCompare from 'utils/shallowCompare';
-import {
-  projectByIdStream,
-  projectBySlugStream,
-  IProjectData,
-} from 'services/projects';
-
-interface InputProps {
-  projectId?: string | null;
-  projectSlug?: string | null;
-  resetOnChange?: boolean;
-}
+import useProject, { Props as InputProps } from 'hooks/useProject';
+import { IProjectData } from 'services/projects';
 
 export type GetProjectChildProps = IProjectData | undefined | null | Error;
 
@@ -24,65 +9,9 @@ interface Props extends InputProps {
   children?: children;
 }
 
-interface State {
-  project: GetProjectChildProps;
-}
+const GetProject = ({ children, ...props }: Props) => {
+  const project = useProject(props);
+  return (children as any)(project);
+};
 
-export default class GetProject extends React.Component<Props, State> {
-  private inputProps$: BehaviorSubject<InputProps>;
-  private subscriptions: Subscription[];
-
-  static defaultProps = {
-    resetOnChange: true,
-  };
-
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      project: undefined,
-    };
-  }
-
-  componentDidMount() {
-    const { projectId, projectSlug, resetOnChange } = this.props;
-
-    this.inputProps$ = new BehaviorSubject({ projectId, projectSlug });
-
-    this.subscriptions = [
-      this.inputProps$
-        .pipe(
-          distinctUntilChanged((prev, next) => shallowCompare(prev, next)),
-          tap(() => resetOnChange && this.setState({ project: undefined })),
-          switchMap(({ projectId, projectSlug }) => {
-            if (isString(projectId)) {
-              return projectByIdStream(projectId).observable;
-            } else if (isString(projectSlug)) {
-              return projectBySlugStream(projectSlug).observable;
-            }
-
-            return of(null);
-          })
-        )
-        .subscribe((project) => {
-          this.setState({
-            project: !isNilOrError(project) ? project.data : project,
-          });
-        }),
-    ];
-  }
-
-  componentDidUpdate() {
-    const { projectId, projectSlug } = this.props;
-    this.inputProps$.next({ projectId, projectSlug });
-  }
-
-  componentWillUnmount() {
-    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
-  }
-
-  render() {
-    const { children } = this.props;
-    const { project } = this.state;
-    return (children as children)(project);
-  }
-}
+export default GetProject;
