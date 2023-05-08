34b65d50f29c7b9832110c1ca3560e2bc32e33ae
brentguf <brentguf@users.noreply.github.com>
2023-05-08 15:50:54 +0200

    dragAndDrop comment

--- front/app/utils/testUtils/dragAndDrop.ts
+++ front/app/utils/testUtils/dragAndDrop.ts
@@ -1,6 +1,7 @@
 import { fireEvent } from './rtl';
 
 export function dragAndDrop(dropZone: HTMLElement, dragElement: HTMLElement) {
+  // https://github.com/testing-library/vue-testing-library/issues/145
   fireEvent.mouseDown(dragElement, { which: 1, button: 0 });
   fireEvent.dragStart(dragElement);
   fireEvent.dragOver(dropZone);
