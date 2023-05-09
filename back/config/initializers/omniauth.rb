31c73a3451213dca7f7fbbd41139db48b48e3fe4
Sebastien Hoorens <sebastien@citizenlab.co>
2023-05-09 17:29:46 +0200

    try again

--- back/config/initializers/omniauth.rb
+++ back/config/initializers/omniauth.rb
@@ -8,9 +8,9 @@ GOOGLE_SETUP_PROC = lambda do |env|
   OmniauthMethods::Google.new.omniauth_setup(AppConfiguration.instance, env)
 end
 
-AZURE_AD_SETUP_PROC = lambda do |env|
-  OmniauthMethods::AzureActiveDirectory.new.omniauth_setup(AppConfiguration.instance, env)
-end
+# AZURE_AD_SETUP_PROC = lambda do |env|
+#   OmniauthMethods::AzureActiveDirectory.new.omniauth_setup(AppConfiguration.instance, env)
+# end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, setup: FACEBOOK_SETUP_PROC
