176108a0a0892b23aeaa350d93a0f86c2593cc80
brentguf <brentguf@users.noreply.github.com>
2023-05-09 16:24:11 +0200

    WrongMaxChoiceIndicator clean-up

--- front/app/containers/Admin/projects/project/poll/WrongMaxChoiceIndicator.tsx
+++ front/app/containers/Admin/projects/project/poll/WrongMaxChoiceIndicator.tsx
@@ -1,54 +1,42 @@
 import React from 'react';
-import GetPollOptions, {
-  GetPollOptionsChildProps,
-} from 'resources/GetPollOptions';
-import { adopt } from 'react-adopt';
 import { isNilOrError } from 'utils/helperUtils';
 import messages from './messages';
 import { FormattedMessage } from 'utils/cl-intl';
 import { StyledIconTooltip, Indicator } from './WrongOptionsIndicator';
 import styled from 'styled-components';
+import usePollOptions from 'hooks/usePollOptions';
 
 const StyledIndicator = styled(Indicator)`
   margin-right: 15px;
 `;
 
-interface InputProps {
+interface Props {
   questionId: string;
   maxAnswers: number | null;
 }
 
-interface DataProps {
-  options: GetPollOptionsChildProps;
-}
+const WrongMaxChoiceIndicator = ({ maxAnswers, questionId }: Props) => {
+  const options = usePollOptions(questionId);
 
-interface Props extends InputProps, DataProps {}
+  if (typeof maxAnswers === 'number') {
+    return maxAnswers < 2 ? (
+      <StyledIndicator>
+        <StyledIconTooltip
+          content={<FormattedMessage {...messages.maxUnderTheMinTooltip} />}
+        />
+        <FormattedMessage {...messages.wrongMax} />
+      </StyledIndicator>
+    ) : !isNilOrError(options) && options.length < maxAnswers ? (
+      <StyledIndicator isWarning>
+        <StyledIconTooltip
+          content={<FormattedMessage {...messages.maxOverTheMaxTooltip} />}
+        />
+        <FormattedMessage {...messages.wrongMax} />
+      </StyledIndicator>
+    ) : null;
+  }
 
-export const WrongMaxChoiceIndicator = ({ options, maxAnswers }: Props) =>
-  typeof maxAnswers === 'number' && maxAnswers < 2 ? (
-    <StyledIndicator>
-      <StyledIconTooltip
-        content={<FormattedMessage {...messages.maxUnderTheMinTooltip} />}
-      />
-      <FormattedMessage {...messages.wrongMax} />
-    </StyledIndicator>
-  ) : !isNilOrError(options) && maxAnswers && options.length < maxAnswers ? (
-    <StyledIndicator isWarning>
-      <StyledIconTooltip
-        content={<FormattedMessage {...messages.maxOverTheMaxTooltip} />}
-      />
-      <FormattedMessage {...messages.wrongMax} />
-    </StyledIndicator>
-  ) : null;
+  return null;
+};
 
-const Data = adopt<DataProps, InputProps>({
-  options: ({ questionId, render }) => (
-    <GetPollOptions questionId={questionId}>{render}</GetPollOptions>
-  ),
-});
-
-export default (inputProps: InputProps) => (
-  <Data {...inputProps}>
-    {(dataprops) => <WrongMaxChoiceIndicator {...inputProps} {...dataprops} />}
-  </Data>
-);
+export default WrongMaxChoiceIndicator;
