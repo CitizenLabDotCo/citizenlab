c09e9a06807b7dcd420e60c6ed64627f1acc4999
Fran <francisco.pensa.web@gmail.com>
2023-05-09 18:26:24 -0300

    [CL-3532] update resource

--- front/app/resources/GetPhases.tsx
+++ front/app/resources/GetPhases.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import { Subscription, BehaviorSubject, of } from 'rxjs';
-import { distinctUntilChanged, switchMap, tap } from 'rxjs/operators';
-import shallowCompare from 'utils/shallowCompare';
-import { IPhaseData, phasesStream } from 'services/phases';
-import { isNilOrError } from 'utils/helperUtils';
+import usePhases from 'api/phases/usePhases';
+import { IPhaseData } from 'api/phases/types';
 
 interface InputProps {
   projectId?: string | null | undefined;
   resetOnChange?: boolean;
 }
+
+export type GetPhasesChildProps = IPhaseData[] | undefined | null;
 
 type children = (renderProps: GetPhasesChildProps) => JSX.Element | null;
 
@@ -16,65 +14,9 @@ interface Props extends InputProps {
   children?: children;
 }
 
-interface State {
-  phases: IPhaseData[] | undefined | null;
-}
+const GetPhases = ({ children, projectId }: Props) => {
+  const { data: phases } = usePhases(projectId);
+  return (children as any)(phases);
+};
 
-export type GetPhasesChildProps = IPhaseData[] | undefined | null;
-
-export default class GetPhases extends React.Component<Props, State> {
-  private inputProps$: BehaviorSubject<InputProps>;
-  private subscriptions: Subscription[];
-
-  static defaultProps = {
-    resetOnChange: true,
-  };
-
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      phases: undefined,
-    };
-  }
-
-  componentDidMount() {
-    const { projectId, resetOnChange } = this.props;
-
-    this.inputProps$ = new BehaviorSubject({ projectId });
-
-    this.subscriptions = [
-      this.inputProps$
-        .pipe(
-          distinctUntilChanged((prev, next) => shallowCompare(prev, next)),
-          tap(() => resetOnChange && this.setState({ phases: undefined })),
-          switchMap(({ projectId }) => {
-            if (projectId) {
-              return phasesStream(projectId).observable;
-            }
-
-            return of(null);
-          })
-        )
-        .subscribe((phases) => {
-          this.setState({
-            phases: !isNilOrError(phases) ? phases.data : phases,
-          });
-        }),
-    ];
-  }
-
-  componentDidUpdate() {
-    const { projectId } = this.props;
-    this.inputProps$.next({ projectId });
-  }
-
-  componentWillUnmount() {
-    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
-  }
-
-  render() {
-    const { children } = this.props;
-    const { phases } = this.state;
-    return (children as children)(phases);
-  }
-}
+export default GetPhases;
