9a40b2bea5b6df9a5e4f56224321a5376ebf1878
Amanda Anderson <anderson_amanda@mail.com>
2023-05-09 09:41:50 +0200

    Add test for update

---
+++ front/app/api/project_folders/useUpdateProjectFolder.test.ts
@@ -0,0 +1,71 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+
+import useUpdateProjectFolder from './useUpdateProjectFolder';
+
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+
+import createQueryClientWrapper from 'utils/testUtils/queryClientWrapper';
+import { projectFolderData } from './__mocks__/useAddProjectFolder';
+
+const apiPath = '*project_folders/:projectFolderId';
+const server = setupServer(
+  rest.patch(apiPath, (_req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ data: projectFolderData }));
+  })
+);
+
+describe('useUpdateProjectFolder', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
+  it('mutates data correctly', async () => {
+    const { result, waitFor } = renderHook(() => useUpdateProjectFolder(), {
+      wrapper: createQueryClientWrapper(),
+    });
+
+    act(() => {
+      result.current.mutate({
+        title_multiloc: {
+          en: 'new folder test',
+          'fr-BE': 'new folder',
+          'nl-BE': 'new folder',
+          'nl-NL': 'new folder',
+        },
+        description_multiloc: {
+          en: '<p>test</p>',
+          'fr-BE': '<p>test</p>',
+          'nl-BE': '<p>test</p>',
+          'nl-NL': '<p>test</p>',
+        },
+        admin_publication_attributes: {
+          publication_status: 'published',
+        },
+        projectFolderId: 'projectFolderId',
+      });
+    });
+
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+    expect(result.current.data?.data).toEqual(projectFolderData);
+  });
+
+  it('returns error correctly', async () => {
+    server.use(
+      rest.patch(apiPath, (_req, res, ctx) => {
+        return res(ctx.status(500));
+      })
+    );
+
+    const { result, waitFor } = renderHook(() => useUpdateProjectFolder(), {
+      wrapper: createQueryClientWrapper(),
+    });
+    act(() => {
+      result.current.mutate({
+        projectFolderId: 'id',
+        title_multiloc: { en: 'name' },
+      });
+    });
+    await waitFor(() => expect(result.current.isError).toBe(true));
+    expect(result.current.error).toBeDefined();
+  });
+});
