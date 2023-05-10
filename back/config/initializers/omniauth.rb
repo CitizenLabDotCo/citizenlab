0f649f88eec754b9e0528ea738d8dd807429a887
Sebastien Hoorens <sebastien@citizenlab.co>
2023-05-10 10:01:59 +0200

    bring back omniauth

--- back/config/initializers/omniauth.rb
+++ back/config/initializers/omniauth.rb
@@ -8,14 +8,14 @@ GOOGLE_SETUP_PROC = lambda do |env|
   OmniauthMethods::Google.new.omniauth_setup(AppConfiguration.instance, env)
 end
 
-# AZURE_AD_SETUP_PROC = lambda do |env|
-#   OmniauthMethods::AzureActiveDirectory.new.omniauth_setup(AppConfiguration.instance, env)
-# end
+AZURE_AD_SETUP_PROC = lambda do |env|
+  OmniauthMethods::AzureActiveDirectory.new.omniauth_setup(AppConfiguration.instance, env)
+end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, setup: FACEBOOK_SETUP_PROC
   provider :google_oauth2, setup: GOOGLE_SETUP_PROC, name: 'google'
-  # provider :azure_activedirectory, setup: AZURE_AD_SETUP_PROC
+  provider :azure_activedirectory, setup: AZURE_AD_SETUP_PROC
 end
 
 OmniAuth.config.full_host = lambda { |_env|
