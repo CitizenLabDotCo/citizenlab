e56857ef44ce3d406f5645a8f3f6e8f861a5e00b
Simon Tharby <simontharby@gmail.com>
2023-05-10 21:45:53 +0100

    [CL-3534] Fix polymorphic relations in QuestionSerializer

--- back/engines/free/polls/app/serializers/polls/web_api/v1/question_serializer.rb
+++ back/engines/free/polls/app/serializers/polls/web_api/v1/question_serializer.rb
@@ -4,7 +4,13 @@ module Polls
   class WebApi::V1::QuestionSerializer < ::WebApi::V1::BaseSerializer
     attributes :question_type, :title_multiloc, :max_options, :ordering
 
-    belongs_to :participation_context, polymorphic: true
+    belongs_to :participation_context, serializer: proc { |object|
+      if object.instance_of? Phase
+        ::WebApi::V1::PhaseSerializer
+      else
+        ::WebApi::V1::ProjectSerializer
+      end
+    }
     has_many :options
   end
 end
