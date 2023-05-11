7c8c180fcac90733aa7da49b022e19b44dd0abd0
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-05-11 17:34:08 +0530

    [CL-3555] _

--- front/app/utils/cl-react-query/queryClient.ts
+++ front/app/utils/cl-react-query/queryClient.ts
@@ -6,6 +6,8 @@ export const queryClient = new QueryClient({
       staleTime: Infinity,
       keepPreviousData: true,
       retry: (_, error) => {
+        if (process.env.NODE_ENV === 'test') return false;
+
         if (typeof error !== 'string') return true;
         return !error.includes("Couldn't find");
       },
