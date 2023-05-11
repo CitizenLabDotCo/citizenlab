16f5d0000306eee67a25e727270fb137e37e96f7
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-05-11 15:09:08 +0530

    [CL-3555] Remove app config prefetch stuff bc it doesn't seem to do anything

--- front/app/utils/cl-react-query/resetQueryCache.ts
+++ front/app/utils/cl-react-query/resetQueryCache.ts
@@ -1,12 +1,5 @@
-import appConfigurationKeys from 'api/app_configuration/keys';
-import { fetchAppConfiguration } from 'api/app_configuration/useAppConfiguration';
 import { queryClient } from './queryClient';
 
 export const resetQueryCache = async () => {
   queryClient.invalidateQueries();
-
-  await queryClient.prefetchQuery({
-    queryKey: appConfigurationKeys.all(),
-    queryFn: fetchAppConfiguration,
-  });
 };
