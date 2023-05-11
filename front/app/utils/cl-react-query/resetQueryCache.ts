2fd1bc42a8d1cfa549666d054ff00efd6c0844ed
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-05-11 14:59:53 +0530

    [CL-3555] Change resetQueryCache to use invalidateQueries instead

--- front/app/utils/cl-react-query/resetQueryCache.ts
+++ front/app/utils/cl-react-query/resetQueryCache.ts
@@ -3,7 +3,8 @@ import { fetchAppConfiguration } from 'api/app_configuration/useAppConfiguration
 import { queryClient } from './queryClient';
 
 export const resetQueryCache = async () => {
-  queryClient.resetQueries();
+  queryClient.invalidateQueries();
+
   await queryClient.prefetchQuery({
     queryKey: appConfigurationKeys.all(),
     queryFn: fetchAppConfiguration,
