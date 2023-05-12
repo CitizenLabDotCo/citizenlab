ad27254c93f49adbbb61d1ecaf20a9de5b8570fe
brentguf <brentguf@users.noreply.github.com>
2023-05-12 12:00:33 +0200

    Fix problem selecting label in test

--- front/app/components/HookForm/SlugInput/index.tsx
+++ front/app/components/HookForm/SlugInput/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import messages from './messages';
 import { FormattedMessage, useIntl } from 'utils/cl-intl';
-import { Text } from '@citizenlab/cl2-component-library';
+import {
+  Text,
+  Label,
+  IconTooltip,
+  Box,
+} from '@citizenlab/cl2-component-library';
 import Warning from 'components/UI/Warning';
 import Input from 'components/HookForm/Input';
 
@@ -16,13 +21,18 @@ const SlugInput = ({ previewUrl, showWarningMessage, slug }: Props) => {
 
   return (
     <>
-      <Input
-        label={formatMessage(messages.urlSlugLabel)}
-        labelTooltipText={formatMessage(messages.slugTooltip)}
-        type="text"
-        name="slug"
-        value={slug}
-      />
+      <Box display="flex" alignItems="center">
+        <Box mr="5px">
+          <Label htmlFor="slug-input">
+            {formatMessage(messages.urlSlugLabel)}
+          </Label>
+        </Box>
+        {/* Margin bottom is needed because of Label's default margin (which we need to remove) */}
+        <Box mb="10px">
+          <IconTooltip content={formatMessage(messages.slugTooltip)} />
+        </Box>
+      </Box>
+      <Input id="slug-input" type="text" name="slug" value={slug} />
       <Text mb={showWarningMessage ? '16px' : '0'}>
         <i>
           <FormattedMessage {...messages.resultingURL} />
