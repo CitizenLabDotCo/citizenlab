7811cd0d6f34674c7210727e8920bdfdc47355e8
brentguf <brentguf@users.noreply.github.com>
2023-05-12 08:35:44 +0200

    mockCustomPage WIP

--- front/app/containers/Admin/pagesAndMenu/containers/CustomPages/Edit/Settings/Settings.test.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/CustomPages/Edit/Settings/Settings.test.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
 import { screen, render, fireEvent, waitFor } from 'utils/testUtils/rtl';
 import EditCustomPageSettings from './';
+import { ICustomPageData } from 'services/customPages';
 
 jest.mock('api/topics/useTopics');
 jest.mock('api/areas/useAreas');
 
-jest.mock('hooks/useCustomPage', () =>
-  jest.fn(() => ({
-    relationships: {
-      nav_bar_item: { data: { id: '123' } },
-      topics: { data: [] },
-      areas: { data: [] },
-    },
-    attributes: {
-      title_multiloc: { en: 'title' },
-      nav_bar_item_title_multiloc: { en: 'user generated content' },
-      slug: 'my-custom-page',
-    },
-  }))
-);
+const mockCustomPage: ICustomPageData = {
+  attributes: {
+    top_info_section_multiloc: {},
+    title_multiloc: { en: 'title' },
+    nav_bar_item_title_multiloc: { en: 'user generated content' },
+    slug: 'my-custom-page',
+    banner_layout: 'fixed_ratio_layout',
+    banner_overlay_color: '#fff',
+    banner_overlay_opacity: 80,
+    banner_cta_button_multiloc: {},
+    // check if this can be null
+    banner_cta_button_type: 'no_button',
+    banner_cta_button_url: 'https://www.website.coms',
+    banner_header_multiloc: {},
+    banner_subheader_multiloc: {},
+    bottom_info_section_multiloc: {},
+    header_bg: null,
+
+    code: 'custom',
+    // not sure about these
+
+    projects_filter_type: 'no_filter',
+    created_at: 'date',
+    updated_at: 'date',
+    banner_enabled: true,
+    bottom_info_section_enabled: true,
+    top_info_section_enabled: true,
+    events_widget_enabled: true,
+    files_section_enabled: true,
+    projects_enabled: true,
+  },
+  relationships: {
+    nav_bar_item: { data: [{ id: '123', type: 'nav_bar_item' }] },
+    topics: { data: [] },
+    areas: { data: [] },
+  },
+};
+jest.mock('hooks/useCustomPage', () => jest.fn(() => ({})));
 
 jest.mock('services/customPages', () => ({
   // `async` simulates the original `updateCustomPage` which is also `async`.
