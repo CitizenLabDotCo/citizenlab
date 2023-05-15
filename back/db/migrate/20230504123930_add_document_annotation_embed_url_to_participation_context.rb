901e63d164adb246fcc1e8d679f5a6e733331207
Simon Tharby <simontharby@gmail.com>
2023-05-15 11:09:38 +0100

    [CL-3595] Linting: Add frozen string literal comment

--- back/db/migrate/20230504123930_add_document_annotation_embed_url_to_participation_context.rb
+++ back/db/migrate/20230504123930_add_document_annotation_embed_url_to_participation_context.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddDocumentAnnotationEmbedUrlToParticipationContext < ActiveRecord::Migration[6.1]
   def change
     add_column :phases, :document_annotation_embed_url, :string, default: nil, null: true
