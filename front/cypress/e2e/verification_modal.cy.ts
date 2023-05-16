965680c56031b8a3142e0d065df1951076b9d9a8
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-05-16 17:43:42 +0530

    [CL-3598] Fix verification modal tests

--- front/cypress/e2e/verification_modal.cy.ts
+++ front/cypress/e2e/verification_modal.cy.ts
@@ -44,32 +44,109 @@ describe('Verification modal', () => {
     });
   });
 
-  describe('shows the additional rules', () => {
-    const firstName = randomString();
-    const lastName = randomString();
-    const email = randomEmail();
-    const password = randomString();
-    let userId: string;
+  describe('Participation with group conditions', () => {
+    it('lets you participate if you meet group conditions', () => {
+      cy.visit('/projects/verified-charlie-poeple-project');
+      cy.get('#e2e-idea-button').should('exist');
+      cy.get('#e2e-idea-button').click({ force: true });
 
-    before(() => {
-      cy.apiSignup(firstName, lastName, email, password).then((user) => {
-        userId = user.body.data.id;
-        cy.setLoginCookie(email, password);
-        cy.visit('/projects/verified-charlie-poeple-project');
-        cy.get('#e2e-project-page');
-        cy.acceptCookies();
-      });
-    });
+      // email/password sign up step
+      cy.get('#e2e-sign-up-email-password-container');
+      const firstName = randomString();
+      const lastName = randomString();
+      const email = `charlie@${randomString()}.com`;
+      const password = randomString();
 
-    it('verifies the user using the bogus form', () => {
-      cy.get('.e2e-idea-button:visible').first().click({ force: true });
+      cy.get('#firstName').type(firstName);
+      cy.get('#lastName').type(lastName);
+      cy.get('#email').type(email);
+      cy.get('#password').type(password);
+      cy.get('#termsAndConditionsAccepted .e2e-checkbox')
+        .click()
+        .should('have.class', 'checked');
+      cy.get('#privacyPolicyAccepted .e2e-checkbox')
+        .click()
+        .should('have.class', 'checked');
+      cy.get('#e2e-signup-password-submit-button').wait(500).click().wait(500);
+
+      // email confirmation step
+      cy.get('#code').should('exist');
+      cy.get('#code').click().type('1234');
+      cy.get('#e2e-verify-email-button').click();
+
+      // verification step: check if it contain correct rules
       cy.get('.e2e-rule-item');
       cy.get('.e2e-rule-item').should('have.length', 1);
       cy.get('.e2e-rule-item').contains('charlie');
+
+      // verification step: fill out bogus
+      cy.get(
+        '#e2e-verification-wizard-method-selection-step #e2e-bogus-button'
+      ).click();
+      cy.get('#e2e-verification-bogus-form');
+      cy.get('#e2e-verification-bogus-submit-button').click();
+
+      // continue in success step
+      cy.get('#e2e-success-continue-button').click();
+
+      // we should now be redirected to idea form
+      cy.location('pathname').should(
+        'eq',
+        `/en/projects/verified-charlie-poeple-project/ideas/new`
+      );
+      cy.logout();
     });
 
-    after(() => {
-      cy.apiRemoveUser(userId);
+    it('does not let you participate if you do not meet group conditions', () => {
+      cy.visit('/projects/verified-charlie-poeple-project');
+      cy.get('#e2e-idea-button').should('exist');
+      cy.get('#e2e-idea-button').click({ force: true });
+
+      // email/password sign up step
+      cy.get('#e2e-sign-up-email-password-container');
+      const firstName = randomString();
+      const lastName = randomString();
+      const email = randomEmail();
+      const password = randomString();
+
+      cy.get('#firstName').type(firstName);
+      cy.get('#lastName').type(lastName);
+      cy.get('#email').type(email);
+      cy.get('#password').type(password);
+      cy.get('#termsAndConditionsAccepted .e2e-checkbox')
+        .click()
+        .should('have.class', 'checked');
+      cy.get('#privacyPolicyAccepted .e2e-checkbox')
+        .click()
+        .should('have.class', 'checked');
+      cy.get('#e2e-signup-password-submit-button').wait(500).click().wait(500);
+
+      // email confirmation step
+      cy.get('#code').should('exist');
+      cy.get('#code').click().type('1234');
+      cy.get('#e2e-verify-email-button').click();
+
+      // verification step: check if it contain correct rules
+      cy.get('.e2e-rule-item');
+      cy.get('.e2e-rule-item').should('have.length', 1);
+      cy.get('.e2e-rule-item').contains('charlie');
+
+      // verification step: fill out bogus
+      cy.get(
+        '#e2e-verification-wizard-method-selection-step #e2e-bogus-button'
+      ).click();
+      cy.get('#e2e-verification-bogus-form');
+      cy.get('#e2e-verification-bogus-submit-button').click();
+
+      // we should now NOT be redirected to idea form
+      cy.location('pathname').should(
+        'eq',
+        `/en/projects/verified-charlie-poeple-project`
+      );
+
+      // button should now be disabled
+      cy.get('#e2e-idea-button > button').should('exist');
+      cy.get('#e2e-idea-button > button').should('be.disabled');
     });
   });
 });
