a2675ef58889770707aa927e102f0795518b995b
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-05-17 18:58:08 +0530

    [CL-3608] _

--- front/cypress/e2e/idea_posting_anonymous.cy.ts
+++ front/cypress/e2e/idea_posting_anonymous.cy.ts
@@ -1,11 +1,19 @@
-import { randomString } from '../support/commands';
+import { randomString, randomEmail } from '../support/commands';
 
 describe('Idea posting anonymous', () => {
-  before(() => {
-    const projectTitle = randomString();
-    const projectDescriptionPreview = randomString();
-    const projectDescription = randomString();
+  let projectId = '';
+  let projectSlug = '';
 
+  const projectTitle = randomString();
+  const projectDescriptionPreview = randomString();
+  const projectDescription = randomString();
+
+  const firstName = randomString();
+  const lastName = randomString();
+  const email = randomEmail();
+  const password = randomString();
+
+  before(() => {
     cy.apiCreateProject({
       type: 'continuous',
       title: projectTitle,
@@ -13,10 +21,41 @@ describe('Idea posting anonymous', () => {
       description: projectDescription,
       publicationStatus: 'published',
       participationMethod: 'ideation',
+    }).then((response) => {
+      projectId = response.body.data.id;
+      projectSlug = response.body.data.attributes.slug;
     });
 
-    // cy.apiSignup()
+    cy.apiSignup(firstName, lastName, email, password);
+  });
 
-    cy.visit(`/projects/${projectTitle}`);
+  beforeEach(() => {
+    cy.setLoginCookie(email, password);
+    cy.visit(`/projects/${projectSlug}/ideas/new`);
+    cy.get('#idea-form');
+    cy.acceptCookies();
+  });
+
+  it('lets you post anonymously', () => {
+    const ideaTitle = randomString();
+    const ideaContent = randomString(300);
+
+    cy.get('#e2e-idea-title-input input').type(ideaTitle);
+    cy.get('#e2e-idea-description-input .ql-editor').type(ideaContent);
+
+    cy.get('.e2e-checkbox').first().click();
+    cy.get('.e2e-submit-idea-form').click();
+
+    cy.get('#e2e-idea-show');
+    cy.location('pathname').should('eq', `/en/ideas/${ideaTitle}`);
+
+    cy.get('.e2e-modal-close-button').should('exist');
+    cy.get('.e2e-modal-close-button').first().click();
+
+    cy.get('e2e-username').should('include.text', 'Anonymous');
+  });
+
+  after(() => {
+    cy.apiRemoveProject(projectId);
   });
 });
