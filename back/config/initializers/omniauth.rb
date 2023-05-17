33eef9900e3b43247447f8023abded006fe9637a
Adrien Dessy <adrien@citizenlab.co>
2023-05-17 10:16:35 +0200

    [CL-3540] Refactor Omniauth initializer
    - Move OpenIDConnect patch to the gem_extensions folder.
    - Reorder things in the initializer.

--- back/config/initializers/omniauth.rb
+++ back/config/initializers/omniauth.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+Rails.application.reloader.to_prepare do
+  OmniAuth::Strategies::OpenIDConnect.prepend(GemExtensions::OmniAuth::Strategies::OpenIdConnect)
+end
+
+# See https://github.com/omniauth/omniauth/wiki/Resolving-CVE-2015-9284
+OmniAuth.config.allowed_request_methods = %i[post get]
+
+OmniAuth.config.full_host = lambda { |_env|
+  AppConfiguration.instance&.base_backend_uri
+}
+
+# Register OmniAuth strategies
+
 FACEBOOK_SETUP_PROC = lambda do |env|
   OmniauthMethods::Facebook.new.omniauth_setup(AppConfiguration.instance, env)
 end
@@ -18,21 +31,3 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, setup: GOOGLE_SETUP_PROC, name: 'google'
   provider :azure_activedirectory, setup: AZURE_AD_SETUP_PROC
 end
-
-OmniAuth.config.full_host = lambda { |_env|
-  AppConfiguration.instance&.base_backend_uri
-}
-
-# See https://github.com/omniauth/omniauth/wiki/Resolving-CVE-2015-9284
-OmniAuth.config.allowed_request_methods = %i[post get]
-
-module OpenIDConnectPatch
-  # Patch +OmniAuth::Strategies::OpenIDConnect+ to allow dynamic specification of the issuer.
-  def issuer
-    return options.issuer.call(env) if options.issuer.respond_to?(:call)
-
-    super
-  end
-end
-
-OmniAuth::Strategies::OpenIDConnect.prepend(OpenIDConnectPatch)
