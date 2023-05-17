770fa31da00ec57cab9f5c6e0365840a8cfa9a94
jamesspeake <james@speake.org>
2023-05-17 08:22:35 +0100

    [CL-3609] Changed salt for anonymous posts

--- back/app/models/concerns/anonymous_participation.rb
+++ back/app/models/concerns/anonymous_participation.rb
@@ -18,7 +18,8 @@ module AnonymousParticipation
     # Then a non-anonymized post gets a consistent avatar across projects
     def set_author_hash
       # SHA-256 would be more secure, but is it really needed? Random UUID on the end is a salt
-      self.author_hash = Digest::MD5.hexdigest(author_id + project_id + '84c168c4-a240-4f0a-8468-9e2cf714d4e1')
+      salt = anonymous? ? project_id + '84c168c4-a240-4f0a-8468-9e2cf714d4e1' : '335b6eb2-9e7c-405c-9221-9b8919b64b8b'
+      self.author_hash = Digest::MD5.hexdigest(author_id + salt)
     end
 
     def anonymize_author
