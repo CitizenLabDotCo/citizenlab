3fce235a78b3b22071002f3ed02baa13847f1d3c
Adrien Dessy <adrien@citizenlab.co>
2023-05-17 11:00:37 +0200

    [CL-3540] Remove openid_connect fork
    We no longer need to explicitly specify openid_connect as a dependency because it is already a dependency of omniauth_openid_connect.
    
    I upgraded omniauth_openid_connect to the latest version, which uses the most recent version of openid_connect. The latest version of openid_connect was refactored to use Faraday as the HTTP client, which allows us to use a Faraday middleware to address the issue we had with JWT encoding (which is why we forked openid_connect in the first place).
    
    As a side effect, we no longer require the HTTPClient patch since we are now using Faraday as the HTTP client.

--- back/config/initializers/omniauth.rb
+++ back/config/initializers/omniauth.rb
@@ -11,6 +11,19 @@ OmniAuth.config.full_host = lambda { |_env|
   AppConfiguration.instance&.base_backend_uri
 }
 
+# Configure the HTTP client of the OpenID Connect gem to use the middleware
+# that parses responses with a content type of application/jwt.
+#
+# This resolves the issue where the OpenID Connect gem assumes that the response
+# for userinfo is always JSON. However, the OpenID Connect specification
+# allows for the response to be a JWT.
+# Refer to https://openid.net/specs/openid-connect-core-1_0.html#UserInfoResponse.
+#
+# The `jwt` middleware is implemented and registered by the faraday-jwt gem.
+OpenIDConnect.http_config do |config|
+  config.response :jwt
+end
+
 # Register OmniAuth strategies
 
 FACEBOOK_SETUP_PROC = lambda do |env|
