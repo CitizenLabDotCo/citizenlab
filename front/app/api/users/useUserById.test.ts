b06ec4fff060c9bd4ceca93ab085c57589b5c674
Iva <iva@citizenlab.co>
2023-05-18 13:04:05 +0300

    Add get tests

---
+++ front/app/api/users/useUserById.test.ts
@@ -0,0 +1,52 @@
+import useUserById from './useUserById';
+
+import { renderHook } from '@testing-library/react-hooks';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+import createQueryClientWrapper from 'utils/testUtils/queryClientWrapper';
+import { usersData } from './__mocks__/useUsers';
+
+const userId = 'be3f645b-3e1d-4afc-b91b-d68c4dc0100b';
+
+const apiPath = `*users/${userId}`;
+
+const server = setupServer(
+  rest.get(apiPath, (_req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ data: usersData[0] }));
+  })
+);
+
+describe('useUserById', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
+  it('returns data correctly', async () => {
+    const { result, waitFor } = renderHook(() => useUserById(userId), {
+      wrapper: createQueryClientWrapper(),
+    });
+
+    expect(result.current.isLoading).toBe(true);
+
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.data?.data).toEqual(usersData[0]);
+  });
+
+  it('returns error correctly', async () => {
+    server.use(
+      rest.get(apiPath, (_req, res, ctx) => {
+        return res(ctx.status(500));
+      })
+    );
+
+    const { result, waitFor } = renderHook(() => useUserById(userId), {
+      wrapper: createQueryClientWrapper(),
+    });
+
+    expect(result.current.isLoading).toBe(true);
+    await waitFor(() => expect(result.current.isError).toBe(true));
+    expect(result.current.error).toBeDefined();
+    expect(result.current.isLoading).toBe(false);
+  });
+});
---
+++ front/app/api/users/useUsers.test.ts
@@ -0,0 +1,51 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import useUsers from './useUsers';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+
+import createQueryClientWrapper from 'utils/testUtils/queryClientWrapper';
+import { usersData } from './__mocks__/useUsers';
+
+const apiPath = '*/users';
+
+const server = setupServer(
+  rest.get(apiPath, (_req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ data: usersData }));
+  })
+);
+
+describe('useUsers', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
+  it('returns data correctly', async () => {
+    const { result, waitFor } = renderHook(() => useUsers({}), {
+      wrapper: createQueryClientWrapper(),
+    });
+
+    expect(result.current.isLoading).toBe(true);
+
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.data?.data).toEqual(usersData);
+  });
+
+  it('returns error correctly', async () => {
+    server.use(
+      rest.get(apiPath, (_req, res, ctx) => {
+        return res(ctx.status(500));
+      })
+    );
+
+    const { result, waitFor } = renderHook(() => useUsers({}), {
+      wrapper: createQueryClientWrapper(),
+    });
+
+    expect(result.current.isLoading).toBe(true);
+    await waitFor(() => expect(result.current.isError).toBe(true));
+    expect(result.current.error).toBeDefined();
+    expect(result.current.isLoading).toBe(false);
+  });
+});
