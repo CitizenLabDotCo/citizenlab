b01824d8c782a15e104538308668801b709f5420
Fran <francisco.pensa.web@gmail.com>
2023-05-19 18:01:52 -0300

    [CL-3424] add consent row info

--- front/app/containers/Admin/messaging/AutomatedEmails/index.tsx
+++ front/app/containers/Admin/messaging/AutomatedEmails/index.tsx
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { isUndefined } from 'lodash-es';
 import { flow, map, groupBy, entries, sortBy } from 'lodash/fp';
 import { ICampaignData } from 'services/campaigns';
-import { Toggle, Box, Text, Title } from '@citizenlab/cl2-component-library';
+import {
+  Toggle,
+  Box,
+  Text,
+  Title,
+  Icon,
+} from '@citizenlab/cl2-component-library';
 import {
   List as AutomatedEmailsList,
   Row,
@@ -14,6 +20,12 @@ import { colors } from 'utils/styleUtils';
 import useUpdateCampaign from 'api/campaigns/useUpdateCampaign';
 import useCampaigns from 'api/campaigns/useCampaigns';
 import useLocalize from 'hooks/useLocalize';
+import styled from 'styled-components';
+
+const StyledIcon = styled(Icon)`
+  flex: 0 0 24px;
+  margin-top: -1.5px;
+`;
 
 const AutomatedEmails = () => {
   const { data: { data: campaigns } = {} } = useCampaigns({
@@ -22,7 +34,7 @@ const AutomatedEmails = () => {
   });
   const { mutate: updateCampaign } = useUpdateCampaign();
   const localize = useLocalize();
-  const [groupedCampaigns, setGroupedCampaigns] = useState([])
+  const [groupedCampaigns, setGroupedCampaigns] = useState([]);
 
   const handleOnEnabledToggle = (campaign: ICampaignData) => () => {
     updateCampaign({
@@ -33,44 +45,42 @@ const AutomatedEmails = () => {
     });
   };
 
-  
   useEffect(() => {
-    const translateCampaigns = (campaign) => ({
-      content_type: localize(campaign.attributes.content_type_multiloc),
-      recipient_role: localize(campaign.attributes.recipient_role_multiloc),
-      recipient_segment: localize(campaign.attributes.recipient_segment_multiloc),
-      trigger:
-        campaign.attributes.trigger_multiloc &&
-        localize(campaign.attributes.trigger_multiloc),
-      admin_campaign_description: localize(
-        campaign.attributes.admin_campaign_description_multiloc
-      ),
-      schedule:
-        campaign.attributes.schedule && localize(campaign.attributes.schedule),
-      ...campaign,
-    })
-  
-    const groupRoleCampaignsByContentType = flow([
-      groupBy('content_type'),
-      entries,
-      sortBy((g:any) => g[1][0].attributes.content_type_ordering),
-    ])
-  
     const groupCampaigns = flow([
-      map(translateCampaigns),
+      map((campaign: any) => ({
+        content_type: localize(campaign.attributes.content_type_multiloc),
+        recipient_role: localize(campaign.attributes.recipient_role_multiloc),
+        recipient_segment: localize(
+          campaign.attributes.recipient_segment_multiloc
+        ),
+        trigger:
+          campaign.attributes.trigger_multiloc &&
+          localize(campaign.attributes.trigger_multiloc),
+        admin_campaign_description: localize(
+          campaign.attributes.admin_campaign_description_multiloc
+        ),
+        schedule:
+          campaign.attributes.schedule_multiloc &&
+          localize(campaign.attributes.schedule_multiloc),
+        ...campaign,
+      })),
       groupBy('recipient_role'),
       entries,
-      sortBy((g:any) => g[1][0].attributes.recipient_role_ordering),
+      sortBy((g: any) => g[1][0].attributes.recipient_role_ordering),
       map(([recipient_role, group]) => [
         recipient_role,
-        groupRoleCampaignsByContentType(group)
+        flow([
+          groupBy('content_type'),
+          entries,
+          sortBy((g: any) => g[1][0].attributes.content_type_ordering),
+        ])(group),
       ]),
-    ])
+    ]);
 
     if (campaigns) {
-      setGroupedCampaigns(groupCampaigns(campaigns))
+      setGroupedCampaigns(groupCampaigns(campaigns));
     }
-  }, [campaigns, localize])
+  }, [campaigns, localize]);
 
   if (!campaigns) return null;
 
@@ -88,29 +98,51 @@ const AutomatedEmails = () => {
         <AutomatedEmailsList>
           {groupedCampaigns.map(([recipient_role, group]: [string, any], i) => (
             <Box key={i}>
-              <Title color="primary" variant='h4'>{recipient_role}</Title>
-              {group.map(
-                ([content_type, campaigns]: [string, any[]], ii) => (
-                  <Box key={ii}>
-                    <Title color="primary" variant='h6'>{content_type}</Title>
-                    {campaigns.map((campaign) => (
-                      <Row key={campaign.id}>
-                        <Toggle
-                          disabled={isUndefined(campaign.attributes.enabled)}
-                          checked={
-                            isUndefined(campaign.attributes.enabled) ||
-                            campaign.attributes.enabled
-                          }
-                          onChange={handleOnEnabledToggle(campaign)}
-                        />
+              <Title color="primary" variant="h4">
+                {recipient_role}
+              </Title>
+              {group.map(([content_type, campaigns]: [string, any[]], ii) => (
+                <Box key={ii}>
+                  <Title color="primary" variant="h6">
+                    {content_type}
+                  </Title>
+                  {campaigns.map((campaign) => (
+                    <Row key={campaign.id}>
+                      <Toggle
+                        disabled={isUndefined(campaign.attributes.enabled)}
+                        checked={
+                          isUndefined(campaign.attributes.enabled) ||
+                          campaign.attributes.enabled
+                        }
+                        onChange={handleOnEnabledToggle(campaign)}
+                      />
+                      <Box>
                         <TextCell className="expand">
                           {campaign.admin_campaign_description}
                         </TextCell>
-                      </Row>
-                    ))}
-                  </Box>
-                )
-              )}
+                        <Box>
+                          <Text>
+                            <StyledIcon
+                              name="user"
+                              fill={colors.textSecondary}
+                              marginRight="6px"
+                            />
+                            {campaign.recipient_segment}
+                          </Text>
+                          <Text>
+                            <StyledIcon
+                              name="clock"
+                              fill={colors.textSecondary}
+                              marginRight="6px"
+                            />
+                            {campaign.trigger || campaign.schedule}
+                          </Text>
+                        </Box>
+                      </Box>
+                    </Row>
+                  ))}
+                </Box>
+              ))}
             </Box>
           ))}
         </AutomatedEmailsList>
