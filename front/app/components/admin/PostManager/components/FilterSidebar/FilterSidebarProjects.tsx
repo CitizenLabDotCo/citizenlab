1979cd76376988264b1102a61a7c1de5151b3a01
Edwin Kato <katedwin88@gmail.com>
2023-05-24 13:25:58 +0300

    [CL-3371] Add link to projects from projects tab

--- front/app/components/admin/PostManager/components/FilterSidebar/FilterSidebarProjects.tsx
+++ front/app/components/admin/PostManager/components/FilterSidebar/FilterSidebarProjects.tsx
@@ -4,6 +4,11 @@ import { Menu, Divider } from 'semantic-ui-react';
 import FilterSidebarProjectsItem from './FilterSidebarProjectsItem';
 import { FormattedMessage } from 'utils/cl-intl';
 import messages from '../../messages';
+import { isNilOrError } from 'utils/helperUtils';
+import { Box, Text } from '@citizenlab/cl2-component-library';
+import Button from 'components/UI/Button';
+import { isAdmin } from 'services/permissions/roles';
+import useAuthUser from 'hooks/useAuthUser';
 
 interface Props {
   projects?: IProjectData[] | null;
@@ -16,6 +21,12 @@ const FilterSidebarProjects = ({
   selectedProject,
   onChangeProjectFilter,
 }: Props) => {
+  const authUser = useAuthUser();
+
+  if (isNilOrError(authUser)) {
+    return null;
+  }
+
   const handleItemClick = (id: string) => () => {
     const projectIds = [id];
     onChangeProjectFilter && onChangeProjectFilter(projectIds);
@@ -36,6 +47,22 @@ const FilterSidebarProjects = ({
           <Menu.Item onClick={clearFilter} active={!selectedProject}>
             <FormattedMessage {...messages.allProjects} />
           </Menu.Item>
+          {isAdmin({ data: authUser }) && (
+            <Box display="inline-flex">
+              <Button
+                buttonStyle="text"
+                icon="edit"
+                pl="12px"
+                linkTo="/admin/projects"
+                iconPos="right"
+                iconSize="14px"
+              >
+                <Text m="0px" color="coolGrey600" fontSize="s" textAlign="left">
+                  <FormattedMessage {...messages.editProjects} />
+                </Text>
+              </Button>
+            </Box>
+          )}
           <Divider />
         </>
       )}
