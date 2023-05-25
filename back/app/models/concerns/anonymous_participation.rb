9cf31641aec8245f1a0308168c1bf31d0c89c418
jamesspeake <james@speake.org>
2023-05-25 10:56:55 +0100

    [CL-3675] Added salts to env files

--- back/app/models/concerns/anonymous_participation.rb
+++ back/app/models/concerns/anonymous_participation.rb
@@ -26,7 +26,9 @@ module AnonymousParticipation
     def set_author_hash
       return if author_id.blank?
 
-      salt = anonymous? ? "#{project_string}84c168c4-a240-4f0a-8468-9e2cf714d4e1" : '335b6eb2-9e7c-405c-9221-9b8919b64b8b'
+      salt_normal = ENV.fetch('AUTHOR_HASH_SALT', '84c168c4-a240-4f0a-8468-9e2cf714d4e1')
+      salt_anon = ENV.fetch('AUTHOR_HASH_SALT_ANON', '335b6eb2-9e7c-405c-9221-9b8919b64b8b')
+      salt = anonymous? ? "#{project_string}#{salt_anon}" : salt_normal
       self.author_hash = Digest::MD5.hexdigest(author_id + salt)
     end
 
