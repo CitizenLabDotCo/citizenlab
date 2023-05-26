b64a15764ed85a35c5f12623dc11109dcb855c42
Amanda Anderson <anderson_amanda@mail.com>
2023-05-26 13:58:37 +0200

    Fix bug when multiple phase access rights opened

--- front/app/modules/commercial/granular_permissions/admin/containers/Granular/ActionsForm.tsx
+++ front/app/modules/commercial/granular_permissions/admin/containers/Granular/ActionsForm.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { isEmpty } from 'lodash-es';
 
@@ -60,114 +60,107 @@ type SharedProps = {
 
 type Props = PostTypeProps & SharedProps;
 
-const ActionsForm = memo(
-  ({
-    permissions,
-    postType,
-    onChange,
-    projectId,
-    initiativeContext,
-    phaseId,
-  }: Props) => {
-    const { data: project } = useProjectById(projectId);
-    const [
-      previousUsersGlobalCustomFields,
-      setPreviousUsersGlobalCustomFields,
-    ] = useState(true);
-    const [
-      previousGroupsGlobalCustomFields,
-      setPreviousGroupsGlobalCustomFields,
-    ] = useState(true);
+const ActionsForm = ({
+  permissions,
+  postType,
+  onChange,
+  projectId,
+  initiativeContext,
+  phaseId,
+}: Props) => {
+  const { data: project } = useProjectById(projectId);
+  const [previousUsersGlobalCustomFields, setPreviousUsersGlobalCustomFields] =
+    useState(true);
+  const [
+    previousGroupsGlobalCustomFields,
+    setPreviousGroupsGlobalCustomFields,
+  ] = useState(true);
 
-    const handlePermissionChange =
-      (permission: IPermissionData) =>
-      (
-        permittedBy: IPermissionData['attributes']['permitted_by'],
-        groupIds: string[]
-      ) => {
-        // Remember what the last values of global custom fields toggles were for 'users' & 'groups'
-        const previousPermittedBy = permission.attributes.permitted_by;
-        if (previousPermittedBy === 'users') {
-          setPreviousUsersGlobalCustomFields(
-            permission.attributes.global_custom_fields
-          );
-        } else if (previousPermittedBy === 'groups') {
-          setPreviousGroupsGlobalCustomFields(
-            permission.attributes.global_custom_fields
-          );
-        }
+  const handlePermissionChange =
+    (permission: IPermissionData) =>
+    (
+      permittedBy: IPermissionData['attributes']['permitted_by'],
+      groupIds: string[]
+    ) => {
+      // Remember what the last values of global custom fields toggles were for 'users' & 'groups'
+      const previousPermittedBy = permission.attributes.permitted_by;
+      if (previousPermittedBy === 'users') {
+        setPreviousUsersGlobalCustomFields(
+          permission.attributes.global_custom_fields
+        );
+      } else if (previousPermittedBy === 'groups') {
+        setPreviousGroupsGlobalCustomFields(
+          permission.attributes.global_custom_fields
+        );
+      }
 
-        // Set global custom fields toggle back to the old value when switching back to 'users' & 'groups'
-        let globalCustomFields = false;
-        if (permittedBy === 'users') {
-          globalCustomFields = previousUsersGlobalCustomFields;
-        } else if (permittedBy === 'groups') {
-          globalCustomFields = previousGroupsGlobalCustomFields;
-        }
+      // Set global custom fields toggle back to the old value when switching back to 'users' & 'groups'
+      let globalCustomFields = false;
+      if (permittedBy === 'users') {
+        globalCustomFields = previousUsersGlobalCustomFields;
+      } else if (permittedBy === 'groups') {
+        globalCustomFields = previousGroupsGlobalCustomFields;
+      }
 
-        onChange({ permission, permittedBy, groupIds, globalCustomFields });
-      };
+      onChange({ permission, permittedBy, groupIds, globalCustomFields });
+    };
 
-    if (isEmpty(permissions)) {
-      return (
-        <p>
-          <FormattedMessage {...messages.noActionsCanBeTakenInThisProject} />
-        </p>
-      );
-    } else {
-      return (
-        <>
-          {permissions.map((permission, index) => {
-            const permissionAction = permission.attributes.action;
-            return (
-              <ActionPermissionWrapper
-                key={permission.id}
-                className={`${index === 0 ? 'first' : ''} ${
-                  index === permissions.length - 1 ? 'last' : ''
-                }`}
-              >
-                <Title variant="h3" color="primary">
-                  <FormattedMessage
-                    {...getPermissionActionSectionSubtitle({
-                      permissionAction,
-                      project: project?.data,
-                      postType,
-                    })}
-                  />
-                </Title>
-                <ActionForm
-                  permissionData={permission}
-                  groupIds={permission.relationships.groups.data.map(
-                    (p) => p.id
-                  )}
-                  projectType={postType}
-                  onChange={handlePermissionChange(permission)}
+  if (isEmpty(permissions)) {
+    return (
+      <p>
+        <FormattedMessage {...messages.noActionsCanBeTakenInThisProject} />
+      </p>
+    );
+  } else {
+    return (
+      <>
+        {permissions.map((permission, index) => {
+          const permissionAction = permission.attributes.action;
+          return (
+            <ActionPermissionWrapper
+              key={permission.id}
+              className={`${index === 0 ? 'first' : ''} ${
+                index === permissions.length - 1 ? 'last' : ''
+              }`}
+            >
+              <Title variant="h3" color="primary">
+                <FormattedMessage
+                  {...getPermissionActionSectionSubtitle({
+                    permissionAction,
+                    project: project?.data,
+                    postType,
+                  })}
                 />
-                {permission.attributes.permitted_by !== 'everyone' &&
-                  permission.attributes.permitted_by !==
-                    'admins_moderators' && (
-                    <Box
-                      pt="10px"
-                      borderLeft={`solid 1px ${colors.grey300}`}
-                      px="20px"
-                      pb="20px"
-                    >
-                      <UserFieldSelection
-                        permission={permission}
-                        projectId={projectId}
-                        phaseId={phaseId}
-                        initiativeContext={initiativeContext}
-                        onChange={onChange}
-                      />
-                    </Box>
-                  )}
-              </ActionPermissionWrapper>
-            );
-          })}
-        </>
-      );
-    }
+              </Title>
+              <ActionForm
+                permissionData={permission}
+                groupIds={permission.relationships.groups.data.map((p) => p.id)}
+                projectType={postType}
+                onChange={handlePermissionChange(permission)}
+              />
+              {permission.attributes.permitted_by !== 'everyone' &&
+                permission.attributes.permitted_by !== 'admins_moderators' && (
+                  <Box
+                    pt="10px"
+                    borderLeft={`solid 1px ${colors.grey300}`}
+                    px="20px"
+                    pb="20px"
+                  >
+                    <UserFieldSelection
+                      permission={permission}
+                      projectId={projectId}
+                      phaseId={phaseId}
+                      initiativeContext={initiativeContext}
+                      onChange={onChange}
+                    />
+                  </Box>
+                )}
+            </ActionPermissionWrapper>
+          );
+        })}
+      </>
+    );
   }
-);
+};
 
 export default ActionsForm;
--- front/app/modules/commercial/granular_permissions/admin/containers/Granular/Timeline.tsx
+++ front/app/modules/commercial/granular_permissions/admin/containers/Granular/Timeline.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { isNilOrError } from 'utils/helperUtils';
 
 // i18n
 import T from 'components/T';
@@ -8,7 +7,6 @@ import messages from './messages';
 
 // styling
 import styled from 'styled-components';
-import { getMethodConfig } from 'utils/participationMethodUtils';
 import {
   Accordion,
   Box,
@@ -58,68 +56,55 @@ const Timeline = ({ projectId }: InputProps) => {
     return null;
   }
 
-  const openedPhase = phases.data.filter(
-    (phase) => phase.id === openedPhaseId
-  )[0];
-
-  const config = getMethodConfig(
-    openedPhase ? openedPhase.attributes.participation_method : 'ideation'
-  );
-
-  if (!isNilOrError(config)) {
-    return (
-      <Container>
-        {phases.data &&
-          phases.data.length > 0 &&
-          phases.data.map((phase, i) => (
-            <Accordion
-              timeoutMilliseconds={1000}
-              transitionHeightPx={1700}
-              isOpenByDefault={false}
-              title={
-                <Title
-                  variant="h3"
-                  color="primary"
-                  my="20px"
-                  style={{ fontWeight: 500 }}
-                >
-                  <FormattedMessage {...messages.phase} />
-                  {` ${i + 1}: `}
-                  <T value={phase.attributes.title_multiloc} />
-                </Title>
-              }
-              key={phase.id}
-              onChange={() => {
-                setOpenedPhaseId(openedPhaseId === phase.id ? null : phase.id);
-              }}
-            >
-              <Box
-                display="flex"
-                flex={'1'}
-                flexDirection="column"
-                background={colors.white}
-                minHeight="100px"
+  return (
+    <Container>
+      {phases.data &&
+        phases.data.length > 0 &&
+        phases.data.map((phase, i) => (
+          <Accordion
+            timeoutMilliseconds={1000}
+            transitionHeightPx={1700}
+            isOpenByDefault={false}
+            title={
+              <Title
+                variant="h3"
+                color="primary"
+                my="20px"
+                style={{ fontWeight: 500 }}
               >
-                <PhaseActionForm
-                  phase={phase}
-                  onChange={handlePermissionChange}
-                  postType={config.postType}
-                  projectId={projectId}
-                />
-              </Box>
-            </Accordion>
-          ))}
-        {!phases.data ||
-          (phases.data.length < 1 && (
-            <p>
-              <FormattedMessage
-                {...messages.noActionsCanBeTakenInThisProject}
+                <FormattedMessage {...messages.phase} />
+                {` ${i + 1}: `}
+                <T value={phase.attributes.title_multiloc} />
+              </Title>
+            }
+            key={phase.id}
+            onChange={() => {
+              setOpenedPhaseId(openedPhaseId === phase.id ? null : phase.id);
+            }}
+          >
+            <Box
+              display="flex"
+              flex={'1'}
+              flexDirection="column"
+              background={colors.white}
+              minHeight="100px"
+            >
+              <PhaseActionForm
+                phase={phase}
+                onChange={handlePermissionChange}
+                projectId={projectId}
               />
-            </p>
-          ))}
-      </Container>
-    );
-  }
+            </Box>
+          </Accordion>
+        ))}
+      {!phases.data ||
+        (phases.data.length < 1 && (
+          <p>
+            <FormattedMessage {...messages.noActionsCanBeTakenInThisProject} />
+          </p>
+        ))}
+    </Container>
+  );
   return null;
 };
 
