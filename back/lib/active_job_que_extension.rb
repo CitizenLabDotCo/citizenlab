0403ff0632c7f1ba6b08036821bb072d1e28066c
Adrien Dessy <adrien@citizenlab.co>
2023-05-26 12:21:15 +0200

    [CL-3518] Adapt ActiveJobQueExtension to the new implementation of perform in que 2.0.0
    It's unclear why the freeze steps were removed when originally patching Que for multi-tenancy support. However, when upgrading to Que 2.0.0, we simply transposed the same logic to the new implementation of the +perform+ method. Ideally, we should investigate why the freeze steps were initially removed and determine if there is a less invasive way to support multi-tenancy.

--- back/lib/active_job_que_extension.rb
+++ back/lib/active_job_que_extension.rb
@@ -10,6 +10,7 @@ module ActiveJobQueExtension
         def perform_retries(should_retry = true)
           self.should_retry = should_retry
         end
+
         # rubocop:enable Style/OptionalBooleanParameter
       end
     end
@@ -19,15 +20,26 @@ module ActiveJobQueExtension
     self.class.should_retry ? super : expire
   end
 
-  # Removing the freeze step from
-  # https://github.com/que-rb/que/blob/77c6b92952b821898c393239ce0e4047b17d7dae/lib/que/active_job/extensions.rb#L14
+  # Removing the freeze steps from
+  # https://github.com/que-rb/que/blob/3d452f22688558e87f71d079029d75eb09bf2203/lib/que/active_job/extensions.rb#L14
+  #
+  # Addendum: It's unclear why the freeze steps were removed when originally patching
+  # Que for multi-tenancy support. However, when upgrading to Que 2.0.0, we simply
+  # transposed the same logic to the new implementation of the +perform+ method.
+  # Ideally, we should investigate why the freeze steps were initially removed and
+  # determine if there is a less invasive way to support multi-tenancy.
   def perform(*args)
-    Que.internal_log(:active_job_perform, self) { { args: args } }
+    args, kwargs = Que.split_out_ruby2_keywords(args)
+
+    Que.internal_log(:active_job_perform, self) do
+      { args: args, kwargs: kwargs }
+    end
 
     _run(
       args: que_filter_args(
         args.map { |a| a.is_a?(Hash) ? a.deep_symbolize_keys : a }
       ),
+      kwargs: que_filter_args(kwargs.deep_symbolize_keys),
       reraise_errors: true
     )
   end
