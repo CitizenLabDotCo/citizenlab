d89520aa2429fb261c31f9694ead87fca646bf62
Edwin Kato <katedwin88@gmail.com>
2023-05-29 13:36:26 +0300

    Restrict viewing widget and api to admins

--- front/app/containers/Admin/tools/Tools.tsx
+++ front/app/containers/Admin/tools/Tools.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Box, Title } from '@citizenlab/cl2-component-library';
 import { useIntl } from 'utils/cl-intl';
+import { isAdmin } from 'services/permissions/roles';
+import useAuthUser from 'hooks/useAuthUser';
+import { isNilOrError } from 'utils/helperUtils';
 import messages from './messages';
 import Workshops from './Workshops';
 import Widget from './Widget';
@@ -8,14 +11,19 @@ import PublicAPI from './PublicAPI';
 
 export const Tools = () => {
   const { formatMessage } = useIntl();
+  const authUser = useAuthUser();
+
+  if (isNilOrError(authUser)) return null;
+
+  const isUserAdmin = isAdmin({ data: authUser });
 
   return (
     <Box width="100%" display="flex" justifyContent="center">
       <Box maxWidth="800px">
         <Title color="primary">{formatMessage(messages.tools)}</Title>
         <Workshops />
-        <Widget />
-        <PublicAPI />
+        {isUserAdmin && <Widget />}
+        {isUserAdmin && <PublicAPI />}
       </Box>
     </Box>
   );
