4f21b02bfe649955887cf34f230f9f78d942ced9
Iva <iva@citizenlab.co>
2023-05-31 12:49:59 +0300

    Move poll responses

--- front/app/services/pollResponses.ts
+++ front/app/services/pollResponses.ts
@@ -1,7 +1,5 @@
 import { API_PATH } from 'containers/App/constants';
 import streams from 'utils/streams';
-import { requestBlob } from 'utils/request';
-import { saveAs } from 'file-saver';
 import { IParticipationContextType } from 'typings';
 import projectsKeys from 'api/projects/keys';
 import { queryClient } from 'utils/cl-react-query/queryClient';
@@ -34,17 +32,6 @@ export async function addPollResponse(
   return response;
 }
 
-export async function exportPollResponses(
-  participationContextId: string,
-  participationContextType: IParticipationContextType,
-  fileName: string
-) {
-  const blob = await requestBlob(
-    `${API_PATH}/${participationContextType}s/${participationContextId}/poll_responses/as_xlsx`,
-    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-  );
-  saveAs(blob, fileName);
-}
 export function getPollResponses(
   participationContextId: string,
   participationContextType: IParticipationContextType
