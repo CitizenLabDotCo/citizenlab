d8be4eb98a60a2020cb14c6d8cd3e2b7d17eb853
jamesspeake <james@speake.org>
2023-05-31 16:59:26 +0100

    [CL-3603] Updated schema

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_24_085443) do
+ActiveRecord::Schema.define(version: 2023_05_24_151508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
