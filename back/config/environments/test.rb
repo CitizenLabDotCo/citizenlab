5c1b29db0540e5fa2d3cb71f9df326d08aedac85
Adrien Dessy <adrien@citizenlab.co>
2023-05-31 17:23:26 +0200

    [CL-3538] Enable caching in the test env
    Caching must be turned on for Rack::Attack to work and Rack::Attack tests to pass.

--- back/config/environments/test.rb
+++ back/config/environments/test.rb
@@ -27,7 +27,10 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  config.cache_store = :null_store
+
+  # Caching must be turned on for Rack::Attack to work and Rack::Attack tests to pass.
+  # config.cache_store = :null_store
+
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
