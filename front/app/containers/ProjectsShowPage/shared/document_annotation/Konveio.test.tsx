9256ef8230b2e4e74ef427d30aa9d6ef04949408
brentguf <brentguf@users.noreply.github.com>
2023-06-01 08:38:13 +0200

    Fix Konveio unit test

--- front/app/containers/ProjectsShowPage/shared/document_annotation/Konveio.test.tsx
+++ front/app/containers/ProjectsShowPage/shared/document_annotation/Konveio.test.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 import { render, screen } from 'utils/testUtils/rtl';
-
 import Konveio from './Konveio';
+import { IUserData } from 'services/users';
+
+const mockAuthUserData: IUserData = {
+  id: 'userId',
+  type: 'user',
+  attributes: {
+    first_name: 'Stewie',
+    last_name: 'McKenzie',
+    locale: 'en',
+    slug: 'stewie-mckenzie',
+    highest_role: 'admin',
+    bio_multiloc: {},
+    roles: [{ type: 'admin' }],
+    registration_completed_at: '',
+    created_at: '',
+    updated_at: '',
+    unread_notifications: 0,
+    invite_status: null,
+    confirmation_required: false,
+    email: 'test@mail.com',
+  },
+};
+jest.mock('hooks/useAuthUser', () => () => mockAuthUserData);
 
 describe('Konveio', () => {
   it('should render', () => {
@@ -11,14 +33,14 @@ describe('Konveio', () => {
     expect(screen.getByTestId('konveiosurvey')).toBeInTheDocument();
   });
 
-  it('append email on query', () => {
+  it('appends the integration and email to the documentUrl', () => {
     render(
       <Konveio documentUrl={'https://demo.konveio.com/node/93?iframe=true'} />
     );
     const displayedIframe = screen.getByTestId('konveiosurvey');
     expect(displayedIframe).toHaveAttribute(
       'src',
-      'https://demo.konveio.com/node/93?iframe=true&integration=CitizenLab'
+      'https://demo.konveio.com/node/93?iframe=true&integration=CitizenLab&username=test%40mail.com'
     );
   });
 });
