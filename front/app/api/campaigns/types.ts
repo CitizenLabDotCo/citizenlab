60d608f40c22e2ac829e777f274490380ce90928
brentguf <brentguf@users.noreply.github.com>
2023-06-01 10:51:11 +0200

    Campaign enabled undefined comment

--- front/app/api/campaigns/types.ts
+++ front/app/api/campaigns/types.ts
@@ -15,6 +15,8 @@ export interface ICampaignData {
   attributes: {
     campaign_name: string;
     campaign_description_multiloc: Multiloc;
+    // At the time of writing, this is undefined
+    // for the invitation email
     enabled?: boolean;
     subject_multiloc: Multiloc;
     body_multiloc: Multiloc;
