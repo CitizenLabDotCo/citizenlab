81d96561d24634b274952f19f4f3cbbf812f4791
Adrien Dessy <adrien@citizenlab.co>
2023-06-02 09:59:40 +0200

    [CL-3548] Refactor routes.rb

--- back/engines/commercial/public_api/config/routes.rb
+++ back/engines/commercial/public_api/config/routes.rb
@@ -10,38 +10,27 @@ PublicApi::Engine.routes.draw do
     end
   end
 
-  # New API endpoints
   namespace :v2 do
     post 'authenticate' => 'api_token#create'
 
-    get '/users', to: 'users#index'
-    get '/users/:id', to: 'users#show'
+    resources :comments, only: %i[index show]
+    resources :ideas, only: %i[index show]
+    resources :initiatives, only: %i[index show]
+    resources :phases, only: %i[index show]
+    resources :topics, only: %i[index show]
+    resources :users, only: %i[index show]
+
+    resources :projects, only: %i[index show] do
+      resources :phases, only: %i[index]
+    end
+
+    get '/ideas/comments', to: 'comments#for_ideas'
+    get '/initiatives/comments', to: 'comments#for_initiatives'
 
     get '/ideas/votes', to: 'votes#for_ideas'
     get '/ideas/comments/votes', to: 'votes#for_idea_comments'
     get '/initiatives/votes', to: 'votes#for_initiatives'
     get '/initiatives/comments/votes', to: 'votes#for_initiative_comments'
-
-    get '/comments', to: 'comments#index'
-    get '/comments/:id', to: 'comments#show'
-    get '/ideas/comments', to: 'comments#for_ideas'
-    get '/initiatives/comments', to: 'comments#for_initiatives'
-
-    get '/ideas', to: 'ideas#index'
-    get '/ideas/:id', to: 'ideas#show'
-
-    get '/initiatives', to: 'initiatives#index'
-    get '/initiatives/:id', to: 'initiatives#show'
-
-    get '/projects', to: 'projects#index'
-    get '/projects/:id', to: 'projects#show'
-
-    get '/phases', to: 'phases#index'
-    get '/phases/:id', to: 'phases#show'
-    get '/projects/:project_id/phases', to: 'phases#by_project'
-
-    get '/topics', to: 'topics#index'
-    get '/topics/:id', to: 'topics#show'
   end
 end
 
