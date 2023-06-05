4743a82a0652904b0860cbe93b8e2135d40e6ef9
Simon Tharby <simontharby@gmail.com>
2023-06-05 22:13:27 +0100

    [CL-3709] Copy/paste much of comments model code

--- back/app/models/internal_comment.rb
+++ back/app/models/internal_comment.rb
@@ -33,4 +33,96 @@
 #  fk_rails_...  (author_id => users.id)
 #
 class InternalComment < ApplicationRecord
+  acts_as_nested_set dependent: :destroy, counter_cache: :children_count
+
+  belongs_to :author, class_name: 'User', optional: true
+  belongs_to :post, polymorphic: true
+
+  before_validation :set_publication_status, on: :create
+  before_validation :sanitize_body_multiloc
+  before_destroy :remove_notifications # Must occur before has_many :notifications (see https://github.com/rails/rails/issues/5205)
+  has_many :notifications, dependent: :nullify
+
+  counter_culture(
+    :post,
+    column_name: proc { |model| model.published? ? 'comments_count' : nil },
+    column_names: {
+      ['comments.publication_status = ?', 'published'] => 'comments_count'
+    },
+    touch: true
+  )
+  counter_culture(
+    %i[idea project],
+    column_name: proc { |model| model.published? ? 'comments_count' : nil },
+    column_names: {
+      ['comments.publication_status = ?', 'published'] => 'comments_count'
+    },
+    touch: true
+  )
+
+  # This code allows us to do something like comments.include(:idea)
+  # After https://stackoverflow.com/a/16124295/3585671
+  belongs_to :idea, -> { joins(:comments).where(comments: { post_type: 'Idea' }) },
+    foreign_key: 'post_id',
+    optional: true,
+    class_name: 'Idea',
+    inverse_of: :internal_comments
+
+  def idea
+    return unless post_type == 'Idea'
+
+    super
+  end
+
+  belongs_to :initiative, -> { joins(:comments).where(comments: { post_type: 'Initiative' }) },
+    foreign_key: 'post_id',
+    optional: true,
+    class_name: 'Initiative',
+    inverse_of: :internal_comments
+
+  def initiative
+    return unless post_type == 'Initiative'
+
+    super
+  end
+
+  PUBLICATION_STATUSES = %w[published deleted]
+
+  validates :body_multiloc, presence: true, multiloc: { presence: true, html: true }
+  validates :publication_status, presence: true, inclusion: { in: PUBLICATION_STATUSES }
+
+  scope :published, -> { where publication_status: 'published' }
+
+  def published?
+    publication_status == 'published'
+  end
+
+  def author_name
+    @author_name ||= author&.full_name
+  end
+
+  def project_id
+    post.try(:project_id)
+  end
+
+  private
+
+  def set_publication_status
+    self.publication_status ||= 'published'
+  end
+
+  def sanitize_body_multiloc
+    service = SanitizationService.new
+    self.body_multiloc = service.sanitize_multiloc body_multiloc, %i[mention]
+    self.body_multiloc = service.remove_multiloc_empty_trailing_tags body_multiloc
+    self.body_multiloc = service.linkify_multiloc body_multiloc
+  end
+
+  def remove_notifications
+    notifications.each do |notification|
+      unless notification.update comment: nil
+        notification.destroy!
+      end
+    end
+  end
 end
