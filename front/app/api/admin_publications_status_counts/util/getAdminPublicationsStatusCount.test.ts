f2e3091b81b4cc4ff447ee33a0809444549a8b30
Iva <iva@citizenlab.co>
2023-06-05 19:06:35 +0300

    Add counts tests

---
+++ front/app/api/admin_publications_status_counts/util/getAdminPublicationsStatusCount.test.ts
@@ -0,0 +1,44 @@
+import getStatusCounts from './getAdminPublicationsStatusCount';
+
+describe('getStatusCounts', () => {
+  getStatusCounts();
+  it('returns the correct status when no data', () => {
+    const result = getStatusCounts();
+    expect(result).toEqual({
+      all: 0,
+    });
+  });
+  it('returns the correct status count when data is present', () => {
+    const result = getStatusCounts({
+      data: {
+        type: 'status_counts',
+        attributes: {
+          status_counts: {
+            draft: 1,
+            published: 2,
+            archived: 3,
+          },
+        },
+      },
+    });
+    expect(result).toEqual({
+      draft: 1,
+      published: 2,
+      archived: 3,
+      all: 6,
+    });
+  });
+  it('returns the correct status count when 0', () => {
+    const result = getStatusCounts({
+      data: {
+        type: 'status_counts',
+        attributes: {
+          status_counts: {},
+        },
+      },
+    });
+    expect(result).toEqual({
+      all: 0,
+    });
+  });
+});
