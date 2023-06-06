fefa480b2506233b2f37019e29e4d2d1c36a4c49
Simon Tharby <simontharby@gmail.com>
2023-06-06 09:16:59 +0100

    [CL-3709] Add internal_comments factory

---
+++ back/spec/factories/internal_comments.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :internal_comment do
+    author
+    association :post, factory: :idea
+    parent { nil }
+    publication_status { 'published' }
+    body_multiloc do
+      {
+        'en' => '<p>I think this is a very good idea!</p>',
+        'nl-BE' => '<p>Geweldig idee!</p>'
+      }
+    end
+
+    factory :Internal_comment_with_mentions do
+      transient do
+        mentioned_users { create_list(:user, 2) }
+      end
+      after(:create) do |comment, evaluator|
+        service = MentionService.new
+        mentions = evaluator.mentioned_users.map do |u|
+          service.add_span_around service.user_to_mention(u), u
+        end
+        comment.update(body_multiloc: {
+          'en' => "#{mentions.join ', '} are sitting in a tree"
+        })
+      end
+    end
+  end
+
+  factory :nested_internal_comment do
+    author
+    association :post, factory: :idea
+    association :parent, factory: :internal_comment
+    publication_status { 'published' }
+    body_multiloc do
+      {
+        'en' => '<p>After some more thinking, there are some issues actually ...!</p>',
+        'nl-BE' => '<p>Na een nachtje slapen moet ik toegeven dat er toch nog wel problemen mee zijn</p>'
+      }
+    end
+  end
+end
