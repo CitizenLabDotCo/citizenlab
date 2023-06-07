a285c46ee9984a0b5b5b8f8b6140eabc6b24c5ca
Adrien Dessy <adrien@citizenlab.co>
2023-06-07 15:24:53 +0200

    [CL-3548] Refactor projects_controller.rb

--- back/engines/commercial/public_api/app/controllers/public_api/v2/projects_controller.rb
+++ back/engines/commercial/public_api/app/controllers/public_api/v2/projects_controller.rb
@@ -3,20 +3,21 @@
 module PublicApi
   class V2::ProjectsController < PublicApiController
     def index
-      @projects = Project.all
+      projects = Project
         .includes(:project_images)
         .includes(:map_config)
         .order(created_at: :desc)
         .page(params[:page_number])
         .per(num_per_page)
-      @projects = common_date_filters @projects
+
+      projects = common_date_filters(projects)
 
       # TODO: Add filter by topic, status & folder_id
 
-      render json: @projects,
+      render json: projects,
         each_serializer: V2::ProjectSerializer,
         adapter: :json,
-        meta: meta_properties(@projects)
+        meta: meta_properties(projects)
     end
 
     def show
