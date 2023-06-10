16043dd7d78f5f24708e85b5ab19c686eaeed55b
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-06-10 20:45:07 +0100

    Create FE API

---
+++ front/app/api/experiments/useABTest.ts
@@ -0,0 +1,83 @@
+import { useMemo, useCallback } from 'react';
+import useAddExperiment from './useAddExperiment';
+import useAuthUser from 'api/me/useAuthUser';
+
+interface ABTestConfig<TreatmentOption extends string> {
+  experiment: string;
+  treatments: TreatmentOption[];
+}
+
+const randomPickFrom = <T>(options: T[]): T => {
+  return options[Math.floor(Math.random() * options.length)];
+};
+
+const toNumber = (str: string) => {
+  let number = 0;
+
+  Array.from(str).forEach((character) => {
+    number = number + character.charCodeAt(0);
+  });
+
+  return number;
+};
+
+// https://github.com/cprosche/mulberry32
+function mulberry32(a: number) {
+  let t = a + 0x6d2b79f5;
+  t = Math.imul(t ^ (t >>> 15), t | 1);
+  t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+  return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+}
+
+const pseudoRandomPickFrom = <T>(options: T[], seed: number) => {
+  return options[Math.floor(mulberry32(seed) * options.length)];
+};
+
+function useABTest<TreatmentOption extends string>({
+  experiment,
+  treatments,
+}: ABTestConfig<TreatmentOption>): {
+  treatment: undefined | TreatmentOption;
+  send: undefined | ((payload: string) => void);
+} {
+  const { mutate: addExperiment } = useAddExperiment();
+  const { data: authUser, isLoading } = useAuthUser();
+
+  const treatment = useMemo(() => {
+    if (isLoading) return;
+
+    if (authUser) {
+      const seed = toNumber(authUser.data.id);
+      return pseudoRandomPickFrom(treatments, seed);
+    } else {
+      return randomPickFrom(treatments);
+    }
+  }, [authUser, isLoading, treatments]);
+
+  const send = useCallback(
+    (payload: string) => {
+      if (!treatment) return;
+
+      addExperiment({
+        name: experiment,
+        treatment,
+        payload,
+      });
+    },
+    [addExperiment, experiment, treatment]
+  );
+
+  if (!treatment) {
+    return {
+      treatment: undefined,
+      send: undefined,
+    };
+  }
+
+  return {
+    treatment,
+    send,
+  };
+}
+
+export default useABTest;
