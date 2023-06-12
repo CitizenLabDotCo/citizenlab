a886fdb4fb57e90c44636bc0e45d6324d9eda73a
Adrien Dessy <adrien@citizenlab.co>
2023-06-12 12:25:41 +0200

    [CL-3538] Add :current_password to the parameter filter for logging

--- back/config/initializers/filter_parameter_logging.rb
+++ back/config/initializers/filter_parameter_logging.rb
@@ -6,5 +6,5 @@
 # sensitive information. See the ActiveSupport::ParameterFilter documentation for supported
 # notations and behaviors.
 Rails.application.config.filter_parameters += %i[
-  password passw secret token _key crypt salt certificate otp ssn
+  password current_password passw secret token _key crypt salt certificate otp ssn
 ]
