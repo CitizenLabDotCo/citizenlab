d0381e35286e827d80efafcb5d93dae269342816
brentguf <brentguf@users.noreply.github.com>
2023-06-13 15:54:59 +0200

    Change NotificationsDropdown test

--- front/app/containers/MainHeader/NotificationMenu/NotificationMenu.test.tsx
+++ front/app/containers/MainHeader/NotificationMenu/NotificationMenu.test.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import { IUserAttributes } from 'api/users/types';
 import { render, screen, userEvent } from 'utils/testUtils/rtl';
 import NotificationMenu from '.';
+import { mockAuthUserData } from 'api/me/__mocks__/useAuthUser';
 
-const mockUserData: {
-  id: string;
-  type: string;
-  attributes: Partial<IUserAttributes>;
-} | null = {
-  id: 'userId',
-  type: 'user',
-  attributes: {
-    unread_notifications: 5,
-  },
-};
-
-type MockUserData = { data: typeof mockUserData } | null;
-let mockAuthUser: MockUserData = {
-  data: mockUserData,
-};
-
-jest.mock('api/me/useAuthUser', () => jest.fn(() => ({ data: mockAuthUser })));
+jest.mock('api/me/useAuthUser', () => () => ({
+  data: { data: mockAuthUserData },
+}));
 
 describe('NotificationMenu', () => {
   it('Opens and closes the dropdown when clicking the notifications icon', async () => {
@@ -28,21 +13,8 @@ describe('NotificationMenu', () => {
     render(<NotificationMenu />);
     const notificationsIcon = screen.getByRole('button');
 
-    // Open the dropdown
     await user.click(notificationsIcon);
-    const dropdown = await screen.findByTestId('notifications-dropdown');
+    const dropdown = screen.getByTestId('notifications-dropdown');
     expect(dropdown).toBeInTheDocument();
-
-    // Close the dropdown
-    await user.click(notificationsIcon);
-    expect(dropdown).not.toBeInTheDocument();
-  });
-
-  it('Does not render if the user is not logged in', () => {
-    mockAuthUser = null;
-    render(<NotificationMenu />);
-
-    const notificationsIcon = screen.queryByRole('button');
-    expect(notificationsIcon).toBeNull();
   });
 });
