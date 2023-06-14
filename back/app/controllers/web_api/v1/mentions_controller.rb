bd3cafaa08e27b21ab5972d6932e658faa97ce9b
Simon Tharby <simontharby@gmail.com>
2023-06-14 15:43:36 +0100

    [CL-3768] Basic implementation + tests

--- back/app/controllers/web_api/v1/mentions_controller.rb
+++ back/app/controllers/web_api/v1/mentions_controller.rb
@@ -7,17 +7,26 @@ class WebApi::V1::MentionsController < ApplicationController
   def users
     limit = params[:limit]&.to_i || 5
     query = params[:mention].gsub(/\W/, "\s")
+    roles = set_roles
+    post = get_post(params[:post_type], params[:post_id])
 
     @users = []
-    if (post_id = params[:post_id]) && (post_type = params[:post_type])
-      post_class = post_type_to_class(post_type)
-      post = post_class.find(post_id)
-      @users = MentionService.new.users_from_post(query, post, limit)
-    end
+    @users = MentionService.new.users_from_post(query, post, limit) if post && roles.empty?
 
     nb_missing_users = limit - @users.size
     if nb_missing_users > 0
-      @users += User.active.by_username(query).where.not(id: @users).limit(nb_missing_users)
+      @users += case roles
+      when %w[admin moderator]
+        query_scope(query).admin
+          .or(query_scope(query).project_moderator(post&.project_id))
+          .limit(nb_missing_users)
+      when ['admin']
+        query_scope(query).admin.limit(nb_missing_users)
+      when ['moderator']
+        query_scope(query).project_moderator(post&.project_id).limit(nb_missing_users)
+      else
+        query_scope(query).limit(nb_missing_users)
+      end
     end
 
     render json: WebApi::V1::UserSerializer.new(
@@ -28,6 +37,23 @@ class WebApi::V1::MentionsController < ApplicationController
 
   private
 
+  def query_scope(query)
+    User.active.by_username(query).where.not(id: @users)
+  end
+
+  def set_roles
+    roles = []
+
+    # Need to move validation stuff to a better place
+    if params[:roles].is_a?(Array)
+      raise 'Invalid roles query parameter(s)' unless params[:roles].uniq - %w[admin moderator] == []
+
+      roles = params[:roles].uniq
+    end
+
+    roles
+  end
+
   # @param [String] type
   # @@return [Class]
   def post_type_to_class(type)
@@ -36,5 +62,11 @@ class WebApi::V1::MentionsController < ApplicationController
     when 'Initiative' then Initiative
     else raise "#{type} is not a post type"
     end
+  end
+
+  def get_post(post_type, post_id)
+    post_class = post_type_to_class(post_type) if post_type
+
+    post_class.find(post_id) if post_class && post_id
   end
 end
--- back/spec/acceptance/mentions_spec.rb
+++ back/spec/acceptance/mentions_spec.rb
@@ -16,6 +16,9 @@ resource 'Mentions' do
     parameter :post_id, 'An post that is used as a context to return related users first', required: false
     parameter :post_type, "The type of post, either 'Idea' or 'Initiative'", required: false
     parameter :limit, 'The number of results to return', required: false
+    parameter :roles, "An array, including 'admin' and or 'moderator' (moderators of post, if specified)." \
+                      'The roles of the users to return, exclusively - response will not include regular users,' \
+                      'regardless of whether they are post author, or have commented on post.', required: false
 
     let(:users) { [create(:user, first_name: 'Flupke')] + create_list(:user, 9, last_name: 'Smith') }
     let(:mention) { users.first.first_name[0..3] }
@@ -62,6 +65,84 @@ resource 'Mentions' do
       assert_status 200
       json_response = json_parse(response_body)
       expect(json_response[:data].pluck(:id)).not_to include users.first.id
+    end
+
+    context 'when role(s) is/are specified using roles parameter' do
+      before { User.delete_all }
+
+      let(:project) { create(:project) }
+      let(:idea) { create(:idea, project: project) }
+      let(:first_name) { 'Aaa' }
+      let(:last_name) { 'Bbb' }
+      let(:names) { { first_name: first_name, last_name: last_name } }
+
+      let!(:regular_user) { create(:user, names) }
+      let!(:admin) { create(:admin, names) }
+      let!(:moderator_of_project) { create(:project_moderator, names.merge({ projects: [project] })) }
+      let!(:moderator_of_other_project) { create(:project_moderator, names) }
+
+      let(:base_query_params) { { post_id: idea.id, post_type: 'Idea', mention: first_name } }
+
+      example 'Does not include regular user, even if has commented on post' do
+        create(:comment, post: idea, author: regular_user)
+
+        do_request base_query_params.merge({ roles: %w[admin moderator] })
+        assert_status 200
+        json_response = json_parse(response_body)
+        expect(json_response[:data].pluck(:id)).not_to include(regular_user.id)
+      end
+
+      example 'Does not include regular user, even if post author' do
+        idea.update!(author_id: regular_user.id)
+
+        do_request base_query_params.merge({ roles: %w[admin moderator] })
+        assert_status 200
+        json_response = json_parse(response_body)
+        expect(json_response[:data].pluck(:id)).not_to include(regular_user.id)
+      end
+
+      example 'Includes only admins when only admin role is specified' do
+        do_request base_query_params.merge({ roles: ['admin'] })
+        assert_status 200
+        json_response = json_parse(response_body)
+        expect(json_response[:data].pluck(:id)).to match_array [admin.id]
+      end
+
+      context 'when a post is specified' do
+        example "Includes only moderators of post's project when only moderator role is specified" do
+          do_request base_query_params.merge({ roles: ['moderator'] })
+          assert_status 200
+          json_response = json_parse(response_body)
+          expect(json_response[:data].pluck(:id)).to match_array [moderator_of_project.id]
+        end
+
+        example "Includes only admins and moderators of post's project when both roles are specified" do
+          do_request base_query_params.merge({ roles: %w[admin moderator] })
+          assert_status 200
+          json_response = json_parse(response_body)
+          expect(json_response[:data].pluck(:id)).to match_array [moderator_of_project.id, admin.id]
+        end
+      end
+
+      context 'when no post is specified' do
+        let(:base_query_params) { { mention: first_name } }
+
+        example 'Includes only project moderators when only moderator role is specified' do
+          do_request base_query_params.merge({ roles: ['moderator'] })
+          assert_status 200
+          json_response = json_parse(response_body)
+          expect(json_response[:data].pluck(:id))
+            .to match_array [moderator_of_project.id, moderator_of_other_project.id]
+        end
+
+        example 'Includes only admins and moderators when both roles are specified' do
+          do_request base_query_params.merge({ roles: %w[admin moderator] })
+          assert_status 200
+          json_response = json_parse(response_body)
+          expect(json_response[:data].pluck(:id))
+            .to match_array [moderator_of_project.id, moderator_of_other_project.id, admin.id]
+        end
+      end
     end
   end
 end
