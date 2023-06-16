32427e47129a68b0565c290ad4d32f0182decf27
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-06-16 15:53:36 +0100

    Back linter

--- back/spec/acceptance/experiments_spec.rb
+++ back/spec/acceptance/experiments_spec.rb
@@ -9,13 +9,13 @@ resource 'Experiments' do
   header 'Content-Type', 'application/json'
 
   get 'web_api/v1/experiments' do
+    before do
+      @experiments = create_list(:experiment, 5)
+    end
+
     context 'When admin' do
       before { admin_header_token }
 
-      before do
-        @experiments = create_list(:experiment, 5)
-      end
-  
       example_request 'List all experiments' do
         assert_status(200)
         expect(response_data.size).to eq 5
@@ -27,7 +27,7 @@ resource 'Experiments' do
     with_options scope: :experiment do
       parameter :name, 'The name of the experiment, as a string', required: true
       parameter :treatment, 'The "treatment", or the option that was randomly assigned to the user', required: true
-      parameter :action, "The action, or how the user responded to the treatment", required: true
+      parameter :action, 'The action, or how the user responded to the treatment', required: true
     end
 
     let(:experiment) { build(:experiment) }
