486125bdb2aa917903cd3639b76da2717d84ebcb
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-06-16 15:29:59 +0100

    Create acceptance test

---
+++ back/spec/acceptance/experiments_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+resource 'Experiments' do
+  explanation 'AB test data'
+  header 'Content-Type', 'application/json'
+
+  get 'web_api/v1/experiments' do
+    context 'When admin' do
+      before { admin_header_token }
+
+      before do
+        @experiments = create_list(:experiment, 5)
+      end
+  
+      example_request 'List all experiments' do
+        assert_status(200)
+        expect(response_data.size).to eq 5
+      end
+    end
+  end
+
+  post 'web_api/v1/experiments' do
+    with_options scope: :experiment do
+      parameter :name, 'The name of the experiment, as a string', required: true
+      parameter :treatment, 'The "treatment", or the option that was randomly assigned to the user', required: true
+      parameter :action, "The action, or how the user responded to the treatment", required: true
+    end
+
+    let(:experiment) { build(:experiment) }
+
+    example_request 'Create an experiment' do
+      expect(response_status).to eq 201
+      expect(response_data.dig(:attributes, :name)).to match 'Button location'
+    end
+  end
+end
