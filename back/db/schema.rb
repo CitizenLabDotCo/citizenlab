a006f7c466e995a4483869d114bdf70cf0ad063b
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-06-16 16:56:43 +0100

    _

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_08_120425) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_09_161522) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
