880062565f8ba8c855c39a568b2fc919896a1575
Simon Tharby <simontharby@gmail.com>
2023-06-20 11:29:17 +0100

    [CL-3823] Add InternalCommentOnIdeaYouModerate + serializer + spec

--- back/app/services/notification_service.rb
+++ back/app/services/notification_service.rb
@@ -12,6 +12,7 @@ class NotificationService
     Notifications::InitiativeAssignedToYou,
     Notifications::InitiativeMarkedAsSpam,
     Notifications::InternalCommentOnIdeaAssignedToYou,
+    Notifications::InternalCommentOnIdeaYouModerate,
     Notifications::InternalCommentOnInitiativeAssignedToYou,
     Notifications::InternalCommentOnYourInternalComment,
     Notifications::InviteAccepted,
--- back/spec/factories/notifications.rb
+++ back/spec/factories/notifications.rb
@@ -39,6 +39,14 @@ FactoryBot.define do
     association :post, factory: :idea
   end
 
+  factory :internal_comment_on_idea_you_moderate,
+    parent: :notification,
+    class: 'Notifications::InternalCommentOnIdeaYouModerate' do
+    initiating_user
+    internal_comment
+    association :post, factory: :idea
+  end
+
   factory :internal_comment_on_initiative_assigned_to_you,
     parent: :notification,
     class: 'Notifications::InternalCommentOnInitiativeAssignedToYou' do
