fecc52fd34296f4c5e1c22cd4bd623752a867f35
brentguf <brentguf@users.noreply.github.com>
2023-06-20 17:42:09 +0200

    InitiativePreviewIndex

--- front/app/components/admin/PostManager/components/PostPreview/Initiative/InitiativePostPreview.tsx
+++ front/app/components/admin/PostManager/components/PostPreview/Initiative/InitiativePostPreview.tsx
@@ -1,8 +1,6 @@
-import React, { Suspense, useEffect, useState } from 'react';
+import React from 'react';
 
 // components
-import SideModal from 'components/UI/SideModal';
-import FullPageSpinner from 'components/UI/FullPageSpinner';
 import AdminInitiativeEdit from 'components/admin/PostManager/components/PostPreview/Initiative/AdminInitiativeEdit';
 import AdminInitiativeContent from 'components/admin/PostManager/components/PostPreview/Initiative/AdminInitiativeContent';
 
@@ -19,46 +17,25 @@ const InitiativePostPreview = ({
   onSwitchPreviewMode,
   mode,
 }: Props) => {
-  const [opened, setOpened] = useState(false);
-
-  useEffect(() => {
-    if (typeof initiativeId === 'string') {
-      setOpened(true);
-    }
-  }, [initiativeId]);
-
   if (!initiativeId) {
     return null;
   }
 
-  const handleOnClose = () => {
-    setOpened(false);
-    onClose();
-  };
-
-  const previewComponent = () => {
-    return {
-      view: (
-        <AdminInitiativeContent
-          initiativeId={initiativeId}
-          closePreview={handleOnClose}
-          handleClickEdit={onSwitchPreviewMode}
-        />
-      ),
-      edit: (
-        <AdminInitiativeEdit
-          initiativeId={initiativeId}
-          goBack={onSwitchPreviewMode}
-        />
-      ),
-    }[mode];
-  };
-
-  return (
-    <SideModal opened={opened} close={handleOnClose}>
-      <Suspense fallback={<FullPageSpinner />}>{previewComponent()}</Suspense>
-    </SideModal>
-  );
+  return {
+    view: (
+      <AdminInitiativeContent
+        initiativeId={initiativeId}
+        closePreview={onClose}
+        handleClickEdit={onSwitchPreviewMode}
+      />
+    ),
+    edit: (
+      <AdminInitiativeEdit
+        initiativeId={initiativeId}
+        goBack={onSwitchPreviewMode}
+      />
+    ),
+  }[mode];
 };
 
 export default InitiativePostPreview;
