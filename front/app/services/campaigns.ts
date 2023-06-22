fe28fd5ff833286555c16fd3f790f610d6a19147
Iva <iva@citizenlab.co>
2023-06-22 14:22:09 +0300

    Covert deliveries

--- front/app/services/campaigns.ts
+++ front/app/services/campaigns.ts
@@ -1,35 +1,7 @@
-import { IRelationship, ILinks } from 'typings';
 import { API_PATH } from 'containers/App/constants';
 import streams, { IStreamParams } from 'utils/streams';
 
 const apiEndpoint = `${API_PATH}/campaigns`;
-
-export interface IDeliveriesData {
-  data: IDeliveryData[];
-  links: ILinks;
-}
-export interface IDeliveryData {
-  id: string;
-  type: string;
-  attributes: {
-    delivery_status:
-      | 'sent'
-      | 'bounced'
-      | 'failed'
-      | 'accepted'
-      | 'delivered'
-      | 'opened'
-      | 'clicked';
-    sent_at: string;
-    created_at: string;
-    updated_at: string;
-  };
-  relationships: {
-    user: {
-      data: IRelationship;
-    };
-  };
-}
 
 export interface ICampaignStats {
   sent: number;
@@ -40,16 +12,6 @@ export interface ICampaignStats {
   opened: number;
   clicked: number;
   all: number;
-}
-
-export function listCampaignDeliveries(
-  campaignId: string,
-  streamParams: IStreamParams | null = null
-) {
-  return streams.get<IDeliveriesData>({
-    apiEndpoint: `${apiEndpoint}/${campaignId}/deliveries`,
-    ...streamParams,
-  });
 }
 
 export function getCampaignStats(
