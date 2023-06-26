aa82c64b769056d8a07449a57064f3fc7e6716b9
Sebastien Hoorens <sebastien@citizenlab.co>
2023-06-26 10:27:47 +0200

    git commit pllls

--- back/app/serializers/web_api/v1/basket_serializer.rb
+++ back/app/serializers/web_api/v1/basket_serializer.rb
@@ -9,5 +9,6 @@ class WebApi::V1::BasketSerializer < WebApi::V1::BaseSerializer
   has_many :ideas do |object|
     object.ideas.order('baskets_ideas.created_at DESC')
   end
+ 
   has_many :ideas_baskets
 end
