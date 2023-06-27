1a8b8dce43e3805140f61d5944ccaf95192b0e45
Iva <iva@citizenlab.co>
2023-06-27 17:44:03 +0300

    Add mutation test

--- front/app/api/onboarding_campaigns/useDismissOnboardingCampaign.ts
+++ front/app/api/onboarding_campaigns/useDismissOnboardingCampaign.ts
@@ -2,9 +2,9 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { CLErrors } from 'typings';
 import fetcher from 'utils/cl-react-query/fetcher';
 import onboardingCampaignsKeys from './keys';
-import { IOnboardingCampaign } from './types';
+import { IOnboardingCampaign, OnboardingCampaignName } from './types';
 
-const dismissCampaign = async (name: string) =>
+const dismissCampaign = async (name: OnboardingCampaignName) =>
   fetcher<IOnboardingCampaign>({
     path: `/onboarding_campaigns/${name}/dismissal`,
     action: 'post',
@@ -13,7 +13,7 @@ const dismissCampaign = async (name: string) =>
 
 const useDismissOnboardingCampaign = () => {
   const queryClient = useQueryClient();
-  return useMutation<IOnboardingCampaign, CLErrors, string>({
+  return useMutation<IOnboardingCampaign, CLErrors, OnboardingCampaignName>({
     mutationFn: dismissCampaign,
     onSuccess: () => {
       queryClient.invalidateQueries({
