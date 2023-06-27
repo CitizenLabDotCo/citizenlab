7426a5a1fc6163a40c70137673d70529e816b984
Sebastien Hoorens <sebastien@citizenlab.co>
2023-06-27 12:04:45 +0200

    lint: add uniqueness index

--- back/db/migrate/20230623085057_add_votes_to_baskets_ideas.rb
+++ back/db/migrate/20230623085057_add_votes_to_baskets_ideas.rb
@@ -5,5 +5,8 @@ class AddVotesToBasketsIdeas < ActiveRecord::Migration[7.0]
     add_column :baskets_ideas, :votes, :integer, null: false, default: 1
     execute 'UPDATE ideas SET budget = 1 WHERE budget < 1'
     execute 'UPDATE baskets_ideas SET votes = ideas.budget FROM ideas WHERE ideas.id = baskets_ideas.idea_id'
+
+    remove_index :baskets_ideas, name: :index_baskets_ideas_on_basket_id
+    add_index :baskets_ideas, %i[basket_id idea_id], unique: true
   end
 end
