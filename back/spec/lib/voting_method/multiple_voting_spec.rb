2c1d4747b2faaccfe35eb9be6cde20fd54580cb6
Sebastien Hoorens <sebastien@citizenlab.co>
2023-06-29 13:46:06 +0200

    fixes after merge

--- back/spec/lib/voting_method/multiple_voting_spec.rb
+++ back/spec/lib/voting_method/multiple_voting_spec.rb
@@ -14,16 +14,16 @@ RSpec.describe VotingMethod::MultipleVoting do
     end
   end
 
-  describe '#validate' do
+  describe '#validate_participation_context' do
     it 'sets no errors when voting_max_total is present' do
       project.voting_max_total = 10
-      voting_method.validate
+      voting_method.validate_participation_context
       expect(project.errors.details).to be_blank
     end
 
     it 'sets an error when voting_max_total is blank' do
       project.voting_max_total = nil
-      voting_method.validate
+      voting_method.validate_participation_context
       expect(project.errors.details).to eq(
         voting_max_total: [error: :blank]
       )
@@ -31,7 +31,7 @@ RSpec.describe VotingMethod::MultipleVoting do
 
     it 'sets an error when voting term singular is set and voting term plural is not' do
       project.voting_term_plural_multiloc = nil
-      voting_method.validate
+      voting_method.validate_participation_context
       expect(project.errors.details).to eq(
         voting_term_plural_multiloc: [error: :blank]
       )
@@ -39,7 +39,7 @@ RSpec.describe VotingMethod::MultipleVoting do
 
     it 'sets an error when voting term plural is set and voting term singular is not' do
       project.voting_term_singular_multiloc = nil
-      voting_method.validate
+      voting_method.validate_participation_context
       expect(project.errors.details).to eq(
         voting_term_singular_multiloc: [error: :blank]
       )
