4a52a3e6b2c9dc88ecd0042c6cb5a240205354df
Simon Tharby <simontharby@gmail.com>
2023-06-29 19:09:46 +0100

    [CL-3881] Use base_frontend_uri to build full url in tests

--- back/engines/free/frontend/spec/services/url_service_spec.rb
+++ back/engines/free/frontend/spec/services/url_service_spec.rb
@@ -10,20 +10,19 @@ describe Frontend::UrlService do
     let(:internal_comment1) { create(:internal_comment, post: idea) }
     let(:initiative) { create(:initiative) }
     let(:internal_comment2) { create(:internal_comment, post: initiative) }
+    let(:base_uri) { AppConfiguration.instance.base_frontend_uri }
 
     it 'returns the correct url for an internal comment on an idea' do
       expect(service.model_to_url(internal_comment1))
-        .to include(
-          "://#{Tenant.current.host}/admin/projects/#{internal_comment1.post.project_id}" \
+        .to eq(
+          "#{base_uri}/admin/projects/#{internal_comment1.post.project_id}" \
           "/ideas/#{internal_comment1.post.id}##{internal_comment1.id}"
         )
     end
 
     it 'returns the correct url for an internal comment on an initiative' do
       expect(service.model_to_url(internal_comment2))
-        .to include(
-          "://#{Tenant.current.host}/admin/initiatives/#{internal_comment2.post.id}##{internal_comment2.id}"
-        )
+        .to eq("#{base_uri}/admin/initiatives/#{internal_comment2.post.id}##{internal_comment2.id}")
     end
   end
 end
