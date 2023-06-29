2ac1efb6bfd20f8fb478257da4f076d340942980
Sebastien Hoorens <sebastien@citizenlab.co>
2023-06-29 15:27:54 +0200

    improved coverage voting methods specs

--- back/spec/lib/voting_method/multiple_voting_spec.rb
+++ back/spec/lib/voting_method/multiple_voting_spec.rb
@@ -45,4 +45,30 @@ RSpec.describe VotingMethod::MultipleVoting do
       )
     end
   end
+
+  describe '#validate_baskets_idea' do
+    it 'sets no errors when the budget is blank' do
+      baskets_idea = build(:baskets_idea, idea: build(:idea, budget: nil))
+      voting_method.validate_baskets_idea baskets_idea
+      expect(baskets_idea.errors.details).to be_blank
+    end
+  end
+
+  describe '#budget_in_form?' do
+    it 'returns false when the user is a moderator of the participation context' do
+      expect(voting_method.budget_in_form?(create(:admin))).to be false
+    end
+  end
+
+  describe '#assign_basket' do
+    it 'does not overwrite the votes' do
+      basket = create(:basket, participation_context: project)
+      [1, 2, 3].map do |budget|
+        create(:baskets_idea, basket: basket, idea: create(:idea, budget: budget, project: project))
+      end
+      voting_method.assign_basket(basket.reload)
+      basket.save!
+      expect(basket.baskets_ideas.map(&:votes)).to contain_exactly 1, 1, 1
+    end
+  end
 end
