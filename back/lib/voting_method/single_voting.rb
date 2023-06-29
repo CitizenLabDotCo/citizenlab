fe1f4acaa8331135886d0594c95f6f12c4083905
jamesspeake <james@speake.org>
2023-06-29 09:04:44 +0100

    [CL-3876] Removed super

--- back/lib/voting_method/single_voting.rb
+++ back/lib/voting_method/single_voting.rb
@@ -6,7 +6,6 @@ module VotingMethod
       if participation_context.voting_max_votes_per_idea != 1
         participation_context.errors.add :voting_max_votes_per_idea, :invalid, message: 'voting_max_votes_per_idea must be 1'
       end
-      super
     end
   end
 end
