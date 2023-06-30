9cec668cf803f5eb0783cb60a59390d83a0e16de
Simon Tharby <simontharby@gmail.com>
2023-06-30 14:36:23 +0100

    [CL-3881] Simplify spec & move to frontend folder

--- back/engines/free/frontend/spec/services/frontend/url_service_spec.rb
+++ back/engines/free/frontend/spec/services/frontend/url_service_spec.rb
@@ -11,10 +11,9 @@ describe Frontend::UrlService do
     let(:internal_comment1) { create(:internal_comment, post: idea) }
     let(:initiative) { create(:initiative) }
     let(:internal_comment2) { create(:internal_comment, post: initiative) }
-    let(:user) { create(:user, locale: 'en') }
 
     it 'returns the correct url for an internal comment on an idea' do
-      expect(service.model_to_url(internal_comment1, locale: user.locale))
+      expect(service.model_to_url(internal_comment1, locale: 'en'))
         .to eq(
           "#{base_uri}/en/admin/projects/#{internal_comment1.post.project_id}" \
           "/ideas/#{internal_comment1.post.id}##{internal_comment1.id}"
@@ -22,7 +21,7 @@ describe Frontend::UrlService do
     end
 
     it 'returns the correct url for an internal comment on an initiative' do
-      expect(service.model_to_url(internal_comment2, locale: user.locale))
+      expect(service.model_to_url(internal_comment2, locale: 'en'))
         .to eq("#{base_uri}/en/admin/initiatives/#{internal_comment2.post.id}##{internal_comment2.id}")
     end
   end
