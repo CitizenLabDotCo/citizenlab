f0fd51e67b37e5d044e3f38b7ae003b07d5e1045
Sebastien Hoorens <sebastien@citizenlab.co>
2023-07-03 17:21:42 +0200

    remove nested attributes

--- back/app/models/basket.rb
+++ back/app/models/basket.rb
@@ -27,7 +27,6 @@ class Basket < ApplicationRecord
 
   has_many :baskets_ideas, -> { order(:created_at) }, dependent: :destroy, inverse_of: :basket
   has_many :ideas, through: :baskets_ideas
-  accepts_nested_attributes_for :baskets_ideas, allow_destroy: true
 
   validates :user, :participation_context, presence: true
   validate :basket_submission, on: :basket_submission
