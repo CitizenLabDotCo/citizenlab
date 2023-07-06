75b297864b46ea368f43cc62eb1191ac53c11621
Amanda Anderson <anderson_amanda@mail.com>
2023-07-06 10:43:51 +0200

    Fix bug on idea page

--- front/app/containers/IdeasShow/components/RightColumnDesktop/AssignVoteControl.tsx
+++ front/app/containers/IdeasShow/components/RightColumnDesktop/AssignVoteControl.tsx
@@ -7,6 +7,9 @@ import useIdeaById from 'api/ideas/useIdeaById';
 import styled from 'styled-components';
 import { colors, media } from 'utils/styleUtils';
 import AssignSingleVotesControl from 'components/AssignSingleVoteControl';
+import usePhases from 'api/phases/usePhases';
+import useProjectById from 'api/projects/useProjectById';
+import { getCurrentPhase } from 'api/phases/utils';
 
 const IdeaPageContainer = styled.div`
   display: flex;
@@ -25,12 +28,19 @@ interface Props {
 
 const AssignVoteControl = memo(({ ideaId, projectId }: Props) => {
   const { data: idea } = useIdeaById(ideaId);
+  const { data: project } = useProjectById(projectId);
+  const { data: phases } = usePhases(project?.data.id);
+  const participationContext = getCurrentPhase(phases?.data) || project?.data;
   const actionDescriptor = idea?.data.attributes.action_descriptor.voting;
   if (!actionDescriptor) return null;
 
   return (
     <IdeaPageContainer>
-      <AssignSingleVotesControl ideaId={ideaId} projectId={projectId} />
+      <AssignSingleVotesControl
+        ideaId={ideaId}
+        projectId={projectId}
+        participationContext={participationContext}
+      />
     </IdeaPageContainer>
   );
 });
