3b6ec6794f95100268683fd332ee1c444b457f47
Amanda Anderson <anderson_amanda@mail.com>
2023-07-07 11:33:36 +0200

    Fix cache invalidation issue

--- front/app/api/baskets/useUpdateBasket.ts
+++ front/app/api/baskets/useUpdateBasket.ts
@@ -32,14 +32,14 @@ const useUpdateBasket = () => {
       const contextId = data.data.relationships.participation_context.data.id;
       if (variables.participation_context_type === 'Project') {
         queryClient.invalidateQueries({
-          queryKey: phasesKeys.item({
-            phaseId: contextId,
+          queryKey: projectsKeys.item({
+            id: contextId,
           }),
         });
       } else {
         queryClient.invalidateQueries({
-          queryKey: projectsKeys.item({
-            id: contextId,
+          queryKey: phasesKeys.item({
+            phaseId: contextId,
           }),
         });
       }
