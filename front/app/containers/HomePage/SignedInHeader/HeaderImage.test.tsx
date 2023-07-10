60a7fa443d697e210bea315edc7703c5d923e8ea
brentguf <brentguf@users.noreply.github.com>
2023-07-10 17:23:12 +0200

    Fix typo in test

--- front/app/containers/HomePage/SignedInHeader/HeaderImage.test.tsx
+++ front/app/containers/HomePage/SignedInHeader/HeaderImage.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import HeaderImage from './HeaderImage';
 import { render, screen } from 'utils/testUtils/rtl';
 
-jest.mock('api/home_page/useHomePageSettings');
+jest.mock('api/home_page/useHomepageSettings');
 
 describe('HeaderImage', () => {
   it('the overlay has the right opacity', () => {
