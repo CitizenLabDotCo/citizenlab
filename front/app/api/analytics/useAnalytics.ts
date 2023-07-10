fd7923f41c03036b037b3830e8b965c143703f28
Iva <iva@citizenlab.co>
2023-07-10 10:48:40 +0300

    Fix analytics resolution display

--- front/app/api/analytics/useAnalytics.ts
+++ front/app/api/analytics/useAnalytics.ts
@@ -1,8 +1,9 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { CLErrors } from 'typings';
 import fetcher, { BaseResponseData } from 'utils/cl-react-query/fetcher';
 import analyticsKeys from './keys';
 import { Query } from './types';
+import { useEffect } from 'react';
 
 const fetchAnalytics = <Response extends BaseResponseData>(query: Query) =>
   fetcher<Response>({
@@ -15,6 +16,16 @@ const useAnalytics = <Response extends BaseResponseData>(
   query: Query,
   onSuccess?: () => void
 ) => {
+  const queryClient = useQueryClient();
+
+  // Call onSuccess if the query is already in the cache
+  useEffect(() => {
+    const queryKey = analyticsKeys.item(query);
+    if (queryClient.getQueryData(queryKey)) {
+      onSuccess && onSuccess();
+    }
+  }, [query, queryClient, onSuccess]);
+
   return useQuery<Response, CLErrors, Response, any>({
     queryKey: analyticsKeys.item(query),
     queryFn: () => fetchAnalytics(query),
