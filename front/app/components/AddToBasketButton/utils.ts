bf32f68b1a483b0281739109f2203963df29eb7d
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-07-11 15:19:43 +0100

    Remove more dead code

--- front/app/components/AddToBasketButton/utils.ts
+++ front/app/components/AddToBasketButton/utils.ts
@@ -1,39 +1,11 @@
-import { getLatestRelevantPhase } from 'api/phases/utils';
-
 // typings
-import { IIdea, IdeaVotingDisabledReason } from 'api/ideas/types';
-import { IPhases } from 'api/phases/types';
-import { IProject } from 'api/projects/types';
+import { IdeaVotingDisabledReason } from 'api/ideas/types';
 import {
   ActionDescriptorFutureEnabled,
   isFixableByAuthentication,
 } from 'utils/actionDescriptors';
 import { IBasket } from 'api/baskets/types';
 import { IBasketsIdeas } from 'api/baskets_ideas/types';
-
-export const getLatestRelevantParticipationContext = (
-  project: IProject | undefined,
-  idea: IIdea | undefined,
-  phases: IPhases | undefined
-) => {
-  if (!project) return;
-  if (project.data.attributes.process_type === 'continuous') {
-    return project.data;
-  }
-  if (!phases) return;
-
-  const ideaPhaseIds = idea?.data.relationships?.phases?.data?.map(
-    (item) => item.id
-  );
-
-  if (!ideaPhaseIds) return;
-
-  const ideaPhases = phases.data.filter((phase) =>
-    ideaPhaseIds.includes(phase.id)
-  );
-
-  return getLatestRelevantPhase(ideaPhases);
-};
 
 export const isButtonEnabled = (
   basket: IBasket | undefined,
