555334e76e52dc546a7f6da692d30ad51393616c
Sebastien Hoorens <sebastien@citizenlab.co>
2023-07-11 17:59:53 +0200

    yes officer

--- back/app/services/xlsx_service.rb
+++ back/app/services/xlsx_service.rb
@@ -143,7 +143,7 @@ class XlsxService
       { header: 'comments',             f: ->(i) { i.comments_count },                                                     skip_sanitization: true },
       { header: 'likes',                f: ->(i) { i.likes_count }, skip_sanitization: true },
       { header: 'dislikes',             f: ->(i) { i.dislikes_count }, skip_sanitization: true },
-      { header: 'url',                  f: ->(i) { Frontend::UrlService.new.model_to_url(i) },                             skip_sanitization: true, hyperlink: true },
+      { header: 'url',                  f: ->(i) { Frontend::UrlService.new.model_to_url(i) }, skip_sanitization: true, hyperlink: true },
       { header: 'project',              f: ->(i) { multiloc_service.t(i&.project&.title_multiloc) } },
       { header: 'topics',               f: ->(i) { i.topics.map { |t| multiloc_service.t(t.title_multiloc) }.join(',') } },
       { header: 'status',               f: ->(i) { multiloc_service.t(i&.idea_status&.title_multiloc) } },
