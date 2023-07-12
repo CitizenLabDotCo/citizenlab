4b565535d9bbc3d93aca4266c9a63ae0df0f47e7
Simon Tharby <simontharby@gmail.com>
2023-07-12 11:50:42 +0100

    [CL-3874] Remove useless test

--- back/spec/models/phase_spec.rb
+++ back/spec/models/phase_spec.rb
@@ -81,7 +81,7 @@ RSpec.describe Phase do
     end
   end
 
-  describe 'emails validation' do
+  describe 'campaigns_settings validation' do
     it 'fails when null' do
       phase = build(:phase, campaigns_settings: nil)
       expect(phase).to be_invalid
@@ -100,12 +100,6 @@ RSpec.describe Phase do
     it 'fails when contains invalid value' do
       phase = build(:phase, campaigns_settings: { invalid_key: 'not_a_boolean' })
       expect(phase).to be_invalid
-    end
-
-    # Not really necessary, but useful while developing
-    it 'succeeds when contains valid key and value' do
-      phase = build(:phase, campaigns_settings: { project_phase_started: false })
-      expect(phase).to be_valid
     end
   end
 
