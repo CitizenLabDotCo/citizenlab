36b43a1cf978edf6eb939ec22929924329c9bdef
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-07-13 15:18:43 +0100

    Scaffolding results component

--- front/app/containers/ProjectsShowPage/timeline/IdeaResults.tsx
+++ front/app/containers/ProjectsShowPage/timeline/IdeaResults.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
+// api
 import useIdeas from 'api/ideas/useIdeas';
 
+// i18n
+import useLocalize from 'hooks/useLocalize';
+
 // components
-import { Box } from '@citizenlab/cl2-component-library';
+import { Box, useBreakpoint } from '@citizenlab/cl2-component-library';
+import { IIdeaData } from 'api/ideas/types';
 
 interface Props {
   phaseId: string;
@@ -14,8 +19,53 @@ const IdeaResults = ({ phaseId }: Props) => {
     phase: phaseId,
     sort: 'baskets_count', // TODO: replace with votes_count when implemented by James
   });
+  const smallerThanPhone = useBreakpoint('phone');
+  const localize = useLocalize();
 
-  return <Box mt="20px">RESULTS TEST</Box>;
+  const desktopRows = useMemo(() => {
+    if (!ideas || smallerThanPhone) return;
+
+    const columns: IIdeaData[][] = [];
+
+    for (let i = 0; i < ideas.data.length; i = i + 2) {
+      const ideaLeft = ideas.data[i];
+      const ideaRight: IIdeaData | undefined = ideas.data[i + 1];
+
+      if (ideaRight) {
+        columns.push([ideaLeft, ideaRight]);
+      } else {
+        columns.push([ideaLeft]);
+      }
+    }
+
+    return columns;
+  }, [ideas, smallerThanPhone]);
+
+  return (
+    <Box mt="20px">
+      {smallerThanPhone
+        ? ideas?.data.map((idea) => (
+            <Box p="20px" bgColor="white" mt="20px" key={idea.id}>
+              {localize(idea.attributes.title_multiloc)}
+            </Box>
+          ))
+        : desktopRows?.map((row, rowIndex) => (
+            <Box
+              key={rowIndex}
+              display="flex"
+              w="100%"
+              mt="20px"
+              justifyContent="space-between"
+            >
+              {row.map((idea) => (
+                <Box p="20px" bgColor="white" key={idea.id}>
+                  {localize(idea.attributes.title_multiloc)}
+                </Box>
+              ))}
+            </Box>
+          ))}
+    </Box>
+  );
 };
 
 export default IdeaResults;
