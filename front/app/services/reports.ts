47438a932ceacd5be14a76fa4ca6da5e2b39a362
Iva <iva@citizenlab.co>
2023-07-13 12:21:31 +0300

    Add test

--- front/app/services/reports.ts
+++ front/app/services/reports.ts
@@ -5,21 +5,6 @@ import { JsonMultiloc } from 'components/admin/ContentBuilder/typings';
 const apiEndpoint = `${API_PATH}/reports`;
 
 // Report layouts
-export interface ReportLayout {
-  id: string;
-  type: 'content_builder_layout';
-  attributes: {
-    enabled: boolean;
-    code: 'report';
-    created_at: string;
-    updated_at: string;
-    craftjs_jsonmultiloc: JsonMultiloc;
-  };
-}
-
-export interface ReportLayoutResponse {
-  data: ReportLayout;
-}
 
 export async function updateReportLayout(
   id: string,
