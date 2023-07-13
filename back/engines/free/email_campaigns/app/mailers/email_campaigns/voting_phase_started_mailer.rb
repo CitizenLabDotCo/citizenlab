ff6d9d55923c200433cebc0c83d2ff34702e162e
jamesspeake <james@speake.org>
2023-07-13 16:09:24 +0100

    [CL-3928] Second two emails complete

--- back/engines/free/email_campaigns/app/mailers/email_campaigns/voting_phase_started_mailer.rb
+++ back/engines/free/email_campaigns/app/mailers/email_campaigns/voting_phase_started_mailer.rb
@@ -9,19 +9,28 @@ module EmailCampaigns
     end
 
     def subject
-      format_message('subject', values: { projectName: project_title })
+      format_message('subject', values: {
+        organizationName: organization_name,
+        projectName: localize_for_recipient(event.project_title_multiloc)
+      })
     end
 
     def header_title
-      format_message('main_header', values: { projectName: project_title })
+      localize_for_recipient(event.project_title_multiloc)
     end
 
     def header_message
-      format_message('event_description', values: { organizationName: organization_name })
+      format_message('event_description', values: {
+        projectName: localize_for_recipient(event.project_title_multiloc),
+        numIdeas: event.ideas.size
+      })
     end
 
     def preheader
-      format_message('preheader', values: { projectName: project_title })
+      format_message('preheader', values: {
+        organizationName: organization_name,
+        projectName: localize_for_recipient(event.project_title_multiloc)
+      })
     end
   end
 end
