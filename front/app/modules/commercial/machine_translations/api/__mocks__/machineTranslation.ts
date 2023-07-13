adf7132e19bf40c86725f8e4c41a6c9e09505b56
Edwin Kato <katedwin88@gmail.com>
2023-07-13 17:43:53 +0300

    Add mock for machine translation

---
+++ front/app/modules/commercial/machine_translations/api/__mocks__/machineTranslation.ts
@@ -0,0 +1,9 @@
+export const machineTranslation = {
+  id: '44c71081-7413-4b39-885d-232e78b2df53',
+  type: 'machine_translation',
+  attributes: {
+    attribute_name: 'body_multiloc',
+    translation: 'test translation',
+    locale_to: 'nl-BE',
+  },
+};
