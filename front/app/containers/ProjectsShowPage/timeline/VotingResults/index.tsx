6775b1e79d0e2bc5b43467491975fca553166e99
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-07-14 10:05:32 +0100

    Fix flex layout

--- front/app/containers/ProjectsShowPage/timeline/VotingResults/index.tsx
+++ front/app/containers/ProjectsShowPage/timeline/VotingResults/index.tsx
@@ -1,4 +1,5 @@
-import React, { useMemo } from 'react';
+import React from 'react';
+// import React, { useMemo } from 'react';
 
 // api
 import useIdeas from 'api/ideas/useIdeas';
@@ -8,7 +9,7 @@ import { Box, useBreakpoint } from '@citizenlab/cl2-component-library';
 import VotingResultCard from './VotingResultCard';
 
 // typings
-import { IIdeaData } from 'api/ideas/types';
+// import { IIdeaData } from 'api/ideas/types';
 
 interface Props {
   phaseId: string;
@@ -19,38 +20,46 @@ const VotingResults = ({ phaseId }: Props) => {
     phase: phaseId,
     sort: 'votes_count',
   });
+  const smallerThanTablet = useBreakpoint('tablet');
   const smallerThanPhone = useBreakpoint('phone');
 
-  const desktopRows = useMemo(() => {
-    if (!ideas || smallerThanPhone) return;
+  // const desktopRows = useMemo(() => {
+  //   if (!ideas || smallerThanPhone) return;
 
-    const columns: { idea: IIdeaData; rank: number }[][] = [];
+  //   const columns: { idea: IIdeaData; rank: number }[][] = [];
 
-    for (let i = 0; i < ideas.data.length; i = i + 2) {
-      // const ideaLeft = ideas.data[i];
-      // const ideaRight: IIdeaData | undefined = ideas.data[i + 1];
-      const left = { idea: ideas.data[i], rank: i + 1 };
-      const ideaRight: IIdeaData | undefined = ideas.data[i + 1];
-      const right = ideaRight ? { idea: ideaRight, rank: i + 2 } : undefined;
+  //   for (let i = 0; i < ideas.data.length; i = i + 2) {
+  //     const left = { idea: ideas.data[i], rank: i + 1 };
+  //     const ideaRight: IIdeaData | undefined = ideas.data[i + 1];
+  //     const right = ideaRight ? { idea: ideaRight, rank: i + 2 } : undefined;
 
-      if (right) {
-        columns.push([left, right]);
-      } else {
-        columns.push([left]);
-      }
-    }
+  //     if (right) {
+  //       columns.push([left, right]);
+  //     } else {
+  //       columns.push([left]);
+  //     }
+  //   }
 
-    return columns;
-  }, [ideas, smallerThanPhone]);
+  //   return columns;
+  // }, [ideas, smallerThanPhone]);
 
-  const mobileRows = useMemo(() => {
-    if (!ideas || !smallerThanPhone) return;
-    return ideas.data.map((idea, i) => ({ idea, rank: i + 1 }));
-  }, [ideas, smallerThanPhone]);
+  // const mobileRows = useMemo(() => {
+  //   if (!ideas || !smallerThanPhone) return;
+  //   return ideas.data.map((idea, i) => ({ idea, rank: i + 1 }));
+  // }, [ideas, smallerThanPhone]);
+
+  const getMx = (i: number) =>
+    smallerThanTablet ? {} : i % 2 ? { ml: '8px' } : { mr: '8px' };
 
   return (
-    <Box mt={smallerThanPhone ? '4px' : '8px'} w="100%">
-      {smallerThanPhone
+    <Box
+      mt={smallerThanPhone ? '4px' : '8px'}
+      w="100%"
+      display="flex"
+      flexWrap="wrap"
+      justifyContent="space-between"
+    >
+      {/* {smallerThanPhone
         ? mobileRows?.map((card) => (
             <Box key={card.idea.id} mb="8px">
               <VotingResultCard {...card} phaseId={phaseId} />
@@ -74,7 +83,19 @@ const VotingResults = ({ phaseId }: Props) => {
                 </Box>
               ))}
             </Box>
-          ))}
+          ))} */}
+      {ideas?.data.map((idea, i) => (
+        <Box
+          key={idea.id}
+          mb={smallerThanPhone ? '8px' : '20px'}
+          flexGrow={0}
+          width={smallerThanTablet ? '100%' : 'calc(50% - 16px)'}
+          {...getMx(i)}
+          // mx="10px"
+        >
+          <VotingResultCard idea={idea} phaseId={phaseId} rank={i + 1} />
+        </Box>
+      ))}
     </Box>
   );
 };
