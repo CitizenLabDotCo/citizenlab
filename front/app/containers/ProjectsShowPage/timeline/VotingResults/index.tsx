9e35055c18a6a1b6852cb19f0b2b60c081259772
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-07-14 10:06:13 +0100

    _

--- front/app/containers/ProjectsShowPage/timeline/VotingResults/index.tsx
+++ front/app/containers/ProjectsShowPage/timeline/VotingResults/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import React, { useMemo } from 'react';
 
 // api
 import useIdeas from 'api/ideas/useIdeas';
@@ -7,9 +6,6 @@ import useIdeas from 'api/ideas/useIdeas';
 // components
 import { Box, useBreakpoint } from '@citizenlab/cl2-component-library';
 import VotingResultCard from './VotingResultCard';
-
-// typings
-// import { IIdeaData } from 'api/ideas/types';
 
 interface Props {
   phaseId: string;
@@ -23,31 +19,6 @@ const VotingResults = ({ phaseId }: Props) => {
   const smallerThanTablet = useBreakpoint('tablet');
   const smallerThanPhone = useBreakpoint('phone');
 
-  // const desktopRows = useMemo(() => {
-  //   if (!ideas || smallerThanPhone) return;
-
-  //   const columns: { idea: IIdeaData; rank: number }[][] = [];
-
-  //   for (let i = 0; i < ideas.data.length; i = i + 2) {
-  //     const left = { idea: ideas.data[i], rank: i + 1 };
-  //     const ideaRight: IIdeaData | undefined = ideas.data[i + 1];
-  //     const right = ideaRight ? { idea: ideaRight, rank: i + 2 } : undefined;
-
-  //     if (right) {
-  //       columns.push([left, right]);
-  //     } else {
-  //       columns.push([left]);
-  //     }
-  //   }
-
-  //   return columns;
-  // }, [ideas, smallerThanPhone]);
-
-  // const mobileRows = useMemo(() => {
-  //   if (!ideas || !smallerThanPhone) return;
-  //   return ideas.data.map((idea, i) => ({ idea, rank: i + 1 }));
-  // }, [ideas, smallerThanPhone]);
-
   const getMx = (i: number) =>
     smallerThanTablet ? {} : i % 2 ? { ml: '8px' } : { mr: '8px' };
 
@@ -59,31 +30,6 @@ const VotingResults = ({ phaseId }: Props) => {
       flexWrap="wrap"
       justifyContent="space-between"
     >
-      {/* {smallerThanPhone
-        ? mobileRows?.map((card) => (
-            <Box key={card.idea.id} mb="8px">
-              <VotingResultCard {...card} phaseId={phaseId} />
-            </Box>
-          ))
-        : desktopRows?.map((row, rowIndex) => (
-            <Box
-              key={rowIndex}
-              display="flex"
-              w="100%"
-              mb="20px"
-              justifyContent="space-between"
-            >
-              {row.map((card, i) => (
-                <Box
-                  w="50%"
-                  key={card.idea.id}
-                  {...(i === 0 ? { mr: '8px' } : { ml: '8px' })}
-                >
-                  <VotingResultCard {...card} phaseId={phaseId} />
-                </Box>
-              ))}
-            </Box>
-          ))} */}
       {ideas?.data.map((idea, i) => (
         <Box
           key={idea.id}
@@ -91,7 +37,6 @@ const VotingResults = ({ phaseId }: Props) => {
           flexGrow={0}
           width={smallerThanTablet ? '100%' : 'calc(50% - 16px)'}
           {...getMx(i)}
-          // mx="10px"
         >
           <VotingResultCard idea={idea} phaseId={phaseId} rank={i + 1} />
         </Box>
