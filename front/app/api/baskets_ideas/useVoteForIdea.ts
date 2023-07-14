893c0b6ce16445be47d3854e5d9412ee4126fc20
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-07-14 17:12:11 +0100

    Fix cache invalidation issue

--- front/app/api/baskets_ideas/useVoteForIdea.ts
+++ front/app/api/baskets_ideas/useVoteForIdea.ts
@@ -6,17 +6,24 @@ import fetcher from 'utils/cl-react-query/fetcher';
 import { IBasketsIdea } from './types';
 import basketsKeys from '../baskets/keys';
 import basketsIdeasKeys from 'api/baskets_ideas/keys';
+import projectsKeys from 'api/projects/keys';
+import phasesKeys from 'api/phases/keys';
+import { IProjectData } from 'api/projects/types';
+import { IPhaseData } from 'api/phases/types';
 
 interface Params {
   idea_id: string;
   votes: number | null;
+  basket_id?: string;
+  project_id: string;
+  phase_id?: string;
 }
 
-export const voteForIdea = async (params: Params) =>
+export const voteForIdea = async ({ idea_id, votes }: Params) =>
   fetcher<IBasketsIdea>({
-    path: `/baskets/ideas/${params.idea_id}`,
+    path: `/baskets/ideas/${idea_id}`,
     action: 'put',
-    body: { baskets_idea: params },
+    body: { baskets_idea: { idea_id, votes } },
   });
 
 const useVoteForIdeaMutation = () => {
@@ -24,7 +31,7 @@ const useVoteForIdeaMutation = () => {
 
   return useMutation<IBasketsIdea, CLErrors, Params>({
     mutationFn: voteForIdea,
-    onSuccess: (data) => {
+    onSuccess: (data, variables) => {
       const basketId = data.data.relationships.basket.data.id;
 
       queryClient.invalidateQueries({
@@ -33,20 +40,56 @@ const useVoteForIdeaMutation = () => {
       queryClient.invalidateQueries({
         queryKey: basketsIdeasKeys.item({ basketId }),
       });
+
+      const { basket_id, project_id, phase_id } = variables;
+
+      const newBasket = basket_id !== basketId;
+
+      if (newBasket) {
+        if (project_id) {
+          queryClient.invalidateQueries({
+            queryKey: projectsKeys.item({ id: project_id }),
+          });
+        }
+
+        if (phase_id) {
+          queryClient.invalidateQueries({
+            queryKey: phasesKeys.item({ phaseId: phase_id }),
+          });
+
+          queryClient.invalidateQueries({
+            queryKey: phasesKeys.list({ projectId: project_id }),
+          });
+        }
+      }
     },
   });
 };
 
-const useVoteForIdea = () => {
+const useVoteForIdea = (participationContext?: IProjectData | IPhaseData) => {
   const [processing, setProcessing] = useState(false);
   const { mutateAsync } = useVoteForIdeaMutation();
 
   const handleVoteForIdea = useCallback(
-    async (ideaId: string, votes: number) => {
-      await mutateAsync({ idea_id: ideaId, votes: votes === 0 ? null : votes });
+    async (ideaId: string, votes: number, basketId?: string) => {
+      if (!participationContext) return;
+
+      await mutateAsync({
+        idea_id: ideaId,
+        votes: votes === 0 ? null : votes,
+        basket_id: basketId,
+        project_id:
+          participationContext.type === 'project'
+            ? participationContext.id
+            : participationContext.relationships.project.data.id,
+        phase_id:
+          participationContext.type === 'phase'
+            ? participationContext.id
+            : undefined,
+      });
       setProcessing(false);
     },
-    [mutateAsync]
+    [mutateAsync, participationContext]
   );
 
   const handleVoteForIdeaDebounced = useMemo(() => {
@@ -54,9 +97,9 @@ const useVoteForIdea = () => {
   }, [handleVoteForIdea]);
 
   const voteForIdea = useCallback(
-    (ideaId: string, votes: number) => {
+    (ideaId: string, votes: number, basketId?: string) => {
       setProcessing(true);
-      handleVoteForIdeaDebounced(ideaId, votes);
+      handleVoteForIdeaDebounced(ideaId, votes, basketId);
     },
     [handleVoteForIdeaDebounced]
   );
