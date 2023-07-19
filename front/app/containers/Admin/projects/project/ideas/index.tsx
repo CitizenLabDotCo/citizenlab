64eb67339280dbef338abf7fbf80172a77011db8
Iva <iva@citizenlab.co>
2023-07-19 11:45:46 +0300

    Add component to idea manager

--- front/app/containers/Admin/projects/project/ideas/index.tsx
+++ front/app/containers/Admin/projects/project/ideas/index.tsx
@@ -14,6 +14,7 @@ import PostManager, { TFilterMenu } from 'components/admin/PostManager';
 // resources
 import useProjectById from 'api/projects/useProjectById';
 import usePhases from 'api/phases/usePhases';
+import Analysis from './Analysis';
 
 const StyledDiv = styled.div`
   margin-bottom: 30px;
@@ -47,6 +48,7 @@ const AdminProjectIdeas = () => {
         </SectionDescription>
       </StyledDiv>
 
+      <Analysis />
       {project && (
         <PostManager
           type="ProjectIdeas"
