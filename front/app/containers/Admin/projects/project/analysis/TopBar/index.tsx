318acfae6acae5d38fb409073b6be71c666897a1
Iva <iva@citizenlab.co>
2023-07-19 12:28:25 +0300

    Add search

--- front/app/containers/Admin/projects/project/analysis/TopBar/index.tsx
+++ front/app/containers/Admin/projects/project/analysis/TopBar/index.tsx
@@ -10,13 +10,16 @@ import clHistory from 'utils/cl-router/history';
 import useProjectById from 'api/projects/useProjectById';
 import { useParams } from 'react-router-dom';
 import useLocalize from 'hooks/useLocalize';
+import SearchInput from 'components/UI/SearchInput';
+
+import { updateSearchParams } from 'utils/cl-router/updateSearchParams';
 
 const TopBar = () => {
   const { projectId } = useParams() as { projectId: string };
   const { data: project } = useProjectById(projectId);
-
   const projectTitle = project?.data.attributes.title_multiloc;
   const localize = useLocalize();
+
   return (
     <Box
       position="fixed"
@@ -35,6 +38,13 @@ const TopBar = () => {
       <Title variant="h4" m="0px">
         {localize(projectTitle)}
       </Title>
+      <Box marginLeft="auto">
+        <SearchInput
+          onChange={(search) => updateSearchParams({ search })}
+          // TODO: add a11y number of search results
+          a11y_numberOfSearchResults={0}
+        />
+      </Box>
     </Box>
   );
 };
