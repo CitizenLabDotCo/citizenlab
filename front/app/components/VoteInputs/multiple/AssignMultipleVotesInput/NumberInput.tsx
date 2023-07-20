d3b4899cb42a8478304f5cf180568358f9726e95
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-07-20 11:08:57 +0100

    Input seems to work well

--- front/app/components/VoteInputs/multiple/AssignMultipleVotesInput/NumberInput.tsx
+++ front/app/components/VoteInputs/multiple/AssignMultipleVotesInput/NumberInput.tsx
@@ -1,11 +1,12 @@
-import React, { FormEvent } from 'react';
+import React, { useState, FormEvent } from 'react';
 import styled from 'styled-components';
-import { fontSizes } from 'utils/styleUtils';
+import { fontSizes, colors } from 'utils/styleUtils';
 
 const StyledInput = styled.input`
   background-color: rgba(0, 0, 0, 0);
   font-size: ${fontSizes.xxl}px;
-  color: ${({ theme }) => theme.colors.textSecondary};
+  font-weight: 600;
+  color: ${colors.black};
   width: 50px;
   &:focus {
     outline-width: 0;
@@ -21,8 +22,15 @@ interface Props {
 const isNumeric = (value: string) => Number(value).toString() === value;
 
 const NumberInput = ({ value, max, onChange }: Props) => {
+  const [isEmpty, setIsEmpty] = useState(false);
+
   const handleChange = (e: FormEvent<HTMLInputElement>) => {
     const newStrValue = e.currentTarget.value;
+
+    if (newStrValue === '') {
+      setIsEmpty(true);
+      return;
+    }
 
     if (!isNumeric(newStrValue)) return;
     const newValue = Number(newStrValue);
@@ -30,10 +38,20 @@ const NumberInput = ({ value, max, onChange }: Props) => {
     if (newValue < 1) return;
 
     onChange(newValue < max ? newValue : max);
+    setIsEmpty(false);
+  };
+
+  const handleBlur = () => {
+    isEmpty ? onChange(0) : null;
   };
 
   return (
-    <StyledInput type="text" value={value.toString()} onChange={handleChange} />
+    <StyledInput
+      type="text"
+      value={isEmpty ? '' : value.toString()}
+      onChange={handleChange}
+      onBlur={handleBlur}
+    />
   );
 };
 
