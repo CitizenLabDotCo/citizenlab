8233d2cd7e20368c4b6d2bce387400b18c8e2f01
Edwin Kato <katedwin88@gmail.com>
2023-07-21 11:05:39 +0300

    Add followUnfollow types

---
+++ front/app/api/follow_unfollow/types.ts
@@ -0,0 +1,33 @@
+import { Keys } from 'utils/cl-react-query/types';
+import { IRelationship } from 'typings';
+import followerKeys from './keys';
+
+export type FollowerKeys = Keys<typeof followerKeys>;
+
+export interface IFollowerData {
+  id: string;
+  type: string;
+  attributes: {
+    created_at: string;
+    updated_at: string;
+  };
+  relationships: {
+    user: {
+      data: IRelationship;
+    };
+    followable: {
+      data: IRelationship;
+    };
+  };
+}
+
+export interface IFollower {
+  data: IFollowerData;
+}
+
+export type FollowableType = 'projects' | 'folders' | 'ideas' | 'proposals';
+
+export type FollowerAdd = {
+  followableType: FollowableType;
+  followableId: string;
+};
