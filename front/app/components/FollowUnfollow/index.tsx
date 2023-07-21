654bfa0f21d8f74fb263139e7eff8ae4e96329e3
Edwin Kato <katedwin88@gmail.com>
2023-07-21 11:21:47 +0300

    Update FollowUnfollow component to work with the add follower hook

--- front/app/components/FollowUnfollow/index.tsx
+++ front/app/components/FollowUnfollow/index.tsx
@@ -2,24 +2,32 @@ import React from 'react';
 import { Button } from '@citizenlab/cl2-component-library';
 import { useIntl } from 'utils/cl-intl';
 import messages from './messages';
+import { FollowableType } from 'api/follow_unfollow/types';
+import useAddFollower from 'api/follow_unfollow/useAddFollower';
 
 type Props = {
-  followableType: 'project' | 'folder' | 'idea' | 'proposal';
+  followableType: FollowableType;
   followableId: string; // id of the project, folder, idea or proposal
   followersCount?: number;
+  isCurrentUserFollowing: boolean;
 };
 
 const FollowUnfollow = ({
   followableType,
   followableId,
   followersCount,
+  isCurrentUserFollowing,
 }: Props) => {
   const { formatMessage } = useIntl();
-  const handleOnClick = () => {
-    console.log('followableType', followableType);
-    console.log('followableId', followableId);
+  const { mutateAsync: addFollower, isLoading } = useAddFollower();
+  const handleOnClick = async () => {
+    const response = await addFollower({
+      followableType,
+      followableId,
+    });
+    console.log('response', response);
   };
-  const text = followableId ? messages.unFollow : messages.follow;
+  const text = isCurrentUserFollowing ? messages.unFollow : messages.follow;
   const followText = followersCount
     ? `${formatMessage(text)} (${followersCount})`
     : formatMessage(text);
@@ -29,6 +37,7 @@ const FollowUnfollow = ({
       buttonStyle="primary-outlined"
       icon="notification"
       onClick={handleOnClick}
+      processing={isLoading}
     >
       {followText}
     </Button>
