56caf1dc8cd1a6fb9b61f2ef01af421af0573243
Alexander <alexander@citizenlab.co>
2023-07-24 18:58:53 +0200

    [TAN-18] Refactor initiative statuses filtering

--- back/app/policies/initiative_status_policy.rb
+++ back/app/policies/initiative_status_policy.rb
@@ -10,6 +10,15 @@ class InitiativeStatusPolicy < ApplicationPolicy
     end
 
     def resolve
+      filter_out_approval_statuses
+    end
+
+    private
+
+    # This method handles the case when approval was turned on,
+    # some initiatives were created, and then approval was turned off.
+    # We still want to show statuses of all initiatives after that.
+    def filter_out_approval_statuses
       initiatives = InitiativePolicy::Scope.new(user, Initiative).resolve
 
       if UserRoleService.new.can_moderate_initiatives?(user)
@@ -19,21 +28,16 @@ class InitiativeStatusPolicy < ApplicationPolicy
           approval_initiatives = initiatives.with_status_code(InitiativeStatus::APPROVAL_CODES)
 
           if approval_initiatives.any?
-            scope.all
+            scope.all # make it possible to change the status of initiatives that were created when approval was on
           else
             scope.where(code: InitiativeStatus::NOT_APPROVAL_CODES)
           end
         end
       else
         codes = InitiativeStatus::NOT_APPROVAL_CODES
-        InitiativeStatus::APPROVAL_CODES.each do |code|
-          # We want to show only statuses used by the current user's initiatives on the initiatives page
-          # http://localhost:3000/en/initiatives
-          # If this user doesn't use these statuses, we hide them, as other users' initiatives with this status
-          # are hidden for this user anyway.
-          codes += [code] if initiatives.with_status_code(code).any?
-        end
-        scope.where(code: codes)
+        # We want to show only statuses of initiatives available to the current user.
+        user_codes = initiatives.joins(:initiative_status).distinct.pluck('initiative_statuses.code')
+        scope.where(code: (codes + user_codes).uniq)
       end
     end
   end
