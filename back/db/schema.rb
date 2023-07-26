f2d075f9e6e5934dff8f0e723e31be69168f3c20
Sebastien Hoorens <sebastien@citizenlab.co>
2023-07-26 11:18:27 +0200

    manual merge

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -1180,11 +1180,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_19_221540) do
     t.integer "posting_limited_max", default: 1
     t.string "document_annotation_embed_url"
     t.boolean "allow_anonymous_participation", default: false, null: false
-    t.integer "voting_max_votes_per_idea"
-    t.jsonb "voting_term_singular_multiloc", default: {}
-    t.jsonb "voting_term_plural_multiloc", default: {}
-    t.integer "baskets_count", default: 0, null: false
-    t.integer "votes_count", default: 0, null: false
     t.integer "followers_count", default: 0, null: false
     t.string "voting_method"
     t.integer "voting_max_votes_per_idea"
