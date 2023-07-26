8e01ab24a13e155fbb2c8ed8c11764114c321866
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2023-07-26 09:31:04 +0200

    TAN-64 Rough intial inputs hook

---
+++ front/app/api/analysis_inputs/keys.ts
@@ -0,0 +1,28 @@
+import { QueryKeys } from 'utils/cl-react-query/types';
+import { IInputsQueryParams } from './types';
+
+const baseKey = { type: 'analysis_input' };
+
+const inputsKeys = {
+  all: () => [baseKey],
+  lists: () => [{ ...baseKey, operation: 'list' }],
+  list: ({
+    analysisId,
+    filters,
+  }: {
+    analysisId: string;
+    filters?: IInputsQueryParams;
+  }) => [
+    { ...baseKey, operation: 'list', analysisId, parameters: { ...filters } },
+  ],
+  items: () => [{ ...baseKey, operation: 'item' }],
+  item: ({ id }: { id: string }) => [
+    {
+      ...baseKey,
+      operation: 'item',
+      parameters: { id },
+    },
+  ],
+} satisfies QueryKeys;
+
+export default inputsKeys;
