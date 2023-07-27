f4d1dd73609ecc6ec68d65975dcdef06c3d1651f
Amanda Anderson <anderson_amanda@mail.com>
2023-07-27 10:46:08 +0200

    Use localize

--- front/app/containers/EventsShowPage/components/MetadataInformation/Location.tsx
+++ front/app/containers/EventsShowPage/components/MetadataInformation/Location.tsx
@@ -8,10 +8,13 @@ import { Container, Content, StyledIcon } from './MetadataInformationStyles';
 
 // typings
 import { Multiloc } from 'typings';
-import useLocale from 'hooks/useLocale';
 
 // utils
 import { isNilOrError } from 'utils/helperUtils';
+
+// api
+import useLocalize from 'hooks/useLocalize';
+import useLocale from 'hooks/useLocale';
 
 export interface Props {
   location: Multiloc;
@@ -19,13 +22,15 @@ export interface Props {
 
 const Location = ({ location }: Props) => {
   const currentLocale = useLocale();
+  const localize = useLocalize();
+
   if (location && !isNilOrError(currentLocale)) {
     return (
       <Container>
         <StyledIcon name="position" ariaHidden />
         <Content>
           <Text color="coolGrey600" fontSize="s">
-            {location[currentLocale]}
+            {localize(location)}
           </Text>
         </Content>
       </Container>
