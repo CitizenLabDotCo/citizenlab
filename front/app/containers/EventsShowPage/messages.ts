5d8299f952985940739eba0077b4c7428e74abd2
Amanda Anderson <anderson_amanda@mail.com>
2023-07-27 10:08:30 +0200

    Remove deadcode

--- front/app/containers/EventsShowPage/messages.ts
+++
@@ -1,8 +0,0 @@
-import { defineMessages } from 'react-intl';
-
-export default defineMessages({
-  attend: {
-    id: 'app.containers.eventsshowpage.attend',
-    defaultMessage: 'Attend',
-  },
-});
