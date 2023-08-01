66b9f05efadc90bb8aaff5f95bfb15e5089da8c6
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-08-01 10:28:49 +0100

    _

--- back/app/controllers/web_api/v1/handwritten_ideas_controller.rb
+++ back/app/controllers/web_api/v1/handwritten_ideas_controller.rb
@@ -1,15 +1,32 @@
 # frozen_string_literal: true
 require "google/cloud/vision"
 
+# https://github.com/weg-li/weg-li/blob/9261e1eecbb6bb6eb16393fe99126b9563ce78f5/app/lib/annotator.rb#L4
 class WebApi::V1::HandwrittenIdeasController < ApplicationController
-  @vision_client = Google::Cloud::Vision.image_annotator
-
   def create
-    @result = @vision_client.document_text_detection(
-      image: params[:file],
-      async: false
-    )
+    request = {
+      requests: [
+        {
+          image: {
+            content: params[:file][:file]
+          },
+          image_context: {
+            language_hints: ["en"]
+          },
+          features: [
+            { type: "DOCUMENT_TEXT_DETECTION" }
+          ]
+        }
+      ]
+    }
 
-    puts @result
+    response = image_annotator.batch_annotate_images(request)
+    response.responses.first.to_h
+  end
+
+  private
+
+  def image_annotator
+    @image_annotator ||= Google::Cloud::Vision.image_annotator
   end
 end
