95d004ac0e05f286e7ac6336559d0f716c3fc738
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2023-08-02 15:22:20 +0200

    TAN-142 Added platform_topic auto taggins spec

--- back/engines/commercial/analysis/spec/lib/auto_tagging_task_spec.rb
+++ back/engines/commercial/analysis/spec/lib/auto_tagging_task_spec.rb
@@ -23,4 +23,30 @@ RSpec.describe Analysis::AutoTaggingTask do
       expect(idea2.tags).not_to include(controversial_tag)
     end
   end
+
+  describe 'PlatformTopic auto_tagging' do
+    it 'works' do
+      att = create(:auto_tagging_task, state: 'queued', auto_tagging_method: 'platform_topic')
+      idea1 = create(:idea_with_topics, project: att.analysis.project, topics_count: 1)
+      topic1 = idea1.topics.first
+      idea2 = create(:idea_with_topics, project: att.analysis.project, topics_count: 1)
+      topic2 = idea2.topics.first
+      shared_topic = create(:topic)
+      idea1.topics << shared_topic
+      idea2.topics << shared_topic
+
+      expect { att.execute }
+        .to change(Analysis::Tag, :count).from(0).to(3)
+
+      att.reload
+      expect(att).to have_attributes({
+        state: 'succeeded',
+        progress: nil
+      })
+      expect(controversial_tag).to be_present
+      expect(idea1.tags).to include(Tag.find_by(name: shared_topic.title_multiloc.values))
+      expect(idea1.tags).to include(Tag.find_by(name: topic1.title_multiloc.values))
+      expect(idea1.tags).not_to include(Tag.find_by(name: topic2.title_multiloc.values))
+    end
+  end
 end
