3c3d828c925fba0eb62617f46ca2ad298a937118
jamesspeake <james@speake.org>
2023-08-04 13:28:58 +0100

    [TAN-178] Scaffolded different structure for import file

--- back/engines/commercial/bulk_import_ideas/app/services/bulk_import_ideas/import_project_ideas_service.rb
+++ back/engines/commercial/bulk_import_ideas/app/services/bulk_import_ideas/import_project_ideas_service.rb
@@ -5,28 +5,33 @@ module BulkImportIdeas
 
     def initialize(project_id)
       @project = Project.find(project_id)
+      @project_fields = IdeaCustomFieldsService.new(Factory.instance.participation_method_for(@project).custom_form).enabled_fields
       super()
     end
 
     def generate_example_xlsx
-      XlsxService.new.hash_array_to_xlsx [
-       {
-         'ID' => '1',
-         'Title_nl-BE' => 'Mijn idee titel',
-         'Title_fr-BE' => 'Mon idée titre',
-         'Body_nl-BE' => 'Mijn idee inhoud',
-         'Body_fr-BE' => 'Mon idée contenu',
-         'Email' => 'moderator@citizenlab.co',
-         'Project' => 'Project 1',
-         'Phase' => 1,
-         'Image URL' => 'https://cl2-seed-and-template-assets.s3.eu-central-1.amazonaws.com/images/people_in_meeting_graphic.png',
-         'Date (dd-mm-yyyy)' => '18-07-2022',
-         'Topics' => 'Mobility; Health and welfare',
-         'Latitude' => 50.5035,
-         'Longitude' => 6.0944,
-         'Location Description' => 'Panorama sur les Hautes Fagnes / Hohes Venn'
-       }
-     ]
+      columns = {
+        'ID' => '1',
+        'Locale' => 'en',
+        'Email' => 'moderator@citizenlab.co',
+        'Date (dd-mm-yyyy)' => '18-07-2022',
+      }
+      columns['Phase'] = 1 if @project.timeline? # Only if there are phases in the project
+
+      ignore_columns = %w[topic_ids idea_files_attributes idea_images_attributes]
+      @project_fields.each do |field|
+        next if field.input_type == 'section'
+        next if ignore_columns.include? field.code
+
+        columns[field.code] = 'test'
+      end
+
+      columns['Image URL'] = 'https://cl2-seed-and-template-assets.s3.eu-central-1.amazonaws.com/images/people_in_meeting_graphic.png'
+      columns['Topics'] = 'Mobility; Health and welfare'
+
+      binding.pry
+
+      XlsxService.new.hash_array_to_xlsx [columns]
     end
   end
 end
