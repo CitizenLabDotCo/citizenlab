4e566048283d5b25cd8a86fb29068791494cdfa3
Amanda Anderson <anderson_amanda@mail.com>
2023-08-07 15:08:57 +0200

    Add project link to event page

--- front/app/containers/EventsShowPage/index.tsx
+++ front/app/containers/EventsShowPage/index.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 // components
 import {
   Box,
+  Icon,
   Spinner,
   Title,
+  colors,
   media,
   useBreakpoint,
+  Text,
 } from '@citizenlab/cl2-component-library';
 import Container from './components/Container';
 import RightColumnDesktop from './components/RightColumnDesktop';
@@ -17,6 +20,7 @@ import VerticalCenterer from 'components/VerticalCenterer';
 import EventTopBarMobile from './components/EventTopBarMobile';
 import EventDescription from './components/EventDescription';
 import InformationSectionMobile from './components/InformationSectionMobile';
+import Link from 'utils/cl-router/Link';
 
 // styling
 import styled from 'styled-components';
@@ -32,6 +36,9 @@ import useProjectById from 'api/projects/useProjectById';
 // utils
 import { isNilOrError } from 'utils/helperUtils';
 import { isUnauthorizedRQ } from 'utils/errorUtils';
+import { useIntl } from 'utils/cl-intl';
+import messages from './messages';
+import useLocalize from 'hooks/useLocalize';
 
 const InnerContainer = styled(Box)`
   min-height: calc(
@@ -56,13 +63,19 @@ const InnerContainer = styled(Box)`
 `;
 const EventsShowPage = () => {
   const isSmallerThanTablet = useBreakpoint('tablet');
+  const { formatMessage } = useIntl();
   const locale = useLocale();
+  const localize = useLocalize();
   const { eventId } = useParams() as {
     eventId: string;
   };
   const { data: event, status, error } = useEvent(eventId);
   const { data: project } = useProjectById(
     event?.data.relationships.project.data.id
+  );
+
+  const projectTitleLocalized = localize(
+    project?.data.attributes.title_multiloc
   );
 
   if (status === 'loading') {
@@ -100,6 +113,45 @@ const EventsShowPage = () => {
               <Title id="e2e-event-title" variant="h1">
                 {event?.data.attributes.title_multiloc[locale]}
               </Title>
+              {projectTitleLocalized && (
+                <Box display="flex" my="24px" gap="8px">
+                  <Box
+                    h="40px"
+                    w="40px"
+                    background={colors.grey200}
+                    borderRadius="90px"
+                  >
+                    <Icon
+                      name="calendar"
+                      fill={colors.primary}
+                      my="auto"
+                      mx="auto"
+                      width="100%"
+                      height="100%"
+                      p="8px"
+                    />
+                  </Box>
+                  <Box>
+                    <Text p="0px" m="0px">
+                      {formatMessage(messages.eventFrom, {
+                        eventTitle: projectTitleLocalized,
+                      })}
+                    </Text>
+                    <Link to={`/projects/${project.data.attributes.slug}`}>
+                      <Text
+                        fontSize="s"
+                        p="0px"
+                        m="0px"
+                        color="coolGrey600"
+                        textDecoration="underline"
+                      >
+                        {formatMessage(messages.goToProject)}
+                      </Text>
+                    </Link>
+                  </Box>
+                </Box>
+              )}
+
               <Box mb="40px">
                 {event && <EventDescription event={event?.data} />}
                 {isSmallerThanTablet && event && (
---
+++ front/app/containers/EventsShowPage/messages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  eventFrom: {
+    id: 'app.containers.EventsShow.eventFrom',
+    defaultMessage: 'Event from "{eventTitle}"',
+  },
+  goToProject: {
+    id: 'app.containers.EventsShow.goToProject',
+    defaultMessage: 'Go to the project',
+  },
+});
