c7d631608a4ecb5fc0a6f28f0daaf5260dd380f5
Simon Tharby <simontharby@gmail.com>
2023-08-09 11:13:45 +0100

    [TAN-157] Remove relationships from serializer

--- back/app/serializers/web_api/v1/cosponsors_initiative_serializer.rb
+++ back/app/serializers/web_api/v1/cosponsors_initiative_serializer.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
 class WebApi::V1::CosponsorsInitiativeSerializer < WebApi::V1::BaseSerializer
-  attributes :user_id, :initiative_id, :status
-
-  belongs_to :user
-  belongs_to :initiative
+  attributes :user_id, :initiative_id, :status, :created_at, :updated_at
 end
