2a0928a7f0393231b2b00d76abf1014eb4663548
jamesspeake <james@speake.org>
2023-08-11 17:13:56 +0100

    [TAN-181] Post migrate change

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -32,14 +32,12 @@
 #  block_reason                        :string
 #  block_end_at                        :datetime
 #  new_email                           :string
-#  unique_code                         :string
 #
 # Indexes
 #
 #  index_users_on_email                      (email)
 #  index_users_on_registration_completed_at  (registration_completed_at)
 #  index_users_on_slug                       (slug) UNIQUE
-#  index_users_on_unique_code                (unique_code) UNIQUE
 #  users_unique_lower_email_idx              (lower((email)::text)) UNIQUE
 #
 class User < ApplicationRecord
