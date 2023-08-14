1004b7255a1d27be9c97303a9801d23cd07c58e5
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2023-08-14 18:47:55 +0200

    TAN-139 Clean up deleted custom_field references from filters

--- back/engines/commercial/analysis/app/models/analysis/summary.rb
+++ back/engines/commercial/analysis/app/models/analysis/summary.rb
@@ -37,10 +37,24 @@ module Analysis
     validates :filters, json: { schema: FILTERS_JSON_SCHEMA }
 
     scope :filters_with_tag_id, ->(tag_id) { where("filters->'tag_ids' ? :tag_id", tag_id: tag_id) }
+    scope :filters_with_custom_field_id, lambda { |custom_field_id|
+      where("(
+        filters ? 'author_custom_#{custom_field_id}' OR
+        filters ? 'author_custom_#{custom_field_id}_from' OR
+        filters ? 'author_custom_#{custom_field_id}_to'
+        )")
+    }
 
     def self.delete_tag_references!(tag_id)
       filters_with_tag_id(tag_id).each do |summary|
         summary.filters['tag_ids'].delete(tag_id)
+        summary.save!
+      end
+    end
+
+    def self.delete_custom_field_references!(custom_field_id)
+      filters_with_custom_field_id(custom_field_id).each do |summary|
+        summary.filters.reject! { |k, _v| k.match?(/^author_custom_#{custom_field_id}.*$/) }
         summary.save!
       end
     end
---
+++ back/engines/commercial/analysis/spec/models/summary_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Analysis::Summary do
+  subject { summary }
+
+  let(:summary) { build(:summary) }
+
+  describe 'Default factory' do
+    it { is_expected.to be_valid }
+  end
+
+  describe 'filters' do
+    it 'is valid when all static properties are set' do
+      summary.filters = {
+        'search' => 'dog',
+        'reactions_from' => 3,
+        'reactions_to' => 6,
+        'votes_from' => 1,
+        'votes_to' => 2,
+        'comments_from' => 0,
+        'comments_to' => 100,
+        'tag_ids' => [SecureRandom.uuid]
+      }
+      expect(summary).to be_valid
+    end
+
+    it 'is valid when custom_author properties are set' do
+      summary.filters = {
+        "author_custom_#{SecureRandom.uuid}" => [4],
+        "author_custom_#{SecureRandom.uuid}_from" => 5,
+        "author_custom_#{SecureRandom.uuid}_to" => 1
+      }
+      expect(summary).to be_valid
+    end
+
+    it 'is invalid when extra properties are set' do
+      summary.filters = {
+        'this is not a filter' => 5
+      }
+      expect(summary).to be_invalid
+    end
+  end
+end
