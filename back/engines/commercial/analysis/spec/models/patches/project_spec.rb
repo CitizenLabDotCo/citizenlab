6243b11fe3bbe34a45c9b85c609daa8342f8d241
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2023-08-14 15:37:33 +0200

    TAN-139 Fix referential integrity for phase/project<>analysis

---
+++ back/engines/commercial/analysis/spec/models/patches/project_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Phase do
+  subject { create(:phase) }
+
+  describe 'Deleting a project' do
+    it 'deletes its analyses' do
+      analysis = create(:analysis)
+      expect { analysis.project.destroy }.to change(Analysis::Analysis, :count).from(1).to(0)
+    end
+  end
+end
