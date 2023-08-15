3eb9d5ef98af9516de61a4aedd2cdd4e4bf445ba
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-08-15 17:10:42 +0100

    Make ideas selectable

--- front/app/containers/Admin/projects/project/offlineInputs/ReviewSection.tsx
+++ front/app/containers/Admin/projects/project/offlineInputs/ReviewSection.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import moment from 'moment';
 
 // routing
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
+import { updateSearchParams } from 'utils/cl-router/updateSearchParams';
 
 // api
 import useImportedIdeas from 'api/import_ideas/useImportedIdeas';
@@ -14,12 +15,25 @@ import useLocalize from 'hooks/useLocalize';
 import { Box, Spinner, Title, Text } from '@citizenlab/cl2-component-library';
 
 // styling
+import styled from 'styled-components';
 import { colors } from 'utils/styleUtils';
+
+// TODO move to component library
+const TEAL50 = '#EDF8FA';
+
+const StyledBox = styled(Box)`
+  &:hover {
+    background-color: ${TEAL50};
+  }
+`;
 
 const ReviewSection = () => {
   const { projectId } = useParams() as {
     projectId: string;
   };
+  const [searchParams] = useSearchParams();
+  const ideaId = searchParams.get('idea_id');
+
   const { data: ideas, isLoading } = useImportedIdeas({ projectId });
   const localize = useLocalize();
 
@@ -42,18 +56,28 @@ const ReviewSection = () => {
       <Box display="flex" w="100%" px="52px" justifyContent="space-between">
         <Box w="25%" borderRight={`1px ${colors.grey400} solid`} pr="8px">
           {ideas.data.map((idea) => (
-            <Box
+            <StyledBox
               key={idea.id}
               py="8px"
               borderBottom={`1px ${colors.grey400} solid`}
+              style={{ cursor: 'pointer' }}
+              bgColor={idea.id === ideaId ? TEAL50 : undefined}
+              onClick={() => {
+                updateSearchParams({ idea_id: idea.id });
+              }}
             >
-              <Text m="0" color="black" fontSize="m">
+              <Text
+                m="0"
+                color="black"
+                fontSize="m"
+                fontWeight={idea.id === ideaId ? 'bold' : 'normal'}
+              >
                 {localize(idea.attributes.title_multiloc)}
               </Text>
               <Text m="0" mt="3px" fontSize="s" color="grey600">
                 {moment(idea.attributes.created_at).format('YYYY-MM-DD')}
               </Text>
-            </Box>
+            </StyledBox>
           ))}
         </Box>
         <Box w="35%" borderRight={`1px ${colors.grey400} solid`}>
