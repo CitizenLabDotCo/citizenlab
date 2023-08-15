5cf3705aa5ba639a031593753b15632a4e228e43
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-08-15 16:44:34 +0100

    Idea selection column (WIP)

--- front/app/containers/Admin/projects/project/offlineInputs/ReviewSection.tsx
+++ front/app/containers/Admin/projects/project/offlineInputs/ReviewSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 // routing
 import { useParams } from 'react-router-dom';
@@ -33,25 +34,32 @@ const ReviewSection = () => {
   if (ideas === undefined || ideas.data.length === 0) return null;
 
   return (
-    <Box mt="40px" w="100%" bgColor={colors.white}>
-      <Box display="flex" justifyContent="center" w="100%">
-        <Box w="800px" p="20px">
-          <Title variant="h2" color="primary">
-            Ideas imported
-          </Title>
-        </Box>
-      </Box>
+    <Box mt="40px" w="100%" bgColor={colors.white} py="32px">
+      <Title variant="h2" color="primary" px="52px" mb="40px">
+        Ideas imported
+      </Title>
 
-      <Box display="flex" w="100%" p="50px" justifyContent="space-between">
-        <Box>
+      <Box display="flex" w="100%" px="52px" justifyContent="space-between">
+        <Box w="25%" borderRight={`1px ${colors.grey400} solid`} pr="8px">
           {ideas.data.map((idea) => (
-            <Box key={idea.id}>
-              <Text>{localize(idea.attributes.title_multiloc)}</Text>
+            <Box
+              key={idea.id}
+              py="8px"
+              borderBottom={`1px ${colors.grey400} solid`}
+            >
+              <Text m="0" color="black" fontSize="m">
+                {localize(idea.attributes.title_multiloc)}
+              </Text>
+              <Text m="0" mt="3px" fontSize="s" color="grey600">
+                {moment(idea.attributes.created_at).format('YYYY-MM-DD')}
+              </Text>
             </Box>
           ))}
         </Box>
-        <Box>Idea form</Box>
-        <Box>PDF preview</Box>
+        <Box w="35%" borderRight={`1px ${colors.grey400} solid`}>
+          Idea form
+        </Box>
+        <Box w="40%">PDF preview</Box>
       </Box>
     </Box>
   );
