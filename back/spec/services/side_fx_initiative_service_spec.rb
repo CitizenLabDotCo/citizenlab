5451c7b5bfbe8033ca5241f5093eb4c5c1c5b30f
Simon Tharby <simontharby@gmail.com>
2023-08-15 15:07:37 +0100

    [TAN-222] Add test of LogActivityJob(s) when cosponsors_initiatives created by after_update

--- back/spec/services/side_fx_initiative_service_spec.rb
+++ back/spec/services/side_fx_initiative_service_spec.rb
@@ -19,5 +19,25 @@ describe SideFxInitiativeService do
         expect(initiative.reload.initiative_status.code).to eq 'review_pending'
       end
     end
+
+    context 'when update results in new cosponsors' do
+      let(:initiative) { create(:initiative, author: user) }
+      let(:cosponsor1) { create(:user) }
+      let(:cosponsor2) { create(:user) }
+
+      it "logs a CosponsorsInitiative 'created' action job" do
+        initiative.update!(cosponsor_ids: [cosponsor1.id, cosponsor2.id])
+        cosponsors_initiatives =
+          CosponsorsInitiative.where(initiative: initiative).where(user_id: [cosponsor1.id, cosponsor2.id])
+
+        expect { service.after_update(initiative, user, _old_cosponsors_ids = []) }
+          .to enqueue_job(LogActivityJob)
+          .with(cosponsors_initiatives[0], 'created', user, cosponsors_initiatives[0].created_at.to_i)
+          .exactly(1).times
+          .and enqueue_job(LogActivityJob)
+          .with(cosponsors_initiatives[1], 'created', user, cosponsors_initiatives[1].created_at.to_i)
+          .exactly(1).times
+      end
+    end
   end
 end
