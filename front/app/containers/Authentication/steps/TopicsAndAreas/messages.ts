a8870d1b9e2aa53b42e311f35ac76a7cc490b2cc
EdwinKato <katedwin88@gmail.com>
2023-08-16 11:48:49 +0300

    Add messages for onboarding topics and areas

---
+++ front/app/containers/Authentication/steps/TopicsAndAreas/messages.ts
@@ -0,0 +1,16 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  topicsOfInterest: {
+    id: 'app.containers.SignUp.topicsAndAreas.topicsOfInterest',
+    defaultMessage: 'Topics of interest:',
+  },
+  areasOfFocus: {
+    id: 'app.containers.SignUp.topicsAndAreas.areasOfFocus',
+    defaultMessage: 'Explore areas of focus',
+  },
+  skip: {
+    id: 'app.containers.SignUp.topicsAndAreas.skip',
+    defaultMessage: 'Skip',
+  },
+});
