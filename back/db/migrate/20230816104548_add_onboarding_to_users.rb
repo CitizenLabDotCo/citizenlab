dcb9f216fc24ba0410e723021068395d810873f0
EdwinKato <katedwin88@gmail.com>
2023-08-16 20:39:38 +0300

    Change migrations

--- back/db/migrate/20230816104548_add_onboarding_to_users.rb
+++ back/db/migrate/20230816104548_add_onboarding_to_users.rb
@@ -1,5 +1,5 @@
 class AddOnboardingToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :onboarding, :jsonb, default: { "topics_and_areas": "ask" }, null: false
+    add_column :users, :onboarding, :jsonb, default: {}, null: false
   end
 end
--- back/db/migrate/20230816105155_set_default_onboarding_for_existing_users.rb
+++
@@ -1,5 +0,0 @@
-class SetDefaultOnboardingForExistingUsers < ActiveRecord::Migration[7.0]
-  def up
-    User.where(onboarding: nil).update_all(onboarding: { "topics_and_areas": "ask" })
-  end
-end
