237b862576d24b3685278b6e07f0ff5d4cf3af05
Brent Guffens <brentguf@users.noreply.github.com>
2023-08-16 10:47:35 +0200

    Link to cosponsor user profile

--- front/app/containers/InitiativesShow/Cosponsors/ListOfCosponsors.tsx
+++ front/app/containers/InitiativesShow/Cosponsors/ListOfCosponsors.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Box, Text } from '@citizenlab/cl2-component-library';
+import { Box } from '@citizenlab/cl2-component-library';
 import Avatar from 'components/Avatar';
 import { IInitiativeCosponsorship } from 'api/initiatives/types';
+import UserName from 'components/UI/UserName';
 
 interface Props {
   cosponsorships: IInitiativeCosponsorship[];
@@ -16,7 +17,7 @@ const ListOfCosponsors = ({ cosponsorships }: Props) => {
             <Box mr="4px">
               <Avatar userId={cosponsorship.user_id} size={32} />
             </Box>
-            <Text fontSize="base">{cosponsorship.name}</Text>
+            <UserName userId={cosponsorship.user_id} isLinkToProfile />
           </Box>
         );
       })}
