e075a3a29575a6451372b18b412be8d6d37ec707
Alexander <alexander@citizenlab.co>
2023-08-17 18:10:47 +0200

    [CL-3499] Use shortened token for Nemlog-in requests
    https://citizenlabco.slack.com/archives/C016C2EHURY/p1692256376654149

--- back/engines/commercial/id_nemlog_in/config/initializers/omniauth.rb
+++ back/engines/commercial/id_nemlog_in/config/initializers/omniauth.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 NEMLOG_IN_SAML_SETUP_PROC = lambda do |env|
-  IdNemlogIn::NemlogInOmniauth.new.omniauth_setup(AppConfiguration.instance, env)
+  omniauth = IdNemlogIn::NemlogInOmniauth.new
+  omniauth.replace_token_param!(env)
+  omniauth.omniauth_setup(AppConfiguration.instance, env)
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
