0c68d409b1d0b4191c2fa89ff2957a7e388e76b8
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2023-08-17 12:59:38 +0200

    TAN-219 Updated design, tag colors and displayed loading state

--- front/app/containers/Admin/projects/project/analysis/Tags/AutotaggingModal.tsx
+++ front/app/containers/Admin/projects/project/analysis/Tags/AutotaggingModal.tsx
@@ -1,25 +1,33 @@
-import { Box, Title, Text, colors } from '@citizenlab/cl2-component-library';
-import { TagType } from 'api/analysis_tags/types';
 import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
 
 import useLaunchAnalysisAutotagging from 'api/analysis_background_tasks/useLaunchAnalysisAutotagging';
-import { useParams } from 'react-router-dom';
-import { TagTypeColorMap } from './Tag';
-import styled from 'styled-components';
+import { TagType } from 'api/analysis_tags/types';
 
-const AutoTagOptionContainer = styled.div`
-  cursor: pointer;
+import {
+  Box,
+  Title,
+  Text,
+  colors,
+  Spinner,
+} from '@citizenlab/cl2-component-library';
+import Tag from './Tag';
+
+const AutoTagOptionContainer = styled.div<{ disabled: boolean }>`
   background-color: ${colors.grey100};
   border-radius: 3px;
-  display: flex;
-  align-items: center;
-  gap: 20px;
-  &:hover {
-    border: 1px black;
-    box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,
-      rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
-  }
-  min-height: 90px;
+  padding: 16px;
+  ${({ disabled }) =>
+    disabled
+      ? ''
+      : `
+      cursor: pointer;
+      &:hover {
+        border: 1px black;
+        box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,
+          rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+      }`}
 `;
 
 const AutoTagOption = ({
@@ -27,35 +35,42 @@ const AutoTagOption = ({
   tagType,
   title,
   onSelect,
+  disabled,
+  isLoading,
 }: {
   children: ReactNode;
   tagType: TagType;
   title: string;
   onSelect: () => void;
+  disabled: boolean;
+  isLoading: boolean;
 }) => {
   return (
-    <AutoTagOptionContainer onClick={() => onSelect()}>
-      <Box
-        color={TagTypeColorMap[tagType].text}
-        background={TagTypeColorMap[tagType].background}
-        px="12px"
-        py="4px"
-        flex="0 0 160px"
-        ml="-14px"
-        boxShadow="rgba(50, 50, 105, 0.15) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px;"
-      >
-        {title}
+    <AutoTagOptionContainer onClick={() => onSelect()} disabled={disabled}>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Tag tagType={tagType} name={title} />
+        {isLoading && (
+          <Box mx="16px">
+            <Spinner size="24px" />
+          </Box>
+        )}
       </Box>
-      <Text px="20px">{children}</Text>
+      <Text my="6px">{children}</Text>
     </AutoTagOptionContainer>
   );
 };
 
 const AutotaggingModal = ({ onCloseModal }: { onCloseModal: () => void }) => {
   const { analysisId } = useParams() as { analysisId: string };
-  const { mutate: launchTagging } = useLaunchAnalysisAutotagging();
+  const {
+    mutate: launchTagging,
+    isLoading,
+    variables,
+  } = useLaunchAnalysisAutotagging();
 
   const handleOnSelectMethod = (tagType) => () => {
+    if (isLoading) return;
+
     launchTagging(
       { analysisId, autoTaggingMethod: tagType },
       {
@@ -65,6 +80,7 @@ const AutotaggingModal = ({ onCloseModal }: { onCloseModal: () => void }) => {
       }
     );
   };
+
   return (
     <Box px="24px">
       <Title mb="32px">What tags do you want to add?</Title>
@@ -72,11 +88,18 @@ const AutotaggingModal = ({ onCloseModal }: { onCloseModal: () => void }) => {
         Auto-tags are automatically derived by the computer. You can change or
         remove them at all times.
       </Text>
-      <Box display="flex" flexDirection="column" gap="16px">
+      <Box
+        display="flex"
+        flexDirection="column"
+        gap="16px"
+        opacity={isLoading ? 0.5 : undefined}
+      >
         <AutoTagOption
           tagType="sentiment"
           title="Sentiment"
           onSelect={handleOnSelectMethod('sentiment')}
+          disabled={isLoading}
+          isLoading={isLoading && variables?.autoTaggingMethod === 'sentiment'}
         >
           <>
             Assign a positive or negative sentiment to each input, derived from
@@ -88,22 +111,20 @@ const AutotaggingModal = ({ onCloseModal }: { onCloseModal: () => void }) => {
           tagType="nlp_topic"
           title="AI tags"
           onSelect={handleOnSelectMethod('nlp_topic')}
+          disabled={isLoading}
+          isLoading={isLoading && variables?.autoTaggingMethod === 'nlp_topic'}
         >
           <>Assign new tags, based on topics derived from the text</>
-        </AutoTagOption>
-
-        <AutoTagOption
-          tagType="language"
-          title="Language"
-          onSelect={handleOnSelectMethod('language')}
-        >
-          <>Detect the language of each input</>
         </AutoTagOption>
 
         <AutoTagOption
           tagType="controversial"
           title="Controversial"
           onSelect={handleOnSelectMethod('controversial')}
+          disabled={isLoading}
+          isLoading={
+            isLoading && variables?.autoTaggingMethod === 'controversial'
+          }
         >
           <>Detect inputs with a significant dislikes/likes ratio</>
         </AutoTagOption>
@@ -112,11 +133,25 @@ const AutotaggingModal = ({ onCloseModal }: { onCloseModal: () => void }) => {
           tagType="platform_topic"
           title="Platform tags"
           onSelect={handleOnSelectMethod('platform_topic')}
+          disabled={isLoading}
+          isLoading={
+            isLoading && variables?.autoTaggingMethod === 'platform_topic'
+          }
         >
           <>
             Assign the existing platform tags that the author picked when
             posting
           </>
+        </AutoTagOption>
+
+        <AutoTagOption
+          tagType="language"
+          title="Language"
+          onSelect={handleOnSelectMethod('language')}
+          disabled={isLoading}
+          isLoading={isLoading && variables?.autoTaggingMethod === 'language'}
+        >
+          <>Detect the language of each input</>
         </AutoTagOption>
       </Box>
     </Box>
