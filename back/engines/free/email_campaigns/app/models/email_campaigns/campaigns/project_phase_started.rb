244ec49af0ed35d5b3b003263360d3857fbcc3a8
Sebastien Hoorens <sebastien@citizenlab.co>
2023-08-22 13:04:18 +0200

    admins cannot disable follow campaigns

--- back/engines/free/email_campaigns/app/models/email_campaigns/campaigns/project_phase_started.rb
+++ back/engines/free/email_campaigns/app/models/email_campaigns/campaigns/project_phase_started.rb
@@ -31,7 +31,6 @@ module EmailCampaigns
     include Consentable
     include ActivityTriggerable
     include RecipientConfigurable
-    include Disableable
     include Trackable
     include LifecycleStageRestrictable
     allow_lifecycle_stages only: ['active']
