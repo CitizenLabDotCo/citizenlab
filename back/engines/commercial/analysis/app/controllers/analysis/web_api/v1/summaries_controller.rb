c25cfbc25bd5573b0440a8a4ce735226e48b06e3
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2023-08-22 10:21:08 +0200

    TAN-252 Centralized filter params parsing for all controllers

--- back/engines/commercial/analysis/app/controllers/analysis/web_api/v1/summaries_controller.rb
+++ back/engines/commercial/analysis/app/controllers/analysis/web_api/v1/summaries_controller.rb
@@ -4,6 +4,7 @@ module Analysis
   module WebApi
     module V1
       class SummariesController < ApplicationController
+        include FilterParamsExtraction
         skip_after_action :verify_policy_scoped # The analysis is authorized instead.
         before_action :set_analysis
         before_action :set_summary, only: [:destroy]
@@ -25,7 +26,7 @@ module Analysis
           @summary = Summary.new(
             analysis: @analysis,
             background_task: SummarizationTask.new(analysis: @analysis),
-            **summary_params
+            filters: filters(params[:summary][:filters])
           )
           plan = SummarizationMethod::Base.plan(@summary)
           render json: WebApi::V1::SummaryPreCheckSerializer.new(
@@ -38,7 +39,7 @@ module Analysis
           @summary = Summary.new(
             analysis: @analysis,
             background_task: SummarizationTask.new(analysis: @analysis),
-            **summary_params
+            filters: filters(params[:summary][:filters])
           )
           plan = SummarizationMethod::Base.plan(@summary)
           @summary.summarization_method = plan.summarization_method_class::SUMMARIZATION_METHOD
@@ -75,33 +76,6 @@ module Analysis
 
         def set_summary
           @summary = @analysis.summaries.find(params[:id])
-        end
-
-        def summary_params
-          permitted_dynamic_filter_keys = []
-          permitted_dynamic_filter_array_keys = { tag_ids: [] }
-
-          params[:summary][:filters].each_key do |key|
-            if key.match?(/^author_custom_([a-f0-9-]+)_(from|to)$/)
-              permitted_dynamic_filter_keys << key
-            elsif key.match?(/^author_custom_([a-f0-9-]+)$/)
-              permitted_dynamic_filter_array_keys[key] = []
-            end
-          end
-
-          params.require(:summary).permit(filters: [
-            :search,
-            :published_at_from,
-            :published_at_to,
-            :reactions_from,
-            :reactions_to,
-            :votes_from,
-            :votes_to,
-            :comments_from,
-            :comments_to,
-            *permitted_dynamic_filter_keys,
-            **permitted_dynamic_filter_array_keys
-          ])
         end
 
         def side_fx_service
