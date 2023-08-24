b3bae5100c2dcecdcd406a43238e57a5c4ed11ae
EdwinKato <katedwin88@gmail.com>
2023-08-24 10:12:16 +0300

    Add Spinner when loading Topics

--- front/app/components/Topics/index.tsx
+++ front/app/components/Topics/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box } from '@citizenlab/cl2-component-library';
+import { Box, Spinner } from '@citizenlab/cl2-component-library';
 import useTopics from 'api/topics/useTopics';
 import Topic from './Topic';
 
@@ -8,17 +8,21 @@ interface Props {
 }
 
 const Topics = ({ showHomePageTopcs = false }: Props) => {
-  const { data: topics } = useTopics({ forHomepageFilter: showHomePageTopcs });
+  const { data: topics, isLoading } = useTopics({
+    forHomepageFilter: showHomePageTopcs,
+  });
 
-  if (!topics || topics.data.length === 0) return null;
+  if (isLoading) {
+    return <Spinner />;
+  }
 
-  return (
+  return topics && topics.data.length > 0 ? (
     <Box display="flex" gap="20px" width="100%" flexWrap="wrap">
       {topics.data.map((topic) => (
         <Topic topic={topic} key={topic.id} />
       ))}
     </Box>
-  );
+  ) : null;
 };
 
 export default Topics;
