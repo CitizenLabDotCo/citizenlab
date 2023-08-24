c083d6a7be3f12886404f9a35e1488aca71e25b4
EdwinKato <katedwin88@gmail.com>
2023-08-24 12:06:54 +0300

    Remove conditions to check for areas and topics in onboarding

--- front/app/containers/Authentication/steps/Onboarding/index.tsx
+++ front/app/containers/Authentication/steps/Onboarding/index.tsx
@@ -3,8 +3,6 @@ import { AuthenticationData } from 'containers/Authentication/typings';
 import useAuthenticationRequirements from 'api/authentication/authentication_requirements/useAuthenticationRequirements';
 import TopicsAndAreas from './TopicsAndAreas';
 import { RequirementStatus } from 'api/authentication/authentication_requirements/types';
-import useAreas from 'api/areas/useAreas';
-import useTopics from 'api/topics/useTopics';
 
 interface Props {
   authenticationData: AuthenticationData;
@@ -16,21 +14,17 @@ const Onboarding = ({ authenticationData, onSubmit, onSkip }: Props) => {
   const { data: authenticationRequirements } = useAuthenticationRequirements(
     authenticationData.context
   );
-  const { data: areas } = useAreas({ forHomepageFilter: true });
-  const { data: topics } = useTopics({ forHomepageFilter: true });
-  const hasAreas = areas && areas.data.length > 0;
-  const hasTopics = topics && topics.data.length > 0;
   const requiresTopicsAndAreasOnboarding = Object.prototype.hasOwnProperty.call(
     authenticationRequirements?.data.attributes.requirements.requirements
       .onboarding,
     'topics_and_areas'
   );
 
-  if ((!hasAreas && !hasTopics) || !requiresTopicsAndAreasOnboarding) {
-    return null;
+  if (requiresTopicsAndAreasOnboarding) {
+    return <TopicsAndAreas onSubmit={onSubmit} onSkip={onSkip} />;
   }
 
-  return <TopicsAndAreas onSubmit={onSubmit} onSkip={onSkip} />;
+  return null;
 };
 
 export default Onboarding;
