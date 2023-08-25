ad3654a3468e9692ba66979d7dce99ee69eec4bb
EdwinKato <katedwin88@gmail.com>
2023-08-25 08:43:22 +0300

    Correct typo

--- front/app/containers/Admin/settings/registration/ToggleShowFollowPreferences/messages.ts
+++ front/app/containers/Admin/settings/registration/ToggleShowFollowPreferences/messages.ts
@@ -4,7 +4,7 @@ export default defineMessages({
   helperText: {
     id: 'app.components.onboarding.helperText',
     defaultMessage:
-      "Request which topics & area's a user wants to follow during onboarding so they can be kept in the loop when new projects launch.",
+      'Request which topics & areas a user wants to follow during onboarding so they can be kept in the loop when new projects launch.',
   },
   requestFollowPreferences: {
     id: 'app.components.onboarding.requestFollowPreferences',
