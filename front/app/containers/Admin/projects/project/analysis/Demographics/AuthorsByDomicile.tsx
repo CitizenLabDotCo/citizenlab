c05254d299be29df17080e665676285263feb138
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2023-08-29 15:06:57 +0200

    TAN-250 Better tooltip and labels, dynamic domicile custom field

--- front/app/containers/Admin/projects/project/analysis/Demographics/AuthorsByDomicile.tsx
+++ front/app/containers/Admin/projects/project/analysis/Demographics/AuthorsByDomicile.tsx
@@ -13,18 +13,53 @@ import {
   LabelList,
 } from 'recharts';
 
-import { Box, colors } from '@citizenlab/cl2-component-library';
+import {
+  Box,
+  colors,
+  defaultStyles,
+  Title,
+  Text,
+} from '@citizenlab/cl2-component-library';
 import useLocalize from 'hooks/useLocalize';
 import useUserCustomFieldsOptions from 'api/user_custom_fields_options/useUserCustomFieldsOptions';
 import { xor } from 'lodash-es';
 import { updateSearchParams } from 'utils/cl-router/updateSearchParams';
 
+// Custom tooltip is rendered when the user hovers over a bar
+const CustomTooltip = ({ payload }: { payload?: any }) => {
+  const total = payload[0]?.payload?.total;
+  const filtered = payload[0]?.payload.filtered;
+
+  return (
+    <Box
+      bg={colors.white}
+      boxShadow={defaultStyles.boxShadowHoverBig}
+      p="12px"
+      borderRadius="3px"
+      w="120px"
+    >
+      <Title variant="h6" my="4px">
+        {payload[0]?.payload?.name}
+      </Title>
+      <Text m="2px">
+        {filtered === total ? (
+          <>{total}</>
+        ) : (
+          <>
+            {filtered} / {total}
+          </>
+        )}
+      </Text>
+    </Box>
+  );
+};
+
 type Props = {
   customFieldId: string;
 };
+
 const AuthorsByDomicile = ({ customFieldId }: Props) => {
   const localize = useLocalize();
-  const filterKey = `author_custom_${customFieldId}`;
   const { analysisId } = useParams() as { analysisId: string };
   const filters = useAnalysisFilterParams();
   const { data: totalAuthorsByDomicile } = useAuthorsByDomicile({
@@ -37,6 +72,8 @@ const AuthorsByDomicile = ({ customFieldId }: Props) => {
   });
   const { data: options } = useUserCustomFieldsOptions(customFieldId);
 
+  const filterKey = `author_custom_${customFieldId}`;
+
   const chartData =
     totalAuthorsByDomicile &&
     Object.entries(totalAuthorsByDomicile?.data.attributes.series.users).map(
@@ -44,7 +81,7 @@ const AuthorsByDomicile = ({ customFieldId }: Props) => {
         const option = options?.data.find((o) => o.id === optionId);
 
         const name = localize(option?.attributes.title_multiloc) || optionId;
-        const shortName = name.slice(0, 1);
+        const shortName = name.slice(0, 2);
         const filtered =
           filteredAuthorsByDomicile?.data.attributes.series.users[optionId];
         return {
@@ -59,7 +96,7 @@ const AuthorsByDomicile = ({ customFieldId }: Props) => {
       }
     );
 
-  const handleClick = (data, _index) => {
+  const handleClick = (data) => {
     const toggledOptions = xor(filters[filterKey] || [], [data.optionKey]);
     if (toggledOptions.length === 0) {
       updateSearchParams({ [filterKey]: undefined });
@@ -72,8 +109,8 @@ const AuthorsByDomicile = ({ customFieldId }: Props) => {
 
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
-      <ResponsiveContainer width="100%" height={110}>
-        <BarChart data={chartData}>
+      <ResponsiveContainer width="100%" height={105}>
+        <BarChart data={chartData} margin={{ top: 20 }}>
           <XAxis dataKey="shortName" />
           <Bar
             stackId="a"
@@ -89,10 +126,16 @@ const AuthorsByDomicile = ({ customFieldId }: Props) => {
                 key={`cell-${index}`}
               />
             ))}
-            <LabelList dataKey="filtered" position="top" />
           </Bar>
-          {/* <Bar stackId='a' dataKey='notFiltered' fill={colors.grey200} name='Not currently filtered' /> */}
-          <Tooltip />
+          <Bar
+            stackId="a"
+            dataKey="notFiltered"
+            fill={colors.grey200}
+            name="Not currently filtered"
+          >
+            <LabelList dataKey="total" position="top" />
+          </Bar>
+          <Tooltip content={<CustomTooltip />} />
         </BarChart>
       </ResponsiveContainer>
     </Box>
--- front/app/containers/Admin/projects/project/analysis/Demographics/index.tsx
+++ front/app/containers/Admin/projects/project/analysis/Demographics/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Box } from '@citizenlab/cl2-component-library';
 import AuthorsByDomicile from './AuthorsByDomicile';
+import useUserCustomFields from 'api/user_custom_fields/useUserCustomFields';
 
 const Demographics = () => {
+  const { data: customFields } = useUserCustomFields();
+
+  const selectedFields = customFields?.data.filter(
+    (field) => field.attributes.code === 'domicile'
+  );
+
   return (
     <Box display="flex" alignItems="center" justifyContent="space-between">
-      <Box flex="1">Age</Box>
-      <Box flex="1">
-        <AuthorsByDomicile customFieldId="54a2f676-cbd7-420a-933e-d778ef599594" />
-      </Box>
+      {selectedFields?.map((field) => (
+        <Box flex="1" key={field.id}>
+          {field.attributes.code === 'domicile' && (
+            <AuthorsByDomicile customFieldId={field.id} />
+          )}
+        </Box>
+      ))}
     </Box>
   );
 };
