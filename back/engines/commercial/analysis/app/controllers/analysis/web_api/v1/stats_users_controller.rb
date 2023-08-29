869ca811c7710674f9b3eccb8c56a7c1a695969b
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2023-08-29 08:43:06 +0200

    TAN-250 authors_by_domicile endpoint

---
+++ back/engines/commercial/analysis/app/controllers/analysis/web_api/v1/stats_users_controller.rb
@@ -0,0 +1,90 @@
+# frozen_string_literal: true
+
+module Analysis
+  module WebApi
+    module V1
+      class StatsUsersController < ApplicationController
+        include FilterParamsExtraction
+        skip_after_action :verify_policy_scoped # The analysis is authorized instead.
+        before_action :set_analysis
+
+        def users_by_age
+          age_stats = AgeStats.calculate(find_users)
+
+          render json: raw_json({
+            total_user_count: age_stats.user_count,
+            unknown_age_count: age_stats.unknown_age_count,
+            series: {
+              user_counts: age_stats.binned_counts,
+              reference_population: age_stats.population_counts,
+              bins: age_stats.bins
+            }
+          })
+        end
+
+        def users_by_custom_field
+          json_response = { series: {
+            users: user_counts,
+            reference_population: reference_population
+          } }
+
+          if custom_field.options.present?
+            json_response[:options] = custom_field.options.to_h do |o|
+              [o.key, o.attributes.slice('title_multiloc', 'ordering')]
+            end
+          end
+
+          render json: raw_json(json_response)
+        rescue NotSupportedFieldTypeError
+          head :not_implemented
+        end
+
+        def authors_by_domicile
+          domicile_field = CustomField.with_resource_type('User').find_by!(code: 'domicile')
+
+          users_count_by_area_id = UserCustomFields::FieldValueCounter.counts_by_field_option(
+            find_users, domicile_field, by: :area_id
+          )
+          areas = Area.all.select(:id, :title_multiloc)
+          render json: raw_json({
+            series: { users: users_count_by_area_id },
+            areas: areas.to_h { |a| [a.id, a.attributes.except('id')] }
+          })
+        end
+
+        private
+
+        def set_analysis
+          @analysis = Analysis.find(params[:analysis_id])
+          authorize(@analysis, :show?)
+        end
+
+        def custom_field
+          @custom_field ||= CustomField.find(params[:custom_field_id])
+        end
+
+        def user_counts
+          @user_counts ||= UserCustomFields::FieldValueCounter.counts_by_field_option(find_users, custom_field)
+        end
+
+        def find_users
+          inputs = InputsFinder.new(@analysis, filters(params)).execute
+          User.where(id: inputs.select(:author_id))
+        end
+
+        def reference_population
+          @reference_population ||= calculate_reference_population
+        end
+
+        def calculate_reference_population
+          return if custom_field.key == 'birthyear'
+          return if (ref_distribution = custom_field.current_ref_distribution).blank?
+
+          ref_distribution.distribution_by_option_key
+        end
+
+        class NotSupportedFieldTypeError < StandardError; end
+      end
+    end
+  end
+end
---
+++ back/engines/commercial/analysis/spec/acceptance/stats_users_spec.rb
@@ -0,0 +1,250 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+def filter_parameters(s)
+  with_options required: false do
+    s.parameter :search, 'Filter by searching in title and body'
+    s.parameter :'author_custom_<uuid>_from', 'Filter by custom field value of the author for numerical fields, larger than or equal to. Replace <uuid> with the custom_field id'
+    s.parameter :'author_custom_<uuid>_to', 'Filter by custom field value of the author for numerical fields, smaller than or equal to. Replace <uuid> with the custom_field id'
+    s.parameter :'author_custom_<uuid>', 'Filter by custom field value of the author, for select, multiselect, date and number fields (union). Replace <uuid> with the custom_field id', type: :array
+    s.parameter :'input_custom_<uuid>_from', 'Filter by custom field value of the input for numerical fields, larger than or equal to. Replace <uuid> with the custom_field id'
+    s.parameter :'input_custom_<uuid>_to', 'Filter by custom field value of the input for numerical fields, smaller than or equal to. Replace <uuid> with the custom_field id'
+    s.parameter :'input_custom_<uuid>', 'Filter by custom field value of the input, for select, multiselect, date and number fields (union). Replace <uuid> with the custom_field id', type: :array
+    s.parameter :published_at_from, 'Filter by input publication date, after or equal to', type: :date
+    s.parameter :published_at_to, 'Filter by input publication date, before or equal to', type: :date
+    s.parameter :reactions_from, 'Filter by number of reactions on the input, larger than or equal to', type: :integer
+    s.parameter :reactions_to, 'Filter by number of reactions on the input, smaller than or equal to', type: :integer
+    s.parameter :votes_from, 'Filter by number of votes on the input, larger than or equal to', type: :integer
+    s.parameter :votes_to, 'Filter by number of votes on the input, smaller than or equal to', type: :integer
+    s.parameter :comments_from, 'Filter by number of comments on the input, larger than or equal to', type: :integer
+    s.parameter :comments_to, 'Filter by number of comments on the input, smaller than or equal to', type: :integer
+  end
+end
+
+resource 'Analysis - Stats - Users' do
+  header 'Content-Type', 'application/json'
+
+  let_it_be(:timezone) { AppConfiguration.instance.settings('core', 'timezone') }
+  let_it_be(:now) { Time.now.in_time_zone(timezone) }
+  let_it_be(:start_at) { (now - 1.year).in_time_zone(timezone).beginning_of_year }
+  let_it_be(:end_at) { (now - 1.year).in_time_zone(timezone).end_of_year }
+
+  let_it_be(:analysis) { create(:analysis) }
+  let_it_be(:project) { analysis.project }
+
+  let_it_be(:cf_birthyear) { create(:custom_field_birthyear) }
+  let_it_be(:cf_gender) { create(:custom_field_gender, :with_options) }
+  let_it_be(:cf_domicile) { create(:custom_field_domicile) }
+  let_it_be(:cf_education) { create(:custom_field_education, :with_options, enabled: true) }
+
+  let(:analysis_id) { analysis.id }
+
+  before { admin_header_token }
+
+  get 'web_api/v1/analyses/:analysis_id/stats/authors_by_domicile' do
+    filter_parameters(self)
+
+    before do
+      @area1, @area2, @area3 = create_list(:area, 3)
+      authors = [@area1, @area1, @area2, nil].map { |area| create(:user, domicile: area&.id) }
+      create(:idea, project: project, author: authors[0], likes_count: 5)
+      create(:idea, project: project, author: authors[0], likes_count: 5)
+      create(:idea, project: project, author: authors[1], likes_count: 5)
+      create(:idea, project: project, author: authors[2], likes_count: 3)
+      create(:idea, project: project, author: authors[3], likes_count: 5)
+    end
+
+    let(:reactions_from) { 4 }
+
+    example_request 'Authors by domicile' do
+      expect(response_status).to eq 200
+      expect(json_response_body.dig(:data, :attributes)).to match({
+        areas: Area.all.to_h { |area| [area.id, area.attributes.slice('title_multiloc')] },
+        series: {
+          users: {
+            @area1.id => 2,
+            @area2.id => 0,
+            @area3.id => 0,
+            outside: 0,
+            _blank: 1
+          }
+        }
+      }.deep_symbolize_keys)
+    end
+  end
+
+  # get 'web_api/v1/analyses/:analysis_id/stats/authors_by_age' do
+  #   filter_parameters(self)
+
+  #   before do
+  #     AppConfiguration.instance.update(created_at: start_at)
+
+  #     travel_to start_at + 16.days do
+  #       birthyears = [1962, 1976, 1980, 1990, 1991, 2005, 2006]
+  #       users = birthyears.map { |year| create(:user, birthyear: year) }
+  #       user_without_birthyear = create(:user, birthyear: nil)
+
+  #       @group = create_group(users + [user_without_birthyear])
+  #     end
+  #   end
+
+  #   example 'Users counts by age' do
+  #     travel_to(Time.zone.local(2020, 1, 1)) { do_request }
+
+  #     expect(response_status).to eq 200
+  #     expect(json_response_body.dig(:data, :attributes)).to match(
+  #       total_user_count: 8,
+  #       unknown_age_count: 1,
+  #       series: {
+  #         user_counts: [0, 2, 2, 1, 1, 1, 0, 0, 0, 0],
+  #         expected_user_counts: nil,
+  #         reference_population: nil,
+  #         bins: UserCustomFields::AgeCounter::DEFAULT_BINS
+  #       }
+  #     )
+  #   end
+  # end
+
+  # describe 'by_custom_field endpoints' do
+  #   get 'web_api/v1/analyses/:analysis_id/stats/authors_by_custom_field/:custom_field_id' do
+  #     filter_parameters(self)
+
+  #     describe 'with select field' do
+  #       before do
+  #         @custom_field = create(:custom_field_select)
+  #         @option1, @option2, @option3 = create_list(:custom_field_option, 3, custom_field: @custom_field)
+
+  #         # We create an option on a different custom_field, but with the same
+  #         # key. This covers a regressions that mixed up custom field options
+  #         # between fields
+  #         @custom_field2 = create(:custom_field_select)
+  #         create(:custom_field_option, key: @option1.key, title_multiloc: { en: 'different' }, custom_field: @custom_field2)
+
+  #         travel_to(start_at - 1.day) do
+  #           create(:user, custom_field_values: { @custom_field.key => @option1.key }, manual_groups: [@group])
+  #         end
+
+  #         travel_to(start_at + 4.days) do
+  #           create(:user, custom_field_values: { @custom_field.key => @option1.key }, manual_groups: [@group])
+  #           create(:user, custom_field_values: { @custom_field.key => @option2.key }, manual_groups: [@group])
+  #           create(:user, manual_groups: [@group])
+  #           create(:user, custom_field_values: { @custom_field.key => @option3.key })
+  #         end
+
+  #         travel_to(end_at + 1.day) do
+  #           create(:user, custom_field_values: { @custom_field.key => @option1.key }, manual_groups: [@group])
+  #         end
+  #       end
+
+  #       let(:custom_field_id) { @custom_field.id }
+
+  #       example_request 'Authors by custom field (select)' do
+  #         expect(response_status).to eq 200
+  #         expect(json_response_body.dig(:data, :attributes)).to match({
+  #           options: {
+  #             @option1.key => { title_multiloc: @option1.title_multiloc, ordering: 0 },
+  #             @option2.key => { title_multiloc: @option2.title_multiloc, ordering: 1 },
+  #             @option3.key => { title_multiloc: @option3.title_multiloc, ordering: 2 }
+  #           },
+  #           series: {
+  #             users: {
+  #               @option1.key => 1,
+  #               @option2.key => 1,
+  #               @option3.key => 0,
+  #               _blank: 1
+  #             },
+  #             expected_users: nil,
+  #             reference_population: nil
+  #           }
+  #         }.deep_symbolize_keys)
+  #       end
+  #     end
+
+  #     describe 'with multiselect field' do
+  #       before do
+  #         @group = create(:group)
+  #         @custom_field = create(:custom_field_multiselect)
+  #         @option1, @option2, @option3 = create_list(:custom_field_option, 3, custom_field: @custom_field)
+  #         travel_to(start_at - 1.day) do
+  #           create(:user, custom_field_values: { @custom_field.key => [@option1.key] }, manual_groups: [@group])
+  #         end
+
+  #         travel_to(start_at + 6.days) do
+  #           create(:user, custom_field_values: { @custom_field.key => [@option1.key] }, manual_groups: [@group])
+  #           create(:user, custom_field_values: { @custom_field.key => [@option1.key, @option2.key] }, manual_groups: [@group])
+  #           create(:user, manual_groups: [@group])
+  #           create(:user, custom_field_values: { @custom_field.key => [@option3.key] })
+  #         end
+
+  #         travel_to(end_at + 1.day) do
+  #           create(:user, custom_field_values: { @custom_field.key => [@option1.key] }, manual_groups: [@group])
+  #         end
+  #       end
+
+  #       let(:custom_field_id) { @custom_field.id }
+
+  #       example_request 'Authors by custom field (multiselect)' do
+  #         expect(response_status).to eq 200
+  #         expect(json_response_body.dig(:data, :attributes)).to match({
+  #           options: {
+  #             @option1.key => { title_multiloc: @option1.title_multiloc, ordering: 0 },
+  #             @option2.key => { title_multiloc: @option2.title_multiloc, ordering: 1 },
+  #             @option3.key => { title_multiloc: @option3.title_multiloc, ordering: 2 }
+  #           },
+  #           series: {
+  #             users: {
+  #               @option1.key => 2,
+  #               @option2.key => 1,
+  #               @option3.key => 0,
+  #               _blank: 1
+  #             },
+  #             expected_users: nil,
+  #             reference_population: nil
+  #           }
+  #         }.deep_symbolize_keys)
+  #       end
+  #     end
+
+  #     describe 'with checkbox field' do
+  #       before do
+  #         @group = create(:group)
+  #         @custom_field = create(:custom_field_checkbox)
+  #         travel_to(start_at - 1.day) do
+  #           create(:user, custom_field_values: { @custom_field.key => false }, manual_groups: [@group])
+  #         end
+
+  #         travel_to(start_at + 24.days) do
+  #           create(:user, custom_field_values: { @custom_field.key => true }, manual_groups: [@group])
+  #           create(:user, custom_field_values: { @custom_field.key => false }, manual_groups: [@group])
+  #           create(:user, manual_groups: [@group])
+  #         end
+
+  #         travel_to(end_at + 1.day) do
+  #           create(:user, custom_field_values: { @custom_field.key => true }, manual_groups: [@group])
+  #         end
+  #       end
+
+  #       let(:group) { @group.id }
+  #       let(:custom_field_id) { @custom_field.id }
+
+  #       example_request 'Authors by custom field (checkbox)' do
+  #         expect(response_status).to eq 200
+  #         expect(json_response_body.dig(:data, :attributes)).to match({
+  #           series: {
+  #             users: {
+  #
+  #               true: 1,
+  #               false: 1,
+  #                 #               _blank: 1
+  #             },
+  #             expected_users: nil,
+  #             reference_population: nil
+  #           }
+  #         })
+  #       end
+  #     end
+  #   end
+  # end
+end
