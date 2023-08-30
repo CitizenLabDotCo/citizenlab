804587b9b41ca6677d3afd94500d44d50be10767
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-08-30 12:25:45 +0100

    _

--- back/engines/commercial/bulk_import_ideas/app/services/bulk_import_ideas/google_form_parser2_service.rb
+++ back/engines/commercial/bulk_import_ideas/app/services/bulk_import_ideas/google_form_parser2_service.rb
@@ -12,11 +12,56 @@ module BulkImportIdeas
       parse_text document.text
     end
 
-    def parse_text
-      
+    def parse_text(text)
+      lines = text.split('/n')
+
+      # documents is an array of documents.
+      # a document is an array of fields.
+      documents = []
+      document = nil
+      field = nil
+
+      lines.each do |line|
+        if is_new_document(line) then
+          document << field unless field.nil?
+          field = nil
+
+          documents << document unless document.nil?
+          document = []
+        end
+
+        if is_field_title(line) then
+          document << field unless field.nil?
+
+          field = {
+            title: line,
+            type: lookup_field_type(line)
+          }
+
+          next
+        end
+
+        next if field.nil?
+
+        if field.type == 
+      end
     end
 
     private
+
+    def is_new_document(line)
+      # Must return true if the line equals the first question,
+      # or in the future some other way of determining the start
+      # of the document
+    end
+
+    def is_field_title(line)
+      # TODO
+    end
+
+    def lookup_field_type(line)
+      # TODO
+    end
 
     def process_upload
       client = Google::Cloud::DocumentAI.document_processor_service
