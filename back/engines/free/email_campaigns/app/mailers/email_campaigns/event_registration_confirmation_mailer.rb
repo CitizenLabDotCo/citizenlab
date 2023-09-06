2c96d9bbe340ae414734841af5960fd0bc197821
Adrien Dessy <adrien@citizenlab.co>
2023-09-06 11:58:13 +0200

    [TAN-171] Implement the complete email for event registration confirmations
    - Add missing event details
    - Add functional buttons (Add to calendar / Go to event)
    - Add missing event details
    
    We didn't include the online link since it has not been released yet.

--- back/engines/free/email_campaigns/app/mailers/email_campaigns/event_registration_confirmation_mailer.rb
+++ back/engines/free/email_campaigns/app/mailers/email_campaigns/event_registration_confirmation_mailer.rb
@@ -3,10 +3,11 @@
 module EmailCampaigns
   class EventRegistrationConfirmationMailer < ApplicationMailer
     include ActionView::Helpers::SanitizeHelper
+    include Rails.application.routes.url_helpers
     layout 'campaign_mailer_minimal'
 
-    helper_method :event_description, :event_location, :event_time, :event_title,
-      :project_title
+    helper_method :add_to_calendar_url, :event_description, :event_location, :event_time,
+      :event_title, :project_title
 
     private
 
@@ -41,12 +42,18 @@ module EmailCampaigns
 
     def event_description
       description = localize_for_recipient(event.event_attributes.description_multiloc)
-      # remove html tags
       strip_tags(description).presence
     end
 
     def project_title
       localize_for_recipient(event.project_title_multiloc)
+    end
+
+    def add_to_calendar_url
+      host = AppConfiguration.instance.base_backend_uri
+      # In a mailer, the host must be specified explicitly for the url helpers to work
+      # because, unlike in a controller, it cannot be inferred from the request.
+      web_api_v1_event_url(event.event_attributes.id, format: 'ics', host: host)
     end
   end
 end
