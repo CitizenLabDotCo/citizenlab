25066d997a44ea29c14e463af82f85f5a99f1587
brentguf <brentguf@users.noreply.github.com>
2023-09-07 15:27:26 +0200

    Add missing cosponsorship reminder show conditions

--- front/app/hooks/useShowCosponsorshipReminder.ts
+++ front/app/hooks/useShowCosponsorshipReminder.ts
@@ -1,4 +1,5 @@
 import useAppConfiguration from 'api/app_configuration/useAppConfiguration';
+import useInitiativeStatus from 'api/initiative_statuses/useInitiativeStatus';
 import useInitiativeById from 'api/initiatives/useInitiativeById';
 import useAuthUser from 'api/me/useAuthUser';
 
@@ -6,20 +7,36 @@ export default function useShowCosponsorshipReminder(initiativeId: string) {
   const { data: appConfiguration } = useAppConfiguration();
   const { data: initiative } = useInitiativeById(initiativeId);
   const { data: authUser } = useAuthUser();
+  const initiativeStatusId =
+    initiative?.data.relationships.initiative_status?.data?.id;
+  const { data: initiativeStatus } = useInitiativeStatus(initiativeStatusId);
 
-  if (!initiative || !authUser || !appConfiguration) return false;
+  if (!initiative || !authUser || !appConfiguration || !initiativeStatus) {
+    return false;
+  }
 
   const authorId = initiative.data.relationships.author.data?.id;
   const signedInUserIsAuthor =
     typeof authorId === 'string' ? authorId === authUser.data.id : false;
+  const requireReview =
+    appConfiguration.data.attributes.settings.initiatives?.require_review;
   const requiredNumberOfCosponsors =
     appConfiguration.data.attributes.settings.initiatives?.cosponsors_number;
   const acceptedCosponsorships =
     initiative.data.attributes.cosponsorships.filter(
       (c) => c.status === 'accepted'
     );
+  const initiativeStatusCode = initiativeStatus.data.attributes.code;
+  const initiativeHasBeenPublished =
+    initiativeStatusCode !== 'review_pending' &&
+    initiativeStatusCode !== 'changes_requested';
 
-  return signedInUserIsAuthor && typeof requiredNumberOfCosponsors === 'number'
+  return signedInUserIsAuthor &&
+    typeof requiredNumberOfCosponsors === 'number' &&
+    // Showing this warning only makes sense if proposal pre-approval/review
+    // is enabled. Otherwise, proposals are published immediately anyway.
+    requireReview === true &&
+    !initiativeHasBeenPublished
     ? requiredNumberOfCosponsors > acceptedCosponsorships.length
     : false;
 }
