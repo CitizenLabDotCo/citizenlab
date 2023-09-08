4f185c0593a0f15487a2bf41e3cd6e96743f6be1
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-09-08 17:36:14 +0100

    Add button that doesnt need BE data for testing

--- front/app/components/UI/Button/Button.stories.tsx
+++ front/app/components/UI/Button/Button.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import Button from '.';
+// import Button from '.';
+import Button from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
---
+++ front/app/components/UI/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// interface Props {
+//   bgColor?: string;
+// }
+
+const Button = ({ bgColor }) => {
+  return <button style={{ backgroundColor: bgColor }}>Bla</button>;
+};
+
+export default Button;
