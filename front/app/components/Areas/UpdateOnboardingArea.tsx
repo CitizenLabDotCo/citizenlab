e3681e88889c91c3607e0b491a0c3e930c157cd9
EdwinKato <katedwin88@gmail.com>
2023-09-08 09:49:15 +0300

    Handle on error when updating

--- front/app/components/Areas/UpdateOnboardingArea.tsx
+++ front/app/components/Areas/UpdateOnboardingArea.tsx
@@ -3,13 +3,17 @@ import { Badge, colors, Button } from '@citizenlab/cl2-component-library';
 import T from 'components/T';
 import { IAreaData } from 'api/areas/types';
 import useUpdateArea from 'api/areas/useUpdateArea';
+import Error from 'components/UI/Error';
+import messages from './messages';
+import { useIntl } from 'utils/cl-intl';
 
 interface Props {
   area: IAreaData;
 }
 
 const UpdateOnboardingArea = ({ area }: Props) => {
-  const { mutate: updateArea, isLoading } = useUpdateArea();
+  const { mutate: updateArea, isLoading, error } = useUpdateArea();
+  const { formatMessage } = useIntl();
   const color = area.attributes.include_in_onboarding
     ? colors.success
     : colors.coolGrey300;
@@ -25,19 +29,22 @@ const UpdateOnboardingArea = ({ area }: Props) => {
   };
 
   return (
-    <Badge color={color} onClick={onClick}>
-      <Button
-        buttonStyle="text"
-        icon={iconName}
-        iconColor={color}
-        iconPos="right"
-        padding="0px"
-        my="0px"
-        processing={isLoading}
-      >
-        <T value={area.attributes.title_multiloc} />
-      </Button>
-    </Badge>
+    <>
+      <Badge color={color} onClick={onClick}>
+        <Button
+          buttonStyle="text"
+          icon={iconName}
+          iconColor={color}
+          iconPos="right"
+          padding="0px"
+          my="0px"
+          processing={isLoading}
+        >
+          <T value={area.attributes.title_multiloc} />
+        </Button>
+      </Badge>
+      {error && <Error text={formatMessage(messages.areaUpdateError)} />}
+    </>
   );
 };
 
--- front/app/components/Topics/UpdateOnboardingTopic.tsx
+++ front/app/components/Topics/UpdateOnboardingTopic.tsx
@@ -3,13 +3,17 @@ import { Badge, colors, Button } from '@citizenlab/cl2-component-library';
 import T from 'components/T';
 import useUpdateTopic from 'api/topics/useUpdateTopic';
 import { ITopicData } from 'api/topics/types';
+import Error from 'components/UI/Error';
+import messages from './messages';
+import { useIntl } from 'utils/cl-intl';
 
 interface Props {
   topic: ITopicData;
 }
 
 const UpdateOnboardingTopic = ({ topic }: Props) => {
-  const { mutate: updateTopic, isLoading } = useUpdateTopic();
+  const { mutate: updateTopic, isLoading, error } = useUpdateTopic();
+  const { formatMessage } = useIntl();
   const color = topic.attributes.include_in_onboarding
     ? colors.success
     : colors.coolGrey300;
@@ -25,19 +29,22 @@ const UpdateOnboardingTopic = ({ topic }: Props) => {
   };
 
   return (
-    <Badge color={color} onClick={onClick}>
-      <Button
-        buttonStyle="text"
-        icon={iconName}
-        iconColor={color}
-        iconPos="right"
-        padding="0px"
-        my="0px"
-        processing={isLoading}
-      >
-        <T value={topic.attributes.title_multiloc} />
-      </Button>
-    </Badge>
+    <>
+      <Badge color={color} onClick={onClick}>
+        <Button
+          buttonStyle="text"
+          icon={iconName}
+          iconColor={color}
+          iconPos="right"
+          padding="0px"
+          my="0px"
+          processing={isLoading}
+        >
+          <T value={topic.attributes.title_multiloc} />
+        </Button>
+      </Badge>
+      {error && <Error text={formatMessage(messages.topicUpdateError)} />}
+    </>
   );
 };
 
