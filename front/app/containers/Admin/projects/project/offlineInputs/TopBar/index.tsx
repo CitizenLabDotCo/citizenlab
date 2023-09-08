9fbfbbf0da9bea5a0b837654e3e7b7f21442fe62
James Speake <jamesspeake@users.noreply.github.com>
2023-09-08 10:15:11 +0100

    Clarity on whether a survey phase
    Co-authored-by: luucvanderzee <luuc_v_d_zee@hotmail.com>

--- front/app/containers/Admin/projects/project/offlineInputs/TopBar/index.tsx
+++ front/app/containers/Admin/projects/project/offlineInputs/TopBar/index.tsx
@@ -38,7 +38,7 @@ const TopBar = ({ onClickPDFImport }: Props) => {
     (phase ? ` - ${localize(phase?.data.attributes.title_multiloc)}` : '');
 
   const isSurvey =
-    phase || project?.data.attributes.participation_method === 'native_survey';
+    phase?.data.attributes.participation_method === 'native_survey' || project?.data.attributes.participation_method === 'native_survey';
 
   const backPath = isSurvey
     ? `/admin/projects/${projectId}/native-survey`
