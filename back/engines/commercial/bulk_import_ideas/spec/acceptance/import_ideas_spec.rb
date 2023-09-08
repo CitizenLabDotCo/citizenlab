2c2f3c85c6c23acbadae313601736554081dc5ea
jamesspeake <james@speake.org>
2023-09-08 14:47:13 +0100

    [CL-4022] Correctly stubbed tests

--- back/engines/commercial/bulk_import_ideas/spec/acceptance/import_ideas_spec.rb
+++ back/engines/commercial/bulk_import_ideas/spec/acceptance/import_ideas_spec.rb
@@ -189,6 +189,7 @@ resource 'BulkImportIdeasImportIdeas' do
           # NOTE: GoogleFormParserService is stubbed to avoid calls to google APIs
           context 'continuous projects' do
             example 'Bulk import ideas from scanned .pdf' do
+              expect_any_instance_of(BulkImportIdeas::GoogleFormParserService).to receive(:process_upload).and_return(nil)
               expect_any_instance_of(BulkImportIdeas::GoogleFormParserService).to receive(:raw_text_page_array).and_return(create_project_bulk_import_raw_text_array)
               do_request
               assert_status 201
@@ -215,6 +216,7 @@ resource 'BulkImportIdeasImportIdeas' do
 
             context 'current phase' do
               example 'Bulk import ideas from scanned .pdf to current phase' do
+                expect_any_instance_of(BulkImportIdeas::GoogleFormParserService).to receive(:process_upload).and_return(nil)
                 expect_any_instance_of(BulkImportIdeas::GoogleFormParserService).to receive(:raw_text_page_array).and_return(create_project_bulk_import_raw_text_array)
                 do_request
                 assert_status 201
@@ -231,6 +233,7 @@ resource 'BulkImportIdeasImportIdeas' do
               let(:phase_id) { project.phases.first.id }
 
               example 'Bulk import ideas from scanned .pdf to a specified phase' do
+                expect_any_instance_of(BulkImportIdeas::GoogleFormParserService).to receive(:process_upload).and_return(nil)
                 expect_any_instance_of(BulkImportIdeas::GoogleFormParserService).to receive(:raw_text_page_array).and_return(create_project_bulk_import_raw_text_array)
                 do_request
                 assert_status 201
