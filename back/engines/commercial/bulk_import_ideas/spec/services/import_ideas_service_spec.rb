7e0a93fdc0516f0ded205e5a7c398df988aeb264
jamesspeake <james@speake.org>
2023-09-08 13:29:35 +0100

    [CL-4022] Test fixes

--- back/engines/commercial/bulk_import_ideas/spec/services/import_ideas_service_spec.rb
+++ back/engines/commercial/bulk_import_ideas/spec/services/import_ideas_service_spec.rb
@@ -17,101 +17,7 @@ describe BulkImportIdeas::ImportIdeasService do
   describe 'import_ideas' do
     before { create(:idea_status, code: 'proposed') }
 
-    it 'imports multiple ideas to existing authors' do
-      project1 = create(:project, title_multiloc: { 'fr-BE' => 'Projet un', 'en' => 'Project 1' })
-      project2 = create(:project, title_multiloc: { 'nl-BE' => 'Project twee', 'en' => 'Project 2' })
-      create(:idea, project: project2)
-      user1 = create(:user, email: 'userimport@citizenlab.co')
-      user2 = create(:user, email: 'userimport2@citizenlab.co')
-
-      idea_rows = [
-        {
-          title_multiloc: { 'en' => 'My idea title' },
-          body_multiloc: { 'en' => 'My idea description' },
-          project_title: 'Project 1',
-          user_email: 'userimport@citizenlab.co'
-        },
-        {
-          title_multiloc: { 'en' => 'My idea title 2' },
-          body_multiloc: { 'en' => 'My idea description 2' },
-          project_title: 'Project 2',
-          user_email: 'userimport2@citizenlab.co'
-        }
-      ]
-      service.import_ideas idea_rows
-
-      expect(project1.reload.ideas_count).to eq 1
-      idea1 = user1.ideas.first
-      expect(idea1.project_id).to eq project1.id
-      expect(idea1.title_multiloc).to eq({ 'en' => 'My idea title' })
-      expect(idea1.body_multiloc).to eq({ 'en' => 'My idea description' })
-      expect(idea1.idea_import.user_created).to be false
-
-      expect(project2.reload.ideas_count).to eq 2
-      idea2 = user2.ideas.first
-      expect(idea2.project_id).to eq project2.id
-      expect(idea2.title_multiloc).to eq({ 'en' => 'My idea title 2' })
-      expect(idea2.body_multiloc).to eq({ 'en' => 'My idea description 2' })
-      expect(idea2.idea_import.user_created).to be false
-    end
-
-    it 'imports multiple ideas and creates new authors - with or without email addresses & names' do
-      project1 = create(:project, title_multiloc: { 'fr-BE' => 'Projet un', 'en' => 'Project 1' })
-      project2 = create(:project, title_multiloc: { 'nl-BE' => 'Project twee', 'en' => 'Project 2' })
-      project3 = create(:project, title_multiloc: { 'fr-BE' => 'Projet trois', 'en' => 'Project 3' })
-
-      idea_rows = [
-        {
-          title_multiloc: { 'en' => 'My idea title' },
-          body_multiloc: { 'en' => 'My idea description' },
-          project_title: 'Project 1',
-          user_email: 'userimport1@citizenlab.co',
-          user_name: 'Gary Import'
-        },
-        {
-          title_multiloc: { 'en' => 'My idea title 2' },
-          body_multiloc: { 'en' => 'My idea description 2' },
-          project_title: 'Project 2',
-          user_email: nil
-        },
-        {
-          title_multiloc: { 'en' => 'My idea title 3' },
-          body_multiloc: { 'en' => 'My idea description 3' },
-          project_title: 'Project 3',
-          user_email: 'userimport1@citizenlab.co'
-        }
-      ]
-      service.import_ideas idea_rows
-
-      expect(project1.reload.ideas_count).to eq 1
-      idea1 = project1.ideas.first
-      expect(idea1.project_id).to eq project1.id
-      expect(idea1.title_multiloc).to eq({ 'en' => 'My idea title' })
-      expect(idea1.body_multiloc).to eq({ 'en' => 'My idea description' })
-      expect(idea1.author[:email]).to eq 'userimport1@citizenlab.co'
-      expect(idea1.author[:first_name]).to eq 'Gary'
-      expect(idea1.author[:last_name]).to eq 'Import'
-      expect(idea1.idea_import.user_created).to be true
-
-      expect(project2.reload.ideas_count).to eq 1
-      idea2 = project2.ideas.first
-      expect(idea2.project_id).to eq project2.id
-      expect(idea2.title_multiloc).to eq({ 'en' => 'My idea title 2' })
-      expect(idea2.body_multiloc).to eq({ 'en' => 'My idea description 2' })
-      expect(idea2.author.email).to be_nil
-      expect(idea2.author.unique_code).not_to be_nil
-      expect(idea2.idea_import.user_created).to be true
-
-      expect(project3.reload.ideas_count).to eq 1
-      idea3 = project3.ideas.first
-      expect(idea3.project_id).to eq project3.id
-      expect(idea3.title_multiloc).to eq({ 'en' => 'My idea title 3' })
-      expect(idea3.body_multiloc).to eq({ 'en' => 'My idea description 3' })
-      expect(idea3.author).to eq idea1.author
-      expect(idea3.idea_import.user_created).to be false
-    end
-
-    it 'imports multiple ideas using project IDs' do
+    it 'imports multiple draft ideas using project IDs' do
       project = create(:project, title_multiloc: { 'fr-BE' => 'Projet un', 'en' => 'Project 1' })
 
       idea_rows = [
@@ -130,7 +36,8 @@ describe BulkImportIdeas::ImportIdeasService do
       ]
       service.import_ideas idea_rows
 
-      expect(project.reload.ideas_count).to eq 2
+      expect(project.ideas.count).to eq 2
+      expect(project.reload.ideas_count).to eq 0
       idea1 = project.ideas.first
       expect(idea1.project_id).to eq project.id
 
@@ -138,7 +45,7 @@ describe BulkImportIdeas::ImportIdeasService do
       expect(idea2.project_id).to eq project.id
     end
 
-    it 'imports multiple ideas with custom fields' do
+    it 'imports multiple draft ideas with custom fields' do
       project = create(:project, title_multiloc: { 'fr-BE' => 'Projet un', 'en' => 'Project 1' })
       custom_form = create(:custom_form, :with_default_fields, participation_context: project)
       create(:custom_field, resource: custom_form, key: 'custom_text_field', title_multiloc: { 'en' => 'Custom text field:' }, enabled: true)
@@ -161,13 +68,14 @@ describe BulkImportIdeas::ImportIdeasService do
       ]
       service.import_ideas idea_rows
 
-      expect(project.reload.ideas_count).to eq 2
+      expect(project.ideas.count).to eq 2
+      expect(project.reload.ideas_count).to eq 0
       expect(project.reload.ideas.pluck(:custom_field_values)).to match_array(
         [{ 'text_field' => 'custom text field content' }, { 'text_field' => 'custom text field content 2' }]
       )
     end
 
-    it 'imports ideas with idea import meta data' do
+    it 'imports draft ideas with idea import meta data' do
       project = create(:project)
 
       idea_rows = [
@@ -189,14 +97,13 @@ describe BulkImportIdeas::ImportIdeasService do
       service.import_ideas idea_rows
 
       ideas = project.reload.ideas
-      expect(project.reload.ideas_count).to eq 2
+      expect(project.ideas.count).to eq 2
+      expect(project.reload.ideas_count).to eq 0
       expect(ideas[0].idea_import).not_to be_nil
       expect(ideas[0].idea_import.page_range).to eq %w[1 2]
       expect(ideas[1].idea_import).not_to be_nil
       expect(ideas[1].idea_import.page_range).to eq %w[3 4]
     end
-
-
 
     it 'imports ideas as draft with publication info' do
       create(:user, email: 'userimport@citizenlab.co')
@@ -219,10 +126,6 @@ describe BulkImportIdeas::ImportIdeasService do
       expect(idea.publication_status).to eq 'draft'
     end
 
-
-
-
-
     it 'ignores validation and can import blank ideas when importing as draft to a project' do
       project = create(:project)
       idea_rows = [
@@ -239,43 +142,6 @@ describe BulkImportIdeas::ImportIdeasService do
 
       expect(project.reload.ideas.count).to eq 2
     end
-
-    it 'returns an error if title and body are blank when not importing as draft' do
-      project = create(:project)
-      idea_rows = [
-        {
-          title_multiloc: {},
-          body_multiloc: {},
-          project_id: project.id
-        }
-      ]
-      expect { service.import_ideas idea_rows }.to raise_error(an_instance_of(BulkImportIdeas::Error))
-    end
-
-    # TODO: Cannot be enabled because mocking image URLs is not working.
-    # it 'imports ideas with images' do
-    #   # WebMock.disable_net_connect!
-    #   # FakeWeb.register_uri(:get, 'https://images.com/image.png', body: 'Hello World!')
-
-    #   create :user, email: 'userimport@citizenlab.co'
-    #   create :project, title_multiloc: { 'en' => 'Project title' }
-
-    #   idea_rows = [
-    #     {
-    #       title_multiloc: { 'en' => 'My idea title' },
-    #       body_multiloc: { 'en' => 'My idea description' },
-    #       project_title: 'Project title',
-    #       user_email: 'userimport@citizenlab.co',
-    #       image_url: 'https://images.com/image.png'
-    #     }
-    #   ]
-
-    #   service.import_ideas idea_rows
-
-    #   expect(Idea.count).to eq 1
-    #   idea = Idea.first
-    #   expect(idea.idea_images.count).to eq 1
-    # end
 
     it 'enqueues tenant dump job after importing ideas' do
       idea_rows = [
