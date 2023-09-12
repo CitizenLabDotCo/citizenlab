760893cb5086f38e60a0a437476508134a5f26e1
Simon Tharby <simontharby@gmail.com>
2023-09-12 13:38:31 +0100

    [CL-3994] serialize app_configration created_at + test

--- back/app/serializers/web_api/v1/app_configuration_serializer.rb
+++ back/app/serializers/web_api/v1/app_configuration_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class WebApi::V1::AppConfigurationSerializer < WebApi::V1::BaseSerializer
-  attributes :name, :host, :style
+  attributes :name, :host, :style, :created_at
 
   attribute :logo do |object|
     object.logo && object.logo.versions.to_h { |k, v| [k.to_s, v.url] }
