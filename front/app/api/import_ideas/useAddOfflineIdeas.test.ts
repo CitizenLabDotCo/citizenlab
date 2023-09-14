0af41d2bbbfaf7d3582a69d4bacdd96cbf957fbd
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-09-14 16:06:43 +0100

    Add useAddOfflineIdeas test

--- front/app/api/import_ideas/useAddOfflineIdeas.test.ts
+++ front/app/api/import_ideas/useAddOfflineIdeas.test.ts
@@ -1,22 +1,39 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 
-import useAddArea from './useAddArea';
+import useAddOfflineIdeas from './useAddOfflineIdeas';
 
 import { setupServer } from 'msw/node';
 import { rest } from 'msw';
 
 import createQueryClientWrapper from 'utils/testUtils/queryClientWrapper';
-import { areasData } from './__mocks__/useAreas';
+import { ideasData } from './__mocks__/useAddOfflineIdeas';
 
 const apiPath = 'projects/:projectId/import_ideas/bulk_create';
 
 const server = setupServer(
   rest.post(apiPath, (_req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ data: areasData[0] }));
+    return res(ctx.status(200), ctx.json({ data: ideasData }));
   })
 );
 
 describe('useAddOfflineIdeas', () => {
   beforeAll(() => server.listen());
   afterAll(() => server.close());
+
+  it('mutates data correctly', async () => {
+    const { result, waitFor } = renderHook(() => useAddOfflineIdeas(), {
+      wrapper: createQueryClientWrapper(),
+    });
+
+    act(() => {
+      result.current.mutate({
+        project_id: 'project1',
+        pdf: 'data:application/pdf;base64,abc123',
+        locale: 'en',
+      });
+    });
+
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+    expect(result.current.data?.data).toEqual(ideasData);
+  });
 });
