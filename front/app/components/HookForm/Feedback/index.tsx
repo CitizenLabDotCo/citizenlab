de2b9c36774e325fe1cb858ebb0f098ddaf4ec89
brentguf <brentguf@users.noreply.github.com>
2023-09-15 11:34:04 +0200

    Pass apiErrorValue to Feedback component error msgs

--- front/app/components/HookForm/Feedback/index.tsx
+++ front/app/components/HookForm/Feedback/index.tsx
@@ -39,6 +39,7 @@ const Feedback = ({ successMessage }: FeedbackProps) => {
       const errors = get(formContextErrors, field) as RHFErrors;
       const standardFieldError = errors?.message;
       const apiError = errors?.error;
+      const apiErrorValue = errors?.value;
       const multilocFieldFirstError = Object.values(
         errors as Record<string, any>
       )[0]?.message;
@@ -56,7 +57,12 @@ const Feedback = ({ successMessage }: FeedbackProps) => {
 
         errorMessages.push({
           field,
-          message: apiErrorMessage ? formatMessage(apiErrorMessage) : '',
+          message: apiErrorMessage
+            ? formatMessage(
+                apiErrorMessage,
+                apiErrorValue ? { value: apiErrorValue } : undefined
+              )
+            : '',
         });
       } else if (multilocFieldFirstError) {
         errorMessages.push({
