d653be7754e35bbd5a2ff9dff518792eab7fe71e
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-09-19 13:32:35 +0100

    Add test for hook

--- front/app/api/ideas_phases/useIdeasPhase.test.ts
+++ front/app/api/ideas_phases/useIdeasPhase.test.ts
@@ -1,17 +1,35 @@
 import { renderHook } from '@testing-library/react-hooks';
 
 import useIdeasPhase from './useIdeasPhase';
-import { ideaData } from './__mocks__/useIdeaById';
+import { ideasPhase } from './__mocks__/useIdeasPhase';
 
 import { setupServer } from 'msw/node';
 import { rest } from 'msw';
 
 import createQueryClientWrapper from 'utils/testUtils/queryClientWrapper';
 
-const apiPath = '*ideas/:id';
+const apiPath = '*ideas_phases/:id';
 
 const server = setupServer(
   rest.get(apiPath, (_req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ data: ideaData[0] }));
+    return res(ctx.status(200), ctx.json({ data: ideasPhase }));
   })
 );
+
+describe('useIdeasPhase', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
+  it('returns data correctly', async () => {
+    const { result, waitFor } = renderHook(() => useIdeasPhase('id'), {
+      wrapper: createQueryClientWrapper(),
+    });
+
+    expect(result.current.isLoading).toBe(true);
+
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.data?.data).toEqual(ideasPhase);
+  });
+});
