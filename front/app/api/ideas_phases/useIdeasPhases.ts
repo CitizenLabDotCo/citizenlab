cdffa14dc1b1061759fc2aa928e55b3be8e68a2a
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-09-20 13:40:55 +0100

    Remove useIdeasPhase

--- front/app/api/ideas_phases/useIdeasPhases.ts
+++ front/app/api/ideas_phases/useIdeasPhases.ts
@@ -1,6 +1,10 @@
 import { useQueries } from '@tanstack/react-query';
+import fetcher from 'utils/cl-react-query/fetcher';
 import ideasPhasesKeys from './keys';
-import { fetchIdeasPhase } from './useIdeasPhase';
+import { Params, IdeasPhase } from './types';
+
+const fetchIdeasPhase = ({ id }: Params) =>
+  fetcher<IdeasPhase>({ path: `/ideas_phases/${id}`, action: 'get' });
 
 const useIdeasPhases = (ids: string[]) => {
   const queries = ids.map((id) => ({
