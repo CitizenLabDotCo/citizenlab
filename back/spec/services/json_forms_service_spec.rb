f8a0cac78adcaee3bdc1d116f0b6360e6c765a3d
Amanda Anderson <anderson_amanda@mail.com>
2023-09-22 15:59:09 +0200

    BE test

--- back/spec/services/json_forms_service_spec.rb
+++ back/spec/services/json_forms_service_spec.rb
@@ -98,6 +98,7 @@ describe JsonFormsService do
                           'field8' =>
               { type: 'array',
                 uniqueItems: true,
+                maxItems: 2,
                 minItems: 1,
                 items: { type: 'string',
                          oneOf: [
