c558bcd67687284dae780e98ea47fbcb018c336d
Iva <iva@citizenlab.co>
2023-09-27 19:11:04 +0300

    An attempt to add the relationship

--- back/engines/commercial/analysis/app/serializers/analysis/web_api/v1/analysis_serializer.rb
+++ back/engines/commercial/analysis/app/serializers/analysis/web_api/v1/analysis_serializer.rb
@@ -7,5 +7,6 @@ class Analysis::WebApi::V1::AnalysisSerializer < WebApi::V1::BaseSerializer
   belongs_to :phase, serializer: ::WebApi::V1::PhaseSerializer
 
   has_many :custom_fields, serializer: ::WebApi::V1::CustomFieldSerializer
-  # has_many :bookmaked_insights, serializer: ::WebApi::V1::InsightSerializer
+  has_many :bookmarked_insightables, polymorphic: true
 end
+
