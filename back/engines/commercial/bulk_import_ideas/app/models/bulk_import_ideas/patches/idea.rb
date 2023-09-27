3e2df313b22d2e4c996f5ee31b861a21dd3d23bc
jamesspeake <james@speake.org>
2023-09-27 10:25:42 +0100

    [CL-4022] Delete created users when deleting draft ideas

--- back/engines/commercial/bulk_import_ideas/app/models/bulk_import_ideas/patches/idea.rb
+++ back/engines/commercial/bulk_import_ideas/app/models/bulk_import_ideas/patches/idea.rb
@@ -6,11 +6,18 @@ module BulkImportIdeas::Patches::Idea
       has_one :idea_import, class_name: 'BulkImportIdeas::IdeaImport', dependent: :destroy
 
       before_update :update_idea_import_approved_at
+      before_destroy :remove_imported_user
 
       def update_idea_import_approved_at
         return unless idea_import && publication_status_changed?(from: 'draft', to: 'published')
 
         idea_import.update!(approved_at: Time.now)
+      end
+
+      def remove_imported_user
+        return unless idea_import&.user_created == true && publication_status == 'draft'
+
+        idea_import.import_user.destroy!
       end
     end
   end
