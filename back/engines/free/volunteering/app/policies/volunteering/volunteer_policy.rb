da6e6efcaf2befa394710a11b86d6fd4a843b3a4
Sebastien Hoorens <sebastien@citizenlab.co>
2023-09-29 16:55:48 +0200

    do not allow creation of a volunteer if denied reason

--- back/engines/free/volunteering/app/policies/volunteering/volunteer_policy.rb
+++ back/engines/free/volunteering/app/policies/volunteering/volunteer_policy.rb
@@ -28,10 +28,15 @@ module Volunteering
     end
 
     def create?
-      # TODO: Don't allow volunteering if not allowed by participation context service
-      user&.active? &&
-        (record.user_id == user.id) &&
-        ProjectPolicy.new(user, record.cause.participation_context.project).show?
+      return false if !user&.active?
+      return false if record.user_id != user.id
+
+      project = record.cause.participation_context.project
+      return false if !ProjectPolicy.new(user, project).show?
+
+      reason = ParticipationContextService.new.volunteering_disabled_reason_for_project project, user
+      raise_not_authorized reason if reason
+      true
     end
 
     def destroy?
