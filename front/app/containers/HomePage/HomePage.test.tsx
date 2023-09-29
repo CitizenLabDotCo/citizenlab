77590f5b7d1debe9dc767227e189fbf3059c7f8c
brentguf <brentguf@users.noreply.github.com>
2023-09-29 10:58:20 +0200

    Attempt to fix flaky HomePage test

--- front/app/containers/HomePage/HomePage.test.tsx
+++ front/app/containers/HomePage/HomePage.test.tsx
@@ -5,25 +5,26 @@ import HomePage, { adminRedirectPath } from '.';
 import { mockAuthUserData } from 'api/me/__mocks__/useAuthUser';
 
 jest.mock('api/home_page/useHomepageSettings');
+jest.mock('api/app_configuration/useAppConfiguration');
 jest.mock('api/me/useAuthUser', () => () => ({
   data: { data: mockAuthUserData },
 }));
 
 describe('HomePage', () => {
-  it.skip('Tries to redirect on hitting the A key if you have admin access', async () => {
-    render(<HomePage />);
+  it('Tries to redirect on hitting the A key if you have admin access', async () => {
     const user = userEvent.setup();
+    render(<HomePage />);
     await user.keyboard('a');
 
     expect(clHistory.push).toHaveBeenCalledWith(adminRedirectPath);
   });
 
-  it.skip('Does not redirect if you do not have admin access', async () => {
+  it('Does not redirect if you do not have admin access', async () => {
     mockAuthUserData.attributes.roles = [];
     mockAuthUserData.attributes.highest_role = 'user';
 
-    render(<HomePage />);
     const user = userEvent.setup();
+    render(<HomePage />);
     await user.keyboard('a');
 
     expect(clHistory.push).not.toHaveBeenCalled();
