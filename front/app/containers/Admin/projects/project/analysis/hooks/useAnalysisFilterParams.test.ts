4ad30a1672dddefe0bce0e260284c9fd12700f89
Iva <iva@citizenlab.co>
2023-10-04 17:56:11 +0300

    Fix hook test

--- front/app/containers/Admin/projects/project/analysis/hooks/useAnalysisFilterParams.test.ts
+++ front/app/containers/Admin/projects/project/analysis/hooks/useAnalysisFilterParams.test.ts
@@ -4,8 +4,8 @@ import useAnalysisFilterParams from './useAnalysisFilterParams';
 const mockSearchParams = new URLSearchParams({
   search: 'example',
   tag_ids: '["1","2","3"]',
-  'author_custom_c80c2e9c-db9d-4e49-8894-d042fba841fa': 'male',
-  'author_custom_7e43a2e3-2fec-4c11-97c2-6bedb7f47910': 'somewhere_else',
+  'author_custom_c80c2e9c-db9d-4e49-8894-d042fba841fa': '["male"]',
+  'author_custom_7e43a2e3-2fec-4c11-97c2-6bedb7f47910': '["somewhere_else"]',
   'author_custom_87e4f5d3-fb0b-4a87-89e4-837933afda6c_from': '2010',
   'author_custom_87e4f5d3-fb0b-4a87-89e4-837933afda6c_to': '2019',
   published_at_from: '2023-09-10',
@@ -25,12 +25,16 @@ describe('useAnalysisFilterParams', () => {
 
     expect(result.current).toEqual({
       search: 'example',
+      'author_custom_7e43a2e3-2fec-4c11-97c2-6bedb7f47910': ['somewhere_else'],
+      'author_custom_87e4f5d3-fb0b-4a87-89e4-837933afda6c_from': '2010',
+      'author_custom_87e4f5d3-fb0b-4a87-89e4-837933afda6c_to': '2019',
+      'author_custom_c80c2e9c-db9d-4e49-8894-d042fba841fa': ['male'],
+      comments_from: '10',
+      published_at_from: '2023-09-10',
+      published_at_to: '2023-09-21',
+      reactions_from: '10',
       tag_ids: ['1', '2', '3'],
-      published_at_from: '2023-01-01',
-      published_at_to: '2023-12-31',
-      votes_from: 10,
-      comments_from: 10,
-      reactions_from: 10,
+      votes_from: '10',
     });
   });
 });
