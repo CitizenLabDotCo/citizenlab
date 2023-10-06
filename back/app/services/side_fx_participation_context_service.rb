076f1ad4e8d31be8cbd3a304d2d2506cb1eaac2f
Sebastien Hoorens <sebastien@citizenlab.co>
2023-10-06 15:36:52 +0200

    pc -> context

--- back/app/services/side_fx_participation_context_service.rb
+++ back/app/services/side_fx_participation_context_service.rb
@@ -3,65 +3,65 @@
 class SideFxParticipationContextService
   include SideFxHelper
 
-  def before_create(pc, user); end
+  def before_create(context, user); end
 
-  def after_create(pc, _user)
+  def after_create(context, _user)
     Surveys::WebhookManagerJob.perform_later(
       'participation_context_created',
-      pc,
-      pc.participation_method,
-      pc.survey_service,
-      pc.survey_embed_url
+      context,
+      context.participation_method,
+      context.survey_service,
+      context.survey_embed_url
     )
   end
 
-  def before_update(pc, _user)
-    method = Factory.instance.participation_method_for pc
-    if method.allowed_ideas_orders.exclude? pc.ideas_order
-      pc.ideas_order = method.allowed_ideas_orders.first
+  def before_update(context, _user)
+    method = Factory.instance.participation_method_for context
+    if method.allowed_ideas_orders.exclude? context.ideas_order
+      context.ideas_order = method.allowed_ideas_orders.first
     end
   end
 
-  def after_update(pc, user)
+  def after_update(context, user)
     %i[
       description_multiloc voting_method voting_max_votes_per_idea voting_max_total voting_min_total
       posting_enabled posting_method posting_limited_max commenting_enabled reacting_enabled
       reacting_like_method reacting_like_limited_max reacting_dislike_enabled presentation_mode
     ].each do |attribute|
-      if pc.send "#{attribute}_previously_changed?"
+      if context.send "#{attribute}_previously_changed?"
         LogActivityJob.perform_later(
-          pc,
+          context,
           "changed_#{attribute}",
           user,
-          pc.updated_at.to_i,
-          payload: { change: pc.send("#{attribute}_previous_change") }
+          context.updated_at.to_i,
+          payload: { change: context.send("#{attribute}_previous_change") }
         )
       end
     end
 
     Surveys::WebhookManagerJob.perform_later(
       'participation_context_changed',
-      pc,
-      pc.participation_method_previous_change&.dig(0) || pc.participation_method,
-      pc.participation_method,
-      pc.survey_service_previous_change&.dig(0) || pc.survey_service,
-      pc.survey_service,
-      pc.survey_embed_url_previous_change&.dig(0) || pc.survey_embed_url,
-      pc.survey_embed_url
+      context,
+      context.participation_method_previous_change&.dig(0) || context.participation_method,
+      context.participation_method,
+      context.survey_service_previous_change&.dig(0) || context.survey_service,
+      context.survey_service,
+      context.survey_embed_url_previous_change&.dig(0) || context.survey_embed_url,
+      context.survey_embed_url
     )
   end
 
-  def before_destroy(pc, _user)
+  def before_destroy(context, _user)
     Surveys::WebhookManagerJob.perform_later(
       'participation_context_to_be_deleted',
-      pc.id,
-      pc.participation_method,
-      pc.survey_service,
-      pc.survey_embed_url
+      context.id,
+      context.participation_method,
+      context.survey_service,
+      context.survey_embed_url
     )
   end
 
-  def after_destroy(frozen_pc, user); end
+  def after_destroy(frozen_context, user); end
 end
 
 SideFxParticipationContextService.prepend(GranularPermissions::Patches::SideFxParticipationContextService)
