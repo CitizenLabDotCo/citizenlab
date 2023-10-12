b942bb4595dc3ce6c00b3d4c4361a7b85aa350b5
jamesspeake <james@speake.org>
2023-10-12 15:17:28 +0100

    [CL-4165] Fix for custom fields schema loading if no current phase

--- back/app/controllers/web_api/v1/project_custom_fields_controller.rb
+++ back/app/controllers/web_api/v1/project_custom_fields_controller.rb
@@ -20,7 +20,7 @@ class WebApi::V1::ProjectCustomFieldsController < ApplicationController
   end
 
   def participation_context
-    @participation_context ||= ParticipationContextService.new.get_participation_context project
+    @participation_context ||= ParticipationContextService.new.get_participation_context(project) || project
   end
 
   def input_term
