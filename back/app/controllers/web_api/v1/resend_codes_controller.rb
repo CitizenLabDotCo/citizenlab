292f0800245fd7030ed35726446ae96900be7209
Sebastien Hoorens <sebastien@citizenlab.co>
2023-10-12 15:59:20 +0200

    small cleanup fix

--- back/app/controllers/web_api/v1/resend_codes_controller.rb
+++ back/app/controllers/web_api/v1/resend_codes_controller.rb
@@ -5,13 +5,7 @@ class WebApi::V1::ResendCodesController < ApplicationController
 
   def create
     RequestConfirmationCodeJob.perform_now(current_user, new_email: resend_code_params[:new_email])
-    result = SendConfirmationCode.call(user: current_user, new_email: resend_code_params[:new_email])
-
-    if result.success?
-      head :ok
-    else # TODO: raise sentry error
-      render json: { errors: result.errors.details }, status: :unprocessable_entity
-    end
+    head :ok
   end
 
   private
