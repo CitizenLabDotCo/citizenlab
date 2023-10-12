590b1f899883afb58e4a1cf23e4ebd727b93f3a1
Sebastien Hoorens <sebastien@citizenlab.co>
2023-10-12 17:57:54 +0200

    fix confirm user spec

--- back/app/controllers/web_api/v1/resend_codes_controller.rb
+++ back/app/controllers/web_api/v1/resend_codes_controller.rb
@@ -4,7 +4,7 @@ class WebApi::V1::ResendCodesController < ApplicationController
   skip_after_action :verify_authorized
 
   def create
-    RequestConfirmationCodeJob.perform_now(current_user, new_email: resend_code_params[:new_email])
+    RequestConfirmationCodeJob.perform_now current_user, new_email: resend_code_params[:new_email]
     if current_user.valid?
       head :ok
     else
