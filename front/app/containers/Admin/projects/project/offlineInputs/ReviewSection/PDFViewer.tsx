786bf64866d52e970b21d83c1c7b10b0bdd05ece
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-10-13 11:33:41 +0200

    _

--- front/app/containers/Admin/projects/project/offlineInputs/ReviewSection/PDFViewer.tsx
+++ front/app/containers/Admin/projects/project/offlineInputs/ReviewSection/PDFViewer.tsx
@@ -21,6 +21,7 @@ interface Props {
 
 const PDFViewer = ({ currentPageIndex, file, pages }: Props) => {
   const [pagesInDocument, setPagesInDocument] = useState<number | null>(null);
+  console.log('!');
 
   const handleLoadSuccess = ({ numPages }: { numPages: number }) => {
     setPagesInDocument(numPages);
