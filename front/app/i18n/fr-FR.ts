adc77f379ac31422cd30d6d623ee7f7019c20e50
brentguf <brentguf@users.noreply.github.com>
2023-10-13 09:58:19 +0200

    Register locales for react-datepicker

--- front/app/i18n/fr-FR.ts
+++ front/app/i18n/fr-FR.ts
@@ -1,5 +1,8 @@
 import { formatTranslationMessages } from './';
+import { registerLocale } from 'react-datepicker';
+import frFR from 'date-fns/locale/fr';
 
+registerLocale('fr-FR', frFR);
 const frFRTranslationMessages = require('translations/fr-FR.json');
 const frFRAdminTranslationMessages = require('translations/admin/fr-FR.json');
 const translationMessages = formatTranslationMessages('fr-FR', {
--- front/app/i18n/pt-BR.ts
+++ front/app/i18n/pt-BR.ts
@@ -1,5 +1,8 @@
 import { formatTranslationMessages } from './';
+import { registerLocale } from 'react-datepicker';
+import ptBR from 'date-fns/locale/pt-BR';
 
+registerLocale('pt-BR', ptBR);
 const ptBRTranslationMessages = require('translations/pt-BR.json');
 const ptBRAdminTranslationMessages = require('translations/admin/pt-BR.json');
 const translationMessages = formatTranslationMessages('pt-BR', {
