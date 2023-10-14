6f70f9f4aa768299a3bd1c945008aed09104f38a
brentguf <brentguf@users.noreply.github.com>
2023-10-14 11:04:00 +0200

    Msg id

--- front/app/components/admin/DateTimePicker/messages.ts
+++ front/app/components/admin/DateTimePicker/messages.ts
@@ -2,7 +2,7 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   time: {
-    id: 'app.components.admin.DateTimePicker',
+    id: 'app.components.admin.DateTimePicker.time',
     defaultMessage: 'Time',
   },
 });
