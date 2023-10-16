43bfb3124d357fc4f03a67eed7d3aead62346d5e
jamesspeake <james@speake.org>
2023-10-16 10:30:51 +0100

    [TAN-396] PR fix

--- back/engines/commercial/public_api/app/policies/public_api/power_bi_template_policy.rb
+++ back/engines/commercial/public_api/app/policies/public_api/power_bi_template_policy.rb
@@ -2,23 +2,6 @@
 
 module PublicApi
   class PowerBiTemplatePolicy < ApplicationPolicy
-    class Scope
-      attr_reader :user, :scope
-
-      def initialize(user, scope)
-        @user  = user
-        @scope = scope
-      end
-
-      def resolve
-        if user&.active? && user&.admin?
-          scope.all
-        else
-          scope.none
-        end
-      end
-    end
-
     def show?
       active? && admin?
     end
