cb80ecb4de404070938daef6f54025997cb74ce5
Iva <iva@citizenlab.co>
2023-10-17 14:33:25 +0300

    Remove default locale

--- back/app/services/location/service.rb
+++ back/app/services/location/service.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 class Location::Service
-  def autocomplete(input, language = 'en')
+  def autocomplete(input, language)
     response = HTTParty.get("https://maps.googleapis.com/maps/api/place/autocomplete/json?input=#{input}&key=#{api_key}&language=#{language}")
     { results: response['predictions'].pluck('description') }
   end
 
-  def geocode(address, language = 'en')
+  def geocode(address, language)
     response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&key=#{api_key}&language=#{language}")
     { location: response['results'].first['geometry']['location'] }
   end
 
-  def reverse_geocode(lat, lng, language = 'en')
+  def reverse_geocode(lat, lng, language)
     response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=#{lat},#{lng}&key=#{api_key}&language=#{language}")
     { formatted_address: response['results'].first['formatted_address'] }
   end
