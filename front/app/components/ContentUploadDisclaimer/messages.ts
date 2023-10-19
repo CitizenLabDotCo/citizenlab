a483aa9a401d7cfab036c50aa86febdd230eb569
Amanda Anderson <anderson_amanda@mail.com>
2023-10-19 11:10:16 +0200

    Update copy

--- front/app/components/ContentUploadDisclaimer/messages.ts
+++ front/app/components/ContentUploadDisclaimer/messages.ts
@@ -2,8 +2,8 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
     contentUploadDisclaimerFull: {
-    id: 'app.components.ContentUploadDisclaimer.contentUploadDisclaimerFull',
-    defaultMessage: 'By uploading content, you declare that this content does not violate any regulations or rights of third parties, such as intellectual property rights, privacy rights, rights to trade secrets, and so on. Consequently, by uploading this content, you undertake to bear full and exclusive liability for all direct and indirect damages resulting from the uploaded content. Furthermore, you undertake to indemnify the city or municipality (platform owner) and CitizenLab against any third party claims or liabilities against third parties, and any associated costs, that would arise or result from the content you uploaded.',
+    id: 'app.components.ContentUploadDisclaimer.contentUploadDisclaimerFull2',
+    defaultMessage: 'By uploading content, you declare that this content does not violate any regulations or rights of third parties, such as intellectual property rights, privacy rights, rights to trade secrets, and so on. Consequently, by uploading this content, you undertake to bear full and exclusive liability for all direct and indirect damages resulting from the uploaded content. Furthermore, you undertake to indemnify the platform owner and CitizenLab against any third party claims or liabilities against third parties, and any associated costs, that would arise or result from the content you uploaded.',
   },
   contentDisclaimerModalHeader: {
     id: 'app.components.ContentUploadDisclaimer.contentDisclaimerModalHeader',
