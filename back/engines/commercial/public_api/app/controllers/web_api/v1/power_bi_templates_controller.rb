fc0004cace77a029de5edd75c76d582b81f8bd04
jamesspeake <james@speake.org>
2023-10-20 12:12:56 +0100

    [TAN-396] PR enhancement

--- back/engines/commercial/public_api/app/controllers/web_api/v1/power_bi_templates_controller.rb
+++ back/engines/commercial/public_api/app/controllers/web_api/v1/power_bi_templates_controller.rb
@@ -6,14 +6,15 @@ module PublicApi
     def show
       authorize :'public_api/power_bi_template'
 
-      if params[:id] == 'dataflow'
+      case params[:id]
+      when 'dataflow'
         template = Rails.root.join('engines/commercial/public_api/files/power_bi/dataflow.json')
         # Search and replace API base URL
         file_text = open(template).read
         host = AppConfiguration.instance.base_backend_uri
         replaced = file_text.gsub(/##BASE_URL##/, "#{host}/api/v2/")
         send_data replaced, type: 'application/octet-stream'
-      elsif params[:id] == 'report'
+      when 'report'
         template = Rails.root.join('engines/commercial/public_api/files/power_bi/report.pbit')
         send_data open(template).read, type: 'application/octet-stream'
       else
