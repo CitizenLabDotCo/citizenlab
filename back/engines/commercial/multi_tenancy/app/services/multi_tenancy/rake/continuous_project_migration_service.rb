611a5c0f550957216ce6dd3c18b0e7fd8ff9556e
jamesspeake <james@speake.org>
2023-10-27 16:19:25 +0100

    [TAN-419] Test for phase settings being copied across

--- back/engines/commercial/multi_tenancy/app/services/multi_tenancy/rake/continuous_project_migration_service.rb
+++ back/engines/commercial/multi_tenancy/app/services/multi_tenancy/rake/continuous_project_migration_service.rb
@@ -15,6 +15,8 @@ class MultiTenancy::Rake::ContinuousProjectMigrationService
     @stats[:projects] = projects.count
     return unless persist_changes
 
+    # TODO: Fail for the whole tenant if one thing fails
+    # TODO: Should we wrap each project in a transaction is that possible?
     projects.each do |project|
       # 1. Change the process_type
       project.update!(process_type: 'timeline')
@@ -22,6 +24,7 @@ class MultiTenancy::Rake::ContinuousProjectMigrationService
       # 2. Create a single phase with the same settings as the project
       phase = create_phase(project)
       project.phases << phase
+      reset_project_defaults(project)
 
       # TODO: Check that projects now has just one phase before continuing
 
@@ -34,14 +37,14 @@ class MultiTenancy::Rake::ContinuousProjectMigrationService
       # 5. Update permission_scope in permissions from project_id to phase_id
       update_permission_scope(project, phase)
 
-      # 6. Add all ideas to the new phase
+      # 6. Add phase_id to creation_phase_id of any native_surveys
+      update_native_survey_creation_phase(project, phase)
+
+      # 7. Add all ideas to the new phase
       add_ideas_to_phase(project, phase)
 
-      # 7. Run function to update basket/votes count on ideas_phases
+      # 8. Run function to update basket/votes count on ideas_phases
       update_counts(project)
-
-      # 8. Add phase_id to creation_phase_id of any native_surveys
-      update_native_survey_creation_phase(project, phase)
 
       # 9. Activities - Do we need to update any of the historic logs? If so we should run this as a separate task - could be huge
 
@@ -53,44 +56,77 @@ class MultiTenancy::Rake::ContinuousProjectMigrationService
 
   def create_phase(project)
     phase = Phase.new(
-      title_multiloc: { en: 'default' }, # TODO: Set this to the participation method name - need the translations in the codebase for this
+      title_multiloc: create_phase_title(project),
       project: project,
-      created_at: project.created_at,
+      created_at: project.created_at, # TODO: Use project publication date if available
       start_at: project.created_at,
       end_at: nil,
 
-      # TODO: Double check that these are all the setting that are the same + remove some from the project?
       participation_method: project.participation_method,
+      allow_anonymous_participation: project.allow_anonymous_participation,
+      campaigns_settings: { project_phase_started: true },
+      document_annotation_embed_url: project.document_annotation_embed_url,
+      ideas_order: project.ideas_order,
+      input_term: project.input_term,
+      poll_anonymous: project.poll_anonymous,
+      presentation_mode: project.presentation_mode,
+      survey_embed_url: project.survey_embed_url,
+      survey_service: project.survey_service,
+
+      commenting_enabled: project.commenting_enabled,
       posting_enabled: project.posting_enabled,
       posting_method: project.posting_method,
       posting_limited_max: project.posting_limited_max,
-      commenting_enabled: project.commenting_enabled,
       reacting_enabled: project.reacting_enabled,
       reacting_like_method: project.reacting_like_method,
       reacting_like_limited_max: project.reacting_like_limited_max,
+      reacting_dislike_enabled: project.reacting_dislike_enabled,
       reacting_dislike_method: project.reacting_dislike_method,
       reacting_dislike_limited_max: project.reacting_dislike_limited_max,
-      survey_embed_url: project.survey_embed_url,
-      survey_service: project.survey_service,
-      presentation_mode: project.presentation_mode,
       voting_method: project.voting_method,
       voting_max_votes_per_idea: project.voting_max_votes_per_idea,
-      voting_term_singular_multiloc: project.voting_term_singular_multiloc,
-      voting_term_plural_multiloc: project.voting_term_plural_multiloc,
       voting_max_total: project.voting_max_total,
       voting_min_total: project.voting_min_total,
+      voting_term_singular_multiloc: project.voting_term_singular_multiloc,
+      voting_term_plural_multiloc: project.voting_term_plural_multiloc,
+
       ideas_count: project.ideas_count,
       baskets_count: project.baskets_count,
-      votes_count: project.votes_count,
-      poll_anonymous: project.poll_anonymous,
-      ideas_order: project.ideas_order,
-      input_term: project.input_term,
-      document_annotation_embed_url: project.document_annotation_embed_url,
-      allow_anonymous_participation: project.allow_anonymous_participation,
-      campaigns_settings: { project_phase_started: true }
+      votes_count: project.votes_count
     )
     phase.save!
     phase
+  end
+
+  def reset_project_defaults(project)
+    project.update!(
+      participation_method: 'ideation',
+      allow_anonymous_participation: false,
+      document_annotation_embed_url: nil,
+      ideas_order: nil,
+      input_term: 'idea',
+      poll_anonymous: false,
+      presentation_mode: 'card',
+      survey_embed_url: nil,
+      survey_service: nil,
+
+      commenting_enabled: true,
+      posting_enabled: true,
+      posting_method: 'unlimited',
+      posting_limited_max: 1,
+      reacting_enabled: true,
+      reacting_like_method: 'unlimited',
+      reacting_like_limited_max: 10,
+      reacting_dislike_enabled: true,
+      reacting_dislike_method: 'unlimited',
+      reacting_dislike_limited_max: 10,
+      voting_method: nil,
+      voting_max_votes_per_idea: nil,
+      voting_max_total: 10,
+      voting_min_total: 0,
+      voting_term_singular_multiloc: {},
+      voting_term_plural_multiloc: {}
+    )
   end
 
   def update_participation_contexts(project, phase)
@@ -125,6 +161,14 @@ class MultiTenancy::Rake::ContinuousProjectMigrationService
     if phase.participation_method == 'native_survey'
       Idea.where(project: project).update!(creation_phase: phase)
     end
+  end
+
+  def create_phase_title(_project)
+    # TODO: Set this to the participation method name - need the translations in the codebase for this
+    # app_locales = AppConfiguration.instance.settings('core', 'locales')
+    # I18n.with_locale(@locale) { I18n.t('form_builder.pdf_export.first_name') }
+    # binding.pry
+    { en: 'default' }
   end
 
   def error_handler(error)
--- back/engines/commercial/multi_tenancy/spec/services/multi_tenancy/rake/continuous_project_migration_service_spec.rb
+++ back/engines/commercial/multi_tenancy/spec/services/multi_tenancy/rake/continuous_project_migration_service_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe MultiTenancy::Rake::ContinuousProjectMigrationService do
   subject(:service) { described_class.new }
 
   shared_examples 'project_settings' do
+    let_it_be(:continuous_project_attributes) { project.attributes.symbolize_keys.clone }
+
     it 'should list number of projects & successful migrations' do
       expect(service.stats).to eq({ projects: 1, success: 1, errors: [] })
     end
@@ -17,13 +19,70 @@ RSpec.describe MultiTenancy::Rake::ContinuousProjectMigrationService do
     it 'creates a single open ended phase for the project' do
       expect(project.phases.count).to eq 1
       expect(project.phases.first.end_at).to be_nil
+
+      # TODO: Check title with all locales is created here
     end
 
     it 'copies all project settings to the phase' do
       phase = project.phases.first
-      expect(phase.participation_method).to eq project.participation_method
-      # TODO: Check the rest of the settings here
-      # TODO: Should it remove the settings from the project too?
+      expect(phase.attributes.symbolize_keys).to include({
+        participation_method: continuous_project_attributes[:participation_method],
+        allow_anonymous_participation: continuous_project_attributes[:allow_anonymous_participation],
+        campaigns_settings: { 'project_phase_started' => true },
+        document_annotation_embed_url: continuous_project_attributes[:document_annotation_embed_url],
+        ideas_order: continuous_project_attributes[:ideas_order],
+        input_term: continuous_project_attributes[:input_term],
+        poll_anonymous: continuous_project_attributes[:poll_anonymous],
+        presentation_mode: continuous_project_attributes[:presentation_mode],
+        survey_embed_url: continuous_project_attributes[:survey_embed_url],
+        survey_service: continuous_project_attributes[:survey_service],
+        commenting_enabled: continuous_project_attributes[:commenting_enabled],
+        posting_enabled: continuous_project_attributes[:posting_enabled],
+        posting_method: continuous_project_attributes[:posting_method],
+        posting_limited_max: continuous_project_attributes[:posting_limited_max],
+        reacting_enabled: continuous_project_attributes[:reacting_enabled],
+        reacting_like_method: continuous_project_attributes[:reacting_like_method],
+        reacting_like_limited_max: continuous_project_attributes[:reacting_like_limited_max],
+        reacting_dislike_enabled: continuous_project_attributes[:reacting_dislike_enabled],
+        reacting_dislike_method: continuous_project_attributes[:reacting_dislike_method],
+        reacting_dislike_limited_max: continuous_project_attributes[:reacting_dislike_limited_max],
+        voting_method: continuous_project_attributes[:voting_method],
+        voting_max_votes_per_idea: continuous_project_attributes[:voting_max_votes_per_idea],
+        voting_max_total: continuous_project_attributes[:voting_max_total],
+        voting_min_total: continuous_project_attributes[:voting_min_total],
+        voting_term_singular_multiloc: continuous_project_attributes[:voting_term_singular_multiloc],
+        voting_term_plural_multiloc: continuous_project_attributes[:voting_term_plural_multiloc]
+      })
+    end
+
+    it 'resets the project to defaults' do
+      expect(project.reload.attributes.symbolize_keys).to include({
+        participation_method: 'ideation',
+        allow_anonymous_participation: false,
+        document_annotation_embed_url: nil,
+        ideas_order: nil,
+        input_term: 'idea',
+        poll_anonymous: false,
+        presentation_mode: 'card',
+        survey_embed_url: nil,
+        survey_service: nil,
+        commenting_enabled: true,
+        posting_enabled: true,
+        posting_method: 'unlimited',
+        posting_limited_max: 1,
+        reacting_enabled: true,
+        reacting_like_method: 'unlimited',
+        reacting_like_limited_max: 10,
+        reacting_dislike_enabled: true,
+        reacting_dislike_method: 'unlimited',
+        reacting_dislike_limited_max: 10,
+        voting_method: nil,
+        voting_max_votes_per_idea: nil,
+        voting_max_total: 10,
+        voting_min_total: 0,
+        voting_term_singular_multiloc: {},
+        voting_term_plural_multiloc: {}
+      })
     end
   end
 
@@ -34,12 +93,15 @@ RSpec.describe MultiTenancy::Rake::ContinuousProjectMigrationService do
         expect(idea.phases.count).to eq 1
         expect(idea.phases.first).to eq phase
       end
+      expect(phase.ideas_count).to be > 0
+      expect(phase.ideas_count).to eq phase.ideas_count
     end
   end
 
   shared_examples 'permissions' do
     it 'moves permissions from project to the phase' do
       # TODO: Should it remove the permissions from the project too? Probably - but check what happens when creating timelines
+      # binding.pry
       expect(project).not_to be_nil
     end
   end
@@ -60,7 +122,9 @@ RSpec.describe MultiTenancy::Rake::ContinuousProjectMigrationService do
     end
 
     context 'with persistence' do
-      before { service.migrate(true) }
+      before do
+        service.migrate(true)
+      end
 
       context 'ideation projects' do
         let_it_be(:project) { create(:continuous_project) }
@@ -68,14 +132,30 @@ RSpec.describe MultiTenancy::Rake::ContinuousProjectMigrationService do
 
         include_examples 'project_settings'
         include_examples 'ideas'
+        include_examples 'permissions'
       end
 
-      context 'native surveys' do
+      context 'voting' do
+        let_it_be(:project) { create(:continuous_budgeting_project) }
+        let_it_be(:ideas) { create_list(:idea, 2, project: project) }
+
+        include_examples 'project_settings'
+        include_examples 'permissions'
+      end
+
+      context 'poll' do
+        let_it_be(:project) { create(:continuous_poll_project) }
+        include_examples 'project_settings'
+        include_examples 'permissions'
+      end
+
+      context 'native survey' do
         let_it_be(:project) { create(:continuous_native_survey_project) }
         let_it_be(:ideas) { create_list(:native_survey_response, 2, project: project) }
 
         include_examples 'project_settings'
         include_examples 'ideas'
+        include_examples 'permissions'
 
         it 'add the creation phase to each idea' do
           phase = project.phases.first
@@ -85,9 +165,16 @@ RSpec.describe MultiTenancy::Rake::ContinuousProjectMigrationService do
         end
       end
 
-      context 'voting' do
-        let_it_be(:project) { create(:continuous_budgeting_project) }
+      context 'survey' do
+        let_it_be(:project) { create(:continuous_survey_project) }
         include_examples 'project_settings'
+        include_examples 'permissions'
+      end
+
+      context 'document_annotation' do
+        let_it_be(:project) { create(:continuous_document_annotation_project) }
+        include_examples 'project_settings'
+        include_examples 'permissions'
       end
 
       context 'volunteering' do
@@ -95,23 +182,8 @@ RSpec.describe MultiTenancy::Rake::ContinuousProjectMigrationService do
         include_examples 'project_settings'
       end
 
-      context 'poll' do
-        let_it_be(:project) { create(:continuous_poll_project) }
-        include_examples 'project_settings'
-      end
-
-      context 'survey' do
-        let_it_be(:project) { create(:continuous_survey_project) }
-        include_examples 'project_settings'
-      end
-
       context 'information' do
         let_it_be(:project) { create(:continuous_project, participation_method: 'information') }
-        include_examples 'project_settings'
-      end
-
-      context 'document_annotation' do
-        let_it_be(:project) { create(:continuous_document_annotation_project) }
         include_examples 'project_settings'
       end
     end
