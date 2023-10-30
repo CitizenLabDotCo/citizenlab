e379110e0d27a8d0ef4c5d5d26fd826ab2381145
EdwinKato <katedwin88@gmail.com>
2023-10-30 13:09:10 +0300

    Add messages for new project settings

---
+++ front/app/containers/Admin/projects/project/settings/messages.ts
@@ -0,0 +1,28 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  projectSettings: {
+    id: 'app.containers.AdminPage.Project.settings.projectSettings',
+    defaultMessage: 'Project settings',
+  },
+  general: {
+    id: 'app.containers.AdminPage.Project.settings.general',
+    defaultMessage: 'General',
+  },
+  description: {
+    id: 'app.containers.AdminPage.Project.settings.description',
+    defaultMessage: 'Description',
+  },
+  projectTags: {
+    id: 'app.containers.AdminPage.Project.settings.projectTags',
+    defaultMessage: 'Project tags',
+  },
+  accessRights: {
+    id: 'app.containers.AdminPage.Project.settings.accessRights',
+    defaultMessage: 'Access rights',
+  },
+  events: {
+    id: 'app.containers.AdminPage.Project.settings.events',
+    defaultMessage: 'Events',
+  },
+});
