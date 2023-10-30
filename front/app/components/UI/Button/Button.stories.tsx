621117dcda371f5df9dcdf6a7f575a602101d95a
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-10-30 09:55:26 +0000

    _

--- front/app/components/UI/Button/Button.stories.tsx
+++ front/app/components/UI/Button/Button.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react';
-
 import Button from '.';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -12,7 +11,7 @@ const meta = {
   },
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    bgColor: { control: 'color' },
+    // bgColor: { control: 'color' },
   },
   args: {
     children: 'Button text',
