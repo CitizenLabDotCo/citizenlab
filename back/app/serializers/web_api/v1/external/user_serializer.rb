1bb3bba3afd321b9f0b631f0e30794edf40cfccc
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2023-11-06 14:08:46 +0100

    TAN-503 Added hightest_role to user external serializer

--- back/app/serializers/web_api/v1/external/user_serializer.rb
+++ back/app/serializers/web_api/v1/external/user_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class WebApi::V1::External::UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :locale, :slug, :avatar, :bio_multiloc
+  attributes :id, :first_name, :last_name, :email, :locale, :slug, :avatar, :bio_multiloc, :highest_role
 
   def avatar
     object.avatar && object.avatar.versions.to_h { |k, v| [k.to_s, v.url] }
