4ae9e6a94a45542c38442448beb1711e13ae6b37
EdwinKato <katedwin88@gmail.com>
2023-11-07 13:34:09 +0300

    Move project tabs out of file

---
+++ front/app/containers/Admin/projects/project/tabs.ts
@@ -0,0 +1,44 @@
+import { ITab, FormatMessage } from 'typings';
+import messages from './messages';
+
+export const getIntialTabs = (formatMessage: FormatMessage): ITab[] => [
+  {
+    label: formatMessage(messages.setup),
+    url: 'setup',
+    name: 'setup',
+  },
+  {
+    label: formatMessage(messages.inputManagerTab),
+    url: 'ideas',
+    name: 'ideas',
+  },
+  {
+    label: formatMessage(messages.inputFormTab),
+    url: 'ideaform',
+    name: 'ideaform',
+  },
+  {
+    label: formatMessage(messages.pollTab),
+    url: 'poll',
+    feature: 'polls',
+    name: 'poll',
+  },
+  {
+    label: formatMessage(messages.surveyTab),
+    url: 'native-survey',
+    name: 'survey',
+    active: (url: string) =>
+      url.endsWith('native-survey') || url.endsWith('native-survey/results'),
+  },
+  {
+    label: formatMessage(messages.surveyResultsTab),
+    url: 'survey-results',
+    name: 'survey-results',
+  },
+  {
+    label: formatMessage(messages.volunteeringTab),
+    url: 'volunteering',
+    feature: 'volunteering',
+    name: 'volunteering',
+  },
+];
