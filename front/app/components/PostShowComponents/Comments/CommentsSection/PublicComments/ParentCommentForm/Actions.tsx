3ce4f706ddaa9825ef105ebe79e5070755d6dd6f
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-11-07 20:42:35 +0000

    Make anonymous posting thing responsive

--- front/app/components/PostShowComponents/Comments/CommentsSection/PublicComments/ParentCommentForm/Actions.tsx
+++ front/app/components/PostShowComponents/Comments/CommentsSection/PublicComments/ParentCommentForm/Actions.tsx
@@ -41,6 +41,7 @@ const Actions = ({
 }: Props) => {
   const { formatMessage } = useIntl();
   const smallerThanTablet = useBreakpoint('tablet');
+  const smallerThanPhone = useBreakpoint('phone');
   const { pathname } = useLocation();
 
   const isAdminPage = isPage('admin', pathname);
@@ -51,49 +52,44 @@ const Actions = ({
 
   const visible = focused || processing;
 
-  return (
-    <Box
-      justifyContent="flex-end"
-      mt="10px"
-      mb="10px"
-      mr="10px"
-      display={visible ? 'flex' : 'none'}
-    >
-      {allowAnonymousParticipation && (
-        <Checkbox
-          id="e2e-anonymous-comment-checkbox"
-          ml="8px"
-          checked={postAnonymously}
-          label={
-            <Text mb="12px" fontSize="s" color="coolGrey600">
-              {formatMessage(messages.postAnonymously)}
-              <IconTooltip
-                content={
-                  <Text color="white" fontSize="s" m="0">
-                    {formatMessage(messages.inputsAssociatedWithProfile)}
-                  </Text>
-                }
-                iconSize="16px"
-                placement="top-start"
-                display="inline"
-                ml="4px"
-                transform="translate(0,-1)"
-              />
+  const checkbox = (
+    <Box display="flex" alignItems="center">
+      <Checkbox
+        id="e2e-anonymous-comment-checkbox"
+        ml="8px"
+        mr="4px"
+        checked={postAnonymously}
+        onChange={() => togglePostAnonymously(!postAnonymously)}
+      />
+      <Text m="0px" fontSize="s" color="coolGrey600">
+        {formatMessage(messages.postAnonymously)}
+        <IconTooltip
+          content={
+            <Text color="white" fontSize="s" m="0">
+              {formatMessage(messages.inputsAssociatedWithProfile)}
             </Text>
           }
-          onChange={() => togglePostAnonymously(!postAnonymously)}
+          iconSize="16px"
+          placement="top-start"
+          display="inline"
+          ml="4px"
+          transform="translate(0,-1)"
         />
-      )}
-      <Box mr="8px">
-        <Button
-          disabled={processing}
-          onClick={close}
-          buttonStyle="secondary"
-          padding={smallerThanTablet ? '6px 12px' : undefined}
-        >
-          <FormattedMessage {...messages.cancel} />
-        </Button>
-      </Box>
+      </Text>
+    </Box>
+  );
+
+  const cancelAndSubmit = (
+    <Box display="flex">
+      <Button
+        disabled={processing}
+        onClick={close}
+        buttonStyle="secondary"
+        padding={smallerThanTablet ? '6px 12px' : undefined}
+        mr="8px"
+      >
+        <FormattedMessage {...messages.cancel} />
+      </Button>
       <Button
         className="e2e-submit-parentcomment"
         processing={processing}
@@ -104,6 +100,47 @@ const Actions = ({
       >
         <FormattedMessage {...buttonText} />
       </Button>
+    </Box>
+  );
+
+  if (smallerThanPhone) {
+    return (
+      <Box
+        justifyContent="flex-end"
+        mt="10px"
+        mb="10px"
+        mr="10px"
+        display={visible ? 'flex' : 'none'}
+      >
+        <Box display="flex" flexDirection="column">
+          {allowAnonymousParticipation && (
+            <Box
+              display="flex"
+              flexDirection="row"
+              justifyContent="flex-end"
+              w="100%"
+              mb="12px"
+            >
+              {checkbox}
+            </Box>
+          )}
+          {cancelAndSubmit}
+        </Box>
+      </Box>
+    );
+  }
+
+  return (
+    <Box
+      flexDirection="row-reverse"
+      justifyContent="space-between"
+      mt="10px"
+      mb="10px"
+      mr="10px"
+      display={visible ? 'flex' : 'none'}
+    >
+      {cancelAndSubmit}
+      {allowAnonymousParticipation && checkbox}
     </Box>
   );
 };
