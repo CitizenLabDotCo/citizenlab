2bbce1da995bda99edcbd92ca56df1fa657890f8
Amanda Anderson <anderson_amanda@mail.com>
2023-11-09 09:14:37 +0100

    Add in temporary handling to success action again

--- front/app/containers/Authentication/SuccessActions/actions/redirectToIdeaForm.ts
+++ front/app/containers/Authentication/SuccessActions/actions/redirectToIdeaForm.ts
@@ -8,7 +8,14 @@ import { stringify } from 'qs';
 
 // typings
 import { LatLng } from 'leaflet';
-import { IUser } from 'api/users/types';
+import { IUser, IUserData } from 'api/users/types';
+
+// api
+import { fetchPhase } from 'api/phases/usePhase';
+import { fetchProjectBySlug } from 'api/projects/useProjectBySlug';
+
+// utils
+import { getIdeaPostingRules } from 'utils/actionTakingRules';
 
 export interface RedirectToIdeaFormParams {
   projectSlug: string;
@@ -19,17 +26,28 @@ export interface RedirectToIdeaFormParams {
 
 export const redirectToIdeaForm =
   ({ projectSlug, latLng, phaseId }: RedirectToIdeaFormParams) =>
-  async () => {
-    trackEventByName(tracks.redirectedToIdeaFrom);
-    const positionParams = latLng ? { lat: latLng.lat, lng: latLng.lng } : {};
-    clHistory.push({
-      pathname: `/projects/${projectSlug}/ideas/new`,
-      search: stringify(
-        {
-          ...positionParams,
-          phase_id: phaseId,
-        },
-        { addQueryPrefix: true }
-      ),
+  async (authUser: IUserData) => {
+    // Fetch latest project data to check if posting is enabled for new authUser
+    const { data: project } = await fetchProjectBySlug({ slug: projectSlug });
+    const { data: phase } = await fetchPhase({ phaseId });
+
+    const { disabledReason } = getIdeaPostingRules({
+      project,
+      phase,
+      authUser,
     });
+    if (disabledReason !== 'postingLimitedMaxReached') {
+      trackEventByName(tracks.redirectedToIdeaFrom);
+      const positionParams = latLng ? { lat: latLng.lat, lng: latLng.lng } : {};
+      clHistory.push({
+        pathname: `/projects/${projectSlug}/ideas/new`,
+        search: stringify(
+          {
+            ...positionParams,
+            phase_id: phaseId,
+          },
+          { addQueryPrefix: true }
+        ),
+      });
+    }
   };
