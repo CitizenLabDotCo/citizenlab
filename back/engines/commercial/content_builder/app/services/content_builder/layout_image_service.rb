b9a4c9e514cdf9efb710ff86bc0b539e360d14f3
Sébastien Hoorens <sebastien@citizenlab.co>
2023-11-10 11:39:11 +0100

    new layout image service + spec

--- back/engines/commercial/content_builder/app/services/content_builder/layout_image_service.rb
+++ back/engines/commercial/content_builder/app/services/content_builder/layout_image_service.rb
@@ -2,12 +2,12 @@
 
 module ContentBuilder
   class LayoutImageService < ::ContentImageService
-    IMAGE_ELEMENT_TYPES = %w[MultilocImage HomepageBanner]
+    IMAGE_ELEMENT_TYPES = %w[ImageMultiloc HomepageBanner]
 
     protected
 
     def image_elements(content)
-      LayoutService.new.select_craftjs_elements_for_type(content, IMAGE_ELEMENT_TYPES).pluck('props').pluck('image')
+      LayoutService.new.select_craftjs_elements_for_types(content, IMAGE_ELEMENT_TYPES).pluck('props').pluck('image')
     end
 
     def content_image_class
@@ -15,19 +15,19 @@ module ContentBuilder
     end
 
     def image_attributes(img_elt, _imageable, _field)
-      { remote_image_url: img_elt['default'] }
+      { remote_image_url: img_elt['imageUrl'] }
     end
 
     def image_attributes_for_element
-      %w[default versions]
+      %w[imageUrl versions]
     end
 
     def code_attribute_for_element
-      'code'
+      'dataCode'
     end
 
     def set_image_attributes!(img_elt, content_image)
-      img_elt['default'] = content_image.image.url
+      img_elt['imageUrl'] = content_image.image.url
       # TODO: add versions
     end
   end
---
+++ back/engines/commercial/content_builder/spec/services/content_builder/layout_image_service_spec.rb
@@ -0,0 +1,245 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ContentBuilder::LayoutImageService do
+  let(:service) { described_class.new }
+
+  describe 'swap_data_images' do
+    it 'removes the src attribute from image elements' do
+      layout_image = create(:layout_image)
+      craftjs_json = {
+        'ROOT' => {
+          'type' => 'div',
+          'isCanvas' => true,
+          'props' => {
+            'id' => 'e2e-content-builder-frame',
+            'style' => {
+              'padding' => '4px',
+              'minHeight' => '160px',
+              'backgroundColor' => '#fff'
+            }
+          },
+          'displayName' => 'div',
+          'custom' => {},
+          'hidden' => false,
+          'nodes' => %w[XGtvXcaUr3 nt24xY6COf],
+          'linkedNodes' => {}
+        },
+        'nt24xY6COf' => {
+          'type' => {
+            'resolvedName' => 'ImageMultiloc'
+          },
+          'isCanvas' => false,
+          'props' => {
+            'image' => {
+              'imageUrl' => layout_image.image.url,
+              'id' => 'image',
+              'alt' => '',
+              'dataCode' => layout_image.code
+            }
+          },
+          'displayName' => 'Image',
+          'custom' => {},
+          'parent' => 'ROOT',
+          'hidden' => false,
+          'nodes' => [],
+          'linkedNodes' => {}
+        },
+        'XGtvXcaUr3' => {
+          'type' => {
+            'resolvedName' => 'Text'
+          },
+          'isCanvas' => false,
+          'props' => {
+            'text' => '<p>This is some text.</p>',
+            'id' => 'text'
+          },
+          'displayName' => 'Text',
+          'custom' => {},
+          'parent' => 'ROOT',
+          'hidden' => false,
+          'nodes' => [],
+          'linkedNodes' => {}
+        }
+      }
+      expected_json = {
+        'ROOT' => {
+          'type' => 'div',
+          'isCanvas' => true,
+          'props' => {
+            'id' => 'e2e-content-builder-frame',
+            'style' => {
+              'padding' => '4px',
+              'minHeight' => '160px',
+              'backgroundColor' => '#fff'
+            }
+          },
+          'displayName' => 'div',
+          'custom' => {},
+          'hidden' => false,
+          'nodes' => %w[XGtvXcaUr3 nt24xY6COf],
+          'linkedNodes' => {}
+        },
+        'nt24xY6COf' => {
+          'type' => {
+            'resolvedName' => 'ImageMultiloc'
+          },
+          'isCanvas' => false,
+          'props' => {
+            'image' => {
+              'id' => 'image',
+              'alt' => '',
+              'dataCode' => layout_image.code
+            }
+          },
+          'displayName' => 'Image',
+          'custom' => {},
+          'parent' => 'ROOT',
+          'hidden' => false,
+          'nodes' => [],
+          'linkedNodes' => {}
+        },
+        'XGtvXcaUr3' => {
+          'type' => {
+            'resolvedName' => 'Text'
+          },
+          'isCanvas' => false,
+          'props' => {
+            'text' => '<p>This is some text.</p>',
+            'id' => 'text'
+          },
+          'displayName' => 'Text',
+          'custom' => {},
+          'parent' => 'ROOT',
+          'hidden' => false,
+          'nodes' => [],
+          'linkedNodes' => {}
+        }
+      }
+
+      imageable = build(:home_page, craftjs_json: craftjs_json)
+      output = service.swap_data_images imageable.craftjs_json # , field: :craftjs_json
+      expect(output).to eq expected_json
+    end
+  end
+
+  describe 'render_data_images' do
+    it 'adds the src attribute to the image elements' do
+      layout_image = create(:layout_image)
+      craftjs_json = {
+        'ROOT' => {
+          'type' => 'div',
+          'isCanvas' => true,
+          'props' => {
+            'id' => 'e2e-content-builder-frame',
+            'style' => {
+              'padding' => '4px',
+              'minHeight' => '160px',
+              'backgroundColor' => '#fff'
+            }
+          },
+          'displayName' => 'div',
+          'custom' => {},
+          'hidden' => false,
+          'nodes' => %w[XGtvXcaUr3 nt24xY6COf],
+          'linkedNodes' => {}
+        },
+        'nt24xY6COf' => {
+          'type' => {
+            'resolvedName' => 'ImageMultiloc'
+          },
+          'isCanvas' => false,
+          'props' => {
+            'image' => {
+              'id' => 'image',
+              'alt' => '',
+              'dataCode' => layout_image.code
+            }
+          },
+          'displayName' => 'Image',
+          'custom' => {},
+          'parent' => 'ROOT',
+          'hidden' => false,
+          'nodes' => [],
+          'linkedNodes' => {}
+        },
+        'XGtvXcaUr3' => {
+          'type' => {
+            'resolvedName' => 'Text'
+          },
+          'isCanvas' => false,
+          'props' => {
+            'text' => '<p>This is some text.</p>',
+            'id' => 'text'
+          },
+          'displayName' => 'Text',
+          'custom' => {},
+          'parent' => 'ROOT',
+          'hidden' => false,
+          'nodes' => [],
+          'linkedNodes' => {}
+        }
+      }
+      expected_json = {
+        'ROOT' => {
+          'type' => 'div',
+          'isCanvas' => true,
+          'props' => {
+            'id' => 'e2e-content-builder-frame',
+            'style' => {
+              'padding' => '4px',
+              'minHeight' => '160px',
+              'backgroundColor' => '#fff'
+            }
+          },
+          'displayName' => 'div',
+          'custom' => {},
+          'hidden' => false,
+          'nodes' => %w[XGtvXcaUr3 nt24xY6COf],
+          'linkedNodes' => {}
+        },
+        'nt24xY6COf' => {
+          'type' => {
+            'resolvedName' => 'ImageMultiloc'
+          },
+          'isCanvas' => false,
+          'props' => {
+            'image' => {
+              'id' => 'image',
+              'alt' => '',
+              'dataCode' => layout_image.code,
+              'imageUrl' => layout_image.image.url
+            }
+          },
+          'displayName' => 'Image',
+          'custom' => {},
+          'parent' => 'ROOT',
+          'hidden' => false,
+          'nodes' => [],
+          'linkedNodes' => {}
+        },
+        'XGtvXcaUr3' => {
+          'type' => {
+            'resolvedName' => 'Text'
+          },
+          'isCanvas' => false,
+          'props' => {
+            'text' => '<p>This is some text.</p>',
+            'id' => 'text'
+          },
+          'displayName' => 'Text',
+          'custom' => {},
+          'parent' => 'ROOT',
+          'hidden' => false,
+          'nodes' => [],
+          'linkedNodes' => {}
+        }
+      }
+
+      imageable = build(:home_page, craftjs_json: craftjs_json)
+      output = service.render_data_images imageable.craftjs_json # , field: :craftjs_json
+      expect(output).to eq expected_json
+    end
+  end
+end
