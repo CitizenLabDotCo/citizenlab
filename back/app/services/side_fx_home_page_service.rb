ddd9ffc944101fc9d60acdaa603ede5c99bbc74c
Sébastien Hoorens <sebastien@citizenlab.co>
2023-11-10 12:53:55 +0100

    text image service signature changes

--- back/app/services/side_fx_home_page_service.rb
+++ back/app/services/side_fx_home_page_service.rb
@@ -8,11 +8,11 @@ class SideFxHomePageService
 
   def before_update(home_page, _ = nil)
     if home_page.top_info_section_multiloc.present?
-      home_page.top_info_section_multiloc = TextImageService.new.swap_data_images_multiloc home_page.top_info_section_multiloc, :top_info_section_multiloc
+      home_page.top_info_section_multiloc = TextImageService.new.swap_data_images_multiloc home_page.top_info_section_multiloc, field: :top_info_section_multiloc, imageable: home_page
     end
 
     if home_page.bottom_info_section_multiloc.present?
-      home_page.bottom_info_section_multiloc = TextImageService.new.swap_data_images_multiloc home_page.bottom_info_section_multiloc, :bottom_info_section_multiloc
+      home_page.bottom_info_section_multiloc = TextImageService.new.swap_data_images_multiloc home_page.bottom_info_section_multiloc, field: :bottom_info_section_multiloc, imageable: home_page
     end
 
     # TODO: move to layout
