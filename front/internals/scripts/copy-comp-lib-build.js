ee55555ef98e975eca171e5075340362c61aa7b2
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-11-10 10:57:23 +0000

    Update script for CI

--- front/internals/scripts/copy-comp-lib-build.js
+++ front/internals/scripts/copy-comp-lib-build.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
 const { execSync } = require('child_process');
 
-execSync('cp ../cl2-component-library/dist/cl2-component-library.esm.js ./internals/jest/cl2-component-library.esm.js')
+const build = '../cl2-component-library/dist/cl2-component-library.esm.js';
+
+if (fs.existsSync(build)) {
+  execSync(`cp ${build} ./internals/jest/cl2-component-library.esm.js`)
+} else {
+  console.log('Could not find component library build. Please build the component library first.')
+}
