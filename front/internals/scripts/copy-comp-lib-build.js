37212a1b167cda80707e23ba6dde6e5181fa35f5
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-11-10 11:11:49 +0000

    More changes to script

--- front/internals/scripts/copy-comp-lib-build.js
+++ front/internals/scripts/copy-comp-lib-build.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
 
-const build = '../cl2-component-library/dist/cl2-component-library.esm.js';
+const buildFileName = 'cl2-component-library.esm.js';
+const compLibBuild = `../cl2-component-library/dist/${buildFileName}`;
 
-if (fs.existsSync(build)) {
-  execSync(`cp ${build} ./internals/jest/cl2-component-library.esm.js`)
-} else {
-  console.log('Could not find component library build. Please build the component library first.')
+// check build in component library
+if (fs.existsSync(compLibBuild)) {
+  execSync(`cp ${compLibBuild} ./internals/jest/${buildFileName}`);
+  return;
+}
+
+const nodeModulesBuild = `./node_modules/@citizenlab/cl2-component-library/${buildFileName};`
+
+// check in node_modules
+if (fs.existsSync(nodeModulesBuild)) {
+  execSync(`cp ${nodeModulesBuild} ./internals/jest/${buildFileName}`);
+  return;
 }
