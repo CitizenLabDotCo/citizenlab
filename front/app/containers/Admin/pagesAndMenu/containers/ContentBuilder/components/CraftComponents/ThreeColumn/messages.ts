2a544a5e1bc6d0f20e709c2cbd2b7e9af759c7af
Iva <iva@citizenlab.co>
2023-11-14 11:49:13 +0200

    Fix intl

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/CraftComponents/ThreeColumn/messages.ts
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/CraftComponents/ThreeColumn/messages.ts
@@ -2,7 +2,7 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   threeColumn: {
-    id: 'app.containers.admin.ContentBuilder.threeColumnLayout',
+    id: 'app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.CraftComponents.threeColumnLayout',
     defaultMessage: '3 column',
   },
 });
--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/CraftComponents/TwoColumn/messages.ts
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/CraftComponents/TwoColumn/messages.ts
@@ -2,23 +2,23 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   twoColumn: {
-    id: 'app.containers.admin.ContentBuilder.twoColumnLayout',
+    id: 'app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.CraftComponents..twoColumnLayout',
     defaultMessage: '2 column',
   },
   twoEvenColumn: {
-    id: 'app.containers.admin.ContentBuilder.twoEvenColumnLayout',
+    id: 'app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.CraftComponents..twoEvenColumnLayout',
     defaultMessage: '2 even columns',
   },
   twoColumnVariant1and2: {
-    id: 'app.containers.admin.ContentBuilder.twoColumnLayoutVariant1-2',
+    id: 'app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.CraftComponents..twoColumnLayoutVariant1-2',
     defaultMessage: '2 columns with 30% and 60% width respectively',
   },
   twoColumnVariant2and1: {
-    id: 'app.containers.admin.ContentBuilder.twoColumnLayoutVariant2-1',
+    id: 'app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.CraftComponents..twoColumnLayoutVariant2-1',
     defaultMessage: '2 columns with 60% and 30% width respectively',
   },
   columnLayoutRadioLabel: {
-    id: 'app.containers.AdminPage.ProjectDescription.columnLayoutRadioLabel',
+    id: 'app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.CraftComponents.columnLayoutRadioLabel',
     defaultMessage: 'Column layout',
   },
 });
