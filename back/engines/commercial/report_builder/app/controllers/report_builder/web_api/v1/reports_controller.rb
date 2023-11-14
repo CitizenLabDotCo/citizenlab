56351b7df141b8e3615f9ce54647de67a800b37f
Alexander <alexander@citizenlab.co>
2023-11-14 12:30:19 +0100

    [TAN-447] Add report association to phase responses

--- back/engines/commercial/report_builder/app/controllers/report_builder/web_api/v1/reports_controller.rb
+++ back/engines/commercial/report_builder/app/controllers/report_builder/web_api/v1/reports_controller.rb
@@ -97,7 +97,7 @@ module ReportBuilder
         end
 
         def serialize_report(report)
-          options = { params: jsonapi_serializer_params, include: [:layout] }
+          options = { params: jsonapi_serializer_params, include: %i[layout report] }
           ReportSerializer.new(report, options).serializable_hash.to_json
         end
 
