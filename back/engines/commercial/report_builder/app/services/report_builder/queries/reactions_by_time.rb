f1c40c8c13dd81a57d8a0bf03b5278502225333c
Alexander <alexander@citizenlab.co>
2023-11-15 18:14:04 +0100

    [TAN-449] Move queries to separate classes

---
+++ back/engines/commercial/report_builder/app/services/report_builder/queries/reactions_by_time.rb
@@ -0,0 +1,40 @@
+# rubocop:disable Naming/VariableName
+module ReportBuilder
+  class Queries::ReactionsByTime < Queries::Base
+    def query(startAt: nil, endAt: nil, projectId: nil, **_other_props)
+      startAt ||= '2017-01-01'
+      endAt ||= Time.now
+      time_series_query = {
+        fact: 'participation',
+        filters: {
+          **date_filter('dimension_date_created', startAt, endAt),
+          **project_filter('dimension_project', projectId),
+          'dimension_type.name': 'reaction',
+          'dimension_type.parent': 'idea'
+        },
+        groups: "dimension_date_created.#{interval(resolution)}",
+        aggregations: {
+          'dimension_date_created.date' => 'first',
+          likes_count: 'sum',
+          dislikes_count: 'sum'
+        }
+      }
+
+      posts_by_time_total = {
+        fact: 'participation',
+        filters: {
+          **date_filter('dimension_date_created', nil, endAt),
+          **project_filter('dimension_project', projectId),
+          'dimension_type.name' => 'reaction',
+          'dimension_type.parent' => 'idea'
+        },
+        aggregations: {
+          reactions_count: 'sum'
+        }
+      }
+
+      [time_series_query, posts_by_time_total]
+    end
+  end
+end
+# rubocop:enable Naming/VariableName
--- back/engines/commercial/report_builder/app/services/report_builder/query_repository.rb
+++ back/engines/commercial/report_builder/app/services/report_builder/query_repository.rb
@@ -1,53 +1,30 @@
 # frozen_string_literal: true
 
-# rubocop:disable Naming/VariableName
 require 'query'
 
-class ReportBuilder::QueryRepository
-  GRAPH_RESOLVED_NAMES_METHODS = {
-    'GenderWidget' => :users_by_gender
-  }.freeze
+module ReportBuilder
+  # TODO: rename to sth else
+  class QueryRepository
+    GRAPH_RESOLVED_NAMES_METHODS = {
+      'GenderWidget' => Queries::UsersByGender,
+      'ReactionsByTime' => Queries::ReactionsByTime
+    }.freeze
 
-  def data_by_graph(graph_resolved_name, props)
-    method = GRAPH_RESOLVED_NAMES_METHODS[graph_resolved_name]
-    return unless method
+    def data_by_graph(graph_resolved_name, props)
+      klass = GRAPH_RESOLVED_NAMES_METHODS[graph_resolved_name]
+      return unless klass
 
-    send(method, **props)
-  end
+      run_query(klass.new.query(**props))
+    end
 
-  protected
+    protected
 
-  def users_by_gender(startAt: nil, endAt: nil, projectId: nil, **_other_props)
-    query(
-      fact: 'participation',
-      groups: 'dimension_user_custom_field_values.value',
-      filters: {
-        'dimension_user.role': ['citizen', 'admin', nil],
-        'dimension_user_custom_field_values.key': 'gender',
-        # TODO: try to move `compact.presence` to Analytics::Query
-        **{
-          'dimension_date_created.date' =>
-            { from: startAt, to: endAt }.compact.presence
-        }.compact,
-        # TODO: use dimension_project_id
-        **{ 'dimension_projects.id': projectId }.compact
-      },
-      aggregations: {
-        'dimension_user_custom_field_values.dimension_user_id': 'count' # we count participants, not participations
-      }
-    )
-  end
-
-  def users_by_birthyear; end
-
-  private
-
-  def query(json_query)
-    query = Analytics::Query.new(json_query.with_indifferent_access)
-    # TODO: it's weird to validate and do not check the result. Fix this.
-    query.validate
-    query.run
-    query.results
+    def run_query(json_query)
+      query = Analytics::Query.new(json_query.with_indifferent_access)
+      # TODO: it's weird to validate and do not check the result. Fix this.
+      query.validate
+      query.run
+      query.results
+    end
   end
 end
-# rubocop:enable Naming/VariableName
