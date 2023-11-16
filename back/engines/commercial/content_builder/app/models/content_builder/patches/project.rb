e21a1ea506260f7fbb4124ab5f168db9986686a3
Sébastien Hoorens <sebastien@citizenlab.co>
2023-11-16 12:02:14 +0100

    identify spec

--- back/engines/commercial/content_builder/app/models/content_builder/patches/project.rb
+++ back/engines/commercial/content_builder/app/models/content_builder/patches/project.rb
@@ -10,12 +10,11 @@ module ContentBuilder
       CRAFTJS_TEXT_QUERY = <<-SQL.squish
         (
           SELECT
-            ARRAY_AGG(CONCAT_WS('. ', text, title, url))
+            ARRAY_AGG(CONCAT_WS('. ', alt, url))
           FROM
             (
               SELECT
-                JSONB_PATH_QUERY("craftjs_jsonmultiloc", '$.**.props.text') AS text,
-                JSONB_PATH_QUERY("craftjs_jsonmultiloc", '$.**.props.title') AS title,
+                JSONB_PATH_QUERY("craftjs_jsonmultiloc", '$.**.props.alt') AS alt,
                 JSONB_PATH_QUERY("craftjs_jsonmultiloc", '$.**.props.url') AS url
               FROM
                 content_builder_layouts
