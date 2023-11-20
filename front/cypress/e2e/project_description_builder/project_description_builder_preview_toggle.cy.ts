697ea1133bbaabea9914192132e91fd248d64ced
EdwinKato <katedwin88@gmail.com>
2023-11-20 07:53:49 +0300

    Fix project description builder preview e2e tests

--- front/cypress/e2e/project_description_builder/project_description_builder_preview_toggle.cy.ts
+++ front/cypress/e2e/project_description_builder/project_description_builder_preview_toggle.cy.ts
@@ -1,7 +1,9 @@
 import { randomString } from '../../support/commands';
+import moment = require('moment');
 
 describe('Project description builder preview', () => {
   let projectId = '';
+  let phaseId: string;
 
   const getIframeBody = () => {
     // get the document
@@ -23,20 +25,33 @@ describe('Project description builder preview', () => {
       const userId = user.body.data.id;
 
       cy.apiCreateProject({
-        type: 'continuous',
+        type: 'timeline',
         title: projectTitle,
         descriptionPreview: projectDescriptionPreview,
         description: projectDescription,
         publicationStatus: 'published',
         participationMethod: 'ideation',
         assigneeId: userId,
-      }).then((project) => {
-        projectId = project.body.data.id;
-        cy.visit(`/admin/projects/${projectId}/description`);
-        cy.get('#e2e-toggle-enable-project-description-builder').click({
-          force: true,
+      })
+        .then((project) => {
+          projectId = project.body.data.id;
+          return cy.apiCreatePhase({
+            projectId,
+            title: 'firstPhaseTitle',
+            startAt: moment().subtract(9, 'month').format('DD/MM/YYYY'),
+            participationMethod: 'ideation',
+            canPost: true,
+            canComment: true,
+            canReact: true,
+          });
+        })
+        .then((phase) => {
+          phaseId = phase.body.data.id;
+          cy.visit(`/admin/projects/${projectId}/settings/description`);
+          cy.get('#e2e-toggle-enable-project-description-builder').click({
+            force: true,
+          });
         });
-      });
     });
   });
 
@@ -71,6 +86,9 @@ describe('Project description builder preview', () => {
     cy.visit(
       `/admin/project-description-builder/projects/${projectId}/description`
     );
+    cy.get('#e2e-draggable-text').dragAndDrop('#e2e-content-builder-frame', {
+      position: 'inside',
+    });
     cy.wait(10000);
     cy.get('#e2e-text-box').should('exist');
     cy.get('#e2e-text-box').click();
@@ -79,9 +97,7 @@ describe('Project description builder preview', () => {
 
     cy.get('#e2e-preview-toggle').click({ force: true });
 
-    getIframeBody()
-      .contains('Edited text.Another edited text.')
-      .should('be.visible');
+    getIframeBody().contains('Another edited text.').should('be.visible');
   });
 
   it('allows user to navigate between mobile and desktop preview and see content in both', () => {
