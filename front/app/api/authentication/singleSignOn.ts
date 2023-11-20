bc508de37a66201879f57236036df90a1d6def70
Amanda Anderson <anderson_amanda@mail.com>
2023-11-20 10:37:50 +0100

    Fix comment

--- front/app/api/authentication/singleSignOn.ts
+++ front/app/api/authentication/singleSignOn.ts
@@ -28,7 +28,7 @@ export interface SSOParams {
   sso_verification_id?: string;
   sso_verification_type?: string;
   error_code?: SignUpInError;
-  // TODO: Refactoring + bettre integration of verification into new
+  // TODO: Refactoring + better integration of verification into new
   // registration flow when there is BE support
   verification_success?: string;
 }
