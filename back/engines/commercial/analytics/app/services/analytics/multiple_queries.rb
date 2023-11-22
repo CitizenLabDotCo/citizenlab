53ea20a2391fcf198063acbc43a26e7409856513
Alexander <alexander@citizenlab.co>
2023-11-22 22:11:15 +0800

    [TAN-449] Pass controller data that we use

--- back/engines/commercial/analytics/app/services/analytics/multiple_queries.rb
+++ back/engines/commercial/analytics/app/services/analytics/multiple_queries.rb
@@ -2,9 +2,8 @@
 
 module Analytics
   class MultipleQueries
-    def initialize(request = nil)
-      # TODO: move pagination controller?
-      @request = request
+    def initialize(original_url: nil)
+      @original_url = original_url
     end
 
     def run(json_query_input)
@@ -47,13 +46,13 @@ module Analytics
     end
 
     def add_pagination_url(paginations)
-      return paginations if @request.nil?
+      return paginations if @original_url.nil?
 
       paginations.map do |pagination|
         pagination.transform_values do |params|
           next unless params
 
-          parsed_url = URI.parse(@request.original_url)
+          parsed_url = URI.parse(@original_url)
           parsed_url.query = params
           parsed_url
         end
