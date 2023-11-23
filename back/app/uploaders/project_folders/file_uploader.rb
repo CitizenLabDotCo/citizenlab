f00b189622422e13e5f55dd38d819a46434706cd
Simon Tharby <simontharby@gmail.com>
2023-11-23 15:03:57 +0000

    [CL-4246] Whitelist image file-types in FileUploader

--- back/app/uploaders/project_folders/file_uploader.rb
+++ back/app/uploaders/project_folders/file_uploader.rb
@@ -3,7 +3,8 @@
 module ProjectFolders
   class FileUploader < BaseFileUploader
     def extension_allowlist
-      %w[avi csv doc docx key mkv mp3 mp4 numbers odp ods odt pages pdf ppt pptx txt xls xlsx]
+      %w[avi csv doc docx key mkv mp3 mp4 numbers odp ods odt pages pdf ppt pptx txt xls xlsx] +
+        BaseImageUploader::ALLOWED_TYPES
     end
 
     def size_range
