fe5192b2ded9184528658fe1e0b9e3d87e17a483
Alexander <alexander@citizenlab.co>
2023-11-24 11:21:14 +0100

    [CL-4257] Document why all_methods is defined in module

--- back/engines/commercial/verification/lib/verification.rb
+++ back/engines/commercial/verification/lib/verification.rb
@@ -3,6 +3,14 @@
 require 'verification/engine'
 
 module Verification
+  # This array `all_methods` is populated in `to_prepare` callbacks in Engines,
+  # and it's used to define the `VerificationMethodSerializer` attributes.
+  # When the code is autoreloaded, VerificationMethodSerializer is loaded BEFORE
+  # calling to_prepare callbacks, which means that `all_methods` is empty at the time.
+  # And so, no attributes are defined in the serializer.
+  # So, all_methods is defined here (in not-autoreloaded code), to keep it from
+  # resetting to empty when the code is autoreloaded.
+  #
   mattr_accessor(:all_methods) { [] }
 
   class << self
