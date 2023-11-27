a7a5523c3ae0562d3ab35d363795e1363b1db827
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2023-11-27 15:57:48 +0000

    Add e2e tests

--- front/cypress/e2e/auth/verification_modal.cy.ts
+++ front/cypress/e2e/auth/verification_modal.cy.ts
@@ -1,4 +1,4 @@
-import { randomString, randomEmail } from '../support/commands';
+import { randomString, randomEmail } from '../../support/commands';
 
 describe('Verification modal', () => {
   describe('shows the additional rules', () => {
