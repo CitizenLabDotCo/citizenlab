92353ef306066eb60a9366e8e91a8efdb63fa222
Alexander <alexander@citizenlab.co>
2023-11-30 12:04:44 +0100

    Add ErrorReporter.report_msg expectations

--- back/spec/services/whiny_open_struct_spec.rb
+++ back/spec/services/whiny_open_struct_spec.rb
@@ -8,12 +8,14 @@ describe WhinyOpenStruct do
       it 'raises error when unknown method is called' do
         hash = { qwe: 123 }
         whiny_open_struct = described_class.new(hash)
+        expect(ErrorReporter).not_to receive(:report_msg)
         expect { whiny_open_struct.unknown_method }.to raise_error(NoMethodError)
       end
 
       it 'does not raise error when trying to access existing member' do
         hash = { key: 'value' }
         whiny_open_struct = described_class.new(hash)
+        expect(ErrorReporter).not_to receive(:report_msg)
         expect { whiny_open_struct.key }.not_to raise_error
         expect(whiny_open_struct.key).to eq('value')
       end
@@ -23,12 +25,14 @@ describe WhinyOpenStruct do
       it 'does not raise error when unknown method is called' do
         hash = {}
         whiny_open_struct = described_class.new(hash, raise_exception: false)
+        expect(ErrorReporter).to receive(:report_msg).and_call_original
         expect { whiny_open_struct.unknown_method }.not_to raise_error
       end
 
       it 'does not raise error when trying to access existing member' do
         hash = { key: 'value' }
         whiny_open_struct = described_class.new(hash)
+        expect(ErrorReporter).not_to receive(:report_msg)
         expect { whiny_open_struct.key }.not_to raise_error
         expect(whiny_open_struct.key).to eq('value')
       end
