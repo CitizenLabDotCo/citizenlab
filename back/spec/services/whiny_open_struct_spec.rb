afd731fd9e2cd20613889aa649894c456489b069
Alexander <alexander@citizenlab.co>
2023-11-30 11:47:49 +0100

    Add WhinyOpenStruct and use it for email campaigns

---
+++ back/spec/services/whiny_open_struct_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe WhinyOpenStruct do
+  describe '#method_missing' do
+    context 'when raise_exception is true' do
+      it 'raises error when unknown method is called' do
+        hash = { qwe: 123 }
+        whiny_open_struct = described_class.new(hash)
+        expect { whiny_open_struct.unknown_method }.to raise_error(NoMethodError)
+      end
+
+      it 'does not raise error when trying to access existing member' do
+        hash = { key: 'value' }
+        whiny_open_struct = described_class.new(hash)
+        expect { whiny_open_struct.key }.not_to raise_error
+        expect(whiny_open_struct.key).to eq('value')
+      end
+    end
+
+    context 'when raise_exception is false' do
+      it 'does not raise error when unknown method is called' do
+        hash = {}
+        whiny_open_struct = described_class.new(hash, raise_exception: false)
+        expect { whiny_open_struct.unknown_method }.not_to raise_error
+      end
+
+      it 'does not raise error when trying to access existing member' do
+        hash = { key: 'value' }
+        whiny_open_struct = described_class.new(hash)
+        expect { whiny_open_struct.key }.not_to raise_error
+        expect(whiny_open_struct.key).to eq('value')
+      end
+    end
+  end
+end
