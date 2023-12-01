181f9f347422d069a15c51c1d86c332e3bbd2c57
Amanda Anderson <anderson_amanda@mail.com>
2023-12-01 13:01:55 +0100

    Remove WIP code

--- front/app/components/VotesNotSubmittedModal/index.tsx
+++ front/app/components/VotesNotSubmittedModal/index.tsx
@@ -26,13 +26,9 @@ import { useIntl } from 'utils/cl-intl';
 
 // types
 import { IBasket } from 'api/baskets/types';
-// eslint-disable-next-line no-restricted-imports
-import { Transition } from 'history';
 
 // utils
 import { getCurrentPhase } from 'api/phases/utils';
-import clHistory from 'utils/cl-router/history';
-import { useLocation } from 'react-router-dom';
 
 type Props = {
   showModal?: boolean;
@@ -48,96 +44,17 @@ const VotesNotSubmittedModal = ({
 }: Props) => {
   const { formatMessage } = useIntl();
   const theme = useTheme();
-  const location = useLocation();
   const isPhoneOrSmaller = useBreakpoint('phone');
   const votingInterface = useVoting();
   const { data: project } = useProjectById(projectId);
   const { data: phases } = usePhases(projectId);
   const currentPhase = getCurrentPhase(phases?.data);
 
-  const [transition, setTransition] = useState<Transition | undefined>(
-    undefined
-  );
   const [showDataUnsubmittedModal, setShowDataUnsubmittedModal] = useState(
     showModal || false
   );
 
-  const leavePage = useRef(false);
   const votesCastRef = useRef(votingInterface?.numberOfVotesCast);
-
-  //
-  //
-  // ********************** WIP CODE ***************************
-  // FUNCTION: Show modal when navigating away from a voting context (idea or project page)
-  // when there is an unsubmitted basket with votes in it.
-  if (currentPhase?.attributes.participation_method === 'voting') {
-    // history.Block() documentation --> https://github.com/remix-run/history/blob/main/docs/blocking-transitions.md
-    const unblock = clHistory.block((transition) => {
-      // ************************************************************
-      // NOTE: This code isn't clean - just trying to get this to work.
-      // ************************************************************
-
-      const allowRedirect = () => {
-        unblock();
-        transition.retry();
-        return null;
-      };
-
-      // If the user has selected "leave page" in the modal, allow the redirect
-      if (leavePage.current) {
-        allowRedirect();
-      }
-
-      // If no votes selected, allow the redirect
-      if (
-        votesCastRef.current === undefined ||
-        votesCastRef.current === 0 ||
-        basket?.data.attributes.total_votes === 0
-      ) {
-        allowRedirect();
-      }
-
-      // If replacing URL or going back, allow redirect. If basket is submitted, allow redirect.
-      if (
-        transition.action === 'REPLACE' ||
-        transition.action === 'POP' ||
-        basketSubmitted
-      ) {
-        allowRedirect();
-      } else {
-        if (
-          // If we're on a project page and going to an idea page (didn't think it needed to be more specific, as
-          // the chance of a user going from a project page to an idea page NOT within project would require
-          // a specific URL being entered into the search bar. Probably unlikely for this to happen.
-          location.pathname.includes('/projects/') &&
-          transition.location.pathname.includes('/ideas/')
-        ) {
-          allowRedirect();
-        }
-        if (
-          // If we're going from an idea page to the associated project page, allow redirect
-          location.pathname.includes('/ideas/') &&
-          transition.location.pathname.includes(
-            `/projects/${project?.data.attributes.slug}`
-          )
-        ) {
-          allowRedirect();
-        } else {
-          setTransition(transition);
-          setShowDataUnsubmittedModal(true);
-          return;
-        }
-      }
-
-      // By default, allow the redirect
-      unblock();
-      transition.retry();
-      return;
-    });
-  }
-  // ^ ********************* WIP CODE **************************
-  // ***********************************************************
-  //
 
   useEffect(() => {
     votesCastRef.current = votingInterface?.numberOfVotesCast;
@@ -207,31 +124,16 @@ const VotesNotSubmittedModal = ({
         </Title>
         <Text>{formatMessage(messages.submitYourVoteDescription)}</Text>
         <Box display="flex" gap="12px" flexDirection="column">
-          {transition ? (
-            <Button
-              width={isPhoneOrSmaller ? '100%' : 'fit-content'}
-              minWidth="260px"
-              buttonStyle="secondary"
-              mx="auto"
-              text={formatMessage(messages.leavePage)}
-              onClick={() => {
-                leavePage.current = true;
-                transition.retry();
-                setShowDataUnsubmittedModal(false);
-              }}
-            />
-          ) : (
-            <Button
-              width={isPhoneOrSmaller ? '100%' : 'fit-content'}
-              minWidth="260px"
-              buttonStyle="secondary"
-              mx="auto"
-              text={formatMessage(messages.doNotSubmitYet)}
-              onClick={() => {
-                setShowDataUnsubmittedModal(false);
-              }}
-            />
-          )}
+          <Button
+            width={isPhoneOrSmaller ? '100%' : 'fit-content'}
+            minWidth="260px"
+            buttonStyle="secondary"
+            mx="auto"
+            text={formatMessage(messages.doNotSubmitYet)}
+            onClick={() => {
+              setShowDataUnsubmittedModal(false);
+            }}
+          />
           {currentPhase && (
             <Box
               width={isPhoneOrSmaller ? '100%' : 'undefined'}
