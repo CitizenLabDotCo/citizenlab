a82be8bc36b878fcd3a5646fa76f5b2420bcd685
EdwinKato <katedwin88@gmail.com>
2023-12-05 09:03:39 +0300

    Remove project access rights tab config

--- front/app/containers/Admin/projects/project/permissions/granular_permissions/index.tsx
+++ front/app/containers/Admin/projects/project/permissions/granular_permissions/index.tsx
@@ -1,40 +1,12 @@
-import useFeatureFlag from 'hooks/useFeatureFlag';
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { ModuleConfiguration } from 'utils/moduleUtils';
 const InitiativeSettingsTab = React.lazy(
   () => import('./components/InitiativeSettingsTab')
 );
-const ProjectSettingsTab = React.lazy(
-  () => import('./components/ProjectSettingsTab')
-);
-const PhasePermissions = React.lazy(
-  () => import('./containers/Granular/PhasePermissions')
-);
-const Granular = React.lazy(() => import('./containers/Granular'));
-const FeatureFlag = React.lazy(() => import('components/FeatureFlag'));
 
 const AdminGranularPermissionsComponent = React.lazy(
   () => import('./containers/permissions')
 );
-
-type RenderOnTabHideConditionProps = {
-  children: ReactNode;
-};
-
-const RenderOnTabHideCondition = ({
-  children,
-}: RenderOnTabHideConditionProps) => {
-  // Could be more than just a feature flag check,
-  // hence we're not using the FeatureFlag component
-  const isGranularPermissionsEnabled = useFeatureFlag({
-    name: 'granular_permissions',
-  });
-
-  if (isGranularPermissionsEnabled) {
-    return <>{children}</>;
-  }
-  return null;
-};
 
 const configuration: ModuleConfiguration = {
   routes: {
@@ -49,13 +21,6 @@ const configuration: ModuleConfiguration = {
     'app.containers.Admin.initiatives.tabs': (props) => (
       <InitiativeSettingsTab {...props} />
     ),
-    'app.containers.Admin.projects.edit.settings': (props) => {
-      return (
-        <RenderOnTabHideCondition>
-          <ProjectSettingsTab {...props} />
-        </RenderOnTabHideCondition>
-      );
-    },
   },
 };
 
