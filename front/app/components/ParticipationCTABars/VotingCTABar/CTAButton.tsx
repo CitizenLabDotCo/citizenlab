6a8ff5b127d398e37c839e7bfc18d1166744afd7
Amanda Anderson <anderson_amanda@mail.com>
2023-12-05 10:56:20 +0100

    Fix small bug

--- front/app/components/ParticipationCTABars/VotingCTABar/CTAButton.tsx
+++ front/app/components/ParticipationCTABars/VotingCTABar/CTAButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 // hooks
 import useBasket from 'api/baskets/useBasket';
@@ -71,7 +71,7 @@ const CTAButton = ({ participationContext, projectId }: Props) => {
   const { data: ideas } = useIdeas({ phase: participationContext.id });
 
   const basketId = participationContext.relationships.user_basket?.data?.id;
-  const { data: basket } = useBasket(basketId);
+  const { data: basket, isFetching: isFetchingBasket } = useBasket(basketId);
   const { mutate: updateBasket } = useUpdateBasket();
   const {
     numberOfVotesCast,
@@ -86,28 +86,30 @@ const CTAButton = ({ participationContext, projectId }: Props) => {
   const votingMaxTotal = participationContext?.attributes.voting_max_total;
   const currency = appConfig?.data.attributes.settings.core.currency;
 
+  const usedUpBudget = useRef(true);
   let votesLeft = 0;
-  let usedUpBudget = true;
 
   if (!isNilOrError(votingMaxTotal) && !isNilOrError(numberOfVotesCast)) {
     votesLeft = votingMaxTotal - numberOfVotesCast;
   }
 
-  // In case of budgeting, we need to check if there are still ideas that the user has enough budget for
-  const ideasUserCanVoteFor = ideas?.data.filter(
-    (idea) =>
-      votesLeft > 0 &&
-      idea.attributes.budget &&
-      idea.attributes.budget <= votesLeft && // Remaining budget is enough to vote for this idea
-      !basket?.data.relationships.ideas.data.find(
-        // Check that idea is not already included in the basket
-        (basketIdea) => idea.id === basketIdea.id
-      )
-  );
+  useEffect(() => {
+    // In case of budgeting, we need to check if there are still ideas that the user has enough budget for
+    const ideasUserCanVoteFor = ideas?.data.filter(
+      (idea) =>
+        votesLeft > 0 &&
+        idea.attributes.budget &&
+        idea.attributes.budget <= votesLeft && // Remaining budget is enough to vote for this idea
+        !basket?.data.relationships.ideas.data.find(
+          // Check that idea is not already included in the basket
+          (basketIdea) => idea.id === basketIdea.id
+        )
+    );
 
-  if (ideasUserCanVoteFor?.length && ideasUserCanVoteFor.length > 0) {
-    usedUpBudget = false;
-  }
+    ideasUserCanVoteFor?.length && ideasUserCanVoteFor.length > 0
+      ? (usedUpBudget.current = false)
+      : (usedUpBudget.current = true);
+  }, [usedUpBudget, ideas, votesLeft, basket]);
 
   const disabledExplanation = getDisabledExplanation(
     formatMessage,
@@ -121,7 +123,7 @@ const CTAButton = ({ participationContext, projectId }: Props) => {
     if (!(votingMethod === 'budgeting') && userHasVotesLeft) {
       setShowModal(true);
       return;
-    } else if (votingMethod === 'budgeting' && !usedUpBudget) {
+    } else if (votingMethod === 'budgeting' && !usedUpBudget.current) {
       setShowModal(true); // There are still options where the cost is within the user's remaining budget
       return;
     }
@@ -192,7 +194,7 @@ const CTAButton = ({ participationContext, projectId }: Props) => {
             padding="6px 12px"
             fontSize="14px"
             disabled={!!disabledExplanation}
-            processing={processing}
+            processing={processing || isFetchingBasket}
             className={disabledExplanation ? '' : 'pulse'}
           >
             <FormattedMessage {...messages.submit} />
