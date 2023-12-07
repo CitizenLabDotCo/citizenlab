b76c18f00e8aef3d88878e33b521db4d265271dd
Amanda Anderson <anderson_amanda@mail.com>
2023-12-07 10:36:18 +0100

    Replace folder title with general text for project go back button

---
+++ front/app/containers/ProjectsShowPage/shared/header/ProjectFolderGoBackButton/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  backToFolder: {
+    id: 'app.containers.projectsShowPage.folderGoBackButton.backToFolder',
+    defaultMessage: 'Back to folder',
+  },
+});
