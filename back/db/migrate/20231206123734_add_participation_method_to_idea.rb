c52d2f631ba60eab415e33e539362325c25a5eb3
jamesspeake <james@speake.org>
2023-12-08 13:54:19 +0000

    [TAN-418] Changed migration

--- back/db/migrate/20231206123734_add_participation_method_to_idea.rb
+++ back/db/migrate/20231206123734_add_participation_method_to_idea.rb
@@ -4,15 +4,16 @@ class AddParticipationMethodToIdea < ActiveRecord::Migration[7.0]
   def change
     add_column :ideas, :participation_method, :string, after: :creation_phase_id, default: 'ideation', null: false
 
-    # Tag existing native surveys
-    Idea.where.not(creation_phase: nil).update_all(participation_method: 'native_survey')
-
-    # Add the creation_phase_id to the rest of the ideas
-    Idea.where(creation_phase: nil).each do |idea|
-      phase =
-        idea.phases.find(&:can_contain_ideas?) ||
-        TimelineService.new.current_or_last_can_contain_ideas_phase(idea.project)
-      idea.update!(creation_phase: phase, participation_method: 'ideation')
+    # Update the data of all ideas
+    Idea.all.each do |idea|
+      if idea.creation_phase_id.blank?
+        phase =
+          idea.phases.find(&:can_contain_ideas?) ||
+          TimelineService.new.current_or_last_can_contain_ideas_phase(idea.project)
+        idea.update!(creation_phase: phase, participation_method: 'ideation')
+      else
+        idea.update!(creation_phase: phase, participation_method: 'native_survey')
+      end
     end
   end
 end
