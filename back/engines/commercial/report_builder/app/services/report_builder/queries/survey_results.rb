a6233c8661440c8b72c8332cebf2695fe931308a
Alexander <alexander@citizenlab.co>
2023-12-13 13:03:20 +0100

    [TAN-536] Make phase_id mandatory

--- back/engines/commercial/report_builder/app/services/report_builder/queries/survey_results.rb
+++ back/engines/commercial/report_builder/app/services/report_builder/queries/survey_results.rb
@@ -1,6 +1,6 @@
 module ReportBuilder
   class Queries::SurveyResults
-    def run_query(phase_id: nil, **_other_props)
+    def run_query(phase_id:, **_other_props)
       phase = Phase.find(phase_id)
       SurveyResultsGeneratorService.new(phase).generate_results
     end
