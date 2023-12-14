073d0593bc929f6d96395f3d6b1035753cb0cf4c
Sébastien Hoorens <sebastien@citizenlab.co>
2023-12-14 18:50:41 +0100

    deleting more pins

--- back/app/policies/static_page_policy.rb
+++ back/app/policies/static_page_policy.rb
@@ -56,7 +56,6 @@ class StaticPagePolicy < ApplicationPolicy
       :bottom_info_section_enabled,
       { bottom_info_section_multiloc: CL2_SUPPORTED_LOCALES },
       :header_bg,
-      { pinned_admin_publication_ids: [] },
       { area_ids: [] },
       { topic_ids: [] },
       { nav_bar_item_title_multiloc: CL2_SUPPORTED_LOCALES }
