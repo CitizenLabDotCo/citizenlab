63b35936ca1743247d798eb03f51731d784cdbd7
brentguf <brentguf@users.noreply.github.com>
2023-12-16 14:20:37 +0100

    Functional + remove react-adopt

--- front/app/components/admin/PostManager/components/PostTable/AssigneeSelect.tsx
+++ front/app/components/admin/PostManager/components/PostTable/AssigneeSelect.tsx
@@ -1,48 +1,35 @@
-import React, { PureComponent } from 'react';
-import { adopt } from 'react-adopt';
+import React from 'react';
 import { Select } from 'semantic-ui-react';
 import { memoize } from 'lodash-es';
 
-// resources
-import GetUsers, { GetUsersChildProps } from 'resources/GetUsers';
-import GetAuthUser, { GetAuthUserChildProps } from 'resources/GetAuthUser';
-
 // i18n
-import { injectIntl } from 'utils/cl-intl';
-import { WrappedComponentProps } from 'react-intl';
+import { useIntl } from 'utils/cl-intl';
 import messages from '../../messages';
 
 // utils
-import { isNilOrError } from 'utils/helperUtils';
+import useUsers from 'api/users/useUsers';
+import useAuthUser from 'api/me/useAuthUser';
+import { IUser, IUsers } from 'api/users/types';
 
-interface DataProps {
-  prospectAssignees: GetUsersChildProps;
-  authUser: GetAuthUserChildProps;
-}
-
-interface InputProps {
-  projectId?: string;
+interface Props {
+  projectId: string;
   assigneeId: string | undefined;
   onAssigneeChange: (assigneeId: string | undefined) => void;
 }
 
-interface Props extends InputProps, DataProps {}
+const AssigneeSelect = ({ projectId, assigneeId, onAssigneeChange }: Props) => {
+  const { formatMessage } = useIntl();
+  const { data: authUser } = useAuthUser();
+  const { data: prospectAssignees } = useUsers({
+    can_moderate_project: projectId,
+  });
 
-class AssigneeSelect extends PureComponent<Props & WrappedComponentProps> {
-  getAssigneeOptions = memoize((prospectAssignees, authUser) => {
-    const {
-      intl: { formatMessage },
-    } = this.props;
-    let assigneeOptions = [] as {
-      value: string;
-      text: string;
-      id?: string;
-      className?: string;
-    }[];
+  if (!prospectAssignees || !authUser) return null;
 
-    if (!isNilOrError(prospectAssignees.usersList) && !isNilOrError(authUser)) {
-      const dynamicOptions = prospectAssignees.usersList
-        .filter((assignee) => assignee.id !== authUser.id)
+  const getAssigneeOptions = memoize(
+    (prospectAssignees: IUsers, authUser: IUser) => {
+      const dynamicOptions = prospectAssignees.data
+        .filter((assignee) => assignee.id !== authUser.data.id)
         .map((assignee) => ({
           value: assignee.id,
           text: formatMessage(messages.assignedTo, {
@@ -53,9 +40,9 @@ class AssigneeSelect extends PureComponent<Props & WrappedComponentProps> {
 
       // Order of assignee filter options:
       // Assigned to me > Unassigned > Assigned to X (other admins/mods)
-      assigneeOptions = [
+      return [
         {
-          value: authUser.id,
+          value: authUser.data.id,
           text: formatMessage(messages.assignedToMe),
           id: 'e2e-assignee-select-assigned-to-user',
         },
@@ -67,46 +54,26 @@ class AssigneeSelect extends PureComponent<Props & WrappedComponentProps> {
         ...dynamicOptions,
       ];
     }
-    return assigneeOptions;
-  });
+  );
 
-  onAssigneeChange = (_event, { value }: { value?: unknown }) => {
-    const { onAssigneeChange } = this.props;
-
+  const handleOnAssigneeChange = (
+    _event: React.SyntheticEvent,
+    { value }: { value?: unknown }
+  ) => {
     if (typeof value === 'string') {
       onAssigneeChange(value === 'unassigned' ? undefined : value);
     }
   };
 
-  render() {
-    const { assigneeId, prospectAssignees, authUser } = this.props;
+  return (
+    <Select
+      id={'post-row-select-assignee'}
+      options={getAssigneeOptions(prospectAssignees, authUser)}
+      onChange={handleOnAssigneeChange}
+      value={assigneeId || 'unassigned'}
+      className="fluid e2e-post-manager-post-row-assignee-select"
+    />
+  );
+};
 
-    return (
-      <Select
-        id={'post-row-select-assignee'}
-        options={this.getAssigneeOptions(prospectAssignees, authUser)}
-        onChange={this.onAssigneeChange}
-        value={assigneeId || 'unassigned'}
-        className="fluid e2e-post-manager-post-row-assignee-select"
-      />
-    );
-  }
-}
-
-const Data = adopt<DataProps, InputProps>({
-  authUser: <GetAuthUser />,
-  prospectAssignees: ({ projectId, render }) =>
-    projectId ? (
-      <GetUsers can_moderate_project={projectId}>{render}</GetUsers>
-    ) : (
-      <GetUsers can_admin>{render}</GetUsers>
-    ),
-});
-
-const AssigneeSelectWithHocs = injectIntl(AssigneeSelect);
-
-export default (inputProps: InputProps) => (
-  <Data {...inputProps}>
-    {(dataProps) => <AssigneeSelectWithHocs {...inputProps} {...dataProps} />}
-  </Data>
-);
+export default AssigneeSelect;
