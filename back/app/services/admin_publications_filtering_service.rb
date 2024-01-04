d898975d32d5e5a454d8f14a6e69a1a5bcf2275b
brentguf <brentguf@users.noreply.github.com>
2024-01-04 15:16:33 +0100

    Lint error

--- back/app/services/admin_publications_filtering_service.rb
+++ back/app/services/admin_publications_filtering_service.rb
@@ -20,7 +20,7 @@ class AdminPublicationsFilteringService
     scope.where(publication_type: Project.name)
   end
 
-  # This filter removes AdminPublications that represent folders, 
+  # This filter removes AdminPublications that represent folders,
   # which contain *only* projects which should not be visible to the current user.
   add_filter('remove_not_allowed_parents') do |scope, options|
     next scope unless ['true', true, '1'].include? options[:remove_not_allowed_parents]
