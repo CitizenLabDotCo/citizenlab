08a97ee4e9caf17f0c5249add45e07b15fd139c9
Alexander <alexander@citizenlab.co>
2024-01-09 19:10:21 +0100

    [TAN-796] Limit and sort ideas

--- back/engines/commercial/report_builder/app/services/report_builder/queries/most_reacted_ideas.rb
+++ back/engines/commercial/report_builder/app/services/report_builder/queries/most_reacted_ideas.rb
@@ -1,14 +1,24 @@
 module ReportBuilder
   class Queries::MostReactedIdeas < ReportBuilder::Queries::Base
-    def run_query(project_id:, phase_id: nil, **_other_props)
+    protected
+
+    def run_query(project_id:, number_of_ideas:, phase_id: nil, **_other_props)
       project = Project.find(project_id)
       phase = phase_id ? Phase.find(phase_id) : project.phases.first
+      ideas = phase.ideas.order(likes_count: :asc).limit(number_of_ideas)
+
       {
-        ideas: ::WebApi::V1::IdeaSerializer.new(phase.ideas, params: { current_user: @current_user }).serializable_hash,
-        project: ::WebApi::V1::ProjectSerializer.new(phase.project, params: { current_user: @current_user }).serializable_hash,
-        phase: ::WebApi::V1::PhaseSerializer.new(phase, params: { current_user: @current_user }).serializable_hash,
-        images: ::WebApi::V1::ImageSerializer.new(phase.ideas.flat_map(&:idea_images), params: { current_user: @current_user }).serializable_hash
+        ideas: serialize(ideas, ::WebApi::V1::IdeaSerializer),
+        project: serialize(project, ::WebApi::V1::ProjectSerializer),
+        phase: serialize(phase, ::WebApi::V1::PhaseSerializer),
+        images: serialize(ideas.flat_map(&:idea_images), ::WebApi::V1::ImageSerializer)
       }
+    end
+
+    private
+
+    def serialize(entity, serializer)
+      serializer.new(entity, params: { current_user: @current_user }).serializable_hash
     end
   end
 end
