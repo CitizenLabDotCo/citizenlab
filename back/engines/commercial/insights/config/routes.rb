20f617971867b351362588517d1c506201c13c09
Sébastien Hoorens <sebastien@citizenlab.co>
2024-01-10 11:39:00 +0100

    disable endpoints for insights

--- back/engines/commercial/insights/config/routes.rb
+++ back/engines/commercial/insights/config/routes.rb
@@ -3,60 +3,60 @@
 Insights::Engine.routes.draw do
   namespace :web_api, defaults: { format: :json } do
     namespace :v1 do
-      scope '/insights' do
-        resources :views do
-          resources :categories do
-            delete :index, on: :collection, action: :destroy_all
-          end
+      # scope '/insights' do
+      #   resources :views do
+      #     resources :categories do
+      #       delete :index, on: :collection, action: :destroy_all
+      #     end
 
-          resources :detected_categories, only: %i[index]
+      #     resources :detected_categories, only: %i[index]
 
-          resources :inputs, only: %i[index show] do
-            get :as_xlsx, on: :collection, action: 'index_xlsx'
+      #     resources :inputs, only: %i[index show] do
+      #       get :as_xlsx, on: :collection, action: 'index_xlsx'
 
-            resources :categories, only: %i[index destroy], controller: 'category_assignments' do
-              collection do
-                # Adds POST & DELETE endpoints at the collection level without
-                # an extra path component. It matches:
-                # - POST  .../inputs/:input_id/categories
-                # - DELETE .../inputs/:input_id/categories
-                post :index, action: :add_categories
-                delete :index, action: :delete_categories
-              end
-            end
+      #       resources :categories, only: %i[index destroy], controller: 'category_assignments' do
+      #         collection do
+      #           # Adds POST & DELETE endpoints at the collection level without
+      #           # an extra path component. It matches:
+      #           # - POST  .../inputs/:input_id/categories
+      #           # - DELETE .../inputs/:input_id/categories
+      #           post :index, action: :add_categories
+      #           delete :index, action: :delete_categories
+      #         end
+      #       end
 
-            resources :suggested_categories, only: %i[index destroy], controller: 'category_suggestions' do
-              delete :index, on: :collection, action: :delete_suggestions
-            end
+      #       resources :suggested_categories, only: %i[index destroy], controller: 'category_suggestions' do
+      #         delete :index, on: :collection, action: :delete_suggestions
+      #       end
 
-            post :processed_flag, controller: 'processed_flags', action: :create
-          end
+      #       post :processed_flag, controller: 'processed_flags', action: :create
+      #     end
 
-          resource :network, only: %i[show]
+      #     resource :network, only: %i[show]
 
-          nested do
-            scope '/tasks' do
-              resources :category_suggestions, controller: 'classification_tasks', only: %i[index create] do
-                delete :index, on: :collection, action: :destroy_tasks
-              end
+      #     nested do
+      #       scope '/tasks' do
+      #         resources :category_suggestions, controller: 'classification_tasks', only: %i[index create] do
+      #           delete :index, on: :collection, action: :destroy_tasks
+      #         end
 
-              resources :text_network_analysis, controller: 'text_network_analysis_tasks', only: %i[index]
-            end
+      #         resources :text_network_analysis, controller: 'text_network_analysis_tasks', only: %i[index]
+      #       end
 
-            scope '/batch' do
-              post :assign_categories, controller: 'batch_assignments'
-              post :remove_categories, controller: 'batch_assignments'
-            end
+      #       scope '/batch' do
+      #         post :assign_categories, controller: 'batch_assignments'
+      #         post :remove_categories, controller: 'batch_assignments'
+      #       end
 
-            scope '/stats' do
-              get :inputs_count, controller: 'stats_inputs', action: :inputs_count
-              scope '/tasks' do
-                get :category_suggestions, controller: 'classification_tasks', action: :count
-              end
-            end
-          end
-        end
-      end
+      #       scope '/stats' do
+      #         get :inputs_count, controller: 'stats_inputs', action: :inputs_count
+      #         scope '/tasks' do
+      #           get :category_suggestions, controller: 'classification_tasks', action: :count
+      #         end
+      #       end
+      #     end
+      #   end
+      # end
     end
   end
 end
