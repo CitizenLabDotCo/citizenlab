7588b0cf2b9f88a22a6711e0c3dcb1bc680fe524
Alexander <alexander@citizenlab.co>
2024-01-11 19:42:40 +0100

    [TAN-796] Fix BE specs

--- back/engines/commercial/report_builder/spec/services/report_builder/query_repository_spec.rb
+++ back/engines/commercial/report_builder/spec/services/report_builder/query_repository_spec.rb
@@ -3,15 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe ReportBuilder::QueryRepository do
-  let(:query_repository) { described_class.new(nil) }
+  let(:query_repository) { described_class.new(build(:user)) }
   let(:props) { {} }
 
   describe '#data_by_graph' do
     context 'when graph_resolved_name is valid' do
-      let(:graph_resolved_name) { 'GenderWidget' }
+      let(:graph_resolved_name) { 'VisitorsWidget' }
 
       it 'runs query and returns results' do
-        expect(query_repository.data_by_graph(graph_resolved_name, props)).to eq([])
+        expect(query_repository.data_by_graph(graph_resolved_name, props)).to be_present
       end
     end
 
