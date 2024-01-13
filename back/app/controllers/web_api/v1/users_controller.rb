77306531c263af139541fdd4dc47df0b31360085
Adrien Dessy <adrien@citizenlab.co>
2024-01-13 16:43:00 +0100

    [TAN-668] Rework parameter extraction for UsersController#update

--- back/app/controllers/web_api/v1/users_controller.rb
+++ back/app/controllers/web_api/v1/users_controller.rb
@@ -152,25 +152,11 @@ class WebApi::V1::UsersController < ApplicationController
   end
 
   def update
-    user_params = permitted_attributes(@user)
+    @user.assign_attributes(update_params)
+    remove_image_if_requested!(@user, update_params, :avatar)
+    authorize(@user)
 
-    # Even if the feature is not activated, we still want to allow the user to remove
-    # their avatar.
-    if !app_configuration.feature_activated?('user_avatars') && !user_params[:avatar].nil?
-      user_params.delete(:avatar)
-    end
-
-    user_params[:custom_field_values] = @user.custom_field_values.merge(user_params[:custom_field_values] || {})
-    user_params[:onboarding] = @user.onboarding.merge(user_params[:onboarding] || {})
-    user_params = user_params.to_h
-
-    CustomFieldService.new.cleanup_custom_field_values! user_params[:custom_field_values]
-    @user.assign_attributes user_params
-
-    remove_image_if_requested!(@user, user_params, :avatar)
-
-    authorize @user
-    save_params = user_params.key?(:custom_field_values) ? { context: :form_submission } : {}
+    save_params = update_params.key?(:custom_field_values) ? { context: :form_submission } : {}
     if @user.save save_params
       SideFxUserService.new.after_update(@user, current_user)
       render json: WebApi::V1::UserSerializer.new(
@@ -296,6 +282,20 @@ class WebApi::V1::UsersController < ApplicationController
 
   def view_private_attributes?
     Pundit.policy!(current_user, (@user || User)).view_private_attributes?
+  end
+
+  def update_params
+    @update_params ||= permitted_attributes(@user).tap do |attrs|
+      attrs[:onboarding] = @user.onboarding.merge(attrs[:onboarding].to_h)
+      attrs[:custom_field_values] = @user.custom_field_values.merge(attrs[:custom_field_values].to_h)
+      CustomFieldService.new.cleanup_custom_field_values!(attrs[:custom_field_values])
+
+      # Even if the feature is not activated, we still want to allow the user to remove
+      # their avatar.
+      if !app_configuration.feature_activated?('user_avatars') && !attrs[:avatar].nil?
+        attrs.delete(:avatar)
+      end
+    end.permit!
   end
 
   def app_configuration
