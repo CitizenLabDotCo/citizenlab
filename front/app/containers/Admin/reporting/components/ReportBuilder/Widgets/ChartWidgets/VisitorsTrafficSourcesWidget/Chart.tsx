6415abcd3a6fc2c4a328682b35ea74c9e336c6c1
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-01-15 12:01:32 +0100

    Traffic sources: center and put legend below on small screens

---
+++ front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ChartWidgets/VisitorsTrafficSourcesWidget/Chart.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+// components
+import PieChart from 'components/admin/Graphs/PieChart';
+import renderTooltip from 'components/admin/GraphCards/VisitorsTrafficSourcesCard/renderTooltip';
+
+// typings
+import { PieRow } from 'components/admin/GraphCards/VisitorsTrafficSourcesCard/useVisitorReferrerTypes/typings';
+import { LegendItem } from 'components/admin/Graphs/_components/Legend/typings';
+import { Layout } from 'components/admin/GraphCards/typings';
+
+interface Props {
+  pieData: PieRow[];
+  innerRef?: React.RefObject<any>;
+  layout?: Layout;
+}
+
+const Chart = ({ pieData, innerRef, layout = 'wide' }: Props) => {
+  const [hoverIndex, setHoverIndex] = useState<number | undefined>();
+
+  const onMouseOver = ({ rowIndex }) => {
+    setHoverIndex(rowIndex);
+  };
+
+  const onMouseOut = () => {
+    setHoverIndex(undefined);
+  };
+
+  const legend = pieData.map(
+    (row): LegendItem => ({
+      icon: 'circle',
+      color: row.color,
+      label: `${row.name} (${row.percentage}%)`,
+    })
+  );
+
+  return (
+    <PieChart
+      width={layout === 'narrow' ? '100%' : 164}
+      height={195}
+      data={pieData}
+      mapping={{
+        angle: 'value',
+        name: 'name',
+        opacity: ({ rowIndex }) => {
+          if (hoverIndex === undefined) return 1;
+          return hoverIndex === rowIndex ? 1 : 0.3;
+        },
+      }}
+      pie={{
+        startAngle: 0,
+        endAngle: 360,
+        outerRadius: 60,
+      }}
+      tooltip={renderTooltip()}
+      legend={{
+        items: legend,
+        marginTop: layout === 'narrow' ? 0 : undefined,
+        marginLeft: layout === 'narrow' ? 10 : 50,
+        maintainGraphSize: layout !== 'narrow',
+        position: layout === 'narrow' ? 'bottom-center' : 'right-center',
+      }}
+      innerRef={innerRef}
+      onMouseOver={onMouseOver}
+      onMouseOut={onMouseOut}
+    />
+  );
+};
+
+export default Chart;
