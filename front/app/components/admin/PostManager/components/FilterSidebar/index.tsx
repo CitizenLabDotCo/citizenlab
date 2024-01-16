47cd2b692b2162058bf0547b37a2c4235a1672e8
brentguf <brentguf@users.noreply.github.com>
2024-01-16 11:21:37 +0100

    Improve comment 2

--- front/app/components/admin/PostManager/components/FilterSidebar/index.tsx
+++ front/app/components/admin/PostManager/components/FilterSidebar/index.tsx
@@ -81,12 +81,12 @@ const FilterSidebar = ({
     if (type === 'ProjectIdeas' && typeof projectId === 'string') {
       return `/admin/projects/${projectId}/settings/tags`;
     } else if (isAdmin({ data: authUser.data })) {
-      // Don't show the link to the platform-wide tag manager if the user is not an admin.
-      // (i.e. project/folder managers that have access to the general input manager at /admin/ideas,
-      // but just for their folders/projects).
       // For admins, both for /admin/ideas (type 'AllIdeas') and /admin/initiatives, we show the link to the platform-wide tag manager
       return '/admin/settings/topics';
     } else {
+      // Don't show the link to the platform-wide tag manager if the user is not an admin.
+      // (i.e. project/folder managers that have access to the general input manager at /admin/ideas,
+      // but just for their folders/projects).
       return null;
     }
   };
