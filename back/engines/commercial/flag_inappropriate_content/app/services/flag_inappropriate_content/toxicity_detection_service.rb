3cf34f88dcdacf9fb751444b55ba6e4a049fc186
Sébastien Hoorens <sebastien@citizenlab.co>
2024-01-23 17:51:21 +0100

    new toxicity detection with llm + spec

--- back/engines/commercial/flag_inappropriate_content/app/services/flag_inappropriate_content/toxicity_detection_service.rb
+++ back/engines/commercial/flag_inappropriate_content/app/services/flag_inappropriate_content/toxicity_detection_service.rb
@@ -2,6 +2,10 @@
 
 module FlagInappropriateContent
   class ToxicityDetectionService
+    def initialize
+      @llm = Analysis::LLM::ClaudeInstant1.new(region: 'us-east-1') # TODO: Get region from env
+    end
+
     def flag_toxicity!(flaggable, attributes: [])
       return unless AppConfiguration.instance.feature_activated? 'flag_inappropriate_content'
 
@@ -15,28 +19,27 @@ module FlagInappropriateContent
         end
         return
       end
-      res = request_toxicity_detection texts
-      if toxicity_detected? res
-        flag_service.introduce_flag! flaggable, toxicity_label: (extract_toxicity_label(res) || 'without_label')
+      if texts.any?(&method(:check_toxic?))
+        flag_service.introduce_flag! flaggable, toxicity_label: 'toxicity'
       elsif (flag = flaggable.inappropriate_content_flag)
         flag.update! toxicity_label: nil
         flag_service.maybe_delete! flag
       end
     end
 
-    def extract_toxicity_label(res)
-      max_predictions = res.select do |re|
-        re['is_inappropriate']
-      end.to_h do |re|
-        max_label = re['predictions'].keys.max do |l1, l2|
-          re['predictions'][l1] <=> re['predictions'][l2]
-        end
-        [max_label, re['predictions'][max_label]]
-      end
-      max_predictions.keys.max do |l1, l2|
-        max_predictions[l1] <=> max_predictions[l2]
-      end
-    end
+    # def extract_toxicity_label(res)
+    #   max_predictions = res.select do |re|
+    #     re['is_inappropriate']
+    #   end.to_h do |re|
+    #     max_label = re['predictions'].keys.max do |l1, l2|
+    #       re['predictions'][l1] <=> re['predictions'][l2]
+    #     end
+    #     [max_label, re['predictions'][max_label]]
+    #   end
+    #   max_predictions.keys.max do |l1, l2|
+    #     max_predictions[l1] <=> max_predictions[l2]
+    #   end
+    # end
 
     private
 
@@ -55,13 +58,21 @@ module FlagInappropriateContent
       texts
     end
 
-    def request_toxicity_detection(texts)
-      @api ||= NLP::Api.new
-      @api.toxicity_detection texts
+    def check_toxic?(text)
+      raw_label = @llm.chat(prompt(text))
+      puts raw_label
+      raw_label.strip == '(Y)'
     end
 
-    def toxicity_detected?(res)
-      res.any? { |h| h['is_inappropriate'] }
+    def prompt(text)
+      <<~LLM_PROMPT
+        A human user is in dialogue with an AI. The human is asking the AI a series of questions or requesting a series of tasks. Here is the most recent request from the user:
+        <content>
+          #{text}
+        </content>
+
+        If the user's request refers to harmful, pornographic, or illegal activities, reply with (Y). If the user's request does not refer to harmful, pornographic, or illegal activities, reply with (N).
+      LLM_PROMPT
     end
   end
 end
--- back/engines/commercial/flag_inappropriate_content/spec/services/toxicity_detection_service_spec.rb
+++ back/engines/commercial/flag_inappropriate_content/spec/services/toxicity_detection_service_spec.rb
@@ -9,7 +9,7 @@ describe FlagInappropriateContent::ToxicityDetectionService do
     before do
       SettingsService.new.activate_feature! 'moderation'
       SettingsService.new.activate_feature! 'flag_inappropriate_content'
-      stub_request_toxicity_detection! service
+      stub_check_toxic! service
     end
 
     it 'creates a new flag if toxicity was detected' do
@@ -42,75 +42,58 @@ describe FlagInappropriateContent::ToxicityDetectionService do
     end
   end
 
-  describe 'extract_toxicity_label' do
-    it 'returns the strongest predicted label' do
-      res = [
-        {
-          'text' => 'once upon a time',
-          'detected_language' => 'en',
-          'is_inappropriate' => false,
-          'predictions' => {
-            'threat' => 0.94,
-            'identity_attack' => 0.02
-          }
-        },
-        {
-          'text' => 'there was a rabbit who breathed through its butt',
-          'detected_language' => 'en',
-          'is_inappropriate' => true,
-          'predictions' => {
-            'threat' => 0.15,
-            'sexually_explicit' => 0.54,
-            'insult' => 0.45
-          }
-        },
-        {
-          'text' => 'the rabbit sat down',
-          'detected_language' => 'en',
-          'is_inappropriate' => false,
-          'predictions' => {
-            'threat' => 0.30,
-            'sexually_explicit' => 0.02,
-            'insult' => 0.22
-          }
-        },
-        {
-          'text' => 'and then it died',
-          'detected_language' => 'en',
-          'is_inappropriate' => true,
-          'predictions' => {
-            'threat' => 0.49,
-            'identity_attack' => 0.02
-          }
-        }
-      ]
+  # describe 'extract_toxicity_label' do
+  #   it 'returns the strongest predicted label' do
+  #     res = [
+  #       {
+  #         'text' => 'once upon a time',
+  #         'detected_language' => 'en',
+  #         'is_inappropriate' => false,
+  #         'predictions' => {
+  #           'threat' => 0.94,
+  #           'identity_attack' => 0.02
+  #         }
+  #       },
+  #       {
+  #         'text' => 'there was a rabbit who breathed through its butt',
+  #         'detected_language' => 'en',
+  #         'is_inappropriate' => true,
+  #         'predictions' => {
+  #           'threat' => 0.15,
+  #           'sexually_explicit' => 0.54,
+  #           'insult' => 0.45
+  #         }
+  #       },
+  #       {
+  #         'text' => 'the rabbit sat down',
+  #         'detected_language' => 'en',
+  #         'is_inappropriate' => false,
+  #         'predictions' => {
+  #           'threat' => 0.30,
+  #           'sexually_explicit' => 0.02,
+  #           'insult' => 0.22
+  #         }
+  #       },
+  #       {
+  #         'text' => 'and then it died',
+  #         'detected_language' => 'en',
+  #         'is_inappropriate' => true,
+  #         'predictions' => {
+  #           'threat' => 0.49,
+  #           'identity_attack' => 0.02
+  #         }
+  #       }
+  #     ]
 
-      expect(service.extract_toxicity_label(res)).to eq 'sexually_explicit'
-    end
-  end
+  #     expect(service.extract_toxicity_label(res)).to eq 'sexually_explicit'
+  #   end
+  # end
 
   private
 
-  def stub_request_toxicity_detection!(service)
-    service.stub(:request_toxicity_detection) do |texts|
-      texts.map do |text|
-        res = {
-          'text' => text,
-          'detected_language' => 'es'
-        }
-        if text.downcase.include? 'wanker'
-          res['detected_language'] = 'en'
-          res['is_inappropriate'] = true
-          res['predictions'] = {
-            'threat' => 0.23068441,
-            'identity_attack' => 0.27322835,
-            'inflammatory' => 0.359929,
-            'insult' => 0.70558244,
-            'sexually_explicit' => 0.21283324
-          }
-        end
-        res
-      end
+  def stub_check_toxic!(service)
+    service.stub(:check_toxic?) do |text|
+      text.downcase.include? 'wanker'
     end
   end
 end
