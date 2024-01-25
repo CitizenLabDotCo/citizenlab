f78da318fa55688f01cd7e4f177ee6c7161d00b2
EdwinKato <katedwin88@gmail.com>
2024-01-25 12:52:36 +0300

    Add function to filter out invisble elements

--- front/app/components/Form/Components/Controls/visibilityUtils.ts
+++ front/app/components/Form/Components/Controls/visibilityUtils.ts
@@ -166,3 +166,24 @@ export const isVisible = (
 
   return true;
 };
+
+export const getVisibleElementsOnPage = (
+  page: PageType,
+  data: any
+): ExtendedUISchema[] => {
+  const otherFieldValues = page.elements
+    .filter((element) => element.options?.otherField)
+    .map((element) => {
+      const parentFieldKey = element.scope.split('/').pop();
+      return { otherFieldKey: element.options?.otherField, parentFieldKey };
+    });
+
+  return page.elements.filter((element) => {
+    const key = element.scope.split('/').pop();
+    const field = otherFieldValues.find((item) => item?.otherFieldKey === key);
+
+    return (
+      !field || (field.parentFieldKey && data[field.parentFieldKey] === 'other')
+    );
+  });
+};
