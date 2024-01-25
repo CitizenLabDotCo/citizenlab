683482bbb8796a8a62a2794b512a22068c18d62a
Simon Tharby <simontharby@gmail.com>
2024-01-25 11:46:42 +0000

    [TAN-943] Add migration to engine and copy into main db/migrate

---
+++ back/db/migrate/20240125114347_add_type_and_url_to_maps_layers.custom_maps.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# This migration comes from custom_maps (originally 20240125113316)
+class AddTypeAndUrlToMapsLayers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :maps_layers, :type, :string, null: false, default: 'geojson'
+    add_column :maps_layers, :url, :string
+  end
+end
---
+++ back/engines/commercial/custom_maps/db/migrate/20240125113316_add_type_and_url_to_maps_layers.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class AddTypeAndUrlToMapsLayers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :maps_layers, :type, :string, null: false, default: 'geojson'
+    add_column :maps_layers, :url, :string
+  end
+end
