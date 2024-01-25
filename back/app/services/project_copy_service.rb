579f65297f1aade644b02498979bafebb6725325
Simon Tharby <simontharby@gmail.com>
2024-01-25 14:13:22 +0000

    [TAN-943] Add new fields to ProjectCopyService yml_maps_layers

--- back/app/services/project_copy_service.rb
+++ back/app/services/project_copy_service.rb
@@ -384,6 +384,8 @@ class ProjectCopyService < TemplateService
     (@project.map_config&.layers || []).map do |layer|
       yml_layer = {
         'map_config_ref' => lookup_ref(layer.map_config_id, :maps_map_config),
+        'layer_type' => layer.layer_type,
+        'url' => layer.url,
         'title_multiloc' => layer.title_multiloc,
         'geojson' => layer.geojson,
         'default_enabled' => layer.default_enabled,
