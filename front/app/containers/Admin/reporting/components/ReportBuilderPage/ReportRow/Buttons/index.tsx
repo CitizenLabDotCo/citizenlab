a01c68b410e858b51ae26ba7ba2b81c66b1b6c56
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-01-25 17:21:17 +0100

    Add reporting to PM admin side menu

--- front/app/containers/Admin/reporting/components/ReportBuilderPage/ReportRow/Buttons/index.tsx
+++ front/app/containers/Admin/reporting/components/ReportBuilderPage/ReportRow/Buttons/index.tsx
@@ -37,7 +37,7 @@ const Buttons = ({
         buttonStyle="white"
         onClick={onDelete}
         processing={isLoading}
-        disabled={isLoading}
+        disabled={isLoading || !canEdit}
         iconSize="18px"
       >
         {formatMessage(messages.delete)}
