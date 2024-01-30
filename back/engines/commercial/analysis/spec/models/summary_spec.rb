f68c4357c1b63f2d9fcd8987b3ef665b252acda8
Sébastien Hoorens <sebastien@citizenlab.co>
2024-01-30 17:39:06 +0100

    model spec for missing_inputs_count

--- back/engines/commercial/analysis/spec/models/summary_spec.rb
+++ back/engines/commercial/analysis/spec/models/summary_spec.rb
@@ -10,4 +10,35 @@ RSpec.describe Analysis::Summary do
   describe 'Default factory' do
     it { is_expected.to be_valid }
   end
+
+  describe 'missing_inputs_count' do
+    subject { summary.missing_inputs_count }
+
+    let(:project) { create(:project_with_active_ideation_phase) }
+    let(:inputs) { create_list(:idea, 2, project: project) }
+    let(:analysis) { create(:analysis, project: project) }
+    let(:insight) { build(:insight, inputs_ids: inputs.map(&:id), analysis: analysis) }
+    let(:summary) { create(:summary, insight: insight) }
+
+    context 'when the inputs didn\'t change' do
+      it { is_expected.to eq 0 }
+    end
+
+    context 'when inputs were added' do
+      before do
+        inputs.first.destroy!
+        create_list(:idea, 2, project: project)
+      end
+
+      it { is_expected.to eq 2 }
+    end
+
+    context 'when inputs were deleted' do
+      before do
+        inputs.first.destroy!
+      end
+
+      it { is_expected.to eq 0 }
+    end
+  end
 end
