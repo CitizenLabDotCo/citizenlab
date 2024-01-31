fdd92360327f84b308d9a89698e68d65c52701e5
Amanda Anderson <anderson_amanda@mail.com>
2024-01-31 16:27:03 +0100

    Remove deadcode

--- front/app/components/EsriMap/utils.tsx
+++ front/app/components/EsriMap/utils.tsx
@@ -34,25 +34,3 @@ export const getMapPinSymbol = (color?: string) => {
     path: 'M16,3.5c-4.142,0-7.5,3.358-7.5,7.5c0,4.143,7.5,18.121,7.5,18.121S23.5,15.143,23.5,11C23.5,6.858,20.143,3.5,16,3.5z M16,14.584c-1.979,0-3.584-1.604-3.584-3.584S14.021,7.416,16,7.416S19.584,9.021,19.584,11S17.979,14.584,16,14.584z',
   });
 };
-
-// Fecth SVG from the maki-icons endpoint and apply color to it. Return a URL to the generated blob.
-export const generateSvgIconUrl = async (makiSymbol: string, color: string) => {
-  const response = await fetch(
-    `https://unpkg.com/@icon/maki-icons/icons/${makiSymbol.toLowerCase()}.svg`
-  );
-
-  const svg = await response.text();
-
-  // Insert the correct color into the fetched SVG
-  const pathElementIndex = svg.indexOf('d='); // Find the start of the svg path data
-  const makiIconSvg = `${svg.slice(
-    0,
-    pathElementIndex
-  )} fill="${color}" ${svg.slice(pathElementIndex)}`;
-
-  // Create a blob from the SVG
-  const blob = new Blob([makiIconSvg], { type: 'image/svg+xml' });
-
-  // Return URL to newly created blob
-  return URL.createObjectURL(blob);
-};
