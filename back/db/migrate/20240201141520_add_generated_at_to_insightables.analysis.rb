59308c8c0cc16acc0c61d5ccc2dbc3a064167abb
Sébastien Hoorens <sebastien@citizenlab.co>
2024-02-01 15:30:29 +0100

    migration to add generated_at to insightables

---
+++ back/db/migrate/20240201141520_add_generated_at_to_insightables.analysis.rb
@@ -0,0 +1,9 @@
+# This migration comes from analytics (originally 20240201141520)
+class AddGeneratedAtToInsightables < ActiveRecord::Migration[7.0]
+  def change
+    %i[analysis_summaries analysis_questions].each do |table|
+      add_column table, :generated_at, :timestamp
+      ActiveRecord::Base.connection.execute "UPDATE #{table} SET generated_at = created_at"
+    end
+  end
+end
---
+++ back/engines/commercial/analysis/db/migrate/20240201141520_add_generated_at_to_insightables.rb
@@ -0,0 +1,8 @@
+class AddGeneratedAtToInsightables < ActiveRecord::Migration[7.0]
+  def change
+    %i[analysis_summaries analysis_questions].each do |table|
+      add_column table, :generated_at, :timestamp
+      ActiveRecord::Base.connection.execute "UPDATE #{table} SET generated_at = created_at"
+    end
+  end
+end
