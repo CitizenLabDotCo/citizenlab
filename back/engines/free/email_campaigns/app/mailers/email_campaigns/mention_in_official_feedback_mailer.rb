7e0669c5c74f5a0a44cafce1af76bba87100ed39
Alexander <alexander@citizenlab.co>
2024-02-01 11:17:16 +0100

    Add comments about absent event attributes

--- back/engines/free/email_campaigns/app/mailers/email_campaigns/mention_in_official_feedback_mailer.rb
+++ back/engines/free/email_campaigns/app/mailers/email_campaigns/mention_in_official_feedback_mailer.rb
@@ -29,7 +29,9 @@ module EmailCampaigns
     end
 
     def preheader
-      format_message('preheader', values: { commentAuthor: event['initiating_user_first_name'] })
+      # initiating_user_first_name is never present.
+      # TODO: fix. We previously removed this field from the event https://github.com/CitizenLabDotCo/citizenlab/commit/72a778f#diff-f9b809a7dcedf9fd13177f3b9b05ac24ddde8f5f36d197d65e256c20b22ae62aL25
+      format_message('preheader', values: { commentAuthor: event.initiating_user_first_name })
     end
   end
 end
--- back/engines/free/email_campaigns/app/mailers/email_campaigns/project_published_mailer.rb
+++ back/engines/free/email_campaigns/app/mailers/email_campaigns/project_published_mailer.rb
@@ -16,6 +16,7 @@ module EmailCampaigns
       format_message(
         'header_message',
         values: {
+          # TODO: fix. We previously removed this field from the event https://github.com/CitizenLabDotCo/citizenlab/pull/5556/files#diff-ade4ca4ef24ba6dab628047c521bfc1addf2e0255d401c931124ec7fea5a07f0L78
           ideaTitle: localize_for_recipient(event.post_title_multiloc),
           organizationName: organization_name
         }
