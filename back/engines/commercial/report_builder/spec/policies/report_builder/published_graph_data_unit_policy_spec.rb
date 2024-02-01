5e88b63c451db31e9bbb6e302cf0f14adcd3c4ca
Alexander <alexander@citizenlab.co>
2024-02-01 18:02:00 +0100

    Speed up and shorten published data policy spec

--- back/engines/commercial/report_builder/spec/policies/report_builder/published_graph_data_unit_policy_spec.rb
+++ back/engines/commercial/report_builder/spec/policies/report_builder/published_graph_data_unit_policy_spec.rb
@@ -5,6 +5,26 @@ require 'rails_helper'
 RSpec.describe ReportBuilder::PublishedGraphDataUnitPolicy do
   subject { described_class.new(user, data_unit) }
 
+  shared_examples 'permits if phase started' do
+    context 'phase started' do
+      let_it_be(:phase) { build(:phase, project: project, start_at: 1.day.ago) }
+      let_it_be(:report) { build(:report, phase: phase) }
+      let_it_be(:data_unit) { build(:published_graph_data_unit, report: report) }
+
+      it { is_expected.to permit(:published) }
+    end
+  end
+
+  shared_examples 'does not permit if phase not started' do
+    context 'phase not started' do
+      let_it_be(:phase) { build(:phase, project: project, start_at: 1.day.from_now) }
+      let_it_be(:report) { build(:report, phase: phase) }
+      let_it_be(:data_unit) { build(:published_graph_data_unit, report: report) }
+
+      it { is_expected.not_to permit(:published) }
+    end
+  end
+
   let_it_be(:project) { create(:project) }
   let_it_be(:phase) { create(:phase, project: project) }
   let_it_be(:report) { create(:report, phase: phase) }
@@ -25,23 +45,8 @@ RSpec.describe ReportBuilder::PublishedGraphDataUnitPolicy do
     context 'when user cannot moderate project' do
       let_it_be(:user) { build(:project_moderator) }
 
-      context 'phase not started' do
-        let_it_be(:project) { create(:project) }
-        let_it_be(:phase) { create(:phase, project: project, start_at: 1.day.from_now) }
-        let_it_be(:report) { create(:report, phase: phase) }
-        let_it_be(:data_unit) { create(:published_graph_data_unit, report: report) }
-
-        it { is_expected.not_to permit(:published) }
-      end
-
-      context 'phase started' do
-        let_it_be(:project) { create(:project) }
-        let_it_be(:phase) { create(:phase, project: project, start_at: 1.day.ago) }
-        let_it_be(:report) { create(:report, phase: phase) }
-        let_it_be(:data_unit) { create(:published_graph_data_unit, report: report) }
-
-        it { is_expected.to permit(:published) }
-      end
+      include_examples 'permits if phase started'
+      include_examples 'does not permit if phase not started'
     end
   end
 
@@ -61,23 +66,8 @@ RSpec.describe ReportBuilder::PublishedGraphDataUnitPolicy do
         allow(PhasePolicy).to receive(:new).and_return(instance_double(PhasePolicy, show?: true))
       end
 
-      context 'phase not started' do
-        let_it_be(:project) { create(:project) }
-        let_it_be(:phase) { create(:phase, project: project, start_at: 1.day.from_now) }
-        let_it_be(:report) { create(:report, phase: phase) }
-        let_it_be(:data_unit) { create(:published_graph_data_unit, report: report) }
-
-        it { is_expected.not_to permit(:published) }
-      end
-
-      context 'phase started' do
-        let_it_be(:project) { create(:project) }
-        let_it_be(:phase) { create(:phase, project: project, start_at: 1.day.ago) }
-        let_it_be(:report) { create(:report, phase: phase) }
-        let_it_be(:data_unit) { create(:published_graph_data_unit, report: report) }
-
-        it { is_expected.to permit(:published) }
-      end
+      include_examples 'permits if phase started'
+      include_examples 'does not permit if phase not started'
     end
   end
 
@@ -97,23 +87,8 @@ RSpec.describe ReportBuilder::PublishedGraphDataUnitPolicy do
         allow(PhasePolicy).to receive(:new).and_return(instance_double(PhasePolicy, show?: true))
       end
 
-      context 'phase not started' do
-        let_it_be(:project) { create(:project) }
-        let_it_be(:phase) { create(:phase, project: project, start_at: 1.day.from_now) }
-        let_it_be(:report) { create(:report, phase: phase) }
-        let_it_be(:data_unit) { create(:published_graph_data_unit, report: report) }
-
-        it { is_expected.not_to permit(:published) }
-      end
-
-      context 'phase started' do
-        let_it_be(:project) { create(:project) }
-        let_it_be(:phase) { create(:phase, project: project, start_at: 1.day.ago) }
-        let_it_be(:report) { create(:report, phase: phase) }
-        let_it_be(:data_unit) { create(:published_graph_data_unit, report: report) }
-
-        it { is_expected.to permit(:published) }
-      end
+      include_examples 'permits if phase started'
+      include_examples 'does not permit if phase not started'
     end
   end
 end
