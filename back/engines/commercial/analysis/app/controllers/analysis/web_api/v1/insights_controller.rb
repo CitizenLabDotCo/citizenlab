c19a0aeddadd7924695e3afd6ae2175d03c15a8a
Sébastien Hoorens <sebastien@citizenlab.co>
2024-02-01 13:19:14 +0100

    a bit more performant

--- back/engines/commercial/analysis/app/controllers/analysis/web_api/v1/insights_controller.rb
+++ back/engines/commercial/analysis/app/controllers/analysis/web_api/v1/insights_controller.rb
@@ -11,7 +11,7 @@ module Analysis
         def index
           insights = @analysis.insights
             .order(created_at: :desc)
-            .includes(insightable: [:background_task, :insight])
+            .includes(insightable: [:background_task, insight: [analysis: [phase: [:ideas], project: [:ideas]]]])
 
           if params[:bookmarked].present?
             insights = insights.where(bookmarked: true)
