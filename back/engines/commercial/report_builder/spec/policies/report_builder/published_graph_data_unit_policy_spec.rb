7bc6069fc830ded68e00b413a666f944f5475803
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-02-01 14:10:49 +0000

    Add tests for published data for visitors

--- back/engines/commercial/report_builder/spec/policies/report_builder/published_graph_data_unit_policy_spec.rb
+++ back/engines/commercial/report_builder/spec/policies/report_builder/published_graph_data_unit_policy_spec.rb
@@ -48,8 +48,6 @@ RSpec.describe ReportBuilder::PublishedGraphDataUnitPolicy do
   context 'when user is normal user' do
     let_it_be(:user) { build(:user) }
 
-    it { is_expected.to permit(:published) }
-
     context 'when user is not project member' do
       before do
         allow(PhasePolicy).to receive(:new).and_return(instance_double(PhasePolicy, show?: false))
@@ -86,21 +84,35 @@ RSpec.describe ReportBuilder::PublishedGraphDataUnitPolicy do
   context 'when user is visitor' do
     let_it_be(:user) { nil }
 
-    it { is_expected.to permit(:published) }
-  end
+    context 'when user is not project member' do
+      before do
+        allow(PhasePolicy).to receive(:new).and_return(instance_double(PhasePolicy, show?: false))
+      end
 
-  context 'when phase has not started' do
-    before { allow(phase).to receive(:started?).and_return(false) }
+      it { is_expected.not_to permit(:published) }
+    end
 
-    it 'does not permit :published for any user' do
-      [
-        build(:admin),
-        build(:project_moderator, projects: [project]),
-        build(:user),
-        nil
-      ].each do |user|
-        subject = described_class.new(user, data_unit)
-        expect(subject).not_to permit(:published)
+    context 'when user is project member' do
+      before do
+        allow(PhasePolicy).to receive(:new).and_return(instance_double(PhasePolicy, show?: true))
+      end
+
+      context 'phase not started' do
+        let_it_be(:project) { create(:project) }
+        let_it_be(:phase) { create(:phase, project: project, start_at: 1.day.from_now) }
+        let_it_be(:report) { create(:report, phase: phase) }
+        let_it_be(:data_unit) { create(:published_graph_data_unit, report: report) }
+
+        it { is_expected.not_to permit(:published) }
+      end
+
+      context 'phase started' do
+        let_it_be(:project) { create(:project) }
+        let_it_be(:phase) { create(:phase, project: project, start_at: 1.day.ago) }
+        let_it_be(:report) { create(:report, phase: phase) }
+        let_it_be(:data_unit) { create(:published_graph_data_unit, report: report) }
+
+        it { is_expected.to permit(:published) }
       end
     end
   end
