d974aaa48433aa296c6ae68aa1cb128a859b6fcb
Simon Tharby <simontharby@gmail.com>
2024-02-06 09:23:59 +0000

    [TAN-975] Fix map_configs factory

--- back/engines/commercial/custom_maps/spec/models/map_config_spec.rb
+++ back/engines/commercial/custom_maps/spec/models/map_config_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe CustomMaps::MapConfig do
   describe 'Default factory' do
     it 'is valid' do
       map_config = build(:map_config)
-      map_config.project.save  # otherwise map_config.project_id is nil => map_config is invalid
+      map_config.mappable.save  # otherwise map_config.mappable_id is nil => map_config is invalid
       expect(map_config).to be_valid
     end
   end
