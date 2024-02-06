8bd1814201c33c21d15e0889e6fb950587f52ffc
EdwinKato <katedwin88@gmail.com>
2024-02-06 08:53:54 +0300

    Add tests for useAddCustomFieldOptionImage hook

---
+++ front/app/api/content_field_option_images/__mocks__/customFieldOptionImage.ts
@@ -0,0 +1,8 @@
+export const customFieldOptionImageData = {
+  id: 'f0036c9f-4dfd-4b3a-a9c2-cb26e0120268',
+  type: 'image',
+  attributes: {
+    image_url: 'test_image_url',
+    code: 'test',
+  },
+};
