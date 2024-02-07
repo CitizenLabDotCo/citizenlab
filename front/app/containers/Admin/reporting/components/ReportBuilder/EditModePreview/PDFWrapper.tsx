30e3966b2c7447fa239eace2f515242eca1567a8
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-02-07 13:30:02 +0000

    Make pdf preview at least a4 height

--- front/app/containers/Admin/reporting/components/ReportBuilder/EditModePreview/PDFWrapper.tsx
+++ front/app/containers/Admin/reporting/components/ReportBuilder/EditModePreview/PDFWrapper.tsx
@@ -4,11 +4,11 @@ import React from 'react';
 import { Box } from '@citizenlab/cl2-component-library';
 
 // constants
-import { A4_WIDTH } from 'containers/Admin/reporting/constants';
+import { A4_WIDTH, A4_HEIGHT } from 'containers/Admin/reporting/constants';
 
 const PDFWrapper = ({ children }) => {
   return (
-    <Box width={A4_WIDTH}>
+    <Box width={A4_WIDTH} minHeight={A4_HEIGHT}>
       <Box background="white" px="30px" py="30px" width="100%" height="100%">
         {children}
       </Box>
