ddcad4923739acebaa65cfbf46848f249f7c6e1f
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-02-08 16:47:51 +0000

    Undo title color

--- front/app/components/IdeaCard/index.tsx
+++ front/app/components/IdeaCard/index.tsx
@@ -180,7 +180,6 @@ const IdeaCard = ({
             mt="4px"
             mb="16px"
             className="e2e-idea-card-title"
-            color="red600"
           >
             {ideaTitle}
           </Title>
