a57ff0e9767db567481ba6610df3fda46dd38f0f
Simon Tharby <simontharby@gmail.com>
2024-02-09 09:32:14 +0000

    [TAN-975] Remove commented out line

--- back/engines/commercial/custom_maps/app/serializers/custom_maps/web_api/v1/project_serializer.rb
+++ back/engines/commercial/custom_maps/app/serializers/custom_maps/web_api/v1/project_serializer.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 # This serializer inherits from WebApi::V1::ProjectSerializer, as the polymorphic association in the
-# MapConfigSerializer belongs_to: mappable association wants a ProjectSerializer in this namespace.
+# MapConfigSerializer, belongs_to: mappable, needs a ProjectSerializer in this namespace.
 class CustomMaps::WebApi::V1::ProjectSerializer < WebApi::V1::ProjectSerializer
-  #  has_one :map_config, serializer: CustomMaps::WebApi::V1::MapConfigSerializer # Copilot suggestion: probably not needed?
 end
