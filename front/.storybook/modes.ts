885469f2b33a551aa0a87345aa7bba4aaf120534
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-02-09 16:03:49 +0000

    Add modes

---
+++ front/.storybook/modes.ts
@@ -0,0 +1,20 @@
+export const allModes = {
+  default: {
+    viewport: {
+      width: 1280
+    }
+  },
+
+  tablet: {
+    viewport: {
+      width: 1200
+    }
+  },
+
+  mobile: {
+    viewport: {
+      width: 769,
+      height: 1280
+    }
+  }
+};
