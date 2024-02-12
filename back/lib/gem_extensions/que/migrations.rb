39ee71092ac0d1474d7063ed379ff2a28757da62
Adrien Dessy <adrien@citizenlab.co>
2024-02-12 17:03:04 +0100

    [TAN-966] Improve documentation

--- back/lib/gem_extensions/que/migrations.rb
+++ back/lib/gem_extensions/que/migrations.rb
@@ -3,12 +3,12 @@
 module GemExtensions
   module Que
     module Migrations
-      # This extension adapts Que's migrations to work with Apartment. The Que database
-      # version (an internal version number used by Que to account for evolutions in how
-      # it persists jobs) is stored as a comment on the `que_jobs` table. However, the
-      # original implementation of `db_version` does not expect the database to contain
-      # multiple `que_jobs` tables and as a result, is returning the version of one of
-      # them indeterministically.
+      # This extension adapts Que's migrations to make them compatible with Apartment.
+      # The Que database version (an internal version number used by Que to version
+      # changes in how it persists jobs) is stored as a comment on the `que_jobs` table.
+      # However, the original implementation of `db_version` does not expect the database
+      # to contain multiple `que_jobs` tables and, as a result, is returning the version
+      # of one of them indeterministically.
       module ClassMethods
         def db_version
           current_schema_name = Apartment::Tenant.current
