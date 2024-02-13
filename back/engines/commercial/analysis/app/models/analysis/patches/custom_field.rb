8883b3b9efa06471ae3df3d0bfbe5fd98f9f293c
Sébastien Hoorens <sebastien@citizenlab.co>
2024-02-13 15:20:06 +0100

    fix association from custom field patch

--- back/engines/commercial/analysis/app/models/analysis/patches/custom_field.rb
+++ back/engines/commercial/analysis/app/models/analysis/patches/custom_field.rb
@@ -3,7 +3,7 @@
 module Analysis::Patches::CustomField
   def self.included(base)
     base.class_eval do
-      has_many :analyses_custom_fields, class_name: 'Analysis::AnalysesCustomField', dependent: :destroy
+      has_many :analyses_custom_fields, class_name: 'Analysis::AdditionalCustomField', dependent: :destroy
     end
   end
 end
