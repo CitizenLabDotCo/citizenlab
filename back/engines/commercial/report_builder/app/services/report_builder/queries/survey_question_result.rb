12ef7153588f497e43cd6a38d5ba31c53687dcd0
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-02-14 17:19:01 +0000

    Update query in BE to handle params correctly

--- back/engines/commercial/report_builder/app/services/report_builder/queries/survey_question_result.rb
+++ back/engines/commercial/report_builder/app/services/report_builder/queries/survey_question_result.rb
@@ -3,7 +3,8 @@ module ReportBuilder
     def run_query(
       phase_id: nil,
       question_id: nil,
-      group_by_user_field_id: nil,
+      slice_mode: nil,
+      slice_field_id: nil,
       **_other_props
     )
       return {} if phase_id.blank? || question_id.blank?
@@ -11,12 +12,17 @@ module ReportBuilder
       phase = Phase.find(phase_id)
       service = SurveyResponseSlicer.new(phase)
 
-      if group_by_user_field_id.nil?
+      if slice_mode.nil? || slice_field_id.nil?
         service.get_result(question_id)
-      else
+      elsif slice_mode == 'user_field'
         service.slice_by_user_field(
           question_id,
-          group_by_user_field_id
+          slice_field_id
+        )
+      else
+        service.slice_by_other_question(
+          question_id,
+          slice_field_id
         )
       end
     end
