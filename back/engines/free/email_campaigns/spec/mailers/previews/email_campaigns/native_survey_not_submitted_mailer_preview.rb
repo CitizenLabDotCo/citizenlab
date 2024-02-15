4b378e48a272bd1dd14379cb6c80a9d68d3e9d7a
jamesspeake <james@speake.org>
2024-02-15 11:55:03 +0000

    [TAN-820] Survey preview fix

--- back/engines/free/email_campaigns/spec/mailers/previews/email_campaigns/native_survey_not_submitted_mailer_preview.rb
+++ back/engines/free/email_campaigns/spec/mailers/previews/email_campaigns/native_survey_not_submitted_mailer_preview.rb
@@ -5,27 +5,14 @@ module EmailCampaigns
     include EmailCampaigns::MailerPreviewRecipient
 
     def campaign_mail
-      # NEW
-      # campaign = EmailCampaigns::Campaigns::NativeSurveyNotSubmitted.first
-      # idea = Idea.where.not(creation_phase: nil).order(created_at: :asc).first
-      # item = Notifications::NativeSurveyNotSubmitted.new(post: idea)
-      # user = idea.author
-      # activity = Activity.new(item: item, user: user)
-      # commands = campaign.generate_commands(recipient: user, activity: activity)
-      # command = commands[0].merge({ recipient: user })
-
-      # OLD
-      # command = {
-      #   recipient: recipient_user,
-      #   event_payload: {
-      #     survey_url: Frontend::UrlService.new.model_to_url(project, locale: recipient_user.locale),
-      #     phase_title_multiloc: project.phases.first.title_multiloc || project.title_multiloc,
-      #     phase_end_at: project.created_at
-      #   }
-      # }
-      # campaign = EmailCampaigns::Campaigns::NativeSurveyNotSubmitted.first
-
-      # campaign.mailer_class.with(campaign: campaign, command: command)
+      campaign = EmailCampaigns::Campaigns::NativeSurveyNotSubmitted.first
+      idea = Idea.where.not(creation_phase: nil).order(created_at: :asc).first
+      item = Notifications::NativeSurveyNotSubmitted.new(post: idea)
+      user = idea.author
+      activity = Activity.new(item: item, user: user)
+      commands = campaign.generate_commands(recipient: user, activity: activity)
+      command = commands[0].merge({ recipient: user })
+      campaign.mailer_class.with(campaign: campaign, command: command).campaign_mail
     end
   end
 end
