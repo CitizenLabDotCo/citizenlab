0cb3fac7b7be9203bda3e98ba5320cec9e7e6eb6
Iva <iva@citizenlab.co>
2024-02-15 15:00:00 +0200

    Add limit

--- back/engines/commercial/analysis/app/services/analysis/inputs_finder.rb
+++ back/engines/commercial/analysis/app/services/analysis/inputs_finder.rb
@@ -22,6 +22,7 @@ module Analysis
       inputs = filter_author_custom_field_range(inputs)
       inputs = filter_input_custom_field_in(inputs)
       inputs = filter_input_custom_field_range(inputs)
+      inputs = filter_limit(inputs)
       search(inputs)
     end
 
@@ -217,6 +218,13 @@ module Analysis
         end
       end
       scope
+    end
+
+    def filter_limit(inputs)
+      return inputs unless params[:limit]
+
+      raise ArgumentError, 'limit must be a positive integer' unless params[:limit].to_i.positive?
+      inputs.limit(params[:limit])
     end
 
     def decode_input_in_custom_keys
