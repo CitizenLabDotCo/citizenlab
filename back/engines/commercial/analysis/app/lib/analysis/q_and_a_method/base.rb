f5fb33a9fb9c1073b2ed49f24a4c9136c8b8fcaa
Sébastien Hoorens <sebastien@citizenlab.co>
2024-02-15 20:09:24 +0100

    reset answers when regenerating q&a

--- back/engines/commercial/analysis/app/lib/analysis/q_and_a_method/base.rb
+++ back/engines/commercial/analysis/app/lib/analysis/q_and_a_method/base.rb
@@ -33,7 +33,8 @@ module Analysis
 
     def execute(plan)
       task.set_in_progress!
-      question.update!(accuracy: plan.accuracy)
+      old_answer = question.answer
+      question.update!(accuracy: plan.accuracy, answer: '')
       question.insight.update!(inputs_ids: filtered_inputs.ids)
 
       begin
@@ -42,6 +43,7 @@ module Analysis
       rescue QAndAFailedError => e
         ErrorReporter.report(e)
         task.set_failed!
+        question.update!(answer: old_answer)
       end
     end
 
