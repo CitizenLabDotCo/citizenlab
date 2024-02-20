80f18a683e6bd20cb1ec2c403089ca55aa7d6a28
brentguf <brentguf@users.noreply.github.com>
2024-02-20 14:09:06 +0100

    Add voting progress bar to proposals with status 'answered'

--- front/app/containers/InitiativesShow/ReactionControl/Answered.tsx
+++ front/app/containers/InitiativesShow/ReactionControl/Answered.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Box, Icon, Text } from '@citizenlab/cl2-component-library';
+import {
+  Box,
+  Icon,
+  colors,
+  fontSizes,
+  media,
+} from '@citizenlab/cl2-component-library';
 
 import { IInitiativeStatusData } from 'api/initiative_statuses/types';
 import { IAppConfigurationSettings } from 'api/app_configuration/types';
@@ -12,6 +18,7 @@ import T from 'components/T';
 import messages from './messages';
 import { FormattedMessage } from 'utils/cl-intl';
 import { IInitiativeData } from 'api/initiatives/types';
+import ProposalProgressBar from './ProposalProgressBar';
 
 const StatusIcon = styled(Icon)`
   path {
@@ -20,6 +27,30 @@ const StatusIcon = styled(Icon)`
   width: 30px;
   height: 30px;
   margin-bottom: 20px;
+`;
+
+const ReactionCounter = styled.div`
+  margin-top: 15px;
+  ${media.tablet`
+    display: none;
+  `}
+`;
+
+const ReactionText = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding-bottom: 4px;
+`;
+
+const ReactionTextLeft = styled.div`
+  font-size: ${fontSizes.base}px;
+  color: ${(props) => props.theme.colors.tenantPrimary};
+`;
+
+const ReactionTextRight = styled.div`
+  font-size: ${fontSizes.base}px;
+  color: ${(props) => props.theme.colors.tenantText};
 `;
 
 interface Props {
@@ -38,8 +69,12 @@ const Answered = ({
   onScrollToOfficialFeedback,
   initiative,
   initiativeStatus,
+  initiativeSettings: { reacting_threshold },
   userReacted,
 }: Props) => {
+  const reactionCount = initiative.attributes.likes_count;
+  const reactionLimit = reacting_threshold;
+
   return (
     <Box>
       <Box mb="16px">
@@ -66,21 +101,24 @@ const Answered = ({
           </StatusExplanation>
         </Box>
       </Box>
-      <Text fontSize="base" mb="8px">
-        <FormattedMessage
-          {...messages.xPeopleVoted}
-          values={{
-            xPeople: (
-              <b>
-                <FormattedMessage
-                  {...messages.xPeople}
-                  values={{ count: initiative.attributes.likes_count }}
-                />
-              </b>
-            ),
-          }}
-        />
-      </Text>
+      <Box mb="24px">
+        <ReactionCounter>
+          <ReactionText aria-hidden={true}>
+            <ReactionTextLeft id="e2e-initiative-not-reacted-reaction-count">
+              <FormattedMessage
+                {...messages.xVotes}
+                values={{ count: reactionCount }}
+              />
+            </ReactionTextLeft>
+            <ReactionTextRight>{reactionLimit}</ReactionTextRight>
+          </ReactionText>
+          <ProposalProgressBar
+            reactionCount={reactionCount}
+            reactionLimit={reactionLimit}
+            barColor={colors.green500}
+          />
+        </ReactionCounter>
+      </Box>
       <Box mb="8px">
         {userReacted ? (
           <Button
