9f27d1efd0138659c77ea95f3cad8a6e6827c8f1
Sébastien Hoorens <sebastien@citizenlab.co>
2024-02-20 11:22:45 +0100

    sidfx: delete analysis when deleting main field

--- back/engines/commercial/analysis/app/services/analysis/patches/side_fx_custom_field_service.rb
+++ back/engines/commercial/analysis/app/services/analysis/patches/side_fx_custom_field_service.rb
@@ -3,9 +3,9 @@
 module Analysis
   module Patches
     module SideFxCustomFieldService
-      # TODO: delete analysis if main field is deleted
       def before_destroy(custom_field, _user)
         super
+        Analysis.where(main_custom_field_id: custom_field.id).each(&:destroy!)
         Insight.delete_custom_field_references!(custom_field.id)
         AutoTaggingTask.delete_custom_field_references!(custom_field.id)
       end
--- back/engines/commercial/analysis/spec/services/side_fx_custom_field_service_spec.rb
+++ back/engines/commercial/analysis/spec/services/side_fx_custom_field_service_spec.rb
@@ -9,11 +9,22 @@ describe SideFxCustomFieldService do
   describe 'before_delete' do
     let_it_be(:custom_field1) { create(:custom_field) }
     let_it_be(:custom_field2) { create(:custom_field) }
-    let_it_be(:analysis) { create(:analysis, main_custom_field: custom_field1, additional_custom_fields: [custom_field2]) }
+    let_it_be(:analysis) { create(:analysis, main_custom_field: nil, additional_custom_fields: [custom_field1, custom_field2]) }
     let_it_be(:insight1) { create(:insight, analysis: analysis, filters: { "author_custom_#{custom_field1.id}" => [], 'votes_from' => 4 }) }
     let_it_be(:insight2) { create(:insight, analysis: analysis, filters: { "author_custom_#{custom_field1.id}_from" => 5, "author_custom_#{custom_field2.id}_from" => 5 }) }
     let_it_be(:insight3) { create(:insight, analysis: analysis, filters: {}) }
     let_it_be(:auto_tagging_task) { create(:auto_tagging_task, analysis: analysis, filters: { "input_custom_#{custom_field2.id}" => [] }) }
+
+    describe do
+      let_it_be(:analysis) { create(:analysis, main_custom_field: custom_field1, additional_custom_fields: [custom_field2]) }
+
+      it 'deletes the analysis with the field as main field', document: false do
+        service.before_destroy(custom_field1, user)
+
+        byebug
+        expect { Analysis::Analysis.find(analysis.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
 
     it 'deletes the custom field reference from the insight filters', document: false do
       service.before_destroy(custom_field1, user)
