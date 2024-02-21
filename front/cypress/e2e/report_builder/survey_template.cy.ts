2af10a1e7ca9c67209b69ced11c1acd3ffde861e
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-02-21 12:16:27 +0000

    _

--- front/cypress/e2e/report_builder/survey_template.cy.ts
+++ front/cypress/e2e/report_builder/survey_template.cy.ts
@@ -1,7 +1,44 @@
-describe('Survey template', () => {
-  before(() => {});
+import { randomString } from '../../support/commands';
+import moment = require('moment');
 
-  after(() => {});
+describe.skip('Survey template', () => {
+  let projectId: string;
+  let projectSlug: string;
+  let phaseId: string;
+
+  before(() => {
+    cy.apiCreateProject({
+      title: randomString(),
+      descriptionPreview: randomString(),
+      description: randomString(),
+      publicationStatus: 'published',
+    })
+      .then((project) => {
+        projectId = project.body.data.id;
+        projectSlug = project.body.data.attributes.slug;
+        return cy.apiCreatePhase({
+          projectId,
+          title: randomString(),
+          startAt: moment().subtract(9, 'month').format('DD/MM/YYYY'),
+          participationMethod: 'native_survey',
+          canPost: true,
+          canComment: true,
+          canReact: true,
+        });
+      })
+      .then((phase) => {
+        phaseId = phase.body.data.id;
+        // TODO add survey questions
+      });
+
+    // TODO create users and respond to survey
+  });
+
+  after(() => {
+    cy.apiRemoveProject(projectId);
+
+    // TODO clean up users
+  });
 
   it('should create a survey template', () => {});
 });
