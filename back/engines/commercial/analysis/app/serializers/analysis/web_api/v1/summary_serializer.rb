14464662e1f240ba12f07fb16c86fae337de6035
Sébastien Hoorens <sebastien@citizenlab.co>
2024-02-22 16:11:56 +0100

    added inputs_ids to insightable serializers

--- back/engines/commercial/analysis/app/serializers/analysis/web_api/v1/summary_serializer.rb
+++ back/engines/commercial/analysis/app/serializers/analysis/web_api/v1/summary_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Analysis::WebApi::V1::SummarySerializer < WebApi::V1::BaseSerializer
-  attributes :summary, :filters, :custom_field_ids, :accuracy, :created_at, :updated_at, :missing_inputs_count, :generated_at
+  attributes :summary, :filters, :inputs_ids, :custom_field_ids, :accuracy, :created_at, :updated_at, :missing_inputs_count, :generated_at
   belongs_to :background_task, class_name: 'Analysis::BackgroundTask'
 end
