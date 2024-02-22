7c0de11f91995a87165413938b7fb202faf9f1a1
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-02-22 16:47:52 +0000

    Finish survey result widget test

--- front/cypress/e2e/report_builder/widgets/survey_question_widget.cy.ts
+++ front/cypress/e2e/report_builder/widgets/survey_question_widget.cy.ts
@@ -104,26 +104,43 @@ describe('Survey question widget', () => {
     // Select project, phase and question
     cy.get('#e2e-report-builder-project-filter-box select').select(projectId);
     cy.get('#e2e-report-builder-phase-filter').select(phaseId);
-    cy.get('#e2e-question-select').select(surveyFields[2].id);
+    cy.get('.e2e-question-select select').first().select(surveyFields[2].id);
 
     // Check if values are correct
+    cy.get('.e2e-survey-question-ungrouped-bars')
+      .first()
+      .contains('50% (4 choices)');
 
-    // Group by gender
-    // TODO
+    cy.get('svg.e2e-progress-bar').should('have.length', 3);
+    cy.get('svg.e2e-progress-bar').first().should('have.attr', 'width', '50%');
+    cy.get('svg.e2e-progress-bar').eq(1).should('have.attr', 'width', '50%');
+    cy.get('svg.e2e-progress-bar').eq(2).should('have.attr', 'width', '0%');
 
-    // Group by other survey question
-    // TODO
+    // Group by gender and confirm correctness
+    cy.get('#e2e-group-mode-select').select('user_field');
+    cy.get('#e2e-user-field-select').select('Gender');
+
+    cy.get('svg.e2e-progress-bar').should('have.length', 5);
+    cy.get('svg.e2e-progress-bar').first().should('have.attr', 'width', '25%');
+    cy.get('svg.e2e-progress-bar').eq(1).should('have.attr', 'width', '25%');
+    cy.get('svg.e2e-progress-bar').eq(2).should('have.attr', 'width', '25%');
+    cy.get('svg.e2e-progress-bar').eq(3).should('have.attr', 'width', '25%');
+    cy.get('svg.e2e-progress-bar').eq(4).should('have.attr', 'width', '0%');
 
     // Save
-    // cy.intercept('PATCH', `/web_api/v1/reports/${reportId}`).as(
-    //   'saveReportLayout'
-    // );
-    // TODO
+    cy.intercept('PATCH', `/web_api/v1/reports/${reportId}`).as(
+      'saveReportLayout'
+    );
+    cy.get('#e2e-content-builder-topbar-save').click();
+    cy.wait('@saveReportLayout');
 
     // Reload page and check if values are still correct
-    // TODO
-
-    // Remove widget
-    // TODO
+    cy.reload();
+    cy.get('svg.e2e-progress-bar').should('have.length', 5);
+    cy.get('svg.e2e-progress-bar').first().should('have.attr', 'width', '25%');
+    cy.get('svg.e2e-progress-bar').eq(1).should('have.attr', 'width', '25%');
+    cy.get('svg.e2e-progress-bar').eq(2).should('have.attr', 'width', '25%');
+    cy.get('svg.e2e-progress-bar').eq(3).should('have.attr', 'width', '25%');
+    cy.get('svg.e2e-progress-bar').eq(4).should('have.attr', 'width', '0%');
   });
 });
