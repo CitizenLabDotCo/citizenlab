5a7111f5f9662a390a523fe322520372ea0f832e
brentguf <brentguf@users.noreply.github.com>
2024-02-25 11:34:35 +0100

    Only render topics component if there are topics

--- front/app/containers/InitiativesShow/LargerThanPhone.tsx
+++ front/app/containers/InitiativesShow/LargerThanPhone.tsx
@@ -221,12 +221,14 @@ const LargerThanPhone = ({
                 <CosponsorShipReminder initiativeId={initiativeId} />
               </Box>
             )}
-            <StyledTopics
-              postType="initiative"
-              postTopicIds={initiative.data.relationships.topics.data.map(
-                (topic) => topic.id
-              )}
-            />
+            {initiative.data.relationships.topics.data.length > 0 && (
+              <StyledTopics
+                postType="initiative"
+                postTopicIds={initiative.data.relationships.topics.data.map(
+                  (topic) => topic.id
+                )}
+              />
+            )}
             <InitiativeHeader>
               <Title
                 postType="initiative"
