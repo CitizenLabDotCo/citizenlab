472f3c42494b336e6181655801f6e0c0c6dfd463
Iva <iva@citizenlab.co>
2024-02-26 15:24:09 +0200

    Clean up question

--- front/app/containers/Admin/projects/project/analysis/Insights/Question.tsx
+++ front/app/containers/Admin/projects/project/analysis/Insights/Question.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useParams, useSearchParams } from 'react-router-dom';
 
 import useDeleteAnalysisInsight from 'api/analysis_insights/useDeleteAnalysisInsight';
-import useAnalysisBackgroundTask from 'api/analysis_background_tasks/useAnalysisBackgroundTask';
 import { IInsightData } from 'api/analysis_insights/types';
 
 import {
@@ -15,7 +14,6 @@ import {
 } from '@citizenlab/cl2-component-library';
 
 import { useIntl } from 'utils/cl-intl';
-import styled from 'styled-components';
 import useAnalysisQuestion from 'api/analysis_questions/useAnalysisQuestion';
 import { updateSearchParams } from 'utils/cl-router/updateSearchParams';
 import Rate from './Rate';
@@ -27,11 +25,6 @@ import { removeRefs } from './util';
 import InsightBody from './InsightBody';
 import InsightFooter from './InsightFooter';
 
-const StyledAnswerText = styled.div`
-  white-space: pre-wrap;
-  word-break: break-word;
-`;
-
 type Props = {
   insight: IInsightData;
 };
@@ -39,7 +32,7 @@ type Props = {
 const Question = ({ insight }: Props) => {
   const [isCopied, setIsCopied] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
-  const { formatMessage, formatDate } = useIntl();
+  const { formatMessage } = useIntl();
   const { analysisId, projectId } = useParams() as {
     analysisId: string;
     projectId: string;
@@ -49,14 +42,6 @@ const Question = ({ insight }: Props) => {
     analysisId,
     id: insight.relationships.insightable.data.id,
   });
-
-  const { data: backgroundTask } = useAnalysisBackgroundTask(
-    analysisId,
-    question?.data.relationships.background_task.data.id
-  );
-  const processing =
-    backgroundTask?.data.attributes.state === 'in_progress' ||
-    backgroundTask?.data.attributes.state === 'queued';
 
   const handleQuestionDelete = (id: string) => {
     if (window.confirm(formatMessage(messages.deleteQuestionConfirmation))) {
@@ -77,7 +62,6 @@ const Question = ({ insight }: Props) => {
   };
 
   if (!question) return null;
-  const hasFilters = !!Object.keys(question.data.attributes.filters).length;
 
   const phaseId = searchParams.get('phase_id') || undefined;
 
