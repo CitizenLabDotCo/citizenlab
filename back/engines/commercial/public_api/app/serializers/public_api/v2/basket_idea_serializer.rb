9a97128b7e87fa0deb3c3b412790929afa923f7b
jamesspeake <james@speake.org>
2024-02-26 21:13:09 +0000

    [TAN-1174] removed unused code

--- back/engines/commercial/public_api/app/serializers/public_api/v2/basket_idea_serializer.rb
+++ back/engines/commercial/public_api/app/serializers/public_api/v2/basket_idea_serializer.rb
@@ -6,18 +6,6 @@ module PublicApi
       type :basket_ideas
       attributes(:basket_id, :idea_id, :votes, :created_at, :updated_at)
 
-      def phase_id
-        object.basket.phase_id
-      end
-
-      def project_id
-        object.basket.phase.project_id
-      end
-
-      def user_id
-        object.basket.user_id
-      end
-
       attribute(:phase_id) { object.basket&.phase_id }
       attribute(:project_id) { object.basket&.phase&.project_id }
       attribute(:user_id) { object.basket&.user_id }
