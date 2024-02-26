91c61a2546c58686d35860eaea067f13eb910fbe
Simon Tharby <simontharby@gmail.com>
2024-02-26 15:13:37 +0000

    [TAN-1073] Move helper method to correct scope

--- back/engines/commercial/custom_maps/app/models/custom_maps/extensions/custom_field.rb
+++ back/engines/commercial/custom_maps/app/models/custom_maps/extensions/custom_field.rb
@@ -7,10 +7,10 @@ module CustomMaps
         base.attr_accessor :map_config_id
 
         base.has_one :map_config, class_name: 'CustomMaps::MapConfig', as: :mappable, dependent: :destroy
+      end
 
-        def supports_map_config?
-          input_type == 'point'
-        end
+      def supports_map_config?
+        input_type == 'point'
       end
     end
   end
