c8bf28bba5eda0ec2e5c1befe22d97177775e1dd
EdwinKato <katedwin88@gmail.com>
2024-02-27 06:44:46 +0300

    Add message strings for the descriptions

--- front/app/containers/Admin/projects/project/phase/phaseParticipationConfig/components/messages.ts
+++ front/app/containers/Admin/projects/project/phase/phaseParticipationConfig/components/messages.ts
@@ -28,4 +28,58 @@ export default defineMessages({
     id: 'app.containers.AdminPage.ProjectEdit.information.new',
     defaultMessage: 'NEW',
   },
+  ideationTitle: {
+    id: 'app.containers.AdminPage.phase.methodPicker.ideationTitle',
+    defaultMessage: 'Collect input and feedback in public',
+  },
+  ideationDescription: {
+    id: 'app.containers.AdminPage.phase.methodPicker.ideationDescription',
+    defaultMessage:
+      "Tap into your users' collective intelligence. Invite them to submit, discuss ideas, and/or provide feedback in a public forum.",
+  },
+  surveyTitle: {
+    id: 'app.containers.AdminPage.phase.methodPicker.surveyTitle',
+    defaultMessage: 'Create a survey',
+  },
+  surveyDescription: {
+    id: 'app.containers.AdminPage.phase.methodPicker.surveyDescription',
+    defaultMessage:
+      "Understand your users' needs and thinking via a wide range of private question types.",
+  },
+  votingTitle: {
+    id: 'app.containers.AdminPage.phase.methodPicker.votingTitle',
+    defaultMessage: 'Conduct a voting or prioritization exercise',
+  },
+  votingDescription: {
+    id: 'app.containers.AdminPage.phase.methodPicker.votingDescription',
+    defaultMessage:
+      'Select a voting method, and have users prioritise between a few different options',
+  },
+  informationTitle: {
+    id: 'app.containers.AdminPage.phase.methodPicker.informationTitle',
+    defaultMessage: 'Share information',
+  },
+  informationDescription: {
+    id: 'app.containers.AdminPage.phase.methodPicker.informationDescription',
+    defaultMessage:
+      'Provide information to users, visualise results from other phases, create data rich reports.',
+  },
+  volunteeringTitle: {
+    id: 'app.containers.AdminPage.phase.methodPicker.volunteeringTitle',
+    defaultMessage: 'Recruit participants or volunteers',
+  },
+  volunteeringDescription: {
+    id: 'app.containers.AdminPage.phase.methodPicker.volunteeringDescription',
+    defaultMessage:
+      'Ask users to volunteer for activities and causes or find participants for a panel.',
+  },
+  documentTitle: {
+    id: 'app.containers.AdminPage.phase.methodPicker.documentTitle',
+    defaultMessage: 'Collect feedback on a document',
+  },
+  documentDescription: {
+    id: 'app.containers.AdminPage.phase.methodPicker.votingDescription',
+    defaultMessage:
+      'Embed an interactive PDF and collect comments and feedback with Konveio.',
+  },
 });
