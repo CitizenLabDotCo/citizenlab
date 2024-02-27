85d7236db566f19c2bf6f5c11b0e83d9a503dd21
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-02-27 11:44:38 +0000

    Migration

--- back/app/models/que_job.rb
+++ back/app/models/que_job.rb
@@ -1,5 +1,30 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: public.que_jobs
+#
+#  priority             :integer          default(100), not null
+#  run_at               :timestamptz      not null
+#  id                   :bigint           not null, primary key
+#  job_class            :text             not null
+#  error_count          :integer          default(0), not null
+#  last_error_message   :text
+#  queue                :text             default("default"), not null
+#  last_error_backtrace :text
+#  finished_at          :timestamptz
+#  expired_at           :timestamptz
+#  args                 :jsonb            not null
+#  data                 :jsonb            not null
+#  job_schema_version   :integer          default(1)
+#
+# Indexes
+#
+#  que_jobs_args_gin_idx                 (args) USING gin
+#  que_jobs_data_gin_idx                 (data) USING gin
+#  que_poll_idx                          (queue,priority,run_at,id) WHERE ((finished_at IS NULL) AND (expired_at IS NULL))
+#  que_poll_idx_with_job_schema_version  (job_schema_version,queue,priority,run_at,id) WHERE ((finished_at IS NULL) AND (expired_at IS NULL))
+#
 require 'que/active_record/model'
 
 class QueJob < Que::ActiveRecord::Model
