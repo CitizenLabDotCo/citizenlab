4bce60bda1b46dd84d9d4d4bdf77cf3fc37e2f03
brentguf <brentguf@users.noreply.github.com>
2024-02-27 08:19:32 +0100

    Show read answer button for "changes requested"

--- front/app/containers/InitiativesShow/ReactionControl/Status/ChangesRequested.tsx
+++ front/app/containers/InitiativesShow/ReactionControl/Status/ChangesRequested.tsx
@@ -24,7 +24,7 @@ const ChangesRequested = (props: StatusComponentProps) => {
       showCountDown={false}
       showProgressBar={false}
       showVoteButtons={false}
-      showReadAnswerButton={false}
+      showReadAnswerButton
     />
   );
 };
