9825087b05d90b0109d2e57d5b994d5912a044fb
EdwinKato <katedwin88@gmail.com>
2024-02-27 10:22:14 +0300

    Add survey options to the method picker

--- front/app/containers/Admin/projects/project/phase/phaseParticipationConfig/components/MethodPicker.tsx
+++ front/app/containers/Admin/projects/project/phase/phaseParticipationConfig/components/MethodPicker.tsx
@@ -1,41 +1,33 @@
 import React, { useState } from 'react';
-import {
-  Box,
-  IconTooltip,
-  Toggle,
-  colors,
-  Title,
-  BoxProps,
-  Icon,
-  Text,
-  Image,
-} from '@citizenlab/cl2-component-library';
-import { useIntl } from 'utils/cl-intl';
+import { Box, Title } from '@citizenlab/cl2-component-library';
+import styled from 'styled-components';
+import { useIntl, FormattedMessage } from 'utils/cl-intl';
 import ParticipationMethodChoice from './ParticipationMethodChoice';
-import ideationImage from './ideation.png';
-import surveyImage from './survey.png';
-import votingImage from './voting.png';
-import informationImage from './information.png';
-import volunteeringImage from './volunteering.png';
-import documentImage from './document.png';
+import ideationImage from './assets/ideation.png';
+import surveyImage from './assets/survey.png';
+import votingImage from './assets/voting.png';
+import informationImage from './assets/information.png';
+import volunteeringImage from './assets/volunteering.png';
+import documentImage from './assets/document.png';
 import messages from './messages';
+import { ParticipationMethod } from 'api/phases/types';
 
-type MethodOption =
-  | 'ideation'
-  | 'survey'
-  | 'voting'
-  | 'information'
-  | 'volunteering'
-  | 'document';
+const LeftAlignedList = styled.ul`
+  text-align: left;
+`;
 
 const MethodPicker = () => {
-  const [selectedMethod, setSelectedMethod] = useState<MethodOption | null>(
-    null
-  );
+  const [selectedMethod, setSelectedMethod] =
+    useState<ParticipationMethod | null>(null);
+  const [showSurveyOptions, setShowSurveyOptions] = useState(false);
   const { formatMessage } = useIntl();
 
-  const handleMethodSelect = (event, method: MethodOption) => {
+  const handleMethodSelect = (event, method: ParticipationMethod) => {
     event.preventDefault();
+    const isSurveyCategory = ['native_survey', 'survey', 'poll'].includes(
+      method
+    );
+    setShowSurveyOptions(isSurveyCategory);
     setSelectedMethod(method);
   };
 
@@ -50,9 +42,13 @@ const MethodPicker = () => {
     {
       title: formatMessage(messages.surveyTitle),
       subtitle: formatMessage(messages.surveyDescription),
-      onClick: (event) => handleMethodSelect(event, 'survey'),
+      onClick: (event) => {
+        event.preventDefault();
+        setShowSurveyOptions(true);
+        setSelectedMethod(null);
+      },
       image: surveyImage,
-      selected: selectedMethod === 'survey',
+      selected: showSurveyOptions,
     },
     {
       title: formatMessage(messages.votingTitle),
@@ -78,31 +74,92 @@ const MethodPicker = () => {
     {
       title: formatMessage(messages.documentTitle),
       subtitle: formatMessage(messages.documentDescription),
-      onClick: (event) => handleMethodSelect(event, 'document'),
+      onClick: (event) => handleMethodSelect(event, 'document_annotation'),
       image: documentImage,
-      selected: selectedMethod === 'document',
+      selected: selectedMethod === 'document_annotation',
     },
   ];
 
   return (
-    <Box
-      style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(3, 1fr)',
-        gap: '20px',
-      }}
-      mb="32px"
-    >
-      {methodData.map((method, index) => (
-        <ParticipationMethodChoice
-          key={index}
-          title={method.title}
-          subtitle={method.subtitle}
-          onClick={method.onClick}
-          image={method.image}
-          selected={method.selected}
-        />
-      ))}
+    <Box mb="32px">
+      <Box
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(3, 1fr)',
+          gap: '16px',
+        }}
+        my="32px"
+      >
+        {methodData.map((method, index) => (
+          <ParticipationMethodChoice
+            key={index}
+            title={method.title}
+            subtitle={method.subtitle}
+            onClick={method.onClick}
+            image={method.image}
+            selected={method.selected}
+          />
+        ))}
+
+        {showSurveyOptions && (
+          <>
+            <Box style={{ gridColumn: '1 / span 3' }}>
+              <Title my="0px" variant="h6" color="primary" textAlign="left">
+                {formatMessage(messages.surveyOptions)}
+              </Title>
+            </Box>
+
+            <ParticipationMethodChoice
+              onClick={(event) => handleMethodSelect(event, 'native_survey')}
+              title={formatMessage(messages.survey)}
+              selected={selectedMethod === 'native_survey'}
+            >
+              <>
+                <LeftAlignedList>
+                  <li>
+                    <FormattedMessage {...messages.aiPoweredInsights} />
+                  </li>
+                  <li>
+                    <FormattedMessage {...messages.manyQuestionTypes} />
+                  </li>
+                  <li>
+                    <FormattedMessage {...messages.logic} />
+                  </li>
+                  <li>
+                    <FormattedMessage {...messages.linkWithReportBuilder} />
+                  </li>
+                </LeftAlignedList>
+              </>
+            </ParticipationMethodChoice>
+
+            <ParticipationMethodChoice
+              onClick={(event) => handleMethodSelect(event, 'poll')}
+              title={formatMessage(messages.quickPoll)}
+              selected={selectedMethod === 'poll'}
+            >
+              <>{formatMessage(messages.quickPollDescription)}</>
+            </ParticipationMethodChoice>
+
+            <ParticipationMethodChoice
+              onClick={(event) => handleMethodSelect(event, 'survey')}
+              title={formatMessage(messages.externalSurvey)}
+              selected={selectedMethod === 'survey'}
+            >
+              <>
+                <FormattedMessage {...messages.embedSurvey} />
+                <LeftAlignedList>
+                  <li>
+                    <FormattedMessage {...messages.lacksAIText} />
+                  </li>
+                  <li>
+                    <FormattedMessage {...messages.lacksReportingText} />
+                  </li>
+                </LeftAlignedList>
+              </>
+            </ParticipationMethodChoice>
+          </>
+        )}
+      </Box>
     </Box>
   );
 };
