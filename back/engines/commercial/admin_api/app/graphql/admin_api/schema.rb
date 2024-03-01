61ccbd55d513c905048334859d795deb4e44b3b7
Adrien Dessy <adrien@citizenlab.co>
2024-03-02 00:03:56 +0100

    [NO-TICKET] Test upgrade ruby 3

--- back/engines/commercial/admin_api/app/graphql/admin_api/schema.rb
+++ back/engines/commercial/admin_api/app/graphql/admin_api/schema.rb
@@ -5,7 +5,7 @@ module AdminApi
     # Required:
     query Types::QueryType
     # Optional:
-    mutation Types::MutationType
+    # mutation Types::MutationType
 
     default_max_page_size 100
   end
