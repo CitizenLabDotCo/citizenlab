f88afb45995efb2039dd2c0ae90950e5f9d3a86a
brentguf <brentguf@users.noreply.github.com>
2024-03-05 09:40:12 +0100

    Use SortableList component

--- front/app/containers/Admin/settings/registration/CustomFieldSettings/index.tsx
+++ front/app/containers/Admin/settings/registration/CustomFieldSettings/index.tsx
@@ -2,9 +2,7 @@
 import React, { Component, MouseEvent } from 'react';
 import Link from 'utils/cl-router/Link';
 import styled from 'styled-components';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { isEqual, clone } from 'lodash-es';
+import { clone } from 'lodash-es';
 
 // i18n
 import { FormattedMessage, injectIntl } from 'utils/cl-intl';
@@ -15,7 +13,11 @@ import T from 'components/T';
 
 // components
 import Button from 'components/UI/Button';
-import { List, SortableRow, TextCell } from 'components/admin/ResourceList';
+import {
+  SortableList,
+  SortableRow,
+  TextCell,
+} from 'components/admin/ResourceList';
 import {
   Toggle,
   Badge,
@@ -70,12 +72,7 @@ const StyledIconTooltip = styled(IconTooltip)`
   margin-left: 5px;
 `;
 
-const CustomFieldsList = styled(List)`
-  margin-bottom: 20px;
-`;
-
 interface State {
-  itemsWhileDragging: IUserCustomFieldData[] | null;
   isProcessing: boolean;
 }
 
@@ -97,26 +94,8 @@ class CustomFields extends Component<Props & WrappedComponentProps, State> {
   constructor(props: Props & WrappedComponentProps) {
     super(props);
     this.state = {
-      itemsWhileDragging: null,
       isProcessing: false,
     };
-  }
-
-  componentDidUpdate(prevProps: Props) {
-    const { itemsWhileDragging } = this.state;
-    const prevCustomFieldsIds =
-      prevProps.userCustomFields &&
-      prevProps.userCustomFields.map((customField) => customField.id);
-    const nextCustomFieldsIds =
-      this.props.userCustomFields &&
-      this.props.userCustomFields.map((customField) => customField.id);
-
-    if (
-      itemsWhileDragging &&
-      !isEqual(prevCustomFieldsIds, nextCustomFieldsIds)
-    ) {
-      this.setState({ itemsWhileDragging: null });
-    }
   }
 
   handleOnDeleteClick = (customFieldId: string) => (event: MouseEvent) => {
@@ -127,7 +106,7 @@ class CustomFields extends Component<Props & WrappedComponentProps, State> {
       event.preventDefault();
 
       if (window.confirm(deleteMessage)) {
-        this.setState({ itemsWhileDragging: null, isProcessing: true });
+        this.setState({ isProcessing: true });
         this.props.deleteUserCustomField(customFieldId).then(() => {
           this.setState({ isProcessing: false });
           queryClient.invalidateQueries({
@@ -162,57 +141,32 @@ class CustomFields extends Component<Props & WrappedComponentProps, State> {
             },
           });
           this.setState({
-            itemsWhileDragging: newListItems,
             isProcessing: false,
           });
         });
     }
   };
 
-  handleDragRow = (fromIndex: number, toIndex: number) => {
-    if (!this.state.isProcessing) {
-      const listItems = this.listItems();
-
-      if (!listItems) return;
-
-      const itemsWhileDragging = clone(listItems);
-      itemsWhileDragging.splice(fromIndex, 1);
-      itemsWhileDragging.splice(toIndex, 0, listItems[fromIndex]);
-      this.setState({ itemsWhileDragging });
-    }
-  };
-
-  handleDropRow = (fieldId: string, toIndex: number) => {
-    const listItems = this.listItems();
-
-    if (!listItems) return;
-
-    const field = listItems.find((listItem) => listItem.id === fieldId);
-
-    if (field && field.attributes.ordering !== toIndex) {
-      this.setState({ isProcessing: true });
-      this.props
-        .reorderCustomFieldForUsers({
-          customFieldId: fieldId,
-          ordering: toIndex,
-        })
-        .then(() => this.setState({ isProcessing: false }));
-    } else {
-      this.setState({ itemsWhileDragging: null });
-    }
-  };
-
   listItems = () => {
-    const { itemsWhileDragging } = this.state;
     const { userCustomFields } = this.props;
-    return itemsWhileDragging || userCustomFields;
+    return userCustomFields;
+  };
+
+  handleReorderCustomFieldOption = (
+    customFieldId: string,
+    ordering: number
+  ) => {
+    this.props.reorderCustomFieldForUsers({
+      customFieldId,
+      ordering,
+    });
   };
 
   render() {
     const {
       intl: { formatMessage },
     } = this.props;
-    const listItems = this.listItems() || [];
+    const listItems = this.props.userCustomFields || [];
     const listItemsLength = listItems.length;
 
     return (
@@ -232,94 +186,109 @@ class CustomFields extends Component<Props & WrappedComponentProps, State> {
             content={<FormattedMessage {...messages.customFieldsTooltip} />}
           />
         </SubSectionTitle>
-        <CustomFieldsList key={listItems.length}>
-          {listItems.map((field, index) => {
-            return (
-              <SortableRow
-                key={field.id}
-                id={field.id}
-                className="e2e-custom-registration-field-row"
-                index={index}
-                isLastItem={index === listItemsLength - 1}
-                moveRow={this.handleDragRow}
-                dropRow={this.handleDropRow}
-              >
-                <Toggle
-                  className={`e2e-custom-registration-field-toggle ${
-                    field.attributes.enabled ? 'enabled' : 'disabled'
-                  }`}
-                  checked={field.attributes.enabled}
-                  disabled={isHiddenField(field)}
-                  onChange={() => this.handleOnEnabledToggle(field)}
-                />
-                <StyledTextCell className="expand">
-                  <TextCellContent>
-                    <T value={field.attributes.title_multiloc} />
-                    {field.attributes.code === 'domicile' && (
-                      <StyledIconTooltip
-                        content={
+        <Box mb="20px">
+          <SortableList
+            items={listItems}
+            onReorder={this.handleReorderCustomFieldOption}
+          >
+            {({ itemsList, handleDragRow, handleDropRow }) => (
+              <>
+                {itemsList.map((field: IUserCustomFieldData, index: number) => {
+                  return (
+                    <SortableRow
+                      key={field.id}
+                      id={field.id}
+                      className="e2e-custom-registration-field-row"
+                      index={index}
+                      isLastItem={index === listItemsLength - 1}
+                      moveRow={handleDragRow}
+                      dropRow={handleDropRow}
+                    >
+                      <Toggle
+                        className={`e2e-custom-registration-field-toggle ${
+                          field.attributes.enabled ? 'enabled' : 'disabled'
+                        }`}
+                        checked={field.attributes.enabled}
+                        disabled={isHiddenField(field)}
+                        onChange={() => this.handleOnEnabledToggle(field)}
+                      />
+                      <StyledTextCell className="expand">
+                        <TextCellContent>
+                          <T value={field.attributes.title_multiloc} />
+                          {field.attributes.code === 'domicile' && (
+                            <StyledIconTooltip
+                              content={
+                                <FormattedMessage
+                                  {...messages.domicileManagementInfo}
+                                  values={{
+                                    geographicAreasTabLink: (
+                                      <Link to={'/admin/settings/areas'}>
+                                        {formatMessage(
+                                          messages.geographicAreasTabLinkText
+                                        )}
+                                      </Link>
+                                    ),
+                                  }}
+                                />
+                              }
+                            />
+                          )}
+                        </TextCellContent>
+                        {field.attributes.required && (
+                          <StyledBadge className="inverse" color={colors.error}>
+                            <FormattedMessage
+                              {...customfieldMessages.required}
+                            />
+                          </StyledBadge>
+                        )}
+                      </StyledTextCell>
+                      {isBuiltInField(field) && (
+                        <div>
                           <FormattedMessage
-                            {...messages.domicileManagementInfo}
-                            values={{
-                              geographicAreasTabLink: (
-                                <Link to={'/admin/settings/areas'}>
-                                  {formatMessage(
-                                    messages.geographicAreasTabLinkText
-                                  )}
-                                </Link>
-                              ),
-                            }}
+                            {...customfieldMessages.defaultField}
                           />
-                        }
-                      />
-                    )}
-                  </TextCellContent>
-                  {field.attributes.required && (
-                    <StyledBadge className="inverse" color={colors.error}>
-                      <FormattedMessage {...customfieldMessages.required} />
-                    </StyledBadge>
-                  )}
-                </StyledTextCell>
-                {isBuiltInField(field) && (
-                  <div>
-                    <FormattedMessage {...customfieldMessages.defaultField} />
-                  </div>
-                )}
-                {isHiddenField(field) && (
-                  <div>
-                    <FormattedMessage {...customfieldMessages.hiddenField} />
-                  </div>
-                )}
-                <Buttons>
-                  {!isBuiltInField(field) && !isHiddenField(field) && (
-                    <Button
-                      className={`e2e-delete-custom-field-btn e2e-${field.attributes.title_multiloc['en-GB']}`}
-                      onClick={this.handleOnDeleteClick(field.id)}
-                      buttonStyle="text"
-                      icon="delete"
-                    >
-                      <FormattedMessage
-                        {...customfieldMessages.deleteButtonLabel}
-                      />
-                    </Button>
-                  )}
-                  {!isHiddenField(field) && (
-                    <Button
-                      className={`e2e-custom-field-edit-btn e2e-${field.attributes.title_multiloc['en-GB']}`}
-                      linkTo={`/admin/settings/registration/custom-fields/${field.id}/field-settings`}
-                      buttonStyle="secondary"
-                      icon="edit"
-                    >
-                      <FormattedMessage
-                        {...customfieldMessages.editButtonLabel}
-                      />
-                    </Button>
-                  )}
-                </Buttons>
-              </SortableRow>
-            );
-          })}
-        </CustomFieldsList>
+                        </div>
+                      )}
+                      {isHiddenField(field) && (
+                        <div>
+                          <FormattedMessage
+                            {...customfieldMessages.hiddenField}
+                          />
+                        </div>
+                      )}
+                      <Buttons>
+                        {!isBuiltInField(field) && !isHiddenField(field) && (
+                          <Button
+                            className={`e2e-delete-custom-field-btn e2e-${field.attributes.title_multiloc['en-GB']}`}
+                            onClick={this.handleOnDeleteClick(field.id)}
+                            buttonStyle="text"
+                            icon="delete"
+                          >
+                            <FormattedMessage
+                              {...customfieldMessages.deleteButtonLabel}
+                            />
+                          </Button>
+                        )}
+                        {!isHiddenField(field) && (
+                          <Button
+                            className={`e2e-custom-field-edit-btn e2e-${field.attributes.title_multiloc['en-GB']}`}
+                            linkTo={`/admin/settings/registration/custom-fields/${field.id}/field-settings`}
+                            buttonStyle="secondary"
+                            icon="edit"
+                          >
+                            <FormattedMessage
+                              {...customfieldMessages.editButtonLabel}
+                            />
+                          </Button>
+                        )}
+                      </Buttons>
+                    </SortableRow>
+                  );
+                })}
+              </>
+            )}
+          </SortableList>
+        </Box>
         <Button
           buttonStyle="cl-blue"
           icon="plus-circle"
@@ -342,13 +311,11 @@ export default () => {
   const { mutateAsync: updateCustomFieldForUsers } = useUpdateUserCustomField();
 
   return (
-    <DndProvider backend={HTML5Backend}>
-      <CustomFieldsListWithHoCs
-        userCustomFields={userCustomFields?.data}
-        deleteUserCustomField={deleteUserCustomField}
-        reorderCustomFieldForUsers={reorderCustomFieldForUsers}
-        updateCustomFieldForUsers={updateCustomFieldForUsers}
-      />
-    </DndProvider>
+    <CustomFieldsListWithHoCs
+      userCustomFields={userCustomFields?.data}
+      deleteUserCustomField={deleteUserCustomField}
+      reorderCustomFieldForUsers={reorderCustomFieldForUsers}
+      updateCustomFieldForUsers={updateCustomFieldForUsers}
+    />
   );
 };
