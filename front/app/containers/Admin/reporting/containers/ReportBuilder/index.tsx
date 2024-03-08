d8bbed32bf8b46276537853fb032dd34520ab2a2
Alexander <alexander@citizenlab.co>
2024-03-08 14:07:12 +0100

    [TAN-1276] Add more comments

--- front/app/containers/Admin/reporting/containers/ReportBuilder/index.tsx
+++ front/app/containers/Admin/reporting/containers/ReportBuilder/index.tsx
@@ -115,6 +115,15 @@ const ReportBuilder = ({
             // But it's good enough for now.
             // Also, see reactions_by_time_widget.cy.ts#getReportLayout
             // for the current pitfalls of the `saved` state.
+            //
+            // Ideally, we should detected this `saved` state in only one way.
+            // Either always via areCraftjsObjectsEqual (probably,
+            // storing `currentNodes` state instead or `saved` state)
+            // or always via setSaved(true) (and then without detecting
+            // when nodes were changed and then changed back w/o saving).
+            // Also, we could move the states from ContentBuilderTopBar
+            // here to manage the entire state in one place and get rid of
+            // `setInterval`.
             setSaved(
               areCraftjsObjectsEqual(
                 query.getSerializedNodes(),
