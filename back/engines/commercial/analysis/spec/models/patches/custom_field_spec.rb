e1efebea9d7f408cacc8c1dbd7da7586b97e914f
Sébastien Hoorens <sebastien@citizenlab.co>
2024-03-12 15:57:43 +0100

    minor

--- back/engines/commercial/analysis/spec/models/patches/custom_field_spec.rb
+++ back/engines/commercial/analysis/spec/models/patches/custom_field_spec.rb
@@ -4,13 +4,13 @@ require 'rails_helper'
 
 RSpec.describe CustomField do
   describe 'Deleting a custom_field' do
-    it 'deletes it from the analysis' do
+    it 'removes it from the analysis (additonal field)' do
       field = create(:custom_field)
       analysis = create(:analysis, additional_custom_fields: [field])
       expect { field.destroy }.to change { analysis.additional_custom_fields.count }.from(1).to(0)
     end
 
-    it 'works (todo)' do
+    it 'removes it from the analysis (main field)' do
       field = create(:custom_field)
       analysis = create(:analysis, main_custom_field: field)
       expect(field.destroy).to be_truthy
