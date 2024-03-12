df14d94ba06fb10c33b57fece2a2d1c207fb604f
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-03-12 14:30:28 +0000

    _

--- front/cypress/e2e/report_builder/widgets/survey_question_widget.cy.ts
+++ front/cypress/e2e/report_builder/widgets/survey_question_widget.cy.ts
@@ -4,10 +4,11 @@ import moment = require('moment');
 
 describe('Survey question widget', () => {
   let projectId: string;
-  let phaseId: string;
+  let surveyPhaseId: string;
   let surveyIncluded: any;
   let surveyFields: ICustomFieldResponse[];
-  let reportId: string;
+
+  let informationPhaseId: string;
 
   const users = Array(4)
     .fill(0)
@@ -43,9 +44,13 @@ describe('Survey question widget', () => {
         });
       })
       .then((phase) => {
-        phaseId = phase.body.data.id;
+        surveyPhaseId = phase.body.data.id;
 
-        cy.apiCreateSurveyQuestions(phaseId, ['page', 'select', 'multiselect']);
+        cy.apiCreateSurveyQuestions(surveyPhaseId, [
+          'page',
+          'select',
+          'multiselect',
+        ]);
       })
       .then((response) => {
         surveyIncluded = response.body.included;
@@ -73,74 +78,129 @@ describe('Survey question widget', () => {
         });
       })
       .then(() => {
-        cy.apiCreateReportBuilder().then((report) => {
-          reportId = report.body.data.id;
+        return cy.apiCreatePhase({
+          projectId,
+          title: randomString(),
+          startAt: moment().subtract(29, 'day').format('DD/MM/YYYY'),
+          participationMethod: 'information',
         });
+      })
+      .then((phase) => {
+        informationPhaseId = phase.body.data.id;
       });
   });
 
   after(() => {
     cy.apiRemoveProject(projectId);
-    cy.apiRemoveReportBuilder(reportId);
 
     userIds.forEach((userId) => {
       cy.apiRemoveUser(userId);
     });
   });
 
-  it('handles survey question widget', () => {
-    cy.setAdminLoginCookie();
-    cy.visit(`/admin/reporting/report-builder/${reportId}/editor`);
+  describe('global report builder', () => {
+    it('works for multiselect question', () => {
+      cy.setAdminLoginCookie();
+      cy.apiCreateReportBuilder().then((report) => {
+        const reportId = report.body.data.id;
 
-    cy.get('#e2e-draggable-survey-question-result-widget').dragAndDrop(
-      '#e2e-content-builder-frame',
-      {
-        position: 'inside',
-      }
-    );
+        cy.visit(`/admin/reporting/report-builder/${reportId}/editor`);
 
-    cy.wait(1000);
+        cy.get('#e2e-draggable-survey-question-result-widget').dragAndDrop(
+          '#e2e-content-builder-frame',
+          {
+            position: 'inside',
+          }
+        );
 
-    // Select project, phase and question
-    cy.get('#e2e-report-builder-project-filter-box select').select(projectId);
-    cy.get('#e2e-phase-filter').select(phaseId);
-    cy.get('.e2e-question-select select').first().select(surveyFields[2].id);
+        cy.wait(1000);
 
-    // Check if values are correct
-    cy.get('.e2e-survey-question-ungrouped-bars')
-      .first()
-      .contains('50% (4 choices)');
+        // Select project, phase and question
+        cy.get('#e2e-report-builder-project-filter-box select').select(
+          projectId
+        );
+        cy.get('#e2e-phase-filter').select(surveyPhaseId);
+        cy.get('.e2e-question-select select')
+          .first()
+          .select(surveyFields[2].id);
 
-    cy.get('svg.e2e-progress-bar').should('have.length', 3);
-    cy.get('svg.e2e-progress-bar').first().should('have.attr', 'width', '50%');
-    cy.get('svg.e2e-progress-bar').eq(1).should('have.attr', 'width', '50%');
-    cy.get('svg.e2e-progress-bar').eq(2).should('have.attr', 'width', '0%');
+        // Check if values are correct
+        cy.get('.e2e-survey-question-ungrouped-bars')
+          .first()
+          .contains('50% (4 choices)');
 
-    // Group by gender and confirm correctness
-    cy.get('#e2e-group-mode-select').select('user_field');
-    cy.get('#e2e-user-field-select').select('Gender');
+        cy.get('svg.e2e-progress-bar').should('have.length', 3);
+        cy.get('svg.e2e-progress-bar')
+          .first()
+          .should('have.attr', 'width', '50%');
+        cy.get('svg.e2e-progress-bar')
+          .eq(1)
+          .should('have.attr', 'width', '50%');
+        cy.get('svg.e2e-progress-bar').eq(2).should('have.attr', 'width', '0%');
 
-    cy.get('svg.e2e-progress-bar').should('have.length', 5);
-    cy.get('svg.e2e-progress-bar').first().should('have.attr', 'width', '25%');
-    cy.get('svg.e2e-progress-bar').eq(1).should('have.attr', 'width', '25%');
-    cy.get('svg.e2e-progress-bar').eq(2).should('have.attr', 'width', '25%');
-    cy.get('svg.e2e-progress-bar').eq(3).should('have.attr', 'width', '25%');
-    cy.get('svg.e2e-progress-bar').eq(4).should('have.attr', 'width', '0%');
+        // Group by gender and confirm correctness
+        cy.get('#e2e-group-mode-select').select('user_field');
+        cy.get('#e2e-user-field-select').select('Gender');
 
-    // Save
-    cy.intercept('PATCH', `/web_api/v1/reports/${reportId}`).as(
-      'saveReportLayout'
-    );
-    cy.get('#e2e-content-builder-topbar-save').click();
-    cy.wait('@saveReportLayout');
+        cy.get('svg.e2e-progress-bar').should('have.length', 5);
+        cy.get('svg.e2e-progress-bar')
+          .first()
+          .should('have.attr', 'width', '25%');
+        cy.get('svg.e2e-progress-bar')
+          .eq(1)
+          .should('have.attr', 'width', '25%');
+        cy.get('svg.e2e-progress-bar')
+          .eq(2)
+          .should('have.attr', 'width', '25%');
+        cy.get('svg.e2e-progress-bar')
+          .eq(3)
+          .should('have.attr', 'width', '25%');
+        cy.get('svg.e2e-progress-bar').eq(4).should('have.attr', 'width', '0%');
 
-    // Reload page and check if values are still correct
-    cy.reload();
-    cy.get('svg.e2e-progress-bar').should('have.length', 5);
-    cy.get('svg.e2e-progress-bar').first().should('have.attr', 'width', '25%');
-    cy.get('svg.e2e-progress-bar').eq(1).should('have.attr', 'width', '25%');
-    cy.get('svg.e2e-progress-bar').eq(2).should('have.attr', 'width', '25%');
-    cy.get('svg.e2e-progress-bar').eq(3).should('have.attr', 'width', '25%');
-    cy.get('svg.e2e-progress-bar').eq(4).should('have.attr', 'width', '0%');
+        // Save
+        cy.intercept('PATCH', `/web_api/v1/reports/${reportId}`).as(
+          'saveReportLayout'
+        );
+        cy.get('#e2e-content-builder-topbar-save').click();
+        cy.wait('@saveReportLayout');
+
+        // Reload page and check if values are still correct
+        cy.reload();
+        cy.get('svg.e2e-progress-bar').should('have.length', 5);
+        cy.get('svg.e2e-progress-bar')
+          .first()
+          .should('have.attr', 'width', '25%');
+        cy.get('svg.e2e-progress-bar')
+          .eq(1)
+          .should('have.attr', 'width', '25%');
+        cy.get('svg.e2e-progress-bar')
+          .eq(2)
+          .should('have.attr', 'width', '25%');
+        cy.get('svg.e2e-progress-bar')
+          .eq(3)
+          .should('have.attr', 'width', '25%');
+        cy.get('svg.e2e-progress-bar').eq(4).should('have.attr', 'width', '0%');
+
+        cy.apiRemoveReportBuilder(reportId);
+      });
+    });
   });
+
+  // describe('phase report builder', () => {
+  //   it('works for select question', () => {
+  //     // TODO
+  //   });
+
+  //   it('works for linear scale', () => {
+
+  //   });
+
+  //   it('works for image question', () => {
+
+  //   });
+
+  //   it('is initialized with correct phase', () => {
+
+  //   });
+  // })
 });
