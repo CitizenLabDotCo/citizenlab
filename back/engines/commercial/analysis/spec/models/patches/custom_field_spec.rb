2aadf48af20498613780cfcabe05845ccb7f75c3
Sébastien Hoorens <sebastien@citizenlab.co>
2024-03-12 15:55:43 +0100

    nullify analysis when deleting main field

--- back/engines/commercial/analysis/spec/models/patches/custom_field_spec.rb
+++ back/engines/commercial/analysis/spec/models/patches/custom_field_spec.rb
@@ -9,5 +9,11 @@ RSpec.describe CustomField do
       analysis = create(:analysis, additional_custom_fields: [field])
       expect { field.destroy }.to change { analysis.additional_custom_fields.count }.from(1).to(0)
     end
+
+    it 'works (todo)' do
+      field = create(:custom_field)
+      analysis = create(:analysis, main_custom_field: field)
+      expect(field.destroy).to be_truthy
+    end
   end
 end
