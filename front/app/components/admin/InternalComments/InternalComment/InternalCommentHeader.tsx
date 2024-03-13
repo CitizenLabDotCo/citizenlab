ae7f0841a32a19dc31e67c7c71d4e839cfc03d6e
brentguf <brentguf@users.noreply.github.com>
2024-03-13 14:23:29 +0100

    Fix comment 2

--- front/app/components/admin/InternalComments/InternalComment/InternalCommentHeader.tsx
+++ front/app/components/admin/InternalComments/InternalComment/InternalCommentHeader.tsx
@@ -94,7 +94,7 @@ const InternalCommentHeader = ({
           fontSize={fontSizes.base}
           fontWeight={400}
           underline={true}
-          // We assume that if you can see an internal comment, you are a moderator or admin
+          // We assume that if you can see an internal comment, you can moderate the idea or initiative that contains it
           userCanModerate
         />
       </Left>
