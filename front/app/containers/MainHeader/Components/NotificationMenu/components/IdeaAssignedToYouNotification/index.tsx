034015b99d1ae9ef13668b263d4b8bff90886987
brentguf <brentguf@users.noreply.github.com>
2024-03-13 09:23:14 +0100

    Fix broken link in notification + get rid of user role permission checks

--- front/app/containers/MainHeader/Components/NotificationMenu/components/IdeaAssignedToYouNotification/index.tsx
+++ front/app/containers/MainHeader/Components/NotificationMenu/components/IdeaAssignedToYouNotification/index.tsx
@@ -1,7 +1,6 @@
 import React, { MouseEvent, KeyboardEvent } from 'react';
 
 import useIdeaBySlug from 'api/ideas/useIdeaBySlug';
-import useAuthUser from 'api/me/useAuthUser';
 import { IIdeaAssignedToYouNotificationData } from 'api/notifications/types';
 
 import T from 'components/T';
@@ -9,7 +8,6 @@ import T from 'components/T';
 import { FormattedMessage } from 'utils/cl-intl';
 import Link from 'utils/cl-router/Link';
 import { isNilOrError } from 'utils/helperUtils';
-import { isAdmin } from 'utils/permissions/roles';
 
 import messages from '../../messages';
 import NotificationWrapper from '../NotificationWrapper';
@@ -20,13 +18,12 @@ interface Props {
 
 const IdeaAssignedToYouNotification = ({ notification }: Props) => {
   const { data: idea } = useIdeaBySlug(notification.attributes.post_slug);
+
+  if (!idea) return null;
+
   const onClickUserName = (event: MouseEvent | KeyboardEvent) => {
     event.stopPropagation();
   };
-  const { data: authUser } = useAuthUser();
-  const projectId = idea ? idea.data.relationships.project.data.id : null;
-
-  if (!authUser || typeof projectId !== 'string') return null;
 
   const getNotificationMessage = (): JSX.Element => {
     const sharedValues = {
@@ -62,13 +59,12 @@ const IdeaAssignedToYouNotification = ({ notification }: Props) => {
     }
   };
 
+  const projectId = idea.data.relationships.project.data.id;
+  const ideaId = idea.data.id;
+
   return (
     <NotificationWrapper
-      linkTo={
-        isAdmin(authUser)
-          ? '/admin/ideas'
-          : `/admin/projects/${projectId}/ideas`
-      }
+      linkTo={`/admin/projects/${projectId}/ideas/${ideaId}`}
       timing={notification.attributes.created_at}
       icon="idea"
       isRead={!!notification.attributes.read_at}
