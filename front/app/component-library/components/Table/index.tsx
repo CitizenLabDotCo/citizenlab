570d4ff2c9858146b693d960244767d8063d2eae
brentguf <brentguf@users.noreply.github.com>
2024-03-18 14:20:24 +0100

    Fix build errors

--- front/app/component-library/components/Table/index.tsx
+++ front/app/component-library/components/Table/index.tsx
@@ -1,7 +1,15 @@
-export { default as Table, Props as TableProps } from './Table';
-export { default as Thead, Props as TheadProps } from './Thead';
-export { default as Tbody, Props as TbodyProps } from './Tbody';
-export { default as Tr, Props as TrProps } from './Tr';
-export { default as Td, Props as TdProps } from './Td';
-export { default as Th, Props as ThProps } from './Th';
-export { default as Tfoot, Props as TfootProps } from './Tfoot';
+export { default as Table } from './Table';
+export { default as Thead } from './Thead';
+export { default as Tbody } from './Tbody';
+export { default as Tr } from './Tr';
+export { default as Td } from './Td';
+export { default as Th } from './Th';
+export { default as Tfoot } from './Tfoot';
+
+export type { Props as TableProps } from './Table';
+export type { Props as TheadProps } from './Thead';
+export type { Props as TbodyProps } from './Tbody';
+export type { Props as TrProps } from './Tr';
+export type { Props as TdProps } from './Td';
+export type { Props as ThProps } from './Th';
+export type { Props as TfootProps } from './Tfoot';
