3bf04466a15484744efd87c7a21149397a7b57a2
Simon Tharby <simontharby@gmail.com>
2024-03-20 19:46:58 +0000

    [TAN-1331] A not very useful test

---
+++ back/spec/requests/rack_cors_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+describe 'Rack::Cors' do
+  it 'does not permit cross-origin requests' do
+    headers = { HTTP_ORIGIN: 'https://www.test.com' }
+    get('/web_api/v1/app_configuration', headers: headers)
+
+    expect(response.headers['Access-Control-Allow-Origin']).to be_blank
+  end
+end
