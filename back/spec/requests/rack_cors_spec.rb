fd487bdda7818f14d3c7d058b94070be1e2f91bb
Simon Tharby <simontharby@gmail.com>
2024-03-20 20:34:33 +0000

    [TAN-1331] Slightly more useful tests

--- back/spec/requests/rack_cors_spec.rb
+++ back/spec/requests/rack_cors_spec.rb
@@ -4,10 +4,45 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
 describe 'Rack::Cors' do
-  it 'does not permit cross-origin requests' do
-    headers = { HTTP_ORIGIN: 'https://www.test.com' }
-    get('/web_api/v1/app_configuration', headers: headers)
+  context 'when any origin is allowed' do
+    let(:app) do
+      Rack::Cors.new(Cl2Back::Application) do
+        allow do
+          origins '*'
+          resource '*', headers: :any, methods: %i[get post put patch delete options head]
+        end
+      end
+    end
 
-    expect(response.headers['Access-Control-Allow-Origin']).to be_blank
+    it 'allows requests from any origin' do
+      headers = {
+        'HTTP_ORIGIN' => 'https://www.some-origin.com'
+      }
+      request = Rack::MockRequest.new(app)
+      response = request.get('/web_api/v1/app_configuration', headers)
+
+      expect(response.headers['Access-Control-Allow-Origin']).to eq('*')
+    end
+  end
+
+  context 'when no origin is allowed' do
+    let(:app) do
+      Rack::Cors.new(Cl2Back::Application) do
+        allow do
+          origins []
+          resource '*', headers: :any, methods: %i[get post put patch delete options head]
+        end
+      end
+    end
+
+    it 'allows requests from any origin' do
+      headers = {
+        'HTTP_ORIGIN' => 'https://www.some-origin.com'
+      }
+      request = Rack::MockRequest.new(app)
+      response = request.get('/web_api/v1/app_configuration', headers)
+
+      expect(response.headers['Access-Control-Allow-Origin']).to be_nil
+    end
   end
 end
