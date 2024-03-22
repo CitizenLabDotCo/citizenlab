73097cda443e217d7fb40d91afea6c49dd8b9d9c
Amanda Anderson <anderson_amanda@mail.com>
2024-03-22 14:00:57 +0100

    Convert urls to RouteType

--- front/app/containers/Admin/pagesAndMenu/routes.tsx
+++ front/app/containers/Admin/pagesAndMenu/routes.tsx
@@ -44,15 +44,13 @@ const CustomPageHeroBannerForm = lazy(
 
 // path utils
 export const ADMIN_PAGES_MENU_PATH = `/admin/pages-menu`;
-const CUSTOM_PAGES_PATH = 'pages';
-const ADMIN_PAGES_MENU_CUSTOM_PAGE_PATH = `${ADMIN_PAGES_MENU_PATH}/${CUSTOM_PAGES_PATH}`;
 
 export const adminCustomPageContentPath = (pageId: string): RouteType => {
-  return `${ADMIN_PAGES_MENU_CUSTOM_PAGE_PATH}/${pageId}/content`;
+  return `/admin/pages-menu/pages/${pageId}/content`;
 };
 
 export const adminCustomPageSettingsPath = (pageId: string) => {
-  return `${ADMIN_PAGES_MENU_CUSTOM_PAGE_PATH}/${pageId}/settings`;
+  return `/admin/pages-menu/pages/${pageId}/settings`;
 };
 
 enum pagesAndMenuRoutes {
@@ -124,7 +122,7 @@ export default () => ({
       ),
     },
     {
-      path: CUSTOM_PAGES_PATH, // pages
+      path: pagesAndMenuRoutes.pages,
       element: <CustomPagesIndex />,
       children: [
         {
