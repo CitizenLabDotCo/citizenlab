789c84581ba6b4d0290748b38de03a83172d7933
Iva <iva@citizenlab.co>
2024-03-26 12:03:35 +0200

    Upgrade dependency cruiser

--- front/.dependency-cruiser.js
+++ front/.dependency-cruiser.js
@@ -33,7 +33,7 @@ module.exports = {
       from: { pathNot: 'app/component-library/' },
       to: {
         path: 'app/component-library/',
-        dependencyTypesNot: ['aliased'],
+        dependencyTypesNot: ['aliased-tsconfig'],
       },
     },
 
@@ -121,7 +121,7 @@ module.exports = {
         "That's problematic as the package either (1) won't be available on live (2 - worse) will be " +
         'available on live with an non-guaranteed version. Fix it by adding the package to the dependencies ' +
         'in your package.json.',
-      from: {},
+      from: { pathNot: 'app/component-library/' },
       to: {
         dependencyTypes: ['npm-no-pkg', 'npm-unknown'],
       },
