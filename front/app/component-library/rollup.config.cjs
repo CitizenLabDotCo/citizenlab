ebbe0c2376d54f0a6a0bfe6922ef5fa6ff627067
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-03-27 09:19:49 +0000

    Move rollup config to component library folder

--- front/app/component-library/rollup.config.cjs
+++ front/app/component-library/rollup.config.cjs
@@ -1,18 +1,18 @@
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
+import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import { dts } from 'rollup-plugin-dts';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import typescript from 'rollup-plugin-typescript';
 
-const packageJson = require('./package.json');
+const packageJson = require('../../package.json');
 
 const external = Object.keys(packageJson.dependencies || {});
 
 const config = [
   {
-    input: 'app/component-library/index.tsx',
+    input: `${__dirname}/index.tsx`,
     output: [
       {
         file: packageJson.main,
@@ -32,15 +32,15 @@ const config = [
       resolve({ preferBuiltins: true }),
       commonjs(),
       typescript({
-        tsconfig: './app/tsconfig.json',
+        tsconfig: `${__dirname}/../tsconfig.json`,
         exclude: ['**/*.test.ts', '**/*.test.tsx'],
       }),
       terser(),
     ],
   },
   {
-    input: 'app/component-library/dist/index.d.ts',
-    output: [{ file: 'app/component-library/dist/index.d.ts', format: 'esm' }],
+    input: `${__dirname}/dist/index.d.ts`,
+    output: [{ file: `${__dirname}/dist/index.d.ts`, format: 'esm' }],
     external: [/\.css$/],
     plugins: [dts()],
   },
