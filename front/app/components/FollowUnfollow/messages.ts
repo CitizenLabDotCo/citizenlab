5b77896c55c36c8575dc36e02cd8296deaff7d99
Amanda Anderson <anderson_amanda@mail.com>
2024-03-27 13:42:32 +0100

    Add tooltip to follow button and create custom message for inputs

--- front/app/components/FollowUnfollow/messages.ts
+++ front/app/components/FollowUnfollow/messages.ts
@@ -13,4 +13,22 @@ export default defineMessages({
     id: 'app.components.followUnfollow.followADiscussion',
     defaultMessage: 'Follow the discussion',
   },
+  followTooltipInputPage: {
+    id: 'app.components.followUnfollow.followTooltipInputPage',
+    defaultMessage:
+      'Following will trigger email updates about status changes, official updates, and comments. You can {unsubscribeLink} at any time.',
+  },
+  followTooltip: {
+    id: 'app.components.followUnfollow.followTooltip',
+    defaultMessage:
+      'Following will trigger email updates about changes. You can {unsubscribeLink} at any time.',
+  },
+  unsubscribe: {
+    id: 'app.components.followUnfollow.unsubscribe',
+    defaultMessage: 'unsubscribe',
+  },
+  unsubscribeUrl: {
+    id: 'app.components.followUnfollow.unsubscribeUrl',
+    defaultMessage: '/profile/edit',
+  },
 });
