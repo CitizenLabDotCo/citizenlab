65d9fe87ed3ec57f604ec8a591db7c7e747604c7
Sébastien Hoorens <sebastien@citizenlab.co>
2024-03-28 15:19:47 +0100

    make report owner optional and nullify when deleting owner

--- back/engines/commercial/report_builder/app/models/report_builder/patches/user.rb
+++ back/engines/commercial/report_builder/app/models/report_builder/patches/user.rb
@@ -3,7 +3,7 @@
 module ReportBuilder::Patches::User
   def self.included(base)
     base.class_eval do
-      has_many :reports, class_name: 'ReportBuilder::Report', foreign_key: 'owner_id', dependent: :destroy
+      has_many :reports, class_name: 'ReportBuilder::Report', foreign_key: 'owner_id', dependent: :nullify
     end
   end
 end
--- back/engines/commercial/report_builder/spec/models/report_builder/report_spec.rb
+++ back/engines/commercial/report_builder/spec/models/report_builder/report_spec.rb
@@ -6,15 +6,15 @@ RSpec.describe ReportBuilder::Report do
   subject(:report) { build(:report) }
 
   it { is_expected.to validate_uniqueness_of(:name) }
-  it { is_expected.to belong_to(:owner).class_name('User') }
+  it { is_expected.to belong_to(:owner).class_name('User').optional }
   it { is_expected.to have_one(:layout).class_name('ContentBuilder::Layout').dependent(:destroy) }
 
   describe 'user deletion' do
-    it 'deletes reports that the user owns' do
+    it 'keeps reports that the user owned' do
       report = create(:report)
       user = report.owner
       expect(user.destroy).to be_truthy
-      expect(described_class.find_by(id: report.id)).to be_nil
+      expect(report.reload.owner).to be_nil
     end
   end
 end
