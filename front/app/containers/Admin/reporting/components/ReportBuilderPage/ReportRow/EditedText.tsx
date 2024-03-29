90abbb1315b36766e083c377b255a0514b1611c5
Sébastien Hoorens <sebastien@citizenlab.co>
2024-03-29 11:03:18 +0100

    deal with missing author when listing reports

--- front/app/containers/Admin/reporting/components/ReportBuilderPage/ReportRow/EditedText.tsx
+++ front/app/containers/Admin/reporting/components/ReportBuilderPage/ReportRow/EditedText.tsx
@@ -19,19 +19,17 @@ const EditedText = ({ createdAt, updatedAt, userId }: Props) => {
   const { data: user } = useUserById(userId);
   const { formatMessage } = useIntl();
 
-  if (!user) return null;
-
   const createdOn = moment(createdAt).format('DD/MM/YYYY');
   const lastUpdateDaysAgo = moment().diff(moment(updatedAt), 'days');
 
   return (
     <Text fontSize="s" color="textSecondary" mb="0px" mt="0px">
       {formatMessage(messages.createdOn, { date: createdOn })}
-      {' • '}
-      {formatMessage(messages.lastUpdate, {
-        days: lastUpdateDaysAgo,
-        author: user.data.attributes.first_name ?? '',
-      })}
+        {user && ' • '}
+        {user && formatMessage(messages.lastUpdate, {
+          days: lastUpdateDaysAgo,
+          author: user.data.attributes.first_name ?? '',
+        })}
     </Text>
   );
 };
