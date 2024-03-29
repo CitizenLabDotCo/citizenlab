0b64b7b1caa49aee208cdf8d7b3c0aefc93eada9
Alexander <91883434+alexander-cit@users.noreply.github.com>
2024-03-29 14:29:15 +0100

    Revert "Revert "[TAN-1106] Update user email when logged in with Vienna SSO""

--- back/engines/commercial/id_vienna_saml/spec/requests/citizen_authentication_spec.rb
+++ back/engines/commercial/id_vienna_saml/spec/requests/citizen_authentication_spec.rb
@@ -14,15 +14,29 @@ describe 'Vienna SAML citizen authentication' do
   context 'when the user already exists' do
     before do
       create(:user, email: 'philipp.test@extern.wien.gv.at', first_name: 'Bob', last_name: 'Alice')
-      send_auth_request
     end
 
     it 'does not overwrite first name & last name with the data from the auth response' do
+      send_auth_request
       user = User.find_by(email: 'philipp.test@extern.wien.gv.at')
       expect(user).to have_attributes(first_name: 'Bob', last_name: 'Alice')
     end
 
+    context 'when the SAML response does not include first name and last name' do
+      before do
+        delete_raw_info_attribute('urn:oid:1.2.40.0.10.2.1.1.261.20')
+        delete_raw_info_attribute('urn:oid:2.5.4.42')
+      end
+
+      it 'does not overwrite first name & last name with empty data from the auth response' do
+        send_auth_request
+        user = User.find_by(email: 'philipp.test@extern.wien.gv.at')
+        expect(user).to have_attributes(first_name: 'Bob', last_name: 'Alice')
+      end
+    end
+
     it 'sets the JWT auth token and redirects to home page' do
+      send_auth_request
       expect(cookies[:cl2_jwt]).to be_present
       expect(response).to redirect_to('/en?')
     end
