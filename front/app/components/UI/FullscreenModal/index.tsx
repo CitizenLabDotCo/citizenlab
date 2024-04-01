18c53f6129b41c152406823f5780e6f5c33b7407
brentguf <brentguf@users.noreply.github.com>
2024-04-01 15:56:54 +0200

    Linter

--- front/app/components/UI/FullscreenModal/index.tsx
+++ front/app/components/UI/FullscreenModal/index.tsx
@@ -8,6 +8,7 @@ import CSSTransition from 'react-transition-group/CSSTransition';
 import { Subscription, fromEvent } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import styled from 'styled-components';
+import { SupportedLocale } from 'typings';
 
 import useLocale from 'hooks/useLocale';
 
@@ -15,7 +16,6 @@ import { trackPage } from 'utils/analytics';
 import clHistory from 'utils/cl-router/history';
 import { removeLocale } from 'utils/cl-router/updateLocationDescriptor';
 import { isNilOrError } from 'utils/helperUtils';
-import { SupportedLocale } from 'typings';
 
 // resource
 
