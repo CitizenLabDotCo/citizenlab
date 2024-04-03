5e3a43a21b2a826a44dfbd79bc190ddfd3e54d1f
Alexander <alexander@citizenlab.co>
2024-04-03 17:50:18 +0200

    [TAN-1480] Add tooltip to disabled PDF button

--- front/app/containers/Admin/projects/project/offlineInputs/TopBar/ImportButtons.tsx
+++ front/app/containers/Admin/projects/project/offlineInputs/TopBar/ImportButtons.tsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 
 import {
+  Badge,
   Box,
   Button,
   Dropdown,
   DropdownListItem,
+  TooltipContentWrapper,
+  colors,
 } from '@citizenlab/cl2-component-library';
+import Tippy from '@tippyjs/react';
+
+import useFeatureFlag from 'hooks/useFeatureFlag';
 
 import { FormattedMessage } from 'utils/cl-intl';
 
@@ -18,6 +24,9 @@ interface Props {
 
 const ImportButtons = ({ onClickPDFImport, onClickExcelImport }: Props) => {
   const [dropdownOpened, setDropdownOpened] = useState(false);
+  const importPrintedFormsEnabled = useFeatureFlag({
+    name: 'import_printed_forms',
+  });
 
   return (
     <Box>
@@ -36,11 +45,38 @@ const ImportButtons = ({ onClickPDFImport, onClickExcelImport }: Props) => {
         onClickOutside={() => setDropdownOpened(false)}
         content={
           <>
-            <DropdownListItem onClick={onClickPDFImport}>
-              <FormattedMessage {...messages.importPDFFile} />
-            </DropdownListItem>
             <DropdownListItem onClick={onClickExcelImport}>
               <FormattedMessage {...messages.importExcelFile} />
+            </DropdownListItem>
+            <DropdownListItem
+              onClick={importPrintedFormsEnabled ? onClickPDFImport : undefined}
+            >
+              <Tippy
+                interactive={true}
+                theme={''}
+                maxWidth={350}
+                content={
+                  <TooltipContentWrapper tippytheme="light">
+                    <FormattedMessage {...messages.disabledPDFImportTooltip} />
+                  </TooltipContentWrapper>
+                }
+              >
+                <Box
+                  display="flex"
+                  style={
+                    importPrintedFormsEnabled ? {} : { cursor: 'not-allowed' }
+                  }
+                >
+                  <Box mr="12px" alignSelf="center">
+                    <FormattedMessage {...messages.importPDFFile} />
+                  </Box>
+                  <Box alignSelf="center">
+                    <Badge color={colors.teal400}>
+                      <FormattedMessage {...messages.premium} />
+                    </Badge>
+                  </Box>
+                </Box>
+              </Tippy>
             </DropdownListItem>
           </>
         }
