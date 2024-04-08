870a8be35ca504b7e375e8342a6ed9eb77739db9
Simon Tharby <simontharby@gmail.com>
2024-04-08 15:20:01 +0100

    [TAN-1515] Fix: provide class_name for has_many :campaigns

--- back/app/models/project.rb
+++ back/app/models/project.rb
@@ -50,7 +50,7 @@ class Project < ApplicationRecord
   has_many :areas, through: :areas_projects
   has_many :groups_projects, dependent: :destroy
   has_many :groups, through: :groups_projects
-  has_many :campaigns, dependent: :destroy # TODO: move to patch in email_campaigns
+  has_many :campaigns, class_name: 'EmailCampaigns::Campaign', dependent: :destroy # TODO: move to patch in email_campaigns
 
   has_many :phases, -> { order(:start_at) }, dependent: :destroy
   has_many :events, -> { order(:start_at) }, dependent: :destroy
