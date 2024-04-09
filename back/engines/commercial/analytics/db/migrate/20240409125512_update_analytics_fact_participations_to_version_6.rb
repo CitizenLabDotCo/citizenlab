a479d924a9a237a6389563efa6ec43ea5cff7412
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-04-09 14:17:28 +0100

    _

--- back/engines/commercial/analytics/db/migrate/20240409125512_update_analytics_fact_participations_to_version_6.rb
+++ back/engines/commercial/analytics/db/migrate/20240409125512_update_analytics_fact_participations_to_version_6.rb
@@ -1,6 +1,5 @@
 class UpdateAnalyticsFactParticipationsToVersion6 < ActiveRecord::Migration[7.0]
   def change
-  
     update_view :analytics_fact_participations, version: 6, revert_to_version: 5
   end
 end
