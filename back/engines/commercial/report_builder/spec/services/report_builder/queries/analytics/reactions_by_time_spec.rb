aebb3f963181c13345814790960a3cca6ae19862
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-04-10 11:13:57 +0100

    Add more test cases to reactions_by_time

--- back/engines/commercial/report_builder/spec/services/report_builder/queries/analytics/reactions_by_time_spec.rb
+++ back/engines/commercial/report_builder/spec/services/report_builder/queries/analytics/reactions_by_time_spec.rb
@@ -6,8 +6,9 @@ RSpec.describe ReportBuilder::Queries::Analytics::ReactionsByTime do
   subject(:query) { described_class.new(build(:user)) }
 
   describe '#run_query' do
-    let_it_be(:date) { Date.new(2022, 9, 1) }
     let_it_be(:project) { create(:project) }
+
+    let_it_be(:date) { Date.new(2022, 9, 1) }
     let_it_be(:activity) do
       create(:dimension_date, date: date)
       create(
@@ -19,6 +20,40 @@ RSpec.describe ReportBuilder::Queries::Analytics::ReactionsByTime do
         payload: {
           reactable_type: 'Idea',
           reactable_id: SecureRandom.uuid
+        }
+      )
+    end
+
+    let_it_be(:idea) { create(:idea, project: project) }
+
+    let_it_be(:date2) { Date.new(2022, 9, 5) }
+    let_it_be(:activity) do
+      create(:dimension_date, date: date2)
+      create(
+        :activity,
+        project_id: project.id,
+        acted_at: date2,
+        item_type: 'Reaction',
+        action: 'idea_disliked',
+        payload: {
+          reactable_type: 'Idea',
+          reactable_id: idea.id
+        }
+      )
+    end
+
+    let_it_be(:date3) { Date.new(2022, 9, 8) }
+    let_it_be(:activity) do
+      create(:dimension_date, date: date3)
+      create(
+        :activity,
+        project_id: project.id,
+        acted_at: date3,
+        item_type: 'Reaction',
+        action: 'idea_disliked',
+        payload: {
+          reactable_type: 'Idea',
+          reactable_id: idea.id
         }
       )
     end
@@ -36,6 +71,52 @@ RSpec.describe ReportBuilder::Queries::Analytics::ReactionsByTime do
           ],
           [],
           [{ 'count_reactable_id' => 1 }]
+        ]
+      )
+    end
+
+    it 'returns reactions by time with dislikes' do
+      params = { start_at: date - 1.day, end_at: date + 5.days, project_id: project.id }
+      expect(query.run_query(**params)).to eq(
+        [
+          [
+            {
+              'count_reactable_id' => 1,
+              'dimension_date_created.month' => '2022-09',
+              'first_dimension_date_created_date' => date
+            }
+          ],
+          [
+            {
+              'count_reactable_id' => 1,
+              'dimension_date_created.month' => '2022-09',
+              'first_dimension_date_created_date' => date2
+            }
+          ],
+          [{ 'count_reactable_id' => 2 }]
+        ]
+      )
+    end
+
+    it 'dedupes duplicate reactions' do
+      params = { start_at: date - 1.day, end_at: date + 10.days, project_id: project.id }
+      expect(query.run_query(**params)).to eq(
+        [
+          [
+            {
+              'count_reactable_id' => 1,
+              'dimension_date_created.month' => '2022-09',
+              'first_dimension_date_created_date' => date
+            }
+          ],
+          [
+            {
+              'count_reactable_id' => 1,
+              'dimension_date_created.month' => '2022-09',
+              'first_dimension_date_created_date' => date2
+            }
+          ],
+          [{ 'count_reactable_id' => 2 }]
         ]
       )
     end
