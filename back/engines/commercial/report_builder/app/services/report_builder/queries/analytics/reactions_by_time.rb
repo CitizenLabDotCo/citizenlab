529e51e2c13ce20917bd17d448b893199528caa1
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-04-10 11:06:08 +0100

    Fix reactions_by_time_spec

--- back/engines/commercial/report_builder/app/services/report_builder/queries/analytics/reactions_by_time.rb
+++ back/engines/commercial/report_builder/app/services/report_builder/queries/analytics/reactions_by_time.rb
@@ -3,18 +3,31 @@ module ReportBuilder
     protected
 
     def query(start_at: nil, end_at: nil, project_id: nil, resolution: nil, **_other_props)
-      time_series_query = {
+      likes_time_series_query = {
         fact: 'participation',
         filters: {
           **date_filter('dimension_date_created', start_at, end_at),
           **project_filter('dimension_project_id', project_id),
-          reactable_type: 'Idea'
+          action_type: 'idea_liked'
         },
         groups: "dimension_date_created.#{interval(resolution)}",
         aggregations: {
           'dimension_date_created.date': 'first',
-          likes_count: 'sum',
-          dislikes_count: 'sum'
+          reactable_id: 'count'
+        }
+      }
+
+      dislikes_time_series_query = {
+        fact: 'participation',
+        filters: {
+          **date_filter('dimension_date_created', start_at, end_at),
+          **project_filter('dimension_project_id', project_id),
+          action_type: 'idea_disliked'
+        },
+        groups: "dimension_date_created.#{interval(resolution)}",
+        aggregations: {
+          'dimension_date_created.date': 'first',
+          reactable_id: 'count'
         }
       }
 
@@ -26,11 +39,11 @@ module ReportBuilder
           reactable_type: 'Idea'
         },
         aggregations: {
-          reactions_count: 'sum'
+          reactable_id: 'count'
         }
       }
 
-      [time_series_query, posts_by_time_total]
+      [likes_time_series_query, dislikes_time_series_query, posts_by_time_total]
     end
   end
 end
