96a07a2cc26b62380179f5c142708005964262ad
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-04-10 15:05:29 +0100

    Fix FE types

--- front/app/components/admin/GraphCards/ReactionsByTimeCard/generateEmptyData.ts
+++ front/app/components/admin/GraphCards/ReactionsByTimeCard/generateEmptyData.ts
@@ -3,15 +3,15 @@ import { Moment } from 'moment';
 import { emptyDateRange } from 'components/admin/GraphCards/_utils/timeSeries';
 import { IResolution } from 'components/admin/ResolutionControl';
 
-import { getEmptyRow } from './useReactionsByTime/parse';
-
 export const generateEmptyData = (
   startAtMoment: Moment | null | undefined,
   endAtMoment: Moment | null | undefined,
   resolution: IResolution
 ) => {
-  return emptyDateRange(startAtMoment, endAtMoment, resolution, (date, i) => ({
-    ...getEmptyRow(date),
-    ...(i === 0 ? { inputs: 10 } : {}),
+  return emptyDateRange(startAtMoment, endAtMoment, resolution, (date) => ({
+    date: date.format('YYYY-MM-DD'),
+    likes: 0,
+    dislikes: 0,
+    total: 0,
   }));
 };
