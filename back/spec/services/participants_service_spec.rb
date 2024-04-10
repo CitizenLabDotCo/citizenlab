41eb2a3bf97eca8a669aa5ac3e96987f6f3d7e09
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-04-10 21:05:32 +0100

    Fix participants_service_spec

--- back/spec/services/participants_service_spec.rb
+++ back/spec/services/participants_service_spec.rb
@@ -262,15 +262,12 @@ describe ParticipantsService do
       idea_disliked_activity = create(:idea_disliked_activity)
       initiative_liked_activity =
         create(:activity, action: 'initiative_liked', item: create(:reaction2, :for_initiative, :up))
-      initiative_disliked_activity =
-        create(:activity, action: 'initiative_disliked', item: create(:reaction2, :for_initiative, :down))
 
       expect(service.filter_engaging_activities(Activity.all))
         .to match_array [
           idea_liked_activity,
           idea_disliked_activity,
-          initiative_liked_activity,
-          initiative_disliked_activity
+          initiative_liked_activity
         ]
     end
 
@@ -287,42 +284,6 @@ describe ParticipantsService do
     it 'filters out an idea changed title activity' do
       create(:changed_title_activity)
       expect(service.filter_engaging_activities(Activity.all)).to be_empty
-    end
-  end
-
-  describe 'with_engagement_scores' do
-    it 'gives idea publishing a score of 5' do
-      activity = create(:published_activity)
-      expect(service.with_engagement_scores(Activity.where(id: activity.id)).first.score).to eq 5
-    end
-
-    it 'gives comment creation a score of 3' do
-      activity = create(:comment_created_activity)
-      expect(service.with_engagement_scores(Activity.where(id: activity.id)).first.score).to eq 3
-    end
-
-    it 'gives idea reacting a score of 1' do
-      like_activity = create(:idea_liked_activity)
-      dislike_activity = create(:idea_disliked_activity)
-      expect(service.with_engagement_scores(Activity.where(id: like_activity.id)).first.score).to eq 1
-      expect(service.with_engagement_scores(Activity.where(id: dislike_activity.id)).first.score).to eq 1
-    end
-
-    it 'gives comment reactions a score of 1' do
-      like_activity = create(:comment_liked_activity)
-      expect(service.with_engagement_scores(Activity.where(id: like_activity.id)).first.score).to eq 1
-    end
-
-    it 'returns 0 for non-engaging activities' do
-      activity = create(:changed_body_activity)
-      expect(service.with_engagement_scores(Activity.where(id: activity.id)).first.score).to eq 0
-    end
-
-    it 'allows adding other select fields to the query' do
-      activity = create(:published_activity)
-      scope = service.with_engagement_scores(Activity.where(id: activity.id).select(:user_id))
-      expect(scope.first.user_id).to eq activity.user_id
-      expect(scope.first.score).to be_present
     end
   end
 end
