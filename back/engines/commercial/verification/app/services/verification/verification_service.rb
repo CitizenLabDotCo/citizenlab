d0ced1fc67eedf8e74d18fc21c1c44664d290a3e
Alexander <alexander@citizenlab.co>
2024-04-12 11:57:36 +0200

    [TAN-866] Create new verification every time (CR fix)

--- back/engines/commercial/verification/app/services/verification/verification_service.rb
+++ back/engines/commercial/verification/app/services/verification/verification_service.rb
@@ -112,19 +112,17 @@ module Verification
         end
       end
 
-      verification = ::Verification::Verification.find_or_initialize_by(
+      verification = ::Verification::Verification.new(
         method_name: method_name,
         hashed_uid: hashed_uid(uid, method_name),
         user: user,
         active: true
       )
 
-      if verification.new_record?
-        @sfxv_service.before_create(verification, user)
-        ActiveRecord::Base.transaction do
-          verification.save!
-          @sfxv_service.after_create(verification, user)
-        end
+      @sfxv_service.before_create(verification, user)
+      ActiveRecord::Base.transaction do
+        verification.save!
+        @sfxv_service.after_create(verification, user)
       end
 
       verification
