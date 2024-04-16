e4a83e3f0b65e0aba4664b7e6a315fc0b607c751
Simon Tharby <simontharby@gmail.com>
2024-04-16 16:37:14 +0100

    [TAN-1575] Fix test, by using view_private_attributes: true

--- back/spec/services/xlsx_service_spec.rb
+++ back/spec/services/xlsx_service_spec.rb
@@ -107,7 +107,7 @@ describe XlsxService do
 
   describe 'generate_attendees_xlsx' do
     let(:users) { create_list(:user, 5) }
-    let(:xlsx) { service.generate_attendees_xlsx(users) }
+    let(:xlsx) { service.generate_attendees_xlsx(users, view_private_attributes: true) }
     let(:workbook) { RubyXL::Parser.parse_buffer(xlsx) }
     let(:worksheet) { workbook.worksheets[0] }
 
