452b35730aa3dac2d44be126ba378adf1d86e13a
jamesspeake <james@speake.org>
2024-04-16 14:29:25 +0100

    [TAN-1482] Added locale for print description

--- back/app/models/custom_field.rb
+++ back/app/models/custom_field.rb
@@ -295,7 +295,13 @@ class CustomField < ApplicationRecord
   def linear_scale_print_description(locale)
     return nil unless linear_scale?
 
-    "Please write a number between 1 (#{minimum_label_multiloc[locale]}) and #{maximum} (#{maximum_label_multiloc[locale]})"
+    min_label = '1' + (minimum_label_multiloc[locale].present? ? " (#{minimum_label_multiloc[locale]})" : '')
+    max_label = maximum.to_s + (maximum_label_multiloc[locale].present? ? " (#{maximum_label_multiloc[locale]})" : '')
+    I18n.t(
+      'form_builder.pdf_export.linear_scale_print_description',
+      min_label: min_label,
+      max_label: max_label,
+    )
   end
 
   private
