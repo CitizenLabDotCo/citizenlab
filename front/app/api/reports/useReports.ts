0c460916ad771f278847a9d1ece08044c8897957
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-04-18 13:41:21 +0100

    Add new params to hook

--- front/app/api/reports/useReports.ts
+++ front/app/api/reports/useReports.ts
@@ -4,19 +4,20 @@ import { CLErrors } from 'typings';
 import fetcher from 'utils/cl-react-query/fetcher';
 
 import reportsKeys from './keys';
-import { ReportsResponse, ReportsKeys } from './types';
+import { ReportsParams, ReportsResponse, ReportsKeys } from './types';
 
-const fetchReports = () => {
+const fetchReports = (queryParams: ReportsParams) => {
   return fetcher<ReportsResponse>({
     path: '/reports',
     action: 'get',
+    queryParams,
   });
 };
 
-const useReports = () => {
+const useReports = (queryParams: ReportsParams = {}) => {
   return useQuery<ReportsResponse, CLErrors, ReportsResponse, ReportsKeys>({
-    queryKey: reportsKeys.lists(),
-    queryFn: () => fetchReports(),
+    queryKey: reportsKeys.list(queryParams),
+    queryFn: () => fetchReports(queryParams),
   });
 };
 
