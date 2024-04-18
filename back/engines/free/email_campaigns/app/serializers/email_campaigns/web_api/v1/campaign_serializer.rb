ba9bffa57304d1f78650b133f36cfa1d01e7cf97
Simon Tharby <simontharby@gmail.com>
2024-04-18 16:49:12 +0100

    [TAN-1515] Remove resource_id from CampaignSerializer

--- back/engines/free/email_campaigns/app/serializers/email_campaigns/web_api/v1/campaign_serializer.rb
+++ back/engines/free/email_campaigns/app/serializers/email_campaigns/web_api/v1/campaign_serializer.rb
@@ -3,7 +3,7 @@
 module EmailCampaigns
   class WebApi::V1::CampaignSerializer < ::WebApi::V1::BaseSerializer
     extend GroupOrderingHelper
-    attributes :created_at, :updated_at, :resource_id
+    attributes :created_at, :updated_at
 
     attribute :campaign_name do |object|
       object.class.campaign_name
