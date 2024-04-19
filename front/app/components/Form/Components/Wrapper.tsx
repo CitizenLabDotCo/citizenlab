2b4ec0a004310102d10e9db5661547ced4a853fb
brentguf <brentguf@users.noreply.github.com>
2024-04-19 12:44:26 +0200

    Declutter heightStyles Wrapper

--- front/app/components/Form/Components/Wrapper.tsx
+++ front/app/components/Form/Components/Wrapper.tsx
@@ -16,25 +16,33 @@ interface Props {
 const Wrapper = ({ id, layoutType, isSurvey, children }: Props) => {
   const isSmallerThanPhone = useBreakpoint('phone');
 
+  const heightStyles = {
+    inline: {
+      minHeight: '100%',
+      height: '100%',
+      maxHeight: 'auto',
+    },
+    fullpage: isSmallerThanPhone
+      ? {
+          minHeight: isSurvey
+            ? '100%'
+            : `calc(100vh - ${stylingConsts.menuHeight}px)`,
+          height: '100%',
+          maxHeight: 'auto',
+        }
+      : {
+          minHeight: '100%',
+          height: '100%',
+          maxHeight: isSurvey
+            ? 'auto'
+            : `calc(100vh - ${stylingConsts.menuHeight}px)`,
+        },
+  }[layoutType];
+
   return (
     <Box
       as="form"
-      minHeight={
-        isSmallerThanPhone && layoutType === 'fullpage' && !isSurvey
-          ? `calc(100vh - ${stylingConsts.menuHeight}px)`
-          : '100%'
-      }
-      height={
-        !isSmallerThanPhone && layoutType === 'fullpage' && !isSurvey
-          ? '100vh'
-          : '100%'
-      }
-      maxHeight={
-        layoutType === 'inline' ||
-        (layoutType === 'fullpage' && (isSmallerThanPhone || isSurvey))
-          ? 'auto'
-          : `calc(100vh - ${stylingConsts.menuHeight}px)`
-      }
+      {...heightStyles}
       display="flex"
       flexDirection="column"
       id={id}
