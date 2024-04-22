2aab112e14b35b29c10707f2df9b3eab38c60d15
Simon Tharby <simontharby@gmail.com>
2024-04-22 19:52:34 +0100

    [TAN-1665] use env.remote_ip not env.ip

--- back/config/initializers/rack_attack.rb
+++ back/config/initializers/rack_attack.rb
@@ -110,14 +110,14 @@ class Rack::Attack
   # Resend code by IP.
   throttle('user/resend_code', limit: 10, period: 5.minutes) do |req|
     if req.path == '/web_api/v1/user/resend_code' && req.post?
-      req.ip
+      req.remote_ip
     end
   end
 
   # Confirm by IP.
   throttle('user/confirm/ip', limit: 5, period: 20.seconds) do |req|
     if req.path == '/web_api/v1/user/confirm' && req.post?
-      req.ip
+      req.remote_ip
     end
   end
 
