32f97f38adc232a6f44b031caa016571fbc4abda
Iva <iva@citizenlab.co>
2024-04-23 11:53:56 +0300

    Fix e2e tests

--- front/cypress/e2e/admin/projects/copy_project.cy.ts
+++ front/cypress/e2e/admin/projects/copy_project.cy.ts
@@ -26,7 +26,7 @@ describe('Copy projects outside folder', () => {
   });
 
   it('allows user to copy project', () => {
-    cy.visit('/admin/projects/');
+    cy.visit('/admin/projects/all');
     cy.acceptCookies();
 
     cy.get('#e2e-admin-projects-list-unsortable')
@@ -101,7 +101,7 @@ describe('Copy projects inside folder', () => {
       folderId = folder.body.data.id;
       cy.apiAddProjectsToFolder([projectId], folderId);
 
-      cy.visit('/admin/projects/');
+      cy.visit('/admin/projects/all');
       cy.acceptCookies();
 
       cy.get('#e2e-admin-projects-list-unsortable')
