881f85917f212a07d6b58d65bd925d74503b3590
Simon Tharby <simontharby@gmail.com>
2024-04-26 16:40:06 +0100

    [TAN-1716] Add 14 more columns

--- back/app/services/xlsx_service.rb
+++ back/app/services/xlsx_service.rb
@@ -288,13 +288,16 @@ class XlsxService
   private
 
   def generate_phase_ideas_votes_sheet(workbook, sheet_name, phase)
+    url_service = Frontend::UrlService.new
     ideas = phase.ideas
     method = phase.voting_method
     t_scope = 'xlsx_export.column_headers'
 
-    columns = [{ header: I18n.t('title', scope: t_scope),
-                 f: ->(i) { multiloc_service.t(i.title_multiloc) },
-                 skip_sanitization: true }]
+    columns = [
+      { header: I18n.t('input_id', scope: t_scope),    f: ->(i) { i.id }, skip_sanitization: true },
+      { header: I18n.t('title', scope: t_scope),       f: ->(i) { multiloc_service.t(i.title_multiloc) } },
+      { header: I18n.t('description', scope: t_scope), f: ->(i) { multiloc_service.t(i.body_multiloc) } }
+    ]
 
     columns += if method == 'budgeting'
       [
@@ -311,13 +314,26 @@ class XlsxService
          skip_sanitization: true }]
     end
 
-    puts "method_inspect: #{method.inspect}"
-
     if method == 'multiple_voting'
       columns << { header: I18n.t('participants', scope: t_scope),
                    f: ->(i) { i.baskets_ideas.joins(:basket).where(basket: { phase_id: phase.id }).where.not(basket: { submitted_at: nil }).size },
                    skip_sanitization: true }
     end
+
+    columns += [
+      { header: I18n.t('input_url', scope: t_scope),       f: ->(i) { url_service.model_to_url(i) }, skip_sanitization: true },
+      { header: I18n.t('attachments', scope: t_scope),     f: ->(i) { i.idea_files.map { |f| f.file.url }.join("\n") }, skip_sanitization: true, width: 2 },
+      { header: I18n.t('tags', scope: t_scope),            f: ->(i) { i.topics.map { |t| multiloc_service.t(t.title_multiloc) }.join(',') }, skip_sanitization: true },
+      { header: I18n.t('latitude', scope: t_scope),        f: ->(i) { i.location_point&.coordinates&.last }, skip_sanitization: true },
+      { header: I18n.t('longitude', scope: t_scope),       f: ->(i) { i.location_point&.coordinates&.first }, skip_sanitization: true },
+      { header: I18n.t('location', scope: t_scope),        f: ->(i) { i.location_description } },
+      { header: I18n.t('project', scope: t_scope),         f: ->(i) { multiloc_service.t(i&.project&.title_multiloc) } },
+      { header: I18n.t('status', scope: t_scope),          f: ->(i) { multiloc_service.t(i&.idea_status&.title_multiloc) } },
+      { header: I18n.t('author_fullname', scope: t_scope), f: ->(i) { format_author_name i } },
+      { header: I18n.t('author_email', scope: t_scope),    f: ->(i) { i.author&.email } },
+      { header: I18n.t('author_id', scope: t_scope),       f: ->(i) { i.author_id } },
+      { header: I18n.t('published_at', scope: t_scope),    f: ->(i) { i.published_at }, skip_sanitization: true }
+    ]
 
     generate_sheet workbook, sheet_name, columns, ideas
   end
