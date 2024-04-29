d193019b5d423b99ce75bdc22c269482fbd445fe
jamesspeake <james@speake.org>
2024-04-29 10:57:08 +0100

    [TAN-1711] PR fix

--- back/engines/commercial/bulk_import_ideas/app/jobs/bulk_import_ideas/idea_pdf_import_job.rb
+++ back/engines/commercial/bulk_import_ideas/app/jobs/bulk_import_ideas/idea_pdf_import_job.rb
@@ -4,12 +4,12 @@ module BulkImportIdeas
   class IdeaPdfImportJob < ApplicationJob
     self.priority = 60
 
-    def run(files, import_user, locale, phase, personal_data_enabled)
+    def run(idea_import_files, import_user, locale, phase, personal_data_enabled)
       file_parser = BulkImportIdeas::Parsers::IdeaPdfFileParser.new(import_user, locale, phase.id, personal_data_enabled)
       import_service = BulkImportIdeas::Importers::IdeaImporter.new(import_user, locale)
 
       idea_rows = []
-      files.each do |file|
+      idea_import_files.each do |file|
         idea_rows += file_parser.parse_rows file
       end
 
