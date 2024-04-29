0500c3e04063a7bcb55fc2a1c5dee1c7051cd491
Simon Tharby <simontharby@gmail.com>
2024-04-29 19:23:04 +0100

    POC: Somewhat inelegant, but working, ordering query

--- back/app/controllers/web_api/v1/admin_publications_controller.rb
+++ back/app/controllers/web_api/v1/admin_publications_controller.rb
@@ -9,8 +9,24 @@ class WebApi::V1::AdminPublicationsController < ApplicationController
     publications = policy_scope(AdminPublication.includes(:parent))
     publications = publication_filterer.filter(publications, params.merge(current_user: current_user))
 
+    publications = publications.select(
+      'admin_publications.*',
+      'CASE WHEN admin_publications.parent_id IS NOT NULL
+      THEN parents.ordering + (admin_publications.ordering / 10.0) + 0.05
+      ELSE admin_publications.ordering
+      END AS compound_ordering'
+    )
+      .joins('LEFT OUTER JOIN admin_publications AS parents ON parents.id = admin_publications.parent_id')
+      .order('compound_ordering ASC')
+
+    # Ruby non-sql version (just for testing)
+    # compound_ordering = publications.index_with { |p| p&.parent ? p.parent.ordering + (p.ordering / 10.0) + 0.05 : p.ordering }
+    # publications = compound_ordering.sort_by { |_k, v| v }.to_h.keys
+    # publications.each { |p| puts p.publication.title_multiloc['en'] } # <= 'correctly' ordered
+    # publications = AdminPublication.where(id: publications.map(&:id)) # This loses the ordering, obviously
+
     @publications = publications.includes(:publication, :children)
-      .order(:ordering)
+
     @publications = paginate @publications
 
     render json: linked_json(
