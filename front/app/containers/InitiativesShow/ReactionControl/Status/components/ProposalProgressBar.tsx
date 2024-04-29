3e5a764e9186437259dbfcb2af552f7b869163ee
EdwinKato <katedwin88@gmail.com>
2024-04-29 11:41:38 +0300

    Fix border

--- front/app/containers/InitiativesShow/ReactionControl/Status/components/ProposalProgressBar.tsx
+++ front/app/containers/InitiativesShow/ReactionControl/Status/components/ProposalProgressBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { colors, Box } from '@citizenlab/cl2-component-library';
-import { useTheme } from 'styled-components';
+import styled, { useTheme } from 'styled-components';
 
 import ProgressBar from 'components/UI/ProgressBar';
 
@@ -9,6 +9,13 @@ import { ScreenReaderOnly } from 'utils/a11y';
 import { FormattedMessage } from 'utils/cl-intl';
 
 import messages from '../../messages';
+
+const StyledProgressBar = styled(ProgressBar)`
+  height: 12px;
+  width: 100%;
+  border: 1px solid ${colors.primary};
+  border-radius: 4px;
+`;
 
 interface Props {
   reactionCount: number;
@@ -29,19 +36,12 @@ const ProposalProgressBar = ({
 
   return (
     <div className={className}>
-      <Box
-        height="12px"
-        width="100%"
-        border={`1px solid ${colors.primary}`}
-        borderRadius="4px"
-      >
-        <ProgressBar
-          progress={reactionCount / reactionLimit}
-          color={barColor || theme.colors.tenantText}
-          bgColor={colors.grey200}
-          bgShaded={bgShaded}
-        />
-      </Box>
+      <StyledProgressBar
+        progress={reactionCount / reactionLimit}
+        color={barColor || theme.colors.tenantText}
+        bgColor={colors.grey200}
+        bgShaded={bgShaded}
+      />
       <ScreenReaderOnly>
         <FormattedMessage
           {...messages.a11y_xVotesOfRequiredY}
