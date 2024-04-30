1f65423a8b8788adb977ba8f57907a92221b212a
brentguf <brentguf@users.noreply.github.com>
2024-04-30 11:12:34 +0200

    Move Modal outside

--- front/app/containers/IdeasNewPage/IdeasNewSurveyForm/SurveyHeading.tsx
+++ front/app/containers/IdeasNewPage/IdeasNewSurveyForm/SurveyHeading.tsx
@@ -135,46 +135,45 @@ const SurveyHeading = ({
             </Box>
           </Box>
         </Box>
-
-        <Modal opened={showLeaveModal} close={closeModal}>
-          <Box display="flex" flexDirection="column" width="100%" p="20px">
-            <Box mb="40px">
-              <Title variant="h3" color="primary">
-                <FormattedMessage {...messages.leaveFormConfirmationQuestion} />
-              </Title>
-              <Text color="primary" fontSize="l">
-                <FormattedMessage
-                  {...(loggedIn
-                    ? messages.leaveFormTextLoggedIn
-                    : messages.leaveSurveyText)}
-                />
-              </Text>
-            </Box>
-            <Box
-              display="flex"
-              flexDirection={isSmallerThanPhone ? 'column' : 'row'}
-              width="100%"
-              alignItems="center"
-              gap="20px"
-            >
-              <Button buttonStyle="secondary" width="100%" onClick={closeModal}>
-                <FormattedMessage {...messages.cancelLeaveSurveyButtonText} />
-              </Button>
-              <Button
-                icon={loggedIn ? 'arrow-left-circle' : 'delete'}
-                data-cy="e2e-confirm-delete-survey-results"
-                buttonStyle={loggedIn ? 'primary' : 'delete'}
-                width="100%"
-                mb={isSmallerThanPhone ? '16px' : undefined}
-                linkTo={`/projects/${project.attributes.slug}`}
-              >
-                <FormattedMessage {...messages.confirmLeaveFormButtonText} />
-              </Button>
-            </Box>
-          </Box>
-        </Modal>
       </Box>
-    </Box>
+      <Modal opened={showLeaveModal} close={closeModal}>
+        <Box display="flex" flexDirection="column" width="100%" p="20px">
+          <Box mb="40px">
+            <Title variant="h3" color="primary">
+              <FormattedMessage {...messages.leaveFormConfirmationQuestion} />
+            </Title>
+            <Text color="primary" fontSize="l">
+              <FormattedMessage
+                {...(loggedIn
+                  ? messages.leaveFormTextLoggedIn
+                  : messages.leaveSurveyText)}
+              />
+            </Text>
+          </Box>
+          <Box
+            display="flex"
+            flexDirection={isSmallerThanPhone ? 'column' : 'row'}
+            width="100%"
+            alignItems="center"
+            gap="20px"
+          >
+            <Button buttonStyle="secondary" width="100%" onClick={closeModal}>
+              <FormattedMessage {...messages.cancelLeaveSurveyButtonText} />
+            </Button>
+            <Button
+              icon={loggedIn ? 'arrow-left-circle' : 'delete'}
+              data-cy="e2e-confirm-delete-survey-results"
+              buttonStyle={loggedIn ? 'primary' : 'delete'}
+              width="100%"
+              mb={isSmallerThanPhone ? '16px' : undefined}
+              linkTo={`/projects/${project.attributes.slug}`}
+            >
+              <FormattedMessage {...messages.confirmLeaveFormButtonText} />
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+    </>
   );
 };
 
