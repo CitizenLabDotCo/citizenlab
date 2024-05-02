2d082b89e3ac8a4d98c82e5f4b6245a597dee2b7
brentguf <brentguf@users.noreply.github.com>
2024-05-02 17:14:58 +0200

    Use same "border" for survey header as in main nav on mobile

--- front/app/containers/IdeasNewPage/IdeasNewSurveyForm/SurveyHeading.tsx
+++ front/app/containers/IdeasNewPage/IdeasNewSurveyForm/SurveyHeading.tsx
@@ -76,7 +76,7 @@ const SurveyHeading = ({ titleText }: Props) => {
             : `${stylingConsts.menuHeight}px`
         }
         px="24px"
-        borderBottom={`1px solid ${colors.divider}`}
+        boxShadow={'0px 2px 4px -1px rgba(0, 0, 0, 0.1)'}
       >
         <StyledSurveyTitle
           color={'tenantPrimary'}
