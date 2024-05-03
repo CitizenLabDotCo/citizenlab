e3966f57f10aac2f2551b64a69e9ba463a3367c4
Simon Tharby <simontharby@gmail.com>
2024-05-03 10:47:08 +0100

    [TAN-1664] Remove duplicate code from class that is inherited from parent

--- back/app/services/xlsx_export/project_baskets_votes_generator.rb
+++ back/app/services/xlsx_export/project_baskets_votes_generator.rb
@@ -37,32 +37,5 @@ module XlsxExport
 
       xlsx_service.generate_sheet workbook, sheet_name, columns, baskets
     end
-
-    def add_suffix_to_duplicate_titles(collection)
-      objects_to_titles = collection.map { |obj| { obj.id => multiloc_service.t(obj.title_multiloc) } }
-      titles = objects_to_titles.map(&:values).flatten
-      duplicated_titles = titles.select { |title| titles.count(title) > 1 }.uniq
-
-      duplicated_titles.each do |title|
-        suffix = 1
-
-        objects_to_titles.each do |hash|
-          next unless hash.value?(title)
-
-          hash[hash.keys.first] = "#{title} (#{suffix})"
-          suffix += 1
-        end
-      end
-
-      objects_to_titles
-    end
-
-    def multiloc_service
-      @multiloc_service ||= MultilocService.new
-    end
-
-    def xlsx_service
-      @xlsx_service ||= XlsxService.new
-    end
   end
 end
