1f55e96e431c7274ab6aca08460508a870d23b17
Adrien Dessy <adrien@citizenlab.co>
2024-05-08 10:14:12 +0200

    [TAN-1521] Add tooltip with exact report modification time

--- front/app/containers/Admin/reporting/components/ReportBuilderPage/ReportRow/EditedText.tsx
+++ front/app/containers/Admin/reporting/components/ReportBuilderPage/ReportRow/EditedText.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Box, Text } from '@citizenlab/cl2-component-library';
 import moment from 'moment';
+import Tippy from '@tippyjs/react';
 
 import Avatar from 'components/Avatar';
 
@@ -23,21 +24,38 @@ const T = ({ children }) => (
   </Text>
 );
 
-const EditedText = ({ createdAt, updatedAt, userId }: Props) => {
-  const { data: user } = useUserById(userId);
+const UpdatedOnMessage = ({ updatedAt }) => {
   const { formatMessage } = useIntl();
 
-  const createdOn = moment(createdAt).format('DD/MM/YYYY');
+  const updatedDatetime = moment(updatedAt).format('YYYY-MM-DD HH:mm:ss');
   const lastUpdateDaysAgo = moment().diff(moment(updatedAt), 'days');
   const updatedOn = formatMessage(messages.lastUpdate, {
     days: lastUpdateDaysAgo,
   });
 
   return (
+    <Tippy
+      content={updatedDatetime}
+      delay={500}
+      placement="right"
+      arrow={false}
+    >
+      <Box>
+        <T>{updatedOn}</T>
+      </Box>
+    </Tippy>
+  );
+};
+
+const EditedText = ({ createdAt, updatedAt, userId }: Props) => {
+  const { data: user } = useUserById(userId);
+  const createdOn = moment(createdAt).format('DD/MM/YYYY');
+
+  return (
     <Box display="flex" alignItems="center">
       <T>{`${createdOn} • `}</T>
       {userId && user && <Avatar userId={userId} size={20} isLinkToProfile />}
-      <T>{updatedOn}</T>
+      <UpdatedOnMessage updatedAt={updatedAt} />
     </Box>
   );
 };
