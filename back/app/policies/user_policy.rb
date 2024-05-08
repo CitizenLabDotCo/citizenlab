40e8ba5ff5a0d0aa30f3e708640ce00e68aea7fd
Sébastien Hoorens <sebastien@citizenlab.co>
2024-05-08 11:18:29 +0200

    apply custom_field_values_params to users endpoint

--- back/app/policies/user_policy.rb
+++ back/app/policies/user_policy.rb
@@ -119,10 +119,7 @@ class UserPolicy < ApplicationPolicy
   private
 
   def allowed_custom_field_keys
-    allowed_fields = allowed_custom_fields
-    simple_keys = allowed_fields.support_single_value.pluck(:key).map(&:to_sym)
-    array_keys = allowed_fields.support_multiple_values.pluck(:key).map(&:to_sym)
-    [*simple_keys, array_keys.index_with { |_k| [] }]
+    CustomFieldsParamsService.new.custom_field_values_params allowed_custom_fields
   end
 
   def allowed_custom_fields
