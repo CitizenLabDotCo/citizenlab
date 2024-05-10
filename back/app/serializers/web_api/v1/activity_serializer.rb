0fd954d291617ee71ca788c2d6f30d23da67c1ed
Simon Tharby <simontharby@gmail.com>
2024-05-10 12:27:34 +0100

    [TAN-1084] Serialize item_type, item_id, project_id

--- back/app/serializers/web_api/v1/activity_serializer.rb
+++ back/app/serializers/web_api/v1/activity_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class WebApi::V1::ActivitySerializer < WebApi::V1::BaseSerializer
-  attributes :action, :acted_at
+  attributes :action, :acted_at, :item_type, :item_id, :project_id
 
   attribute :change do |object|
     object.payload&.dig('change')
