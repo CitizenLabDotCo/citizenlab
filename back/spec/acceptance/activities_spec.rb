a5032eee9a6d9854894ccfc328abeaf6b4f81fd1
Simon Tharby <simontharby@gmail.com>
2024-05-10 14:44:30 +0100

    [TAN-1084] Add Project changed activity to scope + include changes in payload

--- back/spec/acceptance/activities_spec.rb
+++ back/spec/acceptance/activities_spec.rb
@@ -11,10 +11,11 @@ resource 'Activity' do
     admin_header_token
     @user = create(:admin)
     @project = create(:project)
-    @management_activity1 = create(:phase_created_activity, user: @user)
-    @management_activity2 = create(:phase_deleted_activity, project_id: @project.id)
-    @management_activity3 = create(:activity, item_type: 'Project', action: 'created', project_id: @project.id, user: @user)
-    @management_activity4 = create(:activity, item_type: 'Project', action: 'deleted', project_id: @project.id)
+    @activity1 = create(:phase_created_activity, user: @user)
+    @activity2 = create(:phase_deleted_activity, project_id: @project.id)
+    @activity3 = create(:activity, item_type: 'Project', action: 'created', project_id: @project.id, user: @user)
+    @activity4 = create(:activity, item_type: 'Project', action: 'changed', project_id: @project.id)
+    @activity5 = create(:activity, item_type: 'Project', action: 'deleted', project_id: @project.id)
     @non_management_activity = create(:comment_created_activity)
   end
 
@@ -27,7 +28,7 @@ resource 'Activity' do
     example_request 'List all activities' do
       assert_status 200
       json_response = json_parse(response_body)
-      expect(json_response[:data].size).to eq 4
+      expect(json_response[:data].size).to eq 5
     end
 
     example 'List all activities associated with a user' do
@@ -35,16 +36,16 @@ resource 'Activity' do
       assert_status 200
       json_response = json_parse(response_body)
       expect(json_response[:data].size).to eq 2
-      expect(json_response[:data].pluck(:id)).to match_array [@management_activity1.id, @management_activity3.id]
+      expect(json_response[:data].pluck(:id)).to match_array [@activity1.id, @activity3.id]
     end
 
     example 'List all activities associated with a project' do
       do_request project_ids: [@project.id]
       assert_status 200
       json_response = json_parse(response_body)
-      expect(json_response[:data].size).to eq 3
+      expect(json_response[:data].size).to eq 4
       expect(json_response[:data].pluck(:id))
-        .to match_array [@management_activity2.id, @management_activity3.id, @management_activity4.id]
+        .to match_array [@activity2.id, @activity3.id, @activity4.id, @activity5.id]
     end
   end
 end
