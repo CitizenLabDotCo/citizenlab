a3afba030b357d056dd579c64d8f229e93223dcf
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-05-10 10:40:29 +0100

    Add version to migration class

--- back/engines/commercial/analytics/db/migrate/20240510103300_update_dimension_users_view.rb
+++ back/engines/commercial/analytics/db/migrate/20240510103300_update_dimension_users_view.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class UpdateDimensionUsersView < ActiveRecord::Migration[7.0]
+class UpdateDimensionUsersViewV3 < ActiveRecord::Migration[7.0]
   def change
     update_view :analytics_dimension_users, version: 3, revert_to_version: 2
   end
