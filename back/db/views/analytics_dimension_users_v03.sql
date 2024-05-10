2af271823e3100572c8ea4801a901d44abadc6ac
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-05-10 10:44:44 +0100

    _

--- back/db/views/analytics_dimension_users_v03.sql
+++ back/db/views/analytics_dimension_users_v03.sql
@@ -1,10 +1,10 @@
 SELECT
-    id,
-    COALESCE(roles->0->>'type','citizen') AS role,
-    invite_status,
+    u.id,
+    COALESCE(u.roles->0->>'type','citizen') AS role,
+    u.invite_status,
     CASE v.id
         WHEN NULL THEN FALSE
         ELSE TRUE
     END AS is_visitor
-FROM users
+FROM users u
 LEFT JOIN analytics_fact_visits v on v.dimension_user_id = users.id;
