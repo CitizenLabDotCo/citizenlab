8450a1d8319e54e28cb36653b5e18186b22ec459
Simon Tharby <simontharby@gmail.com>
2024-05-10 10:20:10 +0100

    [TAN-1803] Basic policy and spec

---
+++ back/spec/policies/activity_policy_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ActivityPolicy do
+  subject(:policy) { described_class.new(user, activity) }
+
+  let(:scope) { described_class::Scope.new(user, Activity) }
+
+  context 'on an activity for the management feed' do
+    let!(:activity) { create(:admin_rights_given_activity) }
+
+    context 'for a visitor' do
+      let(:user) { nil }
+
+      it { is_expected.not_to permit(:index) }
+
+      it 'does not index the activity' do
+        expect(scope.resolve.size).to eq 0
+      end
+    end
+
+    context 'for a resident' do
+      let(:user) { create(:user) }
+
+      it { is_expected.not_to permit(:index) }
+
+      it 'does not index the activity' do
+        expect(scope.resolve.size).to eq 0
+      end
+    end
+
+    context 'for a project moderator (who is not an admin)' do
+      let(:project) { create(:project) }
+      let(:user) { create(:project_moderator, projects: [project]) }
+
+      it { is_expected.not_to permit(:index) }
+
+      it 'does not index the activity' do
+        expect(scope.resolve.size).to eq 0
+      end
+    end
+
+    context 'for an admin' do
+      let(:user) { create(:admin) }
+
+      it { is_expected.to permit(:index) }
+
+      it 'indexes the activity' do
+        expect(scope.resolve.size).to eq 1
+      end
+    end
+  end
+
+  # context 'on an activity NOT for the management feed' do
+  # end
+end
