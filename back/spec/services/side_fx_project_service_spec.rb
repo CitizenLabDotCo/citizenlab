0da0f4405615f7299ad6b0aea49ca4d15811c986
Simon Tharby <simontharby@gmail.com>
2024-05-11 15:18:08 +0100

    [TAN-1803] Add test to side_fx_project_service_spec

--- back/spec/services/side_fx_project_service_spec.rb
+++ back/spec/services/side_fx_project_service_spec.rb
@@ -79,6 +79,32 @@ describe SideFxProjectService do
         .not_to have_enqueued_job(LogActivityJob)
         .with(project, 'published', user, project.updated_at.to_i, anything)
     end
+
+    it "logs a 'changed_publication_status' action when publication_status is changed" do
+      project.admin_publication.update!(publication_status: 'published')
+
+      project.assign_attributes(admin_publication_attributes: { publication_status: 'archived' })
+      service.before_update project, user
+
+      project.save!
+
+      expect { service.after_update(project, user) }
+        .to have_enqueued_job(LogActivityJob)
+        .with(project, 'changed_publication_status', user, project.updated_at.to_i, anything)
+    end
+
+    it "does not log a 'changed_publication_status' action when a draft project is published" do
+      project.admin_publication.update!(publication_status: 'draft')
+
+      project.assign_attributes(admin_publication_attributes: { publication_status: 'published' })
+      service.before_update project, user
+
+      project.save!
+
+      expect { service.after_update(project, user) }
+        .not_to have_enqueued_job(LogActivityJob)
+        .with(project, 'changed_publication_status', user, project.updated_at.to_i, anything)
+    end
   end
 
   describe 'after_destroy' do
