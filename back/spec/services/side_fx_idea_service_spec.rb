4bfb15befa4b672cc45796b662b47ab0238fe21e
Simon Tharby <simontharby@gmail.com>
2024-05-12 14:39:34 +0100

    [TAN-1803] Add changes to paload of idea changed activity

--- back/spec/services/side_fx_idea_service_spec.rb
+++ back/spec/services/side_fx_idea_service_spec.rb
@@ -49,9 +49,19 @@ describe SideFxIdeaService do
 
     it "logs a 'changed' action job when the idea has changed" do
       idea = create(:idea)
+      old_idea_title = idea.title_multiloc
       idea.update!(title_multiloc: { en: 'something else' })
       expect { service.after_update(idea, user) }
-        .to enqueue_job(LogActivityJob).with(idea, 'changed', any_args).exactly(1).times
+        .to enqueue_job(LogActivityJob).with(
+          idea,
+          'changed',
+          user,
+          idea.updated_at.to_i,
+          payload: { change: {
+            'title_multiloc' => [old_idea_title, idea.title_multiloc], 'updated_at' => [anything, anything]
+          } },
+          project_id: idea.project_id
+        ).exactly(1).times
         .and enqueue_job(Seo::ScrapeFacebookJob).exactly(1).times
     end
 
