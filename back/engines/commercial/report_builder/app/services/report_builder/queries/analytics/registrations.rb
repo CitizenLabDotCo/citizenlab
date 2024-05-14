f9715d687bec12e0941c49ef53904332ae580f9d
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-05-14 13:45:46 +0100

    Finish query

--- back/engines/commercial/report_builder/app/services/report_builder/queries/analytics/registrations.rb
+++ back/engines/commercial/report_builder/app/services/report_builder/queries/analytics/registrations.rb
@@ -35,7 +35,6 @@ module ReportBuilder
       visitors_whole_period_query = {
         fact: 'visit',
         filters: {
-          **project_filter('dimension_projects.id', project_id),
           **date_filter('dimension_date_first_action', start_at, end_at)
         },
         aggregations: {
@@ -43,15 +42,62 @@ module ReportBuilder
         }
       }
 
-      # TODO: registrations filtered by is_visitor = true
+      registrations_visitors_whole_period_query = {
+        fact: 'registration',
+        filters: {
+          **date_filter('dimension_date_registration', start_at, end_at),
+          'dimension_user.is_visitor': 'true'
+        },
+        aggregations: {
+          all: 'count'
+        }
+      }
 
-      [
+      queries = [
         time_series_query,
         registrations_whole_period_query,
-        visitors_whole_period_query
+        visitors_whole_period_query,
+        registrations_visitors_whole_period_query
       ]
 
-      # TODO: compared period
+      if compare_start_at.present? && compare_end_at.present?
+        registrations_compared_period_query = {
+          fact: 'registration',
+          filters: {
+          **date_filter('dimension_date_registration', compare_start_at, compare_end_at)
+          },
+          aggregations: {
+            all: 'count'
+          }
+        }
+
+        visitors_compared_period_query = {
+          fact: 'visit',
+          filters: {
+          **date_filter('dimension_date_first_action', compare_start_at, compare_end_at)
+          },
+          aggregations: {
+            visitor_id: 'count'
+          }
+        }
+
+        registrations_visitors_compared_period_query = {
+          fact: 'registration',
+          filters: {
+            **date_filter('dimension_date_registration', compare_start_at, compare_end_at),
+            'dimension_user.is_visitor': 'true'
+          },
+          aggregations: {
+            all: 'count'
+          }
+        }
+
+        queries << registrations_compared_period_query
+        queries << visitors_compared_period_query
+        queries << registrations_visitors_compared_period_query
+      end
+
+      queries
     end
   end
 end
