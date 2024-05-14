1c796da5f1f53bad08121503ccbdbf81c5d50555
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-05-14 13:51:27 +0100

    Reduce duplication active users query

--- back/engines/commercial/report_builder/app/services/report_builder/queries/analytics/active_users.rb
+++ back/engines/commercial/report_builder/app/services/report_builder/queries/analytics/active_users.rb
@@ -24,27 +24,8 @@ module ReportBuilder
         }
       }
 
-      active_users_whole_period_query = {
-        fact: 'participation',
-        filters: {
-          **project_filter('dimension_project_id', project_id),
-          **date_filter('dimension_date_created', start_at, end_at)
-        },
-        aggregations: {
-          participant_id: 'count'
-        }
-      }
-
-      visitors_whole_period_query = {
-        fact: 'visit',
-        filters: {
-          **project_filter('dimension_projects.id', project_id),
-          **date_filter('dimension_date_first_action', start_at, end_at)
-        },
-        aggregations: {
-          visitor_id: 'count'
-        }
-      }
+      active_users_whole_period_query = active_users_query(start_at, end_at, project_id)
+      visitors_whole_period_query = visitors_query(start_at, end_at, project_id)
 
       # We request the active user count again,
       # but this time we filter by is_visitor = true
@@ -52,17 +33,9 @@ module ReportBuilder
       # conversion rate on the FE, where we only compare
       # visitors (i.e. people that accepted cookies)
       # to active users that also accepted cookies
-      active_visitor_users_whole_period_query = {
-        fact: 'participation',
-        filters: {
-          **project_filter('dimension_project_id', project_id),
-          **date_filter('dimension_date_created', start_at, end_at),
-          'dimension_user.is_visitor': 'true'
-        },
-        aggregations: {
-          participant_id: 'count'
-        }
-      }
+      active_visitor_users_whole_period_query = active_users_query(
+        start_at, end_at, project_id, apply_visitor_filter: true
+      )
 
       queries = [
         time_series_query,
@@ -72,29 +45,8 @@ module ReportBuilder
       ]
 
       if compare_start_at.present? && compare_end_at.present?
-        active_users_compared_period_query = {
-          fact: 'participation',
-          filters: {
-            **project_filter('dimension_project_id', project_id),
-            **date_filter(
-              'dimension_date_created', compare_start_at, compare_end_at
-            )
-          },
-          aggregations: {
-            participant_id: 'count'
-          }
-        }
-
-        visitors_compared_period_query = {
-          fact: 'visit',
-          filters: {
-            **project_filter('dimension_projects.id', project_id),
-            **date_filter('dimension_date_first_action', compare_start_at, compare_end_at)
-          },
-          aggregations: {
-            visitor_id: 'count'
-          }
-        }
+        active_users_compared_period_query = active_users_query(compare_start_at, compare_end_at, project_id)
+        visitors_compared_period_query = visitors_query(compare_start_at, compare_end_at, project_id)
 
         # We request the active user count again,
         # but this time we filter by is_visitor = true
@@ -102,19 +54,9 @@ module ReportBuilder
         # conversion rate on the FE, where we only compare
         # visitors (i.e. people that accepted cookies)
         # to active users that also accepted cookies
-        active_visitor_users_compared_period_query = {
-          fact: 'participation',
-          filters: {
-            **project_filter('dimension_project_id', project_id),
-            **date_filter(
-              'dimension_date_created', compare_start_at, compare_end_at
-            ),
-            'dimension_user.is_visitor': 'true'
-          },
-          aggregations: {
-            participant_id: 'count'
-          }
-        }
+        active_visitor_users_compared_period_query = active_users_query(
+          compare_start_at, compare_end_at, project_id, apply_visitor_filter: true
+        )
 
         queries << active_users_compared_period_query
         queries << visitors_compared_period_query
@@ -123,5 +65,40 @@ module ReportBuilder
 
       queries
     end
+  end
+
+  private
+
+  def active_users_query(start_at, end_at, project_id, apply_visitor_filter: false)
+    {
+      fact: 'participation',
+      filters: {
+        **project_filter('dimension_project_id', project_id),
+        **date_filter('dimension_date_created', start_at, end_at),
+        **visitor_filter(apply_visitor_filter)
+      },
+      aggregations: {
+        participant_id: 'count'
+      }
+    }
+  end
+
+  def visitors_query(start_at, end_at, project_id)
+    {
+      fact: 'visit',
+      filters: {
+          **project_filter('dimension_projects.id', project_id),
+          **date_filter('dimension_date_first_action', start_at, end_at)
+      },
+      aggregations: {
+        visitor_id: 'count'
+      }
+    }
+  end
+
+  def visitor_filter(apply)
+    return {} unless apply
+
+    { 'dimension_user.is_visitor': 'true' }
   end
 end
