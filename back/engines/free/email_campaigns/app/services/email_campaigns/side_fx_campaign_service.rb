9617d4da3c07ed289ced885ad9fee7efa811e446
Simon Tharby <simontharby@gmail.com>
2024-05-14 15:26:23 +0100

    [TAN-1803] Remove project_id from campaign sent activity

--- back/engines/free/email_campaigns/app/services/email_campaigns/side_fx_campaign_service.rb
+++ back/engines/free/email_campaigns/app/services/email_campaigns/side_fx_campaign_service.rb
@@ -25,9 +25,7 @@ module EmailCampaigns
     def before_send(campaign, user); end
 
     def after_send(campaign, user)
-      project_id = campaign.respond_to?(:project) ? campaign.project&.id : nil
-
-      LogActivityJob.perform_later(campaign, 'sent', user, campaign.updated_at.to_i, project_id: project_id)
+      LogActivityJob.perform_later(campaign, 'sent', user, campaign.updated_at.to_i)
     end
 
     def before_destroy(campaign, user); end
