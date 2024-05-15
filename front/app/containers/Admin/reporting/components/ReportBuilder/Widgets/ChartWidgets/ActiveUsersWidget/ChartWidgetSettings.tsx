568143b367e5d4c87e481944d9c2f0c8ece1cf71
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-05-15 13:35:20 +0100

    Add settings

--- front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ChartWidgets/ActiveUsersWidget/ChartWidgetSettings.tsx
+++ front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ChartWidgets/ActiveUsersWidget/ChartWidgetSettings.tsx
@@ -6,39 +6,24 @@ import {
   Box,
   Text,
   Icon,
-  Toggle,
 } from '@citizenlab/cl2-component-library';
 import { useNode } from '@craftjs/core';
-import Tippy from '@tippyjs/react';
 
-import { getComparedTimeRange } from 'components/admin/GraphCards/_utils/query';
 import activeUsersMessages from 'components/admin/GraphCards/ActiveUsersCard/messages';
 
 import { useIntl } from 'utils/cl-intl';
 
+import {
+  ComparisonToggle,
+  HideStatisticsToggle,
+} from '../_shared/StatisticToggles';
 import TimeSeriesWidgetSettings from '../_shared/TimeSeriesWidgetSettings';
-
-import messages from './messages';
 
 const ChartWidgetSettings = () => {
   const { formatMessage } = useIntl();
   const {
     actions: { setProp },
-    startAt,
-    endAt,
-    compareStartAt,
-    compareEndAt,
-    hideStatistics,
-  } = useNode((node) => ({
-    startAt: node.data.props.startAt,
-    endAt: node.data.props.endAt,
-    compareStartAt: node.data.props.compareStartAt,
-    compareEndAt: node.data.props.compareEndAt,
-    hideStatistics: node.data.props.hideStatistics,
-  }));
-
-  const noTimePeriodSelected = !startAt || !endAt;
-  const comparePreviousPeriod = !!compareStartAt && !!compareEndAt;
+  } = useNode();
 
   return (
     <Box>
@@ -76,48 +61,8 @@ const ChartWidgetSettings = () => {
           }
         }}
       />
-      <Box mb="20px">
-        <Tippy
-          content={formatMessage(messages.youNeedToSelectADateRange)}
-          disabled={!noTimePeriodSelected}
-          placement="left-start"
-          zIndex={999999}
-        >
-          <div>
-            <Toggle
-              id="e2e-compare-previous-period-toggle"
-              label={formatMessage(messages.showComparisonLastPeriod)}
-              disabled={noTimePeriodSelected}
-              checked={comparePreviousPeriod}
-              onChange={() => {
-                setProp((props) => {
-                  if (noTimePeriodSelected || comparePreviousPeriod) {
-                    props.compareStartAt = undefined;
-                    props.compareEndAt = undefined;
-                    return;
-                  }
-
-                  const { compare_start_at, compare_end_at } =
-                    getComparedTimeRange(startAt, endAt);
-                  props.compareStartAt = compare_start_at;
-                  props.compareEndAt = compare_end_at;
-                });
-              }}
-            />
-          </div>
-        </Tippy>
-      </Box>
-      <Box mb="20px">
-        <Toggle
-          label={formatMessage(messages.hideStatistics)}
-          checked={!!hideStatistics}
-          onChange={() => {
-            setProp((props) => {
-              props.hideStatistics = !hideStatistics;
-            });
-          }}
-        />
-      </Box>
+      <ComparisonToggle />
+      <HideStatisticsToggle />
     </Box>
   );
 };
--- front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ChartWidgets/RegistrationsWidget/ChartWidgetSettings.tsx
+++ front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ChartWidgets/RegistrationsWidget/ChartWidgetSettings.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
+import { Box } from '@citizenlab/cl2-component-library';
+import { useNode } from '@craftjs/core';
+
+import { TitleInput, DateRangeInput } from '../_shared/ChartWidgetSettings';
+import {
+  ComparisonToggle,
+  HideStatisticsToggle,
+} from '../_shared/StatisticToggles';
+import { ResolutionInput } from '../_shared/TimeSeriesWidgetSettings';
+
 const ChartWidgetSettings = () => {
-  return <></>;
+  const { setProp } = useNode();
+
+  return (
+    <Box>
+      <TitleInput />
+      <DateRangeInput
+        onChangeDateRange={({ startDate, endDate }) => {
+          if (!startDate || !endDate) {
+            // Make sure that we always reset compared date range
+            // if the main date range is not fully set
+            setProp((props) => {
+              props.compareStartAt = undefined;
+              props.compareEndAt = undefined;
+            });
+          }
+        }}
+      />
+      <ResolutionInput />
+      <ComparisonToggle />
+      <HideStatisticsToggle />
+    </Box>
+  );
 };
 
 export default ChartWidgetSettings;
