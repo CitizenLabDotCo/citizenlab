71e66bbf617666086cbbf49873549d1465d64f64
Simon Tharby <simontharby@gmail.com>
2024-05-15 10:31:55 +0100

    [TAN-1803] Demodulize item_type when serializing

--- back/app/serializers/web_api/v1/activity_serializer.rb
+++ back/app/serializers/web_api/v1/activity_serializer.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class WebApi::V1::ActivitySerializer < WebApi::V1::BaseSerializer
-  attributes :action, :acted_at, :item_type, :item_id, :project_id
+  attributes :action, :acted_at, :item_id, :project_id
+
+  attribute :item_type do |object|
+    object.item_type.demodulize.downcase
+  end
 
   attribute :change do |object|
     object.payload&.dig('change')
