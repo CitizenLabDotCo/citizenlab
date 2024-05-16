7168a3fdad68a66f788798954f84a257133cd515
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-05-16 16:11:55 +0100

    Fix spec

--- back/engines/commercial/report_builder/spec/services/report_builder/queries/analytics/visitors_spec.rb
+++ back/engines/commercial/report_builder/spec/services/report_builder/queries/analytics/visitors_spec.rb
@@ -11,13 +11,14 @@ RSpec.describe ReportBuilder::Queries::Analytics::Visitors do
     end
 
     it 'returns correct data for current period' do
+      september = Date.new(2022, 9, 1)
+
       # Create sessions
-      create_list(:session, 2, monthly_user_hash: 'hash1')
-      create(:session, monthly_user_hash: 'hash2')
+      create_list(:session, 2, monthly_user_hash: 'hash1', created_at: september)
+      create(:session, monthly_user_hash: 'hash2', created_at: september)
 
       # Create visits
       referrer_type = create(:dimension_referrer_type)
-      september = Date.new(2022, 9, 1)
       dim_sept = create(:dimension_date, date: september)
 
       create(:fact_visit, visitor_id: 'v-1', duration: 100, pages_visited: 1,
@@ -26,21 +27,21 @@ RSpec.describe ReportBuilder::Queries::Analytics::Visitors do
         dimension_date_first_action: dim_sept, dimension_date_last_action: dim_sept, dimension_referrer_type: referrer_type)
 
       params = {
-        start_at: Date.new(2022, 8, 1),
-        end_at: Date.new(2022, 10, 1)
+        start_at: Date.new(2022, 8, 1).to_s,
+        end_at: Date.new(2022, 10, 1).to_s
       }
 
       expect(query.run_query(**params)).to eq(
         [
           [{
             'count' => 3,
-            'monthly_user_hash' => 2,
+            'count_monthly_user_hash' => 2,
             'dimension_date_created.month' => '2022-09',
-            'first_dimension_date_created' => september
+            'first_dimension_date_created_date' => september
           }],
           [{
             'count' => 3,
-            'monthly_user_hash' => 2
+            'count_monthly_user_hash' => 2
           }],
           [{
             'avg_duration' => 150,
