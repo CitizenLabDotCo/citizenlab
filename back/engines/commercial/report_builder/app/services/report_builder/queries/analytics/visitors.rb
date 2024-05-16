0ec14613449339a92e68e7e4de79e3ca4493a3d5
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-05-16 09:15:20 +0100

    Add compared period to visitors query in BE

--- back/engines/commercial/report_builder/app/services/report_builder/queries/analytics/visitors.rb
+++ back/engines/commercial/report_builder/app/services/report_builder/queries/analytics/visitors.rb
@@ -1,8 +1,36 @@
 module ReportBuilder
   class Queries::Analytics::Visitors < Queries::Analytics::Base
+    def visit_aggregations_query(start_at, end_at)
+      {
+        fact: 'visit',
+        filters: {
+          **project_filter('dimension_projects.id', project_id),
+          **date_filter(
+            'dimension_date_first_action',
+            start_at,
+            end_at
+          )
+        },
+        aggregations: {
+          all: 'count',
+          visitor_id: 'count',
+          duration: 'avg',
+          pages_visited: 'avg'
+        }
+      }
+    end
+
     protected
 
-    def query(start_at: nil, end_at: nil, project_id: nil, resolution: nil, **_other_props)
+    def query(
+      start_at: nil,
+      end_at: nil,
+      project_id: nil,
+      resolution: nil,
+      compare_start_at: nil,
+      compare_end_at: nil,
+      **_other_props
+    )
       time_series_query = {
         fact: 'visit',
         filters: {
@@ -21,25 +49,16 @@ module ReportBuilder
         }
       }
 
-      totals_whole_period_query = {
-        fact: 'visit',
-        filters: {
-          **project_filter('dimension_projects.id', project_id),
-          **date_filter(
-            'dimension_date_first_action',
-            start_at,
-            end_at
-          )
-        },
-        aggregations: {
-          all: 'count',
-          visitor_id: 'count',
-          duration: 'avg',
-          pages_visited: 'avg'
-        }
-      }
+      totals_whole_period_query = visit_aggregations_query(start_at, end_at)
 
-      [time_series_query, totals_whole_period_query]
+      queries = [time_series_query, totals_whole_period_query]
+
+      if compare_start_at.present? && compare_end_at.present?
+        totals_compared_period_query = visit_aggregations_query(start_at, end_at)
+        queries << totals_compared_period_query
+      end
+
+      queries
     end
   end
 end
