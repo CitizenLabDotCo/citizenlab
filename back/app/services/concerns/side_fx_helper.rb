2cbcffb103954a9f0b9078ee6da103b88caa9349
Simon Tharby <simontharby@gmail.com>
2024-05-16 20:39:53 +0100

    [TAN-1803] New SideFxHelper#update_activities_when_item_deleted

--- back/app/services/concerns/side_fx_helper.rb
+++ back/app/services/concerns/side_fx_helper.rb
@@ -33,4 +33,15 @@ module SideFxHelper
   def remove_user_from_past_activities_with_item(item, user)
     user.activities.where(item: item).update_all(user_id: nil)
   end
+
+  def update_activities_when_item_deleted(frozen_item, serialized_item, item_name)
+    # Add the serialized item to the payload of the existing activities for the item,
+    # where the activity is one that will be shown in the Management Feed of activities,
+    # excluding 'deleted' activities (as the serialized item is added to those activites at creation).
+    Activity.where(item: frozen_item).where(action: 'created')
+      .or(Activity.where(item: frozen_item).where(action: 'changed'))
+      .each do |activity|
+        UpdateActivityJob.perform_later(activity, item_name, serialized_item)
+      end
+  end
 end
