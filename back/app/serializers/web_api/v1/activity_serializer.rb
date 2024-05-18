f775fc16ae451b1c434ce1fa7802488537f3d50a
Simon Tharby <simontharby@gmail.com>
2024-05-18 19:57:53 +0100

    [TAN-1803] Remove redundant fallback to item.title_multiloc

--- back/app/serializers/web_api/v1/activity_serializer.rb
+++ back/app/serializers/web_api/v1/activity_serializer.rb
@@ -13,9 +13,7 @@ class WebApi::V1::ActivitySerializer < WebApi::V1::BaseSerializer
 
   attribute :item_title_multiloc do |object|
     item_name = object.item_type == 'ProjectFolders::Folder' ? 'project_folder' : object.item_type.demodulize.downcase
-    payload_title = object.payload.dig(item_name, 'title_multiloc')
-
-    payload_title || object.item.try(:title_multiloc)
+    object.payload.dig(item_name, 'title_multiloc')
   end
 
   attribute :change do |object|
