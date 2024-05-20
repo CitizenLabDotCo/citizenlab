5103363f33e85e9c904b6b43c09b2400c43bfce0
Simon Tharby <simontharby@gmail.com>
2024-05-20 10:53:07 +0100

    [TAN-1803] Don't use instance vars

--- back/app/controllers/web_api/v1/activities_controller.rb
+++ back/app/controllers/web_api/v1/activities_controller.rb
@@ -4,22 +4,22 @@ class WebApi::V1::ActivitiesController < ApplicationController
   skip_before_action :authenticate_user, only: %i[index]
 
   def index
-    @activities = policy_scope Activity
-    @activities = @activities.where(user_id: params[:user_ids]) if params[:user_ids]
-    @activities = @activities.where(project_id: params[:project_ids]) if params[:project_ids]
+    activities = policy_scope Activity
+    activities = activities.where(user_id: params[:user_ids]) if params[:user_ids]
+    activities = activities.where(project_id: params[:project_ids]) if params[:project_ids]
 
-    @activities = case params[:sort]
+    activities = case params[:sort]
     when 'acted_at'
-      @activities.order(acted_at: :asc)
+      activities.order(acted_at: :asc)
     when '-acted_at'
-      @activities.order(acted_at: :desc)
+      activities.order(acted_at: :desc)
     when nil
-      @activities
+      activities
     else
       raise 'Unsupported sort method'
     end
 
-    paginated_activities = paginate @activities
+    paginated_activities = paginate activities
     paginated_activities = paginated_activities.includes(:user)
 
     render json: linked_json(
