a9f8d4e4acd28d1c6f10b90aa86f63df412d27ca
Amanda Anderson <anderson_amanda@mail.com>
2024-05-21 13:45:51 +0200

    Make aria message more descriptive

--- front/app/containers/IdeasShow/components/IdeaNavigationButtons/messages.ts
+++ front/app/containers/IdeasShow/components/IdeaNavigationButtons/messages.ts
@@ -2,7 +2,8 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   ideaIndexAriaMessage: {
-    id: 'app.containers.IdeasShow.IdeaNavigationButtons.ideaIndexAriaMessage',
-    defaultMessage: 'Viewing number {currentIndex} out of {totalInputs} inputs',
+    id: 'app.containers.IdeasShow.IdeaNavigationButtons.ideaIndexAriaMessage2',
+    defaultMessage:
+      'Currently viewing number {currentIndex} out of {totalInputs} inputs',
   },
 });
