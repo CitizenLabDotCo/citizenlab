a583148551097ce0802b23b59b814d944fb9b3aa
Simon Tharby <simontharby@gmail.com>
2024-05-21 11:32:09 +0100

    [TAN-1885] Add translations where appropriate

--- back/app/services/xlsx_service.rb
+++ back/app/services/xlsx_service.rb
@@ -136,10 +136,10 @@ class XlsxService
 
   def generate_attendees_xlsx(users, view_private_attributes: false)
     columns = [
-      { header: 'first_name', f: ->(u) { u.first_name } },
-      { header: 'last_name', f: ->(u) { u.last_name } },
-      { header: 'email', f: ->(u) { u.email } },
-      { header: 'registration_completed_at', f: ->(u) { u.registration_completed_at }, skip_sanitization: true },
+      { header: I18n.t('first_name', scope: 'xlsx_export.column_headers'), f: ->(u) { u.first_name } },
+      { header: I18n.t('last_name', scope: 'xlsx_export.column_headers'), f: ->(u) { u.last_name } },
+      { header: I18n.t('email', scope: 'xlsx_export.column_headers'), f: ->(u) { u.email } },
+      { header: I18n.t('registration_completed_at', scope: 'xlsx_export.column_headers'), f: ->(u) { u.registration_completed_at }, skip_sanitization: true },
       *user_custom_field_columns(:itself)
     ]
     columns.reject! { |c| %w[email first_name last_name].include?(c[:header]) } unless view_private_attributes
