b7da449d60924a85bc5f41f2587a9c8188648d0c
Simon Tharby <simontharby@gmail.com>
2024-05-21 12:06:06 +0100

    [TAN-1885] Move code into new AttendeesGenerator

--- back/app/services/xlsx_service.rb
+++ back/app/services/xlsx_service.rb
@@ -134,19 +134,6 @@ class XlsxService
     generate_xlsx 'Users', columns, users
   end
 
-  def generate_attendees_xlsx(users, view_private_attributes: false)
-    columns = [
-      { header: I18n.t('first_name', scope: 'xlsx_export.column_headers'), f: ->(u) { u.first_name } },
-      { header: I18n.t('last_name', scope: 'xlsx_export.column_headers'), f: ->(u) { u.last_name } },
-      { header: I18n.t('email', scope: 'xlsx_export.column_headers'), f: ->(u) { u.email } },
-      { header: I18n.t('registration_completed_at', scope: 'xlsx_export.column_headers'), f: ->(u) { u.registration_completed_at }, skip_sanitization: true },
-      *user_custom_field_columns(:itself)
-    ]
-    columns.reject! { |c| %w[email first_name last_name].include?(c[:header]) } unless view_private_attributes
-
-    generate_xlsx 'Users', columns, users
-  end
-
   def generate_idea_xlsx_columns(_ideas, view_private_attributes: false, with_tags: false)
     columns = [
       { header: 'id',                   f: ->(i) { i.id }, skip_sanitization: true },
