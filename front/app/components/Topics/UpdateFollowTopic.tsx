2bd0fd7633bd413ad123a7214669f47547921de8
EdwinKato <katedwin88@gmail.com>
2024-05-21 08:04:33 +0300

    Announce following and unfollowing topics

--- front/app/components/Topics/UpdateFollowTopic.tsx
+++ front/app/components/Topics/UpdateFollowTopic.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Badge, colors, Button } from '@citizenlab/cl2-component-library';
 import { useLocation } from 'react-router-dom';
@@ -7,10 +7,16 @@ import useAddFollower from 'api/follow_unfollow/useAddFollower';
 import useDeleteFollower from 'api/follow_unfollow/useDeleteFollower';
 import { ITopicData } from 'api/topics/types';
 
+import useLocalize from 'hooks/useLocalize';
+
 import tracks from 'components/FollowUnfollow/tracks';
 import T from 'components/T';
 
+import { ScreenReaderOnly } from 'utils/a11y';
 import { trackEventByName } from 'utils/analytics';
+import { useIntl } from 'utils/cl-intl';
+
+import messages from './messages';
 
 interface Props {
   topic: ITopicData;
@@ -21,11 +27,29 @@ const UpdateFollowTopic = ({ topic }: Props) => {
   const { mutate: deleteFollower, isLoading: isDeletingFollower } =
     useDeleteFollower();
   const { pathname } = useLocation();
+  const localize = useLocalize();
+  const { formatMessage } = useIntl();
   const isLoading = isAddingFollower || isDeletingFollower;
   const followerId = topic.relationships.user_follower?.data?.id;
   const isFollowing = !!followerId;
   const color = isFollowing ? colors.success : colors.coolGrey300;
   const iconName = isFollowing ? 'check-circle' : 'plus-circle';
+  const localizedTopicTitle = localize(topic.attributes.title_multiloc);
+  const [announcement, setAnnouncement] = useState('');
+
+  useEffect(() => {
+    if (!isLoading) {
+      const message = isFollowing
+        ? formatMessage(messages.followedTopic, {
+            topicTitle: localizedTopicTitle,
+          })
+        : formatMessage(messages.unfollowedTopic, {
+            topicTitle: localizedTopicTitle,
+          });
+      setAnnouncement(message);
+    }
+  }, [isLoading, isFollowing, formatMessage, localizedTopicTitle]);
+
   const handleFollowOrUnfollow = () => {
     if (isFollowing) {
       deleteFollower({
@@ -52,7 +76,7 @@ const UpdateFollowTopic = ({ topic }: Props) => {
   };
 
   return (
-    <Badge color={color} onClick={handleFollowOrUnfollow}>
+    <Badge color={color} onClick={handleFollowOrUnfollow} aria-busy={isLoading}>
       <Button
         buttonStyle="text"
         icon={iconName}
@@ -61,12 +85,14 @@ const UpdateFollowTopic = ({ topic }: Props) => {
         padding="0px"
         my="0px"
         processing={isLoading}
+        ariaPressed={!isLoading ? isFollowing : undefined}
         data-cy={
           isFollowing ? 'e2e-unfollow-topic-button' : 'e2e-follow-topic-button'
         }
       >
         <T value={topic.attributes.title_multiloc} />
       </Button>
+      <ScreenReaderOnly aria-live="polite">{announcement}</ScreenReaderOnly>
     </Badge>
   );
 };
