ac656f0b301f73b2f183a1a110d216fb9c18b796
Sébastien Hoorens <sebastien@citizenlab.co>
2024-05-21 18:34:45 +0200

    added comment

--- back/app/services/user_reduce_service.rb
+++ back/app/services/user_reduce_service.rb
@@ -6,6 +6,7 @@ class UserReduceService
   #
   # See: https://docs.google.com/document/d/1REUc69m2fn1vCtBOMosWjAXDPSSRB9Hgh-Lf1OmXnX0/edit?usp=sharing
 
+  # Ideally, "followers" should not be blacklisted, but there was no time to tackle the uniqueness constraint issue. This is a temporary solution.
   MERGE_TABLES_BLACKLIST = %w[
     activities email_campaigns_campaign_email_commands
     email_campaigns_campaigns email_campaigns_consents
