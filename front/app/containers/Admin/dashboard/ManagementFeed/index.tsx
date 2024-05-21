9e764251c90093304dacde8e84aad58d82a06792
Iva <iva@citizenlab.co>
2024-05-21 12:24:05 +0300

    Add table

--- front/app/containers/Admin/dashboard/ManagementFeed/index.tsx
+++ front/app/containers/Admin/dashboard/ManagementFeed/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
-import { Title } from '@citizenlab/cl2-component-library';
+import {
+  Title,
+  Table,
+  Thead,
+  Th,
+  Tr,
+  colors,
+} from '@citizenlab/cl2-component-library';
 
 import { useIntl } from 'utils/cl-intl';
 
@@ -9,9 +16,30 @@ import messages from './messages';
 const ManagementFeed = () => {
   const { formatMessage } = useIntl();
   return (
-    <Title color="primary" m="0px">
-      {formatMessage(messages.title)}
-    </Title>
+    <>
+      <Title color="primary">{formatMessage(messages.title)}</Title>
+      <Table
+        bgColor={colors.white}
+        innerBorders={{
+          headerCells: true,
+          bodyRows: true,
+        }}
+      >
+        <Thead>
+          <Tr>
+            <Th>{formatMessage(messages.date)}</Th>
+            <Th>{formatMessage(messages.user)}</Th>
+            <Th>{formatMessage(messages.item)}</Th>
+            <Th>{formatMessage(messages.action)}</Th>
+          </Tr>
+        </Thead>
+        {/* <Tr>
+            <Td>{formatMessage(messages.tableRowTitle)}</Td>
+            <Td>{formatMessage(messages.tableRowStatus)}</Td>
+            <Td>{formatMessage(messages.tableRowDate)}</Td>
+            </Tr> */}
+      </Table>
+    </>
   );
 };
 
--- front/app/containers/Admin/dashboard/ManagementFeed/messages.ts
+++ front/app/containers/Admin/dashboard/ManagementFeed/messages.ts
@@ -5,4 +5,20 @@ export default defineMessages({
     id: 'app.Admin.ManagementFeed.title',
     defaultMessage: 'Management feed',
   },
+  date: {
+    id: 'app.Admin.ManagementFeed.date',
+    defaultMessage: 'Date',
+  },
+  user: {
+    id: 'app.Admin.ManagementFeed.user',
+    defaultMessage: 'User',
+  },
+  item: {
+    id: 'app.Admin.ManagementFeed.item',
+    defaultMessage: 'Item',
+  },
+  action: {
+    id: 'app.Admin.ManagementFeed.action',
+    defaultMessage: 'Action',
+  },
 });
