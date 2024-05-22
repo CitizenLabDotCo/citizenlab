357b5148f4740d655cb41780e547ea2c7aadda7b
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-05-22 16:53:46 +0100

    Add basic settings

--- front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ChartWidgets/ParticipationWidget/ChartWidgetSettings.tsx
+++ front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ChartWidgets/ParticipationWidget/ChartWidgetSettings.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
+import { Box } from '@citizenlab/cl2-component-library';
+import { useNode } from '@craftjs/core';
+
+import {
+  ComparisonToggle,
+  HideStatisticsToggle,
+} from '../_shared/StatisticToggles';
+import TimeSeriesWidgetSettings from '../_shared/TimeSeriesWidgetSettings';
+
 const ChartWidgetSettings = () => {
-  return <></>;
+  const {
+    actions: { setProp },
+  } = useNode();
+
+  return (
+    <Box>
+      <TimeSeriesWidgetSettings
+        onChangeDateRange={({ startDate, endDate }) => {
+          if (!startDate || !endDate) {
+            // Make sure that we always reset compared date range
+            // if the main date range is not fully set
+            setProp((props) => {
+              props.compareStartAt = undefined;
+              props.compareEndAt = undefined;
+            });
+          }
+        }}
+      />
+      <ComparisonToggle />
+      <HideStatisticsToggle />
+    </Box>
+  );
 };
 
 export default ChartWidgetSettings;
