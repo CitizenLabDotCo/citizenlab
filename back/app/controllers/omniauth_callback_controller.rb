8e0c86043b2ba11deb122c641382187eae5ea399
Alexander <alexander@citizenlab.co>
2024-05-23 21:51:10 +0200

    [TAN-1735] Fix rubocop offence

--- back/app/controllers/omniauth_callback_controller.rb
+++ back/app/controllers/omniauth_callback_controller.rb
@@ -61,8 +61,7 @@ class OmniauthCallbackController < ApplicationController
     user_attrs = authver_method.profile_to_user_attrs(auth)
 
     @identity = Identity.find_or_build_with_omniauth(auth, authver_method)
-    @user = @identity.user ||
-      find_existing_user(authver_method, auth, user_attrs, verify: verify)
+    @user = @identity.user || find_existing_user(authver_method, auth, user_attrs, verify: verify)
     @user = authentication_service.prevent_user_account_hijacking @user
 
     # https://github.com/CitizenLabDotCo/citizenlab/pull/3055#discussion_r1019061643
