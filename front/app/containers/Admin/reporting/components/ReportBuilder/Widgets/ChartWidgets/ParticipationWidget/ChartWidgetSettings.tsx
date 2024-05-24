316ab5e51980d0b39468fad4b2c21fd910773d32
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-05-24 09:13:24 +0100

    Also fix widget compare period stuff in settings here

--- front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ChartWidgets/ParticipationWidget/ChartWidgetSettings.tsx
+++ front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ChartWidgets/ParticipationWidget/ChartWidgetSettings.tsx
@@ -5,6 +5,8 @@ import { useNode } from '@craftjs/core';
 
 import { ParticipationType } from 'api/graph_data_units/requestTypes';
 
+import { getComparedTimeRange } from 'components/admin/GraphCards/_utils/query';
+
 import { useIntl } from 'utils/cl-intl';
 
 import {
@@ -21,20 +23,37 @@ const ChartWidgetSettings = () => {
   const {
     actions: { setProp },
     participationTypes,
+    compareStartAt,
+    compareEndAt,
   } = useNode((node) => ({
     participationTypes: node.data.props.participationTypes as Record<
       ParticipationType,
       boolean
     >,
+    compareStartAt: node.data.props.compareStartAt,
+    compareEndAt: node.data.props.compareEndAt,
   }));
 
   const { inputs, comments, votes } = participationTypes;
+  const comparePreviousPeriod = !!compareStartAt && !!compareEndAt;
 
   return (
     <Box>
       <TimeSeriesWidgetSettings
         onChangeDateRange={({ startDate, endDate }) => {
-          if (!startDate || !endDate) {
+          if (startDate && endDate) {
+            if (comparePreviousPeriod) {
+              const { compare_start_at, compare_end_at } = getComparedTimeRange(
+                startDate,
+                endDate
+              );
+
+              setProp((props) => {
+                props.compareStartAt = compare_start_at;
+                props.compareEndAt = compare_end_at;
+              });
+            }
+          } else {
             // Make sure that we always reset compared date range
             // if the main date range is not fully set
             setProp((props) => {
