c246075a85f1d7e07d068c2bb7ece75eee5f07f2
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-05-27 08:32:43 +0100

    Add project images to query response

--- back/engines/commercial/report_builder/app/services/report_builder/queries/projects.rb
+++ back/engines/commercial/report_builder/app/services/report_builder/queries/projects.rb
@@ -10,12 +10,23 @@ module ReportBuilder
         .where(id: project_that_have_phases_ids)
         .where.not(id: non_overlapping_project_ids)
 
+      project_images_hash = ProjectImage
+        .where(project_id: overlapping_projects.pluck(:id))
+        .to_a
+        .each_with_object({}) do |project_image, hash|
+          hash[project_image.id] = serialize(project_image, ::WebApi::V1::ImageSerializer)
+        end
+
       {
-        projects: ::WebApi::V1::ProjectSerializer.new(
-          overlapping_projects,
-          params: { current_user: @current_user }
-        ).serializable_hash[:data]
+        projects: serialize(overlapping_projects, ::WebApi::V1::ProjectSerializer),
+        project_images: project_images_hash
       }
+    end
+
+    private
+
+    def serialize(entity, serializer)
+      serializer.new(entity, params: { current_user: @current_user }).serializable_hash[:data]
     end
   end
 end
