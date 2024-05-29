a9a400ec348a5f6d5cd7c45fed7b2fa7019a1083
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-05-29 14:15:21 +0100

    Reuse more for widget settings

--- front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ChartWidgets/ActiveUsersWidget/ChartWidgetSettings.tsx
+++ front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ChartWidgets/ActiveUsersWidget/ChartWidgetSettings.tsx
@@ -7,9 +7,7 @@ import {
   Text,
   Icon,
 } from '@citizenlab/cl2-component-library';
-import { useNode } from '@craftjs/core';
 
-import { getComparedTimeRange } from 'components/admin/GraphCards/_utils/query';
 import activeUsersMessages from 'components/admin/GraphCards/ActiveUsersCard/messages';
 
 import { useIntl } from 'utils/cl-intl';
@@ -22,16 +20,6 @@ import TimeSeriesWidgetSettings from '../_shared/TimeSeriesWidgetSettings';
 
 const ChartWidgetSettings = () => {
   const { formatMessage } = useIntl();
-  const {
-    actions: { setProp },
-    compareStartAt,
-    compareEndAt,
-  } = useNode((node) => ({
-    compareStartAt: node.data.props.compareStartAt,
-    compareEndAt: node.data.props.compareEndAt,
-  }));
-
-  const comparePreviousPeriod = !!compareStartAt && !!compareEndAt;
 
   return (
     <Box>
@@ -57,30 +45,7 @@ const ChartWidgetSettings = () => {
           {formatMessage(activeUsersMessages.cardTitleTooltipMessage)}
         </Text>
       </Box>
-      <TimeSeriesWidgetSettings
-        onChangeDateRange={({ startDate, endDate }) => {
-          if (startDate && endDate) {
-            if (comparePreviousPeriod) {
-              const { compare_start_at, compare_end_at } = getComparedTimeRange(
-                startDate,
-                endDate
-              );
-
-              setProp((props) => {
-                props.compareStartAt = compare_start_at;
-                props.compareEndAt = compare_end_at;
-              });
-            }
-          } else {
-            // Make sure that we always reset compared date range
-            // if the main date range is not fully set
-            setProp((props) => {
-              props.compareStartAt = undefined;
-              props.compareEndAt = undefined;
-            });
-          }
-        }}
-      />
+      <TimeSeriesWidgetSettings resetComparePeriod />
       <ComparisonToggle />
       <HideStatisticsToggle />
     </Box>
