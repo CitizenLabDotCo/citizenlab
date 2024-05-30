5e3ac99e7db6d45070058656561bbec9a692fbd1
Simon Tharby <simontharby@gmail.com>
2024-05-30 15:41:51 +0100

    [TAN-1946] Use more permissive regex

--- front/app/utils/validate.ts
+++ front/app/utils/validate.ts
@@ -6,7 +6,7 @@ export const isValidUrl = (url: string) => {
   // Used this reference for generating a valid URL regex:
   // https://tutorial.eyehunts.com/js/url-regex-validation-javascript-example-code/
   const validUrlRegex =
-    /(http(s)?:\/\/.)(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g;
+    /(http(s)?:\/\/)(www\.)?([a-zA-Z0-9@:%_+-.~#?&//=]{1,256})/g;
 
   if (!validUrlRegex.test(url)) {
     return false;
