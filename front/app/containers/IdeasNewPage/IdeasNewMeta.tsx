b02936f95f12643df229dc3411a40a54b4d90884
brentguf <brentguf@users.noreply.github.com>
2024-06-03 07:31:19 +0200

    Fix Meta components

--- front/app/containers/IdeasNewPage/IdeasNewMeta.tsx
+++ front/app/containers/IdeasNewPage/IdeasNewMeta.tsx
@@ -9,7 +9,6 @@ import { getInputTerm } from 'api/phases/utils';
 import useProjectBySlug from 'api/projects/useProjectBySlug';
 
 import useAppConfigurationLocales from 'hooks/useAppConfigurationLocales';
-import useLocalize from 'hooks/useLocalize';
 
 import { useIntl } from 'utils/cl-intl';
 import getAlternateLinks from 'utils/cl-router/getAlternateLinks';
@@ -20,7 +19,6 @@ import messages from './messages';
 
 const IdeasNewMeta = () => {
   const { formatMessage } = useIntl();
-  const localize = useLocalize();
   const { slug } = useParams();
   const { data: authUser } = useAuthUser();
   const { data: project } = useProjectBySlug(slug);
@@ -28,31 +26,23 @@ const IdeasNewMeta = () => {
   const locales = useAppConfigurationLocales();
   const { location } = window;
 
-  if (project) {
-    const projectName = localize(project.data.attributes.title_multiloc);
+  const inputTerm = getInputTerm(phases?.data);
+  const ideasIndexTitle = formatMessage(
+    getInputTermMessage(inputTerm, {
+      idea: messages.ideaNewMetaTitle1,
+      option: messages.optionMetaTitle1,
+      project: messages.projectMetaTitle1,
+      question: messages.questionMetaTitle1,
+      issue: messages.issueMetaTitle1,
+      contribution: messages.contributionMetaTitle1,
+    })
+  );
+  const ideasIndexDescription = formatMessage(messages.ideaNewMetaDescription);
 
-    const inputTerm = getInputTerm(phases?.data);
-    const ideasIndexTitle = formatMessage(
-      getInputTermMessage(inputTerm, {
-        idea: messages.ideaNewMetaTitle1,
-        option: messages.optionMetaTitle1,
-        project: messages.projectMetaTitle1,
-        question: messages.questionMetaTitle1,
-        issue: messages.issueMetaTitle1,
-        contribution: messages.contributionMetaTitle1,
-      })
-    );
-    const ideasIndexDescription = formatMessage(
-      messages.ideaNewMetaDescription,
-      {
-        projectName,
-      }
-    );
-
-    return (
-      <Helmet>
-        <title>
-          {`
+  return (
+    <Helmet>
+      <title>
+        {`
             ${
               authUser && authUser.data.attributes.unread_notifications
                 ? `(${authUser.data.attributes.unread_notifications}) `
@@ -60,19 +50,16 @@ const IdeasNewMeta = () => {
             }
             ${ideasIndexTitle}
           `}
-        </title>
-        {getAlternateLinks(locales)}
-        {getCanonicalLink()}
-        <meta name="title" content={ideasIndexTitle} />
-        <meta name="description" content={ideasIndexDescription} />
-        <meta property="og:title" content={ideasIndexTitle} />
-        <meta property="og:description" content={ideasIndexDescription} />
-        <meta property="og:url" content={location.href} />
-      </Helmet>
-    );
-  }
-
-  return null;
+      </title>
+      {getAlternateLinks(locales)}
+      {getCanonicalLink()}
+      <meta name="title" content={ideasIndexTitle} />
+      <meta name="description" content={ideasIndexDescription} />
+      <meta property="og:title" content={ideasIndexTitle} />
+      <meta property="og:description" content={ideasIndexDescription} />
+      <meta property="og:url" content={location.href} />
+    </Helmet>
+  );
 };
 
 export default IdeasNewMeta;
--- front/app/containers/IdeasNewSurveyPage/IdeasNewSurveyMeta.tsx
+++ front/app/containers/IdeasNewSurveyPage/IdeasNewSurveyMeta.tsx
@@ -1,43 +1,34 @@
 import React from 'react';
 
 import { Helmet } from 'react-helmet';
-import { useParams } from 'react-router-dom';
 
 import useAuthUser from 'api/me/useAuthUser';
-import useProjectBySlug from 'api/projects/useProjectBySlug';
 
 import useAppConfigurationLocales from 'hooks/useAppConfigurationLocales';
-import useLocalize from 'hooks/useLocalize';
 
 import { useIntl } from 'utils/cl-intl';
 import getAlternateLinks from 'utils/cl-router/getAlternateLinks';
 import getCanonicalLink from 'utils/cl-router/getCanonicalLink';
 
+import ideationMessages from '../IdeasNewPage/messages';
+
 import messages from './messages';
 
 const IdeasNewSurveyMeta = () => {
   const { formatMessage } = useIntl();
-  const localize = useLocalize();
-  const { slug } = useParams();
   const { data: authUser } = useAuthUser();
-  const { data: project } = useProjectBySlug(slug);
   const locales = useAppConfigurationLocales();
   const { location } = window;
 
-  if (project) {
-    const projectName = localize(project.data.attributes.title_multiloc);
-    const ideasIndexTitle = formatMessage(messages.surveyNewMetaTitle1);
-    const ideasIndexDescription = formatMessage(
-      messages.ideaNewMetaDescription,
-      {
-        projectName,
-      }
-    );
+  const ideasIndexTitle = formatMessage(messages.surveyNewMetaTitle1);
+  const ideasIndexDescription = formatMessage(
+    ideationMessages.ideaNewMetaDescription
+  );
 
-    return (
-      <Helmet>
-        <title>
-          {`
+  return (
+    <Helmet>
+      <title>
+        {`
             ${
               authUser && authUser.data.attributes.unread_notifications
                 ? `(${authUser.data.attributes.unread_notifications}) `
@@ -45,19 +36,16 @@ const IdeasNewSurveyMeta = () => {
             }
             ${ideasIndexTitle}
           `}
-        </title>
-        {getAlternateLinks(locales)}
-        {getCanonicalLink()}
-        <meta name="title" content={ideasIndexTitle} />
-        <meta name="description" content={ideasIndexDescription} />
-        <meta property="og:title" content={ideasIndexTitle} />
-        <meta property="og:description" content={ideasIndexDescription} />
-        <meta property="og:url" content={location.href} />
-      </Helmet>
-    );
-  }
-
-  return null;
+      </title>
+      {getAlternateLinks(locales)}
+      {getCanonicalLink()}
+      <meta name="title" content={ideasIndexTitle} />
+      <meta name="description" content={ideasIndexDescription} />
+      <meta property="og:title" content={ideasIndexTitle} />
+      <meta property="og:description" content={ideasIndexDescription} />
+      <meta property="og:url" content={location.href} />
+    </Helmet>
+  );
 };
 
 export default IdeasNewSurveyMeta;
