3643d3c61b6750ff0e2175e0181a8b382d57fe23
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-06-03 20:19:12 +0100

    Extract STALENESS_PERIOD variable

--- front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ProjectsWidget/ProjectsCard/utils.ts
+++ front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ProjectsWidget/ProjectsCard/utils.ts
@@ -2,6 +2,8 @@ import moment, { Moment } from 'moment';
 
 import { Period } from 'api/graph_data_units/responseTypes/ProjectsWidget';
 
+const STALENESS_PERIOD = 30;
+
 export const deriveProjectStatus = (period: Period, now: Moment) => {
   const startAt = moment(period.start_at);
   const lastPhaseStartAt = moment(period.last_phase_start_at);
@@ -13,7 +15,7 @@ export const deriveProjectStatus = (period: Period, now: Moment) => {
 
   if (endAt === null) {
     const daysSinceLastPhaseStart = now.diff(lastPhaseStartAt, 'days');
-    return daysSinceLastPhaseStart < 30 ? 'active' : 'stale';
+    return daysSinceLastPhaseStart < STALENESS_PERIOD ? 'active' : 'stale';
   }
 
   return now.isAfter(endAt) ? 'finished' : 'active';
