cca495e20b55dddc6fbc99cdabda7285a7d8c051
brentguf <brentguf@users.noreply.github.com>
2024-06-03 17:07:31 +0200

    Revert "Remove unnecessary redirect"
    This reverts commit a3a17916dc7e79b7b230fae65a4c13daa176ae43.

--- front/app/api/authentication/sign_in_out/signOut.ts
+++ front/app/api/authentication/sign_in_out/signOut.ts
@@ -1,3 +1,5 @@
+import { RouteType } from 'routes';
+
 import { getJwt, removeJwt, decode } from 'utils/auth/jwt';
 import {
   invalidateQueryCache,
@@ -30,6 +32,10 @@ export default async function signOut() {
         (endsWith(pathname, '/sign-up') || pathname.startsWith('/admin'))
       ) {
         clHistory.push('/');
+      }
+
+      if (pathname && endsWith(pathname, '/ideas/new')) {
+        clHistory.push(pathname.split('/ideas/new')[0] as RouteType);
       }
     }
   }
