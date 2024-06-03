04bdb6346f9b13854be01a01f1940df80c1d6c82
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-06-03 20:03:21 +0100

    Improve query according to Adrien's suggestion

--- back/engines/commercial/report_builder/spec/services/report_builder/queries/projects_spec.rb
+++ back/engines/commercial/report_builder/spec/services/report_builder/queries/projects_spec.rb
@@ -60,14 +60,14 @@ RSpec.describe ReportBuilder::Queries::Projects do
       result = query.run_query(start_at: Date.new(2021, 1, 1), end_at: Date.new(2021, 4, 1))
       expect(result[:periods].count).to eq(2)
       expect(result[:periods][@project1.id]).to eq({
-        start_at: Date.new(2021, 2, 1),
-        last_phase_start_at: Date.new(2021, 2, 1),
-        end_at: Date.new(2021, 3, 1)
+        'start_at' => Date.new(2021, 2, 1),
+        'last_phase_start_at' => Date.new(2021, 2, 1),
+        'end_at' => Date.new(2021, 3, 1)
       })
       expect(result[:periods][@project2.id]).to eq({
-        start_at: Date.new(2021, 2, 1),
-        last_phase_start_at: Date.new(2021, 3, 2),
-        end_at: nil
+        'start_at' => Date.new(2021, 2, 1),
+        'last_phase_start_at' => Date.new(2021, 3, 2),
+        'end_at' => nil
       })
     end
   end
