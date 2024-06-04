307fd364b150de937c92f65f4873dfe5382fa71a
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-06-04 11:48:16 +0100

    Move magic numbers

--- front/app/containers/Admin/reporting/components/ReportBuilder/TopBar/index.tsx
+++ front/app/containers/Admin/reporting/components/ReportBuilder/TopBar/index.tsx
@@ -29,6 +29,8 @@ import Button from 'components/UI/Button';
 import { FormattedMessage, useIntl } from 'utils/cl-intl';
 import clHistory from 'utils/cl-router/history';
 
+import { PLATFORM_TEMPLATE_MIN_NUMBER_OF_NODES_BEFORE_AUTOSAVE } from '../Templates/PlatformTemplate/constants';
+import { PROJECT_TEMPLATE_MIN_NUMBER_OF_NODES_BEFORE_AUTOSAVE } from '../Templates/ProjectTemplate/constants';
 import { View } from '../ViewContainer/typings';
 import ViewPicker from '../ViewContainer/ViewPicker';
 
@@ -162,8 +164,16 @@ const ContentBuilderTopBar = ({
       // if we early return here, we basically wait for the next interval and check
       // again if the number of nodes is already correct.
       const numberOfNodes = Object.keys(nodes).length;
-      if (displayName === 'ProjectTemplate' && numberOfNodes < 5) return;
-      if (displayName === 'PlatformTemplate' && numberOfNodes < 30) return;
+      if (
+        displayName === 'ProjectTemplate' &&
+        numberOfNodes < PROJECT_TEMPLATE_MIN_NUMBER_OF_NODES_BEFORE_AUTOSAVE
+      )
+        return;
+      if (
+        displayName === 'PlatformTemplate' &&
+        numberOfNodes < PLATFORM_TEMPLATE_MIN_NUMBER_OF_NODES_BEFORE_AUTOSAVE
+      )
+        return;
 
       updateReportLayout(
         {
