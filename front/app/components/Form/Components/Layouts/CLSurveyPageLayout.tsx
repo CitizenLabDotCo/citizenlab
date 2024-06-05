b9b0cd15a69cb96f7c41be27cb57f8b4e33b3cf5
brentguf <brentguf@users.noreply.github.com>
2024-06-05 13:54:36 +0200

    Clarify comment further

--- front/app/components/Form/Components/Layouts/CLSurveyPageLayout.tsx
+++ front/app/components/Form/Components/Layouts/CLSurveyPageLayout.tsx
@@ -281,8 +281,9 @@ const CLSurveyPageLayout = memo(
           </Box>
         </Box>
         {/*
-          TODO: we should move the progress bar into IdeasNewSurveyForm/index.tsx if possible.
-          It doesn't belong here as it's not part of the form fields layout. This would also allow us
+          TODO:
+          We should move the footer (progress bar and navigation buttons) into IdeasNewSurveyForm/index.tsx
+          if possible. It doesn't belong here as it's not part of the form fields layout. This would also allow us
           to put the progress bar back on top of the form (as part of the survey header) without
           the scroll bar of the form fields interfering with it. This in turn would allow us to improve
           the form progress UX for screen readers. Our current stance is that it makes more sense to get a
