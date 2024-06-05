977e42ecb0c1a68b1b9a265b85cdbbfbecb2d321
Amanda <33987955+amanda-anderson@users.noreply.github.com>
2024-06-05 17:17:04 +0200

    Update front/app/component-library/components/Radio/index.tsx
    Co-authored-by: Brent Guffens <brentguf@users.noreply.github.com>

--- front/app/component-library/components/Radio/index.tsx
+++ front/app/component-library/components/Radio/index.tsx
@@ -105,7 +105,7 @@ export type Props = {
   onChange?: (arg: any) => void;
   currentValue?: any;
   value: any;
-  usePrimaryBorder?: boolean;
+  usePrimaryBorder: boolean;
   /**
    * Name should be a string that is the same for all radios of the same radio group and unique for each radio group.
    * E.g. if you have a poll with two questions and each question has four answers/radios,
