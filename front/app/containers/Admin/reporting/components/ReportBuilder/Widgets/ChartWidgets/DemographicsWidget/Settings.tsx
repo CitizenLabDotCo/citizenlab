5fc355118fd66aed559ae3fb71c700c9bfbb335a
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-06-12 15:30:34 +0100

    Also replace birthyear by age in demographics widget settings

--- front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ChartWidgets/DemographicsWidget/Settings.tsx
+++ front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ChartWidgets/DemographicsWidget/Settings.tsx
@@ -10,11 +10,19 @@ import {
 } from 'api/user_custom_fields/types';
 import useUserCustomFields from 'api/user_custom_fields/useUserCustomFields';
 
+import useAppConfigurationLocales from 'hooks/useAppConfigurationLocales';
+
 import GroupFilter from 'containers/Admin/dashboard/components/filters/GroupFilter';
 import groupFilterMessages from 'containers/Admin/dashboard/messages';
+import { createMultiloc } from 'containers/Admin/reporting/utils/multiloc';
 
-import { useIntl } from 'utils/cl-intl';
+import {
+  useIntl,
+  useFormatMessageWithLocale,
+  MessageDescriptor,
+} from 'utils/cl-intl';
 
+import platformTemplateMessages from '../../../Templates/PlatformTemplate/messages';
 import UserFieldSelect from '../../_shared/UserFieldSelect';
 import ChartWidgetSettings from '../_shared/ChartWidgetSettings';
 
@@ -32,6 +40,8 @@ export const INPUT_TYPES: IUserCustomFieldInputType[] = ['select', 'number'];
 
 const Settings = () => {
   const { formatMessage } = useIntl();
+  const formatMessageWithLocale = useFormatMessageWithLocale();
+  const appConfigurationLocales = useAppConfigurationLocales();
 
   const { data: userFields } = useUserCustomFields({
     inputTypes: INPUT_TYPES,
@@ -45,6 +55,8 @@ const Settings = () => {
     customFieldId: node.data.props.customFieldId,
     groupId: node.data.props.groupId,
   }));
+
+  if (!appConfigurationLocales) return null;
 
   const setCustomFieldId = (
     value?: string,
@@ -62,11 +74,35 @@ const Settings = () => {
     });
   };
 
+  const toMultiloc = (message: MessageDescriptor) => {
+    return createMultiloc(appConfigurationLocales, (locale) => {
+      return formatMessageWithLocale(locale, message);
+    });
+  };
+
+  toMultiloc(platformTemplateMessages.age); // TODO
+
+  const processedUserFields = userFields?.data
+    .filter(isSupportedField)
+    .map((field) => {
+      if (field.attributes.code === 'birthyear') {
+        return {
+          ...field,
+          attributes: {
+            ...field.attributes,
+            title_multiloc: toMultiloc(platformTemplateMessages.age),
+          },
+        };
+      } else {
+        return field;
+      }
+    });
+
   return (
     <>
       <UserFieldSelect
         userFieldId={customFieldId}
-        userFields={userFields?.data.filter(isSupportedField)}
+        userFields={processedUserFields}
         label={formatMessage(messages.registrationField)}
         emptyOption={false}
         onChange={setCustomFieldId}
