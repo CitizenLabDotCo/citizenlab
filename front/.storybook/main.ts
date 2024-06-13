b1ec83b49b47acf2c4c0d94af3de004366f0e40e
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-06-13 14:08:59 +0100

    Add build settings

--- front/.storybook/main.ts
+++ front/.storybook/main.ts
@@ -65,6 +65,10 @@ const config: StorybookConfig = {
       },
       define: {
         'process.env': process.env
+      },
+      build: {
+        minify: false,
+        sourcemap: false
       }
     });
   },
