1d9b046024f70fc7de5d6cb7490319800f395806
brentguf <brentguf@users.noreply.github.com>
2024-06-13 09:49:34 +0200

    Remove needless checks

--- front/app/modules/commercial/analytics/admin/containers/Visitors/index.tsx
+++ front/app/modules/commercial/analytics/admin/containers/Visitors/index.tsx
@@ -4,9 +4,6 @@ import moment from 'moment';
 
 import { Query } from 'api/analytics/types';
 import useAnalytics from 'api/analytics/useAnalytics';
-import useAuthUser from 'api/me/useAuthUser';
-
-import { isAdmin } from 'utils/permissions/roles';
 
 import VisitorsOverview from './VisitorsOverview';
 
@@ -33,22 +30,14 @@ const query: Query = {
 };
 
 const Visitors = () => {
-  const { data: authUser } = useAuthUser();
-  const { data: analytics } = useAnalytics<Response>(
-    query,
-    undefined,
-    authUser ? isAdmin(authUser) : false
-  );
+  const { data: analytics } = useAnalytics<Response>(query, undefined, true);
 
-  if (!analytics || !isAdmin(authUser)) {
+  if (!analytics) {
     return null;
   }
 
   const [countData] = analytics.data.attributes;
-  if (!countData) return null;
-
   const firstDate = countData.first_dimension_date_created_date;
-
   const uniqueVisitorDataDate =
     typeof firstDate === 'string' ? moment(firstDate) : undefined;
 
