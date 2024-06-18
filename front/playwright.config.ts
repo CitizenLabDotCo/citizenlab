7cf16dd57e7d07b050cc00440110418f68f9281a
Iva <iva@citizenlab.co>
2024-06-18 14:28:28 +0300

    Increase timeout

--- front/playwright.config.ts
+++ front/playwright.config.ts
@@ -28,13 +28,13 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    actionTimeout: 0,
+    actionTimeout: 120000,
   },
 
-  timeout: 0,
+  timeout: 120000,
 
   expect: {
-    timeout: 0,
+    timeout: 120000,
   },
 
   /* Configure projects for major browsers */
