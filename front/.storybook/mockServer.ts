62aa5d9112bc5e6526d1f64d36f9ee77e16d358d
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-06-18 09:21:16 +0100

    Reorganize mockServer

--- front/.storybook/mockServer.ts
+++ front/.storybook/mockServer.ts
@@ -3,6 +3,13 @@ import appConfigurationEndpoints from '../app/api/app_configuration/__mocks__/_m
 import usersMeEndpoints from '../app/api/me/__mocks__/_mockServer';
 import usersEndpoints from '../app/api/users/__mocks__/_mockServer';
 import authenticationRequirementsEndpoints from '../app/api/authentication/authentication_requirements/__mocks__/_mockServer';
+import analyticsEndpoints from '../app/api/analytics/__mocks__/_mockServer';
+import surveyResultsEndpoints from '../app/api/survey_results/__mocks__/_mockServer';
+import graphDataUnitsEndpoints from '../app/api/graph_data_units/__mocks__/_mockServer';
+import onboardingCampaignEndpoints from '../app/api/onboarding_campaigns/__mocks__/_mockServer';
+import phasesEndpoints from '../app/api/phases/__mocks__/_mockServer';
+import basketsEndpoints from '../app/api/baskets/__mocks__/_mockServer';
+import avatarsEndpoints from '../app/api/avatars/__mocks__/_mockServer';
 
 // initiatives mock data
 import initiativesEndpoints from '../app/api/initiatives/__mocks__/_mockServer';
@@ -18,36 +25,22 @@ import ideaImagesEndpoints from '../app/api/idea_images/__mocks__/_mockServer';
 import projectsEndpoints from '../app/api/projects/__mocks__/_mockServer';
 import projectImagesEndpoints from '../app/api/project_images/__mocks__/_mockServer';
 
-// phases mock data
-import phasesEndpoints from '../app/api/phases/__mocks__/_mockServer';
-
-// baskets mock data
-import basketsEndpoints from '../app/api/baskets/__mocks__/_mockServer';
-
-// avatars mock data
-import avatarsEndpoints from '../app/api/avatars/__mocks__/_mockServer';
-
 // reports mock data
 import reportsEndpoints from '../app/api/reports/__mocks__/_mockServer';
 import reportLayoutEndpoints from '../app/api/report_layout/__mocks__/_mockServer';
-
-// analytics mock data
-import analyticsEndpoints from '../app/api/analytics/__mocks__/_mockServer';
-
-// survey results mock data
-import surveyResultsEndpoints from '../app/api/survey_results/__mocks__/_mockServer';
-
-// graph data units mock data
-import graphDataUnitsEndpoints from '../app/api/graph_data_units/__mocks__/_mockServer';
-
-// onboarding campaign mock data
-import onboardingCampaignEndpoints from '../app/api/onboarding_campaigns/__mocks__/_mockServer';
 
 const endpoints = {
   ...appConfigurationEndpoints,
   ...usersMeEndpoints,
   ...usersEndpoints,
   ...authenticationRequirementsEndpoints,
+  ...analyticsEndpoints,
+  ...surveyResultsEndpoints,
+  ...graphDataUnitsEndpoints,
+  ...onboardingCampaignEndpoints,
+  ...phasesEndpoints,
+  ...basketsEndpoints,
+  ...avatarsEndpoints,
 
   ...initiativesEndpoints,
   ...initiativeStatusesEndpoints,
@@ -60,22 +53,8 @@ const endpoints = {
   ...projectsEndpoints,
   ...projectImagesEndpoints,
 
-  ...phasesEndpoints,
-
-  ...basketsEndpoints,
-
-  ...avatarsEndpoints,
-
   ...reportsEndpoints,
   ...reportLayoutEndpoints,
-
-  ...analyticsEndpoints,
-
-  ...surveyResultsEndpoints,
-
-  ...graphDataUnitsEndpoints,
-
-  ...onboardingCampaignEndpoints
 };
 
 export default endpoints;
