157c92756a2a66f9ffcca125e70ffb6a798266cb
Iva <iva@citizenlab.co>
2024-06-18 15:11:58 +0300

    Set timeout at 10 min

--- front/playwright.config.ts
+++ front/playwright.config.ts
@@ -24,17 +24,17 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.CI? 'http://e2e.front:3000' : 'http://localhost:3000',
+    baseURL: process.env.CI ? 'http://e2e.front:3000' : 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    actionTimeout: 120000,
+    actionTimeout: 10 * 60 * 1000, // 10 minutes
   },
 
-  timeout: 120000,
+  timeout: 10 * 60 * 1000, // 10 minutes
 
   expect: {
-    timeout: 120000,
+    timeout: 10 * 60 * 1000, // 10 minutes
   },
 
   /* Configure projects for major browsers */
