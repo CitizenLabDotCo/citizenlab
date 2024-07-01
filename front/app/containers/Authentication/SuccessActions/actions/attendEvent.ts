5023e1c60364834fd89be69a2589f5d857ea811d
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-07-01 13:51:58 +0100

    Add api calls to attendEvent action

--- front/app/containers/Authentication/SuccessActions/actions/attendEvent.ts
+++ front/app/containers/Authentication/SuccessActions/actions/attendEvent.ts
@@ -1,12 +1,19 @@
 import { IEventData } from 'api/events/types';
 import { IUserData } from 'api/users/types';
 
+import { addEventAttendance } from 'api/event_attendance/useAddEventAttendance';
+import { addFollower } from 'api/follow_unfollow/useAddFollower';
+
 export interface AttendEventParams {
   event: IEventData;
 }
 
-export const attendEvent = (_: AttendEventParams) => {
+export const attendEvent = ({ event }: AttendEventParams) => {
   return async (authUser: IUserData) => {
-    // attendFunction(authUser);
+    addEventAttendance({ eventId: event.id, attendeeId: authUser.id });
+    addFollower({
+      followableId: event.relationships.project.data.id,
+      followableType: 'projects',
+    });
   };
 };
