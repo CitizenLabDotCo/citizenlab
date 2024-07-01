062a18d75e9de442de9ee32e8947b8d537c90de9
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-07-01 14:41:45 +0100

    Correctly invalidate stuff in success action

--- front/app/containers/Authentication/SuccessActions/actions/attendEvent.ts
+++ front/app/containers/Authentication/SuccessActions/actions/attendEvent.ts
@@ -4,6 +4,11 @@ import { IUserData } from 'api/users/types';
 import { addEventAttendance } from 'api/event_attendance/useAddEventAttendance';
 import { addFollower } from 'api/follow_unfollow/useAddFollower';
 import eventEmitter from 'utils/eventEmitter';
+import { queryClient } from 'utils/cl-react-query/queryClient';
+
+import projectsKeys from 'api/projects/keys';
+
+import eventsKeys from 'api/events/keys';
 
 export interface AttendEventParams {
   event: IEventData;
@@ -11,13 +16,24 @@ export interface AttendEventParams {
 
 export const attendEvent = ({ event }: AttendEventParams) => {
   return async (authUser: IUserData) => {
+    const projectId = event.relationships.project.data.id;
+    const attendeeId = authUser.id;
+
     await Promise.all([
-      addEventAttendance({ eventId: event.id, attendeeId: authUser.id }),
+      addEventAttendance({ eventId: event.id, attendeeId }),
       addFollower({
-        followableId: event.relationships.project.data.id,
+        followableId: projectId,
         followableType: 'projects',
       }),
     ]);
+
+    queryClient.invalidateQueries({
+      queryKey: projectsKeys.item({ id: projectId }),
+    });
+
+    queryClient.invalidateQueries({
+      queryKey: eventsKeys.list({ attendeeId }),
+    });
 
     eventEmitter.emit('eventAttendance');
   };
