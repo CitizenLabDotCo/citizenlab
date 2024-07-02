684d43db173792f469bd7ad55a3aacf346f49e5e
jamesspeake <james@speake.org>
2024-07-02 17:39:06 +0100

    [TAN-2212] Added attributes to permissions_field serializers

--- back/engines/commercial/multi_tenancy/app/services/multi_tenancy/templates/serializers/permissions_field.rb
+++ back/engines/commercial/multi_tenancy/app/services/multi_tenancy/templates/serializers/permissions_field.rb
@@ -4,7 +4,13 @@ module MultiTenancy
   module Templates
     module Serializers
       class PermissionsField < Base
-        attribute :required
+        attributes %i[
+          required
+          field_type
+          verified
+          enabled
+        ]
+
         ref_attributes %i[custom_field permission]
       end
     end
