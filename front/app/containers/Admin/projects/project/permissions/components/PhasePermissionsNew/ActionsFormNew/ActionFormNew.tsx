a8d9b11c6c8a6be826d318cef723f384fe187700
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-07-02 18:55:09 +0100

    Very rough scaffolding of card buttons

--- front/app/containers/Admin/projects/project/permissions/components/PhasePermissionsNew/ActionsFormNew/ActionFormNew.tsx
+++ front/app/containers/Admin/projects/project/permissions/components/PhasePermissionsNew/ActionsFormNew/ActionFormNew.tsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import { Box, CardButton } from '@citizenlab/cl2-component-library';
+import { useIntl } from 'utils/cl-intl';
+import messages from './messages';
+import actionFormMessages from 'components/admin/ActionsForm/messages';
+import permissionsMessages from 'containers/Admin/projects/project/permissions/messages';
 
 type Props = any;
 
 const ActionFormNew = (_: Props) => {
-  return <>Action!</>;
+  const { formatMessage } = useIntl();
+
+  const permittedBy = 'users' as any; // TODO
+
+  const handlePermittedByUpdate = (_permittedBy: any) => (e) => {
+    e.preventDefault();
+  }; // TODO
+
+  return (
+    <form>
+      <Box display="flex" gap="16px">
+        {/* TODO hide anyone if not survey */}
+        <CardButton
+          id="e2e-permission-anyone"
+          iconName="user-circle"
+          title={formatMessage(permissionsMessages.permissionsAnyoneLabel)}
+          subtitle={formatMessage(
+            permissionsMessages.permissionsAnyoneLabelDescription
+          )}
+          onClick={handlePermittedByUpdate('anyone')} // TODO
+          selected={permittedBy === 'anyone'} // TODO
+        />
+        <CardButton
+          id="e2e-permission-registered-users"
+          iconName="email"
+          title={formatMessage(actionFormMessages.permissionsUsersLabel)}
+          subtitle={formatMessage(
+            actionFormMessages.permissionsUsersLabelDescription
+          )}
+          onClick={handlePermittedByUpdate('users')} // TODO
+          selected={permittedBy === 'users'} // TODO
+        />
+        <CardButton
+          id="e2e-permission-custom"
+          iconName="cog"
+          title={formatMessage(messages.custom)}
+          subtitle={formatMessage(messages.customSubtitle)}
+          onClick={handlePermittedByUpdate('custom')} // TODO
+          selected={permittedBy === 'custom'} // TODO
+        />
+      </Box>
+    </form>
+  );
 };
 
 export default ActionFormNew;
---
+++ front/app/containers/Admin/projects/project/permissions/components/PhasePermissionsNew/ActionsFormNew/messages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  custom: {
+    id: 'app.containers.Admin.projects.project.permissions.components.PhasePermissionsNew.ActionsFormNew.custom',
+    defaultMessage: 'Custom',
+  },
+  customSubtitle: {
+    id: 'app.containers.Admin.projects.project.permissions.components.PhasePermissionsNew.ActionsFormNew.customSubtitle',
+    defaultMessage: 'Add your own rules on who can participate',
+  },
+});
