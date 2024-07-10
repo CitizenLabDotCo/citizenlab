38849fe8d2a72789fc0688d0ef95fa5f41c3b85f
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-07-10 14:44:53 +0100

    _

--- front/app/containers/Admin/projects/project/permissions/components/PhasePermissions/index.tsx
+++ front/app/containers/Admin/projects/project/permissions/components/PhasePermissions/index.tsx
@@ -122,6 +122,8 @@ const ActionsFormWrapper = ({
   projectId,
 }: ActionsFormWrapperProps) => {
   const { data: permissions } = usePhasePermissions({ phaseId: phase.id });
+  console.log({ permissions });
+
   if (!permissions) {
     return null;
   }
