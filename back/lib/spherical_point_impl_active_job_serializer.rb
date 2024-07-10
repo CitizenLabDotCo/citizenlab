d0f45c3b1ed85227e14996acb9b7dce33c29c4ff
Alexander <alexander@citizenlab.co>
2024-07-10 22:45:52 +0200

    [TAN-1736] Fix error serializing SphericalPointImpl
    RGeo::Geographic::SphericalPointImpl instance, that is passed to ActiveJob
    from EventRegistrationConfirmation#generate_commands in event_attributes field,
    cannot be serialized and raises this error:
    Unsupported argument type: RGeo::Geographic::SphericalPointImpl (ActiveJob::SerializationError)
    https://sentry.hq.citizenlab.co/organizations/citizenlab/issues/76573/events/0a7fd4690cc14582b3bacabc078e1e66/?cursor=0%3A600%3A0&project=2
    
    The issue exists since we introduced EventRegistrationConfirmation email.
    Tested and reproduced it in 7f8e6b0c1fa
    
    I found only one issue related to this error, but the fix
    is already used in our postgis-adapter version and it
    doesn't help
    https://github.com/rgeo/activerecord-postgis-adapter/issues/307

---
+++ back/lib/spherical_point_impl_active_job_serializer.rb
@@ -0,0 +1,20 @@
+# https://blog.saeloun.com/2019/09/11/rails-6-custom-serializers-for-activejob-arguments/
+# https://api.rubyonrails.org/classes/ActiveJob/Serializers/ObjectSerializer.html
+class SphericalPointImplActiveJobSerializer < ActiveJob::Serializers::ObjectSerializer
+  def serialize(point)
+    {
+      'x' => point.x,
+      'y' => point.y
+    }
+  end
+
+  def deserialize(hash)
+    RGeo::Geographic.spherical_factory.point(hash['x'], hash['y'])
+  end
+
+  private
+
+  def klass
+    RGeo::Geographic::SphericalPointImpl
+  end
+end
