b4bf5c8176f5d2f5a2a38b6bcc544e624c4e3f6f
Iva <iva@citizenlab.co>
2024-07-16 18:16:34 +0300

    Introduce default configs

--- front/app/containers/Admin/projects/project/phase/phaseParticipationConfig/index.tsx
+++ front/app/containers/Admin/projects/project/phase/phaseParticipationConfig/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import {
   Box,
@@ -18,7 +18,6 @@ import {
   TSurveyService,
   VotingMethod,
 } from 'api/phases/types';
-import { getDefaultSortMethodFallback } from 'api/phases/utils';
 import { IProject } from 'api/projects/types';
 
 import useFeatureFlag from 'hooks/useFeatureFlag';
@@ -109,62 +108,44 @@ const PhaseParticipationConfig = ({
 
   const { data: appConfig } = useAppConfiguration();
   const { formatMessage } = useIntl();
+  const [participationConfig, setParticipationConfig] =
+    useState<IPhaseParticipationConfig>({
+      participation_method:
+        phase?.data.attributes.participation_method || 'ideation',
+      posting_enabled: phase?.data.attributes.posting_enabled ?? true,
+      commenting_enabled: phase?.data.attributes.commenting_enabled ?? true,
+      reacting_enabled: phase?.data.attributes.reacting_enabled ?? true,
+      reacting_like_method:
+        phase?.data.attributes.reacting_like_method || 'unlimited',
+      reacting_like_limited_max:
+        phase?.data.attributes.reacting_like_limited_max || null,
+      reacting_dislike_enabled:
+        phase?.data.attributes.reacting_dislike_enabled ?? true,
+      reacting_dislike_method:
+        phase?.data.attributes.reacting_dislike_method || 'unlimited',
+      reacting_dislike_limited_max:
+        phase?.data.attributes.reacting_dislike_limited_max || null,
+      allow_anonymous_participation:
+        phase?.data.attributes.allow_anonymous_participation || false,
+      voting_method: phase?.data.attributes.voting_method || 'single_voting',
+      voting_min_total: phase?.data.attributes.voting_min_total || null,
+      voting_max_total: phase?.data.attributes.voting_max_total || null,
+      survey_service: phase?.data.attributes.survey_service || null,
+      survey_embed_url: phase?.data.attributes.survey_embed_url || null,
+      voting_max_votes_per_idea:
+        phase?.data.attributes.voting_max_votes_per_idea || null,
+      poll_anonymous: phase?.data.attributes.poll_anonymous || false,
+      presentation_mode: phase?.data.attributes.presentation_mode || 'card',
+      ideas_order: phase?.data.attributes.ideas_order || 'trending',
+      input_term: phase?.data.attributes.input_term || 'idea',
+      document_annotation_embed_url:
+        phase?.data.attributes.document_annotation_embed_url || null,
+      voting_term_singular_multiloc:
+        phase?.data.attributes.voting_term_singular_multiloc || null,
+      voting_term_plural_multiloc:
+        phase?.data.attributes.voting_term_plural_multiloc || null,
+    });
 
-  const [participation_method, setParticipationMethod] =
-    useState<ParticipationMethod>(
-      phase?.data.attributes.participation_method || 'ideation'
-    );
-
-  const [posting_enabled, setPostingEnabled] = useState<
-    IPhaseParticipationConfig['posting_enabled']
-  >(phase?.data.attributes.posting_enabled ?? true);
-
-  const [commenting_enabled, setCommentingEnabled] = useState<
-    IPhaseParticipationConfig['commenting_enabled']
-  >(phase?.data.attributes.commenting_enabled ?? true);
-  const [reacting_enabled, setReactingEnabled] = useState<
-    IPhaseParticipationConfig['reacting_enabled']
-  >(phase?.data.attributes.reacting_enabled ?? true);
-  const [reacting_like_method, setReactingLikeMethod] = useState<
-    IPhaseParticipationConfig['reacting_like_method']
-  >(phase?.data.attributes.reacting_like_method || 'unlimited');
-  const [reacting_dislike_method, setReactingDislikeMethod] = useState<
-    IPhaseParticipationConfig['reacting_dislike_method']
-  >(phase?.data.attributes.reacting_dislike_method || 'unlimited');
-  const [reacting_like_limited_max, setReactingLikeLimitedMax] = useState<
-    IPhaseParticipationConfig['reacting_like_limited_max']
-  >(phase?.data.attributes.reacting_like_limited_max || null);
-  const [reacting_dislike_limited_max, setReactingDislikeLimitedMax] = useState<
-    IPhaseParticipationConfig['reacting_dislike_limited_max']
-  >(phase?.data.attributes.reacting_dislike_limited_max || null);
-  const [reacting_dislike_enabled, setReactingDislikeEnabled] = useState<
-    IPhaseParticipationConfig['reacting_dislike_enabled']
-  >(phase?.data.attributes.reacting_dislike_enabled ?? true);
-  const [allow_anonymous_participation, setAllowAnonymousParticipation] =
-    useState<IPhaseParticipationConfig['allow_anonymous_participation']>(
-      phase?.data.attributes.allow_anonymous_participation || false
-    );
-  const [voting_method, setVotingMethod] = useState<
-    IPhaseParticipationConfig['voting_method']
-  >(phase?.data.attributes.voting_method || 'single_voting');
-  const [presentation_mode, setPresentationMode] = useState<
-    IPhaseParticipationConfig['presentation_mode']
-  >(phase?.data.attributes.presentation_mode || 'card');
-  const [voting_min_total, setVotingMinTotal] = useState<
-    IPhaseParticipationConfig['voting_min_total']
-  >(phase?.data.attributes.voting_min_total || null);
-  const [voting_max_total, setVotingMaxTotal] = useState<
-    IPhaseParticipationConfig['voting_max_total']
-  >(phase?.data.attributes.voting_max_total || null);
-  const [survey_service, setSurveyService] = useState<
-    IPhaseParticipationConfig['survey_service']
-  >(phase?.data.attributes.survey_service || null);
-  const [survey_embed_url, setSurveyEmbedUrl] = useState<
-    IPhaseParticipationConfig['survey_embed_url']
-  >(phase?.data.attributes.survey_embed_url || null);
-  const [voting_max_votes_per_idea, setVotingMaxVotesPerIdea] = useState<
-    IPhaseParticipationConfig['voting_max_votes_per_idea']
-  >(phase?.data.attributes.voting_max_votes_per_idea || null);
   const [noLikingLimitError, setNoLikingLimitError] =
     useState<JSX.Element | null>(null);
   const [noDislikingLimitError, setNoDislikingLimitError] =
@@ -179,73 +160,6 @@ const PhaseParticipationConfig = ({
     string | null
   >(null);
   const [voteTermError, setVoteTermError] = useState<string | null>(null);
-  const [poll_anonymous, setPollAnonymous] = useState<
-    IPhaseParticipationConfig['poll_anonymous']
-  >(phase?.data.attributes.poll_anonymous || false);
-  const [ideas_order, setIdeasOrder] = useState<
-    IPhaseParticipationConfig['ideas_order']
-  >(phase?.data.attributes.ideas_order || 'trending');
-  const [input_term, setInputTerm] = useState<
-    IPhaseParticipationConfig['input_term']
-  >(phase?.data.attributes.input_term || 'idea');
-  const [document_annotation_embed_url, setDocumentAnnotationEmbedUrl] =
-    useState<IPhaseParticipationConfig['document_annotation_embed_url']>(
-      phase?.data.attributes.document_annotation_embed_url || null
-    );
-
-  const [voting_term_singular_multiloc, setVotingTermSingularMultiloc] =
-    useState<IPhaseParticipationConfig['voting_term_singular_multiloc']>(
-      phase?.data.attributes.voting_term_singular_multiloc || null
-    );
-  const [voting_term_plural_multiloc, setVotingTermPluralMultiloc] = useState<
-    IPhaseParticipationConfig['voting_term_plural_multiloc']
-  >(phase?.data.attributes.voting_term_plural_multiloc || null);
-
-  const participationConfig = useMemo(() => {
-    return {
-      participation_method,
-      posting_enabled,
-      commenting_enabled,
-      reacting_enabled,
-      reacting_like_method,
-      reacting_like_limited_max,
-      reacting_dislike_enabled,
-      reacting_dislike_method,
-      allow_anonymous_participation,
-      voting_method,
-      voting_min_total,
-      voting_max_total,
-      voting_max_votes_per_idea,
-      survey_embed_url,
-      survey_service,
-      document_annotation_embed_url,
-      poll_anonymous,
-      presentation_mode,
-      ideas_order,
-      input_term,
-    };
-  }, [
-    participation_method,
-    posting_enabled,
-    commenting_enabled,
-    reacting_enabled,
-    reacting_like_method,
-    reacting_like_limited_max,
-    reacting_dislike_enabled,
-    reacting_dislike_method,
-    allow_anonymous_participation,
-    voting_method,
-    voting_min_total,
-    voting_max_total,
-    voting_max_votes_per_idea,
-    survey_embed_url,
-    survey_service,
-    document_annotation_embed_url,
-    poll_anonymous,
-    presentation_mode,
-    ideas_order,
-    input_term,
-  ]);
 
   useEffect(() => {
     onChange(participationConfig);
@@ -296,117 +210,200 @@ const PhaseParticipationConfig = ({
     const native_survey = participation_method === 'native_survey';
     const voting = participation_method === 'voting';
     const survey = participation_method === 'survey';
-    const ideationOrVoting = ideation || voting;
-    const ideationOrNativeSurvey = ideation || native_survey;
 
-    setParticipationMethod(participation_method);
-    setPostingEnabled(ideationOrNativeSurvey ? true : null);
-    setCommentingEnabled(ideationOrVoting ? true : null);
-    setReactingEnabled(ideationOrNativeSurvey ? true : null);
-    setReactingLikeMethod(ideationOrNativeSurvey ? 'unlimited' : null);
-    setReactingDislikeEnabled(ideationOrNativeSurvey ? true : null);
-    setReactingDislikeMethod(ideationOrNativeSurvey ? 'unlimited' : null);
-    setAllowAnonymousParticipation(ideationOrNativeSurvey ? false : null);
-    setVotingMethod(voting ? 'single_voting' : null);
-    setPresentationMode(ideationOrVoting ? 'card' : null);
-    setSurveyEmbedUrl(null);
-    setSurveyService(survey ? 'typeform' : null);
-    setDocumentAnnotationEmbedUrl(null);
-    setVotingMinTotal(voting ? 0 : null);
-    setVotingMaxTotal(voting ? 100 : null);
-    setVotingMaxVotesPerIdea(voting ? 1 : null);
-    setIdeasOrder(ideation ? getDefaultSortMethodFallback(ideation) : null);
+    const ideationDefaultConfig: IPhaseParticipationConfig = {
+      participation_method,
+      posting_enabled: true,
+      commenting_enabled: true,
+      reacting_enabled: true,
+      reacting_like_method: 'unlimited',
+      reacting_dislike_enabled: true,
+      reacting_dislike_method: 'unlimited',
+      allow_anonymous_participation: false,
+      presentation_mode: 'card',
+      ideas_order: 'trending',
+    };
+
+    const votingDefaultConfig: IPhaseParticipationConfig = {
+      participation_method,
+      voting_method: 'single_voting',
+      voting_min_total: 0,
+      voting_max_total: 100,
+      voting_max_votes_per_idea: 1,
+      voting_term_singular_multiloc: {
+        en: 'vote',
+      },
+      voting_term_plural_multiloc: {
+        en: 'votes',
+      },
+    };
+
+    const surveyDefaultConfig: IPhaseParticipationConfig = {
+      participation_method,
+      survey_service: 'typeform',
+    };
+
+    const nativeSurveyDefaultConfig: IPhaseParticipationConfig = {
+      participation_method,
+      allow_anonymous_participation: false,
+      posting_enabled: true,
+      reacting_enabled: true,
+      reacting_like_method: 'unlimited',
+      reacting_dislike_enabled: true,
+      reacting_dislike_method: 'unlimited',
+    };
+
+    const commonConfig: IPhaseParticipationConfig = {
+      participation_method,
+    };
+
+    setParticipationConfig(() => ({
+      ...(ideation ? ideationDefaultConfig : {}),
+      ...(voting ? votingDefaultConfig : {}),
+      ...(survey ? surveyDefaultConfig : {}),
+      ...(native_survey ? nativeSurveyDefaultConfig : {}),
+      ...commonConfig,
+    }));
   };
 
+  console.log(participationConfig);
+
   const handleSurveyProviderChange = (survey_service: TSurveyService) => {
-    setSurveyService(survey_service);
+    setParticipationConfig((state) => ({
+      ...state,
+      survey_service,
+    }));
   };
 
   const handleSurveyEmbedUrlChange = (survey_embed_url: string) => {
-    setSurveyEmbedUrl(survey_embed_url);
+    setParticipationConfig((state) => ({
+      ...state,
+      survey_embed_url,
+    }));
   };
 
   const handleDocumentAnnotationEmbedUrlChange = (
     document_annotation_embed_url: string
   ) => {
-    setDocumentAnnotationEmbedUrl(document_annotation_embed_url);
+    setParticipationConfig((state) => ({
+      ...state,
+      document_annotation_embed_url,
+    }));
   };
 
   const togglePostingEnabled = () => {
-    setPostingEnabled((state) => !state);
+    setParticipationConfig((state) => ({
+      ...state,
+      posting_enabled: !state.posting_enabled,
+    }));
   };
 
   const toggleCommentingEnabled = () => {
-    setCommentingEnabled((state) => !state);
+    setParticipationConfig((state) => ({
+      ...state,
+      commenting_enabled: !state.commenting_enabled,
+    }));
   };
 
   const toggleReactingEnabled = () => {
-    setReactingEnabled((state) => !state);
+    setParticipationConfig((state) => ({
+      ...state,
+      reacting_enabled: !state.reacting_enabled,
+    }));
   };
 
   const handleReactingLikeMethodOnChange = (
     reacting_like_method: 'unlimited' | 'limited'
   ) => {
-    setReactingLikeMethod(reacting_like_method);
-    setReactingLikeLimitedMax(reacting_like_method === 'unlimited' ? null : 5);
+    setParticipationConfig((state) => ({
+      ...state,
+      reacting_like_method,
+      reacting_like_limited_max:
+        reacting_like_method === 'unlimited' ? null : 5,
+    }));
   };
 
   const handleLikingLimitOnChange = (reacting_like_limited_max: string) => {
-    setReactingLikeLimitedMax(parseInt(reacting_like_limited_max, 10));
+    setParticipationConfig((state) => ({
+      ...state,
+      reacting_like_limited_max: parseInt(reacting_like_limited_max, 10),
+    }));
     setNoLikingLimitError(null);
   };
 
   const handleReactingDislikeEnabledOnChange = (
     reacting_dislike_enabled: boolean
   ) => {
-    setReactingDislikeEnabled(reacting_dislike_enabled);
+    setParticipationConfig((state) => ({
+      ...state,
+      reacting_dislike_enabled,
+    }));
   };
 
   const handleAllowAnonymousParticipationOnChange = (
     allow_anonymous_participation: boolean
   ) => {
-    setAllowAnonymousParticipation(allow_anonymous_participation);
+    setParticipationConfig((state) => ({
+      ...state,
+      allow_anonymous_participation,
+    }));
   };
 
   const handleVotingMethodOnChange = (voting_method: VotingMethod) => {
     const maxVotes = MAX_VOTES_PER_VOTING_METHOD[voting_method];
 
-    setVotingMethod(voting_method);
-    setVotingMaxVotesPerIdea(
-      voting_method === 'single_voting' ? 1 : voting_max_votes_per_idea
-    );
-    setVotingMaxTotal(maxVotes);
+    setParticipationConfig((state) => ({
+      ...state,
+      voting_method,
+      voting_max_votes_per_idea:
+        voting_method === 'single_voting' ? 1 : state.voting_max_votes_per_idea,
+      voting_max_total: maxVotes,
+    }));
   };
 
   const handleReactingDislikeMethodOnChange = (
     reacting_dislike_method: 'unlimited' | 'limited'
   ) => {
-    setReactingDislikeMethod(reacting_dislike_method);
-    setReactingDislikeLimitedMax(
-      reacting_dislike_method === 'unlimited' ? null : 5
-    );
+    setParticipationConfig((state) => ({
+      ...state,
+      reacting_dislike_method,
+      reacting_dislike_limited_max:
+        reacting_dislike_method === 'unlimited' ? null : 5,
+    }));
   };
 
   const handleDislikingLimitOnChange = (
     rreacting_dislike_limited_max: string
   ) => {
-    setReactingDislikeLimitedMax(parseInt(rreacting_dislike_limited_max, 10));
+    setParticipationConfig((state) => ({
+      ...state,
+      reacting_dislike_limited_max: parseInt(rreacting_dislike_limited_max, 10),
+    }));
     setNoDislikingLimitError(null);
   };
 
   const handleIdeasDisplayChange = (presentation_mode: 'map' | 'card') => {
-    setPresentationMode(presentation_mode);
+    setParticipationConfig((state) => ({
+      ...state,
+      presentation_mode,
+    }));
   };
 
   const handleIdeaDefaultSortMethodChange = (
     ideas_order: IdeaDefaultSortMethod
   ) => {
-    setIdeasOrder(ideas_order);
+    setParticipationConfig((state) => ({
+      ...state,
+      ideas_order,
+    }));
   };
 
   const handleVotingMinTotalChange = (newVotingMinTotal: string) => {
     const voting_min_total = parseInt(newVotingMinTotal, 10);
-    setVotingMinTotal(voting_min_total);
+    setParticipationConfig((state) => ({
+      ...state,
+      voting_min_total,
+    }));
     setMinTotalVotesError(null);
   };
 
@@ -414,38 +411,56 @@ const PhaseParticipationConfig = ({
     const voting_max_total = newVotingMaxTotal
       ? parseInt(newVotingMaxTotal, 10)
       : null;
-    setVotingMaxTotal(voting_max_total);
+    setParticipationConfig((state) => ({
+      ...state,
+      voting_max_total,
+    }));
     setMaxTotalVotesError(null);
   };
 
   const handleVotingMaxPerIdeaChange = (newVotingMaxPerIdeaTotal: string) => {
     const voting_max_votes_per_idea = parseInt(newVotingMaxPerIdeaTotal, 10);
-    setVotingMaxVotesPerIdea(voting_max_votes_per_idea);
+    setParticipationConfig((state) => ({
+      ...state,
+      voting_max_votes_per_idea,
+    }));
     setMaxVotesPerOptionError(null);
   };
 
   const handleVoteTermPluralChange = (
     voting_term_plural_multiloc: Multiloc
   ) => {
-    setVotingTermPluralMultiloc(voting_term_plural_multiloc);
+    setParticipationConfig((state) => ({
+      ...state,
+      voting_term_plural_multiloc,
+    }));
     setVoteTermError(null);
   };
 
   const handleVoteTermSingularChange = (
     voting_term_singular_multiloc: Multiloc
   ) => {
-    setVotingTermSingularMultiloc(voting_term_singular_multiloc);
+    setParticipationConfig((state) => ({
+      ...state,
+      voting_term_singular_multiloc,
+    }));
     setVoteTermError(null);
   };
 
   const handleInputTermChange = (option: IOption) => {
     const input_term: InputTerm = option.value;
 
-    setInputTerm(input_term);
+    setParticipationConfig((state) => ({
+      ...state,
+      input_term,
+    }));
   };
 
   const togglePollAnonymous = () => {
-    setPollAnonymous((state) => !state);
+    setParticipationConfig((state) => ({
+      ...state,
+      poll_anonymous: !state.poll_anonymous,
+    }));
   };
 
   const surveyProviders = {
@@ -459,6 +474,32 @@ const PhaseParticipationConfig = ({
     snap_survey: snap_survey_enabled,
     google_forms: google_forms_enabled,
   };
+
+  const {
+    participation_method,
+    posting_enabled,
+    commenting_enabled,
+    reacting_enabled,
+    reacting_like_method,
+    reacting_like_limited_max,
+    reacting_dislike_enabled,
+    reacting_dislike_method,
+    reacting_dislike_limited_max,
+    allow_anonymous_participation,
+    voting_method,
+    voting_min_total,
+    voting_max_total,
+    voting_max_votes_per_idea,
+    voting_term_singular_multiloc,
+    voting_term_plural_multiloc,
+    survey_service,
+    survey_embed_url,
+    poll_anonymous,
+    presentation_mode,
+    ideas_order,
+    input_term,
+    document_annotation_embed_url,
+  } = participationConfig;
 
   const showSurveys =
     surveys_enabled && anyIsDefined(...Object.values(surveyProviders));
