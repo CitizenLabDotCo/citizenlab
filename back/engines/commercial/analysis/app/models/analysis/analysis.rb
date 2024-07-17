7b3adf79e5cb6bdf5b5f56c6464a3f7a804b10f2
Sébastien Hoorens <sebastien@citizenlab.co>
2024-07-17 14:50:02 +0200

    fix spec after fixing offenses

--- back/engines/commercial/analysis/app/models/analysis/analysis.rb
+++ back/engines/commercial/analysis/app/models/analysis/analysis.rb
@@ -90,9 +90,10 @@ module Analysis
     def project_or_phase_form_context
       # TODO: move-participation-method-logic
       allowed_project_methods = %w[ideation voting]
+      project_or_nil = project || nil
       if phase && %w[native_survey proposals].exclude?(phase.participation_method)
         errors.add(:base, :project_or_phase_form_context, message: 'An analysis should be associated with a valid form context. The passed phase is not associated with a form definition.')
-      elsif project&.phases&.none? { |phase| allowed_project_methods.include?(phase.participation_method) }
+      elsif project_or_nil&.phases&.none? { |phase| allowed_project_methods.include?(phase.participation_method) }
         errors.add(:base, :project_or_phase_form_context, message: 'An analysis should be associated with a valid form context. The passed project has no phases supporting a participation method that can hold inputs')
       end
     end
