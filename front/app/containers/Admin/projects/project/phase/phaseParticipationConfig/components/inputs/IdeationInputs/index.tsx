09bb09c4c487fe9f641197abb8e23ddd7e9fa43c
Iva <iva@citizenlab.co>
2024-07-18 11:21:12 +0300

    Show reaction limits in admin UI

--- front/app/containers/Admin/projects/project/phase/phaseParticipationConfig/components/inputs/IdeationInputs/index.tsx
+++ front/app/containers/Admin/projects/project/phase/phaseParticipationConfig/components/inputs/IdeationInputs/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 
 import {
   Radio,
@@ -98,10 +98,6 @@ const IdeationInputs = ({
   handleIdeaDefaultSortMethodChange,
 }: Props) => {
   const { formatMessage } = useIntl();
-  const reactingLimited = useRef(
-    reacting_like_method === 'limited' || reacting_dislike_method === 'limited'
-  );
-  const showReactingLimits = reactingLimited.current;
 
   return (
     <>
@@ -152,54 +148,52 @@ const IdeationInputs = ({
       </StyledSectionField>
       {reacting_enabled && (
         <>
-          {showReactingLimits && (
-            <SectionField>
-              <SubSectionTitle>
-                <FormattedMessage {...messages.likingMethodTitle} />
-              </SubSectionTitle>
-              <Radio
-                onChange={handleReactingLikeMethodOnChange}
-                currentValue={reacting_like_method}
-                value="unlimited"
-                name="likingmethod"
-                id="likingmethod-unlimited"
-                label={<FormattedMessage {...messages.unlimited} />}
-              />
-              <Radio
-                onChange={handleReactingLikeMethodOnChange}
-                currentValue={reacting_like_method}
-                value="limited"
-                name="likingmethod"
-                id="likingmethod-limited"
-                label={<FormattedMessage {...messages.limited} />}
-              />
-              <Error apiErrors={apiErrors && apiErrors.reacting_method} />
+          <SectionField>
+            <SubSectionTitle>
+              <FormattedMessage {...messages.likingMethodTitle} />
+            </SubSectionTitle>
+            <Radio
+              onChange={handleReactingLikeMethodOnChange}
+              currentValue={reacting_like_method}
+              value="unlimited"
+              name="likingmethod"
+              id="likingmethod-unlimited"
+              label={<FormattedMessage {...messages.unlimited} />}
+            />
+            <Radio
+              onChange={handleReactingLikeMethodOnChange}
+              currentValue={reacting_like_method}
+              value="limited"
+              name="likingmethod"
+              id="likingmethod-limited"
+              label={<FormattedMessage {...messages.limited} />}
+            />
+            <Error apiErrors={apiErrors && apiErrors.reacting_method} />
 
-              {reacting_like_method === 'limited' && (
-                <>
-                  <SubSectionTitle>
-                    <FormattedMessage {...messages.maxLikes} />
-                  </SubSectionTitle>
-                  <ReactingLimitInput
-                    id="liking-limit"
-                    type="number"
-                    min="1"
-                    placeholder=""
-                    value={
-                      reacting_like_limited_max
-                        ? reacting_like_limited_max.toString()
-                        : null
-                    }
-                    onChange={handleLikingLimitOnChange}
-                  />
-                  <Error
-                    text={noLikingLimitError}
-                    apiErrors={apiErrors && apiErrors.reacting_limit}
-                  />
-                </>
-              )}
-            </SectionField>
-          )}
+            {reacting_like_method === 'limited' && (
+              <>
+                <SubSectionTitle>
+                  <FormattedMessage {...messages.maxLikes} />
+                </SubSectionTitle>
+                <ReactingLimitInput
+                  id="liking-limit"
+                  type="number"
+                  min="1"
+                  placeholder=""
+                  value={
+                    reacting_like_limited_max
+                      ? reacting_like_limited_max.toString()
+                      : null
+                  }
+                  onChange={handleLikingLimitOnChange}
+                />
+                <Error
+                  text={noLikingLimitError}
+                  apiErrors={apiErrors && apiErrors.reacting_limit}
+                />
+              </>
+            )}
+          </SectionField>
           <FeatureFlag name="disable_disliking">
             <SectionField>
               <SubSectionTitle>
@@ -230,7 +224,7 @@ const IdeationInputs = ({
                 apiErrors={apiErrors && apiErrors.reacting_dislike_enabled}
               />
             </SectionField>
-            {reacting_dislike_enabled && showReactingLimits && (
+            {reacting_dislike_enabled && (
               <SectionField>
                 <SubSectionTitle>
                   <FormattedMessage {...messages.dislikingMethodTitle} />
