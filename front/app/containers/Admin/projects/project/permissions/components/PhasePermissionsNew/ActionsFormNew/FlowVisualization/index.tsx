07c690fff928f04b4845224545167cbbba41405e
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-07-22 13:45:45 +0100

    Flow visualization: create building blocks

--- front/app/containers/Admin/projects/project/permissions/components/PhasePermissionsNew/ActionsFormNew/FlowVisualization/index.tsx
+++ front/app/containers/Admin/projects/project/permissions/components/PhasePermissionsNew/ActionsFormNew/FlowVisualization/index.tsx
@@ -5,14 +5,37 @@ import { Box, stylingConsts, colors } from '@citizenlab/cl2-component-library';
 const FlowVisualization = () => {
   return (
     <Box display="flex" flexDirection="row">
-      <Box
-        borderRadius={stylingConsts.borderRadius}
-        border={`1px solid ${colors.blue700}`}
-        p="20px"
-      >
-        <Box>1.</Box>
-        <Box>Enter and confirm email (or sign up with SSO)</Box>
-      </Box>
+      <Block number={1} text="Enter and confirm email (or sign up with SSO)" />
+      <Edge />
+      <Block number={2} text="Something else" />
+    </Box>
+  );
+};
+
+interface BlockProps {
+  number: number;
+  text: string;
+}
+
+const Block = ({ number, text }: BlockProps) => {
+  return (
+    <Box
+      borderRadius={stylingConsts.borderRadius}
+      border={`1px solid ${colors.blue700}`}
+      bgColor={colors.teal50}
+      p="20px"
+      w="220px"
+    >
+      <Box>{`${number}.`}</Box>
+      <Box>{text}</Box>
+    </Box>
+  );
+};
+
+const Edge = () => {
+  return (
+    <Box w="20px" display="flex" flexDirection="column" justifyContent="center">
+      <Box w="100%" borderBottom={`1px solid ${colors.blue700}`} h="1px" />
     </Box>
   );
 };
