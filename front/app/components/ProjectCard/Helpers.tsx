55d43c8efd6b591631beddaa5f729bc03692785c
EdwinKato <katedwin88@gmail.com>
2024-07-29 12:01:06 +0300

    Reuse ProjectTitle

--- front/app/components/ProjectCard/Helpers.tsx
+++ front/app/components/ProjectCard/Helpers.tsx
@@ -1,3 +1,4 @@
+import { fontSizes, isRtl } from '@citizenlab/cl2-component-library';
 import styled from 'styled-components';
 
 import Image from 'components/UI/Image';
@@ -24,4 +25,21 @@ export const ProjectImage = styled(Image)`
   position: absolute;
   top: 0;
   left: 0;
+`;
+
+export const ProjectTitle = styled.h3`
+  line-height: normal;
+  font-weight: 500;
+  font-size: ${fontSizes.xl}px;
+  color: ${({ theme }) => theme.colors.tenantText};
+  margin: 0;
+  padding: 0;
+
+  ${isRtl`
+    text-align: right;
+  `}
+
+  &:hover {
+    text-decoration: underline;
+  }
 `;
