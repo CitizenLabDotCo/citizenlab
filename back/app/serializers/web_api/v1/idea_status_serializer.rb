42c2053fd7f5e7c01057fa34b80a6b6068026b90
Sébastien Hoorens <sebastien@citizenlab.co>
2024-07-30 18:00:38 +0200

    added can_reorder and can_transition_manually to idea status serializer

--- back/app/serializers/web_api/v1/idea_status_serializer.rb
+++ back/app/serializers/web_api/v1/idea_status_serializer.rb
@@ -2,4 +2,26 @@
 
 class WebApi::V1::IdeaStatusSerializer < WebApi::V1::BaseSerializer
   attributes :title_multiloc, :color, :ordering, :code, :description_multiloc, :ideas_count
+
+  attribute :can_reorder do |status| # TODO: Reuse logic
+    case status.participation_method
+    when 'ideation'
+      status.code != 'proposed'
+    when 'proposals'
+      %w[proposed threshold_reached expired].exlcude? status.code
+    else
+      false
+    end
+  end
+
+  attribute :can_transition_manually do |status| # TODO: Reuse logic
+    case status.participation_method
+    when 'ideation'
+      true
+    when 'proposals'
+      %w[proposed threshold_reached expired].exlcude? status.code
+    else
+      false
+    end
+  end
 end
