070089f95a7cb3c6d548636de77b5f3955c9aee4
Amanda Anderson <anderson_amanda@mail.com>
2024-07-30 17:08:32 +0200

    Add missing message for survey results page

--- front/app/containers/Admin/projects/project/nativeSurvey/messages.ts
+++ front/app/containers/Admin/projects/project/nativeSurvey/messages.ts
@@ -163,6 +163,10 @@ export default defineMessages({
     id: 'app.containers.AdminPage.ProjectEdit.survey.file_upload',
     defaultMessage: 'File upload',
   },
+  shapefile_upload: {
+    id: 'app.containers.AdminPage.ProjectEdit.survey.shapefile_upload',
+    defaultMessage: 'Esri shapefile upload',
+  },
   downloadExcelTemplate: {
     id: 'app.containers.AdminPage.ProjectEdit.survey.downloadExcelTemplate1',
     defaultMessage: 'Download an excel template',
