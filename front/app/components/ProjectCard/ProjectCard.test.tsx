fd2c12642e9fc11276fff7fe16c0acc6b89352b7
EdwinKato <katedwin88@gmail.com>
2024-07-30 06:58:44 +0300

    Add unit tests for small medium and large cards

--- front/app/components/ProjectCard/ProjectCard.test.tsx
+++ front/app/components/ProjectCard/ProjectCard.test.tsx
@@ -2,20 +2,54 @@ import React from 'react';
 
 import { render, screen } from 'utils/testUtils/rtl';
 
-import ProjectCard from '.';
+import LargeProjectCard from './LargeProjectCard';
+import MediumProjectCard from './MediumProjectCard';
+import SmallProjectCard from './SmallProjectCard';
 
 // mock
 jest.mock('api/projects/useProjectById');
 
-describe('ProjectCard', () => {
+describe('LargeProjectCard', () => {
   it('shows the title', () => {
-    render(<ProjectCard projectId="projectId" size="large" />);
+    render(<LargeProjectCard projectId="projectId" />);
 
     expect(screen.getByTestId('project-card-project-title')).toBeVisible();
   });
 
   it('shows the description', () => {
-    render(<ProjectCard projectId="projectId" size="large" />);
+    render(<LargeProjectCard projectId="projectId" />);
+
+    expect(
+      screen.getByTestId('project-card-project-description-preview')
+    ).toBeVisible();
+  });
+});
+
+describe('MediumProjectCard', () => {
+  it('shows the title', () => {
+    render(<MediumProjectCard projectId="projectId" />);
+
+    expect(screen.getByTestId('project-card-project-title')).toBeVisible();
+  });
+
+  it('shows the description', () => {
+    render(<MediumProjectCard projectId="projectId" />);
+
+    expect(
+      screen.getByTestId('project-card-project-description-preview')
+    ).toBeVisible();
+  });
+});
+
+describe('SmallProjectCard', () => {
+  it('shows the title', () => {
+    render(<SmallProjectCard projectId="projectId" />);
+
+    expect(screen.getByTestId('project-card-project-title')).toBeVisible();
+  });
+
+  it('shows the description', () => {
+    render(<SmallProjectCard projectId="projectId" />);
 
     expect(
       screen.getByTestId('project-card-project-description-preview')
