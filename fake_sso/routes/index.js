76bab12383f1f6dfdad8b6692a816f135db1fd25
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-07-30 13:32:27 +0100

    Add nodemon

--- fake_sso/routes/index.js
+++ fake_sso/routes/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-// /* GET home page. */
+// GET home page
 router.get("/", function (_req, res) {
   res.render("index", { title: "Fake SSO" });
 });
@@ -19,27 +19,5 @@ router.post("/oauth2/token", function (req, res) {
     test: "bla",
   });
 });
-
-// router.post('/submit_sso', async function (req, _res) {
-//   const referer = req.get('referer');
-//   const params = new URLSearchParams(referer);
-//   const redirect_uri = params.get('redirect_uri');
-//   const state = params.get('state');
-
-//   axios.post(
-//     redirect_uri,
-//     {
-//       'omniauth.auth': {
-//         provider: 'fake_sso',
-//         uid: 'abc123',
-//         info: {
-//           email: req.body.email,
-//           name: 'FAKE NAME'
-//         }
-//       },
-//       'omniauth.state': state
-//     }
-//   );
-// })
 
 module.exports = router;
