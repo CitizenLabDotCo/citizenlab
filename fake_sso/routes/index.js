cfc4d676714728bbe73767ab846a9f860b0fd60c
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-07-30 15:50:11 +0100

    Add id token and uid and stuff

--- fake_sso/routes/index.js
+++ fake_sso/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createJwt } = require("../utils/createJwt");
+const { createJwt } = require("../utils/jwt");
 
 // GET home page
 router.get("/", (_req, res) => {
@@ -14,21 +14,17 @@ router.get("/oauth2/authorize", (_req, res) => {
 
 // The token endpoint (receives code, returns token)
 router.post("/oauth2/token", async (_req, res) => {
-  const jwt = await createJwt();
-
   res.json({
     token_type: "Bearer",
-    access_token: jwt,
+    id_token: await createJwt(),
+    access_token: "access_token_abc123",
   });
 });
 
 // The resource endpoint (receives token, returns user info)
 router.get("/userinfo", (_req, res) => {
-  console.log(req.query);
-  console.log(req.body);
-
   res.json({
-    // TODO
+    some: "shit",
   });
 });
 
--- fake_sso/utils/createJwt.js
+++ fake_sso/utils/createJwt.js
@@ -1,4 +1,5 @@
 const jose = require("jose");
+const { v4: uuidv4 } = require("uuid");
 
 const secret = new TextEncoder().encode(
   "cc7e0d44fd473002f1c42167459001140ec6389b7353f8088f4d9a95f2f596f2"
@@ -7,7 +8,12 @@ const secret = new TextEncoder().encode(
 const alg = "HS256";
 
 const createJwt = () => {
-  return new jose.SignJWT({ "urn:example:claim": true })
+  const uid = uuidv4();
+
+  return new jose.SignJWT({
+    uid,
+    email: `${uid.slice(6)}@example.com`,
+  })
     .setProtectedHeader({ alg })
     .setIssuedAt()
     .setIssuer("fake_sso")
