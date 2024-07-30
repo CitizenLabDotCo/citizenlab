94fcfafa26fffa47e6291442ab0a29ecaeedc223
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-07-30 14:17:06 +0100

    Add scaffold resource endpoint

--- fake_sso/routes/index.js
+++ fake_sso/routes/index.js
@@ -7,18 +7,28 @@ router.get("/", (_req, res) => {
   res.render("index", { title: "Fake SSO" });
 });
 
-// The authorization route (same as homepage)
+// The authorization endpoint (same as homepage)
 router.get("/oauth2/authorize", (_req, res) => {
   res.render("index", { title: "Fake SSO" });
 });
 
-// The token route (receives code, returns token)
+// The token endpoint (receives code, returns token)
 router.post("/oauth2/token", async (_req, res) => {
   const jwt = await createJwt();
 
   res.json({
     token_type: "Bearer",
     access_token: jwt,
+  });
+});
+
+// The resource endpoint (receives token, returns user info)
+router.get("/userinfo", (_req, res) => {
+  console.log(req.query);
+  console.log(req.body);
+
+  res.json({
+    // TODO
   });
 });
 
