278f574ccce2894c5a25641cc83d63ac73da4a93
Amanda Anderson <anderson_amanda@mail.com>
2024-07-31 18:32:01 +0200

    Change text

--- front/app/containers/Admin/projects/project/analysis/InputPreview/MapPreview/messages.ts
+++ front/app/containers/Admin/projects/project/analysis/InputPreview/MapPreview/messages.ts
@@ -2,7 +2,7 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   noAnswerProvided: {
-    id: 'app.containers.AdminPage.projects.project.analysis.noAnswerProvided',
-    defaultMessage: 'No answer provided',
+    id: 'app.containers.AdminPage.projects.project.analysis.noAnswerProvided2',
+    defaultMessage: 'No answer provided.',
   },
 });
