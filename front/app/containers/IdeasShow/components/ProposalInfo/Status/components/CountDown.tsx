2d4cb74b1f46ee154246305a799157f794877af8
Iva <iva@citizenlab.co>
2024-07-31 15:35:00 +0300

    Add box

--- front/app/containers/IdeasShow/components/ProposalInfo/Status/components/CountDown.tsx
+++ front/app/containers/IdeasShow/components/ProposalInfo/Status/components/CountDown.tsx
@@ -1,6 +1,6 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { colors, fontSizes } from '@citizenlab/cl2-component-library';
+import { Box, colors, fontSizes } from '@citizenlab/cl2-component-library';
 import moment from 'moment';
 import styled from 'styled-components';
 
@@ -8,10 +8,6 @@ import { FormattedMessage } from 'utils/cl-intl';
 import { convertSecondsToDDHHMM } from 'utils/dateUtils';
 
 import messages from '../../messages';
-
-const Container = styled.div`
-  display: flex;
-`;
 
 const TimeComponent = styled.div`
   margin: 0 5px 0 0;
@@ -44,64 +40,49 @@ interface Props {
   className?: string;
 }
 
-interface State {
-  refresh: number;
-}
+const CountDown = ({ targetTime, className }: Props) => {
+  const [_refresh, setRefresh] = useState(Date.now());
 
-class CountDown extends PureComponent<Props, State> {
-  interval: NodeJS.Timeout;
-
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      refresh: Date.now(),
-    };
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(() => {
-      this.setState({ refresh: Date.now() });
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRefresh(Date.now());
     }, 60 * 1000);
-  }
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
+    return () => clearInterval(interval);
+  }, []);
 
-  render() {
-    const start = moment();
-    const end = moment(this.props.targetTime, 'YYYY-MM-DDThh:mm:ss.SSSZ');
-    const durationAsSeconds = moment.duration(end.diff(start)).asSeconds();
-    const expired = durationAsSeconds <= 0;
-    const formattedDuration =
-      convertSecondsToDDHHMM(durationAsSeconds).split(':');
-    const daysLeft = expired ? '0' : formattedDuration[0];
-    const hoursLeft = expired ? '0' : formattedDuration[1];
-    const minutesLeft = expired ? '0' : formattedDuration[2];
+  const start = moment();
+  const end = moment(targetTime, 'YYYY-MM-DDThh:mm:ss.SSSZ');
+  const durationAsSeconds = moment.duration(end.diff(start)).asSeconds();
+  const expired = durationAsSeconds <= 0;
+  const formattedDuration =
+    convertSecondsToDDHHMM(durationAsSeconds).split(':');
+  const daysLeft = expired ? '0' : formattedDuration[0];
+  const hoursLeft = expired ? '0' : formattedDuration[1];
+  const minutesLeft = expired ? '0' : formattedDuration[2];
 
-    return (
-      <Container className={this.props.className}>
-        <TimeComponent>
-          <Count>{daysLeft}</Count>
-          <Unit>
-            <FormattedMessage {...messages.days} />
-          </Unit>
-        </TimeComponent>
-        <TimeComponent>
-          <Count>{hoursLeft}</Count>
-          <Unit>
-            <FormattedMessage {...messages.hours} />
-          </Unit>
-        </TimeComponent>
-        <TimeComponent>
-          <Count>{minutesLeft}</Count>
-          <Unit>
-            <FormattedMessage {...messages.minutes} />
-          </Unit>
-        </TimeComponent>
-      </Container>
-    );
-  }
-}
+  return (
+    <Box display="flex" className={className}>
+      <TimeComponent>
+        <Count>{daysLeft}</Count>
+        <Unit>
+          <FormattedMessage {...messages.days} />
+        </Unit>
+      </TimeComponent>
+      <TimeComponent>
+        <Count>{hoursLeft}</Count>
+        <Unit>
+          <FormattedMessage {...messages.hours} />
+        </Unit>
+      </TimeComponent>
+      <TimeComponent>
+        <Count>{minutesLeft}</Count>
+        <Unit>
+          <FormattedMessage {...messages.minutes} />
+        </Unit>
+      </TimeComponent>
+    </Box>
+  );
+};
 
 export default CountDown;
