a88e214f6f51f8655a06336278ee8d1ba9e08e85
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-08-05 17:42:50 +0100

    _

--- front/app/containers/Admin/projects/project/permissions/components/PhasePermissions/ActionForm/Fields/FieldSelectionModal/messages.ts
+++ front/app/containers/Admin/projects/project/permissions/components/PhasePermissions/ActionForm/Fields/FieldSelectionModal/messages.ts
@@ -29,4 +29,42 @@ export default defineMessages({
     id: 'app.containers.AdminPage.groups.permissions.onlyAdminsCreateQuestion',
     defaultMessage: 'Only admins can create a new question.',
   },
+
+  // AddFieldScreen
+  atLeastOneOptionError: {
+    id: 'app.containers.AdminPage.groups.permissions.atLeastOneOptionError',
+    defaultMessage: 'At least one choice must be provided',
+  },
+  emptyTitleErrorMessage: {
+    id: 'app.containers.AdminPage.groups.permissions.emptyTitleErrorMessage',
+    defaultMessage: 'Please provide a title for all choices',
+  },
+  selectValueError: {
+    id: 'app.containers.AdminPage.groups.permissions.selectValueError',
+    defaultMessage: 'Please select an answer type',
+  },
+  missingTitleLocaleError: {
+    id: 'app.containers.AdminPage.groups.permissions.missingTitleLocaleError',
+    defaultMessage: 'Please fill in the title in all languages',
+  },
+  answerFormat: {
+    id: 'app.containers.AdminPage.groups.permissions.answerFormat',
+    defaultMessage: 'Answer format',
+  },
+  questionTitle: {
+    id: 'app.containers.AdminPage.groups.permissions.questionTitle',
+    defaultMessage: 'Question title',
+  },
+  questionDescription: {
+    id: 'app.containers.AdminPage.groups.permissions.questionDescription',
+    defaultMessage: 'Question description',
+  },
+  answerChoices: {
+    id: 'app.containers.AdminPage.groups.permissions.answerChoices',
+    defaultMessage: 'Answer choices',
+  },
+  addAnswer: {
+    id: 'app.containers.AdminPage.groups.permissions.addAnswer',
+    defaultMessage: 'Add answer',
+  },
 });
