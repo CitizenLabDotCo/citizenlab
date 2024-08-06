75e75507cf23c0539bbc65209839ca543bf20a84
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-08-06 11:20:54 +0100

    Remove HandlePermissionChangeProps

--- front/app/containers/Admin/projects/project/permissions/components/PhasePermissions/typings.ts
+++
@@ -1,9 +0,0 @@
-import { IPermissionData } from 'api/permissions/types';
-
-export type HandlePermissionChangeProps = {
-  permission: IPermissionData;
-  permittedBy?: IPermissionData['attributes']['permitted_by'];
-  globalCustomFields?: IPermissionData['attributes']['global_custom_fields'];
-  groupIds?: string[];
-  phaseId?: string | null;
-};
