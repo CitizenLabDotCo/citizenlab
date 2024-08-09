975cd5dec5a5d60b11b8fc50793913941b856cf1
EdwinKato <katedwin88@gmail.com>
2024-08-09 19:15:55 +0300

    Add messages file

---
+++ front/app/components/UI/FullscreenImage/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  a11yViewImage: {
+    id: 'app.components.UI.FullscreenImage.a11yViewImage',
+    defaultMessage: 'View image',
+  },
+});
