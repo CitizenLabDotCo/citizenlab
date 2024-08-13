c773d0987f807183e813a614caa7be3fd799e49c
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-08-13 16:20:41 +0100

    Make reset button a link and only show if not defaults

--- front/app/components/admin/ActionForm/index.tsx
+++ front/app/components/admin/ActionForm/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
-import { Box, Button, Title, colors } from '@citizenlab/cl2-component-library';
+import {
+  Box,
+  Button,
+  Title,
+  fontSizes,
+} from '@citizenlab/cl2-component-library';
 
 import { IPermissionData } from 'api/permissions/types';
+import usePermissionsCustomFields from 'api/permissions_custom_fields/usePermissionsCustomFields';
 import { PermittedBy } from 'api/phase_permissions/types';
 import usePhase from 'api/phases/usePhase';
 
@@ -14,6 +20,7 @@ import Fields from './Fields';
 import FlowVisualization from './FlowVisualization';
 import GroupSelect from './GroupSelect';
 import messages from './messages';
+import { showGroupSelect, showResetButton } from './utils';
 
 interface Props {
   phaseId?: string;
@@ -23,10 +30,6 @@ interface Props {
   onReset: () => void;
 }
 
-const showGroupSelect = (permittedBy: PermittedBy) => {
-  return permittedBy !== 'everyone' && permittedBy !== 'admins_moderators';
-};
-
 const ActionForm = ({
   phaseId,
   permissionData,
@@ -34,21 +37,28 @@ const ActionForm = ({
   onChange,
   onReset,
 }: Props) => {
-  const { data: phase } = usePhase(phaseId);
-
-  const handlePermittedByUpdate = (permittedBy: PermittedBy) => {
-    onChange(permittedBy, groupIds);
-  };
-
   const {
     id: permissionId,
     attributes: { permitted_by, action, verification_enabled },
   } = permissionData;
 
+  const { data: phase } = usePhase(phaseId);
+
+  const { data: permissionsCustomFields } = usePermissionsCustomFields({
+    phaseId,
+    action,
+  });
+
+  const handlePermittedByUpdate = (permittedBy: PermittedBy) => {
+    onChange(permittedBy, groupIds);
+  };
+
   const participation_method = phase?.data.attributes.participation_method;
 
   const isSurveyAction =
     participation_method === 'native_survey' && action === 'posting_idea';
+
+  if (!permissionsCustomFields) return null;
 
   return (
     <form>
@@ -88,17 +98,26 @@ const ActionForm = ({
         <Box mt="20px">
           <FlowVisualization
             permittedBy={permitted_by}
-            phaseId={phaseId}
-            action={action}
             verificationEnabled={verification_enabled}
+            permissionsCustomFields={permissionsCustomFields.data}
           />
         </Box>
       )}
-      <Box mt="20px" w="100%" display="flex">
-        <Button width="auto" bgColor={colors.primary} onClick={onReset}>
-          <FormattedMessage {...messages.resetExtraQuestionsAndGroups} />
-        </Button>
-      </Box>
+      {showResetButton(permitted_by, permissionsCustomFields.data) && (
+        <Box mt="28px" w="100%" display="flex">
+          <Button
+            width="auto"
+            buttonStyle="text"
+            onClick={onReset}
+            padding="0px"
+            fontSize={`${fontSizes.m}px`}
+          >
+            <span style={{ textDecorationLine: 'underline' }}>
+              <FormattedMessage {...messages.resetExtraQuestionsAndGroups} />
+            </span>
+          </Button>
+        </Box>
+      )}
       <Box mt="20px">
         <Fields phaseId={phaseId} action={action} />
       </Box>
