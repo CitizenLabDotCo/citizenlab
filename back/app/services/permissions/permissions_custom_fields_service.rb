8e239f5041bc7016fc46116e342dddc5df6c665c
jamesspeake <james@speake.org>
2024-08-13 17:12:51 +0100

    [TAN-2382] Restrict permitted_by:everyone to never return fields

--- back/app/services/permissions/permissions_custom_fields_service.rb
+++ back/app/services/permissions/permissions_custom_fields_service.rb
@@ -3,6 +3,10 @@
 module Permissions
   class PermissionsCustomFieldsService
     def fields_for_permission(permission, return_hidden: false)
+      # Currently we don't support custom fields for 'everyone' though the rest of the code is ready for it
+      # So we have added this block until we do support it
+      return [] if permission.permitted_by == 'everyone'
+
       fields = if permission.global_custom_fields
         default_fields(permission)
       else
