876cf085b1b7f80ba17bbbd6bf74e0fc248e7964
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-08-14 20:56:22 +0100

    _

--- back/engines/commercial/multi_tenancy/db/seeds/tenants.rb
+++ back/engines/commercial/multi_tenancy/db/seeds/tenants.rb
@@ -460,8 +460,7 @@ module MultiTenancy
             fake_sso: {
               enabled: true,
               allowed: true,
-              # issuer: 'https://fake-sso.onrender.com'
-              issuer: ''
+              issuer: 'https://fake-sso.onrender.com'
             }
           })
         )
