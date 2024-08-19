dd1ccdd72f77a6e45d8b90120f0216e85828ed76
Sébastien Hoorens <sebastien@citizenlab.co>
2024-08-19 13:58:23 +0200

    alphabetical base method

--- back/lib/participation_method/base.rb
+++ back/lib/participation_method/base.rb
@@ -10,33 +10,7 @@ module ParticipationMethod
       @phase = phase
     end
 
-    def transitive?
-      false
-    end
-
-    def assign_defaults_for_phase
-      # Default is to do nothing.
-    end
-
-    # Remove after unified status implementation
-    def idea_status_method
-      self.class.method_str
-    end
-
-    def generate_slug(input)
-      # Input is not created in this participation method,
-      # so the default is to return nothing.
-    end
-
-    def assign_defaults(input)
-      # Default is to do nothing.
-    end
-
-    def create_default_form!
-      # Default is to do nothing.
-    end
-
-    def default_fields(_custom_form)
+    def additional_export_columns
       []
     end
 
@@ -44,12 +18,16 @@ module ParticipationMethod
       []
     end
 
-    def constraints
-      {}
+    def allowed_ideas_orders
+      []
     end
 
-    def supports_pages_in_form?
-      false
+    def assign_defaults(input)
+      # Default is to do nothing.
+    end
+
+    def assign_defaults_for_phase
+      # Default is to do nothing.
     end
 
     def author_in_form?(_user)
@@ -60,16 +38,12 @@ module ParticipationMethod
       false
     end
 
-    def proposed_budget_in_form?
-      false
+    def constraints
+      {}
     end
 
-    def allowed_ideas_orders
-      []
-    end
-
-    def supports_edits_after_publication?
-      true
+    def create_default_form!
+      # Default is to do nothing.
     end
 
     def custom_form
@@ -77,72 +51,18 @@ module ParticipationMethod
       context.custom_form || CustomForm.new(participation_context: context)
     end
 
-    def supports_inputs_without_author?
-      true
+    def default_fields(_custom_form)
+      []
     end
 
-    def supports_public_visibility?
-      false
+    def generate_slug(input)
+      # Input is not created in this participation method,
+      # so the default is to return nothing.
     end
 
-    def supports_exports?
-      false
-    end
-
-    def supports_commenting?
-      false
-    end
-
-    def supports_reacting?
-      false
-    end
-
-    def supports_automated_statuses?
-      false
-    end
-
-    def supports_status?
-      false
-    end
-
-    def supports_assignment?
-      false
-    end
-
-    def supports_toxicity_detection?
-      true
-    end
-
-    def supports_posting_inputs?
-      false
-    end
-
-    def supports_built_in_fields?
-      false
-    end
-
-    def supports_input_term?
-      false
-    end
-
-    def supports_survey_form?
-      false
-    end
-
-    def supports_permitted_by_everyone?
-      false
-    end
-
-    def supports_answer_visible_to?
-      false
-    end
-
-    def supports_serializing?(_attribute)
-      false
-    end
-
-    def supports_serializing_input?(_attribute)
-      false
+    # Remove after unified status implementation
+    def idea_status_method
+      self.class.method_str
     end
 
     # Should an admin be able to set permissions for disabled actions?
@@ -150,12 +70,88 @@ module ParticipationMethod
       false
     end
 
-    def additional_export_columns
-      []
+    def supports_answer_visible_to?
+      false
     end
-
+    
+    def supports_assignment?
+      false
+    end
+    
+    def supports_automated_statuses?
+      false
+    end
+    
+    def supports_built_in_fields?
+      false
+    end
+    
+    def supports_commenting?
+      false
+    end
+    
+    def supports_edits_after_publication?
+      true
+    end
+    
+    def supports_exports?
+      false
+    end
+    
+    def supports_input_term?
+      false
+    end
+    
+    def supports_inputs_without_author?
+      true
+    end
+    
     def supports_multiple_posts?
       true
+    end
+    
+    def supports_pages_in_form?
+      false
+    end
+    
+    def supports_permitted_by_everyone?
+      false
+    end
+    
+    def supports_posting_inputs?
+      false
+    end
+    
+    def supports_public_visibility?
+      false
+    end
+    
+    def supports_reacting?
+      false
+    end
+    
+    def supports_serializing?(_attribute)
+      false
+    end
+    
+    def supports_serializing_input?(_attribute)
+      false
+    end
+    
+    def supports_status?
+      false
+    end
+    
+    def supports_survey_form?
+      false
+    end
+    
+    def supports_toxicity_detection?
+      true
+    end
+
+    def transitive?
+      false
     end
 
     def use_reactions_as_votes?
@@ -165,5 +161,9 @@ module ParticipationMethod
     private
 
     attr_reader :phase
+
+    def proposed_budget_in_form?
+      false
+    end
   end
 end
