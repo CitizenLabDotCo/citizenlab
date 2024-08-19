03c61e42f5f73d9d354955c72c29a4fb483cd78f
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-08-19 09:21:32 +0100

    Fix project_page_native_survey test

--- front/cypress/e2e/project_page_native_survey.cy.ts
+++ front/cypress/e2e/project_page_native_survey.cy.ts
@@ -218,8 +218,8 @@ describe('Native survey CTA bar', () => {
     cy.visit(`admin/projects/${projectId}/settings/access-rights`);
     cy.get('.e2e-granular-permissions-phase-accordion').first().click();
     cy.get('#e2e-granular-permissions').within(() => {
-      cy.get('#e2e-permission-registered-users').should('exist');
-      cy.get('#e2e-permission-registered-users').click();
+      cy.get('.e2e-permission-registered-users').should('exist');
+      cy.get('.e2e-permission-registered-users').first().click();
     });
 
     cy.logout();
