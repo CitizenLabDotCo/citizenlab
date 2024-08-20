579b4e687a75616609f9e9b44936cfe949a9e3f2
Iva <iva@citizenlab.co>
2024-08-20 15:49:47 +0300

    Reorder endpoint

---
+++ front/app/api/idea_statuses/useReorderIdeaStatus.test.ts
@@ -0,0 +1,56 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { http, HttpResponse } from 'msw';
+import { setupServer } from 'msw/node';
+
+import createQueryClientWrapper from 'utils/testUtils/queryClientWrapper';
+
+import { ideaStatusesData } from './__mocks__/_mockServer';
+import useReorderIdeaStatus from './useReorderIdeaStatus';
+
+const apiPath = '*idea_statuses/:id/reorder';
+const server = setupServer(
+  http.patch(apiPath, () => {
+    return HttpResponse.json({ data: ideaStatusesData[0] }, { status: 200 });
+  })
+);
+
+describe('useReorderIdeaStatus', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
+  it('mutates data correctly', async () => {
+    const { result, waitFor } = renderHook(() => useReorderIdeaStatus(), {
+      wrapper: createQueryClientWrapper(),
+    });
+
+    act(() => {
+      result.current.mutate({
+        id: 'id',
+        ordering: 1,
+      });
+    });
+
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+    expect(result.current.data?.data).toEqual(ideaStatusesData[0]);
+  });
+
+  it('returns error correctly', async () => {
+    server.use(
+      http.patch(apiPath, () => {
+        return HttpResponse.json(null, { status: 500 });
+      })
+    );
+
+    const { result, waitFor } = renderHook(() => useReorderIdeaStatus(), {
+      wrapper: createQueryClientWrapper(),
+    });
+    act(() => {
+      result.current.mutate({
+        id: 'id',
+        ordering: 1,
+      });
+    });
+    await waitFor(() => expect(result.current.isError).toBe(true));
+    expect(result.current.error).toBeDefined();
+  });
+});
