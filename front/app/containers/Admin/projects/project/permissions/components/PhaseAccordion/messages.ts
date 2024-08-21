4648b3490fe6c4d8f45eddc4c4ada392ed60564e
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-08-21 10:24:38 +0100

    Remove phase accordion

--- front/app/containers/Admin/projects/project/permissions/components/PhaseAccordion/messages.ts
+++
@@ -1,8 +0,0 @@
-import { defineMessages } from 'react-intl';
-
-export default defineMessages({
-  phase: {
-    id: 'app.containers.AdminPage.groups.permissions.phase',
-    defaultMessage: 'Phase ',
-  },
-});
