712dda2f94b114152fe3c226cf075946ea5995cf
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-08-21 11:33:03 +0100

    Add some random extra checks to light_authentication_flow

--- front/cypress/e2e/auth/light_authentication_flow.cy.ts
+++ front/cypress/e2e/auth/light_authentication_flow.cy.ts
@@ -40,6 +40,9 @@ describe('Light authentication flow', () => {
         cy.visit(
           `/admin/projects/${projectId}/phases/${phaseId}/access-rights`
         );
+        cy.acceptCookies();
+        cy.get('.e2e-action-accordion-posting_idea');
+        cy.wait(1000);
 
         cy.get('.e2e-action-accordion-posting_idea').click();
         cy.get('.e2e-action-form-posting_idea').within(() => {
