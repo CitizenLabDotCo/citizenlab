7fa6ccd73b4e6531b71560c1bf0a7ab1938c3b7b
Simon Tharby <simontharby@gmail.com>
2024-08-27 17:26:42 +0100

    [TAN-2528] Remove hyperlink style from profile_page values

--- back/app/services/xlsx_service.rb
+++ back/app/services/xlsx_service.rb
@@ -123,7 +123,7 @@ class XlsxService
       { header: 'email', f: ->(u) { u.email } },
       { header: 'first_name', f: ->(u) { u.first_name } },
       { header: 'last_name', f: ->(u) { u.last_name } },
-      { header: 'profile_page', f: ->(u) { url_service.model_to_url(u) }, skip_sanitization: true, hyperlink: true },
+      { header: 'profile_page', f: ->(u) { url_service.model_to_url(u) }, skip_sanitization: true }, # Not hyperlinked, as approx 7x faster without this style
       { header: 'created_at', f: ->(u) { u.created_at }, skip_sanitization: true },
       { header: 'registration_completed_at', f: ->(u) { u.registration_completed_at }, skip_sanitization: true },
       { header: 'invite_status', f: ->(u) { u.invite_status }, skip_sanitization: true },
