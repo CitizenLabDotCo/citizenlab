b711a6cb0e340a316d240e6fd87668cfbeae2de5
Sébastien Hoorens <sebastien@citizenlab.co>
2024-09-02 13:34:19 +0200

    proposals transition decisions: inappropriate content flags

--- back/engines/commercial/flag_inappropriate_content/spec/acceptance/initiatives_spec.rb
+++ back/engines/commercial/flag_inappropriate_content/spec/acceptance/initiatives_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
-# TODO: move-old-proposals-test
+# TODO: cleanup-after-proposals-migration
 resource 'Initiatives' do
   before do
     header 'Content-Type', 'application/json'
