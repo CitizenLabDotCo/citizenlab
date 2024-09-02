912b253b67ad30c323c037bd75bb457888c2af6c
Sébastien Hoorens <sebastien@citizenlab.co>
2024-09-02 16:51:44 +0200

    proposals transition decision: public api

--- back/spec/factories/reactions.rb
+++ back/spec/factories/reactions.rb
@@ -14,31 +14,4 @@ FactoryBot.define do
       association :reactable, factory: :comment
     end
   end
-
-  factory :reaction2, class: 'Reaction' do
-    for_idea
-    up
-
-    user
-
-    trait :for_idea do
-      association :reactable, factory: :idea
-    end
-
-    trait :for_initiative do
-      association :reactable, factory: :initiative
-    end
-
-    trait :for_comment do
-      association :reactable, factory: :comment
-    end
-
-    trait :up do
-      mode { 'up' }
-    end
-
-    trait :down do
-      mode { 'down' }
-    end
-  end
 end
