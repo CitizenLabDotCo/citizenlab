a056f8d23c74cadd7ca10016cdfabb2231962728
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-09-02 14:13:17 +0200

    _

--- back/engines/commercial/verification/spec/acceptance/verification_methods_spec.rb
+++ back/engines/commercial/verification/spec/acceptance/verification_methods_spec.rb
@@ -51,7 +51,14 @@ resource 'Verification methods' do
           type: 'verification_method',
           attributes: {
             name: 'cow',
-            action_metadata: { allowed: false }
+            method_metadata: {
+              allowed_for_verified_actions: false,
+              locked_attributes: [],
+              locked_custom_fields: [],
+              name: 'cow',
+              other_attributes: [],
+              other_custom_fields: []
+            }
           }
         }
       )
@@ -61,8 +68,8 @@ resource 'Verification methods' do
           type: 'verification_method',
           attributes: {
             name: 'fake_sso',
-            action_metadata: {
-              allowed: true,
+            method_metadata: {
+              allowed_for_verified_actions: true,
               name: 'Fake SSO',
               locked_attributes: [
                 { :en => 'First name(s)', :'fr-FR' => 'Prénom(s)', :'nl-NL' => 'Voornamen' },
@@ -84,13 +91,20 @@ resource 'Verification methods' do
           id: '516e134d-e22b-4386-a783-0db4c2708256',
           type: 'verification_method',
           attributes: {
-            name: 'id_card_lookup',
-            ui_method_name: 'By social security number',
-            card_id: 'Social security number',
-            card_id_tooltip: 'You can find this number on you card. We just check, we don\'t store it',
-            card_id_placeholder: 'xx-xxxxx-xx',
-            explainer_image_url: 'https://some.fake/image.png',
-            action_metadata: { allowed: false }
+            :card_id => 'Social security number',
+            :card_id_placeholder => 'xx-xxxxx-xx',
+            :card_id_tooltip => "You can find this number on you card. We just check, we don't store it",
+            :explainer_image_url => 'https://some.fake/image.png',
+            :method_metadata => {
+              :allowed_for_verified_actions => false,
+              :locked_attributes => [],
+              :locked_custom_fields => [],
+              :name => 'id_card_lookup',
+              :other_attributes => [],
+              :other_custom_fields => []
+            },
+            :name => 'id_card_lookup',
+            :ui_method_name => 'By social security number'
           }
         }
       )
@@ -106,8 +120,8 @@ resource 'Verification methods' do
           type: 'verification_method',
           attributes: {
             name: 'fake_sso',
-            action_metadata: {
-              allowed: true,
+            method_metadata: {
+              allowed_for_verified_actions: true,
               name: 'Fake SSO',
               locked_attributes: [
                 { :en => 'First name(s)', :'fr-FR' => 'Prénom(s)', :'nl-NL' => 'Voornamen' },
@@ -119,6 +133,29 @@ resource 'Verification methods' do
               locked_custom_fields: [
                 { :en => 'gender' }, { :en => 'birthyear' }
               ],
+              other_custom_fields: []
+            }
+          }
+        }
+      )
+    end
+  end
+
+  get 'web_api/v1/verification_methods/first_enabled' do
+    example_request 'Returns the first verification method enabled' do
+      expect(status).to eq(200)
+      expect(response_data).to eq(
+        {
+          id: '7ccd453d-0eaf-412a-94a2-ae703b1b3e3f',
+          type: 'verification_method',
+          attributes: {
+            name: 'cow',
+            method_metadata: {
+              allowed_for_verified_actions: false,
+              locked_attributes: [],
+              locked_custom_fields: [],
+              name: 'cow',
+              other_attributes: [],
               other_custom_fields: []
             }
           }
