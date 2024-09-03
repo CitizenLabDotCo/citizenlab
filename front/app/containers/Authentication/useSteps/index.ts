41fd51b9ebc471fb7fab3533c4ffe90fdbc49e58
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-09-03 20:01:45 +0200

    Undo changes to useSteps

--- front/app/containers/Authentication/useSteps/index.ts
+++ front/app/containers/Authentication/useSteps/index.ts
@@ -51,8 +51,7 @@ export default function useSteps() {
     return authenticationDataRef.current;
   }, []);
 
-  // const [currentStep, _setCurrentStep] = useState<Step>('closed');
-  const [currentStep, _setCurrentStep] = useState<Step>('access-denied');
+  const [currentStep, _setCurrentStep] = useState<Step>('closed');
 
   const setCurrentStep = useCallback((step: Step) => {
     if (step === 'closed') {
