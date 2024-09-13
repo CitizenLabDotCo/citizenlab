b5baaea7c0db4b39891aacd85bcb97a866ef5c07
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-09-13 15:16:11 +0200

    Use library css + css variables instead

--- front/app/components/admin/TimelineCalendar/index.tsx
+++ front/app/components/admin/TimelineCalendar/index.tsx
@@ -1,58 +1,15 @@
 import React from 'react';
 
 import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/style.css';
 
 import styled from 'styled-components';
 import { colors } from '@citizenlab/cl2-component-library';
 
 const DayPickerStyles = styled.div`
-  td.rdp-day {
-    margin: 0;
-    padding: 0;
-    text-align: center;
-  }
-
-  button.rdp-day_button {
-    text-align: center;
-    font-size: 14px;
-    width: 36px;
-    height: 36px;
-  }
-
-  td.rdp-range_start {
-    background-color: ${colors.teal100};
-    border-radius: 30px 0 0 30px;
-  }
-
-  td.rdp-range_start > button {
-    background-color: ${colors.teal700};
-    color: white;
-    border-radius: 30px;
-  }
-
-  td.rdp-range_middle > button {
-    background-color: ${colors.teal100};
-  }
-
-  td.rdp-range_end {
-    background-color: ${colors.teal100};
-    border-radius: 0 30px 30px 0;
-  }
-
-  td.rdp-range_end > button {
-    background-color: ${colors.teal700};
-    color: white;
-    border-radius: 30px;
-  }
-
-  svg.rdp-chevron {
-    width: 20px;
-    height: 20px;
-    cursor: pointer;
-  }
-
-  svg.rdp-chevron:hover {
-    fill: ${colors.teal700};
+  .rdp-root {
+    --rdp-accent-color: ${colors.teal700};
+    --rdp-background-color: ${colors.teal100};
   }
 `;
 
@@ -61,6 +18,7 @@ const TimelineCalendar = () => {
     <DayPickerStyles>
       <DayPicker
         mode="range"
+        numberOfMonths={2}
         selected={{
           from: new Date(2024, 8, 1),
           to: new Date(2024, 8, 5),
