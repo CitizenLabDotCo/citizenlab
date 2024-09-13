1d2ba516803bf6f1dfad4547eee0debdbf4b288e
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-09-13 15:48:17 +0200

    Setup locale stuff

--- front/app/components/admin/TimelineCalendar/index.tsx
+++ front/app/components/admin/TimelineCalendar/index.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 
-import { DayPicker } from 'react-day-picker';
-import 'react-day-picker/style.css';
-
-import styled from 'styled-components';
 import { colors } from '@citizenlab/cl2-component-library';
+import 'react-day-picker/style.css';
+import { DayPicker } from 'react-day-picker';
+import styled from 'styled-components';
+
+import useLocale from 'hooks/useLocale';
+
+import { getLocale } from './locales';
 
 const DayPickerStyles = styled.div`
   .rdp-root {
     --rdp-accent-color: ${colors.teal700};
-    --rdp-background-color: ${colors.teal100};
   }
 `;
 
 const TimelineCalendar = () => {
+  const locale = useLocale();
+
   return (
     <DayPickerStyles>
       <DayPicker
         mode="range"
         numberOfMonths={2}
+        captionLayout="dropdown-months"
+        locale={getLocale(locale)}
         selected={{
           from: new Date(2024, 8, 1),
           to: new Date(2024, 8, 5),
--- front/app/i18n/da-DK.ts
+++ front/app/i18n/da-DK.ts
@@ -1,9 +1,12 @@
 import da from 'date-fns/locale/da';
 import { registerLocale } from 'react-datepicker';
 
+import { addLocale } from 'components/admin/TimelineCalendar/locales';
+
 import { formatTranslationMessages } from './';
 
 registerLocale('da-DK', da);
+addLocale('da-DK', da);
 const daDKAdminTranslationMessages = require('translations/admin/da-DK.json');
 const daDKTranslationMessages = require('translations/da-DK.json');
 const translationMessages = formatTranslationMessages('da-DK', {
