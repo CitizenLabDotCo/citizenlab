c802ec444fe1598fb9901d60201986d1b4f60cee
Amanda Anderson <anderson_amanda@mail.com>
2024-09-17 16:05:47 +0200

    Fix typo

--- front/app/components/FormBuilder/components/FormBuilderTopBar/messages.ts
+++ front/app/components/FormBuilder/components/FormBuilderTopBar/messages.ts
@@ -10,8 +10,8 @@ export default defineMessages({
     defaultMessage: 'Autosave',
   },
   autosaveTooltip: {
-    id: 'app.components.FormBuilder.components.FormBuilderTopBar.autosaveTooltip3',
+    id: 'app.components.FormBuilder.components.FormBuilderTopBar.autosaveTooltip4',
     defaultMessage:
-      'Auto-saving is enabled by default when you open the form editor. Any time you close the field setings panel using the "X" button, it will automatically trigger a save.',
+      'Auto-saving is enabled by default when you open the form editor. Any time you close the field settings panel using the "X" button, it will automatically trigger a save.',
   },
 });
