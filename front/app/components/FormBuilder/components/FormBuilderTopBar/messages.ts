16494597485043d25e2f1799202e61f4c425069a
Amanda Anderson <anderson_amanda@mail.com>
2024-09-17 16:01:42 +0200

    Fix tooltip copy

--- front/app/components/FormBuilder/components/FormBuilderTopBar/messages.ts
+++ front/app/components/FormBuilder/components/FormBuilderTopBar/messages.ts
@@ -10,8 +10,8 @@ export default defineMessages({
     defaultMessage: 'Autosave',
   },
   autosaveTooltip: {
-    id: 'app.components.FormBuilder.components.FormBuilderTopBar.autosaveTooltip2',
+    id: 'app.components.FormBuilder.components.FormBuilderTopBar.autosaveTooltip3',
     defaultMessage:
-      'Auto-saving is enabled by default when you open the survey editor. Any time you close the question setings panel using the "X" button, the survey will automatically trigger a save.',
+      'Auto-saving is enabled by default when you open the form editor. Any time you close the field setings panel using the "X" button, it will automatically trigger a save.',
   },
 });
