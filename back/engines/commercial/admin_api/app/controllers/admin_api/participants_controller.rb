db8bb0b7bd761c13c320cc43787be6b85ab04cae
EdwinKato <katedwin88@gmail.com>
2024-09-19 08:11:43 +0300

    Add condition to return uncached participant data

--- back/engines/commercial/admin_api/app/controllers/admin_api/participants_controller.rb
+++ back/engines/commercial/admin_api/app/controllers/admin_api/participants_controller.rb
@@ -5,7 +5,15 @@ module AdminApi
     before_action :set_project
 
     def participants_count
-      count = ParticipantsService.new.project_participants_count(@project)
+      use_cache = params[:use_cache].to_s != 'false' # Default to using cache if the param is not provided
+
+      # Depending on the `use_cache` param, use the cached or uncached method
+      count = if use_cache
+                ParticipantsService.new.project_participants_count(@project)
+              else
+                ParticipantsService.new.project_participants_count_uncached(@project)
+              end
+
       render json: { count: }
     end
 
