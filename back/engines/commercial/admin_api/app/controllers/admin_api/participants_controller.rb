dff42c34f26dcb015359c45a58ea81b732170306
EdwinKato <katedwin88@gmail.com>
2024-09-19 09:15:37 +0300

    Revert participant controller changes

--- back/engines/commercial/admin_api/app/controllers/admin_api/participants_controller.rb
+++ back/engines/commercial/admin_api/app/controllers/admin_api/participants_controller.rb
@@ -5,15 +5,7 @@ module AdminApi
     before_action :set_project
 
     def participants_count
-      use_cache = params[:use_cache].to_s != 'false' # Default to using cache if the param is not provided
-
-      # Depending on the `use_cache` param, use the cached or uncached method
-      count = if use_cache
-                ParticipantsService.new.project_participants_count(@project)
-              else
-                ParticipantsService.new.project_participants_count_uncached(@project)
-              end
-
+      count = ParticipantsService.new.project_participants_count(@project)
       render json: { count: }
     end
 
