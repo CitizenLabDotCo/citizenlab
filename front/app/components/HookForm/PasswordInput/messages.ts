9712900eeedbef66d25ffdef910cd02c0eea8612
Amanda Anderson <anderson_amanda@mail.com>
2024-09-25 14:24:30 +0200

    Add aria label to password field

---
+++ front/app/components/HookForm/PasswordInput/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  passwordLabel: {
+    id: 'app.components.HookForm.PasswordInput.passwordLabel',
+    defaultMessage: 'Password',
+  },
+});
