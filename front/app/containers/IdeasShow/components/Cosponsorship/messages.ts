1c61d2d1f7f3ec71c647bdac396ccd090c812397
Iva <iva@citizenlab.co>
2024-09-26 11:44:41 +0300

    Make copy consistent

--- front/app/containers/IdeasShow/components/Cosponsorship/messages.ts
+++ front/app/containers/IdeasShow/components/Cosponsorship/messages.ts
@@ -2,27 +2,27 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   cosponsors: {
-    id: 'app.containers.IdeasShow.Cosponsorship.cosponsors',
-    defaultMessage: 'Cosponsors',
+    id: 'app.containers.IdeasShow.Cosponsorship.co-sponsors',
+    defaultMessage: 'Co-sponsors',
   },
   pending: {
     id: 'app.containers.IdeasShow.Cosponsorship.pending',
     defaultMessage: 'pending',
   },
   cosponsorInvitation: {
-    id: 'app.containers.IdeasShow.Cosponsorship.cosponsorInvitation',
-    defaultMessage: 'Cosponsorship invitation',
+    id: 'app.containers.IdeasShow.Cosponsorship.co-sponsorInvitation',
+    defaultMessage: 'Co-sponsorship invitation',
   },
   cosponsorInvitationDescription: {
-    id: 'app.containers.IdeasShow.Cosponsorship.cosponsorDescription',
-    defaultMessage: 'You have been invited become a cosponsor.',
+    id: 'app.containers.IdeasShow.Cosponsorship.co-sponsorDescription',
+    defaultMessage: 'You have been invited become a co-sponsor.',
   },
   cosponsorInvitationAccepted: {
     id: 'app.containers.IdeasShow.Cosponsorship.cosponsorInvitationAccepted',
     defaultMessage: 'Invitation accepted',
   },
   cosponsorAcceptInvitation: {
-    id: 'app.containers.IdeasShow.Cosponsorship.cosponsorAcceptInvitation',
+    id: 'app.containers.IdeasShow.Cosponsorship.co-sponsorAcceptInvitation',
     defaultMessage: 'Accept invitation',
   },
 });
