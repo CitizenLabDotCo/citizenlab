71cb2febef7e9cefaf8412dc892093c2a886eba2
Koen Gremmelprez <koen@citizenlab.co>
2024-09-27 15:27:01 +0200

    Update back/engines/commercial/public_api/app/serializers/public_api/v2/area_serializer.rb
    Co-authored-by: adessy <adrien@citizenlab.co>

--- back/engines/commercial/public_api/app/serializers/public_api/v2/area_serializer.rb
+++ back/engines/commercial/public_api/app/serializers/public_api/v2/area_serializer.rb
@@ -1,23 +1,6 @@
 # frozen_string_literal: true
 
 class PublicApi::V2::AreaSerializer < PublicApi::V2::BaseSerializer
-  attributes :id,
-    :title,
-    :description,
-    :created_at,
-    :updated_at
-
-  def title
-    multiloc_service.t(object.title_multiloc)
-  end
-
-  def description
-    multiloc_service.t(object.description_multiloc)
-  end
-
-  private
-
-  def multiloc_service
-    @multiloc_service ||= MultilocService.new
-  end
+  attributes :id, :created_at, :updated_at
+  multiloc_attributes :title_multiloc, :description_multiloc
 end
