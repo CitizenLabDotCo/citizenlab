38a2db87630057fd9d201fbe94f6d01a1f072705
Simon Tharby <simontharby@gmail.com>
2024-09-29 18:18:50 +0100

    [TAN-2646] Reduce n of underscores and replace removed test

--- back/engines/free/email_campaigns/spec/mailers/internal_comment_on_your_internal_comment_mailer_spec.rb
+++ back/engines/free/email_campaigns/spec/mailers/internal_comment_on_your_internal_comment_mailer_spec.rb
@@ -31,36 +31,6 @@ RSpec.describe EmailCampaigns::InternalCommentOnYourInternalCommentMailer do
 
     before_all { EmailCampaigns::UnsubscriptionToken.create!(user_id: recipient.id) }
 
-    it 'renders the subject' do
-      expect(mail.subject).to be_present
-    end
-
-    it 'renders the receiver email' do
-      expect(mail.to).to eq([recipient.email])
-    end
-
-    it 'renders the sender email' do
-      expect(mail.from).to all(end_with('@citizenlab.co'))
-    end
-
-    it 'assigns organisation name' do
-      expect(mail.body.encoded).to match(AppConfiguration.instance.settings('core', 'organization_name')['en'])
-    end
-
-    it 'includes the comment author name' do
-      expect(mail.body.encoded).to include('Matthias')
-    end
-
-    it 'includes the comment body' do
-      expect(mail.body.encoded).to include('Wowzers!')
-    end
-
-    it 'includes the post title' do
-      expect(mail.body.encoded).to include('Permit paving')
-    end
-
-    it 'includes the post body text' do
-      expect(mail.body.encoded).to include('paving your front garden')
-    end
+    include_examples 'internal_comment_campaign_mailer_examples'
   end
 end
