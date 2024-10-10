58ec390cb0ea7af3082d5f6e7fbdd30042909efd
EdwinKato <katedwin88@gmail.com>
2024-10-10 05:26:19 +0300

    Allow draging files

--- front/app/components/UI/FileUploader/index.tsx
+++ front/app/components/UI/FileUploader/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-import styled from 'styled-components';
+import { Box } from '@citizenlab/cl2-component-library';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import { CLErrors, UploadFile } from 'typings';
 
+import { List, SortableRow } from 'components/admin/ResourceList';
 import Error from 'components/UI/Error';
 
 import { ScreenReaderOnly } from 'utils/a11y';
@@ -12,15 +15,12 @@ import FileDisplay, { FileType } from './FileDisplay';
 import FileInput from './FileInput';
 import messages from './messages';
 
-const Container = styled.div`
-  width: 100%;
-`;
-
 export interface Props {
   id: string;
   className?: string;
   onFileAdd: (fileToAdd: UploadFile) => void;
   onFileRemove?: (fileToRemove: FileType) => void;
+  onFileReorder?: (updatedFiles: FileType[]) => void;
   files: FileType[] | null;
   apiErrors?: CLErrors | null;
 }
@@ -28,6 +28,7 @@ export interface Props {
 const FileUploader = ({
   onFileAdd,
   onFileRemove,
+  onFileReorder,
   files,
   apiErrors,
   id,
@@ -45,34 +46,61 @@ const FileUploader = ({
       event.stopPropagation();
       onFileRemove?.(fileToRemove);
     };
-  const fileNames = files ? files.map((file) => file.name).join(', ') : '';
+
+  const handleDragRow = (fromIndex: number, toIndex: number) => {
+    if (files) {
+      const updatedFiles = [...files];
+      const [movedFile] = updatedFiles.splice(fromIndex, 1);
+      updatedFiles.splice(toIndex, 0, movedFile);
+      onFileReorder?.(updatedFiles);
+    }
+  };
+
+  if (!files) return null;
+
+  const fileNames = files.map((file) => file.name).join(', ');
 
   return (
-    <Container
-      className={className}
-      key={id}
-      data-cy="e2e-file-uploader-container"
-    >
-      <FileInput onAdd={handleFileOnAdd} id={id} />
-      <Error fieldName="file" apiErrors={apiErrors?.file} />
+    <DndProvider backend={HTML5Backend}>
+      <Box
+        className={className}
+        key={id}
+        data-cy="e2e-file-uploader-container"
+        w="100%"
+      >
+        <FileInput onAdd={handleFileOnAdd} id={id} />
+        <Error fieldName="file" apiErrors={apiErrors?.file} />
 
-      {files &&
-        files.map((file) => (
-          <FileDisplay
-            key={file.id || file.name}
-            onDeleteClick={handleFileOnRemove(file)}
-            file={file}
+        <List key={files.length} className="files-list e2e-files-list">
+          {files.map((file: FileType, index: number) => (
+            <SortableRow
+              key={`item-${file.id || file.name}`}
+              id={file.id || file.name}
+              index={index}
+              moveRow={handleDragRow}
+              isLastItem={index === files.length - 1}
+            >
+              <Box w="100%">
+                <FileDisplay
+                  key={`item-${file.id || file.name}`}
+                  onDeleteClick={handleFileOnRemove(file)}
+                  file={file}
+                />
+              </Box>
+            </SortableRow>
+          ))}
+        </List>
+
+        <ScreenReaderOnly aria-live="polite">
+          <FormattedMessage
+            {...(files.length > 0
+              ? messages.a11y_filesToBeUploaded
+              : messages.a11y_noFiles)}
+            values={{ fileNames }}
           />
-        ))}
-      <ScreenReaderOnly aria-live="polite">
-        <FormattedMessage
-          {...(files && files.length > 0
-            ? messages.a11y_filesToBeUploaded
-            : messages.a11y_noFiles)}
-          values={{ fileNames }}
-        />
-      </ScreenReaderOnly>
-    </Container>
+        </ScreenReaderOnly>
+      </Box>
+    </DndProvider>
   );
 };
 
