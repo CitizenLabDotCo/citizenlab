b393e610dafd86eefbdc7e92c248045a5f4c35ed
Amanda Anderson <anderson_amanda@mail.com>
2024-10-11 12:04:03 +0200

    Fix lint issues

--- back/db/migrate/20241011816395_add_autoshare_attribute_to_phase_context.rb
+++ back/db/migrate/20241011816395_add_autoshare_attribute_to_phase_context.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class AddAutoshareAttributeToPhaseContext < ActiveRecord::Migration[7.0]
-    def change
-      %i[phases].each do |tablename|
-        add_column tablename, :autoshare_results_enabled, :boolean, default: true, null: true
-      end
+  def change
+    %i[phases].each do |tablename|
+      add_column tablename, :autoshare_results_enabled, :boolean, default: true, null: false
     end
+  end
 end
