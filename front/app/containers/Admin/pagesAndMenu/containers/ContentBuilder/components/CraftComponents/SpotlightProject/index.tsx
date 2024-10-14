52eee0ac229d449002f694d1477a4e4a35b4ad28
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-10-14 11:11:35 +0200

    Scaffold empty widget

---
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/CraftComponents/SpotlightProject/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Settings from './Settings';
+
+const SpotlightProject = () => {
+  return <></>;
+};
+
+SpotlightProject.craft = {
+  related: {
+    settings: Settings,
+  },
+};
+
+export default SpotlightProject;
