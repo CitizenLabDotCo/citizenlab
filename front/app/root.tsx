9caf9e0d8dff78dcb7a7b1f1756ef30f2efd5b0a
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-10-14 19:28:04 +0200

    Prefetch lots of stuff

--- front/app/root.tsx
+++ front/app/root.tsx
@@ -22,8 +22,15 @@ import {
   unstable_HistoryRouter as HistoryRouter,
 } from 'react-router-dom';
 
+import { GLOBAL_CONTEXT } from 'api/authentication/authentication_requirements/constants';
+import { fetchAuthenticationRequirements } from 'api/authentication/authentication_requirements/getAuthenticationRequirements';
+import requirementKeys from 'api/authentication/authentication_requirements/keys';
+import customPagesKeys from 'api/custom_pages/keys';
+import { fetchCustomPages } from 'api/custom_pages/useCustomPages';
 import homepageBuilderKeys from 'api/home_page_layout/keys';
 import { fetchHomepageBuilderLayout } from 'api/home_page_layout/useHomepageLayout';
+import navbarKeys from 'api/navbar/keys';
+import { fetchNavbarItems } from 'api/navbar/useNavbarItems';
 
 import App from 'containers/App';
 import LanguageProvider from 'containers/LanguageProvider';
@@ -67,6 +74,21 @@ const prefetchData = () => {
   queryClient.prefetchQuery({
     queryKey: homepageBuilderKeys.items(),
     queryFn: fetchHomepageBuilderLayout,
+  });
+
+  queryClient.prefetchQuery({
+    queryKey: navbarKeys.list({}),
+    queryFn: () => fetchNavbarItems({}),
+  });
+
+  queryClient.prefetchQuery({
+    queryKey: customPagesKeys.lists(),
+    queryFn: fetchCustomPages,
+  });
+
+  queryClient.prefetchQuery({
+    queryKey: requirementKeys.item(GLOBAL_CONTEXT),
+    queryFn: () => fetchAuthenticationRequirements(GLOBAL_CONTEXT),
   });
 };
 
