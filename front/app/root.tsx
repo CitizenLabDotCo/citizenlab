f1f117457ec043c8d66ef66612dfa00306e6181a
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-10-15 17:58:53 +0200

    Remove custom pages prefetching

--- front/app/root.tsx
+++ front/app/root.tsx
@@ -25,8 +25,6 @@ import {
 import { GLOBAL_CONTEXT } from 'api/authentication/authentication_requirements/constants';
 import { fetchAuthenticationRequirements } from 'api/authentication/authentication_requirements/getAuthenticationRequirements';
 import requirementKeys from 'api/authentication/authentication_requirements/keys';
-import customPagesKeys from 'api/custom_pages/keys';
-import { fetchCustomPages } from 'api/custom_pages/useCustomPages';
 import homepageBuilderKeys from 'api/home_page_layout/keys';
 import { fetchHomepageBuilderLayout } from 'api/home_page_layout/useHomepageLayout';
 import navbarKeys from 'api/navbar/keys';
@@ -79,11 +77,6 @@ const prefetchData = () => {
   queryClient.prefetchQuery({
     queryKey: navbarKeys.list({}),
     queryFn: () => fetchNavbarItems({}),
-  });
-
-  queryClient.prefetchQuery({
-    queryKey: customPagesKeys.lists(),
-    queryFn: fetchCustomPages,
   });
 
   queryClient.prefetchQuery({
