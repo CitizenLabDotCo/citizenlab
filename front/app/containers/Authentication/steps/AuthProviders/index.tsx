022af7aebf058e9cb55abc2543b1b4a9e38e4c25
jamesspeake <james@speake.org>
2024-10-17 11:31:55 +0100

    [TAN-2821] Removed unused dependency

--- front/app/containers/Authentication/steps/AuthProviders/index.tsx
+++ front/app/containers/Authentication/steps/AuthProviders/index.tsx
@@ -1,7 +1,6 @@
 import React, { memo, useCallback } from 'react';
 
 import { Text } from '@citizenlab/cl2-component-library';
-import Box from 'component-library/components/Box';
 import { useLocation, useSearchParams } from 'react-router-dom';
 import styled from 'styled-components';
 
