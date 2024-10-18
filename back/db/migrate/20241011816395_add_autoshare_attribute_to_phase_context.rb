4a632c83940170ebb089c2e24b27395448b9a9b5
Amanda Anderson <anderson_amanda@mail.com>
2024-10-18 15:57:27 +0200

    Fix lint error

--- back/db/migrate/20241011816395_add_autoshare_attribute_to_phase_context.rb
+++ back/db/migrate/20241011816395_add_autoshare_attribute_to_phase_context.rb
@@ -2,6 +2,6 @@
 
 class AddAutoshareAttributeToPhaseContext < ActiveRecord::Migration[7.0]
   def change
-      add_column :phases, :autoshare_results_enabled, :boolean, default: true, null: false
+    add_column :phases, :autoshare_results_enabled, :boolean, default: true, null: false
   end
 end
