2b58a87bcb140d17722c578766a898f06828865a
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-10-18 17:18:46 +0200

    Add pageview spec and factory

---
+++ back/engines/commercial/impact_tracking/spec/factories/pageview.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :pageview, class: 'ImpactTracking::Pageview' do
+    session_id { '8d2c9ee4-8e08-4451-9659-7191c50899c4' }
+    path { '/en/' }
+  end
+end
