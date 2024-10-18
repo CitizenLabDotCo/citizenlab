b218170f005f681de7e4f97b9ecec15b915e1998
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-10-18 15:51:30 +0200

    Fix FE for session creation

--- front/app/modules/commercial/impact_tracking/index.ts
+++ front/app/modules/commercial/impact_tracking/index.ts
@@ -1,10 +1,7 @@
-import createRoutes from 'routes';
 import UAParser from 'ua-parser-js';
 
 import { events$ } from 'utils/analytics';
 import fetcher from 'utils/cl-react-query/fetcher';
-import { getUrlLocale } from 'utils/getUrlLocale';
-import matchPath, { getAllPathsFromRoutes } from 'utils/matchPath';
 import { ModuleConfiguration } from 'utils/moduleUtils';
 
 const signUpInTracks = {
@@ -16,23 +13,12 @@ const trackSessionStarted = () => {
   const uaParser = new UAParser();
   const uaResult = uaParser.getResult();
 
-  const allAppPaths = getAllPathsFromRoutes(createRoutes()[0]);
-
-  const routeMatch = matchPath(window.location.pathname, {
-    paths: allAppPaths,
-    exact: true,
-  });
-
   const referrer = document.referrer ?? window.frames?.top?.document.referrer;
   const deviceType = uaResult.device.type ?? 'desktop';
   const browserName = uaResult.browser.name;
   const browserVersion = uaResult.browser.major;
   const osName = uaResult.os.name;
   const osVersion = uaResult.os.version;
-
-  const entryPath = window.location.pathname;
-  const entryRoute = routeMatch?.path;
-  const entryLocale = getUrlLocale(window.location.pathname);
 
   fetcher({
     path: `/sessions`,
@@ -44,9 +30,6 @@ const trackSessionStarted = () => {
       browserVersion,
       osName,
       osVersion,
-      entryPath,
-      entryRoute,
-      entryLocale,
     },
   });
 };
