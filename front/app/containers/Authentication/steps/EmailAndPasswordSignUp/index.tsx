149ceb8422f8a709615d784c5554d3e41d6d24a5
EdwinKato <katedwin88@gmail.com>
2024-10-22 20:32:26 +0300

    Add profanity error messages in user sign up form

--- front/app/containers/Authentication/steps/EmailAndPasswordSignUp/index.tsx
+++ front/app/containers/Authentication/steps/EmailAndPasswordSignUp/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Box, Text, Label } from '@citizenlab/cl2-component-library';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -11,15 +11,20 @@ import useLocale from 'hooks/useLocale';
 
 import { SetError, State } from 'containers/Authentication/typings';
 import useAnySSOEnabled from 'containers/Authentication/useAnySSOEnabled';
+import userEditMessages from 'containers/UsersEditPage/messages';
 
+import { SectionField } from 'components/admin/Section';
+import Feedback from 'components/HookForm/Feedback';
 import Input from 'components/HookForm/Input';
 import PasswordInput from 'components/HookForm/PasswordInput';
 import { StyledPasswordIconTooltip } from 'components/smallForm';
 import Button from 'components/UI/Button';
+import Error from 'components/UI/Error';
 import { DEFAULT_MINIMUM_PASSWORD_LENGTH } from 'components/UI/PasswordInput';
 
 import { trackEventByName } from 'utils/analytics';
 import { useIntl, FormattedMessage } from 'utils/cl-intl';
+import Link from 'utils/cl-router/Link';
 import {
   isCLErrorsWrapper,
   handleHookFormSubmissionError,
@@ -56,6 +61,7 @@ const EmailAndPasswordSignUp = ({
   const { data: appConfiguration } = useAppConfiguration();
   const locale = useLocale();
   const { formatMessage } = useIntl();
+  const [profanityApiError, setProfanityApiError] = useState(false);
 
   const appConfigSettings = appConfiguration?.data.attributes.settings;
   const minimumPasswordLength =
@@ -92,7 +98,15 @@ const EmailAndPasswordSignUp = ({
         isInvitation: !!state.token,
         token: state.token,
       });
+      setProfanityApiError(false);
     } catch (e) {
+      const profanityApiError = e.errors.base.find(
+        (apiError) => apiError.error === 'includes_banned_words'
+      );
+      if (profanityApiError) {
+        setProfanityApiError(true);
+      }
+
       if (isCLErrorsWrapper(e)) {
         handleHookFormSubmissionError(e, methods.setError);
         return;
@@ -106,6 +120,27 @@ const EmailAndPasswordSignUp = ({
     <Box id="e2e-sign-up-email-password-container">
       <FormProvider {...methods}>
         <form onSubmit={methods.handleSubmit(handleSubmit)}>
+          <SectionField>
+            <Feedback
+              successMessage={formatMessage(userEditMessages.messageSuccess)}
+            />
+            {profanityApiError && (
+              <Error
+                text={
+                  <FormattedMessage
+                    {...userEditMessages.profanityError}
+                    values={{
+                      guidelinesLink: (
+                        <Link to="/pages/faq" target="_blank">
+                          {formatMessage(userEditMessages.guidelinesLinkText)}
+                        </Link>
+                      ),
+                    }}
+                  />
+                }
+              />
+            )}
+          </SectionField>
           <Box id="e2e-firstName-container">
             <Input
               name="first_name"
