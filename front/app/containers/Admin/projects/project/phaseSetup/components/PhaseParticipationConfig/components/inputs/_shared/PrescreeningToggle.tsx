7a17ab1a16e4162d24c0c7aed76109dac46f7379
jamesspeake <james@speake.org>
2024-10-24 12:07:37 +0100

    disable pre-screening idea toggle display unless feature flag enabled

--- front/app/containers/Admin/projects/project/phaseSetup/components/PhaseParticipationConfig/components/inputs/_shared/PrescreeningToggle.tsx
+++ front/app/containers/Admin/projects/project/phaseSetup/components/PhaseParticipationConfig/components/inputs/_shared/PrescreeningToggle.tsx
@@ -19,6 +19,8 @@ const PrescreeningToggle = ({
   togglePrescreeningEnabled,
   prescreeningFeatureAllowed,
 }: Props) => {
+  if (!prescreening_enabled) return null; // Remove when pricing decision made
+
   return (
     <SectionField>
       <SubSectionTitle style={{ marginBottom: '0px' }}>
