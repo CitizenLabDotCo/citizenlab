55eeb7358013d895e60e1bf25b07c8e15861aa76
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-10-25 17:33:07 +0100

    Remove unnecessary components in Frame

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/containers/index.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/containers/index.tsx
@@ -20,8 +20,6 @@ import { ContentBuilderErrors } from 'components/admin/ContentBuilder/typings';
 import ViewContainer from 'components/admin/ContentBuilder/ViewContainer';
 import { View } from 'components/admin/ContentBuilder/ViewContainer/typings';
 
-import HomepageBanner from '../components/CraftComponents/HomepageBanner';
-import Projects from '../components/CraftComponents/Projects';
 import Editor from '../components/Editor';
 import HomepageBuilderToolbox from '../components/HomepageBuilderToolbox';
 import HomepageBuilderTopBar from '../components/HomepageBuilderTopBar';
@@ -102,30 +100,7 @@ const HomepageBuilder = ({ homepageLayout }: Props) => {
           >
             <StyledRightColumn>
               <ViewContainer view={view}>
-                <Frame editorData={initialData}>
-                  <HomepageBanner
-                    homepageSettings={{
-                      banner_avatars_enabled: true,
-                      banner_layout: 'full_width_banner_layout',
-                      banner_signed_in_header_multiloc: {},
-                      banner_cta_signed_in_text_multiloc: {},
-                      banner_cta_signed_in_type: 'no_button',
-                      banner_cta_signed_in_url: null,
-                      banner_signed_out_header_multiloc: {},
-                      banner_signed_out_subheader_multiloc: {},
-                      banner_signed_out_header_overlay_color:
-                        theme.colors.tenantPrimary,
-                      banner_signed_out_header_overlay_opacity: 90,
-                      banner_signed_in_header_overlay_color:
-                        theme.colors.tenantPrimary,
-                      banner_signed_in_header_overlay_opacity: 90,
-                      banner_cta_signed_out_text_multiloc: {},
-                      banner_cta_signed_out_type: 'sign_up_button',
-                      banner_cta_signed_out_url: null,
-                    }}
-                  />
-                  <Projects />
-                </Frame>
+                <Frame editorData={initialData} />
               </ViewContainer>
             </StyledRightColumn>
           </LanguageProvider>
