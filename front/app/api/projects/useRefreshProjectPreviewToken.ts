540b4583b6dd8db7f83cc6712ce7af798d64baa9
Iva <iva@citizenlab.co>
2024-10-29 15:21:52 +0200

    Add refresh hook

---
+++ front/app/api/projects/useRefreshProjectPreviewToken.ts
@@ -0,0 +1,34 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { CLErrorsWrapper } from 'typings';
+
+import fetcher from 'utils/cl-react-query/fetcher';
+
+import projectsKeys from './keys';
+import { IProject } from './types';
+
+export const updateProjectPreviewToken = async ({
+  projectId,
+}: {
+  projectId: string;
+}) =>
+  fetcher<IProject>({
+    path: `/projects/${projectId}/refresh_preview_token`,
+    action: 'post',
+    body: null,
+  });
+
+const useUpdateProjectPreviewToken = () => {
+  const queryClient = useQueryClient();
+  return useMutation<IProject, CLErrorsWrapper, { projectId: string }>({
+    mutationFn: updateProjectPreviewToken,
+    onSuccess: async (data) => {
+      queryClient.invalidateQueries({
+        queryKey: projectsKeys.item({
+          id: data.data.id,
+        }),
+      });
+    },
+  });
+};
+
+export default useUpdateProjectPreviewToken;
