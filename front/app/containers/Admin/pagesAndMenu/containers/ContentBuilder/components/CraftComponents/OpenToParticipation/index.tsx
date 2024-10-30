0de1b95ff1afe7c35c67a8cc2909e84fb971ab0d
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-10-30 14:22:22 +0000

    Add hook to widget (WIP)

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/CraftComponents/OpenToParticipation/index.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/CraftComponents/OpenToParticipation/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
+import useProjectsWithActiveParticipatoryPhase from 'api/projects_mini/useProjectsWithActiveParticipatoryPhase';
+// import ProjectCarrousel from './ProjectCarrousel';
+
 const OpenToParticipation = () => {
+  const { data: projects } = useProjectsWithActiveParticipatoryPhase();
+
+  if (!projects?.data) return null;
+  if (projects.data.length === 0) return null;
+
+  // return <ProjectCarrousel />;
   return <></>;
 };
 
