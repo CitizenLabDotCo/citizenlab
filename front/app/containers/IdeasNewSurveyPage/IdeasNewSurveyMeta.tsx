275ded80e0806f6357763991bf4b14a76cd6b717
EdwinKato <katedwin88@gmail.com>
2024-10-30 13:13:33 +0300

    Maintain the orgName at the end of the title

--- front/app/containers/IdeasNewSurveyPage/IdeasNewSurveyMeta.tsx
+++ front/app/containers/IdeasNewSurveyPage/IdeasNewSurveyMeta.tsx
@@ -4,24 +4,31 @@ import { Helmet } from 'react-helmet';
 
 import useAppConfigurationLocales from 'hooks/useAppConfigurationLocales';
 
+import { useIntl } from 'utils/cl-intl';
 import getAlternateLinks from 'utils/cl-router/getAlternateLinks';
 import getCanonicalLink from 'utils/cl-router/getCanonicalLink';
+
+import messages from './messages';
 
 interface Props {
   surveyTitle: string;
 }
 
 const IdeasNewSurveyMeta = ({ surveyTitle }: Props) => {
+  const { formatMessage } = useIntl();
   const locales = useAppConfigurationLocales();
   const { location } = window;
+  const title = formatMessage(messages.surveyNewMetaTitle2, {
+    surveyTitle,
+  });
 
   return (
     <Helmet>
-      <title>{surveyTitle}</title>
+      <title>{title}</title>
       {getAlternateLinks(locales)}
       {getCanonicalLink()}
-      <meta name="title" content={surveyTitle} />
-      <meta property="og:title" content={surveyTitle} />
+      <meta name="title" content={title} />
+      <meta property="og:title" content={title} />
       <meta property="og:url" content={location.href} />
     </Helmet>
   );
