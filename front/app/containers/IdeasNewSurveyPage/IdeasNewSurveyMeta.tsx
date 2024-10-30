97d1e48d418cdf1a2c469d27a009ff95f79aaed6
EdwinKato <katedwin88@gmail.com>
2024-10-30 13:05:19 +0300

    Use editable survey title as page title for surveys

--- front/app/containers/IdeasNewSurveyPage/IdeasNewSurveyMeta.tsx
+++ front/app/containers/IdeasNewSurveyPage/IdeasNewSurveyMeta.tsx
@@ -2,47 +2,26 @@ import React from 'react';
 
 import { Helmet } from 'react-helmet';
 
-import useAuthUser from 'api/me/useAuthUser';
-
 import useAppConfigurationLocales from 'hooks/useAppConfigurationLocales';
 
-import { useIntl } from 'utils/cl-intl';
 import getAlternateLinks from 'utils/cl-router/getAlternateLinks';
 import getCanonicalLink from 'utils/cl-router/getCanonicalLink';
 
-import ideationMessages from '../IdeasNewPage/messages';
+interface Props {
+  surveyTitle: string;
+}
 
-import messages from './messages';
-
-const IdeasNewSurveyMeta = () => {
-  const { formatMessage } = useIntl();
-  const { data: authUser } = useAuthUser();
+const IdeasNewSurveyMeta = ({ surveyTitle }: Props) => {
   const locales = useAppConfigurationLocales();
   const { location } = window;
 
-  const ideasIndexTitle = formatMessage(messages.surveyNewMetaTitle1);
-  const ideasIndexDescription = formatMessage(
-    ideationMessages.ideaNewMetaDescription
-  );
-
   return (
     <Helmet>
-      <title>
-        {`
-            ${
-              authUser && authUser.data.attributes.unread_notifications
-                ? `(${authUser.data.attributes.unread_notifications}) `
-                : ''
-            }
-            ${ideasIndexTitle}
-          `}
-      </title>
+      <title>{surveyTitle}</title>
       {getAlternateLinks(locales)}
       {getCanonicalLink()}
-      <meta name="title" content={ideasIndexTitle} />
-      <meta name="description" content={ideasIndexDescription} />
-      <meta property="og:title" content={ideasIndexTitle} />
-      <meta property="og:description" content={ideasIndexDescription} />
+      <meta name="title" content={surveyTitle} />
+      <meta property="og:title" content={surveyTitle} />
       <meta property="og:url" content={location.href} />
     </Helmet>
   );
