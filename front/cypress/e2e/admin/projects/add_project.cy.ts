c8a45e1bf97166024f9f26f73dd6c79ebf3ff63c
Iva <iva@citizenlab.co>
2024-11-04 14:57:48 +0200

    Delete irrelevant e2e tests

--- front/cypress/e2e/admin/projects/add_project.cy.ts
+++ front/cypress/e2e/admin/projects/add_project.cy.ts
@@ -44,72 +44,6 @@ describe('Admin: add project', () => {
           .contains('Draft');
       });
 
-      it('creates a draft project', () => {
-        const projectTitleEN = randomString();
-        const projectTitleNLBE = randomString();
-        const projectTitleNLNL = randomString();
-        const projectTitleFRBE = randomString();
-
-        // Select 'Draft' publication status
-        cy.get('.e2e-projecstatus-draft').click();
-
-        // Type random project titles for these required fields
-        cy.get('#project-title').type(projectTitleEN);
-        cy.get('.e2e-localeswitcher.nl-BE').click();
-        cy.get('#project-title').type(projectTitleNLBE);
-        cy.get('.e2e-localeswitcher.nl-NL').click();
-        cy.get('#project-title').type(projectTitleNLNL);
-        cy.get('.e2e-localeswitcher.fr-BE').click();
-        cy.get('#project-title').type(projectTitleFRBE);
-
-        // Submit project
-        cy.get('.e2e-submit-wrapper-button button').click();
-
-        cy.wait(2000);
-
-        // Project should appear on top of the projects list
-        cy.visit('/admin/projects/all');
-        cy.get('#e2e-admin-projects-list-unsortable')
-          .children()
-          .first()
-          .contains(projectTitleEN);
-        cy.get('#e2e-admin-projects-list-unsortable')
-          .children()
-          .first()
-          .contains('Draft');
-      });
-
-      it('creates a published project', () => {
-        const projectTitleEN = randomString();
-        const projectTitleNLBE = randomString();
-        const projectTitleNLNL = randomString();
-        const projectTitleFRBE = randomString();
-
-        // Select 'Draft' publication status
-        cy.get('.e2e-projecstatus-published').click();
-
-        // Type random project titles for these required fields
-        cy.get('#project-title').type(projectTitleEN);
-        cy.get('.e2e-localeswitcher.nl-BE').click();
-        cy.get('#project-title').type(projectTitleNLBE);
-        cy.get('.e2e-localeswitcher.nl-NL').click();
-        cy.get('#project-title').type(projectTitleNLNL);
-        cy.get('.e2e-localeswitcher.fr-BE').click();
-        cy.get('#project-title').type(projectTitleFRBE);
-
-        // Submit project
-        cy.get('.e2e-submit-wrapper-button button').click();
-
-        cy.wait(2000);
-
-        // Project should appear on top of the projects list
-        cy.visit('/admin/projects/all');
-        cy.get('#e2e-admin-projects-list-unsortable')
-          .children()
-          .first()
-          .contains(projectTitleEN);
-      });
-
       it('creates an archived project', () => {
         const projectTitleEN = randomString();
         const projectTitleNLBE = randomString();
@@ -148,14 +82,11 @@ describe('Admin: add project', () => {
     // This test is failing because the project list at the bottom disappears,
     // and I (Luuc) have absolutely no idea why. So disabling for now.
     context.skip('Areas: Selection', () => {
-      it('creates a published project with the correct area', () => {
+      it('creates a project with the correct area', () => {
         const projectTitleEN = randomString();
         const projectTitleNLBE = randomString();
         const projectTitleNLNL = randomString();
         const projectTitleFRBE = randomString();
-
-        // Select 'Published' publication status
-        cy.get('.e2e-projecstatus-published').click();
 
         // Type random project titles for these required fields
         cy.get('#project-title').type(projectTitleEN);
