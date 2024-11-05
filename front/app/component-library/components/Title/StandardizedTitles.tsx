49200531322202c6bb48619989f38112eb02084d
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-11-05 10:48:58 +0000

    Use for accordion + fix spacing

--- front/app/component-library/components/Title/StandardizedTitles.tsx
+++ front/app/component-library/components/Title/StandardizedTitles.tsx
@@ -4,15 +4,15 @@ import { Color } from 'component-library/utils/styleUtils';
 
 import { BoxMarginProps } from '../Box';
 
-import { StyledTitle } from '.';
+import Title from '.';
 
 type Props = {
   color?: Color;
   children: React.ReactNode;
 } & BoxMarginProps;
 
-export const H1 = (props: Props) => <StyledTitle variant="h1" {...props} />;
+export const H1 = (props: Props) => <Title variant="h1" {...props} />;
 
-export const H2 = (props: Props) => <StyledTitle variant="h2" {...props} />;
+export const H2 = (props: Props) => <Title variant="h2" {...props} />;
 
-export const H3 = (props: Props) => <StyledTitle variant="h3" {...props} />;
+export const H3 = (props: Props) => <Title variant="h3" {...props} />;
