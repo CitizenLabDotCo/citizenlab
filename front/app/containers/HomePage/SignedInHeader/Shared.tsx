30a97cc093e54205754429beecd4b8d45fa7b057
luucvanderzee <luuc_v_d_zee@hotmail.com>
2024-11-06 19:11:05 +0000

    Update front/app/containers/HomePage/SignedInHeader/Shared.tsx
    Co-authored-by: Edwin <katedwin88@gmail.com>

--- front/app/containers/HomePage/SignedInHeader/Shared.tsx
+++ front/app/containers/HomePage/SignedInHeader/Shared.tsx
@@ -5,10 +5,7 @@ import Avatar from 'components/Avatar';
 
 export const HeaderContent = styled.div`
   position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
+  inset: 0;
   display: flex;
   align-items: center;
   justify-content: space-between;
