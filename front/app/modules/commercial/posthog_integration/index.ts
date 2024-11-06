79f658e9b1648215278ff1dfdd79454fca19c5d9
EdwinKato <katedwin88@gmail.com>
2024-11-06 15:42:36 +0300

    Refactor PostHog integration to work with updated version

--- front/app/modules/commercial/posthog_integration/index.ts
+++ front/app/modules/commercial/posthog_integration/index.ts
@@ -54,8 +54,8 @@ const initializePosthog = async (
     autocapture: false,
     persistence: 'memory', // no cookies
     loaded(ph) {
-      if (posthog.has_opted_out_capturing({ enable_persistence: false })) {
-        posthog.opt_in_capturing({ enable_persistence: false });
+      if (posthog.has_opted_out_capturing()) {
+        posthog.opt_in_capturing();
       }
 
       if (user) {
@@ -182,9 +182,7 @@ const configuration: ModuleConfiguration = {
             // writing to localstorage. Clearing it, instead, seems to work fine.
             // TODO: Fix this the next time the file is edited.
             // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-            posthogClient?.clear_opt_in_out_capturing({
-              enable_persistence: false,
-            });
+            posthogClient?.clear_opt_in_out_capturing();
           }
         }
       }
