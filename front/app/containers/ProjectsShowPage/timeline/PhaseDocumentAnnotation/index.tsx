766a999d85b70d9da5578e2ab5825ea55600f18a
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-11-07 10:59:01 +0000

    Replace header in konveio phase

--- front/app/containers/ProjectsShowPage/timeline/PhaseDocumentAnnotation/index.tsx
+++ front/app/containers/ProjectsShowPage/timeline/PhaseDocumentAnnotation/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import { Box } from '@citizenlab/cl2-component-library';
+import { Box, H2 } from '@citizenlab/cl2-component-library';
 
 import { IPhaseData } from 'api/phases/types';
 import { IProjectData } from 'api/projects/types';
 
 import DocumentAnnotation from 'containers/ProjectsShowPage/shared/document_annotation';
-import { ProjectPageSectionTitle } from 'containers/ProjectsShowPage/styles';
 
 import { FormattedMessage } from 'utils/cl-intl';
 
@@ -23,9 +22,9 @@ const PhaseDocumentAnnotation = ({ phase, project }: Props) => {
   if (documentUrl) {
     return (
       <Box position="relative" minHeight="500px">
-        <ProjectPageSectionTitle>
+        <H2 m="0">
           <FormattedMessage {...messages.document} />
-        </ProjectPageSectionTitle>
+        </H2>
 
         <DocumentAnnotation
           project={project}
