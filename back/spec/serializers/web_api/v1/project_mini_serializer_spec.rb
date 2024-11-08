3c026ed2de5eb44d14585b08ab45872f8fcac52a
Simon Tharby <simontharby@gmail.com>
2024-11-08 10:43:19 +0000

    [TAN-2782] Test action descriptors in new serializer spec

--- back/spec/serializers/web_api/v1/project_mini_serializer_spec.rb
+++ back/spec/serializers/web_api/v1/project_mini_serializer_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe WebApi::V1::ProjectMiniSerializer do
-  context 'when action descriptors are not passed to serializer in params' do
-    let(:project) { create(:project) }
-    let(:user) { create(:user) }
+  let!(:project) { create(:project) }
+  let!(:user) { create(:user) }
 
+  context 'when action descriptors are not passed to serializer in params' do
     it 'finds and returns the action descriptors' do
       action_descriptors = described_class
         .new(project, params: { current_user: user })
@@ -28,6 +28,38 @@ describe WebApi::V1::ProjectMiniSerializer do
         attending_event: { enabled: true, disabled_reason: nil },
         volunteering: { enabled: false, disabled_reason: 'project_inactive' }
       ))
+    end
+  end
+
+  context 'when action descriptors are passed to serializer in params' do
+    it 'uses the provided action descriptors' do
+      expected_action_descriptors = {
+        posting_idea: { enabled: true, disabled_reason: nil, future_enabled_at: nil },
+        commenting_idea: { enabled: true, disabled_reason: nil },
+        reacting_idea: {
+          enabled: true,
+          disabled_reason: nil,
+          up: { enabled: true, disabled_reason: nil },
+          down: { enabled: true, disabled_reason: nil }
+        },
+        comment_reacting_idea: { enabled: true, disabled_reason: nil },
+        annotating_document: { enabled: false, disabled_reason: 'not_document_annotation' },
+        taking_survey: { enabled: false, disabled_reason: 'not_survey' },
+        taking_poll: { enabled: false, disabled_reason: 'not_poll' },
+        voting: { enabled: false, disabled_reason: 'not_voting' },
+        attending_event: { enabled: true, disabled_reason: nil },
+        volunteering: { enabled: false, disabled_reason: 'not_volunteering' }
+      }
+
+      action_descriptors = described_class
+        .new(
+          project,
+          params: { current_user: user, project_descriptor_pairs: { project.id.to_s => expected_action_descriptors } }
+        )
+        .serializable_hash
+        .dig(:data, :attributes, :action_descriptors)
+
+      expect(action_descriptors).to eq(expected_action_descriptors)
     end
   end
 end
