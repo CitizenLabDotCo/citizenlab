c3e3ad1842f3e41b493695b9492f08de6ab99698
jamesspeake <james@speake.org>
2024-11-11 09:00:50 +0000

    [TAN-3000] Fixed tests for fake_sso verification method

--- back/spec/acceptance/permissions_spec.rb
+++ back/spec/acceptance/permissions_spec.rb
@@ -11,7 +11,7 @@ resource 'Permissions' do
     @project = create(:single_phase_ideation_project)
     @phase = TimelineService.new.current_phase_not_archived(@project)
     Permissions::PermissionsUpdateService.new.update_all_permissions
-    SettingsService.new.activate_feature! 'verification', settings: { verification_methods: [{ name: 'fake_sso' }] }
+    SettingsService.new.activate_feature! 'verification', settings: { verification_methods: [{ name: 'fake_sso', enabled_for_verified_actions: true }] }
   end
 
   let(:project_id) { @project.id }
