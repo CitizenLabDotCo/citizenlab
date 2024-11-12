837ad40704a4e596c3d6311ba06df46ab2f75ec9
Amanda Anderson <anderson_amanda@mail.com>
2024-11-12 12:31:47 +0100

    Remove unnecessary assignment

--- front/app/components/IdeaCards/shared/Filters/SortingBox/SortingBox.tsx
+++ front/app/components/IdeaCards/shared/Filters/SortingBox/SortingBox.tsx
@@ -24,12 +24,8 @@ const SortingBox = ({ handleSortOnChange, phaseId }: SortingBoxProps) => {
   const phaseDefaultSort = phase?.data.attributes.ideas_order;
 
   const [searchParams] = useSearchParams();
-  const currentSortType = searchParams.get('sort') || phaseDefaultSort || 'trending';
-
-  if (!currentSortType) {
-    // 'trending' is the global default type
-    currentSortType = phaseDefaultSort || 'trending';
-  }
+  const currentSortType =
+    searchParams.get('sort') || phaseDefaultSort || 'trending';
 
   return (
     <InputFilterCollapsible title={formatMessage(messages.sortBy)}>
