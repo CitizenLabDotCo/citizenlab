bbae063c193e47a32da41da4231d674d5691bd18
brentguf <brentguf@users.noreply.github.com>
2024-11-12 22:08:15 +0100

    Finish basic label layout & styles

--- front/app/components/IdeaCards/shared/Filters/SortingBox/SortTypeButton.tsx
+++ front/app/components/IdeaCards/shared/Filters/SortingBox/SortTypeButton.tsx
@@ -1,35 +1,44 @@
 import React from 'react';
 
-import { Box } from '@citizenlab/cl2-component-library';
-import { hideVisually } from 'polished';
-import styled from 'styled-components';
+import { Box, colors, Icon } from '@citizenlab/cl2-component-library';
+import { hideVisually, darken } from 'polished';
+import styled, { useTheme } from 'styled-components';
 
 import { FrontOfficeSortOptions } from 'api/ideas/types';
 
 import { useIntl } from 'utils/cl-intl';
 
-import { getLabelForSortingOption } from './utils';
+import { getIconNameForSortingOption, getLabelForSortingOption } from './utils';
 
 const HiddenRadio = styled.input.attrs({ type: 'radio' })`
-  // ${hideVisually()};
-  display: block;
+  ${hideVisually()};
 `;
 
 const Label = styled.label<{ selected: boolean }>`
+  display: flex;
+  align-items: center;
   padding: 8px;
   cursor: pointer;
   border-radius: ${({ theme }) => theme.borderRadius};
+  width: 100%;
+  font-size: ${({ theme }) => theme.fontSizes.base}px;
 
   ${({ theme: { colors }, selected }) =>
     selected
       ? `
     background: ${colors.tenantPrimary};
     color: ${colors.white};
-  `
+    
+    &:hover {
+      background: ${darken(0.15, colors.tenantPrimary)};
+    } `
       : `
     background: ${colors.white};
     color: ${colors.textPrimary};
-  `}
+    
+    &:hover {
+      background: rgba(132, 147, 158, 0.15);
+    }`}
 `;
 
 type SortTypeButtonProps = {
@@ -43,6 +52,7 @@ const SortTypeButton = ({
   handleSortOnChange,
   isSelected,
 }: SortTypeButtonProps) => {
+  const theme = useTheme();
   const { formatMessage } = useIntl();
 
   const handleOnChange = () => {
@@ -60,15 +70,6 @@ const SortTypeButton = ({
     //   textHoverColor="textPrimary"
     //   type="button"
     // >
-    //   {formatMessage(getLabelForSortingOption(sortType))}
-    //   {/* <Box>
-    //     <Icon
-    //       fill={isSelected ? colors.white : theme.colors.textPrimary}
-    //       height="14px"
-    //       name={getIconNameForSortingOption(sortType)}
-    //       my="auto"
-    //     />
-    //   </Box> */}
     // </Button>
     <Box display="flex" alignItems="center">
       <HiddenRadio
@@ -78,6 +79,13 @@ const SortTypeButton = ({
         onChange={handleOnChange}
       />
       <Label htmlFor={sortType} onClick={handleOnChange} selected={isSelected}>
+        <Icon
+          fill={isSelected ? colors.white : theme.colors.textPrimary}
+          height="14px"
+          name={getIconNameForSortingOption(sortType)}
+          mr="4px"
+          ariaHidden
+        />
         {formatMessage(getLabelForSortingOption(sortType))}
       </Label>
     </Box>
