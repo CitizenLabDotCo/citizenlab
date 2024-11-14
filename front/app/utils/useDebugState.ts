721e98df72941d44c3184366e4a6c0ae3678fd16
brentguf <brentguf@users.noreply.github.com>
2024-11-14 16:57:43 +0100

    useDebugState hook

---
+++ front/app/utils/useDebugState.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+function useDebugState<T>(
+  initialValue: T,
+  stateName: string
+): [T, React.Dispatch<React.SetStateAction<T>>] {
+  const [state, setState] = useState<T>(initialValue);
+
+  useEffect(() => {
+    // console.log(`${stateName} initial value:`, initialValue);
+    // console.log(`${stateName} updated to:`, state);
+  }, [state, stateName, initialValue]); // Logs whenever 'state' or 'stateName' changes
+
+  return [state, setState];
+}
+
+export default useDebugState;
