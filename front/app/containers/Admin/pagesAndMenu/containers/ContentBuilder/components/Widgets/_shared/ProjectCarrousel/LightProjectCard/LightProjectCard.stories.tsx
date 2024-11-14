9997e9948976b73f88aa96ded67225c7176afca3
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-11-14 14:34:45 +0000

    Add stories for different time indications

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/ProjectCarrousel/LightProjectCard/LightProjectCard.stories.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/ProjectCarrousel/LightProjectCard/LightProjectCard.stories.tsx
@@ -1,4 +1,4 @@
-import LightProjectCard from './LightProjectCard';
+import LightProjectCard from '.';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
@@ -13,34 +13,68 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const BASE_PROJECT = {
+  attributes: {
+    title_multiloc: {
+      en: 'My cool project',
+    },
+    slug: 'my-cool-project',
+    action_descriptors: {
+      posting_idea: { enabled: true },
+      reacting_idea: { enabled: false },
+      commenting_idea: { enabled: true },
+    },
+    starts_days_from_now: null,
+    ended_days_ago: null,
+  },
+  relationships: {
+    project_images: {
+      data: [
+        {
+          id: '1',
+          type: 'project_image',
+        },
+      ],
+    },
+    current_phase: {
+      data: {
+        id: '1',
+      },
+    },
+  },
+};
+
 export const Primary: Story = {
   args: {
+    project: BASE_PROJECT as any,
+  },
+};
+
+export const UpcomingProject: Story = {
+  args: {
     project: {
+      ...BASE_PROJECT,
       attributes: {
-        title_multiloc: {
-          en: 'My cool project',
-        },
-        slug: 'my-cool-project',
-        action_descriptors: {
-          posting_idea: { enabled: true },
-          reacting_idea: { enabled: false },
-          commenting_idea: { enabled: true },
-        },
+        ...BASE_PROJECT.attributes,
+        starts_days_from_now: 10,
       },
       relationships: {
-        project_images: {
-          data: [
-            {
-              id: '1',
-              type: 'project_image',
-            },
-          ],
-        },
-        current_phase: {
-          data: {
-            id: '1',
-          },
-        },
+        project_images: BASE_PROJECT.relationships.project_images,
+      },
+    } as any,
+  },
+};
+
+export const FinishedProject: Story = {
+  args: {
+    project: {
+      ...BASE_PROJECT,
+      attributes: {
+        ...BASE_PROJECT.attributes,
+        ended_days_ago: 15,
+      },
+      relationships: {
+        project_images: BASE_PROJECT.relationships.project_images,
       },
     } as any,
   },
