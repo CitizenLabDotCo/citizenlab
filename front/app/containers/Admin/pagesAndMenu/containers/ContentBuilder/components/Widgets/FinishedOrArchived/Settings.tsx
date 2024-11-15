0b63550847eef83eeac8d015bb12d103a053cdac
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-11-15 16:24:11 +0000

    _

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/FinishedOrArchived/Settings.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/FinishedOrArchived/Settings.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
+import { Box } from '@citizenlab/cl2-component-library';
+import { useNode } from '@craftjs/core';
+
+// import { FormattedMessage } from 'utils/cl-intl';
+
+import TitleMultilocInput from '../_shared/TitleMultilocInput';
+
+// import messages from './messages';
+
 const Settings = () => {
-  return <></>;
+  const {
+    actions: { setProp },
+    finished,
+    archived,
+  } = useNode((node) => ({
+    finished: node.data.props.finished,
+    archived: node.data.props.archived,
+  }));
+
+  console.log({ finished, archived });
+
+  return (
+    <Box my="20px">
+      {/* <Text mb="32px" color="textSecondary">
+        <FormattedMessage {...messages.thisWidgetShows} formatBold />
+      </Text> */}
+      <TitleMultilocInput name="finished_or_archived_title" />
+    </Box>
+  );
 };
 
 export default Settings;
