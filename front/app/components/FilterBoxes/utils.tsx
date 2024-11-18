dcc05a09583affba1f73ed102e9f338974d9cdb0
Amanda Anderson <anderson_amanda@mail.com>
2024-11-18 16:54:54 +0100

    Remove unnecessary code

--- front/app/components/FilterBoxes/utils.tsx
+++ front/app/components/FilterBoxes/utils.tsx
@@ -1,11 +1,8 @@
 export const scrollToTopIdeasList = () => {
   const ideasListPanel = document.getElementById('ideas-list-scroll-anchor');
-  const boundingBox = ideasListPanel?.getBoundingClientRect();
-  if (boundingBox) {
-    ideasListPanel?.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-      inline: 'start',
-    });
-  }
+  ideasListPanel?.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+    inline: 'start',
+  });
 };
