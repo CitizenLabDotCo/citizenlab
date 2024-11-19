729a297f7f038d4ce3970e0e0af862f1be2128fd
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-11-19 09:39:58 +0000

    Extract shared stuff from ProjectCarrousel

---
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/BaseCarrousel/Containers.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { Box, useBreakpoint } from '@citizenlab/cl2-component-library';
+import styled from 'styled-components';
+
+import { DEFAULT_PADDING } from 'components/admin/ContentBuilder/constants';
+
+const StyledBox = styled(Box)`
+  .scroll-button {
+    opacity: 0;
+    transition: opacity 0.3s;
+    cursor: pointer;
+  }
+
+  &:hover {
+    .scroll-button {
+      opacity: 1;
+    }
+  }
+`;
+
+interface CarrouselContainerProps {
+  children: React.ReactNode;
+}
+
+export const CarrouselContainer = ({ children }: CarrouselContainerProps) => {
+  const isSmallerThanPhone = useBreakpoint('phone');
+
+  return (
+    <Box
+      px={isSmallerThanPhone ? undefined : DEFAULT_PADDING}
+      py={DEFAULT_PADDING}
+      w="100%"
+      display="flex"
+      justifyContent="center"
+    >
+      <StyledBox w="100%" maxWidth="1200px" position="relative">
+        {children}
+      </StyledBox>
+    </Box>
+  );
+};
+
+export const CardContainer = styled.div`
+  scroll-snap-align: start;
+`;
