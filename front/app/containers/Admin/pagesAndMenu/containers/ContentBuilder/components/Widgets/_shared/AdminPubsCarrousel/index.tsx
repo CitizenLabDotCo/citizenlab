70f6e869aba6d673aa448ef13bafd897fbf6458c
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-11-19 10:53:34 +0000

    Carrousel (WIP)

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/AdminPubsCarrousel/index.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/AdminPubsCarrousel/index.tsx
@@ -1,7 +1,179 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
-const AdminPubsCarrousel = () => {
-  return <></>;
+import {
+  Box,
+  Spinner,
+  useBreakpoint,
+  Title,
+} from '@citizenlab/cl2-component-library';
+import useInstanceId from 'component-library/hooks/useInstanceId';
+import { debounce } from 'lodash-es';
+import { useInView } from 'react-intersection-observer';
+
+import { CARD_IMAGE_ASPECT_RATIO } from 'api/project_images/useProjectImages';
+import { MiniProjectData } from 'api/projects_mini/types';
+
+import { DEFAULT_PADDING } from 'components/admin/ContentBuilder/constants';
+
+import { CARD_GAP } from '../BaseCarrousel/constants';
+import { CarrouselContainer, CardContainer } from '../BaseCarrousel/Containers';
+import Gradient from '../BaseCarrousel/Gradient';
+import HorizontalScroll from '../BaseCarrousel/HorizontalScroll';
+import ScrollButton from '../BaseCarrousel/ScrollButton';
+import SkipButton from '../BaseCarrousel/SkipButton';
+import {
+  getUpdatedButtonVisibility,
+  skipCarrousel,
+} from '../BaseCarrousel/utils';
+
+import AdminPubCard from './AdminPubCard';
+import { CARD_WIDTH } from './constants';
+
+interface Props {
+  title: string;
+  projects: MiniProjectData[];
+  hasMore: boolean;
+  onLoadMore: () => void;
+}
+
+const AdminPubsCarrousel = ({
+  title,
+  projects,
+  hasMore,
+  onLoadMore,
+}: Props) => {
+  const [scrollContainerRef, setScrollContainerRef] = useState<
+    HTMLDivElement | undefined
+  >(undefined);
+  const [showPreviousButton, setShowPreviousButton] = useState(false);
+  const [showNextButton, setShowNextButton] = useState(false);
+  const isSmallerThanPhone = useBreakpoint('phone');
+  const instanceId = useInstanceId();
+  const endId = `end-carrousel-${instanceId}`;
+
+  const { ref } = useInView({
+    onChange: (inView) => {
+      if (inView && hasMore) {
+        onLoadMore();
+      }
+    },
+  });
+
+  const handleButtonVisiblity = useCallback(
+    (ref: HTMLDivElement, hasMore: boolean) => {
+      if (isSmallerThanPhone) return;
+      const { showNextButton, showPreviousButton } = getUpdatedButtonVisibility(
+        ref,
+        hasMore
+      );
+
+      setShowNextButton(showNextButton);
+      setShowPreviousButton(showPreviousButton);
+    },
+    [isSmallerThanPhone]
+  );
+
+  useEffect(() => {
+    if (!scrollContainerRef) return;
+
+    const handler = debounce(() => {
+      handleButtonVisiblity(scrollContainerRef, hasMore);
+    }, 100);
+
+    scrollContainerRef.addEventListener('scroll', handler);
+
+    return () => {
+      scrollContainerRef.removeEventListener('scroll', handler);
+    };
+  }, [scrollContainerRef, hasMore, handleButtonVisiblity]);
+
+  const handleKeyDown = (e: React.KeyboardEvent<any>) => {
+    if (e.code === 'Escape') {
+      skipCarrousel(endId);
+    }
+
+    if (e.code === 'Tab' && scrollContainerRef) {
+      setTimeout(() => {
+        e.shiftKey
+          ? (scrollContainerRef.scrollLeft -= CARD_WIDTH + CARD_GAP)
+          : (scrollContainerRef.scrollLeft += CARD_WIDTH + CARD_GAP);
+      }, 50);
+    }
+  };
+
+  return (
+    <>
+      <CarrouselContainer>
+        <Title
+          variant="h3"
+          as="h2"
+          mt="0px"
+          ml={isSmallerThanPhone ? DEFAULT_PADDING : undefined}
+        >
+          {title}
+        </Title>
+        <SkipButton onSkip={() => skipCarrousel(endId)} />
+        <HorizontalScroll
+          setRef={(ref) => {
+            if (ref) {
+              setScrollContainerRef(ref);
+              handleButtonVisiblity(ref, hasMore);
+            }
+          }}
+        >
+          {projects.map((project) => (
+            <CardContainer key={project.id}>
+              <AdminPubCard
+                ml={isSmallerThanPhone ? `${CARD_GAP}px` : undefined}
+                mr={isSmallerThanPhone ? undefined : `${CARD_GAP}px`}
+                // project={project}
+                onKeyDown={handleKeyDown}
+              />
+            </CardContainer>
+          ))}
+          {hasMore && (
+            <CardContainer>
+              <Box
+                ref={ref}
+                w={`${CARD_WIDTH}px`}
+                h={`${CARD_WIDTH / CARD_IMAGE_ASPECT_RATIO}px`}
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Spinner />
+              </Box>
+            </CardContainer>
+          )}
+        </HorizontalScroll>
+        {showPreviousButton && !isSmallerThanPhone && (
+          <>
+            <ScrollButton
+              variant="left"
+              onClick={() => {
+                if (!scrollContainerRef) return;
+                scrollContainerRef.scrollLeft -= CARD_WIDTH + CARD_GAP;
+              }}
+            />
+            <Gradient variant="left" />
+          </>
+        )}
+        {showNextButton && !isSmallerThanPhone && (
+          <>
+            <ScrollButton
+              variant="right"
+              onClick={() => {
+                if (!scrollContainerRef) return;
+                scrollContainerRef.scrollLeft += CARD_WIDTH + CARD_GAP;
+              }}
+            />
+            <Gradient variant="right" />
+          </>
+        )}
+      </CarrouselContainer>
+      <i id={endId} />
+    </>
+  );
 };
 
 export default AdminPubsCarrousel;
