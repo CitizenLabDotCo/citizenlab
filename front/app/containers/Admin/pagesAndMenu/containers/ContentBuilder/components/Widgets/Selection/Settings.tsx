6206bead393d4607a221a44b69ced42d941dab50
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-11-19 19:20:34 +0000

    _

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/Selection/Settings.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/Selection/Settings.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 import { Box, Text } from '@citizenlab/cl2-component-library';
 
+import {
+  SortableList,
+  SortableRow,
+  TextCell,
+} from 'components/admin/ResourceList';
+
 import { FormattedMessage } from 'utils/cl-intl';
 
 import TitleMultilocInput from '../_shared/TitleMultilocInput';
@@ -9,6 +15,10 @@ import TitleMultilocInput from '../_shared/TitleMultilocInput';
 import messages from './messages';
 
 const Settings = () => {
+  const handleReorder = () => {
+    // TODO
+  };
+
   return (
     <Box my="20px">
       <Text mb="32px" color="textSecondary">
@@ -17,6 +27,29 @@ const Settings = () => {
       <Box mb="20px">
         <TitleMultilocInput name="selection_title" />
       </Box>
+      <SortableList
+        items={[]} // TODO
+        onReorder={handleReorder}
+        key={0} // TODO
+      >
+        {({ itemsList, handleDragRow, handleDropRow }) => (
+          <>
+            {itemsList.map((item, index) => (
+              <SortableRow
+                key={item.id}
+                id={item.id}
+                index={index}
+                moveRow={handleDragRow}
+                dropRow={handleDropRow}
+              >
+                <TextCell>
+                  <Text>{item.attributes.title_multiloc}</Text>
+                </TextCell>
+              </SortableRow>
+            ))}
+          </>
+        )}
+      </SortableList>
     </Box>
   );
 };
