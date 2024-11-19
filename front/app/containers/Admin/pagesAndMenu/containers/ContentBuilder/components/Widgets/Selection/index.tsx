c27a63eb05b870334cd0711f06c8f0cef4d7fe7d
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-11-19 14:00:50 +0000

    Scaffold selection widget

---
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/Selection/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import messages from './messages';
+import Settings from './Settings';
+
+const Selection = () => {
+  return <></>;
+};
+
+Selection.craft = {
+  related: {
+    settings: Settings,
+  },
+};
+
+export const selectionTitle = messages.selectionTitle;
+
+export default Selection;
---
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/Selection/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  selectionTitle: {
+    id: 'front.app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.Widgets.Selection.selectionTitle',
+    defaultMessage: 'Selection',
+  },
+});
