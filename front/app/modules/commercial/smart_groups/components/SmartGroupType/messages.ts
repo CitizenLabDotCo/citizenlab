6530c0173e31a961cbda7605c9cc3be9ac1f6926
EdwinKato <katedwin88@gmail.com>
2024-11-21 10:45:16 +0300

    Update messages to clarify license limitations irrespective of specific plan

--- front/app/modules/commercial/smart_groups/components/SmartGroupType/messages.ts
+++ front/app/modules/commercial/smart_groups/components/SmartGroupType/messages.ts
@@ -4,10 +4,6 @@ export default defineMessages({
   smartGroupsAvailability: {
     id: 'app.containers.AdminPage.Users.UsersGroup.smartGroupsAvailability',
     defaultMessage:
-      'Smart groups are available on the Standard and Premium plans.',
-  },
-  learnMorePlans: {
-    id: 'app.containers.AdminPage.Users.UsersGroup.learnMorePlans',
-    defaultMessage: 'Learn more about our plans',
+      'Configuring smart groups is not part of your current license. Reach out to your GovSuccess Manager to learn more about it.',
   },
 });
