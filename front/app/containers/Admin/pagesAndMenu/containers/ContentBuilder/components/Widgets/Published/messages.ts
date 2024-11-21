bf607f2868f621cc18e9be52fef4536843ae855f
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-11-21 14:33:37 +0000

    Add settings

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/Published/messages.ts
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/Published/messages.ts
@@ -5,4 +5,9 @@ export default defineMessages({
     id: 'front.app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.Widgets.Published.publishedTitle',
     defaultMessage: 'Published projects',
   },
+  thisWidgetWillShow: {
+    id: 'front.app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.Widgets.Published.thisWidgetWillShow',
+    defaultMessage:
+      'This widget will case the projects that are currently published, respecting the ordering defined on the projects page. This behavior is the same as the "active" tab of the "legacy" projects widget.',
+  },
 });
