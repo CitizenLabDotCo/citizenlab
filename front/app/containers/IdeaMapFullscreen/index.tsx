e0ff3a2cd5c77cef1babf26a54f0e2801cd8e90d
Amanda Anderson <anderson_amanda@mail.com>
2024-11-22 14:19:02 +0100

    Add route and new container

---
+++ front/app/containers/IdeaMapFullscreen/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import { Box, Spinner } from '@citizenlab/cl2-component-library';
+import { useParams } from 'react-router-dom';
+
+import useIdeaMarkers from 'api/idea_markers/useIdeaMarkers';
+import useProjectMapConfig from 'api/map_config/useProjectMapConfig';
+import useProjectBySlug from 'api/projects/useProjectBySlug';
+
+import IdeasMap from 'components/IdeasMap';
+
+const IdeaMapFullscreen = () => {
+  const { phaseId, slug } = useParams() as {
+    phaseId: string;
+    slug: string;
+  };
+  const { data: project } = useProjectBySlug(slug);
+  const projectId = project?.data.id;
+
+  const { data: mapConfig, isLoading } = useProjectMapConfig(projectId);
+
+  const { data: ideaMarkers } = useIdeaMarkers({
+    projectIds: projectId ? [projectId] : null,
+    phaseId,
+  });
+
+  if (isLoading || !projectId) {
+    return (
+      <Box height="100vh">
+        <Box mt="auto">
+          <Spinner />
+        </Box>
+      </Box>
+    );
+  }
+
+  return (
+    <Box position="absolute" h="100vh" w="100vw">
+      <IdeasMap
+        projectId={projectId}
+        phaseId={phaseId}
+        mapConfig={mapConfig}
+        ideaMarkers={ideaMarkers}
+      />
+    </Box>
+  );
+};
+
+export default IdeaMapFullscreen;
