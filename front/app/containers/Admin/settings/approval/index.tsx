7ea9552c0348fdac3cb482fc6564b2d6f7ebb817
Iva <iva@citizenlab.co>
2024-11-25 16:40:56 +0200

    Refactor multiple select component

--- front/app/containers/Admin/settings/approval/index.tsx
+++ front/app/containers/Admin/settings/approval/index.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
 
+import useUsers from 'api/users/useUsers';
+
+import MultipleSelect from 'components/UI/MultipleSelect';
+
 const Approval = () => {
-  return <div>Approval</div>;
+  const { data: adminUsers } = useUsers({
+    can_admin: true,
+  });
+
+  const { data: approvers } = useUsers({
+    can_approve: true,
+  });
+
+  if (!adminUsers) return null;
+
+  const options = adminUsers.data.map((user) => ({
+    value: user.id,
+    label: `${user.attributes.first_name} ${user.attributes.last_name}`,
+  }));
+
+  const selectedOptions = approvers?.data.map((user) => ({
+    value: user.id,
+    label: `${user.attributes.first_name} ${user.attributes.last_name}`,
+  }));
+
+  return (
+    <div>
+      <MultipleSelect
+        value={selectedOptions}
+        onChange={(value) => console.log(value)}
+        options={options}
+        label="Select approvers"
+      />
+    </div>
+  );
 };
 
 export default Approval;
