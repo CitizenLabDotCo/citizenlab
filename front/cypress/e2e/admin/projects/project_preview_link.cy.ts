d87762af42d977e39c881476cefd932d58d83313
Iva <iva@citizenlab.co>
2024-11-25 13:09:51 +0200

    Fix e2e test slugs

--- front/cypress/e2e/admin/projects/project_preview_link.cy.ts
+++ front/cypress/e2e/admin/projects/project_preview_link.cy.ts
@@ -18,7 +18,7 @@ describe('Admin: edit project', () => {
       projectId = project.body.data.id;
       projectSlug = project.body.data.attributes.slug;
       cy.setAdminLoginCookie();
-      cy.visit(`/en/admin/projects/${projectId}`);
+      cy.visit(`/admin/projects/${projectId}`);
       cy.get('#e2e-share-link').click();
 
       cy.get('#e2e-link')
@@ -36,19 +36,19 @@ describe('Admin: edit project', () => {
   });
 
   it('should not be able to access a draft project without a preview link', () => {
-    cy.visit(`/en/projects/${projectSlug}`);
+    cy.visit(`/projects/${projectSlug}`);
     cy.get('#e2e-not-authorized').should('exist');
   });
 
   it('should be able to access a draft project with a preview link', () => {
     cy.visit(link);
-    cy.url().should('contain', `/en/projects/${projectSlug}`);
+    cy.url().should('contain', `/projects/${projectSlug}`);
     cy.get('#e2e-project-page').should('contain', projectTitle);
   });
 
   it('should not access project with invalid preview link', () => {
     cy.setAdminLoginCookie();
-    cy.visit(`/en/admin/projects/${projectId}`);
+    cy.visit(`/admin/projects/${projectId}`);
     cy.get('#e2e-share-link').click();
     cy.get('#e2e-refresh-link').click();
     cy.logout();
