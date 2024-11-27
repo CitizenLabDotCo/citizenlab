6d6ffec57b3b1b0357ebbbe521186ac0b5fc2cd4
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-11-27 14:03:45 +0000

    Make sure pageviews_controller handles missing routes

--- back/engines/commercial/impact_tracking/app/controllers/impact_tracking/web_api/v1/pageviews_controller.rb
+++ back/engines/commercial/impact_tracking/app/controllers/impact_tracking/web_api/v1/pageviews_controller.rb
@@ -38,6 +38,8 @@ module ImpactTracking
       end
 
       def derive_project_id(path, route)
+        return unless path.present? && route.present?
+
         if route.include? 'projects/:slug'
           return get_project_from_project_slug(path)&.id
         end
