7617e1ff2f0698e6e84aa010b6a1782eb843812f
EdwinKato <katedwin88@gmail.com>
2024-11-27 11:28:35 +0300

    Update smartGroupsAvailability message ID

--- front/app/modules/commercial/smart_groups/components/SmartGroupType/messages.ts
+++ front/app/modules/commercial/smart_groups/components/SmartGroupType/messages.ts
@@ -2,7 +2,7 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   smartGroupsAvailability: {
-    id: 'app.containers.AdminPage.Users.UsersGroup.smartGroupsAvailability',
+    id: 'app.containers.AdminPage.Users.UsersGroup.smartGroupsAvailability1',
     defaultMessage:
       'Configuring smart groups is not part of your current license. Reach out to your GovSuccess Manager to learn more about it.',
   },
