f79ed665870685aba7c2e591cf9380072508b4fd
Sébastien Hoorens <sebastien@citizenlab.co>
2024-12-02 16:59:31 +0100

    fixing migration and schema by adding vector extension to schema postprocessor

--- back/db/migrate/20241120151945_create_embeddings_similarities.rb
+++ back/db/migrate/20241120151945_create_embeddings_similarities.rb
@@ -1,10 +1,10 @@
 class CreateEmbeddingsSimilarities < ActiveRecord::Migration[7.0]
   def change
-    enable_extension 'vector'
+    # enable_extension 'vector'
 
     create_table :embeddings_similarities, id: :uuid do |t|
-      t.column :embedding, 'shared_extensions.vector(1024)'
-      # t.column :embedding, :vector, limit: 1024, null: true
+      # t.column :embedding, 'shared_extensions.vector(1024)'
+      t.column :embedding, :vector, limit: 1024, null: true
       t.references :idea, foreign_key: true, type: :uuid, index: true, null: false
 
       t.timestamps
