9dfb79072ca7028e4edcdfea7cef53be490e8ac6
jamesspeake <james@speake.org>
2024-12-03 13:12:24 +0000

    [TAN-3104] Remove duplicate draft survey responses when survey is submitted

--- back/engines/commercial/idea_assignment/app/services/idea_assignment/patches/side_fx_idea_service.rb
+++ back/engines/commercial/idea_assignment/app/services/idea_assignment/patches/side_fx_idea_service.rb
@@ -12,6 +12,9 @@ module IdeaAssignment
 
       def after_update(idea, user)
         super
+
+        remove_duplicate_survey_responses_on_publish(idea)
+
         return unless idea.assignee_id_previously_changed?
 
         initiating_user = user_for_activity_on_anonymizable_item(idea, @automatic_assignment ? nil : user)
@@ -21,10 +24,26 @@ module IdeaAssignment
 
       def before_publish(idea, user)
         super
+
         return if idea.assignee
 
         idea.assignee = IdeaAssignmentService.new.automatically_assigned_idea_assignee idea
         @automatic_assignment = true if idea.assignee
+      end
+
+      private
+
+      # If a survey is opened in multiple tabs then different draft responses can be created for the same user.
+      # We need to remove any duplicates when the survey is submitted.
+      def remove_duplicate_survey_responses_on_publish(idea)
+        return unless idea.creation_phase&.native_survey? && idea.publication_status_previously_changed?(from: 'draft', to: 'published')
+
+        Idea.where(
+          creation_phase_id: idea.creation_phase_id,
+          author: idea.author,
+          publication_status: 'draft')
+            .where.not(id: idea.id)
+            .destroy_all
       end
     end
   end
