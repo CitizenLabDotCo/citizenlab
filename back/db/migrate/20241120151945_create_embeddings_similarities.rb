9718756db7f7e86a529c2ac2bdcdd82ebe0c5b3f
Sébastien Hoorens <sebastien@citizenlab.co>
2024-12-03 10:49:32 +0100

    trying to get schema right again

--- back/db/migrate/20241120151945_create_embeddings_similarities.rb
+++ back/db/migrate/20241120151945_create_embeddings_similarities.rb
@@ -1,11 +1,13 @@
 class CreateEmbeddingsSimilarities < ActiveRecord::Migration[7.0]
   def change
-    # enable_extension 'vector'
+    enable_extension 'vector'
 
     create_table :embeddings_similarities, id: :uuid do |t|
-      # t.column :embedding, 'shared_extensions.vector(1024)'
-      t.column :embedding, :vector, limit: 1024, null: false
-      t.references :idea, foreign_key: true, type: :uuid, index: true, null: false
+      t.column :embedding, 'shared_extensions.vector(1024)', null: false
+      # t.column :embedding, :vector, limit: 1024, null: false
+      # t.references :idea, foreign_key: true, type: :uuid, index: true, null: false
+      t.references :embeddable, polymorphic: true, type: :uuid, index: true, null: false
+      t.string :context, null: true
 
       t.timestamps
     end
