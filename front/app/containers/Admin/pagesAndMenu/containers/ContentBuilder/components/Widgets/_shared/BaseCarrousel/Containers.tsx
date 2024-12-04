f1232f50d6489e97ccee9764fe0b63b6db9e333a
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-12-04 18:17:52 -0300

    Fix horizontal scroll issue with larger admin publication carrousel

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/BaseCarrousel/Containers.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/BaseCarrousel/Containers.tsx
@@ -33,7 +33,9 @@ export const CarrouselContainer = ({ children }: CarrouselContainerProps) => {
       px={isSmallerThanPhone ? undefined : DEFAULT_PADDING}
       py={DEFAULT_Y_PADDING}
       w="100%"
+      maxWidth="100%"
       display="flex"
+      overflowX="hidden"
       justifyContent="center"
     >
       <StyledBox w="100%" maxWidth="1200px" position="relative">
