ef22921d78c16df9f4fae1ff1c3f4b86d01b820e
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-12-06 11:26:02 -0300

    Simplify load more card even more

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/BaseCarrousel/LoadMoreCard.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/BaseCarrousel/LoadMoreCard.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
 
-import { media, Box, Spinner } from '@citizenlab/cl2-component-library';
-import styled from 'styled-components';
+import { Box, Spinner } from '@citizenlab/cl2-component-library';
 
 import { CARD_IMAGE_ASPECT_RATIO } from 'api/project_images/useProjectImages';
-
-const CardContainer = styled.div`
-  ${media.phone`
-      margin-right: 100vw;
-  `}
-`;
 
 interface Props {
   width: number;
@@ -18,18 +11,16 @@ interface Props {
 
 const LoadMoreCard = ({ width, cardRef }: Props) => {
   return (
-    <CardContainer>
-      <Box
-        ref={cardRef}
-        w={`${width}px`}
-        h={`${width / CARD_IMAGE_ASPECT_RATIO}px`}
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Spinner />
-      </Box>
-    </CardContainer>
+    <Box
+      ref={cardRef}
+      w={`${width}px`}
+      h={`${width / CARD_IMAGE_ASPECT_RATIO}px`}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Spinner />
+    </Box>
   );
 };
 
