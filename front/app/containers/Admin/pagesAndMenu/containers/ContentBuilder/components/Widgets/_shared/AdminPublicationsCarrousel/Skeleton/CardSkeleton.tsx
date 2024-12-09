6063331d03c12cd728a85490e5641df799a4da54
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-12-09 20:00:01 -0300

    Finish admin publication skeleton

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/AdminPublicationsCarrousel/Skeleton/CardSkeleton.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/AdminPublicationsCarrousel/Skeleton/CardSkeleton.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-import { Box, useBreakpoint, Shimmer } from '@citizenlab/cl2-component-library';
+import {
+  Box,
+  useBreakpoint,
+  Shimmer,
+  colors,
+} from '@citizenlab/cl2-component-library';
 import { useTheme } from 'styled-components';
 
 import ImageSkeleton from '../../ImageSkeleton';
@@ -15,7 +20,6 @@ const CardSkeleton = ({ ml, mr }: Props) => {
   const theme = useTheme();
 
   const isSmallerThanPhone = useBreakpoint('phone');
-
   const cardWidth = isSmallerThanPhone ? SMALL_CARD_WIDTH : BIG_CARD_WIDTH;
 
   return (
@@ -23,17 +27,31 @@ const CardSkeleton = ({ ml, mr }: Props) => {
       <ImageSkeleton />
       <Shimmer
         bgColor={theme.colors.tenantText}
-        width={`${cardWidth}px`}
+        width={`${cardWidth * 0.8}px`}
         borderRadius="16px"
         height="20px"
         mt="8px"
       />
       <Shimmer
         bgColor={theme.colors.tenantText}
-        width="140px"
+        width="80px"
         borderRadius="16px"
-        height="20px"
-        mt="8px"
+        height="16px"
+        mt="12px"
+      />
+      <Shimmer
+        bgColor={colors.grey600}
+        width={`${cardWidth}px`}
+        borderRadius="16px"
+        height="16px"
+        mt="12px"
+      />
+      <Shimmer
+        bgColor={colors.grey600}
+        width="90px"
+        borderRadius="16px"
+        height="16px"
+        mt="12px"
       />
     </Box>
   );
