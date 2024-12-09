c445c0998633e76fc14613fca1150d0491e8b68c
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-12-09 09:34:30 -0300

    Scaffold project carrousel skeleton

---
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/ProjectCarrousel/Skeleton/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { CarrouselContainer } from '../../BaseCarrousel/Containers';
+
+const Skeleton = () => {
+  return (
+    <CarrouselContainer>
+      <div>TODO</div>
+    </CarrouselContainer>
+  );
+};
+
+export default Skeleton;
