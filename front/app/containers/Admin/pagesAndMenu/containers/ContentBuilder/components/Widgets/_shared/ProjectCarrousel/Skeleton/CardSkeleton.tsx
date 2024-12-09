f2422e27fb9c2e3fd96d47895b4a39fbe6812f38
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-12-09 14:41:55 -0300

    Card skeleton (WIP)

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/ProjectCarrousel/Skeleton/CardSkeleton.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/ProjectCarrousel/Skeleton/CardSkeleton.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
-const CardSkeleton = () => {
-  return <></>;
+import {
+  stylingConsts,
+  colors,
+  Title,
+} from '@citizenlab/cl2-component-library';
+import styled from 'styled-components';
+
+import { CARD_IMAGE_ASPECT_RATIO } from 'api/project_images/useProjectImages';
+
+import { CardContainer } from '../../BaseCard';
+import { CARD_WIDTH } from '../constants';
+
+const ImageSkeleton = styled.div`
+  width: 100%;
+  display: flex;
+  aspect-ratio: ${CARD_IMAGE_ASPECT_RATIO};
+  margin-right: 10px;
+  overflow: hidden;
+  position: relative;
+  border-radius: ${stylingConsts.borderRadius};
+  background-color: ${colors.grey300};
+`;
+
+interface Props {
+  ml?: string;
+  mr?: string;
+}
+
+const CardSkeleton = ({ ml, mr }: Props) => {
+  return (
+    <CardContainer w={`${CARD_WIDTH}px`} ml={ml} mr={mr}>
+      <ImageSkeleton />
+      <Title variant="h4" as="h3" mt="8px" mb="0px" color="tenantText">
+        Lorem ipsum blabla
+      </Title>
+    </CardContainer>
+  );
 };
 
 export default CardSkeleton;
--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/ProjectCarrousel/Skeleton/index.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/ProjectCarrousel/Skeleton/index.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
 
+import { useBreakpoint, Box } from '@citizenlab/cl2-component-library';
+import { useTheme } from 'styled-components';
+
+import { DEFAULT_PADDING } from 'components/admin/ContentBuilder/constants';
+
+import { CARD_GAP } from '../../BaseCarrousel/constants';
 import { CarrouselContainer } from '../../BaseCarrousel/Containers';
 
+import CardSkeleton from './CardSkeleton';
+
 const Skeleton = () => {
+  const isSmallerThanPhone = useBreakpoint('phone');
+  const theme = useTheme();
+
   return (
     <CarrouselContainer>
-      <div>TODO</div>
+      {/* <Title
+        variant="h2"
+        mt="0px"
+        ml={isSmallerThanPhone ? DEFAULT_PADDING : undefined}
+        color="tenantText"
+      >
+        Test title
+      </Title> */}
+      <Box
+        bgColor={theme.colors.tenantText}
+        width="240px"
+        borderRadius="16px"
+        height="32px"
+        mb="16px"
+        ml={isSmallerThanPhone ? DEFAULT_PADDING : undefined}
+      />
+
+      <Box display="flex" flexDirection="row">
+        <CardSkeleton
+          ml={isSmallerThanPhone ? `${CARD_GAP}px` : undefined}
+          mr={isSmallerThanPhone ? undefined : `${CARD_GAP}px`}
+        />
+        <CardSkeleton
+          ml={isSmallerThanPhone ? `${CARD_GAP}px` : undefined}
+          mr={isSmallerThanPhone ? undefined : `${CARD_GAP}px`}
+        />
+      </Box>
     </CarrouselContainer>
   );
 };
