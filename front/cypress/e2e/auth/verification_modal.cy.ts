61af79e3da9b7c90a37b52638f35205c8453694e
Iva <iva@citizenlab.co>
2024-12-10 16:08:59 +0200

    Fix verification modal test

--- front/cypress/e2e/auth/verification_modal.cy.ts
+++ front/cypress/e2e/auth/verification_modal.cy.ts
@@ -50,8 +50,9 @@ describe('Verification modal', () => {
       cy.visit('/projects/verified-charlie-poeple-project');
       cy.get('.e2e-idea-button').first().find('button').should('exist');
       cy.get('.e2e-idea-button').first().find('button').click({ force: true });
-      cy.get('#e2e-goto-signup').click();
+
       // email/password sign up step
+      cy.get('#e2e-goto-signup').click();
       cy.get('#e2e-sign-up-email-password-container');
       const firstName = randomString();
       const lastName = randomString();
@@ -100,6 +101,7 @@ describe('Verification modal', () => {
       cy.get('.e2e-idea-button').first().find('button').click({ force: true });
 
       // email/password sign up step
+      cy.get('#e2e-goto-signup').click();
       cy.get('#e2e-sign-up-email-password-container');
       const firstName = randomString();
       const lastName = randomString();
