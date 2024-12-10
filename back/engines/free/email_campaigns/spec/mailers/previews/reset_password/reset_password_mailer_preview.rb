aeba28782ece23bb0b5346d2c3735c8e4ba1015f
Adrien Dessy <adrien@citizenlab.co>
2024-12-10 23:57:27 +0100

    [TAN-3197] Fix mailer preview filenames

--- back/engines/free/email_campaigns/spec/mailers/previews/reset_password/reset_password_mailer_preview.rb
+++ back/engines/free/email_campaigns/spec/mailers/previews/reset_password/reset_password_mailer_preview.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-class ResetPasswordMailerPreview < ActionMailer::Preview
-  include EmailCampaigns::MailerPreviewRecipient
+module ResetPassword
+  class ResetPasswordMailerPreview < ActionMailer::Preview
+    include EmailCampaigns::MailerPreviewRecipient
 
-  def send_confirmation_code
-    ::ResetPasswordMailer.with(user: recipient_user, password_reset_url: 'https://demo.stg.govocal.com').send_reset_password
+    def send_confirmation_code
+      ::ResetPasswordMailer.with(user: recipient_user, password_reset_url: 'https://demo.stg.govocal.com').send_reset_password
+    end
   end
 end
