46be1aea30666b8f1023c20f63803f23a31cb1b6
Simon Tharby <simontharby@gmail.com>
2024-12-10 14:56:16 +0000

    [TAN-2042] Remove remove_not_allowed_parents from BE

--- back/spec/services/admin_publications_filtering_service_spec.rb
+++ back/spec/services/admin_publications_filtering_service_spec.rb
@@ -33,29 +33,6 @@ describe AdminPublicationsFilteringService do
     end
   end
 
-  context 'when a normal user searching from the home page' do
-    let(:options) { { depth: 0, remove_not_allowed_parents: true, publication_statuses: %w[archived published] } }
-    let(:base_scope) { Pundit.policy_scope(create(:user), AdminPublication.includes(:parent)) }
-
-    include_examples 'when a user searching from the home page'
-
-    it 'does not include parents when the user has no access to its children' do
-      expect(result.ids).not_to include(*tree_mock.admin_only_parents.ids)
-    end
-  end
-
-  context 'when an admin searching from the home page' do
-    let(:options) { { depth: '0', remove_not_allowed_parents: 'true', publication_statuses: %w[archived published] } }
-    let(:base_scope) { Pundit.policy_scope(create(:admin), AdminPublication.includes(:parent)) }
-
-    include_examples 'when a user searching from the home page'
-
-    it 'does include parents when the admin has access to its children' do
-      # we use not_draft because we filter out draft projects by publication_statuses param
-      expect(result.ids).to include(*tree_mock.admin_only_parents.not_draft.ids)
-    end
-  end
-
   context 'when an admin searching from the admin dashboard' do
     let(:base_scope) { Pundit.policy_scope(create(:admin), AdminPublication.includes(:parent)) }
     let(:options) { {} }
