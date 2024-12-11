07730f7efffe95c32b802aab37a982e0fccee0c3
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-12-11 20:38:37 -0300

    Flesh out loading logic

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/Areas/index.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/Areas/index.tsx
@@ -3,8 +3,11 @@ import React from 'react';
 import { Multiloc } from 'typings';
 
 import useAreas from 'api/areas/useAreas';
+import useProjectsMini from 'api/projects_mini/useProjectsMini';
 
 import useLocalize from 'hooks/useLocalize';
+
+import EmptyState from '../_shared/EmptyState';
 
 import AreaSelection from './AreaSelection';
 import messages from './messages';
@@ -16,24 +19,43 @@ interface Props {
 
 const Areas = ({ titleMultiloc }: Props) => {
   const localize = useLocalize();
-  const { data: areas, isLoading } = useAreas({
+  const { data: areas } = useAreas({
     sort: 'projects_count',
   });
 
-  if (isLoading) {
-    return null; // TODO?
-  }
+  const followedAreas =
+    areas?.data
+      .filter((area) => !!area.relationships.user_follower.data?.id)
+      .map((area) => area.id) ?? [];
 
-  if (!areas) return null;
+  const hasFollowPreferences = followedAreas.length > 0;
 
-  const hasFollowPreferences = areas.data.some(
-    (area) => !!area.relationships.user_follower.data?.id
+  const { data, hasNextPage, fetchNextPage } = useProjectsMini(
+    {
+      endpoint: 'for_areas',
+      areas: followedAreas,
+    },
+    {
+      enabled: hasFollowPreferences,
+    }
   );
+
+  const projects = data?.pages.map((page) => page.data).flat();
 
   const title = localize(titleMultiloc);
 
+  if (!areas) return null; // LOADING
+
   if (!hasFollowPreferences) {
     return <AreaSelection title={title} areas={areas} />;
+  }
+
+  if (!projects) return null; // LOADING
+
+  if (projects.length === 0) {
+    return (
+      <EmptyState titleMultiloc={titleMultiloc} explanation={messages.noData} />
+    );
   }
 
   // Render the carrousel
--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/Areas/messages.ts
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/Areas/messages.ts
@@ -13,4 +13,9 @@ export default defineMessages({
     id: 'front.app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.Widgets.Areas.selectYourX',
     defaultMessage: 'Select your {areaTerm}',
   },
+  noData: {
+    id: 'front.app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.Widgets.Areas.noData',
+    defaultMessage:
+      'This widget will only be shown to the user if there are projects associated with areas that they follow. If you see this message, it means that the areas that you (the admin) are following have no associated projects. This message will not be visible on the real homepage.',
+  },
 });
