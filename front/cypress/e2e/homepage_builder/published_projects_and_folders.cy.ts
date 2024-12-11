c395af9fa6330f27f5a66d56292616985b81fdf0
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-12-11 15:46:26 -0300

    Also add test for published projects and folders

---
+++ front/cypress/e2e/homepage_builder/published_projects_and_folders.cy.ts
@@ -0,0 +1,47 @@
+describe('Published projects and folders widget', () => {
+  it('can be added', () => {
+    // Go to the homepage builder
+    cy.setAdminLoginCookie();
+    cy.visit('en/admin/pages-menu/homepage-builder');
+
+    // Drag in widget
+    cy.get('#e2e-draggable-published').dragAndDrop(
+      '#e2e-content-builder-frame',
+      {
+        position: 'inside',
+      }
+    );
+    cy.wait(1000);
+
+    // Ideally would have liked to test the publication select,
+    // but it's impossible to figure out how to test react-select.
+    // So skipping for now
+
+    // Save
+    cy.get('#e2e-content-builder-topbar-save').click();
+
+    // Check if widget is displayed in homepage
+    cy.goToLandingPage();
+    cy.get('.e2e-published-projects-and-folders').should('exist');
+
+    // Delete widget again
+    cy.setAdminLoginCookie();
+    cy.visit('en/admin/pages-menu/homepage-builder');
+    cy.get('.e2e-published-projects-and-folders')
+      .first()
+      .parent()
+      .click({ force: true });
+
+    cy.get('#e2e-delete-button').click();
+
+    // Save
+    cy.get('#e2e-content-builder-topbar-save').click();
+    cy.wait(1000);
+
+    // Make sure it's not on homepage
+    cy.goToLandingPage();
+    cy.reload();
+    cy.wait(4000);
+    cy.get('.e2e-published-projects-and-folders').should('not.exist');
+  });
+});
