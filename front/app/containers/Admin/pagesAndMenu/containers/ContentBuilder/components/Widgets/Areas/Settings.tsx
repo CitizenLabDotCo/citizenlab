e0111475543b926c5d30dccc25d738b3a02ce0e4
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-12-11 16:00:21 -0300

    Add settings

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/Areas/Settings.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/Areas/Settings.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import { Box } from '@citizenlab/cl2-component-library';
+
+import TitleMultilocInput from '../_shared/TitleMultilocInput';
+
 const Settings = () => {
-  return <></>;
+  return (
+    <Box my="20px">
+      <TitleMultilocInput name="areas_title" />
+    </Box>
+  );
 };
 
 export default Settings;
