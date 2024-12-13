e4acfaf365a053c19d03f35b6b6fd447937a3a80
Simon Tharby <simontharby@gmail.com>
2024-12-13 19:34:24 +0000

    [TAN-22341] Order by count DESC

--- back/app/controllers/web_api/v1/areas_controller.rb
+++ back/app/controllers/web_api/v1/areas_controller.rb
@@ -99,6 +99,7 @@ class WebApi::V1::AreasController < ApplicationController
       .select(
         "areas.id, areas.title_multiloc, COUNT(DISTINCT filtered_projects.id) + #{all_areas_project_count} AS count"
       )
+      .order('count DESC')
       .map { |record| { id: record.id, title_multiloc: record.title_multiloc, count: record.count } }
 
     render json: raw_json({ counts: counts })
