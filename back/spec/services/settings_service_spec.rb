a36da8d5b31342b021fbfd17dc9c6f2c37ffb2a7
jamesspeake <james@speake.org>
2024-12-16 14:53:06 +0000

    [TAN-3350] Moved verification services into core

--- back/spec/services/settings_service_spec.rb
+++ back/spec/services/settings_service_spec.rb
@@ -227,4 +227,61 @@ describe SettingsService do
       expect(ss.format_for_front_end(settings, schema)).to eq expected_settings
     end
   end
+
+  describe 'disable_verification_if_no_methods_enabled' do
+    it 'disables verification if there are no methods enabled' do
+      settings = {
+        'verification' => {
+          'allowed' => true,
+          'enabled' => true
+        }
+      }
+
+      updated_settings = ss.send(:disable_verification_if_no_methods_enabled, settings)
+      expect(updated_settings['verification']['enabled']).to be false
+    end
+
+    it 'disables verification if all methods are hidden from the profile' do
+      settings = {
+        'verification' => {
+          'allowed' => true,
+          'enabled' => true,
+          'verification_methods' => [
+            {
+              'name' => 'nemlog_in',
+              'hide_from_profile' => true
+            },
+            {
+              'name' => 'keycloak',
+              'hide_from_profile' => true
+            }
+          ]
+        }
+      }
+
+      updated_settings = ss.send(:disable_verification_if_no_methods_enabled, settings)
+      expect(updated_settings['verification']['enabled']).to be false
+    end
+
+    it 'does not disable verification if at least one method is NOT hidden from the profile' do
+      settings = {
+        'verification' => {
+          'allowed' => true,
+          'enabled' => true,
+          'verification_methods' => [
+            {
+              'name' => 'nemlog_in'
+            },
+            {
+              'name' => 'keycloak',
+              'hide_in_profile' => true
+            }
+          ]
+        }
+      }
+
+      updated_settings = ss.send(:disable_verification_if_no_methods_enabled, settings)
+      expect(updated_settings['verification']['enabled']).to be true
+    end
+  end
 end
