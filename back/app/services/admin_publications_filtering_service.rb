50aed508d00a280aed297085d2548870d1d8476b
Simon Tharby <simontharby@gmail.com>
2024-12-16 15:23:59 +0000

    [TAN-3370] Revert BE changes

--- back/app/services/admin_publications_filtering_service.rb
+++ back/app/services/admin_publications_filtering_service.rb
@@ -20,6 +20,21 @@ class AdminPublicationsFilteringService
     scope.where(publication_type: Project.name)
   end
 
+  # This filter removes AdminPublications that represent folders,
+  # which contain *only* projects which should not be visible to the current user.
+  add_filter('remove_not_allowed_parents') do |scope, options|
+    next scope unless ['true', true, '1'].include? options[:remove_not_allowed_parents]
+
+    # We remove parents that have only draft projects
+    parents_with_visible_children = scope.where(id: scope.not_draft.filter_map(&:parent_id).uniq)
+    parents_without_any_children  = scope.where(children_allowed: true, children_count: 0)
+    non_parents                   = scope.where(children_allowed: false)
+
+    parents_with_visible_children
+      .or(parents_without_any_children)
+      .or(non_parents)
+  end
+
   add_filter('by_publication_status') do |scope, options|
     publication_status = options[:publication_statuses]
 
