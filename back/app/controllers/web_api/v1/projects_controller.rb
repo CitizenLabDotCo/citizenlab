1190c5a49aea3019f7530011e5072400aef41576
EdwinKato <katedwin88@gmail.com>
2024-12-18 11:49:06 +0300

    Update save_project to set folder_id only if provided in request

--- back/app/controllers/web_api/v1/projects_controller.rb
+++ back/app/controllers/web_api/v1/projects_controller.rb
@@ -319,8 +319,10 @@ class WebApi::V1::ProjectsController < ApplicationController
   end
 
   def save_project(project)
-    # Preserve folder_id unless explicitly provided in params
-    project.folder_id = params.dig(:project, :folder_id) if params.dig(:project, :folder_id).present?
+    # Update folder_id only if it is provided in the request (even if it's nil)
+    if params[:project].key?(:folder_id)
+      project.folder_id = params.dig(:project, :folder_id)
+    end
 
     ActiveRecord::Base.transaction do
       project.save.tap do |saved|
