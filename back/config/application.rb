7a16534511de98517fd1fb02e695336c9a30557c
Adrien Dessy <adrien@citizenlab.co>
2024-12-18 17:30:38 +0100

    [TAN-3184] Eliminate new_framework_defaults_7_0.rb

--- back/config/application.rb
+++ back/config/application.rb
@@ -32,6 +32,9 @@ module Cl2Back
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # TODO: Delete the following settings once we move to config.load_defaults 7.0
+    config.active_support.disable_to_s_conversion = true
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
