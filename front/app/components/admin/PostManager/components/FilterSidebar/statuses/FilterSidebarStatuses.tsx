c93edb63098abe499644628cc29d25ab726ea55a
brentguf <brentguf@users.noreply.github.com>
2024-12-20 14:09:08 +0100

    Replace semantic Divider

--- front/app/components/admin/PostManager/components/FilterSidebar/statuses/FilterSidebarStatuses.tsx
+++ front/app/components/admin/PostManager/components/FilterSidebar/statuses/FilterSidebarStatuses.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import { Box, Text } from '@citizenlab/cl2-component-library';
-import { Menu, Divider } from 'semantic-ui-react';
+import { Menu } from 'semantic-ui-react';
 
 import { IIdeaStatusData } from 'api/idea_statuses/types';
 import useAuthUser from 'api/me/useAuthUser';
 
+import Divider from 'components/admin/Divider';
 import Button from 'components/UI/Button';
 
 import { FormattedMessage } from 'utils/cl-intl';
