79f28b5da6cdb281d821994dc780590bc0626cdc
Sébastien Hoorens <sebastien@citizenlab.co>
2024-12-24 12:11:20 +0100

    delete deprecated notifications

---
+++ back/engines/commercial/multi_tenancy/spec/tasks/fix_notifications_task_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'fix_existing_tenants:remove_deprecated_notifications rake task' do
+  before { load_rake_tasks_if_not_loaded }
+
+  after { Rake::Task['fix_existing_tenants:remove_deprecated_notifications'].reenable }
+
+  it 'deletes notifications for deprecated types' do
+    create_list(:comment_on_idea_you_follow, 2)
+    deprecated = create_list(:comment_on_your_comment, 2).first
+    deprecated.update_columns(type: 'Notifications::InitiativeAssignedToYou')
+
+    expect { Rake::Task['fix_existing_tenants:remove_deprecated_notifications'].invoke }.to change { Notification.count }.by(-1)
+    expect(Notification.pluck(:type).uniq).to match_array ['Notifications::CommentOnIdeaYouFollow', 'Notifications::CommentOnYourComment']
+  end
+end
