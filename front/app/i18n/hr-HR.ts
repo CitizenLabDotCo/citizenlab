6f65d008f68dd2df083f108ab4f263dbb8d4bdca
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-12-27 10:57:19 -0300

    Remove react-datepicker dependency

--- front/app/i18n/hr-HR.ts
+++ front/app/i18n/hr-HR.ts
@@ -1,11 +1,9 @@
 import hr from 'date-fns/locale/hr';
-import { registerLocale } from 'react-datepicker';
 
 import { addLocale } from 'components/admin/DatePickers/_shared/locales';
 
 import { formatTranslationMessages } from './';
 
-registerLocale('hr-HR', hr);
 addLocale('hr-HR', hr);
 const hrHRAdminTranslationMessages = require('translations/admin/hr-HR.json');
 const hrHRTranslationMessages = require('translations/hr-HR.json');
