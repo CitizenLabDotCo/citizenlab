cd8d803d6ee67727ce629b22aa62c02d73070e51
Sébastien Hoorens <sebastien@citizenlab.co>
2024-12-30 18:07:17 +0100

    check if this works

--- back/db/migrate/20241226093506_remove_initiatives_from_comments.rb
+++ back/db/migrate/20241226093506_remove_initiatives_from_comments.rb
@@ -4,7 +4,7 @@ class RemoveInitiativesFromComments < ActiveRecord::Migration[7.0]
       DELETE FROM comments
       WHERE post_type IS NOT NULL AND post_type != 'Idea';
     SQL
-    # remove_column :comments, :post_type, :string # TODO: Remove after merging analytics PR
+    remove_column :comments, :post_type, :string # TODO: Check if this works
     rename_column :comments, :post_id, :idea_id
     add_foreign_key :comments, :ideas, column: :idea_id
 
