90af3939829aa656134c0c19141770363729020c
brentguf <brentguf@users.noreply.github.com>
2025-01-02 10:03:14 +0100

    Fix eslint-disable-next-line violations

--- front/app/containers/IdeasShow/components/RightColumnDesktop/index.tsx
+++ front/app/containers/IdeasShow/components/RightColumnDesktop/index.tsx
@@ -52,15 +52,12 @@ const RightColumnDesktop = ({
   const phase = getCurrentPhase(phases?.data);
   const votingConfig = getVotingMethodConfig(phase?.attributes.voting_method);
 
-  const ideaIsInParticipationContext =
-    // TODO: Fix this the next time the file is edited.
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    phase && idea ? isIdeaInParticipationContext(idea, phase) : undefined;
+  const ideaIsInParticipationContext = phase
+    ? isIdeaInParticipationContext(idea, phase)
+    : undefined;
 
   const commentingEnabled =
-    // TODO: Fix this the next time the file is edited.
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    !!idea?.data.attributes.action_descriptors.commenting_idea.enabled;
+    idea.data.attributes.action_descriptors.commenting_idea.enabled;
 
   const participationMethod = phase?.attributes.participation_method;
 
@@ -103,9 +100,7 @@ const RightColumnDesktop = ({
                 <Divider />
               </>
             )}
-            {/* TODO: Fix this the next time the file is edited. */}
-            {/* eslint-disable-next-line @typescript-eslint/no-unnecessary-condition */}
-            {phase && ideaIsInParticipationContext && votingConfig && (
+            {ideaIsInParticipationContext && votingConfig && (
               <Box pb="24px" mb="24px" borderBottom="solid 1px #ccc">
                 {votingConfig.getIdeaPageVoteInput({
                   ideaId,
@@ -124,15 +119,8 @@ const RightColumnDesktop = ({
                 followableType="ideas"
                 followableId={ideaId}
                 followersCount={idea.data.attributes.followers_count}
-                // TODO: Fix this the next time the file is edited.
-                // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-                followerId={idea.data.relationships.user_follower?.data?.id}
+                followerId={idea.data.relationships.user_follower.data?.id}
                 toolTipType="input"
-                buttonStyle={
-                  participationMethod === 'proposals'
-                    ? 'secondary-outlined'
-                    : 'primary'
-                }
               />
             </Box>
           </Box>
