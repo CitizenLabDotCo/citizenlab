ffbe04d25ac9afe754e43d8020ab6d451b65bb9c
Sébastien Hoorens <sebastien@citizenlab.co>
2025-01-02 12:31:00 +0100

    delete initiative models

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -135,8 +135,6 @@ class User < ApplicationRecord
 
   has_many :ideas, -> { order(:project_id) }, foreign_key: :author_id, dependent: :nullify
   has_many :idea_imports, class_name: 'BulkImportIdeas::IdeaImport', foreign_key: :import_user_id, dependent: :nullify
-  has_many :initiatives, foreign_key: :author_id, dependent: :nullify
-  has_many :assigned_initiatives, class_name: 'Initiative', foreign_key: :assignee_id, dependent: :nullify
   has_many :manual_votes_last_updated_ideas, class_name: 'Idea', foreign_key: :manual_votes_last_updated_by_id, dependent: :nullify
   has_many :manual_voters_last_updated_phases, class_name: 'Phase', foreign_key: :manual_voters_last_updated_by_id, dependent: :nullify
   has_many :comments, foreign_key: :author_id, dependent: :nullify
@@ -146,7 +144,6 @@ class User < ApplicationRecord
   has_many :event_attendances, -> { order(:event_id) }, class_name: 'Events::Attendance', foreign_key: :attendee_id, dependent: :destroy
   has_many :attended_events, through: :event_attendances, source: :event
   has_many :follows, -> { order(:followable_id) }, class_name: 'Follower', dependent: :destroy
-  has_many :cosponsors_initiatives, dependent: :destroy
   has_many :cosponsorships, dependent: :destroy
   has_many :cosponsored_ideas, through: :cosponsorships, source: :idea
 
@@ -161,7 +158,6 @@ class User < ApplicationRecord
   has_many :campaign_email_commands, class_name: 'EmailCampaigns::CampaignEmailCommand', foreign_key: :recipient_id, dependent: :destroy
   has_many :baskets, -> { order(:phase_id) }
   before_destroy :destroy_baskets
-  has_many :initiative_status_changes, dependent: :nullify
 
   has_many :requested_project_reviews, class_name: 'ProjectReview', foreign_key: :requester_id, dependent: :nullify
   has_many :assigned_project_reviews, class_name: 'ProjectReview', foreign_key: :reviewer_id, dependent: :nullify
