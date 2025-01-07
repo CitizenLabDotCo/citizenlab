f571bded9261930319007396e69d25c38ab76403
brentguf <brentguf@users.noreply.github.com>
2025-01-07 11:00:11 +0100

    Basic story

---
+++ front/app/component-library/components/Divider/Divider.stories.tsx
@@ -0,0 +1,15 @@
+import Divider from '.';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Components/Divider',
+  component: Divider,
+} satisfies Meta<typeof Divider>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const IdeaIcon: Story = {
+  args: {},
+};
