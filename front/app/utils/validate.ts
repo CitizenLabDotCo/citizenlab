0facec06f37eff14e1a037c6563765574257a2e1
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-01-07 17:31:36 -0300

    Fix broken unit test

--- front/app/utils/validate.ts
+++ front/app/utils/validate.ts
@@ -8,5 +8,5 @@ const validUrlRegex =
   /(http(s)?:\/\/)(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,64}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g;
 
 export const isValidUrl = (url: string) => {
-  return url.match(validUrlRegex);
+  return !!url.match(validUrlRegex);
 };
