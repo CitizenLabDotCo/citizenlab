70202d12b0609f4490e8ecd016c711ef8ee5b911
brentguf <brentguf@users.noreply.github.com>
2025-01-07 16:13:24 +0100

    ScreeningStatusFilterItem init

---
+++ front/app/components/admin/PostManager/components/FilterSidebar/statuses/ScreeningStatusFilterItem.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {}
+
+const ScreeningStatusFilterItem = ({}: Props) => {
+  return <div>Hello, world!</div>;
+};
+
+export default ScreeningStatusFilterItem;
