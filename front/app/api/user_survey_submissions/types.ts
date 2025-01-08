2f0698a1545b7bbf2ec9f8d8295756b171e916d1
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-01-08 16:53:57 -0300

    Add hook

---
+++ front/app/api/user_survey_submissions/types.ts
@@ -0,0 +1,7 @@
+import { Keys } from 'utils/cl-react-query/types';
+
+import userSurveySubmissionsKeys from './keys';
+
+export type UserSurveySubmissionsKeys = Keys<typeof userSurveySubmissionsKeys>;
+
+export type Response = any;
