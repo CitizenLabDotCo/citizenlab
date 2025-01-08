73a9dc47be6282625eddf6f23c546c0cb78131a6
EdwinKato <katedwin88@gmail.com>
2025-01-08 15:55:45 +0300

    Save logic options for multiselect fields

--- front/app/components/FormBuilder/edit/index.tsx
+++ front/app/components/FormBuilder/edit/index.tsx
@@ -193,7 +193,9 @@ const FormEdit = ({
         ...(field.input_type === 'page' && {
           temp_id: field.temp_id,
         }),
-        ...(['linear_scale', 'select', 'page'].includes(field.input_type)
+        ...(['multiselect', 'linear_scale', 'select', 'page'].includes(
+          field.input_type
+        )
           ? {
               logic: field.logic,
             }
