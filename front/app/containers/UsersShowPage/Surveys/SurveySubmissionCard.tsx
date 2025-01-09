b3a4fa98c1d93c180bb286f1be5dd9083cd27ec1
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-01-09 17:36:00 -0300

    _

--- front/app/containers/UsersShowPage/Surveys/SurveySubmissionCard.tsx
+++ front/app/containers/UsersShowPage/Surveys/SurveySubmissionCard.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { Box, Button, Title, colors } from '@citizenlab/cl2-component-library';
 
 import { IIdeaData } from 'api/ideas/types';
+import useProjectById from 'api/projects/useProjectById';
+
+import useLocalize from 'hooks/useLocalize';
 
 import { downloadSurveySubmission } from './utils';
 
@@ -11,9 +14,14 @@ interface Props {
 }
 
 const SurveySubmissionCard = ({ idea }: Props) => {
+  const { data: project } = useProjectById(idea.relationships.project.data.id);
+  const localize = useLocalize();
+
   const handleClick = () => {
     downloadSurveySubmission(idea.id);
   };
+
+  if (!project) return;
 
   return (
     <Box
@@ -25,7 +33,7 @@ const SurveySubmissionCard = ({ idea }: Props) => {
       alignItems="flex-start"
     >
       <Title variant="h2" mt="0">
-        Card title
+        {localize(project.data.attributes.title_multiloc)}
       </Title>
       <Button w="auto" onClick={handleClick}>
         Click here to download
