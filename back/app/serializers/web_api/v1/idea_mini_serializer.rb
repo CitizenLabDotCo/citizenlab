00b8cc45a4a54e6b951910272cbe6ae7ae138e17
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-01-13 10:00:21 -0300

    Just use normal project serializer instead

--- back/app/serializers/web_api/v1/idea_mini_serializer.rb
+++ back/app/serializers/web_api/v1/idea_mini_serializer.rb
@@ -5,5 +5,5 @@ class WebApi::V1::IdeaMiniSerializer < WebApi::V1::BaseSerializer
   attributes :slug
 
   has_one :creation_phase, serializer: WebApi::V1::PhaseSerializer
-  has_one :project, serializer: WebApi::V1::ProjectMiniSerializer
+  has_one :project, serializer: WebApi::V1::ProjectSerializer
 end
--- front/app/containers/UsersShowPage/Surveys/SurveySubmissionCard/index.tsx
+++ front/app/containers/UsersShowPage/Surveys/SurveySubmissionCard/index.tsx
@@ -26,6 +26,7 @@ const SurveySubmissionCard = ({ ideaMini }: Props) => {
   const { data: phase } = usePhase(
     ideaMini.relationships.creation_phase.data.id
   );
+
   const smallerThanTablet = useBreakpoint('tablet');
   const localize = useLocalize();
 
