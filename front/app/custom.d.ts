8968cdd59f05a3232d6e384ab5e0596d99363328
EdwinKato <katedwin88@gmail.com>
2025-01-14 20:37:07 +0300

    Remove custom TypeScript declaration for Moment.js locale files

--- front/app/custom.d.ts
+++ front/app/custom.d.ts
@@ -12,11 +12,3 @@ declare module '*.jpg' {
   const content: string
   export default content
 }
-
-// TypeScript does not have built-in type definitions for Moment.js locale files under 'moment/dist/locale/*'.
-// This custom declaration is required to avoid "Cannot find module" errors when dynamically importing locales.
-// By declaring these modules with a generic type (`any`), we inform TypeScript that these files exist and can be safely imported.
-declare module 'moment/dist/locale/*' {
-  const locale: any;
-  export default locale;
-}
