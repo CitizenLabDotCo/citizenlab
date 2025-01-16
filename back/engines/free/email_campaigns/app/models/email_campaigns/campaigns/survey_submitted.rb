e84d2e604b8db20dfecf54b4eb09ba84141c323c
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-01-16 09:58:44 -0300

    Create survey_submitted email campaign

---
+++ back/engines/free/email_campaigns/app/models/email_campaigns/campaigns/survey_submitted.rb
@@ -0,0 +1,52 @@
+module EmailCampaigns
+  class Campaigns::SurveySubmitted < Campaign
+    include Consentable
+    include ActivityTriggerable
+    include RecipientConfigurable
+    include Disableable
+    include Trackable
+    include LifecycleStageRestrictable
+    allow_lifecycle_stages only: %w[trial active]
+
+    recipient_filter :filter_recipient
+
+    def mailer_class
+      IdeaPublishedMailer
+    end
+
+    def activity_triggers
+      { 'Idea' => { 'published' => true } }
+    end
+
+    def filter_recipient(users_scope, activity:, time: nil)
+      users_scope.where(id: activity.item.author_id)
+    end
+
+    def self.recipient_role_multiloc_key
+      'email_campaigns.admin_labels.recipient_role.registered_users'
+    end
+
+    def self.recipient_segment_multiloc_key
+      'email_campaigns.admin_labels.recipient_segment.user_who_published_the_input'
+    end
+
+    def self.content_type_multiloc_key
+      'email_campaigns.admin_labels.content_type.inputs'
+    end
+
+    def self.trigger_multiloc_key
+      'email_campaigns.admin_labels.trigger.input_is_published'
+    end
+
+    def generate_commands(recipient:, activity:)
+      idea = activity.item
+      return [] if idea.participation_method_on_creation.supports_public_visibility?
+
+      [{
+        event_payload: {
+          idea_id: idea.id
+        }
+      }]
+    end
+  end
+end
