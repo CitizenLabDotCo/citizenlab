fb9620a1b5a23f554a8c5cacae909a845c02d855
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-01-16 15:09:02 -0300

    survey_submitted_spec (WIP)

---
+++ back/engines/free/email_campaigns/spec/models/email_campaigns/campaigns/survey_submitted_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe EmailCampaigns::Campaigns::SurveySubmitted do
+  describe 'SurveySubmitted Campaign default factory' do
+    it 'is valid' do
+      expect(build(:survey_submitted_campaign)).to be_valid
+    end
+  end
+
+  describe '#generate_commands' do
+    before { create(:idea_status_proposed) }
+
+    let(:campaign) { create(:survey_submitted_campaign) }
+    let(:user) { create(:user, slug: 'user-userson') }
+    let(:project) { create(:single_phase_native_survey_project) }
+    let(:idea) { create(:idea, author: user, project: project, creation_phase: project.phases.first) }
+    let(:activity) { create(:activity, item: idea, action: 'published', user: user) }
+
+    it 'generates a command with the desired payload and tracked content' do
+      command = campaign.generate_commands(recipient: user, activity: activity).first
+
+      expect(command[:event_payload]).to include(
+        idea_id: idea.id,
+        project_title_multiloc: project.title_multiloc,
+        profile_url: 'http://example.org/profile/user-userson/surveys'
+      )
+    end
+
+    # describe do
+    #   let(:idea) do
+    #     project = create(:single_phase_native_survey_project)
+    #     create(:idea, author: user, project: project, creation_phase: project.phases.first)
+    #   end
+
+    #   it "doesn't get triggered for a native survey response" do
+    #     commands = campaign.generate_commands recipient: user, activity: activity
+
+    #     expect(commands).to be_empty
+    #   end
+    # end
+  end
+end
