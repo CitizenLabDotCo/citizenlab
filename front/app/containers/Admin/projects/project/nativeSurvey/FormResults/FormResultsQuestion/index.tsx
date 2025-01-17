33cda59528ed572fe756322e619285179c0eadc3
Amanda Anderson <anderson_amanda@mail.com>
2025-01-17 12:46:46 +0100

    Clean up FormResultsQuestion component

--- front/app/containers/Admin/projects/project/nativeSurvey/FormResults/FormResultsQuestion/index.tsx
+++ front/app/containers/Admin/projects/project/nativeSurvey/FormResults/FormResultsQuestion/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Box, Title, colors } from '@citizenlab/cl2-component-library';
 import { snakeCase } from 'lodash-es';
 
+import { ICustomFieldInputType } from 'api/custom_fields/types';
 import { ResultUngrouped } from 'api/survey_results/types';
 
 import useLocalize from 'hooks/useLocalize';
@@ -19,6 +20,7 @@ import PolygonLocationQuestion from './MappingQuestions/PolygonLocationQuestion'
 import NumberQuestion from './NumberQuestion';
 import RankingQuestion from './RankingQuestion';
 import TextQuestion from './TextQuestion';
+import { determineAnswerType } from './utils';
 
 type FormResultsQuestionProps = {
   questionNumber: number;
@@ -37,7 +39,6 @@ const FormResultsQuestion = ({
 
   const {
     answers,
-    average_rankings,
     textResponses,
     pointResponses,
     lineResponses,
@@ -52,16 +53,8 @@ const FormResultsQuestion = ({
     files,
   } = result;
 
-  const isRanking = !!average_rankings;
-  const isMultipleChoiceAndHasAnswers = !!answers;
-  const hasTextResponses = textResponses && textResponses.length > 0;
-  const hasNumberResponses = numberResponses && numberResponses.length > 0;
-  const isPointAndHasAnswers =
-    inputType === 'point' && pointResponses && pointResponses.length > 0;
-  const isLineAndHasAnswers =
-    inputType === 'line' && lineResponses && lineResponses.length > 0;
-  const isPolygonAndHasAnswers =
-    inputType === 'polygon' && polygonResponses && polygonResponses.length > 0;
+  const hasAnswersOfType: ICustomFieldInputType | undefined =
+    determineAnswerType(result);
 
   return (
     <>
@@ -75,44 +68,77 @@ const FormResultsQuestion = ({
           totalSubmissions={totalSubmissions}
           totalResponses={questionResponseCount}
         />
-        {isRanking && <RankingQuestion result={result} />}
-        {isMultipleChoiceAndHasAnswers && (
-          <SurveyBars questionResult={result} colorScheme={COLOR_SCHEME} />
-        )}
-        {hasTextResponses && (
-          <TextQuestion
-            textResponses={textResponses}
-            customFieldId={customFieldId}
-            hasOtherResponses={isMultipleChoiceAndHasAnswers}
-          />
-        )}
-        {hasNumberResponses && (
-          <NumberQuestion numberResponses={numberResponses} />
-        )}
-        {isPointAndHasAnswers && (
-          <PointLocationQuestion
-            pointResponses={pointResponses}
-            mapConfigId={mapConfigId}
-            customFieldId={customFieldId}
-          />
-        )}
-        {isLineAndHasAnswers && (
-          <LineLocationQuestion
-            lineResponses={lineResponses}
-            mapConfigId={mapConfigId}
-            customFieldId={customFieldId}
-          />
-        )}
-        {isPolygonAndHasAnswers && (
-          <PolygonLocationQuestion
-            polygonResponses={polygonResponses}
-            mapConfigId={mapConfigId}
-            customFieldId={customFieldId}
-          />
-        )}
+
+        {(() => {
+          switch (hasAnswersOfType) {
+            case 'ranking':
+              return <RankingQuestion result={result} />;
+            case 'multiselect':
+              return (
+                <SurveyBars
+                  questionResult={result}
+                  colorScheme={COLOR_SCHEME}
+                />
+              );
+            case 'text':
+              if (textResponses) {
+                return (
+                  <TextQuestion
+                    textResponses={textResponses}
+                    customFieldId={customFieldId}
+                    hasOtherResponses={!!answers}
+                  />
+                );
+              }
+              return null;
+            case 'number': {
+              if (numberResponses) {
+                return <NumberQuestion numberResponses={numberResponses} />;
+              }
+              return null;
+            }
+            case 'point': {
+              if (pointResponses) {
+                return (
+                  <PointLocationQuestion
+                    pointResponses={pointResponses}
+                    mapConfigId={mapConfigId}
+                    customFieldId={customFieldId}
+                  />
+                );
+              }
+              return null;
+            }
+            case 'line': {
+              if (lineResponses) {
+                return (
+                  <LineLocationQuestion
+                    lineResponses={lineResponses}
+                    mapConfigId={mapConfigId}
+                    customFieldId={customFieldId}
+                  />
+                );
+              }
+              return null;
+            }
+            case 'polygon': {
+              if (polygonResponses) {
+                return (
+                  <PolygonLocationQuestion
+                    polygonResponses={polygonResponses}
+                    mapConfigId={mapConfigId}
+                    customFieldId={customFieldId}
+                  />
+                );
+              }
+              return null;
+            }
+            default:
+              return null;
+          }
+        })()}
+
         {files && files.length > 0 && (
-          // TODO: Fix this the next time the file is edited.
-          // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
           <Box display="flex" gap="24px" mt={answers ? '20px' : '0'} w="50%">
             <Box flex="1">
               <Files files={files} />
