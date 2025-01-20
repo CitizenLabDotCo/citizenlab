e39b11e93205f3a5b2e674273ed333edb614a140
Amanda Anderson <anderson_amanda@mail.com>
2025-01-20 09:37:12 +0100

    Move switch statement to own component

--- front/app/containers/Admin/projects/project/nativeSurvey/FormResults/FormResultsQuestion/index.tsx
+++ front/app/containers/Admin/projects/project/nativeSurvey/FormResults/FormResultsQuestion/index.tsx
@@ -1,34 +1,24 @@
 import React from 'react';
 
-import { Box, Title, colors } from '@citizenlab/cl2-component-library';
+import { Box, Title } from '@citizenlab/cl2-component-library';
 import { snakeCase } from 'lodash-es';
 
-import { ICustomFieldInputType } from 'api/custom_fields/types';
 import { ResultUngrouped } from 'api/survey_results/types';
 
 import useLocalize from 'hooks/useLocalize';
 
-import SurveyBars from 'components/admin/Graphs/SurveyBars';
 import T from 'components/T';
 
 import Files from '../Files';
 
+import FormResultQuestionValue from './components/FormResultsQuestionValue';
 import InputType from './InputType';
-import LineLocationQuestion from './MappingQuestions/LineLocationQuestion';
-import PointLocationQuestion from './MappingQuestions/PointLocationQuestion';
-import PolygonLocationQuestion from './MappingQuestions/PolygonLocationQuestion';
-import NumberQuestion from './NumberQuestion';
-import RankingQuestion from './RankingQuestion';
-import TextQuestion from './TextQuestion';
-import { determineAnswerType } from './utils';
 
 type FormResultsQuestionProps = {
   questionNumber: number;
   result: ResultUngrouped;
   totalSubmissions: number;
 };
-
-const COLOR_SCHEME = [colors.primary];
 
 const FormResultsQuestion = ({
   questionNumber,
@@ -39,22 +29,12 @@ const FormResultsQuestion = ({
 
   const {
     answers,
-    textResponses,
-    pointResponses,
-    lineResponses,
-    polygonResponses,
-    numberResponses,
     inputType,
     question,
     required,
     questionResponseCount,
-    customFieldId,
-    mapConfigId,
     files,
   } = result;
-
-  const hasAnswersOfType: ICustomFieldInputType | undefined =
-    determineAnswerType(result);
 
   return (
     <>
@@ -69,72 +49,7 @@ const FormResultsQuestion = ({
           totalResponses={questionResponseCount}
         />
 
-        {(() => {
-          switch (hasAnswersOfType) {
-            case 'ranking':
-              return <RankingQuestion result={result} />;
-            case 'multiselect':
-              return (
-                <SurveyBars
-                  questionResult={result}
-                  colorScheme={COLOR_SCHEME}
-                />
-              );
-            case 'text':
-              return (
-                textResponses && (
-                  <TextQuestion
-                    textResponses={textResponses}
-                    customFieldId={customFieldId}
-                    hasOtherResponses={!!answers}
-                  />
-                )
-              );
-
-            case 'number': {
-              return (
-                numberResponses && (
-                  <NumberQuestion numberResponses={numberResponses} />
-                )
-              );
-            }
-            case 'point': {
-              return (
-                pointResponses && (
-                  <PointLocationQuestion
-                    pointResponses={pointResponses}
-                    mapConfigId={mapConfigId}
-                    customFieldId={customFieldId}
-                  />
-                )
-              );
-            }
-            case 'line': {
-              return (
-                lineResponses && (
-                  <LineLocationQuestion
-                    lineResponses={lineResponses}
-                    mapConfigId={mapConfigId}
-                    customFieldId={customFieldId}
-                  />
-                )
-              );
-            }
-            case 'polygon': {
-              return (
-                polygonResponses && (
-                  <PolygonLocationQuestion
-                    polygonResponses={polygonResponses}
-                    mapConfigId={mapConfigId}
-                    customFieldId={customFieldId}
-                  />
-                )
-              );
-            }
-            default:
-              return null;
-          }
-        })()}
+        <FormResultQuestionValue result={result} />
 
         {files && files.length > 0 && (
           <Box display="flex" gap="24px" mt={answers ? '20px' : '0'} w="50%">
