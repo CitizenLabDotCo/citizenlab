3fccf6106ac9f827a3f0d836684c176b4cf3c8c5
Adrien Dessy <adrien@citizenlab.co>
2025-01-20 11:36:02 +0100

    [TAN-3358] Set up the default behavior of Rails 7.1+ for AR encryption

--- back/config/initializers/new_framework_defaults_7_1.rb
+++ back/config/initializers/new_framework_defaults_7_1.rb
@@ -46,7 +46,7 @@ Rails.application.config.action_controller.allow_deprecated_parameters_hash_equa
 # 3. If you don't currently have data encrypted with Active Record encryption, you can disable this setting to
 # configure the default behavior starting 7.1+:
 #++
-# Rails.application.config.active_record.encryption.support_sha1_for_non_deterministic_encryption = false
+Rails.application.config.active_record.encryption.support_sha1_for_non_deterministic_encryption = false
 
 ###
 # No longer run after_commit callbacks on the first of multiple Active Record
