677aa71d7cc7ec5516bee6faa5ce3f435cc247cd
brentguf <brentguf@users.noreply.github.com>
2025-01-21 10:39:48 +0100

    Show status filter conditions for filters on resident pages (phase & all inputs pages)

--- front/app/components/IdeaCards/shared/Filters/StatusFilterBox.tsx
+++ front/app/components/IdeaCards/shared/Filters/StatusFilterBox.tsx
@@ -4,6 +4,7 @@ import { IdeaStatusParticipationMethod } from 'api/idea_statuses/types';
 import useIdeaStatuses from 'api/idea_statuses/useIdeaStatuses';
 import { IIdeasFilterCountsQueryParameters } from 'api/ideas_filter_counts/types';
 import useIdeasFilterCounts from 'api/ideas_filter_counts/useIdeasFilterCounts';
+import { IPhase } from 'api/phases/types';
 
 import StatusFilter from 'components/FilterBoxes/StatusFilter';
 
@@ -13,6 +14,8 @@ interface Props {
   onChange: (arg: string | null) => void;
   className?: string;
   participationMethod?: IdeaStatusParticipationMethod;
+  // Should be defined whenever this component is displayed within a project phase context.
+  phase: IPhase | undefined;
 }
 
 const StatusFilterBox = ({
@@ -21,9 +24,20 @@ const StatusFilterBox = ({
   className,
   ideaQueryParameters,
   participationMethod,
+  phase,
 }: Props) => {
+  /* 
+    On project phase pages. If the phase setting is undefined: the BE's default is to exclude the screening status when the prescreening_ideation feature is not enabled. If prescreening_ideation is enabled, the phase setting should be defined (either true or false) for new phases. For old phases, that were created before the feature was introduced, the setting is undefined. This will result in excludeScreeningStatus being true (!undefined).
+    
+    On the All inputs page, with no phase, we follow default BE behavior: show all statuses if the prescreening_ideation feature is enabled (similar to general input manager behavior). 
+  */
+  const excludeScreeningStatus = !phase?.data.attributes.prescreening_enabled;
+
   const { data: ideaStatuses } = useIdeaStatuses({
-    queryParams: { participation_method: participationMethod },
+    queryParams: {
+      participation_method: participationMethod,
+      exclude_screening_status: excludeScreeningStatus,
+    },
   });
   const { data: ideasFilterCounts } = useIdeasFilterCounts({
     ...ideaQueryParameters,
