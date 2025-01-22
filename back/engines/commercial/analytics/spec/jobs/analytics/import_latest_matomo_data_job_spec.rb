90c70c9966392e8ae12e6562b4b92d22515adafd
Simon Tharby <simontharby@gmail.com>
2025-01-22 11:43:30 +0000

    [TAN-3600] Various improvements to tests

--- back/engines/commercial/analytics/spec/jobs/analytics/import_latest_matomo_data_job_spec.rb
+++ back/engines/commercial/analytics/spec/jobs/analytics/import_latest_matomo_data_job_spec.rb
@@ -69,59 +69,44 @@ RSpec.describe Analytics::ImportLatestMatomoDataJob do
     end
   end
 
-  context 'when tenant has active lifecycle' do
-    it 'raises an error if the configured matomo site is the default one' do
-      stub_const('ENV', ENV.to_h.merge(
-        'DEFAULT_MATOMO_TENANT_SITE_ID' => AppConfiguration.instance.settings('matomo', 'tenant_site_id')
-      ))
-
-      expect(AppConfiguration.instance.settings.dig('core', 'lifecycle_stage')).to eq('active')
-      expect { described_class.perform_now(Tenant.current.id) }
-        .to raise_error(described_class::MatomoMisconfigurationError)
-    end
-  end
-
-  context 'when tenant has trial lifecycle' do
+  describe 'MatomoMisconfigurationError' do
     before do
-      AppConfiguration.instance.settings['core']['lifecycle_stage'] = 'trial'
-      AppConfiguration.instance.save!
-    end
-
-    after do
-      AppConfiguration.instance.settings['core']['lifecycle_stage'] = 'active'
-      AppConfiguration.instance.save!
-    end
-
-    it 'raises an error if the configured matomo site is the default one' do
       stub_const('ENV', ENV.to_h.merge(
-        'DEFAULT_MATOMO_TENANT_SITE_ID' => AppConfiguration.instance.settings('matomo', 'tenant_site_id')
-      ))
-
-      expect(AppConfiguration.instance.settings.dig('core', 'lifecycle_stage')).to eq('trial')
-      expect { described_class.perform_now(Tenant.current.id) }
-        .to raise_error(described_class::MatomoMisconfigurationError)
-    end
-  end
-
-  context 'when tenant has demo lifecycle' do
-    before do
-      AppConfiguration.instance.settings['core']['lifecycle_stage'] = 'demo'
-      AppConfiguration.instance.save!(validate: false)
+          'DEFAULT_MATOMO_TENANT_SITE_ID' => AppConfiguration.instance.settings('matomo', 'tenant_site_id')
+        ))  
     end
 
-    after do
-      AppConfiguration.instance.settings['core']['lifecycle_stage'] = 'active'
-      AppConfiguration.instance.save!(validate: false)
+    context 'when tenant has active lifecycle' do
+      it 'raises an error if the configured matomo site is the default one' do
+        expect { described_class.perform_now(Tenant.current.id) }
+          .to raise_error(described_class::MatomoMisconfigurationError)
+      end
     end
 
-    it 'does not raise an error if the configured matomo site is the default one' do
-      stub_const('ENV', ENV.to_h.merge(
-        'DEFAULT_MATOMO_TENANT_SITE_ID' => AppConfiguration.instance.settings('matomo', 'tenant_site_id')
-      ))
+    context 'when tenant has trial lifecycle' do
+      before do
+        AppConfiguration.instance.settings['core']['lifecycle_stage'] = 'trial'
+        AppConfiguration.instance.save!
+      end
 
-      expect(AppConfiguration.instance.settings.dig('core', 'lifecycle_stage')).to eq('demo')
-      expect { described_class.perform_now(Tenant.current.id) }
-        .not_to raise_error(described_class::MatomoMisconfigurationError)
+      it 'raises an error if the configured matomo site is the default one' do
+        expect { described_class.perform_now(Tenant.current.id) }
+          .to raise_error(described_class::MatomoMisconfigurationError)
+      end
+    end
+
+    context 'when tenant has demo lifecycle' do
+      before do
+        AppConfiguration.instance.settings['core']['lifecycle_stage'] = 'demo'
+        AppConfiguration.instance.save!(validate: false)
+      end
+
+      it 'does not raise an error if the configured matomo site is the default one' do
+        # We expect this error to be raised because the Matomo client is not configured.
+        # We shouldn't see the MatomoMisconfigurationError, which would be raised before this one.
+        expect { described_class.perform_now(Tenant.current.id) }
+          .to raise_error(Matomo::Client::MissingBaseUriError)
+      end
     end
   end
 end
