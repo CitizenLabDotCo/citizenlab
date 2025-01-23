ee39dc3c03aee636028d553a133f0e70f0abf33f
Simon Tharby <simontharby@gmail.com>
2025-01-23 12:23:38 +0000

    [TAN-2795] Remove comment

--- back/app/models/idea.rb
+++ back/app/models/idea.rb
@@ -284,8 +284,6 @@ class Idea < ApplicationRecord
   end
 
   def will_be_submitted?
-    # It would be better to foresee separate endpoints for publication,
-    # rather than relying on Rails dirty to detect publication.
     from, to = publication_status_change
     from != 'submitted' && to == 'submitted'
   end
