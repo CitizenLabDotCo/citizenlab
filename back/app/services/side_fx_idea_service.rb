33299892287880aed8f7df5ae74b6c40128c0c6b
Simon Tharby <simontharby@gmail.com>
2025-01-23 12:16:43 +0000

    [TAN-2795] Add tests

--- back/app/services/side_fx_idea_service.rb
+++ back/app/services/side_fx_idea_service.rb
@@ -37,7 +37,7 @@ class SideFxIdeaService
     @old_phase_ids = idea.phase_ids
     idea.body_multiloc = TextImageService.new.swap_data_images_multiloc(idea.body_multiloc, field: :body_multiloc, imageable: idea)
     idea.publication_status = 'published' if idea.submitted_or_published? && idea.idea_status&.public_post?
-    before_publish_or_submit idea, user if idea.will_be_published? || idea.will_be_submitted?
+    before_publish_or_submit idea, user if idea.will_be_submitted? || idea.will_be_published?
   end
 
   def after_update(idea, user)
