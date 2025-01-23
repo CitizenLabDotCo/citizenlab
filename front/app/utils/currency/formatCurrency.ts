e403e3c28ea56d6edd1bb7ffdc411128e3ad5218
brentguf <brentguf@users.noreply.github.com>
2025-01-23 15:02:43 +0100

    formatCurrency

---
+++ front/app/utils/currency/formatCurrency.ts
@@ -0,0 +1,18 @@
+import { SupportedLocale } from 'typings';
+
+import { TCurrency } from 'api/app_configuration/types';
+
+function formatCurrency(
+  locale: SupportedLocale,
+  currency: TCurrency,
+  amount: number,
+  { maximumFractionDigits = 0 }: { maximumFractionDigits?: number } = {}
+) {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency,
+    maximumFractionDigits,
+  }).format(amount);
+}
+
+export default formatCurrency;
