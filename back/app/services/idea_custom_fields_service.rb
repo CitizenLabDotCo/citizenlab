ad6a8305b5c70587793ea123525e010b9a922a37
Sébastien Hoorens <sebastien@citizenlab.co>
2025-01-23 16:33:10 +0100

    disable form sync for matrix fields (for now) and forgot to substitute geojson_supported_fields

--- back/app/services/idea_custom_fields_service.rb
+++ back/app/services/idea_custom_fields_service.rb
@@ -40,13 +40,13 @@ class IdeaCustomFieldsService
 
   # Used in the printable PDF export
   def printable_fields
-    ignore_field_types = %w[section page date files image_files point file_upload shapefile_upload topic_ids cosponsor_ids ranking]
+    ignore_field_types = %w[section page date files image_files point file_upload shapefile_upload topic_ids cosponsor_ids ranking matrix_linear_scale]
     fields = enabled_fields.reject { |field| ignore_field_types.include? field.input_type }
     insert_other_option_text_fields(fields)
   end
 
   def importable_fields
-    ignore_field_types = %w[page section date files image_files file_upload shapefile_upload point line polygon cosponsor_ids ranking]
+    ignore_field_types = %w[page section date files image_files file_upload shapefile_upload point line polygon cosponsor_ids ranking matrix_linear_scale]
     enabled_fields_with_other_options.reject { |field| ignore_field_types.include? field.input_type }
   end
 
