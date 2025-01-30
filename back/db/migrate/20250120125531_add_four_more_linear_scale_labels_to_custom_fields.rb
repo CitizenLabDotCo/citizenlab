a0c29ab7821e934521fcdb319e7519d5c1b450ca
EdwinKato <katedwin88@gmail.com>
2025-01-30 13:39:11 +0300

    Remove reversible migration logic for adding linear scale labels to custom fields

--- back/db/migrate/20250120125531_add_four_more_linear_scale_labels_to_custom_fields.rb
+++ back/db/migrate/20250120125531_add_four_more_linear_scale_labels_to_custom_fields.rb
@@ -8,20 +8,5 @@ class AddFourMoreLinearScaleLabelsToCustomFields < ActiveRecord::Migration[7.0]
     add_column :custom_fields, :linear_scale_label_9_multiloc, :jsonb, default: {}, null: false
     add_column :custom_fields, :linear_scale_label_10_multiloc, :jsonb, default: {}, null: false
     add_column :custom_fields, :linear_scale_label_11_multiloc, :jsonb, default: {}, null: false
-
-    reversible do |dir|
-      dir.up do
-        linear_scale_fields = StubCustomField.where(input_type: 'linear_scale')
-        linear_scale_fields.each do |field|
-          max_label = field.maximum_label_multiloc
-
-          (8..11).each do |index|
-            field.send(:"linear_scale_label_#{index}_multiloc=", max_label) if max_label.present?
-          end
-
-          field.save!
-        end
-      end
-    end
   end
 end
