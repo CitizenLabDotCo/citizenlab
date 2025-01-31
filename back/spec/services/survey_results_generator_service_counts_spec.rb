38ce4b9672da87c3ad208861bd38f9d9263ef064
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-01-30 22:09:09 -0300

    _

--- back/spec/services/survey_results_generator_service_counts_spec.rb
+++ back/spec/services/survey_results_generator_service_counts_spec.rb
@@ -191,4 +191,100 @@ describe 'change_counts_for_logic' do
     expect(results[0].keys).not_to include(:key, :questionViewedCount)
   end
 end
+
+describe 'change_counts_for_logic: question logic taking prescedence over page logic' do
+  # NOTE: Truncated results input object without unnecessary attributes for this method
+  let_it_be(:results_without_num_answered) do
+    [
+      {
+        inputType: 'page',
+        totalResponseCount: 5,
+        questionResponseCount: 4,
+        pageNumber: 1,
+        logic: { nextPageNumber: 3, numQuestionsSkipped: 1 },
+        questionViewedCount: 0,
+        key: 'page1'
+      },
+      {
+        inputType: 'select',
+        totalResponseCount: 5,
+        questionResponseCount: 4,
+        logic: {
+          answer: {
+            'option_1' => { id: 'ba98ad2c-668d-4e3f-8760-215c49f34dc1', nextPageNumber: 2, numQuestionsSkipped: 0 }
+          }
+        },
+        questionViewedCount: 0,
+        key: 'question_one',
+        totalPickCount: 4,
+        answers: [
+          { answer: 'option_2', count: 0 },
+          { answer: 'option_1', count: 4 },
+          { answer: nil, count: 1 }
+        ]
+      },
+      {
+        inputType: 'page',
+        totalResponseCount: 5,
+        questionResponseCount: 4,
+        pageNumber: 2,
+        logic: { nextPageNumber: 999, numQuestionsSkipped: 1 },
+        questionViewedCount: 0,
+        key: 'page2'
+      },
+      {
+        inputType: 'text',
+        totalResponseCount: 5,
+        questionResponseCount: 4,
+        pageNumber: nil,
+        logic: {},
+        questionViewedCount: 0,
+        key: 'question_two'
+      },
+      {
+        inputType: 'page',
+        totalResponseCount: 5,
+        questionResponseCount: 4,
+        pageNumber: 3,
+        logic: {},
+        questionViewedCount: 0,
+        key: 'page3'
+      },
+      {
+        inputType: 'text',
+        totalResponseCount: 5,
+        questionResponseCount: 4,
+        pageNumber: nil,
+        logic: {},
+        questionViewedCount: 0,
+        key: 'question_three'
+      },
+    ]
+  end
+
+  let_it_be(:survey_custom_field_responses) do
+    [
+      { 'question_one' => 'option_1', 'question_two' => 'Bla' },
+      { 'question_one' => 'option_1', 'question_two' => 'Bla' },
+      { 'question_one' => 'option_1', 'question_two' => 'Bla' },
+      { 'question_one' => 'option_1'},
+      { 'question_three' => 'Bla' }
+    ]
+  end
+
+  let(:results) { generator.send(:change_counts_for_logic, results_without_num_answered, survey_custom_field_responses) }
+
+  it 'changes the totalResponsesCount when fields are not shown through logic' do
+    expect(results_without_num_answered.pluck(:totalResponseCount)).to eq(
+      [5, 5, 5, 5, 5, 5] # Before
+    )
+    expect(results.pluck(:totalResponseCount)).to eq(
+      [5, 5, 4, 4, 1, 1] # After
+    )
+  end
+
+  it 'has the correct questionResponseCount' do
+
+  end
+end
 end
