a68ed485b4f1808aeb326ef9b07b05f471815c39
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-02-04 15:21:44 -0300

    Add comment to getPageVariant

--- front/app/components/Form/Components/Layouts/utils.ts
+++ front/app/components/Form/Components/Layouts/utils.ts
@@ -183,6 +183,15 @@ export function getFormCompletionPercentage(
   return percentage;
 }
 
+// There are broadly three variants of pages (based on their behavior):
+// 1. The page that comes after the submission, e.g. the "success" page
+// 2. The page where you have the submit button
+// 3. Any other page
+// Page variant 1 is significantly different from the other two.
+// Page variant 2 is exactly the same as 3, with the only different being
+// a different button ("Submit" instead of "Next").
+// The last page is always the 'after submission' page,
+// and the page before that is always the 'submission' page.
 export const getPageVariant = (currentStep: number, numberOfPages: number) => {
   if (currentStep === numberOfPages - 1) {
     return 'after-submission';
