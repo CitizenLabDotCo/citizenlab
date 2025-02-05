b47f8e9b8c4bbb3ea6832c441c6e3ee05c8b7f2a
jamesspeake <james@speake.org>
2025-02-05 14:49:27 +0000

    [TAN-3746] WIP fixing last test

--- back/app/services/survey_results_generator_service.rb
+++ back/app/services/survey_results_generator_service.rb
@@ -559,11 +559,16 @@ class SurveyResultsGeneratorService < FieldVisitorService
     # Finalise the results
     results.map do |question|
       if survey_has_logic
-        # Update the total response count with the new figure
-        question[:totalResponseCount] = question[:questionViewedCount]
+        # Don't update any figures if the response count is higher than the viewed count
+        # Can happen if logic changed after survey responses were collected and looks odd
+        pp "#{question[:questionResponseCount]}  >  #{question[:questionViewedCount]}"
+        if question[:questionViewedCount] >= question[:questionResponseCount]
+          # Update the total response count with the new figure
+          question[:totalResponseCount] = question[:questionViewedCount]
 
-        # Update the total pick count because we've reduced the 'not_answered' answer count
-        question[:totalPickCount] = question[:answers].pluck(:count).sum if question[:totalPickCount]
+          # Update the total pick count because we've reduced the 'not_answered' answer count
+          question[:totalPickCount] = question[:answers].pluck(:count).sum if question[:totalPickCount]
+        end
       end
     end
 
