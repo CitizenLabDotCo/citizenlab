3d9159fd694081b30a64fcf6a337d0ae234e7a9c
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-02-05 15:02:58 -0300

    Add tests for page logic

--- front/cypress/e2e/survey_builder/survey_page_logic.cy.ts
+++ front/cypress/e2e/survey_builder/survey_page_logic.cy.ts
@@ -44,11 +44,95 @@ describe('Survey page logic', () => {
     }
   });
 
-  it('adds page logic', () => {
+  beforeEach(() => {
+    cy.setAdminLoginCookie();
     cy.visit(
       `/admin/projects/${projectId}/phases/${phaseId}/native-survey/edit`
     );
     cy.get('[data-cy="e2e-field-row"]').should('have.length', 3);
-    cy.wait(10000);
+
+    // Make sure first page references "Ending"
+    cy.get('[data-cy="e2e-field-row"]')
+      .first()
+      .find('[data-cy="e2e-field-rule-display"]')
+      .contains('Ending');
+
+    // Add a new page
+    cy.wait(1000);
+    cy.get('[data-cy="e2e-page"').click();
+    cy.get('[data-cy="e2e-field-row"]').should('have.length', 4);
+  });
+
+  it('correctly adds new page', () => {
+    // Make sure first page references "Page 2"
+    cy.get('[data-cy="e2e-field-row"]')
+      .first()
+      .find('[data-cy="e2e-field-rule-display"]')
+      .contains('Page 2');
+
+    // Make sure second page references "Ending"
+    cy.get('[data-cy="e2e-field-row"]')
+      .eq(2) // eq(1) is the question on the first page
+      .find('[data-cy="e2e-field-rule-display"]')
+      .contains('Ending');
+  });
+
+  it('correctly updates page logic when removing pages', () => {
+    // Create another age
+    cy.wait(1000);
+    cy.get('[data-cy="e2e-page"').click();
+
+    // Make sure first page references page 2
+    cy.get('[data-cy="e2e-field-row"]')
+      .first()
+      .find('[data-cy="e2e-field-rule-display"]')
+      .contains('Page 2');
+
+    // Point first page to third page
+    cy.get('[data-cy="e2e-field-row"]').first().click();
+    cy.get('[data-cy="e2e-form-builder-logic-tab"]').click();
+    cy.get('[data-cy="e2e-add-rule-button"]').click();
+    cy.get('[data-cy="e2e-rule-input-select"]').find('select').select('Page 3');
+
+    // Confirm first page references third page
+    cy.get('[data-cy="e2e-field-row"]')
+      .first()
+      .find('[data-cy="e2e-field-rule-display"]')
+      .contains('Page 3');
+
+    // Delete third page
+    cy.get('[data-cy="e2e-more-field-actions"').first().click();
+    cy.get('.e2e-more-actions-list').find('button').first().click();
+
+    // Make sure first page now references page 2 again
+    cy.get('[data-cy="e2e-field-row"]')
+      .first()
+      .find('[data-cy="e2e-field-rule-display"]')
+      .contains('Page 2');
+  });
+
+  it('works when filling out survey', () => {
+    // Point first page to ending
+    cy.get('[data-cy="e2e-field-row"]').first().click();
+    cy.get('[data-cy="e2e-form-builder-logic-tab"]').click();
+    cy.get('[data-cy="e2e-add-rule-button"]').click();
+    cy.get('[data-cy="e2e-rule-input-select"]').find('select').select('Ending');
+
+    // Save
+    cy.get('form').submit();
+    // Make sure we see "Survey successfully saved" message
+    cy.get('[data-testid="feedbackSuccessMessage"]');
+
+    // Go to survey
+    cy.visit(`/projects/${projectSlug}/surveys/new?phase_id=${phaseId}`);
+    cy.acceptCookies();
+
+    // Expect submit button to be there, proving that page 1 goes straight to the end
+    // of the survey (and not to page 2)
+    cy.get('[data-cy="e2e-submit-form"]').click();
+
+    // Expect to be on success page and return to project
+    cy.get('[data-cy="e2e-after-submission"]').click();
+    cy.location('pathname').should('eq', `/en/projects/${projectSlug}`);
   });
 });
