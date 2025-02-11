f1eada80c82d94e8da1f97a3eca3f0f6747b55bd
brentguf <brentguf@users.noreply.github.com>
2025-02-11 11:17:19 +0100

    Move related tests into 1 file

--- front/cypress/e2e/admin/projects/project_settings.cy.ts
+++ front/cypress/e2e/admin/projects/project_settings.cy.ts
@@ -21,6 +21,36 @@ describe('Project settings', () => {
     });
   });
 
+  describe('Project description', () => {
+    describe('when the content builder toggle is disabled', () => {
+      it('links to project description settings', () => {
+        cy.visit(`admin/projects/${projectId}`);
+        cy.acceptCookies();
+        cy.get(
+          '[data-cy="e2e-project-description-preview-link-to-multiloc-settings"]'
+        ).click();
+        // Check if the description multiloc field is displayed
+        cy.get('#e2e-project-description-multiloc-module-active').should(
+          'exist'
+        );
+      });
+    });
+
+    describe('when the content builder toggle is enabled', () => {
+      it('links to the content builder', () => {
+        cy.visit(`admin/projects/${projectId}/settings/description`);
+        cy.get('#e2e-toggle-enable-project-description-builder').click({
+          force: true,
+        });
+        cy.visit(`admin/projects/${projectId}`);
+        cy.get(
+          '[data-cy="e2e-project-description-preview-link-to-content-builder"]'
+        ).click();
+        cy.get('#e2e-project-description-content-builder-page').should('exist');
+      });
+    });
+  });
+
   afterEach(() => {
     if (projectId) {
       cy.apiRemoveProject(projectId);
