7dc3d1cf92dfef5f5fe603fe9ace8680d45c513f
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-02-12 17:13:09 -0300

    Put back safe navigation operators

--- front/app/components/Form/utils/extractElementsByOtherOptionLogic.ts
+++ front/app/components/Form/utils/extractElementsByOtherOptionLogic.ts
@@ -8,12 +8,12 @@ const extractElementsByOtherOptionLogic = (
   const otherFieldValues = page.elements
     .filter((element) => element.options?.otherField)
     .map((element) => {
-      const parentFieldKey = element.scope.split('/').pop();
+      const parentFieldKey = element.scope?.split('/').pop();
       return { otherFieldKey: element.options?.otherField, parentFieldKey };
     });
 
   return page.elements.filter((element) => {
-    const key = element.scope.split('/').pop();
+    const key = element.scope?.split('/').pop();
     const field = otherFieldValues.find((item) => item.otherFieldKey === key);
 
     return (
