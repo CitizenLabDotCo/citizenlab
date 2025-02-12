6adb1f1babaf9a7d99da310347f432cb71d20cc0
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-02-12 16:47:25 -0300

    Fix

--- front/app/components/Form/utils/removeRequiredOtherFields.ts
+++ front/app/components/Form/utils/removeRequiredOtherFields.ts
@@ -1,6 +1,19 @@
-const removeRequiredOtherFields = (requiredFields: string[]) => {
-  // return requiredFields.filter((field) => !field.endsWith('other'));
-  return requiredFields;
+import { JsonSchema } from '@jsonforms/core';
+
+import { FormValues } from '../typings';
+
+const removeRequiredOtherFields = (schema: JsonSchema, data: FormValues) => {
+  return {
+    ...schema,
+    required: (schema.required ?? []).filter((fieldKey) => {
+      if (fieldKey.endsWith('_other')) {
+        const otherFieldKey = fieldKey.split('_other')[0];
+        return data[otherFieldKey] === 'other';
+      }
+
+      return true;
+    }),
+  };
 };
 
 export default removeRequiredOtherFields;
