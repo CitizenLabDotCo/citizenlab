d17c87345e762a4a0e66f359217bf247a3f0a608
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-02-12 16:29:08 -0300

    Remove unused isHidden nonsense

--- front/app/components/Form/utils/extractElementsByOtherOptionLogic.ts
+++ front/app/components/Form/utils/extractElementsByOtherOptionLogic.ts
@@ -1,31 +1,20 @@
-import { ExtendedUISchema } from '../typings';
+import { ExtendedUISchema, FormValues } from '../typings';
 
 // This returns the elements on a page that are visible based on the data and the other option selection. You can pass returnHidden as true to get the hidden elements
 const extractElementsByOtherOptionLogic = (
   page: any,
-  data: any,
-  returnHidden: boolean = false
+  data: FormValues
 ): ExtendedUISchema[] => {
   const otherFieldValues = page.elements
     .filter((element) => element.options?.otherField)
     .map((element) => {
-      const parentFieldKey = element.scope?.split('/').pop();
+      const parentFieldKey = element.scope.split('/').pop();
       return { otherFieldKey: element.options?.otherField, parentFieldKey };
     });
 
   return page.elements.filter((element) => {
-    const key = element.scope?.split('/').pop();
-    const field = otherFieldValues.find((item) => item?.otherFieldKey === key);
-
-    if (returnHidden) {
-      return (
-        field &&
-        (!field.parentFieldKey ||
-          (Array.isArray(data[field.parentFieldKey])
-            ? !data[field.parentFieldKey].includes('other')
-            : data[field.parentFieldKey] !== 'other'))
-      );
-    }
+    const key = element.scope.split('/').pop();
+    const field = otherFieldValues.find((item) => item.otherFieldKey === key);
 
     return (
       !field ||
