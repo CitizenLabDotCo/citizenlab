72a5d5af81688d44293f9f1ad34d9a709f224551
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-02-12 15:50:19 -0300

    Comment out previous fix

--- front/app/components/Form/utils/removeRequiredOtherFields.ts
+++ front/app/components/Form/utils/removeRequiredOtherFields.ts
@@ -1,5 +1,6 @@
 const removeRequiredOtherFields = (requiredFields: string[]) => {
-  return requiredFields.filter((field) => !field.endsWith('other'));
+  // return requiredFields.filter((field) => !field.endsWith('other'));
+  return requiredFields;
 };
 
 export default removeRequiredOtherFields;
