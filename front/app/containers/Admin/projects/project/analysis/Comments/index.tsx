d50e40c63c880526020dc18ed17c4760c72f968f
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-02-13 11:35:36 -0300

    Add proper copy to accordion

--- front/app/containers/Admin/projects/project/analysis/Comments/index.tsx
+++ front/app/containers/Admin/projects/project/analysis/Comments/index.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 
 import { Accordion, Box, Icon, Title } from '@citizenlab/cl2-component-library';
 
+import { useIntl } from 'utils/cl-intl';
+
+import messages from './messages';
+
 interface Props {
   onChange: (value: boolean) => void;
 }
 
 const Comments = ({ onChange }: Props) => {
+  const { formatMessage } = useIntl();
+
   return (
     <Accordion
       title={
         <Box display="flex" alignItems="center" px="24px" py="12px">
           <Icon height="16px" width="16px" name="comments" mr="8px" />
           <Title variant="h5" m="0">
-            Comments
+            {formatMessage(messages.comments)}
           </Title>
         </Box>
       }
---
+++ front/app/containers/Admin/projects/project/analysis/Comments/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  comments: {
+    id: 'app.containers.Admin.projects.project.analysis.Comments.comments',
+    defaultMessage: 'Comments',
+  },
+});
