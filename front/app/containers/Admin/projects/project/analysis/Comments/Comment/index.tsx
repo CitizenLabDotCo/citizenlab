b2b655359cfbd72798dc62055944c24265197994
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-02-13 17:18:22 -0300

    Include author hash

--- front/app/containers/Admin/projects/project/analysis/Comments/Comment/index.tsx
+++ front/app/containers/Admin/projects/project/analysis/Comments/Comment/index.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import { Box, Button, colors } from '@citizenlab/cl2-component-library';
 
+import { ICommentData } from 'api/comments/types';
+
 import useLocalize from 'hooks/useLocalize';
 
+import Author from 'components/Author';
 import QuillEditedContent from 'components/UI/QuillEditedContent';
 
 import { useIntl } from 'utils/cl-intl';
@@ -11,20 +14,32 @@ import { useIntl } from 'utils/cl-intl';
 import messages from '../messages';
 
 interface Props {
-  comment: any;
+  comment: ICommentData;
 }
 
 const Comment = ({ comment }: Props) => {
   const localize = useLocalize();
   const { formatMessage } = useIntl();
 
+  const authorId = comment.relationships.author.data?.id;
+  const { attributes } = comment;
+
   return (
-    <Box
-      mb="8px"
-      pb="8px"
-      key={comment.id}
-      borderBottom={`1px solid ${colors.divider}`}
-    >
+    <Box mb="8px" key={comment.id} borderBottom={`1px solid ${colors.divider}`}>
+      <Box mb="8px">
+        <Author
+          authorId={authorId ?? null}
+          authorHash={
+            attributes.publication_status === 'published'
+              ? attributes.author_hash
+              : undefined
+          }
+          isLinkToProfile={typeof authorId === 'string'}
+          createdAt={attributes.created_at}
+          size={30}
+          showModeratorStyles={false}
+        />
+      </Box>
       <QuillEditedContent textColor={colors.textPrimary}>
         <div
           dangerouslySetInnerHTML={{
