683a13c4efcb6c255af7225d63051b677ca597e2
brentguf <brentguf@users.noreply.github.com>
2025-02-18 08:00:20 +0100

    Only show 40 chars of folder title in preview

--- front/app/containers/Admin/projects/project/projectHeader/LinkToFolderSettings.tsx
+++ front/app/containers/Admin/projects/project/projectHeader/LinkToFolderSettings.tsx
@@ -9,6 +9,7 @@ import useLocalize from 'hooks/useLocalize';
 import { createHighlighterLink } from 'components/Highlighter';
 
 import Link from 'utils/cl-router/Link';
+import { truncate } from 'utils/textUtils';
 
 interface Props {
   folderId: string;
@@ -30,7 +31,7 @@ const LinkToFolderSettings = ({ folderId, projectId }: Props) => {
       <Box display="flex" alignItems="center" gap="4px">
         <Icon name="folder-solid" fill={colors.coolGrey600} width="14px" />
         <Text color="coolGrey600" m="0px" fontSize="s" p="0">
-          {localize(projectFolder.data.attributes.title_multiloc)}
+          {truncate(localize(projectFolder.data.attributes.title_multiloc), 40)}
         </Text>
       </Box>
     </Link>
