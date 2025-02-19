8ce06fb1744891d16eb92f7a434b4c50d7cf070e
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-02-19 17:45:53 -0300

    Put proper title

--- front/app/containers/Admin/projectLibrary/index.tsx
+++ front/app/containers/Admin/projectLibrary/index.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
+import { Box, Title } from '@citizenlab/cl2-component-library';
+
 import useProjectLibraryProjects from 'api/project_library_projects/useProjectLibraryProjects';
 
 const ProjectLibrary = () => {
   const { data: libraryProjects } = useProjectLibraryProjects({});
 
   return (
-    <div>
-      <h1>Project Library</h1>
+    <Box>
+      <Title variant="h1" color="primary">
+        Project Library
+      </Title>
       <ul>
         {libraryProjects?.data.map((project) => (
           <li key={project.id}>{project.attributes.title_en}</li>
         ))}
       </ul>
-    </div>
+    </Box>
   );
 };
 
