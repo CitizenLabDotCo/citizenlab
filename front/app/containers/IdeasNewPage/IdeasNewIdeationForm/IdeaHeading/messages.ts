fcecd62daf5bb4fa93ce7489fcf728e3a01bc6c5
EdwinKato <katedwin88@gmail.com>
2025-02-19 16:32:46 +0300

    Add localized messages for IdeaHeading component in new idea form

---
+++ front/app/containers/IdeasNewPage/IdeasNewIdeationForm/IdeaHeading/messages.ts
@@ -0,0 +1,28 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  editIdeaForm: {
+    id: 'app.containers.IdeasNewPage.ideaHeading.editIdeaForm',
+    defaultMessage: 'Edit idea form',
+  },
+  leaveFormConfirmationQuestion: {
+    id: 'app.containers.IdeasNewPage.ideaHeading.leaveFormConfirmationQuestion',
+    defaultMessage: 'Are you sure you want to leave?',
+  },
+  leaveIdeaText: {
+    id: 'app.containers.IdeasNewPage.ideaHeading.leaveIdeaText',
+    defaultMessage: "Your responses won't be saved.",
+  },
+  confirmLeaveFormButtonText: {
+    id: 'app.containers.IdeasNewPage.ideaHeading.confirmLeaveFormButtonText',
+    defaultMessage: 'Yes, I want to leave',
+  },
+  cancelLeaveIdeaButtonText: {
+    id: 'app.containers.IdeasNewPage.ideaHeading.cancelLeaveIdeaButtonText',
+    defaultMessage: 'Cancel',
+  },
+  leaveIdeaForm: {
+    id: 'app.containers.IdeasNewPage.ideaHeading.leaveIdeaForm',
+    defaultMessage: 'Leave idea form',
+  },
+});
