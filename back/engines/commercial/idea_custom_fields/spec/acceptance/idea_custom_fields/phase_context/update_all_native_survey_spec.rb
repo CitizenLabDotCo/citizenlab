6f8f344c41365a05ba40350f4fcdfaa49d9a8dc4
Amanda Anderson <anderson_amanda@mail.com>
2025-02-20 12:51:47 +0100

    Fix failing spec

--- back/engines/commercial/idea_custom_fields/spec/acceptance/idea_custom_fields/phase_context/update_all_native_survey_spec.rb
+++ back/engines/commercial/idea_custom_fields/spec/acceptance/idea_custom_fields/phase_context/update_all_native_survey_spec.rb
@@ -801,6 +801,12 @@ resource 'Idea Custom Fields' do
             linear_scale_label_3_multiloc: { en: 'Neutral' },
             linear_scale_label_4_multiloc: { en: 'High' },
             linear_scale_label_5_multiloc: { en: 'Highest' },
+            linear_scale_label_6_multiloc: {},
+            linear_scale_label_7_multiloc: {},
+            linear_scale_label_8_multiloc: {},
+            linear_scale_label_9_multiloc: {},
+            linear_scale_label_10_multiloc: {},
+            linear_scale_label_11_multiloc: {}.
             logic: {},
             random_option_ordering: false,
             constraints: {}
