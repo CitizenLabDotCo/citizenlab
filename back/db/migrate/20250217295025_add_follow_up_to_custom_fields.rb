e9fbe06085c926bc1d17d97236e0b6b1106e1987
Amanda Anderson <anderson_amanda@mail.com>
2025-02-20 10:21:55 +0100

    Tweaks to BE code

--- back/db/migrate/20250217295025_add_follow_up_to_custom_fields.rb
+++ back/db/migrate/20250217295025_add_follow_up_to_custom_fields.rb
@@ -1,8 +1,4 @@
-class AddFollowUpToCustomFields < ActiveRecord::Migration[7.0]
-  class StubCustomField < ApplicationRecord
-    self.table_name = 'custom_fields'
-  end
-
+class AddFollowUpToCustomFields < ActiveRecord::Migration[7.1]
   def change
     add_column :custom_fields, :ask_follow_up, :boolean, default: false, null: false
   end
