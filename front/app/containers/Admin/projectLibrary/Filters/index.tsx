0828fcfccec3d7cc6c240fe56a7e5ada49f86bc9
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-02-21 11:50:50 -0300

    Remove topic filter

--- front/app/containers/Admin/projectLibrary/Filters/index.tsx
+++ front/app/containers/Admin/projectLibrary/Filters/index.tsx
@@ -5,14 +5,12 @@ import { Box } from '@citizenlab/cl2-component-library';
 import ParticipationMethod from './ParticipationMethod';
 import PopulationSize from './PopulationSize';
 import Status from './Status';
-import Topic from './Topic';
 
 const Filters = () => {
   return (
     <Box display="flex" flexDirection="row" gap="20px">
       <PopulationSize />
       <ParticipationMethod />
-      <Topic />
       <Status />
     </Box>
   );
