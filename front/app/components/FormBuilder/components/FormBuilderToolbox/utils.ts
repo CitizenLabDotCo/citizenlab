7a4c911de15fb9e936a81885015bf946866a7221
Amanda Anderson <anderson_amanda@mail.com>
2025-02-24 15:20:50 +0100

    Use correct initial label values for sentiment_linear_scale fields

--- front/app/components/FormBuilder/components/FormBuilderToolbox/utils.ts
+++ front/app/components/FormBuilder/components/FormBuilderToolbox/utils.ts
@@ -1,5 +1,80 @@
 import styled from 'styled-components';
 
+import { ICustomFieldInputType } from 'api/custom_fields/types';
+
+import { MessageDescriptor } from 'utils/cl-intl';
+import { FormatMessageValues } from 'utils/cl-intl/useIntl';
+
+import messages from '../messages';
+
 export const DraggableElement = styled.div`
   cursor: move;
 `;
+
+type InitialLinearScaleLabelProps = {
+  value: number;
+  inputType: ICustomFieldInputType;
+  locale: string;
+  formatMessage: (
+    messageDescriptor: MessageDescriptor,
+    values?: FormatMessageValues
+  ) => string;
+};
+
+// Function to get the initial default labels for a linear scale field
+export const getInitialLinearScaleLabel = ({
+  value,
+  inputType,
+  locale,
+  formatMessage,
+}: InitialLinearScaleLabelProps) => {
+  if (inputType === 'matrix_linear_scale') {
+    switch (value) {
+      case 1:
+        return {
+          [locale]: formatMessage(messages.stronglyDisagree),
+        };
+      case 2:
+        return {
+          [locale]: formatMessage(messages.disagree),
+        };
+      case 3:
+        return {
+          [locale]: formatMessage(messages.neutral),
+        };
+      case 4:
+        return {
+          [locale]: formatMessage(messages.agree),
+        };
+      case 5:
+        return {
+          [locale]: formatMessage(messages.stronglyAgree),
+        };
+    }
+  } else if (inputType === 'sentiment_linear_scale') {
+    switch (value) {
+      case 1:
+        return {
+          [locale]: formatMessage(messages.veryBad),
+        };
+      case 2:
+        return {
+          [locale]: formatMessage(messages.bad),
+        };
+      case 3:
+        return {
+          [locale]: formatMessage(messages.ok),
+        };
+      case 4:
+        return {
+          [locale]: formatMessage(messages.good),
+        };
+      case 5:
+        return {
+          [locale]: formatMessage(messages.veryGood),
+        };
+    }
+  }
+
+  return {};
+};
