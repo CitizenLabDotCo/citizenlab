802f5421f0ecf44204d15a4ca5a199a429bc74dc
jamesspeake <james@speake.org>
2025-02-25 14:27:00 +0000

    [TAN-3961] Removed domicile specific code

--- back/app/services/xlsx_service.rb
+++ back/app/services/xlsx_service.rb
@@ -248,13 +248,14 @@ class XlsxService
   end
 
   def value_getter_for_user_custom_field_columns(field, record_to_user, options)
-    if field.domicile? # 'domicile' is a special case
-      areas = Area.all.index_by(&:id)
-      lambda do |record|
-        user = record.send(record_to_user)
-        multiloc_service.t(areas[user.domicile]&.title_multiloc) if user && user.custom_field_values['domicile']
-      end
-    elsif field.support_options? # field with option
+    # if field.domicile? # 'domicile' is a special case
+    #   areas = Area.all.index_by(&:id)
+    #   lambda do |record|
+    #     user = record.send(record_to_user)
+    #     multiloc_service.t(areas[user.domicile]&.title_multiloc) if user && user.custom_field_values['domicile']
+    #   end
+    # elsif field.support_options? # field with option
+    if field.support_options? # field with option
       lambda do |record|
         user = record.send(record_to_user)
         title_multiloc_for user, field, options
