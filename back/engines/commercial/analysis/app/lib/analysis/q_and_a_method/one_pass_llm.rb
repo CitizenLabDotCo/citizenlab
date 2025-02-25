e009052ef7b0567b87386477a21edaa7296e9a1d
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2025-02-25 23:10:28 +0100

    Always explicitly ask for the language of the summary

--- back/engines/commercial/analysis/app/lib/analysis/q_and_a_method/one_pass_llm.rb
+++ back/engines/commercial/analysis/app/lib/analysis/q_and_a_method/one_pass_llm.rb
@@ -75,7 +75,15 @@ module Analysis
 
     def prompt(project, inputs_text)
       project_title = MultilocService.new.t(project.title_multiloc)
-      LLM::Prompt.new.fetch('q_and_a', project_title: project_title, question: question.question, inputs_text: inputs_text, language: Locale.monolingual&.language_copy)
+      LLM::Prompt.new.fetch('q_and_a', project_title: project_title, question: question.question, inputs_text: inputs_text, language: response_language)
+    end
+
+    def response_language
+      locale = Locale.monolingual ||
+               question.activities.where(action: 'created').order(created_at: :desc).first&.user&.locale ||
+               AppConfiguration.instance.settings('core', 'locales').first
+
+      locale.language_copy
     end
   end
 end
--- back/engines/commercial/analysis/app/lib/analysis/summarization_method/one_pass_llm.rb
+++ back/engines/commercial/analysis/app/lib/analysis/summarization_method/one_pass_llm.rb
@@ -78,7 +78,15 @@ module Analysis
 
     def prompt(project, inputs_text)
       project_title = MultilocService.new.t(project.title_multiloc)
-      LLM::Prompt.new.fetch('summarization', project_title: project_title, inputs_text: inputs_text, language: Locale.monolingual&.language_copy)
+      LLM::Prompt.new.fetch('summarization', project_title: project_title, inputs_text: inputs_text, language: response_language)
+    end
+
+    def response_language
+      locale = Locale.monolingual ||
+               summary.activities.where(action: 'created').order(created_at: :desc).first&.user&.locale ||
+               AppConfiguration.instance.settings('core', 'locales').first
+
+      locale.language_copy
     end
   end
 end
