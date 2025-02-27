8f245c041cb31c317d97bcaaa5af6ac0c64c7fff
Sébastien Hoorens <sebastien@citizenlab.co>
2025-02-27 12:09:08 +0100

    deal with layouts in phase2txt + move embeddings tasks out of single use

--- back/engines/commercial/analysis/app/services/analysis/phase_to_text.rb
+++ back/engines/commercial/analysis/app/services/analysis/phase_to_text.rb
@@ -8,11 +8,32 @@ module Analysis
 
     def execute(phase, **options)
       super.merge(
-        'Project title' => @multiloc_service.t(phase.project.title_multiloc),
-        'Project description' => Nokogiri::HTML(@multiloc_service.t(phase.project.description_multiloc)).text, # TODO: Deal with content builder layouts
+        **exectute_project(phase.project),
+        **execute_phase(phase)
+      )
+    end
+
+    private
+
+    def exectute_project(project)
+      description_text = if ContentBuilder::Layout.exists?(content_buildable: project, code: 'project_description', enabled: true)
+        layout = ContentBuilder::Layout.find_by!(content_buildable: project, code: 'project_description', enabled: true)
+        multilocs = ContentBuilder::Craftjs::MultilocsInVisualOrder.new(layout.craftjs_json).extract
+        multilocs.map { |multiloc| Nokogiri::HTML(@multiloc_service.t(multiloc)).text }.join("\n")
+      else
+        Nokogiri::HTML(@multiloc_service.t(project.description_multiloc)).text
+      end
+      {
+        'Project title' => @multiloc_service.t(project.title_multiloc),
+        'Project description' => description_text
+      }
+    end
+
+    def execute_phase(phase)
+      {
         'Phase title' => @multiloc_service.t(phase.title_multiloc),
         'Phase description' => Nokogiri::HTML(@multiloc_service.t(phase.description_multiloc)).text
-      )
+      }
     end
   end
 end
