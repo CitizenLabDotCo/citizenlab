3d46254d2dcb7ae94f4a1804fe9500e19e40a465
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-02-27 13:56:48 +0000

    Add search input

--- front/app/containers/Admin/projectLibrary/Filters/index.tsx
+++ front/app/containers/Admin/projectLibrary/Filters/index.tsx
@@ -4,16 +4,25 @@ import { Box } from '@citizenlab/cl2-component-library';
 
 import Country from './Country';
 import Method from './Method';
+import Search from './Search';
 import Size from './Size';
 import Status from './Status';
 
 const Filters = () => {
   return (
-    <Box display="flex" flexDirection="row" gap="12px">
-      <Country />
-      <Method />
-      <Size />
-      <Status />
+    <Box
+      display="flex"
+      flexDirection="row"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Box display="flex" flexDirection="row" gap="12px">
+        <Country />
+        <Method />
+        <Size />
+        <Status />
+      </Box>
+      <Search />
     </Box>
   );
 };
