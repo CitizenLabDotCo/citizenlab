aa150e03509b0fb41c561d42021b5d96c66c2294
Amanda Anderson <anderson_amanda@mail.com>
2025-02-27 12:45:33 +0100

    Remove changes to FormResults component

--- front/app/containers/Admin/communityMonitor/components/LiveMonitor.tsx
+++ front/app/containers/Admin/communityMonitor/components/LiveMonitor.tsx
@@ -2,27 +2,16 @@ import React from 'react';
 
 import { Box, Title } from '@citizenlab/cl2-component-library';
 
-import useCommunityMonitorProject from 'api/community_monitor/useCommunityMonitorProject';
-
-import FormResults from 'containers/Admin/projects/project/nativeSurvey/FormResults';
-
 import { FormattedMessage } from 'utils/cl-intl';
 
 import messages from './messages';
 
 const LiveMonitor = () => {
-  const { data: project } = useCommunityMonitorProject();
-  const phaseId = project?.data.relationships.current_phase?.data?.id;
-
   return (
     <Box mt="48px">
       <Title color="primary">
         <FormattedMessage {...messages.communityMonitorLabel} />
       </Title>
-      <FormResults
-        projectIdFromProps={project?.data.id}
-        phaseIdFromProps={phaseId}
-      />
     </Box>
   );
 };
