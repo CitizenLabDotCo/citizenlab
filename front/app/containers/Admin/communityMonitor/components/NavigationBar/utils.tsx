6f2e5511830239aa4bd12a1f4ba2f9d7cb67e511
Amanda Anderson <anderson_amanda@mail.com>
2025-02-28 09:58:50 +0100

    Add correct typing

--- front/app/containers/Admin/communityMonitor/components/NavigationBar/utils.tsx
+++ front/app/containers/Admin/communityMonitor/components/NavigationBar/utils.tsx
@@ -1,32 +1,31 @@
-import { RouteType } from 'routes';
-import { FormatMessage } from 'typings';
+import { FormatMessage, ITab } from 'typings';
 
 import messages from '../../messages';
 
 export const getCommunityMonitorTabs = (
   formatMessage: FormatMessage,
   projectId?: string
-) => {
+): ITab[] => {
   return [
     {
       label: formatMessage(messages.liveMonitor),
-      url: '/admin/community-monitor/live-monitor' as RouteType,
+      url: '/admin/community-monitor/live-monitor',
       name: 'live-monitor',
     },
     {
       label: formatMessage(messages.participants),
       name: 'participants',
-      url: `/admin/community-monitor/participants/projects/${projectId}` as RouteType,
+      url: `/admin/community-monitor/participants/projects/${projectId}`,
     },
     {
       label: formatMessage(messages.reports),
-      url: '/admin/community-monitor/reports' as RouteType,
+      url: '/admin/community-monitor/reports',
       name: 'reports',
     },
     {
       label: formatMessage(messages.settings),
       name: 'settings',
-      url: '/admin/community-monitor/settings' as RouteType,
+      url: '/admin/community-monitor/settings',
     },
   ];
 };
