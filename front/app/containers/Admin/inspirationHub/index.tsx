48ec631827b727b61c749dbb295b4d24d37ebab9
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-03-03 14:37:57 +0000

    Hide page if feature not enabled

--- front/app/containers/Admin/inspirationHub/index.tsx
+++ front/app/containers/Admin/inspirationHub/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Box, Title } from '@citizenlab/cl2-component-library';
 
+import useFeatureFlag from 'hooks/useFeatureFlag';
+
 import messages from 'containers/Admin/sideBar/messages';
 
 import { FormattedMessage } from 'utils/cl-intl';
@@ -10,6 +12,9 @@ import Filters from './Filters';
 import ProjectTable from './ProjectTable';
 
 const InspirationHub = () => {
+  const projectLibraryEnabled = useFeatureFlag({ name: 'project_library' });
+  if (!projectLibraryEnabled) return null;
+
   return (
     <Box>
       <Title variant="h1" color="primary" mb="40px">
