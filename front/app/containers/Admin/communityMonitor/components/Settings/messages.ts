6e11fa24bac55929dc88cefe1d09cd3fd1e04613
Amanda Anderson <anderson_amanda@mail.com>
2025-03-03 17:40:42 +0100

    Fix ids

--- front/app/containers/Admin/communityMonitor/components/Settings/messages.ts
+++ front/app/containers/Admin/communityMonitor/components/Settings/messages.ts
@@ -38,19 +38,19 @@ export default defineMessages({
       'Community Monitor Managers can access and manage all Community Monitor settings and data.',
   },
   surveySettings: {
-    id: 'app.containers.Admin.communityMonitor.settings.surveySettings',
+    id: 'app.containers.Admin.communityMonitor.settings.surveySettings2',
     defaultMessage: 'Survey settings',
   },
   accessRights: {
-    id: 'app.containers.Admin.communityMonitor.settings.accessRights',
+    id: 'app.containers.Admin.communityMonitor.settings.accessRights2',
     defaultMessage: 'Access rights',
   },
   management: {
-    id: 'app.containers.Admin.communityMonitor.settings.management',
+    id: 'app.containers.Admin.communityMonitor.settings.management2',
     defaultMessage: 'Management',
   },
   survey: {
-    id: 'app.containers.Admin.communityMonitor.settings.survey',
+    id: 'app.containers.Admin.communityMonitor.settings.survey2',
     defaultMessage: 'Survey',
   },
   openForResponses: {
@@ -58,11 +58,11 @@ export default defineMessages({
     defaultMessage: 'Open for responses',
   },
   importInputs: {
-    id: 'app.containers.AdminPage.ProjectEdit.survey.importInputs',
+    id: 'app.containers.AdminPage.ProjectEdit.survey.importInputs2',
     defaultMessage: 'Import',
   },
   viewSurveyText: {
-    id: 'app.containers.AdminPage.ProjectEdit.survey.viewSurveyText',
+    id: 'app.containers.AdminPage.ProjectEdit.survey.viewSurveyText2',
     defaultMessage: 'View',
   },
   editSurvey: {
