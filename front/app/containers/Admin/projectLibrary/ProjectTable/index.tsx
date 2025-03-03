708060f99355d4d82a788aa9f1893476a1e41be3
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-03-03 13:35:54 +0000

    Add pagination component without annoying spacing

--- front/app/containers/Admin/projectLibrary/ProjectTable/index.tsx
+++ front/app/containers/Admin/projectLibrary/ProjectTable/index.tsx
@@ -10,6 +10,8 @@ import {
 
 import useProjectLibraryProjects from 'api/project_library_projects/useProjectLibraryProjects';
 
+import { PaginationWithoutPositioning } from 'components/Pagination';
+
 import { useRansackParams } from '../utils';
 
 import TableBody from './TableBody';
@@ -23,41 +25,50 @@ const ProjectTable = () => {
   } = useProjectLibraryProjects(useRansackParams());
 
   return (
-    <Box position="relative">
-      <Table
-        border={`1px solid ${colors.grey300}`}
-        borderRadius={stylingConsts.borderRadius}
-        innerBorders={{
-          headerCells: false,
-          bodyRows: false,
-        }}
-      >
-        <TableHead />
-        <TableBody
-          libraryProjects={libraryProjects}
-          isInitialLoading={isInitialLoading}
-        />
-      </Table>
-      {isRefetching && (
-        <Box
-          position="absolute"
-          top="0"
-          left="0"
-          zIndex="10"
-          background="rgba(255, 255, 255, 0.5)"
-          w="100%"
-          h="100%"
+    <Box>
+      <Box position="relative">
+        <Table
+          border={`1px solid ${colors.grey300}`}
+          borderRadius={stylingConsts.borderRadius}
+          innerBorders={{
+            headerCells: false,
+            bodyRows: false,
+          }}
         >
+          <TableHead />
+          <TableBody
+            libraryProjects={libraryProjects}
+            isInitialLoading={isInitialLoading}
+          />
+        </Table>
+        {isRefetching && (
           <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
+            position="absolute"
+            top="0"
+            left="0"
+            zIndex="10"
+            background="rgba(255, 255, 255, 0.5)"
+            w="100%"
             h="100%"
           >
-            <Spinner />
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              h="100%"
+            >
+              <Spinner />
+            </Box>
           </Box>
-        </Box>
-      )}
+        )}
+      </Box>
+      <Box w="100%" mt="20px" display="flex" justifyContent="center">
+        <PaginationWithoutPositioning
+          currentPage={1}
+          totalPages={5}
+          loadPage={() => {}}
+        />
+      </Box>
     </Box>
   );
 };
