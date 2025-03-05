0adcc5c76a72c3a9e4a0fdc47d7795bb4edd1eb9
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-03-05 15:08:19 +0000

    Add population label and tenant name

--- front/app/containers/Admin/inspirationHub/ProjectDrawer/index.tsx
+++ front/app/containers/Admin/inspirationHub/ProjectDrawer/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Box, StatusLabel, colors } from '@citizenlab/cl2-component-library';
 import { useSearchParams } from 'react-router-dom';
 
 import useProjectLibraryProject from 'api/project_library_projects/useProjectLibraryProject';
@@ -7,6 +8,8 @@ import useProjectLibraryProject from 'api/project_library_projects/useProjectLib
 import SideModal from 'components/UI/SideModal';
 
 import { removeSearchParams } from 'utils/cl-router/removeSearchParams';
+
+import ExternalLink from './ExternalLink';
 
 const ProjectDrawer = () => {
   const [searchParams] = useSearchParams();
@@ -18,11 +21,26 @@ const ProjectDrawer = () => {
     removeSearchParams(['project_id']);
   };
 
-  console.log({ project });
+  if (!project) return null;
+
+  const { attributes } = project.data;
 
   return (
     <SideModal opened={!!projectId} close={handleOnClose}>
-      TODO
+      <Box mt="52px" mx="28px">
+        <Box display="flex" flexDirection="row" alignItems="center">
+          <StatusLabel
+            text={attributes.tenant_population_group}
+            backgroundColor={colors.coolGrey600}
+            h="16px"
+            w="24px"
+            ml="4px"
+          />
+          <ExternalLink href={`/todo`} ml="8px" mt="1px">
+            {attributes.tenant_name}
+          </ExternalLink>
+        </Box>
+      </Box>
     </SideModal>
   );
 };
