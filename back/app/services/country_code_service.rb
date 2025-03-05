d6a74bb17381f10871cd0f436e41ccad7d54c334
jinjagit <jinjamail@gmail.com>
2025-03-05 10:55:15 +0000

    [TAN-4013] New CountryCodeService + spec

---
+++ back/app/services/country_code_service.rb
@@ -0,0 +1,9 @@
+class CountryCodeService
+
+  def get_country_code lat, lng
+      return nil if lat.nil? || lng.nil?
+
+      location = Geocoder.search([lat, lng]).first
+      location.country_code.upcase if location
+  end
+end
