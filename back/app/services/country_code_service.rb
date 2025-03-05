9e82d3491e3986116d82ea2eafaa9d16dd6c46ad
jinjagit <jinjamail@gmail.com>
2025-03-05 11:05:48 +0000

    [TAN-4013] Linting

--- back/app/services/country_code_service.rb
+++ back/app/services/country_code_service.rb
@@ -1,9 +1,8 @@
 class CountryCodeService
+  def get_country_code(lat, lng)
+    return nil if lat.nil? || lng.nil?
 
-  def get_country_code lat, lng
-      return nil if lat.nil? || lng.nil?
-
-      location = Geocoder.search([lat, lng]).first
-      location.country_code.upcase if location
+    location = Geocoder.search([lat, lng]).first
+    location&.country_code&.upcase
   end
 end
