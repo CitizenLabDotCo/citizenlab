535970c926af56d9d9a5a772ac4fb8af11fe15f3
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-03-10 11:38:43 +0000

    Create sections

--- front/app/containers/Admin/inspirationHub/index.tsx
+++ front/app/containers/Admin/inspirationHub/index.tsx
@@ -4,11 +4,12 @@ import { Box, Title } from '@citizenlab/cl2-component-library';
 
 import useFeatureFlag from 'hooks/useFeatureFlag';
 
-import messages from 'containers/Admin/sideBar/messages';
+import sidebarMessages from 'containers/Admin/sideBar/messages';
 
 import { FormattedMessage } from 'utils/cl-intl';
 
 import Filters from './Filters';
+import messages from './messages';
 import ProjectDrawer from './ProjectDrawer';
 import ProjectTable from './ProjectTable';
 
@@ -19,10 +20,19 @@ const InspirationHub = () => {
   return (
     <>
       <Box>
-        <Title variant="h1" color="primary" mb="40px">
-          <FormattedMessage {...messages.inspirationHub} />
+        <Title variant="h1" color="primary" mb="36px">
+          <FormattedMessage {...sidebarMessages.inspirationHub} />
         </Title>
         <Box>
+          <Title variant="h2" color="primary" mt="0px">
+            <FormattedMessage {...messages.highlighted} />
+          </Title>
+          TODO
+        </Box>
+        <Box>
+          <Title variant="h2" color="primary">
+            <FormattedMessage {...messages.allProjects} />
+          </Title>
           <Box mb="24px">
             <Filters />
           </Box>
