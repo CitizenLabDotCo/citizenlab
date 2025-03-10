892a43976bf0a4f2d9824b86e99a4b6e37c0ad46
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-03-10 17:43:44 +0000

    Rename size -> population

--- front/app/containers/Admin/inspirationHub/Filters/index.tsx
+++ front/app/containers/Admin/inspirationHub/Filters/index.tsx
@@ -5,8 +5,8 @@ import { Box } from '@citizenlab/cl2-component-library';
 import Country from './Country';
 import Dates from './Dates';
 import Method from './Method';
+import Population from './Population';
 import Search from './Search';
-import Size from './Size';
 import Status from './Status';
 
 const Filters = () => {
@@ -20,7 +20,7 @@ const Filters = () => {
       <Box display="flex" flexDirection="row" gap="12px">
         <Country />
         <Method />
-        <Size />
+        <Population />
         <Status />
         <Dates />
       </Box>
