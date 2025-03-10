ca09a9f56b7c788ed43da4d7615c60446ee6a01c
Sébastien Hoorens <sebastien@citizenlab.co>
2025-03-10 16:10:20 +0100

    first version of similarity by title and description

--- back/spec/factories/embeddings_similarities.rb
+++ back/spec/factories/embeddings_similarities.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :embeddings_similarity do
     association :embeddable, factory: :idea
     embedding { Array.new(1024) { rand(-1.0..1.0) } }
-    embedded_attributes { 'title_body' }
+    embedded_attributes { 'title' }
   end
 end
