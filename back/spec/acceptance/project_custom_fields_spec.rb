dc699489c0ba440b56328ecd6e10ea304e5f112f
EdwinKato <katedwin88@gmail.com>
2025-03-10 11:58:48 +0300

    Remove 'survey_end' from JSON schema properties in Project level Custom Fields specs

--- back/spec/acceptance/project_custom_fields_spec.rb
+++ back/spec/acceptance/project_custom_fields_spec.rb
@@ -50,7 +50,6 @@ resource 'Project level Custom Fields' do
         location_description
         idea_images_attributes
         idea_files_attributes
-        survey_end
       ]
     end
     let(:expected_jsonschema_form_field_keys) { enabled_built_in_field_keys + [custom_field.key.to_sym] }
@@ -91,7 +90,7 @@ resource 'Project level Custom Fields' do
         expect(json_attributes[:json_schema_multiloc].keys).to match_array %i[en fr-FR nl-NL]
         %i[en fr-FR nl-NL].each do |locale|
           expect(json_attributes[:json_schema_multiloc][locale][:properties].keys).to match_array(
-            %i[title_multiloc body_multiloc topic_ids location_description idea_images_attributes idea_files_attributes survey_end]
+            %i[title_multiloc body_multiloc topic_ids location_description idea_images_attributes idea_files_attributes]
           )
         end
         ui_schema = json_attributes[:ui_schema_multiloc][:en]
