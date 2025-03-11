7dc140c35185aac9366b2dc3409bbad5b5804346
jinjagit <jinjamail@gmail.com>
2025-03-11 12:06:59 +0000

    [TAN-4091] Better conditional nesting

--- back/engines/commercial/admin_api/app/controllers/admin_api/project_images_controller.rb
+++ back/engines/commercial/admin_api/app/controllers/admin_api/project_images_controller.rb
@@ -7,18 +7,15 @@ module AdminApi
       # even though the model allows for multiple images.
       project_image = ProjectImage.find_by(project_id: params[:id])
 
-      if project_image
+      if project_image && project_image.image && project_image.image.file
         url = nil
 
         if project_image.image.file.respond_to?(:url)
           url = project_image.image.file.url
         elsif Rails.env.development? # In development, the file is stored locally
-          if project_image.image && project_image.image.file
-            # Check if file responds to path method
-            if project_image.image.file.respond_to?(:path)
-              path = project_image.image.file.path
-              url = "http://localhost:3000/#{path.split('public/').last}" if path
-            end
+          if project_image.image.file.respond_to?(:path)
+            path = project_image.image.file.path
+            url = "http://localhost:3000/#{path.split('public/').last}" if path
           end
         end
         render json: { url: url }
