88c3624fa0d347542ed3deb21076394bcc4bc98f
EdwinKato <katedwin88@gmail.com>
2025-03-13 10:58:13 +0300

    Replace getKey utility with getFieldNameFromPath in form utilities

--- front/app/components/Form/utils/getKey.ts
+++
@@ -1,8 +0,0 @@
-import { getFieldNameFromPath } from 'utils/JSONFormUtils';
-
-// TODO: Replace getKey with getFieldNameFromPath
-const getKey = (scope: string) => {
-  return getFieldNameFromPath(scope);
-};
-
-export default getKey;
