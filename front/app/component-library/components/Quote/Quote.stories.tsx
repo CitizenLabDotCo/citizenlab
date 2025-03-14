1fc611cd2fde3e21a468848efeb31a2c0420cb61
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-03-14 12:17:52 +0000

    _

--- front/app/component-library/components/Quote/Quote.stories.tsx
+++ front/app/component-library/components/Quote/Quote.stories.tsx
@@ -11,5 +11,9 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    maxWidth: '500px',
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tempus dolor eget nisi volutpat luctus. Donec sed semper nisl. Praesent leo nisi, egestas vitae maximus quis, facilisis vel massa. Maecenas maximus viverra ex vitae lobortis. Vestibulum mattis risus pharetra suscipit porttitor. Sed ornare feugiat luctus. Quisque pulvinar efficitur ultrices',
+  },
 };
--- front/app/component-library/components/Quote/index.tsx
+++ front/app/component-library/components/Quote/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Quote = () => {
-  return <></>;
+import { colors } from 'component-library/utils/styleUtils';
+
+import Box, { BoxProps } from '../Box';
+
+const Quote = ({ children, ...props }: BoxProps) => {
+  return (
+    <Box as="blockquote" bgColor={colors.teal50} p="12px" {...props}>
+      {children}
+    </Box>
+  );
 };
 
 export default Quote;
