1f36506486a1dd0f03f17d4911cf59cb9fc3f8d9
Amanda Anderson <anderson_amanda@mail.com>
2025-03-19 14:47:11 +0100

    Remove deadcode

--- front/app/containers/Admin/communityMonitor/components/LiveMonitor/components/HealthScoreWidget/types.ts
+++ front/app/containers/Admin/communityMonitor/components/LiveMonitor/components/HealthScoreWidget/types.ts
@@ -1,21 +1,3 @@
-export type Score = {
-  period: string;
-  score: any;
-};
-
-export type CategoryScore = {
-  category: string;
-  score: Score[];
-  label: string;
-};
-
-export type OverallScores =
-  | {
-      period: string;
-      score: any;
-    }[]
-  | undefined;
-
 export type QuarterlyScores = {
   overallHealthScores: {
     period: string;
