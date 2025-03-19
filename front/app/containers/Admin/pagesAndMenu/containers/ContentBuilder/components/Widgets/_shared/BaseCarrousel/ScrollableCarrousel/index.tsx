fd2603de8d734a6441a96012f5be237b8d5e1b0a
brentguf <brentguf@users.noreply.github.com>
2025-03-19 11:04:39 +0100

    Remove todo from comment

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/BaseCarrousel/ScrollableCarrousel/index.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/_shared/BaseCarrousel/ScrollableCarrousel/index.tsx
@@ -91,10 +91,7 @@ const ScrollableCarrousel = ({
 
   return (
     <Box
-      /* We set position relative to be able to position the skip (& scroll?) buttons.
-        I'd actually prefer to move the SkipButton's absolute positioning styles onto a Box that wraps the button here, so the relative/absolute relationship is clearer.
-        Similarly, I think the ScrollButtons could go inside HorizontalScroll so the positioning relationship could be made clearer.
-      */
+      // We set position relative to be able to position the skip (& scroll?) buttons.
       position="relative"
     >
       <SkipButton onSkip={() => skipCarrousel(endId)} />
