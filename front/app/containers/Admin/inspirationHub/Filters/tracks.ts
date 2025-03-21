982c00fdfdd655b6d57d7e851c56020a4ab0bfc4
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-03-21 10:56:42 +0000

    Add tracking to filters

---
+++ front/app/containers/Admin/inspirationHub/Filters/tracks.ts
@@ -0,0 +1,6 @@
+export default {
+  setCountry: 'Project library: set country filter',
+  setDates: 'Project library: set date filter',
+  setMethod: 'Project library: set method filter',
+  setPopulationGroup: 'Project library: set population group filter',
+};
