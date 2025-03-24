4acb32108196bca1fd4b1c8aa0138c04dda271fc
brentguf <brentguf@users.noreply.github.com>
2025-03-24 16:14:34 +0100

    First finished vesion of "shows projects of the areas I follow" E2E test

--- front/cypress/e2e/homepage_builder/areas_widget.cy.ts
+++ front/cypress/e2e/homepage_builder/areas_widget.cy.ts
@@ -4,6 +4,7 @@ describe('"In your area" (areas) widget', () => {
     // Go to the homepage builder
     cy.setAdminLoginCookie();
     cy.visit('en/admin/pages-menu/homepage-builder');
+    cy.acceptCookies();
 
     // Drag in widget
     cy.get('#e2e-draggable-areas').dragAndDrop('#e2e-content-builder-frame', {
@@ -16,10 +17,11 @@ describe('"In your area" (areas) widget', () => {
   });
 
   after(() => {
-    // Delete widget again
-    cy.setAdminLoginCookie();
     cy.visit('en/admin/pages-menu/homepage-builder');
-    cy.get('.e2e-areas-widget').first().parent().click({ force: true });
+    cy.get('[data-cy="e2e-areas-widget"]')
+      .first()
+      .parent()
+      .click({ force: true });
 
     cy.get('#e2e-delete-button').click();
 
@@ -31,40 +33,55 @@ describe('"In your area" (areas) widget', () => {
     cy.goToLandingPage();
     cy.reload();
     cy.wait(4000);
-    cy.get('.e2e-areas-widget').should('not.exist');
-  });
-
-  it('exists', () => {
-    // Check if widget is displayed in homepage
-    cy.goToLandingPage();
-    cy.get('.e2e-areas-widget').should('exist');
+    cy.get('[data-cy="e2e-areas-widget"]').should('not.exist');
   });
 
   it('shows projects of the areas I follow', () => {
+    // Create project with area
+    cy.visit('/admin/projects/all');
+    cy.get('[data-cy="e2e-new-project-button"]').click();
+    cy.get('.e2e-project-general-form');
+
+    const projectTitleEN = 'Project linked to Carrotgem area';
+    cy.get('#e2e-project-title-setting-field').type(projectTitleEN);
+    cy.get('.e2e-localeswitcher.nl-BE').click();
+    cy.get('#e2e-project-title-setting-field').type(projectTitleEN);
+    cy.get('.e2e-localeswitcher.nl-NL').click();
+    cy.get('#e2e-project-title-setting-field').type(projectTitleEN);
+    cy.get('.e2e-localeswitcher.fr-BE').click();
+    cy.get('#e2e-project-title-setting-field').type(projectTitleEN);
+
+    // Select 'Selection' as Areas option
+    cy.get('.e2e-areas-selection').click();
+
+    // Pick (only) area
+    cy.get('#e2e-area-selector')
+      .click()
+      .find('input')
+      .type('Carrotgem')
+      .trigger('keydown', { keyCode: 13, which: 13 });
+
+    // Submit and publish project
+    cy.get('.e2e-submit-wrapper-button button').click();
+    cy.get('#e2e-publish').click();
+
     // Go to the homepage
-    cy.setAdminLoginCookie();
     cy.goToLandingPage();
+    // Necessary to ensure our widget is loaded
+    cy.reload();
 
     // Opens the modal
     cy.get('[data-cy="e2e-follow-areas-button"]').click();
 
-    // Select areas
-    // TODO
+    // Select area
+    cy.contains('button', 'Carrotgem').click();
 
     // Closes the modal
     cy.get('[data-cy="e2e-follow-areas-modal-done-button"]').click();
 
-    // Shows project(s) of the areas I follow
-    // TODO
+    // Shows the project with title "Project linked to Carrotgem area"
+    cy.get('[data-cy="e2e-areas-widget"]')
+      .find('[data-cy="e2e-light-project-card"]')
+      .should('contain', projectTitleEN);
   });
-
-  it('shows the fallback text when no areas are followed', () => {
-    // Go to the homepage
-    cy.setAdminLoginCookie();
-    cy.goToLandingPage();
-
-    // Shows fallback text
-  });
-
-  it('shows projects in my area after signing up', () => {});
 });
