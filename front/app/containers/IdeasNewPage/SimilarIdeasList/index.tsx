1e0336cff8f3750e0a443e9c1de6afe10cb0725c
EdwinKato <katedwin88@gmail.com>
2025-03-25 12:49:51 +0300

    Fix display of similar ideas to match the design

--- front/app/containers/IdeasNewPage/SimilarIdeasList/index.tsx
+++ front/app/containers/IdeasNewPage/SimilarIdeasList/index.tsx
@@ -23,9 +23,7 @@ interface Props {
 
 const SimilarIdeasList = ({ query }: Props) => {
   const { formatMessage } = useIntl();
-  const { slug } = useParams() as {
-    slug: string;
-  };
+  const { slug } = useParams() as { slug: string };
   const { data: project } = useProjectBySlug(slug);
   const { data: ideas, isLoading } = useSimilarIdeas(
     {
@@ -43,11 +41,11 @@ const SimilarIdeasList = ({ query }: Props) => {
   if (!ideas) return null;
 
   return (
-    <Box borderTop={`1px solid ${colors.grey300}`}>
+    <Box>
       <Text fontSize="s" fontWeight="bold" color="textPrimary" mb="4px">
         {formatMessage(messages.similarSubmissionsPosted)}
       </Text>
-      <Text fontSize="s" color="textSecondary">
+      <Text fontSize="s" color="textSecondary" mb="16px">
         {formatMessage(messages.similarSubmissionsDescription)}
       </Text>
 
@@ -60,6 +58,7 @@ const SimilarIdeasList = ({ query }: Props) => {
               ? `1px solid ${colors.grey300}`
               : undefined
           }
+          py="12px"
         >
           <a
             href={`/ideas/${idea.attributes.slug}`}
@@ -72,33 +71,76 @@ const SimilarIdeasList = ({ query }: Props) => {
               justifyContent="space-between"
               alignItems="center"
             >
-              <Box>
+              <Box display="flex" flexDirection="column" width="100%" mr="8px">
                 <Text
-                  fontSize="s"
+                  variant="bodyM"
                   fontWeight="bold"
                   color="textPrimary"
-                  mb="4px"
+                  my="0px"
                 >
                   {idea.attributes.title_multiloc.en}
                 </Text>
-                <Box display="flex" alignItems="center" gap="4px">
-                  <Icon
-                    name="user"
-                    width="12px"
-                    height="12px"
-                    fill={colors.textPrimary}
-                    transform="scaleX(-1)"
-                  />
-                  <Text fontSize="xs" color="textPrimary">
-                    {idea.attributes.author_name}
-                  </Text>
+
+                <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Box display="flex" alignItems="center" gap="4px">
+                    <Icon
+                      name="user-circle"
+                      width="16px"
+                      height="16px"
+                      fill={colors.textPrimary}
+                    />
+                    <Text variant="bodyS" color="grey700" m="0px">
+                      {idea.attributes.author_name}
+                    </Text>
+                  </Box>
+
+                  <Box display="flex" alignItems="center" gap="16px">
+                    <Box display="flex" alignItems="center" gap="4px">
+                      <Icon
+                        name="vote-up"
+                        width="14px"
+                        height="14px"
+                        fill={colors.grey700}
+                      />
+                      <Text fontSize="xs" color="grey700" m="0">
+                        {idea.attributes.likes_count}
+                      </Text>
+                    </Box>
+                    <Box display="flex" alignItems="center" gap="4px">
+                      <Icon
+                        name="vote-down"
+                        width="14px"
+                        height="14px"
+                        fill={colors.grey700}
+                      />
+                      <Text fontSize="xs" color="grey700" m="0">
+                        {idea.attributes.dislikes_count}
+                      </Text>
+                    </Box>
+                    <Box display="flex" alignItems="center" gap="4px">
+                      <Icon
+                        name="comments"
+                        width="14px"
+                        height="14px"
+                        fill={colors.grey700}
+                      />
+                      <Text fontSize="xs" color="grey700" m="0">
+                        {idea.attributes.comments_count}
+                      </Text>
+                    </Box>
+                  </Box>
                 </Box>
               </Box>
+
               <Icon
                 name="chevron-right"
-                width="16px"
-                height="16px"
-                fill={colors.textSecondary}
+                width="20px"
+                height="20px"
+                fill={colors.textPrimary}
               />
             </Box>
           </a>
