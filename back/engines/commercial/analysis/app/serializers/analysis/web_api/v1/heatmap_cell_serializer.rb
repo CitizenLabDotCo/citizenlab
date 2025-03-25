e91fea4c204e1fcae3cc1a8f61a85e54eed7e053
Koen Gremmelprez <koen.gremmelprez@gmail.com>
2025-03-25 16:00:29 +0100

    Updated the heatmaps API to respond with CustomFieldBin row/columns

--- back/engines/commercial/analysis/app/serializers/analysis/web_api/v1/heatmap_cell_serializer.rb
+++ back/engines/commercial/analysis/app/serializers/analysis/web_api/v1/heatmap_cell_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Analysis::WebApi::V1::HeatmapCellSerializer < WebApi::V1::BaseSerializer
-  attributes :created_at, :updated_at, :unit, :count, :lift, :p_value, :row_bin_value, :column_bin_value
+  attributes :created_at, :updated_at, :unit, :count, :lift, :p_value
 
   attribute :statement_multiloc do |object|
     Analysis::HeatmapCellStatementGenerator.new.generate(object)
@@ -9,22 +9,18 @@ class Analysis::WebApi::V1::HeatmapCellSerializer < WebApi::V1::BaseSerializer
 
   belongs_to :row, serializer: lambda { |object, _params|
     case object
-    when CustomFieldOption
-      WebApi::V1::CustomFieldOptionSerializer
+    when CustomFieldBin
+      WebApi::V1::CustomFieldBinSerializer
     when Analysis::Tag
       Analysis::WebApi::V1::TagSerializer
-    when CustomField
-      WebApi::V1::CustomFieldSerializer
     end
   }
   belongs_to :column, serializer: lambda { |object, _params|
     case object
-    when CustomFieldOption
-      WebApi::V1::CustomFieldOptionSerializer
+    when CustomFieldBin
+      WebApi::V1::CustomFieldBinSerializer
     when Analysis::Tag
       Analysis::WebApi::V1::TagSerializer
-    when CustomField
-      WebApi::V1::CustomFieldSerializer
     end
   }
 
