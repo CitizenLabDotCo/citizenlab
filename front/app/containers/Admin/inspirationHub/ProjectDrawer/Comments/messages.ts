da34f46b9d552b4c7b84fa8b04f168e98edde1c7
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-03-27 17:27:11 +0000

    Add copy for comment edit component

---
+++ front/app/containers/Admin/inspirationHub/ProjectDrawer/Comments/messages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  save: {
+    id: 'front.app.containers.Admin.inspirationHub.ProjectDrawer.Comments.save',
+    defaultMessage: 'Save',
+  },
+  cancel: {
+    id: 'front.app.containers.Admin.inspirationHub.ProjectDrawer.Comments.cancel',
+    defaultMessage: 'Cancel',
+  },
+});
