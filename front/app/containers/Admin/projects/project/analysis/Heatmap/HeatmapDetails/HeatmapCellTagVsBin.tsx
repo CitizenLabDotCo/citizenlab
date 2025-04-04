13b5d117a9e68c566bb25ddd538b33571fadb2ca
Iva <iva@citizenlab.co>
2025-04-04 22:12:38 +0300

    Fix for option type

--- front/app/containers/Admin/projects/project/analysis/Heatmap/HeatmapDetails/HeatmapCellTagVsBin.tsx
+++ front/app/containers/Admin/projects/project/analysis/Heatmap/HeatmapDetails/HeatmapCellTagVsBin.tsx
@@ -12,14 +12,11 @@ import {
 } from '@citizenlab/cl2-component-library';
 
 import { IAnalysisHeatmapCellData } from 'api/analysis_heat_map_cells/types';
-import {
-  AuthorCustomFilterKey,
-  InputCustomFilterKey,
-} from 'api/analysis_inputs/types';
 import useAddAnalysisSummary from 'api/analysis_summaries/useAddAnalysisSummary';
 import { ITagData } from 'api/analysis_tags/types';
 import { ICustomFieldBinData } from 'api/custom_field_bins/types';
 import useUserCustomFields from 'api/user_custom_fields/useUserCustomFields';
+import useUserCustomFieldsOption from 'api/user_custom_fields_options/useUserCustomFieldsOption';
 
 import useLocalize from 'hooks/useLocalize';
 
@@ -32,7 +29,6 @@ import {
   getCellBgColor,
   getCellTextColor,
 } from './utils';
-
 interface Props {
   cell?: IAnalysisHeatmapCellData;
   row: ITagData | ICustomFieldBinData;
@@ -51,54 +47,101 @@ const SummarizeButton = ({
   const { mutate: addSummary } = useAddAnalysisSummary();
   const { data: userCustomFields } = useUserCustomFields();
 
-  const userCustomFieldsIds = userCustomFields?.data.map(
-    (customField) => customField.id
-  );
+  const userCustomFieldsIds =
+    userCustomFields?.data.map((customField) => customField.id) || [];
 
-  const handleSummarize = () => {
-    if (column.relationships.custom_field_option === null || !cell) return;
+  const isRowOptionBin =
+    'custom_field' in row.relationships &&
+    'type' in row.attributes &&
+    row.attributes.type === 'CustomFieldBins::OptionBin';
 
-    const formFilters = (bin: ITagData | ICustomFieldBinData) => {
-      // Handle Tag type
-      if (bin.type === 'tag') {
-        return { tag_ids: [bin.id] };
-      }
+  const rowCustomFieldId =
+    'custom_field' in row.relationships
+      ? row.relationships.custom_field.data?.id || ''
+      : '';
 
-      // For custom field bins
-      const fieldId = bin.relationships.custom_field.data.id;
-      const isUserField = userCustomFieldsIds?.includes(fieldId);
-      const keyPrefix = isUserField ? 'author_custom_' : 'input_custom_';
-      const isRangeType = [
-        'CustomFieldBins::AgeBin',
-        'CustomFieldBins::RangeBin',
-      ].includes(bin.attributes.type);
+  const rowOptionId =
+    'custom_field_option' in row.relationships &&
+    row.relationships.custom_field_option?.data
+      ? row.relationships.custom_field_option.data.id
+      : '';
 
-      if (isRangeType) {
-        // For range type bins
-        const fromKey = `${keyPrefix}${fieldId}_from`;
-        const toKey = `${keyPrefix}${fieldId}_to`;
+  const { data: rowCustomFieldOption } = useUserCustomFieldsOption({
+    customFieldId: rowCustomFieldId,
+    optionId: rowOptionId,
+    enabled: isRowOptionBin,
+  });
 
+  const { data: columnCustomFieldOption } = useUserCustomFieldsOption({
+    customFieldId: column.relationships.custom_field.data?.id || '',
+    optionId: column.relationships.custom_field_option?.data?.id || '',
+    enabled: column.attributes.type === 'CustomFieldBins::OptionBin',
+  });
+
+  const formFilters = (
+    bin: ITagData | ICustomFieldBinData,
+    type: 'row' | 'column'
+  ) => {
+    // Handle Tag type
+    if (bin.type === 'tag') {
+      return { tag_ids: [bin.id] };
+    }
+
+    // For custom field bins
+    const fieldId = bin.relationships.custom_field.data?.id || '';
+    const isUserField = userCustomFieldsIds.includes(fieldId);
+    const keyPrefix = isUserField ? 'author_custom_' : 'input_custom_';
+    const binType = bin.attributes.type;
+
+    const isRangeType = [
+      'CustomFieldBins::AgeBin',
+      'CustomFieldBins::RangeBin',
+    ].includes(binType);
+    const isOptionBin = binType === 'CustomFieldBins::OptionBin';
+
+    if (isRangeType) {
+      // For range type bins
+      const fromKey = `${keyPrefix}${fieldId}_from`;
+      const toKey = `${keyPrefix}${fieldId}_to`;
+
+      return {
+        [fromKey]: bin.attributes.range?.begin.toString(),
+        [toKey]: bin.attributes.range?.end?.toString(),
+      };
+    }
+
+    if (isOptionBin) {
+      // For option bins
+      const key = `${keyPrefix}${fieldId}`;
+
+      if (type === 'row') {
         return {
-          [fromKey]: bin.attributes.range?.begin.toString(),
-          [toKey]: bin.attributes.range?.end?.toString(),
+          [key]: rowCustomFieldOption?.data.attributes.key
+            ? [rowCustomFieldOption.data.attributes.key]
+            : undefined,
         };
       }
 
-      // For other bin types
-      const key = `${keyPrefix}${fieldId}`;
       return {
-        [key]:
-          bin.attributes.values?.map((value) => value.toString()) || undefined,
+        [key]: columnCustomFieldOption?.data.attributes.key
+          ? [columnCustomFieldOption.data.attributes.key]
+          : undefined,
       };
-    };
+    }
 
-    const filters: {
-      tag_ids?: string[];
-      [authorKey: AuthorCustomFilterKey]: string | string[] | undefined;
-      [inputKey: InputCustomFilterKey]: string | string[] | undefined;
-    } = {
-      ...formFilters(row),
-      ...formFilters(column),
+    // For other bin types
+    const key = `${keyPrefix}${fieldId}`;
+    return {
+      [key]: bin.attributes.values?.map((value) => value.toString()),
+    };
+  };
+
+  const handleSummarize = () => {
+    if (!cell || column.relationships.custom_field_option === null) return;
+
+    const filters = {
+      ...formFilters(row, 'row'),
+      ...formFilters(column, 'column'),
     };
 
     addSummary({
@@ -107,11 +150,14 @@ const SummarizeButton = ({
     });
   };
 
+  const isDisabled = !cell || column.relationships.custom_field_option === null;
+
   return (
     <Button
       buttonStyle="secondary-outlined"
       icon="stars"
-      onClick={() => handleSummarize()}
+      onClick={handleSummarize}
+      disabled={isDisabled}
     >
       <FormattedMessage {...messages.summarize} />
     </Button>
