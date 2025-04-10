c143f36b93c42b1e29548a6f8f0fe465a1ad0c59
jamesspeake <james@speake.org>
2025-04-10 13:18:15 +0100

    [TAN-4228] Rubocop fixes

--- back/engines/commercial/multi_tenancy/db/seeds/community_monitor.rb
+++ back/engines/commercial/multi_tenancy/db/seeds/community_monitor.rb
@@ -14,22 +14,22 @@ module MultiTenancy
         runner.num_ideas.times { create_survey_response(Time.zone.now) }
 
         # Responses for previous quarter
-        runner.num_ideas.times { create_survey_response(Time.zone.now - 3.months) }
+        runner.num_ideas.times { create_survey_response(3.months.ago) }
       end
 
       private
 
       def create_survey_response(created_at)
-        response = @question_keys.each_with_object({}) { |k, accu| accu[k] = rand(1..5) }
+        response = @question_keys.index_with { |_k| rand(1..5) }
         Idea.create!({
-                              # idea_status: runner.rand_instance(IdeaStatus.for_public_posts),
-                              project: @project,
-                              phases: [@phase],
-                              creation_phase: @phase,
-                              publication_status: 'published',
-                              created_at: created_at,
-                              custom_field_values: response
-                            })
+          # idea_status: runner.rand_instance(IdeaStatus.for_public_posts),
+          project: @project,
+          phases: [@phase],
+          creation_phase: @phase,
+          publication_status: 'published',
+          created_at: created_at,
+          custom_field_values: response
+        })
       end
     end
   end
