3d36e764faf574ca1fcec9aabe96ac5ffae5a841
jamesspeake <james@speake.org>
2025-04-10 13:41:16 +0100

    [TAN-4228] Added followup responses for community monitor seeds

--- back/engines/commercial/multi_tenancy/db/seeds/community_monitor.rb
+++ back/engines/commercial/multi_tenancy/db/seeds/community_monitor.rb
@@ -20,15 +20,16 @@ module MultiTenancy
       private
 
       def create_survey_response(created_at)
-        response = @question_keys.index_with { |_k| rand(1..5) }
+        answers = @question_keys.index_with { |_k| rand(1..5) }
+        follow_ups = @question_keys.each_with_object({}) { |k, accu| accu[k + '_follow_up'] = Faker::Movie.quote }
+
         Idea.create!({
-          # idea_status: runner.rand_instance(IdeaStatus.for_public_posts),
           project: @project,
           phases: [@phase],
           creation_phase: @phase,
           publication_status: 'published',
           created_at: created_at,
-          custom_field_values: response
+          custom_field_values: answers.merge(follow_ups),
         })
       end
     end
