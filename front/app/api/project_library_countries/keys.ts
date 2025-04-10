628040d432ea73e5bff5790a9648f56ab541a69f
jinjagit <jinjamail@gmail.com>
2025-04-10 16:39:04 +0100

    [TAN-4341] Don't change type name

--- front/app/api/project_library_countries/keys.ts
+++ front/app/api/project_library_countries/keys.ts
@@ -1,6 +1,6 @@
 const projectLibraryCountriesKeys = {
   all: (locale?: string) =>
-    ['project_library_countries', ...(locale ? [locale] : [])] as const,
+    ['project_library_tenant_countries', ...(locale ? [locale] : [])] as const,
 };
 
 export default projectLibraryCountriesKeys;
