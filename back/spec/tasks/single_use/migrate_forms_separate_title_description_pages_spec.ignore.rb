43ca60ef97e41e5db69e4cab74ad9c4ef87d11db
Sébastien Hoorens <sebastien@citizenlab.co>
2025-04-14 14:40:01 +0200

    added spec for preserving copy after migration

--- back/spec/tasks/single_use/migrate_forms_separate_title_description_pages_spec.ignore.rb
+++ back/spec/tasks/single_use/migrate_forms_separate_title_description_pages_spec.ignore.rb
@@ -71,7 +71,140 @@ describe 'migrate_custom_forms:separate_title_body_pages rake task' do
     expect(form.reload.custom_fields.pluck(:code)).to eq %w[uploads_page title_page details_page]
   end
 
-  # TODO: Test preservation of custom copy (title and description)
+  it 'Preserves custom copy of pages in the migrated form' do
+    form1 = create(:custom_form) # title_page title page body page
+    create_field_before(:title_page, form1).update_columns(
+      title_multiloc: { 'en' => 'Custom title page title' },
+      description_multiloc: { 'en' => 'Custom title page description' }
+    )
+    create_field_before(:title, form1)
+    create_field_before(:page, form1).update!(
+      title_multiloc: { 'en' => 'Custom body page title' },
+      description_multiloc: { 'en' => 'Custom body page description' }
+    )
+    create_field_before(:body, form1)
+    create_field_before(:page, form1).update!(
+      title_multiloc: { 'en' => 'Custom end page title' },
+      description_multiloc: { 'en' => 'Custom end page description' }
+    )
+
+    form2 = create(:custom_form) # page title body extra page
+    create_field_before(:page, form2).update_columns(
+      title_multiloc: { 'en' => 'Custom first page title' },
+      description_multiloc: { 'en' => 'Custom first page description' }
+    )
+    create_field_before(:title, form2)
+    create_field_before(:body, form2)
+    create_field_before(:extra, form2)
+    create_field_before(:page, form2).update!(
+      title_multiloc: { 'en' => 'Custom end page title' },
+      description_multiloc: { 'en' => 'Custom end page description' }
+    )
+
+    form3 = create(:custom_form) # title_page extra title body page
+    create_field_before(:title_page, form3).update_columns(
+      title_multiloc: { 'en' => 'Custom title page title' },
+      description_multiloc: { 'en' => 'Custom title page description' }
+    )
+    create_field_before(:extra, form3)
+    create_field_before(:title, form3)
+    create_field_before(:body, form3)
+    create_field_before(:page, form3).update!(
+      title_multiloc: { 'en' => 'Custom end page title' },
+      description_multiloc: { 'en' => 'Custom end page description' }
+    )
+
+    form4 = create(:custom_form) # page extra title body page
+    create_field_before(:page, form4).update_columns(
+      title_multiloc: { 'en' => 'Custom first page title' },
+      description_multiloc: { 'en' => 'Custom first page description' }
+    )
+    create_field_before(:extra, form4)
+    create_field_before(:title, form4)
+    create_field_before(:body, form4)
+    create_field_before(:page, form4).update!(
+      title_multiloc: { 'en' => 'Custom end page title' },
+      description_multiloc: { 'en' => 'Custom end page description' }
+    )
+
+    Rake::Task['migrate_custom_forms:separate_title_body_pages'].invoke
+
+    expect(form1.reload.custom_fields.pluck(:code)).to eq ['title_page', 'title_multiloc', 'body_page', 'body_multiloc', nil]
+    expect(form1.custom_fields.map(&:title_multiloc)).to match [
+      hash_including('en' => 'What is your idea?'),
+      anything,
+      { 'en' => 'Custom body page title' },
+      anything,
+      { 'en' => 'Custom end page title' }
+    ]
+    expect(form1.custom_fields.map(&:description_multiloc)).to match [
+      { 'en' => 'Custom title page description' },
+      anything,
+      { 'en' => 'Custom body page description' },
+      anything,
+      { 'en' => 'Custom end page description' }
+    ]
+
+    expect(form2.reload.custom_fields.pluck(:code)).to eq ['title_page', 'title_multiloc', 'body_page', 'body_multiloc', nil, nil, nil]
+    expect(form2.custom_fields.map(&:title_multiloc)).to match [
+      hash_including('en' => 'What is your idea?'),
+      anything,
+      hash_including('en' => 'Tell us more'),
+      anything,
+      anything,
+      anything,
+      { 'en' => 'Custom end page title' }
+    ]
+    expect(form2.custom_fields.map(&:description_multiloc)).to match [
+      { 'en' => 'Custom first page description' },
+      anything,
+      {},
+      anything,
+      anything,
+      anything,
+      { 'en' => 'Custom end page description' }
+    ]
+
+    expect(form3.reload.custom_fields.pluck(:code)).to eq [nil, nil, 'title_page', 'title_multiloc', 'body_page', 'body_multiloc', nil]
+    expect(form3.custom_fields.map(&:title_multiloc)).to match [
+      anything,
+      anything,
+      hash_including('en' => 'What is your idea?'),
+      anything,
+      hash_including('en' => 'Tell us more'),
+      anything,
+      { 'en' => 'Custom end page title' }
+    ]
+    expect(form3.custom_fields.map(&:description_multiloc)).to match [
+      anything,
+      anything,
+      { 'en' => 'Custom title page description' },
+      anything,
+      {},
+      anything,
+      { 'en' => 'Custom end page description' }
+    ]
+
+    expect(form4.reload.custom_fields.pluck(:code)).to eq [nil, nil, 'title_page', 'title_multiloc', 'body_page', 'body_multiloc', nil]
+    expect(form4.custom_fields.map(&:title_multiloc)).to match [
+      { 'en' => 'Custom first page title' },
+      anything,
+      hash_including('en' => 'What is your idea?'),
+      anything,
+      hash_including('en' => 'Tell us more'),
+      anything,
+      { 'en' => 'Custom end page title' }
+    ]
+    expect(form4.custom_fields.map(&:description_multiloc)).to match [
+      { 'en' => 'Custom first page description' },
+      anything,
+      anything,
+      anything,
+      {},
+      anything,
+      { 'en' => 'Custom end page description' }
+    ]
+  end
 
   def create_field_before(sym, form)
     case sym
