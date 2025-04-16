9b6f8b22d01d8815b658c1ed9c77e93232503c80
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-04-16 11:18:48 -0300

    Always show sidebar nav item

--- front/app/containers/Admin/sideBar/navItems.ts
+++ front/app/containers/Admin/sideBar/navItems.ts
@@ -86,7 +86,6 @@ const getDefaultNavItems = ({ data }: IAppConfiguration): NavItem[] => {
       link: getInspirationHubLink(country_code),
       iconName: 'globe',
       message: 'inspirationHub',
-      featureNames: ['project_library'],
     },
     {
       name: 'tools',
