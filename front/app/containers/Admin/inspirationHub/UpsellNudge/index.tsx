7ece8d95108fb9d00010ab71d94e2b80c44e1cd3
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2025-04-16 11:16:43 -0300

    Copy over upsell nudge from community monitor

--- front/app/containers/Admin/inspirationHub/UpsellNudge/index.tsx
+++ front/app/containers/Admin/inspirationHub/UpsellNudge/index.tsx
@@ -1,7 +1,66 @@
 import React from 'react';
 
+import {
+  Box,
+  Title,
+  Text,
+  colors,
+  stylingConsts,
+  Button,
+  Tooltip,
+} from '@citizenlab/cl2-component-library';
+import { useTheme } from 'styled-components';
+
+import { useIntl } from 'utils/cl-intl';
+
+import messages from './messages';
+
 const UpsellNudge = () => {
-  return <>Upsell</>;
+  const theme = useTheme();
+  const { formatMessage } = useIntl();
+
+  const upsellBulletPoints = [
+    messages.bulletPoint1,
+    messages.bulletPoint2,
+    messages.bulletPoint3,
+  ];
+
+  return (
+    <Box
+      p="32px"
+      background={colors.white}
+      borderRadius={stylingConsts.borderRadius}
+      display="flex"
+      gap="32px"
+      maxWidth="960px"
+    >
+      <Box maxWidth="480px">
+        <Title>{formatMessage(messages.upsellTitle)}</Title>
+        <Text>{formatMessage(messages.upsellDescription)}</Text>
+        {upsellBulletPoints.map((message, index) => (
+          <Box display="flex" key={index} alignItems="center">
+            <Box mr="4px">✅</Box>
+            <Text m="0px">{formatMessage(message)}</Text>
+          </Box>
+        ))}
+        <Box mt="28px" display="flex" gap="12px">
+          <Tooltip
+            disabled={false}
+            content={formatMessage(messages.featureNotIncluded)}
+          >
+            <Button disabled={true} icon="lock">
+              {formatMessage(messages.enableInspirationHub)}
+            </Button>
+          </Tooltip>
+
+          <Button buttonStyle="text" textColor={theme.colors.tenantPrimary}>
+            {formatMessage(messages.learnMore)}
+          </Button>
+        </Box>
+      </Box>
+      <Box>TODO</Box>
+    </Box>
+  );
 };
 
 export default UpsellNudge;
