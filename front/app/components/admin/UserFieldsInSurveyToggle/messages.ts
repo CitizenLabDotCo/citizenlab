02419d7675ae0feacee881b3cce1a27b7720087b
jamesspeake <james@speake.org>
2025-04-17 17:20:57 +0100

    Added user fields in form toggle to community monitor

---
+++ front/app/components/admin/UserFieldsInSurveyToggle/messages.ts
@@ -0,0 +1,17 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  userFieldsInSurveyTitle: {
+    id: 'app.containers.AdminPage.ProjectEdit.NativeSurvey.userFieldsInSurveyTitle2',
+    defaultMessage: 'Demographic fields in survey form',
+  },
+  userFieldsInSurveyToggle: {
+    id: 'app.containers.AdminPage.ProjectEdit.NativeSurvey.userFieldsInSurveyToggle2',
+    defaultMessage: 'Show demographic fields in survey?',
+  },
+  userFieldsInSurveyDescription: {
+    id: 'app.containers.AdminPage.ProjectEdit.NativeSurvey.userFieldsInSurveyDescription2',
+    defaultMessage:
+      'If you enable this option, user registration fields will be shown as the last page in the survey instead of as part of the signup process.',
+  },
+});
--- front/app/containers/Admin/projects/project/phaseSetup/components/PhaseParticipationConfig/components/messages.ts
+++ front/app/containers/Admin/projects/project/phaseSetup/components/PhaseParticipationConfig/components/messages.ts
@@ -192,17 +192,4 @@ export default defineMessages({
     defaultMessage:
       'This controls how similar two descriptions must be to show suggestions. Use values between 0 and 1. Only adjust if you know what you’re doing.',
   },
-  userFieldsInSurveyTitle: {
-    id: 'app.containers.AdminPage.ProjectEdit.NativeSurvey.userFieldsInSurveyTitle2',
-    defaultMessage: 'Demographic fields in survey form',
-  },
-  userFieldsInSurveyToggle: {
-    id: 'app.containers.AdminPage.ProjectEdit.NativeSurvey.userFieldsInSurveyToggle2',
-    defaultMessage: 'Show demographic fields in survey?',
-  },
-  userFieldsInSurveyDescription: {
-    id: 'app.containers.AdminPage.ProjectEdit.NativeSurvey.userFieldsInSurveyDescription2',
-    defaultMessage:
-      'If you enable this option, user registration fields will be shown as the last page in the survey instead of as part of the signup process.',
-  },
 });
