b6c166d0520f19be00d2d0ac3559bc41c5e5310f
EdwinKato <katedwin88@gmail.com>
2025-05-06 21:41:39 +0300

    Add CommonGround types for results and progress management

---
+++ front/app/api/common_ground/types.ts
@@ -0,0 +1,87 @@
+import { IRelationship, Multiloc } from 'typings';
+
+import { Keys } from 'utils/cl-react-query/types';
+
+import commonGroundProgressKeys from './keys/commonGroundProgressKeys';
+import commonGroundResultsKeys from './keys/commonGroundResultsKeys';
+
+export type CommonGroundResultsKeys = Keys<typeof commonGroundResultsKeys>;
+
+export type CommonGroundProgressKeys = Keys<typeof commonGroundProgressKeys>;
+
+export interface CommonGroundResultItem {
+  label: Multiloc;
+  agree: number;
+  unsure: number;
+  disagree: number;
+  total: number;
+}
+
+export interface CommonGroundResultsData {
+  id: string;
+  type: 'phase-results';
+  attributes: {
+    numParticipants: number;
+    numStatements: number;
+    numVotes: number;
+    majority: CommonGroundResultItem[];
+    divisive: CommonGroundResultItem[];
+  };
+}
+
+export interface CommonGroundStatement {
+  id: string;
+  type: 'statement';
+  attributes: {
+    author: string;
+    publishedAt: string;
+    body: Multiloc;
+    reactions: {
+      agree: number;
+      unsure: number;
+      disagree: number;
+    };
+  };
+}
+
+export interface CommonGroundProgressStatement {
+  id: string;
+  author: string;
+  publishedAt: string;
+  body: Multiloc;
+}
+
+export interface ProgressData {
+  id: string;
+  type: 'phase-progress';
+  attributes: {
+    numIdeas: number;
+    numIdeasReacted: number;
+    nextIdea: CommonGroundProgressStatement | null;
+  };
+  relationships: {
+    next_idea: {
+      data: IRelationship[];
+    };
+  };
+}
+
+export type CommonGroundReactionMode = 'agree' | 'unsure' | 'disagree';
+
+export interface ICommonGroundResults {
+  data: CommonGroundResultsData;
+}
+
+export interface ICommonGroundStatements {
+  data: CommonGroundStatement[];
+}
+
+export interface ICommonGroundProgress {
+  data: ProgressData;
+}
+
+export interface ReactToIdeaObject {
+  phaseId?: string | undefined;
+  ideaId: string;
+  mode: CommonGroundReactionMode;
+}
