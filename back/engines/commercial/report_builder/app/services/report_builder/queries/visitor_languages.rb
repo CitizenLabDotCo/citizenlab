6a97eea71151201bc88e9bea0763d063b20780fa
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-05-07 19:13:04 -0300

    Scaffold query languages widget

---
+++ back/engines/commercial/report_builder/app/services/report_builder/queries/visitor_languages.rb
@@ -0,0 +1,13 @@
+module ReportBuilder
+  class Queries::Visitors < ReportBuilder::Queries::Base
+    def run_query(
+      start_at: nil,
+      end_at: nil,
+      project_id: nil,
+      exclude_roles: nil,
+      **_other_props
+    )
+      # TODO
+    end
+  end
+end
