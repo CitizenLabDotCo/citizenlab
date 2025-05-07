a3099df6514486c0128fa3a7c46bcae74fa16478
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-05-07 19:00:19 -0300

    Work on query (WIP)

--- back/engines/commercial/report_builder/app/services/report_builder/queries/visitor_types.rb
+++ back/engines/commercial/report_builder/app/services/report_builder/queries/visitor_types.rb
@@ -7,7 +7,32 @@ module ReportBuilder
       exclude_roles: nil,
       **_other_props
     )
-      # TODO
+      start_date, end_date = TimeBoundariesParser.new(start_at, end_at).parse
+
+      sessions = ImpactTracking::Session.where(created_at: start_date..end_date)
+      sessions = apply_project_filter_if_needed(sessions, project_id)
+      sessions = exclude_roles_if_needed(sessions, exclude_roles)
+
+      sessions
+        .group(:visitor_)
+    end
+
+    def apply_project_filter_if_needed(sessions, project_id)
+      if project_id.present?
+        sessions_with_project = ImpactTracking::Pageview.where(project_id: project_id).select(:session_id)
+        sessions = sessions.where(id: sessions_with_project)
+      end
+
+      sessions
+    end
+
+    def exclude_roles_if_needed(sessions, exclude_roles)
+      if exclude_roles == 'exclude_admins_and_moderators'
+        sessions = sessions
+          .where("highest_role IS NULL OR highest_role = 'user'")
+      end
+
+      sessions
     end
   end
 end
