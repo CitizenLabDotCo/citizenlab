9806b63229c4fa2626ed6b8e67dcbc5fe88e55bb
Adrien Dessy <adrien@citizenlab.co>
2025-05-08 18:13:07 +0200

    [TAN-4519] Set `supports_public_visibility?` to true in CommonGround

--- back/lib/participation_method/common_ground.rb
+++ back/lib/participation_method/common_ground.rb
@@ -21,6 +21,17 @@ module ParticipationMethod
       input.idea_status ||= IdeaStatus.find_by!(code: 'proposed', participation_method: 'ideation')
     end
 
+    # This is necessary in order to be able to retrieve the inputs using the +IdeasFinder+
+    # class, which excludes inputs that aren't publicly visible. While this isn't a great
+    # reason to set the flag to true, it doesn't cause too much harm. The main issue with
+    # this approach is that it makes the ideas indexable, even though we don't necessarily
+    # want the users to be able to bypass the routing logic of the Common Ground
+    # participation method and access the ideas directly.
+    # [TODO] The behaviour of the +IdeasFinder+ class could potentially be reworked.
+    def supports_public_visibility?
+      true
+    end
+
     def default_fields(custom_form)
       multiloc_service = MultilocService.new
 
