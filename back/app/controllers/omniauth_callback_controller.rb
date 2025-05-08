a39bf21b8a2c03e689a7a7f3a3da0f6fcfbddd27
jinjagit <jinjamail@gmail.com>
2025-05-08 14:22:58 +0100

    [TAN-4535] Use sameSite: 'Lax' for SSO

--- back/app/controllers/omniauth_callback_controller.rb
+++ back/app/controllers/omniauth_callback_controller.rb
@@ -203,7 +203,7 @@ class OmniauthCallbackController < ApplicationController
       value: auth_token(@user, provider).token,
       expires: 1.month.from_now,
       secure: !Rails.env.local?,
-      same_site: 'Strict'
+      same_site: 'Lax' # Strict won't work due to SSO redirect, so we explicitly document use of Lax
     }
   end
 
