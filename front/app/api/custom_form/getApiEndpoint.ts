26bacf208f1be7ba8b3367a360ab6639a0018d0f
brentguf <brentguf@users.noreply.github.com>
2025-05-08 15:40:05 +0200

    Extend getApiEndpoint comment

--- front/app/api/custom_form/getApiEndpoint.ts
+++ front/app/api/custom_form/getApiEndpoint.ts
@@ -3,7 +3,8 @@ import { IPhaseData } from 'api/phases/types';
 const getApiEndpoint = (phase: IPhaseData) => {
   /* For Ideation & voting, when you configure the form it gets applied to ALL ideation/voting phases within a project, 
   however when you configure the form for surveys for example, 
-  each survey phase within a project can have a different form */
+  each survey phase within a project can have a different form. 
+  Hence we call different endpoints depending on the participation method. */
   return phase.attributes.participation_method === 'ideation' ||
     phase.attributes.participation_method === 'voting'
     ? `projects/${phase.relationships.project.data.id}/custom_form`
