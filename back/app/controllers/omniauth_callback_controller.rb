d7721db544e158b662c3cbe1ad37d8bbe7b07980
Simon Tharby <simontharby@gmail.com>
2025-05-08 13:06:44 +0100

    Revert "[TAN-4535] Set SameSite and Secure cookie headers"

--- back/app/controllers/omniauth_callback_controller.rb
+++ back/app/controllers/omniauth_callback_controller.rb
@@ -201,9 +201,7 @@ class OmniauthCallbackController < ApplicationController
   def set_auth_cookie(provider: nil)
     cookies[:cl2_jwt] = {
       value: auth_token(@user, provider).token,
-      expires: 1.month.from_now,
-      secure: !Rails.env.local?,
-      same_site: 'Strict'
+      expires: 1.month.from_now
     }
   end
 
