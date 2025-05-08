2c3895ecb7c06fb98cd623ce2e7a06c1821dcd4b
brentguf <brentguf@users.noreply.github.com>
2025-05-08 15:45:30 +0200

    participationMethod variable

--- front/app/api/custom_form/getApiEndpoint.ts
+++ front/app/api/custom_form/getApiEndpoint.ts
@@ -1,12 +1,13 @@
 import { IPhaseData } from 'api/phases/types';
 
 const getApiEndpoint = (phase: IPhaseData) => {
+  const participationMethod = phase.attributes.participation_method;
+
   /* For Ideation & voting, when you configure the form it gets applied to ALL ideation/voting phases within a project, 
   however when you configure the form for surveys for example, 
   each survey phase within a project can have a different form. 
   Hence we call different endpoints depending on the participation method. */
-  return phase.attributes.participation_method === 'ideation' ||
-    phase.attributes.participation_method === 'voting'
+  return participationMethod === 'ideation' || participationMethod === 'voting'
     ? `projects/${phase.relationships.project.data.id}/custom_form`
     : `phases/${phase.id}/custom_form`;
 };
