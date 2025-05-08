7dd6828a46b38d0a63ac330862a9c0d44dde0d8d
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-05-08 09:59:20 -0300

    Calculate locales from path

--- back/engines/commercial/report_builder/app/services/report_builder/queries/visitor_languages.rb
+++ back/engines/commercial/report_builder/app/services/report_builder/queries/visitor_languages.rb
@@ -6,12 +6,25 @@ module ReportBuilder
       project_id: nil,
       exclude_roles: nil,
       **_other_props
-    )
-      locales = CL2_SUPPORTED_LOCALES.map(&:to_s)
-      
+    ) 
       start_date, end_date = TimeBoundariesParser.new(start_at, end_at).parse
 
-      # TODO
+      pageviews = ImpactTracking::Pageview
+        .where(created_at: start_date..end_date)
+
+      if project_id.present?
+        pageviews = pageviews.where(project_id: project_id)
+      end
+
+      # TODO exclude_roles
+
+      cases = CL2_SUPPORTED_LOCALES.map do |locale|
+        "WHEN starts_with(path, '/#{locale}') THEN '#{locale}'"    
+      end
+
+      locales = pageviews
+        .select("CASE #{cases} ELSE NULL END AS locale")
+        .where('locale IS NOT NULL')
     end
   end
 end
