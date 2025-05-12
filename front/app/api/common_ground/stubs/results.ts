ada5e7895f7c8bfdc7077aaf813759c34eac925e
EdwinKato <katedwin88@gmail.com>
2025-05-12 11:01:32 +0300

    Add sample data to results

--- front/app/api/common_ground/stubs/results.ts
+++ front/app/api/common_ground/stubs/results.ts
@@ -5,25 +5,117 @@ export const sampleCommonGroundResults: ICommonGroundResults = {
     id: 'stub-phase-results-1',
     type: 'phase-results',
     attributes: {
-      numParticipants: 120,
-      numStatements: 2,
-      numVotes: 30,
+      numParticipants: 121,
+      numStatements: 731,
+      numVotes: 10827,
       majority: [
         {
-          label: { en: 'Invest in public transport.' },
-          agree: 80,
-          unsure: 10,
-          disagree: 10,
-          total: 100,
+          label: {
+            en: 'Older adults need safer sidewalks, benches, and public toilets.',
+          },
+          agree: 43,
+          unsure: 41,
+          disagree: 15,
+          total: 324,
+        },
+        {
+          label: {
+            en: 'Coping with rent discourages landlords from investing or improving their properties.',
+          },
+          agree: 77,
+          unsure: 14,
+          disagree: 8,
+          total: 117,
+        },
+        {
+          label: {
+            en: 'The city should convert more vacant buildings into affordable housing units.',
+          },
+          agree: 83,
+          unsure: 11,
+          disagree: 5,
+          total: 111,
         },
       ],
       divisive: [
         {
-          label: { en: 'Pedestrianize the city center.' },
-          agree: 50,
-          unsure: 50,
-          disagree: 0,
-          total: 100,
+          label: { en: 'Rent freezes unfairly restrict property owners.' },
+          agree: 44,
+          unsure: 12,
+          disagree: 44,
+          total: 50,
+        },
+        {
+          label: {
+            en: 'Coping with rent discourages landlords from investing or improving their properties.',
+          },
+          agree: 77,
+          unsure: 14,
+          disagree: 8,
+          total: 117,
+        },
+        {
+          label: {
+            en: 'The city should convert more vacant buildings into affordable housing units.',
+          },
+          agree: 83,
+          unsure: 11,
+          disagree: 5,
+          total: 111,
+        },
+      ],
+      uncertain: [
+        {
+          label: { en: 'Rent freezes unfairly restrict property owners.' },
+          agree: 44,
+          unsure: 12,
+          disagree: 44,
+          total: 50,
+        },
+        {
+          label: {
+            en: 'Coping with rent discourages landlords from investing or improving their properties.',
+          },
+          agree: 77,
+          unsure: 14,
+          disagree: 8,
+          total: 117,
+        },
+        {
+          label: {
+            en: 'The city should convert more vacant buildings into affordable housing units.',
+          },
+          agree: 83,
+          unsure: 11,
+          disagree: 5,
+          total: 111,
+        },
+      ],
+      allStatements: [
+        {
+          label: { en: 'Rent freezes unfairly restrict property owners.' },
+          agree: 44,
+          unsure: 12,
+          disagree: 44,
+          total: 50,
+        },
+        {
+          label: {
+            en: 'Coping with rent discourages landlords from investing or improving their properties.',
+          },
+          agree: 77,
+          unsure: 14,
+          disagree: 8,
+          total: 117,
+        },
+        {
+          label: {
+            en: 'The city should convert more vacant buildings into affordable housing units.',
+          },
+          agree: 83,
+          unsure: 11,
+          disagree: 5,
+          total: 111,
         },
       ],
     },
