bb71130ab7d13c9fb39d363c7296f31b3378a4a4
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-05-12 14:48:48 -0300

    Add more tests

--- back/engines/commercial/report_builder/spec/services/report_builder/queries/device_types_spec.rb
+++ back/engines/commercial/report_builder/spec/services/report_builder/queries/device_types_spec.rb
@@ -32,5 +32,59 @@ RSpec.describe ReportBuilder::Queries::DeviceTypes do
         }
       })
     end
+
+    it 'filters by project' do
+      project = create(:project_with_active_ideation_phase)
+      project_path = "/en/projects/#{project.slug}"
+      project_id = project.id
+
+      2.times do
+        session1 = create(:session, created_at: Date.new(2023, 2, 1), device_type: 'desktop_or_other')
+        create(:pageview, session_id: session1.id, path: '/en/')
+      end
+
+      3.times do
+        session2 = create(:session, created_at: Date.new(2023, 2, 1), device_type: 'mobile')
+        create(:pageview, session_id: session2.id, path: project_path, project_id: project_id)
+      end
+
+      params = {
+        start_at: Date.new(2023, 1, 1),
+        end_at: Date.new(2023, 3, 1),
+        project_id: project_id
+      }
+
+      result = expect(query.run_query(**params)).to eq({
+        counts_per_device_type: {
+          'mobile' => 3
+        }
+      })
+    end
+
+    it 'applies exclude_roles filter' do
+      2.times do
+        create(:session, created_at: Date.new(2023, 2, 1), device_type: 'desktop_or_other', highest_role: 'admin')
+      end
+
+      3.times do
+        create(:session, created_at: Date.new(2023, 2, 1), device_type: 'mobile')
+      end
+
+      params = {
+        start_at: Date.new(2023, 1, 1),
+        end_at: Date.new(2023, 3, 1),
+        exclude_roles: 'exclude_admins_and_moderators'
+      }
+
+      result = expect(query.run_query(**params)).to eq({
+        counts_per_device_type: {
+          'mobile' => 3
+        }
+      })
+    end
+
+    # it 'works if everything is nil' do
+      # TODO
+    # end
   end
 end
