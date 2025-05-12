ed73882ded20869eee77bc26381850d00cf13ab5
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-05-12 17:13:54 -0300

    Scaffold DeviceTypesCard

---
+++ front/app/containers/Admin/dashboard/visitors/DeviceTypesCard/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  title: {
+    id: 'app.modules.commercial.analytics.admin.components.DeviceTypesCard.title',
+    defaultMessage: 'Device Types',
+  },
+});
---
+++ front/app/containers/Admin/dashboard/visitors/DeviceTypesCard/useDeviceTypes.ts
@@ -0,0 +1,5 @@
+const useDeviceTypes = () => {
+  return {};
+};
+
+export default useDeviceTypes;
