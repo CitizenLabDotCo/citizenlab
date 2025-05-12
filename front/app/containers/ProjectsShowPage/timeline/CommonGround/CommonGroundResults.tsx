7cd35b37bb6fbc84393a7ecc56088d3652d15c9a
EdwinKato <katedwin88@gmail.com>
2025-05-12 11:52:55 +0300

    Make the results look like the designs

--- front/app/containers/ProjectsShowPage/timeline/CommonGround/CommonGroundResults.tsx
+++ front/app/containers/ProjectsShowPage/timeline/CommonGround/CommonGroundResults.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import {
   Box,
+  Divider,
   Title,
-  defaultCardStyle,
   useBreakpoint,
 } from '@citizenlab/cl2-component-library';
-import styled from 'styled-components';
 
+import { CommonGroundResultItem } from 'api/common_ground/types';
 import useCommonGroundResults from 'api/common_ground/useCommonGroundResults';
 
 import T from 'components/T';
@@ -16,12 +16,6 @@ import { useIntl } from 'utils/cl-intl';
 
 import messages from './messages';
 import OutcomeBreakdownBar from './OutcomeBreakdownBar';
-
-const ResultCard = styled.div`
-  ${defaultCardStyle};
-  padding: 16px;
-  margin-bottom: 16px;
-`;
 
 interface Props {
   phaseId: string;
@@ -36,40 +30,45 @@ const CommonGroundResults = ({ phaseId }: Props) => {
   const { numParticipants, numStatements, numVotes, majority, divisive } =
     results.data.attributes;
 
-  const renderStatementList = (title: string, items: typeof majority) => (
-    <Box mb="24px">
-      <Title variant="h3">{title}</Title>
+  const renderList = (title: string, items: CommonGroundResultItem[]) => (
+    <Box mb="32px">
+      <Title variant="h4">{title}</Title>
       {items.map((item, index) => (
-        <ResultCard key={index}>
-          <Box
-            display="flex"
-            flexDirection={isPhone ? 'column' : 'row'}
-            justifyContent="space-between"
-            alignItems={isPhone ? 'flex-start' : 'center'}
-          >
+        <React.Fragment key={index}>
+          <Box py="12px">
             <Box
-              width={isPhone ? '100%' : 'calc(100% - 182px)'}
-              mb={isPhone ? '8px' : undefined}
-              mr={isPhone ? undefined : '16px'}
+              display="flex"
+              flexDirection={isPhone ? 'column' : 'row'}
+              alignItems={isPhone ? 'flex-start' : 'center'}
+              justifyContent="space-between"
             >
-              <T value={item.label} supportHtml />
-            </Box>
-            <Box width={isPhone ? '100%' : '166px'} flexShrink={0}>
-              <OutcomeBreakdownBar
-                agreedPercent={Math.round((item.agree / item.total) * 100)}
-                unsurePercent={Math.round((item.unsure / item.total) * 100)}
-                disagreePercent={Math.round((item.disagree / item.total) * 100)}
-                totalCount={item.total}
-              />
+              <Box
+                width={isPhone ? '100%' : 'calc(100% - 182px)'}
+                mb={isPhone ? '8px' : undefined}
+                mr={isPhone ? undefined : '16px'}
+              >
+                <T value={item.label} supportHtml />
+              </Box>
+              <Box width="166px" flexShrink={0}>
+                <OutcomeBreakdownBar
+                  agreedPercent={Math.round((item.agree / item.total) * 100)}
+                  unsurePercent={Math.round((item.unsure / item.total) * 100)}
+                  disagreePercent={Math.round(
+                    (item.disagree / item.total) * 100
+                  )}
+                  totalCount={item.total}
+                />
+              </Box>
             </Box>
           </Box>
-        </ResultCard>
+          <Divider m="0px" />
+        </React.Fragment>
       ))}
     </Box>
   );
 
   return (
-    <Box mt="24px">
+    <Box mt="24px" bg="white" p="30px 30px 48px 30px">
       <Box display="flex" mb="24px">
         <Box mr="24px">
           <strong>{formatMessage(messages.participants)}</strong>:{' '}
@@ -83,8 +82,8 @@ const CommonGroundResults = ({ phaseId }: Props) => {
         </Box>
       </Box>
 
-      {renderStatementList(formatMessage(messages.majority), majority)}
-      {renderStatementList(formatMessage(messages.divisive), divisive)}
+      {renderList(formatMessage(messages.majority), majority)}
+      {renderList(formatMessage(messages.divisive), divisive)}
     </Box>
   );
 };
