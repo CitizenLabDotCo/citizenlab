386b7994675fd10007d490f3026d32bc200f85a5
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-05-13 16:07:41 -0300

    Completely replace image

--- front/app/containers/Admin/tools/Workshops/index.tsx
+++ front/app/containers/Admin/tools/Workshops/index.tsx
@@ -9,7 +9,7 @@ import { useIntl } from 'utils/cl-intl';
 
 import messages from '../messages';
 
-import workshopsImage from './workshops.jpg';
+import workshopsImage from './workshops.png';
 
 export const Workshops = () => {
   const { formatMessage } = useIntl();
