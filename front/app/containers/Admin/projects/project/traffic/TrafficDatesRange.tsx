497a22d9b69d1d939f1228a680bf8cc2a375cb03
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-05-13 14:11:32 -0300

    Add resolution control

--- front/app/containers/Admin/projects/project/traffic/TrafficDatesRange.tsx
+++ front/app/containers/Admin/projects/project/traffic/TrafficDatesRange.tsx
@@ -4,6 +4,9 @@ import { Box, Text } from '@citizenlab/cl2-component-library';
 import { useParams } from 'react-router-dom';
 
 import DateRangePicker from 'components/admin/DatePickers/DateRangePicker';
+import ResolutionControl, {
+  IResolution,
+} from 'components/admin/ResolutionControl';
 import Warning from 'components/UI/Warning';
 
 import { useIntl } from 'utils/cl-intl';
@@ -26,6 +29,7 @@ const TrafficDatesRange = ({
 
   const [startAt, setStartAt] = useState(defaultStartDate);
   const [endAt, setEndAt] = useState(defaultEndDate);
+  const [resolution, setResolution] = useState<IResolution>('month');
 
   return (
     <div>
@@ -33,7 +37,7 @@ const TrafficDatesRange = ({
         <Text variant="bodyM" color="textSecondary" mb="5px">
           {formatMessage(messages.selectPeriod)}
         </Text>
-        <Box w="100%" display="flex">
+        <Box w="100%" display="flex" justifyContent="space-between">
           <DateRangePicker
             selectedRange={{
               from: parseBackendDateString(startAt),
@@ -44,6 +48,7 @@ const TrafficDatesRange = ({
               setEndAt(toBackendDateString(to));
             }}
           />
+          <ResolutionControl value={resolution} onChange={setResolution} />
         </Box>
       </Box>
       <Box mx="44px" mb="20px">
@@ -59,7 +64,7 @@ const TrafficDatesRange = ({
           projectId={projectId}
           startAtMoment={moment(startAt)}
           endAtMoment={moment(endAt)}
-          resolution="month"
+          resolution={resolution}
         />
       </Box>
     </div>
