ebb45db56525a29e1d6568386e375b222f459ced
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-05-13 15:50:28 -0300

    Try converting image

--- front/app/containers/Admin/tools/Workshops/index.tsx
+++ front/app/containers/Admin/tools/Workshops/index.tsx
@@ -9,7 +9,7 @@ import { useIntl } from 'utils/cl-intl';
 
 import messages from '../messages';
 
-import workshopsImage from './workshops2.png';
+import workshopsImage from './workshops.jpg';
 
 export const Workshops = () => {
   const { formatMessage } = useIntl();
