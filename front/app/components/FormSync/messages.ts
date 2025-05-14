4c0f84fc31756012e8f626b83ff037c1d0fe979d
brentguf <brentguf@users.noreply.github.com>
2025-05-14 10:16:51 +0200

    Re-add tooltip to Excel template button

--- front/app/components/FormSync/messages.ts
+++ front/app/components/FormSync/messages.ts
@@ -5,4 +5,9 @@ export default defineMessages({
     id: 'app.containers.AdminPage.ProjectIdeaForm.downloadExcelTemplate',
     defaultMessage: 'Download an Excel template',
   },
+  downloadExcelTemplateTooltip: {
+    id: 'app.components.admin.ProjectEdit.survey.downloadExcelTemplateTooltip',
+    defaultMessage:
+      'Excel templates will not include any mapping input questions as these are not supported for bulk importing at this time.',
+  },
 });
