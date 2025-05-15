ebfa5e2057b47d71069fb805e2963adf0639c38b
brentguf <brentguf@users.noreply.github.com>
2025-05-15 16:59:56 +0200

    fix e2e: Add overlooked warning modal to survey edit button

--- front/app/containers/Admin/projects/project/surveyForm/TabPanel.tsx
+++ front/app/containers/Admin/projects/project/surveyForm/TabPanel.tsx
@@ -6,10 +6,10 @@ import { useParams } from 'react-router-dom';
 import { SectionTitle, SectionDescription } from 'components/admin/Section';
 import DownloadPDFButtonWithModal from 'components/FormBuilder/components/FormBuilderTopBar/DownloadPDFButtonWithModal';
 import ExcelDownloadButton from 'components/FormSync/ExcelDownloadButton';
-import Button from 'components/UI/ButtonWithLink';
 
 import { FormattedMessage } from 'utils/cl-intl';
 
+import EditButtonWithWarningModal from './EditButtonWithWarningModal';
 import messages from './messages';
 
 const TabPanel = ({
@@ -28,15 +28,7 @@ const TabPanel = ({
         <FormattedMessage {...messages.inputFormDescription} />
       </SectionDescription>
       <Box display="flex">
-        <Button
-          mr="8px"
-          linkTo={`/admin/projects/${projectId}/phases/${phaseId}/native-survey/edit`}
-          width="auto"
-          icon="edit"
-          data-cy="e2e-edit-survey-form"
-        >
-          <FormattedMessage {...messages.editSurveyForm} />
-        </Button>
+        <EditButtonWithWarningModal projectId={projectId} phaseId={phaseId} />
         <DownloadPDFButtonWithModal
           mr="8px"
           formType="survey"
