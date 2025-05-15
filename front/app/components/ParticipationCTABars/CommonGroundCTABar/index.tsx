5d825a32e53fc4f6e45f13a023cc798b329ba0ff
EdwinKato <katedwin88@gmail.com>
2025-05-15 12:03:14 +0300

    Handle permissions for common ground CTA

--- front/app/components/ParticipationCTABars/CommonGroundCTABar/index.tsx
+++ front/app/components/ParticipationCTABars/CommonGroundCTABar/index.tsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
 import { Button } from '@citizenlab/cl2-component-library';
+import { useLocation } from 'react-router-dom';
 import { useTheme } from 'styled-components';
 
 import { IPhaseData } from 'api/phases/types';
 import { getCurrentPhase, getLastPhase } from 'api/phases/utils';
+
+import { triggerAuthenticationFlow } from 'containers/Authentication/events';
+import { SuccessAction } from 'containers/Authentication/SuccessActions/actions';
+import { scrollTo } from 'containers/Authentication/SuccessActions/actions/scrollTo';
 
 import ParticipationCTAContent from 'components/ParticipationCTABars/ParticipationCTAContent';
 import {
@@ -12,6 +17,7 @@ import {
   hasProjectEndedOrIsArchived,
 } from 'components/ParticipationCTABars/utils';
 
+import { isFixableByAuthentication } from 'utils/actionDescriptors';
 import { FormattedMessage } from 'utils/cl-intl';
 
 import messages from '../messages';
@@ -19,32 +25,79 @@ import messages from '../messages';
 const CommonGroundCTABar = ({ phases, project }: CTABarProps) => {
   const theme = useTheme();
   const [currentPhase, setCurrentPhase] = useState<IPhaseData | undefined>();
+  const { pathname, hash: divId } = useLocation();
 
   useEffect(() => {
     setCurrentPhase(getCurrentPhase(phases) || getLastPhase(phases));
   }, [phases]);
 
-  if (hasProjectEndedOrIsArchived(project, currentPhase)) {
+  useEffect(() => {
+    const element = document.getElementById(divId);
+    if (element) {
+      element.scrollIntoView();
+    }
+  }, [divId]);
+
+  const { enabled, disabled_reason } =
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    project.attributes.action_descriptors.react_on_inputs || {
+      enabled: true,
+      disabled_reason: null,
+    }; // Remove this after it is addec on the BE
+
+  const showSignIn = enabled || isFixableByAuthentication(disabled_reason);
+
+  if (!currentPhase || hasProjectEndedOrIsArchived(project, currentPhase)) {
     return null;
   }
+
+  const handleReactToInputs = () => {
+    const scrollToParams = {
+      elementId: 'common-ground-tabs',
+      pathname,
+      projectSlug: project.attributes.slug,
+      currentPhase,
+    };
+
+    if (enabled) {
+      scrollTo(scrollToParams)();
+      return;
+    }
+
+    if (isFixableByAuthentication(disabled_reason)) {
+      const successAction: SuccessAction = {
+        name: 'scrollTo',
+        params: scrollToParams,
+      };
+
+      triggerAuthenticationFlow({
+        context: {
+          type: 'phase',
+          action: 'taking_survey',
+          id: currentPhase.id,
+        },
+        successAction,
+      });
+    }
+  };
 
   return (
     <ParticipationCTAContent
       currentPhase={currentPhase}
       CTAButton={
-        <Button
-          onClick={() => {
-            // Handle scrollToElement
-          }}
-          fontWeight="500"
-          bgColor={theme.colors.white}
-          textColor={theme.colors.tenantText}
-          textHoverColor={theme.colors.black}
-          padding="6px 12px"
-          fontSize="14px"
-        >
-          <FormattedMessage {...messages.viewInputs} />
-        </Button>
+        showSignIn ? (
+          <Button
+            onClick={handleReactToInputs}
+            fontWeight="500"
+            bgColor={theme.colors.white}
+            textColor={theme.colors.tenantText}
+            textHoverColor={theme.colors.black}
+            padding="6px 12px"
+            fontSize="14px"
+          >
+            <FormattedMessage {...messages.viewInputs} />
+          </Button>
+        ) : null
       }
     />
   );
