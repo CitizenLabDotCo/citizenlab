ccd2fe4224a30b502921f2e510aaa3966e3b722d
brentguf <brentguf@users.noreply.github.com>
2025-05-16 11:53:13 +0200

    Remove unused/duplicate message

--- front/app/components/admin/SurveyDropdownSettings/messages.ts
+++ front/app/components/admin/SurveyDropdownSettings/messages.ts
@@ -1,10 +1,6 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  duplicateAnotherSurvey: {
-    id: 'app.components.admin.ProjectEdit.survey.duplicateAnotherSurvey',
-    defaultMessage: 'Duplicate another survey',
-  },
   downloadResults: {
     id: 'app.components.admin.ProjectEdit.survey.downloadResults2',
     defaultMessage: 'Download survey results',
