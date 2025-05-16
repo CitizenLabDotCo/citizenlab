8a00c756b81887d77412e1a74f30f22781f57f75
Adrien Dessy <adrien@citizenlab.co>
2025-05-16 14:46:16 +0200

    [TAN-4520] Add algorithm to rank the top consensus ideas

--- back/app/services/common_ground/results_service.rb
+++ back/app/services/common_ground/results_service.rb
@@ -5,22 +5,27 @@ module CommonGround
       @phase = phase
     end
 
-    def results
+    # @param [Integer] n Number of ideas to return for each category
+    def results(n = 5)
       Results.new(
         phase_id: @phase.id,
-        top_consensus_ideas: top_consensus_ideas,
-        top_controversial_ideas: top_controversial_ideas
+        top_consensus_ideas: top_consensus_ideas(n),
+        top_controversial_ideas: top_controversial_ideas(n)
       )
     end
 
     private
 
-    def top_consensus_ideas
-      @phase.ideas.take(3)
+    def top_consensus_ideas(n)
+      @phase
+        .ideas.published
+        .where('likes_count + dislikes_count > 0')
+        .order(Arel.sql('greatest(likes_count, dislikes_count) * 1.0 / (likes_count + dislikes_count) desc'))
+        .limit(n)
     end
 
-    def top_controversial_ideas
-      @phase.ideas.take(3)
+    def top_controversial_ideas(n)
+      @phase.ideas.take(n)
     end
 
     Results = Struct.new(
