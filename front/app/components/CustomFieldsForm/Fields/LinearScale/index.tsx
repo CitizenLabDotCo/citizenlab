9a1c5436a2c3b5b718dc57be9dd47be95010967a
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-05-20 14:04:50 -0300

    Add errors

--- front/app/components/CustomFieldsForm/Fields/LinearScale/index.tsx
+++ front/app/components/CustomFieldsForm/Fields/LinearScale/index.tsx
@@ -5,24 +5,52 @@ import { Controller, useFormContext } from 'react-hook-form';
 import { IFlatCustomField } from 'api/custom_fields/types';
 
 import LinearScale from './LinearScale';
+import Error, { TFieldName } from 'components/UI/Error';
+import { CLError, RHFErrors } from 'typings';
 
 interface Props {
   question: IFlatCustomField;
 }
 
 const LinearScaleField = ({ question }: Props) => {
-  const { control } = useFormContext();
+  const {
+    control,
+    formState: { errors: formContextErrors },
+  } = useFormContext();
 
   const name = question.key;
 
+  const errors = formContextErrors[name] as RHFErrors;
+  const validationError = errors?.message;
+  const apiError = errors?.error && ([errors] as CLError[]);
+
   return (
-    <Controller
-      name={name}
-      control={control}
-      render={({ field: { ref: _ref, ...field } }) => {
-        return <LinearScale question={question} {...field} />;
-      }}
-    />
+    <>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field: { ref: _ref, ...field } }) => {
+          return <LinearScale question={question} {...field} />;
+        }}
+      />
+      {validationError && (
+        <Error
+          marginTop="8px"
+          marginBottom="8px"
+          text={validationError}
+          scrollIntoView={false}
+        />
+      )}
+      {apiError && (
+        <Error
+          fieldName={name as TFieldName}
+          apiErrors={apiError}
+          marginTop="8px"
+          marginBottom="8px"
+          scrollIntoView={false}
+        />
+      )}
+    </>
   );
 };
 
