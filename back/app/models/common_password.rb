edd286c8149261e5d9a059676f88e46dcb52e73b
jinjagit <jinjamail@gmail.com>
2025-05-20 11:12:24 +0100

    [TAN-4271] Use test fixture file in dev env also (keep db:reset fast)

--- back/app/models/common_password.rb
+++ back/app/models/common_password.rb
@@ -17,7 +17,7 @@ class CommonPassword < ApplicationRecord
   TEST_PASSWORDS_FILE = './spec/fixtures/common_passwords_test.txt'
 
   def self.initialize!
-    file_path = Rails.env.test? ? TEST_PASSWORDS_FILE : COMMON_PASSWORDS_FILE
+    file_path = Rails.env.test? || Rails.env.dev? ? TEST_PASSWORDS_FILE : COMMON_PASSWORDS_FILE
 
     CommonPassword.delete_all
     pwds = open(file_path).readlines.map do |password|
