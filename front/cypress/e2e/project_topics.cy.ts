db6c39bf6776031f834074458e682d1b1d5bebc2
EdwinKato <katedwin88@gmail.com>
2025-05-20 15:15:26 +0300

    Fix e2e tests in project_topics e2e tests

--- front/cypress/e2e/project_topics.cy.ts
+++ front/cypress/e2e/project_topics.cy.ts
@@ -167,7 +167,7 @@ describe('Project topics', () => {
       cy.visit(`projects/${projectSlug}/ideas/new?phase_id=${phaseId}`);
 
       // Fill in the title and description since these are required
-      cy.get('#e2e-idea-title-input input').type(title);
+      cy.get('#e2e-idea-title-input input').type(title, { delay: 0 });
       cy.get('#e2e-idea-title-input input').should('contain.value', title);
 
       cy.get('[data-cy="e2e-next-page"]').should('be.visible').click();
@@ -215,7 +215,7 @@ describe('Project topics', () => {
       cy.visit(`projects/${projectSlug}/ideas/new?phase_id=${phaseId}`);
 
       // Fill in the title and description since these are required
-      cy.get('#e2e-idea-title-input input').type(title);
+      cy.get('#e2e-idea-title-input input').type(title, { delay: 0 });
       cy.get('#e2e-idea-title-input input').should('contain.value', title);
 
       cy.get('[data-cy="e2e-next-page"]').should('be.visible').click();
@@ -250,7 +250,7 @@ describe('Project topics', () => {
       // Go to idea form for our project
       cy.visit(`projects/${projectSlug}/ideas/new?phase_id=${phaseId}`);
 
-      cy.get('#e2e-idea-title-input input').type(title);
+      cy.get('#e2e-idea-title-input input').type(title, { delay: 0 });
       cy.get('#e2e-idea-title-input input').should('contain.value', title);
 
       cy.get('[data-cy="e2e-next-page"]').should('be.visible').click();
