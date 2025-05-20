e15d4dbf138d3fa82b667f4aa5316f3b39e909b0
jinjagit <jinjamail@gmail.com>
2025-05-20 11:20:00 +0100

    [TAN-4271] Invert logic only load full list ehwn env == production

--- back/app/models/common_password.rb
+++ back/app/models/common_password.rb
@@ -18,7 +18,7 @@ class CommonPassword < ApplicationRecord
 
   def self.initialize!
     # Use the test file in test and dev environments, to keep db:reset fast
-    file_path = Rails.env.test? || Rails.env.dev? ? TEST_PASSWORDS_FILE : COMMON_PASSWORDS_FILE
+    file_path = Rails.env.production? ? COMMON_PASSWORDS_FILE : TEST_PASSWORDS_FILE
 
     CommonPassword.delete_all
     pwds = open(file_path).readlines.map do |password|
