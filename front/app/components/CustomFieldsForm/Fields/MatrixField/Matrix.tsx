e3e1bcb5b8762607a7412917ef7ff58296b7ae6a
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-05-21 13:53:07 -0300

    Scaffold MatrixField

---
+++ front/app/components/CustomFieldsForm/Fields/MatrixField/Matrix.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Matrix = () => {
+  return <></>;
+};
+
+export default Matrix;
---
+++ front/app/components/CustomFieldsForm/Fields/MatrixField/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import { Controller, useFormContext } from 'react-hook-form';
+
+import { IFlatCustomField } from 'api/custom_fields/types';
+
+import Matrix from './Matrix';
+import Error, { TFieldName } from 'components/UI/Error';
+import { CLError, RHFErrors } from 'typings';
+
+interface Props {
+  question: IFlatCustomField;
+}
+
+const MatrixField = ({ question }: Props) => {
+  const {
+    control,
+    formState: { errors: formContextErrors },
+  } = useFormContext();
+
+  const name = question.key;
+
+  const errors = formContextErrors[name] as RHFErrors;
+  const validationError = errors?.message;
+  const apiError = errors?.error && ([errors] as CLError[]);
+
+  return (
+    <>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field: { ref: _ref, ...field } }) => {
+          return <Matrix question={question} {...field} />;
+        }}
+      />
+      {validationError && (
+        <Error
+          marginTop="8px"
+          marginBottom="8px"
+          text={validationError}
+          scrollIntoView={false}
+        />
+      )}
+      {apiError && (
+        <Error
+          fieldName={name as TFieldName}
+          apiErrors={apiError}
+          marginTop="8px"
+          marginBottom="8px"
+          scrollIntoView={false}
+        />
+      )}
+    </>
+  );
+};
+
+export default MatrixField;
