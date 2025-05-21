8caeabd5b461790a9b4de51772377f2eaf5b9957
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-05-21 11:48:00 -0300

    Add rating field

---
+++ front/app/components/CustomFieldsForm/Fields/RatingField/index.tsx
@@ -0,0 +1,12 @@
+import { IFlatCustomField } from 'api/custom_fields/types';
+import React from 'react';
+
+interface Props {
+  question: IFlatCustomField;
+}
+
+const RatingField = ({ question }: Props) => {
+  return <></>;
+};
+
+export default RatingField;
