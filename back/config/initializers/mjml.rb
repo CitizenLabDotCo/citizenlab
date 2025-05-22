34f1d17151f760a5fd9a09d84a3827c6c73a1caf
Sébastien Hoorens <sebastien@citizenlab.co>
2025-05-22 12:53:43 +0200

    Remove MJML parser

--- back/config/initializers/mjml.rb
+++ back/config/initializers/mjml.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
 Mjml.setup do |config|
-  # Use custom MJML binary with custom version.
-  # This slightly reduces the time to initialize Rails
-  # as mjml-rails doesn't need to search for the binary.
-  config.mjml_binary = ENV.fetch('MJML_BINARY_PATH', '/usr/bin/mjml')
   config.use_mrml = true
 end
