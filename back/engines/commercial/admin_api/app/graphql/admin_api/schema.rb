d4f1dd529512d4b11ad6c233df4265337f9fb58e
jinjagit <jinjamail@gmail.com>
2025-05-22 12:29:30 +0100

    [No-ticket] comment out disabling introspection

--- back/engines/commercial/admin_api/app/graphql/admin_api/schema.rb
+++ back/engines/commercial/admin_api/app/graphql/admin_api/schema.rb
@@ -6,6 +6,6 @@ module AdminApi
     default_max_page_size 100
 
     # Disable introspection in non-production environments for security
-    disable_introspection_entry_points if Rails.env.production?
+    # disable_introspection_entry_points if Rails.env.production?
   end
 end
