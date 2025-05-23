aa267143c0abe652ed60907c4c7aeafb838f42bf
EdwinKato <katedwin88@gmail.com>
2025-05-23 18:15:57 +0300

    Remove duplication

--- front/cypress/e2e/idea_new_page.cy.ts
+++ front/cypress/e2e/idea_new_page.cy.ts
@@ -137,267 +137,25 @@ describe('Idea submission form', () => {
     cy.get('#idea-form');
     cy.contains('Add new idea').should('exist');
     // add a title and description
-    cy.get('#e2e-idea-title-input input').click().type(ideaTitle, { delay: 0 });
+    cy.get('#e2e-idea-title-input input').type(ideaTitle, { delay: 0 });
     cy.get('#e2e-idea-title-input input').should('contain.value', ideaTitle);
 
     cy.dataCy('e2e-next-page').should('be.visible').click();
-
-    cy.get('#e2e-idea-description-input .ql-editor').type(ideaContent, {
-      delay: 10,
-    });
-    cy.get('#e2e-idea-description-input .ql-editor').contains(ideaContent);
-    cy.wait(100);
-
-    // Go to the next page of the idea form
-    cy.dataCy('e2e-next-page').should('be.visible').click();
-
-    // verify that image and file upload components are present
-    cy.get('#e2e-idea-image-upload');
-    cy.get('#e2e-idea-file-upload');
-
-    // Go to the page with topics
-    cy.dataCy('e2e-next-page').should('be.visible').click();
-
-    // add a topic
-    cy.get('.e2e-topics-picker').find('button').eq(4).click();
-
-    // verify that the topic has been selected
-    cy.get('.e2e-topics-picker')
-      .find('button.selected')
-      .should('have.length', 1);
-
-    // add a location
-    cy.get('.e2e-idea-form-location-input-field input').type(
-      'Boulevard Anspach Brussels'
-    );
-    cy.wait(10000);
-    cy.get('.e2e-idea-form-location-input-field input').type('{enter}');
-
-    // save the form
-    cy.dataCy('e2e-submit-form').click();
-    cy.wait(3000);
-
-    cy.dataCy('e2e-after-submission').should('exist');
-    cy.dataCy('e2e-after-submission').click();
-
-    // verify the content of the newly created idea page
-    cy.location('pathname').should('eq', `/en/ideas/${ideaTitle}`);
-    cy.get('#e2e-idea-show');
-    cy.get('#e2e-idea-show').find('#e2e-idea-title').contains(ideaTitle);
-    cy.get('#e2e-idea-show')
-      .find('#e2e-idea-description')
-      .contains(ideaContent);
-    cy.get('#e2e-idea-show')
-      .find('#e2e-idea-topics')
-      .find('.e2e-idea-topic')
-      .should('have.length', 1);
-    cy.get('#e2e-idea-show').contains('Boulevard Anspach');
-    cy.get('#e2e-idea-show')
-      .find('.e2e-author-link .e2e-username')
-      .contains(`${firstName} ${lastName}`);
-  });
-});
-
-describe('Idea new page for timeline project', () => {
-  const firstName = randomString();
-  const lastName = randomString();
-  const email = randomEmail();
-  const password = randomString();
-
-  const projectTitle = randomString();
-  const projectDescriptionPreview = randomString();
-  const projectDescription = randomString();
-
-  const phasePastTitle = randomString();
-
-  let projectId: string;
-  let projectSlug: string;
-
-  const twoMonthsAgo = moment().subtract(2, 'month').format('DD/MM/YYYY');
-  const inTwoMonths = moment().add(2, 'month').format('DD/MM/YYYY');
-
-  before(() => {
-    cy.apiSignup(firstName, lastName, email, password);
-
-    // create new project
-    cy.apiCreateProject({
-      title: projectTitle,
-      descriptionPreview: projectDescriptionPreview,
-      description: projectDescription,
-      publicationStatus: 'published',
-    }).then((project) => {
-      projectId = project.body.data.id;
-      projectSlug = project.body.data.attributes.slug;
-      // create active ideation phase
-      cy.apiCreatePhase({
-        projectId,
-        title: phasePastTitle,
-        startAt: twoMonthsAgo,
-        endAt: inTwoMonths,
-        participationMethod: 'ideation',
-        canComment: true,
-        canPost: true,
-        canReact: true,
-        description: `description ${phasePastTitle}`,
-      });
-    });
-  });
-
-  beforeEach(() => {
-    cy.setLoginCookie(email, password);
-    cy.visit(`/projects/${projectSlug}/ideas/new`);
-    cy.get('#idea-form');
-    cy.acceptCookies();
-  });
-
-  it('has a working idea form', () => {
-    const ideaTitle = randomString(40);
-    const ideaContent = randomString(60);
-
-    cy.get('#e2e-idea-new-page');
-    cy.get('#idea-form');
-    cy.contains('Add new idea').should('exist');
-    // add a title and description
-    cy.get('#e2e-idea-title-input input').click().type(ideaTitle, { delay: 0 });
-    cy.get('#e2e-idea-title-input input').should('contain.value', ideaTitle);
-
-    // Go to the next page of the idea form
-    cy.get('[data-cy="e2e-next-page"]').should('be.visible').click();
-
-    cy.get('#e2e-idea-description-input .ql-editor').type(ideaContent, {
-      delay: 10,
-    });
-    cy.get('#e2e-idea-description-input .ql-editor').contains(ideaContent);
-    cy.wait(100);
-
-    // Go to the next page of the idea form
-    cy.dataCy('e2e-next-page').should('be.visible').click();
-
-    // verify that image and file upload components are present
-    cy.get('#e2e-idea-image-upload');
-    cy.get('#e2e-idea-file-upload');
-
-    // Go to the page with topics
-    cy.dataCy('e2e-next-page').should('be.visible').click();
-
-    // add a topic
-    cy.get('.e2e-topics-picker').find('button').eq(4).click();
-
-    // verify that the topic has been selected
-    cy.get('.e2e-topics-picker')
-      .find('button.selected')
-      .should('have.length', 1);
-
-    // add a location
-    cy.get('.e2e-idea-form-location-input-field input').type(
-      'Boulevard Anspach Brussels'
-    );
-    cy.wait(10000);
-    cy.get('.e2e-idea-form-location-input-field input').type('{enter}');
-
-    // save the form
-    cy.dataCy('e2e-submit-form').click();
-    cy.wait(3000);
-
-    cy.dataCy('e2e-after-submission').should('exist');
-    cy.dataCy('e2e-after-submission').click();
-
-    // verify the content of the newly created idea page
-    cy.get('#e2e-idea-show');
-    cy.location('pathname').should('eq', `/en/ideas/${ideaTitle}`);
-    cy.get('#e2e-idea-show').find('#e2e-idea-title').contains(ideaTitle);
-    cy.get('#e2e-idea-show')
-      .find('#e2e-idea-description')
-      .contains(ideaContent);
-    cy.get('#e2e-idea-show')
-      .find('#e2e-idea-topics')
-      .find('.e2e-idea-topic')
-      .should('have.length', 1);
-    cy.get('#e2e-idea-show').contains('Boulevard Anspach');
-    cy.get('#e2e-idea-show')
-      .find('.e2e-author-link .e2e-username')
-      .contains(`${firstName} ${lastName}`);
-  });
-});
-
-describe('Idea new page for timeline project', () => {
-  const firstName = randomString();
-  const lastName = randomString();
-  const email = randomEmail();
-  const password = randomString();
-
-  const projectTitle = randomString();
-  const projectDescriptionPreview = randomString();
-  const projectDescription = randomString();
-
-  const phasePastTitle = randomString();
-
-  let projectId: string;
-  let projectSlug: string;
-
-  const twoMonthsAgo = moment().subtract(2, 'month').format('DD/MM/YYYY');
-  const inTwoMonths = moment().add(2, 'month').format('DD/MM/YYYY');
-
-  before(() => {
-    cy.apiSignup(firstName, lastName, email, password);
-
-    // create new project
-    cy.apiCreateProject({
-      title: projectTitle,
-      descriptionPreview: projectDescriptionPreview,
-      description: projectDescription,
-      publicationStatus: 'published',
-    }).then((project) => {
-      projectId = project.body.data.id;
-      projectSlug = project.body.data.attributes.slug;
-      // create active ideation phase
-      cy.apiCreatePhase({
-        projectId,
-        title: phasePastTitle,
-        startAt: twoMonthsAgo,
-        endAt: inTwoMonths,
-        participationMethod: 'ideation',
-        canComment: true,
-        canPost: true,
-        canReact: true,
-        description: `description ${phasePastTitle}`,
-      });
-    });
-  });
-
-  beforeEach(() => {
-    cy.setLoginCookie(email, password);
-    cy.visit(`/projects/${projectSlug}/ideas/new`);
-    cy.get('#idea-form');
-    cy.acceptCookies();
-  });
-
-  it('has a working idea form', () => {
-    const ideaTitle = randomString(40);
-    const ideaContent = randomString(60);
-
-    cy.get('#e2e-idea-new-page');
-    cy.get('#idea-form');
-    cy.contains('Add new idea').should('exist');
-    // add a title and description
-    cy.get('#e2e-idea-title-input input').click().type(ideaTitle, { delay: 0 });
-    cy.get('#e2e-idea-title-input input').should('contain.value', ideaTitle);
-
-    // Go to the next page of the idea form
-    cy.get('[data-cy="e2e-next-page"]').should('be.visible').click();
 
     cy.get('#e2e-idea-description-input .ql-editor').type(ideaContent);
     cy.get('#e2e-idea-description-input .ql-editor').contains(ideaContent);
 
+    cy.wait(1000);
+
     // Go to the next page of the idea form
-    cy.get('[data-cy="e2e-next-page"]').should('be.visible').click();
+    cy.dataCy('e2e-next-page').should('be.visible').click();
 
     // verify that image and file upload components are present
     cy.get('#e2e-idea-image-upload');
     cy.get('#e2e-idea-file-upload');
 
     // Go to the page with topics
-    cy.get('[data-cy="e2e-next-page"]').should('be.visible').click();
+    cy.dataCy('e2e-next-page').should('be.visible').click();
 
     // add a topic
     cy.get('.e2e-topics-picker').find('button').eq(4).click();
@@ -415,15 +173,15 @@ describe('Idea new page for timeline project', () => {
     cy.get('.e2e-idea-form-location-input-field input').type('{enter}');
 
     // save the form
-    cy.get('[data-cy="e2e-submit-form"]').click();
+    cy.dataCy('e2e-submit-form').click();
     cy.wait(3000);
 
-    cy.get('[data-cy="e2e-after-submission"]').should('exist');
-    cy.get('[data-cy="e2e-after-submission"]').click();
+    cy.dataCy('e2e-after-submission').should('exist');
+    cy.dataCy('e2e-after-submission').click();
 
     // verify the content of the newly created idea page
-    cy.get('#e2e-idea-show');
     cy.location('pathname').should('eq', `/en/ideas/${ideaTitle}`);
+    cy.get('#e2e-idea-show');
     cy.get('#e2e-idea-show').find('#e2e-idea-title').contains(ideaTitle);
     cy.get('#e2e-idea-show')
       .find('#e2e-idea-description')
