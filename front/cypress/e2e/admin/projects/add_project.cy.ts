a1577c1a25a81eab8906ed7027bab4a3af8606e2
Amanda Anderson <anderson_amanda@mail.com>
2025-05-23 09:13:58 +0200

    Code cleanup

--- front/cypress/e2e/admin/projects/add_project.cy.ts
+++ front/cypress/e2e/admin/projects/add_project.cy.ts
@@ -76,7 +76,7 @@ describe('Admin: add project', () => {
         // Submit project
         cy.get('.e2e-submit-wrapper-button button').click();
 
-        cy.wait(2000);
+        // Wait for the project creation to finish and check the response
         cy.wait('@createProject').then((interception) => {
           const projectId = interception.response?.body.data.id;
 
