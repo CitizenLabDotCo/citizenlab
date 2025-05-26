bf7a12cfcd0b40f3ef367059d829b6f8dc833adf
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-05-26 10:30:00 -0300

    Update messages to not use area term in a sentence

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/Areas/messages.ts
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/Areas/messages.ts
@@ -5,28 +5,24 @@ export default defineMessages({
     id: 'front.app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.Widgets.Areas.areasTitle2',
     defaultMessage: 'In your area',
   },
-  thereAreCurrentlyNoProjects: {
-    id: 'front.app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.Widgets.Areas.thereAreCurrentlyNoProjects',
+  thereAreCurrentlyNoProjects1: {
+    id: 'front.app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.Widgets.Areas.thereAreCurrentlyNoProjects1',
     defaultMessage:
-      'There are currently no active projects for you in the {areasTerm} that you follow.',
+      'There are currently no active projects given your follow preferences.',
   },
   thisWidgetShows: {
     id: 'front.app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.Widgets.Areas.thisWidgetShows2',
     defaultMessage:
       'This widget shows projects associated with the "areas" the user follows. Note that your platform might use a different name for "areas"- see the "Areas" tab in the platform settings. If the user does not follow any areas yet, the widget will show the available areas to follow. In this case the widget will show a maximum of 100 areas.',
   },
-  followAreas1: {
-    id: 'front.app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.Widgets.Areas.followAreas1',
+  followPreferences: {
+    id: 'front.app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.Widgets.Areas.followPreferences',
     defaultMessage: 'Follow preferences',
   },
-  areasYouFollow1: {
-    id: 'front.app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.Widgets.Areas.areasYouFollow1',
-    defaultMessage: '{capitalizedAreasTerm} you follow',
-  },
-  areaButtonsInfo1: {
-    id: 'front.app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.Widgets.Areas.areaButtonsInfo1',
+  areaButtonsInfo2: {
+    id: 'front.app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.Widgets.Areas.areaButtonsInfo2',
     defaultMessage:
-      'Click on the buttons below to follow or unfollow the {areasTerm} you would like to see projects for. The number of projects in each {areaTerm} is shown in brackets.',
+      'Click on the buttons below to follow or unfollow. The number of projects is shown in brackets.',
   },
   done: {
     id: 'front.app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.Widgets.Areas.done',
