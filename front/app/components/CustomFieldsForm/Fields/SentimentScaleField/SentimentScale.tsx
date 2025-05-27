2c4048b1328446dac1b39de6160b4f98228bc3ff
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-05-27 16:48:34 -0300

    Scaffold sentiment scale index

--- front/app/components/CustomFieldsForm/Fields/SentimentScaleField/SentimentScale.tsx
+++ front/app/components/CustomFieldsForm/Fields/SentimentScaleField/SentimentScale.tsx
@@ -1,21 +1,14 @@
-import React from 'react';
+import React, { useRef, useEffect, useCallback } from 'react';
+
+import { Box, Table } from '@citizenlab/cl2-component-library';
 
 import { IFlatCustomField } from 'api/custom_fields/types';
 
-import {
-  Box,
-  Button,
-  Icon,
-  Table,
-  Tbody,
-  Td,
-  Text,
-  Th,
-  Thead,
-  Tr,
-} from '@citizenlab/cl2-component-library';
-import TableHead from './TableHead';
+import { useIntl } from 'utils/cl-intl';
+
 import TableBody from './TableBody';
+import TableHead from './TableHead';
+import { getAriaValueText, handleKeyboardKeyChange } from './utils';
 
 interface Props {
   value?: number;
@@ -23,8 +16,55 @@ interface Props {
   onChange: (value: number) => void;
 }
 
+const minimum = 1;
+const maximum = 5;
+
 const SentimentScale = ({ value: data, question, onChange }: Props) => {
-  console.log(data);
+  const sliderRef = useRef<HTMLDivElement>(null);
+  const { formatMessage } = useIntl();
+
+  const id = question.key;
+
+  // Get the aria-label for the slider
+  const getAriaLabel = useCallback(
+    (value: number, total: number) => {
+      return getAriaValueText({
+        value,
+        total,
+        question,
+        formatMessage,
+      });
+    },
+    [question, formatMessage]
+  );
+
+  // Set the aria-valuenow and aria-valuetext attributes on the slider when the data changes
+  useEffect(() => {
+    if (sliderRef.current) {
+      sliderRef.current.setAttribute('aria-valuenow', String(data || minimum));
+      sliderRef.current.setAttribute(
+        'aria-valuetext',
+        getAriaLabel(data || minimum, maximum)
+      );
+    }
+  }, [data, getAriaLabel]);
+
+  // Handle keyboard input for the slider
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    const value = data || minimum;
+    let newValue = value;
+    newValue = handleKeyboardKeyChange(event, value);
+
+    onChange(newValue);
+    if (sliderRef.current) {
+      sliderRef.current.setAttribute('aria-valuenow', String(newValue));
+      sliderRef.current.setAttribute(
+        'aria-valuetext',
+        getAriaLabel(newValue, 5)
+      );
+    }
+    event.preventDefault();
+  };
 
   return (
     <Box
@@ -33,7 +73,7 @@ const SentimentScale = ({ value: data, question, onChange }: Props) => {
       ref={sliderRef}
       aria-valuemin={minimum}
       aria-valuemax={maximum}
-      aria-labelledby={sanitizeForClassname(id)}
+      aria-labelledby={id}
       onKeyDown={(event) => {
         if (event.key !== 'Tab' && !event.metaKey) {
           // Don't override the default tab behaviour or meta key (E.g. Mac command key)
--- front/app/components/Form/Components/Controls/SentimentLinearScaleControl/utils.ts
+++ front/app/components/Form/Components/Controls/SentimentLinearScaleControl/utils.ts
@@ -5,44 +5,6 @@ import { FormatMessageValues } from 'utils/cl-intl/useIntl';
 
 import messages from '../messages';
 
-import Sentiment1Png from './assets/sentiment_1.png';
-import Sentiment2Png from './assets/sentiment_2.png';
-import Sentiment3Png from './assets/sentiment_3.png';
-import Sentiment4Png from './assets/sentiment_4.png';
-import Sentiment5Png from './assets/sentiment_5.png';
-
-export const handleKeyboardKeyChange = (event, value) => {
-  switch (event.key) {
-    case 'ArrowLeft':
-    case 'ArrowDown':
-      return Math.max(1, value - 1);
-    case 'ArrowRight':
-    case 'ArrowUp':
-      return Math.min(5, value + 1);
-    case 'Home':
-      return 1;
-    case 'End':
-      return 5;
-    case 'Enter':
-      return value;
-    case 'Escape':
-      return;
-    default:
-      return value;
-  }
-};
-
-const sentimentEmojis: Record<1 | 2 | 3 | 4 | 5, string> = {
-  1: Sentiment1Png,
-  2: Sentiment2Png,
-  3: Sentiment3Png,
-  4: Sentiment4Png,
-  5: Sentiment5Png,
-};
-
-export const getSentimentEmoji = (index: number) =>
-  sentimentEmojis[index as keyof typeof sentimentEmojis];
-
 type GetAriaValueTextProps = {
   uischema: ControlElement;
   formatMessage: (
