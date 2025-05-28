1785a02efe9932481742114322d707555959be69
brentguf <brentguf@users.noreply.github.com>
2025-05-28 16:42:45 +0200

    Fix skipped test

--- front/cypress/e2e/project_card.cy.ts
+++ front/cypress/e2e/project_card.cy.ts
@@ -1,55 +1,16 @@
-import { randomString } from '../support/commands';
-import moment = require('moment');
-
 describe('Project card component', () => {
-  const projectTitle = randomString();
-  const projectDescriptionPreview = randomString(30);
   let projectId: string;
 
   before(() => {
-    cy.apiCreateProject({
-      title: projectTitle,
-      descriptionPreview: projectDescriptionPreview,
-      description: randomString(),
-      publicationStatus: 'published',
-    }).then((project) => {
-      projectId = project.body.data.id;
-      cy.apiCreatePhase({
-        projectId,
-        title: 'phaseTitle',
-        startAt: moment().subtract(2, 'month').format('DD/MM/YYYY'),
-        endAt: moment().add(2, 'days').format('DD/MM/YYYY'),
-        participationMethod: 'ideation',
-        canComment: true,
-        canPost: true,
-        canReact: true,
-      });
+    cy.createProjectWithIdeationPhase().then((result) => {
+      projectId = result.projectId;
     });
   });
 
-  it.skip('shows the title, description, progress bar and cta', () => {
+  it('navigates to project page on click title', () => {
     cy.goToLandingPage();
 
-    cy.get('.e2e-project-card').should('exist');
-    const projectCard = cy.get('.e2e-project-card').first();
-
-    projectCard.contains(projectTitle);
-
-    projectCard
-      .get('.e2e-project-card-project-description-preview')
-      .contains(projectDescriptionPreview);
-
-    projectCard.get('.e2e-project-card-time-remaining');
-
-    projectCard.get('.e2e-project-card-cta').contains('Submit your idea');
-  });
-
-  it.skip('navigates to project page on click title', () => {
-    cy.goToLandingPage();
-
-    cy.get('.e2e-project-card').first().should('exist');
-    cy.get('.e2e-project-card-project-title').should('exist');
-    cy.get('.e2e-project-card-project-title').first().click();
+    cy.dataCy('e2e-light-project-card').first().should('be.visible').click();
 
     cy.url().should('include', '/en/projects');
     cy.get('#e2e-project-page');
