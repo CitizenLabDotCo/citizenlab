743cfca5fb7f33953151cb49564fd043e0735131
Adrien Dessy <adrien@citizenlab.co>
2025-06-04 17:06:38 +0200

    [TAN-4519] Fix Common Ground test
    The `supports_multiple_posts` method has been replaced by `allow_posting_again_after` in a concurrent PR.

--- back/spec/lib/participation_method/common_ground_spec.rb
+++ back/spec/lib/participation_method/common_ground_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe ParticipationMethod::CommonGround do
   its(:transitive?) { is_expected.to be(false) }
   its(:supports_status?) { is_expected.to be(false) }
   its(:supports_inputs_without_author?) { is_expected.to be(true) }
-  its(:supports_multiple_posts?) { is_expected.to be(true) }
+  its(:allow_posting_again_after) { is_expected.to eq(0.seconds) }
   its(:supports_edits_after_publication?) { is_expected.to be(true) }
   its(:supports_permitted_by_everyone?) { is_expected.to be(false) }
   its(:supports_multiple_phase_reports?) { is_expected.to be(false) }
