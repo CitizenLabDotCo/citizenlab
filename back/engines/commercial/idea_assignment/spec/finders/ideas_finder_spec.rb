04cabe04f43d3e05dca297ab7950c35481dafbc8
jinjagit <jinjamail@gmail.com>
2025-06-05 11:08:49 +0100

    [TAN-4443] Linting

--- back/engines/commercial/idea_assignment/spec/finders/ideas_finder_spec.rb
+++ back/engines/commercial/idea_assignment/spec/finders/ideas_finder_spec.rb
@@ -34,13 +34,10 @@ describe IdeasFinder do
 
     before do
       @idea_without_content = create(:idea)
-      @idea_without_content.update_columns(
-        title_multiloc: {}, 
-        body_multiloc: {}
-      )
+      @idea_without_content.update_columns(title_multiloc: {}, body_multiloc: {})
     end
 
-    # Note: If a phase is changed from native survey to ideation,
+    # NOTE: If a phase is changed from native survey to ideation,
     # this can mean that ideas with no displayable content exist for the phase.
     it 'does not return ideas with no content' do
       expect(result_record_ids).to match_array (unassigned_ideas + assigned_ideas).map(&:id)
