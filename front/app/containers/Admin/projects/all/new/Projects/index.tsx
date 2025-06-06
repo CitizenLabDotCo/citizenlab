4d7354fb941dc3bf49758f5f7c10f5c0dc4fd734
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-06-06 18:06:29 -0300

    Scaffold table

--- front/app/containers/Admin/projects/all/new/Projects/index.tsx
+++ front/app/containers/Admin/projects/all/new/Projects/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
-import useProjectsMini from 'api/projects_mini/useProjectsMini';
+import { Box } from '@citizenlab/cl2-component-library';
+
+import Table from './Table';
 
 const Projects = () => {
-  const { data } = useProjectsMini({
-    endpoint: 'for_admin',
-    sort: 'phase_starting_or_ending_soon',
-  });
-
-  console.log(data);
-
-  return <>Projects!</>;
+  return (
+    <Box>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        p="16px"
+        border={`1px solid #ccc`}
+      >
+        FILTERS
+      </Box>
+      <Box mt="16px">
+        <Table />
+      </Box>
+    </Box>
+  );
 };
 
 export default Projects;
