6679f9a6ae212863ed7c20f68bcab98652c99825
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-06-06 15:20:46 -0300

    It works

--- front/app/containers/Admin/projects/all/new/Projects/index.tsx
+++ front/app/containers/Admin/projects/all/new/Projects/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
+import useProjectsMini from 'api/projects_mini/useProjectsMini';
+
 const Projects = () => {
+  const { data } = useProjectsMini({
+    endpoint: 'for_admin',
+    sort: 'phase_starting_or_ending_soon',
+  });
+
+  console.log(data);
+
   return <>Projects!</>;
 };
 
