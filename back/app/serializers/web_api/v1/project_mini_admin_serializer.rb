6dbcfb7bddd190f9b2045b7e8d3011698c5ee825
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-06-09 10:46:57 -0300

    Add start and end dates

--- back/app/serializers/web_api/v1/project_mini_admin_serializer.rb
+++ back/app/serializers/web_api/v1/project_mini_admin_serializer.rb
@@ -3,6 +3,16 @@
 class WebApi::V1::ProjectMiniAdminSerializer < WebApi::V1::BaseSerializer
   attributes(:title_multiloc)
 
+  attribute :first_phase_start_date do |object|
+    first_phase = object.phases.order(start_at: :asc).first
+    first_phase&.start_at
+  end
+
+  attribute :last_phase_end_date do |object|
+    last_phase = object.phases.order(start_at: :desc).first
+    last_phase&.end_at
+  end
+
   has_one :current_phase, serializer: WebApi::V1::PhaseMiniSerializer, record_type: :phase do |object|
     TimelineService.new.current_phase(object)
   end
