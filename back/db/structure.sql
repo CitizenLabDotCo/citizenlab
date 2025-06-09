04d5e9ddd5d27b2d61611402bf6e58aeb3d7eadd
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-06-09 15:20:48 -0300

    Update structure

--- back/db/structure.sql
+++ back/db/structure.sql
@@ -202,6 +202,7 @@ DROP INDEX IF EXISTS public.index_polls_response_options_on_response_id;
 DROP INDEX IF EXISTS public.index_polls_response_options_on_option_id;
 DROP INDEX IF EXISTS public.index_polls_questions_on_phase_id;
 DROP INDEX IF EXISTS public.index_polls_options_on_question_id;
+DROP INDEX IF EXISTS public.index_phases_on_start_at_and_end_at;
 DROP INDEX IF EXISTS public.index_phases_on_project_id;
 DROP INDEX IF EXISTS public.index_phases_on_manual_voters_last_updated_by_id;
 DROP INDEX IF EXISTS public.index_phase_files_on_phase_id;
@@ -5598,6 +5599,13 @@ CREATE INDEX index_phases_on_project_id ON public.phases USING btree (project_id
 
 
 --
+-- Name: index_phases_on_start_at_and_end_at; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_phases_on_start_at_and_end_at ON public.phases USING btree (start_at, end_at);
+
+
+--
 -- Name: index_polls_options_on_question_id; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -7086,6 +7094,7 @@ ALTER TABLE ONLY public.ideas_topics
 SET search_path TO public,shared_extensions;
 
 INSERT INTO "schema_migrations" (version) VALUES
+('20250609151800'),
 ('20250513160156'),
 ('20250509140651'),
 ('20250509131056'),
