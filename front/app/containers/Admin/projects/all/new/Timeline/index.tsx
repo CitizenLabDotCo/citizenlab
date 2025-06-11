c8c42de16d3a9edc0d0aa57e9175df65b73f46e5
EdwinKato <katedwin88@gmail.com>
2025-06-11 10:59:23 +0300

    Implement Timeline component with project data loading and Gantt chart visualization

--- front/app/containers/Admin/projects/all/new/Timeline/index.tsx
+++ front/app/containers/Admin/projects/all/new/Timeline/index.tsx
@@ -1,7 +1,49 @@
 import React from 'react';
 
+import { Spinner, Text } from '@citizenlab/cl2-component-library';
+
+import useProjectsMiniAdmin from 'api/projects_mini_admin/useProjectsMiniAdmin';
+
+import useLocalize from 'hooks/useLocalize';
+
+import Centerer from 'components/UI/Centerer';
+
+import { useIntl } from 'utils/cl-intl';
+
+import messages from './messages';
+import ProjectGanttChart, { GanttProject } from './ProjectGanttChart';
+
 const Timeline = () => {
-  return <>Timeline!</>;
+  const localize = useLocalize();
+  const { formatMessage } = useIntl();
+  const { data, isLoading, isError } = useProjectsMiniAdmin({
+    sort: 'phase_starting_or_ending_soon',
+  });
+
+  const projectsData = data?.pages.map((page) => page.data).flat() || [];
+
+  const projects: GanttProject[] = projectsData.map((project) => ({
+    id: project.id,
+    title: localize(project.attributes.title_multiloc),
+    start: project.attributes.first_phase_start_date,
+    end: project.attributes.last_phase_end_date,
+    folder: localize(project.attributes.folder_title_multiloc),
+    daysLeft: 63,
+  }));
+
+  if (isLoading) {
+    return (
+      <Centerer>
+        <Spinner />
+      </Centerer>
+    );
+  }
+
+  if (isError) {
+    return <Text>{formatMessage(messages.failedToLoadTimelineError)}</Text>;
+  }
+
+  return <ProjectGanttChart projects={projects} />;
 };
 
 export default Timeline;
