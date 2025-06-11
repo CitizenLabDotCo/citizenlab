00af74e1b111dd88b1df6549ec1946fdc5d30726
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-06-11 11:26:49 -0300

    Fix pagination

--- front/app/containers/Admin/projects/all/new/Projects/Table/index.tsx
+++ front/app/containers/Admin/projects/all/new/Projects/Table/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
+  Box,
   Table as TableComponent,
   Thead,
   Tr,
@@ -12,44 +13,62 @@ import {
 
 import useProjectsMiniAdmin from 'api/projects_mini_admin/useProjectsMiniAdmin';
 
+import { PaginationWithoutPositioning } from 'components/Pagination';
+
 import { useIntl } from 'utils/cl-intl';
+import { getPageNumberFromUrl } from 'utils/paginationUtils';
 
 import messages from './messages';
 import Row from './Row';
 
 const Table = () => {
   const { formatMessage } = useIntl();
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const { data } = useProjectsMiniAdmin({
+  const { data: projects } = useProjectsMiniAdmin({
     sort: 'phase_starting_or_ending_soon',
+    'page[size]': 10,
+    'page[number]': currentPage,
   });
 
-  const projects = data?.pages.map((page) => page.data).flat();
+  const lastPageLink = projects?.links.last;
+  const lastPage = lastPageLink ? getPageNumberFromUrl(lastPageLink) ?? 1 : 1;
 
   return (
-    <TableComponent
-      border={`1px solid ${colors.grey300}`}
-      borderRadius={stylingConsts.borderRadius}
-      innerBorders={{
-        bodyRows: true,
-      }}
-    >
-      <Thead>
-        <Tr background={colors.grey50}>
-          <Th py="16px">{formatMessage(messages.project)}</Th>
-          <Th py="16px">{formatMessage(messages.currentPhase)}</Th>
-          <Th py="16px">{formatMessage(messages.projectStart)}</Th>
-          <Th py="16px">{formatMessage(messages.projectEnd)}</Th>
-          <Th py="16px">{formatMessage(messages.status)}</Th>
-          <Th py="16px">{formatMessage(messages.visibility)}</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {projects?.map((project) => (
-          <Row project={project} key={project.id} />
-        ))}
-      </Tbody>
-    </TableComponent>
+    <Box>
+      <TableComponent
+        border={`1px solid ${colors.grey300}`}
+        borderRadius={stylingConsts.borderRadius}
+        innerBorders={{
+          bodyRows: true,
+        }}
+      >
+        <Thead>
+          <Tr background={colors.grey50}>
+            <Th py="16px">{formatMessage(messages.project)}</Th>
+            <Th py="16px">{formatMessage(messages.currentPhase)}</Th>
+            <Th py="16px">{formatMessage(messages.projectStart)}</Th>
+            <Th py="16px">{formatMessage(messages.projectEnd)}</Th>
+            <Th py="16px">{formatMessage(messages.status)}</Th>
+            <Th py="16px">{formatMessage(messages.visibility)}</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {projects?.data.map((project) => (
+            <Row project={project} key={project.id} />
+          ))}
+        </Tbody>
+      </TableComponent>
+      {lastPage > 1 && (
+        <Box mt="12px">
+          <PaginationWithoutPositioning
+            currentPage={currentPage}
+            totalPages={lastPage}
+            loadPage={setCurrentPage}
+          />
+        </Box>
+      )}
+    </Box>
   );
 };
 
