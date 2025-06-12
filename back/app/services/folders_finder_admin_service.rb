eb81a499af334baf51af2c4a5f67ee1b36cda4f0
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-06-12 18:20:38 -0300

    Add filter_status

--- back/app/services/folders_finder_admin_service.rb
+++ back/app/services/folders_finder_admin_service.rb
@@ -3,4 +3,14 @@ class FoldersFinderAdminService
     # TODO
     scope
   end
+
+  # FILTERING METHODS
+  def self.filter_status(scope, params = {})
+    status = params[:status] || []
+    return scope if status.blank?
+
+    scope
+      .joins("INNER JOIN admin_publications ON admin_publications.publication_id = project_folders_folders.id AND admin_publications.publication_type = 'ProjectFolders::Folder'")
+      .where(admin_publications: { publication_status: status })
+  end
 end
--- back/spec/services/folders_finder_admin_service_spec.rb
+++ back/spec/services/folders_finder_admin_service_spec.rb
@@ -9,4 +9,32 @@ describe ProjectsFinderAdminService do
       expect(result).to eq(scope)
     end
   end
+
+  describe 'self.filter_status' do
+    let!(:published_folder) { create(:project_folder) }
+    let!(:draft_folder) do
+      folder = create(:project_folder)
+      publication = AdminPublication.find_by(publication_id: folder.id, publication_type: 'ProjectFolders::Folder')
+      publication.update!(publication_status: 'draft')
+      folder
+    end
+
+    it 'filters by status' do
+      scope = ProjectFolders::Folder.all
+      params = { status: ['published'] }
+      result = FoldersFinderAdminService.filter_status(scope, params)
+
+      expect(result).to include(published_folder)
+      expect(result).not_to include(draft_folder)
+    end
+
+    it 'returns original scope if no status provided' do
+      scope = ProjectFolders::Folder.all
+      params = {}
+      result = FoldersFinderAdminService.filter_status(scope, params)
+
+      expect(result).to include(published_folder)
+      expect(result).to include(draft_folder)
+    end
+  end
 end
