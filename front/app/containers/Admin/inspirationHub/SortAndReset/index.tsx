bf5a49519d2f7eccdfeab727bb997a341d59175d
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-06-12 13:48:04 -0300

    Also remove country filter on reset

--- front/app/containers/Admin/inspirationHub/SortAndReset/index.tsx
+++ front/app/containers/Admin/inspirationHub/SortAndReset/index.tsx
@@ -3,16 +3,13 @@ import React from 'react';
 import { Box, Button } from '@citizenlab/cl2-component-library';
 
 import { FormattedMessage } from 'utils/cl-intl';
-import { updateSearchParams } from 'utils/cl-router/updateSearchParams';
 
 import messages from '../messages';
-import { useRansackParam, clearRansackParams } from '../utils';
+import { clearRansackParams } from '../utils';
 
 import Sort from './Sort';
 
 const SortAndReset = () => {
-  const countryCode = useRansackParam('q[tenant_country_code_in]');
-
   return (
     <Box display="flex" mt="8px">
       <Sort />
@@ -22,10 +19,6 @@ const SortAndReset = () => {
         ml="8px"
         onClick={() => {
           clearRansackParams();
-
-          if (countryCode) {
-            updateSearchParams({ 'q[tenant_country_code_in]': countryCode });
-          }
         }}
       >
         <FormattedMessage {...messages.resetFilters} />
