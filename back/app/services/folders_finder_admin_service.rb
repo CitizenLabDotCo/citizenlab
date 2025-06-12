b3d842b998b97c8b0a630027c751c7694db0144b
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-06-12 18:26:52 -0300

    Add filter_folder_manager

--- back/app/services/folders_finder_admin_service.rb
+++ back/app/services/folders_finder_admin_service.rb
@@ -13,4 +13,22 @@ class FoldersFinderAdminService
       .joins("INNER JOIN admin_publications ON admin_publications.publication_id = project_folders_folders.id AND admin_publications.publication_type = 'ProjectFolders::Folder'")
       .where(admin_publications: { publication_status: status })
   end
+
+  def self.filter_folder_manager(scope, params = {})
+    manager_ids = params[:managers] || []
+    return scope if manager_ids.blank?
+
+    managers = User.where(id: manager_ids)
+    moderated_folders = []
+
+    managers.each do |manager|
+      manager.roles.each do |role|
+        if role['type'] == 'project_folder_moderator'
+          moderated_folders << role['project_folder_id']
+        end
+      end
+    end
+
+    scope.where(id: moderated_folders)
+  end
 end
--- back/spec/services/folders_finder_admin_service_spec.rb
+++ back/spec/services/folders_finder_admin_service_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-describe ProjectsFinderAdminService do
+describe FoldersFinderAdminService do
   describe 'self.execute' do
     it 'works' do
       scope = ProjectFolders::Folder.all
       params = {}
-      result = FoldersFinderAdminService.execute(scope, params)
+      result = described_class.execute(scope, params)
       expect(result).to eq(scope)
     end
   end
@@ -22,7 +22,7 @@ describe ProjectsFinderAdminService do
     it 'filters by status' do
       scope = ProjectFolders::Folder.all
       params = { status: ['published'] }
-      result = FoldersFinderAdminService.filter_status(scope, params)
+      result = described_class.filter_status(scope, params)
 
       expect(result).to include(published_folder)
       expect(result).not_to include(draft_folder)
@@ -31,10 +31,29 @@ describe ProjectsFinderAdminService do
     it 'returns original scope if no status provided' do
       scope = ProjectFolders::Folder.all
       params = {}
-      result = FoldersFinderAdminService.filter_status(scope, params)
+      result = described_class.filter_status(scope, params)
 
       expect(result).to include(published_folder)
       expect(result).to include(draft_folder)
+    end
+  end
+
+  describe 'self.filter_folder_manager' do
+    let!(:f1) { create(:project_folder) }
+    let!(:f2) { create(:project_folder) }
+    let!(:f3) { create(:project_folder) }
+
+    let!(:user1) { create(:user, roles: [{ 'type' => 'project_folder_moderator', project_folder_id: f1.id }]) }
+    let!(:user2) { create(:user, roles: [{ 'type' => 'project_folder_moderator', project_folder_id: f2.id }]) }
+
+    it 'returns folders managed by specified users' do
+      result = described_class.filter_folder_manager(ProjectFolders::Folder.all, { managers: [user1.id] })
+      expect(result.pluck(:id)).to eq([f1.id])
+    end
+
+    it 'returns all folders when no managers specified' do
+      result = described_class.filter_folder_manager(ProjectFolders::Folder.all, {})
+      expect(result.pluck(:id)).to match_array([f1.id, f2.id, f3.id])
     end
   end
 end
