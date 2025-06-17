a5fb6fd6c1733c9ccd6b08fbff3aa93672a0023b
brentguf <brentguf@users.noreply.github.com>
2025-06-17 17:11:21 +0200

    Build error

--- front/app/components/FormBuilder/components/FormBuilderSettings/ContentSettings/index.tsx
+++ front/app/components/FormBuilder/components/FormBuilderSettings/ContentSettings/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { Box, Text } from '@citizenlab/cl2-component-library';
 import { useFormContext } from 'react-hook-form';
-import { SupportedLocale } from 'typings';
 
 import { IFlatCustomFieldWithIndex } from 'api/custom_fields/types';
 
@@ -22,7 +21,6 @@ import FieldTypeSwitcher from '../FieldTypeSwitcher';
 
 type ContentSettingsProps = {
   field: IFlatCustomFieldWithIndex;
-  locales: SupportedLocale[];
   formHasSubmissions: boolean;
 };
 
