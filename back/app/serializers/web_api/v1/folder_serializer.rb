21aca0878561db1ae3f53ad48bf7c96c20f70484
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-06-17 16:02:38 +0100

    Simplify visible_projects_count

--- back/app/serializers/web_api/v1/folder_serializer.rb
+++ back/app/serializers/web_api/v1/folder_serializer.rb
@@ -24,7 +24,7 @@ class WebApi::V1::FolderSerializer < WebApi::V1::BaseSerializer
   end
 
   attribute :visible_projects_count do |object, params|
-    params.dig(:visible_children_count_by_parent_id, object.admin_publication.id) || Pundit.policy_scope(user_context(params), Project).where(id: object.admin_publication.children.map(&:publication_id)).count
+    params.dig(:visible_children_count_by_parent_id, object.admin_publication.id)
   end
 
   attribute :avatars_count do |object, params|
