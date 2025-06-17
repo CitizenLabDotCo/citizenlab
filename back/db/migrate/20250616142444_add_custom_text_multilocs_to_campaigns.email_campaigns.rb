26bdf623e8c07cf7f6f32d5a2d72cf72a6b74a0e
jamesspeake <james@speake.org>
2025-06-17 13:58:28 +0100

    [TAN-4778] Removed migration

--- back/db/migrate/20250616142444_add_custom_text_multilocs_to_campaigns.email_campaigns.rb
+++ back/db/migrate/20250616142444_add_custom_text_multilocs_to_campaigns.email_campaigns.rb
@@ -1,7 +1,7 @@
 # This migration comes from email_campaigns (originally 20250616150856)
-class SplitCustomTextMultilocInCampaigns < ActiveRecord::Migration[7.1]
+class AddCustomTextMultilocsToCampaigns < ActiveRecord::Migration[7.1]
   def change
-    rename_column :email_campaigns_campaigns, :custom_text_multiloc, :title_multiloc
+    add_column :email_campaigns_campaigns, :title_multiloc, :jsonb, default: {}
     add_column :email_campaigns_campaigns, :intro_multiloc, :jsonb, default: {}
     add_column :email_campaigns_campaigns, :button_text_multiloc, :jsonb, default: {}
   end
--- back/engines/free/email_campaigns/db/migrate/20250616150856_add_custom_text_multilocs_to_campaigns.email_campaigns.rb
+++ back/engines/free/email_campaigns/db/migrate/20250616150856_add_custom_text_multilocs_to_campaigns.email_campaigns.rb
@@ -1,6 +1,6 @@
-class SplitCustomTextMultilocInCampaigns < ActiveRecord::Migration[7.1]
+class AddCustomTextMultilocsToCampaigns < ActiveRecord::Migration[7.1]
   def change
-    rename_column :email_campaigns_campaigns, :custom_text_multiloc, :title_multiloc
+    add_column :email_campaigns_campaigns, :title_multiloc, :jsonb, default: {}
     add_column :email_campaigns_campaigns, :intro_multiloc, :jsonb, default: {}
     add_column :email_campaigns_campaigns, :button_text_multiloc, :jsonb, default: {}
   end
