f0c20285e9b9fe421369876639f16b9f4825039b
EdwinKato <katedwin88@gmail.com>
2025-06-17 20:10:55 +0300

    Refetch every 5 seconds

--- front/app/api/copy_inputs/useJobInProgressByPhase.ts
+++ front/app/api/copy_inputs/useJobInProgressByPhase.ts
@@ -1,5 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { CLErrors } from 'typings';
+import ideasKeys from 'api/ideas/keys';
 
 import fetcher from 'utils/cl-react-query/fetcher';
 
@@ -20,11 +21,29 @@ const fetchInProgressPhaseJobs = async (
   });
 };
 
-const useJobInProgressByPhase = (phaseId: string) =>
-  useQuery<IJobs | undefined, CLErrors>({
+const useJobInProgressByPhase = (phaseId: string) => {
+  const queryClient = useQueryClient();
+  return useQuery<IJobs | undefined, CLErrors>({
     queryKey: jobsKeys.list({ phaseId }),
     queryFn: () => fetchInProgressPhaseJobs(phaseId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        // Ideally, we should only invalidate the ideas list for the current phase,
+        // but since the ideas list is not filtered by phase, we invalidate all ideas for now.
+        queryKey: ideasKeys.lists(),
+      });
+    },
     keepPreviousData: true,
+    refetchInterval: (data) => {
+      // Stop polling if data is undefined or if its "data" property is an empty array.
+      if (!data || data.data.length === 0) {
+        return false;
+      }
+
+      // Otherwise, if there are jobs, refetch every 5 seconds.
+      return 5000;
+    },
   });
+};
 
 export default useJobInProgressByPhase;
