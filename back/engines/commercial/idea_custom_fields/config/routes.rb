99e86568e82cc576fb6640eb0babdeff451d1536
jinjagit <jinjamail@gmail.com>
2025-06-18 15:02:17 +0100

    [TAN-4791] Remove temprary Admin scope from routes and start 'fixing' FE requests

--- back/engines/commercial/idea_custom_fields/config/routes.rb
+++ back/engines/commercial/idea_custom_fields/config/routes.rb
@@ -3,30 +3,27 @@
 IdeaCustomFields::Engine.routes.draw do
   namespace :web_api, defaults: { format: :json } do
     namespace :v1 do
-      # Use scope instead of namespace to keep 'admin' in the URL
-      scope path: 'admin' do
-        resources :projects, only: [] do
-          resources(
-            :custom_fields,
-            only: %i[index show],
-            controller: 'idea_custom_fields',
-            defaults: { container_type: 'Project' }
-          ) do
-            patch 'update_all', on: :collection
-            resources :custom_field_matrix_statements, controller: '/web_api/v1/custom_field_matrix_statements', only: %i[index show]
-          end
+      resources :projects, only: [] do
+        resources(
+          :custom_fields,
+          only: %i[index show],
+          controller: 'idea_custom_fields',
+          defaults: { container_type: 'Project' }
+        ) do
+          patch 'update_all', on: :collection
+          resources :custom_field_matrix_statements, controller: '/web_api/v1/custom_field_matrix_statements', only: %i[index show]
         end
-        resources :phases, only: [] do
-          resources(
-            :custom_fields,
-            only: %i[index show],
-            controller: 'idea_custom_fields',
-            defaults: { container_type: 'Phase' }
-          ) do
-            patch 'update_all', on: :collection
-            get :as_geojson, on: :member, action: 'as_geojson'
-            resources :custom_field_matrix_statements, controller: '/web_api/v1/custom_field_matrix_statements', only: %i[index show]
-          end
+      end
+      resources :phases, only: [] do
+        resources(
+          :custom_fields,
+          only: %i[index show],
+          controller: 'idea_custom_fields',
+          defaults: { container_type: 'Phase' }
+        ) do
+          patch 'update_all', on: :collection
+          get :as_geojson, on: :member, action: 'as_geojson'
+          resources :custom_field_matrix_statements, controller: '/web_api/v1/custom_field_matrix_statements', only: %i[index show]
         end
       end
     end
