eb57ce6200879945053619e6c2760c43fafcb5db
EdwinKato <katedwin88@gmail.com>
2025-06-18 11:39:04 +0300

    Include pagination and filters, enhancing project data display

--- front/app/containers/Admin/projects/all/new/Timeline/index.tsx
+++ front/app/containers/Admin/projects/all/new/Timeline/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Spinner, Text } from '@citizenlab/cl2-component-library';
+import { Box, Spinner, Text } from '@citizenlab/cl2-component-library';
 
 import useProjectsMiniAdmin from 'api/projects_mini_admin/useProjectsMiniAdmin';
 
 import useLocalize from 'hooks/useLocalize';
 
+import { PaginationWithoutPositioning } from 'components/Pagination';
 import Centerer from 'components/UI/Centerer';
 
 import { useIntl } from 'utils/cl-intl';
+import { getPageNumberFromUrl } from 'utils/paginationUtils';
+
+import Filters from '../Projects/Filters';
+import { useParams } from '../Projects/utils';
 
 import messages from './messages';
 import ProjectGanttChart, { GanttProject } from './ProjectGanttChart';
@@ -16,12 +21,19 @@ import ProjectGanttChart, { GanttProject } from './ProjectGanttChart';
 const Timeline = () => {
   const localize = useLocalize();
   const { formatMessage } = useIntl();
+  const [currentPage, setCurrentPage] = useState(1);
+  const params = useParams();
+
   const {
     data: projects,
     isLoading,
+    isFetching,
     isError,
   } = useProjectsMiniAdmin({
-    sort: 'phase_starting_or_ending_soon',
+    ...params,
+    sort: params.sort ?? 'phase_starting_or_ending_soon',
+    'page[size]': 10,
+    'page[number]': currentPage,
   });
 
   const projectsGanttData: GanttProject[] = (projects?.data || []).map(
@@ -31,9 +43,11 @@ const Timeline = () => {
       start: project.attributes.first_phase_start_date,
       end: project.attributes.last_phase_end_date,
       folder: localize(project.attributes.folder_title_multiloc),
-      daysLeft: 63,
     })
   );
+
+  const lastPageLink = projects?.links.last;
+  const lastPage = lastPageLink ? getPageNumberFromUrl(lastPageLink) ?? 1 : 1;
 
   if (isLoading) {
     return (
@@ -47,7 +61,43 @@ const Timeline = () => {
     return <Text>{formatMessage(messages.failedToLoadTimelineError)}</Text>;
   }
 
-  return <ProjectGanttChart projects={projectsGanttData} />;
+  return (
+    <Box>
+      <Filters />
+
+      <Box position="relative" mt="16px">
+        <ProjectGanttChart projects={projectsGanttData} />
+
+        {isFetching && (
+          <Box
+            position="absolute"
+            w="100%"
+            top="0"
+            left="0"
+            right="0"
+            bottom="0"
+            bg="rgba(255, 255, 255, 0.7)"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            borderRadius="8px"
+          >
+            <Spinner />
+          </Box>
+        )}
+      </Box>
+
+      {lastPage > 1 && (
+        <Box mt="12px">
+          <PaginationWithoutPositioning
+            currentPage={currentPage}
+            totalPages={lastPage}
+            loadPage={setCurrentPage}
+          />
+        </Box>
+      )}
+    </Box>
+  );
 };
 
 export default Timeline;
