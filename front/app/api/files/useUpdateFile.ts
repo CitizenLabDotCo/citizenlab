548369b03efb2f741a6f60d9f926c67f855bb2d0
Amanda Anderson <anderson_amanda@mail.com>
2025-06-19 09:48:40 +0200

    Add remaining initial endpoints for file post and patch

---
+++ front/app/api/files/useUpdateFile.ts
@@ -0,0 +1,28 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { omit } from 'lodash-es';
+import { CLErrors } from 'typings';
+
+import fetcher from 'utils/cl-react-query/fetcher';
+
+import eventsKeys from './keys';
+import { IFile, IUpdateFileProperties } from './types';
+
+const updateFile = async (requestBody: IUpdateFileProperties) => {
+  return fetcher<IFile>({
+    path: `/file/${requestBody.id}`,
+    action: 'patch',
+    body: { ...omit(requestBody, 'id') },
+  });
+};
+
+const useUpdateFile = () => {
+  const queryClient = useQueryClient();
+  return useMutation<IFile, CLErrors, IUpdateFileProperties>({
+    mutationFn: updateFile,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: eventsKeys.lists() });
+    },
+  });
+};
+
+export default useUpdateFile;
