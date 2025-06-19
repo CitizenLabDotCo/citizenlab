db20b9cc88f5e183baac7ccea5659f7a68f45569
EdwinKato <katedwin88@gmail.com>
2025-06-19 12:00:53 +0300

    Move types

---
+++ front/app/components/UI/GanttChart/types.ts
@@ -0,0 +1,23 @@
+export type GanttItem = {
+  id: string;
+  title: string;
+  start: string | null;
+  end: string | null;
+};
+
+export type GanttChartProps = {
+  chartTitle?: string;
+  items: GanttItem[];
+  startDate?: Date;
+  endDate?: Date;
+  leftColumnWidth?: number;
+  dayWidth?: number;
+  monthWidth?: number;
+  rowHeight?: number;
+  timelineHeight?: number;
+  onItemClick?: (item: GanttItem) => void;
+  renderItemTooltip?: (item: GanttItem) => React.ReactNode;
+  renderItemLabel?: (item: GanttItem) => React.ReactNode;
+  getItemColor?: (item: GanttItem) => string;
+  showTodayLine?: boolean;
+};
