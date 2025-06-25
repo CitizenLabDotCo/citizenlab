77f4949b26114bb7eda0d10ddabe78065c360b1b
Sébastien Hoorens <sebastien@citizenlab.co>
2025-06-25 16:53:37 +0200

    Added campaign delivery tracking spec to remaining mailer specs (part 7)

--- back/engines/free/email_campaigns/spec/mailers/voting_phase_started_mailer_spec.rb
+++ back/engines/free/email_campaigns/spec/mailers/voting_phase_started_mailer_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require_relative 'shared_examples_for_campaign_delivery_tracking'
 
 RSpec.describe EmailCampaigns::VotingPhaseStartedMailer do
   describe 'campaign_mail' do
@@ -52,12 +53,15 @@ RSpec.describe EmailCampaigns::VotingPhaseStartedMailer do
       }
     end
 
-    let_it_be(:mail) { described_class.with(command: command, campaign: campaign).campaign_mail.deliver_now }
+    let_it_be(:mailer) { described_class.with(command: command, campaign: campaign) }
+    let_it_be(:mail) { mailer.campaign_mail.deliver_now }
     let_it_be(:body) { mail_body(mail) }
 
     before_all do
       EmailCampaigns::UnsubscriptionToken.create!(user_id: recipient.id)
     end
+
+    include_examples 'campaign delivery tracking'
 
     it 'renders the subject including project title' do
       expect(mail.subject).to match 'The voting phase started'
