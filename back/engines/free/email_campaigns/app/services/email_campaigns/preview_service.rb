3497042fb2dbc336a7c54c07302d9ebceca86c8b
jamesspeake <james@speake.org>
2025-06-27 14:03:27 +0100

    [TAN-4899] Moved preview data to separate service

---
+++ back/engines/free/email_campaigns/app/services/email_campaigns/preview_service.rb
@@ -0,0 +1,36 @@
+module EmailCampaigns
+  class PreviewService
+    PreviewUser = Struct.new(:first_name, :last_name, :display_name)
+    PreviewContentItem = Struct.new(:id, :title_multiloc, :body_multiloc, :url)
+    PreviewData = Struct.new(:idea, :project, :comment, :proposal, :author)
+
+    # Static content that can be used across all email previews.
+    def self.preview_data(recipient)
+        PreviewData.new(
+          idea: PreviewContentItem.new(
+            id: SecureRandom.uuid,
+            title_multiloc: MultilocService.new.i18n_to_multiloc('email_campaigns.preview_data.idea_title'),
+            url: "/#{recipient.locale}/ideas/example-idea",
+            ),
+          proposal: PreviewContentItem.new(
+            id: SecureRandom.uuid,
+            title_multiloc: MultilocService.new.i18n_to_multiloc('email_campaigns.preview_data.idea_title'),
+            ),
+          project: PreviewContentItem.new(
+            id: SecureRandom.uuid,
+            title_multiloc: MultilocService.new.i18n_to_multiloc('email_campaigns.preview_data.project_title'),
+            url: "/#{recipient.locale}/projects/example-project",
+            ),
+          comment: PreviewContentItem.new(
+            id: SecureRandom.uuid,
+            body_multiloc: MultilocService.new.i18n_to_multiloc('email_campaigns.preview_data.comment_body'),
+            ),
+          author: PreviewUser.new(
+            first_name: I18n.t('email_campaigns.preview_data.author_first_name', locale: recipient.locale),
+            last_name: I18n.t('email_campaigns.preview_data.author_last_name', locale: recipient.locale),
+            display_name: I18n.t('email_campaigns.preview_data.author_display_name', locale: recipient.locale)
+          )
+        )
+    end
+  end
+end
