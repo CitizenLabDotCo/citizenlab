b2aa0c0dc60ec1921432b0a92762788f6ae2a146
brentguf <brentguf@users.noreply.github.com>
2025-07-01 11:01:17 +0200

    Add intercom targets

--- front/app/containers/Admin/communityMonitor/components/Settings/components/SurveySettings/index.tsx
+++ front/app/containers/Admin/communityMonitor/components/Settings/components/SurveySettings/index.tsx
@@ -114,7 +114,10 @@ const SurveySettings = () => {
             {formatMessage(messages.survey)}
           </Title>
           <Box display="flex" justifyContent="center" alignItems="center">
-            <Box mr="16px">
+            <Box
+              mr="16px"
+              data-intercom-target="community-monitor-open-for-responses-toggle"
+            >
               <Toggle
                 checked={postingEnabled}
                 label={formatMessage(messages.openForResponses)}
@@ -145,6 +148,7 @@ const SurveySettings = () => {
                   : clHistory.push(editFormLink);
               }}
               data-cy="e2e-edit-survey-content"
+              data-intercom-target="edit-community-monitor-survey-content"
             >
               {formatMessage(messages.editSurvey)}
             </ButtonWithLink>
