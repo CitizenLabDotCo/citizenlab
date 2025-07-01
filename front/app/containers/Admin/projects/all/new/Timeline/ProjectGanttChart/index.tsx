2412af65ed8746359f44e249ced9bd64a399cc15
EdwinKato <katedwin88@gmail.com>
2025-07-01 16:25:59 +0300

    Refactor ProjectGanttChart to use ProjectTooltip for item tooltips

--- front/app/containers/Admin/projects/all/new/Timeline/ProjectGanttChart/index.tsx
+++ front/app/containers/Admin/projects/all/new/Timeline/ProjectGanttChart/index.tsx
@@ -1,37 +1,16 @@
 import React from 'react';
 
-import { Box } from '@citizenlab/cl2-component-library';
-
 import GanttChart from 'components/UI/GanttChart';
 import { GanttItem } from 'components/UI/GanttChart/types';
 
 import { useIntl } from 'utils/cl-intl';
 import clHistory from 'utils/cl-router/history';
 
-import messages from '../messages';
+import messages from './messages';
+import ProjectTooltip from './ProjectTooltip';
 
-type ProjectGanttChartProps = {
-  projects: GanttItem[];
-};
-
-const ProjectGanttChart = ({ projects }: ProjectGanttChartProps) => {
+const ProjectGanttChart = ({ projects }: { projects: GanttItem[] }) => {
   const { formatMessage } = useIntl();
-
-  const renderProjectTooltip = (project: GanttItem) => {
-    return (
-      <Box p="8px">
-        <Box style={{ fontWeight: 'bold' }}>{project.title}</Box>
-        {project.start && project.end ? (
-          <Box mt="4px">
-            {new Date(project.start).toLocaleDateString()} -{' '}
-            {new Date(project.end).toLocaleDateString()}
-          </Box>
-        ) : (
-          <Box mt="4px">{formatMessage(messages.noEndDay)}</Box>
-        )}
-      </Box>
-    );
-  };
 
   const onItemLabelClick = (project: GanttItem) => {
     clHistory.push(`/admin/projects/${project.id}`);
@@ -40,7 +19,7 @@ const ProjectGanttChart = ({ projects }: ProjectGanttChartProps) => {
   return (
     <GanttChart
       items={projects}
-      renderItemTooltip={renderProjectTooltip}
+      renderItemTooltip={(project) => <ProjectTooltip project={project} />}
       chartTitle={formatMessage(messages.project)}
       onItemLabelClick={onItemLabelClick}
     />
