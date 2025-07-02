62a03c0c370d496c1d57d07cd6c0a598e8f1377b
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-07-02 10:40:53 +0200

    Fix mock stuff

--- front/app/hooks/useCountryCodeSupportedInProjectLibrary.ts
+++ front/app/hooks/useCountryCodeSupportedInProjectLibrary.ts
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 
-import useAppConfiguration from 'api/app_configuration/__mocks__/useAppConfiguration';
+import useAppConfiguration from 'api/app_configuration/useAppConfiguration';
 import { coreSettings } from 'api/app_configuration/utils';
 import useProjectLibraryCountries from 'api/project_library_countries/useProjectLibraryCountries';
 
 const useCountryCodeSupportedInProjectLibrary = () => {
   const { data: appConfiguration } = useAppConfiguration();
-  const { country_code } = coreSettings(appConfiguration.data);
+  const country_code = appConfiguration
+    ? coreSettings(appConfiguration.data).country_code
+    : null;
 
   // If the project library countries fetch fails,
   // react-query will retry the request indefinitely for some reason.
