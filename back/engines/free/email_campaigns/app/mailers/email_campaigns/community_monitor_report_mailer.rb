7bd1715acf336798c553412beb198a178ecd1b83
jamesspeake <james@speake.org>
2025-07-02 16:13:04 +0100

    [TAN-4899] Made community monitor report editable

--- back/engines/free/email_campaigns/app/mailers/email_campaigns/community_monitor_report_mailer.rb
+++ back/engines/free/email_campaigns/app/mailers/email_campaigns/community_monitor_report_mailer.rb
@@ -2,22 +2,38 @@
 
 module EmailCampaigns
   class CommunityMonitorReportMailer < ApplicationMailer
-    private
+    include EditableWithPreview
 
-    def subject
-      format_message('subject')
-    end
+    class << self
+      def campaign_class
+        Campaigns::CommunityMonitorReport
+      end
 
-    def header_title
-      format_message('title')
-    end
+      def editable_regions
+        [
+          define_editable_region(
+            :subject_multiloc, default_message_key: 'subject'
+          ),
+          define_editable_region(
+            :title_multiloc, default_message_key: 'title'
+          ),
+          define_editable_region(
+            :intro_multiloc, default_message_key: 'text_introduction', type: 'html', allow_blank_locales: true
+          ),
+          define_editable_region(
+            :button_text_multiloc, default_message_key: 'cta_report_button'
+          )
+        ]
+      end
 
-    def header_message
-      format_message('text_introduction')
-    end
-
-    def preheader
-      format_message('subject')
+      def preview_command(recipient: nil)
+        {
+          recipient: recipient,
+          event_payload:{
+            report_url: "#{Frontend::UrlService.new.home_url(locale: Locale.new(recipient.locale))}/admin/community-monitor/reports"
+          }
+        }
+      end
     end
   end
 end
