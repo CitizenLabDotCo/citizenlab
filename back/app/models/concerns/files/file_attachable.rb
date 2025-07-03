a75a38b849e295741f40e1ac2f15fbd8b9ddfa65
Adrien Dessy <209268+adessy@users.noreply.github.com>
2025-07-03 18:47:54 +0200

    [TAN-4958] Extract file attachment associations into `Files::FileAttachable` concern

---
+++ back/app/models/concerns/files/file_attachable.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Files
+  module FileAttachable
+    extend ActiveSupport::Concern
+
+    included do
+      has_many :file_attachments, -> { ordered },
+        as: :attachable,
+        class_name: 'Files::FileAttachment',
+        dependent: :destroy
+
+      has_many :attached_files, through: :file_attachments, source: :file, class_name: 'Files::File'
+    end
+  end
+end
