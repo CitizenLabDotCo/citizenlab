c97d88d15140cfdbcda44191fca3128b28759b61
Iva <iva@citizenlab.co>
2025-07-03 14:56:38 +0300

    Fix test

--- front/app/containers/App/CommunityMonitorModal/utils.ts
+++ front/app/containers/App/CommunityMonitorModal/utils.ts
@@ -1,32 +1,3 @@
-import { Layout, UISchemaElement } from '@jsonforms/core';
-
-// findFirstSentimentLinearScale:
-// Finds the first element with an input_type of 'sentiment_linear_scale' in a uiSchema and returns it.
-export function findFirstSentimentLinearScale(
-  uiSchema: Layout | null
-): any | null {
-  if (!uiSchema?.elements) return null; // Ensure schema and elements exist
-
-  let firstQuestionUiSchema: UISchemaElement | null = null;
-
-  // Loop through the pages, and find the first Sentiment Scale question
-  uiSchema.elements.forEach((page) => {
-    if (firstQuestionUiSchema) return; // We already found the first question
-
-    const questionMatch = page['elements'].find(
-      (element: UISchemaElement) =>
-        element.type === 'Control' &&
-        element.options?.input_type === 'sentiment_linear_scale'
-    );
-
-    if (questionMatch) {
-      firstQuestionUiSchema = questionMatch;
-    }
-  });
-
-  return firstQuestionUiSchema;
-}
-
 // isAllowedOnUrl:
 // This function checks if the user is on a custom page or the homepage.
 export const isAllowedOnUrl = (location: string) => {
