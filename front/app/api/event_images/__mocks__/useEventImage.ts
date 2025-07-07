c5da8b97d7b76dce17a24b235e56a11f839d2a43
brentguf <brentguf@users.noreply.github.com>
2025-07-07 11:30:47 +0200

    Add type and missing attributes to event image mock

--- front/app/api/event_images/__mocks__/useEventImage.ts
+++ front/app/api/event_images/__mocks__/useEventImage.ts
@@ -1,17 +1,20 @@
-export const eventImageData = [
-  {
-    id: '30c1b604-71fd-4ac4-9cd7-3e5601d9cb0f',
-    type: 'image',
-    attributes: {
-      ordering: null,
-      created_at: '2023-02-28T05:56:37.762Z',
-      updated_at: '2023-02-28T05:56:37.762Z',
-      versions: {
-        small: 'http://localhost:4000/uploads/small.png',
-        medium: 'http://localhost:4000/uploads/medium.png',
-        large: 'http://localhost:4000/uploads/large.png',
-        fb: 'http://localhost:4000/uploads/fb.png',
-      },
+import { IEventImageData } from '../types';
+
+export const eventImageData: IEventImageData = {
+  id: '30c1b604-71fd-4ac4-9cd7-3e5601d9cb0f',
+  type: 'image',
+  attributes: {
+    ordering: 1,
+    created_at: '2023-02-28T05:56:37.762Z',
+    updated_at: '2023-02-28T05:56:37.762Z',
+    versions: {
+      small: 'http://localhost:4000/uploads/small.png',
+      medium: 'http://localhost:4000/uploads/medium.png',
+      large: 'http://localhost:4000/uploads/large.png',
+      fb: 'http://localhost:4000/uploads/fb.png',
+    },
+    alt_text_multiloc: {
+      en: 'An event image',
     },
   },
-];
+};
