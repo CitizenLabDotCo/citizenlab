95264dcfb158fa92069b4d5d1e3b4c9c6053c01a
Amanda Anderson <anderson_amanda@mail.com>
2025-07-08 14:18:15 +0200

    Code cleanup

--- front/app/containers/Admin/projects/project/files/components/FilesUpload/components/FileDropzone.tsx
+++ front/app/containers/Admin/projects/project/files/components/FilesUpload/components/FileDropzone.tsx
@@ -7,46 +7,46 @@ import {
   stylingConsts,
   colors,
 } from '@citizenlab/cl2-component-library';
-import { DropzoneInputProps, DropzoneRootProps } from 'react-dropzone';
+import { useDropzone } from 'react-dropzone';
 
 import { useIntl } from 'utils/cl-intl';
 
 import messages from '../../messages';
 
 type Props = {
-  getRootProps: <T extends DropzoneRootProps>(props?: T) => T;
-  getInputProps: <T extends DropzoneInputProps>(props?: T) => T;
+  getRootProps: ReturnType<typeof useDropzone>['getRootProps'];
+  getInputProps: ReturnType<typeof useDropzone>['getInputProps'];
   open: () => void;
 };
 
 const FileDropzone = ({ getRootProps, getInputProps, open }: Props) => {
   const { formatMessage } = useIntl();
+
+  // TODO: Add some additional bullet points before the dropzone once Product decides.
+
   return (
-    <>
-      TODO: Add some additional bullet points here once Product decides.
-      <Box
-        {...getRootProps()}
-        p="20px"
-        border={`1px dashed ${colors.coolGrey300}`}
-        borderRadius={stylingConsts.borderRadius}
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        flexDirection="column"
-        mt="30px"
-      >
-        <Text m="0px" color="coolGrey600">
-          {formatMessage(messages.dragAndDropFiles)}
-        </Text>
-        <Button
-          buttonStyle="admin-dark"
-          text={formatMessage(messages.chooseFiles)}
-          mt="10px"
-          onClick={open}
-        />
-        <input {...getInputProps()} />
-      </Box>
-    </>
+    <Box
+      {...getRootProps()}
+      p="20px"
+      border={`1px dashed ${colors.coolGrey300}`}
+      borderRadius={stylingConsts.borderRadius}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+      mt="30px"
+    >
+      <Text m="0px" color="coolGrey600">
+        {formatMessage(messages.dragAndDropFiles)}
+      </Text>
+      <Button
+        buttonStyle="admin-dark"
+        text={formatMessage(messages.chooseFiles)}
+        mt="10px"
+        onClick={open}
+      />
+      <input {...getInputProps()} />
+    </Box>
   );
 };
 
