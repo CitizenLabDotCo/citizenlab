f8890a8a8d9e201774dfc8eb626a608105813b34
Iva <iva@citizenlab.co>
2025-07-08 08:04:20 +0300

    Rename file from tsx to ts

--- front/app/components/SurveyTimeToComplete/messages.ts
+++ front/app/components/SurveyTimeToComplete/messages.ts
@@ -2,7 +2,7 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   xMinutesToComplete: {
-    id: 'app.components.CommunityMonitorModal.xMinutesToCompleteSurvey',
+    id: 'app.components.CommunityMonitorModal.xMinutesToComplete',
     defaultMessage:
       '{minutes, plural, =0 {Takes <1 minute} one {Takes 1 minute} other {Takes # minutes}}',
   },
