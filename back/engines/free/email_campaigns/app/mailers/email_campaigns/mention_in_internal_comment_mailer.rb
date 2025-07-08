ef6e2f9d13b1f3b1730875570e9405bf81ff87b3
jamesspeake <james@speake.org>
2025-07-08 13:53:28 +0100

    [TAN-4900] Converted internal comment emails to editable

--- back/engines/free/email_campaigns/app/mailers/email_campaigns/mention_in_internal_comment_mailer.rb
+++ back/engines/free/email_campaigns/app/mailers/email_campaigns/mention_in_internal_comment_mailer.rb
@@ -2,18 +2,5 @@
 
 module EmailCampaigns
   class MentionInInternalCommentMailer < BaseInternalCommentMailer
-    protected
-
-    def subject
-      format_message('subject', values: { firstName: event.initiating_user_first_name })
-    end
-
-    def header_title
-      format_message('main_header', values: { firstName: event.initiating_user_first_name })
-    end
-
-    def preheader
-      format_message('preheader', values: { authorNameFull: event.internal_comment_author_name })
-    end
   end
 end
