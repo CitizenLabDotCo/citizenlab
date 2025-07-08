e0ffce36008c580bf9524aba1c027898e50e8c2c
jamesspeake <james@speake.org>
2025-07-08 14:41:07 +0100

    [TAN-4900] Converted mention in official feedback to editable

--- back/engines/free/email_campaigns/app/services/email_campaigns/preview_service.rb
+++ back/engines/free/email_campaigns/app/services/email_campaigns/preview_service.rb
@@ -1,38 +1,39 @@
 module EmailCampaigns
   class PreviewService
-    PreviewUser = Struct.new(:first_name, :last_name, :display_name)
+    PreviewUser = Struct.new(:first_name, :last_name, :display_name, :display_name_multiloc)
     PreviewContentItem = Struct.new(:id, :title_multiloc, :body_multiloc, :url, :deleted_reason)
     PreviewEvent = Struct.new(:id, :title_multiloc, :address1, :address2_multiloc)
     PreviewData = Struct.new(:idea, :project, :comment, :proposal, :event, :author, :initiator, :organization_name, :placeholder_image_url)
 
     # Static content that can be used across all email previews.
     def preview_data(recipient)
+      multiloc_service = MultilocService.new
       PreviewData.new(
         idea: PreviewContentItem.new(
           id: SecureRandom.uuid,
-          title_multiloc: MultilocService.new.i18n_to_multiloc('email_campaigns.preview_data.idea_title'),
-          body_multiloc: MultilocService.new.i18n_to_multiloc('email_campaigns.preview_data.idea_body'),
+          title_multiloc: multiloc_service.i18n_to_multiloc('email_campaigns.preview_data.idea_title'),
+          body_multiloc: multiloc_service.i18n_to_multiloc('email_campaigns.preview_data.idea_body'),
           url: "/#{recipient.locale}/ideas/example-idea"
         ),
         proposal: PreviewContentItem.new(
           id: SecureRandom.uuid,
-          title_multiloc: MultilocService.new.i18n_to_multiloc('email_campaigns.preview_data.idea_title')
+          title_multiloc: multiloc_service.i18n_to_multiloc('email_campaigns.preview_data.idea_title')
         ),
         project: PreviewContentItem.new(
           id: SecureRandom.uuid,
-          title_multiloc: MultilocService.new.i18n_to_multiloc('email_campaigns.preview_data.project_title'),
+          title_multiloc: multiloc_service.i18n_to_multiloc('email_campaigns.preview_data.project_title'),
           url: "/#{recipient.locale}/projects/example-project"
         ),
         comment: PreviewContentItem.new(
           id: SecureRandom.uuid,
-          body_multiloc: MultilocService.new.i18n_to_multiloc('email_campaigns.preview_data.comment_body'),
+          body_multiloc: multiloc_service.i18n_to_multiloc('email_campaigns.preview_data.comment_body'),
           deleted_reason: I18n.t('email_campaigns.preview_data.comment_deleted_reason', locale: recipient.locale)
         ),
         event: PreviewEvent.new(
           id: SecureRandom.uuid,
-          title_multiloc: MultilocService.new.i18n_to_multiloc('email_campaigns.preview_data.event_title'),
+          title_multiloc: multiloc_service.i18n_to_multiloc('email_campaigns.preview_data.event_title'),
           address1: I18n.t('email_campaigns.preview_data.event_address1', locale: recipient.locale),
-          address2_multiloc: MultilocService.new.i18n_to_multiloc('email_campaigns.preview_data.event_address2')
+          address2_multiloc: multiloc_service.i18n_to_multiloc('email_campaigns.preview_data.event_address2')
         ),
         author: PreviewUser.new(
           first_name: I18n.t('email_campaigns.preview_data.author_first_name', locale: recipient.locale),
@@ -42,9 +43,10 @@ module EmailCampaigns
         initiator: PreviewUser.new(
           first_name: I18n.t('email_campaigns.preview_data.initiator_first_name', locale: recipient.locale),
           last_name: I18n.t('email_campaigns.preview_data.initiator_last_name', locale: recipient.locale),
-          display_name: I18n.t('email_campaigns.preview_data.initiator_display_name', locale: recipient.locale)
+          display_name: I18n.t('email_campaigns.preview_data.initiator_display_name', locale: recipient.locale),
+          display_name_multiloc: multiloc_service.i18n_to_multiloc('email_campaigns.preview_data.initiator_display_name')
         ),
-        organization_name: MultilocService.new.t(AppConfiguration.instance.settings('core', 'organization_name'), recipient.locale),
+        organization_name: multiloc_service.t(AppConfiguration.instance.settings('core', 'organization_name'), recipient.locale),
         placeholder_image_url: 'https://cl2-seed-and-template-assets.s3.eu-central-1.amazonaws.com/images/placeholder.jpg'
       )
     end
