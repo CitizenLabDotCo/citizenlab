7e13d521916d3e30d4395bd8c437bd0cc0f83ed6
EdwinKato <katedwin88@gmail.com>
2025-07-08 14:07:24 +0300

    Extract CurrentPhaseInfo and PhaseList components

--- front/app/containers/Admin/projects/all/new/Timeline/ProjectGanttChart/ProjectTooltip.tsx
+++ front/app/containers/Admin/projects/all/new/Timeline/ProjectGanttChart/ProjectTooltip.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Box, Text } from '@citizenlab/cl2-component-library';
 
 import useAppConfiguration from 'api/app_configuration/useAppConfiguration';
+import { IPhaseData } from 'api/phases/types';
 import usePhases from 'api/phases/usePhases';
 import { getCurrentPhase } from 'api/phases/utils';
 
@@ -17,89 +18,103 @@ import { getPeriodRemainingUntil } from 'utils/dateUtils';
 
 import messages from './messages';
 
+type CurrentPhaseInfoProps = {
+  currentPhase: IPhaseData | undefined;
+  tenantTimezone: string | undefined;
+};
+
+const CurrentPhaseInfo = ({
+  currentPhase,
+  tenantTimezone,
+}: CurrentPhaseInfoProps) => {
+  const localize = useLocalize();
+  const { formatMessage } = useIntl();
+  if (!currentPhase || !tenantTimezone) {
+    return null;
+  }
+
+  const phaseName = localize(currentPhase.attributes.title_multiloc);
+  const participationMethod = formatMessage(
+    participationMethodMessage[currentPhase.attributes.participation_method]
+  );
+
+  let daysLeftNode: React.ReactNode = null;
+
+  if (currentPhase.attributes.end_at) {
+    const daysLeft = getPeriodRemainingUntil(
+      currentPhase.attributes.end_at,
+      tenantTimezone,
+      'days'
+    );
+    if (daysLeft > 0) {
+      daysLeftNode = (
+        <Text fontWeight="bold" color="white" my="0px" variant="bodyS">
+          {formatMessage(messages.daysLeft, { days: daysLeft })}
+        </Text>
+      );
+    }
+  }
+
+  return (
+    <Box mt="8px">
+      <Text fontWeight="bold" color="white" my="0px" variant="bodyS">
+        {formatMessage(messages.currentPhase, {
+          phaseName,
+          participationMethod,
+        })}
+      </Text>
+      {daysLeftNode}
+    </Box>
+  );
+};
+
+const PhaseList = ({ phases }: { phases: IPhaseData[] }) => {
+  const { formatMessage } = useIntl();
+  const localize = useLocalize();
+
+  if (phases.length === 0) {
+    return <Box mt="8px">{formatMessage(messages.noPhases)}</Box>;
+  }
+
+  return (
+    <Box mt="8px">
+      <Box>
+        <Text fontWeight="bold" color="white" my="0px">
+          {formatMessage(messages.phaseListTitle)}
+        </Text>
+      </Box>
+      {phases.map((phase, idx) => (
+        <Box key={phase.id} ml="8px">
+          {formatMessage(messages.phaseListItem, {
+            number: idx + 1,
+            phaseName: localize(phase.attributes.title_multiloc),
+            participationMethod: formatMessage(
+              participationMethodMessage[phase.attributes.participation_method]
+            ),
+          })}
+        </Box>
+      ))}
+    </Box>
+  );
+};
+
 const ProjectTooltip = ({ project }: { project: GanttItem }) => {
   const { formatMessage } = useIntl();
   const { data: appConfiguration } = useAppConfiguration();
-  const localize = useLocalize();
   const { data: phasesData } = usePhases(project.id);
+
   const phases = phasesData?.data || [];
   const currentPhase = getCurrentPhase(phases);
   const folderName = project.folder || undefined;
+  const tenantTimezone =
+    appConfiguration?.data.attributes.settings.core.timezone;
+
   const startDate = project.start
     ? new Date(project.start).toLocaleDateString()
     : undefined;
   const endDate = project.end
     ? new Date(project.end).toLocaleDateString()
     : undefined;
-
-  const tenantTimezone =
-    appConfiguration?.data.attributes.settings.core.timezone;
-
-  if (!tenantTimezone) {
-    return null;
-  }
-
-  let daysLeftBlock: React.ReactNode = undefined;
-  let currentPhaseBlock: React.ReactNode = undefined;
-  if (currentPhase) {
-    const phaseName = localize(currentPhase.attributes.title_multiloc);
-    const participationMethod = formatMessage(
-      participationMethodMessage[currentPhase.attributes.participation_method]
-    );
-    if (currentPhase.attributes.end_at) {
-      const daysLeft = getPeriodRemainingUntil(
-        currentPhase.attributes.end_at,
-        tenantTimezone,
-        'days'
-      );
-      if (daysLeft > 0) {
-        daysLeftBlock = (
-          <Text fontWeight="bold" color="white" my="0px" variant="bodyS">
-            {formatMessage(messages.daysLeft, { days: daysLeft })}
-          </Text>
-        );
-      }
-    }
-    currentPhaseBlock = (
-      <Box mt="8px">
-        <Text fontWeight="bold" color="white" my="0px" variant="bodyS">
-          {formatMessage(messages.currentPhase, {
-            phaseName,
-            participationMethod,
-          })}
-        </Text>
-        {daysLeftBlock}
-      </Box>
-    );
-  }
-
-  let phasesBlock: React.ReactNode = undefined;
-  if (phases.length > 0) {
-    phasesBlock = (
-      <Box mt="8px">
-        <Box>
-          <Text fontWeight="bold" color="white" my="0px">
-            {formatMessage(messages.phaseListTitle)}
-          </Text>
-        </Box>
-        {phases.map((phase, idx) => (
-          <Box key={phase.id} ml="8px">
-            {formatMessage(messages.phaseListItem, {
-              number: idx + 1,
-              phaseName: localize(phase.attributes.title_multiloc),
-              participationMethod: formatMessage(
-                participationMethodMessage[
-                  phase.attributes.participation_method
-                ]
-              ),
-            })}
-          </Box>
-        ))}
-      </Box>
-    );
-  } else {
-    phasesBlock = <Box mt="8px">{formatMessage(messages.noPhases)}</Box>;
-  }
 
   return (
     <Box p="8px">
@@ -124,8 +139,12 @@ const ProjectTooltip = ({ project }: { project: GanttItem }) => {
       ) : (
         <Box mt="4px">{formatMessage(messages.noEndDate)}</Box>
       )}
-      {currentPhaseBlock}
-      {phasesBlock}
+
+      <CurrentPhaseInfo
+        currentPhase={currentPhase}
+        tenantTimezone={tenantTimezone}
+      />
+      <PhaseList phases={phases} />
     </Box>
   );
 };
