00a7b40513a568692327a3f3b5998fe951576c14
Iva <iva@citizenlab.co>
2025-07-09 16:00:27 +0300

    Fix e2e tests

--- front/cypress/e2e/auth/cookie_consent.cy.ts
+++ front/cypress/e2e/auth/cookie_consent.cy.ts
@@ -2,6 +2,7 @@ import { randomString, randomEmail } from '../../support/commands';
 
 describe('Cookie consent form for not-signed-in users', () => {
   it('Shows the correct options when not signed in', () => {
+    cy.clearCookies();
     cy.visit('/');
     cy.get('#e2e-landing-page').should('exist');
     cy.get('#e2e-cookie-banner').should('exist');
@@ -17,7 +18,8 @@ describe('Cookie consent form for signed-in users', () => {
   const email = randomEmail();
   const password = randomString();
 
-  before(() => {
+  beforeEach(() => {
+    cy.clearCookies();
     cy.apiSignup(firstName, lastName, email, password).then((user) => {
       cy.setLoginCookie(email, password);
     });
@@ -34,7 +36,8 @@ describe('Cookie consent form for signed-in users', () => {
 });
 
 describe('Cookie consent form for signed-in admins', () => {
-  before(() => {
+  beforeEach(() => {
+    cy.clearCookies();
     cy.setAdminLoginCookie();
   });
 
