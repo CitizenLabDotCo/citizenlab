f52c82c97438955bc5e3fa5925f37bc6bab0521d
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-07-14 11:41:48 +0200

    Add FE hook

---
+++ front/app/api/participant_counts/types.ts
@@ -0,0 +1,17 @@
+import { Keys } from 'utils/cl-react-query/types';
+
+import participantCountKeys from './keys';
+
+export type ParticipantCountsHash = Record<string, number>;
+
+export type ParticipantCounts = {
+  data: {
+    id: string;
+    type: 'participant_counts';
+    attributes: {
+      participant_counts: ParticipantCountsHash;
+    };
+  };
+};
+
+export type ParticipantCountsKeys = Keys<typeof participantCountKeys>;
---
+++ front/app/api/participant_counts/useParticipantCounts.ts
@@ -0,0 +1,31 @@
+import { useQuery } from '@tanstack/react-query';
+import { CLErrors } from 'typings';
+
+import fetcher from 'utils/cl-react-query/fetcher';
+
+import participantCountKeys from './keys';
+import { ParticipantCounts, ParticipantCountsKeys } from './types';
+
+const fetchParticipantCounts = async (projectIds: string[]) => {
+  return fetcher<ParticipantCounts>({
+    path: '/projects/participant_counts',
+    action: 'get',
+    queryParams: {
+      project_ids: projectIds,
+    },
+  });
+};
+
+const useParticipantCounts = (projectIds: string[]) => {
+  return useQuery<
+    ParticipantCounts,
+    CLErrors,
+    ParticipantCounts,
+    ParticipantCountsKeys
+  >({
+    queryKey: participantCountKeys.list({ project_ids: projectIds }),
+    queryFn: () => fetchParticipantCounts(projectIds),
+  });
+};
+
+export default useParticipantCounts;
