06b424e0385ed2d6116003678553a156507266aa
Sébastien Hoorens <sebastien@citizenlab.co>
2025-07-14 12:05:16 +0200

    spec fixes

--- back/engines/free/email_campaigns/spec/models/email_campaigns/campaign_spec.rb
+++ back/engines/free/email_campaigns/spec/models/email_campaigns/campaign_spec.rb
@@ -19,62 +19,6 @@ RSpec.describe EmailCampaigns::Campaign do
     end
   end
 
-  describe '#enabled' do
-    let(:global_enabled) { true }
-    let!(:global_campaign) { create(:project_phase_started_campaign, enabled: global_enabled) }
-
-    context 'on a global campaign without context campaigns' do
-      it 'returns true if the global campaign is enabled' do
-        expect(global_campaign.enabled).to be true
-      end
-
-      describe do
-        let(:global_enabled) { false }
-
-        it 'returns false if the global campaign is disabled' do
-          expect(global_campaign.enabled).to be false
-        end
-      end
-    end
-
-    context 'on context campaigns' do
-      let(:context_enabled) { true }
-      let(:context_campaign) { create(:project_phase_started_campaign, context: create(:phase), enabled: context_enabled) }
-
-      it 'returns true if the global and context campaigns are both enabled' do
-        expect(context_campaign.enabled).to be true
-        expect(global_campaign.enabled).to be true
-      end
-
-      describe do
-        let(:context_enabled) { false }
-
-        it 'returns false if the context campaign is disabled while the global campaign is enabled' do
-          expect(context_campaign.enabled).to be false
-          expect(global_campaign.enabled).to be true
-        end
-      end
-
-      describe do
-        let(:global_enabled) { false }
-
-        it 'returns false if the global campaign is disabled while the context campaign is enabled' do
-          expect(context_campaign.enabled).to be false
-          expect(global_campaign.enabled).to be false
-        end
-
-        describe do
-          let(:context_enabled) { false }
-
-          it 'returns false if both the global and context campaigns are disabled' do
-            expect(context_campaign.enabled).to be false
-            expect(global_campaign.enabled).to be false
-          end
-        end
-      end
-    end
-  end
-
   describe '#activity_context' do
     let(:campaign) { create(:comment_on_your_comment_campaign) }
     let(:notification_activity) { create(:activity, item: create(:notification)) }
