6f252ae67ffc361082b6910eb4af2e65a9a0a959
EdwinKato <katedwin88@gmail.com>
2025-07-16 10:20:12 +0300

    Refactor project folders table to use infinite scrolling

--- front/app/containers/Admin/projects/all/new/Folders/Table/index.tsx
+++ front/app/containers/Admin/projects/all/new/Folders/Table/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useMemo, useRef } from 'react';
 
 import {
   Box,
@@ -11,12 +11,13 @@ import {
   stylingConsts,
   Spinner,
 } from '@citizenlab/cl2-component-library';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useInView } from 'react-intersection-observer';
 
-import useProjectFoldersAdmin from 'api/project_folders_mini/useProjectFoldersAdmin';
-
-import { PaginationWithoutPositioning } from 'components/Pagination';
+import { MiniProjectFolders, Parameters } from 'api/project_folders_mini/types';
 
 import { useIntl } from 'utils/cl-intl';
+import fetcher from 'utils/cl-react-query/fetcher';
 import { getPageNumberFromUrl } from 'utils/paginationUtils';
 
 import { useParams } from '../utils';
@@ -24,23 +25,92 @@ import { useParams } from '../utils';
 import messages from './messages';
 import Row from './Row';
 
-const Table = () => {
+const PAGE_SIZE = 10;
+
+const fetchProjectFoldersAdmin = async (
+  queryParams: Parameters
+): Promise<MiniProjectFolders> =>
+  fetcher<MiniProjectFolders>({
+    path: '/project_folders/for_admin',
+    action: 'get',
+    queryParams: {
+      ...queryParams,
+      'page[number]': queryParams['page[number]'] ?? 1,
+      'page[size]': queryParams['page[size]'] ?? PAGE_SIZE,
+    },
+  });
+
+const Table: React.FC = () => {
   const { formatMessage } = useIntl();
-  const [currentPage, setCurrentPage] = useState(1);
+  const { ...params } = useParams();
+  const didFetchRef = useRef(false);
 
-  const { data: folders, isFetching } = useProjectFoldersAdmin(useParams());
+  const {
+    data,
+    isFetching,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+    status,
+  } = useInfiniteQuery(
+    ['projectFolders', params],
+    ({ pageParam = 1 }) =>
+      fetchProjectFoldersAdmin({
+        ...params,
+        'page[number]': pageParam,
+        'page[size]': PAGE_SIZE,
+      }),
+    {
+      getNextPageParam: (lastPage) => {
+        const nextLink = lastPage.links?.next;
+        return nextLink ? getPageNumberFromUrl(nextLink) : undefined;
+      },
+    }
+  );
 
-  const lastPageLink = folders?.links?.last;
-  const lastPage = lastPageLink ? getPageNumberFromUrl(lastPageLink) ?? 1 : 1;
+  // flatten pages into one list
+  const folders = useMemo(
+    () => data?.pages.flatMap((page) => page.data) ?? [],
+    [data?.pages]
+  );
+
+  // sentinel
+  const { ref: loadMoreRef, inView } = useInView({
+    rootMargin: '0px 0px 100px 0px',
+    threshold: 0,
+  });
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage && !didFetchRef.current) {
+      didFetchRef.current = true;
+      fetchNextPage();
+    } else if (!inView) {
+      didFetchRef.current = false;
+    }
+  }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage]);
+
+  const getSentinelMessage = () => {
+    if (isFetchingNextPage) {
+      return messages.loadingMore;
+    }
+    if (hasNextPage) {
+      return messages.scrollDownToLoadMore;
+    }
+    // Only show "All loaded" if the query is done
+    if (status === 'success') {
+      return messages.allFoldersHaveLoaded;
+    }
+
+    return null;
+  };
+  const sentinelMessage = getSentinelMessage();
 
   return (
     <Box position="relative" w="100%" h="100%">
       <TableComponent
         border={`1px solid ${colors.grey300}`}
         borderRadius={stylingConsts.borderRadius}
-        innerBorders={{
-          bodyRows: true,
-        }}
+        innerBorders={{ bodyRows: true }}
       >
         <Thead>
           <Tr background={colors.grey50}>
@@ -51,33 +121,23 @@ const Table = () => {
           </Tr>
         </Thead>
         <Tbody>
-          {folders?.data.map((folder) => (
-            <Row folder={folder} key={folder.id} />
+          {folders.map((folder) => (
+            <Row key={folder.id} folder={folder} />
           ))}
         </Tbody>
       </TableComponent>
-      {lastPage > 1 && (
-        <Box mt="12px">
-          <PaginationWithoutPositioning
-            currentPage={currentPage}
-            totalPages={lastPage}
-            loadPage={setCurrentPage}
-          />
-        </Box>
-      )}
-      {isFetching && (
+
+      <Box ref={loadMoreRef} mt="12px" display="flex" justifyContent="center">
+        {sentinelMessage && formatMessage(sentinelMessage)}
+      </Box>
+
+      {(isFetching || status === 'loading') && (
         <Box
-          position="absolute"
           w="100%"
-          top="0"
-          left="0"
-          right="0"
-          bottom="0"
-          bgColor="white"
+          p="4px"
           display="flex"
           alignItems="center"
           justifyContent="center"
-          opacity={0.7}
         >
           <Spinner />
         </Box>
