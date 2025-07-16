74ba6caa0b5f7ca5cbcf0a6a4e7614ed6e717723
EdwinKato <katedwin88@gmail.com>
2025-07-16 11:17:05 +0300

    Add useInfiniteProjectsMiniAdmin hook for infinite scrolling in projects admin

---
+++ front/app/api/projects_mini_admin/useInfiniteProjectsMiniAdmin.ts
@@ -0,0 +1,44 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { CLErrors } from 'typings';
+
+import fetcher from 'utils/cl-react-query/fetcher';
+import { getPageNumberFromUrl } from 'utils/paginationUtils';
+
+import miniProjectsKeys from './keys';
+import { ProjectsMiniAdmin, Parameters } from './types';
+
+const DEFAULT_PAGE_SIZE = 10;
+
+const fetchPage = async (
+  params: Omit<Parameters, 'page[number]' | 'page[size]'>,
+  page: number,
+  pageSize: number
+): Promise<ProjectsMiniAdmin> => {
+  return fetcher<ProjectsMiniAdmin>({
+    path: '/projects/for_admin',
+    action: 'get',
+    queryParams: {
+      ...params,
+      'page[number]': page,
+      'page[size]': pageSize,
+    },
+  });
+};
+
+export const useInfiniteProjectsMiniAdmin = (
+  params: Omit<Parameters, 'page[number]' | 'page[size]'>,
+  pageSize: number = DEFAULT_PAGE_SIZE
+) => {
+  return useInfiniteQuery<ProjectsMiniAdmin, CLErrors>(
+    // key: drop page info so pages vary only by pageParam
+    miniProjectsKeys.list(params),
+    // fetcher: given pageParam
+    ({ pageParam = 1 }) => fetchPage(params, pageParam, pageSize),
+    {
+      getNextPageParam: (lastPage) => {
+        const nextLink = lastPage.links.next;
+        return nextLink ? getPageNumberFromUrl(nextLink) : undefined;
+      },
+    }
+  );
+};
