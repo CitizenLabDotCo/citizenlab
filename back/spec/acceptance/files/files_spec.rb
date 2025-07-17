c3bc33e271bb4c15cc15f5e5ed99973046523611
Adrien Dessy <209268+adessy@users.noreply.github.com>
2025-07-17 08:50:42 +0200

    [TAN-4829] Fix broken test

--- back/spec/acceptance/files/files_spec.rb
+++ back/spec/acceptance/files/files_spec.rb
@@ -204,14 +204,19 @@ resource 'Files' do
 
         assert_status 201
 
+        file = Files::File.find(response_data[:id])
+        # It has been correctly associated with the project
+        expect(file.project_ids).to contain_exactly(project)
+
         expect(response_data).to match(
           id: be_present,
           type: 'file',
           attributes: {
             name: name,
-            size: 1_645_987,
+            content: { url: file.content.url },
             mime_type: 'application/pdf',
             category: 'other',
+            size: 1_645_987,
             created_at: be_present,
             updated_at: be_present
           },
@@ -220,9 +225,6 @@ resource 'Files' do
             projects: { data: [{ id: project, type: 'project' }] }
           }
         )
-
-        file = Files::File.find(response_data[:id])
-        expect(file.project_ids).to contain_exactly(project)
       end
 
       example 'Create a file with category' do
