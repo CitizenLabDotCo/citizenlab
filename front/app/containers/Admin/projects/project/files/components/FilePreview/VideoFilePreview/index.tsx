2bdbd36ea01c4b6e596f7fa4f3a04d3248749651
Amanda Anderson <anderson_amanda@mail.com>
2025-07-18 15:57:34 +0200

    Code cleanup after code review

--- front/app/containers/Admin/projects/project/files/components/FilePreview/VideoFilePreview/index.tsx
+++ front/app/containers/Admin/projects/project/files/components/FilePreview/VideoFilePreview/index.tsx
@@ -4,14 +4,17 @@ import { Box, stylingConsts } from '@citizenlab/cl2-component-library';
 
 type Props = {
   url: string;
+  title?: string;
 };
 
-const VideoFilePreview = ({ url }: Props) => {
+const VideoFilePreview = ({ url, title }: Props) => {
   return (
     <Box mt="24px">
       <video
         src={url}
+        title={title}
         controls
+        preload="metadata"
         style={{
           width: '100%',
           maxHeight: '500px',
