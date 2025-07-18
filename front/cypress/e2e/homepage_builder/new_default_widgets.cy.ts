5ba8989bdfa576f91fa54133633b096d7bbdfeda
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-07-18 17:13:20 -0300

    Delete tests

--- front/cypress/e2e/homepage_builder/new_default_widgets.cy.ts
+++ front/cypress/e2e/homepage_builder/new_default_widgets.cy.ts
@@ -72,18 +72,4 @@ describe('New default widgets', () => {
       cy.apiRemoveProject(projectId);
     }
   });
-
-  it('old project card: navigates to project page on click title', () => {
-    cy.dataCy('e2e-project-card').first().should('be.visible').click();
-
-    cy.url().should('include', '/en/projects');
-    cy.get('#e2e-project-page');
-  });
-
-  it('new (light) project card navigates to project page on click title', () => {
-    cy.dataCy('e2e-light-project-card').first().should('be.visible').click();
-
-    cy.url().should('include', '/en/projects');
-    cy.get('#e2e-project-page');
-  });
 });
