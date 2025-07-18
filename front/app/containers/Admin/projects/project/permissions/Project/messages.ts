b13c9f06a3b28ab190782f1c9c3f7169ac872e69
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-07-18 11:19:06 -0300

    Remove dead code

--- front/app/containers/Admin/projects/project/permissions/Project/messages.ts
+++
@@ -1,8 +0,0 @@
-import { defineMessages } from 'react-intl';
-
-export default defineMessages({
-  shouldProjectBeListed: {
-    id: 'app.containers.Admin.projects.project.permissions.Project.shouldProjectBeListed',
-    defaultMessage: 'Should the project be listed?',
-  },
-});
