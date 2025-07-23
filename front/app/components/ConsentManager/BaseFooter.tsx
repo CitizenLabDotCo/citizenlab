8f72e03981ea0fc3e47012b6d91a18668c077908
brentguf <brentguf@users.noreply.github.com>
2025-07-23 10:58:42 +0200

    Refactor Footer components to use BaseFooter for consistent layout

---
+++ front/app/components/ConsentManager/BaseFooter.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react';
+
+import { Box } from '@citizenlab/cl2-component-library';
+
+interface Props {
+  children: ReactNode;
+}
+
+const BaseFooter = ({ children }: Props) => {
+  return (
+    <Box
+      width="100%"
+      display="flex"
+      alignItems="center"
+      justifyContent="flex-end"
+      gap={'8px'}
+    >
+      {children}
+    </Box>
+  );
+};
+
+export default BaseFooter;
