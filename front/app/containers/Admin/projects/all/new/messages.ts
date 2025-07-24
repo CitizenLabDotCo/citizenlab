5f61d6dbc9f0a8616f4e003d19dd3d2e9a7528c4
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-07-24 15:49:57 -0300

    Fix duplicate message id

--- front/app/containers/Admin/projects/all/new/messages.ts
+++ front/app/containers/Admin/projects/all/new/messages.ts
@@ -18,7 +18,7 @@ export default defineMessages({
     defaultMessage: 'Ordering',
   },
   draft: {
-    id: 'app.containers.Admin.projects.all.new.Projects.Table.draft',
+    id: 'app.containers.Admin.projects.all.new.Projects.draft',
     defaultMessage: 'Draft',
   },
   published: {
