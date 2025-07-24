96ccf231803176558ea41d0f0ba550b68076a575
brentguf <brentguf@users.noreply.github.com>
2025-07-24 09:40:53 +0200

    Add comment to clarify behavior when accessing cookie policy from modal

--- front/app/components/ConsentManager/index.tsx
+++ front/app/components/ConsentManager/index.tsx
@@ -59,6 +59,12 @@ const ConsentManager = () => {
   );
 
   useEffect(() => {
+    /*
+      When we click the link to the cookie policy in the modal, 
+      we wouldn't be able to read the cookie policy without this, 
+      because the modal is on top of the page.
+      Search the codebase for 'from=cookie-modal' to see where this is used.
+    */
     if (from === 'cookie-modal') {
       setScreen(null);
     } else if (isConsentRequired) {
