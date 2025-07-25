95ac2d2b262e596792f36ab0d68f4a460710e2ad
kogre <koen@govocal.com>
2025-07-25 15:22:00 +0200

    Retry error logging and cleanup

--- back/app/jobs/files/generate_preview_job.rb
+++ back/app/jobs/files/generate_preview_job.rb
@@ -4,19 +4,11 @@ module Files
 
     def run(preview)
       PreviewService.new.generate_preview_content(preview)
-      ic error_count
     rescue StandardError => e
       if error_count >= (self.class.maximum_retry_count - 1)
-        ErrorReporter.report(
-          e,
-          extra: {
-            file_id: preview.file.id,
-            file_name: preview.file.name,
-            file_content_type: preview.file.content.content_type
-          }
-        )
         preview.update!(status: 'failed')
       end
+      raise e
     end
   end
 end
