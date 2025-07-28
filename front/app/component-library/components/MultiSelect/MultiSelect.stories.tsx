f394dd79a39ffc8cd6bd1eb13ec8a0b6198b94d9
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-07-28 11:35:06 -0300

    Add title prop

--- front/app/component-library/components/MultiSelect/MultiSelect.stories.tsx
+++ front/app/component-library/components/MultiSelect/MultiSelect.stories.tsx
@@ -11,5 +11,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    title: 'Multi-select',
+  },
 };
--- front/app/component-library/components/MultiSelect/index.tsx
+++ front/app/component-library/components/MultiSelect/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 import InputContainer from 'component-library/utils/containers/InputContainer';
 
-const MultiSelect = () => {
-  return <InputContainer>Multiselect</InputContainer>;
+interface Props {
+  title: string | JSX.Element;
+}
+
+const MultiSelect = ({ title }: Props) => {
+  return <InputContainer>{title}</InputContainer>;
 };
 
 export default MultiSelect;
