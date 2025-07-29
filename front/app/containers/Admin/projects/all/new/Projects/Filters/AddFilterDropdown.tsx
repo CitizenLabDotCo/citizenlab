13944641376d2f3dde76b6c94e1561c2d2aaa518
EdwinKato <katedwin88@gmail.com>
2025-07-29 10:14:08 +0300

    Add AddFilterDropdown component for dynamic filter selection

---
+++ front/app/containers/Admin/projects/all/new/Projects/Filters/AddFilterDropdown.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+
+import {
+  Box,
+  Button,
+  Dropdown,
+  DropdownListItem,
+} from '@citizenlab/cl2-component-library';
+
+import { useIntl } from 'utils/cl-intl';
+
+import { FilterType } from './DynamicFilters';
+import messages from './messages';
+
+interface FilterConfig {
+  type: FilterType;
+  label: string;
+  paramKey: string;
+  component: React.ComponentType<any>;
+}
+
+interface Props {
+  availableFilters: FilterConfig[];
+  onAddFilter: (filterType: FilterType) => void;
+}
+
+const AddFilterDropdown = ({ availableFilters, onAddFilter }: Props) => {
+  const { formatMessage } = useIntl();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleAddFilter = (filterType: FilterType) => {
+    onAddFilter(filterType);
+    setIsOpen(false);
+  };
+
+  return (
+    <Box position="relative">
+      <Button
+        buttonStyle="secondary"
+        icon="plus"
+        onClick={() => setIsOpen(!isOpen)}
+        text={formatMessage(messages.addFilter)}
+      />
+      <Dropdown
+        opened={isOpen}
+        onClickOutside={() => setIsOpen(false)}
+        content={
+          <Box>
+            {availableFilters.map((filter, index) => (
+              <DropdownListItem
+                key={index}
+                onClick={() => handleAddFilter(filter.type)}
+              >
+                {filter.label}
+              </DropdownListItem>
+            ))}
+          </Box>
+        }
+      />
+    </Box>
+  );
+};
+
+export default AddFilterDropdown;
