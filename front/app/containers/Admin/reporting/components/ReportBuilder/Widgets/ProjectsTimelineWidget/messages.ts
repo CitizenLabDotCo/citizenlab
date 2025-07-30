cf4eeb80baad251dbedfa45abdade8bd5b1c6ca7
EdwinKato <katedwin88@gmail.com>
2025-07-30 16:32:00 +0300

    Add localization messages for ProjectsTimelineWidget

--- front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ProjectsTimelineWidget/messages.ts
+++ front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ProjectsTimelineWidget/messages.ts
@@ -14,7 +14,139 @@ export default defineMessages({
     defaultMessage: 'Published',
   },
   archived: {
-    id: 'app.containers/Admin/reporting/components/ReportBuilder/Widgets/ProjectsTimelineWidget.archived',
+    id: 'app.containers/Admin/reporting/components/ReportBuilder/ProjectsTimelineWidget.archived',
     defaultMessage: 'Archived',
+  },
+  participationStates: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.participationStates',
+    defaultMessage: 'Participation states',
+  },
+  notStarted: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.notStarted',
+    defaultMessage: 'Not started',
+  },
+  collectingData: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.collectingData',
+    defaultMessage: 'Collecting data',
+  },
+  informing: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.informing',
+    defaultMessage: 'Informing',
+  },
+  past: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.past',
+    defaultMessage: 'Past',
+  },
+  visibility: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.visibility',
+    defaultMessage: 'Visibility',
+  },
+  public: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.public',
+    defaultMessage: 'Public',
+  },
+  groups: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.groups',
+    defaultMessage: 'Groups',
+  },
+  admins: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.admins',
+    defaultMessage: 'Admins',
+  },
+  discoverability: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.discoverability',
+    defaultMessage: 'Discoverability',
+  },
+  listed: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.listed',
+    defaultMessage: 'Listed',
+  },
+  unlisted: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.unlisted',
+    defaultMessage: 'Unlisted',
+  },
+  defaultTimeRange: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.defaultTimeRange',
+    defaultMessage: 'Default time range',
+  },
+  month: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.month',
+    defaultMessage: 'Month',
+  },
+  quarter: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.quarter',
+    defaultMessage: 'Quarter',
+  },
+  year: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.year',
+    defaultMessage: 'Year',
+  },
+  multiyear: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.multiyear',
+    defaultMessage: 'Multi-year',
+  },
+  showProjectBars: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.showProjectBars',
+    defaultMessage: 'Show project bars',
+  },
+  showTodayLine: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.showTodayLine',
+    defaultMessage: 'Show today line',
+  },
+  colorByStatus: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.colorByStatus',
+    defaultMessage: 'Color by status',
+  },
+  yes: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.yes',
+    defaultMessage: 'Yes',
+  },
+  no: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.no',
+    defaultMessage: 'No',
+  },
+  search: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.search',
+    defaultMessage: 'Search projects',
+  },
+  participationMethods: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.participationMethods',
+    defaultMessage: 'Participation methods',
+  },
+  ideation: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.ideation',
+    defaultMessage: 'Ideation',
+  },
+  voting: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.voting',
+    defaultMessage: 'Voting',
+  },
+  information: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.information',
+    defaultMessage: 'Information',
+  },
+  survey: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.survey',
+    defaultMessage: 'Survey',
+  },
+  poll: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.poll',
+    defaultMessage: 'Poll',
+  },
+  documentAnnotation: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.documentAnnotation',
+    defaultMessage: 'Document Annotation',
+  },
+  volunteering: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.volunteering',
+    defaultMessage: 'Volunteering',
+  },
+  managers: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.managers',
+    defaultMessage: 'Managers',
+  },
+  folders: {
+    id: 'app.containers.Admin.reporting.components.ReportBuilder.Widgets.ProjectsTimelineWidget.folders',
+    defaultMessage: 'Folders',
   },
 });
