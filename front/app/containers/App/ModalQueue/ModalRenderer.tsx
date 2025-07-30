302d378d101e1c3189aa13c81b8c13fe34fb930a
brentguf <brentguf@users.noreply.github.com>
2025-07-30 10:58:44 +0200

    Wrap modal component with Suspense to prevent page going blank when modal has not loaded yet

--- front/app/containers/App/ModalQueue/ModalRenderer.tsx
+++ front/app/containers/App/ModalQueue/ModalRenderer.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+
+import { Spinner } from '@citizenlab/cl2-component-library';
 
 import modalRegistry, { ModalId } from './modals/modalRegistry';
 
@@ -7,7 +9,14 @@ const ModalRenderer = ({ modalId }: { modalId: ModalId | null }) => {
 
   const ModalComponent = modalRegistry[modalId].component;
 
-  return <ModalComponent />;
+  return (
+    // Suspense is needed because the modals are loaded lazily.
+    // Removing the Suspense will cause an error if there are multiple
+    // modals being rendered and the next one takes some time to load.
+    <Suspense fallback={<Spinner />}>
+      <ModalComponent />
+    </Suspense>
+  );
 };
 
 export default ModalRenderer;
