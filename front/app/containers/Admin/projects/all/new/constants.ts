94dfb24da08a695405055ce8d70e2ed33f245026
EdwinKato <katedwin88@gmail.com>
2025-07-31 14:51:59 +0300

    Add pending approval status to publication status labels

--- front/app/containers/Admin/projects/all/new/constants.ts
+++ front/app/containers/Admin/projects/all/new/constants.ts
@@ -5,10 +5,11 @@ import { MessageDescriptor } from 'utils/cl-intl';
 import messages from './messages';
 
 export const PUBLICATION_STATUS_LABELS: Record<
-  PublicationStatus,
+  PublicationStatus | 'pending_approval',
   MessageDescriptor
 > = {
   draft: messages.draft,
+  pending_approval: messages.pendingApproval,
   published: messages.published,
   archived: messages.archived,
 };
