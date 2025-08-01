8105a488a5f3743abaaf8d71fb1669a01183e518
jinjagit <jinjamail@gmail.com>
2025-08-01 14:34:07 +0100

    [TAN-5215] Specify polymorphic associations, and remove olymorphic_associations method

--- back/app/models/application_record.rb
+++ back/app/models/application_record.rb
@@ -14,12 +14,6 @@ class ApplicationRecord < ActiveRecord::Base
     self.to_be_destroyed = true
   end
 
-  def self.polymorphic_associations(name, as)
-    ActiveRecord::Base.descendants.select do |claz|
-      claz.reflect_on_all_associations.select { |asc| asc.name == name.to_sym && asc.options[:as] == as.to_sym }.present?
-    end
-  end
-
   def self.disable_inheritance
     initial_inheritance_column = inheritance_column
     self.inheritance_column = :_type_disabled
