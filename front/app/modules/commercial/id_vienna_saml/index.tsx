66c0ae99879e65298d6c4e05dcc35d5d3f85af77
jinjagit <jinjamail@gmail.com>
2025-08-01 14:01:26 +0100

    [TAN-5224] Fix id_vienna_saml

--- front/app/modules/commercial/id_vienna_saml/index.tsx
+++ front/app/modules/commercial/id_vienna_saml/index.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-
-import FeatureFlag from 'components/FeatureFlag';
+import React, { lazy, Suspense } from 'react';
 
 import { ModuleConfiguration } from 'utils/moduleUtils';
 
 import ViennaSamlButton from './components/ViennaSamlButton';
 
+const FeatureFlag = lazy(() => import('components/FeatureFlag'));
+
 const configuration: ModuleConfiguration = {
   outlets: {
     'app.components.SignUpIn.AuthProviders.ContainerStart': (props) => {
       return (
-        <FeatureFlag name="vienna_citizen_login">
-          <ViennaSamlButton {...props} />
-        </FeatureFlag>
+        <Suspense fallback={null}>
+          <FeatureFlag name="vienna_citizen_login">
+            <ViennaSamlButton {...props} />
+          </FeatureFlag>
+        </Suspense>
       );
     },
   },
