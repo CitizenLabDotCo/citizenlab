1bcdb6a16750740efedcdd3c957c26ac67e87e34
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-08-04 10:02:43 -0300

    Add other filter events

--- front/app/containers/Admin/projects/all/new/_shared/FilterBar/Filters/Status.tsx
+++ front/app/containers/Admin/projects/all/new/_shared/FilterBar/Filters/Status.tsx
@@ -4,12 +4,14 @@ import { PublicationStatus } from 'api/projects/types';
 
 import MultiSelect from 'components/UI/MultiSelect';
 
+import { trackEventByName } from 'utils/analytics';
 import { useIntl } from 'utils/cl-intl';
 
 import { PUBLICATION_STATUS_LABELS } from '../../constants';
 import { useParam, setParam } from '../../params';
 
 import messages from './messages';
+import tracks from './tracks';
 
 const OPTIONS = [
   { value: 'published', message: PUBLICATION_STATUS_LABELS.published },
@@ -38,6 +40,9 @@ const Status = ({ mr, onClear }: Props) => {
       mr={mr}
       onChange={(statuses) => {
         setParam('status', statuses as PublicationStatus[]);
+        trackEventByName(tracks.setStatus, {
+          statuses: JSON.stringify(statuses),
+        });
       }}
       title={formatMessage(messages.status)}
       onClear={onClear}
