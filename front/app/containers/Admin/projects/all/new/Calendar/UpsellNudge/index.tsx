3c84eb9000df7ff5c46d1c248868dd0e4df7b141
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-08-05 10:17:40 -0300

    Add premium icon

--- front/app/containers/Admin/projects/all/new/Calendar/UpsellNudge/index.tsx
+++ front/app/containers/Admin/projects/all/new/Calendar/UpsellNudge/index.tsx
@@ -14,44 +14,48 @@ import { useIntl } from 'utils/cl-intl';
 
 import Calendar from './calendar.png';
 import messages from './messages';
+import PremiumIcon from './PremiumIcon';
 
 const UpsellNudge = () => {
   const { formatMessage } = useIntl();
 
   return (
-    <Box>
-      <Box
-        background={colors.white}
-        borderRadius={stylingConsts.borderRadius}
-        display="flex"
-        gap="32px"
-        maxWidth="960px"
-      >
-        <Box maxWidth="480px">
-          <Title variant="h2">{formatMessage(messages.upsellTitle)}</Title>
-          <Text>{formatMessage(messages.upsellDescription)}</Text>
-          <Box mt="28px" display="flex" gap="12px">
-            <Tooltip content={formatMessage(messages.featureNotIncluded)}>
-              <Button disabled={true} icon="lock">
-                {formatMessage(messages.enableCalendarView)}
-              </Button>
-            </Tooltip>
+    <Box
+      background={colors.white}
+      borderRadius={stylingConsts.borderRadius}
+      display="flex"
+      gap="32px"
+      maxWidth="860px"
+      position="relative"
+    >
+      <Box maxWidth="430">
+        <Title variant="h2">{formatMessage(messages.upsellTitle)}</Title>
+        <Text>{formatMessage(messages.upsellDescription)}</Text>
+        <Box mt="28px" display="flex" gap="12px">
+          <Tooltip content={formatMessage(messages.featureNotIncluded)}>
+            <Button disabled={true} icon="lock">
+              {formatMessage(messages.enableCalendarView)}
+            </Button>
+          </Tooltip>
 
-            {/* <Button buttonStyle="text" textColor={theme.colors.tenantPrimary}>
+          {/* <Button buttonStyle="text" textColor={theme.colors.tenantPrimary}>
               {formatMessage(messages.learnMore)}
             </Button> */}
-          </Box>
         </Box>
-        <Box
-          display="flex"
-          flexDirection="column"
-          maxWidth="480px"
-          w="100%"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <img src={Calendar} alt="" width="100%" />
-        </Box>
+      </Box>
+      <Box
+        display="flex"
+        flexDirection="column"
+        maxWidth="430px"
+        w="100%"
+        alignItems="center"
+        justifyContent="flex-start"
+        mt="40px"
+      >
+        <img src={Calendar} alt="" width="100%" />
+      </Box>
+      <Box position="absolute" top="20px" right="0">
+        <PremiumIcon />
       </Box>
     </Box>
   );
