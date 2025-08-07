40e5069dd04e3afae454b2efe06207ce9d5aa039
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-08-07 10:42:13 -0300

    Add tests for FM

--- front/cypress/e2e/admin/projects/projects_overview.cy.ts
+++ front/cypress/e2e/admin/projects/projects_overview.cy.ts
@@ -131,6 +131,81 @@ describe('Projects overview: project moderator', () => {
   });
 });
 
-// describe('Projects overview: folder moderator', () => {
+describe('Projects overview: folder moderator', () => {
+  let userId: string;
+  let projectOneId: string;
+  let projectTwoId: string;
+  let folderId: string;
 
-// });
+  const projectOneTitle = randomString();
+  const projectTwoTitle = randomString();
+  const folderTitle = randomString();
+  const folderShortDescription = randomString(30);
+  const email = randomEmail();
+  const password = randomString();
+
+  before(() => {
+    cy.apiCreateProject({
+      title: projectOneTitle,
+      descriptionPreview: randomString(),
+      description: randomString(),
+      publicationStatus: 'published',
+    }).then((projectOne) => {
+      projectOneId = projectOne.body.data.id;
+      cy.apiCreateProject({
+        title: projectTwoTitle,
+        descriptionPreview: randomString(),
+        description: randomString(),
+        publicationStatus: 'published',
+      }).then((projectTwo) => {
+        projectTwoId = projectTwo.body.data.id;
+        cy.apiCreateFolder({
+          title: folderTitle,
+          descriptionPreview: folderShortDescription,
+          description: randomString(),
+          publicationStatus: 'published',
+        }).then((folder) => {
+          folderId = folder.body.data.id;
+
+          cy.apiAddProjectsToFolder(
+            [projectOneId, projectTwoId],
+            folderId
+          ).then(() => {
+            cy.apiCreateModeratorForFolder({
+              firstName: 'John',
+              lastName: 'Doe',
+              email,
+              password,
+              folderId,
+            }).then((moderator) => {
+              userId = moderator.body.data.id;
+            });
+          });
+        });
+      });
+    });
+  });
+
+  it('shows projects in the folder you can moderate', () => {
+    cy.setLoginCookie(email, password);
+    cy.visit('/admin/projects');
+    cy.dataCy('projects-overview-table-row').should('have.length', 2);
+
+    // Set sort
+    cy.dataCy('projects-overview-sort-select').select('recently_created_asc');
+
+    // Make sure projects are in the correct order
+    cy.dataCy('projects-overview-table-row').first().contains(projectOneTitle);
+    cy.dataCy('projects-overview-table-row').last().contains(projectTwoTitle);
+  });
+
+  it('shows folders you can moderate', () => {
+    cy.setLoginCookie(email, password);
+    cy.visit('/admin/projects');
+    cy.dataCy('projects-overview-folders-tab').click();
+    cy.dataCy('projects-overview-folder-table-row').should('have.length', 1);
+    cy.dataCy('projects-overview-folder-table-row')
+      .first()
+      .contains(folderTitle);
+  });
+});
