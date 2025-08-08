f298b1b46dfefe10f3d69440547bb5023a0706c3
EdwinKato <katedwin88@gmail.com>
2025-08-08 21:53:12 +0300

    Remove unused option functions from ProjectsTimelineWidget utils

--- front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ProjectsTimelineWidget/Settings/utils.ts
+++ front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ProjectsTimelineWidget/Settings/utils.ts
@@ -4,68 +4,6 @@ import { ProjectSortableParam } from 'api/projects_mini_admin/types';
 
 import projectFilterMessages from 'containers/Admin/projects/all/new/_shared/FilterBar/Filters/messages';
 
-import messages from '../messages';
-
-export const getPublicationStatusOptions = (
-  formatMessage: FormatMessage
-): IOption[] => [
-  { value: 'published', label: formatMessage(messages.published) },
-  { value: 'archived', label: formatMessage(messages.archived) },
-];
-
-export const getBooleanOptions = (formatMessage: FormatMessage): IOption[] => [
-  { value: 'true', label: formatMessage(messages.yes) },
-  { value: 'false', label: formatMessage(messages.no) },
-];
-
-export const getParticipationStateOptions = (
-  formatMessage: FormatMessage
-): IOption[] => [
-  {
-    value: 'not_started',
-    label: formatMessage(projectFilterMessages.notStarted),
-  },
-  {
-    value: 'collecting_data',
-    label: formatMessage(projectFilterMessages.collectingData),
-  },
-  {
-    value: 'informing',
-    label: formatMessage(projectFilterMessages.informing),
-  },
-  { value: 'past', label: formatMessage(projectFilterMessages.past) },
-];
-
-export const getVisibilityOptions = (
-  formatMessage: FormatMessage
-): IOption[] => [
-  {
-    value: 'public',
-    label: formatMessage(projectFilterMessages.visibilityPublic),
-  },
-  {
-    value: 'groups',
-    label: formatMessage(projectFilterMessages.visibilityGroups),
-  },
-  {
-    value: 'admins',
-    label: formatMessage(projectFilterMessages.visibilityAdmins),
-  },
-];
-
-export const getDiscoverabilityOptions = (
-  formatMessage: FormatMessage
-): IOption[] => [
-  {
-    value: 'listed',
-    label: formatMessage(projectFilterMessages.discoverabilityPublic),
-  },
-  {
-    value: 'unlisted',
-    label: formatMessage(projectFilterMessages.discoverabilityHidden),
-  },
-];
-
 export const getParticipationMethodOptions = (
   formatMessage: FormatMessage
 ): IOption[] => [
