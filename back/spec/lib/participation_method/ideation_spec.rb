bd2dd3df7080f260ef7a0a0fa4d730637612dfd9
Sébastien Hoorens <sebastien@citizenlab.co>
2025-08-12 17:27:55 +0200

    Participation method specs for supported campaigns

--- back/spec/lib/participation_method/ideation_spec.rb
+++ back/spec/lib/participation_method/ideation_spec.rb
@@ -172,6 +172,12 @@ RSpec.describe ParticipationMethod::Ideation do
     end
   end
 
+  describe '#supported_email_campaigns' do
+    it 'returns campaigns supported for ideation' do
+      expect(participation_method.supported_email_campaigns).to match_array %w[comment_deleted_by_admin comment_on_idea_you_follow comment_on_your_comment idea_published mention_in_official_feedback official_feedback_on_idea_you_follow project_phase_started status_change_on_idea_you_follow your_input_in_screening]
+    end
+  end
+
   describe '#supports_serializing?' do
     it 'returns false for all attributes' do
       %i[
