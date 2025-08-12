7a25fe38460657706bfe3dc3181f686469a60b7b
EdwinKato <katedwin88@gmail.com>
2025-08-12 12:55:57 +0300

    Refactor Folders component to manage filter open state with custom hook

--- front/app/containers/Admin/projects/all/_shared/FilterBar/Filters/Folders.tsx
+++ front/app/containers/Admin/projects/all/_shared/FilterBar/Filters/Folders.tsx
@@ -11,22 +11,32 @@ import { useIntl } from 'utils/cl-intl';
 
 import { useParam, setParam } from '../../params';
 
+import { useFilterOpenByDefault } from './hooks/useFilterOpenByDefault';
 import messages from './messages';
 import tracks from './tracks';
 
 interface Props {
   onClear: () => void;
+  shouldOpenByDefault?: boolean;
+  onOpened?: () => void;
 }
 
-const Folders = ({ onClear }: Props) => {
+const Folders = ({ onClear, shouldOpenByDefault, onOpened }: Props) => {
   const folderIds = useParam('folder_ids') ?? [];
   const { formatMessage } = useIntl();
   const localize = useLocalize();
+  const filterRef = useRef<HTMLDivElement>(null);
 
   // Disabled by default because we want to only make the request when the user opens the dropdown
   // This prevents unnecessary requests when the component is mounted and the user doesn't interact with it
   const { data, isLoading, refetch } = useProjectFolders({}, false);
   const hasFetchedRef = useRef(false);
+
+  const { isOpened } = useFilterOpenByDefault({
+    shouldOpenByDefault,
+    onOpened,
+    filterRef,
+  });
 
   const folderOptions =
     data?.data.map((folder) => ({
@@ -42,20 +52,24 @@ const Folders = ({ onClear }: Props) => {
   };
 
   return (
-    <MultiSelect
-      title={formatMessage(messages.folders)}
-      options={folderOptions}
-      selected={folderIds}
-      isLoading={isLoading}
-      onOpen={handleOpen}
-      onChange={(folderIds) => {
-        setParam('folder_ids', folderIds);
-        trackEventByName(tracks.setFolder, {
-          folderIds: JSON.stringify(folderIds),
-        });
-      }}
-      onClear={onClear}
-    />
+    <div ref={filterRef}>
+      <MultiSelect
+        title={formatMessage(messages.folders)}
+        options={folderOptions}
+        selected={folderIds}
+        isLoading={isLoading}
+        onOpen={handleOpen}
+        onChange={(folderIds) => {
+          setParam('folder_ids', folderIds);
+          trackEventByName(tracks.setFolder, {
+            folderIds: JSON.stringify(folderIds),
+          });
+        }}
+        onClear={onClear}
+        dataCy="projects-overview-filter-folders"
+        opened={isOpened}
+      />
+    </div>
   );
 };
 
