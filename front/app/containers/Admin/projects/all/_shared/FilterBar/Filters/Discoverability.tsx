4640df473c1be648a9a18c6ed8514be6259a292c
EdwinKato <katedwin88@gmail.com>
2025-08-12 12:56:05 +0300

    Refactor DiscoverabilityFilter to manage filter open state with custom hook

--- front/app/containers/Admin/projects/all/_shared/FilterBar/Filters/Discoverability.tsx
+++ front/app/containers/Admin/projects/all/_shared/FilterBar/Filters/Discoverability.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import { Discoverability } from 'api/projects_mini_admin/types';
 
@@ -9,6 +9,7 @@ import { MessageDescriptor, useIntl } from 'utils/cl-intl';
 
 import { useParam, setParam } from '../../params';
 
+import { useFilterOpenByDefault } from './hooks/useFilterOpenByDefault';
 import messages from './messages';
 import tracks from './tracks';
 
@@ -28,11 +29,24 @@ const OPTIONS: {
 
 interface Props {
   onClear: () => void;
+  shouldOpenByDefault?: boolean;
+  onOpened?: () => void;
 }
 
-const DiscoverabilityFilter = ({ onClear }: Props) => {
+const DiscoverabilityFilter = ({
+  onClear,
+  shouldOpenByDefault,
+  onOpened,
+}: Props) => {
   const value = useParam('discoverability') ?? [];
   const { formatMessage } = useIntl();
+  const filterRef = useRef<HTMLDivElement>(null);
+
+  const { isOpened, setIsOpened } = useFilterOpenByDefault({
+    shouldOpenByDefault,
+    onOpened,
+    filterRef,
+  });
 
   const options = OPTIONS.map((option) => ({
     label: formatMessage(option.message),
@@ -40,18 +54,23 @@ const DiscoverabilityFilter = ({ onClear }: Props) => {
   }));
 
   return (
-    <MultiSelect
-      title={formatMessage(messages.discoverabilityLabel)}
-      options={options}
-      selected={value}
-      onChange={(discoverability) => {
-        setParam('discoverability', discoverability as Discoverability[]);
-        trackEventByName(tracks.setDiscoverability, {
-          discoverabilities: JSON.stringify(discoverability),
-        });
-      }}
-      onClear={onClear}
-    />
+    <div ref={filterRef}>
+      <MultiSelect
+        title={formatMessage(messages.discoverabilityLabel)}
+        options={options}
+        selected={value}
+        onChange={(discoverability) => {
+          setParam('discoverability', discoverability as Discoverability[]);
+          trackEventByName(tracks.setDiscoverability, {
+            discoverabilities: JSON.stringify(discoverability),
+          });
+        }}
+        onClear={onClear}
+        dataCy="projects-overview-filter-discoverability"
+        opened={isOpened}
+        onOpen={() => setIsOpened(true)}
+      />
+    </div>
   );
 };
 
