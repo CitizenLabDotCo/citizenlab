e762f50e0f38b4035fb22f937999e82b9cfd31dc
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-08-13 20:37:16 -0300

    Make vimeo and YT fit kind of nicely

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/VideoEmbed/index.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/VideoEmbed/index.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
 
+import { Box } from '@citizenlab/cl2-component-library';
+import styled from 'styled-components';
+
 import messages from './messages';
 
-const markup = `
+const Container = styled.div`
+  overflow: hidden;
+  padding-bottom: 56.25%;
+  position: relative;
+  height: 0;
 
+  iframe {
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+  }
 `;
 
 interface Props {
   markup: string;
 }
 
-const VideoEmbed = ({ markup: _markup }: Props) => {
-  return <div dangerouslySetInnerHTML={{ __html: markup }} />;
+const VideoEmbed = ({ markup }: Props) => {
+  return (
+    <Box w="100%" display="flex" justifyContent="center">
+      <Box w="800px">
+        <Container>
+          <div dangerouslySetInnerHTML={{ __html: markup }} />
+        </Container>
+      </Box>
+    </Box>
+  );
 };
 
 export const videoEmbedTitle = messages.videoEmbed;
