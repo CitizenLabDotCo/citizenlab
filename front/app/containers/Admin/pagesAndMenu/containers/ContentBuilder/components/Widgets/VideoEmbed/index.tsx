cd87c4eda51e615ce4b1cc6c9df5e57009ee82a8
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-08-13 20:22:35 -0300

    Add to toolbox

--- front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/VideoEmbed/index.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/VideoEmbed/index.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 import messages from './messages';
 
-const VideoEmbed = () => {
-  return <></>;
+const markup = `
+
+`;
+
+interface Props {
+  markup: string;
+}
+
+const VideoEmbed = ({ markup: _markup }: Props) => {
+  return <div dangerouslySetInnerHTML={{ __html: markup }} />;
 };
 
 export const videoEmbedTitle = messages.videoEmbed;
