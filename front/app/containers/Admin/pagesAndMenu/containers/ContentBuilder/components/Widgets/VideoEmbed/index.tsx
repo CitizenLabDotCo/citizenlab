fb1f509600511b6808bd66c205968dde8458ffcc
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-08-13 17:04:20 -0300

    Scaffold widget

---
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/VideoEmbed/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import messages from './messages';
+
+const VideoEmbed = () => {
+  return <></>;
+};
+
+export const videoEmbedTitle = messages.videoEmbed;
+
+export default VideoEmbed;
---
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/VideoEmbed/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  videoEmbed: {
+    id: 'app.containers.Admin.pagesAndMenu.containers.ContentBuilder.components.Widgets.VideoEmbed.videoEmbed',
+    defaultMessage: 'Video Embed',
+  },
+});
