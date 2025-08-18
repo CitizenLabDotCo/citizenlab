7837d35b312a352a18af28e453bf64be7f9d2900
Adrien Dessy <209268+adessy@users.noreply.github.com>
2025-08-18 10:45:43 +0200

    [TAN-5175] Rework RubyLLM config to use Azure OpenAI

--- back/config/initializers/ruby_llm.rb
+++ back/config/initializers/ruby_llm.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
 RubyLLM.configure do |config|
-  config.openai_api_key = ENV.fetch('OPENAI_API_KEY', nil)
+  config.openai_api_key = ENV.fetch('AZURE_OPENAI_API_KEY')
+  config.openai_api_base = ENV.fetch('AZURE_OPENAI_URI')
 end
+
+# Patch RubyLLM to be able to use Azure OpenAI (instead of OpenAI).
+# Native support for Azure OpenAI is planned for a future version.
+# See https://github.com/crmne/ruby_llm/issues/15.
+module RubyLLMPatch
+  module Providers
+    module OpenAI
+      module Chat
+        def completion_url(...)
+          'openai/v1/chat/completions?api-version=preview'
+        end
+      end
+    end
+  end
+end
+
+RubyLLM::Providers::OpenAI::Chat.prepend(RubyLLMPatch::Providers::OpenAI::Chat)
