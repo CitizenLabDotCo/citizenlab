bb81aed68e6c2b767382ae385f0bc870d18e0ab7
EdwinKato <katedwin88@gmail.com>
2025-08-18 14:43:53 +0300

    Add MoreNavbarItem component for dropdown navigation

---
+++ front/app/containers/MainHeader/Components/DesktopNavItems/MoreNavbarItem.tsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+
+import { Dropdown, colors, Button } from '@citizenlab/cl2-component-library';
+import { RouteType } from 'routes';
+import styled from 'styled-components';
+import { Multiloc } from 'typings';
+
+import T from 'components/T';
+
+import { useIntl } from 'utils/cl-intl';
+import Link from 'utils/cl-router/Link';
+
+import messages from '../../messages';
+
+const DropdownListItem = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 11px 16px;
+  color: ${colors.textSecondary};
+  text-decoration: none;
+  font-size: 14px;
+  font-weight: 500;
+  transition: background-color 100ms ease-out;
+  white-space: nowrap;
+
+  &:hover {
+    background-color: ${colors.grey300};
+    color: ${colors.textSecondary};
+    text-decoration: none;
+  }
+
+  &.active {
+    background-color: ${colors.teal500};
+    color: white;
+  }
+`;
+
+interface NavbarItemProps {
+  linkTo: RouteType;
+  navigationItemTitle: Multiloc;
+  onlyActiveOnIndex?: boolean;
+}
+
+interface Props {
+  overflowItems: NavbarItemProps[];
+}
+
+const MoreNavbarItem = ({ overflowItems }: Props) => {
+  const { formatMessage } = useIntl();
+  const [dropdownOpened, setDropdownOpened] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpened(!dropdownOpened);
+  };
+
+  const closeDropdown = () => {
+    setDropdownOpened(false);
+  };
+
+  if (overflowItems.length === 0) {
+    return null;
+  }
+
+  return (
+    <li style={{ position: 'relative', display: 'flex', alignItems: 'center' }}>
+      <Button
+        icon="dots-horizontal"
+        iconPos="right"
+        text={formatMessage(messages.more)}
+        onClick={toggleDropdown}
+        buttonStyle="text"
+      />
+      <Dropdown
+        opened={dropdownOpened}
+        onClickOutside={closeDropdown}
+        top="100%"
+        left="0"
+        width="200px"
+        zIndex="9999"
+        content={
+          <>
+            {overflowItems.map((item, index) => (
+              <DropdownListItem
+                key={index}
+                to={item.linkTo}
+                onlyActiveOnIndex={item.onlyActiveOnIndex}
+                onClick={closeDropdown}
+                scrollToTop
+              >
+                <T value={item.navigationItemTitle} />
+              </DropdownListItem>
+            ))}
+          </>
+        }
+      />
+    </li>
+  );
+};
+
+export default MoreNavbarItem;
