f46223adf92271e6b114eaee43ad93accd38fe6c
EdwinKato <katedwin88@gmail.com>
2025-08-19 16:07:25 +0300

    Add messages for character limits in CharacterLimitSettings component

---
+++ front/app/components/FormBuilder/components/FormBuilderSettings/CharacterLimitSettings/messages.ts
@@ -0,0 +1,16 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  characterLimits: {
+    id: 'app.components.formBuilder.characterLimits',
+    defaultMessage: 'Character limits',
+  },
+  minCharacters: {
+    id: 'app.components.formBuilder.minCharacters',
+    defaultMessage: 'Min characters',
+  },
+  maxCharacters: {
+    id: 'app.components.formBuilder.maxCharacters',
+    defaultMessage: 'Max characters',
+  },
+});
