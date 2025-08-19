6148a4badaed51cd60f8313c7129efb61c913b00
Sébastien Hoorens <sebastien@citizenlab.co>
2025-08-19 16:15:39 +0200

    Editable voting phase started and project published mailers

--- back/engines/free/email_campaigns/app/mailers/email_campaigns/project_published_mailer.rb
+++ back/engines/free/email_campaigns/app/mailers/email_campaigns/project_published_mailer.rb
@@ -2,30 +2,29 @@
 
 module EmailCampaigns
   class ProjectPublishedMailer < ApplicationMailer
-    private
+    include EditableWithPreview
 
-    def preheader
-      format_message('preheader')
+    def editable
+      %i[subject_multiloc title_multiloc intro_multiloc button_text_multiloc]
     end
 
-    def subject
-      format_message('subject', values: { organizationName: organization_name })
+    def substitution_variables
+      {
+        projectName: localize_for_recipient(event&.project_title_multiloc),
+        organizationName: organization_name
+      }
     end
 
-    def header_title
-      format_message('header_title', values: { organizationName: organization_name })
-    end
-
-    def header_message
-      format_message(
-        'header_message',
-        values: {
-          # post_title_multiloc is never present.
-          # TODO: fix. We previously removed this field from the event https://github.com/CitizenLabDotCo/citizenlab/pull/5556/files#diff-ade4ca4ef24ba6dab628047c521bfc1addf2e0255d401c931124ec7fea5a07f0L78
-          ideaTitle: localize_for_recipient(event['post_title_multiloc']),
-          organizationName: organization_name
+    def preview_command(recipient)
+      data = preview_service.preview_data(recipient)
+      {
+        recipient: recipient,
+        event_payload: {
+          project_title_multiloc: data.project.title_multiloc,
+          project_url: data.project.url,
+          unfollow_url: data.project.url
         }
-      )
+      }
     end
   end
 end
--- back/engines/free/email_campaigns/spec/mailers/voting_phase_started_mailer_spec.rb
+++ back/engines/free/email_campaigns/spec/mailers/voting_phase_started_mailer_spec.rb
@@ -5,15 +5,14 @@ require 'rails_helper'
 RSpec.describe EmailCampaigns::VotingPhaseStartedMailer do
   describe 'campaign_mail' do
     let_it_be(:recipient) { create(:user, locale: 'en') }
-    let_it_be(:campaign) { EmailCampaigns::Campaigns::VotingPhaseStarted.create! }
     let_it_be(:project) { create(:project_with_phases) }
     let_it_be(:command) do
       {
         recipient: recipient,
         event_payload: {
-          project_url: Frontend::UrlService.new.model_to_url(project, locale: Locale.new(recipient.locale)),
-          project_title_multiloc: project.title_multiloc,
-          phase_title_multiloc: project.phases.first.title_multiloc,
+          project_url: 'https://govocal.com/projects/example',
+          project_title_multiloc: { 'en' => 'Example project title' },
+          phase_title_multiloc: { 'en' => 'Example phase title' },
           ideas: [
             {
               title_multiloc: {
@@ -52,9 +51,10 @@ RSpec.describe EmailCampaigns::VotingPhaseStartedMailer do
       }
     end
 
-    let_it_be(:mailer) { described_class.with(command: command, campaign: campaign) }
-    let_it_be(:mail) { mailer.campaign_mail.deliver_now }
-    let_it_be(:body) { mail_body(mail) }
+    let(:campaign) { create(:voting_phase_started_campaign) }
+    let(:mailer) { described_class.with(command: command, campaign: campaign) }
+    let(:mail) { mailer.campaign_mail.deliver_now }
+    let(:body) { mail_body(mail) }
 
     before_all do
       EmailCampaigns::UnsubscriptionToken.create!(user_id: recipient.id)
@@ -63,8 +63,7 @@ RSpec.describe EmailCampaigns::VotingPhaseStartedMailer do
     include_examples 'campaign delivery tracking'
 
     it 'renders the subject including project title' do
-      expect(mail.subject).to match 'The voting phase started'
-      expect(mail.subject).to match project.title_multiloc['en']
+      expect(mail.subject).to eq 'Liege: The voting phase started for Example project title'
     end
 
     it 'renders the receiver email' do
@@ -75,23 +74,123 @@ RSpec.describe EmailCampaigns::VotingPhaseStartedMailer do
       expect(mail.from).to all(end_with('@citizenlab.co'))
     end
 
-    it 'displays the correct body - including project title and number of options' do
-      expect(body).to match 'asking you to vote between'
-      expect(body).to match project.title_multiloc['en']
-      expect(body).to match '2 options' # number of ideas passed to the email
+    it 'includes the header' do
+      expect(body).to have_tag('div') do
+        with_tag 'h1' do
+          with_text(/Example project title/)
+        end
+        with_tag 'p' do
+          with_text(/The project "Example project title" is asking you to vote between 2 options:/)
+        end
+      end
     end
 
-    it 'lists the ideas for the phase in the body' do
-      expect(body).to match('An idea title')
-      expect(body).to match('Another idea title')
-      expect(body).to match('ideas/an-idea')
-      expect(body).to match('uploads/small_image.jpeg')
+    it 'lists the voted ideas' do
+      expect(body).to have_tag('div') do
+        with_tag 'tr' do
+          with_tag('img', with: { src: 'http://localhost:4000/uploads/small_image.jpeg' })
+          with_tag('a', with: { href: 'http://localhost:3000/en/ideas/an-idea' }) do
+            with_text(/An idea title/)
+          end
+        end
+        with_tag 'tr' do
+          with_tag('img', with: { src: 'http://localhost:4000/uploads/small_image.jpeg' })
+          with_tag('a', with: { href: 'http://localhost:3000/en/ideas/another-idea' }) do
+            with_text(/Another idea title/)
+          end
+        end
+      end
     end
 
-    it "displays 'Go to the platform to vote' button with correct link" do
-      project_url = Frontend::UrlService.new.model_to_url(project, locale: Locale.new(recipient.locale))
-      expect(body).to match project_url
-      expect(body).to match 'Go to the platform to vote'
+    it 'includes the pre-CTA message' do
+      expect(body).to have_tag('p') do
+        with_text(/Click the button below to participate/)
+      end
+    end
+
+    it 'includes the CTA' do
+      expect(body).to have_tag('a', with: { href: 'https://govocal.com/projects/example' }) do
+        with_text(/Go to the platform to vote/)
+      end
+    end
+
+    context 'with custom text' do
+      let!(:global_campaign) do
+        create(
+          :voting_phase_started_campaign,
+          subject_multiloc: { 'en' => 'Custom Global Subject - {{ organizationName }}' },
+          title_multiloc: { 'en' => 'NEW TITLE FOR {{ projectName }}' },
+          button_text_multiloc: { 'en' => 'CLICK THE GLOBAL BUTTON' }
+        )
+      end
+      let!(:context_campaign) do
+        create(
+          :voting_phase_started_campaign,
+          context: create(:phase),
+          subject_multiloc: { 'en' => 'Custom Context Subject - {{ numIdeas }}' },
+          intro_multiloc: { 'en' => 'NEW BODY TEXT' },
+          button_text_multiloc: { 'en' => 'CLICK THE CONTEXT BUTTON' },
+          reply_to: 'noreply@govocal.com'
+        )
+      end
+
+      context 'on a global campaign' do
+        let(:campaign) { global_campaign }
+
+        it 'can customise the subject' do
+          expect(mail.subject).to eq 'Custom Global Subject - Liege'
+        end
+
+        it 'renders the reply to email' do
+          expect(mail.reply_to).to eq [ENV.fetch('DEFAULT_FROM_EMAIL', 'hello@citizenlab.co')]
+        end
+
+        it 'can customize the header' do
+          expect(body).to have_tag('div') do
+            with_tag 'h1' do
+              with_text(/NEW TITLE FOR Example project title/)
+            end
+            with_tag 'p' do
+              with_text(/The project "Example project title" is asking you to vote between 2 options:/)
+            end
+          end
+        end
+
+        it 'includes the CTA' do
+          expect(body).to have_tag('a', with: { href: 'https://govocal.com/projects/example' }) do
+            with_text(/CLICK THE GLOBAL BUTTON/)
+          end
+        end
+      end
+
+      context 'on a context campaign' do
+        let(:campaign) { context_campaign }
+
+        it 'can customise the subject' do
+          expect(mail.subject).to eq 'Custom Context Subject - 2'
+        end
+
+        it 'can customize the reply to email' do
+          expect(mail.reply_to).to eq ['noreply@govocal.com']
+        end
+
+        it 'can customize the header and fall back to global customzations' do
+          expect(body).to have_tag('div') do
+            with_tag 'h1' do
+              with_text(/NEW TITLE FOR Example project title/)
+            end
+            with_tag 'p' do
+              with_text(/NEW BODY TEXT/)
+            end
+          end
+        end
+
+        it 'includes the CTA' do
+          expect(body).to have_tag('a', with: { href: 'https://govocal.com/projects/example' }) do
+            with_text(/CLICK THE CONTEXT BUTTON/)
+          end
+        end
+      end
     end
   end
 end
