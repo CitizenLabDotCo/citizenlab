2fdbb218690dfcba0d9388f9cfa0854ff4ec42d0
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-08-20 14:12:17 -0300

    _

--- back/app/services/concerns/custom_field_user_overrides.rb
+++ back/app/services/concerns/custom_field_user_overrides.rb
@@ -5,7 +5,10 @@ module CustomFieldUserOverrides
 
   def user_birthyear_to_json_schema_field(field, locale, app_configuration)
     normal_field = number_to_json_schema_field(field, locale)
-    normal_field[:enum] = (1900..(Time.now.year - 12)).to_a.reverse
+
+    min_age = app_configuration.settings('core', 'min_user_age') || 12
+
+    normal_field[:enum] = (1900..(Time.now.year - min_age)).to_a.reverse
     normal_field
   end
 end
