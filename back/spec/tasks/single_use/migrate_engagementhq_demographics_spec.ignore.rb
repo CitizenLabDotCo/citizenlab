66bd9227300f1c07629b1201a26d36e3b36a648b
Sébastien Hoorens <sebastien@citizenlab.co>
2025-08-25 12:21:54 +0200

    Fix offense

--- back/spec/tasks/single_use/migrate_engagementhq_demographics_spec.ignore.rb
+++ back/spec/tasks/single_use/migrate_engagementhq_demographics_spec.ignore.rb
@@ -10,7 +10,7 @@ describe 'rake single_use:migrate_engagementhq_demographics' do
     connections.options.create!([
       { key: 'residential_property_owner_condo_townhouse_house_etc_in_new_west_jfd', title_multiloc: { 'en' => 'Residential property owner (condo, townhouse, house, etc.) in New West' } },
       { key: 'student_in_new_west_z75', title_multiloc: { 'en' => 'Student in New West' } },
-      { key: 'under_housed_or_unhoused_having_inadequate_or_unstable_housing_atrisk_of_experiencing_homelessness_experiencing_hidden_homelessness_i_e_couchsurfing_or_unsheltered_in_new_west_jh2', title_multiloc: { 'en' => 'Under-housed or unhoused (having inadequate or unstable housing, at risk of experiencing homelessness, experiencing hidden homelessness – i.e. couchsurfing, or unsheltered) in New West' } },
+      { key: 'under_housed_or_unhoused_having_inadequate_or_unstable_housing_atrisk_of_experiencing_homelessness_experiencing_hidden_homelessness_i_e_couchsurfing_or_unsheltered_in_new_west_jh2', title_multiloc: { 'en' => 'Under-housed or unhoused (having inadequate or unstable housing, at risk of experiencing homelessness, experiencing hidden homelessness – i.e. couchsurfing, or unsheltered) in New West' } }
     ])
     create(:custom_field_multiselect, :for_registration, key: 'optional_more_info_about_you_select_any_all_that_apply_snh')
 
