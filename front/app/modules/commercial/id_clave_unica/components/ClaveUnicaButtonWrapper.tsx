1c4362709fd33cc8e457d29919a83045d0e034ca
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-08-28 15:20:05 -0300

    Put clave unica button wrapper in module

---
+++ front/app/modules/commercial/id_clave_unica/components/ClaveUnicaButtonWrapper.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { AUTH_PATH } from 'containers/App/constants';
+
+import ClaveUnicaButton from 'components/UI/ClaveUnicaButton/ClaveUnicaButton';
+
+import { getJwt } from 'utils/auth/jwt';
+import { removeUrlLocale } from 'utils/removeUrlLocale';
+
+interface Props {
+  message: string | JSX.Element;
+  disabled?: boolean;
+}
+
+const ClaveUnicaButtonWrapper = ({ message, disabled = false }: Props) => {
+  const handleOnClick = () => {
+    const jwt = getJwt();
+    window.location.href = `${AUTH_PATH}/clave_unica?token=${jwt}&verification_pathname=${removeUrlLocale(
+      window.location.pathname
+    )}`;
+  };
+
+  return (
+    <ClaveUnicaButton
+      disabled={disabled}
+      message={message}
+      onClick={handleOnClick}
+    />
+  );
+};
+
+export default ClaveUnicaButtonWrapper;
