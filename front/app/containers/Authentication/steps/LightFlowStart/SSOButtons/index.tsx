9a2baa5314b6a160a1d96596d7689244fee977a9
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-08-28 11:44:46 -0300

    Use shared sso button component in light flow too

--- front/app/containers/Authentication/steps/LightFlowStart/SSOButtons/index.tsx
+++ front/app/containers/Authentication/steps/LightFlowStart/SSOButtons/index.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 import { Box } from '@citizenlab/cl2-component-library';
 
-import useFeatureFlag from 'hooks/useFeatureFlag';
-
 import { SSOProviderWithoutVienna } from 'containers/Authentication/typings';
+import useAuthConfig from 'containers/Authentication/useAuthConfig';
 
 import Error from 'components/UI/Error';
 import Or from 'components/UI/Or';
 
 import { useIntl } from 'utils/cl-intl';
+
+import SSOButtonsExceptFC from '../../_components/SSOButtonsExceptFC';
 
 import messages from './messages';
 import SSOButton from './SSOButton';
@@ -18,50 +19,12 @@ interface Props {
   onClickSSO: (ssoProvider: SSOProviderWithoutVienna) => void;
 }
 
-const SSOButtons = (props: Props) => {
+const SSOButtons = ({ onClickSSO }: Props) => {
   const { formatMessage } = useIntl();
+  const { passwordLoginEnabled, ssoProviders, anySSOProviderEnabled } =
+    useAuthConfig();
 
-  // TODO: JS - This code is repeated in other places
-  const passwordLoginEnabled = useFeatureFlag({ name: 'password_login' });
-  const googleLoginEnabled = useFeatureFlag({ name: 'google_login' });
-  const facebookLoginEnabled = useFeatureFlag({ name: 'facebook_login' });
-  const azureAdLoginEnabled = useFeatureFlag({ name: 'azure_ad_login' });
-  const azureB2cProviderName = useFeatureFlag({ name: 'azure_ad_b2c_login' });
-  const franceconnectLoginEnabled = useFeatureFlag({
-    name: 'franceconnect_login',
-  });
-  const claveUnicaLoginEnabled = useFeatureFlag({
-    name: 'clave_unica_login',
-  });
-  const hoplrLoginEnabled = useFeatureFlag({
-    name: 'hoplr_login',
-  });
-  const idAustriaLoginEnabled = useFeatureFlag({
-    name: 'id_austria_login',
-  });
-  const criiptoLoginEnabled = useFeatureFlag({
-    name: 'criipto_login',
-  });
-  const keycloakLoginEnabled = useFeatureFlag({
-    name: 'keycloak_login',
-  });
-  const twodayLoginEnabled = useFeatureFlag({
-    name: 'twoday_login',
-  });
-
-  if (
-    !googleLoginEnabled &&
-    !facebookLoginEnabled &&
-    !azureAdLoginEnabled &&
-    !azureB2cProviderName &&
-    !franceconnectLoginEnabled &&
-    !claveUnicaLoginEnabled &&
-    !hoplrLoginEnabled &&
-    !idAustriaLoginEnabled &&
-    !criiptoLoginEnabled &&
-    !keycloakLoginEnabled &&
-    !twodayLoginEnabled
-  ) {
+  if (anySSOProviderEnabled) {
     if (passwordLoginEnabled) {
       return null;
     }
@@ -80,19 +43,14 @@ const SSOButtons = (props: Props) => {
         </Box>
       )}
       <Box>
-        {googleLoginEnabled && <SSOButton ssoProvider="google" {...props} />}
-        {facebookLoginEnabled && (
-          <SSOButton ssoProvider="facebook" {...props} />
+        {ssoProviders.franceconnect && (
+          <SSOButton ssoProvider="facebook" onClickSSO={onClickSSO} />
         )}
-        {franceconnectLoginEnabled && (
-          <SSOButton ssoProvider="franceconnect" {...props} />
-        )}
-        {azureAdLoginEnabled && (
-          <SSOButton ssoProvider="azureactivedirectory" {...props} />
-        )}
-        {azureB2cProviderName && (
-          <SSOButton ssoProvider="azureactivedirectory_b2c" {...props} />
-        )}
+        <SSOButtonsExceptFC
+          showConsent={false}
+          flow="signup"
+          onSelectAuthProvider={onClickSSO}
+        />
       </Box>
     </>
   );
