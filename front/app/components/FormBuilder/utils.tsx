31d93feb820244ecdb967222be71d1726d97db85
EdwinKato <katedwin88@gmail.com>
2025-09-02 11:32:32 +0300

    Add support for HTML multilingual text field in additional settings

--- front/app/components/FormBuilder/utils.tsx
+++ front/app/components/FormBuilder/utils.tsx
@@ -211,6 +211,7 @@ export function getAdditionalSettings(
     case 'text':
     case 'multiline_text':
     case 'text_multiloc':
+    case 'html_multiloc':
       return (
         <CharacterLimitSettings
           minCharactersName={`customFields.${field.index}.min_characters`}
