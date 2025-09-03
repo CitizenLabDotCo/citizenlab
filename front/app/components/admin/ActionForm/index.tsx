d8a3f1c7c18faefcdf67647b9b3fd6a8c1aeb096
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-09-03 14:42:17 -0300

    Remove survey-specific stuff from old ActionForm

--- front/app/components/admin/ActionForm/index.tsx
+++ front/app/components/admin/ActionForm/index.tsx
@@ -5,15 +5,12 @@ import {
   Button,
   Title,
   fontSizes,
-  Text,
-  Toggle,
 } from '@citizenlab/cl2-component-library';
 import { Multiloc } from 'typings';
 
 import { IPermissionData } from 'api/permissions/types';
 import usePermissionsCustomFields from 'api/permissions_custom_fields/usePermissionsCustomFields';
 import { PermittedBy } from 'api/phase_permissions/types';
-import usePhase from 'api/phases/usePhase';
 
 import { FormattedMessage } from 'utils/cl-intl';
 
@@ -49,14 +46,11 @@ const ActionForm = ({ phaseId, permissionData, onChange, onReset }: Props) => {
       action,
       verification_enabled,
       verification_expiry,
-      everyone_tracking_enabled,
     },
     relationships,
   } = permissionData;
 
   const groupIds = relationships.groups.data.map((p) => p.id);
-
-  const { data: phase } = usePhase(phaseId);
 
   const { data: permissionsCustomFields } = usePermissionsCustomFields({
     phaseId,
@@ -65,26 +59,6 @@ const ActionForm = ({ phaseId, permissionData, onChange, onReset }: Props) => {
 
   const handlePermittedByUpdate = (permitted_by: PermittedBy) => {
     onChange({ permitted_by });
-  };
-
-  const participation_method = phase?.data.attributes.participation_method;
-
-  const isSurveyAction =
-    (participation_method &&
-      ['native_survey', 'community_monitor_survey'].includes(
-        participation_method
-      )) ||
-    (participation_method === 'survey' && action === 'taking_survey');
-
-  const userFieldsInForm = !!phase?.data.attributes.user_fields_in_form;
-
-  // Currently only community monitor supports everyone tracking
-  const canUseEveryoneTracking =
-    participation_method === 'community_monitor_survey' &&
-    action === 'posting_idea' &&
-    permitted_by === 'everyone';
-  const handleEveryoneTrackingUpdate = (everyone_tracking_enabled: boolean) => {
-    onChange({ everyone_tracking_enabled });
   };
 
   if (!permissionsCustomFields) return null;
@@ -109,7 +83,7 @@ const ActionForm = ({ phaseId, permissionData, onChange, onReset }: Props) => {
           </Box>
           <Box display="flex" gap="16px">
             <CardButtons
-              showAnyone={isSurveyAction}
+              showAnyone={false}
               permittedBy={permitted_by}
               onUpdate={handlePermittedByUpdate}
             />
@@ -146,8 +120,8 @@ const ActionForm = ({ phaseId, permissionData, onChange, onReset }: Props) => {
             <Fields
               phaseId={phaseId}
               action={action}
-              showAddQuestion={permitted_by !== 'everyone' || userFieldsInForm}
-              userFieldsInForm={userFieldsInForm}
+              showAddQuestion={permitted_by !== 'everyone'}
+              userFieldsInForm={false}
             />
           </Box>
           <Box mt="20px">
@@ -159,7 +133,7 @@ const ActionForm = ({ phaseId, permissionData, onChange, onReset }: Props) => {
               onChangeVerificationExpiry={(verification_expiry) => {
                 onChange({ verification_expiry });
               }}
-              userFieldsInForm={userFieldsInForm}
+              userFieldsInForm={false}
             />
           </Box>
           {showResetButton(
@@ -181,44 +155,6 @@ const ActionForm = ({ phaseId, permissionData, onChange, onReset }: Props) => {
                   />
                 </span>
               </Button>
-            </Box>
-          )}
-          {canUseEveryoneTracking && (
-            <Box mt="28px" width="90%">
-              <Title variant="h4" color="primary" mb={'8px'}>
-                <FormattedMessage {...messages.everyoneTracking} />
-              </Title>
-              <Toggle
-                checked={everyone_tracking_enabled || false}
-                onChange={() => {
-                  handleEveryoneTrackingUpdate(!everyone_tracking_enabled);
-                }}
-                label={
-                  <Box ml="8px" id="e2e-everyone-tracking-toggle">
-                    <Box display="flex">
-                      <Text
-                        color="primary"
-                        mb="0px"
-                        fontSize="m"
-                        fontWeight="semi-bold"
-                      >
-                        <FormattedMessage
-                          {...messages.everyoneTrackingToggle}
-                        />
-                      </Text>
-                    </Box>
-
-                    <Text color="coolGrey600" mt="0px" fontSize="m">
-                      <FormattedMessage
-                        {...messages.everyoneTrackingDescription}
-                      />{' '}
-                      <span style={{ fontStyle: 'italic' }}>
-                        <FormattedMessage {...messages.everyoneTrackingNote} />
-                      </span>
-                    </Text>
-                  </Box>
-                }
-              />
             </Box>
           )}
         </>
