75e6fc696d4e1964dd1061254a01ef92c276f94e
Amanda Anderson <anderson_amanda@mail.com>
2025-09-03 11:36:55 +0200

    Add missing types

--- front/app/api/file_attachments/types.ts
+++ front/app/api/file_attachments/types.ts
@@ -1,12 +1,39 @@
 import { IRelationship } from 'typings';
 
+import { Keys } from 'utils/cl-react-query/types';
+
+import fileAttachmentsKeys from './keys';
+
 type AttachableType = 'Phase' | 'Project' | 'Event';
+
+export type FileAttachmentKeys = Keys<typeof fileAttachmentsKeys>;
 
 export type QueryParameters = {
   attachable_type?: AttachableType;
   attachable_id?: string;
   file_id?: number;
 };
+
+export type IFileAttachments = {
+  data: IFileAttachment[];
+};
+
+export type IFileAttachmentData = {
+  type: 'file_attachment';
+  id: string;
+  attributes: {
+    position: number;
+  };
+  relationships: {
+    attachable: { data: IRelationship };
+    file: { data: IRelationship };
+  };
+};
+
+export interface IUpdateFileAttachmentProperties {
+  id: string;
+  position: number;
+}
 
 export interface IAddFileAttachmentProperties {
   file_id: string;
@@ -15,6 +42,7 @@ export interface IAddFileAttachmentProperties {
 }
 
 export interface IFileAttachment {
+  type: 'file_attachment';
   data: {
     id: string;
     type: 'file_attachment';
