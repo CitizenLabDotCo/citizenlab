c7662c2910e520c167613fd72ba63799443feaad
Amanda Anderson <anderson_amanda@mail.com>
2025-09-03 11:36:14 +0200

    Add hooks for remaining endpoints

---
+++ front/app/api/file_attachments/useDeleteFileAttachment.ts
@@ -0,0 +1,26 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import fetcher from 'utils/cl-react-query/fetcher';
+
+import filesKeys from './keys';
+
+const deleteFileAttachment = (id: string) =>
+  fetcher({
+    path: `/file_attachments/${id}`,
+    action: 'delete',
+  });
+
+const useDeleteFileAttachment = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteFileAttachment,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: filesKeys.lists(),
+      });
+    },
+  });
+};
+
+export default useDeleteFileAttachment;
---
+++ front/app/api/file_attachments/useUpdateFileAttachment.ts
@@ -0,0 +1,34 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { omit } from 'lodash-es';
+import { CLErrors } from 'typings';
+
+import fetcher from 'utils/cl-react-query/fetcher';
+
+import fileAttachmentKeys from './keys';
+import { IFileAttachment, IUpdateFileAttachmentProperties } from './types';
+
+const updateFileAttachment = async (
+  requestBody: IUpdateFileAttachmentProperties
+) => {
+  return fetcher<IFileAttachment>({
+    path: `/file_attachments/${requestBody.id}`,
+    action: 'patch',
+    body: { ...omit(requestBody, 'id') },
+  });
+};
+
+const useUpdateFileAttachment = () => {
+  const queryClient = useQueryClient();
+  return useMutation<
+    IFileAttachment,
+    CLErrors,
+    IUpdateFileAttachmentProperties
+  >({
+    mutationFn: updateFileAttachment,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: fileAttachmentKeys.lists() });
+    },
+  });
+};
+
+export default useUpdateFileAttachment;
