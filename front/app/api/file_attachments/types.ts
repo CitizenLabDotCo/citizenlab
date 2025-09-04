fabf80c20d87386fbc82b8e590c1149ff33ac6b8
Amanda Anderson <anderson_amanda@mail.com>
2025-09-04 17:49:18 +0200

    Fix types

--- front/app/api/file_attachments/types.ts
+++ front/app/api/file_attachments/types.ts
@@ -4,7 +4,7 @@ import { Keys } from 'utils/cl-react-query/types';
 
 import fileAttachmentsKeys from './keys';
 
-type AttachableType = 'Phase' | 'Project' | 'Event';
+export type AttachableType = 'Phase' | 'Project' | 'Event';
 
 export type FileAttachmentKeys = Keys<typeof fileAttachmentsKeys>;
 
@@ -42,10 +42,9 @@ export interface IAddFileAttachmentProperties {
 }
 
 export interface IFileAttachment {
-  type: 'file_attachment';
   data: {
-    id: string;
     type: 'file_attachment';
+    id: string;
     attributes: {
       position: number;
     };
