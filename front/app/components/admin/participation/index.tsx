1f7fdceef8426899e88233713299260072f5949e
EdwinKato <katedwin88@gmail.com>
2025-09-08 09:46:25 +0300

    Refactor ProjectParticipation component to include navigation tabs for users and demographics

--- front/app/components/admin/participation/index.tsx
+++ front/app/components/admin/participation/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 
 import usePhases from 'api/phases/usePhases';
 
-import ParticipationDatesRange from './ParticipationDateRange';
+import NavigationTabs, {
+  Tab,
+  TabsPageLayout,
+} from 'components/admin/NavigationTabs';
+
+import { useIntl } from 'utils/cl-intl';
+
+import Demographics from './Demographics';
+import messages from './messages';
+import Users from './Users';
 
 const ProjectParticipation = () => {
   const { projectId } = useParams() as { projectId: string };
+  const { pathname } = useLocation();
+  const { formatMessage } = useIntl();
   const { data: phases } = usePhases(projectId);
 
   const startOfFirstPhase = phases?.data[0]?.attributes.start_at;
@@ -15,11 +26,35 @@ const ProjectParticipation = () => {
     phases?.data[phases.data.length - 1]?.attributes.end_at;
 
   if (!phases) return null;
+
+  const isDemographicsTab = pathname.includes('/demographics');
+
   return (
-    <ParticipationDatesRange
-      defaultStartDate={startOfFirstPhase}
-      defaultEndDate={endOfLastPhase ?? undefined}
-    />
+    <>
+      <NavigationTabs>
+        <Tab
+          label={formatMessage(messages.usersTab)}
+          url={`/admin/projects/${projectId}/participation`}
+          active={!isDemographicsTab}
+        />
+        <Tab
+          label={formatMessage(messages.demographicsTab)}
+          url={`/admin/projects/${projectId}/participation/demographics`}
+          active={isDemographicsTab}
+        />
+      </NavigationTabs>
+
+      <TabsPageLayout>
+        {isDemographicsTab ? (
+          <Demographics
+            defaultStartDate={startOfFirstPhase}
+            defaultEndDate={endOfLastPhase ?? undefined}
+          />
+        ) : (
+          <Users />
+        )}
+      </TabsPageLayout>
+    </>
   );
 };
 
