9c01002c54d7a523857f07d6fc6ce0b195e441f5
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-09-10 19:39:26 -0300

    Add anonymity attribute to phase serializer

--- back/app/serializers/web_api/v1/phase_serializer.rb
+++ back/app/serializers/web_api/v1/phase_serializer.rb
@@ -12,7 +12,7 @@ class WebApi::V1::PhaseSerializer < WebApi::V1::BaseSerializer
     :allow_anonymous_participation, :presentation_mode, :ideas_order, :input_term, :vote_term,
     :prescreening_enabled, :manual_voters_amount, :manual_votes_count,
     :similarity_enabled, :similarity_threshold_title, :similarity_threshold_body,
-    :survey_popup_frequency
+    :survey_popup_frequency, :anonymity
 
   %i[
     voting_method voting_max_total voting_min_total
