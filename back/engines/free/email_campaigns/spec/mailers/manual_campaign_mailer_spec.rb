3290d9023afa812735976cd5c7b76a61eea8ff66
Sébastien Hoorens <sebastien@citizenlab.co>
2025-09-15 19:30:45 +0200

    Fix preheader for manual participants + mailer spec

--- back/engines/free/email_campaigns/spec/mailers/manual_campaign_mailer_spec.rb
+++ back/engines/free/email_campaigns/spec/mailers/manual_campaign_mailer_spec.rb
@@ -4,69 +4,110 @@ require 'rails_helper'
 
 RSpec.describe EmailCampaigns::ManualCampaignMailer do
   describe 'campaign_mail' do
-    let_it_be(:recipient) { create(:user, locale: 'en') }
-    let_it_be(:campaign) do
-      EmailCampaigns::Campaigns::Manual.create!(
+    let_it_be(:recipient) { create(:user, locale: 'en', first_name: 'Manuel', last_name: 'Campene') }
+
+    let(:command) do
+      campaign.generate_commands(recipient:).first.merge({ recipient: recipient })
+    end
+    let(:mailer) { described_class.with(command:, campaign:) }
+    let(:mail) { mailer.campaign_mail.deliver_now }
+    let(:body) { mail_body(mail) }
+
+    before_all { EmailCampaigns::UnsubscriptionToken.create!(user_id: recipient.id) }
+
+    context 'for a global manual campaign' do
+      let(:campaign) do
+        EmailCampaigns::Campaigns::Manual.create!(
         subject_multiloc: { 'en' => 'Title' },
         body_multiloc: {
           'en' => '
-            <ul>
-              <li>
-                <h1>{{ first_name }}</h1>
-                <p> Here\'s your test email</p>
-              </li>
-            </ul>
+            <h2>{{ first_name }} {{ last_name }}</h2>
+            <p> Here\'s your test email</p>
           '
         },
         sender: 'organization'
       )
     end
-    let_it_be(:command) do
-      {
-        author: create(:admin),
-        event_payload: {},
-        subject_multiloc: { 'en' => 'Title' },
-        body_multiloc: {
-          'en' => '
-            <ul>
-              <li>
-                <h1>{{ first_name }} {{ last_name }}</h1>
-                <p> Here\'s your test email</p>
-              </li>
-            </ul>
-          '
-        },
-        sender: 'organization',
-        reply_to: 'replyto',
-        recipient: recipient
-      }
+
+      include_examples 'campaign delivery tracking'
+
+      it 'renders the subject' do
+        expect(mail.subject).to end_with('Title')
+      end
+
+      it 'renders the receiver email' do
+        expect(mail.to).to eq([recipient.email])
+      end
+
+      it 'renders the sender email' do
+        expect(mail.from).to all(end_with('@citizenlab.co'))
+      end
+
+      it 'includes the preheader' do
+        expect(body).to have_tag('div') do
+          with_text(/You have mail from Liege/)
+        end
+      end
+
+      it 'includes the header' do
+        expect(body).to have_tag('h2') do
+          with_text(/Manuel Campene/)
+        end
+      end
+
+      it 'includes the paragraph' do
+        expect(body).to have_tag('p') do
+          with_text(/Here's your test email/)
+        end
+      end
     end
 
-    let_it_be(:mailer) { described_class.with(command:, campaign:) }
-    let_it_be(:mail) { mailer.campaign_mail.deliver_now }
+    context 'for a manual project participants campaign' do
+      let(:campaign) do
+        EmailCampaigns::Campaigns::ManualProjectParticipants.create!(
+          subject_multiloc: { 'en' => 'Title' },
+          body_multiloc: {
+            'en' => '
+              <h2>{{ first_name }} {{ last_name }}</h2>
+              <p> Here\'s your test email</p>
+            '
+          },
+          sender: 'organization',
+          context: create(:project)
+        )
+      end
 
-    before_all { EmailCampaigns::UnsubscriptionToken.create!(user_id: recipient.id) }
+      include_examples 'campaign delivery tracking'
 
-    include_examples 'campaign delivery tracking'
+      it 'renders the subject' do
+        expect(mail.subject).to end_with('Title')
+      end
 
-    it 'renders the subject' do
-      expect(mail.subject).to end_with('Title')
-    end
+      it 'renders the receiver email' do
+        expect(mail.to).to eq([recipient.email])
+      end
 
-    it 'renders the sender email' do
-      expect(mail.from).to all(end_with('@citizenlab.co'))
-    end
+      it 'renders the sender email' do
+        expect(mail.from).to all(end_with('@citizenlab.co'))
+      end
 
-    it 'assigns organisation name' do
-      expect(mail.body.encoded).to match(AppConfiguration.instance.settings('core', 'organization_name', 'en'))
-    end
+      it 'includes the preheader' do
+        expect(body).to have_tag('div') do
+          with_text(/You have mail from Liege/)
+        end
+      end
 
-    it 'includes the first name body content as defined in liquid email content' do
-      expect(mail.body.encoded).to include(recipient.first_name)
-    end
+      it 'includes the header' do
+        expect(body).to have_tag('h2') do
+          with_text(/Manuel Campene/)
+        end
+      end
 
-    it 'includes the last name body content as defined in liquid email content' do
-      expect(mail.body.encoded).to include(recipient.last_name)
+      it 'includes the paragraph' do
+        expect(body).to have_tag('p') do
+          with_text(/Here's your test email/)
+        end
+      end
     end
   end
 end
