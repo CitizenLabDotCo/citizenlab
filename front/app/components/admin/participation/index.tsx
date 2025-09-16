00c1d3d13f5e886e6fde88211cc5ef8f5a8237a1
EdwinKato <katedwin88@gmail.com>
2025-09-16 08:54:44 +0300

    refactor: simplify ProjectParticipation component structure

--- front/app/components/admin/participation/index.tsx
+++ front/app/components/admin/participation/index.tsx
@@ -5,10 +5,7 @@ import { useLocation, useParams } from 'react-router-dom';
 
 import usePhases from 'api/phases/usePhases';
 
-import NavigationTabs, {
-  Tab,
-  TabsPageLayout,
-} from 'components/admin/NavigationTabs';
+import NavigationTabs, { Tab } from 'components/admin/NavigationTabs';
 
 import { useIntl } from 'utils/cl-intl';
 import { isTopBarNavActive } from 'utils/helperUtils';
@@ -34,30 +31,31 @@ const ProjectParticipation = () => {
   const isDemographicsTab = pathname.includes('/demographics');
 
   return (
-    <>
-      <NavigationTabs position="relative">
-        <Tab
-          label={formatMessage(messages.usersTab)}
-          url={`/admin/projects/${projectId}/participation`}
-          active={isTopBarNavActive(
-            basePath,
-            pathname,
-            `/admin/projects/${projectId}/participation`
-          )}
-        />
-        <Tab
-          label={formatMessage(messages.demographicsTab)}
-          url={`/admin/projects/${projectId}/participation/demographics`}
-          active={isTopBarNavActive(
-            basePath,
-            pathname,
-            `/admin/projects/${projectId}/participation/demographics`
-          )}
-        />
-      </NavigationTabs>
-
-      <TabsPageLayout paddingTop={42}>
-        <Box background={colors.white} p={`${defaultAdminCardPadding}px`}>
+    <Box p="8px 24px 24px 24px">
+      <Box background={colors.white}>
+        <Box position="sticky" top="0" background={colors.white} zIndex="1">
+          <NavigationTabs position="relative">
+            <Tab
+              label={formatMessage(messages.usersTab)}
+              url={`/admin/projects/${projectId}/participation`}
+              active={isTopBarNavActive(
+                basePath,
+                pathname,
+                `/admin/projects/${projectId}/participation`
+              )}
+            />
+            <Tab
+              label={formatMessage(messages.demographicsTab)}
+              url={`/admin/projects/${projectId}/participation/demographics`}
+              active={isTopBarNavActive(
+                basePath,
+                pathname,
+                `/admin/projects/${projectId}/participation/demographics`
+              )}
+            />
+          </NavigationTabs>
+        </Box>
+        <Box p={`${defaultAdminCardPadding}px`}>
           {isDemographicsTab ? (
             <Demographics
               defaultStartDate={startOfFirstPhase}
@@ -67,8 +65,8 @@ const ProjectParticipation = () => {
             <Users />
           )}
         </Box>
-      </TabsPageLayout>
-    </>
+      </Box>
+    </Box>
   );
 };
 
