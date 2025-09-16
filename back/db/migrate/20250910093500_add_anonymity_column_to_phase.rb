9fe3242a9bea081ce407c58d710d16cdd8540d54
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-09-16 10:16:55 -0300

    Update migration

--- back/db/migrate/20250910093500_add_anonymity_column_to_phase.rb
+++ back/db/migrate/20250910093500_add_anonymity_column_to_phase.rb
@@ -2,28 +2,47 @@
 
 class AddAnonymityColumnToPhase < ActiveRecord::Migration[7.1]
   def change
-    add_column :phases, :anonymity, :string, default: 'collect_all_data_available', null: false
-
     reversible do |dir|
+      add_column :permissions, :user_fields_in_form, :boolean, default: false, null: false
+      add_column :permissions, :user_data_collection, :string, default: 'all_data', null: false
+
       dir.up do
-        Phase.reset_column_information
+        Permission.reset_column_information
+        
+        # STEP 1: move user_fields_in_form from phases to permissions
+        Permission
+          .joins(:phase)
+          .where(phases: { user_fields_in_form: true })
+          .update_all(user_fields_in_form: true)
+
+        # STEP 2: set new user_data_collection column
 
         # Native survey phases that had allow_anonymous_participation = true,
         # and user_fields_in_form = false, were effectively working as fully anonymous surveys.
-        Phase
-          .where(participation_method: 'native_survey')
-          .where(allow_anonymous_participation: true)
-          .where(user_fields_in_form: false)
-          .update_all(anonymity: 'full_anonymity')
+        # So we set user_data_collection to 'anonymous' for their permissions.
+        Permission
+          .joins(:phase)
+          .where(phases: { 
+            participation_method: 'native_survey', 
+            allow_anonymous_participation: true,
+            user_fields_in_form: false
+          })
+          .update_all(user_data_collection: 'anonymous')
 
         # While native survey phases that had allow_anonymous_participation = true,
         # and user_fields_in_form = true, were still collecting demographic data.
-        Phase
-          .where(participation_method: 'native_survey')
-          .where(allow_anonymous_participation: true)
-          .where(user_fields_in_form: true)
-          .update_all(anonymity: 'demographics_only')
+        # So we set user_data_collection to 'demographics_only' for their permissions.
+        Permission
+          .joins(:phase)
+          .where(phases: { 
+            participation_method: 'native_survey', 
+            allow_anonymous_participation: true,
+            user_fields_in_form: true
+          })
+          .update_all(user_data_collection: 'anonymous')
       end
+
+      remove_column :phases, :user_fields_in_form
     end
   end
 end
