c9711342562a1e5cc991756ebb3b3f08d001e2b3
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-09-17 11:15:05 -0300

    Scaffold permissions_users e2e tests

---
+++ front/cypress/e2e/native_survey_permissions/permission_users.cy.ts
@@ -0,0 +1,27 @@
+describe('Native survey permission: users', () => {
+  describe('Collect all data', () => {
+    describe('Ask demographic questions in registration flow', () => {
+      // TODO
+    });
+
+    describe('Ask demographic questions in survey', () => {
+      // TODO
+    });
+  });
+
+  describe('Collect demographics only', () => {
+    describe('Ask demographic questions in registration flow', () => {
+      // TODO
+    });
+
+    describe('Ask demographic questions in survey', () => {
+      // TODO
+    });
+  });
+
+  describe('Full anonymity', () => {
+    describe('Ask demographic questions in registration flow', () => {
+      // TODO
+    });
+  });
+});
