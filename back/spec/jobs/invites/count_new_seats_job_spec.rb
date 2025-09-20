13521c44e9f7ed6d03c96d2d1965697e568ac69d
jinjagit <jinjamail@gmail.com>
2025-09-20 12:54:03 +0100

    [TAN-5473] Use shared examples in count_new_seats_job_spec

--- back/spec/jobs/invites/count_new_seats_job_spec.rb
+++ back/spec/jobs/invites/count_new_seats_job_spec.rb
@@ -8,30 +8,29 @@ RSpec.describe Invites::CountNewSeatsJob do
     let!(:_admin) { create(:admin) }
     let!(:_project_moderator2) { create(:project_moderator, email: emails[0], projects: [project]) }
     let!(:_admin2) { create(:admin, email: emails[1]) }
-
-    let(:roles) do
-      # only the highest role is actually used
-      [
-        { 'type' => 'admin' },
-        { 'type' => 'project_moderator', 'project_id' => @project.id }
-      ]
-    end
     let!(:user) { create(:admin) }
 
-    describe 'with manually specified emails' do
-      let(:invites_import) { create(:invites_import, job_type: 'count_new_seats', importer: user) }
+    shared_examples 'count new seats job' do |params_key:, invalid_email_1:, invalid_email_2:, duplicate_rows:, invalid_rows:, extra_args: {}|
+      let(:invites_import) { create(:invites_import, job_type: extra_args[:job_type], importer: user) }
       let(:create_params) do
-        {
+        params = {
           roles: [
             { 'type' => 'admin' },
             { 'type' => 'project_moderator', 'project_id' => project.id }
-          ],
-          emails: emails
+          ]
         }
+        if params_key == :emails
+          params[:emails] = emails
+        else
+          hash_array = emails.map { |email| { email: email, admin: true } }
+          xlsx_stringio = XlsxService.new.hash_array_to_xlsx(hash_array)
+          params[:xlsx] = "data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,#{Base64.encode64(xlsx_stringio.read)}"
+        end
+        params
       end
 
       it 'sets the correct result in the invites_import result attribute' do
-        described_class.perform_now(user, create_params, invites_import.id)
+        described_class.perform_now(user, create_params, invites_import.id, **extra_args.except(:job_type))
         invites_import.reload
 
         expect(invites_import.result).to include(
@@ -43,84 +42,48 @@ RSpec.describe Invites::CountNewSeatsJob do
       it 'does not create any users or invites' do
         original_users = User.all.to_a
 
-        described_class.perform_now(user, create_params, invites_import.id)
+        described_class.perform_now(user, create_params, invites_import.id, **extra_args.except(:job_type))
 
         expect(User.all.to_a).to eq(original_users)
         expect(Invite.count).to eq(0)
       end
 
       it 'sets the expected errors in the invites_import result attribute' do
-        emails[0] = 'invalid_email_1'
-        emails[3] = 'invalid_email_2'
+        emails[0] = invalid_email_1
+        emails[3] = invalid_email_2
         emails[4] = emails[1]
 
-        described_class.perform_now(user, create_params, invites_import.id)
+        described_class.perform_now(user, create_params, invites_import.id, **extra_args.except(:job_type))
         invites_import.reload
 
         expect(invites_import.result).to eq(
           'errors' => [
-            { 'error' => 'emails_duplicate', 'ignore' => false, 'rows' => [1, 4], 'value' => emails[1] },
-            { 'error' => 'invalid_email', 'ignore' => false, 'raw_error' => 'Validation failed: Email is invalid', 'row' => 0, 'value' => 'invalid_email_1' },
-            { 'error' => 'invalid_email', 'ignore' => false, 'raw_error' => 'Validation failed: Email is invalid', 'row' => 3, 'value' => 'invalid_email_2' }
+            { 'error' => 'emails_duplicate', 'ignore' => false, 'rows' => duplicate_rows, 'value' => emails[1] },
+            { 'error' => 'invalid_email', 'ignore' => false, 'raw_error' => 'Validation failed: Email is invalid', 'row' => invalid_rows[0], 'value' => invalid_email_1 },
+            { 'error' => 'invalid_email', 'ignore' => false, 'raw_error' => 'Validation failed: Email is invalid', 'row' => invalid_rows[1], 'value' => invalid_email_2 }
           ]
         )
       end
+    end
+
+    describe 'with manually specified emails' do
+      it_behaves_like 'count new seats job',
+        params_key: :emails,
+        invalid_email_1: 'invalid_email_1',
+        invalid_email_2: 'invalid_email_2',
+        duplicate_rows: [1, 4],
+        invalid_rows: [0, 3],
+        extra_args: { job_type: 'count_new_seats' }
     end
 
     describe 'with xlsx import' do
-      let(:invites_import) { create(:invites_import, job_type: 'count_new_seats_xlsx', importer: user) }
-      let(:xlsx) do
-        hash_array = emails.map { |email| { email: email, admin: true } }
-        xlsx_stringio = XlsxService.new.hash_array_to_xlsx(hash_array)
-
-        "data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,#{Base64.encode64(xlsx_stringio.read)}"
-      end
-
-      let(:create_params) do
-        {
-          roles: [
-            { 'type' => 'admin' },
-            { 'type' => 'project_moderator', 'project_id' => project.id }
-          ],
-          xlsx: xlsx
-        }
-      end
-
-      it 'sets the correct result in the invites_import result attribute' do
-        described_class.perform_now(user, create_params, invites_import.id, xlsx_import: true)
-        invites_import.reload
-
-        expect(invites_import.result).to include(
-          'newly_added_admins_number' => 4,
-          'newly_added_moderators_number' => -1
-        )
-      end
-
-      it 'does not create any users or invites' do
-        original_users = User.all.to_a
-
-        described_class.perform_now(user, create_params, invites_import.id, xlsx_import: true)
-
-        expect(User.all.to_a).to eq(original_users)
-        expect(Invite.count).to eq(0)
-      end
-
-      it 'sets the expected errors in the invites_import result attribute' do
-        emails[0] = 'invalid_email_a'
-        emails[3] = 'invalid_email_b'
-        emails[4] = emails[1]
-
-        described_class.perform_now(user, create_params, invites_import.id, xlsx_import: true)
-        invites_import.reload
-
-        expect(invites_import.result).to eq(
-          'errors' => [
-            { 'error' => 'emails_duplicate', 'ignore' => false, 'rows' => [3, 6], 'value' => emails[1] },
-            { 'error' => 'invalid_email', 'ignore' => false, 'raw_error' => 'Validation failed: Email is invalid', 'row' => 2, 'value' => 'invalid_email_a' },
-            { 'error' => 'invalid_email', 'ignore' => false, 'raw_error' => 'Validation failed: Email is invalid', 'row' => 5, 'value' => 'invalid_email_b' }
-          ]
-        )
-      end
+      it_behaves_like 'count new seats job',
+        params_key: :xlsx,
+        invalid_email_1: 'invalid_email_a',
+        invalid_email_2: 'invalid_email_b',
+        duplicate_rows: [3, 6],
+        invalid_rows: [2, 5],
+        extra_args: { job_type: 'count_new_seats_xlsx', xlsx_import: true }
     end
   end
 end
