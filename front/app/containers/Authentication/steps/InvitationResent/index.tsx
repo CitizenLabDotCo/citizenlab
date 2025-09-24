f0e3ddf1a575d210d95e66c3b99f0c8a0dd977fb
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-09-24 14:30:33 -0300

    _

--- front/app/containers/Authentication/steps/InvitationResent/index.tsx
+++ front/app/containers/Authentication/steps/InvitationResent/index.tsx
@@ -2,23 +2,31 @@ import React from 'react';
 
 import { Box } from '@citizenlab/cl2-component-library';
 
+import { State } from 'containers/Authentication/typings';
+
 import Warning from 'components/UI/Warning';
 
 import { useIntl } from 'utils/cl-intl';
 
-import Invitation, { Props } from '../Invitation';
+import Invitation, { Props as InvitationProps } from '../Invitation';
 
 import messages from './messages';
 
-const InvitationResent = (props: Props) => {
+type Props = InvitationProps & {
+  state: State;
+};
+
+const InvitationResent = ({ state, ...props }: Props) => {
   const { formatMessage } = useIntl();
+
+  if (!state.email) return null;
 
   return (
     <>
       <Box mb="24px">
         <Warning>
           {formatMessage(messages.emailAlreadyTaken, {
-            email: 'your@email.com',
+            email: state.email,
           })}
         </Warning>
       </Box>
