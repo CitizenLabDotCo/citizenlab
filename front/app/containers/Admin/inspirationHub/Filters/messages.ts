9aa1a87f0da6f6bb9a6e2bb9afdc92ea1d1df355
jinjagit <jinjamail@gmail.com>
2025-09-29 10:51:52 +0100

    [TAN-5571] Add FDE messages for topics

--- front/app/containers/Admin/inspirationHub/Filters/messages.ts
+++ front/app/containers/Admin/inspirationHub/Filters/messages.ts
@@ -21,4 +21,128 @@ export default defineMessages({
     id: 'app.containers.Admin.inspirationHub.Filters.Topic',
     defaultMessage: 'Topic',
   },
+  transportGeneral: {
+    id: 'app.containers.Admin.inspirationHub.Topics.transportGeneral',
+    defaultMessage: 'Transport: General',
+  },
+  transportRoads: {
+    id: 'app.containers.Admin.inspirationHub.Topics.transportRoads',
+    defaultMessage: 'Transport: Roads',
+  },
+  transportBicycles: {
+    id: 'app.containers.Admin.inspirationHub.Topics.transportBicycles',
+    defaultMessage: 'Transport: Bicycles',
+  },
+  transportPublicTransit: {
+    id: 'app.containers.Admin.inspirationHub.Topics.transportPublicTransit',
+    defaultMessage: 'Transport: Public Transit',
+  },
+  transportEvChargingStations: {
+    id: 'app.containers.Admin.inspirationHub.Topics.transportEvChargingStations',
+    defaultMessage: 'Transport: EV Charging Stations',
+  },
+  transportParkingFacilities: {
+    id: 'app.containers.Admin.inspirationHub.Topics.transportParkingFacilities',
+    defaultMessage: 'Transport: Parking Facilities',
+  },
+  environmentGeneral: {
+    id: 'app.containers.Admin.inspirationHub.Topics.environmentGeneral',
+    defaultMessage: 'Environment: General',
+  },
+  environmentParks: {
+    id: 'app.containers.Admin.inspirationHub.Topics.environmentParks',
+    defaultMessage: 'Environment: Parks',
+  },
+  environmentTrails: {
+    id: 'app.containers.Admin.inspirationHub.Topics.environmentTrails',
+    defaultMessage: 'Environment: Trails',
+  },
+  environmentRenewableEnergy: {
+    id: 'app.containers.Admin.inspirationHub.Topics.environmentRenewableEnergy',
+    defaultMessage: 'Environment: Renewable Energy',
+  },
+  environmentWaste: {
+    id: 'app.containers.Admin.inspirationHub.Topics.environmentWaste',
+    defaultMessage: 'Environment: Waste',
+  },
+  environmentWildlifeConservation: {
+    id: 'app.containers.Admin.inspirationHub.Topics.environmentWildlifeConservation',
+    defaultMessage: 'Environment: Wildlife Conservation',
+  },
+  urbanDevelopmentGeneral: {
+    id: 'app.containers.Admin.inspirationHub.Topics.urbanDevelopmentGeneral',
+    defaultMessage: 'Urban Development: General',
+  },
+  urbanDevelopmentAffordableHousing: {
+    id: 'app.containers.Admin.inspirationHub.Topics.urbanDevelopmentAffordableHousing',
+    defaultMessage: 'Urban Development: Affordable Housing',
+  },
+  urbanDevelopmentResidentialZoning: {
+    id: 'app.containers.Admin.inspirationHub.Topics.urbanDevelopmentResidentialZoning',
+    defaultMessage: 'Urban Development: Residential Zoning',
+  },
+  urbanDevelopmentShopping: {
+    id: 'app.containers.Admin.inspirationHub.Topics.urbanDevelopmentShopping',
+    defaultMessage: 'Urban Development: Shopping',
+  },
+  urbanDevelopmentBusiness: {
+    id: 'app.containers.Admin.inspirationHub.Topics.urbanDevelopmentBusiness',
+    defaultMessage: 'Urban Development: Business',
+  },
+  urbanDevelopmentPlazas: {
+    id: 'app.containers.Admin.inspirationHub.Topics.urbanDevelopmentPlazas',
+    defaultMessage: 'Urban Development: Plazas',
+  },
+  urbanDevelopmentArt: {
+    id: 'app.containers.Admin.inspirationHub.Topics.urbanDevelopmentArt',
+    defaultMessage: 'Urban Development: Art',
+  },
+  communityGeneral: {
+    id: 'app.containers.Admin.inspirationHub.Topics.communityGeneral',
+    defaultMessage: 'Community: General',
+  },
+  communityPublicHealth: {
+    id: 'app.containers.Admin.inspirationHub.Topics.communityPublicHealth',
+    defaultMessage: 'Community: Public Health',
+  },
+  communityEmergencyServices: {
+    id: 'app.containers.Admin.inspirationHub.Topics.communityEmergencyServices',
+    defaultMessage: 'Community: Emergency Services',
+  },
+  communitySchool: {
+    id: 'app.containers.Admin.inspirationHub.Topics.communitySchool',
+    defaultMessage: 'Community: School',
+  },
+  communityLibrary: {
+    id: 'app.containers.Admin.inspirationHub.Topics.communityLibrary',
+    defaultMessage: 'Community: Library',
+  },
+  communitySports: {
+    id: 'app.containers.Admin.inspirationHub.Topics.communitySports',
+    defaultMessage: 'Community: Sports',
+  },
+  communityCommunityCenters: {
+    id: 'app.containers.Admin.inspirationHub.Topics.communityCommunityCenters',
+    defaultMessage: 'Community: Community Centers',
+  },
+  communitySupportPrograms: {
+    id: 'app.containers.Admin.inspirationHub.Topics.communitySupportPrograms',
+    defaultMessage: 'Community: Support Programs',
+  },
+  technologyGeneral: {
+    id: 'app.containers.Admin.inspirationHub.Topics.technologyGeneral',
+    defaultMessage: 'Technology: General',
+  },
+  technologySmartCity: {
+    id: 'app.containers.Admin.inspirationHub.Topics.technologySmartCity',
+    defaultMessage: 'Technology: Smart City',
+  },
+  technologyTelecommunication: {
+    id: 'app.containers.Admin.inspirationHub.Topics.technologyTelecommunication',
+    defaultMessage: 'Technology: Telecommunication',
+  },
+  technologyDigitalLiteracy: {
+    id: 'app.containers.Admin.inspirationHub.Topics.technologyDigitalLiteracy',
+    defaultMessage: 'Technology: Digital Literacy',
+  },
 });
