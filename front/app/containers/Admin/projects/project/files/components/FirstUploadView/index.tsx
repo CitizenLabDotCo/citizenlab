15fd2bf3787e10e9f5134c3be9421759e1d96c67
Amanda Anderson <anderson_amanda@mail.com>
2025-09-29 10:10:50 +0200

    Remove sense-making mention without causing deadcode.

--- front/app/containers/Admin/projects/project/files/components/FirstUploadView/index.tsx
+++ front/app/containers/Admin/projects/project/files/components/FirstUploadView/index.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 
 import { Box, colors } from '@citizenlab/cl2-component-library';
 
+import useFeatureFlag from 'hooks/useFeatureFlag';
+
 import FilesUpload from '../FilesUpload';
+
+import FeatureDescription from './components/FeatureDescription';
 
 type Props = {
   setShowFirstUploadView?: (value: boolean) => void;
 };
 const FirstUploadView = ({ setShowFirstUploadView }: Props) => {
+  const isDataRepositoryAIAnalysisEnabled = useFeatureFlag({
+    name: 'data_repository_ai_analysis',
+  }); // TODO: Re-enable once sense-making feature is ready. Remove this FF check.
+
   return (
     <Box display="flex" justifyContent="center" mt="40px">
       <Box
@@ -20,9 +28,11 @@ const FirstUploadView = ({ setShowFirstUploadView }: Props) => {
         <Box>
           <FilesUpload setShowFirstUploadView={setShowFirstUploadView} />
         </Box>
-        {/* <Box width="400px"> // TODO: Re-enable once sense-making feature is ready.
-          <FeatureDescription />
-        </Box> */}
+        {isDataRepositoryAIAnalysisEnabled && ( // TODO: Re-enable once sense-making feature is ready. Remove this conditional.
+          <Box width="400px">
+            <FeatureDescription />
+          </Box>
+        )}
       </Box>
     </Box>
   );
