276830f6ce02a4a83f70f526a8804930f97a1601
jamesspeake <james@speake.org>
2025-09-29 10:40:15 +0100

    [TAN-5473] Fixed deadcode

--- front/app/api/invites/invitesImportKeys.ts
+++ front/app/api/invites/invitesImportKeys.ts
@@ -2,7 +2,7 @@ import { QueryKeys } from 'utils/cl-react-query/types';
 
 const baseKey = { type: 'invites_import' };
 
-export const invitesImportKeys = {
+const invitesImportKeys = {
   all: () => [baseKey],
   items: () => [{ ...baseKey, operation: 'item' }],
   item: ({ id }: { id?: string | null }) => [
