fcf92605a88529d2d9970348bc2662fa022df1c0
Sébastien Hoorens <sebastien@citizenlab.co>
2025-10-01 19:21:26 +0200

    Validation of locks for community monitoring was not supported

--- back/app/services/custom_fields_validation_service.rb
+++ back/app/services/custom_fields_validation_service.rb
@@ -52,6 +52,8 @@ class CustomFieldsValidationService
       next if !field_constraints
 
       default_field = default_fields.find { |f| f.code == field.code }
+      # TODO: Remove and check constraints for default community monitoring fields
+      next if !default_field
 
       field_constraints.dig(:locks, :attributes)&.each do |attribute|
         if field[attribute] != default_field.send(attribute)
