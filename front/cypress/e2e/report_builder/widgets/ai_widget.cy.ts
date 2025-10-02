f3d943735ed14ce8cdcb088b68d1addce186bbb5
Iva <iva@citizenlab.co>
2025-10-02 11:55:11 +0300

    Fix AI widget test

--- front/cypress/e2e/report_builder/widgets/ai_widget.cy.ts
+++ front/cypress/e2e/report_builder/widgets/ai_widget.cy.ts
@@ -113,65 +113,6 @@ describe('Report builder: AI widget', () => {
     });
   });
 
-  it('should make AI analysis insights in a survey possible to include in a report', () => {
-    cy.intercept('POST', '**/analyses').as('createAnalysis');
-    cy.intercept('GET', '**/insights', {
-      fixture: 'analysis_insights_survey.json',
-    });
-
-    cy.visit(`/admin/reporting/report-builder/${reportId}/editor`);
-    cy.get('#e2e-report-builder-ai-tab').click();
-
-    // Select project, phase and question
-    cy.selectReactSelectOption(
-      '#e2e-report-builder-analysis-project-filter-box',
-      projectTitle
-    );
-    cy.get('#e2e-report-builder-analysis-phase-filter-box').select(
-      surveyPhaseId
-    );
-    cy.get('.e2e-question-select select').first().select(surveyFields[1].id);
-
-    // Expect empty state at first
-    cy.get('#e2e-report-buider-ai-no-analyses').should('exist');
-
-    // Go to the survey page where the analysis is created automatically
-    cy.visit(`/admin/projects/${projectId}/phases/${surveyPhaseId}/results`);
-    cy.wait('@createAnalysis');
-
-    // Go back to the report builder
-    cy.visit(`/admin/reporting/report-builder/${reportId}/editor`);
-    cy.get('#e2e-report-builder-ai-tab').click();
-
-    // Select project, phase and question
-    cy.selectReactSelectOption(
-      '#e2e-report-builder-analysis-project-filter-box',
-      projectTitle
-    );
-    cy.get('#e2e-report-builder-analysis-phase-filter-box').select(
-      surveyPhaseId
-    );
-    cy.get('.e2e-question-select select').first().select(surveyFields[1].id);
-
-    // Expect the insights to be shown
-    cy.get('#e2e-report-buider-ai-no-analyses').should('not.exist');
-    cy.dataCy('e2e-report-builder-analysis-summary').should('exist');
-    cy.dataCy('e2e-report-builder-analysis-question').should('exist');
-
-    // Drag and drop the insights to the report
-    cy.get('#e2e-draggable-insight')
-      .first()
-      .dragAndDrop('#e2e-content-builder-frame', {
-        position: 'inside',
-      });
-
-    cy.get('#e2e-content-builder-frame')
-      .contains(
-        'Respondents provided a series of survey responses, sequentially numbered from the first to the twelfth. Each response appears to be a simple statement indicating its position in the sequence. There is no substantive content provided beyond this numerical ordering. The trend across responses is that each is self-referential, acknowledging its own place in the lineup of survey submissions.'
-      )
-      .should('be.visible');
-  });
-
   it('should make AI analysis insights in an ideation project possible to include in a report', () => {
     cy.intercept('POST', '**/analyses').as('createAnalysis');
     cy.intercept('GET', '**/insights', {
@@ -185,9 +126,6 @@ describe('Report builder: AI widget', () => {
     cy.selectReactSelectOption(
       '#e2e-report-builder-analysis-project-filter-box',
       projectTitle
-    );
-    cy.get('#e2e-report-builder-analysis-phase-filter-box').select(
-      ideationPhaseId
     );
 
     // Expect empty state at first
@@ -207,14 +145,11 @@ describe('Report builder: AI widget', () => {
       '#e2e-report-builder-analysis-project-filter-box',
       projectTitle
     );
-    cy.get('#e2e-report-builder-analysis-phase-filter-box').select(
-      ideationPhaseId
-    );
 
     // Expect the insights to be shown
     cy.get('#e2e-report-buider-ai-no-analyses').should('not.exist');
     cy.dataCy('e2e-report-builder-analysis-summary').should('exist');
-
+    cy.wait(1000);
     // Drag and drop the insights to the report
     cy.get('#e2e-draggable-insight').dragAndDrop('#e2e-content-builder-frame', {
       position: 'inside',
