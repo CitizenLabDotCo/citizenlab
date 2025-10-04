b644721acb7103181346c4a162d5093874303429
jinjagit <jinjamail@gmail.com>
2025-10-04 15:23:44 +0100

    [TAN-5555] Remove redundant flattened custom ordering in index action

--- back/app/controllers/web_api/v1/admin_publications_controller.rb
+++ back/app/controllers/web_api/v1/admin_publications_controller.rb
@@ -25,15 +25,7 @@ class WebApi::V1::AdminPublicationsController < ApplicationController
       puts "title with fallback v3: #{title_with_fallback}"
     end
 
-    # A flattened ordering, such that project publications with a parent (projects in folders) are ordered
-    # first by their parent's :ordering, and then by their own :ordering (their ordering within the folder).
-    admin_publications = admin_publications.select(
-      'admin_publications.*',
-      'CASE WHEN admin_publications.parent_id IS NULL THEN admin_publications.ordering ELSE parents.ordering END
-      AS root_ordering'
-    )
-      .joins('LEFT OUTER JOIN admin_publications AS parents ON parents.id = admin_publications.parent_id')
-      .order('root_ordering, admin_publications.ordering')
+    admin_publications = admin_publications.order('admin_publications.ordering ASC')
 
     @admin_publications = paginate admin_publications
 
