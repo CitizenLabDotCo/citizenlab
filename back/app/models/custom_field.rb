d14062d51d6287743d977a0b24de2c58aa8d97a8
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-10-07 16:34:41 -0300

    Remove comment in custom_field

--- back/app/models/custom_field.rb
+++ back/app/models/custom_field.rb
@@ -49,11 +49,6 @@
 #
 #  index_custom_fields_on_resource_type_and_resource_id  (resource_type,resource_id)
 #
-
-# support table :
-# Jsonforms (under dynamic_idea_form and jsonforms_custom_fields) supports all INPUT_TYPES
-# The older react json form version works only with text number multiline_text select multiselect checkbox date
-# The other types will fail for user custom fields and render a shallow schema for idea custom fields with only the required, hidden, title and description.
 class CustomField < ApplicationRecord
   acts_as_list column: :ordering, top_of_list: 0, scope: [:resource_id]
 
