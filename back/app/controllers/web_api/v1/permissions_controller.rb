ab8fe474dd97406b06b2981aaf335cf143ffd4d2
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-10-07 14:43:47 -0300

    Update permissions policy

--- back/app/controllers/web_api/v1/permissions_controller.rb
+++ back/app/controllers/web_api/v1/permissions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class WebApi::V1::PermissionsController < ApplicationController
-  before_action :set_permission, only: %i[show update reset requirements schema access_denied_explanation]
+  before_action :set_permission, only: %i[show update reset requirements access_denied_explanation]
   skip_before_action :authenticate_user
 
   def index
@@ -104,28 +104,6 @@ class WebApi::V1::PermissionsController < ApplicationController
       group_ids: [],
       access_denied_explanation_multiloc: CL2_SUPPORTED_LOCALES
     )
-  end
-
-  # lock any fields locked by verification method(s)
-  def mark_locked_json_forms_fields(schemas)
-    locked_custom_fields = verification_service.locked_custom_fields(current_user).map(&:to_s)
-
-    # Mark fields as locked & read only
-    schemas[:ui_schema_multiloc].each_value do |ui_schema|
-      ui_schema[:elements]
-        .select { |e| locked_custom_fields.any? { |field| e[:scope].end_with?(field) } }
-        .each_with_index do |element, index|
-        ui_schema[:elements][index] = element.merge(options: element[:options].to_h.merge(readonly: true, verificationLocked: true))
-      end
-    end
-
-    # Mark fields as required (if not already required)
-    schemas[:json_schema_multiloc].each_value do |json_schema|
-      json_schema[:required] = [] if json_schema[:required].nil?
-      locked_custom_fields.each do |locked_field|
-        json_schema[:required] << locked_field if json_schema[:required].exclude?(locked_field)
-      end
-    end
   end
 
   def verification_service
