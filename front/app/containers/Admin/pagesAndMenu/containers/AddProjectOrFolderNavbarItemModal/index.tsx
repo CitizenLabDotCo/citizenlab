f226d2c2993ddc37e8b56c625fde137179b6c30d
jinjagit <jinjamail@gmail.com>
2025-10-08 17:46:35 +0100

    [TAN-5555] Linting: Remove unused import

--- front/app/containers/Admin/pagesAndMenu/containers/AddProjectOrFolderNavbarItemModal/index.tsx
+++ front/app/containers/Admin/pagesAndMenu/containers/AddProjectOrFolderNavbarItemModal/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 import { Box, Text } from '@citizenlab/cl2-component-library';
 import { yupResolver } from '@hookform/resolvers/yup';
