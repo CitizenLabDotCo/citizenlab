440c7c8d0497b5f5e86173b69fd250e5d8bde4d7
Adrien Dessy <209268+adessy@users.noreply.github.com>
2025-10-13 13:36:44 +0200

    [TAN-5196] Refine Q&A errors and defer retries to the job scheduler
    By deferring the retry logic to the job scheduler, we can avoid suspending the thread.

--- back/engines/commercial/analysis/app/lib/analysis/q_and_a_method/base.rb
+++ back/engines/commercial/analysis/app/lib/analysis/q_and_a_method/base.rb
@@ -9,9 +9,6 @@ module Analysis
       LLM::GPT4oMini.new
     ]
 
-    class QAndAFailedError < StandardError; end
-    class TooManyInputs < QAndAFailedError; end
-
     class << self
       def method_classes
         @method_classes ||= [QAndAMethod::OnePassLLM]
@@ -32,27 +29,32 @@ module Analysis
     end
 
     def execute(plan)
-      task.set_in_progress!
+      execute!(plan)
+    rescue StandardError => e
+      ErrorReporter.report(e)
+      task.set_failed!
+    end
+
+    def execute!(plan)
       old_answer = question.answer
       old_accuracy = question.accuracy
       old_input_ids = question.insight.inputs_ids
       old_custom_field_ids = question.insight.custom_field_ids
-      question.update!(accuracy: plan.accuracy, answer: '')
+
       custom_field_ids = {
         main_custom_field_id: analysis.main_custom_field_id,
         additional_custom_field_ids: analysis.additional_custom_field_ids
       }
-      question.insight.update!(inputs_ids: filtered_inputs.ids, custom_field_ids: custom_field_ids)
 
-      begin
-        run(plan)
-        task.set_succeeded!
-      rescue QAndAFailedError => e
-        ErrorReporter.report(e)
-        task.set_failed!
-        question.update!(accuracy: old_accuracy, answer: old_answer)
-        question.insight.update!(inputs_ids: old_input_ids, custom_field_ids: old_custom_field_ids)
-      end
+      task.set_in_progress!
+      question.update!(accuracy: plan.accuracy, answer: '')
+      question.insight.update!(inputs_ids: filtered_inputs.ids, custom_field_ids: custom_field_ids)
+      run(plan)
+      task.set_succeeded!
+    rescue StandardError
+      question.update!(accuracy: old_accuracy, answer: old_answer)
+      question.insight.update!(inputs_ids: old_input_ids, custom_field_ids: old_custom_field_ids)
+      raise
     end
 
     # Should be implemented by subclasses.
