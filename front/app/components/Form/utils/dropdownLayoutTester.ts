4894261ad7a5e21fa79ec90a93e84203f42f25e6
Iva <iva@citizenlab.co>
2025-10-13 13:42:07 +0300

    Delete dead code

--- front/app/components/Form/utils/dropdownLayoutTester.ts
+++
@@ -1,9 +0,0 @@
-import { Tester } from '@jsonforms/core';
-
-const dropdownLayoutTester: Tester = (uischema) => {
-  // TODO: Fix this the next time the file is edited.
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  return uischema?.options?.dropdown_layout || false;
-};
-
-export default dropdownLayoutTester;
