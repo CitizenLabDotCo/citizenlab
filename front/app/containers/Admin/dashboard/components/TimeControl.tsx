a2ca597a66ed1c8aae75ecb14e4843ef4df6f345
EdwinKato <katedwin88@gmail.com>
2025-10-14 17:06:35 +0200

    Add height="48px" to DateRangePicker in TimeControl for consistent styling

--- front/app/containers/Admin/dashboard/components/TimeControl.tsx
+++ front/app/containers/Admin/dashboard/components/TimeControl.tsx
@@ -219,6 +219,7 @@ const TimeControl = ({
             endDate: to ? moment(to) : null,
           });
         }}
+        height="48px"
       />
       {tooltip && <IconTooltip ml="12px" content={tooltip} />}
     </Container>
