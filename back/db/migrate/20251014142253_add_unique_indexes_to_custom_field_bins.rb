12fbceb3c754457aac5e9e9fc64c1318a1271075
kogre <koen@govocal.com>
2025-10-21 15:01:40 +0200

    rubocop

--- back/db/migrate/20251014142253_add_unique_indexes_to_custom_field_bins.rb
+++ back/db/migrate/20251014142253_add_unique_indexes_to_custom_field_bins.rb
@@ -4,28 +4,28 @@ class AddUniqueIndexesToCustomFieldBins < ActiveRecord::Migration[7.1]
   def up
     # OptionBin: unique on [custom_field_id, custom_field_option_id] when type is CustomFieldBins::OptionBin
     add_index :custom_field_bins,
-      [:custom_field_id, :custom_field_option_id],
+      %i[custom_field_id custom_field_option_id],
       unique: true,
       where: "type = 'CustomFieldBins::OptionBin'",
       name: 'index_option_bins_on_cf_and_option'
 
     # ValueBin: unique on [custom_field_id, values] when type is CustomFieldBins::ValueBin
     add_index :custom_field_bins,
-      [:custom_field_id, :values],
+      %i[custom_field_id values],
       unique: true,
       where: "type = 'CustomFieldBins::ValueBin'",
       name: 'index_value_bins_on_cf_and_values'
 
     # RangeBin: unique on [custom_field_id, range] when type is CustomFieldBins::RangeBin
     add_index :custom_field_bins,
-      [:custom_field_id, :range],
+      %i[custom_field_id range],
       unique: true,
       where: "type = 'CustomFieldBins::RangeBin'",
       name: 'index_range_bins_on_cf_and_range'
 
     # AgeBin: unique on [custom_field_id, range] when type is CustomFieldBins::AgeBin
     add_index :custom_field_bins,
-      [:custom_field_id, :range],
+      %i[custom_field_id range],
       unique: true,
       where: "type = 'CustomFieldBins::AgeBin'",
       name: 'index_age_bins_on_cf_and_range'
