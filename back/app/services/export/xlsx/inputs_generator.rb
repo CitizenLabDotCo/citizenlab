0dfbbe0f6263d2c02838e5b7fce8e1c55d51d90e
Amanda Anderson <anderson_amanda@mail.com>
2025-10-27 11:48:03 +0100

    Fix community monitor export

--- back/app/services/export/xlsx/inputs_generator.rb
+++ back/app/services/export/xlsx/inputs_generator.rb
@@ -66,7 +66,7 @@ module Export
 
       def generate_phase_sheet(workbook, phase)
         inputs = eager_load_inputs(phase.ideas.submitted_or_published)
-        inputs = inputs.with_content unless phase.participation_method == 'native_survey'
+        inputs = inputs.with_content unless phase.participation_method == 'native_survey' or phase.participation_method == 'community_monitor_survey'
 
         generate_sheet(workbook, inputs, phase)
       end
