61124cfbecd48dd116e0e05152fe861d96a4489d
Amanda Anderson <anderson_amanda@mail.com>
2025-10-28 14:47:39 +0100

    Include creation phase title in public api Idea response

--- back/engines/commercial/public_api/app/serializers/public_api/v2/idea_serializer.rb
+++ back/engines/commercial/public_api/app/serializers/public_api/v2/idea_serializer.rb
@@ -26,6 +26,7 @@ class PublicApi::V2::IdeaSerializer < PublicApi::V2::BaseSerializer
     :assigned_at,
     :proposed_budget,
     :creation_phase_id,
+    :creation_phase_title,
     :images, # Not in spec
     :href, # Not in spec
     :status, # idea_status in spec
@@ -60,6 +61,10 @@ class PublicApi::V2::IdeaSerializer < PublicApi::V2::BaseSerializer
 
   def href
     Frontend::UrlService.new.model_to_url object
+  end
+
+  def creation_phase_title
+    multiloc_service.t(object.creation_phase&.title_multiloc)
   end
 
   private
