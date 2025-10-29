9e2db5ee936c952527e2a586c9c6ce6ba3069dc3
Sébastien Hoorens <sebastien@citizenlab.co>
2025-10-29 14:12:49 +0100

    Spec for caching of as_markers action

--- back/engines/commercial/aggressive_caching/spec/acceptance/ideas_spec.rb
+++ back/engines/commercial/aggressive_caching/spec/acceptance/ideas_spec.rb
@@ -84,4 +84,41 @@ resource 'Ideas', :clear_cache, document: false do
       end
     end
   end
+
+  get 'web_api/v1/ideas/as_markers' do
+    let(:cache_key) { 'api_response/example.org/web_api/v1/ideas/as_markers.json' }
+
+    example 'caches for a visitor' do
+      expect(Rails.cache.read(cache_key)).to be_nil
+      do_request
+      expect(status).to eq 200
+      expect(Rails.cache.read(cache_key)).to be_present
+    end
+
+    context 'when logged in' do
+      before { header_token_for create(:user) }
+
+      example 'does not cache' do
+        expect(Rails.cache.read(cache_key)).to be_nil
+        do_request
+        expect(status).to eq 200
+        expect(Rails.cache.read(cache_key)).to be_nil
+      end
+    end
+
+    context 'with aggressive caching disabled' do
+      before do
+        settings = AppConfiguration.instance.settings
+        settings['aggressive_caching'] = { 'enabled' => false, 'allowed' => true }
+        AppConfiguration.instance.update!(settings:)
+      end
+
+      example 'does not cache' do
+        expect(Rails.cache.read(cache_key)).to be_nil
+        do_request
+        expect(status).to eq 200
+        expect(Rails.cache.read(cache_key)).to be_nil
+      end
+    end
+  end
 end
