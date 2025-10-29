f72a8143c865c325db74631b105b9ad64bace26c
Sébastien Hoorens <sebastien@citizenlab.co>
2025-10-29 13:23:23 +0100

    use Rails 7.2 defaults

--- back/config/application.rb
+++ back/config/application.rb
@@ -23,7 +23,7 @@ module Cl2Back
     require_dependency Rails.root.join('lib/citizen_lab')
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
