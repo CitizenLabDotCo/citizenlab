d6c116b98210b241857152ec5a3fcc084d8749ab
Amanda Anderson <anderson_amanda@mail.com>
2025-10-30 15:45:57 +0100

    Remove unused code

--- back/engines/commercial/public_api/app/serializers/public_api/v2/file_attachment_serializer.rb
+++ back/engines/commercial/public_api/app/serializers/public_api/v2/file_attachment_serializer.rb
@@ -3,9 +3,9 @@
 class PublicApi::V2::FileAttachmentSerializer < PublicApi::V2::BaseSerializer
   attributes :id, :project_id, :created_at, :updated_at, :attachable_type, :attachable_id
 
-  attribute(:file_url) { |attachment| object.file.content.url }
-  attribute(:file_name) { |attachment| object.file.name }
-  attribute(:file_size) { |attachment| object.file.size }
+  attribute(:file_url) { object.file.content.url }
+  attribute(:file_name) { object.file.name }
+  attribute(:file_size) { object.file.size }
 
   def project_id
     case object.attachable_type
