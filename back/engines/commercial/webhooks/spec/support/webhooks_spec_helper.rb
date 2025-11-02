2bb010c4793df6391f6ec2a8e62a252a43c46531
kogre <koen@govocal.com>
2025-11-02 20:18:38 +0100

    rubocop

--- back/engines/commercial/webhooks/spec/support/webhooks_spec_helper.rb
+++ back/engines/commercial/webhooks/spec/support/webhooks_spec_helper.rb
@@ -2,7 +2,7 @@
 
 RSpec.configure do |config|
   # Stub DNS resolution ONLY for webhooks engine specs to avoid network calls during validation
-  config.before do |example|
+  config.before do
     allow(Resolv).to receive(:getaddresses).and_call_original
     allow(Resolv).to receive(:getaddresses).with(a_string_matching(/webhook.example.com.*/)).and_return(['93.184.216.34'])
   end
