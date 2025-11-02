5693ecb765781fde385d6d9ed46c12585a7c96f9
kogre <koen@govocal.com>
2025-11-02 15:43:57 +0100

    Fix failing specs through limited scope of Resolv mock

--- back/engines/commercial/webhooks/spec/support/webhooks_spec_helper.rb
+++ back/engines/commercial/webhooks/spec/support/webhooks_spec_helper.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
-  # Stub DNS resolution globally for webhook tests to avoid network calls
-  config.before do
-    allow(Resolv).to receive(:getaddresses).and_return(['93.184.216.34'])
+  # Stub DNS resolution ONLY for webhooks engine specs to avoid network calls during validation
+  # We scope this to only files in the webhooks directory using metadata[:file_path]
+  config.before do |example|
+    # Only stub for specs in the webhooks engine
+    if example.metadata[:file_path].to_s.include?('engines/commercial/webhooks/')
+      allow(Resolv).to receive(:getaddresses).and_call_original
+      allow(Resolv).to receive(:getaddresses).with(any_args).and_return(['93.184.216.34'])
+    end
   end
 end
