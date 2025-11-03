c4f660a862d384d9282bf70fe73262eeb6be47a2
Amira Bayoumi <amira.bayoumii@gmail.com>
2025-11-03 10:59:19 +0100

    show long links as hover tooltip in Visitors table

--- front/app/components/admin/GraphCards/VisitorsTrafficSourcesCard/Table.tsx
+++ front/app/components/admin/GraphCards/VisitorsTrafficSourcesCard/Table.tsx
@@ -11,11 +11,13 @@ import {
   colors,
   stylingConsts,
   Text,
+  IconTooltip,
 } from '@citizenlab/cl2-component-library';
 
 import Pagination from 'components/Pagination';
 
 import { FormattedMessage } from 'utils/cl-intl';
+import { truncate } from 'utils/textUtils';
 
 import messages from './messages';
 import ReferrerListLink from './RefferListLink';
@@ -62,9 +64,36 @@ const TableComponent = ({ tableData, onOpenModal }: Props) => {
           {paginatedData.map((row, i) => (
             <Tr key={i}>
               <Td background={colors.grey50}>
-                <Text color="primary" fontSize="s" wordBreak="break-word">
-                  ({row.referrer_type}) {row.referrer}
-                </Text>
+                {row.referrer.length <= 70 ? (
+                  <Text
+                    m="0px"
+                    color="primary"
+                    fontSize="s"
+                    wordBreak="break-word"
+                  >
+                    ({row.referrer_type}) {row.referrer}
+                  </Text>
+                ) : (
+                  <>
+                    <Text
+                      m="0px"
+                      color="primary"
+                      fontSize="s"
+                      wordBreak="break-word"
+                    >
+                      ({row.referrer_type}) {truncate(row.referrer, 70)}
+                    </Text>{' '}
+                    <IconTooltip
+                      content={
+                        <Text wordBreak="break-word" color="white" fontSize="s">
+                          {row.referrer}
+                        </Text>
+                      }
+                      icon="info-outline"
+                      placement="bottom"
+                    />
+                  </>
+                )}
               </Td>
               <Td>{row.visits}</Td>
               <Td>{row.visitors}</Td>
