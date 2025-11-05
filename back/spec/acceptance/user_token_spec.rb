bc9c179b88bd39384b3165b66470e18a07837de8
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-11-05 14:15:02 -0300

    Add test to confirm that user_token will be issued if confirmation is disabled

--- back/spec/acceptance/user_token_spec.rb
+++ back/spec/acceptance/user_token_spec.rb
@@ -15,187 +15,251 @@ resource 'User Token' do
       parameter :remember_me, required: false
     end
 
-    context 'when user is confirmed' do
-      let(:email) { 'test@email.com' }
-      let(:password) { '12345678' }
-      let(:remember_me) { false }
-
-      let!(:user) { create(:user, email: email, password: password) }
-
+    context 'when user_confirmation is enabled' do
       before do
-        allow(Time).to receive(:now).and_return(Time.now)
         SettingsService.new.activate_feature! 'user_confirmation'
       end
 
-      example_request 'Create JWT token creates expected payload' do
-        assert_status 201
+      context 'when user is confirmed' do
+        let(:email) { 'test@email.com' }
+        let(:password) { '12345678' }
+        let(:remember_me) { false }
 
-        jwt = JWT.decode(json_response_body[:jwt], nil, false).first
+        let!(:user) { create(:user, email: email, password: password) }
 
-        expect(jwt['sub']).to eq(user.id)
-        expect(jwt['highest_role']).to eq('user')
-        expect(jwt['cluster']).to eq('local')
-        expect(jwt['tenant']).to eq(Tenant.current.id)
-        expect(jwt['exp']).to eq((Time.now + 1.day).to_i)
-      end
+        before do
+          allow(Time).to receive(:now).and_return(Time.now)
+        end
 
-      context 'when the user is an admin' do
-        let!(:user) { create(:admin, email: email, password: password) }
-
-        example_request 'Create JWT token for admin' do
+        example_request 'Create JWT token creates expected payload' do
           assert_status 201
 
           jwt = JWT.decode(json_response_body[:jwt], nil, false).first
 
           expect(jwt['sub']).to eq(user.id)
-          expect(jwt['highest_role']).to eq('admin')
-        end
-      end
-
-      context 'when the user is a project moderator' do
-        let!(:user) { create(:project_moderator, email: email, password: password) }
-
-        example_request 'Create JWT token for project moderator' do
-          assert_status 201
-
-          jwt = JWT.decode(json_response_body[:jwt], nil, false).first
-
-          expect(jwt['sub']).to eq(user.id)
-          expect(jwt['highest_role']).to eq('project_moderator')
-        end
-      end
-
-      context 'when the user is a folder moderator' do
-        let!(:user) { create(:project_folder_moderator, email: email, password: password) }
-
-        example_request 'Create JWT token for project folder moderator' do
-          assert_status 201
-
-          jwt = JWT.decode(json_response_body[:jwt], nil, false).first
-
-          expect(jwt['sub']).to eq(user.id)
-          expect(jwt['highest_role']).to eq('project_folder_moderator')
-        end
-      end
-
-      example_request 'Create JWT token with 1 day expiration' do
-        assert_status 201
-
-        jwt = JWT.decode(json_response_body[:jwt], nil, false).first
-
-        expect(jwt['exp']).to eq((Time.now + 1.day).to_i)
-      end
-
-      context 'when remember_me is sent' do
-        let(:remember_me) { true }
-
-        example_request 'create JWT token with default expiration' do
-          expect(status).to eq(201)
-
-          jwt = JWT.decode(json_response_body[:jwt], nil, false).first
-          expect(jwt['exp']).to eq((Time.now + 30.days).to_i)
+          expect(jwt['highest_role']).to eq('user')
+          expect(jwt['cluster']).to eq('local')
+          expect(jwt['tenant']).to eq(Tenant.current.id)
+          expect(jwt['exp']).to eq((Time.now + 1.day).to_i)
         end
 
-        context 'when authentication_token_lifetime_in_days is configured' do
-          before do
-            config = AppConfiguration.instance
-            config.settings['core']['authentication_token_lifetime_in_days'] = token_lifetime
-            config.save!
+        context 'when the user is an admin' do
+          let!(:user) { create(:admin, email: email, password: password) }
+
+          example_request 'Create JWT token for admin' do
+            assert_status 201
+
+            jwt = JWT.decode(json_response_body[:jwt], nil, false).first
+
+            expect(jwt['sub']).to eq(user.id)
+            expect(jwt['highest_role']).to eq('admin')
           end
+        end
 
-          let(:token_lifetime) { 8 }
+        context 'when the user is a project moderator' do
+          let!(:user) { create(:project_moderator, email: email, password: password) }
 
-          example_request 'create JWT token with expiration from settings' do
+          example_request 'Create JWT token for project moderator' do
+            assert_status 201
+
+            jwt = JWT.decode(json_response_body[:jwt], nil, false).first
+
+            expect(jwt['sub']).to eq(user.id)
+            expect(jwt['highest_role']).to eq('project_moderator')
+          end
+        end
+
+        context 'when the user is a folder moderator' do
+          let!(:user) { create(:project_folder_moderator, email: email, password: password) }
+
+          example_request 'Create JWT token for project folder moderator' do
+            assert_status 201
+
+            jwt = JWT.decode(json_response_body[:jwt], nil, false).first
+
+            expect(jwt['sub']).to eq(user.id)
+            expect(jwt['highest_role']).to eq('project_folder_moderator')
+          end
+        end
+
+        example_request 'Create JWT token with 1 day expiration' do
+          assert_status 201
+
+          jwt = JWT.decode(json_response_body[:jwt], nil, false).first
+
+          expect(jwt['exp']).to eq((Time.now + 1.day).to_i)
+        end
+
+        context 'when remember_me is sent' do
+          let(:remember_me) { true }
+
+          example_request 'create JWT token with default expiration' do
             expect(status).to eq(201)
 
             jwt = JWT.decode(json_response_body[:jwt], nil, false).first
-            expect(jwt['exp']).to eq((Time.now + token_lifetime.days).to_i)
-          end
-        end
-
-        context 'when password login is turned off' do
-          before { SettingsService.new.deactivate_feature! 'password_login' }
-
-          example '[error] does not allow a regular user to log in with a password', document: false do
-            do_request
-            assert_status 404
+            expect(jwt['exp']).to eq((Time.now + 30.days).to_i)
           end
 
-          example '[error] does not allow a regular admin in with a password', document: false do
-            user.update!(roles: [{ type: 'admin' }])
-            do_request
-            assert_status 404
+          context 'when authentication_token_lifetime_in_days is configured' do
+            before do
+              config = AppConfiguration.instance
+              config.settings['core']['authentication_token_lifetime_in_days'] = token_lifetime
+              config.save!
+            end
+
+            let(:token_lifetime) { 8 }
+
+            example_request 'create JWT token with expiration from settings' do
+              expect(status).to eq(201)
+
+              jwt = JWT.decode(json_response_body[:jwt], nil, false).first
+              expect(jwt['exp']).to eq((Time.now + token_lifetime.days).to_i)
+            end
           end
 
-          context 'super admin' do
-            let(:email) { 'hello@citizenlab.co' }
+          context 'when password login is turned off' do
+            before { SettingsService.new.deactivate_feature! 'password_login' }
 
-            example 'allows a super admin to log in with a password', document: false do
-              user.update!(email: email, roles: [{ type: 'admin' }])
+            example '[error] does not allow a regular user to log in with a password', document: false do
               do_request
-              assert_status 201
+              assert_status 404
+            end
+
+            example '[error] does not allow a regular admin in with a password', document: false do
+              user.update!(roles: [{ type: 'admin' }])
+              do_request
+              assert_status 404
+            end
+
+            context 'super admin' do
+              let(:email) { 'hello@citizenlab.co' }
+
+              example 'allows a super admin to log in with a password', document: false do
+                user.update!(email: email, roles: [{ type: 'admin' }])
+                do_request
+                assert_status 201
+              end
             end
           end
         end
+
+        example 'Does not create JWT token with invalid password' do
+          do_request(auth: { email: email, password: 'wrongpassword' })
+          expect(status).to eq(404)
+        end
       end
 
-      example 'Does not create JWT token with invalid password' do
-        do_request(auth: { email: email, password: 'wrongpassword' })
-        expect(status).to eq(404)
-      end
-    end
+      context 'when the user is unconfirmed' do
+        let(:email) { 'test@email.com' }
+        let(:password) { '12345678' }
+        let(:remember_me) { false }
 
-    context 'when the user is unconfirmed' do
-      let(:email) { 'test@email.com' }
-      let(:password) { '12345678' }
-      let(:remember_me) { false }
+        let!(:user) { create(:user_with_confirmation, email: email, password: password) }
 
-      let!(:user) { create(:user_with_confirmation, email: email, password: password) }
-
-      before do
-        allow(Time).to receive(:now).and_return(Time.now)
-        SettingsService.new.activate_feature! 'user_confirmation'
-      end
-
-      example_request '[error] no JWT token is returned' do
-        assert_status 404
-      end
-    end
-
-    context 'when the user is an invited user' do
-      let(:email) { 'test@email.com' }
-      let(:password) { '12345678' }
-
-      before do
-        create(:invited_user, email: email, password: password)
-      end
-
-      example_request '[error] no JWT token is returned' do
-        assert_status 404
-      end
-    end
-
-    context 'when no password is used' do
-      let(:email) { 'test@email.com' }
-      let(:password) { '' }
-
-      before do
-        SettingsService.new.activate_feature! 'user_confirmation'
-      end
-
-      context 'when user has password' do
-        let!(:user) { create(:user, email: email, password: 'other_password') }
+        before do
+          allow(Time).to receive(:now).and_return(Time.now)
+        end
 
         example_request '[error] no JWT token is returned' do
           assert_status 404
         end
       end
 
-      context 'when user has no password' do
-        let!(:user) { create(:user_no_password, email: email) }
+      context 'when the user is an invited user' do
+        let(:email) { 'test@email.com' }
+        let(:password) { '12345678' }
+
+        before do
+          create(:invited_user, email: email, password: password)
+        end
 
         example_request '[error] no JWT token is returned' do
           assert_status 404
+        end
+      end
+
+      context 'when no password is used' do
+        let(:email) { 'test@email.com' }
+        let(:password) { '' }
+
+        context 'when user has password' do
+          let!(:user) { create(:user, email: email, password: 'other_password') }
+
+          example_request '[error] no JWT token is returned' do
+            assert_status 404
+          end
+        end
+
+        context 'when user has no password' do
+          let!(:user) { create(:user_no_password, email: email) }
+
+          example_request '[error] no JWT token is returned' do
+            assert_status 404
+          end
+        end
+      end
+    end
+
+    context 'when user_confirmation is disabled' do
+      before do
+        SettingsService.new.deactivate_feature! 'user_confirmation'
+      end
+
+      context 'when the user is unconfirmed' do
+        let(:email) { 'test@email.com' }
+        let(:password) { '12345678' }
+        let(:remember_me) { false }
+
+        let!(:user) { create(:user_with_confirmation, email: email, password: password) }
+
+        before do
+          allow(Time).to receive(:now).and_return(Time.now)
+        end
+
+        example_request 'Create JWT token creates expected payload' do
+          assert_status 201
+
+          jwt = JWT.decode(json_response_body[:jwt], nil, false).first
+
+          expect(jwt['sub']).to eq(user.id)
+          expect(jwt['highest_role']).to eq('user')
+          expect(jwt['cluster']).to eq('local')
+          expect(jwt['tenant']).to eq(Tenant.current.id)
+          expect(jwt['exp']).to eq((Time.now + 1.day).to_i)
+        end
+      end
+
+      context 'when the user is an invited user' do
+        let(:email) { 'test@email.com' }
+        let(:password) { '12345678' }
+
+        before do
+          create(:invited_user, email: email, password: password)
+        end
+
+        example_request '[error] no JWT token is returned' do
+          assert_status 404
+        end
+      end
+
+      context 'when no password is used' do
+        let(:email) { 'test@email.com' }
+        let(:password) { '' }
+
+        context 'when user has password' do
+          let!(:user) { create(:user, email: email, password: 'other_password') }
+
+          example_request '[error] no JWT token is returned' do
+            assert_status 404
+          end
+        end
+
+        context 'when user has no password' do
+          let!(:user) { create(:user_no_password, email: email) }
+
+          example_request '[error] no JWT token is returned' do
+            assert_status 404
+          end
         end
       end
     end
