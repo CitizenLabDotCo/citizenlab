091e52edcd99556502292ba4160423a2b5d0091e
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-11-05 20:06:25 -0300

    Add test for confirming already confirmed account

--- back/spec/acceptance/confirmations_spec.rb
+++ back/spec/acceptance/confirmations_spec.rb
@@ -92,6 +92,14 @@ resource 'Confirmations' do
           anything
         )
       end
+
+      example 'does not allow confirming a user already confirmed' do
+        code = user.email_confirmation_code
+        do_request(confirmation: { email:, code: })
+        assert_status 200
+        do_request(confirmation: { email:, code: })
+        assert_status 422
+      end
     end
   end
 end
