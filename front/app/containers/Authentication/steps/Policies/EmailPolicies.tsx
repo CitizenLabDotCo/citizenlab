69cb6adffc1eeb55ff4ac0aa1e2f3961b810dd89
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-11-05 11:59:03 -0300

    Add create account message to policies (WIP)

--- front/app/containers/Authentication/steps/Policies/EmailPolicies.tsx
+++ front/app/containers/Authentication/steps/Policies/EmailPolicies.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import { Text, Icon, colors } from '@citizenlab/cl2-component-library';
+
 import useLocale from 'hooks/useLocale';
 
+import { FormattedMessage } from 'utils/cl-intl';
 import { isNilOrError } from 'utils/helperUtils';
 
 import { SetError, State } from '../../typings';
 
+import messages from './messages';
 import PoliciesForm from './PoliciesForm';
 
 interface Props {
@@ -29,7 +33,28 @@ const EmailPolicies = ({ state, loading, setError, onAccept }: Props) => {
     }
   };
 
-  return <PoliciesForm loading={loading} onSubmit={handleSubmit} />;
+  return (
+    <>
+      <Text mt="0px" mb="28px">
+        <Icon
+          width="20px"
+          height="20px"
+          name="user-circle"
+          fill={colors.textSecondary}
+          mr="8px"
+          transform="translate(0,-1)"
+        />
+        <FormattedMessage
+          {...messages.createANewAccountWith}
+          values={{
+            email: <strong>{state.email}</strong>,
+            changeLink: 'TODO',
+          }}
+        />
+      </Text>
+      <PoliciesForm loading={loading} onSubmit={handleSubmit} />
+    </>
+  );
 };
 
 export default EmailPolicies;
--- front/app/containers/Authentication/steps/Policies/messages.ts
+++ front/app/containers/Authentication/steps/Policies/messages.ts
@@ -10,4 +10,8 @@ export default defineMessages({
     defaultMessage:
       "By continuing, you agree to receive emails from this platform. You can select which emails you wish to receive in the 'My Settings' page.",
   },
+  createANewAccountWith: {
+    id: 'app.containers.NewAuthModal.steps.Policies.createANewAccountWith',
+    defaultMessage: 'Create a new account with: {email} {changeLink}',
+  },
 });
