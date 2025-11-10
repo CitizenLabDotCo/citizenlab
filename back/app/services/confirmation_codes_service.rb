7a857acecb65c4f201fe5c8914b985a35d7d9180
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-11-10 20:57:45 -0300

    Remove logging

--- back/app/services/confirmation_codes_service.rb
+++ back/app/services/confirmation_codes_service.rb
@@ -4,33 +4,11 @@ class ConfirmationCodesService
   MAX_RETRIES = ENV.fetch('EMAIL_CONFIRMATION_MAX_RETRIES', 5).to_i
 
   def permit_request_code_unauthenticated(user)
-    Rails.logger.debug '=' * 20
-    Rails.logger.debug 'fajewpiofjareg1'
-    Rails.logger.debug '=' * 20
     return false unless correct_feature_flags_enabled?
-
-    Rails.logger.debug '=' * 20
-    Rails.logger.debug 'fajewpiofjareg2'
-    Rails.logger.debug '=' * 20
     return false if user.nil?
-
-    Rails.logger.debug '=' * 20
-    Rails.logger.debug 'fajewpiofjareg3'
-    Rails.logger.debug '=' * 20
     return false if user.email.blank?
-
-    Rails.logger.debug '=' * 20
-    Rails.logger.debug 'fajewpiofjareg4'
-    Rails.logger.debug '=' * 20
     return false if user.password_digest?
-
-    Rails.logger.debug '=' * 20
-    Rails.logger.debug 'fajewpiofjareg5'
-    Rails.logger.debug '=' * 20
     return false if user.email_confirmation_code_reset_count >= MAX_RETRIES - 1
-
-    Rails.logger.debug '=' * 20
-    Rails.logger.debug '=' * 20
 
     true
   end
