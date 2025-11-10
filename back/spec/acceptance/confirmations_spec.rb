8c9fb46644a05f01f12120b9ffeb28eb32c19c5a
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-11-10 13:59:16 -0300

    Reuse examples

--- back/spec/acceptance/confirmations_spec.rb
+++ back/spec/acceptance/confirmations_spec.rb
@@ -4,11 +4,82 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
 resource 'Confirmations' do
-  explanation 'User can confirm their emails using a 6 digit code'
+  explanation 'User can confirm their emails using a 4 digit code'
 
   before do
     set_api_content_type
     SettingsService.new.activate_feature! 'user_confirmation'
+  end
+
+  # Extract shared examples
+  shared_examples 'confirmation code validation' do
+    example 'returns an ok status passing the right code' do
+      do_request(confirmation: { email: user.email, code: user.email_confirmation_code })
+      assert_status 200
+    end
+
+    example "logs 'completed_registration' activity job when passed the right code" do
+      do_request(confirmation: { email: user.email, code: user.email_confirmation_code })
+      expect(LogActivityJob).to have_been_enqueued.with(
+        user,
+        'completed_registration',
+        user,
+        user.updated_at.to_i
+      ).exactly(1).times
+    end
+
+    example 'returns an auth token when passing the right code' do
+      do_request(confirmation: { email: user.email, code: user.email_confirmation_code })
+      assert_status 200
+      json_response = json_parse response_body
+      expect(json_response[:data][:attributes]).to have_key(:auth_token)
+      token = json_response[:data][:attributes][:auth_token][:token]
+      expect(token[0..2]).to eq 'eyJ' # JWTs start with 'eyJ'
+    end
+
+    example 'returns an code.blank error code when no code is passed' do
+      do_request(confirmation: { email: user.email, code: nil })
+
+      assert_status 422
+      json_response = json_parse response_body
+      expect(json_response).to include_response_error(:code, 'blank')
+    end
+
+    example 'returns an code.invalid error code when the code is invalid' do
+      do_request(confirmation: { email: user.email, code: 'badcode' })
+
+      assert_status 422
+      json_response = json_parse response_body
+      expect(json_response).to include_response_error(:code, 'invalid')
+    end
+
+    example "does not log 'completed_registration' activity when the code is invalid" do
+      ActiveJob::Base.queue_adapter.enqueued_jobs.clear
+
+      do_request(confirmation: { email: user.email, code: 'badcode' })
+
+      expect(LogActivityJob).not_to have_been_enqueued.with(
+        anything,
+        'completed_registration',
+        anything,
+        anything
+      )
+    end
+
+    example 'does not allow confirming a user already confirmed' do
+      code = user.email_confirmation_code
+      do_request(confirmation: { email: user.email, code: code })
+      assert_status 200
+      do_request(confirmation: { email: user.email, code: code })
+      assert_status 422
+    end
+
+    example 'does not allow confirming a user with password' do
+      user_with_password = create(:user_with_confirmation, password: 'password123')
+      code = user_with_password.email_confirmation_code
+      do_request(confirmation: { email: user_with_password.email, code: code })
+      assert_status 422
+    end
   end
 
   post 'web_api/v1/user/confirm_code_unauthenticated' do
@@ -33,80 +104,38 @@ resource 'Confirmations' do
 
     context 'when email exists' do
       let(:user) { create(:user_with_confirmation, password: nil) }
-      let(:email) { user.email }
 
       before do
         RequestConfirmationCodeJob.perform_now user
       end
 
-      example 'returns an ok status passing the right code' do
-        do_request(confirmation: { email:, code: user.email_confirmation_code })
-        assert_status 200
+      include_examples 'confirmation code validation'
+    end
+  end
+
+  post 'web_api/v1/user/confirm_code_authenticated' do
+    with_options scope: :confirmation do
+      parameter :code, 'The 4-digit confirmation code received by email.'
+    end
+
+    context 'when user is not authenticated' do
+      let(:user) { create(:user_with_confirmation, password: 'password123') }
+
+      example 'returns an unauthorized status when the user is not authenticated' do
+        do_request(confirmation: { code: '1234' })
+        expect(status).to eq 401
+      end
+    end
+
+    context 'when user is authenticated' do
+      let(:user) { create(:user_with_confirmation, password: 'password123') }
+      
+      before do
+        header_token_for user
+        RequestConfirmationCodeJob.perform_now user
       end
 
-      example "logs 'completed_registration' activity job when passed the right code" do
-        do_request(confirmation: { email:, code: user.email_confirmation_code })
-        expect(LogActivityJob).to have_been_enqueued.with(
-          user,
-          'completed_registration',
-          user,
-          user.updated_at.to_i
-        ).exactly(1).times
-      end
-
-      example 'returns an auth token when passing the right code' do
-        do_request(confirmation: { email:, code: user.email_confirmation_code })
-        assert_status 200
-        json_response = json_parse response_body
-        expect(json_response[:data][:attributes]).to have_key(:auth_token)
-        token = json_response[:data][:attributes][:auth_token][:token]
-        expect(token[0..2]).to eq 'eyJ' # JWTs start with 'eyJ'
-      end
-
-      example 'returns an code.blank error code when no code is passed' do
-        do_request(confirmation: { email:, code: nil })
-
-        assert_status 422
-        json_response = json_parse response_body
-        expect(json_response).to include_response_error(:code, 'blank')
-      end
-
-      example 'returns an code.invalid error code when the code is invalid' do
-        do_request(confirmation: { email:, code: 'badcode' })
-
-        assert_status 422
-        json_response = json_parse response_body
-        expect(json_response).to include_response_error(:code, 'invalid')
-      end
-
-      example "does not log 'completed_registration' activity when the code is invalid" do
-        # Clear any previously enqueued jobs
-        ActiveJob::Base.queue_adapter.enqueued_jobs.clear
-
-        do_request(confirmation: { email:, code: 'badcode' })
-
-        expect(LogActivityJob).not_to have_been_enqueued.with(
-          anything,
-          'completed_registration',
-          anything,
-          anything
-        )
-      end
-
-      example 'does not allow confirming a user already confirmed' do
-        code = user.email_confirmation_code
-        do_request(confirmation: { email:, code: })
-        assert_status 200
-        do_request(confirmation: { email:, code: })
-        assert_status 422
-      end
-
-      example 'does not allow confirming a user with password' do
-        user = create(:user_with_confirmation, password: 'password123')
-        code = user.email_confirmation_code
-        do_request(confirmation: { email:, code: })
-        assert_status 422
-      end
+      include_examples 'confirmation code validation'
     end
   end
 end
