2796d19a19d792ce0cd331efa6246d7d9a11d2ec
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-11-10 17:12:06 -0300

    Remove comments confirmations controller

--- back/app/controllers/web_api/v1/confirmations_controller.rb
+++ back/app/controllers/web_api/v1/confirmations_controller.rb
@@ -89,29 +89,4 @@ class WebApi::V1::ConfirmationsController < ApplicationController
   def confirmation_codes_service
     @confirmation_codes_service ||= ConfirmationCodesService.new
   end
-
-  # skip_before_action :authenticate_user
-  # skip_after_action :verify_authorized
-
-  # def create
-  #   user = User.find_by(email: confirmation_params[:email])
-  #   result = ConfirmUser.call(user:, code: confirmation_params[:code])
-
-  #   if result.success?
-  #     SideFxUserService.new.after_update(user, user)
-
-  #     payload = user.to_token_payload
-  #     auth_token = AuthToken::AuthToken.new payload: payload
-
-  #     render json: raw_json({ auth_token: })
-  #   else
-  #     render json: { errors: result.errors.details }, status: :unprocessable_entity
-  #   end
-  # end
-
-  # private
-
-  # def confirmation_params
-  #   params.require(:confirmation).permit(:email, :code)
-  # end
 end
