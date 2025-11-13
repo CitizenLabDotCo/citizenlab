78b67f8ebf59cb94e222b28f358242e16d464d6b
Adrien Dessy <209268+adessy@users.noreply.github.com>
2025-11-13 19:03:24 +0100

    Reapply "[TAN-5588] Fix rubocop offenses"
    This reverts commit a05ad892d59014481411f182894c539dfab56bbd.

--- back/app/models/concerns/imageable.rb
+++ back/app/models/concerns/imageable.rb
@@ -2,7 +2,7 @@ module Imageable
   extend ActiveSupport::Concern
 
   class_methods do
-    def has_many_text_images(from:, as: :text_images)
+    def has_many_text_images(from:, as: :text_images) # rubocop:disable Naming/PredicateName
       TextImageService.setup_image_extraction(self, from, as)
     end
   end
