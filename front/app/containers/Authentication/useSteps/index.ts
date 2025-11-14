1b113480a03a73b5bdb9f9afbce3f3fa44fa93aa
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-11-14 19:22:03 -0300

    On return from SSO: set email if available

--- front/app/containers/Authentication/useSteps/index.ts
+++ front/app/containers/Authentication/useSteps/index.ts
@@ -320,7 +320,7 @@ export default function useSteps() {
       };
 
       const flow = sso_flow ?? 'signin';
-      updateState({ flow });
+      updateState({ flow, email: authUser.data.attributes.email ?? null });
       transition(currentStep, 'RESUME_FLOW_AFTER_SSO')(flow);
 
       // Check that the path is the same as the one stored in local storage
