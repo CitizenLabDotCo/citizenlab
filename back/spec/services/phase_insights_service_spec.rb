de06787c2ba9869c592bd2faad1f9cada78c31d8
jinjagit <jinjamail@gmail.com>
2025-11-18 17:09:46 +0000

    [TAN-5782] Linting

--- back/spec/services/phase_insights_service_spec.rb
+++ back/spec/services/phase_insights_service_spec.rb
@@ -3,18 +3,17 @@ require 'rails_helper'
 describe PhaseInsightsService do
   let(:service) { described_class.new }
 
-  let(:participation) { create(:basket_participation, :with_votes, vote_count: 15)}
+  let(:participation) { create(:basket_participation, :with_votes, vote_count: 15) }
 
   it 'does a thing' do
-    expect(service).to be_a(PhaseInsightsService)
     expect(participation).to match({
-      id:  be_a(String),
+      id: be_a(String),
       action: "voting",
       acted_at: be_a(Time),
       classname: "Basket",
       user_id: be_a(String),
       user_custom_field_values: {},
       votes: 15
-  })
+    })
   end
 end
