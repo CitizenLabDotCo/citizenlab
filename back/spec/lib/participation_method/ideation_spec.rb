ac6cbda3ba1d468ae9ae52c1837087e279585081
Adrien Dessy <209268+adessy@users.noreply.github.com>
2025-11-19 14:52:05 +0100

    [TAN-5742] Add account-less participation for ideation
    Enable the "None" authentication option for ideation and proposals
    phases, allowing users to post ideas without creating an account.

--- back/spec/lib/participation_method/ideation_spec.rb
+++ back/spec/lib/participation_method/ideation_spec.rb
@@ -228,9 +228,9 @@ RSpec.describe ParticipationMethod::Ideation do
   its(:supports_edits_after_publication?) { is_expected.to be true }
   its(:supports_exports?) { is_expected.to be true }
   its(:supports_input_term?) { is_expected.to be true }
-  its(:supports_inputs_without_author?) { is_expected.to be false }
+  its(:supports_inputs_without_author?) { is_expected.to be true }
   its(:allow_posting_again_after) { is_expected.to eq 0.seconds }
-  its(:supports_permitted_by_everyone?) { is_expected.to be false }
+  its(:supports_permitted_by_everyone?) { is_expected.to be true }
   its(:supports_public_visibility?) { is_expected.to be true }
   its(:supports_status?) { is_expected.to be true }
   its(:supports_submission?) { is_expected.to be true }
