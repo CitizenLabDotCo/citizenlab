717c7ef0a3ba23b75445b87d3e4a861016511362
jinjagit <jinjamail@gmail.com>
2025-11-25 15:51:05 +0000

    [TAN-5782] Add tests to participation_method/proposals_spec

--- back/spec/lib/participation_method/proposals_spec.rb
+++ back/spec/lib/participation_method/proposals_spec.rb
@@ -220,6 +220,93 @@ RSpec.describe ParticipationMethod::Proposals do
     end
   end
 
+  describe '#participation_ideas_submitted' do
+    let(:user1) { create(:user) }
+    let!(:idea1) { create(:idea, phases: [phase], created_at: 20.days.ago, published_at: 20.days.ago, author: user1, creation_phase_id: phase.id) } # before phase start
+    let!(:idea2) { create(:idea, phases: [phase], created_at: 10.days.ago, published_at: 10.days.ago, author: user1, creation_phase_id: phase.id) } # during phase
+    let!(:idea3) { create(:idea, phases: [phase], created_at: 1.day.ago, published_at: 1.day.ago, author: user1, creation_phase_id: phase.id) } # after phase end
+
+    let(:user2) { create(:user) }
+    let!(:idea4) { create(:idea, phases: [phase], created_at: 10.days.ago, published_at: nil, publication_status: 'submitted', author: user2, creation_phase_id: phase.id) } # during phase, submitted but not published
+    let!(:idea5) { create(:idea, phases: [phase], created_at: 10.days.ago, published_at: nil, author: user2, publication_status: 'draft', creation_phase_id: phase.id) } # during phase, but not submitted nor published
+
+    let!(:idea6) { create(:idea, phases: [phase], created_at: 10.days.ago, published_at: 10.days.ago, author: nil, author_hash: 'some_author_hash', creation_phase_id: phase.id) } # during phase, no author (e.g. anonymous participation)
+    let!(:idea7) { create(:idea, phases: [phase], created_at: 10.days.ago, published_at: 10.days.ago, author: nil, author_hash: nil, creation_phase_id: phase.id) } # during phase, no author nor author_hash (e.g. imported idea)
+    before { phase.update!(start_at: 15.days.ago, end_at: 2.days.ago) }
+
+    it 'returns the participation ideas published data for published ideas published during phase' do
+      participation_ideas_submitted = participation_method.send(:participation_ideas_submitted)
+
+      expect(participation_ideas_submitted).to match_array([
+        {
+          item_id: idea2.id,
+          action: 'posting_idea',
+          acted_at: a_kind_of(Time),
+          classname: 'Idea',
+          participant_id: user1.id,
+          user_custom_field_values: {}
+        },
+        {
+          item_id: idea4.id,
+          action: 'posting_idea',
+          acted_at: a_kind_of(Time),
+          classname: 'Idea',
+          participant_id: user2.id,
+          user_custom_field_values: {}
+        },
+        {
+          item_id: idea6.id,
+          action: 'posting_idea',
+          acted_at: a_kind_of(Time),
+          classname: 'Idea',
+          participant_id: 'some_author_hash',
+          user_custom_field_values: {}
+        },
+        {
+          item_id: idea7.id,
+          action: 'posting_idea',
+          acted_at: a_kind_of(Time),
+          classname: 'Idea',
+          participant_id: idea7.id,
+          user_custom_field_values: {}
+        }
+      ])
+
+      first_participation = participation_ideas_submitted.first
+      expect(first_participation[:acted_at])
+        .to be_within(1.second).of(Idea.find(first_participation[:item_id]).submitted_at)
+    end
+
+    it 'correctly handles phases with no end date' do
+      phase.update!(end_at: nil)
+      participation_ideas_submitted = participation_method.send(:participation_ideas_submitted)
+
+      expect(participation_ideas_submitted.pluck(:item_id)).to match_array([
+        idea2.id,
+        idea3.id,
+        idea4.id,
+        idea6.id,
+        idea7.id
+      ])
+    end
+
+    it 'does not include ideas that are not submitted' do
+      participation_ideas_submitted = participation_method.send(:participation_ideas_submitted)
+
+      idea_ids = participation_ideas_submitted.map { |p| p[:item_id] }
+      expect(idea_ids).not_to include(idea5.id)
+    end
+
+    it 'does not include transitive ideas' do
+      idea2.creation_phase_id = nil
+      idea2.save!(validate: false) # skip validations to allow non-transitive idea
+      participation_ideas_submitted = participation_method.send(:participation_ideas_submitted)
+
+      idea_ids = participation_ideas_submitted.map { |p| p[:item_id] }
+      expect(idea_ids).not_to include(idea2.id)
+    end
+  end
+
   its(:additional_export_columns) { is_expected.to eq %w[manual_votes] }
   its(:allowed_ideas_orders) { is_expected.to eq %w[trending random popular -new new comments_count] }
   its(:return_disabled_actions?) { is_expected.to be false }
