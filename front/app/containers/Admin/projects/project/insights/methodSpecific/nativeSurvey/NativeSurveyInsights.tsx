c9de8a199be25ce6b43d4e85ff01cef64b0714a6
EdwinKato <katedwin88@gmail.com>
2025-11-26 16:26:07 +0300

    Simplify NativeSurveyInsights to use FormResults with PDF context

--- front/app/containers/Admin/projects/project/insights/methodSpecific/nativeSurvey/NativeSurveyInsights.tsx
+++ front/app/containers/Admin/projects/project/insights/methodSpecific/nativeSurvey/NativeSurveyInsights.tsx
@@ -1,41 +1,26 @@
 import React from 'react';
 
-import { Box, Title } from 'component-library';
-
 import usePhase from 'api/phases/usePhase';
 
 import FormResults from 'components/admin/FormResults';
 
-import { FormattedMessage } from 'utils/cl-intl';
-
-import messages from '../../messages';
+import { usePdfExportContext } from '../../PdfExportContext';
 import { MethodSpecificInsightProps } from '../types';
 
 const NativeSurveyInsights = ({ phaseId }: MethodSpecificInsightProps) => {
   const { data: phase } = usePhase(phaseId);
+  const { isPdfExport } = usePdfExportContext();
 
   if (!phase) {
     return null;
   }
 
   return (
-    <Box
-      background="white"
-      borderRadius="3px"
-      py="24px"
-      display="flex"
-      flexDirection="column"
-      gap="16px"
-    >
-      <Title variant="h3" as="h2" color="textPrimary" m="0px">
-        <FormattedMessage {...messages.questions} />
-      </Title>
-
-      <FormResults
-        projectId={phase.data.relationships.project.data.id}
-        phaseId={phase.data.id}
-      />
-    </Box>
+    <FormResults
+      projectId={phase.data.relationships.project.data.id}
+      phaseId={phase.data.id}
+      isPdfExport={isPdfExport}
+    />
   );
 };
 
