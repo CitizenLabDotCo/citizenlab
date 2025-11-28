eb5a84668875c42a140f0d7615cb911671d06a70
AchrafGoVocal <achraf@govocal.com>
2025-11-28 17:28:07 +0100

    [TAN-5839] Remove unused exports from ReportAdminPublicationSelector

--- front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/_shared/ReportAdminPublicationSelector/index.ts
+++ front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/_shared/ReportAdminPublicationSelector/index.ts
@@ -1,4 +1,2 @@
 export { default as ReportAdminPublicationSearchInput } from './ReportAdminPublicationSearchInput';
 export { default as ReportAdminPublicationList } from './ReportAdminPublicationList';
-export { default as ReportAdminPublicationOptionLabel } from './ReportAdminPublicationOptionLabel';
-export * from './utils';
