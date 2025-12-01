a5350846bbec1f0b79658002aef63d7e20109344
Adrien Dessy <209268+adessy@users.noreply.github.com>
2025-12-01 22:49:59 +0100

    [TAN-5945] Add EmailBan serializer
    Replace manual JSON rendering with proper JSONAPI serializer.

--- back/app/controllers/web_api/v1/email_bans_controller.rb
+++ back/app/controllers/web_api/v1/email_bans_controller.rb
@@ -2,35 +2,28 @@
 
 class WebApi::V1::EmailBansController < ApplicationController
   def count
-    authorize :email_ban
+    authorize(:email_ban)
     render json: raw_json({ count: EmailBan.distinct.count(:email_hash) })
   end
 
   # GET /email_bans/:email
   def show
-    authorize :email_ban
+    authorize(:email_ban)
     ban = EmailBan.find_for(params[:email])
-    if ban
-      render json: raw_json({
-        id: ban.id,
-        reason: ban.reason,
-        created_at: ban.created_at,
-        banned_by_id: ban.banned_by_id
-      })
-    else
-      head :not_found
-    end
+    return head :not_found unless ban
+
+    render json: WebApi::V1::EmailBanSerializer
+      .new(ban, params: jsonapi_serializer_params)
+      .serializable_hash
   end
 
   # DELETE /email_bans/:email
   def destroy
-    authorize :email_ban
+    authorize(:email_ban)
     ban = EmailBan.find_for(params[:email])
-    if ban
-      ban.destroy!
-      head :ok
-    else
-      head :not_found
-    end
+    return head :not_found unless ban
+
+    ban.destroy!
+    head :ok
   end
 end
