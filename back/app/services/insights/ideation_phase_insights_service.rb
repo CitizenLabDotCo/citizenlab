2cd7594b06122c72f3ef42e756c330192e900519
jinjagit <jinjamail@gmail.com>
2025-12-03 10:58:58 +0000

    [TAN-5782] Add consistently use created_at for ideas posted acted_at

--- back/app/services/insights/ideation_phase_insights_service.rb
+++ back/app/services/insights/ideation_phase_insights_service.rb
@@ -26,7 +26,7 @@ module Insights
         {
           item_id: idea.id,
           action: 'posting_idea',
-          acted_at: idea.published_at, # analytics_fact_participations uses created_at, so maybe we should use that here too?
+          acted_at: idea.created_at,
           classname: 'Idea',
           participant_id: participant_id(idea.id, idea.author_id, idea.author_hash),
           user_custom_field_values: idea&.author&.custom_field_values || {}
