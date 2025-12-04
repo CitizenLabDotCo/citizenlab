388893d5be6555adf24b9c306cca4ae9aeb7611d
jinjagit <jinjamail@gmail.com>
2025-12-04 10:45:49 +0000

    [TAN-5975] Linting

--- back/spec/acceptance/phase_insights/common_ground_phase_insights_spec.rb
+++ back/spec/acceptance/phase_insights/common_ground_phase_insights_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
 resource 'Phase insights' do
-  before { admin_header_token }
+  before do
+    admin_header_token
+    # This reference time means we can expect exact dates in the chart data
+    travel_to(Time.zone.parse('2025-12-02 12:00:00'))
+  end
 
   let!(:custom_field_gender) { create(:custom_field, resource_type: 'User', key: 'gender', input_type: 'select', title_multiloc: { en: 'Gender' }) }
   let!(:custom_field_option_male) { create(:custom_field_option, custom_field: custom_field_gender, key: 'male', title_multiloc: { en: 'Male' }) }
@@ -26,13 +30,6 @@ resource 'Phase insights' do
       bins: [18, 25, 35, 45, 55, 65, nil], # Age ranges: <18, 18-25, 25-35, 35-45, 45-65, >65
       counts: [50, 200, 400, 300, 50, 700] # Population in each bin
     )
-  end
-
-  around do |example|
-    # This reference time means we can expect exact dates in the chart data
-    travel_to(Time.zone.parse('2025-12-02 12:00:00')) do
-      example.run
-    end
   end
 
   let(:common_ground_phase) do
