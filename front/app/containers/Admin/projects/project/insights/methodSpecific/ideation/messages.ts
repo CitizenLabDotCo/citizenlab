a824ffa2ae82813905587c80535f2f9c0330be4d
EdwinKato <katedwin88@gmail.com>
2025-12-04 10:06:45 +0300

    Add messages

---
+++ front/app/containers/Admin/projects/project/insights/methodSpecific/ideation/messages.ts
@@ -0,0 +1,33 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  whatArePeopleSaying: {
+    id: 'app.containers.Admin.projects.project.insights.ideation.whatArePeopleSaying',
+    defaultMessage: 'What are people saying?',
+  },
+  refresh: {
+    id: 'app.containers.Admin.projects.project.insights.ideation.refresh',
+    defaultMessage: 'Refresh',
+  },
+  explore: {
+    id: 'app.containers.Admin.projects.project.insights.ideation.explore',
+    defaultMessage: 'Explore',
+  },
+  loading: {
+    id: 'app.containers.Admin.projects.project.insights.ideation.loading',
+    defaultMessage: 'Loading...',
+  },
+  generatingSummary: {
+    id: 'app.containers.Admin.projects.project.insights.ideation.generatingSummary',
+    defaultMessage: 'Generating summary...',
+  },
+  inputsLabel: {
+    id: 'app.containers.Admin.projects.project.insights.ideation.inputsLabel',
+    defaultMessage: 'inputs',
+  },
+  newResponses: {
+    id: 'app.containers.Admin.projects.project.insights.ideation.newResponses',
+    defaultMessage:
+      '{count, plural, one {# new response} other {# new responses}}',
+  },
+});
