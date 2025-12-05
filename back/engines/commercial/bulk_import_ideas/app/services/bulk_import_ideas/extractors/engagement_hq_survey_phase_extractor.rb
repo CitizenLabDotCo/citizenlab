8c628042e8c86ac8197086eee1ad6cb3aed4c695
jamesspeake <james@speake.org>
2025-12-05 17:33:17 +0000

    [TAN-5920] Fix bug with missing engagement HQ imports

--- back/engines/commercial/bulk_import_ideas/app/services/bulk_import_ideas/extractors/engagement_hq_survey_phase_extractor.rb
+++ back/engines/commercial/bulk_import_ideas/app/services/bulk_import_ideas/extractors/engagement_hq_survey_phase_extractor.rb
@@ -12,7 +12,7 @@ module BulkImportIdeas::Extractors
 
     def ideas_row_range
       # The last row is the first row where the date is empty in the 'Date Published (dd-mm-yyyy)' column (index 3)
-      last_row = @worksheet.drop(4).each_with_index.find { |row, _row_index| row[3].nil? || row[3].value.blank? }&.last
+      last_row = @worksheet.each_with_index.find { |row, index| index > 3 && (row[3].nil? || row[3].value.blank?) }&.last
 
       # Skip the first 4 rows which are headers and metadata
       [4, last_row]
