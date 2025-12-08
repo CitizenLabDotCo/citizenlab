5cbe79e06ad6bae173fdb4371851784ca353f312
Amanda Anderson <anderson_amanda@mail.com>
2025-12-08 14:10:22 +0100

    Fix unit test

--- front/app/components/admin/ContentBuilder/Widgets/FileAttachment/utils.test.ts
+++ front/app/components/admin/ContentBuilder/Widgets/FileAttachment/utils.test.ts
@@ -1,73 +1,52 @@
-import { getUsedFileIds, getFileUsageCount } from '../utils';
+import { SerializedNodes } from '@craftjs/core';
+
+import { getFileUsageCount } from './utils';
 
 describe('FileAttachment utils', () => {
-  describe('getUsedFileIds', () => {
-    it('extracts fileIds from FileAttachment widgets', () => {
-      const craftjsJson = {
-        ROOT: {
-          type: { resolvedName: 'Container' },
-          nodes: ['file1', 'file2'],
-        },
-        file1: {
-          type: { resolvedName: 'FileAttachment' },
-          props: { fileId: 'file-123' },
-        },
-        file2: {
-          type: { resolvedName: 'FileAttachment' },
-          props: { fileId: 'file-456' },
-        },
-        other: {
-          type: { resolvedName: 'Text' },
-          props: { text: 'Some text' },
-        },
-      };
-
-      const result = getUsedFileIds(craftjsJson);
-      expect(result).toEqual(new Set(['file-123', 'file-456']));
-    });
-
-    it('handles widgets without fileId', () => {
-      const craftjsJson = {
-        ROOT: {
-          type: { resolvedName: 'Container' },
-          nodes: ['file1'],
-        },
-        file1: {
-          type: { resolvedName: 'FileAttachment' },
-          props: {}, // No fileId
-        },
-      };
-
-      const result = getUsedFileIds(craftjsJson);
-      expect(result).toEqual(new Set([]));
-    });
-
-    it('handles empty craftjs JSON', () => {
-      const result = getUsedFileIds({});
-      expect(result).toEqual(new Set([]));
-    });
-  });
-
   describe('getFileUsageCount', () => {
     it('counts how many times a file is used', () => {
       const craftjsJson = {
         ROOT: {
           type: { resolvedName: 'Container' },
+          props: {},
+          displayName: 'Container',
+          isCanvas: true,
+          parent: null,
           nodes: ['file1', 'file2', 'file3'],
+          hidden: false,
+          linkedNodes: {},
         },
         file1: {
           type: { resolvedName: 'FileAttachment' },
           props: { fileId: 'file-123' },
+          displayName: 'FileAttachment',
+          isCanvas: false,
+          parent: 'ROOT',
+          nodes: [],
+          hidden: false,
+          linkedNodes: {},
         },
         file2: {
           type: { resolvedName: 'FileAttachment' },
           props: { fileId: 'file-123' }, // Same file
+          displayName: 'FileAttachment',
+          isCanvas: false,
+          parent: 'ROOT',
+          nodes: [],
+          hidden: false,
+          linkedNodes: {},
         },
         file3: {
           type: { resolvedName: 'FileAttachment' },
           props: { fileId: 'file-456' }, // Different file
+          displayName: 'FileAttachment',
+          isCanvas: false,
+          parent: 'ROOT',
+          nodes: [],
+          hidden: false,
+          linkedNodes: {},
         },
-      };
+      } as SerializedNodes;
 
       expect(getFileUsageCount(craftjsJson, 'file-123')).toBe(2);
       expect(getFileUsageCount(craftjsJson, 'file-456')).toBe(1);
