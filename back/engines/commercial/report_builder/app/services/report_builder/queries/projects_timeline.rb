1cf4af8bbac2baffa1c8cc619e2a4f06927a832e
AchrafGoVocal <achraf@govocal.com>
2025-12-08 11:11:49 +0100

    [TAN-5840] Fix undefined method

--- back/engines/commercial/report_builder/app/services/report_builder/queries/projects_timeline.rb
+++ back/engines/commercial/report_builder/app/services/report_builder/queries/projects_timeline.rb
@@ -59,7 +59,7 @@ module ReportBuilder
         {
           id: project.id,
           title: project.title_multiloc,
-          start_date: first_phase&.start_at || project.first_published_at,
+          start_date: first_phase&.start_at || project.admin_publication.first_published_at,
           end_date: last_phase&.end_at,
           current_phase_start_date: current_phase&.start_at,
           current_phase_end_date: current_phase&.end_at,
