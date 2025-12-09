c271a75b0191a83e08bd4dd00e3d08043be6a90e
EdwinKato <katedwin88@gmail.com>
2025-12-09 07:37:49 +0300

    Add messages for proposal status and submission notifications

---
+++ front/app/containers/Admin/projects/project/insights/methodSpecific/proposals/messages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  proposalStatus: {
+    id: 'app.containers.Admin.projects.project.insights.proposalStatus',
+    defaultMessage: 'Proposal Status',
+  },
+  noProposalsSubmitted: {
+    id: 'app.containers.Admin.projects.project.insights.noProposalsSubmitted',
+    defaultMessage: 'No proposals have been submitted yet.',
+  },
+});
