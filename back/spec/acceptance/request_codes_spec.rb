23aa26f5356a40cad7edcb4eaad1d25e51dfe8fd
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-12-10 11:36:06 -0300

    Add status codes to request_codes_spec

--- back/spec/acceptance/request_codes_spec.rb
+++ back/spec/acceptance/request_codes_spec.rb
@@ -21,18 +21,21 @@ resource 'Request codes' do
     example 'It works with a passwordless user' do
       user = create(:user_no_password)
       do_request(request_code: { email: user.email })
+      expect(response_status).to eq 200
       expect(RequestConfirmationCodeJob).to have_received(:perform_now).with(user).once
     end
 
     example 'It does not work with a user with password' do
       user = create(:user)
       do_request(request_code: { email: user.email })
+      expect(response_status).to eq 200
       expect(RequestConfirmationCodeJob).not_to have_received(:perform_now)
     end
 
     example 'It does not work if user reached email_confirmation_code_reset_count' do
       user = create(:user_no_password, email_confirmation_code_reset_count: 4)
       do_request(request_code: { email: user.email })
+      expect(response_status).to eq 200
       expect(RequestConfirmationCodeJob).not_to have_received(:perform_now)
     end
 
@@ -40,6 +43,7 @@ resource 'Request codes' do
       user = create(:user_no_password, new_email: 'new@email.com')
       expect(user.new_email).to eq 'new@email.com'
       do_request(request_code: { email: user.email })
+      expect(response_status).to eq 200
       expect(user.reload.new_email).to be_nil
     end
   end
@@ -53,6 +57,7 @@ resource 'Request codes' do
       user = create(:user_with_confirmation)
       header_token_for(user)
       do_request
+      expect(response_status).to eq 200
       expect(RequestConfirmationCodeJob).to have_received(:perform_now).with(user).once
     end
 
@@ -60,6 +65,7 @@ resource 'Request codes' do
       user = create(:user)
       header_token_for(user)
       do_request
+      expect(response_status).to eq 200
       expect(RequestConfirmationCodeJob).not_to have_received(:perform_now)
     end
 
@@ -67,6 +73,7 @@ resource 'Request codes' do
       user = create(:user_no_password)
       header_token_for(user)
       do_request
+      expect(response_status).to eq 200
       expect(RequestConfirmationCodeJob).to have_received(:perform_now).with(user).once
     end
 
@@ -74,6 +81,7 @@ resource 'Request codes' do
       user = create(:user_with_confirmation, email_confirmation_code_reset_count: 4)
       header_token_for(user)
       do_request
+      expect(response_status).to eq 200
       expect(RequestConfirmationCodeJob).not_to have_received(:perform_now)
     end
 
@@ -82,6 +90,7 @@ resource 'Request codes' do
       user.update_column(:email, nil)
       header_token_for(user)
       do_request
+      expect(response_status).to eq 200
       expect(RequestConfirmationCodeJob).not_to have_received(:perform_now)
     end
   end
@@ -99,6 +108,7 @@ resource 'Request codes' do
       user = create(:user)
       header_token_for(user)
       do_request(request_code: { new_email: 'new_email@example.com' })
+      expect(response_status).to eq 200
       expect(RequestConfirmationCodeJob).to have_received(:perform_now)
         .with(user, new_email: 'new_email@example.com').once
     end
@@ -107,6 +117,7 @@ resource 'Request codes' do
       user = create(:user)
       header_token_for(user)
       do_request(request_code: { new_email: '' })
+      expect(response_status).to eq 200
       expect(RequestConfirmationCodeJob).not_to have_received(:perform_now)
     end
 
@@ -114,6 +125,7 @@ resource 'Request codes' do
       user = create(:user, email_confirmation_code_reset_count: 4)
       header_token_for(user)
       do_request(request_code: { new_email: 'new_email@example.com' })
+      expect(response_status).to eq 200
       expect(RequestConfirmationCodeJob).not_to have_received(:perform_now)
     end
   end
