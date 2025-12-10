de19800049d42744da2827551d8f7a5915e24fed
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-12-10 11:45:44 -0300

    Make sure we explicitly pass flow in emailFlow

--- front/app/containers/Authentication/useSteps/stepConfig/emailFlow.ts
+++ front/app/containers/Authentication/useSteps/stepConfig/emailFlow.ts
@@ -64,7 +64,7 @@ export const emailFlow = (
             const missingDataStep = checkMissingData(
               requirements,
               authenticationData,
-              state.flow,
+              'signin',
               true
             );
 
