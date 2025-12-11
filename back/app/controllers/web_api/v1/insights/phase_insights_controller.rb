19df470a6918954be23f2acb0a96d55c6956a1c7
jinjagit <jinjamail@gmail.com>
2025-12-11 14:27:55 +0000

    [TAN-6016] Improve error logic for controller action

--- back/app/controllers/web_api/v1/insights/phase_insights_controller.rb
+++ back/app/controllers/web_api/v1/insights/phase_insights_controller.rb
@@ -15,14 +15,14 @@ module WebApi
         end
 
         def votes_with_grouping
-          unless @phase.participation_method == 'voting'
-            render json: { errors: { phase: [{ error: 'Not a voting phase' }] } }, status: :unprocessable_entity
+          group_by = params.permit(:group_by)[:group_by]
+          custom_field = group_by.present? ? CustomField.find_by(key: group_by) : nil
+          error = validate_voting_phase_and_custom_field(group_by, custom_field)
+          
+          if error
+            render json: { errors: error }, status: :unprocessable_entity
             return
           end
-
-          group_by = params.permit(:group_by)[:group_by]
-          custom_field = validate_and_find_custom_field(group_by)
-          return if performed? # Stop if custom_field validation rendered an error
 
           counts_data = @phase.pmethod.phase_insights_class.new(@phase).vote_counts_with_user_custom_field_grouping(custom_field)
 
@@ -39,26 +39,20 @@ module WebApi
           authorize @phase
         end
 
-        def validate_and_find_custom_field(group_by)
+        def validate_voting_phase_and_custom_field(group_by, custom_field)
+          unless @phase.participation_method == 'voting'
+            return { phase: [{ error: 'Not a voting phase' }] }
+          end
+
           return nil if group_by.blank?
 
-          custom_field = CustomField.find_by(key: group_by)
-          error = nil
-
           if custom_field.nil?
-            error = 'custom_field not found with the key provided'
+            { group_by: [{ error: 'custom_field not found with the key provided' }] }
           elsif custom_field.resource_type != 'User'
-            error = 'Invalid custom_field resource_type for grouping'
+            { group_by: [{ error: 'Invalid custom_field resource_type for grouping' }] }
           elsif !custom_field.support_reference_distribution? && custom_field.key != 'birthyear'
-            error = 'Custom field input_type or key not supported for grouping'
+            { group_by: [{ error: 'Custom field input_type or key not supported for grouping' }] }
           end
-
-          if error
-            render json: { errors: { group_by: [{ error: error }] } }, status: :unprocessable_entity
-            return nil
-          end
-
-          custom_field
         end
       end
     end
