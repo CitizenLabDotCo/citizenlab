122871ca0a31922f73a3f226f7795e03ac65113b
EdwinKato <katedwin88@gmail.com>
2025-12-11 14:33:18 +0300

    Refactor VotingIdeaRow to simplify component structure and remove clustering logic

--- front/app/containers/Admin/projects/project/insights/methodSpecific/voting/VotingIdeaRow.tsx
+++ front/app/containers/Admin/projects/project/insights/methodSpecific/voting/VotingIdeaRow.tsx
@@ -6,49 +6,26 @@ import useLocalize from 'hooks/useLocalize';
 
 import { useIntl } from 'utils/cl-intl';
 
-import ClusteredVotingRow from './ClusteredVotingRow';
 import messages from './messages';
 import SimpleVotingRow from './SimpleVotingRow';
 
 interface Props {
   idea: VotingIdeaResult;
   maxVotes: number;
-  clusterBy?: string;
-  demographicKeys?: string[];
-  demographicLabels?: string[];
 }
 
-const VotingIdeaRow = ({
-  idea,
-  maxVotes,
-  clusterBy,
-  demographicKeys,
-  demographicLabels,
-}: Props) => {
+const VotingIdeaRow = ({ idea, maxVotes }: Props) => {
   const localize = useLocalize();
   const { formatMessage } = useIntl();
 
   const title = localize(idea.title_multiloc);
-  const isClustered =
-    clusterBy && demographicKeys && demographicKeys.length > 0;
-
-  if (!isClustered) {
-    return (
-      <SimpleVotingRow
-        idea={idea}
-        title={title}
-        maxVotes={maxVotes}
-        tooltip={formatMessage(messages.votesTooltip)}
-      />
-    );
-  }
 
   return (
-    <ClusteredVotingRow
+    <SimpleVotingRow
       idea={idea}
       title={title}
-      demographicKeys={demographicKeys}
-      demographicLabels={demographicLabels}
+      maxVotes={maxVotes}
+      tooltip={formatMessage(messages.votesTooltip)}
     />
   );
 };
