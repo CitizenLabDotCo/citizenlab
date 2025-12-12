5041c05aa6b670fc0b2ec71a0e5f033523da5d1e
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-12-12 12:14:21 -0300

    Add tests for unauth

--- back/spec/services/user_confirmation_service_spec.rb
+++ back/spec/services/user_confirmation_service_spec.rb
@@ -5,14 +5,76 @@ require 'rails_helper'
 RSpec.describe UserConfirmationService do
   subject(:service) { described_class.new }
 
-  let(:user) { create(:user_with_confirmation) }
-
   before do
     SettingsService.new.activate_feature! 'user_confirmation'
     RequestConfirmationCodeJob.perform_now user
   end
 
+  describe '#validate_and_confirm_unauthenticated!' do
+    before do
+      SettingsService.new.activate_feature! 'password_login'
+    end
+
+    let(:user) { create(:user_no_password) }
+
+    context 'when the user is nil' do
+      it 'returns a user blank error' do
+        result = service.validate_and_confirm_unauthenticated!(nil, '1234')
+
+        expect(result.success?).to be false
+        expect(result.errors.details).to eq({ user: [{ error: :blank }] })
+      end
+    end
+
+    context 'when the code is nil' do
+      it 'returns a code blank error' do
+        result = service.validate_and_confirm_unauthenticated!(user, nil)
+
+        expect(result.success?).to be false
+        expect(result.errors.details).to eq({ code: [{ error: :blank }] })
+      end
+    end
+
+    context 'when the code is incorrect' do
+      it 'returns a code invalid error' do
+        result = service.validate_and_confirm_unauthenticated!(user, 'failcode')
+
+        expect(result.success?).to be false
+        expect(result.errors.details).to eq(code: [{ error: :invalid }])
+      end
+    end
+
+    context 'when the code has expired' do
+      before do
+        user.update(email_confirmation_code_sent_at: 1.week.ago)
+      end
+
+      it 'returns a code expired error' do
+        result = service.validate_and_confirm_unauthenticated!(user, user.email_confirmation_code)
+
+        expect(result.success?).to be false
+        expect(result.errors.details).to eq(code: [{ error: :expired }])
+      end
+    end
+
+    context 'when the code has expired and is invalid' do
+      before do
+        user.update(email_confirmation_code_sent_at: 1.week.ago)
+      end
+
+      it 'returns a code invalid error' do
+        result = service.validate_and_confirm_unauthenticated!(user, 'failcode')
+
+        expect(result.success?).to be false
+        expect(result.errors.details).to eq(code: [{ error: :invalid }])
+      end
+    end
+  end
+
+
   describe '#validate_and_confirm_authenticated!' do
+    let(:user) { create(:user_with_confirmation) }
+
     context 'when the user is nil' do
       it 'returns a user blank error' do
         result = service.validate_and_confirm_authenticated!(nil, '1234')
