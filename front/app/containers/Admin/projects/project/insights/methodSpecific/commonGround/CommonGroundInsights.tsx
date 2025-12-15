f3e114a7ce57efb953972d408a58116fd8dbd657
EdwinKato <katedwin88@gmail.com>
2025-12-15 12:14:02 +0300

    Remove unused Common Ground insights components and related files

--- front/app/containers/Admin/projects/project/insights/methodSpecific/commonGround/CommonGroundInsights.tsx
+++ front/app/containers/Admin/projects/project/insights/methodSpecific/commonGround/CommonGroundInsights.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 
-import { Box } from '@citizenlab/cl2-component-library';
+import CommonGroundResults from 'containers/ProjectsShowPage/timeline/CommonGround/CommonGroundResults';
 
 import { MethodSpecificInsightProps } from '../types';
 
-import StatementsList from './StatementsList';
-
 const CommonGroundInsights = ({ phaseId }: MethodSpecificInsightProps) => {
-  return (
-    <Box mt="8px" bg="white">
-      <StatementsList phaseId={phaseId} />
-    </Box>
-  );
+  return <CommonGroundResults phaseId={phaseId} />;
 };
 
 export default CommonGroundInsights;
