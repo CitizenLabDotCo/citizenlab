7e62dc7c61840b38780e0a38eb07bf26dac6d90d
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-12-16 18:32:04 -0300

    Create post participation flow start step

---
+++ front/app/containers/Authentication/steps/PostParticipationFlowStart/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import { Box } from '@citizenlab/cl2-component-library';
+
+import { SSOProvider } from 'api/authentication/singleSignOn';
+
+import oldMessages from 'containers/Authentication/steps/_components/AuthProviderButton/messages';
+import { SetError } from 'containers/Authentication/typings';
+import useAuthConfig from 'containers/Authentication/useAuthConfig';
+
+import FranceConnectButton from 'components/UI/FranceConnectButton';
+import Or from 'components/UI/Or';
+
+import { useIntl } from 'utils/cl-intl';
+
+import EmailForm from '../EmailFlowStart/EmailForm';
+import SSOButtons from '../EmailFlowStart/SSOButtons';
+
+import messages from './messages';
+
+interface Props {
+  loading: boolean;
+  setError: SetError;
+  onSubmit: (email: string) => void;
+  onSwitchToSSO: (ssoProvider: SSOProvider) => void;
+  onEnterFranceConnect: () => void;
+}
+
+const PostParticipationFlowStart = ({
+  loading,
+  setError,
+  onSubmit,
+  onSwitchToSSO,
+  onEnterFranceConnect,
+}: Props) => {
+  const { passwordLoginEnabled, ssoProviders } = useAuthConfig();
+  const { formatMessage } = useIntl();
+
+  return (
+    <Box data-cy="post-participation-flow-start">
+      {ssoProviders.franceconnect && (
+        <>
+          <FranceConnectButton
+            logoAlt={formatMessage(oldMessages.signUpButtonAltText, {
+              loginMechanismName: 'FranceConnect',
+            })}
+            onClick={onEnterFranceConnect}
+          />
+          {passwordLoginEnabled && (
+            <Box mt="24px">
+              <Or />
+            </Box>
+          )}
+        </>
+      )}
+      {passwordLoginEnabled && (
+        <EmailForm
+          loading={loading}
+          topText={messages.dropUsYourEmailIfYouWantToStayUpdated}
+          setError={setError}
+          onSubmit={onSubmit}
+        />
+      )}
+      <SSOButtons onClickSSO={onSwitchToSSO} />
+    </Box>
+  );
+};
+
+export default PostParticipationFlowStart;
---
+++ front/app/containers/Authentication/steps/PostParticipationFlowStart/messages.ts
@@ -0,0 +1,9 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  dropUsYourEmailIfYouWantToStayUpdated: {
+    id: 'app.containers.Authentication.steps.PostParticipationFlowStart.dropUsYourEmailIfYouWantToStayUpdated',
+    defaultMessage:
+      'Drop us your email if you want to stay updated on future participation opportunities and results.',
+  },
+});
