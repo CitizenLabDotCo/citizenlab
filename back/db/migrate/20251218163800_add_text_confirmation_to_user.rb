00736db68b7824e31713ed232418d1d74ad82f34
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-12-18 17:08:31 -0300

    Add phone number to migration

--- back/db/migrate/20251218163800_add_text_confirmation_to_user.rb
+++ back/db/migrate/20251218163800_add_text_confirmation_to_user.rb
@@ -2,6 +2,7 @@
 
 class AddTextConfirmationToUser < ActiveRecord::Migration[7.1]
   def change
+    add_column :users, :phone_number, :string, null: true
     add_column :users, :phone_confirmed_at, :datetime, null: true
     add_column :users, :phone_confirmation_code, :string, null: true
     add_column :users, :phone_confirmation_retry_count, :integer, null: false, default: 0
