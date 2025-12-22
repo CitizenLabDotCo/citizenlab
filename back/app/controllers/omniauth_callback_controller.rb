4a94bcc789150ee9fe7decbaa5c64e7a54601356
jamesspeake <james@speake.org>
2025-12-22 10:33:01 +0000

    [TAN-5906] Attempt to fix Hoplr log back in issue

--- back/app/controllers/omniauth_callback_controller.rb
+++ back/app/controllers/omniauth_callback_controller.rb
@@ -234,6 +234,8 @@ class OmniauthCallbackController < ApplicationController
 
   # Updates the auth_hash in a users identity to ensure tokens are up to date for logout (mainly for FranceConnect)
   def update_identity!(auth, identity, authver_method)
+    return if authver_method.class.name == "IdHoplr::HoplrOmniauth" # Temp fix for Hoplr not being able to log back in
+
     identity.update_auth_hash!(auth, authver_method)
   end
 
