01136f58370ec2fa040b039f4e0495f4c69f9b3a
jamesspeake <james@speake.org>
2025-12-29 14:49:36 +0000

    [TAN-6062] Fixed tests

--- back/engines/commercial/report_builder/spec/services/report_builder/queries/visitors_spec.rb
+++ back/engines/commercial/report_builder/spec/services/report_builder/queries/visitors_spec.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+  # frozen_string_literal: true
 
 require 'rails_helper'
 
@@ -8,7 +8,10 @@ RSpec.describe ReportBuilder::Queries::Visitors do
   describe '#run_query' do
     before_all do
       # Make TimeBoundariesParser work as expected
-      AppConfiguration.instance.update!(created_at: Date.new(2021, 1, 1))
+      AppConfiguration.instance.update!(
+        created_at: Date.new(2021, 1, 1),
+        platform_start_at: Date.new(2021, 1, 1)
+      )
 
       ### SEPTEMBER
       september = Date.new(2022, 9, 10)
