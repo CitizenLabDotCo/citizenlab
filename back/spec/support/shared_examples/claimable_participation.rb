5ecb5cca936eb3fffa83a1f55496b5f532f7795f
Adrien Dessy <209268+adessy@users.noreply.github.com>
2026-01-12 13:44:00 +0100

    [TAN-6011] Fix NOT NULL constraint violation when deleting claimable items

--- back/spec/support/shared_examples/claimable_participation.rb
+++ back/spec/support/shared_examples/claimable_participation.rb
@@ -5,6 +5,6 @@
 #   it_behaves_like 'claimable_participation'
 RSpec.shared_examples 'claimable_participation' do
   describe 'claim_token association' do
-    it { is_expected.to have_one(:claim_token).dependent(:nullify) }
+    it { is_expected.to have_one(:claim_token).dependent(:destroy) }
   end
 end
