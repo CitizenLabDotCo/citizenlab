6254fca075de152c40625716d6e049e7c17043be
Amanda Anderson <anderson_amanda@mail.com>
2026-01-13 10:13:35 +0100

    Support videoask video format in whitelist

--- back/app/services/url_validation_service.rb
+++ back/app/services/url_validation_service.rb
@@ -7,7 +7,8 @@ class UrlValidationService
     %r{\A(?:http(?:s?):)?//(.+)?(wistia.com|wi.st).*/},
     %r{\A(?:http(?:s?):)?//(?:www\.)?dailymotion\.com/embed/video/?(.+)},
     %r{\A(https?://)?media\.videotool\.dk/?\?vn=[\w-]+},
-    %r{\A(https?://)(?:www\.)?dreambroker\.com/channel/([\w-]+)/iframe/([\w\-\#/]+)}
+    %r{\A(https?://)(?:www\.)?dreambroker\.com/channel/([\w-]+)/iframe/([\w\-\#/]+)},
+    %r{\A(?:http(?:s?):)?//(?:www\.)?videoask\.com/([\w\-_]+)}
   ].freeze
 
   def video_whitelisted?(url)
