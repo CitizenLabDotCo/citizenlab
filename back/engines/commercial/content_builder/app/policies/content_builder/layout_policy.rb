5e247645ec189c2f50ff9d2701799e2a1cd828be
Adrien Dessy <209268+adessy@users.noreply.github.com>
2026-01-14 13:08:31 +0100

    [TAN-5957] Add file authorization check to `LayoutPolicy#update?`
    When updating a layout with file attachments in craftjs_json, verify
    the user is authorized to use those files via FilePolicy#update?.

--- back/engines/commercial/content_builder/app/policies/content_builder/layout_policy.rb
+++ back/engines/commercial/content_builder/app/policies/content_builder/layout_policy.rb
@@ -17,7 +17,7 @@ module ContentBuilder
     end
 
     def update?
-      moderator?
+      moderator? && authorized_to_attach_files?
     end
 
     def destroy?
@@ -32,6 +32,25 @@ module ContentBuilder
 
     def user_role_service
       @user_role_service ||= UserRoleService.new
+    end
+
+    # Checks that the user is authorized to use all files referenced in craftjs_json.
+    def authorized_to_attach_files?
+      file_ids = FileAttachmentProcessorService.extract_file_ids(record.craftjs_json)
+      return true if file_ids.empty?
+
+      # All referenced files must exist, and the user must be authorized to use them.
+      files = ::Files::File.where(id: file_ids).index_by(&:id)
+      file_ids.all? do |file_id|
+        file = files[file_id]
+        next false unless file
+
+        # Ideally, we would check `policy_for(file_attachment).create?`, but that causes a
+        # circular dependency since `FileAttachmentPolicy#create?` calls
+        # `policy_for(attachable).update?`. As a workaround, we essentially inline the
+        # relevant part of `FileAttachmentPolicy#create?` here.
+        policy_for(file).update?
+      end
     end
   end
 end
