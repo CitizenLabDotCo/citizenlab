ee45037840cb4df177c8456f9c11d4880209aa34
Sébastien Hoorens <sebastien@citizenlab.co>
2026-01-14 17:25:11 +0100

    claude: requested: more concise specs

--- back/spec/lib/input_strategy/rating_spec.rb
+++ back/spec/lib/input_strategy/rating_spec.rb
@@ -3,102 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe InputStrategy::Rating do
+  subject(:input_strategy) { described_class.new(custom_field) }
+
   let(:custom_field) { build(:custom_field_rating) }
-  let(:input_strategy) { described_class.new(custom_field) }
 
-  describe '#supports_average?' do
-    it 'returns true' do
-      expect(input_strategy.supports_average?).to be true
-    end
-  end
-
-  describe '#supports_options?' do
-    it 'returns false' do
-      expect(input_strategy.supports_options?).to be false
-    end
-  end
-
-  describe '#supports_other_option?' do
-    it 'returns false' do
-      expect(input_strategy.supports_other_option?).to be false
-    end
-  end
-
-  describe '#supports_option_images?' do
-    it 'returns false' do
-      expect(input_strategy.supports_option_images?).to be false
-    end
-  end
-
-  describe '#supports_follow_up?' do
-    it 'returns false' do
-      expect(input_strategy.supports_follow_up?).to be false
-    end
-  end
-
-  describe '#supports_text?' do
-    it 'returns false' do
-      expect(input_strategy.supports_text?).to be false
-    end
-  end
-
-  describe '#supports_linear_scale?' do
-    it 'returns true' do
-      expect(input_strategy.supports_linear_scale?).to be true
-    end
-  end
-
-  describe '#supports_linear_scale_labels?' do
-    it 'returns false' do
-      expect(input_strategy.supports_linear_scale_labels?).to be false
-    end
-  end
-
-  describe '#supports_matrix_statements?' do
-    it 'returns false' do
-      expect(input_strategy.supports_matrix_statements?).to be false
-    end
-  end
-
-  describe '#supports_single_selection?' do
-    it 'returns true' do
-      expect(input_strategy.supports_single_selection?).to be true
-    end
-  end
-
-  describe '#supports_multiple_selection?' do
-    it 'returns false' do
-      expect(input_strategy.supports_multiple_selection?).to be false
-    end
-  end
-
-  describe '#supports_selection?' do
-    it 'returns true' do
-      expect(input_strategy.supports_selection?).to be true
-    end
-  end
-
-  describe '#supports_select_count?' do
-    it 'returns false' do
-      expect(input_strategy.supports_select_count?).to be false
-    end
-  end
-
-  describe '#supports_dropdown_layout?' do
-    it 'returns false' do
-      expect(input_strategy.supports_dropdown_layout?).to be false
-    end
-  end
-
-  describe '#supports_xlsx_export?' do
-    it 'returns true' do
-      expect(input_strategy.supports_xlsx_export?).to be true
-    end
-  end
-
-  describe '#supports_geojson?' do
-    it 'returns true' do
-      expect(input_strategy.supports_geojson?).to be true
-    end
-  end
+  its(:supports_average?) { is_expected.to be true }
+  its(:supports_options?) { is_expected.to be false }
+  its(:supports_other_option?) { is_expected.to be false }
+  its(:supports_option_images?) { is_expected.to be false }
+  its(:supports_follow_up?) { is_expected.to be false }
+  its(:supports_text?) { is_expected.to be false }
+  its(:supports_linear_scale?) { is_expected.to be true }
+  its(:supports_linear_scale_labels?) { is_expected.to be false }
+  its(:supports_matrix_statements?) { is_expected.to be false }
+  its(:supports_single_selection?) { is_expected.to be true }
+  its(:supports_multiple_selection?) { is_expected.to be false }
+  its(:supports_selection?) { is_expected.to be true }
+  its(:supports_select_count?) { is_expected.to be false }
+  its(:supports_dropdown_layout?) { is_expected.to be false }
+  its(:supports_xlsx_export?) { is_expected.to be true }
+  its(:supports_geojson?) { is_expected.to be true }
 end
