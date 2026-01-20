80e72d28f6acea3d23e8154ec0d754e853500716
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-01-20 17:59:56 -0300

    _

--- back/spec/acceptance/ideas/user_fields_in_form_ideation.rb
+++ back/spec/acceptance/ideas/user_fields_in_form_ideation.rb
@@ -17,12 +17,13 @@ resource 'Ideas' do
 
       # Create project with form
       @project = create(:single_phase_ideation_project, phase_attrs: { with_permissions: true })
-      @project.phases.first.permissions.find_by(action: 'posting_idea').update!(
+      @phase = @project.phases.first
+      @phase.permissions.find_by(action: 'posting_idea').update!(
         global_custom_fields: false
       )
 
-      @permission = @project.phases.first.permissions.find_by(action: 'posting_idea')
-      @custom_form = create(:custom_form, :with_default_fields, participation_context: @project.phases.first)
+      @permission = @phase.permissions.find_by(action: 'posting_idea')
+      @custom_form = create(:custom_form, :with_default_fields, participation_context: @phase)
 
       # Create registration (demographic) question and
       # add to permission
@@ -103,9 +104,43 @@ resource 'Ideas' do
         end
       end
 
-      # context 'when logged in but anonymous' do
-      #   # TODO
-      # end
+      context 'when logged in but anonymous' do
+        before do
+          @user = create(:user)
+          header_token_for @user
+          @phase.update!(allow_anonymous_participation: true)
+        end
+
+        it 'stores values that have corresponding custom field in form' do
+          do_request({
+            idea: {
+              publication_status: 'published',
+              project_id: @project.id,
+              title_multiloc: { 'en' => 'My Idea Title' },
+              body_multiloc: { 'en' => 'My Idea Body' },
+              u_select_field: 'option1',
+              u_nonexistent_field: 'whatever',
+              anonymous: true
+            }
+          })
+
+          assert_status 201
+          expect(Idea.count).to eq 1
+          idea = Idea.first
+          expect(idea.custom_field_values).to eq({
+            'u_select_field' => 'option1'
+          })
+          expect(idea.title_multiloc).to eq({ 'en' => 'My Idea Title' })
+          expect(idea.body_multiloc).to eq({ 'en' => 'My Idea Body' })
+
+          # Make sure user id is not linked to idea
+          expect(idea.author_id).to be(nil)
+
+          # Make sure not stored in user profile
+          user = User.find(@user.id)
+          expect(user.custom_field_values).to eq({})
+        end
+      end
     end
   end
 end
