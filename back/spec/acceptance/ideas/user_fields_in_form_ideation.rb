11cb68a0b85b8f17680db39c06b2a9d324161460
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-01-20 18:20:37 -0300

    _

--- back/spec/acceptance/ideas/user_fields_in_form_ideation.rb
+++ back/spec/acceptance/ideas/user_fields_in_form_ideation.rb
@@ -37,13 +37,14 @@ resource 'Ideas' do
       create(:permissions_custom_field, custom_field: @user_select_field, permission: @permission)
     end
 
-    context 'permission: everyone' do
+    context 'permitted_by: everyone' do
+      # If permission: everyone, the fields are always asked as last page of the form
       before do
         @permission.update!(permitted_by: 'everyone')
       end
 
       context 'when visitor' do
-        it 'stores values that have corresponding custom field in form' do
+        it 'stores values in idea' do
           do_request({
             idea: {
               publication_status: 'published',
@@ -72,7 +73,7 @@ resource 'Ideas' do
           header_token_for @user
         end
 
-        it 'stores values that have corresponding custom field in form' do
+        it 'stores values in idea and profile, adds author_id' do
           do_request({
             idea: {
               publication_status: 'published',
@@ -111,7 +112,7 @@ resource 'Ideas' do
           @phase.update!(allow_anonymous_participation: true)
         end
 
-        it 'stores values that have corresponding custom field in form' do
+        it 'stores values in idea but not profile, does not add author_id' do
           do_request({
             idea: {
               publication_status: 'published',
@@ -139,6 +140,176 @@ resource 'Ideas' do
           # Make sure not stored in user profile
           user = User.find(@user.id)
           expect(user.custom_field_values).to eq({})
+        end
+      end
+    end
+
+    # context 'permitted_by: everyone_confirmed_email' do
+    #   context 'permitted_by: users' do
+    #     before do
+    #       @permission.update!(permitted_by: 'users')
+    #     end
+
+    #     context 'user fields in reg flow' do
+    #       # TODO
+    #     end
+
+    #     context 'user fields in form' do
+    #       # TODO
+    #     end
+    #   end
+    # end
+
+    context 'permitted_by: users' do
+      before do
+        @permission.update!(permitted_by: 'users')
+      end
+
+      context 'user fields in reg flow' do
+        before do
+          @permission.update!(user_fields_in_form: false)
+        end
+        
+        context 'when logged in' do
+          before do
+            @user = create(:user, custom_field_values: { gender: 'female' })
+            header_token_for @user
+          end
+
+          it 'copies values from profile into idea' do
+            do_request({
+              idea: {
+                publication_status: 'published',
+                project_id: @project.id,
+                title_multiloc: { 'en' => 'My Idea Title' },
+                body_multiloc: { 'en' => 'My Idea Body' },
+              }
+            })
+
+            assert_status 201
+            expect(Idea.count).to eq 1
+            idea = Idea.first
+            expect(idea.custom_field_values).to eq({
+              'u_gender' => 'female'
+            })
+            expect(idea.title_multiloc).to eq({ 'en' => 'My Idea Title' })
+            expect(idea.body_multiloc).to eq({ 'en' => 'My Idea Body' })
+
+            # Make sure user id is linked to idea
+            expect(idea.author_id).to eq(@user.id)
+          end
+        end
+
+        context 'when logged in but anonymous' do
+          before do
+            @user = create(:user)
+            header_token_for @user
+            @phase.update!(allow_anonymous_participation: true)
+          end
+
+          it 'does not copy values from profile into idea' do
+            do_request({
+              idea: {
+                publication_status: 'published',
+                project_id: @project.id,
+                title_multiloc: { 'en' => 'My Idea Title' },
+                body_multiloc: { 'en' => 'My Idea Body' },
+                anonymous: true
+              }
+            })
+
+            assert_status 201
+            expect(Idea.count).to eq 1
+            idea = Idea.first
+            expect(idea.custom_field_values).to eq({})
+            expect(idea.title_multiloc).to eq({ 'en' => 'My Idea Title' })
+            expect(idea.body_multiloc).to eq({ 'en' => 'My Idea Body' })
+
+            # Make sure user id is not linked to idea
+            expect(idea.author_id).to be(nil)
+          end
+        end
+      end
+
+      context 'user fields in form' do
+        before do
+          @permission.update!(user_fields_in_form: true)
+        end
+
+        context 'when logged in' do
+          before do
+            @user = create(:user)
+            header_token_for @user
+          end
+
+          it 'stores values in idea and profile, adds author_id' do
+            do_request({
+              idea: {
+                publication_status: 'published',
+                project_id: @project.id,
+                title_multiloc: { 'en' => 'My Idea Title' },
+                body_multiloc: { 'en' => 'My Idea Body' },
+                u_select_field: 'option1',
+                u_nonexistent_field: 'whatever'
+              }
+            })
+
+            assert_status 201
+            expect(Idea.count).to eq 1
+            idea = Idea.first
+            expect(idea.custom_field_values).to eq({
+              'u_select_field' => 'option1'
+            })
+            expect(idea.title_multiloc).to eq({ 'en' => 'My Idea Title' })
+            expect(idea.body_multiloc).to eq({ 'en' => 'My Idea Body' })
+
+            # Make sure user id is linked to idea
+            expect(idea.author_id).to eq(@user.id)
+
+            # Make sure also stored in user profile
+            user = User.find(@user.id)
+            expect(user.custom_field_values).to eq({
+              'select_field' => 'option1'
+            })
+          end
+        end
+
+        context 'when logged in but anonymous' do
+          before do
+            @user = create(:user)
+            header_token_for @user
+            @phase.update!(allow_anonymous_participation: true)
+          end
+
+          it 'stores values in idea but not profile, does not add author_id' do
+            do_request({
+              idea: {
+                publication_status: 'published',
+                project_id: @project.id,
+                title_multiloc: { 'en' => 'My Idea Title' },
+                body_multiloc: { 'en' => 'My Idea Body' },
+                u_select_field: 'option1',
+                u_nonexistent_field: 'whatever',
+                anonymous: true
+              }
+            })
+
+            assert_status 201
+            expect(Idea.count).to eq 1
+            idea = Idea.first
+            expect(idea.custom_field_values).to eq({
+              'u_select_field' => 'option1'
+            })
+            expect(idea.title_multiloc).to eq({ 'en' => 'My Idea Title' })
+            expect(idea.body_multiloc).to eq({ 'en' => 'My Idea Body' })
+
+            # Make sure user id is not linked to idea
+            expect(idea.author_id).to be(nil)
+
+            # Make sure not stored in user profile
+            user = User.find(@user.id)
+            expect(user.custom_field_values).to eq({})
+          end
         end
       end
     end
