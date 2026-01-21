8dc68afac7d720dac400f8698e7e3dba87d1a685
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-01-21 14:02:20 -0300

    _

--- back/spec/acceptance/ideas/user_fields_in_form_survey_spec.rb
+++ back/spec/acceptance/ideas/user_fields_in_form_survey_spec.rb
@@ -9,6 +9,27 @@ resource 'Ideas' do
   before do
     header 'Content-Type', 'application/json'
     create(:idea_status_proposed)
+
+    # Create project with form
+    @project = create(:single_phase_native_survey_project, phase_attrs: { with_permissions: true })
+    @permission = @project.phases.first.permissions.find_by(action: 'posting_idea')
+    @permission.update!(
+      global_custom_fields: false
+    )
+
+    @custom_form = create(:custom_form, :with_default_fields, participation_context: @project.phases.first)
+    @custom_field = @custom_form.custom_fields.find_by(input_type: 'select')
+
+    # Create registration (demographic) question and
+    # add to permission
+    @user_select_field = create(
+      :custom_field_select,
+      :for_registration,
+      :with_options,
+      key: 'select_field',
+      enabled: true
+    )
+    create(:permissions_custom_field, custom_field: @user_select_field, permission: @permission)
   end
 
   post 'web_api/v1/ideas' do
@@ -18,27 +39,9 @@ resource 'Ideas' do
       # when we submit the first page, and every subsequent page is an UPDATE.
 
       before do
-        # Create project with form
-        @project = create(:single_phase_native_survey_project, phase_attrs: { with_permissions: true })
-        @project.phases.first.permissions.find_by(action: 'posting_idea').update!(
-          permitted_by: 'everyone',
-          global_custom_fields: false
+        @permission.update!(
+          permitted_by: 'everyone'
         )
-
-        @permission = @project.phases.first.permissions.find_by(action: 'posting_idea')
-        @custom_form = create(:custom_form, :with_default_fields, participation_context: @project.phases.first)
-        @custom_field = @custom_form.custom_fields.find_by(input_type: 'select')
-
-        # Create registration (demographic) question and
-        # add to permission
-        @user_select_field = create(
-          :custom_field_select,
-          :for_registration,
-          :with_options,
-          key: 'select_field',
-          enabled: true
-        )
-        create(:permissions_custom_field, custom_field: @user_select_field, permission: @permission)
       end
 
       it 'stores values that have corresponding custom field in form' do
@@ -61,4 +64,8 @@ resource 'Ideas' do
       end
     end
   end
+
+  # patch 'web_api/v1/ideas/:id' do
+
+  # end
 end
