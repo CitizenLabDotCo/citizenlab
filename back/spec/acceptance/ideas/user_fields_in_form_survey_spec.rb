0cae12eaf75583a9df672ed03af1da2ab2189949
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-01-21 15:25:16 -0300

    _

--- back/spec/acceptance/ideas/user_fields_in_form_survey_spec.rb
+++ back/spec/acceptance/ideas/user_fields_in_form_survey_spec.rb
@@ -26,18 +26,25 @@ resource 'Ideas' do
       :custom_field_select,
       :for_registration,
       :with_options,
-      key: 'select_field',
+      key: 'user_select_field',
       enabled: true
     )
     create(:permissions_custom_field, custom_field: @user_select_field, permission: @permission)
   end
 
+  # This endpoint is used in the context of ideation in 3 situations:
+  # 1. The user is a visitor and permitted_by is everyone.
+  #   In this case, the whole survey gets submitted at once at the end in one POST request.
+  # 2. The user is logged in and the survey has >1 page.
+  #   In this case, the survey is submitted in multiple steps, with the first step being a POST request.
+  #   After that, each request is a PATCH. Only the final PATCH publishes the idea.
+  # 3. The user is logged in and the survey has exactly 1 page.
+  #   In this case, the survey is submitted in one POST request and published immediately at
+  #   after filling out the first page.
+  # 
+  # In this POST block, we are going to focus on 1 and 3. Below, we will tackle 2
   post 'web_api/v1/ideas' do
     context 'when visitor and permitted_by is everyone' do
-      # For surveys, this is the only case where we use the POST endpoint to submit
-      # the whole survey at once. In all other cases we first create the idea
-      # when we submit the first page, and every subsequent page is an UPDATE.
-
       before do
         @permission.update!(
           permitted_by: 'everyone'
@@ -50,7 +57,7 @@ resource 'Ideas' do
             publication_status: 'published',
             project_id: @project.id,
             @custom_field.key => 'option2',
-            u_select_field: 'option1',
+            u_user_select_field: 'option1',
             u_nonexistent_field: 'whatever'
           }
         })
@@ -59,13 +66,100 @@ resource 'Ideas' do
         expect(Idea.count).to eq 1
         expect(Idea.first.custom_field_values).to eq({
           @custom_field.key => 'option2',
-          'u_select_field' => 'option1'
+          'u_user_select_field' => 'option1'
         })
       end
     end
+
+    context 'when logged in and permitted_by is users and user_fields_in_form is false' do
+      before do
+        @permission.update!(
+          permitted_by: 'users',
+          user_fields_in_form: false
+        )
+        @user = create(:user, custom_field_values: { @user_select_field.key => 'option1' } )
+        header_token_for @user
+      end
+
+      context 'when user_data_collection is all_data' do
+        before do
+          @permission.update!(
+            user_data_collection: 'all_data'
+          )
+        end
+
+        it 'stores values from profile in idea, adds author_id' do
+          do_request({
+            idea: {
+              publication_status: 'published',
+              project_id: @project.id,
+              @custom_field.key => 'option2',
+            }
+          })
+
+          assert_status 201
+          expect(Idea.count).to eq 1
+          idea = Idea.first
+          expect(idea.custom_field_values).to eq({
+            @custom_field.key => 'option2',
+            'u_user_select_field' => 'option1'
+          })
+          expect(idea.author_id).to eq(@user.id)
+        end
+      end
+
+      context 'when user_data_collection is demographics_only' do
+        before do
+          @permission.update!(
+            user_data_collection: 'demographics_only'
+          )
+        end
+
+        it 'stores values from profile in idea, does not add author_id' do
+          do_request({
+            idea: {
+              publication_status: 'published',
+              project_id: @project.id,
+              @custom_field.key => 'option2',
+            }
+          })
+
+          assert_status 201
+          expect(Idea.count).to eq 1
+          idea = Idea.first
+          expect(idea.custom_field_values).to eq({
+            @custom_field.key => 'option2',
+            'u_user_select_field' => 'option1'
+          })
+          expect(idea.author_id).to be(nil)
+        end
+      end
+
+      context 'when user_data_collection is anonymous' do
+        before do
+          @permission.update!(
+            user_data_collection: 'anonymous'
+          )
+        end
+
+        it 'does not store values from profile in idea, does not add author_id' do
+          do_request({
+            idea: {
+              publication_status: 'published',
+              project_id: @project.id,
+              @custom_field.key => 'option2',
+            }
+          })
+
+          assert_status 201
+          expect(Idea.count).to eq 1
+          idea = Idea.first
+          expect(idea.custom_field_values).to eq({
+            @custom_field.key => 'option2'
+          })
+          expect(idea.author_id).to be(nil)
+        end
+      end
+    end
   end
-
-  # patch 'web_api/v1/ideas/:id' do
-
-  # end
 end
