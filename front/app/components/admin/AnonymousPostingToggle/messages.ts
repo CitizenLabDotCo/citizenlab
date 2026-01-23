cc03a9630b2b4a0974fe95a568080d684d3473f1
Amira Bayoumi <amira.bayoumii@gmail.com>
2026-01-23 13:27:51 +0100

    update link 2

--- front/app/components/admin/AnonymousPostingToggle/messages.ts
+++ front/app/components/admin/AnonymousPostingToggle/messages.ts
@@ -27,10 +27,10 @@ export default defineMessages({
     id: 'app.components.AnonymousPostingToggle.userAnonymitySupportTooltipLinkText',
     defaultMessage: 'support article',
   },
-  userAnonymitySupportTooltipLinkUrl: {
-    id: 'app.components.AnonymousPostingToggle.userAnonymitySupportTooltipLinkUrl',
+  userAnonymitySupportTooltipLinkUrl2: {
+    id: 'app.components.AnonymousPostingToggle.userAnonymitySupportTooltipLinkUrl2',
     defaultMessage:
-      'https://support.govocal.com/en/articles/7946486-enabling-anonymous-participation',
+      'https://support.govocal.com/en/articles/527583-enabling-anonymous-participation',
   },
   anonymousParticipationAutoEnabled: {
     id: 'app.components.AnonymousPostingToggle.anonymousParticipationAutoEnabled',
