1cf7daeeed134e960169310a6d61a86e19992b0e
kogre <koen@govocal.com>
2026-01-23 14:51:50 +0100

    Automatically clean up overlapping parent-child topic assignments to ideas and support nested topics in seeds

--- back/app/models/ideas_input_topic.rb
+++ back/app/models/ideas_input_topic.rb
@@ -27,15 +27,29 @@ class IdeasInputTopic < ApplicationRecord
 
   validates :input_topic_id, uniqueness: { scope: :idea_id }
 
-  after_create :remove_parent_topic_assignment
+  after_create :enfore_clean_topic_hierarchy
 
   private
 
-  # When assigning an idea to a subtopic, remove the assignment to the parent topic
-  def remove_parent_topic_assignment
-    parent_topic = input_topic.parent
-    return if parent_topic.blank?
+  def enfore_clean_topic_hierarchy
+    remove_parent_if_child_added
+    remove_self_if_parent_of_existing_child_added
+  end
 
-    IdeasInputTopic.where(idea_id: idea_id, input_topic_id: parent_topic.id).destroy_all
+  # When assigning an idea to a subtopic, remove the assignment to the parent topic
+  def remove_parent_if_child_added
+    parent_topic_id = input_topic.parent_id
+    return if parent_topic_id.blank?
+
+    IdeasInputTopic.where(idea_id: idea_id, input_topic_id: parent_topic_id).destroy_all
+  end
+
+  def remove_self_if_parent_of_existing_child_added
+    child_topic_ids = input_topic.children.ids
+    return if child_topic_ids.empty?
+
+    if IdeasInputTopic.exists?(idea_id: idea_id, input_topic_id: child_topic_ids)
+      delete
+    end
   end
 end
