c97b95e67ff4783381b16600df43dfd3d25f0ad1
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-01-27 18:28:48 -0300

    Tweak last survey page boxes

--- front/app/components/CustomFieldsForm/PostParticipationBox/index.tsx
+++ front/app/components/CustomFieldsForm/PostParticipationBox/index.tsx
@@ -24,6 +24,7 @@ const PostParticipationBox = ({ onCreateAccount }: Props) => {
       borderRadius={stylingConsts.borderRadius}
       px="12px"
       pb="12px"
+      mb="16px"
     >
       <Title variant="h4" as="h2">
         <FormattedMessage {...messages.stayConnected} />
