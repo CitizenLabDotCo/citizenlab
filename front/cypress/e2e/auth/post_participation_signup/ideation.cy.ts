25298eb4634811eccfce206c3cdc6b16482dbda7
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-01-28 10:48:57 -0300

    Undo changes to ideation e2e test

--- front/cypress/e2e/auth/post_participation_signup/ideation.cy.ts
+++ front/cypress/e2e/auth/post_participation_signup/ideation.cy.ts
@@ -1,8 +1,4 @@
 import { signUpEmailConformation, enterUserInfo } from '../../../support/auth';
-import moment = require('moment');
-import { randomString } from '../../../support/commands';
-import { updatePermission } from '../../../support/permitted_by_utils';
-import { fillOutTitleAndBody } from '../../ideation_permissions/_utils';
 
 describe('Post Participation Signup: ideation', () => {
   let projectId: string;
