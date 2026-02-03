fc2bb34e9ee06af0085fd3df823a32d5fc3174a2
EdwinKato <katedwin88@gmail.com>
2026-02-03 17:23:42 +0300

    Wrap voting insights for export

--- front/app/containers/Admin/projects/project/insights/methodSpecific/voting/VotingInsights.tsx
+++ front/app/containers/Admin/projects/project/insights/methodSpecific/voting/VotingInsights.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 
 import { Box } from '@citizenlab/cl2-component-library';
 
+import ExportableInsight from '../../word/ExportableInsight';
 import { MethodSpecificInsightProps } from '../types';
 
 import VoteResults from './VoteResults';
 
 const VotingInsights = ({ phaseId }: MethodSpecificInsightProps) => {
   return (
-    <Box my="12px" bg="white">
-      <VoteResults phaseId={phaseId} />
-    </Box>
+    <ExportableInsight exportId="vote-results" my="12px" bg="white">
+      <Box>
+        <VoteResults phaseId={phaseId} />
+      </Box>
+    </ExportableInsight>
   );
 };
 
