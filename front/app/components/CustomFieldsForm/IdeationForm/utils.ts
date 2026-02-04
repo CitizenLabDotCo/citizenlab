53606d79232eac3f7d7fd0a99d296cb999f11e46
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-02-04 09:51:27 -0300

    Add to ideation form as well

--- front/app/components/CustomFieldsForm/IdeationForm/utils.ts
+++ front/app/components/CustomFieldsForm/IdeationForm/utils.ts
@@ -1,15 +1,27 @@
-import { IIdea } from 'api/ideas/types';
+import { IIdeaData } from 'api/ideas/types';
 import { IPhase } from 'api/phases/types';
 import { IUser } from 'api/users/types';
 
 import { addPrefix } from '../util';
 
 export const getInitialData = (
-  draftIdea: IIdea | undefined,
+  idea: IIdeaData | undefined,
   authUser: IUser | undefined,
   phase: IPhase
 ) => {
-  const initialFormData = draftIdea?.data.attributes ?? {};
+  const initialFormData = idea
+    ? {
+        ...idea.attributes,
+        author_id: idea.relationships.author?.data?.id,
+        cosponsor_ids: idea.relationships.cosponsors?.data?.map(
+          (cosponsor) => cosponsor.id
+        ),
+        topic_ids: idea.relationships.input_topics?.data.map(
+          (topic) => topic.id
+        ),
+      }
+    : {};
+
   const customFieldValues = authUser?.data.attributes.custom_field_values;
 
   if (phase.data.attributes.user_fields_in_form_enabled && customFieldValues) {
