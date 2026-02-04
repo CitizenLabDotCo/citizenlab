78acaa673ca761e543f2d92eba6077298e43731b
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-02-04 09:46:03 -0300

    Add prefix

--- front/app/components/CustomFieldsForm/SurveyForm/utils.ts
+++ front/app/components/CustomFieldsForm/SurveyForm/utils.ts
@@ -8,13 +8,26 @@ export const getInitialData = (
   phase: IPhase
 ) => {
   const initialFormData = draftIdea?.data.attributes ?? {};
+  const customFieldValues = authUser?.data.attributes.custom_field_values;
 
-  if (phase.data.attributes.user_fields_in_form_enabled && authUser) {
+  if (phase.data.attributes.user_fields_in_form_enabled && customFieldValues) {
     return {
       ...initialFormData,
-      ...authUser.data.attributes.custom_field_values,
+      ...addPrefix(customFieldValues),
     };
   }
 
   return initialFormData;
+};
+
+const PREFIX = 'u_';
+
+const addPrefix = (customFieldValues: Record<string, any>) => {
+  const newValues = {};
+
+  for (const key in customFieldValues) {
+    newValues[`${PREFIX}${key}`] = customFieldValues[key];
+  }
+
+  return newValues;
 };
