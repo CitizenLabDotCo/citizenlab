889789ec83e97845f2bdee49ef8e7533e0c09609
EdwinKato <katedwin88@gmail.com>
2026-02-09 12:51:30 +0300

    Remove live data badge

--- front/app/containers/Admin/projects/project/insights/messages.ts
+++ front/app/containers/Admin/projects/project/insights/messages.ts
@@ -5,10 +5,6 @@ export default defineMessages({
     id: 'app.containers.Admin.projects.project.insights',
     defaultMessage: 'Insights',
   },
-  liveData: {
-    id: 'app.containers.Admin.projects.project.insights.liveData',
-    defaultMessage: 'Live data',
-  },
   insightsSubtitle1: {
     id: 'app.containers.Admin.projects.project.insights.insightsSubtitle1',
     defaultMessage: 'Track engagement in real-time',
