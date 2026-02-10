9341d9c412dc09e3e60a1db21ee579fad70ab0ad
Amira Bayoumi <amira.bayoumii@gmail.com>
2026-02-10 11:55:12 +0100

    WIP remove useIdeasCount test

--- front/app/api/idea_count/useIdeasCount.test.ts
+++ front/app/api/idea_count/useIdeasCount.test.ts
@@ -1,58 +1,58 @@
-import { http, HttpResponse } from 'msw';
-import { setupServer } from 'msw/node';
+// import { http, HttpResponse } from 'msw';
+// import { setupServer } from 'msw/node';
 
-import createQueryClientWrapper from 'utils/testUtils/queryClientWrapper';
-import { renderHook, waitFor } from 'utils/testUtils/rtl';
+// import createQueryClientWrapper from 'utils/testUtils/queryClientWrapper';
+// import { renderHook, waitFor } from 'utils/testUtils/rtl';
 
-import useIdeasCount from './useIdeasCount';
+// import useIdeasCount from './useIdeasCount';
 
-const apiPath = '*stats/ideas_count';
+// const apiPath = '*stats/ideas_count';
 
-const statData = {
-  data: {
-    type: 'ideas_count',
-    attributes: {
-      count: 5,
-    },
-  },
-};
-const server = setupServer(
-  http.get(apiPath, () => {
-    return HttpResponse.json({ data: statData }, { status: 200 });
-  })
-);
+// const statData = {
+//   data: {
+//     type: 'ideas_count',
+//     attributes: {
+//       count: 5,
+//     },
+//   },
+// };
+// const server = setupServer(
+//   http.get(apiPath, () => {
+//     return HttpResponse.json({ data: statData }, { status: 200 });
+//   })
+// );
 
-describe('useIdeasCount', () => {
-  beforeAll(() => server.listen());
-  afterAll(() => server.close());
+// describe('useIdeasCount', () => {
+//   beforeAll(() => server.listen());
+//   afterAll(() => server.close());
 
-  it('returns data correctly', async () => {
-    const { result } = renderHook(() => useIdeasCount({}), {
-      wrapper: createQueryClientWrapper(),
-    });
+//   it('returns data correctly', async () => {
+//     const { result } = renderHook(() => useIdeasCount({}), {
+//       wrapper: createQueryClientWrapper(),
+//     });
 
-    expect(result.current.isLoading).toBe(true);
+//     expect(result.current.isLoading).toBe(true);
 
-    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+//     await waitFor(() => expect(result.current.isSuccess).toBe(true));
 
-    expect(result.current.isLoading).toBe(false);
-    expect(result.current.data?.data).toEqual(statData);
-  });
+//     expect(result.current.isLoading).toBe(false);
+//     expect(result.current.data?.data).toEqual(statData);
+//   });
 
-  it('returns error correctly', async () => {
-    server.use(
-      http.get(apiPath, () => {
-        return HttpResponse.json(null, { status: 500 });
-      })
-    );
+//   it('returns error correctly', async () => {
+//     server.use(
+//       http.get(apiPath, () => {
+//         return HttpResponse.json(null, { status: 500 });
+//       })
+//     );
 
-    const { result } = renderHook(() => useIdeasCount({}), {
-      wrapper: createQueryClientWrapper(),
-    });
+//     const { result } = renderHook(() => useIdeasCount({}), {
+//       wrapper: createQueryClientWrapper(),
+//     });
 
-    expect(result.current.isLoading).toBe(true);
-    await waitFor(() => expect(result.current.isError).toBe(true));
-    expect(result.current.error).toBeDefined();
-    expect(result.current.isLoading).toBe(false);
-  });
-});
+//     expect(result.current.isLoading).toBe(true);
+//     await waitFor(() => expect(result.current.isError).toBe(true));
+//     expect(result.current.error).toBeDefined();
+//     expect(result.current.isLoading).toBe(false);
+//   });
+// });
