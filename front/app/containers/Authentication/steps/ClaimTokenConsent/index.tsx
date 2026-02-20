42115671255af2a44d8fc9f1ad663e7daad6345b
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-02-20 10:02:37 -0300

    Change copy ClaimTokenConsent step, add different explanation per input type

--- front/app/containers/Authentication/steps/ClaimTokenConsent/index.tsx
+++ front/app/containers/Authentication/steps/ClaimTokenConsent/index.tsx
@@ -2,20 +2,27 @@ import React from 'react';
 
 import { Box, Text, Button } from '@citizenlab/cl2-component-library';
 
-import { FormattedMessage } from 'utils/cl-intl';
+import { FormattedMessage, MessageDescriptor } from 'utils/cl-intl';
 
 import messages from './messages';
 
+const INPUT_TYPE_TO_MESSAGE: Record<Props['inputType'], MessageDescriptor> = {
+  idea: messages.linkContributionTextIdeation,
+  proposal: messages.linkContributionTextProposal,
+  survey: messages.linkContributionTextSurvey,
+};
+
 interface Props {
+  inputType: 'idea' | 'proposal' | 'survey';
   onLink: () => void;
   onKeepAnonymous: () => void;
 }
 
-const ClaimTokenConsent = ({ onLink, onKeepAnonymous }: Props) => {
+const ClaimTokenConsent = ({ inputType, onLink, onKeepAnonymous }: Props) => {
   return (
     <Box>
       <Text mt="0px">
-        <FormattedMessage {...messages.youRecentlyParticipatedWhileLoggedOut} />
+        <FormattedMessage {...INPUT_TYPE_TO_MESSAGE[inputType]} />
       </Text>
       <Box display="flex" mt="32px">
         <Button width="auto" onClick={onLink} mr="12px">
--- front/app/containers/Authentication/steps/ClaimTokenConsent/messages.ts
+++ front/app/containers/Authentication/steps/ClaimTokenConsent/messages.ts
@@ -1,14 +1,20 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  linkYourContributions: {
-    id: 'app.containers.Authentication.steps.ClaimTokenConsent.linkYourContributions',
-    defaultMessage: 'Link your contributions to stay updated',
+  linkContributionTextIdeation: {
+    id: 'app.containers.Authentication.steps.ClaimTokenConsent.linkContributionTextIdeation',
+    defaultMessage:
+      'Do you want to link the input that you just submitted to your new account? By linking it to your account, you will be updated when other users or admins interacts with your input. You will also be able to edit or delete it. If you do not link your input, you will still receive updates about the project in general.',
   },
-  youRecentlyParticipatedWhileLoggedOut: {
+  linkContributionTextProposal: {
+    id: 'app.containers.Authentication.steps.ClaimTokenConsent.linkContributionTextProposal',
+    defaultMessage:
+      'Do you want to link the input that you just submitted to your new account? By linking it to your account, you will be updated when other users or admins interacts with your input. You will also be able to edit or delete it. If you do not link your input, you will still receive updates about the project in general.',
+  },
+  linkContributionTextSurvey: {
     id: 'app.containers.Authentication.steps.ClaimTokenConsent.youRecentlyParticipatedWhileLoggedOut',
     defaultMessage:
-      'You recently participated in one or more projects while logged out. Do you want to link these participations to your account? This will help you stay updated on the projects you participated in and receive relevant notifications.',
+      'Do you want to link the input that you just submitted to your new account? By linking it to your account, you will be able to find back your responses later.',
   },
   linkParticipations: {
     id: 'app.containers.Authentication.steps.ClaimTokenConsent.linkParticipations',
