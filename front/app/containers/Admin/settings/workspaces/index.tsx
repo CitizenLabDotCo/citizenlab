1c7b36cf4ad0a9334cd0bcaea2e17e42b17a2aa6
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-02-25 14:07:37 -0300

    Make sure page is only visible when feature enabled

--- front/app/containers/Admin/settings/workspaces/index.tsx
+++ front/app/containers/Admin/settings/workspaces/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
+import useFeatureFlag from 'hooks/useFeatureFlag';
+
 const WorkspacesMain = () => {
+  const workspacesEnabled = useFeatureFlag({ name: 'workspaces' });
+  if (!workspacesEnabled) return null;
+
   return (
     <div>
       <h1>Workspaces Settings</h1>
