5a80bc9a01c998f120e28fa8d608d279f773ac63
jinjagit <jinjamail@gmail.com>
2026-03-02 16:23:55 +0000

    [TAN-7205] Basic CRUD, serializer, and policy - no tests

---
+++ back/app/policies/space_policy.rb
@@ -0,0 +1,33 @@
+class SpacePolicy < ApplicationPolicy
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      @scope = scope.includes(:folders, :projects)
+
+      # TODO: Include space admins/managers here once those roles are implemented
+      active_admin? ? scope.all : scope.none
+    end
+  end
+
+  def create?
+    active_admin?
+  end
+
+  def show?
+    # TODO: Include space admins/managers here once those roles are implemented
+    active_admin?
+  end
+
+  def update?
+    active_admin?
+  end
+
+  def destroy?
+    active_admin?
+  end
+
+  private
+
+  def active_admin?
+    user&.admin? && user.active?
+  end
+end
