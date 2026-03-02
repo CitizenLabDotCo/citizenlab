1f8689eed49c3a308a096d2b570b91eaf5197a5c
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-03-02 12:19:03 -0300

    Scaffold new space page

--- front/app/containers/Admin/spaces/NewSpace/index.tsx
+++ front/app/containers/Admin/spaces/NewSpace/index.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
+import { Box, colors, stylingConsts } from '@citizenlab/cl2-component-library';
+
+import { SectionTitle } from 'components/admin/Section';
+import GoBackButton from 'components/UI/GoBackButton';
+
+import { FormattedMessage } from 'utils/cl-intl';
+import clHistory from 'utils/cl-router/history';
+
+import messages from './messages';
+
+const goBack = () => {
+  clHistory.goBack();
+};
+
 const NewSpace = () => {
-  return <></>;
+  return (
+    <Box px="92px" py="84px">
+      <GoBackButton onClick={goBack} />
+      <Box
+        bgColor={colors.white}
+        border={`1px solid ${colors.borderLight}`}
+        borderRadius={stylingConsts.borderRadius}
+        mt="20px"
+        px="52px"
+        py="44px"
+      >
+        <SectionTitle>
+          <FormattedMessage {...messages.createANewSpace} />
+        </SectionTitle>
+      </Box>
+    </Box>
+  );
 };
 
 export default NewSpace;
---
+++ front/app/containers/Admin/spaces/NewSpace/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  createANewSpace: {
+    id: 'app.containers.Admin.spaces.NewSpace.createANewSpace',
+    defaultMessage: 'Create a new space',
+  },
+});
