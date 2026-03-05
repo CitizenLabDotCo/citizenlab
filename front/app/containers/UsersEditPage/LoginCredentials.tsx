4f09b331aba6f2fddee25415c6872c49ae129d16
jamesspeake <james@speake.org>
2026-03-05 11:13:12 +0000

    [TAN-7268] Enable email change for SSO methods that do not return an email by default

--- front/app/containers/UsersEditPage/LoginCredentials.tsx
+++ front/app/containers/UsersEditPage/LoginCredentials.tsx
@@ -10,12 +10,35 @@ import { FormSection, FormSectionTitle } from 'components/UI/FormComponents';
 import { FormattedMessage } from 'utils/cl-intl';
 
 import messages from './messages';
+import useFeatureFlag from 'hooks/useFeatureFlag';
 
 type PasswordChangeProps = {
   user: IUserData;
 };
 
 const LoginCredentials = ({ user }: PasswordChangeProps) => {
+  const passwordLoginEnabled = useFeatureFlag({ name: 'password_login' });
+
+  // Also need to show the change email button for SSO methods that don't return an email by default (email_always_present? == false)
+  const claveUnicaEnabled = useFeatureFlag({ name: 'clave_unica_login' });
+  const idAustriaEnabled = useFeatureFlag({ name: 'id_austria_login' });
+  const nemloginEnabled = useFeatureFlag({ name: 'nemlog_in_login' });
+  const acmEnabled = useFeatureFlag({ name: 'acm_login' });
+  const criiptoEnabled = useFeatureFlag({ name: 'criipto_login' });
+  const twodayEnabled = useFeatureFlag({ name: 'twoday_login' });
+
+  const showChangePassword = passwordLoginEnabled;
+  const showChangeEmail =
+    passwordLoginEnabled ||
+    claveUnicaEnabled ||
+    idAustriaEnabled ||
+    nemloginEnabled ||
+    acmEnabled ||
+    criiptoEnabled ||
+    twodayEnabled;
+
+  if (!showChangeEmail && !showChangePassword) return null;
+
   const userHasPreviousPassword = !user.attributes.no_password;
 
   const passwordChangeButtonText = !userHasPreviousPassword
@@ -39,16 +62,18 @@ const LoginCredentials = ({ user }: PasswordChangeProps) => {
         >
           <FormattedMessage {...messages.changeEmail} />
         </ButtonWithLink>
-        <ButtonWithLink
-          linkTo="/profile/change-password"
-          scrollToTop
-          width="auto"
-          justifyWrapper="left"
-          buttonStyle="secondary-outlined"
-          icon="lock"
-        >
-          <FormattedMessage {...passwordChangeButtonText} />
-        </ButtonWithLink>
+        {showChangePassword && (
+          <ButtonWithLink
+            linkTo="/profile/change-password"
+            scrollToTop
+            width="auto"
+            justifyWrapper="left"
+            buttonStyle="secondary-outlined"
+            icon="lock"
+          >
+            <FormattedMessage {...passwordChangeButtonText} />
+          </ButtonWithLink>
+        )}
       </Box>
     </FormSection>
   );
