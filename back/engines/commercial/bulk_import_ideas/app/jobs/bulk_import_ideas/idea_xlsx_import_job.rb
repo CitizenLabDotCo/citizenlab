b9bf46fc823f8fcb0776686732607f4cf0c67fe2
Amanda Anderson <anderson_amanda@mail.com>
2026-03-09 17:03:30 +0100

    Separate out the import jobs into pdf and xlsx version

---
+++ back/engines/commercial/bulk_import_ideas/app/jobs/bulk_import_ideas/idea_xlsx_import_job.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module BulkImportIdeas
+  class IdeaXlsxImportJob < ApplicationJob
+    self.priority = 60
+    perform_retries false
+
+    def run(idea_import_files, import_user, locale, phase, personal_data_enabled, first_idea_index)
+      file_parser = Parsers::IdeaXlsxFileParser.new(import_user, locale, phase.id, personal_data_enabled)
+      import_service = Importers::IdeaImporter.new(import_user, locale)
+
+      idea_rows = []
+      idea_import_files.each do |file|
+        idea_rows += file_parser.parse_rows file
+      end
+
+      ideas = import_service.import(idea_rows)
+      users = import_service.imported_users
+
+      SideFxBulkImportService.new.after_success(import_user, phase, 'idea', 'xlsx', ideas, users)
+    rescue StandardError => e
+      e.params[:row] += first_idea_index if e.instance_of?(BulkImportIdeas::Error) && e.params[:row]
+      SideFxBulkImportService.new.after_failure(import_user, phase, 'idea', 'xlsx', e.to_s)
+      raise e
+    end
+  end
+end
