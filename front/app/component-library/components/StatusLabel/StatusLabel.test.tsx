b289ae5007d1d50065aee33c676e231d92e586f4
Iva <iva@citizenlab.co>
2026-03-09 17:49:47 +0200

    Fix unit tests

--- front/app/component-library/components/StatusLabel/StatusLabel.test.tsx
+++ front/app/component-library/components/StatusLabel/StatusLabel.test.tsx
@@ -46,8 +46,6 @@ describe('<StatusLabel />', () => {
     );
     const statusLabel = screen.getByText('Border Test');
     const style = window.getComputedStyle(statusLabel);
-    expect(style.border.toLowerCase()).toBe(
-      `1px solid ${colors.textSecondary}`.toLowerCase()
-    );
+    expect(style.border).toBe('1px solid rgb(89, 107, 122)');
   });
 });
