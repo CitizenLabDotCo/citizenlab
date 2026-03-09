d57bbf348b33b1db4c800d8448a4f7f9829d3fae
Iva <iva@citizenlab.co>
2026-03-09 15:29:02 +0200

    Delete mock

--- front/internals/jest/setup.js
+++ front/internals/jest/setup.js
@@ -136,27 +136,6 @@ HTMLCanvasElement.prototype.toDataURL = jest.fn(() => 'data:image/png;base64,');
 
 jest.mock('polished');
 jest.mock('@enzedonline/quill-blot-formatter2');
-jest.mock('history', () => ({
-  createBrowserHistory: () => ({
-    replace: jest.fn(),
-    length: 0,
-    location: {
-      pathname: '',
-      search: '',
-      state: '',
-      hash: '',
-    },
-    action: 'REPLACE',
-    push: jest.fn(),
-    go: jest.fn(),
-    goBack: jest.fn(),
-    goForward: jest.fn(),
-    block: jest.fn(),
-    listen: jest.fn(),
-    createHref: jest.fn(),
-    parsePath: jest.fn(),
-  }),
-}));
 
 jest.mock('utils/cl-router/Link');
 jest.mock('utils/cl-router/history');
