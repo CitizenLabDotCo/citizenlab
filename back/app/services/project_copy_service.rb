478a25299838826abaef744b986f8243174c06a9
Adrien Dessy <209268+adessy@users.noreply.github.com>
2026-03-10 18:24:42 +0100

    WIP

--- back/app/services/project_copy_service.rb
+++ back/app/services/project_copy_service.rb
@@ -312,8 +312,12 @@ class ProjectCopyService < TemplateService # rubocop:disable Metrics/ClassLength
   def yml_phases(shift_timestamps: 0, timeline_start_at: nil)
     if timeline_start_at && @project.phases.first
       kickoff_at = @project.phases.first.start_at
-      shift_timestamps = ((Date.parse(timeline_start_at).to_time - kickoff_at) / 1.day).to_i
+      timeline_start_at = AppConfiguration.timezone.parse(timeline_start_at)
+      # We could add support for arbitrary time shifts in the future, but for now we keep
+      # the existing behavior of shifting by a whole number of days
+      shift_timestamps = ((timeline_start_at.to_time - kickoff_at) / 1.day).to_i
     end
+
     @project.phases.map do |phase|
       yml_phase = {
         'project_ref' => lookup_ref(phase.project_id, :project),
