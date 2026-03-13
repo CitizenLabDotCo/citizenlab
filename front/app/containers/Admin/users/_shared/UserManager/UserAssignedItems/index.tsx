056a0e782b8b5363b16c61e5d8c7ca638b77d60f
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-03-13 11:34:38 -0300

    Fix empty states

--- front/app/containers/Admin/users/_shared/UserManager/UserAssignedItems/index.tsx
+++ front/app/containers/Admin/users/_shared/UserManager/UserAssignedItems/index.tsx
@@ -32,27 +32,37 @@ const UserAssignedItems = ({ user }: Props) => {
     );
   }
 
+  const hasFolders = foldersUserModerates.length > 0;
+
   return (
     <Box>
-      <Title variant="h3" mt="0px">
-        Folders user manages
-      </Title>
-      <Text>
-        This user can manage the following folders, including the projects
-        inside of them:
-      </Text>
-      <TreeView
-        nodes={foldersUserModerates}
-        lockedProjectTooltip={messages.lockedProject}
-        removeButtonMessage={messages.remove}
-      />
-      <Title variant="h3" mt="40px">
-        Projects user manages
-      </Title>
-      <TreeView
-        nodes={projectsUserModerates}
-        removeButtonMessage={messages.remove}
-      />
+      {hasFolders && (
+        <>
+          <Title variant="h3" mt="0px">
+            Folders user manages
+          </Title>
+          <Text>
+            This user can manage the following folders, including the projects
+            inside of them:
+          </Text>
+          <TreeView
+            nodes={foldersUserModerates}
+            lockedProjectTooltip={messages.lockedProject}
+            removeButtonMessage={messages.remove}
+          />
+        </>
+      )}
+      {projectsUserModerates.length > 0 && (
+        <>
+          <Title variant="h3" mt={hasFolders ? '40px' : '0px'}>
+            Projects user manages
+          </Title>
+          <TreeView
+            nodes={projectsUserModerates}
+            removeButtonMessage={messages.remove}
+          />
+        </>
+      )}
     </Box>
   );
 };
