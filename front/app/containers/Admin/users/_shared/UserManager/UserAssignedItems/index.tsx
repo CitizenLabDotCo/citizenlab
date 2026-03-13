7398f0c9d61567ebea0927c153c6c48d3214b3d8
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-03-12 21:07:30 -0300

    Add more explanation about folders

--- front/app/containers/Admin/users/_shared/UserManager/UserAssignedItems/index.tsx
+++ front/app/containers/Admin/users/_shared/UserManager/UserAssignedItems/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Box, Title } from '@citizenlab/cl2-component-library';
+import { Box, Title, Text } from '@citizenlab/cl2-component-library';
 
 import useTreeView from 'api/spaces/useTreeView';
 import { IUserData } from 'api/users/types';
@@ -27,15 +27,19 @@ const UserAssignedItems2 = ({ user }: Props) => {
   return (
     <Box>
       <Title variant="h3" mt="0px">
-        Folders user moderates
+        Folders user manages
       </Title>
+      <Text>
+        This user can manage the following folders, including the projects
+        inside of them:
+      </Text>
       <TreeView
         nodes={foldersUserModerates}
         lockedProjectTooltip={messages.lockedProject}
         removeButtonMessage={messages.remove}
       />
       <Title variant="h3" mt="40px">
-        Projects user moderates
+        Projects user manages
       </Title>
       <TreeView
         nodes={projectsUserModerates}
