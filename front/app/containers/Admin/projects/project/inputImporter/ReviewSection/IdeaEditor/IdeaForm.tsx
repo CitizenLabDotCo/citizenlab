5f073017e0768f2c538f640240f9a68800ae3e58
Amanda Anderson <anderson_amanda@mail.com>
2026-03-16 15:04:50 +0100

    Show errors on form load in FormSync so more obvious

--- front/app/containers/Admin/projects/project/inputImporter/ReviewSection/IdeaEditor/IdeaForm.tsx
+++ front/app/containers/Admin/projects/project/inputImporter/ReviewSection/IdeaEditor/IdeaForm.tsx
@@ -56,6 +56,11 @@ const IdeaForm = ({
     defaultValues: formData,
   });
 
+  // Trigger validation on mount so errors show immediately
+  useEffect(() => {
+    methods.trigger();
+  }, [methods]);
+
   // Ensure initial form validation state is correct
   useEffect(() => {
     setIdeaFormDataValid(methods.formState.isValid);
