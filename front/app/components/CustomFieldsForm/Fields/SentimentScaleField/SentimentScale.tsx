8ed4c2dba19467e7269a9ab7fd04762a01618176
EdwinKato <katedwin88@gmail.com>
2026-03-17 14:44:44 +0300

    Remove invalid aria-required from SentimentScale slider role

--- front/app/components/CustomFieldsForm/Fields/SentimentScaleField/SentimentScale.tsx
+++ front/app/components/CustomFieldsForm/Fields/SentimentScaleField/SentimentScale.tsx
@@ -77,7 +77,6 @@ const SentimentScale = ({ value: data, question, onChange }: Props) => {
       aria-valuemin={MINIMUM}
       aria-valuemax={MAXIMUM}
       aria-labelledby={`${sanitizeForClassname(id)}-label`}
-      aria-required={question.required}
       onKeyDown={(event) => {
         if (event.key !== 'Tab' && !event.metaKey) {
           // Don't override the default tab behaviour or meta key (E.g. Mac command key)
