16d8f7fd3a6a7fa4d60ca3c543aebbe7797ed652
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-03-18 18:08:07 -0300

    Make seats overview more distinct kind of page

--- front/app/containers/Admin/users/SeatsOverview/index.tsx
+++ front/app/containers/Admin/users/SeatsOverview/index.tsx
@@ -1,12 +1,14 @@
 import React, { useMemo } from 'react';
 
-import { Box, Title } from '@citizenlab/cl2-component-library';
+import { Box, Title, colors } from '@citizenlab/cl2-component-library';
 
 import useBilledSeats from 'api/users/useBilledSeats';
 
 import SeatInfo from 'components/admin/SeatBasedBilling/SeatInfo';
+import GoBackButton from 'components/UI/GoBackButton';
 
 import { FormattedMessage } from 'utils/cl-intl';
+import clHistory from 'utils/cl-router/history';
 
 import messages from './messages';
 import Table from './Table';
@@ -28,24 +30,39 @@ const SeatsOverview = () => {
   );
 
   return (
-    <Box>
-      <Title>
-        <FormattedMessage {...messages.seatsOverview} />
-      </Title>
-      <Title variant="h2">
-        <FormattedMessage {...messages.adminSeats} />
-      </Title>
-      <Box mb="20px">
-        <SeatInfo seatType="admin" />
+    <Box
+      bgColor={colors.white}
+      w="100%"
+      h="100vh"
+      py="45px"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+    >
+      <Box px="51px" maxWidth="1400px" w="100%">
+        <GoBackButton
+          onClick={() => {
+            clHistory.goBack();
+          }}
+        />
+        <Title>
+          <FormattedMessage {...messages.seatsOverview} />
+        </Title>
+        <Title variant="h2">
+          <FormattedMessage {...messages.adminSeats} />
+        </Title>
+        <Box mb="20px">
+          <SeatInfo seatType="admin" />
+        </Box>
+        <Table users={billedAdmins} />
+        <Title variant="h2" mt="40px">
+          <FormattedMessage {...messages.moderatorsSeats} />
+        </Title>
+        <Box mb="20px">
+          <SeatInfo seatType="moderator" />
+        </Box>
+        <Table users={billedModerators} />
       </Box>
-      <Table users={billedAdmins} />
-      <Title variant="h2" mt="40px">
-        <FormattedMessage {...messages.moderatorsSeats} />
-      </Title>
-      <Box mb="20px">
-        <SeatInfo seatType="moderator" />
-      </Box>
-      <Table users={billedModerators} />
     </Box>
   );
 };
