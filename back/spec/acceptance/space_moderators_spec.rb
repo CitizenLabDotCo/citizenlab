a5172f5c815aeadd075deff7a585300eef2abd00
jinjagit <jinjamail@gmail.com>
2026-03-19 10:38:37 +0000

    [TAN-7310] Add basic spave_moderator acceptance tests

--- back/spec/acceptance/space_moderators_spec.rb
+++ back/spec/acceptance/space_moderators_spec.rb
@@ -42,7 +42,7 @@ resource 'Moderators' do
 
       let(:moderator) { create(:space_moderator, spaces: [space]) }
 
-      example 'Get one moderator by id' do
+      example 'Get one space moderator by id' do
         do_request space_id: space.id, user_id: moderator.id
         expect(status).to eq 200
 
@@ -104,6 +104,64 @@ resource 'Moderators' do
       example '[error] List all moderators of a space not moderated by the user' do
         do_request space_id: other_space.id
         expect(status).to eq(401)
+      end
+    end
+
+    get 'web_api/v1/spaces/:space_id/moderators/:user_id' do
+      ValidationErrorHelper.new.error_fields(self, User)
+
+      let(:moderator) { create(:space_moderator, spaces: [space]) }
+
+      example 'Get one space moderator by id' do
+        do_request space_id: space.id, user_id: moderator.id
+        expect(status).to eq 200
+
+        json_response = json_parse(response_body)
+        expect(json_response.dig(:data, :id)).to eq moderator.id
+      end
+
+      example '[error] Get one space moderator by id of a space not moderated by the user' do
+        moderator_of_other_space = create(:space_moderator, spaces: [other_space])
+
+        do_request space_id: other_space.id, user_id: moderator_of_other_space.id
+        expect(status).to eq 401
+      end
+    end
+
+    post 'web_api/v1/spaces/:space_id/moderators' do
+      with_options scope: :space_moderator do
+        parameter :user_id, 'The id of user to become moderator (the id of the moderator will be the same).', required: true
+      end
+
+      ValidationErrorHelper.new.error_fields(self, User)
+
+      let(:user) { create(:user) }
+
+      example 'Add a space moderator role to a user' do
+        do_request space_id: space.id, space_moderator: { user_id: user.id }
+        expect(response_status).to eq 201
+
+        json_response = json_parse(response_body)
+        expect(json_response.dig(:data, :id)).to eq user.id
+        expect(user.reload.roles).to eq([{ 'type' => 'space_moderator', 'space_id' => space.id }])
+      end
+
+      example '[error] Add a space moderator role to a user of a space not moderated by the user' do
+        do_request space_id: other_space.id, space_moderator: { user_id: user.id }
+        expect(response_status).to eq 401
+      end
+    end
+
+    delete 'web_api/v1/spaces/:space_id/moderators/:user_id' do
+      ValidationErrorHelper.new.error_fields(self, User)
+
+      let(:moderator) { create(:space_moderator, spaces: [space]) }
+
+      example '[error] Remove a space moderator role from a user' do
+        do_request space_id: space.id, user_id: moderator.id
+        expect(response_status).to eq 401
+
+        expect(moderator.reload.roles).to eq([{ 'type' => 'space_moderator', 'space_id' => space.id }])
       end
     end
   end
