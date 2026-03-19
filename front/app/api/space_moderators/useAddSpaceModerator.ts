42ec7e8cb4f45b1469b95ce6387f0ceec734302f
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-03-19 16:55:06 -0300

    useAddSpaceModerator

---
+++ front/app/api/space_moderators/useAddSpaceModerator.ts
@@ -0,0 +1,53 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { CLErrors } from 'typings';
+
+import adminPublicationsKeys from 'api/admin_publications/keys';
+import invalidateSeatsCache from 'api/seats/invalidateSeatsCache';
+import usersKeys from 'api/users/keys';
+import { IUsers } from 'api/users/types';
+import userCountKeys from 'api/users_count/keys';
+
+import fetcher from 'utils/cl-react-query/fetcher';
+
+import spaceModeratorsKeys from './keys';
+
+interface Params {
+  spaceId: string;
+  userId: string;
+}
+
+const addSpaceModerator = ({ spaceId, userId }: Params) => {
+  return fetcher<IUsers>({
+    path: `/spaces/${spaceId}/moderators`,
+    action: 'post',
+    body: {
+      space_moderator: {
+        user_id: userId,
+      },
+    },
+  });
+};
+
+const useAddSpaceModerator = () => {
+  const queryClient = useQueryClient();
+  return useMutation<IUsers, CLErrors, Params>({
+    mutationFn: addSpaceModerator,
+    onSuccess: async (_data, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: spaceModeratorsKeys.list({
+          spaceId: variables.spaceId,
+        }),
+      });
+      queryClient.invalidateQueries({ queryKey: usersKeys.lists() });
+      queryClient.invalidateQueries({
+        queryKey: userCountKeys.items(),
+      });
+      queryClient.invalidateQueries({
+        queryKey: adminPublicationsKeys.lists(),
+      });
+      invalidateSeatsCache();
+    },
+  });
+};
+
+export default useAddSpaceModerator;
