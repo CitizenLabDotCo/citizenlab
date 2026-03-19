98ab19bc00d71817c90bd05facf7dce8106be38c
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-03-19 18:24:38 -0300

    Put back AddByEmail

---
+++ front/app/components/admin/AddByEmail/messages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  typeEmailToInviteModerator: {
+    id: 'app.components.admin.AddByEmail.inviteModeratorByEmail',
+    defaultMessage: 'Type email to invite moderator...',
+  },
+  invite: {
+    id: 'app.components.admin.AddByEmail.invite',
+    defaultMessage: 'Invite',
+  },
+});
