eedce13fb38289c6c6dd05ef547d7d6bd940ce95
jamesspeake <james@speake.org>
2026-03-26 16:46:42 +0000

    [TAN-6530] updated index with safety assured

--- back/db/migrate/20260303191050_add_token_expiry_key_to_users.rb
+++ back/db/migrate/20260303191050_add_token_expiry_key_to_users.rb
@@ -1,6 +1,8 @@
 class AddTokenExpiryKeyToUsers < ActiveRecord::Migration[7.2]
   def change
-    add_column :users, :token_expiry_key, :string
-    add_index :users, :token_expiry_key
+    safety_assured do
+      add_column :users, :token_expiry_key, :string
+      add_index :users, :token_expiry_key
+    end
   end
 end
