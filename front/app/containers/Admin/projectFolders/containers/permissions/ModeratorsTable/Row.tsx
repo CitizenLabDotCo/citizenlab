7aa8531ba612bcbfd02e90f349e35cbe5641141a
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-03-26 12:03:25 -0300

    _

---
+++ front/app/containers/Admin/projectFolders/containers/permissions/ModeratorsTable/Row.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { IUserData } from 'api/users/types';
+
+interface Props {
+  moderator: IUserData;
+}
+
+const Row = ({ moderator }: Props) => {
+  return <></>;
+};
+
+export default Row;
---
+++ front/app/containers/Admin/projectFolders/containers/permissions/ModeratorsTable/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import {
+  Table as TableComponent,
+  Thead,
+  Tr,
+  Tbody,
+  colors,
+  stylingConsts,
+} from '@citizenlab/cl2-component-library';
+
+import { IUserData } from 'api/users/types';
+
+import HeaderCell from 'components/admin/UsersTable/HeaderCell';
+import userTableMessages from 'components/admin/UsersTable/messages';
+
+import Row from './Row';
+
+interface Props {
+  moderators: IUserData[];
+}
+
+const ModeratorsTable = ({ moderators }: Props) => {
+  return (
+    <TableComponent
+      border={`1px solid ${colors.grey300}`}
+      borderRadius={stylingConsts.borderRadius}
+      innerBorders={{ bodyRows: true }}
+    >
+      <Thead>
+        <Tr background={colors.grey50}>
+          <HeaderCell message={userTableMessages.name} />
+          <HeaderCell message={userTableMessages.role} />
+          <HeaderCell message={userTableMessages.inviteStatus} />
+          <HeaderCell message={userTableMessages.options} />
+        </Tr>
+      </Thead>
+      <Tbody>
+        {moderators.map((moderator) => (
+          <Row key={moderator.id} moderator={moderator} />
+        ))}
+      </Tbody>
+    </TableComponent>
+  );
+};
+
+export default ModeratorsTable;
