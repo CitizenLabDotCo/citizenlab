5f31e47333c706e6d27fd96f8bda88f75a1142c9
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-04-06 13:56:24 +0200

    _

--- front/app/containers/Admin/projectFolders/containers/settings/ProjectFolderForm/index.tsx
+++ front/app/containers/Admin/projectFolders/containers/settings/ProjectFolderForm/index.tsx
@@ -5,6 +5,7 @@ import { isEmpty, isEqual } from 'lodash-es';
 import { CLErrors, Multiloc, UploadFile } from 'typings';
 
 import useAdminPublication from 'api/admin_publications/useAdminPublication';
+import useAuthUser from 'api/me/useAuthUser';
 import useAddProjectFolderFile from 'api/project_folder_files/useAddProjectFolderFile';
 import useProjectFolderFiles from 'api/project_folder_files/useProjectFolderFiles';
 import {
@@ -47,6 +48,7 @@ import { FormattedMessage, useIntl } from 'utils/cl-intl';
 import clHistory from 'utils/cl-router/history';
 import { convertUrlToUploadFile } from 'utils/fileUtils';
 import { isNilOrError, isError } from 'utils/helperUtils';
+import { isSpaceModerator } from 'utils/permissions/roles';
 import { validateSlug } from 'utils/textUtils';
 
 import messages from '../../messages';
@@ -107,6 +109,8 @@ const ProjectFolderForm = ({ mode, projectFolderId }: Props) => {
     useFeatureFlag({
       name: 'project_description_builder',
     }) && projectFolder; // description builder cannot be used when creating a folder
+
+  const { data: authUser } = useAuthUser();
 
   /*
     ==============
@@ -309,6 +313,7 @@ const ProjectFolderForm = ({ mode, projectFolderId }: Props) => {
 
   const validate = useCallback(() => {
     let valid = false;
+
     if (!isNilOrError(tenantLocales)) {
       // check that all fields have content for all tenant locales
       valid = tenantLocales.every(
@@ -318,6 +323,11 @@ const ProjectFolderForm = ({ mode, projectFolderId }: Props) => {
           !isEmpty(shortDescriptionMultiloc?.[locale])
       );
     }
+
+    if (isSpaceModerator(authUser) && !spaceId) {
+      valid = false;
+    }
+
     if (!valid) {
       setSubmitState('error');
     }
@@ -328,6 +338,8 @@ const ProjectFolderForm = ({ mode, projectFolderId }: Props) => {
     titleMultiloc,
     descriptionMultiloc,
     shortDescriptionMultiloc,
+    authUser,
+    spaceId,
   ]);
 
   const saveForm = async () => {
