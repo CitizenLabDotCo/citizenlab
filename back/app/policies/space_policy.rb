db6805168185fe5f5da509ae4bd67e7e52517b16
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-04-06 09:43:08 +0200

    Fix policy scope

--- back/app/policies/space_policy.rb
+++ back/app/policies/space_policy.rb
@@ -1,10 +1,17 @@
 class SpacePolicy < ApplicationPolicy
   class Scope < ApplicationPolicy::Scope
     def resolve
-      @scope = scope.includes(:folders, :projects)
+      scope = scope.includes(:folders, :projects)
 
-      # TODO: Include space admins/managers here once those roles are implemented
-      active_admin? ? scope.all : scope.none
+      if active_admin?
+        return scope.all
+      end
+
+      if user.space_moderator?
+        return scope.where(id: user.moderated_space_ids)
+      end
+
+      scope.none
     end
   end
 
