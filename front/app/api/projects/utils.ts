9db66077d02500d56d0a6881ddaf5509dd45a180
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-04-07 14:45:03 +0200

    Expand cache invalidation on CRUD project

--- front/app/api/projects/utils.ts
+++ front/app/api/projects/utils.ts
@@ -1,5 +1,36 @@
 import { RouteType } from 'routes';
 
+import adminPublicationsKeys from 'api/admin_publications/keys';
+import adminPublicationsStatusCountsKeys from 'api/admin_publications_status_counts/keys';
+import areasKeys from 'api/areas/keys';
+import globalTopicsKeys from 'api/global_topics/keys';
+import meKeys from 'api/me/keys';
+import projectsMiniAdminKeys from 'api/projects_mini_admin/keys';
+import spacesKeys from 'api/spaces/keys';
+
+import { queryClient } from 'utils/cl-react-query/queryClient';
+
+import projectsKeys from './keys';
+
 export function getProjectUrl(slug: string): RouteType {
   return `/projects/${slug}`;
 }
+
+export const invalidateOnCRUD = () => {
+  queryClient.invalidateQueries({ queryKey: projectsKeys.all() });
+  queryClient.invalidateQueries({ queryKey: globalTopicsKeys.lists() });
+  queryClient.invalidateQueries({ queryKey: areasKeys.lists() });
+  queryClient.invalidateQueries({
+    queryKey: adminPublicationsKeys.all(),
+  });
+  queryClient.invalidateQueries({
+    queryKey: adminPublicationsStatusCountsKeys.items(),
+  });
+  queryClient.invalidateQueries({ queryKey: meKeys.all() });
+  queryClient.invalidateQueries({
+    queryKey: projectsMiniAdminKeys.lists(),
+  });
+  queryClient.invalidateQueries({
+    queryKey: spacesKeys.all(),
+  });
+};
