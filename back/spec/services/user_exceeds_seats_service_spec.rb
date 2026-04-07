394525c2dfb660dab8792ec301e3c67efc5ebd66
jinjagit <jinjamail@gmail.com>
2026-04-07 15:11:20 +0100

    [TAN-7344] 3 small tweaks

--- back/spec/services/user_exceeds_seats_service_spec.rb
+++ back/spec/services/user_exceeds_seats_service_spec.rb
@@ -1,17 +1,8 @@
-# frozen_string_literal: true
-
-# filepath: /Users/work/cl/citizenlab/back/spec/services/user_exceeds_seats_service_spec.rb
-
 require 'rails_helper'
 
 describe UserExceedsSeatsService do
   let(:config) { AppConfiguration.instance }
   let(:user) { create(:user) }
-
-  before do
-    config.settings['core'] ||= {}
-    config.save!
-  end
 
   context 'when user assigned moderator' do
     let(:params) { { 'seat_type' => 'moderator', 'user_id' => user.id } }
