040347ec6ed437bfc31c6e71c9d08966cff654e0
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-04-10 12:06:04 -0300

    Add tab for space managers

---
+++ front/app/containers/Admin/users/tabs/SpaceModerators.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { Box } from '@citizenlab/cl2-component-library';
+import styled from 'styled-components';
+
+import SeatInfo from 'components/admin/SeatBasedBilling/SeatInfo';
+
+import UserManager from '../_shared/UserManager';
+import UsersHeader from '../_shared/UsersHeader';
+import messages from '../messages';
+
+const StyledBox = styled(Box)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-auto-rows: 1fr;
+`;
+
+const SpaceModerators = () => (
+  <>
+    <UsersHeader title={messages.spaceManagers} />
+    <UserManager spaceModeratorsOnly notCitizenlabMember includeInactive />
+    <StyledBox mt="20px">
+      <SeatInfo seatType="moderator" />
+    </StyledBox>
+  </>
+);
+
+export default SpaceModerators;
