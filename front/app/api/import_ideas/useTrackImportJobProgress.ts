89ef2bace410ceaca7243132639e52119ae27473
AchrafGoVocal <achraf@govocal.com>
2026-04-14 14:17:14 +0200

    [TAN-7380] Clean up some logic into a hook

---
+++ front/app/api/import_ideas/useTrackImportJobProgress.ts
@@ -0,0 +1,16 @@
+import useImportJobProgress from './useImportJobProgress';
+
+const useTrackImportJobProgress = (phaseId: string) => {
+  const { data } = useImportJobProgress(phaseId);
+
+  const latestJob = data?.data[0];
+
+  return {
+    importing: latestJob?.attributes.completed_at === null,
+    importHasErrors: (latestJob?.attributes.error_count ?? 0) > 0,
+    importProgress: latestJob?.attributes.progress ?? 0,
+    importTotal: latestJob?.attributes.total ?? 0,
+  };
+};
+
+export default useTrackImportJobProgress;
