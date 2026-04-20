18bc2bea881a31a2272f4423e5b5a5a3fc248b3c
AchrafGoVocal <achraf@govocal.com>
2026-04-20 10:14:07 +0200

    [TAN-7380] Update parameter documentation and add small test for filtering jobs

--- back/spec/acceptance/jobs_spec.rb
+++ back/spec/acceptance/jobs_spec.rb
@@ -14,6 +14,7 @@ resource 'Jobs' do
     parameter :context_type, 'Filter by context type', required: false, type: :string
     parameter :context_id, 'Filter by context id', required: false, type: :string
     parameter :completed, 'Filter by completion status', required: false, type: :boolean
+    parameter :root_job_type, 'Filter by root job type', required: false, type: :string
 
     let_it_be(:jobs) { create_list(:jobs_tracker, 3) }
 
@@ -82,6 +83,16 @@ resource 'Jobs' do
             expect(status).to eq(200)
             expect(response_ids).to match_array(jobs.map(&:id))
           end
+        end
+      end
+
+      describe 'filter by root job type' do
+        let!(:job) { create(:jobs_tracker, root_job_type: 'SomeSpecificJob') }
+        let(:root_job_type) { 'SomeSpecificJob' }
+
+        example_request 'Filter by root job type' do
+          expect(status).to eq(200)
+          expect(response_ids).to eq [job.id]
         end
       end
     end
