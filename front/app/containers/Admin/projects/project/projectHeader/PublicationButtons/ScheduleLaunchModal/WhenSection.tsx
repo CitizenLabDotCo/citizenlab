56a7dd048610ad252baac38c1861c3dc05909d4a
Iva <iva@citizenlab.co>
2026-04-20 15:54:57 +0300

    Add constraints to date time selection

--- front/app/containers/Admin/projects/project/projectHeader/PublicationButtons/ScheduleLaunchModal/WhenSection.tsx
+++ front/app/containers/Admin/projects/project/projectHeader/PublicationButtons/ScheduleLaunchModal/WhenSection.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import { Box, Text } from '@citizenlab/cl2-component-library';
+import moment from 'moment-timezone';
 
 import useAppConfiguration from 'api/app_configuration/useAppConfiguration';
 
 import DateSinglePicker from 'components/admin/DatePickers/DateSinglePicker';
+import TimeInput from 'components/admin/DateTimeSelection/TimeInput';
 import { useIntl } from 'utils/cl-intl';
 
 import messages from './messages';
-import TimeInput from 'components/admin/DateTimeSelection/TimeInput';
 
 interface Props {
   selectedDate: Date;
@@ -27,13 +28,15 @@ const WhenSection = ({
   const { data: appConfiguration } = useAppConfiguration();
   const timezone =
     appConfiguration?.data.attributes.settings.core.timezone ?? '';
+  const tenantTimeNow = timezone ? moment().tz(timezone).toDate() : new Date();
+  const gmtOffset = timezone ? moment().tz(timezone).format('Z') : '';
 
   return (
     <Box mb="8px">
       <Text fontWeight="bold" mb="12px">
         {formatMessage(messages.when)}
       </Text>
-      <Box display="flex" gap="12px">
+      <Box display="flex" gap="12px" alignItems="center">
         <DateSinglePicker
           selectedDate={selectedDate}
           onChange={(date) => {
@@ -44,11 +47,17 @@ const WhenSection = ({
             onDateChange(date);
           }}
           placement="right"
+          disabledPast={{ before: new Date() }}
         />
-        <TimeInput selectedTime={selectedTime} onChange={onTimeChange} />
+        <TimeInput
+          selectedTime={selectedTime}
+          onChange={onTimeChange}
+          selectedDate={selectedDate}
+          currentTimeInTz={tenantTimeNow}
+        />
         {timezone && (
           <Text color="grey600" fontSize="s">
-            {timezone}
+            GMT{gmtOffset}
           </Text>
         )}
       </Box>
