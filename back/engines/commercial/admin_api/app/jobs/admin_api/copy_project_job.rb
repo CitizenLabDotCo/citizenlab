e22eeb4f44314941ccffc31d10877cb12bbc465a
Sébastien Hoorens <sebastien@citizenlab.co>
2026-04-20 14:48:42 +0200

    requested: user_id -> back/engines/commercial/admin_api/app/jobs/admin_api/copy_project_job.rb

--- back/engines/commercial/admin_api/app/jobs/admin_api/copy_project_job.rb
+++ back/engines/commercial/admin_api/app/jobs/admin_api/copy_project_job.rb
@@ -8,10 +8,10 @@ module AdminApi
     # references (that are encoded with anchors and aliases in the YAML) that are used to
     # represent associations between the different models. If we were to use a hash,
     # those links would be lost during the serialization-deserialization of job arguments.
-    def run(template_yaml, user_id, folder_id = nil)
+    def run(template_yaml, moderator_id, folder_id = nil)
       check_in_tenant!
       folder = ProjectFolders::Folder.find(folder_id) if folder_id
-      moderator = User.find(user_id)
+      moderator = User.find(moderator_id)
       template = ::MultiTenancy::Templates::Utils.parse_yml(template_yaml)
       project = ProjectCopyService.new.import(template, folder: folder)
       SideFxProjectService.new.after_create(project, moderator)
