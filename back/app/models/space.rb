8eb9eef04ed3bdf3fb29f966ca3eacb39c73d188
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-04-20 13:26:57 -0300

    Add search to BE

--- back/app/models/space.rb
+++ back/app/models/space.rb
@@ -9,9 +9,15 @@
 #  updated_at           :datetime         not null
 #
 class Space < ApplicationRecord
+  include PgSearch::Model
+
   has_many :projects, dependent: :nullify
   has_many :folders, dependent: :nullify, class_name: 'ProjectFolders::Folder'
 
   validates :title_multiloc, presence: true, multiloc: { presence: true }
   validates :description_multiloc, multiloc: { presence: false, html: true }
+
+  pg_search_scope :search_by_title,
+    against: :title_multiloc,
+    using: { tsearch: { prefix: true } }
 end
