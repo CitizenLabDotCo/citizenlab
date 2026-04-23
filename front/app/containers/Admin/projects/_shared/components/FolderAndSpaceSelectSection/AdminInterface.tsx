89766aa8cc3f0525f6b083082c4b3543a1851b88
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-04-23 18:30:12 -0300

    _

--- front/app/containers/Admin/projects/_shared/components/FolderAndSpaceSelectSection/AdminInterface.tsx
+++ front/app/containers/Admin/projects/_shared/components/FolderAndSpaceSelectSection/AdminInterface.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import useProjectFolders from 'api/project_folders/useProjectFolders';
+// import useProjectFolders from 'api/project_folders/useProjectFolders';
 
 import SpaceSelectSection from 'components/admin/SpaceSelectSection';
 
 import { Props } from './types';
 
 const AdminInterface = ({ space_id, folder_id }: Props) => {
-  const { data: folders } = useProjectFolders();
+  // const { data: folders } = useProjectFolders();
 
   return (
     <SpaceSelectSection
