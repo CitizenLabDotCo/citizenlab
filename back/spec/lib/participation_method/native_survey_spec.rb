fe7026628bfef9260e485639676168f3231663f9
Amanda Anderson <anderson_amanda@mail.com>
2026-04-24 09:15:37 +0200

    Update specs after changing default survey fields

--- back/spec/lib/participation_method/native_survey_spec.rb
+++ back/spec/lib/participation_method/native_survey_spec.rb
@@ -71,33 +71,11 @@ RSpec.describe ParticipationMethod::NativeSurvey do
       # in the database, but not necessarily in memory.
       phase_in_db = Phase.find(phase.id)
 
-      expect(phase_in_db.custom_form.custom_fields.size).to eq 3
+      expect(phase_in_db.custom_form.custom_fields.size).to eq 2
 
       question_page = phase_in_db.custom_form.custom_fields[0]
       expect(question_page.title_multiloc).to eq({})
       expect(question_page.description_multiloc).to eq({})
-
-      field = phase_in_db.custom_form.custom_fields[1]
-      expect(field.title_multiloc).to match({
-        'en' => 'Default question',
-        'fr-FR' => 'Question par défaut',
-        'nl-NL' => 'Standaardvraag'
-      })
-      expect(field.description_multiloc).to eq({})
-      options = field.options
-      expect(options.size).to eq 2
-      expect(options[0].key).to eq 'option1'
-      expect(options[1].key).to eq 'option2'
-      expect(options[0].title_multiloc).to match({
-        'en' => 'First option',
-        'fr-FR' => 'Première option',
-        'nl-NL' => 'Eerste optie'
-      })
-      expect(options[1].title_multiloc).to match({
-        'en' => 'Second option',
-        'fr-FR' => 'Deuxième option',
-        'nl-NL' => 'Tweede optie'
-      })
     end
   end
 
