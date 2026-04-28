49bbd63b6ee1ae923186d1328ae353aca5b18b96
jinjagit <jinjamail@gmail.com>
2026-04-28 12:11:06 +0100

    [TAN-7562] Don't change TrackIntercomService in this PR branch

--- back/app/services/track_intercom_service.rb
+++ back/app/services/track_intercom_service.rb
@@ -32,7 +32,7 @@ class TrackIntercomService
   def track_user?(user)
     return false if user.super_admin?
 
-    user.admin? || user.moderator?
+    user.admin? || user.project_moderator?
   end
 
   def identify_tenant(tenant)
