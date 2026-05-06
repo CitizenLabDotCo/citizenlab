a9a508622e112bab3939c6666d7242bf891bcd30
Amira Bayoumi <amira.bayoumii@gmail.com>
2026-05-06 12:49:59 +0200

    keep link margin bottom

--- front/app/containers/MainHeader/Components/ProjectsListItem/index.tsx
+++ front/app/containers/MainHeader/Components/ProjectsListItem/index.tsx
@@ -10,6 +10,7 @@ const ProjectsListItem = styled(Link)`
   line-height: 21px;
   text-decoration: none;
   padding: 10px;
+  margin-bottom: 4px;
   display: block;
   background: transparent;
   border-radius: ${(props) => props.theme.borderRadius};
