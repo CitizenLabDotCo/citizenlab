43162fd62b121a1b72472c094bbbf9f7feed36af
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-05-12 16:28:01 -0300

    Add placeholder in all input page

--- front/app/components/IdeaCards/messages.ts
+++ front/app/components/IdeaCards/messages.ts
@@ -249,4 +249,8 @@ export default defineMessages({
     id: 'app.containers.IdeaCards.searchContributions',
     defaultMessage: 'Search contributions',
   },
+  searchPosts: {
+    id: 'app.containers.IdeaCards.searchPosts',
+    defaultMessage: 'Search posts',
+  },
 });
