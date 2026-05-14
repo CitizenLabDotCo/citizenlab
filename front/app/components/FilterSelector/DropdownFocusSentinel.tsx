47b9dfb9988094177fbe4a647ba624f56485a91b
jamesspeake <james@speake.org>
2026-05-14 13:14:45 +0100

    [TAN-7782] used existing hidden styling code

--- front/app/components/FilterSelector/DropdownFocusSentinel.tsx
+++ front/app/components/FilterSelector/DropdownFocusSentinel.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import { invisibleA11yText } from '@citizenlab/cl2-component-library';
+import styled from 'styled-components';
+
 import { useIntl } from 'utils/cl-intl';
 
 import messages from './messages';
+
+const HiddenButton = styled.button`
+  ${invisibleA11yText()}
+`;
 
 interface Props {
   opened: boolean;
@@ -15,25 +22,14 @@ const DropdownFocusSentinel = ({ opened, onClose }: Props) => {
   const { formatMessage } = useIntl();
 
   return (
-    <button
+    <HiddenButton
       tabIndex={opened ? 0 : -1}
       aria-hidden={!opened}
       onFocus={() => onClose()}
       onClick={onClose}
-      style={{
-        position: 'absolute',
-        width: 1,
-        height: 1,
-        padding: 0,
-        margin: -1,
-        overflow: 'hidden',
-        clip: 'rect(0, 0, 0, 0)',
-        whiteSpace: 'nowrap',
-        border: 0,
-      }}
     >
       {formatMessage(messages.closeMenu)}
-    </button>
+    </HiddenButton>
   );
 };
 
