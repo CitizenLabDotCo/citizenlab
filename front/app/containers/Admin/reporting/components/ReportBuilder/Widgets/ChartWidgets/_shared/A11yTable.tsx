3c64d047589d48ae3a1a3bc6c8cacf48a8329e38
Amira Bayoumi <amira.bayoumii@gmail.com>
2026-05-18 11:10:20 +0200

    add screenreaderonly wrapper to a11ytable

--- front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ChartWidgets/_shared/A11yTable.tsx
+++ front/app/containers/Admin/reporting/components/ReportBuilder/Widgets/ChartWidgets/_shared/A11yTable.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import { invisibleA11yText } from 'component-library/utils/styleUtils';
-import styled from 'styled-components';
+import { ScreenReaderOnly } from 'utils/a11y';
 
 interface Column {
   key: string;
@@ -14,43 +13,44 @@ interface DataTableProps {
   data: any[];
   caption: string;
 }
-const StyledTable = styled('table')`
-  ${invisibleA11yText}
-`;
 
 const A11yTable: React.FC<DataTableProps> = ({ columns, data, caption }) => {
   return (
-    <StyledTable role="table">
-      <caption>{caption}</caption>
-      <thead>
-        <tr>
-          {columns.map((column) => (
-            <th key={column.key} scope="col">
-              {column.label}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {columns.map((column, colIndex) => {
-              const value = row[column.key];
-              const content = column.render ? column.render(value, row) : value;
-
-              if (colIndex === 0) {
-                return (
-                  <th key={column.key} scope="row">
-                    {content}
-                  </th>
-                );
-              }
-              return <td key={column.key}>{content}</td>;
-            })}
+    <ScreenReaderOnly>
+      <table role="table">
+        <caption>{caption}</caption>
+        <thead>
+          <tr>
+            {columns.map((column) => (
+              <th key={column.key} scope="col">
+                {column.label}
+              </th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </StyledTable>
+        </thead>
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {columns.map((column, colIndex) => {
+                const value = row[column.key];
+                const content = column.render
+                  ? column.render(value, row)
+                  : value;
+
+                if (colIndex === 0) {
+                  return (
+                    <th key={column.key} scope="row">
+                      {content}
+                    </th>
+                  );
+                }
+                return <td key={column.key}>{content}</td>;
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </ScreenReaderOnly>
   );
 };
 
