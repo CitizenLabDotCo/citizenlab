6e93fea5a955fc8f4638e6a6f17bbfda13a493c9
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-05-20 16:47:43 -0300

    Simplify poll spec

--- back/engines/free/polls/spec/policies/response_policy_spec.rb
+++ back/engines/free/polls/spec/policies/response_policy_spec.rb
@@ -21,12 +21,7 @@ describe Polls::ResponsePolicy do
   end
 
   context 'for a resident' do
-    let(:user) do
-      user = response.user
-      EmailConfirmation.create!(user:)
-      user.email_confirmation.confirm!
-      user
-    end
+    let(:user) { response.user }
 
     it { is_expected.to permit(:create) }
 
