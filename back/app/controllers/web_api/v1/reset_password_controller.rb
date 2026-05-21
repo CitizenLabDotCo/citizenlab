82b7e02e76ba2ed21f11e8411959fb51e020af10
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-05-21 13:56:28 -0300

    Use confirm! in reset_password_controller instead of manually setting confirmation_required etc

--- back/app/controllers/web_api/v1/reset_password_controller.rb
+++ back/app/controllers/web_api/v1/reset_password_controller.rb
@@ -30,11 +30,8 @@ class WebApi::V1::ResetPasswordController < ApplicationController
       )
 
       # Resetting the password also proves that the user has access to the email,
-      # so we can confirm the user if they were pending confirmation.
-      if @user.confirmation_required?
-        @user.email_confirmed_at = Time.zone.now
-        @user.confirmation_required = false
-      end
+      # so we can confirm the user if they were pending confirmation. 
+      @user.email_confirmation.confirm! if @user.confirmation_required?
 
       if @user.save
         render json: WebApi::V1::UserSerializer.new(
