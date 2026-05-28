60e1be310b41ad6ada3d5ccaeaf2bf9b954e74d0
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-05-28 17:43:40 -0300

    Remove ignored columns

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -221,13 +221,6 @@ class User < ApplicationRecord
   scope :not_blocked, -> { where(block_end_at: nil).or(where('? > block_end_at', Time.zone.now)) }
   scope :active, -> { registered.not_blocked }
 
-  self.ignored_columns = %w[
-    email_confirmation_code
-    email_confirmation_retry_count
-    email_confirmation_code_reset_count
-    email_confirmation_code_sent_at
-  ]
-
   def update_merging_custom_fields!(attributes)
     attributes = attributes.deep_stringify_keys
     update!(
