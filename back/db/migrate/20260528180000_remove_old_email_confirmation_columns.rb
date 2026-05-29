299c871ad6617d730fa67423191d959fcf695a1c
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-05-29 11:12:31 -0300

    Wrap in safety_assured

--- back/db/migrate/20260528180000_remove_old_email_confirmation_columns.rb
+++ back/db/migrate/20260528180000_remove_old_email_confirmation_columns.rb
@@ -2,10 +2,12 @@
 
 class RemoveOldEmailConfirmationColumns < ActiveRecord::Migration[7.2]
   def up
-    remove_column :users, :email_confirmation_code, :string
-    remove_column :users, :email_confirmation_retry_count, :integer
-    remove_column :users, :email_confirmation_code_reset_count, :integer
-    remove_column :users, :email_confirmation_code_sent_at, :datetime
+    safety_assured do
+      remove_column :users, :email_confirmation_code, :string
+      remove_column :users, :email_confirmation_retry_count, :integer
+      remove_column :users, :email_confirmation_code_reset_count, :integer
+      remove_column :users, :email_confirmation_code_sent_at, :datetime
+    end
   end
 
   def down
