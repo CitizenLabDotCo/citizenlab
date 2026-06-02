96ccb5aafab10a4b4cbe699bce8fcad7bec072f2
EdwinKato <katedwin88@gmail.com>
2026-06-02 11:55:14 +0300

    Add navigation messages for redesigned project back office

---
+++ front/app/containers/Admin/projects/project/newBackoffice/messages.ts
@@ -0,0 +1,17 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  projectPageNav: {
+    id: 'app.containers.Admin.projects.project.projectPageNav',
+    defaultMessage: 'Project page',
+  },
+  settingsNav: {
+    id: 'app.containers.Admin.projects.project.settingsNav',
+    defaultMessage: 'Settings',
+  },
+  projectPagePlaceholder: {
+    id: 'app.containers.Admin.projects.project.projectPagePlaceholder',
+    defaultMessage:
+      'Project page preview and content builder are coming in a later step.',
+  },
+});
