b20924e794c2b61097bc165c547939909cbd8dcb
stephanie rousset <strabon@club-internet.fr>
2026-06-03 11:32:45 +0200

    fix: remove project_widjet from types

--- front/app/api/app_configuration/types.ts
+++ front/app/api/app_configuration/types.ts
@@ -234,7 +234,6 @@ export interface IAppConfigurationSettings {
   project_scheduling?: AppConfigurationFeature;
   draft_phase_description?: AppConfigurationFeature;
   email_scheduling?: AppConfigurationFeature;
-  project_widget?: AppConfigurationFeature;
   phase_datetime_setup?: AppConfigurationFeature;
   custom_smtp?: AppConfigurationFeature;
   hide_submission_removal_text?: AppConfigurationFeature;
