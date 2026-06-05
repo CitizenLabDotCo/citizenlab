bee7aa610daf7c5540014b8432683e7a30f03608
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-06-05 13:13:40 -0300

    Refactor useAuthConfig

--- front/app/containers/Authentication/useAuthConfig/index.tsx
+++ front/app/containers/Authentication/useAuthConfig/index.tsx
@@ -14,12 +14,7 @@ export default function useAuthConfig() {
   // All SSO methods (including the built-in Facebook/Google/Azure ones) are
   // configured as verification methods, and the `/verification_methods` endpoint
   // exposes a `authentication_method` flag for the ones that can be used to authenticate.
-  const { data: verificationMethods } = useVerificationMethods();
-  const hasLoginMethod = (name: TVerificationMethodName) =>
-    !!verificationMethods?.data.some(
-      (method) =>
-        method.attributes.name === name && method.attributes.authentication_method
-    );
+  const { data: idMethods } = useVerificationMethods();
 
   // Allows testing of specific SSO providers without showing to all users
   // e.g. ?provider=keycloak
@@ -37,11 +32,17 @@ export default function useAuthConfig() {
   const passwordLoginEnabled =
     useFeatureFlag({ name: 'password_login' }) || isSuperAdmin;
 
-  const google = hasLoginMethod('google');
+  const ssoProviders = (idMethods?.data || []).reduce((providers, method) => {
+    const { authentication_method, name } = method.attributes;
+    const enabled = authentication_method || providerForTest === name;
 
-  const facebook = hasLoginMethod('facebook');
+    return {
+      ...providers,
+      [name]: enabled,
+    };
+  }, {} as Record<TVerificationMethodName, boolean>);
 
-  const azureAdMethod = verificationMethods?.data.find(
+  const azureAdMethod = idMethods?.data.find(
     (method): method is IDAzureAdMethod =>
       method.attributes.name === 'azureactivedirectory' &&
       method.attributes.authentication_method
@@ -49,35 +50,16 @@ export default function useAuthConfig() {
   const azureAdSettings = azureAdMethod?.attributes;
   const azureAdVisiblity = azureAdSettings?.visibility;
   const azureAdIsVisible = ['show', undefined].includes(azureAdVisiblity);
+  const azureAdOverride = !!azureAdMethod && (azureAdIsVisible || showAdminOnlyMethods);
 
-  const azureAd = !!azureAdMethod && (azureAdIsVisible || showAdminOnlyMethods);
-
-  const azureAdB2cMethod = verificationMethods?.data.find(
+  const azureAdB2cMethod = idMethods?.data.find(
     (method): method is IDAzureAdB2cMethod =>
       method.attributes.name === 'azureactivedirectory_b2c' &&
       method.attributes.authentication_method
   );
   const azureAdB2cSettings = azureAdB2cMethod?.attributes;
-  const azureAdB2c = !!azureAdB2cMethod;
 
-  const ssoProviders = {
-    google,
-    facebook,
-    azureAd,
-    azureAdB2c,
-    franceconnect: hasLoginMethod('franceconnect'),
-    viennaCitizen: hasLoginMethod('vienna_citizen'),
-    claveUnica: hasLoginMethod('clave_unica'),
-    hoplr: hasLoginMethod('hoplr'),
-    idAustria: hasLoginMethod('id_austria'),
-    criipto: hasLoginMethod('criipto'),
-    nemlogIn: hasLoginMethod('nemlog_in'),
-    keycloak: hasLoginMethod('keycloak') || providerForTest === 'keycloak',
-    twoday: hasLoginMethod('twoday') || providerForTest === 'twoday',
-    acm: hasLoginMethod('acm') || providerForTest === 'acm',
-    federa: hasLoginMethod('federa'),
-    fakeSso: hasLoginMethod('fake_sso'),
-  };
+  ssoProviders.azureactivedirectory = azureAdOverride;
 
   return {
     passwordLoginEnabled,
