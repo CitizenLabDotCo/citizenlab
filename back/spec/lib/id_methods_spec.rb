f66ba501cfc121d4df1a017808018bd7a2502e0a
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-06-05 19:54:59 -0300

    Fix id_methods_spec

--- back/spec/lib/id_methods_spec.rb
+++ back/spec/lib/id_methods_spec.rb
@@ -11,14 +11,14 @@ describe IdMethods do
     end
 
     it 'adds methods that are exposed through #all_methods' do
-      mthd = Struct.new(:id).new('9fb591e7-f577-40a7-8596-03e406d7eebe')
+      mthd = Struct.new(:id, :name).new('9fb591e7-f577-40a7-8596-03e406d7eebe', 'Test Method')
       described_class.add_method(mthd)
       expect(described_class.all_methods).to include(mthd)
     end
 
     it 'replaces duplicate methods with the same .id' do
-      mthd1 = Struct.new(:id).new('9fb591e7-f577-40a7-8596-03e406d7eebe')
-      mthd2 = Struct.new(:id).new('9fb591e7-f577-40a7-8596-03e406d7eebe')
+      mthd1 = Struct.new(:id, :name).new('9fb591e7-f577-40a7-8596-03e406d7eebe', 'Test Method 1')
+      mthd2 = Struct.new(:id, :name).new('9fb591e7-f577-40a7-8596-03e406d7eebe', 'Test Method 2')
       described_class.add_method(mthd1)
       described_class.add_method(mthd2)
       expect(described_class.all_methods.select { |m| m.id == '9fb591e7-f577-40a7-8596-03e406d7eebe' }).to contain_exactly(mthd2)
