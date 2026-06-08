b7a93a795c90fd0c5f4b7a27ed0cb49e21ed3685
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-06-08 13:41:54 -0300

    Rename verification_methods controller, move it to different place and module

--- back/app/controllers/web_api/v1/id_methods_controller.rb
+++ back/app/controllers/web_api/v1/id_methods_controller.rb
@@ -2,7 +2,7 @@
 
 module WebApi
   module V1
-    module Verification
+    module IdMethods
       class VerificationMethodsController < ApplicationController
         skip_before_action :authenticate_user
 
