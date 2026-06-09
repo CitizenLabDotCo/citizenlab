73d368eaceadc23b51d9f22e7161703b04b12707
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-06-09 14:32:12 -0300

    _

--- front/app/api/id_methods/types.ts
+++ front/app/api/id_methods/types.ts
@@ -22,30 +22,30 @@ export const omniauthVerificationTypes = [
 ];
 
 export type IdMethodName =
+  | 'acm'
   | 'auth0'
+  | 'azureactivedirectory'
+  | 'azureactivedirectory_b2c'
   | 'bogus'
-  | 'fake_sso'
   | 'bosa_fas'
   | 'clave_unica'
   | 'cow'
   | 'criipto'
+  | 'facebook'
+  | 'fake_sso'
   | 'federa'
   | 'franceconnect'
   | 'gent_rrn'
+  | 'google'
+  | 'hoplr'
+  | 'id_austria'
   | 'id_card_lookup'
   | 'keycloak'
-  | 'twoday'
   | 'nemlog_in'
   | 'oostende_rrn'
-  | 'id_austria'
-  | 'acm'
-  | 'hoplr'
+  | 'twoday'
   | 'vienna_citizen'
-  | 'vienna_employee'
-  | 'facebook'
-  | 'google'
-  | 'azureactivedirectory'
-  | 'azureactivedirectory_b2c';
+  | 'vienna_employee';
 
 export interface IVerificationMethods {
   data: TVerificationMethod[];
@@ -80,49 +80,12 @@ type TGenericMethod<
   } & ExtraAttributes;
 };
 
-export type IDFakeSSOMethod = TGenericMethod<'fake_sso'>;
-
-export type IDLookupMethod = TGenericMethod<
-  'id_card_lookup',
-  {
-    card_id: string;
-    card_id_placeholder: string;
-    card_id_tooltip: string;
-    explainer_image_url: string;
-    ui_method_name: string;
-  }
->;
-
-export type IDCriiptoMethod = TGenericMethod<
-  'criipto',
-  { ui_method_name: string }
->;
-
-export type IDKeycloakMethod = TGenericMethod<
-  'keycloak',
-  { ui_method_name: string; provider: IconNames }
->;
-
-export type IDTwodayMethod = TGenericMethod<
-  'twoday',
-  { ui_method_name: string }
->;
-
 export type IDAcmMethod = TGenericMethod<'acm', { ui_method_name: string }>;
 
 export type IDAuth0Method = TGenericMethod<
   'auth0',
   { method_name_multiloc: Multiloc }
 >;
-
-export type IDIdAustriaMethod = TGenericMethod<
-  'id_austria',
-  { ui_method_name: string }
->;
-
-export type IDFacebookMethod = TGenericMethod<'facebook', { app_id?: string }>;
-
-export type IDGoogleMethod = TGenericMethod<'google'>;
 
 export type IDAzureAdMethod = TGenericMethod<
   'azureactivedirectory',
@@ -139,27 +102,57 @@ export type IDAzureAdB2cMethod = TGenericMethod<
   { logo_url?: string; login_mechanism_name?: string }
 >;
 
+export type IDCriiptoMethod = TGenericMethod<
+  'criipto',
+  { ui_method_name: string }
+>;
+
+export type IDFacebookMethod = TGenericMethod<'facebook', { app_id?: string }>;
+
+export type IDIdAustriaMethod = TGenericMethod<
+  'id_austria',
+  { ui_method_name: string }
+>;
+
+export type IDLookupMethod = TGenericMethod<
+  'id_card_lookup',
+  {
+    card_id: string;
+    card_id_placeholder: string;
+    card_id_tooltip: string;
+    explainer_image_url: string;
+    ui_method_name: string;
+  }
+>;
+
+export type IDKeycloakMethod = TGenericMethod<
+  'keycloak',
+  { ui_method_name: string; provider: IconNames }
+>;
+
+export type IDTwodayMethod = TGenericMethod<
+  'twoday',
+  { ui_method_name: string }
+>;
+
 // Methods that carry no method-specific attributes beyond the generic ones.
 type TOtherMethodName = Exclude<
   IdMethodName,
-  | 'fake_sso'
-  | 'id_card_lookup'
-  | 'criipto'
-  | 'keycloak'
-  | 'twoday'
   | 'acm'
   | 'auth0'
-  | 'id_austria'
-  | 'facebook'
-  | 'google'
   | 'azureactivedirectory'
   | 'azureactivedirectory_b2c'
+  | 'criipto'
+  | 'facebook'
+  | 'id_austria'
+  | 'id_card_lookup'
+  | 'keycloak'
+  | 'twoday'
 >;
 
 export type IDOtherMethod = TGenericMethod<TOtherMethodName>;
 
 export type TVerificationMethod =
-  | IDFakeSSOMethod
   | IDLookupMethod
   | IDCriiptoMethod
   | IDKeycloakMethod
@@ -168,7 +161,6 @@ export type TVerificationMethod =
   | IDAuth0Method
   | IDIdAustriaMethod
   | IDFacebookMethod
-  | IDGoogleMethod
   | IDAzureAdMethod
   | IDAzureAdB2cMethod
   | IDOtherMethod;
