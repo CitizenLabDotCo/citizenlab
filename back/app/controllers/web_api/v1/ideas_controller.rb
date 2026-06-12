fe9553f3e923044d7cf142764786708a47ae7f36
Sébastien Hoorens <sebastien@citizenlab.co>
2026-06-12 09:42:42 +0200

    fix offenses

--- back/app/controllers/web_api/v1/ideas_controller.rb
+++ back/app/controllers/web_api/v1/ideas_controller.rb
@@ -148,7 +148,7 @@ class WebApi::V1::IdeasController < ApplicationController
   #   Normal users always post in an active phase. They should never provide a phase id.
   #   Users who can moderate projects post in an active phase if no phase id is given.
   #   Users who can moderate projects post in the given phase if a phase id is given.
-  def create # rubocop:disable Metrics/MethodLength
+  def create
     send_error and return unless phase_for_input
 
     form = phase_for_input.pmethod.custom_form
@@ -239,7 +239,7 @@ class WebApi::V1::IdeasController < ApplicationController
     end
   end
 
-  def update # rubocop:disable Metrics/MethodLength
+  def update
     input = Idea.find params[:id]
 
     authorize input
