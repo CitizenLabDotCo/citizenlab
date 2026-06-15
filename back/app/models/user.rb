19cbacdcee6319297646b93a512bf9a7cd5f6095
jamesspeake <james@speake.org>
2026-06-15 11:46:46 +0100

    [TAN-7997] Allow email case change when accepting invites

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -379,6 +379,10 @@ class User < ApplicationRecord
                   (new_email_changed? || email_changed?) &&
                   email_was.present?
 
+    # Ignore changes that only differ in letter-case (e.g. accepting an invite with
+    # `small@big.com` for an address stored as `SMALL@BIG.COM`)
+    return if email_changed_only_in_case? && !new_email_changed?
+
     if no_password? && confirmation_required # only for light registration
       # Avoid security hole where passwordless user can change when they are authenticated without confirmation
       errors.add :email, :change_not_permitted, value: email, message: 'change not permitted - user not active'
@@ -415,6 +419,10 @@ class User < ApplicationRecord
 
   def email_or_new_email_changed?
     new_record? || email_changed? || new_email_changed?
+  end
+
+  def email_changed_only_in_case?
+    email_changed? && email.present? && email_was.present? && email.casecmp?(email_was)
   end
 
   def validate_email_domains_blacklist
