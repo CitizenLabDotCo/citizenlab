20058d0d47587c29124c95e2cbbbb4ee78801543
stephanie rousset <strabon@club-internet.fr>
2026-06-16 16:19:50 +0200

    feat: add new html block widget in content builders

---
+++ front/app/components/admin/ContentBuilder/Widgets/HtmlBlockMultiloc/index.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import { Box } from '@citizenlab/cl2-component-library';
+import { useNode } from '@craftjs/core';
+import { Multiloc } from 'typings';
+
+import useLocalize from 'hooks/useLocalize';
+import TextAreaMultilocWithLocaleSwitcher from 'components/UI/TextAreaMultilocWithLocaleSwitcher';
+
+import messages from './messages';
+import styled from 'styled-components';
+
+interface Props {
+  html?: Multiloc;
+}
+
+const StyledBox = styled(Box)`
+  textarea {
+    font-family: 'monospace', monospace !important;
+  }
+`;
+
+const HtmlBlockMultiloc = ({ html }: Props) => {
+  const localize = useLocalize();
+
+  return (
+    <Box
+      className="e2e-html-block"
+      minHeight="26px"
+      maxWidth="1200px"
+      margin="0 auto"
+    >
+      <div dangerouslySetInnerHTML={{ __html: localize(html) }} />
+    </Box>
+  );
+};
+
+const HtmlBlockMultilocSettings = () => {
+  const {
+    actions: { setProp },
+    html,
+  } = useNode((node) => ({
+    html: node.data.props.html,
+  }));
+
+  return (
+    <StyledBox background="#ffffff" marginBottom="20px">
+      <TextAreaMultilocWithLocaleSwitcher
+        id="html-block"
+        minRows={16}
+        valueMultiloc={html}
+        onChange={(value) => {
+          setProp((props: Props) => (props.html = value));
+        }}
+      />
+    </StyledBox>
+  );
+};
+
+HtmlBlockMultiloc.craft = {
+  props: {
+    html: {},
+  },
+  related: {
+    settings: HtmlBlockMultilocSettings,
+  },
+  custom: {
+    title: messages.htmlBlockMultiloc,
+  },
+};
+
+export default HtmlBlockMultiloc;
---
+++ front/app/containers/Admin/pagesAndMenu/containers/ContentBuilder/components/Widgets/HtmlBlockMultiloc/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import { Box } from '@citizenlab/cl2-component-library';
+import { useNode } from '@craftjs/core';
+import { Multiloc } from 'typings';
+
+import useLocalize from 'hooks/useLocalize';
+import messages from 'components/admin/ContentBuilder/Widgets/HtmlBlockMultiloc/messages';
+import TextAreaMultilocWithLocaleSwitcher from 'components/UI/TextAreaMultilocWithLocaleSwitcher';
+import styled from 'styled-components';
+
+interface Props {
+  html?: Multiloc;
+}
+
+const StyledBox = styled(Box)`
+  textarea {
+    font-family: 'monospace', monospace !important;
+  }
+`;
+
+const HtmlBlockMultiloc = ({ html }: Props) => {
+  const localize = useLocalize();
+
+  return (
+    <Box
+      className="e2e-html-block"
+      minHeight="40px"
+      maxWidth="1200px"
+      margin="0 auto"
+    >
+      <div dangerouslySetInnerHTML={{ __html: localize(html) }} />
+    </Box>
+  );
+};
+
+const HtmlBlockMultilocSettings = () => {
+  const {
+    actions: { setProp },
+    html,
+  } = useNode((node) => ({
+    html: node.data.props.html,
+  }));
+
+  return (
+    <StyledBox background="#ffffff" marginBottom="20px">
+      <TextAreaMultilocWithLocaleSwitcher
+        id="html-block"
+        minRows={16}
+        valueMultiloc={html}
+        onChange={(value) => {
+          setProp((props: Props) => (props.html = value));
+        }}
+      />
+    </StyledBox>
+  );
+};
+
+HtmlBlockMultiloc.craft = {
+  props: {
+    html: {},
+  },
+  related: {
+    settings: HtmlBlockMultilocSettings,
+  },
+};
+
+export const htmlBlockMultilocTitle = messages.htmlBlockMultiloc;
+
+export default HtmlBlockMultiloc;
