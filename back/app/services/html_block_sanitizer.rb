e6c69cba8fa44affb39b93e221b137fe8e6a7ca3
stephanie rousset <strabon@club-internet.fr>
2026-06-16 16:17:21 +0200

    feat: add specific sanitizer for html block

---
+++ back/app/services/html_block_sanitizer.rb
@@ -0,0 +1,39 @@
+class HtmlBlockSanitizer
+
+  def sanitize(html)
+    return '' if html.blank?
+
+    sanitizer = Rails::HTML5::SafeListSanitizer.new
+    # sanitize via the supplied tags and attributes
+    sanitized = sanitizer.sanitize(
+      html,
+      tags: custom_tags,
+      attributes: custom_attributes
+    )
+
+    force_noopener(sanitized)
+  end
+
+  # Force rel="noopener noreferrer" on links with target="_blank"
+  def force_noopener(html)
+    return '' if html.blank?
+
+    doc = Nokogiri::HTML::DocumentFragment.parse(html)
+    doc.css('a[target="_blank"]').each do |node|
+      node['rel'] = 'noopener noreferrer'
+    end
+    doc.to_html
+  end
+
+  def custom_tags
+    Loofah::HTML5::WhiteList::ALLOWED_ELEMENTS_WITH_LIBXML2 + %w[
+      video audio source comment iframe
+    ] - %w[form input button select textarea fieldset]
+  end
+
+  def custom_attributes
+    Loofah::HTML5::WhiteList::ALLOWED_ATTRIBUTES + %w[
+      frameborder allowfullscreen
+    ] - %w[onerror]
+  end
+end
