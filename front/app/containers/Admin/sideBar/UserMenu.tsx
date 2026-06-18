a8eacb8c315624fc5f5076f4fafc76372e1a9a78
EdwinKato <katedwin88@gmail.com>
2026-06-18 16:02:49 +0300

    Show user name on hover in collapsed rail

--- front/app/containers/Admin/sideBar/UserMenu.tsx
+++ front/app/containers/Admin/sideBar/UserMenu.tsx
@@ -23,6 +23,7 @@ import { getFullName } from 'utils/textUtils';
 import LanguageSelectorPopup from './LanguageSelectorPopup';
 import LocaleSelectorPopup from './LocaleSelectorPopup';
 import messages from './messages';
+import RailTooltip from './RailTooltip';
 import SidebarCollapsedContext from './SidebarCollapsedContext';
 import { ItemMenu, StyledBox, StyledText } from './styles';
 
@@ -63,126 +64,131 @@ export const UserMenu = () => {
   const roleMessage = getRoleMessage(authUser.data);
 
   return (
-    <StyledBox
-      as="button"
-      width={isSmallerThanTablet ? '56px' : '100%'}
-      display="flex"
-      justifyContent="flex-start"
-      onClick={() => setIsUserMenuPopupOpen(true)}
-      p="0px"
-      position="relative"
+    <RailTooltip
+      label={getFullName(authUser.data)}
+      disabled={!isSmallerThanTablet}
     >
-      <Box
+      <StyledBox
+        as="button"
+        width={isSmallerThanTablet ? '56px' : '100%'}
         display="flex"
-        alignItems="center"
-        w="100%"
-        p={isSmallerThanTablet ? '10px 0' : '10px 8px 10px 16px'}
-        justifyContent={isSmallerThanTablet ? 'center' : undefined}
+        justifyContent="flex-start"
+        onClick={() => setIsUserMenuPopupOpen(true)}
+        p="0px"
+        position="relative"
       >
-        {/*
+        <Box
+          display="flex"
+          alignItems="center"
+          w="100%"
+          p={isSmallerThanTablet ? '10px 0' : '10px 8px 10px 16px'}
+          justifyContent={isSmallerThanTablet ? 'center' : undefined}
+        >
+          {/*
               Margins are needed to align with other icons/items in sidebar.
               Changes in Avatar component are needed so size prop behaves correctly.
             */}
-        <Box
-          ml={isSmallerThanTablet ? '0' : '-2px'}
-          mr={isSmallerThanTablet ? '0' : '6px'}
-        >
-          <Avatar userId={authUser.data.id} size={20} />
-        </Box>
-        {!isSmallerThanTablet && (
           <Box
-            display="flex"
-            flex="1"
-            flexDirection="column"
-            w="100%"
-            ml="7px"
-            overflow="hidden"
+            ml={isSmallerThanTablet ? '0' : '-2px'}
+            mr={isSmallerThanTablet ? '0' : '6px'}
           >
-            <StyledText
-              color="white"
-              my="0px"
-              fontSize="m"
-              textOverflow="ellipsis"
-              whiteSpace="nowrap"
-              overflow="hidden"
+            <Avatar userId={authUser.data.id} size={20} />
+          </Box>
+          {!isSmallerThanTablet && (
+            <Box
+              display="flex"
+              flex="1"
+              flexDirection="column"
               w="100%"
-              textAlign="left"
+              ml="7px"
+              overflow="hidden"
             >
-              {getFullName(authUser.data)}
-            </StyledText>
-            <Box opacity={0.5}>
-              <Text
+              <StyledText
                 color="white"
                 my="0px"
-                fontSize="xs"
+                fontSize="m"
                 textOverflow="ellipsis"
                 whiteSpace="nowrap"
                 overflow="hidden"
                 w="100%"
                 textAlign="left"
               >
-                {roleMessage ? formatMessage(roleMessage) : ''}
-              </Text>
+                {getFullName(authUser.data)}
+              </StyledText>
+              <Box opacity={0.5}>
+                <Text
+                  color="white"
+                  my="0px"
+                  fontSize="xs"
+                  textOverflow="ellipsis"
+                  whiteSpace="nowrap"
+                  overflow="hidden"
+                  w="100%"
+                  textAlign="left"
+                >
+                  {roleMessage ? formatMessage(roleMessage) : ''}
+                </Text>
+              </Box>
             </Box>
-          </Box>
-        )}
-        <Box ref={iconDivRef}>
-          {!isSmallerThanTablet && (
-            <Icon name="chevron-right" fill={colors.white} />
           )}
+          <Box ref={iconDivRef}>
+            {!isSmallerThanTablet && (
+              <Icon name="chevron-right" fill={colors.white} />
+            )}
+          </Box>
         </Box>
-      </Box>
-      <Dropdown
-        opened={isUserMenuPopupOpen}
-        onClickOutside={handleUserMenuPopupClose}
-        left={isSmallerThanTablet ? '60px' : '200px'}
-        mobileLeft="60px"
-        top="-40px"
-        content={
-          <Box>
-            {tenantLocales.length > 1 && (
+        <Dropdown
+          opened={isUserMenuPopupOpen}
+          onClickOutside={handleUserMenuPopupClose}
+          left={isSmallerThanTablet ? '60px' : '200px'}
+          mobileLeft="60px"
+          top="-40px"
+          content={
+            <Box>
+              {tenantLocales.length > 1 && (
+                <ItemMenu
+                  buttonStyle="text"
+                  onClick={() => setIsLocaleSelectorOpen(!isLocaleSelectorOpen)}
+                >
+                  <Box display="flex" justifyContent="space-between" w="100%">
+                    <LanguageSelectorPopup
+                      setIsLocaleSelectorOpen={setIsLocaleSelectorOpen}
+                      isLocaleSelectorOpen={isLocaleSelectorOpen}
+                    />
+                  </Box>
+                </ItemMenu>
+              )}
               <ItemMenu
+                to="/profile/$userId"
+                params={{ userId: authUser.data.id }}
                 buttonStyle="text"
-                onClick={() => setIsLocaleSelectorOpen(!isLocaleSelectorOpen)}
               >
                 <Box display="flex" justifyContent="space-between" w="100%">
-                  <LanguageSelectorPopup
-                    setIsLocaleSelectorOpen={setIsLocaleSelectorOpen}
-                    isLocaleSelectorOpen={isLocaleSelectorOpen}
-                  />
+                  <Text my="0px" color="coolGrey600">
+                    {formatMessage({ ...messages.myProfile })}
+                  </Text>
+                  <Icon name="user" fill={colors.grey600} />
                 </Box>
               </ItemMenu>
-            )}
-            <ItemMenu
-              to="/profile/$userId"
-              params={{ userId: authUser.data.id }}
-              buttonStyle="text"
-            >
-              <Box display="flex" justifyContent="space-between" w="100%">
-                <Text my="0px" color="coolGrey600">
-                  {formatMessage({ ...messages.myProfile })}
-                </Text>
-                <Icon name="user" fill={colors.grey600} />
-              </Box>
-            </ItemMenu>
-            <ItemMenu buttonStyle="text" onClick={signOut}>
-              <Box display="flex" justifyContent="space-between" w="100%">
-                <Text my="0px" color="coolGrey600">
-                  {formatMessage({ ...messages.signOut })}
-                </Text>
-                <Icon name="power" fill={colors.grey600} />
-              </Box>
-            </ItemMenu>
-          </Box>
-        }
-      />
+              <ItemMenu buttonStyle="text" onClick={signOut}>
+                <Box display="flex" justifyContent="space-between" w="100%">
+                  <Text my="0px" color="coolGrey600">
+                    {formatMessage({ ...messages.signOut })}
+                  </Text>
+                  <Icon name="power" fill={colors.grey600} />
+                </Box>
+              </ItemMenu>
+            </Box>
+          }
+        />
 
-      <LocaleSelectorPopup
-        isLocaleSelectorOpen={isLocaleSelectorOpen}
-        setIsLocaleSelectorOpen={setIsLocaleSelectorOpen}
-        tenantLocales={tenantLocales}
-      />
-    </StyledBox>
+        <LocaleSelectorPopup
+          isLocaleSelectorOpen={isLocaleSelectorOpen}
+          setIsLocaleSelectorOpen={setIsLocaleSelectorOpen}
+          tenantLocales={tenantLocales}
+        />
+      </StyledBox>
+    </RailTooltip>
   );
 };
 
