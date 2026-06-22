1e17cb6d32cfa5a7b07938775680a7c228e8189c
Iva <iva@citizenlab.co>
2026-06-22 10:26:12 +0300

    Fix ideas feed for non-active phases

--- front/app/api/idea_exposure/useAddIdeaExposure.ts
+++ front/app/api/idea_exposure/useAddIdeaExposure.ts
@@ -7,13 +7,14 @@ import { IIdeaExposure } from './types';
 
 interface AddIdeaExposureParams {
   ideaId: string;
+  phaseId?: string;
 }
 
-const addIdeaExposure = async ({ ideaId }: AddIdeaExposureParams) =>
+const addIdeaExposure = async ({ ideaId, phaseId }: AddIdeaExposureParams) =>
   fetcher<IIdeaExposure>({
     path: `/ideas/${ideaId}/exposures`,
     action: 'post',
-    body: null,
+    body: phaseId ? { phase_id: phaseId } : null,
   });
 
 const useAddIdeaExposure = () => {
