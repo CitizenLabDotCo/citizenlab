b023fd2b14ca3e959d53d1f99203bb1d5eeb7bd9
EdwinKato <katedwin88@gmail.com>
2026-06-25 14:30:35 +0300

    Add project page builder messages

---
+++ front/app/components/ProjectPageBuilder/messages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  projectPageHeading: {
+    id: 'app.containers.ProjectPageBuilder.projectPageHeading',
+    defaultMessage: 'Project page',
+  },
+  saveError: {
+    id: 'app.containers.ProjectPageBuilder.saveError',
+    defaultMessage: 'There was an error saving the project page.',
+  },
+});
