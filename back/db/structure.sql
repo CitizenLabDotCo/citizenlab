0c85397ad8966c1bc7b6d823baa13b54b3fe2506
Amanda Anderson <anderson_amanda@mail.com>
2026-07-01 13:51:54 +0200

    Migrate natie survey phases with submissions disabled

--- back/db/structure.sql
+++ back/db/structure.sql
@@ -8841,6 +8841,7 @@ ALTER TABLE ONLY public.project_reviews
 SET search_path TO public,shared_extensions;
 
 INSERT INTO "schema_migrations" (version) VALUES
+('20260701113056'),
 ('20260617120000'),
 ('20260617090200'),
 ('20260617090100'),
