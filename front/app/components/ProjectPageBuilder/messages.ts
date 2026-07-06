3ceccce24a603d31fc4f146fe64bdd74ab06616f
EdwinKato <katedwin88@gmail.com>
2026-07-06 12:35:12 +0300

    Add the locked toolbox note message

--- front/app/components/ProjectPageBuilder/messages.ts
+++ front/app/components/ProjectPageBuilder/messages.ts
@@ -9,4 +9,9 @@ export default defineMessages({
     id: 'app.containers.ProjectPageBuilder.saveError',
     defaultMessage: 'There was an error saving the project page.',
   },
+  toolboxLockedNote: {
+    id: 'app.containers.ProjectPageBuilder.toolboxLockedNote',
+    defaultMessage:
+      'The page sections are locked for now. Widgets can be placed inside the Description section.',
+  },
 });
