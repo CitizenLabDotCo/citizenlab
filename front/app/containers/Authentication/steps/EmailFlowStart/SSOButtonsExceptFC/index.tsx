f13d990d3a3bcef251978a020082612aec332217
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-07-06 16:22:02 +0200

    Put back azure AD override logic

--- front/app/containers/Authentication/steps/EmailFlowStart/SSOButtonsExceptFC/index.tsx
+++ front/app/containers/Authentication/steps/EmailFlowStart/SSOButtonsExceptFC/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 import useIdMethods from 'api/id_methods/useIdMethods';
+import { getAzureConfig } from 'api/id_methods/utils';
 
 import { SSOProviderWithoutVienna } from 'containers/Authentication/typings';
+
+import { useLocation } from 'utils/router';
 
 import SSOButton from './SSOButton';
 
@@ -12,9 +15,22 @@ interface Props {
 
 // Renders all enabled SSO providers (except FranceConnect), in display order.
 const SSOButtonsExceptFC = ({ onClickSSO }: Props) => {
+  // A hidden path that will show all methods inc any that are admin only
+  const { pathname } = useLocation();
+  const showAdminOnlyMethods = pathname.endsWith('/sign-in/admin');
   const { data: idMethods } = useIdMethods();
 
+  const azureConfig = getAzureConfig(idMethods);
+  const azureAdVisibility = azureConfig?.attributes.visibility;
+  const azureAdIsVisible = ['show', undefined].includes(azureAdVisibility);
+  const azureAdEnabled =
+    !!azureConfig && (azureAdIsVisible || showAdminOnlyMethods);
+
   const authenticationMethodsExceptFC = idMethods?.data.filter((method) => {
+    if (method.attributes.name === 'azureactivedirectory') {
+      return azureAdEnabled;
+    }
+
     const isAuthMethod = method.attributes.authentication_method;
     const isFC = method.attributes.name === 'franceconnect';
     return !isFC && isAuthMethod;
