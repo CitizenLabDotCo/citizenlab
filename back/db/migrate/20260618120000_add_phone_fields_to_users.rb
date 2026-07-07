8a168a5550d81109311c623681a3f91f26b66ed6
AchrafGoVocal <achraf@govocal.com>
2026-07-07 11:22:42 +0200

    [TAN-8120] Remove redundant null: true in migration

--- back/db/migrate/20260618120000_add_phone_fields_to_users.rb
+++ back/db/migrate/20260618120000_add_phone_fields_to_users.rb
@@ -2,7 +2,7 @@
 
 class AddPhoneFieldsToUsers < ActiveRecord::Migration[7.2]
   def change
-    add_column :users, :phone, :string, null: true
+    add_column :users, :phone, :string
     add_column :users, :new_phone, :string
     add_column :users, :phone_confirmed_at, :datetime
   end
