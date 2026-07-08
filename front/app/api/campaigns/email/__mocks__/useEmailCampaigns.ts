9b4e3762364427e221c1cad007b336f5036b2032
AchrafGoVocal <achraf@govocal.com>
2026-07-08 09:51:31 +0200

    [TAN-8120] Restore escaping in email campaigns mock links to match original

--- front/app/api/campaigns/email/__mocks__/useEmailCampaigns.ts
+++ front/app/api/campaigns/email/__mocks__/useEmailCampaigns.ts
@@ -93,10 +93,10 @@ export const emailCampaignsData: IEmailCampaignData[] = [
 ];
 
 export const links = {
-  self: 'http://localhost:3000/web_api/v1/campaigns?depth=0&page%5Bnumber%5D=1&page%5Bsize%5D=1000&publication_statuses%5B%5D=published&publication_statuses%5B%5D=archived&remove_not_allowed_parents=true',
+  self: 'http://localhost:3000/web_api/v1/campaigns?depth=0\u0026page%5Bnumber%5D=1\u0026page%5Bsize%5D=1000\u0026publication_statuses%5B%5D=published\u0026publication_statuses%5B%5D=archived\u0026remove_not_allowed_parents=true',
   first:
-    'http://localhost:3000/web_api/v1/campaigns?depth=0&page%5Bnumber%5D=1&page%5Bsize%5D=1000&publication_statuses%5B%5D=published&publication_statuses%5B%5D=archived&remove_not_allowed_parents=true',
-  last: 'http://localhost:3000/web_api/v1/campaigns?depth=0&page%5Bnumber%5D=2&page%5Bsize%5D=1000&publication_statuses%5B%5D=published&publication_statuses%5B%5D=archived&remove_not_allowed_parents=true',
+    'http://localhost:3000/web_api/v1/campaigns?depth=0\u0026page%5Bnumber%5D=1\u0026page%5Bsize%5D=1000\u0026publication_statuses%5B%5D=published\u0026publication_statuses%5B%5D=archived\u0026remove_not_allowed_parents=true',
+  last: 'http://localhost:3000/web_api/v1/campaigns?depth=0\u0026page%5Bnumber%5D=2\u0026page%5Bsize%5D=1000\u0026publication_statuses%5B%5D=published\u0026publication_statuses%5B%5D=archived\u0026remove_not_allowed_parents=true',
   prev: null,
-  next: 'http://localhost:3000/web_api/v1/campaigns?depth=0&page%5Bnumber%5D=2&page%5Bsize%5D=1000&publication_statuses%5B%5D=published&publication_statuses%5B%5D=archived&remove_not_allowed_parents=true',
+  next: 'http://localhost:3000/web_api/v1/campaigns?depth=0\u0026page%5Bnumber%5D=2\u0026page%5Bsize%5D=1000\u0026publication_statuses%5B%5D=published\u0026publication_statuses%5B%5D=archived\u0026remove_not_allowed_parents=true',
 };
