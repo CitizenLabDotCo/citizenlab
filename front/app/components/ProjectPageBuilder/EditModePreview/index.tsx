ad0c979e7d1b0ac128973106d2b95180998e084b
EdwinKato <katedwin88@gmail.com>
2026-07-09 18:13:57 +0300

    Carry the parallel_participation param into the edit-mode preview iframe

--- front/app/components/ProjectPageBuilder/EditModePreview/index.tsx
+++ front/app/components/ProjectPageBuilder/EditModePreview/index.tsx
@@ -20,7 +20,7 @@ const ProjectPageBuilderEditModePreview = React.forwardRef<
 
   return (
     <EditModePreview
-      iframeSrc={`/${locale}/admin/project-page-builder/projects/${projectId}/preview?selected_locale=${locale}`}
+      iframeSrc={`/${locale}/admin/project-page-builder/projects/${projectId}/preview?selected_locale=${locale}&parallel_participation=true`}
       ref={ref}
     />
   );
