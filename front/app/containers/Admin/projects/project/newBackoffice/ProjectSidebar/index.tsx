622e443963a2b7e3ad02ddc19b9b4038270306d2
EdwinKato <katedwin88@gmail.com>
2026-07-21 13:40:25 +0300

    Gate the extras sidebar section behind the parallel_participation flag

--- front/app/containers/Admin/projects/project/newBackoffice/ProjectSidebar/index.tsx
+++ front/app/containers/Admin/projects/project/newBackoffice/ProjectSidebar/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Box, colors } from '@citizenlab/cl2-component-library';
 
+import useFeatureFlag from 'hooks/useFeatureFlag';
+
 import ExtrasPhases from '../ExtrasPhases';
 import ProjectNavRail from '../ProjectNavRail';
 import TimelinePhases from '../TimelinePhases';
@@ -12,24 +14,30 @@ interface Props {
   projectId: string;
 }
 
-const ProjectSidebar = ({ projectId }: Props) => (
-  <Box
-    flex={`0 0 ${SIDEBAR_WIDTH}`}
-    width={SIDEBAR_WIDTH}
-    background={colors.white}
-    borderRight={`1px solid ${colors.grey200}`}
-    display="flex"
-    flexDirection="column"
-    height="100%"
-    minHeight="0"
-    overflow="hidden"
-  >
-    <Box flex="1 1 auto" minHeight="0" overflowY="auto">
-      <ProjectNavRail projectId={projectId} />
-      <TimelinePhases projectId={projectId} />
-      <ExtrasPhases projectId={projectId} />
+const ProjectSidebar = ({ projectId }: Props) => {
+  const extraSurveysEnabled = useFeatureFlag({
+    name: 'parallel_participation',
+  });
+
+  return (
+    <Box
+      flex={`0 0 ${SIDEBAR_WIDTH}`}
+      width={SIDEBAR_WIDTH}
+      background={colors.white}
+      borderRight={`1px solid ${colors.grey200}`}
+      display="flex"
+      flexDirection="column"
+      height="100%"
+      minHeight="0"
+      overflow="hidden"
+    >
+      <Box flex="1 1 auto" minHeight="0" overflowY="auto">
+        <ProjectNavRail projectId={projectId} />
+        <TimelinePhases projectId={projectId} />
+        {extraSurveysEnabled && <ExtrasPhases projectId={projectId} />}
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 export default ProjectSidebar;
