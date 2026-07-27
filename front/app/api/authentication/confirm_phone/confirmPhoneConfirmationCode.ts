08567668c69ac817330e683621784d5b2e1555e7
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-07-27 15:44:15 -0300

    Add FE function to confirm code phone

--- front/app/api/authentication/confirm_phone/confirmPhoneConfirmationCode.ts
+++ front/app/api/authentication/confirm_phone/confirmPhoneConfirmationCode.ts
@@ -1,12 +1,21 @@
+import requirementsKeys from 'api/authentication/authentication_requirements/keys';
 import meKeys from 'api/me/keys';
 
 import fetcher from 'utils/cl-react-query/fetcher';
 import { queryClient } from 'utils/cl-react-query/queryClient';
 
+export const confirmCodePhone = (code: string) => {
+  return confirmCode('confirm_code_phone', code);
+};
+
 export const confirmCodeNewPhone = async (code: string) => {
+  return confirmCode('confirm_code_new_phone', code);
+};
+
+export const confirmCode = async (endpoint: string, code: string) => {
   try {
     await fetcher({
-      path: `/user/confirm_code_new_phone`,
+      path: `/user/${endpoint}`,
       action: 'post',
       body: {
         confirmation: { code },
@@ -14,6 +23,7 @@ export const confirmCodeNewPhone = async (code: string) => {
     });
 
     queryClient.invalidateQueries({ queryKey: meKeys.all() });
+    queryClient.invalidateQueries({ queryKey: requirementsKeys.all() });
 
     return true;
   } catch (errors) {
