fdf045a3ce2d4c4b082e5bfe8a49ee17f564849b
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-07-27 11:07:35 -0300

    Update comment

--- front/app/containers/Authentication/useSteps/stepConfig/confirmationSteps.ts
+++ front/app/containers/Authentication/useSteps/stepConfig/confirmationSteps.ts
@@ -18,6 +18,7 @@ import { doesNotMeetGroupCriteria, checkMissingData } from './utils';
 
 // Here we put all the steps related to confirmation email and phone
 // EXCEPT the ones that are part of the main email flow
+// (i.e. email:unauthenticated-confirmation)
 export const confirmationSteps = (
   getAuthenticationData: () => AuthenticationData,
   getRequirements: GetRequirements,
