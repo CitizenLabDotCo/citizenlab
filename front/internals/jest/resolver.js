8d8d84586c0b608f04ffdcb81c408deddaeb145d
Iva <iva@citizenlab.co>
2026-07-28 13:03:46 +0300

    Bump jest and speed up unit tests

--- front/internals/jest/resolver.js
+++ front/internals/jest/resolver.js
@@ -16,7 +16,7 @@ module.exports = (path, options) => {
       //
       // uuid used to be handled here too, but as of v14 it is ESM-only and no longer ships a
       // CommonJS "main", so deleting its exports leaves nothing to resolve. Instead we let its
-      // ESM resolve normally and have babel-jest transform it (see transformIgnorePatterns in
+      // ESM resolve normally and have ts-jest transform it (see transformIgnorePatterns in
       // jest.config.js).
       if (pkg.name === 'nanoid') {
         delete pkg['exports'];
