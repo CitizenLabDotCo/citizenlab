c109d4c7709fcea63a15706df97b31073a61a212
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-08-04 18:00:17 -0300

    Remove tmp folder

--- tmp/AccessSection/MethodRow/messages.ts
+++
@@ -1,9 +0,0 @@
-import { defineMessages } from "react-intl";
-
-export default defineMessages({
-  atLeastOneMethodMustStayEnabled: {
-    id: "front.app.components.admin.ActionForm.AccessSections.AccessSection.atLeastOneMethodMustStayEnabled",
-    defaultMessage:
-      "At least one authentication method must stay enabled, so this one can’t be turned off.",
-  },
-});
