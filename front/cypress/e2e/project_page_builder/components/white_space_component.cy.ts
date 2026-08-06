712630c4dd498ea48391f88366272b0a0defd586
EdwinKato <katedwin88@gmail.com>
2026-08-06 07:32:49 +0300

    Remove an unused projectDescription

--- front/cypress/e2e/project_page_builder/components/white_space_component.cy.ts
+++ front/cypress/e2e/project_page_builder/components/white_space_component.cy.ts
@@ -9,7 +9,6 @@ describe('Project description builder White space component', () => {
     cy.getAdminAuthUser().then((user) => {
       const projectTitle = randomString();
       const projectDescriptionPreview = randomString();
-      const projectDescription = 'Original project description.';
       const userId = user.body.data.id;
 
       cy.apiCreateProject({
