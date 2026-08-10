867323a4bf445eba50980b733745e16637718237
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-08-10 17:29:23 -0300

    _

--- front/app/components/admin/ActionForm/index.tsx
+++ front/app/components/admin/ActionForm/index.tsx
@@ -14,7 +14,7 @@ import usePermissionsPhaseCustomFields from 'api/permissions_phase_custom_fields
 
 import { FormattedMessage, useIntl } from 'utils/cl-intl';
 
-import AccessSection from './AccessSection/AccessSection';
+import AccessSection from './AccessSection';
 import DataSection from './DataSection';
 import { buildSummary, getGroupIds } from './logic';
 import messages from './messages';
