8a4aa3e5acf547cf26e2e366472d1f7184e977be
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-08-11 16:09:06 -0300

    _

--- back/app/controllers/concerns/locked_user_custom_fields_concern.rb
+++ back/app/controllers/concerns/locked_user_custom_fields_concern.rb
@@ -14,10 +14,6 @@ module LockedUserCustomFieldsConcern
     return {} unless current_user
 
     locked_custom_field_keys = Verification::VerificationService.new.locked_custom_fields_keys(current_user).map(&:to_s)
-
-    # The constraints are keyed by key as well: not every locked field has a code
-    # (e.g. verification methods that lock a field configured by key), so we
-    # cannot key them by code.
     CustomField.where(key: locked_custom_field_keys).to_h { |field| [field.key.to_sym, { locked: true }] }
   end
 end
