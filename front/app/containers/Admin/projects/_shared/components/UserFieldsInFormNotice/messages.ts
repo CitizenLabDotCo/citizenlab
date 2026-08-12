bbefdf030f98e89092b5d891870a2d07b01d8fbd
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-08-12 14:26:47 -0300

    Also add notice for ideation and proposals projects

--- front/app/containers/Admin/projects/_shared/components/UserFieldsInFormNotice/messages.ts
+++ front/app/containers/Admin/projects/_shared/components/UserFieldsInFormNotice/messages.ts
@@ -7,7 +7,7 @@ export default defineMessages({
       "'Demographic fields in form' is enabled. When the form is displayed any configured demographic questions will be added on a new page immediately before the end of the form. These questions can be changed in the {accessRightsSettingsLink}.",
   },
   accessRightsSettings: {
-    id: 'app.components.formBuilder.nativeSurvey.UserFieldsInFormNotice.accessRightsSettings',
-    defaultMessage: 'access rights settings for this survey',
+    id: 'app.components.formBuilder.nativeSurvey.UserFieldsInFormNotice.accessRightsSettings2',
+    defaultMessage: 'access rights settings for this phase',
   },
 });
