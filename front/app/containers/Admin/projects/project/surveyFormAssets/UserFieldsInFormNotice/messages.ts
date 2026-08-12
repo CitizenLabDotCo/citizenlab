ba36ff9cf96b33889d7c47db96d5a44b6edcb8ca
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-08-12 14:23:46 -0300

    Make copy more generic

--- front/app/containers/Admin/projects/project/surveyFormAssets/UserFieldsInFormNotice/messages.ts
+++ front/app/containers/Admin/projects/project/surveyFormAssets/UserFieldsInFormNotice/messages.ts
@@ -2,9 +2,9 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   fieldsEnabledMessage: {
-    id: 'app.components.formBuilder.nativeSurvey.UserFieldsInFormNotice.fieldsEnabledMessage2',
+    id: 'app.components.formBuilder.nativeSurvey.UserFieldsInFormNotice.fieldsEnabledMessage3',
     defaultMessage:
-      "'Demographic fields in survey form' is enabled. When the survey form is displayed any configured demographic questions will be added on a new page immediately before the end of the survey. These questions can be changed in the {accessRightsSettingsLink}.",
+      "'Demographic fields in form' is enabled. When the form is displayed any configured demographic questions will be added on a new page immediately before the end of the form. These questions can be changed in the {accessRightsSettingsLink}.",
   },
   accessRightsSettings: {
     id: 'app.components.formBuilder.nativeSurvey.UserFieldsInFormNotice.accessRightsSettings',
