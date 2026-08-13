efe6716618ca0a2f567d2dc30748b15a332cd8a4
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-08-13 15:50:01 -0300

    _

--- front/app/containers/Admin/settings/registration/VisitingPermission/messages.ts
+++ front/app/containers/Admin/settings/registration/VisitingPermission/messages.ts
@@ -2,7 +2,7 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   platformAccessTitle: {
-    id: 'app.containers.AdminPage.SettingsPage.registration.VisitingPermission.platformAccessTitle',
+    id: 'app.containers.AdminPage.SettingsPage.registration.VisitingPermission.platformAccessTitle1',
     defaultMessage: 'Platform access and personal info collection',
   },
   subtitle1: {
