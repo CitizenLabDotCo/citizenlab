170e62d626ad54b3c33de9a8f21c59abb7d151f2
Iva <iva@citizenlab.co>
2026-08-19 14:47:56 +0300

    Fix comment

--- front/app/components/CustomFieldsForm/constants.ts
+++ front/app/components/CustomFieldsForm/constants.ts
@@ -7,6 +7,6 @@ export const SEARCHABLE_OPTION_COUNT = 10;
 
 /**
  * Past this many options an inline list of radio buttons is too long to scan,
- * so a single select is shown as a dropdown whatever `dropdown_layout` says.
+ * so a single select is shown as a dropdown always.
  */
 export const LIST_LAYOUT_MAX_OPTIONS = 20;
