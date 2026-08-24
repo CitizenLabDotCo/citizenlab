ca03b5b8ed1550350f2648c447b6313f0440eded
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-08-24 12:06:49 -0300

    _

--- front/cypress/support/auth.ts
+++ front/cypress/support/auth.ts
@@ -61,10 +61,10 @@ export const confirmEmail = (cy: Cypress.Chainable) => {
 
 export const confirmPhone = (cy: Cypress.Chainable) => {
   cy.dataCy('phone-code-input').should('exist');
-  // Codes are created and sent asynchronously,
-  // so if we enter the code too quickly it will be invalid (since it is not yet created).
-  // Therefore we wait a bit before entering the code.
-  cy.wait(10000);
+  // Codes are created and sent asynchronously (by the `que` worker), so if we enter the
+  // code too quickly it is not there yet and gets rejected. Que picks the job up over
+  // LISTEN/NOTIFY, so this is a matter of milliseconds in practice.
+  cy.wait(3000);
   cy.dataCy('phone-code-input').find('input').type('123456');
   cy.dataCy('phone-confirm-button').click();
 };
