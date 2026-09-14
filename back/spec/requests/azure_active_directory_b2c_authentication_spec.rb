b330ca15b111cabea2b73983f40632f93cb0a101
luucvanderzee <luuc_v_d_zee@hotmail.com>
2026-09-14 13:45:04 -0300

    Fix azure b2c spec

--- back/spec/requests/azure_active_directory_b2c_authentication_spec.rb
+++ back/spec/requests/azure_active_directory_b2c_authentication_spec.rb
@@ -63,11 +63,11 @@ context 'Azure AD B2C authentication' do
       verified: false,
       first_name: 'Alexander',
       last_name: 'CitizenLab',
-      email: nil,
-      new_email: 'alexander@citizenlab.co',
+      email: 'alexander@citizenlab.co',
+      new_email: nil,
       locale: 'en'
     })
-    expect(user.confirmation_required?).to be(true)
+    expect(user.confirmation_required?).to be(false)
   end
 
   it 'successfully authenticates new user' do
