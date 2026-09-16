6103c0466d89c713a987bdb3bb0ff7c0a01b2a0d
stephanie rousset <strabon@club-internet.fr>
2026-09-16 14:23:20 +0200

    fix: update condition in apply_projects_listed_scope method

--- back/app/controllers/web_api/v1/events_controller.rb
+++ back/app/controllers/web_api/v1/events_controller.rb
@@ -211,7 +211,9 @@ class WebApi::V1::EventsController < ApplicationController
     # If we do not filter by project_ids, we hide events from unlisted projects
     # This way, when people list all events, they don't see events from unlisted projects
     # But if someone filters by an unlisted project we do want to show those events
-    if params[:project_ids].blank?
+    # Same when filtering by attendee_id: the scope is already limited to that user's
+    # own registrations, and EventPolicy::Scope has checked the caller may see them
+    if params[:project_ids].blank? && params[:attendee_id].blank?
       # This 'show_unlisted_events_user_can_moderate' flag is needed in e.g.
       # the case of smart groups, where a moderator should be able to see unlisted
       # events of projects that they can moderate, but not other unlisted events.
