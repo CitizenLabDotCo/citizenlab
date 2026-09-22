d99c46e6cae1695a218d7369c48c8ac75ffdab6a
EdwinKato <katedwin88@gmail.com>
2026-09-22 17:33:30 +0300

    [TAN-8563] Add a predicate for an admin's own early access opt-ins

--- back/app/policies/user_policy.rb
+++ back/app/policies/user_policy.rb
@@ -122,6 +122,10 @@ class UserPolicy < ApplicationPolicy
     !!((user && (instance&.id == user.id || user.admin?)) || instance&.invite_pending?)
   end
 
+  def own_early_access_features?
+    !!user && admin? && record.id == user.id
+  end
+
   def permitted_attributes_for_create
     %i[email locale]
   end
@@ -134,7 +138,7 @@ class UserPolicy < ApplicationPolicy
     # avatar is allowed even if the feature "user_avatars" is not activated to allow
     # users to remove their avatar.
     shared = [:first_name, :last_name, :password, :avatar, :locale, { onboarding: [:topics_and_areas], custom_field_values: allowed_custom_field_keys, bio_multiloc: CL2_SUPPORTED_LOCALES }]
-    shared += [{ early_access_features: [] }] if admin? && record.id == user.id
+    shared += [{ early_access_features: [] }] if own_early_access_features?
     attributes = admin? ? shared + [roles: %i[type project_id project_folder_id project_reviewer]] : shared
     attributes - verification_service.locked_attributes(record) # locked attributes cannot be updated
   end
