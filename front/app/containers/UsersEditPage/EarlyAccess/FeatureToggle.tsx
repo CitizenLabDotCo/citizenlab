135e495490a362d9a4bf7d2ee4c50e0f01b7b70a
EdwinKato <katedwin88@gmail.com>
2026-09-22 17:33:44 +0300

    [TAN-8563] Let an early access toggle be disabled and use the fontWeight prop

--- front/app/containers/UsersEditPage/EarlyAccess/FeatureToggle.tsx
+++ front/app/containers/UsersEditPage/EarlyAccess/FeatureToggle.tsx
@@ -9,17 +9,18 @@ import {
 } from '@citizenlab/cl2-component-library';
 import { MessageDescriptor } from 'react-intl';
 
-import EarlyAccessBadge, {
-  EarlyAccessLevel,
-} from 'components/admin/EarlyAccessBadge';
+import { EarlyAccessLevel } from 'api/users/types';
+
+import EarlyAccessBadge from 'components/admin/EarlyAccessBadge';
 
 import { FormattedMessage } from 'utils/cl-intl';
 
 type Props = {
   title: MessageDescriptor;
   description: MessageDescriptor;
-  level: EarlyAccessLevel;
+  level?: EarlyAccessLevel;
   checked: boolean;
+  disabled?: boolean;
   onChange: () => void;
 };
 
@@ -28,24 +29,21 @@ const FeatureToggle = ({
   description,
   level,
   checked,
+  disabled,
   onChange,
 }: Props) => (
   <Box as="label" display="flex" mb="20px">
     <Box w="fit-content" display="flex" flexDirection="row-reverse">
       <Toggle
         checked={checked}
+        disabled={disabled}
         onChange={onChange}
         labelTextColor={colors.primary}
       />
     </Box>
     <Box display="flex" flexDirection="column" pl="1rem">
       <Box display="flex" alignItems="center" gap="8px">
-        <Title
-          color="primary"
-          fontSize="l"
-          my="0px"
-          style={{ fontWeight: 500 }}
-        >
+        <Title color="primary" fontSize="l" my="0px" fontWeight="semi-bold">
           <FormattedMessage {...title} />
         </Title>
         <EarlyAccessBadge level={level} />
