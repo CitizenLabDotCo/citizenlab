47a0c2168a8bd59b6f5554238da9e661197cb66f
EdwinKato <katedwin88@gmail.com>
2026-09-22 17:33:43 +0300

    [TAN-8563] Drop the level from the front end early access registry

--- front/app/containers/UsersEditPage/EarlyAccess/features.ts
+++ front/app/containers/UsersEditPage/EarlyAccess/features.ts
@@ -2,11 +2,8 @@ import { MessageDescriptor } from 'react-intl';
 
 import { TAppConfigurationSetting } from 'api/app_configuration/types';
 
-import { EarlyAccessLevel } from 'components/admin/EarlyAccessBadge';
-
 type EarlyAccessFeature = {
   name: TAppConfigurationSetting;
-  level: EarlyAccessLevel;
   title: MessageDescriptor;
   description: MessageDescriptor;
 };
