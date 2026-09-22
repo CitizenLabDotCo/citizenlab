e26d672dab173b91d935e7c343038c4255a607b7
EdwinKato <katedwin88@gmail.com>
2026-09-22 17:33:44 +0300

    [TAN-8563] Add copy for a failed early access change

--- front/app/containers/UsersEditPage/EarlyAccess/messages.ts
+++ front/app/containers/UsersEditPage/EarlyAccess/messages.ts
@@ -10,4 +10,9 @@ export default defineMessages({
     defaultMessage:
       'Try out features we are still finishing. What you switch on here only changes what you see, not what anyone else on the platform sees.',
   },
+  earlyAccessSaveError: {
+    id: 'app.containers.UsersEditPage.EarlyAccess.earlyAccessSaveError',
+    defaultMessage:
+      'We could not save that change. This feature may no longer be available to you. Reload the page and try again.',
+  },
 });
